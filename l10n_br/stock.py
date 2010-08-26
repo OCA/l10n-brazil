71f329ed13da240e12ee11422507a8216e3fc923
Renato Lima <renatonlima@gmail.com>
2010-08-25 21:14:17 -0300

    Convertidos para versão 6 os metodos onchange do parceiro, estabelecimento (Loja) e endereço de faturamento em sale.order, adicionado a categoria de operação fiscal e operação fiscal. criado os campos de posição categoria de operação fiscal, operação fiscal e posição fiscal nas movimentações de estoque para as notas geradas apartir das movimentações de estoque. Removido o campo state_nfe da account.invoice pois será alterado o workflow para as atividades da nfe serem incluido no workflow da invoice

---
+++ l10n_br/stock.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+#################################################################################
+#                                                                               #
+# Copyright (C) 2009  Renato Lima - Akretion                                    #
+#                                                                               #
+#This program is free software: you can redistribute it and/or modify           #
+#it under the terms of the GNU General Public License as published by           #
+#the Free Software Foundation, either version 3 of the License, or              #
+#(at your option) any later version.                                            #
+#                                                                               #
+#This program is distributed in the hope that it will be useful,                #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+#GNU General Public License for more details.                                   #
+#                                                                               #
+#You should have received a copy of the GNU General Public License              #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
+#################################################################################
+
+
+from osv import osv, fields
+
+
+class stock_picking(osv.osv):
+    _inherit = "stock.picking"
+    _description = "Picking List"
+    
+    _columns = {
+                'fiscal_operation_category_id': fields.many2one('l10n_br.fiscal.operation.category', 'Categoria', requeried=True),
+                'fiscal_operation_id': fields.many2one('l10n_br.fiscal.operation', 'Operação Fiscal', domain="[('fiscal_operation_category_id','=',fiscal_operation_category_id)]"),
+                'fiscal_position': fields.many2one('account.fiscal.position', 'Posição Fiscal', domain="[('fiscal_operation_id','=',fiscal_operation_id)]"),
+                }
+
+    #TODO Fazer a dedução da operação e posição fiscal ao mudar o parceiro
+    def onchange_partner_in(self, cr, uid, context=None, partner_id=None):
+        
+        result = super(stock_picking, self).onchange_partner_in(cr, uid, context, partner_id)
+        return result
+    
+    def action_invoice_create(self, cr, uid, ids, journal_id=False, group=False, type='out_invoice', context=None):
+        
+        result = super(stock_picking, self).action_invoice_create(cr, uid, ids, journal_id, group, type, context)
+        return result
+            
+stock_picking()
