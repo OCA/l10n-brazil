4636180a6c515a12ddc2a05f9e463296a1af5172
Raphaël Valyi <rvalyi@gmail.com>
2010-10-25 21:08:39 -0200

    [REF] extracted l10n_br_base, l10n_sale and l10n_stock from l10n_br; renamed l10n_br into 10n_br_account. BE CAREFFUL WITH MIGRATIONS, HACK YOUR ir.model.data, XML files and module dependencies first!!

---
+++ l10n_br_base/country.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+#################################################################################
+#                                                                               #
+# Copyright (C) 2009  Renato Lima - Akretion                                    #
+#                                                                               #
+#This program is free software: you can redistribute it and/or modify           #
+#it under the terms of the GNU General Public License as published by           #
+#the Free Software Foundation, either version 3 of the License, or              #
+#(at your option) any later version.                                            #
+#                                                                               #
+#This program is distributed in the hope that it will be useful,                #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+#GNU General Public License for more details.                                   #
+#                                                                               #
+#You should have received a copy of the GNU General Public License              #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
+#################################################################################
+
+from osv import osv, fields
+
+##############################################################################
+# País Personalizado
+##############################################################################
+class res_country(osv.osv):
+    _inherit = 'res.country'
+    _columns = {
+        'bc_code': fields.char('Cód. BC', size=5),
+    }
+res_country()
+
+##############################################################################
+# Estado Personalizado
+##############################################################################
+class res_country_state(osv.osv):
+    _inherit = 'res.country.state'
+    _columns = {
+        'ibge_code': fields.char('Cód. IBGE', size=2),
+    }
+res_country_state()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
