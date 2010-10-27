45458304255ac1b5052c0286c2006068bcb39d66
Renato Lima <renatonlima@gmail.com>
2010-10-27 11:08:56 -0200

    Incluido as mudanças recentes da localização, corrigido dependencias, incluido o modulo l10n_br_purchase, desmenbrado o modulo l10n_br_data em l10n_br_data_base e l10n_br_data_account

---
+++ l10n_br_purchase/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+#################################################################################
+#                                                                               #
+# Copyright (C) 2009  Renato Lima - Akretion                                    #
+#                                                                               #
+#This program is free software: you can redistribute it and/or modify           #
+#it under the terms of the GNU General Public License as published by           #
+#the Free Software Foundation, either version 3 of the License, or              #
+#(at your option) any later version.                                            #
+#                                                                               #
+#This program is distributed in the hope that it will be useful,                #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+#GNU General Public License for more details.                                   #
+#                                                                               #
+#You should have received a copy of the GNU General Public License              #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
+#################################################################################
+
+{
+    'name' : 'Brazilian Localization Purchase',
+    'description' : 'Brazilian Localization for Purchase',
+    'category' : 'Localisation/Account Charts',
+    'author' : 'OpenERP Brasil',
+    'website' : 'http://openerpbrasil.org',
+    'version' : '0.6',
+    'depends' : [
+		'account_fiscal_position_rule_purchase',
+		],
+    'init_xml': [
+		'data/l10n_br_purchase_data.xml',
+		],
+    'update_xml' : [
+        'purchase_view.xml',
+        'security/ir.model.access.csv',
+        'security/l10n_br_purchase_security.xml',
+    ],
+    'installable': True
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
