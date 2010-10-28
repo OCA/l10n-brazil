66360cc7c7ea7b93a61f6643bdc9113c3755ffb9
Renato Lima <renatonlima@gmail.com>
2010-10-28 02:05:09 -0200

    Adicionado o modulo l10n_br_product

---
+++ l10n_br_product/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+#################################################################################
+#                                                                               #
+# Copyright (C) 2009  Renato Lima - Akretion                                    #
+#                                                                               #
+#This program is free software: you can redistribute it and/or modify           #
+#it under the terms of the GNU General Public License as published by           #
+#the Free Software Foundation, either version 3 of the License, or              #
+#(at your option) any later version.                                            #
+#                                                                               #
+#This program is distributed in the hope that it will be useful,                #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+#GNU General Public License for more details.                                   #
+#                                                                               #
+#You should have received a copy of the GNU General Public License              #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
+#################################################################################
+
+
+{
+    'name': 'l10n_br Produto',
+    'version': '0.1',
+    'category': 'Generic Modules',
+    'description': """
+    Modulo de Produtos
+    """,
+    'author': 'OpenERP Brasil',
+    'website': 'http://www.openerpbrasil.org',
+    'depends': ['l10n_br_account'],
+    'init_xml': [],
+    'update_xml': ['product_view.xml'],
+    'test': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
