6e4dbffc8b08c21950dd3945a9021a6afa3320ad
Renato Lima <renatonlima@gmail.com>
2010-11-29 17:30:15 -0200

    Adicionado integração com NFe v2.00 feita através de arquivos XML ou TXT para ser importado no Emissor de NFe e modulo l10n_br_fp_rule_sale_link para resolver as compatibilidades

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -38,6 +38,7 @@
 		'partner_view.xml',
 		'account_invoice_view.xml',
         'account_invoice_workflow.xml',
+        'wizard/l10n_br_account_nfe_export_view.xml',
         'account_view.xml',
 		'account_fiscal_position_rule_view.xml',
         'security/ir.model.access.csv',
