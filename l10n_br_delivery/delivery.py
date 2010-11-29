883f2b6877fbf84448014f952822d5e0c01e5b8a
Renato Lima <renatonlima@gmail.com>
2010-11-29 17:30:15 -0200

    Adicionado integração com NFe v2.00 feita através de arquivos XML ou TXT para ser importado no Emissor de NFe e modulo l10n_br_fp_rule_sale_link para resolver as compatibilidades

--- l10n_br_delivery/delivery.py
+++ l10n_br_delivery/delivery.py
@@ -17,18 +17,42 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
-import time
 from osv import fields,osv
-from tools.translate import _
 
 class delivery_carrier(osv.osv):
     _inherit = "delivery.carrier"
 
     _columns = {
         'antt_code': fields.char('Codigo ANTT', size=32),
+ #       'vehicle_ids': fields.one2many('l10n_br_delivey.carrier.vehicle', 'carrier_id', 'Vehicles'),
     }
 
 delivery_carrier()
+
+class delivery_carrier_vehicle(osv.osv):    
+    
+    _name = "l10n_br_delivey.carrier.vehicle"
+    _description = "Veiculos das transportadoras"
+    
+    _columns = {
+        'name': fields.char('Nome', size=32),
+        'description': fields.char('Descrição', size=132),
+        'plate': fields.char('Placa', size=7),
+        'driver': fields.char('Condudor', size=64),
+        'antt_code': fields.char('Codigo ANTT', size=32),
+        'country_id': fields.many2one('res.country', 'País'),
+        'state_id': fields.many2one('res.country.state', 'Municipio', domain="[('country_id','=',country_id)]"),
+        'city_id': fields.many2one('l10n_br_base.city', 'Municipio', domain="[('state_id','=',state_id)]"),
+        'active': fields.boolean('Ativo'),
+        'manufacture_year': fields.char('Ano de Fabricação', size=4),
+        'model_year': fields.char('Ano do Modelo', size=4),
+        'type': fields.selection([('bau','Caminhão Baú')], 'Ano do Modelo'),
+        'carrier_id': fields.many2one('delivery.carrier', 'Carrier', select=True, required=True, ondelete='cascade'),        
+    }
+
+delivery_carrier_vehicle()
+
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
