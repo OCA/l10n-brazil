7a65ddf79fb33c8677d554e64155130c7fc4750a
Renato Lima <renatonlima@gmail.com>
2010-12-09 05:16:06 -0200

    Adicionado customizações nos calculos dos tributos e beneficios fiscais como redução de base de calculo e mudança das cst para cada imposto por operação, posição fiscal e NCM

--- l10n_br_delivery/delivery.py
+++ l10n_br_delivery/delivery.py
@@ -52,5 +52,56 @@ class l10n_br_delivery_carrier_vehicle(osv.osv):
 
 l10n_br_delivery_carrier_vehicle()
 
+
+class l10n_br_delivery_shipment(osv.osv):    
+    
+    _name = 'l10n_br_delivery.shipment'
+    
+    def _cal_weight(self, cr, uid, ids, name, args, context=None):
+        res = {}
+        uom_obj = self.pool.get('product.uom')
+        for picking in self.browse(cr, uid, ids, context):
+            total_weight = total_weight_net = 0.00
+
+            for move in picking.move_lines:
+                total_weight += move.weight
+                total_weight_net += move.weight_net
+
+            res[picking.id] = {
+                                'weight': total_weight,
+                                'weight_net': total_weight_net,
+                              }
+        return res
+
+    def _get_picking_line(self, cr, uid, ids, context=None):
+            result = {}
+            for line in self.pool.get('stock.move').browse(cr, uid, ids, context=context):
+                result[line.picking_id.id] = True
+            return result.keys()
+    
+    _columns = {
+        'code': fields.char('Nome', size=32),
+        'description': fields.char('Descrição', size=132),
+        'carrier_id': fields.many2one('delivery.carrier', 'Carrier', select=True, required=True),
+        'vehicle_id': fields.many2one('l10n_br_delivery.carrier.vehicle', 'Vehicle', select=True, required=True),
+        
+        #'weight': fields.function(_cal_weight, method=True, type='float', string='Weight', digits_compute= dp.get_precision('Stock Weight'), multi='_cal_weight',
+        #          store={
+        #         'stock.picking': (lambda self, cr, uid, ids, c={}: ids, ['move_lines'], 20),
+        #         'stock.move': (_get_picking_line, ['product_id','product_qty','product_uom','product_uos_qty'], 20),
+        #         }),
+        #'weight_net': fields.function(_cal_weight, method=True, type='float', string='Net Weight', digits_compute= dp.get_precision('Stock Weight'), multi='_cal_weight',
+        #          store={
+        #         'stock.picking': (lambda self, cr, uid, ids, c={}: ids, ['move_lines'], 20),
+        #         'stock.move': (_get_picking_line, ['product_id','product_qty','product_uom','product_uos_qty'], 20),
+        #         }),
+        'volume': fields.float('Volume'),
+        'carrier_tracking_ref': fields.char('Carrier Tracking Ref', size=32),
+        'number_of_packages': fields.integer('Number of Packages'),
+    }
+    
+        
+l10n_br_delivery_shipment()
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
