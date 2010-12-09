5827bbead6df30abd956d8a35cab85110e6bbeb8
Renato Lima <renato.lima@akretion.com>
2010-12-09 05:16:06 -0200

    Adicionado customizações nos calculos dos tributos e beneficios fiscais como redução de base de calculo e mudança das cst para cada imposto por operação, posição fiscal e NCM

--- l10n_br_account/account.py
+++ l10n_br_account/account.py
@@ -51,33 +51,59 @@ account_tax_template()
 class account_tax(osv.osv):
     _inherit = 'account.tax'
     
+    def get_precision_tax():
+        def change_digit_tax(cr):
+            res = pooler.get_pool(cr.dbname).get('decimal.precision').precision_get(cr, 1, 'Account')
+            return (16, res+2)
+        return change_digit_tax
+    
     _columns = {
         'tax_discount': fields.boolean('Descontar Imposto do Preço', help="Marque isso se este imposto é descontado no preço, exemplo: (ICMS, PIS e etc.)."),
+        'base_reduction': fields.float('Redução de Base', required=True, digits_compute=get_precision_tax(), help="Um percentual decimal em % entre 0-1."),
+    }
+    _defaults = {
+        'base_reduction': 0,
     }
     
     def compute_all(self, cr, uid, taxes, price_unit, quantity, address_id=None, product=None, partner=None):
         """
         RETURN: {
-                'total': 0.0,                # Total without taxes
-                'total_included: 0.0,        # Total with taxes
-                'total_tax_discount: 0.0,    # Total Tax Discounts
-                'taxes': []                  # List of taxes, see compute for the format
+                'total': 0.0,                 # Total without taxes
+                'total_included': 0.0,        # Total with taxes
+                'total_base': 0.0,            # Total Base
+                'total_tax_discount': 0.0,    # Total Tax Discounts
+                'taxes': []                   # List of taxes, see compute for the format
             }
         """
         
         tax_obj = self.pool.get('account.tax')        
         result = super(account_tax, self).compute_all(cr, uid, taxes, price_unit, quantity, address_id, product, partner)
         totaldc = 0.0 
+        totalbr = 0.0
+        obj_precision = self.pool.get('decimal.precision')
+        prec = obj_precision.precision_get(cr, uid, 'Account')
+        
         for tax in result['taxes']:
             tax_brw = tax_obj.browse(cr, uid, tax['id'])
+            
             if tax_brw.tax_discount:
+            
+                if tax_brw.base_reduction <> 0:
+                    tax['amount'] = round(tax['amount'] * (1 - tax_brw.base_reduction), prec)    
+
                 totaldc += tax['amount']
-        
+            
+            if tax_brw.amount <> 0:
+                tax['total_base'] = round(result['total'] * (1 - tax_brw.base_reduction), prec)
+            else:
+                tax['total_base'] = 0
+
         return {
             'total': result['total'],
             'total_included': result['total_included'],
+            #'total_base': totalbr,
+            'total_tax_discount': totaldc,
             'taxes': result['taxes'],
-            'total_tax_discount': totaldc
         }
     
 account_tax()
