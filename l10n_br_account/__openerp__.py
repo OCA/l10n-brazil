7e7b815dcd3880c2b1ccdcc9803274d0fa98c072
Raphaël Valyi <rvalyi@gmail.com>
2010-12-31 11:21:46 -0200

    [FIX] those modules don't define any new chart of account. Since OpenERP use the module category to detect if a module brings a new chart, we should change it here

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -20,7 +20,7 @@
 {
     'name' : 'Brazilian Localization',
     'description' : 'Brazilian Localization',
-    'category' : 'Localisation/Account Charts',
+    'category' : 'Localisation',
     'author' : 'OpenERP Brasil',
     'website' : 'http://openerpbrasil.org',
     'version' : '0.6',
