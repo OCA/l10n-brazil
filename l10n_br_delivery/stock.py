dc903e06242f679a03b85fb5f965c9c07ba72fed
Renato Lima <renatonlima@gmail.com>
2011-01-14 13:12:37 -0200

    l10n_br_delivery - Adicionado campos Tipo do Frete, Veículo, Total do Frete na nota fiscal e o Tipo do Frete na Ordem de Entrega. l10n_br_account - Adicionado nas linhas da notas fiscal detalhamento dos impostos ICMS, ICMS ST, IPI, PIS e COFINS, alterado a rotina de exportação da notas fiscais para o emissor de nfe, para sair a observação da NFE, dados do veículos e tipo do frete. l10n_br_purchase - Alterado a rotina da geração da nota fiscal de entrada, pois agora o relacionado entre ordem de compra e nota fiscal é um many2many.

--- l10n_br_delivery/stock.py
+++ l10n_br_delivery/stock.py
@@ -24,6 +24,7 @@ class stock_picking(osv.osv):
     _description = "Picking List"
     _columns = {
         'vehicle_id': fields.many2one('l10n_br_delivery.carrier.vehicle', 'Veículo'),
+        'incoterm': fields.many2one('stock.incoterms', 'Tipo do Frete', help="Incoterm which stands for 'International Commercial terms' implies its a series of sales terms which are used in the commercial transaction."),
     }
 
     def _invoice_line_hook(self, cr, uid, move_line, invoice_line_id):
@@ -36,7 +37,19 @@ class stock_picking(osv.osv):
     def _invoice_hook(self, cr, uid, picking, invoice_id):
         '''Call after the creation of the invoice'''
 
-        self.pool.get('account.invoice').write(cr, uid, invoice_id, {'fiscal_operation_category_id': picking.fiscal_operation_category_id.id, 'fiscal_operation_id': picking.fiscal_operation_id.id, 'cfop_id': picking.fiscal_operation_id.cfop_id.id, 'fiscal_document_id': picking.fiscal_operation_id.fiscal_document_id.id, 'fiscal_position': picking.fiscal_position.id, 'carrier_id': picking.carrier_id, 'weight': picking.weight, 'weight_net': picking.weight_net, 'number_of_packages': picking.number_of_packages})
+        self.pool.get('account.invoice').write(cr, uid, invoice_id, {
+                                                                     'fiscal_operation_category_id': picking.fiscal_operation_category_id.id, 
+                                                                     'fiscal_operation_id': picking.fiscal_operation_id.id, 
+                                                                     'cfop_id': picking.fiscal_operation_id.cfop_id.id, 
+                                                                     'fiscal_document_id': picking.fiscal_operation_id.fiscal_document_id.id, 
+                                                                     'fiscal_position': picking.fiscal_position.id,
+                                                                     'carrier_id': picking.carrier_id.id,
+                                                                     'vehicle_id': picking.vehicle_id.id,
+                                                                     'incoterm': picking.incoterm.id,
+                                                                     'weight': picking.weight, 
+                                                                     'weight_net': picking.weight_net, 
+                                                                     'number_of_packages': picking.number_of_packages
+                                                                     })
 
         return super(stock_picking, self)._invoice_hook(cr, uid, picking, invoice_id)
 
