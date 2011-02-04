286e71d2782dd7115e86a5892385293522d0d9f5
Renato Lima <renatonlima@gmail.com>
2011-02-04 12:08:32 -0200

    l10n_br_stock, concertado bug ao criar nota fiscal apartir do picking #4

--- l10n_br_stock/stock.py
+++ l10n_br_stock/stock.py
@@ -117,12 +117,12 @@ class stock_picking(osv.osv):
     def _invoice_line_hook(self, cr, uid, move_line, invoice_line_id):
         '''Call after the creation of the invoice line'''
         
-        if not move_line.fiscal_operation_id:
-            raise osv.except_osv(_('Movimentação sem operação fiscal !'),_("Não existe operação fiscal para uma linha desta separação."))
+        if not move_line.sale_line_id.fiscal_operation_id:
+            raise osv.except_osv(_('Movimentação sem operação fiscal !'),_("Não existe operação fiscal para uma linha de vendas relacionada ao produto %s .") % (move_line.product_id.name))
         
-        obj_fo = self.pool.get('l10n_br_account.fiscal.operation').browse(cr, uid, move_line.fiscal_operation_id.id)
+        obj_fo = self.pool.get('l10n_br_account.fiscal.operation').browse(cr, uid, move_line.sale_line_id.fiscal_operation_id.id)
 
-        self.pool.get('account.invoice.line').write(cr, uid, invoice_line_id, {'cfop_id': obj_fo.cfop_id.id, 'fiscal_operation_category_id': move_line.fiscal_operation_category_id.id ,'fiscal_operation_id': move_line.fiscal_operation_id.id})
+        self.pool.get('account.invoice.line').write(cr, uid, invoice_line_id, {'cfop_id': obj_fo.cfop_id.id, 'fiscal_operation_category_id': move_line.sale_line_id.fiscal_operation_category_id.id ,'fiscal_operation_id': move_line.sale_line_id.fiscal_operation_id.id})
 
         return super(stock_picking, self)._invoice_line_hook(cr, uid, move_line, invoice_line_id)
 
@@ -143,12 +143,3 @@ class stock_picking(osv.osv):
         return super(stock_picking, self)._invoice_hook(cr, uid, picking, invoice_id)
 
 stock_picking()
-
-class stock_move(osv.osv):
-    _inherit = 'stock.move'
-    _columns = {
-                'fiscal_operation_category_id': fields.many2one('l10n_br_account.fiscal.operation.category', 'Categoria'),
-                'fiscal_operation_id': fields.many2one('l10n_br_account.fiscal.operation', 'Operação Fiscal', domain="[('fiscal_operation_category_id','=',fiscal_operation_category_id)]"),
-                }
-
-stock_move()
