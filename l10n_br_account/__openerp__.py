b5bd3fd0a4b39e7f8d8b594b4a50068604f018af
Renato Lima <renato.lima@akretion.com>
2011-04-12 19:52:47 -0300

    l10n_br_account - Resolução da nota fiscal de serviço.

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -21,7 +21,7 @@
     'name' : 'Brazilian Localization',
     'description' : 'Brazilian Localization',
     'category' : 'Localisation',
-    'author' : 'OpenERP Brasil',
+    'author' : 'Akretion, OpenERP Brasil',
     'website' : 'http://openerpbrasil.org',
     'version' : '0.6',
     'depends' : [
