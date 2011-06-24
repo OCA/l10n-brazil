dc3db00130d6d1bd33e413c2c945648152e371b5
Renato Lima <renatonlima@gmail.com>
2011-06-24 17:02:21 -0300

    [FIX] #2 change _make_invoice method to add service type code on invoices

--- l10n_br_sale/sale.py
+++ l10n_br_sale/sale.py
@@ -218,13 +218,10 @@ class sale_order(osv.osv):
         for inv in inv_obj.browse(cr, uid, inv_ids,context=None):
             
             service_type_id = False
-            comment = False
+            comment = ''
+            fiscal_type = ''
             
-            if order.fiscal_operation_id.inv_copy_note:
-               comment = order.fiscal_operation_id.note
-            
-            if order.note:
-                comment += ' - ' + order.note
+
             
             inv_line_ids = map(lambda x: x.id, inv.invoice_line)
             
@@ -237,14 +234,27 @@ class sale_order(osv.osv):
                                                                         'cfop_id': (order_line.fiscal_operation_id and order_line.fiscal_operation_id.cfop_id.id) or (order.fiscal_operation_id and order.fiscal_operation_id.cfop_id.id) or False})
                     
                     if order_line.product_id.fiscal_type == 'service':
+                        fiscal_operation_category_id = order_line.fiscal_operation_category_id or order.fiscal_operation_category_id or False
+                        fiscal_operation_id = order_line.fiscal_operation_id or order.fiscal_operation_id or False
+                        #Em quanto não tem as posições fiscais na linha coloca falso na nota de serviço
+                        fiscal_position = False
                         service_type_id = (order_line.fiscal_operation_id and order_line.fiscal_operation_id.service_type_id.id) or (order.fiscal_operation_id and order.fiscal_operation_id.service_type_id.id) or False
-        
-            inv_l10n_br = {'fiscal_operation_category_id': order.fiscal_operation_category_id.id, 
-                           'fiscal_operation_id': order.fiscal_operation_id.id, 
+                        fiscal_type = order_line.product_id.fiscal_type
+            
+            if fiscal_operation_id or order.fiscal_operation_id.inv_copy_note:
+               comment = fiscal_operation_id and fiscal_operation_id.note or ''
+            
+            if order.note:
+                comment += ' - ' + order.note
+            
+            inv_l10n_br = {'fiscal_operation_category_id': fiscal_operation_category_id and fiscal_operation_category_id.id, 
+                           'fiscal_operation_id': fiscal_operation_id and fiscal_operation_id.id, 
                            'cfop_id': order.fiscal_operation_id.cfop_id and order.fiscal_operation_id.cfop_id.id, 
                            'fiscal_document_id': order.fiscal_operation_id.fiscal_document_id.id, 
                            'document_serie_id': fiscal_document_serie_ids[0].id,
                            'service_type_id': service_type_id,
+                           'fiscal_type': fiscal_type or 'product',
+                           'fiscal_position': fiscal_position,
                            'comment': comment,
                            }
             
