2975184f6185bf2d59a73cf594af6f2bf6613202
Renato Lima <renato.lima@akretion.com>
2011-06-29 01:52:23 -0300

    l10n_br_crm - finalização da localização do módulo crm na localização brasileira

--- l10n_br_crm/__init__.py
+++ l10n_br_crm/__init__.py
@@ -17,7 +17,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
-import crm
+import crm_lead
 import wizard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
