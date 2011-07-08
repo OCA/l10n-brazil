13854b405089d585d820ccbd0a00390fe52dc823
Renato Lima <renatonlima@gmail.com>
2011-07-08 16:34:25 -0300

    l10n_br_purchase: incluido a categoria de operação fiscal padrão de compras no cadastro da empresa para que ao digitar um pedido de compras já venha como padrão.

--- l10n_br_purchase/__init__.py
+++ l10n_br_purchase/__init__.py
@@ -17,6 +17,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
+import res_company
 import purchase
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
