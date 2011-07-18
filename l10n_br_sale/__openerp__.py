ee8295799ac8897cb91de2103e3517f750a83df8
Renato Lima <renatonlima@gmail.com>
2011-07-18 04:16:53 -0300

    deixar explicito a licença da localização

--- l10n_br_sale/__openerp__.py
+++ l10n_br_sale/__openerp__.py
@@ -21,7 +21,7 @@
     'name' : 'Brazilian Localization Sale',
     'description' : 'Brazilian Localization for Sale',
     'category' : 'Localisation',
-    'license': 'Affero GPL-3',
+    'license': 'AGPL-3',
     'author' : 'Akretion, OpenERP Brasil',
     'website' : 'http://openerpbrasil.org',
     'version' : '0.6',
