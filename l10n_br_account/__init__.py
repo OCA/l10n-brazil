e6d8c26fd3f265efb21296f1addfc3030b56cde7
Renato Lima <renatonlima@gmail.com>
2011-11-11 01:55:34 -0200

    l10n_br_account e l10n_br_sale: migração dos modulos para a versão trunk do OpenERP

--- l10n_br_account/__init__.py
+++ l10n_br_account/__init__.py
@@ -21,7 +21,7 @@ import l10n_br_account
 import res_company
 import partner
 import account
-import invoice
+import account_invoice
 import product
 import account_fiscal_position_rule
 import wizard
