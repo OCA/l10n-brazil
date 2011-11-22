c7679178fb1fe7ec155806c6b9bfa464b70185d5
Renato Lima <renatonlima@gmail.com>
2011-11-22 20:53:38 -0200

    l10n_br_accounts e l10n_br_base: atualização do estilo do código e comentário de licenciamento

--- l10n_br_base/l10n_br_base.py
+++ l10n_br_base/l10n_br_base.py
@@ -11,41 +11,41 @@
 #This program is distributed in the hope that it will be useful,                #
 #but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU General Public License for more details.                                   #
+#GNU Affero General Public License for more details.                            #
 #                                                                               #
-#You should have received a copy of the GNU General Public License              #
+#You should have received a copy of the GNU Affero General Public License       #
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
 from osv import osv, fields
 
-#################################################################################
-# Municipios e Códigos do IBGE
-#################################################################################
 class l10n_br_base_city(osv.osv):
+
     _name = 'l10n_br_base.city'
     _description = 'Municipios e Códigos do IBGE'
+
     _columns = {
-        'name': fields.char('Nome', size=64, required=True),
-        'state_id': fields.many2one('res.country.state', 'Estado', required=True),
-        'ibge_code': fields.char('Codigo IBGE', size=7),
-    }
+                'name': fields.char('Nome', size=64, required=True),
+                'state_id': fields.many2one('res.country.state', 'Estado', required=True),
+                'ibge_code': fields.char('Codigo IBGE', size=7),
+               }
+
 l10n_br_base_city()
 
-#################################################################################
-# CEP - Código de endereçamento Postal
-#################################################################################
 class l10n_br_base_cep(osv.osv):
+
     _name = 'l10n_br_base.cep'
     _rec_name = 'code'
+
     _columns = {
-        'code': fields.char('CEP', size=8, required=True),
-        'street_type': fields.char('Tipo', size=26),
-        'street': fields.char('Logradouro', size=72),
-        'district': fields.char('Bairro', size=72),
-        'state_id': fields.many2one('res.country.state', 'Estado', required=True),
-        'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Cidade', required=True, domain="[('state_id','=',state_id)]"),
-    }
+                'code': fields.char('CEP', size=8, required=True),
+                'street_type': fields.char('Tipo', size=26),
+                'street': fields.char('Logradouro', size=72),
+                'district': fields.char('Bairro', size=72),
+                'state_id': fields.many2one('res.country.state', 'Estado', required=True),
+                'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Cidade', required=True, domain="[('state_id','=',state_id)]"),
+                }
+
 l10n_br_base_cep()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
