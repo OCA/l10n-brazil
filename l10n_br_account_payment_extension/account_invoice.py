91ca040881c1fe48a736bfa79a39dd8b9c8d40ec
Renato Lima <renatonlima@gmail.com>
2011-11-22 20:53:38 -0200

    l10n_br_accounts e l10n_br_base: atualização do estilo do código e comentário de licenciamento

--- l10n_br_account_payment_extension/account_invoice.py
+++ l10n_br_account_payment_extension/account_invoice.py
@@ -11,9 +11,9 @@
 #This program is distributed in the hope that it will be useful,                #
 #but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU General Public License for more details.                                   #
+#GNU Affero General Public License for more details.                            #
 #                                                                               #
-#You should have received a copy of the GNU General Public License              #
+#You should have received a copy of the GNU Affero General Public License       #
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
@@ -27,12 +27,15 @@ from tools.translate import _
 from datetime import datetime
 
 class account_invoice(osv.osv):
+    
     _inherit = 'account.invoice'
 
-    def onchange_partner_id(self, cr, uid, ids, type, partner_id,\
-            date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False, fiscal_operation_category_id=False):
+    def onchange_partner_id(self, cr, uid, ids, type, partner_id, \
+                            date_invoice=False, payment_term=False, partner_bank_id=False, \
+                            company_id=False, fiscal_operation_category_id=False):
 
-        result = super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id, date_invoice, payment_term, partner_bank_id, company_id)
+        result = super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id, 
+                                                                  date_invoice, payment_term, partner_bank_id, company_id)
         return result
     
 account_invoice()
