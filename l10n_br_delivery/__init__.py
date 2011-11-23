96c98e5de8b5b6f18cca51d3a73de2da958eb21c
Renato Lima <renatonlima@gmail.com>
2011-11-23 01:31:14 -0200

    alterado comentários dos arquivos *.py e mudado estilo de codificação para ficar mais limpo o código

--- l10n_br_delivery/__init__.py
+++ l10n_br_delivery/__init__.py
@@ -11,9 +11,9 @@
 #This program is distributed in the hope that it will be useful,                #
 #but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU General Public License for more details.                                   #
+#GNU Affero General Public License for more details.                            #
 #                                                                               #
-#You should have received a copy of the GNU General Public License              #
+#You should have received a copy of the GNU Affero General Public License       #
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
@@ -24,4 +24,3 @@ import invoice
 import stock
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
