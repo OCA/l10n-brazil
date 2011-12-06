9c254becc2f6efb053078285445749fcaca88cd7
Renato Lima <renatonlima@gmail.com>
2011-12-06 13:38:51 -0200

    l10n_br_data_zip: incluido o campo país nos arquivos csv de CEP, alterado o objeto l10n_br_base.cep para l10n_br_base.zip

--- l10n_br_base/l10n_br_base.py
+++ l10n_br_base/l10n_br_base.py
@@ -42,8 +42,11 @@ class l10n_br_base_zip(osv.osv):
                 'street_type': fields.char('Tipo', size=26),
                 'street': fields.char('Logradouro', size=72),
                 'district': fields.char('Bairro', size=72),
-                'state_id': fields.many2one('res.country.state', 'Estado', required=True),
-                'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Cidade', required=True, domain="[('state_id','=',state_id)]"),
+                'country_id': fields.many2one('res.country', 'Country'),
+                'state_id': fields.many2one("res.country.state", 'Estado', 
+                                            domain="[('country_id','=',country_id)]"),
+                'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Cidade', 
+                                                   required=True, domain="[('state_id','=',state_id)]"),
                 }
 
 l10n_br_base_zip()
