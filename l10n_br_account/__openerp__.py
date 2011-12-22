52374a41d487797bec08a4e8d48247ab253d906f
Renato Lima <renato.lima@akretion.com>
2011-12-22 01:23:57 -0200

    l10n_br_account e l10n_br_data_account: modificações para atender as necessidades de empresas com regime tributário simples nacional

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -35,10 +35,12 @@
                  ],
     'update_xml' : [
                     'account_view.xml',
+                    'account.account.template.csv',
+                    'account.tax.code.template.csv',
+                    'l10n_br_account_data.xml',
                     'account_fiscal_position_rule_view.xml',
                     'account_invoice_view.xml',
                     'account_invoice_workflow.xml',
-                    'l10n_br_account_data.xml',
                     'l10n_br_account_view.xml',
                     'partner_view.xml',
                     'product_view.xml',
