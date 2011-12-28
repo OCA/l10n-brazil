d466d7a28fe43093fafc24277e37850cb5fef3e9
Renato Lima <renato.lima@akretion.com>
2011-12-28 16:44:20 -0200

    l10n_br_stock: inclido o modulo l10n_br_account na dependencia do modulo, l10n_br_account: incluido no wizard de exportação o ambiente de produção/homologação da nfe

--- l10n_br_account/wizard/l10n_br_account_nfe_export.py
+++ l10n_br_account/wizard/l10n_br_account_nfe_export.py
@@ -32,7 +32,8 @@ class l10n_br_account_nfe_export(osv.osv_memory):
         'company_id': fields.many2one('res.company', 'Company'),
         'file_type': fields.selection([('xml','XML'),('txt','TXT')], 'Tipo do Arquivo'),
         'import_status_draft': fields.boolean('Importar NFs com status em rascunho'),
-        'state':fields.selection([('init','init'),('done','done')], 'state', readonly=True),
+        'state': fields.selection([('init','init'),('done','done')], 'state', readonly=True),
+        'nfe_environment': fields.selection([('1','Produção'),('2','Homologação')], 'Ambiente'),
     }
 
     _defaults = {
@@ -40,19 +41,20 @@ class l10n_br_account_nfe_export(osv.osv_memory):
         'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'account.invoice', context=c),
         'file_type': 'txt',
         'import_status_draft': False,
+        'nfe_environment': '1',
     }
 
     def nfe_export(self, cr, uid, ids, context=None):
         
         data = self.read(cr, uid, ids, [], context=context)[0]
-        
+        print data['nfe_environment']
         inv_obj = self.pool.get('account.invoice')
         inv_ids = inv_obj.search(cr, uid, [('state','=','sefaz_export'),('nfe_export_date','=',False),('company_id','=',data['company_id'][0]),('own_invoice','=',True)])
 
         if data['file_type'] == 'xml':
-            file = inv_obj.nfe_export_xml(cr, uid, inv_ids)
+            file = inv_obj.nfe_export_xml(cr, uid, inv_ids, data['nfe_environment'])
         else:
-            file = inv_obj.nfe_export_txt(cr, uid, inv_ids)
+            file = inv_obj.nfe_export_txt(cr, uid, inv_ids, data['nfe_environment'])
         file_total = file
         
         self.write(cr, uid, ids, {'file': base64.b64encode(file_total), 'state': 'done'}, context=context)
