34aabdbdbcc1b5ebd399bd8bbc537f91d6acc009
Renato Lima <renatonlima@gmail.com>
2011-12-28 17:05:02 -0200

    [FIX] l10n_br_account_payment: Corrigido __init__.py import invoice para import account_invoice

--- l10n_br_account_payment/__init__.py
+++ l10n_br_account_payment/__init__.py
@@ -17,7 +17,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
-import invoice
+import account_invoice
 import wizard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
