4e13673bc6f33ed0ab3e5b240dfb22d0488cd4fc
Renato Lima <renatonlima@gmail.com>
2012-04-11 19:03:52 -0300

    l10n_br_sale: customizado a tela de análise de vendas para incluir informações fiscais

--- l10n_br_sale/__init__.py
+++ l10n_br_sale/__init__.py
@@ -18,6 +18,7 @@
 #################################################################################
 
 import sale
+import report
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
