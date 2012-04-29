53b32a86c0f5b00675c77f47dc8037837b17f665
Vinicius Dittgen <vinipd@gmail.com>
2012-04-29 10:55:10 -0300

    limpa pep8 no arquivo l10n_br_account_nfe_export.py

--- l10n_br_account/wizard/l10n_br_account_nfe_export.py
+++ l10n_br_account/wizard/l10n_br_account_nfe_export.py
@@ -20,6 +20,7 @@
 from osv import osv, fields
 import base64
 
+
 class l10n_br_account_nfe_export(osv.osv_memory):
     """ Exportar Nota Fiscal Eletrônica """
 
@@ -30,35 +31,34 @@ class l10n_br_account_nfe_export(osv.osv_memory):
     _columns = {
         'file': fields.binary('Arquivo', readonly=True),
         'company_id': fields.many2one('res.company', 'Company'),
-        'file_type': fields.selection([('xml','XML'),('txt','TXT')], 'Tipo do Arquivo'),
+        'file_type': fields.selection([('xml', 'XML'), ('txt', 'TXT')], 'Tipo do Arquivo'),
         'import_status_draft': fields.boolean('Importar NFs com status em rascunho'),
-        'state': fields.selection([('init','init'),('done','done')], 'state', readonly=True),
-        'nfe_environment': fields.selection([('1','Produção'),('2','Homologação')], 'Ambiente'),
+        'state': fields.selection([('init', 'init'), ('done', 'done')], 'state', readonly=True),
+        'nfe_environment': fields.selection([('1', 'Produção'), ('2', 'Homologação')], 'Ambiente'),
     }
 
     _defaults = {
         'state': 'init',
-        'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'account.invoice', context=c),
+        'company_id': lambda self, cr, uid, c: self.pool.get('res.company')._company_default_get(cr, uid, 'account.invoice', context=c),
         'file_type': 'txt',
         'import_status_draft': False,
         'nfe_environment': '1',
     }
 
     def nfe_export(self, cr, uid, ids, context=None):
-        
         data = self.read(cr, uid, ids, [], context=context)[0]
 
         inv_obj = self.pool.get('account.invoice')
-        inv_ids = inv_obj.search(cr, uid, [('state','=','sefaz_export'),('nfe_export_date','=',False),('company_id','=',data['company_id'][0]),('own_invoice','=',True)])
+        inv_ids = inv_obj.search(cr, uid, [('state', '=', 'sefaz_export'), ('nfe_export_date', '=', False), ('company_id', '=', data['company_id'][0]), ('own_invoice', '=', True)])
 
         if data['file_type'] == 'xml':
             file = inv_obj.nfe_export_xml(cr, uid, inv_ids, data['nfe_environment'])
         else:
             file = inv_obj.nfe_export_txt(cr, uid, inv_ids, data['nfe_environment'])
         file_total = file
-        
+
         self.write(cr, uid, ids, {'file': base64.b64encode(file_total), 'state': 'done'}, context=context)
-        
+
         return False
 
 l10n_br_account_nfe_export()
