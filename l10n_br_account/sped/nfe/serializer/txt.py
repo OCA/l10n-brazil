fe9e9328b0889513ffc10ab2686141b87314321f
Renato Lima <renatonlima@gmail.com>
2012-10-11 11:16:13 -0300

    [FIX]

--- l10n_br_account/sped/nfe/serializer/txt.py
+++ l10n_br_account/sped/nfe/serializer/txt.py
@@ -17,7 +17,6 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
-print 'Aqui'
 import time
 from datetime import datetime
 import netsvc
