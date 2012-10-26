3505c19515c5cbf80d4b16f0279f6b8660aab19d
Renato Lima <renatonlima@gmail.com>
2012-10-25 22:26:45 -0200

    l10n_br_base: limpeza e formatação do código

--- l10n_br_base/country.py
+++ l10n_br_base/country.py
@@ -1,42 +1,36 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2009  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
 from osv import osv, fields
 
-class res_country(osv.osv):
-    
-    _inherit = 'res.country'
 
+class res_country(osv.osv):
+    _inherit = 'res.country'
     _columns = {
-                'bc_code': fields.char('Cód. BC', size=5),
-               }
+        'bc_code': fields.char('Cód. BC', size=5)}
 
 res_country()
 
-class res_country_state(osv.osv):
 
+class res_country_state(osv.osv):
     _inherit = 'res.country.state'
-    
     _columns = {
-                'ibge_code': fields.char('Cód. IBGE', size=2),
-               }
+        'ibge_code': fields.char('Cód. IBGE', size=2)}
 
 res_country_state()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
