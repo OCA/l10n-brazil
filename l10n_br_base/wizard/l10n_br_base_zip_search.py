59fa7aabc5f99b6bf8902a37c97409a79923b27b
Renato Lima <renato.lima@akretion.com>
2012-10-25 22:26:45 -0200

    l10n_br_base: limpeza e formatação do código

--- l10n_br_base/wizard/l10n_br_base_zip_search.py
+++ l10n_br_base/wizard/l10n_br_base_zip_search.py
@@ -1,58 +1,60 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2011  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+###############################################################################
+#                                                                             # 
+# Copyright (C) 2011  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
 import re
 import string
 
 from osv import osv, fields
 
+
 class l10n_br_base_zip_search(osv.osv_memory):
-    
     _name = 'l10n_br_base.zip.search'
     _description = 'Zipcode Search'
-
-    _inherit = "ir.wizard.screen"
-    
+    _inherit = 'ir.wizard.screen'
     _columns = {
-                'code': fields.char('CEP', size=8),
-                'street': fields.char('Logradouro', size=72),
-                'district': fields.char('Bairro', size=72),
-                'country_id': fields.many2one('res.country', 'Country'),
-                'state_id': fields.many2one("res.country.state", 'Estado',
-                                            domain="[('country_id','=',country_id)]"),
-                'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Cidade',
-                                                   domain="[('state_id','=',state_id)]"),
-                'zip_ids': fields.many2many('l10n_br_base.zip','zip_search','zip_id', 
-                                                'zip_search_id', "CEP", readonly=True),
-                'state':fields.selection([('init','init'),('done','done')], 'state', readonly=True),
-                }
+        'code': fields.char('CEP', size=8),
+        'street': fields.char('Logradouro', size=72),
+        'district': fields.char('Bairro', size=72),
+        'country_id': fields.many2one('res.country', 'Country'),
+        'state_id': fields.many2one(
+            "res.country.state", 'Estado',
+            domain="[('country_id','=',country_id)]"),
+        'l10n_br_city_id': fields.many2one(
+            'l10n_br_base.city', 'Cidade',
+            domain="[('state_id','=',state_id)]"),
+        'zip_ids': fields.many2many('l10n_br_base.zip','zip_search',
+                                    'zip_id', 'zip_search_id', 'CEP',
+                                    readonly=True),
+        'state':fields.selection([('init','init'),
+                                  ('done','done')], 'state', readonly=True)}
     
     _defaults = {
-                'state': 'init'
-                }
-    
+        'state': 'init'}
+
     def default_get(self, cr, uid, fields_list, context=None):
         
         if context is None:
             context = {}
         
-        data = super(l10n_br_base_zip_search, self).default_get(cr, uid, fields_list, context)
+        data = super(l10n_br_base_zip_search, self).default_get(
+            cr, uid, fields_list, context)
+        
         data['code'] = context.get('zip', False)
         data['street'] = context.get('street', False)
         data['district'] = context.get('district', False)
@@ -131,5 +133,3 @@ class l10n_br_base_zip_search(osv.osv_memory):
         return {'type': 'ir.actions.act_window_close'}
     
 l10n_br_base_zip_search()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
