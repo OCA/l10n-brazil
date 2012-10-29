21913c80b5114c85fd86baf014fd822ba33b2260
Renato Lima <renatonlima@gmail.com>
2012-10-29 19:04:42 -0200

    l10n_br_base: limpeza e formatação do código

--- l10n_br_base/__openerp__.py
+++ l10n_br_base/__openerp__.py
@@ -1,21 +1,21 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2009  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
 {
     'name' : 'Brazilian Localization Base',
@@ -31,20 +31,18 @@
                 'l10n_br_base.city.csv',
                 ],
     'update_xml' : [
-                'l10n_br_base_data.xml',
-        		'l10n_br_base_view.xml',
-        		'res_country_view.xml',
-                'wizard/l10n_br_base_zip_search_view.xml',
-        		'partner_view.xml',
-                'security/ir.model.access.csv',
-                'security/l10n_br_base_security.xml',
-    ],
+                    'l10n_br_base_data.xml',
+                    'l10n_br_base_view.xml',
+                    'res_country_view.xml',
+                    'wizard/l10n_br_base_zip_search_view.xml',
+                    'partner_view.xml',
+                    'security/ir.model.access.csv',
+                    'security/l10n_br_base_security.xml',
+                    ],
     'test': [
             'test/base_inscr_est_valid.yml',
             'test/base_inscr_est_invalid.yml',
-    ],
+            ],
     'demo_xml': ['l10n_br_base_demo.xml'],
     'installable': True
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
