601d5dc28fb5e65a2d2abc93f1bc9a8bf8bf0786
Renato Lima <renato.lima@akretion.com>
2012-10-30 13:11:12 -0200

    l10n_br_crm: limpeza e formatação do código

--- l10n_br_crm/crm_lead.py
+++ l10n_br_crm/crm_lead.py
@@ -1,34 +1,34 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2012  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+###############################################################################
+#                                                                             #
+# Copyright (C) 2012  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
 from osv import fields, osv
 from crm.crm import crm_case
+
 
 class crm_lead(crm_case, osv.osv):
     """ CRM Lead Case """
     _inherit = "crm.lead"
     _columns = {
-	            'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Municipio', 
-                                                   domain="[('state_id','=',state_id)]"),
-                'district': fields.char('Bairro', size=32),
-                'number': fields.char('Número', size=10),
-                }
-crm_lead()
+        'l10n_br_city_id': fields.many2one(
+            'l10n_br_base.city', 'Municipio',
+            domain="[('state_id','=',state_id)]"),
+        'district': fields.char('Bairro', size=32),
+        'number': fields.char('Número', size=10)}
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+crm_lead()
