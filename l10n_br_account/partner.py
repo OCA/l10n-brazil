da0183f140931527a1f3cb2184563e2e4ebe1f07
Renato Lima <renato.lima@akretion.com>
2012-11-02 01:58:09 -0200

    l10n_br_account: inserido novos campos no objeto posição fiscal e visões

--- l10n_br_account/partner.py
+++ l10n_br_account/partner.py
@@ -20,7 +20,23 @@
 from osv import osv, fields
 
 FISCAL_POSITION_COLUMNS = {
-    'cfop_id': fields.many2one('l10n_br_account.cfop', 'CFOP')}
+    'cfop_id': fields.many2one('l10n_br_account.cfop', 'CFOP'),
+    'fiscal_category_id': fields.many2one('l10n_br_account.fiscal.category',
+                                          'Categoria Fiscal'),
+    'fiscal_category_type': fields.related(
+        'fiscal_category_id', 'type', type='char', readonly=True,
+        relation='l10n_br_account.fiscal.category', store=True, string='Type'),
+    'fiscal_category_fiscal_type': fields.related(
+        'fiscal_category_id', 'fiscal_type', type='char', readonly=True,
+        relation='l10n_br_account.fiscal.category', store=True,
+        string='Fiscal Type'),
+    'refund_fiscal_category_id': fields.many2one(
+        'l10n_br_account.fiscal.category', 'Categoria Fiscal de Devolução'),
+    'inv_copy_note': fields.boolean('Copiar Observação na Nota Fiscal'),
+    'asset_operation': fields.boolean('Operação de Aquisição de Ativo',
+                                      help="Caso seja marcada essa opção,"
+                                      " será incluido o IPI na base de "
+                                      "calculo do ICMS.")}
 
 
 class account_fiscal_position_template(osv.osv):
@@ -30,11 +46,30 @@ class account_fiscal_position_template(osv.osv):
 account_fiscal_position_template()
 
 
+class account_fiscal_position_tax_template(osv.osv):
+    _inherit = 'account.fiscal.position.tax.template'
+
+    _columns = {
+        'tax_code_dest_id': fields.many2one('account.tax.code.template',
+                                            'Replacement Tax')}
+
+account_fiscal_position_tax_template()
+
+
 class account_fiscal_position(osv.osv):
     _inherit = 'account.fiscal.position'
     _columns = FISCAL_POSITION_COLUMNS
 
 account_fiscal_position()
+
+
+class account_fiscal_position_tax(osv.osv):
+    _inherit = 'account.fiscal.position.tax'
+    _columns = {
+        'tax_code_dest_id': fields.many2one('account.tax.code',
+                                            'Replacement Tax')}
+
+account_fiscal_position_tax()
 
 
 class res_partner(osv.osv):
