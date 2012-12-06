9f94e82d8d2283b69c8c42f27893e04b4b44c0df
Raphaël Valyi <rvalyi@gmail.com>
2012-12-06 02:50:45 +0000

    [MIGR][l10n_br_account] migrated, enabled again and cleaned up product view

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -44,7 +44,7 @@
                     'account_invoice_workflow.xml',
                     'l10n_br_account_view.xml',
                     'partner_view.xml',
-#                    'product_view.xml',
+                    'product_view.xml',
 #                    'res_company_view.xml',
 #                    'account_product_fiscal_classification_view.xml',
                     'security/ir.model.access.csv',
