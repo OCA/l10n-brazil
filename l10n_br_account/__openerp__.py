ec9d516b3c8d357952b98eb9cd4a9b718b52de6a
Raphaël Valyi <rvalyi@akretion.com>
2012-12-06 03:20:16 +0000

    [MIGR][FIX][l10n_br_account] fixed broken tax views; should also be backported; enabled account_product_fiscal_classification_view.xml again then

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -1,22 +1,22 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2009  Renato Lima - Akretion                                    #
-# Copyright (C) 2011  Vinicius Dittgen - PROGE, Leonardo Santagada - PROGE      #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+# Copyright (C) 2011  Vinicius Dittgen - PROGE, Leonardo Santagada - PROGE    #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
 {
     'name': 'Brazilian Localization',
@@ -45,8 +45,8 @@
                     'l10n_br_account_view.xml',
                     'partner_view.xml',
                     'product_view.xml',
-#                    'res_company_view.xml',
-#                    'account_product_fiscal_classification_view.xml',
+                    'res_company_view.xml',
+                    'account_product_fiscal_classification_view.xml',
                     'security/ir.model.access.csv',
                     'security/l10n_br_account_security.xml',
 #                    'wizard/l10n_br_account_nfe_export_view.xml',
@@ -66,4 +66,3 @@
     'installable': True,
     'auto_install': True,
 }
-
