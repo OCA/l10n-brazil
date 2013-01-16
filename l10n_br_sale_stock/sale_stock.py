820daa635ac92b786efa2ae2ca3586e0d2ff7521
Renato Lima <renatonlima@gmail.com>
2013-01-16 16:41:15 -0200

    [MIGR] finalização do módulo l10n_br_sale_stock e correções do l10n_br_account e l10n_br_account

--- l10n_br_sale_stock/sale_stock.py
+++ l10n_br_sale_stock/sale_stock.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2013  Raphaël Valyi - Akretion                                #
-# Copyright (C) 2013  Renato Lima - Akretion                                  #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+# Copyright (C) 2012  Raphaël Valyi - Akretion                                #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
@@ -18,23 +18,17 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-{
-    'name': 'l10n_br_sale_stock',
-    'description': 'Brazilian Localization for sale_stock_module',
-    'category': 'Localisation',
-    'license': 'AGPL-3',
-    'author': 'Akretion, OpenERPBrasil.org',
-    'website': 'http://openerpbrasil.org',
-    'version': '0.1',
-    'depends': [
-                'l10n_br_sale',
-                'sale_stock',
-               ],
-    'init_xml': [],
-    'update_xml': [
-                   'sale_stock_view.xml',
-                   ],
-    'demo_xml': [],
-    'installable': True,
-    'auto_install': True
-}
+import decimal_precision as dp
+from osv import fields, osv
+
+
+class sale_order(osv.Model):
+    _inherit = 'sale.order'
+
+    def _prepare_order_picking(self, cr, uid, order, context=None):
+        result = super(sale_order, self)._prepare_order_picking(cr, uid, order, context)
+        result['fiscal_category_id'] = order.fiscal_category_id and order.fiscal_category_id.id
+        result['fiscal_position'] = order.fiscal_position and order.fiscal_position.id
+        return result
+
+sale_order()
