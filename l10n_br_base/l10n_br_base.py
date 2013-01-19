1a65edcca617e188c1e912f25b92545a8d795e9c
Renato Lima <renato.lima@akretion.com>
2013-01-18 22:37:21 -0200

    Formatação do código dos modulos l10n_base e l10n_br_data_zip e removida classe não utilizada de ceps no modulo l10n_br_base

--- l10n_br_base/l10n_br_base.py
+++ l10n_br_base/l10n_br_base.py
@@ -33,24 +33,3 @@ class l10n_br_base_city(osv.Model):
                                     required=True),
         'ibge_code': fields.char('Codigo IBGE', size=7)
     }
-
-
-class l10n_br_base_zip(osv.Model):
-    """ Este objeto persiste todos os códigos postais que podem ser
-    utilizados para pesquisar e auxiliar o preenchimento dos endereços.
-    """
-    _name = 'l10n_br_base.zip'
-    _description = 'CEP'
-    _rec_name = 'code'
-    _columns = {
-        'code': fields.char('CEP', size=8, required=True),
-        'street_type': fields.char('Tipo', size=26),
-        'street': fields.char('Logradouro', size=72),
-        'district': fields.char('Bairro', size=72),
-        'country_id': fields.many2one('res.country', 'Country'),
-        'state_id': fields.many2one('res.country.state', 'Estado',
-                                    domain="[('country_id','=',country_id)]"),
-        'l10n_br_city_id': fields.many2one(
-            'l10n_br_base.city', 'Cidade',
-            required=True, domain="[('state_id','=',state_id)]")
-    }
