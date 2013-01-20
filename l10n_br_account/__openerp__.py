0836c8d5895ab469ce2a669ec3d49a698ed13612
Renato Lima <renatonlima@gmail.com>
2013-01-20 21:42:40 -0200

    [MIGR] l10n_br_account: assistentes de exportação de NFe

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -50,8 +50,8 @@
                     'account_product_fiscal_classification_view.xml',
                     'security/ir.model.access.csv',
                     'security/l10n_br_account_security.xml',
-#                    'wizard/l10n_br_account_nfe_export_view.xml',
-                    #'wizard/nfe_export_from_invoice_view.xml',
+                    'wizard/l10n_br_account_nfe_export_view.xml',
+                    'wizard/nfe_export_from_invoice_view.xml',
                     ],
     'demo_xml': [
                  'demo/account.account.csv',
