61cb6be7f09b57cf8be61e6f1e4800cc7f3cbb0d
Renato Lima <renato.lima@akretion.com>
2013-01-28 00:34:28 -0200

    l10n_br_account: atualizada a cst de origem de produtos

--- l10n_br_account/product.py
+++ l10n_br_account/product.py
@@ -32,12 +32,17 @@ class product_template(osv.Model):
         'is_on_service_invoice': fields.boolean('On Service Invoice?',
                                                 help="True if invoiced along "
                                                 "with service"),
-        'origin': fields.selection([('0', 'Nacional'),
-                                    ('1', 'Internacional'),
-                                    ('2', 'Inter. Adiquirido Internamente')],
-                                   'Origem')
+        'origin': fields.selection(
+            [('0', 'Nacional, exceto as indicadas nos códigos 3 a 5'),
+            ('1', 'Estrangeira - Importação direta, exceto a indicada no código 6'),
+            ('2', 'Estrangeira - Adquirida no mercado interno, exceto a indicada no código 7'),
+            ('3', 'Nacional, mercadoria ou bem com Conteúdo de Importação superior a  40% (quarenta por cento)'),
+            ('4', 'Nacional, cuja produção tenha sido feita em conformidade com os processos produtivos básicos de que tratam o Decreto-Lei nº 288/67, e as Leis nºs 8.248/91, 8.387/91, 10.176/01 e 11.484/07'),
+            ('5', 'Nacional, mercadoria ou bem com Conteúdo de Importação inferior ou igual a 40% (quarenta por cento)'),
+            ('6', 'Estrangeira - Importação direta, sem similar nacional, constante em lista de Resolução CAMEX'),
+            ('7', 'Estrangeira - Adquirida no mercado interno, sem similar nacional, constante em lista de Resolução CAMEX')],
+            'Origem')
     }
-
     _defaults = {
         'fiscal_type': 'product',
         'is_on_service_invoice': False,
