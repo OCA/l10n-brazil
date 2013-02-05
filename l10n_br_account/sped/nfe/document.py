b2899f449f2d5b6063c3fea7474022762741413b
Renato Lima <renatonlima@gmail.com>
2013-02-05 14:29:10 -0200

    [MIGR] adicionado arquivos no modulo 10n_br_account

---
+++ l10n_br_account/sped/nfe/document.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2013  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
+
+from osv import fields, osv
+from ..document import FiscalDocument
+
+
+class NFe200(FiscalDocument):
+
+    def __init__(self):
+        pass
+
+    def _serializer():
+        pass
+
+    def _deserializer():
+        pass
+
+    def get_xml():
+        pass
+
+    def set_xml():
+        pass
+
+    def get_txt():
+        pass
+
+    def set_xml():
+        pass
