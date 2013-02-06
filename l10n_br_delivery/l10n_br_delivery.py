1c63bc8356721b604a90f69dd8a7375185a2c3f8
Renato Lima <renatonlima@gmail.com>
2013-02-06 17:08:18 -0200

    [MIGR] migração do modulo l10n_br_delivery

--- l10n_br_delivery/l10n_br_delivery.py
+++ l10n_br_delivery/l10n_br_delivery.py
@@ -1,64 +1,68 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2010  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+###############################################################################
+#                                                                             #
+# Copyright (C) 2010  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
 
-from osv import fields,osv
+from osv import fields, osv
 
-class l10n_br_delivery_carrier_vehicle(osv.osv):    
-    
+
+class l10n_br_delivery_carrier_vehicle(osv.Model):
     _name = 'l10n_br_delivery.carrier.vehicle'
     _description = 'Veiculos das transportadoras'
-    
     _columns = {
-                'name': fields.char('Nome',required=True, size=32),
-                'description': fields.char('Descrição', size=132),
-                'plate': fields.char('Placa', size=7),
-                'driver': fields.char('Condudor', size=64),
-                'rntc_code': fields.char('Codigo ANTT', size=32),
-                'country_id': fields.many2one('res.country', 'País'),
-                'state_id': fields.many2one('res.country.state', 'Estado', domain="[('country_id','=',country_id)]"),
-                'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Municipio', domain="[('state_id','=',state_id)]"),
-                'active': fields.boolean('Ativo'),
-                'manufacture_year': fields.char('Ano de Fabricação', size=4),
-                'model_year': fields.char('Ano do Modelo', size=4),
-                'type': fields.selection([('bau','Caminhão Baú')], 'Ano do Modelo'),
-                'carrier_id': fields.many2one('delivery.carrier', 'Carrier', select=True, required=True, ondelete='cascade'),        
-                }
+        'name': fields.char('Nome', required=True, size=32),
+        'description': fields.char('Descrição', size=132),
+        'plate': fields.char('Placa', size=7),
+        'driver': fields.char('Condudor', size=64),
+        'rntc_code': fields.char('Codigo ANTT', size=32),
+        'country_id': fields.many2one('res.country', 'País'),
+        'state_id': fields.many2one(
+            'res.country.state', 'Estado',
+            domain="[('country_id', '=', country_id)]"),
+        'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Municipio',
+            domain="[('state_id','=',state_id)]"),
+        'active': fields.boolean('Ativo'),
+        'manufacture_year': fields.char('Ano de Fabricação', size=4),
+        'model_year': fields.char('Ano do Modelo', size=4),
+        'type': fields.selection([('bau', 'Caminhão Baú')], 'Ano do Modelo'),
+        'carrier_id': fields.many2one(
+            'delivery.carrier', 'Carrier', select=True,
+            required=True, ondelete='cascade'),
+    }
 
-l10n_br_delivery_carrier_vehicle()
 
-class l10n_br_delivery_shipment(osv.osv):    
-    
+class l10n_br_delivery_shipment(osv.Model):
     _name = 'l10n_br_delivery.shipment'
-
     _columns = {
-            'code': fields.char('Nome', size=32),
-            'description': fields.char('Descrição', size=132),
-            'carrier_id': fields.many2one('delivery.carrier', 'Carrier', select=True, required=True),
-            'vehicle_id': fields.many2one('l10n_br_delivery.carrier.vehicle', 'Vehicle', select=True, required=True),
-            'volume': fields.float('Volume'),
-            'carrier_tracking_ref': fields.char('Carrier Tracking Ref', size=32),
-            'number_of_packages': fields.integer('Number of Packages'),
-            }    
+        'code': fields.char('Nome', size=32),
+        'description': fields.char('Descrição', size=132),
+        'carrier_id': fields.many2one(
+            'delivery.carrier', 'Carrier', select=True, required=True),
+        'vehicle_id': fields.many2one(
+            'l10n_br_delivery.carrier.vehicle', 'Vehicle', select=True,
+            required=True),
+        'volume': fields.float('Volume'),
+        'carrier_tracking_ref': fields.char('Carrier Tracking Ref', size=32),
+        'number_of_packages': fields.integer('Number of Packages'),
+    }
 
     def _cal_weight(self, cr, uid, ids, name, args, context=None):
         res = {}
-        uom_obj = self.pool.get('product.uom')
+        #uom_obj = self.pool.get('product.uom')
         for picking in self.browse(cr, uid, ids, context):
             total_weight = total_weight_net = 0.00
 
@@ -77,7 +81,3 @@ class l10n_br_delivery_shipment(osv.osv):
             for line in self.pool.get('stock.move').browse(cr, uid, ids, context=context):
                 result[line.picking_id.id] = True
             return result.keys()
-
-l10n_br_delivery_shipment()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- l10n_br_delivery/sale.py
+++ l10n_br_delivery/sale.py
@@ -14,50 +14,38 @@
 #GNU Affero General Public License for more details.                          #
 #                                                                             #
 #You should have received a copy of the GNU Affero General Public License     #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        # 
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-import time 
-import netsvc
-import decimal_precision as dp
-from osv import fields, osv
-import pooler
-from tools import config
-from tools.translate import _
+from osv import osv
 
 
-class sale_order(osv.osv):
+class sale_order(osv.Model):
     _inherit = 'sale.order'
 
-    def action_invoice_create(self, cr, uid, ids, grouped=False,
-                              states=['confirmed', 'done', 'exception'],
-                              date_inv = False, context=None):
+    def _prepare_invoice(self, cr, uid, order, lines, context=None):
+        """Prepare the dict of values to create the new invoice for a
+           sale order. This method may be overridden to implement custom
+           invoice generation (making sure to call super() to establish
+           a clean extension chain).
 
-        result = super(sale_order, self).action_invoice_create(
-            cr, uid, ids, grouped, states, date_inv, context)
+           :param browse_record order: sale.order record to invoice
+           :param list(int) line: list of invoice line IDs that must be
+                                  attached to the invoice
+           :return: dict of value to create() the invoice
+        """
+        result = super(sale_order, self)._prepare_invoice(
+            cr, uid, order, lines, context)
 
-        if not result:
-            return result
-
-        for order in self.browse(cr, uid, ids):
-            for invoice in order.invoice_ids:
-                if invoice.state in ('draft'):
-                    self.pool.get('account.invoice').write(
-                        cr, uid, invoice.id, {
-                            'partner_shipping_id': order.partner_shipping_id.id,
-                            'carrier_id': order.carrier_id.id,
-                            'incoterm': order.incoterm and order.incoterm.id or False})
-
-        return result
-    
-    def action_ship_create(self, cr, uid, ids, *args):
-        result = super(sale_order, self).action_ship_create(cr, uid, ids, *args)
-
-        for order in self.browse(cr, uid, ids, context={}):
-            for picking in order.picking_ids:
-                self.pool.get('stock.picking').write(cr, uid, picking.id, {'incoterm': order.incoterm.id})
-
+        result['carrier_id'] = order.carrier_id.id,
+        result['incoterm'] = order.incoterm and order.incoterm.id or False
         return result
 
-sale_order()
+    def _prepare_order_picking(self, cr, uid, order, context=None):
+        result = super(sale_order, self)._prepare_order_picking(cr, uid,
+            order, context)
 
+        # TODO - Possível bug do modulo delivery do OpenERP
+        # porque esse campo já deveria ser copiado pelo módulo nativo delivery
+        result['incoterm'] = order.incoterm and order.incoterm.id or False
+        return result
