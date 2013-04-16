b9dd2b7ca5c087a7949e1a66860d92030c327560
Renato Lima <renato.lima@akretion.com>
2013-04-16 10:52:07 -0300

    [MERGE] migração do módulo l10n_br_account para versão 7.0, incluido tipo de serviço no cadastro de produtos

--- l10n_br_account/product.py
+++ l10n_br_account/product.py
@@ -17,31 +17,32 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-from osv import osv, fields
+from openerp.osv import orm, fields
 
 
-class product_template(osv.Model):
+class product_template(orm.Model):
     _inherit = 'product.template'
     _columns = {
         'fiscal_category_default_ids': fields.one2many(
             'l10n_br_account.product.category', 'product_tmpl_id',
-            'Categoria de Operação Fiscal Padrões'),
-        'fiscal_type': fields.selection([('product', 'Produto'),
-                                         ('service', 'Serviço')],
-                                        'Tipo Fiscal', requeried=True),
-        'is_on_service_invoice': fields.boolean('On Service Invoice?',
-                                                help="True if invoiced along "
-                                                "with service"),
+            u'Categoria de Operação Fiscal Padrões'),
+        'fiscal_type': fields.selection(
+            [('product', 'Produto'), ('service', 'Serviço')],
+            'Tipo Fiscal', requeried=True),
+        'is_on_service_invoice': fields.boolean(
+            'On Service Invoice?', help="True if invoiced along with service"),
         'origin': fields.selection(
-            [('0', 'Nacional, exceto as indicadas nos códigos 3 a 5'),
-            ('1', 'Estrangeira - Importação direta, exceto a indicada no código 6'),
-            ('2', 'Estrangeira - Adquirida no mercado interno, exceto a indicada no código 7'),
-            ('3', 'Nacional, mercadoria ou bem com Conteúdo de Importação superior a  40% (quarenta por cento)'),
-            ('4', 'Nacional, cuja produção tenha sido feita em conformidade com os processos produtivos básicos de que tratam o Decreto-Lei nº 288/67, e as Leis nºs 8.248/91, 8.387/91, 10.176/01 e 11.484/07'),
-            ('5', 'Nacional, mercadoria ou bem com Conteúdo de Importação inferior ou igual a 40% (quarenta por cento)'),
-            ('6', 'Estrangeira - Importação direta, sem similar nacional, constante em lista de Resolução CAMEX'),
-            ('7', 'Estrangeira - Adquirida no mercado interno, sem similar nacional, constante em lista de Resolução CAMEX')],
-            'Origem')
+            [('0', u'0 - Nacional, exceto as indicadas nos códigos 3 a 5'),
+            ('1', u'1 - Estrangeira - Importação direta, exceto a indicada no código 6'),
+            ('2', u'2 - Estrangeira - Adquirida no mercado interno, exceto a indicada no código 7'),
+            ('3', u'3 - Nacional, mercadoria ou bem com Conteúdo de Importação superior a  40% (quarenta por cento)'),
+            ('4', u'4 - Nacional, cuja produção tenha sido feita em conformidade com os processos produtivos básicos de que tratam o Decreto-Lei nº 288/67, e as Leis nºs 8.248/91, 8.387/91, 10.176/01 e 11.484/07'),
+            ('5', u'5 - Nacional, mercadoria ou bem com Conteúdo de Importação inferior ou igual a 40% (quarenta por cento)'),
+            ('6', u'6 - Estrangeira - Importação direta, sem similar nacional, constante em lista de Resolução CAMEX'),
+            ('7', u'7 - Estrangeira - Adquirida no mercado interno, sem similar nacional, constante em lista de Resolução CAMEX')],
+            'Origem'),
+        'service_type_id': fields.many2one(
+            'l10n_br_account.service.type', u'Tipo de Serviço'),
     }
     _defaults = {
         'fiscal_type': 'product',
@@ -50,7 +51,7 @@ class product_template(osv.Model):
     }
 
 
-class l10n_br_account_product_fiscal_category(osv.Model):
+class l10n_br_account_product_fiscal_category(orm.Model):
     _name = 'l10n_br_account.product.category'
     _columns = {
         'fiscal_category_source_id': fields.many2one(
