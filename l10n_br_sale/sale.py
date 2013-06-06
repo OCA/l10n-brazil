a5f22e8e38fc223ec10b2b353ed8267cd6dc5081
Danimar Ribeiro <danimaribeiro@gmail.com>
2013-06-06 00:04:29 -0300

    Fixed problem converting prospects to sales order.

--- l10n_br_sale/sale.py
+++ l10n_br_sale/sale.py
@@ -73,9 +73,9 @@ class sale_order(orm.Model):
     def _default_fiscal_category(self, cr, uid, context=None):
         shop_id = context.get("shop_id", self.default_get(
             cr, uid, ["shop_id"], context)["shop_id"])
-        return  self.pool.get("sale.shop").read(
-            cr, uid, [shop_id], ["default_fc_id"])[0]["default_fc_id"]
-
+        value = self.pool.get("sale.shop").read(cr, uid, [shop_id], ["default_fc_id.id"])
+        return  value[0]["id"]
+    
     _defaults = {
         'fiscal_category_id': _default_fiscal_category,
     }
