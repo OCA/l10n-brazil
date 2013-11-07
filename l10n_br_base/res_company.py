6b3a2e0156ab2134162148154c86ef0a1afaf482
Renato Lima <renato.lima@akretion.com>
2013-11-07 18:26:23 -0200

    Correções na formação do código, adicionado a mascara do campo zip na empresa, trocado botão de busca de cep no parceiro e no crm

--- l10n_br_base/res_company.py
+++ l10n_br_base/res_company.py
@@ -5,7 +5,9 @@
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    Thinkopen - Brasil
-#    Copyright (C) Thinkopen Solutions (<http://www.thinkopensolutions.com.br>).
+#    Copyright (C) Thinkopen Solutions (<http://www.thinkopensolutions.com.br>)
+#    Akretion
+#    Copyright (C) Akretion (<http://www.akretion.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -23,11 +25,72 @@
 ##############################################################################
 
 import re
+
 from openerp.osv import fields, orm
 
-class res_company(orm.Model):
-    _name = "res.company"
+
+class ResCompany(orm.Model):
     _inherit = 'res.company'
+
+    def _get_l10n_br_data(self, cr, uid, ids, field_names, arg, context=None):
+        """ Read the l10n_br specific functional fields. """
+        result = {}
+        companies = self.browse(cr, uid, ids, context=context)
+        for company in companies:
+            result[company.id] = {
+                'legal_name': company.partner_id.legal_name,
+                'cnpj_cpf': company.partner_id.cnpj_cpf,
+                'inscr_est': company.partner_id.inscr_est,
+                'inscr_mun': company.partner_id.inscr_mun,
+                'suframa': company.partner_id.suframa
+            }
+        return result
+
+    def _set_l10n_br_data(self, cr, uid, company_id, name,
+                            value, arg, context=None):
+        """ Write the l10n_br specific functional fields. """
+        company = self.browse(cr, uid, company_id, context=context)
+        if company.partner_id:
+            part_obj = self.pool.get('res.partner')
+            part_obj.write(cr, uid, company.partner_id.id,
+                {name: value or False}, context=context)
+        return True
+
+    def _get_address_data(self, cr, uid, ids, field_names, arg, context=None):
+        return super(ResCompany, self)._get_address_data(
+            cr, uid, ids, field_names, arg, context=context)
+
+    def _set_address_data(self, cr, uid, company_id, name,
+                            value, arg, context=None):
+        return super(ResCompany, self)._set_address_data(
+            cr, uid, company_id, name, value, arg, context=context)
+
+    _columns = {
+        'cnpj_cpf': fields.function(
+            _get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=18,
+            type='char', string='CNPJ/CPF', multi='l10n_br'),
+        'inscr_est': fields.function(
+            _get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=16,
+            type='char', string='Inscr. Estadual', multi='l10n_br'),
+        'inscr_mun': fields.function(
+            _get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=18,
+            type='char', string='Inscr. Municipal', multi='l10n_br'),
+        'suframa': fields.function(
+            _get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=18,
+            type='char', string='Suframa', multi='l10n_br'),
+        'legal_name': fields.function(
+            _get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=128,
+            type='char', string=u'Razão Social', multi='l10n_br'),
+        'l10n_br_city_id': fields.function(
+            _get_address_data, fnct_inv=_set_address_data, type='many2one',
+            relation='l10n_br_base.city', string="City", multi='address'),
+        'district': fields.function(
+            _get_address_data, fnct_inv=_set_address_data, size=32,
+            type='char', string="Bairro", multi='address'),
+        'number': fields.function(
+            _get_address_data, fnct_inv=_set_address_data, size=10,
+            type='char', string="Número", multi='address'),
+    }
 
     def onchange_mask_cnpj_cpf(self, cr, uid, ids, cnpj_cpf):
         result = {'value': {}}
@@ -38,44 +101,7 @@ class res_company(orm.Model):
                 % (val[0:2], val[2:5], val[5:8], val[8:12], val[12:14])
             result['value'].update({'cnpj_cpf': cnpj_cpf})
         return result
-    
-    def _get_l10n_br_data(self, cr, uid, ids, field_names, arg, context=None):
-        """ Read the l10n_br specific functional fields. """
-        result = {}
-        companies = self.browse(cr, uid, ids, context=context)
-        for company in companies:
-            result[company.id] = {'legal_name': company.partner_id.legal_name,
-                      'cnpj_cpf': company.partner_id.cnpj_cpf,
-                      'inscr_est': company.partner_id.inscr_est,
-                      'inscr_mun': company.partner_id.inscr_mun,
-                      'suframa': company.partner_id.suframa}
-        return result
-    
-    def _set_l10n_br_data(self, cr, uid, company_id, name, value, arg, context=None):
-        """ Write the l10n_br specific functional fields. """
-        company = self.browse(cr, uid, company_id, context=context)
-        if company.partner_id:
-            part_obj = self.pool.get('res.partner')
-            part_obj.write(cr, uid, company.partner_id.id, {name: value or False}, context=context)
-        return True
-    
-    def _get_address_data(self, cr, uid, ids, field_names, arg, context=None):
-        return super(res_company,self)._get_address_data(cr, uid, ids, field_names, arg, context=context)
-     
-    def _set_address_data(self, cr, uid, company_id, name, value, arg, context=None):
-        return super(res_company,self)._set_address_data(cr, uid, company_id, name, value, arg, context=context)
 
-    _columns = {
-        'cnpj_cpf': fields.function(_get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=18, type='char', string='CNPJ/CPF', multi='l10n_br'),
-        'inscr_est': fields.function(_get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=16, type='char', string='Inscr. Estadual', multi='l10n_br'),
-        'inscr_mun': fields.function(_get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=18, type='char', string='Inscr. Municipal', multi='l10n_br'),
-        'suframa': fields.function(_get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=18, type='char', string='Suframa', multi='l10n_br'),
-        'legal_name': fields.function(_get_l10n_br_data, fnct_inv=_set_l10n_br_data, size=128, type='char', string=u'Razão Social', multi='l10n_br'),
-        'l10n_br_city_id': fields.function(_get_address_data, fnct_inv=_set_address_data, type='many2one', relation='l10n_br_base.city', string="City", multi='address'),
-        'district': fields.function(_get_address_data, fnct_inv=_set_address_data, size=32, type='char', string="Bairro", multi='address'),
-        'number': fields.function(_get_address_data, fnct_inv=_set_address_data, size=10, type='char', string="Número", multi='address'),
-    }
-    
     def onchange_l10n_br_city_id(self, cr, uid, ids, l10n_br_city_id):
         """ Ao alterar o campo l10n_br_city_id que é um campo relacional
         com o l10n_br_base.city que são os municípios do IBGE, copia o nome
@@ -99,4 +125,18 @@ class res_company(orm.Model):
             result['value']['city'] = obj_city['name']
             result['value']['l10n_br_city_id'] = obj_city['id']
 
+        return result
+
+    def onchange_mask_zip(self, cr, uid, ids, code_zip):
+
+        result = {'value': {'zip': False}}
+
+        if not code_zip:
+            return result
+
+        val = re.sub('[^0-9]', '', code_zip)
+
+        if len(val) == 8:
+            code_zip = "%s-%s" % (val[0:5], val[5:8])
+            result['value']['zip'] = code_zip
         return result
