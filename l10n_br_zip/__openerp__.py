5d7b894354c83d2aecb4fb9b6c01be76101c2117
Renato Lima <renatonlima@gmail.com>
2013-12-12 14:38:17 -0200

    Atualização das versões dos módulos

--- l10n_br_zip/__openerp__.py
+++ l10n_br_zip/__openerp__.py
@@ -22,7 +22,7 @@
     'description': 'Brazilian Localisation ZIP Codes',
     'license': 'AGPL-3',
     'author': 'Akretion, OpenERP Brasil',
-    'version': '0.1',
+    'version': '7.0',
     'depends': [
         'l10n_br_base',
     ],
