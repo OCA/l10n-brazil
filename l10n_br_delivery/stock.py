aba5eede005e84750bedc8e23d0a05e6485119cc
mileo <luisfelipe@mileo.co>
2014-01-20 15:31:23 -0200

    [FIX] Class name

--- l10n_br_delivery/stock.py
+++ l10n_br_delivery/stock.py
@@ -37,7 +37,7 @@ class StockPicking(orm.Model):
 
     def _prepare_invoice_line(self, cr, uid, group, picking, move_line,
                               invoice_id, invoice_vals, context=None):
-        result = super(stock_picking, self)._prepare_invoice_line(
+        result = super(StockPicking, self)._prepare_invoice_line(
             cr, uid, group, picking, move_line, invoice_id, invoice_vals,
             context)
         #TODO: Calcular o valor correto em caso de alteração da quantidade
