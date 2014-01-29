badb05b4e97e1d2cd0157adc729d06332f2f34ba
root <luiz.ferreira@monocon.com.br>
2014-01-29 11:16:11 -0200

    Adicionado Módulo de Scrum para Projetos

---
+++ l10n_br_scrum/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Project Scrum',
+    'version': '1.0',
+    'category': 'Project',
+    'description': '',
+    'author': 'Monocon',
+    'website': 'http://www.openerpbrasil.com.br',
+    'summary': 'Project Scrum',
+    'depends': [
+        'base',
+        'project',
+    ],
+    'data': [
+        'project_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
