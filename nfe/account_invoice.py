f16c6d42e4cb31b8849c8c57316e598b064eb679
Renato Lima <renatonlima@gmail.com>
2014-01-31 15:19:54 -0200

    [REF] Refatorando objetos e visões do módulo nfe

--- nfe/account_invoice.py
+++ nfe/account_invoice.py
@@ -18,41 +18,19 @@
 ###############################################################################
 
 import os
-import re
-import pysped.nfe
-import base64
 import datetime
-import netsvc
-from openerp.osv import osv, fields, orm
+from openerp.osv import orm
 from openerp.tools.translate import _
-from os.path import expanduser
 from .sped.nfe.document import NFe200
 from .sped.nfe.validator.xml import validation
 from .sped.nfe.validator.config_check import validate_nfe_configuration
 from .sped.nfe.processing.xml import monta_caminho_nfe
 from .sped.nfe.processing.xml import send
-from pysped.nfe import webservices_flags
 
 
-class AccountInvoice(osv.Model):
-    #--- account_invoice overwritten methods
+class AccountInvoice(orm.Model):
+    """account_invoice overwritten methods"""
     _inherit = 'account.invoice'
-
-    # _columns = {
-    #     'send_nfe_invoice_id': fields.many2one('l10n_br_nfe.send_sefaz', 'Envio NFe'),
-    #     'cancel_nfe_invoice_id': fields.many2one('l10n_br_nfe.send_sefaz', 'Cancelamento NFe'),
-    # }
-
-#     #Override methods of super class
-#     def copy(self, cr, uid, ids, defaults, context=None):
-#         defaults['send_nfe_invoice_id'] = None
-#         return super(account_invoice,self).copy(cr, uid, ids, defaults, context)
-#      def action_cancel(self, cr, uid, ids, context=None):
-#          self.cancel_invoice_online(cr, uid, ids, context)
-#          return super(account_invoice,self).action_cancel(cr, uid, ids, context)
-
-    #---Workflow actions  
-
 
     def nfe_check(self, cr, uid, ids, context=None):
 
@@ -64,35 +42,38 @@ class AccountInvoice(osv.Model):
             validate_nfe_configuration(company)
 
             nfe_obj = NFe200()
-            nfes = nfe_obj.get_xml(cr, uid, ids, int(company.nfe_environment) )
+            nfes = nfe_obj.get_xml(cr, uid, ids, int(company.nfe_environment))
             for nfe in nfes:
                 erro = validation(nfe['nfe'])
                 nfe_key = nfe['key'][3:]
                 if erro:
-                    raise orm.except_orm( _(u'Erro na validação da NFe!'), erro)
+                    raise orm.except_orm(
+                         _(u'Erro na validação da NFe!'), erro)
 
-                self.write(cr, uid, inv.id, {'nfe_access_key': nfe_key })
-                save_dir =  company.nfe_export_folder + monta_caminho_nfe( int(company.nfe_environment) , chave_nfe=nfe_key) + 'tmp/'
+                self.write(cr, uid, inv.id, {'nfe_access_key': nfe_key})
+                save_dir =  company.nfe_export_folder + monta_caminho_nfe(int(company.nfe_environment), chave_nfe=nfe_key) + 'tmp/'
                 nfe_file = nfe['nfe'].encode('utf8')
-                
+
                 file_path = save_dir + nfe_key + '-nfe.xml'
                 try:
                     if not os.path.exists(save_dir):
                         os.makedirs(save_dir)
                     f = open(file_path, 'w')
                 except IOError:
-                    raise orm.except_orm(_(u'Erro!'), 
-                        _(u'Não é foi possivel salvar o arquivo em disco, verifique as permissões de escrita e \
-                    o caminho da pasta'))
+                    raise orm.except_orm(
+                        _(u'Erro!'),
+                        _(u"""Não foi possível salvar o arquivo em disco,
+                            verifique as permissões de escrita e o caminho
+                            da pasta"""))
                 else:
                     f.write(nfe_file)
                     f.close()
 
                 event_obj = self.pool.get('l10n_br_account.document_event')
-                nfe_send_id = event_obj.create(cr, uid, { 
+                nfe_send_id = event_obj.create(cr, uid, {
                     'type': '0',
                     'company_id': company.id,
-                    'origin': '[NF-E]'+ inv.internal_number,
+                    'origin': '[NF-E]' + inv.internal_number,
                     'file_sent': file_path,
                     'create_date': datetime.datetime.now(),
                     'state': 'draft',
@@ -109,13 +90,13 @@ class AccountInvoice(osv.Model):
 
             event_obj = self.pool.get('l10n_br_account.document_event')
 
-            arquivo =  inv.account_document_event_ids[0].file_sent
+            arquivo = inv.account_document_event_ids[0].file_sent
 
             nfe_obj = NFe200()
             nfe = []
             results = []
-            protNFe= {}
-            protNFe["state"] = 'exception'
+            protNFe = {}
+            protNFe["state"] = 'sefaz_exception'
             protNFe["status_code"] = ''
             protNFe["message"] = ''
 
@@ -126,7 +107,7 @@ class AccountInvoice(osv.Model):
 
                     vals = {
                             'type': str(processo.webservice),
-                            'status': processo.resposta.cStat.valor, 
+                            'status': processo.resposta.cStat.valor,
                             'response': '',
                             'company_id': company.id,
                             'origin': '[NF-E]' + inv.internal_number,
@@ -134,22 +115,22 @@ class AccountInvoice(osv.Model):
                             'file_returned': processo.arquivos[1]['arquivo'],
                             'message': processo.resposta.xMotivo.valor,
                             'state': 'done',
-                            'document_event_ids': inv.id }
+                            'document_event_ids': inv.id}
                     results.append(vals)
 
                     if processo.webservice == 1:
                         for prot in processo.resposta.protNFe:
                             protNFe["status_code"] = prot.infProt.cStat.valor
                             protNFe["message"] = prot.infProt.xMotivo.valor
-                            if prot.infProt.cStat.valor in ('100', '150', '110', '301', '302'):        
+                            if prot.infProt.cStat.valor in ('100', '150', '110', '301', '302'):
                                 protNFe["state"] = 'open'
             except Exception as e:
                 vals = {
                         'type': '-1',
-                        'status': '000', 
+                        'status': '000',
                         'response': 'response',
                         'company_id': company.id,
-                        'origin': '[NF-E]'+ inv.internal_number,
+                        'origin': '[NF-E]' + inv.internal_number,
                         'file_sent': 'False',
                         'file_returned': 'False',
                         'message': 'Erro desconhecido ' + e.message,
@@ -160,78 +141,16 @@ class AccountInvoice(osv.Model):
             finally:
                 for result in results:
                     if result['type'] == '0':
-                        event_obj.write(cr, uid, inv.account_document_event_ids[0].id , result, context)
+                        event_obj.write(
+                            cr, uid, inv.account_document_event_ids[0].id,
+                            result, context)
                     else:
                         event_obj.create(cr, uid, result, context)
 
-                self.write(cr, uid, inv.id, { 
-                     'nfe_status': protNFe["status_code"] + ' - ' + protNFe["message"], 
+                self.write(cr, uid, inv.id, {
+                     'nfe_status': protNFe["status_code"] + ' - ' + protNFe["message"],
                      'nfe_date': datetime.datetime.now(),
-                     'state':  protNFe["state"]
-                     }, context)     
+                     'state': protNFe["state"]
+                     }, context)
 
         return result
-
-    #--- Class methods
-    # def cancel_invoice_online(self, cr, uid, ids,context=None):        
-    #     record = self.browse(cr, uid, ids[0])
-    #     if record.document_serie_id:
-    #         if record.document_serie_id.fiscal_document_id:
-    #             if not record.document_serie_id.fiscal_document_id.electronic:
-    #                 return
-                
-    #     if record.state in ('open','paid'): 
-    #         company_pool = self.pool.get('res.company')        
-    #         company = company_pool.browse(cr, uid, record.company_id.id)
-            
-    #         validate_nfe_configuration(company)
-    #         validate_invoice_cancel(record)            
-                
-    #         p = pysped.nfe.ProcessadorNFe()
-            
-    #         p.versao = '2.00' if (company.nfe_version == '200') else '1.10'
-    #         p.estado = company.partner_id.l10n_br_city_id.state_id.code
-        
-    #         file_content_decoded = base64.decodestring(company.nfe_a1_file)        
-    #         p.certificado.stream_certificado = file_content_decoded
-    #         p.certificado.senha = company.nfe_a1_password
-    
-    #         p.salva_arquivos      = True
-    #         p.contingencia_SCAN   = False
-    #         p.caminho = company.nfe_export_folder or os.path.join(expanduser("~"), company.name)
-            
-    #         processo = p.cancelar_nota_evento(
-    #             chave_nfe = record.nfe_access_key,
-    #             numero_protocolo=record.nfe_status,
-    #             justificativa='Somente um teste de cancelamento' #TODO Colocar a justificativa de cancelamento num wizard de cancelamento.
-    #         )
-
-    #         nfe_send_pool = self.pool.get('l10n_br_nfe.send_sefaz')
-    #         nfe_send_id = 0
-    #         if not record.send_nfe_invoice_id:
-    #             nfe_send_id = nfe_send_pool.create(cr, uid, { 'name': 'Envio NFe', 'start_date': datetime.datetime.now()}, context)
-    #             self.write(cr, uid, ids, {'send_nfe_invoice_id': nfe_send_id})
-    #         else:
-    #             nfe_send_id = record.send_nfe_invoice_id.id
-                
-                
-    #         sucesso = 'Error'
-    #         if processo.resposta.infCanc.cStat.valor == '101':
-    #             sucesso = 'success'
-    #         result_pool =  self.pool.get('l10n_br_nfe.send_sefaz_result')
-    #         result_pool.create(cr, uid, {'send_sefaz_id': nfe_send_id , 'xml_type': 'Cancelamento', 
-    #                         'name':'Envio_Cancelamento.xml', 'file':base64.b64encode(processo.envio.xml.encode('utf8')), 
-    #                         'name_result':'Retorno_Cancelamento.xml', 'file_result':base64.b64encode(processo.resposta.xml.encode('utf8')),
-    #                         'status':sucesso, 'status_code':processo.resposta.infCanc.cStat.valor, 
-    #                         'message':processo.resposta.infCanc.xMotivo.valor}, context)
-            
-    #     elif record.state in ('sefaz_export','sefaz_exception'):
-    #         result_pool =  self.pool.get('l10n_br_account.invoice.invalid.number')
-            
-    #         invalidate_number_id = result_pool.create(cr, uid, {'company_id':record.company_id.id,
-    #             'fiscal_document_id':record.fiscal_document_id.id,'document_serie_id':record.document_serie_id.id,
-    #             'number_start':record.internal_number,'number_end':record.internal_number, 
-    #             'justificative':'Inutilização originada do cancelamento da fatura: ' + record.internal_number}, context)
-            
-    #         invoice_invalidate = result_pool.browse(cr, uid, invalidate_number_id, context)
-    #         invoice_invalidate.action_draft_done(cr, uid, [invalidate_number_id], context)
--- nfe/l10n_br_account.py
+++ nfe/l10n_br_account.py
@@ -17,108 +17,63 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-import os
-import re
-import pysped.nfe
-import base64
 import datetime
-import netsvc
-from openerp.osv import osv, fields, orm
+from openerp.osv import orm, fields
 from openerp.tools.translate import _
-from os.path import expanduser
 from sped.nfe.validator.config_check import *
 
-# class l10n_br_nfe_send_sefaz(osv.Model):
-#     """ Classe para salvar o retorno dos metodos de envio de cancelamento, inutilização e recepção de nota """
-#     _name = 'l10n_br_nfe.send_sefaz'
-#     _description = 'Envia os dados para o SEFAZ NFe'
-#     _columns = {                
-#         'name': fields.char('Nome', size=255),
-#         'file': fields.binary('Xml processado', readonly=True),        
-#         'state': fields.selection(
-#             [('init', 'init'), ('done', 'done')], 'state', readonly=True),
-#         'start_date': fields.datetime('Inicio'),
-#         'end_date': fields.datetime('Fim'),
-#         'nfe_environment': fields.selection(
-#             [('1', u'Produção'), ('2', u'Homologação')], 'Ambiente'),        
-#         'nfe_export_result': fields.one2many(
-#             'l10n_br_nfe.send_sefaz_result', 'send_sefaz_id',
-#             'NFe Export Result'),        
-#     }
-#     _defaults = {
-#         'state': 'init',        
-#         'nfe_environment': '2',        
-#     }
 
-
-# class l10n_br_nfe_send_sefaz_result(osv.Model):
-#     _name = 'l10n_br_nfe.send_sefaz_result'
-#     _columns = {
-#         'send_sefaz_id': fields.many2one(
-#             'l10n_br_nfe.send_sefaz', 'Envio SEFAZ',
-#             ondelete='cascade', select=True),
-#         'xml_type': fields.char('Tipo', size=255),
-#         'name':fields.char('Nome envio', size=255),                
-#         'file': fields.binary('Xml envio', readonly=True),
-#         'name_result':fields.char('Nome retorno', size=255),        
-#         'file_result': fields.binary('Xml retorno', readonly=True),        
-#         'status': fields.selection(
-#             [('success', 'Sucesso'), ('error', 'Erro')], 'Status'),
-#         'status_code':fields.char('Código',size=5),
-#         'message': fields.char('Mensagem', size=255),
-#     }
-    
-# class res_company(osv.Model):
-#     _inherit = 'res.company'
-#     _columns = {
-#         'save_xml_folder': fields.boolean('Salvar Xml na Pasta?'),
-#     }
-
-
-class l10n_br_account_invoice_invalid_number(osv.Model):
-    _inherit = 'l10n_br_account.invoice.invalid.number'    
+class L10n_brAccountInvoiceInvalidNumber(orm.Model):
+    _inherit = 'l10n_br_account.invoice.invalid.number'
     _columns = {
-        # 'invalidate_number_nfe_invoice_id': fields.many2one('l10n_br_nfe.send_sefaz', 'Inutilização numeração NFe'),            
-        'state': fields.selection(
-            [('draft', 'Rascunho'), ('not_authorized', 'Não autorizado'),('done', u'Autorizado Sefaz')], 'Status', required=True),        
-        'status':fields.char('Status', size=10),
-        'message':fields.char('Mensagem', size=200),
+        'state': fields.selection([
+            ('draft', 'Rascunho'),
+            ('not_authorized', 'Não autorizado'),
+            ('done', u'Autorizado Sefaz')], 'Status', required=True),
+        'status': fields.char('Status', size=10),
+        'message': fields.char('Mensagem', size=200),
     }
-    
-    def action_draft_done(self, cr, uid, ids, *args):
+
+    def action_draft_done(self, cr, uid, ids, context=None, *args):
         try:
-            processo = self.send_request_to_sefaz(cr, uid, ids , args)
-            
+            processo = self.send_request_to_sefaz(cr, uid, ids, args)
+            values = {
+                'message': processo.resposta.infInut.xMotivo.valor,
+            }
+
             if processo.resposta.infInut.cStat.valor == '102':
-                self.write(cr, uid, ids, {'state': 'done', 'status':'102', 'message':processo.resposta.infInut.xMotivo.valor})
+                values['state'] = 'done'
+                values['status'] = '102'
+                self.write(cr, uid, ids, values, context=context)
             else:
-                self.write(cr, uid, ids, {'state':'not_authorized', 'status':processo.resposta.infInut.cStat.valor,
-                        'message':processo.resposta.infInut.xMotivo.valor})
-                
+                values['state'] = 'not_authorized'
+                values['status'] = processo.resposta.infInut.cStat.valor
+                self.write(cr, uid, ids, values, context=context)
+
         except orm.except_orm, ex:
-            raise ex          
-        except Exception,e:
-            raise orm.except_orm(_('Error !'), e.message)       
+            raise ex
+        except Exception, e:
+            raise orm.except_orm(_('Error !'), e.message)
         return True
 
-class L10n_brAccountInvoiceCancel(orm.Model):
 
+class L10n_brAccountInvoiceCancel(orm.Model):
     _inherit = 'l10n_br_account.invoice.cancel'
 
     def action_draft_done(self, cr, uid, ids, *args):
         self.write(cr, uid, ids, {'state': 'done'})
         return True
 
-class L10n_brDocumentEvent(orm.Model):
 
+class L10n_brDocumentEvent(orm.Model):
     _inherit = 'l10n_br_account.document_event'
 
     def set_done(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        self.write(cr, uid, ids, {'state':'done', 'end_date': datetime.datetime.now()}, context=context)
+        values = {'state': 'done', 'end_date': datetime.datetime.now()}
+        self.write(cr, uid, ids, values, context=context)
         return True
-        
-#--- Validation methods used before send any data to SEFAZ
+
 
 
