71bdb236a8ac25e5e80751abea7ef8a37056662a
Renato Lima <renatonlima@gmail.com>
2014-03-15 02:03:20 -0300

    Extraido métodos do módulo l10n_br_delivery para o novo módulo l10n_br_sale_product

--- l10n_br_sale_stock/sale.py
+++ l10n_br_sale_stock/sale.py
@@ -24,6 +24,25 @@ from openerp.osv import orm
 class SaleOrder(orm.Model):
     _inherit = 'sale.order'
 
+    def _prepare_invoice(self, cr, uid, order, lines, context=None):
+        """Prepare the dict of values to create the new invoice for a
+           sale order. This method may be overridden to implement custom
+           invoice generation (making sure to call super() to establish
+           a clean extension chain).
+
+           :param browse_record order: sale.order record to invoice
+           :param list(int) line: list of invoice line IDs that must be
+                                  attached to the invoice
+           :return: dict of value to create() the invoice
+        """
+        result = super(SaleOrder, self)._prepare_invoice(
+            cr, uid, order, lines, context)
+
+        if order.incoterm:
+            result['incoterm'] = order.incoterm.id
+
+        return result
+
     def _prepare_order_picking(self, cr, uid, order, context=None):
         result = super(SaleOrder, self)._prepare_order_picking(cr, uid,
             order, context)
