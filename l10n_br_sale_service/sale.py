3f94adf4106aea4b763d6cab3d9c4e996b71580c
Renato Lima <renatonlima@gmail.com>
2014-03-17 15:46:33 -0300

    Finalização do módulo l10n_br_sale_product

--- l10n_br_sale_service/sale.py
+++ l10n_br_sale_service/sale.py
@@ -24,3 +24,42 @@ from openerp.addons import decimal_precision as dp
 
 class SaleOrder(orm.Model):
     _inherit = 'sale.order'
+
+    def _make_invoice(self, cr, uid, order, lines, context=None):
+        if context is None:
+            context = {}
+        obj_invoice_line = self.pool.get('account.invoice.line')
+        lines_service = []
+        lines_product = []
+        inv_product_ids = []
+        inv_service_ids = []
+        inv_id_product = False
+        inv_id_service = False
+
+        if (order.fiscal_category_id and not
+            order.fiscal_category_id.property_journal):
+            raise orm.except_orm(
+                    _('Error !'),
+                    _("""There is no journal defined for this company in Fiscal
+                    Category: %s Company: %s""") % (
+                        order.fiscal_category_id.name, order.company_id.name))
+
+        for inv_line in obj_invoice_line.browse(cr, uid, lines, context=context):
+            if inv_line.product_id.fiscal_type == 'service':
+                lines_service.append(inv_line.id)
+            elif inv_line.product_id.fiscal_type == 'product':
+                lines_product.append(inv_line.id)
+
+        if lines_product:
+            context['fiscal_type'] = 'product'
+            inv_id_product = super(SaleOrder, self)._make_invoice(
+                cr, uid, order, lines_product, context=context)
+            inv_product_ids.append(inv_id_product)
+
+        if lines_service:
+            context['fiscal_type'] = 'service'
+            inv_id_service = super(SaleOrder, self)._make_invoice(
+                cr, uid, order, lines_service, context=context)
+            inv_service_ids.append(inv_id_service)
+
+        return inv_id_product or inv_id_service
