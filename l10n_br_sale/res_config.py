60d2ae6c7d13c169d20ade5e2f14b5b7a915e470
Renato Lima <renatonlima@gmail.com>
2014-03-18 16:00:23 -0300

    Adicionado o campo de observação fiscal na nota fiscal e criano uma opção em vendas onde pode escolher copiar ou não a observação na nota fiscal

--- l10n_br_sale/res_config.py
+++ l10n_br_sale/res_config.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2009  Renato Lima - Akretion                                  #
+# Copyright (C) 2014  Renato Lima - Akretion                                  #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
@@ -17,6 +17,36 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-import sale
-import res_config
-import report
+from openerp import SUPERUSER_ID
+from openerp.osv import orm, fields
+
+
+class SaleConfiguration(orm.TransientModel):
+    _inherit = 'sale.config.settings'
+    _columns = {
+        'copy_note': fields.boolean(
+            u'Copiar Observações nos Documentos Fiscais'),
+    }
+
+    def _get_default_copy_note(self, cr, uid, ids, context=None):
+        ir_values = self.pool.get('ir.values')
+        user = self.pool.get('res.users').browse(cr, uid, uid, context)
+        result = ir_values.get_default(
+            cr, uid, 'sale.order', 'copy_note', company_id=user.company_id.id)
+        return result
+
+    _defaults = {
+        'copy_note': _get_default_copy_note,
+    }
+
+    def set_sale_defaults(self, cr, uid, ids, context=None):
+
+        result = super(SaleConfiguration, self).set_sale_defaults(
+            cr, uid, ids, context)
+        wizard = self.browse(cr, uid, ids, context)[0]
+        ir_values = self.pool.get('ir.values')
+
+        user = self.pool.get('res.users').browse(cr, uid, uid, context)
+        ir_values.set_default(cr, SUPERUSER_ID, 'sale.order', 'copy_note',
+            wizard.copy_note, company_id=user.company_id.id)
+        return result
