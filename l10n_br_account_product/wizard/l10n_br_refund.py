d569dee3ca2e155a948780773b57b380e85fdc27
mileo <luisfelipe@mileo.co>
2014-04-02 23:12:17 -0300

    [FIX]

--- l10n_br_account_product/wizard/l10n_br_refund.py
+++ l10n_br_account_product/wizard/l10n_br_refund.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2013  Florian da Costa - Akretion                                  #
+# Copyright (C) 2013  Florian da Costa - Akretion                             #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
