cb4bccb49588fd1baa83e13c98055534ab7d9544
MStuttgart <michellstut@gmail.com>
2014-09-17 12:02:22 -0300

    [FIX] Correção de campos com caracteres especiais, sem a tag de unicode

--- l10n_br_account_product/account_invoice.py
+++ l10n_br_account_product/account_invoice.py
@@ -121,13 +121,13 @@ class AccountInvoice(orm.Model):
             ('proforma', 'Pro-forma'),
             ('proforma2', 'Pro-forma'),
             ('sefaz_export', 'Enviar para Receita'),
-            ('sefaz_exception', 'Erro de autorização da Receita'),
+            ('sefaz_exception', u'Erro de autorização da Receita'),
             ('sefaz_cancelled', 'Cancelado no Sefaz'),
             ('open', 'Open'),
             ('paid', 'Paid'),
             ('cancel', 'Cancelled')
             ], 'State', select=True, readonly=True,
-            help=' * The \'Draft\' state is used when a user is encoding a new and unconfirmed Invoice. \
+            help=u' * The \'Draft\' state is used when a user is encoding a new and unconfirmed Invoice. \
             \n* The \'Pro-forma\' when invoice is in Pro-forma state,invoice does not have an invoice number. \
             \n* The \'Open\' state is used when user create invoice,a invoice number is generated.Its in open state till user does not pay invoice. \
             \n* The \'Paid\' state is set automatically when invoice is paid.\
@@ -136,13 +136,13 @@ class AccountInvoice(orm.Model):
             \n* The \'Cancelled\' state is used when user cancel invoice.'),
         'fiscal_type': fields.selection(PRODUCT_FISCAL_TYPE, 'Tipo Fiscal', required=True),
         'partner_shipping_id': fields.many2one(
-            'res.partner', 'Endereço de Entrega', readonly=True,
+            'res.partner', u'Endereço de Entrega', readonly=True,
             states={'draft': [('readonly', False)]},
             help="Shipping address for current sales order."),
         'nfe_purpose': fields.selection(
             [('1', 'Normal'),
              ('2', 'Complementar'),
-             ('3', 'Ajuste')], 'Finalidade da Emissão', readonly=True,
+             ('3', 'Ajuste')], u'Finalidade da Emissão', readonly=True,
             states={'draft': [('readonly', False)]}),
         'nfe_access_key': fields.char(
             'Chave de Acesso NFE', size=44,
@@ -152,7 +152,7 @@ class AccountInvoice(orm.Model):
             readonly=True, states={'draft': [('readonly', False)]}),        
         'nfe_status': fields.char('Status na Sefaz', size=44, readonly=True),
         'nfe_date': fields.datetime('Data do Status NFE', readonly=True),
-        'nfe_export_date': fields.datetime('Exportação NFE', readonly=True),
+        'nfe_export_date': fields.datetime(u'Exportação NFE', readonly=True),
         'cfop_ids': fields.function(
             _get_cfops, method=True, type='many2many',
             relation='l10n_br_account_product.cfop', string='CFOP'),
@@ -514,7 +514,7 @@ class AccountInvoiceLine(orm.Model):
             domain="[('fiscal_category_id','=',fiscal_category_id)]"),
         'cfop_id': fields.many2one('l10n_br_account_product.cfop', 'CFOP'),
         'fiscal_classification_id': fields.many2one(
-            'account.product.fiscal.classification', 'Classficação Fiscal'),
+            'account.product.fiscal.classification', u'Classficação Fiscal'),
         'product_type': fields.selection(
             [('product', 'Produto'), ('service', u'Serviço')],
             'Tipo do Produto', required=True),
@@ -542,8 +542,8 @@ class AccountInvoiceLine(orm.Model):
         'icms_origin': fields.selection(PRODUCT_ORIGIN, 'Origem'),
         'icms_base_type': fields.selection(
             [('0', 'Margem Valor Agregado (%)'), ('1', 'Pauta (valor)'),
-            ('2', 'Preço Tabelado Máximo (valor)'),
-            ('3', 'Valor da Operação')],
+            ('2', u'Preço Tabelado Máximo (valor)'),
+            ('3', u'Valor da Operação')],
             'Tipo Base ICMS', required=True),
         'icms_base': fields.float('Base ICMS', required=True,
             digits_compute=dp.get_precision('Account')),
@@ -553,10 +553,10 @@ class AccountInvoiceLine(orm.Model):
             digits_compute=dp.get_precision('Account')),
         'icms_percent': fields.float('Perc ICMS',
             digits_compute=dp.get_precision('Discount')),
-        'icms_percent_reduction': fields.float('Perc Redução de Base ICMS',
+        'icms_percent_reduction': fields.float(u'Perc Redução de Base ICMS',
             digits_compute=dp.get_precision('Discount')),
         'icms_st_base_type': fields.selection(
-            [('0', 'Preço tabelado ou máximo  sugerido'),
+            [('0', u'Preço tabelado ou máximo  sugerido'),
             ('1', 'Lista Negativa (valor)'),
             ('2', 'Lista Positiva (valor)'), ('3', 'Lista Neutra (valor)'),
             ('4', 'Margem Valor Agregado (%)'), ('5', 'Pauta (valor)')],
@@ -568,7 +568,7 @@ class AccountInvoiceLine(orm.Model):
         'icms_st_percent': fields.float('Percentual ICMS ST',
             digits_compute=dp.get_precision('Discount')),
         'icms_st_percent_reduction': fields.float(
-            'Perc Redução de Base ICMS ST',
+            u'Perc Redução de Base ICMS ST',
             digits_compute=dp.get_precision('Discount')),
         'icms_st_mva': fields.float('MVA ICMS ST',
             digits_compute=dp.get_precision('Discount')),
@@ -582,7 +582,7 @@ class AccountInvoiceLine(orm.Model):
             ('S', 'Substituta'), ('I', 'Isenta')], 'Tipo do ISSQN',
             required=True),
         'service_type_id': fields.many2one(
-            'l10n_br_account.service.type', 'Tipo de Serviço'),
+            'l10n_br_account.service.type', u'Tipo de Serviço'),
         'issqn_base': fields.float('Base ISSQN', required=True,
             digits_compute=dp.get_precision('Account')),
         'issqn_percent': fields.float('Perc ISSQN', required=True,
