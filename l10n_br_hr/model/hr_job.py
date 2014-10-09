4c70ff09ea30720136ed63f214401b7f829042e5
Luis Felipe Mileo <mileo@kmee.com.br>
2014-10-09 20:58:47 -0300

    [NEW] Modulo de localicazão dos recursos humanos

--- l10n_br_hr/model/hr_job.py
+++ l10n_br_hr/model/hr_job.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 # (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
@@ -9,3 +10,37 @@ class HrJob(models.Model):
     _inherit = 'hr.job'
 
     cbo_id = fields.Many2one('l10n_br_hr.cbo', 'CBO')
+=======
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Brazillian Human Resources Payroll module for OpenERP
+#    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
+#    @author Luis Felipe Mileo <mileo@kmee.com.br>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+from tools.translate import _
+
+class HrJob(orm.Model):
+    
+    _inherit = 'hr.job'
+
+    _columns = {
+            'cbo_id' : fields.many2one('l10n_br_hr.cbo', 'CBO'),
+    }
+>>>>>>> [NEW] Modulo de localicazão dos recursos humanos
--- l10n_br_hr/model/res_company.py
+++ l10n_br_hr/model/res_company.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 # (c) 2014 Kmee - Rafael da Silva Lima <rafael.lima@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
@@ -10,3 +11,35 @@ class ResCompany(models.Model):
 
     check_benefits = fields.Boolean(u'Vale Refeição e Vale Alimentação',
                                     required=False)
+=======
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Brazillian Human Resources Payroll module for OpenERP
+#    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
+#    @author Rafael da Silva Lima <rafael.lima@kmee.com.br>
+#            
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields,osv
+
+class ResCompany(osv.osv):
+    _inherit = 'res.company'
+  
+    _columns = {
+        'check_benefits': fields.boolean('Valley Food and Meal Valley simultaneous', required=False),
+       }
+>>>>>>> [NEW] Modulo de localicazão dos recursos humanos
