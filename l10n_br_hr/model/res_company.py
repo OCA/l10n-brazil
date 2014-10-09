1615af675db8349f31b5bc661ad29bf949f20a7d
Luis Felipe Mileo <mileo@kmee.com.br>
2014-10-09 20:58:47 -0300

    [NEW] Modulo de localicazão dos recursos humanos

---
+++ l10n_br_hr/model/res_company.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Brazillian Human Resources Payroll module for OpenERP
+#    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
+#    @author Rafael da Silva Lima <rafael.lima@kmee.com.br>
+#            
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields,osv
+
+class ResCompany(osv.osv):
+    _inherit = 'res.company'
+  
+    _columns = {
+        'check_benefits': fields.boolean('Valley Food and Meal Valley simultaneous', required=False),
+       }
