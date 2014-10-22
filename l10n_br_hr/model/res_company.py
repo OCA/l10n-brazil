b6219ab0a55eb36f3e07df124cdb535cea926967
Rafael Lima <rafael.lima@kmee.com.br>
2014-10-22 17:38:12 -0200

    [IMP] Modificações módulos RH
    Conflicts:
    	l10n_br_hr_payroll/__openerp__.py

--- l10n_br_hr/model/res_company.py
+++ l10n_br_hr/model/res_company.py
@@ -20,9 +20,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields,osv
+from openerp.osv import orm, fields
 
-class ResCompany(osv.osv):
+class ResCompany(orm.Model):
     _inherit = 'res.company'
   
     _columns = {
