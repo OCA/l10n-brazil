10f8ca1ba8ee9be398fddbc5d40412ae8f44c7c7
Renato Lima <renato.lima@akretion.com>
2014-10-23 02:07:42 -0200

    migrate res.company to new API

--- l10n_br_account/res_company.py
+++ l10n_br_account/res_company.py
@@ -17,7 +17,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields
 from openerp.addons import decimal_precision as dp
 
 COMPANY_FISCAL_TYPE = [
@@ -28,40 +28,31 @@ COMPANY_FISCAL_TYPE = [
 
 COMPANY_FISCAL_TYPE_DEFAULT = '3'
 
-SQL_CONSTRAINTS = [
-    ('l10n_br_tax_definition_tax_id_uniq', 'unique (tax_id, company_id)',
-    u'Imposto já existente nesta empresa!')
-]
 
-
-class ResCompany(orm.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
-    _columns = {
-        'service_invoice_id': fields.many2one(
-            'l10n_br_account.fiscal.document',
-            'Documento Fiscal'),
-        'document_serie_service_id': fields.many2one(
-            'l10n_br_account.document.serie', u'Série Fiscais para Serviço',
-            domain="[('company_id', '=', active_id),('active','=',True),"
-            "('fiscal_type','=','service')]"),
-        'annual_revenue': fields.float(
-            'Faturamento Anual', required=True,
-            digits_compute=dp.get_precision('Account'),
-            help=u"Faturamento Bruto dos últimos 12 meses"),
-        'fiscal_type': fields.selection(COMPANY_FISCAL_TYPE,
-            u'Regime Tributário', required=True),
-        'cnae_main_id': fields.many2one(
-            'l10n_br_account.cnae', u'CNAE Primário'),
-        'cnae_secondary_ids': fields.many2many(
-            'l10n_br_account.cnae', 'res_company_l10n_br_account_cnae',
-            'company_id', 'cnae_id', u'CNAE Segundários'),
-        'ecnpj_a1_file': fields.binary('Arquivo e-CNPJ A1'),
-        'ecnpj_a1_password': fields.char('Senha e-CNPJ A1', size=64),
-        'fiscal_rule_parent_id': fields.many2one(
-            'account.fiscal.position.rule', u'Conjunto de Regras Fiscais',
-            domain="[('parent_id', '=', False)]"),
-    }
-    _defaults = {
-        'fiscal_type': COMPANY_FISCAL_TYPE_DEFAULT,
-        'annual_revenue': 0.00,
-    }
+
+    service_invoice_id = fields.Many2one(
+        'l10n_br_account.fiscal.document',
+        'Documento Fiscal')
+    document_serie_service_id = fields.Many2one(
+        'l10n_br_account.document.serie', u'Série Fiscais para Serviço',
+        domain="[('company_id', '=', active_id),('active','=',True),"
+        "('fiscal_type','=','service')]")
+    annual_revenue = fields.Float(
+        'Faturamento Anual', required=True,
+        digits_compute=dp.get_precision('Account'), default=0.00,
+        help="Faturamento Bruto dos últimos 12 meses")
+    fiscal_type = fields.Selection(
+        COMPANY_FISCAL_TYPE, 'Regime Tributário', required=True,
+        default=COMPANY_FISCAL_TYPE_DEFAULT)
+    cnae_main_id = fields.Many2one(
+        'l10n_br_account.cnae', 'CNAE Primário')
+    cnae_secondary_ids = fields.Many2many(
+        'l10n_br_account.cnae', 'res_company_l10n_br_account_cnae',
+        'company_id', 'cnae_id', 'CNAE Segundários')
+    ecnpj_a1_file = fields.Binary('Arquivo e-CNPJ A1')
+    ecnpj_a1_password = fields.Char('Senha e-CNPJ A1', size=64)
+    fiscal_rule_parent_id = fields.Many2one(
+        'account.fiscal.position.rule', u'Conjunto de Regras Fiscais',
+        domain="[('parent_id', '=', False)]")
