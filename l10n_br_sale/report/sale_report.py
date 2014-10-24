0c93ba8e186c7e99987641fef59e69f191c758c5
Renato Lima <renatonlima@gmail.com>
2014-10-24 17:36:49 -0200

    migrate sale.report

--- l10n_br_sale/report/sale_report.py
+++ l10n_br_sale/report/sale_report.py
@@ -31,44 +31,34 @@ class sale_report(orm.Model):
             'account.fiscal.position', 'Fiscal Position', readonly=True)
     }
 
-    def init(self, cr):
-        tools.drop_view_if_exists(cr, 'sale_report')
-        cr.execute("""create or replace view sale_report as (
-                select
-                    min(l.id) as id,
+    def _select(self):
+        select_str = """
+             SELECT min(l.id) as id,
                     l.product_id as product_id,
+                    t.uom_id as product_uom,
                     l.fiscal_category_id as fiscal_category_id,
                     l.fiscal_position as fiscal_position,
-                    t.uom_id as product_uom,
                     sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
                     sum(l.product_uom_qty * l.price_unit * (100.0-l.discount) / 100.0) as price_total,
-                    1 as nbr,
+                    count(*) as nbr,
                     s.date_order as date,
                     s.date_confirm as date_confirm,
-                    to_char(s.date_order, 'YYYY') as year,
-                    to_char(s.date_order, 'MM') as month,
-                    to_char(s.date_order, 'YYYY-MM-DD') as day,
                     s.partner_id as partner_id,
                     s.user_id as user_id,
-                    s.shop_id as shop_id,
                     s.company_id as company_id,
                     extract(epoch from avg(date_trunc('day',s.date_confirm)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
                     s.state,
                     t.categ_id as categ_id,
                     s.pricelist_id as pricelist_id,
                     s.project_id as analytic_account_id
-                from
-                    sale_order s
-                    join sale_order_line l on (s.id=l.order_id)
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t on (p.product_tmpl_id=t.id)
-                    left join product_uom u on (u.id=l.product_uom)
-                    left join product_uom u2 on (u2.id=t.uom_id)
-                group by
+        """
+        return select_str
+
+    def _group_by(self):
+        group_by_str = """
+            GROUP BY l.product_id,
                     l.fiscal_category_id,
                     l.fiscal_position,
-                    l.product_id,
-                    l.product_uom_qty,
                     l.order_id,
                     t.uom_id,
                     t.categ_id,
@@ -76,9 +66,9 @@ class sale_report(orm.Model):
                     s.date_confirm,
                     s.partner_id,
                     s.user_id,
-                    s.shop_id,
                     s.company_id,
                     s.state,
                     s.pricelist_id,
-                    s.project_id)
-        """)
+                    s.project_id
+        """
+        return group_by_str
