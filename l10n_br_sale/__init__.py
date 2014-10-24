ac1ec9597c196509b8f26e3024b7d729e74b4198
Renato Lima <renato.lima@akretion.com>
2014-10-24 17:11:05 -0200

    add res.company

--- l10n_br_sale/__init__.py
+++ l10n_br_sale/__init__.py
@@ -17,6 +17,7 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
+import res_company
 import sale
 import res_config
 import report
