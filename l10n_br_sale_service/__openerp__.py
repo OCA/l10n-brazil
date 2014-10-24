f631d313e95a352bc9faa6bfa021863b9a1652d6
Renato Lima <renatonlima@gmail.com>
2014-10-24 18:06:06 -0200

    update metadata

--- l10n_br_sale_service/__openerp__.py
+++ l10n_br_sale_service/__openerp__.py
@@ -22,9 +22,9 @@
     'description': 'Brazilian Localization Sale Service',
     'category': 'Localisation',
     'license': 'AGPL-3',
-    'author': 'Akretion, OpenERP Brasil',
-    'website': 'http://openerpbrasil.org',
-    'version': '7.0',
+    'author': 'Akretion, Odoo Brasil',
+    'website': 'http://odoo-brasil.org',
+    'version': '8.0',
     'depends': [
         'l10n_br_sale',
         'l10n_br_account_service',
