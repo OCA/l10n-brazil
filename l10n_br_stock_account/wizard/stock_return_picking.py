e33121ae8e8d6688c8bc32716f304655cc36166a
Renato Lima <renatonlima@gmail.com>
2014-10-25 01:52:54 -0200

    migrate wizards

--- l10n_br_stock_account/wizard/stock_return_picking.py
+++ l10n_br_stock_account/wizard/stock_return_picking.py
@@ -17,18 +17,18 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-from openerp.osv import orm
+from openerp import models
 from openerp.tools.translate import _
 
 
-class stock_return_picking(orm.TransientModel):
+class stock_return_picking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
     def _fiscal_position_map(self, cr, uid, result, **kwargs):
         kwargs['context'].update({'use_domain': ('use_picking', '=', True)})
         fp_rule_obj = self.pool.get('account.fiscal.position.rule')
         return fp_rule_obj.apply_fiscal_mapping(cr, uid, result, **kwargs)
-    
+
     def create_returns(self, cr, uid, ids, context=None):
         """
          Creates return picking.
@@ -42,41 +42,41 @@ class stock_return_picking(orm.TransientModel):
 
         picking_obj = self.pool.get('stock.picking')
         move_obj = self.pool.get('stock.move')
-        picking_type = context.get('default_type')        
-    
+        picking_type = context.get('default_type')
+
         if not context:
             context = {}
 
-        for send_picking in picking_obj.browse(cr, uid, context.get('active_ids'), context):        
+        for send_picking in picking_obj.browse(cr, uid, context.get('active_ids'), context):
 
             result = super(stock_return_picking, self).create_returns(
                 cr, uid, ids, context)
 
             result_domain = eval(result['domain'])
-            picking_ids = result_domain and result_domain[0] and result_domain[0][2]            
+            picking_ids = result_domain and result_domain[0] and result_domain[0][2]
 
             for picking in picking_obj.browse(cr, uid, picking_ids, context=context):
-                
+
                 move_ids = move_obj.search(cr, uid, [('picking_id', '=', picking.id)])
-                
-    
+
+
                 fiscal_category_id = send_picking.fiscal_category_id \
                     and send_picking.fiscal_category_id.refund_fiscal_category_id \
                     and send_picking.fiscal_category_id.refund_fiscal_category_id.id
-        
+
                 if not fiscal_category_id:
                     raise orm.except_orm(
                         _('Error!'),
                         _("""This Fiscal Operation does not has Fiscal Operation
                         for Returns!"""))
-     
+
                 values = {
                     'fiscal_category_id': fiscal_category_id,
                     'fiscal_position': False}
-     
+
                 partner_invoice_id = self.pool.get('res.partner').address_get(
                     cr, uid, [picking.partner_id.id], ['invoice'])['invoice']
-     
+
                 kwargs = {
                    'partner_id': picking.partner_id.id,
                    'partner_invoice_id': partner_invoice_id,
@@ -85,22 +85,22 @@ class stock_return_picking(orm.TransientModel):
                    'context': context,
                    'fiscal_category_id': fiscal_category_id
                 }
-                
+
                 values.update(self._fiscal_position_map(
                     cr, uid, {'value': {}}, **kwargs).get('value'))
 
                 picking_obj.write(cr, uid, [picking.id], values)
-                               
+
                 for idx, send_move in enumerate(send_picking.move_lines):
-                     line_fiscal_category_id = send_move.fiscal_category_id.refund_fiscal_category_id.id                     
+                     line_fiscal_category_id = send_move.fiscal_category_id.refund_fiscal_category_id.id
                      context.update({'parent_fiscal_category_id':line_fiscal_category_id,
-                                     'picking_type' : picking_type,                                    
+                                     'picking_type' : picking_type,
                                     })
-                     
+
                      line_onchange = move_obj.onchange_product_id(cr, uid, ids, send_move.product_id.id, send_move.location_id.id,
                              send_move.location_dest_id.id, picking.partner_id.id, context)
-                     
+
                      line_onchange['value']['fiscal_category_id'] = line_fiscal_category_id
                      move_obj.write(cr, uid, move_ids[idx],line_onchange['value'],context)
-                     
+
             return result
