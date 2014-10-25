7b606a0efc688ce6fb1c74e7bd2b4ade58c4aef0
Renato Lima <renatonlima@gmail.com>
2014-10-25 01:39:59 -0200

    migrate res.company object to new API

--- l10n_br_stock_account/res_company.py
+++ l10n_br_stock_account/res_company.py
@@ -17,23 +17,22 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class res_company(orm.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
-    _columns = {
-        'stock_fiscal_category_id': fields.many2one(
-            'l10n_br_account.fiscal.category',
-            u'Categoria Fiscal Padrão Estoque'),
-        'stock_in_fiscal_category_id': fields.many2one(
-            'l10n_br_account.fiscal.category',
-            u'Categoria Fiscal Padrão de Entrada',
-            domain="[('journal_type', 'in', ('sale_refund', 'purchase')), "
-            "('fiscal_type', '=', 'product'), ('type', '=', 'input')]"),
-        'stock_out_fiscal_category_id': fields.many2one(
-            'l10n_br_account.fiscal.category',
-            u'Categoria Fiscal Padrão Saída',
-            domain="[('journal_type', 'in', ('purchase_refund', 'sale')), "
-            "('fiscal_type', '=', 'product'), ('type', '=', 'output')]"),
-    }
+
+    stock_fiscal_category_id = fields.Many2one(
+        'l10n_br_account.fiscal.category',
+        u'Categoria Fiscal Padrão Estoque')
+    stock_in_fiscal_category_id = fields.Many2one(
+        'l10n_br_account.fiscal.category',
+        u'Categoria Fiscal Padrão de Entrada',
+        domain="[('journal_type', 'in', ('sale_refund', 'purchase')), "
+        "('fiscal_type', '=', 'product'), ('type', '=', 'input')]")
+    stock_out_fiscal_category_id = fields.Many2one(
+        'l10n_br_account.fiscal.category',
+        u'Categoria Fiscal Padrão Saída',
+        domain="[('journal_type', 'in', ('purchase_refund', 'sale')), "
+        "('fiscal_type', '=', 'product'), ('type', '=', 'output')]")
