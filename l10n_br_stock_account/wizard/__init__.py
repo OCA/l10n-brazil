a32919e64b6c023d54b4c8295abfcd8f01a57834
Renato Lima <renatonlima@gmail.com>
2014-10-25 01:31:37 -0200

    move wizard and security folder to l10n_br_stock_account

--- l10n_br_stock_account/wizard/__init__.py
+++ l10n_br_stock_account/wizard/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2014  Renato Lima - Akretion                                  #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
@@ -17,23 +17,5 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-{
-    'name': 'Brazilian Localization WMS Accounting',
-    'description': 'Brazilian Localization WMS Accounting',
-    'category': 'Localisation',
-    'license': 'AGPL-3',
-    'author': 'Akretion, Odoo Brasil',
-    'website': 'http://odoo-brasil.org',
-    'version': '8.0',
-    'depends': [
-        'l10n_br_account_product',
-        'account_fiscal_position_rule_stock',
-    ],
-    'data': [
-        'wizard/stock_invoice_onshipping_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'demo': [],
-    'installable': True,
-    'auto_install': True,
-}
+import stock_invoice_onshipping
+import stock_return_picking
