2ca761f7701171ac467d4acc063efadf8ba2fe75
Renato Lima <renatonlima@gmail.com>
2014-10-27 10:45:40 -0200

    update metadata info
    Conflicts:
    	l10n_br_delivery/__openerp__.py

--- l10n_br_delivery/__openerp__.py
+++ l10n_br_delivery/__openerp__.py
@@ -20,9 +20,9 @@
 {
     'name': 'Brazilian Localization Delivery',
     'license': 'AGPL-3',
-    'author': 'Akretion, OpenERP Brasil',
-    'website': 'http://openerpbrasil.org',
-    'version': '7.0',
+    'author': 'Akretion, Odoo Brasil',
+    'website': 'http://odoo-brasil.org',
+    'version': '8.0',
     'depends': [
         'l10n_br_sale_stock',
         'delivery',
@@ -36,5 +36,5 @@
     ],
     'demo': [],
     'category': 'Localisation',
-    'installable': False,
+    'installable': True,
 }
