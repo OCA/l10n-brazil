803f1b8cdfeed289c63dea81c3e341ca322d2cfc
MStuttgart <michellstut@gmail.com>
2014-11-13 15:47:19 -0200

    [UPDATE] copyright updated

--- nfe/sped/nfe/validator/xml.py
+++ nfe/sped/nfe/validator/xml.py
@@ -2,6 +2,7 @@
 ###############################################################################
 #                                                                             #
 # Copyright (C) 2013  Renato Lima - Akretion                                  #
+# Copyright (C) 2014  Michell Stuttgart Faria - KMEE                          #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
