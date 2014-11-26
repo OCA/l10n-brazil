9d250a9a5bc00bd794cf569f38d493488e35cfdc
Luis Felipe Mileo <mileo@kmee.com.br>
2014-11-26 20:30:05 -0200

    [MIG] Set module as uninstallable and move them to __unported__ folder

---
+++ __unported__/l10n_br_product/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
+
+{
+    'name': 'Brazilian Localization Product',
+    'version': '7.0',
+    'category': 'Generic Modules',
+    'license': 'AGPL-3',
+    'description': """
+    Modulo de Produtos
+    """,
+    'author': 'Akretion, OpenERP Brasil',
+    'website': 'http://www.openerpbrasil.org',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'l10n_br_product_data.xml',
+    ],
+    'demo': [
+        'l10n_br_product_demo.xml',
+    ],
+    'test': [],
+    'installable': False,
+    'active': False,
+    'auto_install': True,
+}
