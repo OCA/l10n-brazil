02af202bba991a6990edaaa115a98209eec253e8
Renato Lima <renatonlima@gmail.com>
2014-12-12 09:01:33 -0200

    fix nfe document
    fix nfe document
    
    fix nfe document

--- l10n_br_account_product/sped/nfe/document.py
+++ l10n_br_account_product/sped/nfe/document.py
@@ -172,12 +172,12 @@ class NFe200(FiscalDocument):
             if inv.partner_id.country_id.bc_code:
                 partner_bc_code = inv.partner_id.country_id.bc_code[1:]
 
-            if inv.partner_id.country_id.id != company_addr_default.country_id.id:
+            if inv.partner_id.country_id.id != company.country_id.id:
                 address_invoice_state_code = 'EX'
                 address_invoice_city = 'Exterior'
                 address_invoice_city_code = '9999999'
-                UFEmbarq = company_addr_default.state_id.code
-                XLocEmbarq = company_addr_default.city
+                UFEmbarq = company.state_id.code
+                XLocEmbarq = company.city
             else:
                 address_invoice_state_code = inv.partner_id.state_id.code
                 address_invoice_city = inv.partner_id.l10n_br_city_id.name or ''
@@ -343,7 +343,13 @@ class NFe200(FiscalDocument):
                     dup = Dup_200()
                     dup.nDup.valor = line.name
                     dup.dVenc.valor = line.date_maturity or inv.date_due or inv.date_invoice
-                    dup.vDup.valor = str("%.2f" % line.debit)
+
+                    if inv.type in ('out_invoice', 'in_refund'):
+                        value = line.debit
+                    else:
+                        value = line.credit
+
+                    dup.vDup.valor = str("%.2f" % value)
                     nfe.infNFe.cobr.dup.append(dup)
 
             #
