79ce1231efe16207fb4594607b1838870157c0f4
Renato Lima <renato.lima@akretion.com>
2014-12-13 12:42:50 -0200

    begining migrate l10n_br_zip wizards objects to Odoo 8.0 new API

--- l10n_br_zip/wizard/l10n_br_zip_search.py
+++ l10n_br_zip/wizard/l10n_br_zip_search.py
@@ -17,36 +17,32 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-from openerp.osv import osv, fields
+from openerp import models, fields, api
 
 
-class L10n_brZipSearch(osv.TransientModel):
+class L10n_brZipSearch(models.TransientModel):
     _name = 'l10n_br.zip.search'
     _description = 'Zipcode Search'
 
-    _columns = {
-        'zip': fields.char('CEP', size=8),
-        'street': fields.char('Logradouro', size=72),
-        'district': fields.char('Bairro', size=72),
-        'country_id': fields.many2one('res.country', 'Country'),
-        'state_id': fields.many2one(
-            "res.country.state", 'Estado',
-            domain="[('country_id','=',country_id)]"),
-        'l10n_br_city_id': fields.many2one(
-            'l10n_br_base.city', 'Cidade',
-            domain="[('state_id','=',state_id)]"),
-        'zip_ids': fields.many2many('l10n_br.zip.result', 'zip_search',
-                                    'zip_search_id', 'zip_id', 'CEP',
-                                    readonly=False),
-        'state': fields.selection(
-            [('init', 'init'), ('done', 'done')], 'state', readonly=True),
-        'address_id': fields.integer('Id do objeto', invisible=True),
-        'object_name': fields.char('Nome do bjeto', size=100, invisible=True),
-    }
+    zip = fields.Char('CEP', size=8)
+    street = fields.Char('Logradouro', size=72)
+    district = fields.Char('Bairro', size=72)
+    country_id = fields.Many2one('res.country', 'Country')
+    state_id = fields.Many2one(
+        "res.country.state", 'Estado',
+        domain="[('country_id','=',country_id)]")
+    l10n_br_city_id = fields.Many2one(
+        'l10n_br_base.city', 'Cidade',
+        domain="[('state_id','=',state_id)]")
+    zip_ids = fields.Many2many(
+        'l10n_br.zip.result', 'zip_search', 'zip_search_id',
+        'zip_id', 'CEP', readonly=False)
+    state = fields.Selection(
+        [('init', 'init'), ('done', 'done')],
+        'state', readonly=True, default='init')
+    address_id = fields.Integer('Id do objeto', invisible=True)
+    object_name = fields.Char('Nome do bjeto', size=100, invisible=True)
 
-    _defaults = {
-        'state': 'init'
-    }
 
     def create(self, cr, uid, vals, context):
         result = super(L10n_brZipSearch, self).create(cr, uid, vals, context)
@@ -73,10 +69,11 @@ class L10n_brZipSearch(osv.TransientModel):
 
         return data
 
-    def zip_search(self, cr, uid, ids, context=None):
-        data = self.read(cr, uid, ids, [], context=context)[0]
-        obj_zip = self.pool.get('l10n_br.zip')
-        obj_zip_result = self.pool.get('l10n_br.zip.result')
+    @api.one
+    def zip_search(self):
+        data = self
+        obj_zip = self.env['l10n_br.zip']
+        obj_zip_result = self.env['l10n_br.zip.result']
         domain = obj_zip.set_domain(
             country_id=data['country_id'][0],
             state_id=data['state_id'][0],
@@ -86,15 +83,13 @@ class L10n_brZipSearch(osv.TransientModel):
             zip=data['zip']
         )
 
-        # Search zip_ids
-        zip_ids = obj_zip.search(cr, uid, domain)
+        # Search zips
+        zips = obj_zip.search(domain)
         #MAP zip to zip.search.result
         zip_result_ids = obj_zip_result.map_to_zip_result(
-            cr, uid, ids, context, zip_ids,
-            data['object_name'], data['address_id'])
-        self.write(cr, uid, ids,
-                   {'state': 'done',
-                    'zip_ids': [[6, 0, zip_result_ids]]}, context=context)
+            zips.ids, data['object_name'], data['address_id'])
+        self.write(
+            {'state': 'done', 'zip_ids': [[6, 0, zip_result_ids]]})
 
         return {
             'type': 'ir.actions.act_window',
@@ -125,53 +120,51 @@ class L10n_brZipSearch(osv.TransientModel):
         }
 
 
-class L10n_brZipResult(osv.TransientModel):
+class L10n_brZipResult(models.TransientModel):
     _name = 'l10n_br.zip.result'
     _description = 'Zipcode result'
 
-    _columns = {
-        'zip_id': fields.many2one(
-            'l10n_br.zip', 'Zipcode', readonly=True, invisible=True),
-        'search_id': fields.many2one(
-            'l10n_br.zip.search', 'Search', readonly=True, invisible=True),
-        'address_id': fields.integer('Id do objeto', invisible=True),
-        'object_name': fields.char('Nome do bjeto', size=100, invisible=True),
-        #ZIPCODE data to be shown
-        'zip': fields.char('CEP', size=9, readonly=True),
-        'street': fields.char('Logradouro', size=72, readonly=True),
-        'street_type': fields.char('Tipo', size=26, readonly=True),
-        'district': fields.char('Bairro', size=72, readonly=True),
-        'country_id': fields.many2one('res.country', 'Country', readonly=True),
-        'state_id': fields.many2one('res.country.state', 'Estado',
-            domain="[('country_id', '=', country_id)]", readonly=True),
-        'l10n_br_city_id': fields.many2one(
-            'l10n_br_base.city', 'Cidade', required=True,
-            domain="[('state_id', '=', state_id)]", readonly=True),
-        }
+    zip_id = fields.Many2one(
+        'l10n_br.zip', 'Zipcode', readonly=True, invisible=True)
+    search_id = fields.Many2one(
+        'l10n_br.zip.search', 'Search', readonly=True, invisible=True)
+    address_id = fields.Integer('Id do objeto', invisible=True)
+    object_name = fields.Char('Nome do bjeto', size=100, invisible=True)
+    #ZIPCODE data to be shown
+    zip = fields.Char('CEP', size=9, readonly=True)
+    street = fields.Char('Logradouro', size=72, readonly=True)
+    street_type = fields.Char('Tipo', size=26, readonly=True)
+    district = fields.Char('Bairro', size=72, readonly=True)
+    country_id = fields.Many2one('res.country', 'Country', readonly=True)
+    state_id = fields.Many2one('res.country.state', 'Estado',
+        domain="[('country_id', '=', country_id)]", readonly=True)
+    l10n_br_city_id = fields.Many2one(
+        'l10n_br_base.city', 'Cidade', required=True,
+        domain="[('state_id', '=', state_id)]", readonly=True)
 
-    def map_to_zip_result(self, cr, uid, ids, context,
-                            zip_data, object_name, address_id):
-        obj_zip = self.pool.get('l10n_br.zip')
+    @api.model
+    def map_to_zip_result(self, zip_data, object_name, address_id):
+        obj_zip = self.env['l10n_br.zip']
         result = []
 
         for zip_read in zip_data:
-            zip_data = obj_zip.set_result(cr, uid, ids, context, zip_read)
+            zip_data = obj_zip.set_result(zip_read)
             zip_result_data = zip_data
             zip_result_data['object_name'] = object_name
             zip_result_data['address_id'] = address_id
 
-            zip_result_id = self.create(
-                cr, uid, zip_result_data, context=context)
+            zip_result_id = self.create(zip_result_data)
             result.append(zip_result_id)
         return result
 
-    def zip_select(self, cr, uid, ids, context=None):
-        data = self.read(cr, uid, ids, [], context=context)[0]
+    @api.one
+    def zip_select(self):
+        data = self
         address_id = data['address_id']
         object_name = data['object_name']
         if address_id and object_name:
             obj = self.pool.get(object_name)
-            obj_zip = self.pool.get('l10n_br.zip')
-            result = obj_zip.set_result(cr, uid, ids, context, data)
-            obj.write(cr, uid, [address_id], result, context=context)
+            obj_zip = self.env['l10n_br.zip']
+            result = obj_zip.set_result(data)
+            obj.write([address_id], result)
         return True
