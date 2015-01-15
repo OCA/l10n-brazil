14460d2ded9bbcc204a9232ab37da2728ab70e5c
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-01-15 11:07:15 -0200

    Changed paths from travis build

--- .travis_install_nightly.sh
+++ .travis_install_nightly.sh
@@ -2,9 +2,9 @@
 # Install the nightly version of OpenERP
 cd ..
 sudo apt-get -ym install python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-imaging bzr
-wget http://nightly.openerp.com/7.0/nightly/src/openerp-7.0-latest.tar.gz
+wget https://nightly.odoo.com/8.0/nightly/src/odoo_8.0.latest.tar.gz
 mkdir -p src
-tar -xf openerp-7.0-latest.tar.gz -C src
+tar -xf odoo_8.0.latest.tar.gz -C src
 
 for f in ./src/*
 do
@@ -17,5 +17,5 @@ rm -rf ./src
 cd openerp
 python setup.py --quiet install
 cd ..
-bzr branch --stacked lp:openerp-fiscal-rules fiscal_rules
+git clone https://github.com/odoobrasil-fiscal/account-fiscal-rule.git fiscal_rules
 bzr branch --stacked lp:~akretion-team/server-env-tools/web-context-tunnel server-env-tools
