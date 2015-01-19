ac6b060dde55f231e2e9d718311eb997b7003111
Renato Lima <renatonlima@gmail.com>
2015-01-19 16:38:27 -0200

    merge from 8.0

--- l10n_br_sale_product/res_company.py
+++ l10n_br_sale_product/res_company.py
@@ -23,6 +23,17 @@ from openerp import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    default_ind_pres = fields.Selection([
+        ('0', u'Não se aplica'),
+        ('1', u'Operação presencial'),
+        ('2', u'Operação não presencial, pela Internet'),
+        ('3', u'Operação não presencial, Teleatendimento'),
+        ('4', u'NFC-e em operação com entrega em domicílio'),
+        ('9', u'Operação não presencial, outros'),
+        ], u'Tipo de operação',
+        help=u'Indicador de presença do comprador no \
+            \nestabelecimento comercial no momento \
+            \nda operação.')
     account_freight_id = fields.Many2one(
         'account.account', 'Freight Sale Tax Account')
     account_insurance_id = fields.Many2one(
