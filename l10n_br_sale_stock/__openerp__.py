c34544e1bda7117bb5e6ef674c548014de704d7e
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-01-25 21:50:19 -0200

    Changed test files path to make modules instalable

--- l10n_br_sale_stock/__openerp__.py
+++ l10n_br_sale_stock/__openerp__.py
@@ -36,10 +36,10 @@
         'sale_stock_view.xml',
     ],
     'demo': [
-        'l10n_br_sale_stock_demo.xml',
-        'test/sale_order_demo.yml'
+        'l10n_br_sale_stock_demo.xml'        
     ],
     'test': [
+        'test/sale_order_demo.yml'
     ],
     'installable': True,
     'auto_install': True,
