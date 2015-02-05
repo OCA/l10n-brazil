814d6ae29a65bf0ad1d592873cf2b93e94246f5e
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-02-04 23:06:07 -0200

    Enabling unit tests travis script

--- .travis_test_script.sh
+++ .travis_test_script.sh
@@ -4,7 +4,7 @@ cd ../openerp
 pwd
 ls ..
 
-./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --stop-after-init --addons-path=../fiscal_rules,../l10n_br_core,../server-env-tools -i l10n_br_base,l10n_br_crm_zip,l10n_br_data_base,l10n_br_account,l10n_br_sale_stock,l10n_br_delivery,l10n_br_purchase,l10n_br_account_payment,10n_br_account_voucher -d l10n_br_test > >(tee stdout.log)
+./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --stop-after-init --test-enable --addons-path=../fiscal_rules,../l10n_br_core,../server-env-tools -i l10n_br_base,l10n_br_crm_zip,l10n_br_data_base,l10n_br_account,l10n_br_sale_stock,l10n_br_delivery,l10n_br_purchase,l10n_br_account_payment,10n_br_account_voucher -d l10n_br_test > >(tee stdout.log)
 
 if $(grep -v mail stdout.log | grep -q ERROR)
 then
