874587976bbeb054e520f93ed74cafa9d68edfaa
mstuttgart <michellstut@gmail.com>
2015-02-10 18:30:07 -0200

    Ajustes no codigo

--- l10n_br_stock/stock.py
+++ l10n_br_stock/stock.py
@@ -163,6 +163,7 @@ class StockPicking(models.Model):
         }
         return self._fiscal_position_map(result, **kwargs)
 
+
     @api.model
     @api.returns
     def _prepare_invoice_line(self, group, picking, move_line,
