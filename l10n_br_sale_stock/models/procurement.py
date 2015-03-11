9d1a87031a732b761b9817ff6639b97d203b942a
Renato Lima <renatonlima@gmail.com>
2015-03-11 17:31:30 -0300

    moved models and views to a folder as new pattern and replace uncoding to coding in py files

--- l10n_br_sale_stock/models/procurement.py
+++ l10n_br_sale_stock/models/procurement.py
@@ -3,18 +3,18 @@
 #                                                                             #
 # Copyright (C) 2014  Renato Lima - Akretion                                  #
 #                                                                             #
-# This program is free software: you can redistribute it and/or modify        #
-# it under the terms of the GNU Affero General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or           #
-# (at your option) any later version.                                         #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
 #                                                                             #
-# This program is distributed in the hope that it will be useful,             #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
-# GNU Affero General Public License for more details.                         #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
 #                                                                             #
-# You should have received a copy of the GNU Affero General Public License    #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
 from openerp import models, api
@@ -28,8 +28,7 @@ class ProcurementOrder(models.Model):
         result = super(ProcurementOrder, self)._run_move_create(procurement)
         if procurement.sale_line_id:
             result.update({
-                'fiscal_category_id': (procurement
-                                       .sale_line_id.fiscal_category_id.id),
+                'fiscal_category_id': procurement.sale_line_id.fiscal_category_id.id,
                 'fiscal_position': procurement.sale_line_id.fiscal_position.id,
             })
         return result
