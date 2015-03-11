df187837a221f6e3e9713c2c7cfea5b0ea56aaae
Renato Lima <renatonlima@gmail.com>
2015-03-11 14:39:15 -0300

    removed edf folder

--- l10n_br_account/sped/__init__.py
+++ l10n_br_account/sped/__init__.py
@@ -17,5 +17,4 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-import document
-import efd
+from . import document
