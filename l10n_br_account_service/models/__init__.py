08a966dc60b74529f2ed1327be841d5dcb24bd49
Renato Lima <renatonlima@gmail.com>
2015-03-11 15:25:40 -0300

    added new __init__.py in model and replace uncoding to coding in py files

--- l10n_br_account_service/models/__init__.py
+++ l10n_br_account_service/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2013  Renato Lima - Akretion                                  #
+# Copyright (C) 2015  Renato Lima - Akretion                                  #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
@@ -17,15 +17,10 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-from openerp import models, fields
+from . import account_invoice
+from . import l10n_br_account
+from . import l10n_br_account_service
+from . import product
+from . import res_company
+from . import res_partner
 
-from .l10n_br_account_service import (
-    PRODUCT_FISCAL_TYPE,
-    PRODUCT_FISCAL_TYPE_DEFAULT)
-
-
-class L10n_brAccountFiscalCategory(models.Model):
-    _inherit = 'l10n_br_account.fiscal.category'
-    
-    fiscal_type = fields.Selection(PRODUCT_FISCAL_TYPE, 'Tipo Fiscal',
-        required=True, default=PRODUCT_FISCAL_TYPE_DEFAULT)
