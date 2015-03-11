92d6508b272826795a9c2831fcc76d10eeda7c2c
Renato Lima <renatonlima@gmail.com>
2015-03-11 17:26:22 -0300

    moved models and views to a folder as new pattern and replace uncoding to coding in py files

--- l10n_br_sale_service/__init__.py
+++ l10n_br_sale_service/__init__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ###############################################################################
 #                                                                             #
 # Copyright (C) 2014  Renato Lima - Akretion                                  #
@@ -17,4 +17,4 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-import sale
+from . import models
