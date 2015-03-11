55e8f7b63abc3dc241dec339b8f93530ce626c4e
Renato Lima <renatonlima@gmail.com>
2015-03-11 15:52:45 -0300

    moved models and views to a folder as new pattern and replace uncoding to coding in py files

--- l10n_br_crm_zip/models/__init__.py
+++ l10n_br_crm_zip/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2011  Fabio Negrini - Akretion                                #
+# Copyright (C) 2011  Renato Lima - Akretion                                  #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
@@ -17,23 +17,4 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
-{
-    'name': 'Brazilian Localization CRM Zip',
-    'description': "ZIP Search Integration for Brazilian Localization of CRM module",
-    'category': 'Localization',
-    'license': 'AGPL-3',
-    'author': 'Fabio Negrini, Akretion, Odoo Brasil',
-    'website': 'http://odoo-brasil.org',
-    'version': '8.0',
-    'depends': [
-        'l10n_br_zip',
-        'l10n_br_crm',
-    ],
-    'data': [
-        'views/crm_lead_view.xml',
-        'views/crm_opportunity_view.xml',
-    ],
-    'demo': [],
-    'installable': True,
-    'auto_install': True,
-}
+from . import crm_lead
