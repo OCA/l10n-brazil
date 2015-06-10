5310806e3a40275e4b894a0c356b709067a36ee6
Raphaël Valyi <rvalyi@akretion.com>
2015-06-10 06:13:10 +0000

    copyright dates

--- l10n_br_crm/models/__init__.py
+++ l10n_br_crm/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2015  Renato Lima - Akretion                                  #
+# Copyright (C) 2011 - TODAY  Renato Lima - Akretion                          #
 #                                                                             #
 #This program is free software: you can redistribute it and/or modify         #
 #it under the terms of the GNU Affero General Public License as published by  #
