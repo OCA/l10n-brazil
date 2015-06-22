5ba59697eadc1a0ada1ad9e2d605c93ca39126f2
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-06-22 17:46:02 -0300

    Remoção de import não usado

--- l10n_br_hr_payroll/model/l10n_br_hr_cbo.py
+++ l10n_br_hr_payroll/model/l10n_br_hr_cbo.py
@@ -21,7 +21,10 @@
 ###############################################################################
 
 from openerp.osv import orm, fields
+<<<<<<< HEAD
 from openerp.tools.translate import _
+=======
+>>>>>>> Remoção de import não usado
 
 
 class L10nBrHrCbo(orm.Model):
