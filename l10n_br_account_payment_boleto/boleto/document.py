2387f92b667ee19c3bb8759f3279084830601414
Luis Felipe Mileo <mileo@kmee.com.br>
2015-06-26 06:33:46 -0300

    [NEW] Boleto factory implementation, working and printing: Itau, Banco do Brasil, HSBC, Santander, Caixa Federal e Caixa Federal Sigcb

--- l10n_br_account_payment_boleto/boleto/document.py
+++ l10n_br_account_payment_boleto/boleto/document.py
@@ -28,42 +28,50 @@ try:
     from cStringIO import StringIO
 except ImportError:
     from StringIO import StringIO
+BoletoException = bank.BoletoException
 
+class Boleto:
 
-class Boleto(object):
-    """
+    boleto = object
+    account_number = ''
+    account_digit = ''
 
-    """
+    branch_number = ''
+    branch_digit = ''
+
+    @staticmethod
+    def getBoleto(move_line):
+        boleto_type = move_line.payment_mode_id.boleto_type
+        try:
+            if boleto_type:
+                return dict_boleto[boleto_type][0](move_line)
+        except:
+            pass
+
+    @staticmethod
+    def getBoletoClass(move_line):
+        bank_code = move_line.payment_mode_id.bank_id.bank.bic
+        return bank.get_class_for_codigo(bank_code)
 
     def __init__(self, move_line):
-        """
-        Get bank class from bank code.
-            '001': 'bancodobrasil.BoletoBB',
-            '041': 'banrisul.BoletoBanrisul',
-            '237': 'bradesco.BoletoBradesco',
-            '104': 'caixa.BoletoCaixa',
-            '399': 'hsbc.BoletoHsbc',
-            '341': 'itau.BoletoItau',
-            '356': 'real.BoletoReal',
-            '033': 'santander.BoletoSantander',
-        :param bank_code:
-        :return:
-        """
-        if move_line:
-            bank_code = move_line.payment_mode_id.bank_id.bank.bic
-            if bank_code in '001':
-                self.boleto = bank.get_class_for_codigo(bank_code)(7, 2)
-            elif bank_code in '104':
-                from pyboleto.bank.caixa_sigcb import BoletoCaixaSigcb
+        self._cedente(move_line.company_id)
+        self._sacado(move_line.partner_id)
+        self._move_line(move_line)
 
-                self.boleto = BoletoCaixaSigcb()
-            else:
-                self.boleto = bank.get_class_for_codigo(bank_code)
-            self.create(move_line)
+    def getAccountNumber(self):
+        if self.account_digit:
+            return str(self.account_number + '-' +
+                       self.account_digit).encode('utf-8')
+        return self.account_number.encode('utf-8')
 
-    def create(self, move_line):
-        self.boleto.nosso_numero = move_line.ref.encode('utf-8')
-        self.boleto.numero_documento = move_line.name.encode('utf-8')
+    def getBranchNumber(self):
+        if self.branch_digit:
+            return str(self.branch_number + '-' +
+                       self.branch_digit).encode('utf-8')
+        return self.branch_number.encode('utf-8')
+
+    def _move_line(self, move_line):
+        self._payment_mode(move_line.payment_mode_id)
         self.boleto.data_vencimento = datetime.date(datetime.strptime(
             move_line.date_maturity, '%Y-%m-%d'))
         self.boleto.data_documento = datetime.date(datetime.strptime(
@@ -75,67 +83,23 @@ class Boleto(object):
         self.boleto.especie = \
             move_line.currency_id and move_line.currency_id.symbol or 'R$'
         self.boleto.quantidade = ''  # str("%.2f" % move_line.amount_currency)
-        self.instrucoes(move_line.payment_mode_id)
-        self.payment_mode(move_line.payment_mode_id)
-        self.cedente(move_line.company_id)
-        self.sacado(move_line.partner_id)
+        self.boleto.numero_documento = move_line.name.encode('utf-8')
 
-    def instrucoes(self, payment_mode_id):
-        """
-        :param payment_mode_id:
-        :return:
-        """
-        self.instrucoes = [
-            payment_mode_id.instrucoes.encode('utf-8'),
-            # move_line.payment_mode_id.instructions_2.encode('utf-8'),
-            # move_line.payment_mode_id.instructions_3.encode('utf-8'),
-            # move_line.payment_mode_id.instructions_4.encode('utf-8'),
-            # move_line.payment_mode_id.instructions_5.encode('utf-8'),
-            # move_line.payment_mode_id.instructions_6.encode('utf-8'),
-            # move_line.payment_mode_id.instructions_7.encode('utf-8'),
-        ]
-        # d.demonstrativo = [
-        #     move_line.payment_mode_id.demonstrative_1.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_2.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_3.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_4.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_5.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_6.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_7.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_8.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_9.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_10.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_11.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_12.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_13.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_14.encode('utf-8'),
-        #     move_line.payment_mode_id.demonstrative_15.encode('utf-8'),
-        #     ]
-
-    def payment_mode(self, payment_mode_id):
+    def _payment_mode(self, payment_mode_id):
         """
         :param payment_mode:
         :return:
         """
-        self.boleto.convenio = payment_mode_id.boleto_convenio.encode('utf-8')
-        self.boleto.especie_documento = \
-            payment_mode_id.boleto_modalidade.encode('utf-8')
+        self.boleto.convenio = payment_mode_id.boleto_convenio
+        self.boleto.especie_documento = payment_mode_id.boleto_modalidade
         self.boleto.aceite = payment_mode_id.boleto_aceite
-        self.boleto.carteira = payment_mode_id.boleto_carteira.encode('utf-8')
-        self.boleto.agencia_cedente = \
-            payment_mode_id.bank_id.bra_number.encode('utf-8')
-        self.boleto.conta_cedente = str(
-            payment_mode_id.bank_id.acc_number +
-            payment_mode_id.bank_id.acc_number_dig
-        ).encode('utf-8')
-        self.boleto.instrucoes = payment_mode_id.instrucoes
+        self.boleto.carteira = payment_mode_id.boleto_carteira
 
-    def cedente(self, company):
+    def _cedente(self, company):
         """
         :param company:
         :return:
         """
-
         self.boleto.cedente = company.partner_id.legal_name.encode('utf-8')
         self.boleto.cedente_documento = company.cnpj_cpf.encode('utf-8')
         self.boleto.cedente_bairro = company.district
@@ -143,28 +107,22 @@ class Boleto(object):
         self.boleto.cedente_cidade = company.city
         self.boleto.cedente_endereco = company.street + ', ' + company.number
         self.boleto.cedente_uf = company.state_id.code
+        self.boleto.agencia_cedente = self.getBranchNumber()
+        self.boleto.conta_cedente = self.getAccountNumber()
 
-    def sacado(self, partner):
+    def _sacado(self, partner):
         """
 
         :param partner:
         :return:
         """
-
-        self.boleto.sacado = \
-            [
-                "{0} - CNPJ/CPF: {1}".format(partner.legal_name,
-                                             partner.cnpj_cpf),
-                "{0}, {1}".format(partner.street, partner.number),
-                # "{2}".format(fname, lname, age),
-            ]
-        # self.boleto.sacado_endereco = partner.street + ', ' +
-        # self.boleto.sacado_cidade = partner.city
-        # self.boleto.sacado_bairro = partner.district
-        # self.boleto.sacado_uf = partner.state_id.code
-        # self.boleto.sacado_cep = partner.zip
-        # self.boleto.sacado_nome = partner.legal_name
-        # self.boleto.sacado_documento = partner.cnpj_cpf
+        self.boleto.sacado_endereco = partner.street + ', ' + partner.number
+        self.boleto.sacado_cidade = partner.city
+        self.boleto.sacado_bairro = partner.district
+        self.boleto.sacado_uf = partner.state_id.code
+        self.boleto.sacado_cep = partner.zip
+        self.boleto.sacado_nome = partner.legal_name
+        self.boleto.sacado_documento = partner.cnpj_cpf
 
     @classmethod
     def get_pdfs(cls, boleto_list):
@@ -173,7 +131,6 @@ class Boleto(object):
         :param boletoList:
         :return:
         """
-
         fbuffer = StringIO()
 
         fbuffer.reset()
@@ -188,3 +145,143 @@ class Boleto(object):
 
         fbuffer.close()
         return boleto_file
+
+class BoletoBB(Boleto):
+
+    def __init__(self, move_line):
+        # TODO: size o convenio and nosso numero, replace (7,2)
+        # Size of convenio 4, 6, 7 or 8
+        #  Nosso Numero format. 1 or 2
+        #  Used only for convenio=6
+        #  1: Nosso Numero with 5 positions
+        #  2: Nosso Numero with 17 positions
+        self.boleto = Boleto.getBoletoClass(move_line)(7,2)
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+        self.boleto.nosso_numero = "111201166" #12345678
+
+
+class BoletoBarisul(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+        self.boleto.nosso_numero = "111201166" #12345678
+
+
+class BoletoBradesco(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        #bank specific
+        self.account_digit = move_line.payment_mode_id.bank_id.acc_number_dig
+        self.branch_digit = move_line.payment_mode_id.bank_id.bra_number_dig
+        #end bank specific
+        Boleto.__init__(self, move_line)
+        self.boleto.nosso_numero = "111201166" #12345678
+
+class BoletoCaixa(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        #bank specific
+        self.account_digit = move_line.payment_mode_id.bank_id.acc_number_dig
+        #end bank specific
+        Boleto.__init__(self, move_line)
+        self.boleto.nosso_numero = "111201166" #12345678
+
+class BoletoHsbc(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+
+class BoletoItau157(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+
+class BoletoItau(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+        self.boleto.nosso_numero = '1234567'
+
+class BoletoReal(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+
+class BoletoSantander101(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+        self.boleto.ios = '0'
+        self.boleto.nosso_numero = "111201166" #12345678
+
+
+class BoletoStatander101201(Boleto):
+
+    def __init__(self, move_line):
+        self.boleto = Boleto.getBoletoClass(move_line)()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        Boleto.__init__(self, move_line)
+        self.boleto.ios = '0'
+        self.boleto.nosso_numero = "111201166" #12345678
+
+
+class BoletoCaixaSigcb(Boleto):
+
+    def __init__(self, move_line):
+        from pyboleto.bank.caixa_sigcb import BoletoCaixaSigcb
+        self.boleto = BoletoCaixaSigcb()
+        self.account_number = move_line.payment_mode_id.bank_id.acc_number
+        self.branch_number = move_line.payment_mode_id.bank_id.bra_number
+        #bank specific
+        self.account_digit = move_line.payment_mode_id.bank_id.acc_number_dig
+        #end bank specific
+        Boleto.__init__(self, move_line)
+        self.boleto.nosso_numero = "111201166" #12345678
+
+
+dict_boleto = {
+    '1' : (BoletoBB, 'Banco do Brasil 18'),
+    '2' : (BoletoBarisul, 'Barisul x'),
+    '3' : (BoletoBradesco, 'Bradesco 06, 03'),
+    '4' : (BoletoCaixa, 'Caixa Economica SR'),
+    '5' : (BoletoHsbc, 'HSBC CNR CSB'),
+    '6' : (BoletoItau157, 'Itau 157'),
+    '7' : (BoletoItau, 'Itau 175, 174, 178, 104, 109'),
+    '8' : (BoletoReal, 'Real 57'),
+    '9' : (BoletoSantander101, 'Santander 102'),
+    '10': (BoletoStatander101201, 'Santander 101, 201'),
+    '11': (BoletoCaixaSigcb, 'Caixa Sigcb'),
+}
+
+def getBoletoSelection():
+    list = []
+    for i in dict_boleto:
+        list.append((i,dict_boleto[i][1]))
+    return list
