908d5575ab8deec90e88653f17210d91b23bee58
Luis Felipe Mileo <mileo@kmee.com.br>
2015-06-30 11:30:50 -0300

    [NEW] WIP implementacao nosso numero

--- l10n_br_account_payment_boleto/models/account_move_line.py
+++ l10n_br_account_payment_boleto/models/account_move_line.py
@@ -31,6 +31,8 @@ class AccountMoveLine(models.Model):
 
     date_payment_created = fields.Date(
         u'Data da criação do pagamento', readonly=True)
+    boleto_own_number = fields.Char(
+        u'Nosso Número', readonly=True)
 
     @api.multi
     def send_payment(self):
@@ -38,10 +40,31 @@ class AccountMoveLine(models.Model):
 
         for move_line in self:
             try:
+
                 if move_line.payment_mode_id.type_payment == '00':
-                    boleto = Boleto.getBoleto(move_line)
+                    number_type = move_line.company_id.own_number_type
+                    if not move_line.boleto_own_number:
+                        if number_type == '0':
+                            #TODO: Generate sequence in other moment!
+                            # One invoice have
+                            # nosso_numero = self.env['ir.sequence'].next_by_id(
+                            #     move_line.company_id.own_number_sequence.id)
+                            pass
+                        elif number_type == '1':
+                            #FIXME: Quando tiver mais que 10 parcelas vai sair do padrão!
+                            nosso_numero = move_line.name.replace('/','0')
+                        elif number_type == '2':
+                            pass
+                    else:
+                        nosso_numero = move_line.boleto_own_number
+
+                    boleto = Boleto.getBoleto(move_line, nosso_numero)
                     if boleto:
                         move_line.date_payment_created = date.today()
+                        move_line.transaction_ref = \
+                            boleto.boleto.format_nosso_numero()
+                        move_line.boleto_own_number = nosso_numero
+
                     boleto_list.append(boleto.boleto)
             except BoletoException:
                 continue
