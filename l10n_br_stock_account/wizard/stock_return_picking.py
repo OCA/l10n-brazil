52dac9f713474a0cecfd4b6e3fafacda1e097aa3
Renato Lima <renatonlima@gmail.com>
2015-07-16 21:49:44 -0300

    PEP8
    pep8 cleared (hopefully)

--- l10n_br_stock_account/wizard/stock_return_picking.py
+++ l10n_br_stock_account/wizard/stock_return_picking.py
@@ -1,20 +1,20 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ###############################################################################
-#                                                                             #
-# Copyright (C) 2009  Renato Lima - Akretion                                  #
-#                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU Affero General Public License as published by  #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
-#                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU Affero General Public License for more details.                          #
-#                                                                             #
-#You should have received a copy of the GNU Affero General Public License     #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+#
+# Copyright (C) 2009  Renato Lima - Akretion
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 from openerp import models, api
@@ -43,7 +43,6 @@ class StockReturnPicking(models.TransientModel):
         context = dict(self.env.context)
         picking_obj = self.env['stock.picking']
         move_obj = self.env['stock.move']
-        picking_type = context.get('default_type')
 
         for send_picking in picking_obj.browse(context.get('active_ids')):
 
@@ -54,7 +53,6 @@ class StockReturnPicking(models.TransientModel):
 
             for picking in picking_obj.browse(picking_ids):
 
-                move_ids = move_obj.search([('picking_id', '=', picking.id)])
                 fiscal_category_id = send_picking.fiscal_category_id.refund_fiscal_category_id.id
 
                 if not fiscal_category_id:
