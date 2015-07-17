73e8370a291b7068b9e368c679cd571a9a476e78
Renato Lima <renatonlima@gmail.com>
2015-07-16 21:49:44 -0300

    PEP8
    pep8 cleared (hopefully)

--- l10n_br_stock/models/stock.py
+++ l10n_br_stock/models/stock.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
-#                                                                             #
-# Copyright (C) 2009  Renato Lima - Akretion                                  #
-# Copyright (C) 2012  Raphaël Valyi - Akretion                                #
-# Copyright (C) 2014  Luis Felipe Miléo - KMEE - www.kmee.com.br              #
-#                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU Affero General Public License as published by  #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
-#                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU Affero General Public License for more details.                          #
-#                                                                             #
-#You should have received a copy of the GNU Affero General Public License     #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+#
+# Copyright (C) 2009  Renato Lima - Akretion
+# Copyright (C) 2012  Raphaël Valyi - Akretion
+# Copyright (C) 2014  Luis Felipe Miléo - KMEE - www.kmee.com.br
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 from openerp import models, fields
@@ -25,8 +25,10 @@ from openerp import models, fields
 class StockIncoterms(models.Model):
     _inherit = 'stock.incoterms'
 
-    freight_responsibility = fields.Selection(
-        [('0', 'Emitente'),
-        ('1', u'Destinatário'),
-        ('2', 'Terceiros'),
-        ('9', 'Sem Frete')], 'Frete por Conta', required=True, default='0')
+    freight_responsibility = fields.Selection([('0', u'Emitente'),
+                                               ('1', u'Destinatário'),
+                                               ('2', u'Terceiros'),
+                                               ('9', u'Sem Frete')],
+                                              'Frete por Conta',
+                                              required=True,
+                                              default=False)
