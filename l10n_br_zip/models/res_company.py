c84d479822435bac300e93d7c9205ad84665f027
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-07-29 22:22:21 -0300

    Migration for the new api module l10n_br_zip

--- l10n_br_zip/models/res_company.py
+++ l10n_br_zip/models/res_company.py
@@ -22,46 +22,12 @@
 #
 ##############################################################################
 
-from openerp.osv import orm
+from openerp import models, api
 
 
-# TODO migrate to new API
-class res_company(orm.Model):
+class res_company(models.Model):
     _inherit = 'res.company'
 
-    def zip_search(self, cr, uid, ids, context=None):
-        obj_zip = self.pool.get('l10n_br.zip')
-        for res_company in self.browse(cr, uid, ids):
-            zip_ids = obj_zip.zip_search_multi(
-                cr, uid, ids, context,
-                country_id=res_company.country_id.id,
-                state_id=res_company.state_id.id,
-                l10n_br_city_id=res_company.l10n_br_city_id.id,
-                district=res_company.district,
-                street=res_company.street,
-                zip_code=res_company.zip,
-            )
-
-            zip_data = obj_zip.read(cr, uid, zip_ids, False, context)
-            obj_zip_result = self.pool.get('l10n_br.zip.result')
-            zip_ids = obj_zip_result.map_to_zip_result(
-                cr, uid, 0, context, zip_data, self._name, ids[0])
-
-            if len(zip_ids) == 1:  # FIXME
-                result = obj_zip.set_result(cr, uid, ids, context, zip_data[0])
-                self.write(cr, uid, [res_company.id], result, context)
-                return True
-            else:
-                if len(zip_ids) > 1:
-                    return obj_zip.create_wizard(
-                        cr, uid, ids, context, self._name,
-                        country_id=res_company.country_id.id,
-                        state_id=res_company.state_id.id,
-                        l10n_br_city_id=res_company.l10n_br_city_id.id,
-                        district=res_company.district,
-                        street=res_company.street,
-                        zip_code=res_company.zip,
-                        zip_ids=zip_ids
-                    )
-                else:
-                    return True
+    @api.multi
+    def zip_search(self):
+        return self.partner_id.zip_search()
