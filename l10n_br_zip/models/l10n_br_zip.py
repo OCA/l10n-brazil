5dd7f6e5d9ff129fa1a9e15422b50cbf9258c2e4
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-08-27 14:23:18 -0300

    Migração do módulo l10n_br_zip para nova api

--- l10n_br_zip/models/l10n_br_zip.py
+++ l10n_br_zip/models/l10n_br_zip.py
@@ -19,41 +19,41 @@
 
 import re
 
-from openerp.osv import orm, fields
+from openerp import models, fields
+from openerp.exceptions import except_orm
 
 
-class L10n_brZip(orm.Model):
+class L10n_brZip(models.Model):
     """ Este objeto persiste todos os códigos postais que podem ser
     utilizados para pesquisar e auxiliar o preenchimento dos endereços.
     """
     _name = 'l10n_br.zip'
     _description = 'CEP'
     _rec_name = 'zip'
-    _columns = {
-        'zip': fields.char('CEP', size=8, required=True),
-        'street_type': fields.char('Tipo', size=26),
-        'street': fields.char('Logradouro', size=72),
-        'district': fields.char('Bairro', size=72),
-        'country_id': fields.many2one('res.country', 'Country'),
-        'state_id': fields.many2one(
-            'res.country.state', 'Estado',
-            domain="[('country_id','=',country_id)]"),
-        'l10n_br_city_id': fields.many2one(
-            'l10n_br_base.city', 'Cidade',
-            required=True, domain="[('state_id','=',state_id)]"),
-    }
+
+    zip = fields.Char('CEP', size=8, required=True)
+    street_type = fields.Char('Tipo', size=26)
+    street = fields.Char('Logradouro', size=72)
+    district = fields.Char('Bairro', size=72)
+    country_id = fields.Many2one('res.country', 'Country')
+    state_id = fields.Many2one(
+        'res.country.state', 'Estado',
+        domain="[('country_id','=',country_id)]")
+    l10n_br_city_id = fields.Many2one(
+        'l10n_br_base.city', 'Cidade',
+        required=True, domain="[('state_id','=',state_id)]")
 
     def set_domain(self, country_id=False, state_id=False,
-                l10n_br_city_id=False, district=False,
-                street=False, zip_code=False):
+                   l10n_br_city_id=False, district=False,
+                   street=False, zip_code=False):
         domain = []
         if zip_code:
             new_zip = re.sub('[^0-9]', '', zip_code or '')
             domain.append(('zip', '=', new_zip))
         else:
             if not state_id or not l10n_br_city_id or \
-                len(street or '') == 0:
-                raise orm.except_orm(
+                    len(street or '') == 0:
+                raise except_orm(
                     u'Parametros insuficientes',
                     u'Necessário informar Estado, município e logradouro')
 
@@ -64,32 +64,33 @@ class L10n_brZip(orm.Model):
             if l10n_br_city_id:
                 domain.append(('l10n_br_city_id', '=', l10n_br_city_id))
             if district:
-                domain.append(('district', 'like', district))
+                domain.append(('district', 'ilike', district))
             if street:
-                domain.append(('street', 'like', street))
+                domain.append(('street', 'ilike', street))
 
         return domain
 
-    def set_result(self, cr, uid, ids, context, zip_read=None):
-        if zip_read:
-            zip_code = zip_read['zip']
+    def set_result(self, zip_obj=None):
+        if zip_obj:
+            zip_code = zip_obj.zip
             if len(zip_code) == 8:
                 zip_code = '%s-%s' % (zip_code[0:5], zip_code[5:8])
             result = {
-                'country_id': zip_read['country_id'] and zip_read['country_id'][0] or False,
-                'state_id': zip_read['state_id'] and zip_read['state_id'][0] or False,
-                'l10n_br_city_id': zip_read['l10n_br_city_id'] and zip_read['l10n_br_city_id'][0] or False,
-                'district': (zip_read['district'] or ''),
-                'street': ((zip_read['street_type'] or '') + ' ' + (zip_read['street'] or '')),
+                'country_id': zip_obj.country_id.id,
+                'state_id': zip_obj.state_id.id,
+                'l10n_br_city_id': zip_obj.l10n_br_city_id.id,
+                'district': zip_obj.district,
+                'street': ((zip_obj.street_type or '') +
+                           ' ' + (zip_obj.street or '')),
                 'zip': zip_code,
             }
         else:
             result = {}
         return result
 
-    def zip_search_multi(self, cr, uid, ids, context, country_id=False,
-                        state_id=False, l10n_br_city_id=False,
-                        district=False, street=False, zip_code=False):
+    def zip_search_multi(self, country_id=False,
+                         state_id=False, l10n_br_city_id=False,
+                         district=False, street=False, zip_code=False):
         domain = self.set_domain(
             country_id=country_id,
             state_id=state_id,
@@ -97,12 +98,12 @@ class L10n_brZip(orm.Model):
             district=district,
             street=street,
             zip_code=zip_code)
-        return self.search(cr, uid, domain)
+        return self.search(domain)
 
     def zip_search(self, cr, uid, ids, context,
-                country_id=False, state_id=False,
-                l10n_br_city_id=False, district=False,
-                street=False, zip_code=False):
+                   country_id=False, state_id=False,
+                   l10n_br_city_id=False, district=False,
+                   street=False, zip_code=False):
         result = self.set_result(cr, uid, ids, context)
         zip_id = self.zip_search_multi(
             cr, uid, ids, context,
@@ -115,12 +116,11 @@ class L10n_brZip(orm.Model):
         else:
             return False
 
-    def create_wizard(self, cr, uid, ids, context,
-                    object_name, country_id=False,
-                    state_id=False, l10n_br_city_id=False,
-                    district=False, street=False, zip_code=False,
-                    zip_ids=False):
-
+    def create_wizard(self, object_name, address_id, country_id=False,
+                      state_id=False, l10n_br_city_id=False,
+                      district=False, street=False, zip_code=False,
+                      zip_ids=False):
+        context = dict(self.env.context)
         context.update({
             'zip': zip_code,
             'street': street,
@@ -129,7 +129,7 @@ class L10n_brZip(orm.Model):
             'state_id': state_id,
             'l10n_br_city_id': l10n_br_city_id,
             'zip_ids': zip_ids,
-            'address_id': ids[0],
+            'address_id': address_id,
             'object_name': object_name})
 
         result = {
