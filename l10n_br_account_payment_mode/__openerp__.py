c56385455de01435befa23ec50994c292623b6b0
Luis Felipe Miléo <mileo@kmee.com.br>
2015-08-27 17:21:15 -0300

    [FIX] dependencies

--- l10n_br_account_payment_mode/__openerp__.py
+++ l10n_br_account_payment_mode/__openerp__.py
@@ -32,6 +32,7 @@
     'website': 'http://www.kmee.com.br',
     'depends': [
         'l10n_br_account',
+        'l10n_br_data_base',
         'account_payment_partner',
         'account_due_list',
         ],
