e4eecec8bf8ac376e11fbefc7d909df0fe840018
Raphael Valyi <rvalyi@akretion.com>
2015-08-30 03:24:45 -0300

    PEP 8 - WIP

--- l10n_br_account_product/models/res_partner.py
+++ l10n_br_account_product/models/res_partner.py
@@ -38,9 +38,6 @@ class AccountFiscalPositionTaxTemplate(models.Model):
     ncm_id = fields.Many2one(
         'account.product.fiscal.classification.template', 'NCM')
 
-class AccountFiscalPosition(models.Model):
-    _inherit = 'account.fiscal.position'
-
 
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
@@ -59,14 +56,14 @@ class AccountFiscalPosition(models.Model):
         if not context:
             context = {}
         if fposition_id and fposition_id.company_id and \
-        context.get('type_tax_use') in ('sale', 'all'):
+                context.get('type_tax_use') in ('sale', 'all'):
             if context.get('fiscal_type', 'product') == 'product':
                 company_tax_ids = self.pool.get('res.company').read(
                     cr, uid, fposition_id.company_id.id, ['product_tax_ids'],
                     context=context)['product_tax_ids']
 
             company_taxes = self.pool.get('account.tax').browse(
-                    cr, uid, company_tax_ids, context=context)
+                cr, uid, company_tax_ids, context=context)
             if taxes:
                 all_taxes = taxes + company_taxes
             else:
@@ -107,7 +104,8 @@ class AccountFiscalPosition(models.Model):
     def _map_tax(self, product_id, taxes):
         result = {}
         product = self.env['product.product'].browse(product_id)
-        if self.company_id and self.env.context.get('type_tax_use') in ('sale', 'all'):
+        if self.company_id and \
+                self.env.context.get('type_tax_use') in ('sale', 'all'):
             if self.env.context.get('fiscal_type', 'product') == 'product':
                 company_taxes = self.company_id.product_tax_definition_line
                 for tax_def in company_taxes:
@@ -133,7 +131,8 @@ class AccountFiscalPosition(models.Model):
         map_taxes_ncm = self.env['account.fiscal.position.tax'].browse()
         for tax in taxes:
             for map in self.tax_ids:
-                if map.tax_src_id == tax or map.tax_code_src_id == tax.tax_code_id:
+                if map.tax_src_id == tax or \
+                        map.tax_code_src_id == tax.tax_code_id:
                     if map.tax_dest_id or tax.tax_code_id:
                         if map.ncm_id == product.ncm_id:
                             map_taxes_ncm |= map
