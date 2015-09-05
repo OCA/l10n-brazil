7e07795fc73533ef7683f824b6c0b38bbede1d93
Renato Lima <renatonlima@gmail.com>
2015-09-05 16:33:35 -0300

    Created procurement order and copy of fiscal category and position

--- l10n_br_sale_stock/sale.py
+++ l10n_br_sale_stock/sale.py
@@ -18,7 +18,22 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ###############################################################################
 
+
+from openerp import api, models
 from openerp.osv import orm
+
+class ProcurementOrder(models.Model):
+    _inherit = "procurement.order"
+
+    @api.model
+    def _run_move_create(self, procurement):
+        result = super(ProcurementOrder, self)._run_move_create(procurement)
+        if procurement.sale_line_id:
+            result.update({
+                'fiscal_category_id': procurement.sale_line_id.fiscal_category_id.id,
+                'fiscal_position': procurement.sale_line_id.fiscal_position.id,
+            })
+        return result
 
 
 class SaleOrder(orm.Model):
@@ -42,25 +57,3 @@ class SaleOrder(orm.Model):
             result['incoterm'] = order.incoterm.id
 
         return result
-
-    # def _prepare_order_picking(self, cr, uid, order, context=None):
-    #     # TODO: Migrate this! Metodo descontinuado
-    #     result = super(SaleOrder, self)._prepare_order_picking(cr, uid,
-    #         order, context)
-    #     result['fiscal_category_id'] = order.fiscal_category_id and \
-    #     order.fiscal_category_id.id
-    #     result['fiscal_position'] = order.fiscal_position and \
-    #     order.fiscal_position.id
-    #     result['ind_pres'] = order.ind_pres or False
-    #     return result
-    #
-    #
-    # def _prepare_order_line_move(self, cr, uid, order, line, picking_id, date_planned, context=None):
-    #     # TODO: Migrate this! Metodo descontinuado
-    #     result = super(SaleOrder, self)._prepare_order_line_move( cr, uid,
-    #            order, line, picking_id, date_planned, context)
-    #     result['fiscal_category_id'] = line.fiscal_category_id and \
-    #     line.fiscal_category_id.id
-    #     result['fiscal_position'] = line.fiscal_position and \
-    #     line.fiscal_position.id
-    #     return result
