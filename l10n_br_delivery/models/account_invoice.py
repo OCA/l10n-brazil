09cbee7acb0b9ff5863b1a6dfbe50d7b3bc4296c
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-10-09 14:47:39 -0300

    Modificado interface dos transportes, criado onchange para setar os campos automaticamente quando existe um metodo cadastrado e um veículo
    Conflicts:
    	l10n_br_delivery/views/account_invoice_view.xml

--- l10n_br_delivery/models/account_invoice.py
+++ l10n_br_delivery/models/account_invoice.py
@@ -17,26 +17,39 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
 
-from openerp.osv import orm, fields
+from openerp import api, fields, models
 from openerp.tools.translate import _
 
 
-class AccountInvoice(orm.Model):
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-    _columns = {
-        'carrier_id': fields.many2one(
-            'delivery.carrier', 'Transportadora', readonly=True,
-            states={'draft': [('readonly', False)]}),
-        'vehicle_id': fields.many2one(
+    
+    carrier_id = fields.Many2one(
+            'delivery.carrier', 'Método de transporte', readonly=True,
+            states={'draft': [('readonly', False)]})
+    vehicle_id = fields.Many2one(
             'l10n_br_delivery.carrier.vehicle', u'Veículo', readonly=True,
-            states={'draft': [('readonly', False)]}),
-        'incoterm': fields.many2one(
+            states={'draft': [('readonly', False)]})
+    incoterm = fields.Many2one(
             'stock.incoterms', 'Tipo do Frete', readonly=True,
             states={'draft': [('readonly', False)]},
             help="Incoterm which stands for 'International Commercial terms' "
             "implies its a series of sales terms which are used in the "
-            "commercial transaction."),
-    }
+            "commercial transaction.")    
+    
+    @api.onchange('carrier_id')
+    def onchange_carrier_id(self):
+        self.partner_carrier_id = self.carrier_id.partner_id
+        
+    @api.onchange('vehicle_id')
+    def onchange_vehicle_id(self):
+        self.vehicle_plate = self.vehicle_id.plate
+        self.vehicle_state_id = self.vehicle_id.state_id
+        self.vehicle_l10n_br_city_id = self.vehicle_id.l10n_br_city_id
+        
+    @api.onchange('incoterm')
+    def onchange_incoterm(self):
+        self.freight_responsibility = self.incoterm.freight_responsibility
 
     def nfe_check(self, cr, uid, ids, context=None):
         result = super(AccountInvoice, self).nfe_check(cr, uid, ids, context)
