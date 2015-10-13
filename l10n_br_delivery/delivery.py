7963d52852a36753f0ddbedd556e69003736bf18
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-13 16:58:08 +0200

    [MOV] move addons out of __unported__ (they remain not installable)

---
+++ l10n_br_delivery/delivery.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2010  Renato Lima - Akretion                                  #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU Affero General Public License for more details.                          #
+#                                                                             #
+#You should have received a copy of the GNU Affero General Public License     #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
+
+from openerp.osv import orm, fields
+
+
+class DeliveryCarrier(orm.Model):
+    _inherit = 'delivery.carrier'
+    _columns = {
+        'antt_code': fields.char('Codigo ANTT', size=32),
+        'vehicle_ids': fields.one2many(
+        'l10n_br_delivery.carrier.vehicle', 'carrier_id', u'Veículos'),
+    }
