6b3a60e619b2828911cf9d7a5e8c55123959a4db
Raphael Valyi <rvalyi@akretion.com>
2015-10-21 18:11:02 -0200

    pep8 hopefully cleared

--- l10n_br_sale_service/models/sale.py
+++ l10n_br_sale_service/models/sale.py
@@ -3,18 +3,18 @@
 #                                                                             #
 # Copyright (C) 2014  Renato Lima - Akretion                                  #
 #                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU Affero General Public License as published by  #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
 #                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU Affero General Public License for more details.                          #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU Affero General Public License for more details.                         #
 #                                                                             #
-#You should have received a copy of the GNU Affero General Public License     #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
 
 from openerp.osv import orm
@@ -38,14 +38,16 @@ class SaleOrder(orm.Model):
         inv_id_service = False
 
         if (order.fiscal_category_id and not
-            order.fiscal_category_id.property_journal):
+            (order
+             .fiscal_category_id.property_journal)):
             raise orm.except_orm(
                     _('Error !'),
                     _("""There is no journal defined for this company in Fiscal
                     Category: %s Company: %s""") % (
                         order.fiscal_category_id.name, order.company_id.name))
 
-        for inv_line in obj_invoice_line.browse(cr, uid, lines, context=context):
+        for inv_line in obj_invoice_line.browse(cr, uid, lines,
+                                                context=context):
             if inv_line.product_id.fiscal_type == 'service':
                 lines_service.append(inv_line.id)
             elif inv_line.product_id.fiscal_type == 'product':
