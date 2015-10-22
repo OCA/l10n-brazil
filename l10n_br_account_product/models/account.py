8bd157068d28abeddf97566dbefeb8ff7dfd51b1
Raphael Valyi <rvalyi@akretion.com>
2015-10-22 00:43:51 -0200

    pep8 wip
    pep8 wip

--- l10n_br_account_product/models/account.py
+++ l10n_br_account_product/models/account.py
@@ -185,7 +185,14 @@ class AccountTax(models.Model):
                 freight_value + other_costs_value
 
         result_icms = self._compute_tax(
-            cr, uid, specific_icms, total_base, product, quantity, precision, base_tax)
+            cr,
+            uid,
+            specific_icms,
+            total_base,
+            product,
+            quantity,
+            precision,
+            base_tax)
         totaldc += result_icms['tax_discount']
         calculed_taxes += result_icms['taxes']
         if result_icms['taxes']:
@@ -207,8 +214,10 @@ class AccountTax(models.Model):
                 'taxes'][0]['base_reduction']
             icms_st_base = round((((result['total'] + ipi_value) * (1 - icms_st_percent_reduction)) * (
                 1 + result_icmsst['taxes'][0]['amount_mva'])), precision)
-            icms_st_base_other = round(((result['total'] + ipi_value) * (
-                1 + result_icmsst['taxes'][0]['amount_mva'])), precision) - icms_st_base
+            icms_st_base_other = round(
+                ((result['total'] + ipi_value) * (
+                    1 + result_icmsst['taxes'][0]['amount_mva'])),
+                precision) - icms_st_base
             result_icmsst['taxes'][0]['total_base'] = icms_st_base
             result_icmsst['taxes'][0]['amount'] = round(
                 (icms_st_base * icms_st_percent) - icms_value, precision)
