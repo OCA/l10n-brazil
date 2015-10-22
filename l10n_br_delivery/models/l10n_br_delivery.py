a44726ecee1c923543c38c4d3f22420b11959f27
Raphael Valyi <rvalyi@akretion.com>
2015-10-21 23:06:45 -0200

    pep8 hopefully cleared

--- l10n_br_delivery/models/l10n_br_delivery.py
+++ l10n_br_delivery/models/l10n_br_delivery.py
@@ -3,18 +3,18 @@
 #                                                                             #
 # Copyright (C) 2010  Renato Lima - Akretion                                  #
 #                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU Affero General Public License as published by  #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
 #                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU Affero General Public License for more details.                          #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU Affero General Public License for more details.                         #
 #                                                                             #
-#You should have received a copy of the GNU Affero General Public License     #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
 
 from openerp import models, fields
@@ -77,8 +77,8 @@ class L10nBrDeliveryShipment(models.Model):
         return result
 
     def _get_picking_line(self, cr, uid, ids, context=None):
-            result = {}
-            for line in self.pool.get('stock.move').browse(
-                cr, uid, ids, context=context):
-                result[line.picking_id.id] = True
-            return list(result.keys())
+        result = {}
+        for line in self.pool.get('stock.move').browse(cr, uid, ids,
+                                                       context=context):
+            result[line.picking_id.id] = True
+        return list(result.keys())
