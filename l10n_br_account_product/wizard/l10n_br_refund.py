fed1120748b58ce094fd4a2781d66eadef7ea64f
Raphael Valyi <rvalyi@akretion.com>
2015-10-24 18:04:55 -0200

    pep8 wizard cleared

--- l10n_br_account_product/wizard/l10n_br_refund.py
+++ l10n_br_account_product/wizard/l10n_br_refund.py
@@ -34,37 +34,38 @@ JOURNAL_TYPE = {
     'in_refund': 'purchase_refund'
 }
 
+
 class account_invoice_refund(orm.TransientModel):
 
     _inherit = "account.invoice.refund"
     _columns = {
         'fiscal_category_id': fields.many2one(
             'l10n_br_account.fiscal.category', 'Categoria Fiscal',
-            domain="[('journal_type', '=', 'sale_refund'), ('fiscal_type', '=', 'product')]", required=True),
+            domain="[('journal_type', '=', 'sale_refund'),\
+                    ('fiscal_type', '=', 'product')]",
+            required=True),
     }
 
-
     def compute_refund(self, cr, uid, ids, mode='refund', context=None):
-
-        inv_obj = self.pool.get('account.invoice')
-        line_obj = self.pool.get('account.invoice.line')
+        inv_obj = self.pool['account.invoice']
+        line_obj = self.pool['account.invoice.line']
         if not context:
             context = {}
-
-        for send_invoice in inv_obj.browse(cr, uid, context.get('active_ids'), context):
-
-            fiscal_category_id = send_invoice.fiscal_category_id.refund_fiscal_category_id.id
-
-            res = super(account_invoice_refund, self).compute_refund(cr, uid, ids, mode, context)
+        for send_invoice in inv_obj.browse(cr, uid, context.get('active_ids'),
+                                           context):
+            fiscal_category_id = (send_invoice.fiscal_category_id
+                                  .refund_fiscal_category_id.id)
+            res = (super(account_invoice_refund, self).
+                   compute_refund(cr, uid, ids, mode, context))
             domain = res['domain']
 
             ids_domain = [x for x in domain if x[0] == 'id'][0]
             invoice_ids = ids_domain[2]
 
-            for invoice in inv_obj.browse(cr, uid, invoice_ids, context=context):
-                
-                line_ids = line_obj.search(cr, uid, [('invoice_id', '=', invoice.id)])
-
+            for invoice in inv_obj.browse(cr, uid, invoice_ids,
+                                          context=context):
+                line_ids = line_obj.search(cr, uid,
+                                           [('invoice_id', '=', invoice.id)])
                 if invoice.payment_term:
                     payment_term = invoice.payment_term.id
                 else:
@@ -73,25 +74,40 @@ class account_invoice_refund(orm.TransientModel):
                     bank = invoice.partner_bank_id.id
                 else:
                     bank = False
-                onchange = inv_obj.onchange_partner_id(cr, uid, [invoice.id], 'out_refund', invoice.partner_id.id, invoice.date_invoice, payment_term, bank, invoice.company_id.id, fiscal_category_id)
+                onchange = inv_obj.onchange_partner_id(cr,
+                                                       uid,
+                                                       [invoice.id],
+                                                       'out_refund',
+                                                       invoice.partner_id.id,
+                                                       invoice.date_invoice,
+                                                       payment_term,
+                                                       bank,
+                                                       invoice.company_id.id,
+                                                       fiscal_category_id)
                 onchange['value']['fiscal_category_id'] = fiscal_category_id
 
                 for idx, send_line in enumerate(send_invoice.invoice_line):
+                    line_fiscal_category_id = (send_line.fiscal_category_id
+                                               .refund_fiscal_category_id.id)
+                    line_onchange = line_obj.onchange_fiscal_category_id(
+                        cr, uid, ids, invoice.partner_id.id,
+                        invoice.company_id.id, send_line.product_id.id,
+                        line_fiscal_category_id, send_line.account_id.id,
+                        context)
+                    line_onchange['value']['fiscal_category_id'] = \
+                        line_fiscal_category_id
 
-                    line_fiscal_category_id = send_line.fiscal_category_id.refund_fiscal_category_id.id
-                    line_onchange = line_obj.onchange_fiscal_category_id(cr, uid, ids, invoice.partner_id.id,
-                                     invoice.company_id.id, send_line.product_id.id, line_fiscal_category_id,
-                                     send_line.account_id.id, context)
-                    line_onchange['value']['fiscal_category_id'] = line_fiscal_category_id
-
-                    line_obj.write(cr, uid, line_ids[idx],line_onchange['value'],context)
-                inv_obj.write(cr, uid, [invoice.id], onchange['value'], context=context)
+                    line_obj.write(cr, uid, line_ids[idx],
+                                   line_onchange['value'], context)
+                inv_obj.write(cr, uid, [invoice.id],
+                              onchange['value'], context=context)
             return res
 
-
-
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
-        res = super(account_invoice_refund, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar, submenu)
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
+                        context=None, toolbar=False, submenu=False):
+        res = (super(account_invoice_refund, self)
+               .fields_view_get(cr, uid, view_id, view_type, context,
+                                toolbar, submenu))
         if not context:
             context = {}
         type = context.get('type', 'out_invoice')
@@ -100,6 +116,9 @@ class account_invoice_refund(orm.TransientModel):
         eview = etree.fromstring(res['arch'])
         fiscal_categ = eview.xpath("//field[@name='fiscal_category_id']")
         for field in fiscal_categ:
-            field.set('domain', "[('journal_type', '=', '%s'),('fiscal_type', '=', 'product'), ('type', '=', '%s')]" % (journal_type, type,))
+            field.set('domain', "[('journal_type', '=', '%s'),\
+                                 ('fiscal_type', '=', 'product'),\
+                                 ('type', '=', '%s')]" % (journal_type,
+                                                          type))
         res['arch'] = etree.tostring(eview)
         return res
