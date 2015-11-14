bad00e0ac461e357b80491d1636c4bba4dd3e80a
Luis Felipe Mileo <mileo@kmee.com.br>
2015-11-14 01:53:25 -0200

    [NEW] Invoice costs ratio wizard

--- l10n_br_account_product/wizard/__init__.py
+++ l10n_br_account_product/wizard/__init__.py
@@ -22,3 +22,4 @@ from . import l10n_br_account_nfe_export_invoice
 from . import l10n_br_account_nfe_export
 from . import l10n_br_account_document_status_sefaz
 from . import account_invoice_refund
+from . import l10n_br_account_invoice_costs_ratio
