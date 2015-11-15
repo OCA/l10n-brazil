bad3b0d85517effc89fa91a410e121738166e65b
Luis Felipe Mileo <mileo@kmee.com.br>
2015-11-15 02:09:28 -0200

    [FIX] fixing version number

--- l10n_br_account/__openerp__.py
+++ l10n_br_account/__openerp__.py
@@ -23,7 +23,7 @@
     'license': 'AGPL-3',
     'author': 'Akretion, Odoo Community Association (OCA)',
     'website': 'http://odoobrasil.org',
-    'version': '8.0.2.0.0',
+    'version': '8.0.1.0.1',
     'depends': [
         'l10n_br',
         'l10n_br_base',
