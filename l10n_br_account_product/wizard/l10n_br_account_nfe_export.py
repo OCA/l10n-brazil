c74b838d9a4c61e98cd8d5d5678b368043dc336d
Luis Felipe Mileo <mileo@kmee.com.br>
2015-11-15 00:36:32 -0200

    [MIF] l10n_br_account_product/wizard/l10n_br_account_nfe_export.py to new API.

--- l10n_br_account_product/wizard/l10n_br_account_nfe_export.py
+++ l10n_br_account_product/wizard/l10n_br_account_nfe_export.py
@@ -17,43 +17,41 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 
 
-class L10nBrAccountNfeExport(orm.TransientModel):
+class L10nBrAccountNfeExport(models.TransientModel):
     """ Exportar Nota Fiscal Eletrônica """
     _name = 'l10n_br_account_product.nfe_export'
     _inherit = 'l10n_br_account_product.nfe_export_invoice'
-    _columns = {
-        'company_id': fields.many2one('res.company', 'Company'),
-        'import_status_draft': fields.boolean("Importar NFs com status "
-                                              "em rascunho"),
-        'nfe_export_result': fields.one2many(
-            'l10n_br_account_product.nfe_export_result', 'wizard_id',
-            'NFe Export Result'),
-    }
-    _defaults = {
-        'company_id': lambda self, cr, uid, c:
-            self.pool.get('res.company')._company_default_get(
-                cr, uid, 'account.invoice', context=c),
-    }
 
-    def _get_invoice_ids(self, cr, uid, data, context=None):
-        if not context:
-            context = {}
-        return self.pool.get('account.invoice').search(
-            cr, uid,
-            [('state', '=', 'sefaz_export'),
-             ('nfe_export_date', '=', False),
-             ('company_id', '=', data['company_id'][0]),
-             ('issuer', '=', '0')])
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        string='Company',
+        default=lambda self: self.env['res.company']._company_default_get(
+            'l10n_br_account_product.nfe_export'))
+    import_status_draft = fields.Boolean(
+        string="Importar NFs com status em rascunho")
+    nfe_export_result = fields.One2many(
+        comodel_name='l10n_br_account_product.nfe_export_result',
+        inverse_name='wizard_id',
+        string='NFe Export Result')
+
+    @api.multi
+    def _get_invoice_ids(self, data):
+        return self.pool.get('account.invoice').search([
+            ('state', '=', 'sefaz_export'),
+            ('nfe_export_date', '=', False),
+            ('company_id', '=', data['company_id'][0]),
+            ('issuer', '=', '0')])
 
 
-class L10nBrAccountNfeExportResult(orm.TransientModel):
+class L10nBrAccountNfeExportResult(models.TransientModel):
     _name = 'l10n_br_account_product.nfe_export_result'
     _inherit = 'l10n_br_account_product.nfe_export_invoice_result'
-    _columns = {
-        'wizard_id': fields.many2one(
-            'l10n_br_account_product.nfe_export', 'Wizard ID',
-            ondelete='cascade', select=True),
-    }
+
+    wizard_id = fields.Many2one(
+        comodel_name='l10n_br_account_product.nfe_export',
+        string='Wizard ID',
+        ondelete='cascade',
+        select=True)
