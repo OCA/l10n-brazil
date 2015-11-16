7228d88b0c2d94ea48dfd56f7ada3ca4bcfc3338
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-11-16 19:17:12 -0200

    Migrating modules to new api

--- l10n_br_account_withholding/account_invoice.py
+++ l10n_br_account_withholding/account_invoice.py
@@ -21,42 +21,39 @@
 
 import time
 
-from openerp.osv import orm, fields
+from openerp import api, fields, models
 from openerp.addons import decimal_precision as dp
 
 
-class AccountInvoice(orm.Model):
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    _columns = {
-        'issqn_wh': fields.boolean(u'Retém ISSQN'),
-        'issqn_value_wh': fields.float('Valor da retenção do ISSQN',
-                                       digits_compute=dp.get_precision('Account')),
-        'pis_wh': fields.boolean(u'Retém PIS'),
-        'pis_value_wh': fields.float('Valor da retenção do PIS',
-                                     digits_compute=dp.get_precision('Account')),
-        'cofins_wh': fields.boolean(u'Retém COFINS'),
-        'cofins_value_wh': fields.float('Valor da retenção do Cofins',
-                                        digits_compute=dp.get_precision('Account')),
-        'csll_wh': fields.boolean(u'Retém CSLL'),
-        'csll_value_wh': fields.float('Valor da retenção de CSLL',
-                                      digits_compute=dp.get_precision('Account')),
-        'irrf_wh': fields.boolean(u'Retém IRRF'),
-        'irrf_base_wh': fields.float('Base de calculo retenção do IRRF',
-                                  digits_compute=dp.get_precision('Account')),
-        'irrf_value_wh': fields.float('Valor da retenção de IRRF',
-                                      digits_compute=dp.get_precision('Account')),
-        'inss_wh': fields.boolean(u'Retém INSS'),
-        'inss_base_wh': fields.float('Base de Cálculo da Retenção da Previdência Social',
-                                  digits_compute=dp.get_precision('Account')),
-        'inss_value_wh': fields.float('Valor da Retenção da Previdência Social ',
-                                      digits_compute=dp.get_precision('Account')),
-        # 'icms_st_value_wh': fields.float('Valor ICMS ST', required=True,
-        #    digits_compute=dp.get_precision('Account')),
-    }
+    issqn_wh = fields.Boolean(u'Retém ISSQN')
+    issqn_value_wh = fields.Float(u'Valor da retenção do ISSQN',
+                                  digits_compute=dp.get_precision('Account'))
+    pis_wh = fields.Boolean(u'Retém PIS')
+    pis_value_wh = fields.Float(u'Valor da retenção do PIS',
+                                digits_compute=dp.get_precision('Account'))
+    cofins_wh = fields.Boolean(u'Retém COFINS')
+    cofins_value_wh = fields.Float(u'Valor da retenção do Cofins',
+                                   digits_compute=dp.get_precision('Account'))
+    csll_wh = fields.Boolean(u'Retém CSLL')
+    csll_value_wh = fields.Float(u'Valor da retenção de CSLL',
+                                 digits_compute=dp.get_precision('Account'))
+    irrf_wh = fields.Boolean(u'Retém IRRF')
+    irrf_base_wh = fields.Float(u'Base de calculo retenção do IRRF',
+                                digits_compute=dp.get_precision('Account'))
+    irrf_value_wh = fields.Float(u'Valor da retenção de IRRF',
+                                 digits_compute=dp.get_precision('Account'))
+    inss_wh = fields.Boolean(u'Retém INSS')
+    inss_base_wh = fields.Float(
+        u'Base de Cálculo da Retenção da Previdência Social',
+        digits_compute=dp.get_precision('Account'))
+    inss_value_wh = fields.Float(u'Valor da Retenção da Previdência Social ',
+                                 digits_compute=dp.get_precision('Account'))
 
     def _whitholding_map(self, cr, uid, result, context=None, **kwargs):
-        #TODO: Implementar o mapeamento cliente x empresa.
+        # TODO: Implementar o mapeamento cliente x empresa.
 
         if not context:
             context = {}
@@ -66,15 +63,20 @@ class AccountInvoice(orm.Model):
         obj_company = self.pool.get('res.company').browse(
             cr, uid, kwargs.get('company_id', False))
 
-        result['value']['issqn_wh'] = obj_company.issqn_wh and obj_partner.partner_fiscal_type_id.issqn_wh
-        result['value']['inss_wh'] = obj_company.inss_wh and obj_partner.partner_fiscal_type_id.inss_wh
-        result['value']['pis_wh'] = obj_company.pis_wh and obj_partner.partner_fiscal_type_id.pis_wh
-        result['value']['cofins_wh'] = obj_company.cofins_wh and obj_partner.partner_fiscal_type_id.cofins_wh
-        result['value']['csll_wh'] = obj_company.csll_wh and obj_partner.partner_fiscal_type_id.csll_wh
-        result['value']['irrf_wh'] = obj_company.irrf_wh and obj_partner.partner_fiscal_type_id.irrf_wh
+        result['value'][
+            'issqn_wh'] = obj_company.issqn_wh and obj_partner.partner_fiscal_type_id.issqn_wh
+        result['value'][
+            'inss_wh'] = obj_company.inss_wh and obj_partner.partner_fiscal_type_id.inss_wh
+        result['value'][
+            'pis_wh'] = obj_company.pis_wh and obj_partner.partner_fiscal_type_id.pis_wh
+        result['value'][
+            'cofins_wh'] = obj_company.cofins_wh and obj_partner.partner_fiscal_type_id.cofins_wh
+        result['value'][
+            'csll_wh'] = obj_company.csll_wh and obj_partner.partner_fiscal_type_id.csll_wh
+        result['value'][
+            'irrf_wh'] = obj_company.irrf_wh and obj_partner.partner_fiscal_type_id.irrf_wh
 
         return result
-
 
     def onchange_partner_id(self, cr, uid, ids, type, partner_id,
                             date_invoice=False, payment_term=False,
@@ -91,10 +93,17 @@ class AccountInvoice(orm.Model):
             fiscal_category_id=fiscal_category_id)
 
     def finalize_invoice_move_lines(self, cr, uid, invoice_browse, move_lines):
-        move_lines = super(AccountInvoice, self).finalize_invoice_move_lines(cr, uid, invoice_browse, move_lines)
+        move_lines = super(
+            AccountInvoice,
+            self).finalize_invoice_move_lines(
+            cr,
+            uid,
+            invoice_browse,
+            move_lines)
 
-        #veriricar se o cliente x empresa retem o imposto e chamar o metodo correspondente
-
+        # veriricar se o cliente x empresa retem o imposto e chamar o metodo
+        # correspondente
+     
         self._compute_wh(cr, uid, invoice_browse)
         return move_lines
 
@@ -113,40 +122,42 @@ class AccountInvoice(orm.Model):
             cr.execute(period_select)
             period_result = cr.dictfetchall()
 
-            inv_state = ['paid', 'open']  #Incluir o estado = sefaz_export?
-            inv_type = ['out_invoice']  #TODO: Tratar compras e devoluções
+            inv_state = ['paid', 'open']  # Incluir o estado = sefaz_export?
+            inv_type = ['out_invoice']  # TODO: Tratar compras e devoluções
             invoice_ids = self.search(cr, uid, [('partner_id', '=', invoice_browse.partner_id.id),
-                                                ('period_id', '=', period_result[0]['id']),
+                                                ('period_id',
+                                                 '=',
+                                                 period_result[0]['id']),
                                                 ('state', 'in', inv_state),
                                                 ('type', 'in', inv_type),
                                                 ])
-            result =  {
+            result = {
                 'pis_value_wh': 0.00,
                 'cofins_value_wh': 0.00,
                 'csll_value_wh': 0.00,
                 'irrf_value_wh': 0.00,
                 'issqn_value_wh': 0.00,
-                }
-            witholded =  {
+            }
+            witholded = {
                 'pis_value_wh': 0.00,
                 'cofins_value_wh': 0.00,
                 'csll_value_wh': 0.00,
                 'irrf_value_wh': 0.00,
                 'issqn_value_wh': 0.00,
                 'irrf_base_wh': 0.00,
-                }
+            }
             invoice = {
                 'pis': 0.00,
                 'cofins': 0.00,
                 'csll': 0.00,
                 'ir': 0.00,
-                }
+            }
             previous = {
                 'pis': 0.00,
                 'cofins': 0.00,
                 'csll': 0.00,
                 'ir': 0.00,
-                }
+            }
             amount_previous = 0.00
 
             for inv in self.browse(cr, uid, invoice_ids):
@@ -171,32 +182,42 @@ class AccountInvoice(orm.Model):
                     invoice['cofins'] += current_line.cofins_value
                     invoice['csll'] += current_line.csll_value
                     invoice['ir'] += current_line.ir_value
-                    #ISSQN
-                    if current_line.issqn_cst_id.code in ('R') and invoice_browse.issqn_wh: #TODO invoice_browse.issqn_wh ERROOOOOOO !!! só entra quando
+                    # ISSQN
+                    # TODO invoice_browse.issqn_wh ERROOOOOOO !!! só entra
+                    # quando
+                    if current_line.issqn_cst_id.code in (
+                            'R') and invoice_browse.issqn_wh:
                         result['issqn_value_wh'] += current_line.issqn_value
 
            # PIS / COFINS / CSLL
-            if (amount_previous + invoice_browse.amount_services) > invoice_browse.company_id.cofins_csll_pis_wh_base:
+            if (amount_previous +
+                    invoice_browse.amount_services) > invoice_browse.company_id.cofins_csll_pis_wh_base:
                 if invoice_browse.pis_wh:
-                    result['pis_value_wh'] =  invoice['pis'] + previous['pis'] - witholded['pis_value_wh']
+                    result['pis_value_wh'] = invoice['pis'] + \
+                        previous['pis'] - witholded['pis_value_wh']
                 if invoice_browse.cofins_wh:
-                    result['cofins_value_wh'] = invoice['cofins'] + previous['cofins'] - witholded['cofins_value_wh']
+                    result['cofins_value_wh'] = invoice['cofins'] + \
+                        previous['cofins'] - witholded['cofins_value_wh']
                 if invoice_browse.csll_wh:
-                    result['csll_value_wh'] = invoice['csll'] + previous['csll'] - witholded['csll_value_wh']
+                    result['csll_value_wh'] = invoice['csll'] + \
+                        previous['csll'] - witholded['csll_value_wh']
 
-            #IR: Existem divergencias entre as normativas verificar melhor a legislação
-            #Pode ser que o total deva acumular para os proximos meses.
+            # IR: Existem divergencias entre as normativas verificar melhor a legislação
+            # Pode ser que o total deva acumular para os proximos meses.
             if invoice_browse.irrf_wh:
-                irrf_base = amount_previous + invoice_browse.amount_services - witholded['irrf_base_wh']
-                irrf_value_wh = irrf_base * invoice_browse.partner_id.partner_fiscal_type_id.irrf_wh_percent / 100
+                irrf_base = amount_previous + \
+                    invoice_browse.amount_services - witholded['irrf_base_wh']
+                irrf_value_wh = irrf_base * \
+                    invoice_browse.partner_id.partner_fiscal_type_id.irrf_wh_percent / \
+                    100
                 if irrf_value_wh > invoice_browse.company_id.irrf_wh_base:
                     result['irrf_value_wh'] = irrf_value_wh
                     result['irrf_base_wh'] = irrf_base
 
-            #INSS
+            # INSS
             if invoice_browse.inss_wh:
                 pass
-                #TODO
+                # TODO
 
             self.write(cr, uid, [invoice_browse.id], result)
 
@@ -244,4 +265,4 @@ class AccountInvoice(orm.Model):
 #     def move_line_get(self, cr, uid, invoice_id):
 #         res = super(AccountInvoiceTax, self).move_line_get(cr, uid, invoice_id)
 #         return res
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
