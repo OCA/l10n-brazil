4f1170b7fb647c36619d106df14c95d919e903be
Luis Felipe Mileo <mileo@kmee.com.br>
2015-11-16 01:12:23 -0200

    [NEW] ResConfig settings for l10n_br_account, [FIX] IPBT [NEW] WS token
    squash

---
+++ l10n_br_account/models/res_config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 KMEE (http://www.kmee.com.br)
+#    @author Luis Felipe Miléo (mileo@kmee.com.br)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class ResConfig(models.Model):
+    _inherit = 'account.config.settings'
+
+    ipbt_token = fields.Char(
+        string='IPBT Token',
+        related='company_id.ipbt_token'
+    )
