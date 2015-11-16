f5930336ed2296b2bc1c159276ce469569215b6e
kram3r <ananias@kmee.com.br>
2015-11-16 18:17:54 -0200

    [FIX] sale_analysis_report

--- l10n_br_sale/report/sale_report.py
+++ l10n_br_sale/report/sale_report.py
@@ -1,20 +1,20 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ###############################################################################
-#                                                                             #
-# Copyright (C) 2011  Renato Lima - Akretion                                  #
-#                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU Affero General Public License as published by  #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
-#                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU General Public License for more details.                                 #
-#                                                                             #
-#You should have received a copy of the GNU General Public License            #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+#
+# Copyright (C) 2011  Renato Lima - Akretion
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 from openerp import tools
@@ -32,45 +32,10 @@ class sale_report(orm.Model):
     }
 
     def _select(self):
-        select_str = """
-            SELECT min(l.id) as id,
-                    l.product_id as product_id,
-                    t.uom_id as product_uom,
-                    sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
-                    sum(l.product_uom_qty * l.price_unit * (100.0-l.discount) / 100.0) as price_total,
-                    count(*) as nbr,
-                    s.date_order as date,
-                    s.date_confirm as date_confirm,
-                    s.partner_id as partner_id,
-                    s.user_id as user_id,
-                    s.company_id as company_id,
-                    extract(epoch from avg(date_trunc('day',s.date_confirm)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
-                    s.state,
-                    t.categ_id as categ_id,
-                    s.pricelist_id as pricelist_id,
-                    s.project_id as analytic_account_id,
-                    s.section_id as section_id,
-                    l.fiscal_category_id as fiscal_category_id,
-                    l.fiscal_position as fiscal_position
-        """
-        return select_str
+        return  super(sale_report, self)._select() + \
+            ", l.fiscal_category_id as fiscal_category_id, " \
+            "l.fiscal_position as fiscal_position"
 
     def _group_by(self):
-        group_by_str = """
-            GROUP BY l.product_id,
-                    l.order_id,
-                    t.uom_id,
-                    t.categ_id,
-                    s.date_order,
-                    s.date_confirm,
-                    s.partner_id,
-                    s.user_id,
-                    s.company_id,
-                    s.state,
-                    s.pricelist_id,
-                    s.project_id,
-                    s.section_id,
-                    l.fiscal_category_id,
-                    l.fiscal_position
-        """
-        return group_by_str
+        return super(sale_report, self)._group_by() + \
+            ", l.fiscal_category_id, l.fiscal_position"
