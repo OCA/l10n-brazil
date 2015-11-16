70bc1fe920d64d3de905f440d26362a136dac6b4
Renato Lima <renatonlima@gmail.com>
2015-11-16 12:44:12 -0200

    changed l10n_br_sale version and edit readme files

--- l10n_br_sale/__openerp__.py
+++ l10n_br_sale/__openerp__.py
@@ -22,9 +22,9 @@
     'description': 'Brazilian Localization for Sale',
     'category': 'Localisation',
     'license': 'AGPL-3',
-    'author': 'Akretion, Odoo Brasil',
+    'author': 'Akretion, Odoo Community Association (OCA)',
     'website': 'http://odoo-brasil.org',
-    'version': '8.0',
+    'version': '8.0.1.0.0',
     'depends': [
         'l10n_br_account',
         'account_fiscal_position_rule_sale',
