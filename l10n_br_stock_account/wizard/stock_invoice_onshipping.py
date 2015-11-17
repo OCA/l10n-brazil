deb34ccbfa6fb959504fec099fd0fc3c7570ba07
Renato Lima <renatonlima@gmail.com>
2015-11-17 17:47:02 -0200

    pep8
    remove from __openerp__ invalid key
    
    pep8

--- l10n_br_stock_account/wizard/stock_invoice_onshipping.py
+++ l10n_br_stock_account/wizard/stock_invoice_onshipping.py
@@ -1,24 +1,24 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ###############################################################################
-#                                                                             #
-# Copyright (C) 2009  Renato Lima - Akretion                                  #
-#                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU Affero General Public License as published by  #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
-#                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU Affero General Public License for more details.                          #
-#                                                                             #
-#You should have received a copy of the GNU Affero General Public License     #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+#
+# Copyright (C) 2009  Renato Lima - Akretion
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 from openerp import models, fields, api, _
-from openerp.exceptions import except_orm
+from openerp.exceptions import Warning as UserError
 
 
 class StockInvoiceOnShipping(models.TransientModel):
@@ -37,12 +37,12 @@ class StockInvoiceOnShipping(models.TransientModel):
         for picking in self.env['stock.picking'].browse(active_ids):
             journal_id = picking.fiscal_category_id.property_journal
             if not journal_id:
-                raise except_orm(
+                raise UserError(
                     _('Invalid Journal!'),
-                    _('There is not journal defined for this company: %s in \
-                    fiscal operation: %s !') % (picking.company_id.name,
-                                                picking.fiscal_category_id.name))
-
+                    _('There is not journal defined for this company: %s in '
+                      'fiscal operation: %s !') %
+                    (picking.company_id.name,
+                     picking.fiscal_category_id.name))
             self.write({'journal_id': journal_id.id})
         result = super(StockInvoiceOnShipping, self).create_invoice()
         return result
