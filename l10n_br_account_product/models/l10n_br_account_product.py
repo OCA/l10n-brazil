f7ff4f35cd8d7322e2aeae64c2008870ccff220d
mstuttgart <michellstut@gmail.com>
2015-11-28 14:25:19 -0200

    Flake8 adjust

--- l10n_br_account_product/models/l10n_br_account_product.py
+++ l10n_br_account_product/models/l10n_br_account_product.py
@@ -344,6 +344,7 @@ class ImportDeclarationLine(models.Model):
                                    digits=dp.get_precision('Account'),
                                    default=0.00)
 
+
 class L10nBrIcmsRelief(models.Model):
 
     _name = 'l10n_br_account_product.icms_relief'
@@ -359,24 +360,15 @@ class L10nBrIPIGuideline(models.Model):
     _name = 'l10n_br_account_product.ipi_guideline'
     _description = 'IPI Guidelines'
 
-    code = fields.Char(
-        u'Código', size=3, required=True)
-    name = fields.Text(
-        u'Descrição Enquadramento Legal do IPI',
-        required=True
-    )
-    cst_group = fields.Selection([
-            ('imunidade', u'Imunidade'),
-            ('suspensao', u'Suspensão'),
-            ('isencao', u'Isenção'),
-            ('reducao', u'Redução'),
-            ('outros', u'Outros'),
-    ],
-        string='Grupo CST',
-        required=True
-    )
+    code = fields.Char(u'Código', size=3, required=True)
+    name = fields.Text(u'Descrição Enquadramento Legal do IPI', required=True)
+    cst_group = fields.Selection([('imunidade', u'Imunidade'),
+                                  ('suspensao', u'Suspensão'),
+                                  ('isencao', u'Isenção'),
+                                  ('reducao', u'Redução'),
+                                  ('outros', u'Outros'),
+                                  ], string='Grupo CST', required=True)
     tax_code_in_id = fields.Many2one(
         'account.tax.code.template', string=u'CST Entrada')
     tax_code_out_id = fields.Many2one(
         'account.tax.code.template', string=u'CST Saída')
-
