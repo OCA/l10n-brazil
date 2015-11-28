4eba89b65a07f5885c01d29108c3c580568d1add
Luis Felipe Mileo <mileo@kmee.com.br>
2015-11-28 05:00:13 -0200

    [NEW] Add fields to serializer

--- l10n_br_account_product/sped/nfe/document.py
+++ l10n_br_account_product/sped/nfe/document.py
@@ -347,7 +347,8 @@ class NFe200(FiscalDocument):
 
         self.det.prod.NCM.valor = punctuation_rm(
             invoice_line.fiscal_classification_id.code or '')[:8]
-        self.det.prod.EXTIPI.valor = ''
+        self.det.prod.EXTIPI.valor = punctuation_rm(
+            invoice_line.fiscal_classification_id.code or '')[8:]
         self.det.prod.nFCI.valor = invoice_line.fci or ''
         self.det.prod.CFOP.valor = invoice_line.cfop_id.code
         self.det.prod.uCom.valor = invoice_line.uos_id.name or ''
@@ -391,6 +392,8 @@ class NFe200(FiscalDocument):
                 "%.2f" % invoice_line.icms_percent)
             self.det.imposto.ICMS.vICMS.valor = str(
                 "%.2f" % invoice_line.icms_value)
+            self.det.imposto.ICMS.motDesICMS.valor = (
+                invoice_line.icms_relief_id.code or '')
 
             # ICMS ST
             self.det.imposto.ICMS.modBCST.valor = (
@@ -405,6 +408,9 @@ class NFe200(FiscalDocument):
                 "%.2f" % invoice_line.icms_st_percent)
             self.det.imposto.ICMS.vICMSST.valor = str(
                 "%.2f" % invoice_line.icms_st_value)
+
+
+
 
             # IPI
             self.det.imposto.IPI.CST.valor = invoice_line.ipi_cst_id.code
@@ -423,6 +429,8 @@ class NFe200(FiscalDocument):
                         "%.2f" % invoice_line.ipi_percent)
             self.det.imposto.IPI.vIPI.valor = str(
                 "%.2f" % invoice_line.ipi_value)
+            self.det.imposto.IPI.cEnq = (
+                invoice_line.ipi_guideline_id.code or '999')
 
         else:
             # ISSQN
