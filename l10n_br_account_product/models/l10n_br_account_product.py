5935d9538cc6a7d8dc2b0c7a98c9380891b9ee41
Luis Felipe Mileo <mileo@kmee.com.br>
2015-11-28 02:14:59 -0200

    CEST, IPI GUIDELINES, VIEWS

--- l10n_br_account_product/models/l10n_br_account_product.py
+++ l10n_br_account_product/models/l10n_br_account_product.py
@@ -351,28 +351,32 @@ class L10nBrIcmsRelief(models.Model):
 
     code = fields.Char(u'Código', size=2, required=True)
     name = fields.Char('Nome', size=256, required=True)
+    active = fields.Boolean('Ativo', default=True)
 
 
-class L10nBrIPIRelief(models.Model):
+class L10nBrIPIGuideline(models.Model):
 
     _name = 'l10n_br_account_product.ipi_guideline'
-    _description = 'IPI Guideline'
+    _description = 'IPI Guidelines'
 
     code = fields.Char(
         u'Código', size=3, required=True)
+    name = fields.Text(
+        u'Descrição Enquadramento Legal do IPI',
+        required=True
+    )
     cst_group = fields.Selection([
             ('imunidade', u'Imunidade'),
             ('suspensao', u'Suspensão'),
             ('isencao', u'Isenção'),
-            ('reducao', u'Redução'),
             ('reducao', u'Redução'),
             ('outros', u'Outros'),
     ],
         string='Grupo CST',
         required=True
     )
-    name = fields.Char(
-        u'Descrição Enquadramento Legal do IPI',
-        size=256,
-        required=True
-    )
+    tax_code_in_id = fields.Many2one(
+        'account.tax.code.template', string=u'CST Entrada')
+    tax_code_out_id = fields.Many2one(
+        'account.tax.code.template', string=u'CST Saída')
+
