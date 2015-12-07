a81054db76c231fcbd9d4811695879e7feda4ebb
Luis Felipe Mileo <mileo@kmee.com.br>
2015-12-07 17:19:12 -0200

    [ADD] pre-migration script of l10n_br_sale_product

---
+++ l10n_br_sale_product/8.0.1.0.0/pre-migration.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 KMEE INFORMATICA LTDA - Luis Felipe Miléo
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+from openerp.openupgrade import openupgrade
+
+
+logger = logging.getLogger('OpenUpgrade.l10n_br_sale')
+
+column_renames = {
+    'res_company': [
+        ('default_fiscal_category_sale_id', 'sale_fiscal_category_id'),
+    ],
+}
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.rename_columns(cr, column_renames)
