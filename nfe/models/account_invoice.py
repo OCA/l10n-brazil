2a4c7a6c6f7e3e245391d042800eac2527cb03e6
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2015-12-10 18:02:07 -0200

    [MIGR] migrate account_invoice

--- nfe/models/account_invoice.py
+++ nfe/models/account_invoice.py
@@ -21,8 +21,9 @@ import os
 import logging
 import datetime
 
-from openerp.osv import orm
 from openerp.tools.translate import _
+from openerp import models, fields, api
+from openerp.exceptions import RedirectWarning
 
 from openerp.addons.nfe.sped.nfe.nfe_factory import NfeFactory
 from openerp.addons.nfe.sped.nfe.validator.xml import XMLValidator
@@ -34,11 +35,15 @@ from openerp.addons.nfe.sped.nfe.validator.config_check import \
 _logger = logging.getLogger(__name__)
 
 
-class AccountInvoice(orm.Model):
+class AccountInvoice(models.Model):
     """account_invoice overwritten methods"""
     _inherit = 'account.invoice'
 
-    def attach_file_event(self, cr, uid, ids, seq, att_type, ext, context):
+    cce_document_event_ids = fields.One2many(
+        'l10n_br_account.invoice.cce', 'invoice_id', u'Eventos')
+
+    @api.multi
+    def attach_file_event(self, seq, att_type, ext):
         """
         Implemente esse metodo na sua classe de manipulação de arquivos
         :param cr:
@@ -55,37 +60,32 @@ class AccountInvoice(orm.Model):
     def _get_nfe_factory(self, nfe_version):
         return NfeFactory().get_nfe(nfe_version)
 
-    def nfe_export(self, cr, uid, ids, context=None):
-        user_pool = self.pool.get('res.users')
-        if not context:
-            context = user_pool.context_get(cr, uid, context)
+    @api.multi
+    def nfe_export(self):
 
-        for inv in self.browse(cr, uid, ids, context):
+        for inv in self:
 
-            company_pool = self.pool.get('res.company')
-            company = company_pool.browse(cr, uid, inv.company_id.id)
-
-            validate_nfe_configuration(company)
+            validate_nfe_configuration(inv.company_id)
 
             nfe_obj = self._get_nfe_factory(inv.nfe_version)
 
             # nfe_obj = NFe310()
-            nfes = nfe_obj.get_xml(
-                cr, uid, ids, int(
-                    company.nfe_environment), context)
+            nfes = nfe_obj.get_xml(self.env.cr, self.env.uid, self.ids,
+                                   int(inv.company_id.nfe_environment),
+                                   self.env.context)
 
             for nfe in nfes:
                 # erro = nfe_obj.validation(nfe['nfe'])
                 erro = XMLValidator.validation(nfe['nfe'], nfe_obj)
                 nfe_key = nfe['key'][3:]
                 if erro:
-                    raise orm.except_orm(
-                        _(u'Erro na validação da NFe!'), erro)
+                    raise RedirectWarning(
+                        erro, _(u'Erro na validaço da NFe!'))
 
-                self.write(cr, uid, inv.id, {'nfe_access_key': nfe_key})
+                inv.write({'nfe_access_key': nfe_key})
                 save_dir = os.path.join(
                     monta_caminho_nfe(
-                        company,
+                        inv.company_id,
                         chave_nfe=nfe_key) +
                     'tmp/')
                 nfe_file = nfe['nfe'].encode('utf8')
@@ -96,44 +96,37 @@ class AccountInvoice(orm.Model):
                         os.makedirs(save_dir)
                     f = open(file_path, 'w')
                 except IOError:
-                    raise orm.except_orm(
-                        _(u'Erro!'),
-                        _(u"""Não foi possível salvar o arquivo em disco,
-                            verifique as permissões de escrita e o caminho
-                            da pasta"""))
+                    raise RedirectWarning(
+                        _(u'Erro!'), _(u"""Não foi possível salvar o arquivo
+                            em disco, verifique as permissões de escrita
+                            e o caminho da pasta"""))
                 else:
                     f.write(nfe_file)
                     f.close()
 
-                    event_obj = self.pool.get('l10n_br_account.document_event')
-                    event_obj.create(cr, uid, {
+                    event_obj = self.env['l10n_br_account.document_event']
+                    event_obj.create({
                         'type': '0',
-                        'company_id': company.id,
+                        'company_id': inv.company_id.id,
                         'origin': '[NF-E]' + inv.internal_number,
                         'file_sent': file_path,
                         'create_date': datetime.datetime.now(),
                         'state': 'draft',
                         'document_event_ids': inv.id
-                    }, context)
-                    self.write(
-                        cr, uid, ids, {
-                            'state': 'sefaz_export'}, context=context)
+                    })
+                    inv.write({'state': 'sefaz_export'})
 
-    def action_invoice_send_nfe(self, cr, uid, ids, context=None):
+    @api.multi
+    def action_invoice_send_nfe(self):
 
-        for inv in self.browse(cr, uid, ids):
+        for inv in self:
 
-            company_pool = self.pool.get('res.company')
-            company = company_pool.browse(cr, uid, inv.company_id.id)
-            event_obj = self.pool.get('l10n_br_account.document_event')
+            event_obj = self.env['l10n_br_account.document_event']
             event = max(
-                event_obj.search(
-                    cr, uid, [
+                event_obj.search([
                         ('document_event_ids', '=', inv.id),
-                        ('type', '=', '0')],
-                    context=context))
-            send_event = event_obj.browse(cr, uid, [event])[0]
-            arquivo = send_event.file_sent
+                        ('type', '=', '0')]))
+            arquivo = event.file_sent
             nfe_obj = self._get_nfe_factory(inv.nfe_version)
 
             nfe = []
@@ -145,12 +138,12 @@ class AccountInvoice(orm.Model):
             protNFe["nfe_protocol_number"] = ''
             try:
                 nfe.append(nfe_obj.set_xml(arquivo))
-                for processo in send(company, nfe):
+                for processo in send(inv.company_id, nfe):
                     vals = {
                         'type': str(processo.webservice),
                         'status': processo.resposta.cStat.valor,
                         'response': '',
-                        'company_id': company.id,
+                        'company_id': inv.company_id.id,
                         'origin': '[NF-E]' + inv.internal_number,
                         # TODO: Manipular os arquivos manualmente
                         # 'file_sent': processo.arquivos[0]['arquivo'],
@@ -172,19 +165,15 @@ class AccountInvoice(orm.Model):
                             elif prot.infProt.cStat.valor in ('110', '301',
                                                               '302'):
                                 protNFe["state"] = 'sefaz_denied'
-                        self.attach_file_event(
-                            cr, uid, [
-                                inv.id], None, 'nfe', 'xml', context)
-                        self.attach_file_event(
-                            cr, uid, [
-                                inv.id], None, None, 'pdf', context)
+                        self.attach_file_event(None, 'nfe', 'xml')
+                        self.attach_file_event(None, None, 'pdf')
             except Exception as e:
                 _logger.error(e.message, exc_info=True)
                 vals = {
                     'type': '-1',
                     'status': '000',
                     'response': 'response',
-                    'company_id': company.id,
+                    'company_id': self.company_id.id,
                     'origin': '[NF-E]' + inv.internal_number,
                     'file_sent': 'False',
                     'file_returned': 'False',
@@ -196,62 +185,55 @@ class AccountInvoice(orm.Model):
             finally:
                 for result in results:
                     if result['type'] == '0':
-                        event_obj.write(
-                            cr, uid, inv.account_document_event_ids[0].id,
-                            result, context)
+                        event_obj.write(result)
                     else:
-                        event_obj.create(cr, uid, result, context)
+                        event_obj.create(result)
 
                 self.write(cr, uid, inv.id, {
-                    'nfe_status': protNFe["status_code"] + ' - ' +
-                    protNFe["message"],
+                    'nfe_status': protNFe["status_code"] + ' - '
+                    + protNFe["message"],
                     'nfe_date': datetime.datetime.now(),
                     'state': protNFe["state"],
                     'nfe_protocol_number': protNFe["nfe_protocol_number"],
-                }, context)
+                })
         return True
 
     def button_cancel(self, cr, uid, ids, context=None):
+        assert len(ids) == 1, ('This option should only be used for a single '
+                               'id at a time.')
+        if context is None:
+            context = {}
+        inv = self.browse(cr, uid, ids[0], context=context)
 
-        document_serie_id = inv.document_serie_id
-        fiscal_document_id = inv.document_serie_id.fiscal_document_id
-        electronic = inv.document_serie_id.fiscal_document_id.electronic
-        nfe_protocol = inv.nfe_protocol_number
+        document_serie_id = self.document_serie_id
+        fiscal_document_id = self.document_serie_id.fiscal_document_id
+        electronic = self.document_serie_id.fiscal_document_id.electronic
+        nfe_protocol = self.nfe_protocol_number
 
         if ((document_serie_id and fiscal_document_id and not electronic) or
                 not nfe_protocol):
             return super(AccountInvoice, self).action_cancel(cr, uid,
-                                                       [inv.id], context)
+                                                             [inv.id], context)
         else:
             ctx = dict(context)
-            result = self.pool.get('ir.actions.act_window').for_xml_id(
-                cr, uid, 'nfe',
-                'action_nfe_invoice_cancel_form', context)
+            result = self.env['ir.actions.act_window'].for_xml_id(
+                'nfe',
+                'action_nfe_invoice_cancel_form')
             return result
 
     def cancel_invoice_online(self, cr, uid, ids, justificative, context=None):
-        
+        event_obj = self.pool.get('l10n_br_account.document_event')
+
         for inv in self.browse(cr, uid, ids, context):
-
-            document_serie_id = inv.document_serie_id
-            fiscal_document_id = inv.document_serie_id.fiscal_document_id
-            electronic = inv.document_serie_id.fiscal_document_id.electronic
-
-            if (document_serie_id and fiscal_document_id and not electronic):
-                return False
-                      
-            event_obj = self.pool.get('l10n_br_account.document_event')
             if inv.state in ('open', 'paid'):
-                company_pool = self.pool.get('res.company')
-                company = company_pool.browse(cr, uid, inv.company_id.id)
 
-                validate_nfe_configuration(company)
+                validate_nfe_configuration(self.company_id)
                 validate_invoice_cancel(inv)
 
                 results = []
                 try:
                     processo = cancel(
-                        company,
+                        self.company_id,
                         inv.nfe_access_key,
                         inv.nfe_protocol_number,
                         justificative)
@@ -259,15 +241,13 @@ class AccountInvoice(orm.Model):
                         'type': str(processo.webservice),
                         'status': processo.resposta.cStat.valor,
                         'response': '',
-                        'company_id': company.id,
+                        'company_id': self.company_id.id,
                         'origin': '[NF-E] {0}'.format(inv.internal_number),
                         'message': processo.resposta.xMotivo.valor,
                         'state': 'done',
                         'document_event_ids': inv.id}
 
-                    self.attach_file_event(
-                        cr, uid, [
-                            inv.id], None, 'can', 'xml', context)
+                    self.attach_file_event(None, 'can', 'xml')
                     for prot in processo.resposta.retEvento:
                         vals["status"] = prot.infEvento.cStat.valor
                         vals["message"] = prot.infEvento.xEvento.valor
@@ -281,21 +261,18 @@ class AccountInvoice(orm.Model):
                                               # homologado fora de prazo
                                               '155'):  # Cancelamento
                                               # homologado fora de prazo
-                            result = super(
-                                AccountInvoice, self).action_cancel(
-                                cr, uid, [inv.id], context)
+                            # Fixme:
+                            result = super(AccountInvoice, self).action_cancel()
                             if result:
-                                self.write(
-                                    cr, uid, [inv.id], {
+                                self.write({
                                         'state': 'sefaz_cancelled',
                                         'nfe_status': vals["status"] +
                                         ' - ' + vals["message"]
                                     })
-                                obj_cancel = self.pool.get(
-                                    'l10n_br_account.invoice.cancel')
-                                obj_cancel.create(
-                                    cr, uid,
-                                    {'invoice_id': inv.id,
+                                obj_cancel = self.env[
+                                    'l10n_br_account.invoice.cancel']
+                                obj_cancel.create({
+                                     'invoice_id': inv.id,
                                      'justificative': justificative,
                                      })
                     results.append(vals)
@@ -305,7 +282,7 @@ class AccountInvoice(orm.Model):
                         'type': '-1',
                         'status': '000',
                         'response': 'response',
-                        'company_id': company.id,
+                        'company_id': self.company_id.id,
                         'origin': '[NF-E] {0}'.format(inv.internal_number),
                         'file_sent': 'OpenFalse',
                         'file_returned': 'False',
@@ -316,7 +293,7 @@ class AccountInvoice(orm.Model):
                     results.append(vals)
                 finally:
                     for result in results:
-                        event_obj.create(cr, uid, result)
+                        event_obj.create(result)
 
             elif inv.state in ('sefaz_export', 'sefaz_exception'):
                 _logger.error(
@@ -325,26 +302,26 @@ class AccountInvoice(orm.Model):
                 # TODO
         return
 
-    def invoice_print(self, cr, uid, ids, context=None):
+    @api.multi
+    def invoice_print(self):
 
-        for inv in self.browse(cr, uid, ids, context):
+        for inv in self:
 
             document_serie_id = inv.document_serie_id
             fiscal_document_id = inv.document_serie_id.fiscal_document_id
             electronic = inv.document_serie_id.fiscal_document_id.electronic
 
-            if (document_serie_id and fiscal_document_id and not electronic):
-                return super(AccountInvoice, self).invoice_print(
-                    cr, uid, ids, context=context)
+            if document_serie_id and fiscal_document_id and not electronic:
+                return super(AccountInvoice, self).invoice_print()
 
-            assert len(ids) == 1, 'This option should only be ' \
-                                  'used for a single id at a time.'
+            assert len(inv.ids) == 1, 'This option should only be ' \
+                                      'used for a single id at a time.'
 
-            self.write(cr, uid, ids, {'sent': True}, context=context)
+            self.write({'sent': True})
             datas = {
-                'ids': ids,
+                'ids': inv.ids,
                 'model': 'account.invoice',
-                'form': self.read(cr, uid, ids[0], context=context)
+                'form': self.read(inv.id)
             }
 
             return {
