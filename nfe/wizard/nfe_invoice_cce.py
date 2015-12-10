0cd775e7a2f8bc8cf0256f92ebb82f8c217153bb
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2015-12-10 18:08:03 -0200

    [MIGR] migrate nfe_invoice_cce

--- nfe/wizard/nfe_invoice_cce.py
+++ nfe/wizard/nfe_invoice_cce.py
@@ -19,21 +19,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-from openerp.osv import fields, orm
+from openerp import models, fields, api
 from openerp.addons.nfe.sped.nfe.processing.xml import send_correction_letter
 
 
-class NfeInvoiceCce(orm.TransientModel):
+class NfeInvoiceCce(models.TransientModel):
 
     _name = 'nfe.invoice_cce'
 
-    _columns = {
-        'mensagem': fields.text('Mensagem', required=True),
-    }
+    mensagem = fields.Text('Mensagem', required=True)
 
-    def _check_name(self, cr, uid, ids):
+    @api.multi
+    def _check_name(self):
 
-        for nfe in self.browse(cr, uid, ids):
+        for nfe in self:
 
             if not (len(nfe.mensagem) >= 15):
                 return False
@@ -45,29 +44,24 @@ class NfeInvoiceCce(orm.TransientModel):
          'Tamanho de mensagem inválida !',
          ['mensagem'])]
 
-    def action_enviar_carta(self, cr, uid, ids, context=None):
+    @api.multi
+    def action_enviar_carta(self):
 
-        if context is None:
-            context = {}
+        correcao = self.mensagem
+        obj_invoice = self.env['account.invoice'].browse(
+            self.env.context['active_id'])
+        obj_cce = self.env['l10n_br_account.invoice.cce']
 
-        correcao = self.browse(cr, uid, ids)[0].mensagem
-
-        obj_invoice = self.pool.get('account.invoice')
-        obj_cce = self.pool.get('l10n_br_account.invoice.cce')
-        invoice_ids = context and context.get('active_ids') or []
-
-        for invoice in obj_invoice.browse(cr, uid, invoice_ids):
+        for invoice in obj_invoice:
             chave_nfe = invoice.nfe_access_key
 
-            company_pool = self.pool.get('res.company')
-            company = company_pool.browse(cr, uid, invoice.company_id.id)
-            event_obj = self.pool.get('l10n_br_account.document_event')
+            event_obj = self.env['l10n_br_account.document_event']
             domain = [('invoice_id', '=', invoice.id)]
-            sequencia = len(obj_cce.search(cr, uid, domain)) + 1
+            sequencia = len(obj_cce.search(domain)) + 1
             results = []
             try:
                 processo = send_correction_letter(
-                    company,
+                    invoice.company_id,
                     chave_nfe,
                     sequencia,
                     correcao)
@@ -76,7 +70,7 @@ class NfeInvoiceCce(orm.TransientModel):
                     'status': processo.resposta.retEvento[0].infEvento.
                     cStat.valor,
                     'response': '',
-                    'company_id': company.id,
+                    'company_id': invoice.company_id.id,
                     'origin': '[CC-E] ' + str(invoice.internal_number),
                     'message': processo.resposta.retEvento[0].infEvento.
                     xEvento.valor,
@@ -84,20 +78,16 @@ class NfeInvoiceCce(orm.TransientModel):
                     'document_event_ids': invoice.id}
                 results.append(vals)
                 obj_invoice.attach_file_event(
-                    cr,
-                    uid,
-                    invoice_ids,
                     sequencia,
                     'cce',
-                    'xml',
-                    context)
+                    'xml')
 
             except Exception as e:
                 vals = {
                     'type': '-1',
                     'status': '000',
                     'response': 'response',
-                    'company_id': company.id,
+                    'company_id': invoice.company_id.id,
                     'origin': '[CC-E]' + str(invoice.internal_number),
                     'file_sent': 'False',
                     'file_returned': 'False',
@@ -108,11 +98,9 @@ class NfeInvoiceCce(orm.TransientModel):
                 results.append(vals)
             finally:
                 for result in results:
-                    event_obj.create(cr, uid, result)
-                    obj_cce.create(cr, uid,
-                                   {'invoice_id': invoice.id,
+                    event_obj.create(result)
+                    obj_cce.create({'invoice_id': invoice.id,
                                     'motivo': correcao,
                                     'sequencia': sequencia,
-                                    }
-                                   )
+                                    })
         return {'type': 'ir.actions.act_window_close'}
