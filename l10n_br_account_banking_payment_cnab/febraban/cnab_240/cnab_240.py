dbc390e6de1ed82256bde405ab3a7f4ee8f67e6e
fernandomr <fernandofmr92@gmail.com>
2015-12-16 09:26:06 -0200

    dígito verificador agencia/conta

--- l10n_br_account_banking_payment_cnab/febraban/cnab_240/cnab_240.py
+++ l10n_br_account_banking_payment_cnab/febraban/cnab_240/cnab_240.py
@@ -88,7 +88,7 @@ class Cnab240(Cnab):
                 self.order.mode.bank_id.bra_number_dig,
             'cedente_nome': self.order.company_id.legal_name,
             # DV ag e conta
-            'cedente_dv_ag_cc': u"5",  # FIXME
+            'cedente_dv_ag_cc': (self.order.mode.bank_id.bra_acc_dig),
             'arquivo_codigo': 1,  # Remessa/Retorno
             'servico_operacao': u'R',
             'nome_banco': unicode(self.order.mode.bank_id.bank_name),
--- l10n_br_account_banking_payment_cnab/model/__init__.py
+++ l10n_br_account_banking_payment_cnab/model/__init__.py
@@ -3,3 +3,4 @@
 from . import payment_order
 from . import l10n_br_CnabSequence
 from . import payment_mode
+from . import res_partner_bank
