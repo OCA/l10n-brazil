9b6f95c819f07127cf6946531b11d42ff1ce4d29
fernandomr <fernandofmr92@gmail.com>
2015-12-16 09:26:06 -0200

    dígito verificador agencia/conta

--- l10n_br_account_banking_payment/l10n_br_account_banking_payment_cnab/febraban/cnab_240/cnab_240.py
+++ l10n_br_account_banking_payment/l10n_br_account_banking_payment_cnab/febraban/cnab_240/cnab_240.py
@@ -141,16 +141,21 @@ class Cnab240(Cnab):
             'arquivo_hora_de_geracao': self.hora_agora(),
             # 19.0 TODO: Número sequencial de arquivo
             'arquivo_sequencia': int(self.get_file_numeration()),
-            # 20.0
-            'arquivo_layout': 103,
-            # 21.0
-            'arquivo_densidade': 0,
-            # 22.0
-            'reservado_banco': '',
-            # 23.0
-            'reservado_empresa': 'EMPRESA 100',
-            # 24.0
-            # CNAB - Uso Exclusivo FEBRABAN / CNAB
+            'cedente_inscricao_tipo': self.inscricao_tipo,
+            'cedente_inscricao_numero': int(punctuation_rm(
+                self.order.company_id.cnpj_cpf)),
+            'cedente_agencia': int(
+                self.order.mode.bank_id.bra_number),
+            'cedente_conta': int(self.order.mode.bank_id.acc_number),
+            'cedente_conta_dv': (self.order.mode.bank_id.acc_number_dig),
+            'cedente_agencia_dv':
+                self.order.mode.bank_id.bra_number_dig,
+            'cedente_nome': self.order.company_id.legal_name,
+            # DV ag e conta
+            'cedente_dv_ag_cc': (self.order.mode.bank_id.bra_acc_dig),
+            'arquivo_codigo': 1,  # Remessa/Retorno
+            'servico_operacao': u'R',
+            'nome_banco': unicode(self.order.mode.bank_id.bank_name),
         }
 
         return header_arquivo
@@ -275,7 +280,10 @@ class Cnab240(Cnab):
             'cedente_agencia': int(self.order.mode.bank_id.bra_number),
             'cedente_conta': int(self.order.mode.bank_id.acc_number),
             'cedente_conta_dv': self.order.mode.bank_id.acc_number_dig,
-            'cedente_agencia_dv': self.order.mode.bank_id.bra_number_dig,
+            'cedente_agencia_dv':
+                self.order.mode.bank_id.bra_number_dig,
+            # DV ag e cc
+            'cedente_dv_ag_cc': (self.order.mode.bank_id.bra_acc_dig),
             'identificacao_titulo': u'0000000',  # TODO
             'identificacao_titulo_banco': u'0000000',  # TODO
             'identificacao_titulo_empresa': line.move_line_id.move_id.name,
