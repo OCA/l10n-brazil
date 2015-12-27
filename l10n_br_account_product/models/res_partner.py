a8ff190e526a46086c42f52801ae39d68162506e
Luis Felipe Mileo <mileo@kmee.com.br>
2015-12-27 21:29:44 -0200

    [NEW] product origin in fiscal position tax map

--- l10n_br_account_product/models/res_partner.py
+++ l10n_br_account_product/models/res_partner.py
@@ -18,6 +18,8 @@
 ###############################################################################
 
 from openerp import models, fields, api
+from openerp.addons.l10n_br_account_product.models.product import \
+    PRODUCT_ORIGIN
 
 
 class AccountFiscalPositionTemplate(models.Model):
@@ -37,7 +39,7 @@ class AccountFiscalPositionTaxTemplate(models.Model):
 
     fiscal_classification_id = fields.Many2one(
         'account.product.fiscal.classification.template', 'NCM')
-
+    origin = fields.Selection(PRODUCT_ORIGIN, 'Origem',)
 
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
@@ -177,3 +179,4 @@ class AccountFiscalPositionTax(models.Model):
 
     fiscal_classification_id = fields.Many2one(
         'account.product.fiscal.classification', 'NCM')
+    origin = fields.Selection(PRODUCT_ORIGIN, 'Origem',)
