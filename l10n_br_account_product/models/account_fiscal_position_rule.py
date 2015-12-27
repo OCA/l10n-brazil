9ed27a2742d1fae7ede8d275e26691115152ed02
Luis Felipe Mileo <mileo@kmee.com.br>
2015-12-27 21:01:31 -0200

    [NEW] account_fiscal_position_rule: product_fcp_map

--- l10n_br_account_product/models/account_fiscal_position_rule.py
+++ l10n_br_account_product/models/account_fiscal_position_rule.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2015  Renato Lima - Akretion                                  #
+# Copyright (C) 2015 - Luis Felipe Miléo - KMEE                               #
 #                                                                             #
 # This program is free software: you can redistribute it and/or modify        #
 # it under the terms of the GNU Affero General Public License as published by #
@@ -17,13 +17,25 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 ###############################################################################
 
-from . import res_company
-from . import l10n_br_account_product
-from . import l10n_br_account
-from . import account_product_fiscal_classification
-from . import account_invoice
-from . import account
-from . import product
-from . import res_partner
-from . import res_country
-from . import account_fiscal_position_rule
+import time
+
+from openerp import models, fields, api
+
+
+class AccountFiscalPositionRule(models.Model):
+    _inherit = 'account.fiscal.position.rule'
+
+    def product_fcp_map(self, product_id, to_state=None):
+        result = self.env['account.tax']
+        fiscal_classification_id = self.env['product.product'].browse(
+            product_id).fiscal_classification_id
+        fcp = self.env[
+            'l10n_br_tax.fcp'].search(
+            [('fiscal_classification_id', '=', fiscal_classification_id.id),
+             '|', ('to_state_id', '=', False),
+             ('to_state_id', '=', to_state.id)])
+        if fcp:
+            result |= fcp.fcp_tax_id
+        else:
+            result |= to_state.fcp_tax_id
+        return result
