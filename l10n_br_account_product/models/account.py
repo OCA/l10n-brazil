8b97572453e0fb812d669977a5a05ca307182fdf
Luis Felipe Mileo <mileo@kmee.com.br>
2015-12-28 02:41:14 -0200

    [NEW] Calculo diferencial do ICMS e FCP

--- l10n_br_account_product/models/account.py
+++ l10n_br_account_product/models/account.py
@@ -3,6 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, fields, api
+from openerp.exceptions import Warning as UserError
 
 
 class AccountPaymentTerm(models.Model):
@@ -142,7 +143,7 @@ class AccountTax(models.Model):
                 tax['icms_st_base_type'] = tax_brw.icms_st_base_type
 
         common_taxes = [tx for tx in result['taxes'] if tx[
-            'domain'] not in ['icms', 'icmsst', 'ipi']]
+            'domain'] not in ['icms', 'icmsst', 'ipi', 'icmsinter']]
         result_tax = self._compute_tax(cr, uid, common_taxes, result['total'],
                                        product, quantity, precision, base_tax)
         totaldc += result_tax['tax_discount']
@@ -152,6 +153,11 @@ class AccountTax(models.Model):
         specific_ipi = [tx for tx in result['taxes'] if tx['domain'] == 'ipi']
         result_ipi = self._compute_tax(cr, uid, specific_ipi, result['total'],
                                        product, quantity, precision, base_tax)
+        # Calcula a FCP
+        specific_fcp = [tx for tx in result['taxes']
+                        if tx['domain'] == 'icmsfcp']
+        result_fcp = self._compute_tax(cr, uid, specific_fcp, result['total'],
+                                       product, quantity, precision, base_tax)
         totaldc += result_ipi['tax_discount']
         calculed_taxes += result_ipi['taxes']
         for ipi in result_ipi['taxes']:
@@ -160,9 +166,72 @@ class AccountTax(models.Model):
         # Calcula ICMS
         specific_icms = [tx for tx in result['taxes']
                          if tx['domain'] == 'icms']
+        difa = {}
         if fiscal_position and fiscal_position.asset_operation:
             total_base = result['total'] + insurance_value + \
                 freight_value + other_costs_value + ipi_value
+
+            # Para operações fora do estado e aquisição de ativo
+            # TODO: Verificar se existem outras condições que
+            # devem passar por este trecho
+            if fiscal_position and fiscal_position.cfop_id.id_dest == '2':
+                specific_icms_inter = [tx for tx in result['taxes']
+                                 if tx['domain'] == 'icmsinter']
+                try:
+                # Devido as operações com redução de base devemos verificar os
+                # totais da operação
+                    if (specific_icms_inter[0]['amount'] == \
+                            specific_icms[0]['amount']):
+                        pass
+                    elif (specific_icms_inter[0]['amount'] <
+                              specific_icms[0]['amount']):
+                        aux = specific_icms_inter[0]
+                        specific_icms_inter[0] = specific_icms[0]['amount']
+                        specific_icms[0]['amount'] = aux
+                    else:
+                        # BASE UNICA
+                        difa['vBCUFDest'] = total_base
+                        #ICMS origem = [BC x ALQ INTER]
+                        difa['pICMSUFDest'] = specific_icms_inter[0]['percent']
+                        #TODO mapear percentual com l10n_br_tax.icms_partition
+                        difa['pICMSInterPart'] = 0.40
+                        #ICMS interno Destino = [BC x ALQ intra]
+                        difa['pICMSInter'] = specific_icms[0]['percent']
+                        #ICMS destino = [BC x ALQ intra] - ICMS origem
+                        icms_difa = ((difa['vBCUFDest'] * difa['pICMSUFDest'])-
+                                     (difa['vBCUFDest'] * difa['pICMSInter']))
+                        if result_fcp['taxes']:
+                            # % Fundo pobreza
+                            difa['pFCPUFDest'] = \
+                                result_fcp['taxes'][0]['percent']
+                            difa['vFCPUFDest'] = \
+                                difa['vBCUFDest'] * difa['pFCPUFDest']
+                            # FIXME: O ICMS deve ter o FCP incluso?
+                            icms_difa -= difa['vFCPUFDest']
+                        difa['vICMSUFDest'] = \
+                            icms_difa * difa['pICMSInterPart']
+                        difa['vICMSUFRemet'] = \
+                            icms_difa * (1-difa['pICMSInterPart'])
+                        specific_icms_inter[0]['percent'] = \
+                            difa['pICMSUFDest'] - difa['pICMSInter']
+                        #FIXME: Criar um lancaçamento para cada % rateado
+                        specific_icms_inter[0]['amount'] = icms_difa
+                        result_icms_inter = self._compute_tax(
+                            cr,
+                            uid,
+                            specific_icms_inter,
+                            total_base,
+                            product,
+                            quantity,
+                            precision,
+                            base_tax)
+                        totaldc += result_icms_inter['tax_discount']
+                        calculed_taxes += result_icms_inter['taxes']
+
+                except:
+                    raise UserError(u'Tributação do ICMS para a UF de destino',
+                              u'Configurada incorretamente')
+
         else:
             total_base = result['total'] + insurance_value + \
                 freight_value + other_costs_value
