0ebc4c972d6783325823a6006d58d2547dfa9bf3
yk2kus <yogeshkushwaha4@gmail.com>
2016-01-14 17:02:59 -0200

    add fiscal_comment in view

--- l10n_br_account_product/models/account_invoice.py
+++ l10n_br_account_product/models/account_invoice.py
@@ -714,7 +714,6 @@ class AccountInvoiceLine(models.Model):
         'Outros Custos', digits=dp.get_precision('Account'), default=0.00)
     freight_value = fields.Float(
         'Frete', digits=dp.get_precision('Account'), default=0.00)
-    fiscal_comment = fields.Text(u'Observação Fiscal')
 
     def _amount_tax_icms(self, tax=None):
         result = {
