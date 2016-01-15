2e58ddee4b13994352c482a186c355ddd85615a7
Luis Felipe Mileo <mileo@kmee.com.br>
2016-01-15 02:28:39 -0200

    [REF] Removing view and model [ADD] New features in pag for

--- l10n_br_account_banking_payment/l10n_br_account_banking_payment_cnab/febraban/pag_for/pag_for500.py
+++ l10n_br_account_banking_payment/l10n_br_account_banking_payment_cnab/febraban/pag_for/pag_for500.py
@@ -24,12 +24,126 @@ from ..cnab import Cnab
 from cnab240.tipos import Arquivo
 from decimal import Decimal
 from openerp.addons.l10n_br_base.tools.misc import punctuation_rm
+from pyboleto.data import BoletoData
 import datetime
 import re
 import string
 import unicodedata
 import time
 
+
+TIPO_CONTA_FORNECEDOR = [
+    ('1', u'Conta corrente'),
+    ('2', u'Conta Poupança'),
+]
+
+TIPO_DOCUMENTO = [
+    ('01', u'Nota Fiscal/Fatura'),
+    ('02', u'Fatura'),
+    ('03', u'Nota Fiscal'),
+    ('01', u'Duplicata'),
+    ('01', u'Outros'),
+]
+
+MODALIDADE = [
+    ('01', u'01 - Crédito em conta-corrente ou poupança Bradesco'),
+    ('02', u'02 - Cheque OP ( Ordem de Pagamento'),
+    ('03', u'03 - DOC COMPE'),
+    ('05', u'05 - Crédito em conta real time'),
+    ('08', u'08 - TED'),
+    ('30', u'30 - Rastreamento de Títulos'),
+    ('31', u'31 - Títulos de terceiros'),
+]
+
+TIPO_MOVIMENTO = [
+    ('0', u'Inclusão'),
+    ('5', u'Alteração'),
+    ('9', u'Exclusão'),
+]
+
+CODIGO_MOVIMENTO = [
+    ('00', u'Autoriza agendamento'),
+    ('25', u'Desautoriza Agendamento'),
+    ('50', u'Efetuar Alegçação'),
+]
+
+TIPO_DOC = [
+    ('C', u'Titularidade Diferente'),
+    ('D', u'Mesma Titularidade'),
+]
+
+FINALIDADE_DOC_TED = [
+    ('01', u'Crédito em Conta Corrente'),
+    ('02', u'Pagamento de Aluguel /Condomínios'),
+    ('03', u'Pagamento de Duplicatas/Títulos'),
+    ('04', u'Pagamento de Dividendos'),
+    ('05', u'Pagamento de Mensal. Escolares'),
+    ('06', u'Pagamento de Salário'),
+    ('07', u'Pagamento de Fornec/Honor.'),
+    ('08', u'Operações de Câmbio /Fundos /Bolsa de Valores'),
+    ('09', u'Repasse de Arrec./Pagto de Tributos'),
+    ('10', u'Transferência Internacional em Reais'),
+    ('11', u'DOC COMPE/TED para Poupança'),
+    ('12', u'DOC COMPE/TED para Depósito Judicial'),
+    ('13', u'Pensão Alimentícia'),
+    ('14', u'Restituição de Imposto de Renda'),
+    ('18', u'Operações Seguro Habit.', u'SFH'),
+    ('19', u'Operações do FDS', u'Caixa'),
+    ('20', u'Pagamento De Operação De Crédito'),
+    ('23', u'Taxa de Administração'),
+    ('27', u'Pagamento Acordo/Execução Judicial'),
+    ('28', u'Liquidação de Empréstimos Consignados'),
+    ('29', u'Pagamento de Bolsa Auxilio'),
+    ('30', u'Remuneração A Cooperado'),
+    ('31', u'Pagamento de Prebenda'),
+    ('33', u'Pagamento de Juros sobre Capital Próprio'),
+    ('34', u'Pagamento de Rendimentos ou Amortização s/ Cotas '
+           u'e/ou Debêntures'),
+    ('35', u'Taxa De Serviço'),
+    ('37', u'Pagamento de Juros e/ou Amortização de Títulos '
+           u'Depositados em Garantia.'),
+    ('38', u'Estorno Ou Restituição', u'Diversos'),
+    ('59', u'Restituição de Prêmios de Seguros'),
+    ('60', u'Pagamento de Indenização Sinistro Seguro'),
+    ('61', u'Pagamento de Premio de Co-Seguro'),
+    ('63', u'Pagamento de Indenização Sinistro Co-Seguro'),
+    ('64', u'Pagamento de Premio De Resseguro'),
+    ('65', u'Restituição de Premio De Resseguro'),
+    ('66', u'Pagamento de Indenização Sinistro Resseguro'),
+    ('67', u'Restituição Indenização Sinistro Resseguro'),
+    ('68', u'Pagamento de Despesas Com Sinistro'),
+    ('69', u'Pagamento de Inspeções/Vistorias Prévias'),
+    ('70', u'Pagamento de Resgate de Titulo de Capitalização'),
+    ('71', u'Pagamento de Sorteio de Titulo de Capitalização'),
+    ('72', u'Devolução Mensal de Titulo de Capitalização.'),
+    ('73', u'Restituição de Contribuição de Plano Previdenciário'),
+    ('74', u'Pagamento de Beneficio Previdenciário Pecúlio'),
+    ('75', u'Pagamento de Beneficio Previdenciário Pensão'),
+    ('76', u'Pagamento de Beneficio Previdenciário Aposentadoria'),
+    ('77', u'Pagamento de Resgate Previdenciário'),
+    ('78', u'Pagamento de Comissão de Corretagem'),
+    ('79', u'Pagamento de Transferências/Portabilidade de Reserva '
+           u'Seguro/Previdência'),
+    ('80', u'Pagamento de Impostos'),
+    ('81', u'Pagamento de Serviços Públicos'),
+    ('82', u'Pagamento de Honorários'),
+    ('83', u'Pagamento de Corretoras'),
+    ('84', u'Repasse de Valores BNDES'),
+    ('85', u'Liquidação de Compromissos com BNDES'),
+    ('86', u'Compra e Venda de Ações'),
+    ('87', u'Contratos Referenciados em Ações ou Índices de Ações'),
+    ('88', u'Operação De Cambio'),
+    ('89', u'Pagamento de Boleto Bancário em Cartório'),
+    ('90', u'Pagamento de Tarifas pela Prestação de Serviços de Arrecadação'
+           u' de Convênios'),
+    ('91', u'Operações no Mercado de Renda Fixa e Variável com Utilização '
+           u'de Intermediário'),
+    ('92', u'Operação de Câmbio Mercado Interbancário Instituições '
+           u'sem Reservas Bancárias'),
+    ('93', u'Pagamento de Operações com Identificação de Destinatário Final'),
+    ('94', u'Ordem Bancaria do Tesouro - OBT'),
+    ('99', u'Outros'),
+]
 
 class PagFor500(Cnab):
     """
@@ -237,3 +351,7 @@ class PagFor500(Cnab):
 
     def hora_agora(self):
         return (int(time.strftime("%H%M%S")))
+
+    @staticmethod
+    def modulo11(num, base, r):
+        return BoletoData.modulo11(num, base=9, r=0)
