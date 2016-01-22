eba13e6be45eb77925f051ee58c792134c32adfd
Renato Lima <renatonlima@gmail.com>
2016-01-22 17:53:32 -0200

    comment test

--- l10n_br_sale_service/tests/__init__.py
+++ l10n_br_sale_service/tests/__init__.py
@@ -17,4 +17,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-from . import test_sale_order_service
+# from . import test_sale_order_service
