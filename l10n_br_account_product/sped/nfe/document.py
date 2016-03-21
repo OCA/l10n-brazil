4bf0f3e9caa855405956ee08e62c7b01597c78c9
Luis Felipe Mileo <mileo@kmee.com.br>
2016-03-21 07:42:47 -0300

    [REF] Campo natureza da operacao referente ao nome da categoria fiscal

--- l10n_br_account_product/sped/nfe/document.py
+++ l10n_br_account_product/sped/nfe/document.py
@@ -115,7 +115,7 @@ class NFe200(FiscalDocument):
         self.nfe.infNFe.ide.cUF.valor = (company.state_id and
                                          company.state_id.ibge_code or '')
         self.nfe.infNFe.ide.cNF.valor = ''
-        self.nfe.infNFe.ide.natOp.valor = invoice.cfop_ids[0].small_name or ''
+        self.nfe.infNFe.ide.natOp.valor = invoice.fiscal_category_id.name or ''
         self.nfe.infNFe.ide.indPag.valor = (invoice.payment_term and
                                             invoice.payment_term.indPag or '0')
         self.nfe.infNFe.ide.mod.valor = invoice.fiscal_document_id.code or ''
