b38b7dbf06bd90d04fc80f31c105ad9c11a665ae
Luis Felipe Mileo <mileo@kmee.com.br>
2016-04-15 16:37:25 -0300

    [WIP][NEW] Stock Account Report

---
+++ l10n_br_stock_account/report/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 KMEE INFORMATICA LTDA (<http://www.kmee.com.br>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import stock_valuation_history_xls
