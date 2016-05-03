0bd84a42290169ba99d4f5e87dc778fb4cc8889a
Luis Felipe Mileo <mileo@kmee.com.br>
2016-05-04 02:59:22 +0800

    [NEW] Partner Special Type Conflicts: 	l10n_br_account/models/l10n_br_account.py

--- l10n_br_account/models/res_partner.py
+++ l10n_br_account/models/res_partner.py
@@ -325,6 +325,12 @@ class ResPartner(models.Model):
         domain="[('is_company', '=', is_company)]",
         default=_default_partner_fiscal_type_id)
 
+    partner_special_fiscal_type_id = fields.Many2many(
+        comodel_name='l10n_br_account.partner.special.fiscal.type',
+        relation='res_partner_l10n_br_special_type',
+        string='Regime especial'
+    )
+
     @api.multi
     def onchange_mask_cnpj_cpf(self, is_company, cnpj_cpf):
         result = super(ResPartner, self).onchange_mask_cnpj_cpf(
