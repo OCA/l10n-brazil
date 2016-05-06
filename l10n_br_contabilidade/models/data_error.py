b6b241702978cf57ea846a8725a03c75812d5e9e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-05-06 14:27:19 +0200

    make SimpleArray produce DataError in memberwise operations
    Put DataError and mis_safe_eval in their own files

---
+++ l10n_br_contabilidade/models/data_error.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# © 2016 Akretion (<http://akretion.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+class DataError(Exception):
+
+    def __init__(self, name, msg):
+        self.name = name
+        self.msg = msg
