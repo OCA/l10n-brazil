24c741fc81300dd59d0628c0f379cd1de85901c9
Luis Felipe Mileo <mileo@kmee.com.br>
2016-07-01 09:13:40 -0300

    [REF] Refatorcao do sat

--- local/addons/hw_sat/controllers/main.py
+++ local/addons/hw_sat/controllers/main.py
@@ -4,8 +4,6 @@ import time
 from threading import Thread, Lock
 from requests import ConnectionError
 from decimal import Decimal
-import tempfile
-
 import openerp.addons.hw_proxy.controllers.main as hw_proxy
 from openerp import http
 
@@ -50,19 +48,11 @@ class Sat(Thread):
         self.sat_path = sat_path
         self.impressora = impressora
         self.printer_params = printer_params
-        self.device = self.get_device()
-
         self.lock = Lock()
         self.satlock = Lock()
         self.status = {'status': 'connecting', 'messages': []}
-        self.input_dir = '/dev/serial/by-path/'
         self.printer = False
-        self.probed_device_paths = []
-        self.path_to_scale = ''
-
-    # def set_client_sat(self):
-    #     #TODO: Verificar se o sat e' hub.
-    #     return
+        self.device = self._get_device()
 
     def lockedstart(self):
         with self.lock:
@@ -95,37 +85,14 @@ class Sat(Thread):
             elif status == 'disconnected' and message:
                 _logger.warning('Disconnected SAT: '+message)
 
-    def get_device(self):
-        try:
-
-            if not self.sat_path and not self.codigo_ativacao:
-                self.set_status('error', 'Dados do sat incorretos')
-                return None
-
-            client_sat = ClienteSATLocal(
+    def _get_device(self):
+        if not self.sat_path and not self.codigo_ativacao:
+            self.set_status('error', 'Dados do sat incorretos')
+            return None
+        return ClienteSATLocal(
             BibliotecaSAT(self.sat_path),
             codigo_ativacao=self.codigo_ativacao
-            )
-
-            return client_sat
-
-        except ErroRespostaSATInvalida as resposta:
-            # o equipamento retornou uma resposta que não faz sentido;
-            # loga, e lança novamente ou lida de alguma maneira
-            self.set_status('error', 'SAT Not Found {0}'.format(
-                resposta))
-            return None
-        except ExcecaoRespostaSAT as resposta:
-            self.set_status('error', 'SAT Not Found {0}'.format(
-                resposta))
-            return None
-        except ConnectionError as resposta:
-            self.set_status('disconnected', 'SAT Not Found {0}'.format(
-                resposta))
-            return None
-        except Exception as ex:
-            self.set_status('error', str(ex))
-            return None
+        )
 
     def status_sat(self):
         with self.satlock:
@@ -133,7 +100,6 @@ class Sat(Thread):
                 try:
                     if self.device.consultar_sat():
                         self.set_status('connected', 'Connected to SAT')
-
                 except ErroRespostaSATInvalida as ex_sat_invalida:
                     # o equipamento retornou uma resposta que não faz sentido;
                     # loga, e lança novamente ou lida de alguma maneira
@@ -146,6 +112,101 @@ class Sat(Thread):
                 except Exception as ex:
                     self.set_status('error', str(ex))
                     self.device = None
+
+    def __prepare_send_detail_cfe(self, item):
+        return Detalhamento(
+            produto=ProdutoServico(
+                cProd=unicode(int),
+                xProd=item['product_name'],
+                CFOP='5102',
+                uCom=item['unit_name'],
+                qCom=Decimal(item['quantity']).quantize(FOURPLACES),
+                vUnCom=Decimal(item['price']).quantize(TWOPLACES),
+                indRegra='A'),
+            imposto=Imposto(
+                icms=ICMSSN102(Orig='2', CSOSN='500'),
+                pis=PISSN(CST='49'),
+                cofins=COFINSSN(CST='49'))
+        )
+
+    def __prepare_send_cfe(self, json):
+        detalhamentos = []
+        for item in json['orderlines']:
+            detalhamentos.append(self.__prepare_send_detail_cfe(item))
+
+        return CFeVenda(
+            CNPJ=json['company']['cnpj_software_house'],
+            signAC=constantes.ASSINATURA_AC_TESTE,
+            numeroCaixa=2,
+            emitente=Emitente(
+                CNPJ=json['company']['cnpj'],
+                IE=json['company']['ie'],
+                indRatISSQN='N'),
+            detalhamentos=detalhamentos,
+            pagamentos=[
+                MeioPagamento(
+                    cMP=constantes.WA03_DINHEIRO,
+                    vMP=Decimal(json['subtotal']).quantize(
+                        TWOPLACES)),
+            ]
+        )
+
+    def _send_cfe(self, json):
+        resposta = self.device.enviar_dados_venda(
+            self.__prepare_send_cfe(json))
+        self._print_extrato_venda(resposta.chaveConsulta, resposta.xml())
+        return {
+            'xml': resposta.arquivoCFeSAT,
+            'numSessao': resposta.numeroSessao,
+            'chave_cfe': resposta.chaveConsulta,
+        }
+
+    def __prepare_cancel_cfe(self, chCanc):
+        return CFeCancelamento(
+            chCanc=chCanc,
+            CNPJ='16716114000172',
+            signAC=constantes.ASSINATURA_AC_TESTE,
+            numeroCaixa=2
+        )
+
+    def _cancel_cfe(self, chave_cfe):
+        resposta = self.device.cancelar_ultima_venda(
+            chave_cfe,
+            self.__prepare_cancel_cfe(chave_cfe)
+        )
+        self._print_extrato_cancelamento(
+            resposta.chaveConsulta, resposta.xml())
+        return {
+            'xml': resposta.arquivoCFeSAT,
+            'numSessao': resposta.numeroSessao,
+            'chave_cfe': resposta.chaveConsulta,
+        }
+
+    def action_call_sat(self, task, json=False):
+
+        _logger.info('SAT: Task {0}'.format(task))
+
+        try:
+
+            if task == 'connect':
+                pass
+            elif task == 'get_device':
+                return self._get_device()
+            elif task == 'reprint':
+                return self._reprint_cfe(json)
+            elif task == 'send':
+                return self._send_cfe(json)
+            elif task == 'cancel':
+                return self._cancel_cfe(json)
+        except ErroRespostaSATInvalida as ex:
+            _logger.error('SAT Error: '+ex)
+            return {'excessao': ex}
+        except ExcecaoRespostaSAT as ex:
+            _logger.error('SAT Error: '+ex)
+            return {'excessao': ex}
+        except Exception as ex:
+            _logger.error('SAT Error: '+ex)
+            return {'excessao': ex}
 
     def _init_printer(self, config):
         from escpos.serial import SerialSettings
@@ -171,146 +232,33 @@ class Sat(Thread):
 
     def _print_extrato_venda(self, chaveConsulta, xml):
         if not self.printer:
-            return
+            return False
         file_path = '/tmp/' + chaveConsulta + '.xml'
         with open(file_path, 'w') as temp:
             temp.write(xml)
         with open(file_path, 'r') as fp:
             extrato = ExtratoCFeVenda(fp, self.printer)
             extrato.imprimir()
+        return True
 
     def _print_extrato_cancelamento(self, chaveConsulta, xml):
         if not self.printer:
-            return
+            return False
         file_path = '/tmp/' + chaveConsulta + '.xml'
         with open(file_path, 'w') as temp:
             temp.write(xml)
         with open(file_path, 'r') as fp:
             extrato = ExtratoCFeCancelamento(fp, self.printer)
             extrato.imprimir()
+        return True
 
-    def montar_cfe(self, json):
-        detalhamentos = []
-
-        for item in json['orderlines']:
-            detalhamentos.append(
-                Detalhamento(
-                    produto=ProdutoServico(
-                        cProd=unicode(int),
-                        xProd=item['product_name'],
-                        CFOP='5102',
-                        uCom=item['unit_name'],
-                        qCom=Decimal(item['quantity']).quantize(FOURPLACES),
-                        vUnCom=Decimal(item['price']).quantize(TWOPLACES),
-                        indRegra='A'),
-                    imposto=Imposto(
-                        icms=ICMSSN102(Orig='2', CSOSN='500'),
-                        pis=PISSN(CST='49'),
-                        cofins=COFINSSN(CST='49'))),
-            )
-
-        cfe = CFeVenda(
-            CNPJ=json['company']['cnpj_software_house'],
-            signAC=constantes.ASSINATURA_AC_TESTE,
-            numeroCaixa=2,
-            emitente=Emitente(
-                CNPJ=json['company']['cnpj'],
-                IE=json['company']['ie'],
-                indRatISSQN='N'),
-            detalhamentos=detalhamentos,
-            pagamentos=[
-                MeioPagamento(
-                    cMP=constantes.WA03_DINHEIRO,
-                    vMP=Decimal(json['subtotal']).quantize(
-                        TWOPLACES)),
-            ]
-        )
-        return cfe
-
-    def sat(self, json):
-        resposta = None
-        json_salvar_cfe = {
-            'xml': '',
-            'chaveCfe': '',
-            'numSessao': '',
-            'chave_cfe': '',
-            'excessao': False
-        }
-
-        try:
-            resposta = self.device.enviar_dados_venda(self.montar_cfe(json))
-            json_salvar_cfe = {
-                'xml': resposta.arquivoCFeSAT,
-                'numSessao': resposta.numeroSessao,
-                'chave_cfe': resposta.chaveConsulta
-            }
-            self._print_extrato_venda(resposta.chaveConsulta, resposta.xml())
-
-        except ErroRespostaSATInvalida as ex_sat_invalida:
-            json_salvar_cfe = {
-                'excessao': ex_sat_invalida
-            }
-
-        except ExcecaoRespostaSAT as ex_resposta:
-            json_salvar_cfe = {
-                'excessao': ex_resposta
-            }
-
-        except Exception as ex:
-            json_salvar_cfe = {
-                'excessao': ex
-            }
-
-        return json_salvar_cfe
-
-    def montar_xml_cfe_cancelamento(self, chCanc):
-        cfecanc = CFeCancelamento(
-            chCanc=chCanc,
-            CNPJ='16716114000172',
-            signAC=constantes.ASSINATURA_AC_TESTE,
-            numeroCaixa=2
-        )
-        return cfecanc
-
-    def sat_cancelar_cfe(self, chave_cfe):
-        json_salvar_cancelamento_cfe = {
-            'xml': '',
-            'chaveCfe': '',
-            'numSessao': '',
-            'chave_cfe': '',
-            'excessao': False
-        }
-        try:
-            resposta = self.device.cancelar_ultima_venda(
-                chave_cfe,
-                self.montar_xml_cfe_cancelamento(chave_cfe)
-            )
-            self._print_extrato_cancelamento(
-                resposta.chaveConsulta, resposta.xml())
-
-            json_salvar_cancelamento_cfe = {
-                'xml': resposta.arquivoCFeSAT,
-                'numSessao': resposta.numeroSessao,
-                'chave_cfe': resposta.chaveConsulta
-            }
-
-            return json_salvar_cancelamento_cfe
-        except ErroRespostaSATInvalida as ex_sat_invalida:
-            json_cfe = {
-                'excessao': ex_sat_invalida
-            }
-            return json_cfe
-        except ExcecaoRespostaSAT as ex_resposta:
-            json_cfe = {
-                'excessao': ex_resposta
-            }
-            return json_cfe
-        except Exception as ex:
-            json_cfe = {
-                'excessao': ex
-            }
-
-        return json_cfe
+    def _reprint_cfe(self, json):
+        if json['canceled_order']:
+            return self._print_extrato_cancelamento(
+                json['chave_cfe'], json['xml'])
+        else:
+            return self._print_extrato_venda(
+                json['chave_cfe'], json['xml'])
 
     def run(self):
         self.device = None
@@ -320,53 +268,33 @@ class Sat(Thread):
                 time.sleep(2)
             else:
                 with self.satlock:
-                    self.device = self.get_device()
+                    self.device = self.action_call_sat('get_device')
                 if not self.device:
                     time.sleep(1)
-sat_thread = None
-# if satcfe:
-#     sat_thread = Sat()
-#     hw_proxy.drivers['satcfe'] = sat_thread
-    
+
 
 class SatDriver(hw_proxy.Proxy):
+
+    # TODO: Temos um problema quando o sat é iniciado depois do POS
+    # @http.route('/hw_proxy/status_json', type='json', auth='none', cors='*')
+    # def status_json(self):
+    #     if not hw_proxy.drivers['satcfe'].device:
+    #         hw_proxy.drivers['satcfe'].get_device()
+    #     return self.get_status()
+
     @http.route('/hw_proxy/init/', type='json', auth='none', cors='*')
-    def sat_init(self, json):
-        sat_thread = Sat(
-            json['codigo_ativacao'],
-            json['sat_path'],
-            json['impressora'],
-            json['printer_params']
-        )
-        hw_proxy.drivers['satcfe'] = sat_thread
+    def init(self, json):
+        hw_proxy.drivers['satcfe'] = Sat(**json)
         return True
 
     @http.route('/hw_proxy/enviar_cfe_sat/', type='json', auth='none', cors='*')
     def enviar_cfe_sat(self, json):
-        if hw_proxy.drivers['satcfe']:
-            if hw_proxy.drivers['satcfe'].status['status'] == 'connected':
-                _logger.info('SAT: Enviando CFE')
-                return hw_proxy.drivers['satcfe'].sat(json)
-            return False
+        return hw_proxy.drivers['satcfe'].action_call_sat('send', json)
 
     @http.route('/hw_proxy/cancelar_cfe/', type='json', auth='none', cors='*')
-    def cancelar_cfe(self, chave_cfe):
-        if hw_proxy.drivers['satcfe']:
-            if hw_proxy.drivers['satcfe'].status['status'] == 'connected':
-                _logger.info('SAT: Cancelando CFE')
-                return hw_proxy.drivers['satcfe'].sat_cancelar_cfe(chave_cfe)
-            return False
+    def cancelar_cfe(self, json):
+        return hw_proxy.drivers['satcfe'].action_call_sat('cancel', json)
 
     @http.route('/hw_proxy/reprint_cfe/', type='json', auth='none', cors='*')
     def reprint_cfe(self, json):
-        if hw_proxy.drivers['satcfe']:
-            _logger.info('SAT: Reimpressao CFE')
-            if json['canceled_order']:
-                hw_proxy.drivers['satcfe']._print_extrato_cancelamento(
-                    json['chave_cfe'], json['xml'])
-                return True
-            else:
-                hw_proxy.drivers['satcfe']._print_extrato_venda(
-                    json['chave_cfe'], json['xml'])
-                return True
-        return False
+        return hw_proxy.drivers['satcfe'].action_call_sat('print', json)
