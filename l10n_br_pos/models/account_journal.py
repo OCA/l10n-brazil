2dbeab2cc62b7c077299d4e16dc0fe06a7bd3141
Luis Felipe Mileo <mileo@kmee.com.br>
2016-07-06 02:14:59 -0300

    [FIX] License

--- l10n_br_pos/models/account_journal.py
+++ l10n_br_pos/models/account_journal.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# (c) 2016 Kmee - Fernando Marcato
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br) - Fernando Marcato
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
 
