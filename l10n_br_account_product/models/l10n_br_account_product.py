85ae3fbd0d8b8811334a4b4f656f16be2499adc4
Developer <renato.lima@akretion.com.br>
2016-07-13 16:27:41 +0000

    Alterado visão de percentual de partilha

--- l10n_br_account_product/models/l10n_br_account_product.py
+++ l10n_br_account_product/models/l10n_br_account_product.py
@@ -364,6 +364,15 @@ class L10nBrTaxIcmsPartition(models.Model):
     _name = 'l10n_br_tax.icms_partition'
     _description = 'Icms Partition'
 
-    date = fields.Date(u'Ano', required=True)
-    rate_percentual = fields.Float(u'Percentual Interestadual de Rateio',
-                                   required=True)
+    date_start = fields.Date(
+        u'Data Inicial',
+        required=True
+    )
+    date_end = fields.Date(
+        u'Data Final',
+        required=True
+    )
+    rate = fields.Float(
+        u'Percentual Interestadual de Rateio',
+        required=True
+    )
