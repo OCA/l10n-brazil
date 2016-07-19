6a29a16fb09e205c2c1f92e1aff346c1757ea6c3
Luis Felipe Mileo <mileo@kmee.com.br>
2016-07-19 11:27:27 -0300

    [FIX] res_config.py
    Remove space

--- l10n_br_base/models/res_config.py
+++ l10n_br_base/models/res_config.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from openerp import fields, models
 from openerp.tools.safe_eval import safe_eval
 
@@ -6,9 +7,9 @@ from openerp.tools.safe_eval import safe_eval
 class res_config(models.TransientModel):
     _inherit = 'base.config.settings'
 
-    allow_cnpj_multi_ie = fields.Boolean (
-        string = u'Permitir o cadastro de Customers com CNPJs iguais',
-        default = True,
+    allow_cnpj_multi_ie = fields.Boolean(
+        string=u'Permitir o cadastro de Customers com CNPJs iguais',
+        default=True,
     )
 
     def get_default_allow_cnpj_multi_ie(self, cr, uid, fields, context=None):
@@ -21,5 +22,5 @@ class res_config(models.TransientModel):
     def set_allow_cnpj_multi_ie(self, cr, uid, ids, context=None):
         config = self.browse(cr, uid, ids[0], context=context)
         icp = self.pool.get('ir.config_parameter')
-        icp.set_param(cr, uid, 'l10n_br_base_allow_cnpj_multi_ie', repr(config.allow_cnpj_multi_ie))
-
+        icp.set_param(cr, uid, 'l10n_br_base_allow_cnpj_multi_ie',
+                      repr(config.allow_cnpj_multi_ie))
