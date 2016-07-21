a3dcc7ca73764109ce7776a1c01c26f514c5a33a
Magno Costa - Akretion <magno.costa@akretion.com.br>
2016-07-21 14:56:14 -0300

    Add test files to function amount_to_text and fix the diference between Milhao to Milhoes.

---
+++ l10n_br_base/tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @ 2016 Akretion - www.akretion.com.br -
+#   Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+# NOTE we can remove this once this is merged
+# https://github.com/odoo/odoo/pull/11388
+
+from . import test_amount_to_text
