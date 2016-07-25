fcabf2642d656b2109b6a96f77f417bb213ef003
fernandomr <fernandofmr92@gmail.com>
2016-07-25 19:35:23 -0300

    WIP migracao
    Conflicts:
    	l10n_br_hr_payroll/__init__.py
    	l10n_br_hr_payroll/__openerp__.py
    	l10n_br_hr_payroll/model/__init__.py
    	l10n_br_hr_payroll/model/hr_contract.py

--- l10n_br_hr/__init__.py
+++ l10n_br_hr/__init__.py
@@ -1,24 +1,6 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Brazillian Human Resources Payroll module for OpenERP
-#    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
-#    @author Luis Felipe Mileo <mileo@kmee.com.br>
-#    @author Matheus Lima Felix <matheus.felix@kmee.com.br>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
+# (c) 2014 Kmee - Matheus Lima Felix <matheus.felix@kmee.com.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import model
--- l10n_br_hr/model/hr_job.py
+++ l10n_br_hr/model/hr_job.py
@@ -1,27 +1,9 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Brazillian Human Resources Payroll module for OpenERP
-#    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
-#    @author Luis Felipe Mileo <mileo@kmee.com.br>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp.osv import orm, fields
-from tools.translate import _
+from openerp.tools.translate import _
 
 class HrJob(orm.Model):
     
