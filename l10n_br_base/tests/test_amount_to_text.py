fdb529bb049aa8f8c00246854a4152ee296b7436
Magno Costa <magno.costa@akretion.com.br>
2016-07-28 13:19:03 -0300

    Change br to pt_BR to keep translations standards.Remove 'Note to remove after merged' from tests files because the test remains in localization after merged.

--- l10n_br_base/tests/test_amount_to_text.py
+++ l10n_br_base/tests/test_amount_to_text.py
@@ -3,51 +3,49 @@
 #   Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-# NOTE we can remove this once this is merged
-# https://github.com/odoo/odoo/pull/11388
 
-from openerp.tools import amount_to_text_br
+from openerp.tools import amount_to_text_pt_BR
 from openerp.tests.common import TransactionCase
 
 
 class Tests(TransactionCase):
 
     def test_01_amount_to_text(self):
-        text_number = amount_to_text_br(99.99, 'REAIS')
+        text_number = amount_to_text_pt_BR(99.99, 'REAIS')
         self.assertEqual(
             text_number,
             'Noventa e Nove REAIS e  Noventa e Nove Centavos')
 
     def test_02_amount_to_text(self):
-        text_number = amount_to_text_br(1999.99, 'REAIS')
+        text_number = amount_to_text_pt_BR(1999.99, 'REAIS')
         self.assertEqual(
             text_number,
             'Um Mil, Novecentos e Noventa e Nove REAIS'
             ' e  Noventa e Nove Centavos')
 
     def test_03_amount_to_text(self):
-        text_number = amount_to_text_br(77777.0, 'REAIS')
+        text_number = amount_to_text_pt_BR(77777.0, 'REAIS')
         self.assertEqual(
             text_number,
             u'Setenta e Sete Mil, Setecentos e Setenta'
             u' e Sete REAIS e  Zero Centavo')
 
     def test_04_amount_to_text(self):
-        text_number = amount_to_text_br(1856333.0, 'REAIS')
+        text_number = amount_to_text_pt_BR(1856333.0, 'REAIS')
         self.assertEqual(
             text_number,
             u'Um Milhão, Oitocentos e Cinquenta e Seis Mil,'
             u' Trezentos e Trinta e Três REAIS e  Zero Centavo')
 
     def test_05_amount_to_text(self):
-        text_number = amount_to_text_br(9999999.0, 'REAIS')
+        text_number = amount_to_text_pt_BR(9999999.0, 'REAIS')
         self.assertEqual(
             text_number,
             u'Nove Milhões, Novecentos e Noventa e Nove Mil,'
             u' Novecentos e Noventa e Nove REAIS e  Zero Centavo')
 
     def test_06_amount_to_text(self):
-        text_number = amount_to_text_br(9999999999.0, 'REAIS')
+        text_number = amount_to_text_pt_BR(9999999999.0, 'REAIS')
         self.assertEqual(
             text_number,
             u'Nove Bilhões, Novecentos e Noventa e Nove Milhões,'
