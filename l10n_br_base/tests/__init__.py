18b07302acc0ab77137899c2cf7cf0490740842c
Magno Costa - Akretion <magno.costa@akretion.com.br>
2016-07-28 13:19:03 -0300

    Change br to pt_BR to keep translations standards.Remove 'Note to remove after merged' from tests files because the test remains in localization after merged.

--- l10n_br_base/tests/__init__.py
+++ l10n_br_base/tests/__init__.py
@@ -3,7 +3,5 @@
 #   Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-# NOTE we can remove this once this is merged
-# https://github.com/odoo/odoo/pull/11388
 
 from . import test_amount_to_text
