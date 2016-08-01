23175371bdd216f1836b75bcfd5f5f28d88d0dda
fernandomr <fernandofmr92@gmail.com>
2016-08-01 18:36:23 -0300

    WIP migration to new api

--- l10n_br_hr/model/res_company.py
+++ l10n_br_hr/model/res_company.py
@@ -2,11 +2,10 @@
 # (c) 2014 Kmee - Rafael da Silva Lima <rafael.lima@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 
-class ResCompany(orm.Model):
+
+class ResCompany(models.Model):
     _inherit = 'res.company'
-  
-    _columns = {
-        'check_benefits': fields.boolean('Valley Food and Meal Valley simultaneous', required=False),
-       }
+
+    check_benefits = fields.Boolean('Valley Food and Meal Valley simultaneous', required=False)
