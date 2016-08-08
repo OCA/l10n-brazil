96bc9fadac10c855980697a891651a8cbf66c9ec
sadamo <daniel.sadamo@kmee.com.br>
2016-08-08 10:11:43 -0300

    [NEW] created new related document for SAT refund

--- l10n_br_pos/models/l10n_br_account_product.py
+++ l10n_br_pos/models/l10n_br_account_product.py
@@ -2,7 +2,16 @@
 # © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import api, fields, models
+
+DOCUMENT_TYPE = [
+    ('nf', 'NF'),
+    ('nfe', 'NF-e'),
+    ('cte', 'CT-e'),
+    ('nfrural', 'NF Produtor'),
+    ('cf', 'Cupom Fiscal'),
+    ('sat', 'CF-e/SAT')
+]
 
 
 class L10nbrAccountCFOP(models.Model):
@@ -11,6 +20,30 @@ class L10nbrAccountCFOP(models.Model):
 
     is_pos = fields.Boolean(
         string=u"Permitida no POS",
-        help=u"Marque esta selaçao para que a cfop possa ser utililizada no "\
-             u"NFC-E/SAT"
-    )
+        help=u"Marque esta selaçao para que a cfop possa ser utililizada no "
+             u"NFC-E/SAT")
+
+
+class L10nbrAccountDocumentRelated(models.Model):
+    _inherit = 'l10n_br_account_product.document.related'
+
+    pos_order_related_id = fields.Many2one('pos.order',
+                                           string=u'Documento Fiscal SAT',
+                                           ondelete='cascade', select=True)
+
+    document_type = fields.Selection(
+        [('nf', 'NF'), ('nfe', 'NF-e'), ('cte', 'CT-e'),
+         ('nfrural', 'NF Produtor'), ('cf', 'Cupom Fiscal'),
+         ('sat', 'CF-e/SAT')],
+        'Tipo Documento', required=True)
+
+    @api.onchange('pos_order_related_id')
+    def onchange_pos_order_related_id(self):
+        result = {'value': {}}
+
+        if self.pos_order_related_id:
+            result['value']['document_type'] = 'sat'
+            result['value']['access_key'] = self.pos_order_related_id.chave_cfe[3:]
+            result['value']['date'] = self.pos_order_related_id.date_order[:10]
+
+        return result
