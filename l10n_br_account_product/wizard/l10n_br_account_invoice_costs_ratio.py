cfe2b55e8ef659c20802792e572a94c69a85ef64
Raphaël Valyi <rvalyi@akretion.com>
2016-08-15 17:29:15 -0300

    lighter new style OCA license headers

--- l10n_br_account_product/wizard/l10n_br_account_invoice_costs_ratio.py
+++ l10n_br_account_product/wizard/l10n_br_account_invoice_costs_ratio.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
 #    Copyright (C) 2014 KMEE (http://www.kmee.com.br)
 #    @author Daniel Sadamo <sadamo@kmee.com.br>
 #    @author Luis Felipe Mileo <mileo@kmee.com.br>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, fields, api
 from openerp.addons.l10n_br_base.tools.misc import calc_price_ratio
