4399ac1a071be2cd0539441a6df66e5deca35d9e
sadamo <daniel.sadamo@kmee.com.br>
2016-08-17 16:36:23 -0300

    [IMP] Relates the POS module with the stock module

--- l10n_br_pos/__openerp__.py
+++ l10n_br_pos/__openerp__.py
@@ -11,11 +11,12 @@
     "license": "AGPL-3",
     "category": "Point Of Sale",
     "depends": [
-        'l10n_br_account_product',
+        'l10n_br_sale_stock',
         'point_of_sale',
         'pos_pricelist',
         'pos_payment_term',
         'nfe_attach',
+        'pos_order_picking_link'
     ],
     'data': [
         "wizard/l10n_br_pos_order_return.xml",
