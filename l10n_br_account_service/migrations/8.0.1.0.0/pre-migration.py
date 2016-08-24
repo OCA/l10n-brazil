2a2c02736c67e7680be25f502acc7a3bb029bd20
Magno Costa - Akretion <magno.costa@akretion.com.br>
2016-08-24 16:58:07 -0300

    Included migration files from branch 7.0 (old develop) to 8.0 for l10n_br_account_service

--- l10n_br_account_service/migrations/8.0.1.0.0/pre-migration.py
+++ l10n_br_account_service/migrations/8.0.1.0.0/pre-migration.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015  Renato Lima - Akretion
+# Copyright (C) 2015  Renato Lima - www.akretion.com.br
+# Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-# TODO
-# Migration
-# =========
 
-# - Rename ir.model.data fiscal_category_venda_servico
-#   to fc_00255d8d158663217906abb312aa0759
+from openerp.openupgrade import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+
+    cr.execute(
+        """
+        UPDATE ir_model_data
+         SET name = 'fc_00255d8d158663217906abb312aa0759'
+          WHERE name = 'fiscal_category_venda_servico'
+        """)
