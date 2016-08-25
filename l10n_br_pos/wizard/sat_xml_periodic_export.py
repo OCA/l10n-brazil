8b29489d5bf3cb08317770a6a4d317d57d1a7852
Luis Felipe Mileo <mileo@kmee.com.br>
2016-08-25 00:42:18 -0300

    OCA clean headers
    for i in `find ./ -iname '*.py'`; do sed -i ':a;N;$!ba;s|#                                                                             #\n# This program is free.*#\n###############################################################################|# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html|g' $i; done
    
    for i in `find ./ -iname '*.py'`; do sed -i ':a;N;$!ba;s|#\n# This program.*###############################################################################|# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html|g' $i; done
    
    for i in `find ./ -iname '*.py'`; do sed -i ':a;N;$!ba;s|###############################################################################\n#                                                                             #||g' $i; done
    
    for i in `find ./ -iname '*.py'`; do sed -i ':a;N;$!ba;s|###############################################################################\n#||g' $i; done

--- l10n_br_pos/wizard/sat_xml_periodic_export.py
+++ l10n_br_pos/wizard/sat_xml_periodic_export.py
@@ -1,21 +1,7 @@
 # -*- encoding: utf-8 -*-
-###############################################################################
-#                                                                             #
+
 # Copyright (C) 2016 Luiz Felipe do Divino - KMEE - www.kmee.com.br           #
-#                                                                             #
-# This program is free software: you can redistribute it and/or modify        #
-# it under the terms of the GNU Affero General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or           #
-# (at your option) any later version.                                         #
-#                                                                             #
-# This program is distributed in the hope that it will be useful,             #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
-# GNU General Public License for more details.                                #
-#                                                                             #
-# You should have received a copy of the GNU General Public License           #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
-###############################################################################
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, fields, api, _
 import os
