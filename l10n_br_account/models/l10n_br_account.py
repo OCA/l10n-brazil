cb61dcb89c6c9300815b7cb010bff7366eb9bab0
Renato Lima <renato.lima@akretion.com>
2016-08-25 01:47:58 -0300

    removed empty spaces from headers

--- l10n_br_account/models/l10n_br_account.py
+++ l10n_br_account/models/l10n_br_account.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion                           #
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
