982f7ff6c23155f45e033eeb3dbb237d34244955
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2016-09-08 17:28:31 -0300

    [REF] Refatorado funcao para criar o botao 'ultimas vendas'

--- l10n_br_pos/static/src/js/models.js
+++ l10n_br_pos/static/src/js/models.js
@@ -91,7 +91,7 @@ function l10n_br_pos_models(instance, module) {
             });
             this.models.push({
                 model:  'res.partner',
-                fields: ['name', 'cnpj_cpf', 'street','city','state_id','country_id','vat','phone','zip','mobile','email','ean13','write_date'],
+                fields: ['name', 'cnpj_cpf', 'street','city','state_id','country_id','vat','phone','zip','mobile','email','ean13','write_date', 'debit', 'credit', 'credit_limit'],
                 domain: [['customer','=',true]],
                 loaded: function(self,partners){
                     self.partners = partners;
