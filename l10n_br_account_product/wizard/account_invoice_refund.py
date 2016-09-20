b0a3ad85d0aa2098cd77b55dc4e5338b7ca11549
renato <renato.lima@akretion.com.br>
2016-09-20 13:32:24 -0300

    fix older header and remove blank spaces

--- l10n_br_account_product/wizard/account_invoice_refund.py
+++ l10n_br_account_product/wizard/account_invoice_refund.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013  Florian da Costa - Akretion                             #
+# Copyright (C) 2013  Florian da Costa - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from lxml import etree
