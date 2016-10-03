61ff2b4e8b995de195cbc67a5119167e9ceda74f
mstuttgart <michellstut@gmail.com>
2016-10-03 14:28:21 -0300

    [NEW] Add zip_sarch to ResPartnerBank

--- l10n_br_zip/models/res_partner.py
+++ l10n_br_zip/models/res_partner.py
@@ -47,3 +47,45 @@ class ResPartner(models.Model):
                 )
             else:
                 raise UserError(_('Nenhum registro encontrado'))
+
+
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner.bank'
+
+    @api.multi
+    def zip_search(self):
+        self.ensure_one()
+        obj_zip = self.env['l10n_br.zip']
+
+        zip_ids = obj_zip.zip_search_multi(
+            country_id=self.country_id.id,
+            state_id=self.state_id.id,
+            l10n_br_city_id=self.l10n_br_city_id.id,
+            district=self.district,
+            street=self.street,
+            zip_code=self.zip,
+        )
+
+        if len(zip_ids) == 1:
+            result = obj_zip.set_result(zip_ids[0])
+            self.write(result)
+            return True
+        else:
+            if len(zip_ids) > 1:
+                obj_zip_result = self.env['l10n_br.zip.result']
+                zip_ids = obj_zip_result.map_to_zip_result(
+                    zip_ids, self._name, self.id)
+
+                return obj_zip.create_wizard(
+                    self._name,
+                    self.id,
+                    country_id=self.country_id.id,
+                    state_id=self.state_id.id,
+                    l10n_br_city_id=self.l10n_br_city_id.id,
+                    district=self.district,
+                    street=self.street,
+                    zip_code=self.zip,
+                    zip_ids=[zip.id for zip in zip_ids]
+                )
+            else:
+                raise UserError(_('Nenhum registro encontrado'))
