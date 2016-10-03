a14493929ce92d3c22b037188c70b366d37bc100
mstuttgart <michellstut@gmail.com>
2016-10-03 10:49:45 -0300

    [NEW] Add l10n_br_city, state_id and country_id fields in res.bank

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2015  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import res_bank
 from . import res_partner
 from . import res_country
 from . import res_company
