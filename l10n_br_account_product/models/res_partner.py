089ee1d9c29a0e660bf3e667e0e1957e1ae05365
renato <renato.lima@akretion.com.br>
2016-10-05 09:58:59 -0300

    pep8

--- l10n_br_account_product/models/res_partner.py
+++ l10n_br_account_product/models/res_partner.py
@@ -30,6 +30,7 @@ class AccountFiscalPositionTaxTemplate(models.Model):
         'l10n_br_account_product.icms_relief', string=u'Desoneração ICMS')
     origin = fields.Selection(PRODUCT_ORIGIN, 'Origem',)
 
+
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
@@ -133,12 +134,13 @@ class AccountFiscalPosition(models.Model):
             partner = self.env['res.partner'].browse(
                 self.env.context.get('partner_id'))
             if (self.env.context.get('type_tax_use') in ('sale', 'all') and
-                self.env.context.get('fiscal_type', 'product') == 'product'):
+                    self.env.context.get('fiscal_type',
+                                         'product') == 'product'):
                 state_taxes = partner.state_id.product_tax_definition_line
                 for tax_def in state_taxes:
                     if tax_def.tax_id and \
-                    (not tax_def.fiscal_classification_id or
-                     tax_def.fiscal_classification_id == product_fc):
+                            (not tax_def.fiscal_classification_id or
+                             tax_def.fiscal_classification_id == product_fc):
                         taxes |= tax_def.tax_id
                         result[tax_def.tax_id.domain] = {
                             'tax': tax_def.tax_id,
@@ -163,7 +165,7 @@ class AccountFiscalPosition(models.Model):
                             map_taxes_origin |= map
                         if (map.fiscal_classification_id.id ==
                                 product.fiscal_classification_id.id and
-                                    map.origin == product.origin):
+                                map.origin == product.origin):
                             map_taxes_origin_ncm |= map
                         else:
                             map_taxes |= map
