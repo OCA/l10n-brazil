225490fbe6e77c1338dcb64f0906e77151b309f5
sadamo <daniel.sadamo@kmee.com.br>
2016-10-07 15:30:33 -0300

    Fix PEP8

--- l10n_br_hr/__openerp__.py
+++ l10n_br_hr/__openerp__.py
@@ -3,22 +3,23 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name' : 'Brazilian Localization HR',
-    'description' : """
-Brazilian Localization HR with informations refered to the national context of HR""",
-    'category' : 'Localization',
-    'author' : 'KMEE',
+    'name': 'Brazilian Localization HR',
+    'description': """
+Brazilian Localization HR with informations refered
+to the national context of HR""",
+    'category': 'Localization',
+    'author': 'KMEE',
     'maintainer': 'KMEE',
-    'website' : 'http://www.kmee.com.br',
-    'version' : '0.1',
-    'depends' : ['hr','l10n_br_base'],
+    'website': 'http://www.kmee.com.br',
+    'version': '0.1',
+    'depends': ['hr', 'l10n_br_base'],
     'data': [
-             'data/l10n_br_hr.cbo.csv',
-             'security/ir.model.access.csv',
-             'view/l10n_br_hr_cbo_view.xml',
-             'view/hr_employee_view.xml',
-             'view/hr_job_view.xml',
-             ],
+        'data/l10n_br_hr.cbo.csv',
+        'security/ir.model.access.csv',
+        'view/l10n_br_hr_cbo_view.xml',
+        'view/hr_employee_view.xml',
+        'view/hr_job_view.xml',
+    ],
     'test': [],
     'installable': True,
     'images': [],
