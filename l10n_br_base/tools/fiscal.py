877bbccca9cd0e16f80eb6ecfc040d5181859e91
sadamo <daniel.sadamo@kmee.com.br>
2016-10-20 10:23:55 -0200

    Move PIS/PASEP validation to l10n_br_base

--- l10n_br_base/tools/fiscal.py
+++ l10n_br_base/tools/fiscal.py
@@ -490,3 +490,28 @@ def validate_cpf(cpf):
         return True
 
     return False
+
+def validate_pis_pasep(pis_pasep):
+    digits = []
+    for c in pis_pasep:
+        if c == '.' or c == ' ' or c == '\t':
+            continue
+        if c == '-':
+            if len(digits) != 10:
+                return False
+            continue
+        if c.isdigit():
+            digits.append(int(c))
+            continue
+        return False
+    if len(digits) != 11:
+        return False
+    height = [int(x) for x in "3298765432"]
+    total = 0
+    for i in range(10):
+        total += digits[i] * height[i]
+    rest = total % 11
+    if rest != 0:
+        rest = 11 - rest
+    if rest == digits[10]:
+        return True
