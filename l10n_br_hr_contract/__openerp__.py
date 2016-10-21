3249a930be309b8f595af642759f32fb0d619042
sadamo <daniel.sadamo@kmee.com.br>
2016-10-21 11:28:10 -0200

    [NEW] Module l10n_br_hr_contract

---
+++ l10n_br_hr_contract/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016  Daniel Sadamo - KMEE Informática
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Brazilian Localization HR Contract',
+    'category': 'Localisation',
+    'license': 'AGPL-3',
+    'author': 'KMEE Informática, Odoo Community Association (OCA)',
+    'website': 'http://odoo-brasil.org',
+    'version': '8.0.0.0.0',
+    'depends': [
+        'hr_contract',
+    ],
+    'data': [
+
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
