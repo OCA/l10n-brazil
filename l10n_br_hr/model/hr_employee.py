27e7b06976bf8260d6f7f25a169c29ec4886f8f3
sadamo <daniel.sadamo@kmee.com.br>
2016-10-21 16:42:45 -0200

    Fields, labels, names in english language

--- l10n_br_hr/model/hr_employee.py
+++ l10n_br_hr/model/hr_employee.py
@@ -12,45 +12,45 @@ from openerp.exceptions import ValidationError
 
 
 NATIONALITY_CODE = [
-    ('10', '10 - Brasileiro'),
-    ('20', '20 - Naturalizado Brasileiro'),
-    ('21', '21 - Argentino'),
-    ('22', '22 - Boliviano'),
-    ('23', '23 - Chileno'),
-    ('24', '24 - Paraguaio'),
-    ('25', '25 - Uruguaio'),
-    ('26', '26 - Venezuelano'),
-    ('27', '27 - Colombiano'),
-    ('28', '28 - Peruano'),
-    ('29', '29 - Equatoriano'),
-    ('30', u'30 - Alemão'),
-    ('31', '31 - Belga'),
-    ('32', u'32 - Britânico'),
-    ('34', '34 - Canadense'),
-    ('35', '35 - Espanhol'),
-    ('36', '36 - Norte-americano'),
-    ('37', u'37 - Francês'),
-    ('38', u'38 - Suíço'),
-    ('39', '39 - Italiano'),
-    ('40', '40 - Haitiano'),
-    ('41', u'41 - Japonês'),
-    ('42', u'42 - Chines'),
-    ('43', '43 - Coreano'),
-    ('44', '44 - Russo'),
-    ('45', u'45 - Português'),
-    ('46', u'46 - Paquistanês'),
-    ('47', '47 - Indiano'),
-    ('48', '48 - Outros latino-americanos'),
-    ('49', u'49 - Outros asiáticos'),
-    ('50', u'50 - Bengalês'),
-    ('51', '51 - Outros Europeus'),
-    ('60', '60 - Angolano'),
-    ('61', u'61 - Congolês'),
-    ('62', '62 - Sul-africano'),
-    ('63', u'63 - Ganês'),
-    ('64', u'64 - Senegalês'),
-    ('70', '70 - Outros Africanos'),
-    ('80', '80 - Outros')
+    ('10', '10 - BRAZILIAN'),
+    ('20', '20 - BRAZILIAN NATURALIZED'),
+    ('21', '21 - ARGENTINE'),
+    ('22', '22 - BOLIVIAN'),
+    ('23', '23 - CHILEAN'),
+    ('24', '24 - PARAGUAYAN'),
+    ('25', '25 - URUGUAYAN'),
+    ('26', '26 - VENEZUELAN'),
+    ('27', '27 - COLOMBIAN'),
+    ('28', '28 - PERUVIAN'),
+    ('29', '29 - ECUADORIAN'),
+    ('30', '30 - GERMAN'),
+    ('31', '31 - BELGIAN'),
+    ('32', '32 - BRITISH'),
+    ('34', '34 - CANADIAN'),
+    ('35', '35 - SPANISH'),
+    ('36', '36 - NORTH-AMERICAN'),
+    ('37', '37 - FRENCH'),
+    ('38', '38 - SWISS'),
+    ('39', '39 - ITALIAN'),
+    ('40', '40 - HAITIAN'),
+    ('41', '41 - JAPANESE'),
+    ('42', '42 - CHINESE'),
+    ('43', '43 - KOREAN'),
+    ('44', '44 - RUSSIAN'),
+    ('45', '45 - PORTUGUESE'),
+    ('46', '46 - PAKISTANI'),
+    ('47', '47 - INDIAN'),
+    ('48', '48 - OTHER LATIN-AMERICAN'),
+    ('49', '49 - OTHER ASIAN'),
+    ('50', '50 - BANGLA'),
+    ('51', '51 - OTHER EUROPEAN'),
+    ('60', '60 - ANGOLAN'),
+    ('61', '61 - CONGOLESE'),
+    ('62', '62 - SOUTH AFRICAN'),
+    ('63', '63 - GHANAIAN'),
+    ('64', '64 - SENEGALESE'),
+    ('70', '70 - OTHER AFRICAN'),
+    ('80', '80 - OTHER')
 ]
 
 
@@ -70,8 +70,7 @@ class HrEmployee(models.Model):
             if datetime.strptime(
                     dependent.dependent_dob, DEFAULT_SERVER_DATE_FORMAT
                     ).date() > datetime.now().date():
-                raise ValidationError(_(u'Data de nascimento do '
-                                        u'dependente %s inválida'
+                raise ValidationError(_('Invalid birth date for dependent %s'
                                         % dependent.dependent_name))
 
     def _check_dependent_type(self):
@@ -87,8 +86,8 @@ class HrEmployee(models.Model):
                 seen.add(dep_type)
             elif dep_type in seen and dep_type in restrictions:
                 raise ValidationError(
-                    _(u'Já existe um dependente com o mesmo grau '
-                      u'de parentesco de %s'
+                    _('A dependent with the same level of relatedness'
+                      ' already exists for dependent %s'
                       % dependent.dependent_name))
 
     @api.constrains('pis_pasep')
@@ -99,61 +98,60 @@ class HrEmployee(models.Model):
         elif fiscal.validate_pis_pasep(self.pis_pasep):
             return True
         else:
-            raise ValidationError(_(u'PIS/PASEP inválido'))
+            raise ValidationError(_('Invalid PIS/PASEP'))
 
-    check_cpf = fields.Boolean('Verificar CPF', default=True)
+    check_cpf = fields.Boolean('Check CPF', default=True)
     pis_pasep = fields.Char(u'PIS/PASEP', size=15)
-    ctps = fields.Char('CTPS', help=u'Número do CTPS')
-    ctps_series = fields.Char('Série')
-    ctps_date = fields.Date(u'Data de emissão')
-    ctps_uf_id = fields.Many2one(string='UF da CTPS',
+    ctps = fields.Char('CTPS', help='CTPS number')
+    ctps_series = fields.Char('CTPS series')
+    ctps_date = fields.Date('CTPS emission date')
+    ctps_uf_id = fields.Many2one(string='CTPS district',
                                  comodel_name='res.country.state')
-    creservist = fields.Char('Título de reservista')
-    cresv_categ = fields.Selection(string='Categoria de reservista',
+    creservist = fields.Char('Military service status certificate')
+    cresv_categ = fields.Selection(string='Military service status category',
                                    selection=[
-                                       ('1', 'Primeira Categoria'),
-                                       ('2', 'Segunda Categoria'),
-                                       ('3', 'Terceira Categoria')],
-                                   default=('3', 'Terceira Categoria'))
+                                       ('1', 'First Category'),
+                                       ('2', 'Second Category'),
+                                       ('3', 'Third Category')],
+                                   default=('3', 'Third Category'))
     ginstru = fields.Selection(
-        string=u'Grau de instrução',
+        string='Educational attainment',
         selection=[
-            ('fundamental_incompleto', 'Fundamental incompleto'),
-            ('fundamental', 'Fundamental completo'),
-            ('medio_incompleto', u'Ensino médio incompleto'),
-            ('medio', u'Ensino médio completo'),
-            ('superior_incompleto', 'Superior incompleto'),
-            ('superior', 'Superior completo'),
-            ('mestrado', 'Mestrado'),
-            ('doutorado', 'Doutorado')
+            ('incomplete_elementary_school', 'Incomplete elementary school'),
+            ('complete_elementary_school', 'Complete elementary school'),
+            ('incomplete_high_school', 'Incomplete high school'),
+            ('complete_high_school', 'Complete high school'),
+            ('incomplete_graduation', 'Incomplete graduation'),
+            ('Graduation', 'Complete graduation'),
+            ('master', 'Master'),
+            ('phd', 'Ph.D')
         ])
-    have_dependent = fields.Boolean('Possui dependentes')
+    have_dependent = fields.Boolean('Has dependents')
     dependent_ids = fields.One2many(comodel_name='hr.employee.dependent',
                                     inverse_name='employee_id',
-                                    string='Dependentes')
-    rg = fields.Char(string='RG', help=u'Número do RG')
+                                    string='Dependents')
+    rg = fields.Char(string='RG', help='National ID number')
     cpf = fields.Char(string='CPF', related='address_home_id.cnpj_cpf',
                       required=True)
-    organ_exp = fields.Char(string=u'Órgão expeditor')
-    rg_emission = fields.Date(string='Data de emissão')
-    voter_title = fields.Char(string=u'Título de eleitor')
-    voter_zone = fields.Char(string='Zona de votação')
-    voter_section = fields.Char(string=u'Seção de votação')
-    driver_license = fields.Char(string=u'Número da CNH')
-    driver_categ = fields.Char(string=u'Categoria de habilitação')
-    father_name = fields.Char(string=u'Nome do pai')
-    mother_name = fields.Char(string=u'Nome da mãe')
-    expiration_date = fields.Date(string=u'Validade')
-    sindicate = fields.Char(string=u'Sindicato', help=u'Sigla do sindicato')
-    ethnicity = fields.Selection(string=u'Raça/Cor', selection=[
-        ('indigena', u'Indígena'),
-        ('branca', 'Branco'),
-        ('negra', 'Negro'),
-        ('amarela', 'Amarelo'),
-        ('parda', 'Pardo'),
-        ('nao_informada', u'Não Informado')
+    organ_exp = fields.Char(string='Dispatcher organ')
+    rg_emission = fields.Date(string='Emission date')
+    voter_title = fields.Char(string='Voter title')
+    voter_zone = fields.Char(string='Voter zone')
+    voter_section = fields.Char(string='Voter section')
+    driver_license = fields.Char(string='Driver license number')
+    driver_categ = fields.Char(string='Driver license category')
+    father_name = fields.Char(string='Father name')
+    mother_name = fields.Char(string='Mother name')
+    expiration_date = fields.Date(string='Expiration date')
+    ethnicity = fields.Selection(string='Ethnicity', selection=[
+        ('indigene', 'Indigene'),
+        ('white', 'White'),
+        ('black', 'Black'),
+        ('asian', 'Asian'),
+        ('pardo', 'Pardo'),
+        ('not_informed', 'Not informed')
     ])
-    blood_type = fields.Selection(string=u'Tipo sanguíneo', selection=[
+    blood_type = fields.Selection(string='Blood type', selection=[
         ('a+', 'A+'),
         ('a-', 'A-'),
         ('b+', 'B+'),
@@ -163,35 +161,33 @@ class HrEmployee(models.Model):
         ('ab+', 'AB+'),
         ('ab-', 'AB-'),
     ])
-    deficiencies_ids = fields.Many2many(string=u'Deficiências',
+    deficiencies_ids = fields.Many2many(string='Deficiencies',
                                         comodel_name='hr.deficiency')
-    identity_type_id = fields.Many2one(string='Tipo de identidade',
+    identity_type_id = fields.Many2one(string='ID type',
                                        comodel_name='hr.identity.type')
-    identity_validity = fields.Date(string='Validade da identidade')
-    identity_uf_id = fields.Many2one(string='UF da identidade',
+    identity_validity = fields.Date(string='ID expiration date')
+    identity_uf_id = fields.Many2one(string='ID expedition district',
                                      comodel_name='res.country.state')
     identity_city_id = fields.Many2one(
-        string=u'Município da identidade',
+        string='ID expedition city',
         comodel_name='l10n_br_base.city',
         domain="[('state_id','=',identity_uf_id)]")
     civil_certificate_type_id = fields.Many2one(
-        string=u'Tipo de certidão civil',
+        string='Civil certificate type',
         comodel_name='hr.civil.certificate.type')
-    alternate_phone = fields.Char(string='Telefone alternativo')
-    emergency_phone = fields.Char(string=u'Telefone de emergência')
-    talk_to = fields.Char(string='Falar com')
-    alternate_email = fields.Char(string='Email alternativo')
-    chronic_disease_ids = fields.Many2many(string=u'Doenças Crônicas',
+    alternate_phone = fields.Char(string='Alternate phone')
+    emergency_phone = fields.Char(string='Emergency phone')
+    talk_to = fields.Char(string='Emergency contact name')
+    alternate_email = fields.Char(string='Alternate email')
+    chronic_disease_ids = fields.Many2many(string='Chronic Diseases',
                                            comodel_name='hr.chronic.disease')
     marital = fields.Selection(selection_add=[
-        ('stable_union', u'União Estável'), ('separado', 'Separado')])
-    registration = fields.Char(string=u'Matrícula')
-    nationality_code = fields.Selection(string=u'Código de nacionalidade',
+        ('common_law_marriage', 'Common law marriage'), ('separated', 'Separated')])
+    registration = fields.Char(string='Registration number')
+    nationality_code = fields.Selection(string='Nationality code',
                                         selection=NATIONALITY_CODE)
-    arrival_year = fields.Integer(string="Ano de chegada ao Brasil",
-                                  default=None)
-    country_id = fields.Many2one(string="Nacionalidade",
-                                 comodel_name='res.country',
+    arrival_year = fields.Integer(string="Arrival year in Brazil")
+    country_id = fields.Many2one(comodel_name='res.country',
                                  default=_default_country)
 
     @api.model
@@ -222,22 +218,21 @@ class HrEmployeeDependent(models.Model):
     def _validate_cpf(self):
         if self.dependent_cpf:
             if not fiscal.validate_cpf(self.dependent_cpf):
-                raise ValidationError(_
-                                      (u'CPF do dependente %s inválido'
-                                       % self.dependent_name))
+                raise ValidationError(_('Invalid CPF for dependent %s'
+                                        % self.dependent_name))
 
     employee_id = fields.Many2one(comodel_name='hr.employee',
-                                  string=u'Funcionário')
-    dependent_name = fields.Char(string=u'Nome', size=64, required=True)
-    dependent_dob = fields.Date(string=u'Data de Nascimento', required=True)
-    dependent_type_id = fields.Many2one(string='Grau de parentesco',
+                                  string='Employee')
+    dependent_name = fields.Char(string='Name', size=64, required=True)
+    dependent_dob = fields.Date(string='Date of birth', required=True)
+    dependent_type_id = fields.Many2one(string='Relatedness',
                                         required=True,
                                         comodel_name='hr.dependent.type')
-    pension_benefits = fields.Float(string=u'Valor da Pensão')
-    dependent_verification = fields.Boolean(string=u'É dependente')
-    health_verification = fields.Boolean(string=u'Utiliza o plano de saúde')
-    dependent_gender = fields.Selection(string='Sexo', selection=[
-        ('m', 'Masculino'),
-        ('f', 'Feminino')])
+    pension_benefits = fields.Float(string='Allowance value')
+    dependent_verification = fields.Boolean(string='Is dependent')
+    health_verification = fields.Boolean(string='Healthcare plan')
+    dependent_gender = fields.Selection(string='Gender', selection=[
+        ('m', 'Male'),
+        ('f', 'Female')])
     dependent_rg = fields.Char(string='RG')
     dependent_cpf = fields.Char(string='CPF')
