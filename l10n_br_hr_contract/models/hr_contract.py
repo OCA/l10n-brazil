38fb0a2da23bd0517e20a6945938bb4116689b42
sadamo <daniel.sadamo@kmee.com.br>
2016-10-25 10:23:36 -0200

    WIP new fields in contract

--- l10n_br_hr_contract/models/hr_contract.py
+++ l10n_br_hr_contract/models/hr_contract.py
@@ -4,32 +4,67 @@
 
 from openerp import models, fields, api
 
+MONTHS = [
+    ('january', 'January'),
+    ('february', 'February'),
+    ('march', 'March'),
+    ('april', 'April'),
+    ('may', 'May'),
+    ('june', 'June'),
+    ('july', 'July'),
+    ('august', 'August'),
+    ('september', 'September'),
+    ('october', 'October'),
+    ('november', 'November'),
+    ('december', 'December'),
+]
+
 
 class HrContractAdmissionType(models.Model):
     _name = 'hr.contract.admission.type'
 
-    name = fields.Char(string=u'Tipo de Admissão')
+    name = fields.Char(string='Admission type')
 
 
 class HrContractLaborBondType(models.Model):
     _name = 'hr.contract.labor.bond.type'
 
-    name = fields.Char(string=u'Vínculo trabalhista')
+    name = fields.Char(string='Labor bond')
 
 
 class HrContractLaborRegime(models.Model):
     _name = 'hr.contract.labor.regime'
 
-    name = fields.Char(string='Regime Trabalhista')
+    name = fields.Char(string='Labor regime')
 
 
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
     admission_type_id = fields.Many2one(
-        string=u'Tipo de Admissão',
+        string='Admission type',
         comodel_name='hr.contract.admission.type')
     labor_bond_type_id = fields.Many2one(
-        string=u'Tipo de vínculo trabalhista',
+        string='Labor bond type',
         comodel_name='hr.contract.labor.bond.type')
+    labor_regime_id = fields.Many2one(
+        string='Labor regime', comodel_name='hr.contract.labor.regime')
+    welfare_policy = fields.Selection(
+        string='Welfare policy',
+        selection=[
+            ('rgps', u'Regime Geral da Previdência Social'),
+            ('rpps', u'Regime Próprio da Previdência Social')])
+    salary_unit = fields.Selection(string='Salary Unity',
+                                   selection=[
+                                       ('month', 'Per Month'),
+                                       ('hour', 'Per Hour')])
+    weekly_hours = fields.Float(string='Weekly hours')
+    monthly_hours = fields.Float(string='Monthly hours')
+    trade_union = fields.Char(string='Trade union')
+    trade_union_cnpj = fields.Char(string='Trade union CNPJ')
+    trade_union_entity_code = fields.Char(string='Trade union entity code')
+    base_date_month = fields.Selection(string='Base date month',
+                                       selection=MONTHS)
+
+
 
