16e0ee13948297287a63b1440e8b539b1bc2c520
Luis Felipe Mileo <mileo@kmee.com.br>
2016-10-26 07:55:20 -0200

    [NEW] Unit tests for pis/pasef validation

---
+++ l10n_br_base/tests/test_tools_fiscal.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @ 2016 KMEE - www.kmee.com.br -
+#   Luis Felipe Miléo <mileo@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp.addons.l10n_br_base.tools.fiscal import validate_pis_pasep
+from openerp.tests.common import TransactionCase
+
+
+class Tests(TransactionCase):
+
+    def test_01_validate_pis_pasep(self):
+        self.assertTrue(validate_pis_pasep('496.85994.95-6'))
+
+    def test_02_validate_pis_pasep(self):
+        self.assertFalse(validate_pis_pasep('496.85994.95-7'))
