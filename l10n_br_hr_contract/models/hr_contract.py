5948274602cd62fd930e29a424826cf65cb46ffd
sadamo <daniel.sadamo@kmee.com.br>
2016-10-27 18:38:04 -0200

    Added resign information, RAIS based data

--- l10n_br_hr_contract/models/hr_contract.py
+++ l10n_br_hr_contract/models/hr_contract.py
@@ -24,12 +24,34 @@ class HrContractAdmissionType(models.Model):
     _name = 'hr.contract.admission.type'
 
     name = fields.Char(string='Admission type')
+    code = fields.Char(string='Code')
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            result.append((record['id'], name))
+        return result
 
 
 class HrContractLaborBondType(models.Model):
     _name = 'hr.contract.labor.bond.type'
 
     name = fields.Char(string='Labor bond')
+    code = fields.Char(string='Code')
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            result.append((record['id'], name))
+        return result
 
 
 class HrContractLaborRegime(models.Model):
@@ -49,6 +71,47 @@ class HrContractLaborRegime(models.Model):
         return result
 
 
+class HrContractSalaryUnit(models.Model):
+    _name = 'hr.contract.salary.unit'
+
+    name = fields.Char(string='Salary unit')
+    code = fields.Char(string='Code')
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            result.append((record['id'], name))
+        return result
+
+
+class HrContractResignationCause(models.Model):
+    _name = 'hr.contract.resignation.cause'
+
+    name = fields.Char(string='Resignation cause')
+    code = fields.Char(string='Resignation cause code')
+    fgts_withdraw_code = fields.Char(string='FGTS withdrawal code')
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            result.append((record['id'], name))
+        return result
+
+
+class HrContractNoticeTermination(models.Model):
+    _name = 'hr.contract.notice.termination'
+
+    name = fields.Char(string='Notice of termination type')
+
+
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
@@ -65,19 +128,31 @@ class HrContract(models.Model):
         selection=[
             ('rgps', u'Regime Geral da Previdência Social'),
             ('rpps', u'Regime Próprio da Previdência Social')])
-    salary_unit = fields.Selection(string='Salary Unity',
-                                   selection=[
-                                       ('month', 'Per Month'),
-                                       ('hour', 'Per Hour')])
+    salary_unit = fields.Many2one(string='Salary Unity',
+                                  comodel_name='hr.contract.salary.unit')
     weekly_hours = fields.Float(string='Weekly hours')
     monthly_hours = fields.Float(string='Monthly hours')
-    trade_union = fields.Char(string='Trade union')
-    trade_union_cnpj = fields.Char(string='Trade union CNPJ')
-    trade_union_entity_code = fields.Char(string='Trade union entity code')
+    union = fields.Char(string='Union')
+    union_cnpj = fields.Char(string='Union CNPJ')
+    union_entity_code = fields.Char(string='Union entity code')
     month_base_date = fields.Selection(string='Base date month',
                                        selection=MONTHS)
-    discount_trade_union_contribution = fields.Boolean(
-        string='Discount trade union contribution in admission')
-
-
-
+    discount_union_contribution = fields.Boolean(
+        string='Discount union contribution in admission')
+    resignation_date = fields.Date(string='Resignation date')
+    resignation_cause_id = fields.Many2one(
+        comodel_name='hr.contract.resignation.cause',
+        string='Resignation cause')
+    notice_of_termination_id = fields.Many2one(
+        string='Notice of termination type',
+        comodel_name='hr.contract.notice.termination'
+    )
+    notice_of_termination_date = fields.Date(
+        string='Notice of termination date')
+    notice_of_termination_payment_date = fields.Date(
+        string='Notice of termination payment date'
+    )
+    by_death = fields.Char(string='By death',
+                           help='Death certificate/Process/Beneficiary')
+    resignation_code = fields.Char(related='resignation_cause_id.code',
+                                   invisible=True)
