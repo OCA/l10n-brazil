9d65ad9a7d38fc29a5c7704f3ab1ef7696ccae4e
Ari Caldeira <aristides.caldeira@tauga.com.br>
2016-11-16 21:12:39 -0200

    Melhorando vínculo entre partner - participante, empresa - company

--- sped/models/res_partner.py
+++ sped/models/res_partner.py
@@ -17,11 +17,33 @@ class Partner(models.Model):
     def _is_brazilian_partner(self):
         self.is_brazilian_partner = self.sped_participante_id
 
-    is_brazilian_partner = fields.Boolean('Is a Brazilian partner?', compute=_is_brazilian_partner)
+    is_brazilian_partner = fields.Boolean('Is a Brazilian partner?', compute=_is_brazilian_partner, store=True)
 
     @api.one
     @api.depends('sped_participante_id', 'sped_empresa_id')
     def _is_brazilian_company(self):
         self.is_brazilian_company = self.sped_participante_id and self.sped_empresa_id
 
-    is_brazilian_company = fields.Boolean('Is a Brazilian company?', compute=_is_brazilian_company)
+    is_brazilian_company = fields.Boolean('Is a Brazilian company?', compute=_is_brazilian_company, store=True)
+
+    @api.one
+    def _original_company_id(self):
+        company_ids = self.env['res.company'].search([('partner_id', '=', self.id)])
+
+        if len(company_ids) > 0:
+            self.original_company_id = company_ids[0]
+        else:
+            self.original_company_id = False
+
+    original_company_id = fields.Many2one('res.company', 'Original Company', compute=_original_company_id, store=True, ondelete='cascade')
+
+    @api.one
+    def _original_user_id(self):
+        user_ids = self.env['res.users'].search([('partner_id', '=', self.id)])
+
+        if len(user_ids) > 0:
+            self.original_user_id = user_ids[0]
+        else:
+            self.original_user_id = False
+
+    original_user_id = fields.Many2one('res.users', 'Original User', compute=_original_user_id, store=True, ondelete='cascade')
