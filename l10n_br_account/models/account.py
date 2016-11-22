39d0e5cb8a0d60c0c15f05ee80c0481596641b00
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2016-11-22 19:58:45 -0200

    migrate l10n_br_account to V 10.0

--- l10n_br_account/models/account.py
+++ l10n_br_account/models/account.py
@@ -14,28 +14,29 @@ class AccountJournal(models.Model):
 class AccountTaxComputation(models.Model):
     _name = 'account.tax.computation'
 
+    domain = fields.Char(u'Domain', size=64)
     name = fields.Char('Name', size=64)
 
 
 class AccountTax(models.Model):
     _inherit = 'account.tax'
 
-    def _compute_tax(self, cr, uid, taxes, total_line, product, product_qty,
+    domain = fields.Char('Domain', required=True, size=8)
+
+    @api.model
+    def _compute_tax(self, taxes, total_line, product, product_qty,
                      precision):
         result = {'tax_discount': 0.0, 'taxes': []}
 
         for tax in taxes:
             if tax.get('type') == 'weight' and product:
-                product_read = self.pool.get('product.product').read(
-                    cr, uid, product, ['weight_net'])
-                weight_net = product_read.get('weight_net', 0.0)
+                weight_net = product.weight_net
                 float_val = product_qty * weight_net * tax['percent']
                 tax['amount'] = round(float_val, precision)
 
             if tax.get('type') == 'quantity':
                 tax['amount'] = round(product_qty * tax['percent'], precision)
 
-            tax['amount'] = round(total_line * tax['percent'], precision)
             tax['amount'] = round(
                 tax['amount'] * (1 - tax['base_reduction']), precision)
 
@@ -54,41 +55,42 @@ class AccountTax(models.Model):
         result['taxes'] = taxes
         return result
 
-    @api.v7
-    def compute_all(self, cr, uid, taxes, price_unit, quantity,
-                    product=None, partner=None, force_excluded=False,
-                    fiscal_position=False, insurance_value=0.0,
-                    freight_value=0.0, other_costs_value=0.0):
+    @api.multi
+    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None):
         """Compute taxes
 
-        Returns a dict of the form::
+                Returns a dict of the form::
 
-        {
-            'total': Total without taxes,
-            'total_included': Total with taxes,
-            'total_tax_discount': Total Tax Discounts,
-            'taxes': <list of taxes, objects>,
-            'total_base': Total Base by tax,
-        }
+                {
+                    'total': Total without taxes,
+                    'total_included': Total with taxes,
+                    'total_tax_discount': Total Tax Discounts,
+                    'taxes': <list of taxes, objects>,
+                    'total_base': Total Base by tax,
+                }
 
-        :Parameters:
-            - 'cr': Database cursor.
-            - 'uid': Current user.
-            - 'taxes': List with all taxes id.
-            - 'price_unit': Product price unit.
-            - 'quantity': Product quantity.
-            - 'force_excluded': Used to say that we don't want to consider
-                                the value of field price_include of tax.
-                                It's used in encoding by line where you don't
-                                matter if you encoded a tax with that boolean
-                                to True or False.
-        """
-        obj_precision = self.pool.get('decimal.precision')
-        precision = obj_precision.precision_get(cr, uid, 'Account')
-        result = super(
-            AccountTax, self).compute_all(
-            cr, uid, taxes, price_unit, quantity, product=product,
-            partner=partner, force_excluded=force_excluded)
+                :Parameters:
+                    - 'cr': Database cursor.
+                    - 'uid': Current user.
+                    - 'taxes': List with all taxes id.
+                    - 'price_unit': Product price unit.
+                    - 'quantity': Product quantity.
+                    - 'force_excluded': Used to say that we don't want to consider
+                                        the value of field price_include of tax.
+                                        It's used in encoding by line where you don't
+                                        matter if you encoded a tax with that boolean
+                                        to True or False.
+                """
+        precision = self.env['decimal.precision'].precision_get('Account')
+        if not currency:
+	    if len(self) == 0:
+	        company_id = self.env.user.company_id
+            else:
+	        company_id = self[0].company_id
+            currency = company_id.currency_id
+        taxes = self
+        result = super(AccountTax, self).compute_all(price_unit, currency=currency, quantity=quantity, product=product, partner=partner)
+
         totaldc = 0.0
         calculed_taxes = []
 
@@ -97,43 +99,31 @@ class AccountTax(models.Model):
             if tax_list:
                 tax_brw = tax_list[0]
             tax['domain'] = tax_brw.domain
-            tax['type'] = tax_brw.type
+            tax['type'] = tax_brw.type_tax_use
             tax['percent'] = tax_brw.amount
             tax['base_reduction'] = tax_brw.base_reduction
             tax['amount_mva'] = tax_brw.amount_mva
-            tax['tax_discount'] = tax_brw.base_code_id.tax_discount
+            tax['tax_discount'] = tax_brw.tax_discount
 
         common_taxes = [tx for tx in result['taxes'] if tx['domain']]
         result_tax = self._compute_tax(
-            cr, uid, common_taxes, result['total'],
+            common_taxes, result['total_excluded'],
             product, quantity, precision)
         totaldc += result_tax['tax_discount']
         calculed_taxes += result_tax['taxes']
 
         return {
-            'total': result['total'],
+            'total_excluded': result['total_excluded'],
             'total_included': result['total_included'],
             'total_tax_discount': totaldc,
             'taxes': calculed_taxes
         }
 
-    @api.v8
-    def compute_all(self, price_unit, quantity, product=None, partner=None,
-                    force_excluded=False, fiscal_position=False,
-                    insurance_value=0.0, freight_value=0.0,
-                    other_costs_value=0.0):
-        return self._model.compute_all(
-            self._cr, self._uid, self, price_unit, quantity,
-            product=product, partner=partner, force_excluded=force_excluded,
-            fiscal_position=fiscal_position, insurance_value=insurance_value,
-            freight_value=freight_value, other_costs_value=other_costs_value)
-
 
 class WizardMultiChartsAccounts(models.TransientModel):
     _inherit = 'wizard.multi.charts.accounts'
 
-    @api.v7
-    def execute(self, cr, uid, ids, context=None):
+    def execute(self):
         """This function is called at the confirmation of the wizard to
         generate the COA from the templates. It will read all the provided
         information to create the accounts, the banks, the journals, the
@@ -149,30 +139,24 @@ class WizardMultiChartsAccounts(models.TransientModel):
             - 'ids': orm_memory id used to read all data.
             - 'context': Context.
         """
-        result = super(WizardMultiChartsAccounts, self).execute(
-            cr, uid, ids, context)
+        result = super(WizardMultiChartsAccounts, self).execute()
 
-        obj_multi = self.browse(cr, uid, ids[0])
-        obj_fp_template = self.pool.get('account.fiscal.position.template')
-        obj_fp = self.pool.get('account.fiscal.position')
+        obj_multi = self[0]
+        obj_fp_template = self.env['account.fiscal.position.template']
+        obj_fp = self.env['account.fiscal.position']
 
         chart_template_id = obj_multi.chart_template_id.id
         company_id = obj_multi.company_id.id
 
-        fp_template_ids = obj_fp_template.search(
-            cr, uid, [('chart_template_id', '=', chart_template_id)])
+        fp_template_ids = obj_fp_template.search([('chart_template_id', '=', chart_template_id)])
 
-        for fp_template in obj_fp_template.browse(cr, uid, fp_template_ids,
-                                                  context=context):
+        for fp_template in fp_template_ids:
             if fp_template.cfop_id:
                 fp_id = obj_fp.search(
-                    cr, uid,
                     [('name', '=', fp_template.name),
                      ('company_id', '=', company_id)])
-
                 if fp_id:
-                    obj_fp.write(
-                        cr, uid, fp_id,
+                    fp_id.write(
                         {'cfop_id': fp_template.cfop_id.id})
         return result
 
@@ -180,24 +164,20 @@ class WizardMultiChartsAccounts(models.TransientModel):
 class AccountAccount(models.Model):
     _inherit = 'account.account'
 
-    @api.v7
-    def _check_allow_type_change(self, cr, uid, ids, new_type, context=None):
+    def _check_allow_type_change(self, new_type, context=None):
         """Hack to allow re-shaping demo chart of account in demo mode"""
-        cr.execute("""SELECT demo
+        self._cr.execute("""SELECT demo
             FROM ir_module_module WHERE name = 'l10n_br_account';""")
-        if cr.fetchone()[0]:
+        if self._cr.fetchone()[0]:
             return True
         else:
-            return super(AccountAccount, self)._check_allow_type_change(
-                cr, uid, ids, context)
+            return super(AccountAccount, self)._check_allow_type_change()
 
-    @api.v7
-    def _check_allow_code_change(self, cr, uid, ids, context=None):
+    def _check_allow_code_change(self, context=None):
         """Hack to allow re-shaping demo chart of account in demo mode"""
-        cr.execute("""SELECT demo
+        self._cr.execute("""SELECT demo
             FROM ir_module_module WHERE name = 'l10n_br_account';""")
-        if cr.fetchone()[0]:
+        if self._cr.fetchone()[0]:
             return True
         else:
-            return super(AccountAccount, self)._check_allow_code_change(
-                cr, uid, ids, context)
+            return super(AccountAccount, self)._check_allow_code_change()
