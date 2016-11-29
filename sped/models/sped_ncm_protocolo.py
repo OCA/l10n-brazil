901422b1a07b0368e9eac9bc5487af5931357a08
Ari Caldeira <aristides.caldeira@tauga.com.br>
2016-11-29 09:07:16 -0200

    Liberando cálculos dos impostos básicos

--- sped/models/sped_ncm_protocolo.py
+++ sped/models/sped_ncm_protocolo.py
@@ -11,3 +11,16 @@ class NCM(models.Model):
     _name = 'sped.ncm'
 
     protocolo_ids = fields.One2many('sped.protocolo.icms.ncm', 'ncm_id', 'Protocolos')
+
+    def busca_mva(self, protocolo):
+        busca = [
+            ('ncm_id', '=', self.id),
+            ('protocolo_id', '=', protocolo.id),
+        ]
+
+        protocolo_ids = self.protocolo_ids.search(busca)
+
+        if len(protocolo_ids) != 0:
+            return protocolo_ids[0]
+
+        return
