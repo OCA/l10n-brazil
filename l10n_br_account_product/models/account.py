54ff48f8103c0d30a66bdde305c863c324ea1005
Developer <renato.lima@akretion.com.br>
2016-11-30 12:52:55 +0000

    fix compute_all without fiscal_position arg

--- l10n_br_account_product/models/account.py
+++ l10n_br_account_product/models/account.py
@@ -123,8 +123,10 @@ class AccountTax(models.Model):
         totaldc = icms_value = 0.0
         ipi_value = 0.0
         calculed_taxes = []
-        id_dest = (fiscal_position.cfop_id and
-                   fiscal_position.cfop_id.id_dest or False)
+        id_dest = u''
+        if fiscal_position:
+            id_dest = (fiscal_position.cfop_id and
+                       fiscal_position.cfop_id.id_dest or False)
 
         for tax in result['taxes']:
             tax_list = [tx for tx in taxes if tx.id == tax['id']]
