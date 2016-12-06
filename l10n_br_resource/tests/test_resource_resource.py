d0c46ca3c9063caaa3aceebd86018f0be78e8884
Luis Felipe Mileo <mileo@kmee.com.br>
2016-12-06 07:43:23 -0200

    [NEW] Create l10n_br_resource module structure

---
+++ l10n_br_resource/tests/test_resource_resource.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 KMEE - Luis Felipe Mileo <mileo@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestResourceResource(TransactionCase):
+
+    def setUp(self):
+        super(TestResourceResource, self).setUp()
+
+    def test_1(self):
+        pass
