641c4f768ce0ab654e1f9ebe66310d10d70eff29
Luis Felipe Mileo <mileo@kmee.com.br>
2016-12-06 08:03:32 -0200

    [NEW] Add calendar hierarchy

--- l10n_br_resource/models/resource_calendar.py
+++ l10n_br_resource/models/resource_calendar.py
@@ -8,3 +8,21 @@ from openerp import api, fields, models, _
 class ResourceCalendar(models.Model):
 
     _inherit = 'resource.calendar'
+
+    parent_id = fields.Many2one(
+        'resource.calendar',
+        string='Parent Calendar',
+        ondelete='restrict',
+        index=True)
+    child_ids = fields.One2many(
+        'resource.calendar', 'parent_id',
+        string='Child Calendar')
+    _parent_store = True
+    parent_left = fields.Integer(index=True)
+    parent_right = fields.Integer(index=True)
+
+    @api.constrains('parent_id')
+    def _check_hierarchy(self):
+        if not self._check_recursion():
+            raise models.ValidationError(
+                'Error! You cannot create recursive calendars.')
