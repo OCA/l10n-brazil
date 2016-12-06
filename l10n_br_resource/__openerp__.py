4ef0e91d27de17011f065babbaa780186631f49e
Luis Felipe Mileo <mileo@kmee.com.br>
2016-12-06 07:31:47 -0200

    [NEW] Module l10n_br_resource

---
+++ l10n_br_resource/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Resource',
+    'summary': """
+        This module extend core resource to create important brazilian informations. Define a Brazilian calendar and some tools to compute dates used in financial and payroll modules""",
+    'version': '8.0.0.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'www.odoobrasil.org.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
