a0b6979b6145c489beaea5c1b23107fa35c2ff1a
sadamo <daniel.sadamo@kmee.com.br>
2016-12-07 12:15:29 -0200

    [REM] Removed module l10n_br_account_withholding

--- l10n_br_account_withholding/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 KMEE(http://www.kmee.com.br)
-#   @author Luis Felipe Mileo <mileo@kmee.com.br>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import models
--- l10n_br_account_withholding/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 KMEE(http://www.kmee.com.br)
-#   @author Luis Felipe Mileo <mileo@kmee.com.br>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import account_invoice
-from . import l10n_br_account
-from . import res_company
-from . import sale_order
