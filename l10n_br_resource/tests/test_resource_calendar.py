5974b8f9ec88201ac733e67bb8e5c0bc546500f0
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-08 15:38:38 -0200

    [FIX] Ajuste para passar nos testes

--- l10n_br_resource/tests/test_resource_calendar.py
+++ l10n_br_resource/tests/test_resource_calendar.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import fields
-from openerp.tests.common import TransactionCase
 from openerp.addons.resource.tests import common
 
 
@@ -15,25 +14,59 @@ class TestResourceCalendar(common.TestResourceCommon):
         self.resource_calendar = self.env['resource.calendar']
         self.resource_leaves = self.env['resource.calendar.leaves']
 
-        # # Some date for demo
-        # self.date3 = fields.Datetime.from_string('2016-10-04 10:11:12')
-        # self.date4 = fields.Datetime.from_string('2016-10-07 10:11:12')
-
-        # # Set as holiday a week after date3
-        self.holiday_start = fields.Datetime.from_string('2016-10-11 00:00:00')
-        self.holiday_end = fields.Datetime.from_string('2016-10-11 23:59:59')
-
-    def test_00_add_leave(self):
-        """ Inclusao de um novo Feriado """
-        calendar = self.resource_calendar.browse(self.calendar_id)
-        leave_ids = calendar.leave_ids
-        self.leave_nacional_01 = self.resource_leaves.create({
-            'name': 'Feriado Nacional 01',
-            'date_from': self.holiday_start,
-            'date_to': self.holiday_end,
-            'calendar_id': calendar.id
+        self.nacional_calendar_id = self.resource_calendar.create({
+            'name': 'Calendario Nacional',
         })
-        self.assertEqual(len(leave_ids) + 1, len(calendar.leave_ids))
+        self.estadual_calendar_id = self.resource_calendar.create({
+            'name': 'Calendario Nacional',
+            'parent_id': self.nacional_calendar_id.id,
+        })
+        self.municipal_calendar_id = self.resource_calendar.create({
+            'name': 'Calendario Nacional',
+            'parent_id': self.estadual_calendar_id.id,
+        })
 
-    def test_01_add_leave(self):
-        pass
+    def test_00_add_leave_nacional(self):
+        """ Inclusao de um novo Feriado no calendario nacional """
+        leave_nacional_01 = self.resource_leaves.create({
+            'name': 'Feriado Nacional 01',
+            'date_from': fields.Datetime.from_string('2016-12-24 00:00:00'),
+            'date_to': fields.Datetime.from_string('2016-12-24 23:59:59'),
+            'calendar_id': self.nacional_calendar_id.id,
+            'leave_type': 'F',
+            'abrangencia': 'N',
+        })
+        self.assertEqual(leave_nacional_01.name, 'Feriado Nacional 01')
+        self.assertEqual(leave_nacional_01.calendar_id,
+                         self.nacional_calendar_id)
+        self.assertEqual(1, len(self.nacional_calendar_id.leave_ids))
+
+    def test_01_add_leave_estadual(self):
+        """ Inclusao de um novo Feriado no calendario Estadual """
+        leave_estadual_01 = self.resource_leaves.create({
+            'name': 'Feriado Estadual 01',
+            'date_from': fields.Datetime.from_string('2016-07-16 00:00:00'),
+            'date_to': fields.Datetime.from_string('2016-07-16 23:59:59'),
+            'calendar_id': self.estadual_calendar_id.id,
+            'leave_type': 'F',
+            'abrangencia': 'E',
+        })
+        self.assertEqual(leave_estadual_01.name, 'Feriado Estadual 01')
+        self.assertEqual(leave_estadual_01.calendar_id,
+                         self.estadual_calendar_id)
+        self.assertEqual(1, len(self.estadual_calendar_id.leave_ids))
+
+    def test_01_add_leave_municipal(self):
+        """ Inclusao de um novo Feriado no calendario municipal """
+        leave_municipal_01 = self.resource_leaves.create({
+            'name': 'Feriado Municipal 01',
+            'date_from': fields.Datetime.from_string('2016-03-10 00:00:00'),
+            'date_to': fields.Datetime.from_string('2016-03-10 23:59:59'),
+            'calendar_id': self.municipal_calendar_id.id,
+            'leave_type': 'F',
+            'abrangencia': 'E',
+        })
+        self.assertEqual(leave_municipal_01.name, 'Feriado Municipal 01')
+        self.assertEqual(leave_municipal_01.calendar_id,
+                         self.municipal_calendar_id)
+        self.assertEqual(1, len(self.municipal_calendar_id.leave_ids))
