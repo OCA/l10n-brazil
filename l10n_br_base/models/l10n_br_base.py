a3df797ff26a04c1907c76020e38481ef5d06c48
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-12 12:58:56 -0200

    Replace openerp imports to odoo

--- l10n_br_base/models/l10n_br_base.py
+++ l10n_br_base/models/l10n_br_base.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2009  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class L10nBrBaseCity(models.Model):
