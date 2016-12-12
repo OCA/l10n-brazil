6dc9bdd875a9924f5c0682259fcc99a456af1a3d
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-12 12:00:07 -0200

    [MIG] migrate module l10n_br_zip_correios to V10

--- l10n_br_zip_correios/models/webservice_client.py
+++ l10n_br_zip_correios/models/webservice_client.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-#    Address from Brazilian Localization ZIP by Correios to Odoo
-#    Copyright (C) 2015 KMEE (http://www.kmee.com.br)
-#    @author Michell Stuttgart <michell.stuttgart@kmee.com.br>
+# Copyright (C) 2015 KMEE (http://www.kmee.com.br)
+# @author Michell Stuttgart <michell.stuttgart@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import logging
@@ -25,6 +24,7 @@ _logger = logging.getLogger(__name__)
 
 
 class WebServiceClient(object):
+    """Address from Brazilian Localization ZIP by Correios to Odoo"""
 
     def get_address(self, zip_code):
 
