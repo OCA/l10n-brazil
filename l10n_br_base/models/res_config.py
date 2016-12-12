72f819e5323cc400d1544c98851a24c5982ba674
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-12 12:58:56 -0200

    [FIX] Replace openerp imports to odoo

--- l10n_br_base/models/res_config.py
+++ l10n_br_base/models/res_config.py
@@ -2,7 +2,7 @@
 # @ 2016 Kmee - www.kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class ResConfig(models.TransientModel):
