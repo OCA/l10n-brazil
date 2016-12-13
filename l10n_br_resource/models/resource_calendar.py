1b68076c170184337b9e5bf56584183258647892
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-13 10:25:55 -0200

    [FIX] Ajuste para passar nos testes

--- l10n_br_resource/models/resource_calendar.py
+++ l10n_br_resource/models/resource_calendar.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright 2016 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
 # Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
