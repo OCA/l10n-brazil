72b64c6f444330e29bf1dc77f8d5657a5f43b8b0
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-15 15:50:53 -0200

    [FIX]  Replace openerp imports to odoo

--- l10n_br_zip/models/res_partner.py
+++ l10n_br_zip/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2010-2012  Renato Lima (Akretion)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, api
+from odoo import models, api
 
 
 class ResPartner(models.Model):
