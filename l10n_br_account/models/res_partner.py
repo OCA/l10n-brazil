29458161c73a3e5d067187152d03292ccd633531
Renato Lima <renato.lima@akretion.com>
2016-12-15 19:04:14 -0200

    migrate fiscal position

--- l10n_br_account/models/res_partner.py
+++ l10n_br_account/models/res_partner.py
@@ -4,44 +4,76 @@
 
 from odoo import models, fields, api
 
-from .l10n_br_account import PRODUCT_FISCAL_TYPE, TYPE
+from .l10n_br_account import TYPE
 
 
-class AccountFiscalPositionTemplate(models.Model):
-    _inherit = 'account.fiscal.position.template'
+class AccountFiscalPositionAbstract(object):
 
-    name = fields.Char('Fiscal Position', size=128, required=True)
     fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category', 'Categoria Fiscal')
-    fiscal_category_fiscal_type = fields.Selection(
-        PRODUCT_FISCAL_TYPE, related='fiscal_category_id.fiscal_type',
-        readonly=True, store=True, string='Fiscal Type')
+        'l10n_br_account.fiscal.category',
+        u'Categoria Fiscal'
+    )
+
     type = fields.Selection(
-        TYPE, 'Tipo')
-    type_tax_use = fields.Selection(
-        [('sale', 'Sale'), ('purchase', 'Purchase'), ('all', 'All')],
-        'Tax Application')
-    inv_copy_note = fields.Boolean('Copiar Observação na Nota Fiscal')
+        TYPE,
+        related='fiscal_category_id.type',
+        readonly=True,
+        store=True,
+        string=u'Fiscal Type'
+    )
+
+    inv_copy_note = fields.Boolean(
+        string=u'Copiar Observação na Nota Fiscal'
+    )
+
     asset_operation = fields.Boolean(
-        'Operação de Aquisição de Ativo',
-        help="""Caso seja marcada essa opção, será incluido o IPI na base de
-        calculo do ICMS.""")
+        string=u'Operação de Aquisição de Ativo',
+        help=u"""Caso seja marcada essa opção, será incluido o IPI na base de
+        calculo do ICMS."""
+    )
+
     state = fields.Selection(
         [('draft', u'Rascunho'),
-         ('review', u'Revisão'), ('approved', u'Aprovada'),
-         ('unapproved', u'Não Aprovada')], 'Status', readonly=True,
-        track_visibility='onchange', select=True, default='draft')
+         ('review', u'Revisão'),
+         ('approved', u'Aprovada'),
+         ('unapproved', u'Não Aprovada')],
+        string='Status',
+        readonly=True,
+        track_visibility='onchange',
+        index=True,
+        default='draft'
+    )
 
-    @api.onchange('type')
-    def onchange_type(self):
-        type_tax = {'input': 'purhcase', 'output': 'sale'}
-	self.type_tax_use = type_tax.get(self.type, 'none')
-	self.tax_ids = False
 
-    @api.onchange('fiscal_category_id')
-    def onchange_fiscal_category_id(self):
-        if self.fiscal_category_id:
-            self.fiscal_category_fiscal_type = self.fiscal_category_id.fiscal_type
+class AccountFiscalPositionTaxAbstract(object):
+
+    tax_group_id = fields.Many2one(
+        comodel_name='account.tax.group',
+        string=u'Grupo de Impostos',
+    )
+
+    @api.onchange('tax_src_id',
+                  'tax_group_id',
+                  'position_id')
+    def onchange_tax_group(self):
+        type_tax_use = {'input': 'purchase', 'output': 'sale'}
+
+        domain = [('type_tax_use', 'in',
+                  (type_tax_use.get(self.position_id.type), 'none'))]
+
+        if self.tax_group_id:
+            domain.append(('tax_group_id', '=', self.tax_group_id.id))
+
+        if self.tax_src_id:
+            domain.append(('tax_group_id', '=',
+                           self.tax_src_id.tax_group_id.id))
+
+        return {'domain': {'tax_dest_id': domain, 'tax_src_id': domain}}
+
+
+class AccountFiscalPositionTemplate(AccountFiscalPositionAbstract,
+                                    models.Model):
+    _inherit = 'account.fiscal.position.template'
 
     def generate_fiscal_position(self, chart_temp_id,
                                  tax_template_ref, acc_template_ref,
@@ -91,6 +123,21 @@ class AccountFiscalPositionTemplate(models.Model):
                     tax.tax_src_id and
                     tax_template_ref.get(tax.tax_src_id.id, False),
                     'tax_code_src_id':
+                    tax.tax_code_src_id,
+                'type': position.type,
+                'state': position.state,
+                'type_tax_use': position.type_tax_use,
+                'cfop_id':
+                    position.cfop_id and position.cfop_id.id or False,
+                'inv_copy_note': position.inv_copy_note,
+                'asset_operation': position.asset_operation,
+                'fiscal_category_id': position.fiscal_category_id and position.fiscal_category_id.id or False})
+            for tax in position.tax_ids:
+                obj_tax_fp.create({
+                    'tax_src_id':
+                    tax.tax_src_id and
+                    tax_template_ref.get(tax.tax_src_id.id, False),
+                    'tax_code_src_id':
                     tax.tax_code_src_id and
                     tax_code_template_ref.get(tax.tax_code_src_id.id, False),
                     'tax_src_domain': tax.tax_src_domain,
@@ -110,175 +157,58 @@ class AccountFiscalPositionTemplate(models.Model):
         return True
 
 
-class AccountFiscalPositionTaxTemplate(models.Model):
+class AccountFiscalPositionTaxTemplate(AccountFiscalPositionTaxAbstract,
+                                       models.Model):
+
     _inherit = 'account.fiscal.position.tax.template'
 
     tax_src_id = fields.Many2one(
-        'account.tax.template', string='Tax Source', required=False)
-    #tax_code_src_id = fields.Many2one(
-    #    'account.tax.code.template', string=u'Código Taxa Origem')
-    #TODO MIG
-    # tax_src_domain = fields.Char(
-    #     related='tax_src_id.domain', string='Tax Domain')
-    #tax_code_dest_id = fields.Many2one(
-    #    'account.tax.code.template', string='Replacement Tax Code')
-
-    @staticmethod
-    def _tax_domain(tax_src_id, tax_code_src_id):
-        tax_domain = False
-        if tax_src_id:
-            tax_domain = tax_src_id.domain
-        if tax_code_src_id:
-            tax_domain = tax_code_src_id.domain
-        return tax_domain
-
-    #@api.onchange('tax_src_id', 'tax_code_src_id')
-    def onchange_tax_src_id(self):
-        if self.tax_code_src_id or self.tax_src_id:
-            self.tax_src_domain = self._tax_domain(
-                self.tax_src_id,
-                self.tax_code_src_id
-            )
+        comodel_name='account.tax.template',
+        string='Tax on Product',
+        required=False
+    )
 
 
-class AccountFiscalPosition(models.Model):
+class AccountFiscalPosition(AccountFiscalPositionAbstract,
+                            models.Model):
+
     _inherit = 'account.fiscal.position'
 
-    name = fields.Char('Fiscal Position', size=128, required=True)
-    fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category', 'Categoria Fiscal')
-    fiscal_category_fiscal_type = fields.Selection(
-        PRODUCT_FISCAL_TYPE, related='fiscal_category_id.fiscal_type',
-        readonly=True, store=True, string='Fiscal Type')
-    type = fields.Selection(
-        TYPE, 'Tipo')
-    type_tax_use = fields.Selection(
-        [('sale', 'Sale'), ('purchase', 'Purchase'), ('all', 'All')],
-        'Tax Application')
-    inv_copy_note = fields.Boolean('Copiar Observação na Nota Fiscal')
-    asset_operation = fields.Boolean(
-        'Operação de Aquisição de Ativo',
-        help="""Caso seja marcada essa opção, será incluido o IPI na base de
-        calculo do ICMS.""")
-    state = fields.Selection(
-        [('draft', u'Rascunho'),
-         ('review', u'Revisão'), ('approved', u'Aprovada'),
-         ('unapproved', u'Não Aprovada')], 'Status', readonly=True,
-        track_visibility='onchange', select=True, default='draft')
+    @api.model
+    def map_tax(self, taxes, product=None, partner=None):
+        result = self.env['account.tax'].browse()
 
-    @api.onchange('type')
-    def onchange_type(self):
-        type_tax = {'input': 'purhcase', 'output': 'sale'}
-	self.type_tax_use = type_tax.get(self.type, 'none')
-	self.tax_ids = False
+        if self.company_id and \
+                self.env.context.get('type_tax_use') in ('sale', 'all'):
+            if self.env.context.get('fiscal_type', 'product') == 'service':
+                company_taxes = self.company_id.service_tax_ids
 
-    @api.onchange('fiscal_category_id')
-    def onchange_fiscal_category_id(self):
-        if self.fiscal_category_id:
-            self.fiscal_category_fiscal_type = self.fiscal_category_id.fiscal_type
+            if taxes:
+                taxes |= company_taxes
+
+        for tax in taxes:
+            tax_count = 0
+            for t in self.tax_ids:
+                if (t.tax_src_id == tax or
+                        t.tax_group_id == tax.tax_group_id):
+                    tax_count += 1
+                    if t.tax_dest_id:
+                        result |= t.tax_dest_id
+            if not tax_count:
+                result |= tax
+        return result
 
 
-    #@TODO MIG
+class AccountFiscalPositionTax(AccountFiscalPositionTaxAbstract,
+                               models.Model):
 
-    # @api.v7
-    # def map_tax(self, cr, uid, fposition_id, taxes, context=None):
-    #     result = []
-    #     if not context:
-    #         context = {}
-    #     if fposition_id and fposition_id.company_id and\
-    #             context.get('type_tax_use') in ('sale', 'all'):
-    #         if context.get('fiscal_type', 'product') == 'product':
-    #             company_tax_ids = self.env['res.company').read(
-    #                 cr, uid, fposition_id.company_id.id, ['product_tax_ids'],
-    #                 context=context)['product_tax_ids']
-    #         else:
-    #             company_tax_ids = self.env['res.company').read(
-    #                 cr, uid, fposition_id.company_id.id, ['service_tax_ids'],
-    #                 context=context)['service_tax_ids']
-    #
-    #         company_taxes = self.env['account.tax').browse(
-    #             cr, uid, company_tax_ids, context=context)
-    #         if taxes:
-    #             all_taxes = taxes + company_taxes
-    #         else:
-    #             all_taxes = company_taxes
-    #         taxes = all_taxes
-    #
-    #     if not taxes:
-    #         return []
-    #     if not fposition_id:
-    #         return map(lambda x: x.id, taxes)
-    #     for t in taxes:
-    #         ok = False
-    #         tax_src = False
-    #         for tax in fposition_id.tax_ids:
-    #             tax_src = tax.tax_src_id and tax.tax_src_id.id == t.id
-    #             tax_code_src = tax.tax_code_src_id and \
-    #                 tax.tax_code_src_id.id == t.tax_code_id.id
-    #
-    #             if tax_src or tax_code_src:
-    #                 if tax.tax_dest_id:
-    #                     result.append(tax.tax_dest_id.id)
-    #                 ok = True
-    #         if not ok:
-    #             result.append(t.id)
-    #
-    #     return list(set(result))
-    #
-    # @api.v8
-    # def map_tax(self, taxes):
-    #     result = self.env['account.tax'].browse()
-    #     if self.company_id and \
-    #             self.env.context.get('type_tax_use') in ('sale', 'all'):
-    #         if self.env.context.get('fiscal_type', 'product') == 'product':
-    #             company_taxes = self.company_id.product_tax_ids
-    #         else:
-    #             company_taxes = self.company_id.service_tax_ids
-    #
-    #         if taxes:
-    #             taxes |= company_taxes
-    #
-    #     for tax in taxes:
-    #         for t in self.tax_ids:
-    #             if t.tax_src_id == tax or t.tax_code_src_id == tax.tax_code_id:
-    #                 if t.tax_dest_id:
-    #                     result |= t.tax_dest_id
-    #                 break
-    #         else:
-    #             result |= tax
-    #     return result
-
-
-class AccountFiscalPositionTax(models.Model):
     _inherit = 'account.fiscal.position.tax'
 
     tax_src_id = fields.Many2one(
-        'account.tax', string='Tax Source', required=False)
-    # TODO MIG
-    tax_code_src_id = fields.Many2one(
-        'account.tax.group', u'Código Taxa Origem')
-    #TODO MIG
-    #tax_src_domain = fields.Char(related='tax_src_id.domain')
-    tax_code_dest_id = fields.Many2one(
-        'account.tax.group', 'Replacement Tax Code')
-
-    @staticmethod
-    def _tax_domain(tax_src_id, tax_code_src_id):
-        tax_domain = False
-        if tax_src_id:
-            tax_domain = tax_src_id.domain
-        if tax_code_src_id:
-            tax_domain = tax_code_src_id.domain
-        return tax_domain
-
-    #@api.onchange('tax_src_id', 'tax_code_src_id')
-    def onchange_tax_src_id(self):
-        if self.tax_code_src_id or self.tax_src_id:
-            self.tax_src_domain = self._tax_domain(
-                self.tax_src_id,
-                self.tax_code_src_id
-            )
-
+        comodel_name='account.tax',
+        string='Tax on Product',
+        required=False
+    )
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
