43a5e892bb2f35d550f014c163b2c5ab233f00d3
Renato Lima <renato.lima@akretion.com>
2016-12-15 15:04:57 -0200

    [FIX] imports

--- l10n_br_account/models/product.py
+++ l10n_br_account/models/product.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 from .l10n_br_account import PRODUCT_FISCAL_TYPE, PRODUCT_FISCAL_TYPE_DEFAULT
 
 
