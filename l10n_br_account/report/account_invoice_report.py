f95738d90e29b55050d8e6295159327092d76f41
Renato Lima <renato.lima@akretion.com>
2016-12-15 15:04:57 -0200

    [FIX] imports

--- l10n_br_account/report/account_invoice_report.py
+++ l10n_br_account/report/account_invoice_report.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2016  Magno Costa - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class AccountInvoiceReport(models.Model):
