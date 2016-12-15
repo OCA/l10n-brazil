08a7845cea3435c6802a4a46859342504ff5e96a
renato <renato.lima@akretion.com.br>
2016-12-15 15:12:45 -0200

    Migrated object account.tax

--- l10n_br_account/models/account.py
+++ l10n_br_account/models/account.py
@@ -11,21 +11,12 @@ class AccountJournal(models.Model):
     revenue_expense = fields.Boolean('Gera Financeiro')
 
 
-class AccountTaxComputation(models.Model):
-    _name = 'account.tax.computation'
-
-    domain = fields.Char(u'Domain', size=64)
-    name = fields.Char('Name', size=64)
-
-
 class AccountTax(models.Model):
     _inherit = 'account.tax'
 
-    domain = fields.Char('Domain', required=True, size=8)
-
     @api.model
-    def _compute_tax(self, taxes, total_line, product, product_qty,
-                     precision):
+    def _compute_tax(self, taxes, total_line,
+                     product, product_qty, precision):
         result = {'tax_discount': 0.0, 'taxes': []}
 
         for tax in taxes:
@@ -56,31 +47,36 @@ class AccountTax(models.Model):
         return result
 
     @api.multi
-    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None):
-        """Compute taxes
+    def compute_all(self, price_unit, currency=None,
+                    quantity=1.0, product=None, partner=None):
+        """Returns all information required to apply taxes 
+        (in self + their children in case of a tax goup).
+        
+        We consider the sequence of the parent for group of taxes.
+            Eg. considering letters as taxes and alphabetic order as sequence:
+            [G, B([A, D, F]), E, C] will be computed as [A, D, F, C, E, G]
 
-                Returns a dict of the form::
+        Args:
+            price_unit (float): Product price unit
+            currency (ResCurrency): Currency
+            quantity (float): Product quantity
+            product (ProductProduct): Product
+            partner (ResPartner): Partner
 
-                {
-                    'total': Total without taxes,
-                    'total_included': Total with taxes,
-                    'total_tax_discount': Total Tax Discounts,
-                    'taxes': <list of taxes, objects>,
-                    'total_base': Total Base by tax,
-                }
+        Returns: {
+            'total_excluded': 0.0,    # Total without taxes
+            'total_included': 0.0,    # Total with taxes
+            'taxes': [{
+                'id': int,
+                'name': str,
+                'amount': float,
+                'sequence': int,
+                'account_id': int,
+                'refund_account_id': int,
+                'analytic': boolean,
+            }]
+        """
 
-                :Parameters:
-                    - 'cr': Database cursor.
-                    - 'uid': Current user.
-                    - 'taxes': List with all taxes id.
-                    - 'price_unit': Product price unit.
-                    - 'quantity': Product quantity.
-                    - 'force_excluded': Used to say that we don't want to consider
-                                        the value of field price_include of tax.
-                                        It's used in encoding by line where you don't
-                                        matter if you encoded a tax with that boolean
-                                        to True or False.
-                """
         precision = self.env['decimal.precision'].precision_get('Account')
         if not currency:
 	    if len(self) == 0:
@@ -89,7 +85,11 @@ class AccountTax(models.Model):
 	        company_id = self[0].company_id
             currency = company_id.currency_id
         taxes = self
-        result = super(AccountTax, self).compute_all(price_unit, currency=currency, quantity=quantity, product=product, partner=partner)
+        result = super(AccountTax, self).compute_all(price_unit,
+                                                     currency=currency,
+                                                     quantity=quantity,
+                                                     product=product,
+                                                     partner=partner)
 
         totaldc = 0.0
         calculed_taxes = []
@@ -98,14 +98,14 @@ class AccountTax(models.Model):
             tax_list = [tx for tx in taxes if tx.id == tax['id']]
             if tax_list:
                 tax_brw = tax_list[0]
-            tax['domain'] = tax_brw.domain
+            tax['group'] = tax_brw.tax_group_id.name
             tax['type'] = tax_brw.type_tax_use
             tax['percent'] = tax_brw.amount
             tax['base_reduction'] = tax_brw.base_reduction
             tax['amount_mva'] = tax_brw.amount_mva
             tax['tax_discount'] = tax_brw.tax_discount
 
-        common_taxes = [tx for tx in result['taxes'] if tx['domain']]
+        common_taxes = [tx for tx in result['taxes'] if tx['group']]
         result_tax = self._compute_tax(
             common_taxes, result['total_excluded'],
             product, quantity, precision)
