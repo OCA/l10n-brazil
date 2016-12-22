a5b0fee23bc40e39c5e595767aa7ab4536e92cd7
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-22 17:10:53 -0200

    [ADD] funcao de validacao de dias de acordo com tipo de holiday

--- l10n_br_hr_holiday/models/hr_holidays.py
+++ l10n_br_hr_holiday/models/hr_holidays.py
@@ -15,3 +15,25 @@ class HrHolidays(models.Model):
         string=u"Mensagem",
         related='holiday_status_id.message',
     )
+
+    def validate_days_status_id(self):
+        resource_calendar_obj = self.env['resource.calendar']
+        for record in self:
+            if record.holiday_status_id.type_day:
+                if record.holiday_status_id.type_day == u'uteis':
+                   if resource_calendar_obj.retorna_num_dias_uteis(
+                            datetime.strptime(
+                                record.date_from,"%Y-%m-%d %H:%M:%S"),
+                            datetime.strptime(
+                                record.date_to,"%Y-%m-%d %H:%M:%S")
+                    ) > record.holiday_status_id.days_limit:
+                        raise UserError(_("Número de dias excedido!"))
+                if record.holiday_status_id.type_day == u'corridos':
+                    if record.number_of_days_temp > \
+                            record.holiday_status_id.days_limit:
+                        raise UserError(_("Número de dias excedido!"))
+
+    @api.multi
+    def holidays_confirm(self):
+        self.validate_days_status_id()
+        return super(HrHolidays, self).holidays_confirm()
