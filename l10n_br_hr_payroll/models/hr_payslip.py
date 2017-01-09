b3dad97c3145776ed69f22d5c66d19b66b497deb
Hendrix Costa <hendrixcosta@gmail.com>
2017-01-09 19:22:27 -0200

    [ADD] Função para calcular as referencias da folha de pagamento (Dias Base/faltas/DSR perdidos)

--- l10n_br_hr_payroll/models/hr_payslip.py
+++ l10n_br_hr_payroll/models/hr_payslip.py
@@ -16,16 +16,16 @@ class HrPayslip(models.Model):
         @return: returns a list of dict containing the input that should
         be applied for the given contract between date_from and date_to
         """
-
         result = []
         for contract_id in self:
+
             # get dias uteis
-            worked_days = self.env['resource.calendar'].retorna_num_dias_uteis(
+            worked_days = self.env['resource.calendar'].get_dias_base(
                 fields.Datetime.from_string(date_from),
                 fields.Datetime.from_string(date_to),
             )
             attendances = {
-                 'name': u"Dias Úteis",
+                 'name': u"Dias Base",
                  'sequence': 1,
                  'code': 'WORK100',
                  'number_of_days': worked_days,
@@ -33,4 +33,35 @@ class HrPayslip(models.Model):
                  'contract_id': contract_id.id,
             }
             result += [attendances]
-        return result
+
+            # get faltas
+            leaves = {}
+            hr_contract = self.env['hr.contract'].browse(contract_id.id)
+            leaves = self.env['resource.calendar'].get_ocurrences(
+                hr_contract.employee_id.id, date_from,date_to
+            )
+            if leaves.get('faltas_nao_remuneradas'):
+                attendances = {
+                     'name': u"Faltas Não remuneradas",
+                     'sequence': 2,
+                     'code': 'FNR',
+                     'number_of_days': -len(leaves['faltas_nao_remuneradas']),
+                     'number_of_hours': 0.0,
+                     'contract_id': contract_id.id,
+                }
+                result += [attendances]
+
+            # get discount DSR
+            quantity_DSR_discount = self.env['resource.calendar'].\
+                get_quantity_discount_DSR(leaves['faltas_nao_remuneradas'])
+            if leaves.get('faltas_nao_remuneradas'):
+                ref_quantity_DSR_discount = {
+                    'name': u"DSR a serem descontados",
+                    'sequence': 3,
+                    'code': 'DSR',
+                    'number_of_days': -quantity_DSR_discount,
+                    'number_of_hours': 0.0,
+                    'contract_id': contract_id.id,
+                }
+                result += [ref_quantity_DSR_discount]
+            return result
