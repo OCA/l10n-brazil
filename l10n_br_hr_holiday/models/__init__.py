479ba18a6154b19e380f64530404523d1e8e8bfd
Hendrix Costa <hendrixcosta@gmail.com>
2017-01-09 19:22:27 -0200

    [ADD] Função para calcular as referencias da folha de pagamento (Dias Base/faltas/DSR perdidos)

--- l10n_br_hr_holiday/models/__init__.py
+++ l10n_br_hr_holiday/models/__init__.py
@@ -1,2 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import hr_holidays
 from . import hr_holidays_status
+from . import resource_calendar
