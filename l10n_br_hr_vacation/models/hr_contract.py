245da36a7cb604142acaa482dc968ab50596318a
Hendrix Costa <hendrixcosta@gmail.com>
2017-02-01 17:21:26 -0200

    [ADD] Criar controle de ferias

--- l10n_br_hr_vacation/models/hr_contract.py
+++ l10n_br_hr_vacation/models/hr_contract.py
@@ -2,7 +2,7 @@
 # Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import api, models, fields
 
 
 class HrContract(models.Model):
@@ -13,3 +13,13 @@ class HrContract(models.Model):
         inverse_name='contract_id',
         string='Periodos Aquisitivos Alocados'
     )
+
+    @api.model
+    def create(self, vals):
+        controle_ferias = self.env['hr.vacation.control'].create({
+            'inicio_aquisitivo' : '2017-01-01',
+            'fim_aquisitivo' : '2017-01-31',
+        })
+        hr_contract_id = super(HrContract, self).create(vals)
+        hr_contract_id.vacation_control_ids = controle_ferias
+        return hr_contract_id
