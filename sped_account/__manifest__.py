691bb7e9a924faae6290ecfa2d047eb1ff2e1824
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-02-02 20:46:40 -0200

    [WIP] Adcionando módulo interligação sped - account

---
+++ sped_account/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 Taŭga Tecnologia - Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
+#
+
+{
+    'name': u'SPED - Account',
+    'version': '10.0.1.0.0',
+    'author': u'Ari Caldeira',
+    'maintainer': u'Taŭga Tecnologia',
+    'category': u'Base',
+    'depends': [
+        'sped',
+        'account',
+    ],
+    'installable': True,
+    'application': False,
+    'data': [
+        #'data/inherited_product_uom_category_data.xml',
+        #'data/inherited_product_uom_data.xml',
+        #'data/inherited_sped_unidade_data.xml',
+
+        'views/inherited_account_invoice_view.xml',
+    ]
+}
---
+++ sped_account/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 Taŭga Tecnologia - Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
+#
+
+
+from __future__ import division, print_function, unicode_literals
+
+from inherited_sped_documento import *
+from inherited_sped_documento_item import *
+
+from inherited_account_invoice import *
+from inherited_account_invoice_line import *
