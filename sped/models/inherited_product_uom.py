723e0e3db1d10631a474d48de16d7b2bee66a01b
vonpupp <vonpupp@gmail.com>
2017-02-03 16:49:14 -0200

    PEP8 inherited_product_uom

--- sped/models/inherited_product_uom.py
+++ sped/models/inherited_product_uom.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2016 Taŭga Tecnologia - Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# Copyright 2016 Taŭga Tecnologia
+#   Aristides Caldeira <aristides.caldeira@tauga.com.br>
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
 #
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductUoM(models.Model):
