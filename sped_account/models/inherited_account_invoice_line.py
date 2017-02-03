bda68aee170fc2f624ba0b3e4b1ce19062b3ccb6
Luis Felipe Mileo <mileo@kmee.com.br>
2017-02-03 20:34:55 -0200

    [REM] unused import

--- sped_account/models/inherited_account_invoice_line.py
+++ sped_account/models/inherited_account_invoice_line.py
@@ -5,6 +5,17 @@
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
 #
 
+
+# import logging
+# _logger = logging.getLogger(__name__)
+
+# try:
+# from pybrasil.valor import valor_por_extenso_item
+# from pybrasil.valor.decimal import Decimal as D
+
+# except (ImportError, IOError) as err:
+# _logger.debug(err)
+
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
