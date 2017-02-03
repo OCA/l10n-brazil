42327485975bbd0be9fa6b16ef6c64c843c2c872
Luis Felipe Mileo <mileo@kmee.com.br>
2017-02-03 17:54:29 -0200

    fix imports

--- sped/models/__init__.py
+++ sped/models/__init__.py
@@ -1,75 +1,76 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2016 Taŭga Tecnologia - Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# Copyright 2016 Taŭga Tecnologia
+#   Aristides Caldeira <aristides.caldeira@tauga.com.br>
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
 #
 
 #
 # Cópia de views
 #
-from inherited_ir_ui_view import *
+from . import inherited_ir_ui_view
 
 #
 # Model Base
 #
-from sped_base import *
+from . import sped_base
 
 #
 # Tabelas Geográficas
 #
-from sped_pais import *
-from sped_estado import *
-from sped_municipio import *
+from . import sped_pais
+from . import sped_estado
+from . import sped_municipio
 #
 # Cadastros básicos
 #
-from sped_participante import *
-from sped_empresa import *
-from inherited_res_partner import *
+from . import sped_participante
+from . import sped_empresa
+from . import inherited_res_partner
 #
 # Unidade é requisito para NCM e produtos
 #
-from sped_unidade import *
-from inherited_product_uom import *
+from . import sped_unidade
+from . import inherited_product_uom
 #
 # Alíquotas
 #
-from sped_aliquota_icms_proprio import *
-from sped_aliquota_icms_st import *
-from sped_aliquota_ipi import *
-from sped_aliquota_pis_cofins import *
-from sped_aliquota_simples import *
+from . import sped_aliquota_icms_proprio
+from . import sped_aliquota_icms_st
+from . import sped_aliquota_ipi
+from . import sped_aliquota_pis_cofins
+from . import sped_aliquota_simples
 #
 # Tabelas Fiscais
 #
-from sped_cest import *
-from sped_cfop import *
-from sped_cnae import *
-from sped_ncm import *
-from sped_servico import *
-from sped_nbs import *
-from sped_ibptax import *
-from sped_ncm_cest import *
-from sped_ncm_ibptax import *
+from . import sped_cest
+from . import sped_cfop
+from . import sped_cnae
+from . import sped_ncm
+from . import sped_servico
+from . import sped_nbs
+from . import sped_ibptax
+from . import sped_ncm_cest
+from . import sped_ncm_ibptax
 
-from sped_aliquota_iss import *
-from sped_protocolo_icms import *
-from sped_ncm_protocolo import *
+from . import sped_aliquota_iss
+from . import sped_protocolo_icms
+from . import sped_ncm_protocolo
 #
-from sped_certificado import *
+from . import sped_certificado
 #
-from sped_produto import *
-from inherited_product_template import *
-from inherited_product_product import *
+from . import sped_produto
+from . import inherited_product_template
+from . import inherited_product_product
 #
 # #
 # # Fiscal e faturamento
 # #
-from sped_natureza_operacao import *
-from sped_operacao import *
-from sped_operacao_item import *
-from sped_veiculo import *
-from sped_documento import *
-from sped_documento_item import *
-from sped_documento_volume import *
-from sped_documento_duplicata import *
+from . import sped_natureza_operacao
+from . import sped_operacao
+from . import sped_operacao_item
+from . import sped_veiculo
+from . import sped_documento
+from . import sped_documento_item
+from . import sped_documento_volume
+from . import sped_documento_duplicata
