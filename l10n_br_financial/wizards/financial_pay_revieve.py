19dd4046b4311e6772a5b56053f6e85a77734905
luanaduarte <luana.duarte@kmee.com.br>
2017-02-09 18:05:30 -0200

    [ADD] Balanco financeiro

--- l10n_br_financial/wizards/financial_pay_revieve.py
+++ l10n_br_financial/wizards/financial_pay_revieve.py
@@ -8,6 +8,7 @@ from odoo import api, fields, models, _
 class FinancialPay_revieve(models.TransientModel):
 
     _name = 'financial.pay_revieve'
+    valor_pago = fields.Float()
 
     name = fields.Char()
 
@@ -15,12 +16,36 @@ class FinancialPay_revieve(models.TransientModel):
     def doit(self):
         for wizard in self:
             # TODO
-            pass
-        action = {
-            'type': 'ir.actions.act_window',
-            'name': 'Action Name',  # TODO
-            'res_model': 'result.model',  # TODO
-            'domain': [('id', '=', result_ids)],  # TODO
-            'view_mode': 'form,tree',
-        }
-        return action
+
+            active_id = self._context['active_id']
+            account_financial = self.env['financial.move']
+
+            financial_to_pay = account_financial.browse(active_id)
+
+            if financial_to_pay.type == 'p':
+                payment_type = 'pp'
+            else:
+                payment_type = 'rr'
+
+
+
+            account_financial.create({
+                'currency_id': 1,
+                'company_id': 1,
+                'amount_document': wizard.valor_pago,
+                'payment_id': active_id,
+                'type': payment_type,
+            })
+
+        # action = {
+        #     'type': 'ir.actions.act_window',
+        #     'name': 'Action Name',  # TODO
+        #     'res_model': 'result.model',  # TODO
+        #     'domain': [('id', '=', result_ids)],  # TODO
+        #     'view_mode': 'form,tree',
+        # }
+        # return action
+        return True
+
+
+
