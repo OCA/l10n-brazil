de7edd03d87d1637832850c528359e4c74fa4b2e
Luis Felipe Mileo <mileo@kmee.com.br>
2017-02-15 15:38:34 -0200

    [REF] Importação automatica do calendário durante a instalação do módulo

--- l10n_br_resource/__init__.py
+++ l10n_br_resource/__init__.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# Copyright 2017 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models, wizard
+
+import logging
+from odoo import api, fields, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+
+def create_national_calendar(cr, registry):
+    """Generate """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    wizard_pybrasil = env['wizard.pybrasil.holiday.import'].create(
+        {
+            'start_date': fields.Date.today(),
+            'interval_type': 'years',
+            'interval_number': 1,
+        }
+    )
+    wizard_pybrasil.holiday_import()
+    _logger.info(
+        """Create new Brazilian calendar.
+        \n from {start_date} to {end_date} """.format(
+            start_date=wizard_pybrasil.start_date,
+            end_date=wizard_pybrasil.end_date
+        ))
