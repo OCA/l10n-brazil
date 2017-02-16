d4027d0c6e8bfdd31fb58b1b98951802854067d6
Luis Felipe Mileo <mileo@kmee.com.br>
2017-02-16 13:29:33 -0200

    [NEW] Test for asserting amount_document is >= 0

--- l10n_br_financial/tests/test_financial_move.py
+++ l10n_br_financial/tests/test_financial_move.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
-
+from odoo.exceptions import ValidationError
 
 class TestFinancialMove(TransactionCase):
 
@@ -26,3 +26,16 @@ class TestFinancialMove(TransactionCase):
         ))
         self.assertEqual(cr_1.business_due_date, '2017-03-01')
 
+    def test_2(self):
+        """DADO a data de vencimento de 27/02/2017
+        QUANDO criado um lançamento de contas a receber
+        ENTÃO a data de vencimento útil deve ser de 01/03/2017
+        """
+        with self.assertRaises(ValidationError):
+            cr_2 = self.financial_move.create(
+                dict(due_date='2017-02-27',
+                     company_id=self.main_company.id,
+                     currency_id=self.currency_euro.id,
+                     amount_document=0.00,
+                     )
+            )
