5a663797ddcd0f3e862062c94cbe8a75e15b445e
Luis Felipe Mileo <mileo@kmee.com.br>
2017-02-16 12:30:19 -0200

    [NEW] Constrains to check if amount_document is higher then zero

--- l10n_br_financial/tests/test_financial_move.py
+++ l10n_br_financial/tests/test_financial_move.py
@@ -17,11 +17,12 @@ class TestFinancialMove(TransactionCase):
         """ DADO a data de vencimento de 27/02/2017
         QUANDO criado um lançamento de contas a receber
         ENTÃO a data de vencimento útil deve ser de 01/03/2017
-        :return:
         """
         cr_1 = self.financial_move.create(dict(
             due_date='2017-02-27',
             company_id=self.main_company.id,
             currency_id=self.currency_euro.id,
+            amount_document=100.00,
         ))
         self.assertEqual(cr_1.business_due_date, '2017-03-01')
+
