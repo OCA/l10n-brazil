5bc5270bab58e2534f7f578a4d3ee38212b00481
Hendrix Costa <hendrixcosta@gmail.com>
2017-02-20 19:16:14 -0300

    [REM] Removendo relatorio de aviso previo (portado para outro repositorio)

--- l10n_br_hr_payroll/__openerp__.py
+++ l10n_br_hr_payroll/__openerp__.py
@@ -43,7 +43,6 @@
         'views/hr_salary_rule.xml',
         'views/hr_payroll_structure.xml',
         'views/hr_payslip_run.xml',
-        'reports/reports.xml',
     ],
     'demo': [
         'demo/hr_contract.xml',
--- l10n_br_hr_payroll/reports/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-# Copyright (C) 2017 - Hendrix Costa - KMEE
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-import payslip_report_aviso_ferias
