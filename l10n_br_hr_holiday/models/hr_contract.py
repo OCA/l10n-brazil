1377bf8219ecefc627f63c45f4f6a4cb28bae617
Hendrix Costa <hendrixcosta@gmail.com>
2017-03-08 20:31:46 -0300

    [IMP] Relacao entre contrato e holidays

---
+++ l10n_br_hr_holiday/models/hr_contract.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+from datetime import datetime
+
+
+class HrContract(models.Model):
+    _inherit = 'hr.contract'
+
+    hr_holiday_ids = fields.One2many(
+        comodel_name='hr.holidays',
+        inverse_name='contrato_id',
+        string='Abonos de faltas'
+    )
