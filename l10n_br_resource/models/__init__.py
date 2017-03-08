31abe4b948842b6d6c80dda07bba8e3322b27d81
Hendrix Costa <hendrixcosta@gmail.com>
2017-03-08 17:40:17 -0300

    [FIX] Separando classes em arquivos diferentes

--- l10n_br_resource/models/__init__.py
+++ l10n_br_resource/models/__init__.py
@@ -3,3 +3,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import resource_calendar
+from . import resource_calendar_leaves
--- l10n_br_resource/models/resource_calendar.py
+++ l10n_br_resource/models/resource_calendar.py
@@ -235,33 +235,3 @@ class ResourceCalendar(models.Model):
             return 30
         else:
             return quantidade_dias
-
-
-class ResourceCalendarLeave(models.Model):
-
-    _inherit = 'resource.calendar.leaves'
-
-    country_id = fields.Many2one(
-        'res.country', string=u'País',
-        related='calendar_id.country_id',
-    )
-    state_id = fields.Many2one(
-        'res.country.state', u'Estado',
-        related='calendar_id.state_id',
-        domain="[('country_id','=',country_id)]",
-        readonly=True
-    )
-    l10n_br_city_id = fields.Many2one(
-        'l10n_br_base.city', u'Municipio',
-        related='calendar_id.l10n_br_city_id',
-        domain="[('state_id','=',state_id)]",
-        readonly=True
-    )
-    leave_type = fields.Selection(
-        string=u'Tipo',
-        selection=[item for item in TIPO_FERIADO.iteritems()],
-    )
-    abrangencia = fields.Selection(
-        string=u'Abrangencia',
-        selection=[item for item in ABRANGENCIA_FERIADO.iteritems()],
-    )
