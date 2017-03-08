20edd061a0a263006c0e9577e37383cf35bd2405
Hendrix Costa <hendrixcosta@gmail.com>
2017-03-08 19:17:09 -0300

    [FIX] Refatoração nas dependencias

--- l10n_br_hr_syndicate/__openerp__.py
+++ l10n_br_hr_syndicate/__openerp__.py
@@ -11,9 +11,6 @@
     'website': 'http://www.kmee.com.br',
     'version': '8.0.0.0.1',
     'depends': [
-        'l10n_br_hr_holiday',
-        'l10n_br_resource',
-        'l10n_br_hr_contract',
         'l10n_br_hr_payroll'
     ],
     'data': [
