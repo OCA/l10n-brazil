ea0595675dfefd268274926760a8302db0cea00c
Luis Felipe Mileo <mileo@kmee.com.br>
2017-03-16 23:24:01 -0300

    [REF] pylint tests

--- l10n_br_account_banking_payment_cnab/febraban/cnab_240/cnab_240.py
+++ l10n_br_account_banking_payment_cnab/febraban/cnab_240/cnab_240.py
@@ -37,7 +37,7 @@ _logger = logging.getLogger(__name__)
 try:
     from cnab240.tipos import Arquivo
 except ImportError as err:
-    _logger.debug = (err)
+    _logger.debug = err
 
 
 class Cnab240(Cnab):
