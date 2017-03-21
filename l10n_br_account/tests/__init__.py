c0b1af08055ef00fce07e66d733dd57755348d41
sadamo <daniel.sadamo@kmee.com.br>
2017-03-21 18:01:39 -0300

    Migrate tests to python format

---
+++ l10n_br_account/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017  Daniel Sadamo - KMEE INFORMATICA LTDA
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import test_account_invoice
