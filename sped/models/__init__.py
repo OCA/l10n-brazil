32261eb29a73ca9ea026aa9174d48e0ea2f52b9f
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-04 17:25:48 -0300

    [FEA] Sincronizando as casas decimais do SPED com a moeda do valor       unitário

--- sped/models/__init__.py
+++ sped/models/__init__.py
@@ -11,19 +11,34 @@
 from . import inherited_ir_ui_view
 
 #
+# Sincronização de casas decimais
+#
+from . import inherited_decimal_precision
+
+#
 # Model Base
 #
 from . import sped_base
 
 #
+# Tabelas Geográficas
+#
+from . import sped_pais
+from . import sped_estado
+from . import sped_municipio
+
+#
 # Cadastros básicos
 #
 from . import sped_empresa
+from . import inherited_res_partner
+
 #
 # Unidade é requisito para NCM e produtos
 #
 from . import sped_unidade
 from . import inherited_product_uom
+
 #
 # Alíquotas
 #
@@ -32,6 +47,7 @@ from . import sped_aliquota_icms_st
 from . import sped_aliquota_ipi
 from . import sped_aliquota_pis_cofins
 from . import sped_aliquota_simples
+
 #
 # Tabelas Fiscais
 #
@@ -47,6 +63,7 @@ from . import sped_ncm_ibptax
 from . import sped_aliquota_iss
 from . import sped_protocolo_icms
 from . import sped_ncm_protocolo
+
 #
 # Produtos e serviços
 #
@@ -69,6 +86,7 @@ from . import sped_documento_referenciado
 from . import sped_documento_item
 from . import sped_documento_item_declaracao_importacao
 from . import sped_documento_item_declaracao_importacao_adicao
+
 #
 # Parcelamentos e pagamentos
 #
