b1773cc99b718b7fc5dc883a71c078284f44ae6a
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-10 15:31:12 -0300

    [FEA] Movendo mais models para o base

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
+#
 # Copyright (C) 2015  Renato Lima - Akretion
+# Copyright 2016 Taŭga Tecnologia
+#   Aristides Caldeira <aristides.caldeira@tauga.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#
 
-# from . import res_bank
-# from . import res_partner
-# from . import res_country
-# from . import res_company
-# from . import l10n_br_base
-# from . import res_config
+#
+# Cópia de views
+#
+from . import inherited_ir_ui_view
+
+#
+# Sincronização de casas decimais
+#
+from . import inherited_decimal_precision
+
+#
+# Model Base
+#
+from . import sped_base
 
 #
 # Tabelas Geográficas
