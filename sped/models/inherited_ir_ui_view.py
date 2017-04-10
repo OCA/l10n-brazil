2b56690ba513253c7341c85a2da7b23ba91bb169
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-10 14:07:43 -0300

    [FIX] Cópia de views com outros módulos pré-instalados

--- sped/models/inherited_ir_ui_view.py
+++ sped/models/inherited_ir_ui_view.py
@@ -5,8 +5,14 @@
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
 #
 
-from odoo import api, fields, models
+from lxml import etree
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 from odoo.modules.module import get_resource_from_path
+from odoo.tools.parse_version import parse_version
+from odoo.tools.view_validation import valid_view
+from odoo.tools.translate import encode
 
 
 class IrUiView(models.Model):
@@ -18,42 +24,57 @@ class IrUiView(models.Model):
         ondelete='restrict',
         index=True,
     )
-    # copy_id = fields.Many2one(
-    # 'ir.ui.view', string='Copied View', index=True)
-    # inherit_id = fields.Many2one(
-    # 'ir.ui.view', string='Inherited View', index=True)
-
-    def _compute_arch(self):
-        for view in self:
-            if view.copy_id:
-                view.arch = view.copy_id.arch
-            else:
-                super(IrUiView, view)._compute_arch()
-
-    def _inverse_arch(self):
-        for view in self:
-            if not view.copy_id:
-                super(IrUiView, view)._inverse_arch()
-                continue
-
-            data = dict(arch_db=view.copy_id.arch)
-
-            if 'install_mode_data' in self._context:
-                imd = self._context['install_mode_data']
-                if '.' not in imd['xml_id']:
-                    imd['xml_id'] = '%s.%s' % (imd['module'], imd['xml_id'])
-                if self._name == imd['model'] and (not view.xml_id or view.xml_id == imd['xml_id']):
-                    # we store the relative path to the resource instead of the absolute path, if found
-                    # (it will be missing e.g. when importing data-only modules using base_import_module)
-                    path_info = get_resource_from_path(imd['xml_file'])
-                    if path_info:
-                        data['arch_fs'] = '/'.join(path_info[0:2])
-
-            view.write(data)
 
     @api.model
     def create(self, values):
+        #
+        # Ensures that the arch of the copied view is realy copied from the
+        # original view
+        #
         if values.get('copy_id'):
-            values['type'] = self.browse(values['copy_id']).type
+            values['arch'] = self.browse(values['copy_id']).arch
 
         return super(IrUiView, self).create(values)
+
+    @api.model
+    def write(self, values):
+        copied_views = []
+
+        for view in self:
+            #
+            # Saves copied views for latter proccess
+            #
+            cv = self.search([('copy_id', '=', view.id)])
+            if cv:
+                copied_views += [[view.id, cv]]
+
+            #
+            # Ensures that the arch of the copied view remains copied from the
+            # original view
+            #
+            if values.get('copy_id'):
+                values['arch'] = self.browse(values['copy_id']).arch
+
+        res = super(IrUiView, self).write(values)
+
+        if copied_views:
+            for view_id, cv in copied_views:
+                view = self.browse(view_id)
+                cv.write({'arch': view.arch})
+
+        return res
+
+    @api.constrains('arch_db')
+    def _check_xml(self):
+        for view in self:
+            #
+            # When the view is being copied, there's no need to validate it's
+            # arch, because the original view has already been validated
+            #
+            if view.copy_id:
+                continue
+
+            super(IrUiView, view)._check_xml()
+
+        return True
+
