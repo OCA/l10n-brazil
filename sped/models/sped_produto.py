6e8576c53b54c0dc233e5d8ab922251347786e51
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-18 17:12:00 -0300

    [FIX] Corrigindo pesquisas (name_search) [FEA] Código e nome de produto únicos [FEA] Preço unitário automático [FEA] Transportadora e condição no participante

--- sped/models/sped_produto.py
+++ sped/models/sped_produto.py
@@ -7,20 +7,18 @@
 
 import logging
 
-import odoo.addons.decimal_precision as dp
-from odoo import api, fields, models
 from odoo.addons.l10n_br_base.constante_tributaria import (
     ORIGEM_MERCADORIA,
     TIPO_PRODUTO_SERVICO,
     TIPO_PRODUTO_SERVICO_MATERIAL_USO_CONSUMO,
     TIPO_PRODUTO_SERVICO_SERVICOS,
 )
-from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
 try:
     from pybrasil.produto import valida_ean
+    from pybrasil.base import tira_acentos
 
 except (ImportError, IOError) as err:
     _logger.debug(err)
@@ -47,10 +45,24 @@ class Produto(models.Model):
         size=120,
         index=True,
     )
+    nome_unico = fields.Char(
+        string=u'Nome',
+        size=120,
+        index=True,
+        compute='_compute_nome_unico',
+        store=True,
+    )
     codigo = fields.Char(
         string=u'Código',
         size=60,
         index=True,
+    )
+    codigo_unico = fields.Char(
+        string=u'Código',
+        size=60,
+        index=True,
+        compute='_compute_codigo_unico',
+        store=True
     )
     codigo_barras = fields.Char(
         string=u'Código de barras',
@@ -187,6 +199,58 @@ class Produto(models.Model):
     def onchange_codigo_barras(self):
         return self._valida_codigo_barras()
 
+    @api.depends('codigo')
+    def _compute_codigo_unico(self):
+        for produto in self:
+            codigo_unico = produto.codigo or ''
+            codigo_unico = codigo_unico.lower().strip()
+            codigo_unico = codigo_unico.replace(u' ', u' ')
+            codigo_unico = codigo_unico.replace(u'²', u'2')
+            codigo_unico = codigo_unico.replace(u'³', u'3')
+            codigo_unico = tira_acentos(codigo_unico)
+            produto.codigo_unico = codigo_unico
+
+    @api.depends('nome')
+    def _compute_nome_unico(self):
+        for produto in self:
+            nome_unico = produto.nome or ''
+            nome_unico = nome_unico.lower().strip()
+            nome_unico = nome_unico.replace(u' ', u' ')
+            nome_unico = nome_unico.replace(u'²', u'2')
+            nome_unico = nome_unico.replace(u'³', u'3')
+            nome_unico = tira_acentos(nome_unico)
+            produto.nome_unico = nome_unico
+
+    @api.depends('codigo')
+    def _check_codigo(self):
+        for produto in self:
+            if produto.id:
+                produto_ids = self.search([
+                    ('codigo_unico', '=', produto.codigo_unico),
+                    ('id', '!=', produto.id)
+                ])
+            else:
+                produto_ids = self.search([
+                    ('codigo_unico', '=', produto.codigo_unico)
+                ])
+
+            if len(produto_ids) > 0:
+                raise ValidationError(u'Código de produto já existe!')
+
+    @api.depends('nome')
+    def _check_nome(self):
+        for produto in self:
+            if produto.id:
+                produto_ids = self.search(
+                    [('nome_unico', '=', produto.nome_unico),
+                     ('id', '!=', produto.id)])
+            else:
+                produto_ids = self.search(
+                    [('nome_unico', '=', produto.nome_unico)])
+
+            if len(produto_ids) > 0:
+                raise ValidationError(u'Nome de produto já existe!')
+
     @api.onchange('ncm_id')
     def onchange_ncm(self):
         if len(self.ncm_id.cest_ids) == 1:
@@ -257,3 +321,37 @@ class Produto(models.Model):
         res = super(Produto, self).write(dados)
         self.sync_to_product()
         return res
+
+    @api.multi
+    def name_get(self):
+        res = []
+
+        for produto in self:
+            nome = u'['
+            nome += produto.codigo
+            nome += u'] '
+            nome += produto.nome
+            res.append((produto.id, nome))
+
+        return res
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        args = args or []
+        if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
+            args += [
+                '|',
+                ['codigo', operator, name],
+                '|',
+                ['codigo_unico', operator, name.lower()],
+                '|',
+                ['codigo_barras', '=', name],
+                '|',
+                ['nome', operator, name],
+                ['nome_unico', operator, name.lower()],
+            ]
+            produtos = self.search(args, limit=limit)
+            return produtos.name_get()
+
+        return super(Produto, self).name_search(name=name, args=args,
+                                                operator=operator, limit=limit)
