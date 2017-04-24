903d2a8e440707af042748f83441fad67e8ee758
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-24 14:27:55 -0300

    [FEA] Novas opções de vencimentos

--- l10n_br_base/models/inherited_account_payment_term.py
+++ l10n_br_base/models/inherited_account_payment_term.py
@@ -11,13 +11,28 @@ import logging
 
 from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
-from odoo.addons.l10n_br_base.constante_tributaria import *
+from odoo.addons.l10n_br_base.constante_tributaria import (
+    FORMA_PAGAMENTO,
+    BANDEIRA_CARTAO,
+    INTEGRACAO_CARTAO,
+    INTEGRACAO_CARTAO_NAO_INTEGRADO,
+    FORMA_PAGAMENTO_CARTOES,
+    FORMA_PAGAMENTO_CARTAO_CREDITO,
+    FORMA_PAGAMENTO_CARTAO_DEBITO,
+    FORMA_PAGAMENTO_OUTROS,
+    FORMA_PAGAMENTO_DICT,
+    BANDEIRA_CARTAO_DICT,
+)
 
 _logger = logging.getLogger(__name__)
 
 try:
     from pybrasil.valor.decimal import Decimal as D
-    from pybrasil.data
+    from pybrasil.data import (
+        dia_util_pagamento,
+        DIA_SEGUNDA, DIA_TERCA, DIA_QUARTA, DIA_QUINTA, DIA_SEXTA,
+        primeiro_dia_mes, ultimo_dia_mes, dias_uteis,
+    )
 
 except (ImportError, IOError) as err:
     _logger.debug(err)
@@ -28,6 +43,77 @@ class AccountPaymentTerm(models.Model):
     _rec_name = 'nome_comercial'
     _order = 'sequence, name'
 
+    DIAS_UTEIS = (
+        (str(DIA_SEGUNDA), 'Segunda-feira'),
+        (str(DIA_TERCA), 'Terça-feira'),
+        (str(DIA_QUARTA), 'Quarta-feira'),
+        (str(DIA_QUINTA), 'Quinta-feira'),
+        (str(DIA_SEXTA), 'Sexta-feira'),
+    )
+
+    ADIA_ANTECIPA_DIA_UTIL = (
+        ('P', 'Adia'),
+        ('A', 'Antecipa'),
+    )
+    ADIA_DIA_UTIL = 'P'
+    ANTECIPA_DIA_UTIL = 'A'
+
+    DIAS_MES = (
+        ('1', '1º'),
+        #('2', '2'),
+        #('3', '3'),
+        #('4', '4'),
+        ('5', '5'),
+        #('6', '6'),
+        #('7', '7'),
+        #('8', '8'),
+        #('9', '9'),
+        ('10', '10'),
+        #('11', '11'),
+        #('12', '12'),
+        #('13', '13'),
+        #('14', '14'),
+        ('15', '15'),
+        #('16', '16'),
+        #('17', '17'),
+        #('18', '18'),
+        #('19', '19'),
+        ('20', '20'),
+        #('21', '21'),
+        #('22', '22'),
+        #('23', '23'),
+        #('24', '24'),
+        ('25', '25'),
+        #('26', '26'),
+        #('27', '27'),
+        #('28', '28'),
+        #('29', '29'),
+        ('30', '30'),
+        #('31', '31'),
+    )
+    DIAS_MES_UTIL = (
+        ('1', '1º'),
+        #('2', '2º'),
+        #('3', '3º'),
+        #('4', '4º'),
+        ('5', '5º'),
+        #('6', '6º'),
+        #('7', '7º'),
+        #('8', '8º'),
+        #('9', '9º'),
+        ('10', '10º'),
+        #('11', '11º'),
+        #('12', '12º'),
+        #('13', '13º'),
+        #('14', '14º'),
+        ('15', '15º'),
+        #('16', '16º'),
+        #('17', '17º'),
+        #('18', '18º'),
+        #('19', '19º'),
+        ('20', '20º'),
+    )
+
     sequence = fields.Integer(
         default=10,
     )
@@ -35,8 +121,8 @@ class AccountPaymentTerm(models.Model):
         string='Em parcelas mensais?',
         default=True,
     )
-    tem_entrada = fields.Boolean(
-        string='Tem entrada?',
+    com_entrada = fields.Boolean(
+        string='Com entrada?',
     )
     meses = fields.Integer(
         string='Meses',
@@ -44,9 +130,27 @@ class AccountPaymentTerm(models.Model):
     somente_dias_uteis = fields.Boolean(
         string='Somente dias úteis?',
     )
+    antecipa_dia_util = fields.Selection(
+        selection=ADIA_ANTECIPA_DIA_UTIL,
+        string='Adia ou antecipa dia útil?',
+        default=ADIA_DIA_UTIL,
+    )
+    todo_dia_mes = fields.Selection(
+        selection=DIAS_MES,
+        string='Vencimento todo dia',
+    )
+    todo_dia_mes_util = fields.Selection(
+        selection=DIAS_MES_UTIL,
+        string='Vencimento todo dia útil',
+    )
+    evitar_dia_semana = fields.Selection(
+        selection=DIAS_UTEIS,
+        string='Evitar vencimento em',
+    )
     forma_pagamento = fields.Selection(
         selection=FORMA_PAGAMENTO,
         string='Forma de pagamento',
+        default=FORMA_PAGAMENTO_OUTROS,
     )
     bandeira_cartao = fields.Selection(
         selection=BANDEIRA_CARTAO,
@@ -116,6 +220,50 @@ class AccountPaymentTerm(models.Model):
 
             payment_term.nome_comercial = nome_comercial
 
+    def _verifica_dia_util(self, data):
+        self.ensure_one()
+
+        if self.somente_dias_uteis:
+            if self.env.user.company_id.sped_empresa_id:
+                empresa = self.env.user.company_id.sped_empresa_id
+                if self.antecipa_dia_util == self.ANTECIPA_DIA_UTIL:
+                    data = dia_util_pagamento(data, empresa.estado,
+                                              empresa.cidade, antecipa=True)
+                else:
+                    data = dia_util_pagamento(data, empresa.estado,
+                                              empresa.cidade)
+            else:
+                if self.antecipa_dia_util == self.ANTECIPA_DIA_UTIL:
+                    data = dia_util_pagamento(data, antecipa=True)
+                else:
+                    data = dia_util_pagamento(data)
+
+        if self.evitar_dia_semana and data.weekday() == \
+            int(self.evitar_dia_semana):
+            data += relativedelta(days=1)
+            data = self._verifica_dia_util(data)
+
+        return data
+
+    def _verifica_dia_mes(self, data):
+        self.ensure_one()
+
+        if self.todo_dia_mes:
+            data += relativedelta(day=int(self.todo_dia_mes))
+
+        elif self.todo_dia_mes_util:
+            if self.env.user.company_id.sped_empresa_id:
+                empresa = self.env.user.company_id.sped_empresa_id
+                dias = dias_uteis(primeiro_dia_mes(data), ultimo_dia_mes(data),
+                                  empresa.estado, empresa.cidade)
+            else:
+                dias = dias_uteis(primeiro_dia_mes(data), ultimo_dia_mes(data))
+
+            if int(self.todo_dia_mes_util) <= len(dias):
+                data = dias[int(self.todo_dia_mes_util) - 1]
+
+        return data
+
     def compute(self, value, date_ref=False):
         self.ensure_one()
 
@@ -143,21 +291,14 @@ class AccountPaymentTerm(models.Model):
         for i in range(meses):
             proxima_data = fields.Date.from_string(data_referencia)
 
-            if self.tem_entrada:
-                proxima_data += relativedelta(meses=i)
+            if self.com_entrada:
+                proxima_data += relativedelta(months=i)
 
             else:
-                proxima_data += relativedelta(meses=i + 1)
+                proxima_data += relativedelta(months=i + 1)
 
-            if self.somente_dias_uteis:
-                if self.env.user.company_id.sped_empresa_id:
-                    empresa = self.env.user.company_id.sped_empresa_id
-                    proxima_data = dia_util_pagamento(proxima_data,
-                                                      empresa.estado,
-                                                      empresa.cidade
-                                                      )
-                else:
-                    proxima_data = dia_util_pagamento(proxima_data)
+            proxima_data = self._verifica_dia_mes(proxima_data)
+            proxima_data = self._verifica_dia_util(proxima_data)
 
             parcela = [
                 fields.Date.to_string(proxima_data),
