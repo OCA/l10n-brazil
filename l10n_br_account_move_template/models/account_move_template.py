b29add126dccaed4e64df537136ce411663b140a
bruna.braga <bruna_braga@unifei.edu.br>
2017-04-25 18:21:52 -0300

    [FIX] proodut_origin field

--- l10n_br_account_move_template/models/account_move_template.py
+++ l10n_br_account_move_template/models/account_move_template.py
@@ -3,6 +3,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, fields, models
+from openerp.addons.l10n_br_account_product.models.product import \
+    PRODUCT_ORIGIN
 
 OPERATION_NATURE = [
     ('venda', u'Venda'),
@@ -15,27 +17,6 @@ OPERATION_POSITION = [
     ('dentro_estado', u'Dentro do estado'),
     ('exportacao', u'Exportação'),
 
-]
-
-PRODUCT_ORIGIN = [
-    ('0', u'0 - Nacional, exceto as indicadas nos códigos 3 a 5'),
-    ('1', u'1 - Estrangeira - Importação direta, exceto a indicada no código'
-     ' 6'),
-    ('2', u'2 - Estrangeira - Adquirida no mercado interno, exceto a indicada'
-     u' no código 7'),
-    ('3', u'3 - Nacional, mercadoria ou bem com Conteúdo de Importação'
-     ' superior a 40% (quarenta por cento)'),
-    ('4', u'4 - Nacional, cuja produção tenha sido feita em conformidade com'
-     u' os processos produtivos básicos de que tratam o Decreto-Lei nº 288/67,'
-     u' e as Leis nºs 8.248/91, 8.387/91, 10.176/01 e 11.484/07'),
-    ('5', u'5 - Nacional, mercadoria ou bem com Conteúdo de Importação'
-     u' inferior ou igual a 40% (quarenta por cento)'),
-    ('6', u'6 - Estrangeira - Importação direta, sem similar nacional,'
-     u' constante em lista de Resolução CAMEX'),
-    ('7', u'7 - Estrangeira - Adquirida no mercado interno, sem similar'
-     u' nacional, constante em lista de Resolução CAMEX'),
-    ('8', u'8 - Nacional, mercadoria ou bem com Conteúdo de Importação'
-     u' superior a 70%')
 ]
 
 TERM = [
@@ -86,18 +67,14 @@ class AccountMoveTemplate(models.Model):
         related='fiscal_type.type',
         string=u'Tipo fiscal do produto'
     )
-    p_origin = fields.Many2one(
-        comodel_name='product.product',
-        string=u'Origem do produto'
-    )
     product_origin = fields.Selection(
-        related='p_origin.origin',
+        selection=PRODUCT_ORIGIN,
         string=u'Origem do produto'
     )
     term = fields.Selection(selection=TERM)
     # TODO: qual a melhor forma de estruturar?
     # operation_purpose = fields.Selection(selection=OPERATION_PURPOSE)
-    #account_move_type = fields.Selection(selection=MOVE_TYPE)
+    # account_move_type = fields.Selection(selection=MOVE_TYPE)
     credit_account_id = fields.Many2one(
         comodel_name='account.account', string=u'Conta de credito'
     )
@@ -128,7 +105,8 @@ class AccountMoveTemplate(models.Model):
                 company_id=invoice.company_id.id,
                 fiscal_document_id=invoice.fiscal_document_id.id,
                 product_origin=line.product_id.origin,
-
+                # operation_destination=line.product_id.cfop.id_dest,
+                # product_fiscal_type=line.product.template.type,
             )
         )
         for key, value in values_dict.iteritems():
