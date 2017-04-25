de5698f523cea40c0396ccda715074924797c59a
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-24 22:04:11 -0300

    [FIX] PyLint - imports

--- l10n_br_base/models/inherited_account_payment_term.py
+++ l10n_br_base/models/inherited_account_payment_term.py
@@ -9,9 +9,10 @@ from __future__ import division, print_function, unicode_literals
 
 import logging
 
-from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
-from odoo.addons.l10n_br_base.constante_tributaria import (
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from ..constante_tributaria import (
     FORMA_PAGAMENTO,
     BANDEIRA_CARTAO,
     INTEGRACAO_CARTAO,
