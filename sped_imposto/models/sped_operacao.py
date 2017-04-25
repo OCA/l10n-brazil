872625ae068acf19b1eab5b92d9f0b99d290a167
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-04-25 19:52:14 -0300

    [FEA] Model sped.calculo.imposto independente do item do doc. fiscal [FIX] Ajustando referência das unidades de medida nos dados dos NCMs [FEA] Views do produto/serviço separadas do l10n_br_base [FEA] Cálculo do imposto pela operação fiscal (método)

--- sped_imposto/models/sped_operacao.py
+++ sped_imposto/models/sped_operacao.py
@@ -200,7 +200,29 @@ class SpedOperacaoFiscal(models.Model):
     preco_automatico = fields.Selection(
         selection=[
             ('V', 'Venda'),
-            ('C', 'Custo')
+            ('C', 'Custo'),
         ],
         string='Traz preço automático?',
     )
+
+    def calcula_imposto(self):
+        self.ensure_one()
+
+        calculo = self.env['sped.calculo.imposto'].new()
+        calculo.operacao_id = self.id
+        #calculo.empresa_id = empresa_id
+        #calculo.participante_id = participante_id
+        #calculo.produto_id = produto_id
+        #calculo.quantidade = quantidade
+        #calculo.vr_unitario = vr_unitario
+
+        calculo.empresa_id = 1
+        calculo.participante_id = 1
+        calculo.produto_id = 1
+        calculo.quantidade = 10
+        calculo.vr_unitario = 100
+        calculo.data_emissao = fields.Date.today()
+        calculo.calcula_impostos()
+
+        print(calculo.vr_nf)
+
