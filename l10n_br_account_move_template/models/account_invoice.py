59d0079bcec45c6231a81ec3f6bd7e72769b9172
Hendrix Costa <hendrixcosta@gmail.com>
2017-04-28 12:10:14 -0300

    [ADD] Criar contra-partidas apenas para as partidas corretas e remover partidas dos produtos

--- l10n_br_account_move_template/models/account_invoice.py
+++ l10n_br_account_move_template/models/account_invoice.py
@@ -2,40 +2,70 @@
 # Copyright (C) 2017 - Daniel Sadamo - KMEE INFORMATICA
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models
+from openerp import models
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def define_account(self, move_line):
+    def define_account(self, move_line, type):
+        """
+        Definir contas das novas partidas de acordo com o roteiro contabil
+        :param move_line:  - dict - partida duplicada para contra-partida
+                                    na conta contabil
+        :param type: sting - Tipo de partida ['CLIENTE', 'RECEITA', 'IMPOSTO']
+        :return:
+        """
         amt = self.env['account.move.template']
-        if move_line[2].tax_amount == 0:  # definir linha receita
+        if type == 'CLIENTE':
             # definir parâmetros search
             amt = amt.search([('type', '=', 'receipt')])
+
             if move_line[2].debit:
-                am = amt.debit_account_id
+                account_id = amt.debit_account_id
             else:
-                am = amt.credit_account_id
+                account_id = amt.credit_account_id
 
-        if False:  # definir linha impostopr
+        elif type == 'IMPOSTO':
             amt = amt.search([]) #definir parâmetros search
-            am = amt.debit_account_id
+            account_id = amt.debit_account_id
 
-        return am or move_line['account_id']
+        # Se definir uma conta no mapeamento, seta a conta,
+        # senão fica com a conta padrão
+        if account_id:
+            move_line['account_id'] = account_id
 
     def finalize_invoice_move_lines(self, move_lines):
+        """
+        Sobrescrever hook do core para criar as contrapartidas de acordo com
+        o tipo da partida e mapear as contas seguindo o roteiro contábil.
+        :param move_lines: lista com [0,0, dict] - Move lines geradas pelo core
+        :return: - lista com [0,0, dict] onde o dict são as partidas a serem 
+                                         criadas para o lançamento contabil
+                                         da invoice.
+        """
         move_lines = super(AccountInvoice, self).finalize_invoice_move_lines(
             move_lines)
         lines = list(move_lines)
         for move in lines:
-            partida = move[2]
-            move[2]['account_id'] = self.define_account(move[2])
-            partida['debit'] = move[2]['credit']
-            partida['credit'] = move[2]['debit']
-            partida['account_id'] = self.define_account(partida)
-            # FIXME: Ele considera apenas os lançamentos duplicados, mas no
-            # caso de receita, tanto lançamento original quanto duplicado mudam
-            # vide docs
-            move_lines.append([0, 0, partida])
+            if move[2].get('product_id', False):
+                type =  'RECEITA'
+                move_lines.remove(move)
+
+            elif move[2].get('tax_amount', False):
+                type =  'IMPOSTO'
+
+            else:
+                type = 'CLIENTE'
+
+            # Criar contra-partidas
+            if type in ['IMPOSTO', 'CLIENTE']:
+                partida = dict(move[2])
+                partida['name'] = 'Contrapartida - ' + move[2]['name']
+                move[2]['account_id'] = 30
+                partida['debit'] = move[2]['credit']
+                partida['credit'] = move[2]['debit']
+                move_lines.append([0, 0, partida])
+                # self.define_account(move, type)
+
         return move_lines
