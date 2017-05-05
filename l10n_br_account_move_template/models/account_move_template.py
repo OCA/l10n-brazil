2396fd05e5fcb7f434b3c7b2ae8a4996cf9417ff
bruna.braga <bruna_braga@unifei.edu.br>
2017-05-05 12:00:47 -0300

    [FIX] refactor move_template

--- l10n_br_account_move_template/models/account_move_template.py
+++ l10n_br_account_move_template/models/account_move_template.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2017 - Daniel Sadamo - KMEE INFORMATICA
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from openerp import api, fields, models
 from openerp.addons.l10n_br_account_product.models.product import \
     PRODUCT_ORIGIN
 
@@ -42,15 +42,45 @@ class AccountMoveTemplate(models.Model):
         comodel_name='res.company',
     )
 
-    fiscal_category_ids = fields.Many2one(
-        comodel_name='l10n_br_account.fiscal.category',
+    fiscal_category_ids = fields.Many2many(
+        'l10n_br_account.fiscal.category',
+        'account_move_template_fiscal_category',
+        'template_id',
+        'category_id',
         string=u'Categoria da operação',
+        # domain="[('tem_account_template', '=', False)]",
     )
+    move_template_ids = fields.One2many(
+        'account.move.line.template',
+        inverse_name='template_id',
+    )
+
+    @api.constrains('fiscal_category_ids')
+    def _constraints_fiscal_categories(self):
+        for category in self.fiscal_category_ids:
+            if len(category.move_template_ids) >= 2:
+                raise Warning(u'A categoria %s já tem roteiro' %category.name)
+
+    # @api.onchange('fiscal_category_ids')
+    # def _compute_tem_account_template(self):
+    #     for category in self.env['l10n_br_account.fiscal.category'].search([]):
+    #         if category in self.fiscal_category_ids:
+    #             print "true"
+    #             category.write({'tem_account_template': True})
+    #         else:
+    #             print 'False'
+    #             category.write({'tem_account_template': False})
 
 
 class AccountMoveLineTemplate(models.Model):
     _name = 'account.move.line.template'
     _order = 'sequence asc'
+
+    template_id = fields.Many2one(
+        comodel_name='account.move.template',
+        required=True,
+        ondelete='cascade',
+    )
 
     sequence = fields.Integer(
         string=u'Sequence',
@@ -84,11 +114,31 @@ class AccountMoveLineTemplate(models.Model):
                                                u'debito'
     )
 
-
 class L10nBrAccountFiscalCategory(models.Model):
     _inherit = 'l10n_br_account.fiscal.category'
     _description = 'Categoria Fiscal'
 
-    move_template_id = fields.One2many(
-        'account.move.template', 'fiscal_category_ids',
+    move_template_ids = fields.Many2many(
+        'account.move.template',
+        'account_move_template_fiscal_category',
+        'category_id',
+        'template_id',
+        string=u'Modelos de contabilização',
     )
+
+    # tem_account_template = fields.Boolean(
+    #     string=u'Tem account template?',
+    #     # compute='_compute_tem_account_template',
+    #     # store=True,
+    # )
+
+    # @api.depends('move_template_ids.fiscal_category_ids')
+    # def _compute_tem_account_template(self):
+    #     for category in self:
+    #         if category.move_template_ids and len(category.move_template_ids)\
+    #                 > 0:
+    #             print "true"
+    #             category.tem_account_template = True
+    #         else:
+    #             print 'False'
+    #             category.tem_account_template = False
