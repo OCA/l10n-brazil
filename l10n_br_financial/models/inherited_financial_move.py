236cf0ee097c158197421283c431085ba5085429
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2017-05-09 19:40:58 -0300

    [ADD] US#382 Tesouraria no financeiro brasileiro

--- l10n_br_financial/models/inherited_financial_move.py
+++ l10n_br_financial/models/inherited_financial_move.py
@@ -11,7 +11,13 @@ class FinancialMove(models.Model):
 
     _inherit = b'financial.move'
 
-    tipo_documento = fields.Many2one(
+    tipo_documento_id = fields.Many2one(
         string='Tipo do documento',
         comodel_name=b'financeiro.tipo_documento',
     )
+
+    @api.onchange('date')
+    def computar_date_maturity(self):
+        for move in self:
+            if move.financial_type in ('money_in', 'money_out'):
+                move.date_maturity = move.date
