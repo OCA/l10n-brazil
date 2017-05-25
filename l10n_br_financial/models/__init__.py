f3cffb524b98aed852c55be9782b7cfc25308e57
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-05-25 11:44:39 -0300

    Revert "[WIP] Base para o módulo de financeiro brasileiro"
    This reverts commit e557c0bf1311da86b992fb629e78ab1a476d89d8.

--- l10n_br_financial/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import inherited_account_payment_method
-from . import inherited_account_payment_mode
-from . import inherited_financial_move
-from . import financeiro_cheque
-from . import financeiro_tipo_documento
