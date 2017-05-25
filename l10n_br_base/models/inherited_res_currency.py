25069dc1adf6cf53fd31f2fcab8a358e7601a7a0
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-05-25 10:29:13 -0300

    [FEA] Ajustando tratamento das moedas e índices

--- l10n_br_base/models/inherited_res_currency.py
+++ l10n_br_base/models/inherited_res_currency.py
@@ -7,7 +7,7 @@
 
 from __future__ import division, print_function, unicode_literals
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResCurrency(models.Model):
@@ -27,3 +27,17 @@ class ResCurrency(models.Model):
     is_uom = fields.Boolean(
         string='Is UOM?',
     )
+    is_currency = fields.Boolean(
+        string='Is currency?',
+        compute='_compute_is_currency',
+        store=True,
+    )
+
+    @api.depends('is_symbol', 'is_index', 'is_uom')
+    def _compute_is_currency(self):
+        for currency in self:
+            currency.is_currency = not (
+                currency.is_symbol
+                or currency.is_index
+                or currency.is_uom
+            )
