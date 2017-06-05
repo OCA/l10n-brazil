48f79224fc574b7a0cd63d6634fdb78b44460add
Luis Felipe Mileo <mileo@kmee.com.br>
2017-06-05 01:51:04 -0300

    [WIP][NEW][sped_purchase] Purchase localization módule

---
+++ sped_purchase/models/inherited_purchase_order_line.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from __future__ import division, print_function, unicode_literals
+
+import logging
+
+from odoo import api, fields, models, _
+import odoo.addons.decimal_precision as dp
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = b'purchase.order.line'
+
+    is_brazilian = fields.Boolean(
+        string=u'Is a Brazilian Invoice?',
+        related='order_id.is_brazilian',
+    )
+
+
+class PurchaseOrderLineBrazil(models.Model):
+
+    _name = b'purchase.order.line.brazil'
+    _description = 'Linhas'
+    _inherit = 'sped.calculo.imposto.item'
+    _inherits = {'purchase.order.line': 'purchase_line_id'}
+    _abstract = False
+
+    purchase_line_id = fields.Many2one(
+        comodel_name='purchase.order.line',
+        string='Purchase Line original',
+        ondelete='restrict',
+        required=True,
+    )
+    empresa_id = fields.Many2one(
+        comodel_name='sped.empresa',
+        string='Empresa',
+        related='order_id.sped_empresa_id',
+        readonly=True,
+    )
+    participante_id = fields.Many2one(
+        comodel_name='sped.participante',
+        string='Destinatário/Remetente',
+        related='order_id.sped_participante_id',
+        readonly=True,
+    )
+    operacao_id = fields.Many2one(
+        comodel_name='sped.operacao',
+        string='Operação Fiscal',
+        related='order_id.sped_operacao_produto_id',
+        readonly=True,
+    )
+    data_emissao = fields.Datetime(
+        string='Data de emissão',
+        related='order_id.date_order',
+        readonly=True,
+    )
+
+    #
+    # Campos readonly
+    #
+    unidade_readonly_id = fields.Many2one(
+        comodel_name='sped.unidade',
+        string='Unidade',
+        ondelete='restrict',
+        compute='_compute_readonly',
+    )
+    unidade_tributacao_readonly_id = fields.Many2one(
+        comodel_name='sped.unidade',
+        string='Unidade para tributação',
+        ondelete='restrict',
+        compute='_compute_readonly',
+    )
+    vr_produtos_readonly = fields.Monetary(
+        string='Valor do produto/serviço',
+        compute='_compute_readonly',
+    )
+    vr_produtos_tributacao_readonly = fields.Monetary(
+        string='Valor do produto/serviço para tributação',
+        compute='_compute_readonly',
+    )
+    vr_operacao_readonly = fields.Monetary(
+        string='Valor da operação',
+        compute='_compute_readonly',
+    )
+    vr_operacao_tributacao_readonly = fields.Monetary(
+        string='Valor da operação para tributação',
+        compute='_compute_readonly',
+    )
+    vr_nf_readonly = fields.Monetary(
+        string='Valor da NF',
+        compute='_compute_readonly',
+    )
+    vr_fatura_readonly = fields.Monetary(
+        string='Valor da fatura',
+        compute='_compute_readonly',
+    )
+    vr_unitario_custo_comercial_readonly = fields.Float(
+        string='Custo unitário comercial',
+        compute='_compute_readonly',
+        digits=dp.get_precision('SPED - Valor Unitário'),
+    )
+    vr_custo_comercial_readonly = fields.Monetary(
+        string='Custo comercial',
+        compute='_compute_readonly',
+    )
+    peso_bruto_readonly = fields.Monetary(
+        string='Peso bruto',
+        currency_field='currency_peso_id',
+        compute='_compute_readonly',
+    )
+    peso_liquido_readonly = fields.Monetary(
+        string='Peso líquido',
+        currency_field='currency_peso_id',
+        compute='_compute_readonly',
+    )
+    quantidade_especie_readonly = fields.Float(
+        string='Quantidade por espécie/embalagem',
+        digits=dp.get_precision('SPED - Quantidade'),
+        compute='_compute_readonly',
+    )
+    permite_alteracao = fields.Boolean(
+        string='Permite alteração?',
+        compute='_compute_permite_alteracao',
+    )
+
+    @api.onchange('produto_id')
+    def onchange_product_id_date(self):
+        domain = {}
+        if not self.order_id:
+            return
+
+        if not self.data_emissao:
+            warning = {
+                'title': _('Warning!'),
+                'message': _(
+                    'Por favor defina a data da fatura, \n'
+                    'para permtir o cálculo correto dos impostos'),
+            }
+            return {'warning': warning}
+        if not (self.order_id.sped_operacao_produto_id or
+                    self.order_id.sped_operacao_servico_id):
+            warning = {
+                'title': _('Warning!'),
+                'message': _(
+                    'Por favor defina a operação'),
+            }
+            return {'warning': warning}
+        if self.produto_id:
+            self.name = self.produto_id.nome
+
+    @api.depends('modelo', 'emissao')
+    def _compute_permite_alteracao(self):
+        for item in self:
+            item.permite_alteracao = True
+
+    @api.depends('unidade_id', 'unidade_tributacao_id',
+                 'vr_produtos', 'vr_operacao',
+                 'vr_produtos_tributacao', 'vr_operacao_tributacao',
+                 'vr_nf', 'vr_fatura',
+                 'vr_unitario_custo_comercial', 'vr_custo_comercial')
+    def _compute_readonly(self):
+        for item in self:
+            item.unidade_readonly_id = \
+                item.unidade_id.id if item.unidade_id else False
+            if item.unidade_tributacao_id:
+                item.unidade_tributacao_readonly_id = \
+                    item.unidade_tributacao_id.id
+            else:
+                item.unidade_tributacao_readonly_id = False
+
+            item.vr_produtos_readonly = item.vr_produtos
+            item.vr_operacao_readonly = item.vr_operacao
+            item.vr_produtos_tributacao_readonly = item.vr_produtos_tributacao
+            item.vr_operacao_tributacao_readonly = item.vr_operacao_tributacao
+            item.vr_nf_readonly = item.vr_nf
+            item.vr_fatura_readonly = item.vr_fatura
+            item.vr_unitario_custo_comercial_readonly = \
+                item.vr_unitario_custo_comercial
+            item.vr_custo_comercial_readonly = item.vr_custo_comercial
+            item.peso_bruto_readonly = item.peso_bruto
+            item.peso_liquido_readonly = item.peso_liquido
+            item.quantidade_especie_readonly = item.quantidade_especie
