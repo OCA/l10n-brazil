cca8125deab98644a8681ad7d0626fc798f4bfad
Luis Felipe Mileo <mileo@kmee.com.br>
2017-06-05 03:46:23 -0300

    [NEW] Campos de frete/outros/seguro e método amount_all_brazil

--- sped_imposto/models/sped_calculo_imposto.py
+++ sped_imposto/models/sped_calculo_imposto.py
@@ -9,7 +9,7 @@ import logging
 from odoo import api, fields, models, _
 import odoo.addons.decimal_precision as dp
 from odoo.exceptions import ValidationError
-
+from openerp.addons.l10n_br_base.tools.misc import calc_price_ratio
 from odoo.addons.l10n_br_base.constante_tributaria import (
     MODELO_FISCAL_EMISSAO_PRODUTO,
     MODELO_FISCAL_EMISSAO_SERVICO,
@@ -23,6 +23,51 @@ class SpedCalculoImposto(SpedBase):
     """ Definie informações essenciais para as operações brasileiras"""
 
     _abstract = False
+
+    def _get_date(self):
+        """
+        Return the document date
+        Used in get_info
+        """
+        raise NotImplementedError
+
+    def _amount_all_brazil(self):
+        #
+        # Tratamos os impostos brasileiros;
+        # amount_untaxed é equivalente ao valor dos produtos
+        #
+        self.amount_untaxed = \
+            sum(item.vr_produtos for item in self.brazil_line_ids)
+        #
+        # amount_tax são os imposto que são somados no valor total da NF;
+        # no nosso caso, não só impostos, mas todos os valores que entram
+        # no total da NF: outras despesas acessórias, frete etc.
+        # E, como o amount_total é o valor DA FATURA, não da NF, somamos este
+        # primeiro, e listamos o valor dos impostos considerando valores
+        # acessórios, e potencias retenções de imposto que estejam
+        # reduzindo o valor
+        #
+        self.amount_total = \
+            sum(item.vr_fatura for item in self.brazil_line_ids)
+
+        self.amount_tax = self.amount_total - self.amount_untaxed
+        amount_total_company_signed = self.amount_total
+        amount_untaxed_signed = self.amount_untaxed
+        if (self.currency_id and self.company_id and
+                self.currency_id != self.company_id.currency_id):
+            currency_id = self.currency_id.with_context(
+                date=self._get_date())
+            amount_total_company_signed = \
+                currency_id.compute(self.amount_total,
+                                    self.company_id.currency_id)
+            amount_untaxed_signed = \
+                currency_id.compute(self.amount_untaxed,
+                                    self.company_id.currency_id)
+        # FIX ME
+        # sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+        # self.amount_total_company_signed = amount_total_company_signed * sign
+        # self.amount_total_signed = self.amount_total * sign
+        # self.amount_untaxed_signed = amount_untaxed_signed * sign
 
     @api.model
     def _default_company_id(self):
@@ -41,6 +86,57 @@ class SpedCalculoImposto(SpedBase):
                             invoice.partner_id.sped_participante_id
                     continue
             invoice.is_brazilian = False
+
+    @api.one
+    def _get_costs_value(self):
+        """ Read the l10n_br specific functional fields. """
+        freight = costs = insurance = 0.0
+        for line in self.brazil_line_ids:
+            freight += line.vr_frete
+            insurance += line.vr_seguro
+            costs += line.vr_outras
+        self.vr_frete = freight
+        self.vr_outras = costs
+        self.vr_seguro = insurance
+
+    @api.one
+    def _set_amount_freight(self):
+        for line in self.brazil_line_ids:
+            if not self.vr_frete:
+                break
+            line.write({
+                'vr_frete': calc_price_ratio(
+                    line.vr_nf,
+                    self.vr_frete,
+                    line.order_id.amount_untaxed),
+            })
+        return True
+
+    @api.one
+    def _set_amount_insurance(self):
+        for line in self.brazil_line_ids:
+            if not self.vr_seguro:
+                break
+            line.write({
+                'vr_seguro': calc_price_ratio(
+                    line.vr_nf,
+                    self.vr_seguro,
+                    line.order_id.amount_untaxed),
+            })
+        return True
+
+    @api.one
+    def _set_amount_costs(self):
+        for line in self.brazil_line_ids:
+            if not self.vr_outras:
+                break
+            line.write({
+                'vr_outras': calc_price_ratio(
+                    line.vr_nf,
+                    self.vr_outras,
+                    line.order_id.amount_untaxed),
+            })
+        return True
 
     is_brazilian = fields.Boolean(
         string=u'Is a Brazilian?',
@@ -78,6 +174,19 @@ class SpedCalculoImposto(SpedBase):
         string='Condição de pagamento',
         domain=[('forma_pagamento', '!=', False)],
     )
+    vr_frete = fields.Float(
+        compute=_get_costs_value, inverse=_set_amount_freight,
+        string='Frete', default=0.00, digits=dp.get_precision('Account'),
+        readonly=True, states={'draft': [('readonly', False)]})
+    vr_outras = fields.Float(
+        compute=_get_costs_value, inverse=_set_amount_costs,
+        string='Outros Custos', default=0.00,
+        digits=dp.get_precision('Account'),
+        readonly=True, states={'draft': [('readonly', False)]})
+    vr_seguro = fields.Float(
+        compute=_get_costs_value, inverse=_set_amount_insurance,
+        string='Seguro', default=0.00, digits=dp.get_precision('Account'),
+        readonly=True, states={'draft': [('readonly', False)]})
 
     @api.onchange('condicao_pagamento_id')
     def _onchange_condicao_pagamento_id(self):
