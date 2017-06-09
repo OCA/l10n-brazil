460c6915e9816b2bd6b045b4b808a7e9ab68c237
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-06-08 21:37:19 -0300

    [FIX] Renomeando módulo e models para consistência da nomenclatura

---
+++ sped_financial/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sped Financeiro',
+    'summary': """
+        Integracão entre o controle financeiro e o módulo fiscal""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'www.odoobrasil.org.br',
+    'depends': [
+        'sped',
+        'l10n_br_financial',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
