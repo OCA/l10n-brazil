ac8c342f2049b5e1a9e92be493d683f451bec2a7
Hendrix Costa <hendrixcosta@gmail.com>
2017-06-09 06:14:53 -0300

    [FIX] Portado o modulo do cashflow report para o modulo do financeiro

--- cashflow_report/reports/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 KMEE
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import abstract_report_xlsx
-from . import financial_cashflow_report
-from . import financial_cashflow_report_xlsx
