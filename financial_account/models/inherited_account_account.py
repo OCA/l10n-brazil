32b2b721ce79aac74b5299edc782c6aab23e800c
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-06-14 15:01:02 -0300

    [WIP] Separação do financeiro/contabilidade

--- financial_account/models/inherited_account_account.py
+++ financial_account/models/inherited_account_account.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#
 # Copyright 2016 Taŭga Tecnologia
 #    Aristides Caldeira <aristides.caldeira@tauga.com.br>
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
