b14df3e20e2dea2ca86cf552b003652f9710f3dc
Hendrix Costa <hendrixcosta@gmail.com>
2017-06-22 14:38:57 -0300

    [ADD] Campo de categoria do contrato e Arquivo para variaveis constantes do RH

--- l10n_br_hr_arquivos_governo/models/l10n_br_hr_contract.py
+++ l10n_br_hr_arquivos_governo/models/l10n_br_hr_contract.py
@@ -2,12 +2,18 @@
 # Copyright 2017 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from openerp import api, exceptions, fields, models, _
 
+from ..constantes_rh import *
 
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
     weekly_hours = fields.Float(
         default=40,
+    )
+    categoria = fields.Selection(
+        selection=CATEGORIA_TRABALHADOR,
+        string="Categoria do Contrato",
+        required=True,
     )
