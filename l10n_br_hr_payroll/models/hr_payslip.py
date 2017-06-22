8532ad4ccc0f501272075c7d7f3350a76f72c4e2
Wagner Pereira <wagner.pereira@kmee.com.br>
2017-06-22 16:13:50 -0300

    [Fix] Compute date_to using data_afastamento minus 1

--- l10n_br_hr_payroll/models/hr_payslip.py
+++ l10n_br_hr_payroll/models/hr_payslip.py
@@ -4,7 +4,7 @@
 
 import logging
 from calendar import monthrange
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from dateutil.relativedelta import relativedelta
 from lxml import etree
@@ -1523,7 +1523,7 @@ class HrPayslip(models.Model):
 
             if record.data_afastamento and \
                ultimo_dia_do_mes > record.data_afastamento:
-                record.date_to = record.data_afastamento
+                record.date_to = fields.Date.from_string(record.data_afastamento) - timedelta(days=1)
 
     @api.multi
     def _checar_holerites_aprovados(self):
