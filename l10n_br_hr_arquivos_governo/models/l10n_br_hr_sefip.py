2cb2540d37da522f01df619fc432478083350ef3
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2017-06-30 00:43:11 -0300

    [fix]usar somente holerites 'Confirmados'

--- l10n_br_hr_arquivos_governo/models/l10n_br_hr_sefip.py
+++ l10n_br_hr_arquivos_governo/models/l10n_br_hr_sefip.py
@@ -399,6 +399,7 @@ class L10nBrSefip(models.Model):
         folha_ids = self.env['hr.payslip'].search([
             ('mes_do_ano', '=', self.mes),
             ('ano', '=', self.ano),
+            ('state', '=', 'done'),
             ('company_id.partner_id.cnpj_cpf', 'like', raiz)
         ])
         return folha_ids
