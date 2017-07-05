f95fd21c1262e926dded4385167ae517b69dc0d7
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-05 13:37:10 -0300

    [MIG] Backport manifest to version 8

--- financial_account/__openerp__.py
+++ financial_account/__openerp__.py
@@ -6,7 +6,7 @@
     'name': 'Financial Account',
     'summary': """
         Integrate financial module with Odoo accounting""",
-    'version': '10.0.1.0.0',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE,Odoo Community Association (OCA)',
     'website': 'www.kmee.com.br',
