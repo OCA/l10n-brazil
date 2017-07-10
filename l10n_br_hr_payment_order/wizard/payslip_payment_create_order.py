9a1987ed37a1130c77611e68a41274878c5b75ec
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-07-10 17:26:22 -0300

    [ADD] Campos para a busca dos holerites no wizard

--- l10n_br_hr_payment_order/wizard/payslip_payment_create_order.py
+++ l10n_br_hr_payment_order/wizard/payslip_payment_create_order.py
@@ -2,21 +2,38 @@
 # Copyright 2017 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from datetime import datetime
+from openerp import api, models, fields, _
+from openerp.addons.l10n_br_hr_payroll.models.hr_payslip import TIPO_DE_FOLHA,\
+    MES_DO_ANO
 
 
 class PayslipPaymentCreateOrder(models.Model):
     _name = "payslip.payment.order.create"
 
-    duedate = fields.Date(
-        string='Due Date',
-        required=True
+    tipo_de_folha = fields.Selection(
+        selection=TIPO_DE_FOLHA,
+        string=u'Tipo de folha',
+        default='normal',
+    )
+
+    mes_do_ano = fields.Selection(
+        selection=MES_DO_ANO,
+        string=u'Mês',
+        required=True,
+        default=datetime.now().month,
+    )
+
+    ano = fields.Integer(
+        strin="Ano",
+        default=datetime.now().year,
+        required=True,
     )
 
     entries = fields.Many2many(
-        'account.move.line',
-        'line_pay_payslip_rel',
-        'pay_id',
-        'line_id',
+        comodel_name='hr.payslip.line',
+        rel='payorder_line_payslip_line_rel',
+        column1='pay_id',
+        column2='line_id',
         string='Entries'
     )
