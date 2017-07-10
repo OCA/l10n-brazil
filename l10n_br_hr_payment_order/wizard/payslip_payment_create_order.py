8c25f2c8ff1f9849e3f7378f3245c6105ed373d2
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-07-10 17:27:28 -0300

    [ADD] Funcoes para buscar linhas dos holerites
    Funcoes para buscar as linhas a serem pagas nos holerites do periodo
    selecionado no wizard.

--- l10n_br_hr_payment_order/wizard/payslip_payment_create_order.py
+++ l10n_br_hr_payment_order/wizard/payslip_payment_create_order.py
@@ -37,3 +37,54 @@ class PayslipPaymentCreateOrder(models.Model):
         column2='line_id',
         string='Entries'
     )
+
+    @api.model
+    def default_get(self, field_list):
+        res = super(PayslipPaymentCreateOrder, self).default_get(field_list)
+        context = self.env.context
+        if ('entries' in field_list and context.get('lines_id') and
+                context.get('populate_results')):
+            res.update({'entries': context['lines_id']})
+        return res
+
+    @api.multi
+    def buscar_linhas_holerites(self):
+        payslip_obj = self.env['hr.payslip']
+        payslip_line_obj = self.env['hr.payslip.line']
+        payslip_ids = payslip_obj.search(
+            [
+                ('tipo_de_folha', '=', self.tipo_de_folha),
+                ('mes_do_ano', '=', self.mes_do_ano),
+                ('ano', '=', self.ano),
+                ('state', '=', 'done')
+            ]
+        )
+        rubricas_obj = self.env['hr.salary.rule']
+        rubricas_pagaveis = rubricas_obj.search(
+            [
+                ('eh_pagavel', '=', True)
+            ]
+        )
+        payslip_line_ids = payslip_line_obj.search(
+            [
+                ('slip_id', 'in', payslip_ids.ids),
+                ('salary_rule_id', 'in', rubricas_pagaveis.ids)
+            ]
+        )
+        context = self.env.context.copy()
+        context['lines_id'] = payslip_line_ids.ids
+        context['populate_results'] = True
+        model_data_obj = self.env['ir.model.data']
+        model_datas = model_data_obj.search(
+            [('model', '=', 'ir.ui.view'),
+             ('name', '=', 'payslip_payment_lines_create_order_view')])
+        return {'name': _('Entry Lines'),
+                'context': context,
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'payslip.payment.order.create',
+                'views': [(model_datas[0].res_id, 'form')],
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                }
+
