8eddbc90c2038dc7802cc4428aa15eb6b865fb20
Hendrix Costa <hendrixcosta@gmail.com>
2017-07-10 22:49:13 -0300

    [ADD] Campo de forma de lançamento

--- l10n_br_account_banking_payment_cnab/model/payment_order.py
+++ l10n_br_account_banking_payment_cnab/model/payment_order.py
@@ -1,42 +1,43 @@
 # -*- coding: utf-8 -*-
-# #############################################################################
-#
-#
-#    Copyright (C) 2012 KMEE (http://www.kmee.com.br)
-#    @author Fernando Marcato Rodrigues
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012 KMEE - Fernando Marcato Rodrigues
+# Copyright 2017 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from __future__ import division, print_function, unicode_literals
 
 from openerp import api, models, fields
 
-# TODO: funcao a ser chamada por ação automatizada para resetar o sufixo
-#     diariamente
+import logging
+_logger = logging.getLogger(__name__)
+try:
+    from cnab240.constantes import FORMA_LANCAMENTO
+except ImportError as err:
+    _logger.debug = (err)
 
 
 class PaymentOrder(models.Model):
-    _inherit = 'payment.order'
+    _inherit = b'payment.order'
 
     file_number = fields.Integer(u'Número sequencial do arquivo')
     # TODO adicionar domain para permitir o modo de pagamento correspondente
     # ao mode
+
     serie_id = fields.Many2one(
-        'l10n_br_cnab.sequence', u'Sequencia interna')
+        'l10n_br_cnab.sequence',
+        u'Sequencia interna'
+    )
+
     sufixo_arquivo = fields.Integer(u'Sufixo do arquivo')
+
     serie_sufixo_arquivo = fields.Many2one(
-        'l10n_br_cnab_file_sufix.sequence', u'Série do Sufixo do arquivo')
+        u'Série do Sufixo do arquivo',
+        'l10n_br_cnab_file_sufix.sequence',
+    )
+
+    forma_lancamento = fields.Selection(
+        string='Forma de Lançamento do CNAB',
+        selection=FORMA_LANCAMENTO,
+    )
 
     @api.multi
     def get_next_number(self):
@@ -48,6 +49,9 @@ class PaymentOrder(models.Model):
             seq_no = sequence.get_id(ord.serie_id.internal_sequence_id.id)
             ord.write({'file_number': seq_no})
         return seq_no
+
+    # TODO: funcao a ser chamada por ação automatizada para resetar o sufixo
+    #     diariamente
 
     @api.multi
     def get_next_sufixo(self):
