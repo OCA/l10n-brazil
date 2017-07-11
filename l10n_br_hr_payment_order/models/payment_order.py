6284f3727a44f5444ec24b2e80e2f59e072cc53d
the-bmb <bruna_braga@unifei.edu.br>
2017-07-11 20:52:02 -0300

    [ADD] states payment_line

--- l10n_br_hr_payment_order/models/payment_order.py
+++ l10n_br_hr_payment_order/models/payment_order.py
@@ -4,16 +4,45 @@
 
 from openerp import fields, models
 
+STATES = [('draft', 'Rascunho'),
+          ('done', 'Confirmado'),
+          ('paid', 'Pago'),
+          ('error', 'Erro'),
+          ('cancel', 'Cancelado')]
+
 
 class PaymentOrder(models.Model):
 
     _inherit = 'payment.order'
 
+    def action_done(self):
+        super(PaymentOrder, self).action_done()
+        for line in self.line_ids:
+            line.action_done()
+        pass
+
 
 class PaymentLine(models.Model):
     _inherit = 'payment.line'
 
+    # order_id
+    state = fields.Selection(
+        selection=STATES,
+        default='draft',
+        string='Status'
+    )
     payslip_id = fields.Many2one(
         string="Ref do Holerite",
         comodel_name="hr.payslip",
     )
+
+    def action_done(self):
+        self.state = 'done'
+
+    # FIXME
+    def action_paid(self):
+        self.state = 'pid'
+
+    # Caso de retorno
+    def verify_error(self):
+        pass
