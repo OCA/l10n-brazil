adaee54b98c5cc4000eeba7ee20cf9cb78b8d6e2
Hendrix Costa <hendrixcosta@gmail.com>
2017-07-12 19:35:53 -0300

    [FIX] Add Default ao campo aviso_ao_favorecido

--- l10n_br_account_banking_payment/l10n_br_account_banking_payment_cnab/model/payment_line.py
+++ l10n_br_account_banking_payment/l10n_br_account_banking_payment_cnab/model/payment_line.py
@@ -61,6 +61,7 @@ class PaymentLine(models.Model):
         selection=AVISO_FAVORECIDO,
         string=u'Aviso ao Favorecido',
         help=u'Campo P006 do CNAB',
+        default=0,
     )
     abatimento = fields.Float(
         digits=(13, 2),
