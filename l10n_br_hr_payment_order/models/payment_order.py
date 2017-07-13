eb105dc8b851e8c99638400ca2ff89d246be3003
the-bmb <bruna_braga@unifei.edu.br>
2017-07-13 08:24:17 -0300

    [REM] status payment_line

--- l10n_br_hr_payment_order/models/payment_order.py
+++ l10n_br_hr_payment_order/models/payment_order.py
@@ -4,12 +4,6 @@
 
 from openerp import fields, models
 
-STATUS = [('draft', 'Rascunho'),
-          ('done', 'Confirmado'),
-          ('paid', 'Pago'),
-          ('error', 'Erro'),
-          ('cancel', 'Cancelado')]
-
 
 class PaymentOrder(models.Model):
 
@@ -25,25 +19,7 @@ class PaymentOrder(models.Model):
 class PaymentLine(models.Model):
     _inherit = 'payment.line'
 
-    # order_id
-    status = fields.Selection(
-        selection=STATUS,
-        default='draft',
-        string='Status',
-        readonly=True,
-    )
     payslip_id = fields.Many2one(
         string="Ref do Holerite",
         comodel_name="hr.payslip",
     )
-
-    def action_done(self):
-        self.status = 'done'
-
-    # FIXME
-    def action_paid(self):
-        self.status = 'paid'
-
-    # Caso de retorno
-    def verify_error(self):
-        pass
