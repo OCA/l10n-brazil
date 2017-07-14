4c58d411948c7bbd57169fd47e725b6a320a9c13
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-14 09:28:50 -0300

    [FIX] financial_account security

--- financial_account/__openerp__.py
+++ financial_account/__openerp__.py
@@ -15,7 +15,7 @@
         'account',
     ],
     'data': [
-        'security/financial_account_move_matrix.xml',
+        'security/ir.model.access.csv',
         'views/inherited_res_partner_bank_view.xml',
         'views/inherited_financial_move_debt_base_view.xml',
         'views/inherited_financial_move_payment_base_view.xml',
