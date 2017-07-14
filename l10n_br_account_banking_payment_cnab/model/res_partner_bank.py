5171a2b9256753ff3bf68789c7e510596bcc074b
Hendrix Costa <hendrixcosta@gmail.com>
2017-07-14 20:06:14 -0300

    [FIX] Flake8

--- l10n_br_account_banking_payment_cnab/model/res_partner_bank.py
+++ l10n_br_account_banking_payment_cnab/model/res_partner_bank.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-# #############################################################################
-#
-#
-#    Copyright (C) 2012 KMEE (http://www.kmee.com.br)
-#    @author Fernando Marcato Rodrigues
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012 KMEE - Fernando Marcato Rodrigues
+# Copyright 2017 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
 
@@ -29,18 +12,24 @@ class ResPartnerBank(models.Model):
     _inherit = 'res.partner.bank'
 
     codigo_da_empresa = fields.Integer(
-        u'Código da empresa', size=20,
+        u'Código da empresa',
+        size=20,
         help=u"Será informado pelo banco depois do cadastro do beneficiário "
-             u"na agência")
+             u"na agência"
+    )
+
     tipo_de_conta = fields.Selection(
-        [('01', u'Conta corrente individual'),
-         ('02', u'Conta poupança individual'),
-         ('03', u'Conta depósito judicial/Depósito em consignação individual'),
-         ('11', u'Conta corrente conjunta'),
-         ('12', u'Conta poupança conjunta'),
-         ('13', u'Conta depósito judicial/Depósito em consignação conjunta')
-         ],
-        u'Tipo de Conta', default='01'
+        selection=[
+            ('01', u'Conta corrente individual'),
+            ('02', u'Conta poupança individual'),
+            ('03', u'Conta depósito judicial/Depósito em consignação '
+                   u'individual'),
+            ('11', u'Conta corrente conjunta'),
+            ('12', u'Conta poupança conjunta'),
+            ('13', u'Conta depósito judicial/Depósito em consignação '
+                   u'conjunta')],
+        string=u'Tipo de Conta',
+        default='01'
     )
 
     bra_number = fields.Char(
