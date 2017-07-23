6b3fd0edaeb54332eee2a3bb573230d2d52165db
Luis Felipe Mileo <mileo@kmee.com.br>
2017-07-23 01:52:40 -0300

    [NEW] Importação de payment.lines a partir de financial.moves

--- l10n_br_account_banking_payment/l10n_br_financial_payment_order/models/inherited_payment_order.py
+++ l10n_br_account_banking_payment/l10n_br_financial_payment_order/models/inherited_payment_order.py
@@ -7,8 +7,15 @@ from __future__ import division, print_function, unicode_literals
 from openerp import api, fields, models, _
 from openerp.exceptions import ValidationError
 # from openerp.addons.l10n_br_hr_payroll.models.hr_payslip import TIPO_DE_FOLHA
-
-from ..constantes import TIPO_ORDEM_PAGAMENTO
+from openerp.addons.financial.constants import (
+    FINANCIAL_DEBT_2RECEIVE,
+    FINANCIAL_DEBT_2PAY,
+)
+from ..constantes import (
+    TIPO_ORDEM_PAGAMENTO,
+    TIPO_ORDEM_PAGAMENTO_BOLETO,
+    TIPO_ORDEM_PAGAMENTO_PAGAMENTO
+)
 
 
 class PaymentOrder(models.Model):
@@ -66,9 +73,147 @@ class PaymentOrder(models.Model):
         self.write({'state': 'cancel'})
         return True
 
+    @api.multi
+    def _prepare_financial_payment_line(self, line):
+        """This function is designed to be inherited
+        The resulting dict is passed to the create method of payment.line"""
+
+        self.ensure_one()
+        _today = fields.Date.context_today(self)
+        date_to_pay = False  # no payment date => immediate payment
+
+        if self.date_prefered == 'due':
+            #
+            # Caso a data preferida seja a data de vencimento
+            #
+
+            #
+            # TODO: Verificar o que fazer com lançamentos vencidos
+            #
+
+            date_to_pay = (
+                line.date_maturity
+                if line.date_maturity and line.date_maturity > _today
+                else False)
+
+        elif self.date_prefered == 'fixed':
+            #
+            # Caso da data preferida seja uma data fixa e esta data seja maior
+            # que a data de hoje.
+            #
+            date_to_pay = (
+                self.date_scheduled
+                if self.date_scheduled and self.date_scheduled > _today
+                else False)
+
+        state = 'normal'
+        communication = line.display_name or '-'
+        # communication = line.ref or '-'
+        # TODO:
+        # - Implementar novamente o campo ref? Pois o display name pode
+        # ser alterado
+        # - Como devemos lidar com um deposito identificado?
+        # - Podemos inserir com uma moeda diferente da ordem de pagamento?
+        #
+
+        amount_currency = line.amount_document
+
+        # TODO: Devemos expressar o valor com sinal negativo em alguns casos?
+        # if self.payment_order_type == 'debit':
+        #     amount_currency *= -1
+        #
+
+        #
+        # Método para buscar a conta bancária aplicável a operação:
+        # - Exemplo de um caso de uso possível:
+        #         - O funcionário tem 2 contas, mas ao receber o seu salário,
+        #       o valor deve ser depositado na conta salário
+
+        # line2bank = line.line2bank(self.mode.id)
+
+        res = {
+            'financial_id': line.id,
+            'amount_currency': amount_currency,
+            # 'bank_id': line2bank.get(line.id),
+            'order_id': self.id,
+            'partner_id': line.partner_id and line.partner_id.id or False,
+            'communication': communication,
+            'state': state,
+            'date': date_to_pay,
+            'currency': (line.currency_id and line.currency_id.id or
+                    line.journal_id.currency.id or
+                    line.journal_id.company_id.currency_id.id)
+        }
+        return res
+
+    @api.multi
+    def filter_financial_lines(self, lines):
+        """ Filter move lines before proposing them for inclusion
+            in the payment order.
+
+        This implementation filters out financial lines that are already
+        included in draft or open payment orders. This prevents the
+        user to include the same line in two different open payment
+        orders.
+
+        See also https://github.com/OCA/bank-payment/issues/93.
+
+        :param lines: recordset of move lines
+        :returns: list of move line ids
+        """
+        self.ensure_one()
+        to_exclude = self.env['payment.line']. \
+            search([('order_id.state', 'in', ('draft', 'open')),
+                    ('financial_id', 'in', lines.ids)]).mapped('financial_id')
+
+        # TODO: Verificar quando podemos incluir duas vezes um financial_move.
+
+        return lines - to_exclude
+
+    @api.multi
+    def extend_payment_order_domain(self, domain):
+        self.ensure_one()
+
+        domain += [
+            ('company_id', '=', self.company_id.id),
+            # ('amount_residual', '>', 0), # FIXME
+        ]
+
+        if self.mode.tipo_pagamento == TIPO_ORDEM_PAGAMENTO_BOLETO:
+            domain += [
+                ('payment_mode_id', '=', self.mode.id),
+                ('type', '=', FINANCIAL_DEBT_2RECEIVE),
+            ]
+        elif self.mode.tipo_pagamento == TIPO_ORDEM_PAGAMENTO_PAGAMENTO:
+            domain += [
+                ('type', '=', FINANCIAL_DEBT_2PAY),
+            ]
+        else:
+            raise NotImplemented
+
     @api.one
     def financial_payment_import(self):
-        print ("dummy")
+        """
+        1. Buscar
+        2. Extender
+        3. Filtrar
+        4. Preparar
+        5. Criar
+
+        :return:
+        """
+        payment_line_obj = self.env['payment.line']
+
+        domain = []
+
+        self.extend_payment_order_domain(domain)
+        lines = self.env['financial.move'].search(domain)
+        filtered_lines = self.filter_financial_lines(lines)
+
+        for line in filtered_lines:
+            vals = self._prepare_financial_payment_line(line)
+            payment_line_obj.create(vals)
+
         return
 
 
--- l10n_br_account_banking_payment/l10n_br_financial_payment_order/models/operacoes/cobranca.py
+++ l10n_br_account_banking_payment/l10n_br_financial_payment_order/models/operacoes/cobranca.py
@@ -12,18 +12,20 @@ from openerp.addons.financial.constants import FINANCIAL_DEBT_2RECEIVE
 class PaymentOrder(models.Model):
     _inherit = b'payment.order'
 
-    def financial_payment_import(self):
-        self.ensure_one()
-
-        if self.tipo_pagamento == 'cobranca':
-            financial_move_ids = self.env['financial.move'].search([
-                ('company_id', '=', self.company_id.id),
-                ('type', '=', FINANCIAL_DEBT_2RECEIVE),
-                # ('document_type_id', '=', )
-            ])
-
-            print (financial_move_ids)
-
-        result = super(PaymentOrder, self).financial_payment_import()
-
-        return result
+    # @api.one
+    # def financial_payment_import(self):
+    #     self.ensure_one()
+    #
+    #     if self.tipo_pagamento == 'cobranca':
+    #         financial_move_ids = self.env['financial.move'].search([
+    #             ('company_id', '=', self.company_id.id),
+    #             ('type', '=', FINANCIAL_DEBT_2RECEIVE),
+    #             ('payment_mode_id', '=', self.mode.id),
+    #             # ('document_type_id', '=', )
+    #         ])
+    #
+    #         print (financial_move_ids)
+    #
+    #     result = super(PaymentOrder, self).financial_payment_import()
+    #
+    #     return result
