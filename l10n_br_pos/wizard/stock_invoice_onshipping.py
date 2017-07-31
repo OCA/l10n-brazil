d24d67b3b7c3fee005799b1918c8b9db3016e011
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-07-31 12:02:28 -0300

    [REF] Invoice Wizards

--- l10n_br_pos/wizard/stock_invoice_onshipping.py
+++ l10n_br_pos/wizard/stock_invoice_onshipping.py
@@ -4,37 +4,23 @@
 
 from openerp import models, fields, api
 
-FISCAL_DOC_REF = [
-    ('account.invoice', u'Fatura'),
-    ('pos.order', u'Pedido do PDV')
-]
 
+class StockInvoiceOnShippingRelatedDocument(models.TransientModel):
+    _inherit = 'stock.invoice.onshipping.related.document'
 
-
-class StockInvoiceOnShipping(models.TransientModel):
-    _inherit = 'stock.invoice.onshipping'
+    @api.model
+    def get_picking_document_relationships(self):
+        return super(
+            StockInvoiceOnShippingRelatedDocument, self
+        ).get_picking_document_relationships() + [
+            'move_lines.origin_returned_move_id.picking_id.pos_order_ids',
+        ]
 
     @api.multi
-    def _compute_fiscal_doc_ref(self):
-        result = super(StockInvoiceOnShipping, self)._compute_fiscal_doc_ref()
-        id_in_model = result.split(',')[1]
-        if id_in_model != '0':
-            return result
-        else:
-            picking_obj = self.env['stock.picking']
-            for record in picking_obj.browse(
-                    self._context.get('active_ids', False)):
-                move = record.move_lines[0]
-                if move.origin_returned_move_id:
-                    ref_id = self.env['pos.order'].search([
-                        ('chave_cfe', '=',
-                         move.origin_returned_move_id.
-                         picking_id.fiscal_document_access_key)
-                    ], limit=1).id
-                    result = 'pos.order,%d' % ref_id
-            return result
-
-    fiscal_doc_ref = fields.Reference(selection=FISCAL_DOC_REF,
-                                      readonly=False,
-                                      default=_compute_fiscal_doc_ref,
-                                      string=u'Documento Fiscal Relacionado')
+    def compute_all_for_pos_order(self):
+        self.document_type = "sat"
+        document = self.fiscal_doc_ref
+        self.document_date = document.date_order
+        self.document_partner_id = document.partner_id
+        self.document_amount = document.amount_total
+        self.access_key = document.chave_cfe[3:]
