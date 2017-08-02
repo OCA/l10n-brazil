a2e11c21e83906693b99dd2c2e1eb8ec6963ce11
Leonardo Rochael Almeida <leorochael@gmail.com>
2017-08-02 16:08:19 -0300

    [IMP] Faturas: múltiplos documentos relacionados
    No wizard de criação de faturas, permite relacionar mais de
    um documento de cada vez.
    
    Subclasses que queiram relacionar mais de um tipo de documento agora
    só precisam listar qual o relacionamento com o picking original e como
    mapear os dados do documento relacionado.

--- l10n_br_pos/models/stock.py
+++ l10n_br_pos/models/stock.py
@@ -25,23 +25,3 @@ class StockPicking(models.Model):
         u'Chave de acesso do Documento', compute_sudo=True,
         compute=_get_fiscal_document_access_key,
         store=True)
-
-    @api.model
-    def _create_invoice_from_picking(self, picking, vals):
-        result = {}
-        fiscal_doc_ref = self._context.get(
-                'fiscal_doc_ref', False)
-        if (vals.get('type', False) == 'out_refund' and
-                fiscal_doc_ref and
-                fiscal_doc_ref._name == 'pos.order'):
-            result['fiscal_document_related_ids'] = \
-                [(0, False,
-                  {
-                    'pos_order_related_id': fiscal_doc_ref.id,
-                     'document_type': 'sat',
-                     'access_key': fiscal_doc_ref.chave_cfe[3:]
-                  }
-                  )]
-        vals.update(result)
-        return super(StockPicking, self)._create_invoice_from_picking(
-            picking, vals)
--- l10n_br_pos/wizard/stock_invoice_onshipping.py
+++ l10n_br_pos/wizard/stock_invoice_onshipping.py
@@ -4,26 +4,23 @@
 
 from openerp import models, fields, api
 
-FISCAL_DOC_REF = [
-    ('pos.order', u'Pedido do PDV')
-]
 
+class StockInvoiceOnShippingRelatedDocument(models.TransientModel):
+    _inherit = 'stock.invoice.onshipping.related.document'
 
-
-class StockInvoiceOnShipping(models.TransientModel):
-    _inherit = 'stock.invoice.onshipping'
-
-    @api.multi
-    def _fiscal_doc_ref_selection(self):
-        prev = super(StockInvoiceOnShipping, self)._fiscal_doc_ref_selection()
-        return prev + FISCAL_DOC_REF
+    @api.model
+    def get_picking_document_relationships(self):
+        return super(
+            StockInvoiceOnShippingRelatedDocument, self
+        ).get_picking_document_relationships() + [
+            'move_lines.origin_returned_move_id.picking_id.pos_order_ids',
+        ]
 
     @api.multi
-    def _fiscal_doc_ref_default(self):
-        result = super(StockInvoiceOnShipping, self)._fiscal_doc_ref_default()
-        id_in_model = result.split(',')[1]
-        if id_in_model == '0':
-            return_picking_ids = self.get_returned_picking_ids()
-            ref_id = return_picking_ids.mapped('pos_order_ids')[:1].id
-            result = 'pos.order,%d' % ref_id
-        return result
+    def compute_all_for_pos_order(self):
+        self.document_type = "sat"
+        document = self.fiscal_doc_ref
+        self.document_date = document.date_order
+        self.document_partner_id = document.partner_id
+        self.document_amount = document.amount_total
+        self.access_key = document.chave_cfe[3:]
