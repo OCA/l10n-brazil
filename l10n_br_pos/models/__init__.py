a8e06d1192ca1f11342041cd761cbc2d5f87c0e7
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-08-07 17:43:50 -0300

    [IMP] Handle the return of the SAT for the sales flow

--- l10n_br_pos/models/__init__.py
+++ l10n_br_pos/models/__init__.py
@@ -2,9 +2,9 @@
 # © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import pos_order
-import pos_config
-import res_company
-import account_journal
+from . import pos_order
+from . import pos_config
+from . import res_company
+from . import account_journal
 from . import l10n_br_account_product
 from . import stock
