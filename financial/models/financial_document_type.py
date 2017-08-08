marcelsavegnago/l10n-brazil-kmee

[FIX] Optimize imports

financial/models/financial_document_type.py
@@ -4,7 +4,7 @@
 
 from __future__ import division, print_function, unicode_literals
 
-from openerp import api, fields, models, _
+from openerp import fields, models
 
 
 class FinancialDocumentType(models.Model):

financial/reports/report_xlsx_financial_cashflow_wizard.py
@@ -8,7 +8,8 @@
 from __future__ import division, print_function, unicode_literals
 
 from datetime import datetime
-from openerp import api, fields, models
+
+from openerp import fields, models
 
 
 class ReportXlsxFinancialCashflowWizard(models.TransientModel):

financial/tests/financial_test_classes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from openerp.tests.common import SingleTransactionCase
 from datetime import datetime, timedelta
-import time
+
+from openerp.tests.common import SingleTransactionCase
 
 
 class FinancialTestCase(SingleTransactionCase):

OCA/l10n-brazil

financial/models/financial_document_type.py
@@ -4,7 +4,7 @@
 
 from __future__ import division, print_function, unicode_literals
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class FinancialDocumentType(models.Model):

financial/reports/report_xlsx_financial_cashflow_wizard.py
@@ -8,7 +8,8 @@
 from __future__ import division, print_function, unicode_literals
 
 from datetime import datetime
-from odoo import api, fields, models
+
+from odoo import fields, models
 
 
 class ReportXlsxFinancialCashflowWizard(models.TransientModel):

financial/tests/financial_test_classes.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo.tests.common import SingleTransactionCase
 from datetime import datetime, timedelta
 import time
+
+from odoo.tests.common import SingleTransactionCase
 
 
 class FinancialTestCase(SingleTransactionCase):