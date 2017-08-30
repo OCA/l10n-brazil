b9f10df66413c304a92f2b3c0114f6b9f39e7b44
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-08-29 22:10:08 -0300

    [FEA] Preparando sped_base para rateios complexos [FEA] Separando data e hora com fuso de Brasília

--- l10n_br_base/models/sped_base.py
+++ l10n_br_base/models/sped_base.py
@@ -7,7 +7,17 @@
 
 from __future__ import division, print_function, unicode_literals
 
+import logging
+
 from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from pybrasil.data import parse_datetime, data_hora_horario_brasilia
+
+except (ImportError, IOError) as err:
+    _logger.debug(err)
 
 
 class SpedBase(object):
@@ -27,6 +37,15 @@ class SpedBase(object):
         string='Percentual',
         compute='_compute_currency_id',
         default=lambda self: self.env.ref('l10n_br_base.SIMBOLO_ALIQUOTA'),
+        context={'only_currencies': False},
+        domain=[['is_symbol', '=', True]],
+    )
+    currency_aliquota_rateio_id = fields.Many2one(
+        comodel_name='res.currency',
+        string='Percentual',
+        compute='_compute_currency_id',
+        default=lambda self:
+            self.env.ref('l10n_br_base.SIMBOLO_ALIQUOTA_RATEIO'),
         context={'only_currencies': False},
         domain=[['is_symbol', '=', True]],
     )
@@ -53,7 +72,16 @@ class SpedBase(object):
             item.currency_id = self.env.ref('base.BRL').id
             item.currency_aliquota_id = self.env.ref(
                 'l10n_br_base.SIMBOLO_ALIQUOTA').id
+            item.currency_aliquota_rateio_id = self.env.ref(
+                'l10n_br_base.SIMBOLO_ALIQUOTA_RATEIO').id
             item.currency_unitario_id = self.env.ref(
                 'l10n_br_base.SIMBOLO_VALOR_UNITARIO').id
             item.currency_peso_id = self.env.ref(
                 'l10n_br_base.SIMBOLO_PESO').id
+
+    def _separa_data_hora(self, data_hora_odoo):
+        data_hora = data_hora_horario_brasilia(
+            parse_datetime(data_hora_odoo + ' UTC'))
+        data = str(data_hora)[:10]
+        hora = str(data_hora)[11:19]
+        return data, hora
