457e2053cf258eb3dc519b82d90d113fb62b90d3
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-09-01 18:23:56 -0300

    [FIX] Localização pela metade, faltaram campos

--- sped_financial/models/inherited_financial_move.py
+++ sped_financial/models/inherited_financial_move.py
@@ -7,11 +7,20 @@
 from __future__ import division, print_function, unicode_literals
 
 from odoo import api, fields, models, _
+from openerp.addons.l10n_br_base.models.sped_base import SpedBase
 
 
-class FinancialMove(models.Model):
+class FinancialMove(SpedBase, models.Model):
     _inherit = 'financial.move'
 
+    empresa_id = fields.Many2one(
+        comodel_name='sped.empresa',
+        string='Empresa',
+    )
+    participante_id = fields.Many2one(
+        comodel_name='sped.participante',
+        string='Destinatário/Remetente'
+    )
     doc_source_id = fields.Reference(
         selection_add=[('sped.documento', 'Documento Fiscal')],
     )
@@ -41,3 +50,26 @@ class FinancialMove(models.Model):
                     date_maturity
                 )
             move.date_business_maturity = date_business_maturity
+
+    def _sincroniza_empresa_company_participante_partner(self):
+        for documento in self:
+            documento.company_id = documento.empresa_id.company_id
+            documento.partner_id = documento.participante_id.partner_id
+
+    @api.onchange('empresa_id', 'participante_id')
+    def _onchange_empresa_participante(self):
+        self._sincroniza_empresa_company_participante_partner()
+
+    @api.depends('empresa_id', 'participante_id')
+    def _depends_empresa_participante(self):
+        self._sincroniza_empresa_company_participante_partner()
+
+    @api.model
+    def create(self, dados):
+        dados = self._mantem_sincronia_cadastros(dados)
+        return super(SaleOrder, self).create(dados)
+
+    @api.model
+    def write(self, dados):
+        dados = self._mantem_sincronia_cadastros(dados)
+        return super(SaleOrder, self).write(dados)
