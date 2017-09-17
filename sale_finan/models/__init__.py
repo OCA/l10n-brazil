535cca3eea31fcd779db6c503cf17750cab52ff1
Ari Caldeira <ari.caldeira@tauga.com.br>
2017-09-17 17:35:56 -0300

    [FIX] Faltou arquivo

---
+++ sale_finan/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Taŭga Tecnologia
+#   Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#
+
+from . import inherited_account_payment_term
+from . import inherited_sale_order
