2f4d7094843f96305340a8471ac26dbafdcd24eb
Ari Caldeira <ari.caldeira@tauga.com.br>
2017-09-17 17:07:07 -0300

    [FEA] Adicionando aviso de módulo deprecado

--- l10n_br_financial/__manifest__.py
+++ l10n_br_financial/__manifest__.py
@@ -3,9 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Br Financial',
-    'summary': """
-        Financeiro brasileiro (Tesouraria e Integracao Bancaria)""",
+    'name': '* [OLD] L10n Br Financial',
+    'summary': '''
+        * [OLD] Do not install this module, it is here for migration only
+        ''',
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE,Odoo Community Association (OCA)',
