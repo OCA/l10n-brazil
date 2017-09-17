995a3e393af07f5c062ec980248cd0433f298c43
Ari Caldeira <ari.caldeira@tauga.com.br>
2017-09-17 11:20:43 -0300

    [FEA] Controle de limite de crédito na venda

---
+++ sale_finan/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Taŭga Tecnologia
+#   Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
+#
+
+{
+    'name': 'Finan Vendas',
+    'summary': 'Finan Vendas',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': u'Odoo Community Association (OCA), Ari Caldeira',
+    'depends': [
+        'sped_sale',
+        'finan',
+    ],
+    'data': [
+        'views/inherited_account_payment_term_view.xml',
+        'views/inherited_sale_order_orcamento_view.xml',
+    ],
+    'installable': True,
+}
--- sale_finan/models/inherited_account_payment_term.py
+++ sale_finan/models/inherited_account_payment_term.py
@@ -2,13 +2,14 @@
 # Copyright 2017 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from __future__ import division, print_function, unicode_literals
+
 from odoo import fields, models
 
 
 class AccountPaymentTerm(models.Model):
     _inherit = 'account.payment.term'
 
-    vende_acima_limite_cliente = fields.Boolean(
-        string=u'Vender acima do limite de crédito?',
-        default=False,
+    vende_acima_limite_credito = fields.Boolean(
+        string='Vender acima do limite de crédito?',
     )
