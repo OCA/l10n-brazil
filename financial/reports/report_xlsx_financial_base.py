marcelsavegnago/l10n-brazil-kmee

[NEW] Formatação de data nos relatorios financeiros

financial/reports/report_xlsx_financial_base.py
@@ -8,6 +8,7 @@
 from __future__ import division, print_function, unicode_literals
 
 from openerp.report import report_sxw
+from openerp import fields
 from openerp.addons.report_xlsx.report.report_xlsx import ReportXlsx
 
 from decimal import Decimal
@@ -328,6 +329,11 @@ class ReportXlsxFinancialBase(ReportXlsx):
                     isinstance(value, (int, float, Decimal)):
                 self.sheet.write_number(self.current_row, current_column,
                                         value, style)
+            elif column.get('type', 'string') == 'date':
+                self.sheet.write(
+                    self.current_row, current_column,
+                    fields.Date.from_string(value), style
+                )
             else:
                 self.sheet.write(self.current_row, current_column,
                                  value or '', style)

OCA/l10n-brazil

financial/reports/report_xlsx_financial_base.py
@@ -8,6 +8,7 @@
 from __future__ import division, print_function, unicode_literals
 
 from odoo.report import report_sxw
+from odoo import fields
 from odoo.addons.report_xlsx.report.report_xlsx import ReportXlsx
 
 from decimal import Decimal
@@ -328,6 +329,11 @@ class ReportXlsxFinancialBase(ReportXlsx):
                     isinstance(value, (int, float, Decimal)):
                 self.sheet.write_number(self.current_row, current_column,
                                         value, style)
+            elif column.get('type', 'string') == 'date':
+                self.sheet.write(
+                    self.current_row, current_column,
+                    fields.Date.from_string(value), style
+                )
             else:
                 self.sheet.write(self.current_row, current_column,
                                  value or '', style)