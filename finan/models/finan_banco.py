72349bdf205797d5435099bd8a49eb59336339a7
Ari Caldeira <ari.caldeira@tauga.com.br>
2017-10-09 18:24:02 -0300

    [WIP] Dashboard com saldos bancários

--- finan/models/finan_banco.py
+++ finan/models/finan_banco.py
@@ -7,9 +7,23 @@
 
 from __future__ import division, print_function, unicode_literals
 
+import logging
+import json
+
 from odoo import api, fields, models
 from odoo.addons.l10n_br_base.models.sped_base import SpedBase
 from ..constantes import *
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from pybrasil.data import hoje
+    from pybrasil.valor import formata_valor
+    from pybrasil.valor.decimal import Decimal as D, json_decimal_default
+    json._default_encoder.default = json_decimal_default
+
+except (ImportError, IOError) as err:
+    _logger.debug(err)
 
 
 class FinanBanco(SpedBase, models.Model):
@@ -80,30 +94,18 @@ class FinanBanco(SpedBase, models.Model):
         #string='Saldo inicial',
     #)
 
+    #
+    # Dashboards
+    #
+    dashboard_saldo_hoje = fields.Text(
+        compute='_compute_dashboard_saldo_hoje'
+    )
+
     @api.depends('banco', 'agencia', 'conta', 'conta_digito', 'tipo',
                  'titular_id')
     def _compute_banco(self):
         for banco in self:
-            nome = FINAN_BANCO_DICT[banco.banco][6:]
-            nome += ' / '
-            nome += FINAN_TIPO_CONTA_BANCARIA_DICT[banco.tipo]
-
-            if banco.banco != FINAN_BANCO_INTERNO:
-                nome += ' / '
-                nome += ' ag. '
-                nome += banco.agencia
-
-            nome += ' / '
-            nome += banco.conta
-
-            if banco.banco != FINAN_BANCO_INTERNO:
-                nome += '-'
-                nome += banco.conta_digito
-
-            nome += ' / '
-            nome += banco.titular_id.name_get()[0][1]
-
-            banco.nome = nome
+            banco.nome = banco.name_get()[0][1]
 
     @api.depends('titular_id', 'titular_id.cnpj_cpf')
     def _compute_cnpj_cpf(self):
@@ -160,3 +162,44 @@ class FinanBanco(SpedBase, models.Model):
         return super(FinanBanco, self).name_search(name=name, args=args,
                                                          operator=operator,
                                                          limit=limit)
+
+    def dados_dashboard_saldo_hoje(self):
+        self.ensure_one()
+
+        dados = {
+            'titulo': FINAN_TIPO_CONTA_BANCARIA_DICT[self.tipo],
+            'banco': self.with_context(banco_sem_titular=True).nome,
+            'titular': self.titular_id.name_get()[0][1],
+            'currency_id': self.currency_id.id,
+        }
+        entrada = D(0)
+        saida = D(0)
+        saldo = D(0)
+
+        #
+        # Busca os dados de hoje
+        #
+        saldo_dia = self.env['finan.banco.saldo'].search(
+            [('banco_id', '=', self.id), ('data', '<=', str(hoje()))],
+            order='data desc',
+            limit=1,
+        )
+
+        if saldo_dia:
+            entrada = D(saldo_dia.entrada)
+            saida = D(saldo_dia.saida)
+            saldo = D(saldo_dia.saldo)
+
+        dados['entrada'] = self.currency_id.symbol + ' ' + \
+            formata_valor(entrada)
+        dados['saida'] = self.currency_id.symbol + ' ' + \
+            formata_valor(saida)
+        dados['saldo'] = self.currency_id.symbol + ' ' + \
+            formata_valor(saldo)
+
+        return dados
+
+    def _compute_dashboard_saldo_hoje(self):
+        for banco in self:
+            banco.dashboard_saldo_hoje = \
+                json.dumps(banco.dados_dashboard_saldo_hoje())
