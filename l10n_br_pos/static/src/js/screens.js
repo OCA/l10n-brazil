1f293e6a05ba7de3ffee98b4521d8bac788f1687
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-10-17 12:59:43 -0200

    [FIX] Remove punctuation from CPF/CNPJ field

--- l10n_br_pos/static/src/js/screens.js
+++ l10n_br_pos/static/src/js/screens.js
@@ -236,7 +236,7 @@ function l10n_br_pos_screens(instance, module) {
                             if (partner) {
                                 self.pos.get('selectedOrder').set_client(partner);
                                 currentOrder = self.pos.get('selectedOrder').attributes;
-                                currentOrder["cpf_nota"] = cpf;
+                                currentOrder["cpf_nota"] = cpf.replace(/[^\d]+/g,'');
                                 self.pos_widget.payment_screen.validate_order();
                             } else {
                                 new_partner = {};
@@ -263,7 +263,7 @@ function l10n_br_pos_screens(instance, module) {
                                         return true;
                                     }).then(function () {
                                         currentOrder = self.pos.get('selectedOrder').attributes;
-                                        currentOrder["cpf_nota"] = cpf;
+                                        currentOrder["cpf_nota"] = cpf.replace(/[^\d]+/g,'');
                                         self.pos_widget.payment_screen.validate_order();
                                     });
                                 });
@@ -275,7 +275,7 @@ function l10n_br_pos_screens(instance, module) {
                         }
                     } else {
                         currentOrder = self.pos.get('selectedOrder').attributes;
-                        currentOrder["cpf_nota"] = currentOrder.client.cnpj_cpf;
+                        currentOrder["cpf_nota"] = currentOrder.client.cnpj_cpf.replace(/[^\d]+/g,'');
                         self.pos_widget.payment_screen.validate_order();
                     }
                 } else {
