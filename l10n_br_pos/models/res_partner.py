9e35f4b0b17e6af86012f4468ea40d7ca927997d
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-10-23 15:16:39 -0200

    [FIX] Validar o cadastro de cpf/cnpj com pontuacao
    - e quando o cliente for do tipo pessoa fisica atribuir a opcao de
    'nao contribuinte' para este novo cadastro.

--- l10n_br_pos/models/res_partner.py
+++ l10n_br_pos/models/res_partner.py
@@ -13,15 +13,32 @@ class ResPartner(models.Model):
         partner_id = self.browse(partner)
         return partner_id.credit_limit
 
+    @api.multi
+    def _mask_cnpj_cpf(self, cpfcnpj_type, cnpj_cpf):
+        if cnpj_cpf:
+            if cpfcnpj_type == 'cnpj' and len(cnpj_cpf) == 14:
+                cnpj_cpf = "%s.%s.%s/%s-%s" % (
+                    cnpj_cpf[0:2], cnpj_cpf[2:5], cnpj_cpf[5:8],
+                    cnpj_cpf[8:12], cnpj_cpf[12:14]
+                )
+            elif cpfcnpj_type == 'cpf' and len(cnpj_cpf) == 11:
+                cnpj_cpf = "%s.%s.%s-%s" % (
+                    cnpj_cpf[0:3], cnpj_cpf[3:6], cnpj_cpf[6:9], cnpj_cpf[9:11]
+                )
+        return cnpj_cpf
+
     @api.model
     def create_from_ui(self, partner):
+        from openerp.addons.l10n_br_base.tools import misc
+        cnpj_cpf = misc.punctuation_rm(partner['cnpj_cpf'])
+        cnpj_cpf_type = 'cpf' if len(cnpj_cpf) == 11 else 'cnpj'
+        partner['cnpj_cpf'] = self._mask_cnpj_cpf(cnpj_cpf_type, cnpj_cpf)
         res = super(ResPartner, self).create_from_ui(partner)
         partner_id = self.browse(res)
         partner_id.legal_name = partner['name']
-        fiscal_type = self.env['l10n_br_account.partner.fiscal.type'].search(
-            [('name', '=', 'Não Contribuinte PF')]
-        )
-        partner_id.partner_fiscal_type_id = fiscal_type.id
+        if cnpj_cpf_type:
+            fiscal_type = self.env.ref('l10n_br_account.partner_fiscal_type_4')
+            partner_id.partner_fiscal_type_id = fiscal_type.id
         if self.env.user.company_id.parent_id:
             partner_id.company_id = self.env.user.company_id.parent_id.id
         return res
