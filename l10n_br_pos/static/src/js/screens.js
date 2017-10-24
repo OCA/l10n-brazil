a31d005c73bc7224baae2bf7662204f9ac056be4
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-10-24 12:54:30 -0200

    fixup! [ADD] Informacoes adicionais no cupom fiscal

--- l10n_br_pos/static/src/js/screens.js
+++ l10n_br_pos/static/src/js/screens.js
@@ -398,6 +398,7 @@ function l10n_br_pos_screens(instance, module) {
         },
         validate_order: function(options) {
             this._super();
+            var self = this;
             options = options || {};
             var currentOrder = this.pos.get('selectedOrder');
 
@@ -453,7 +454,7 @@ function l10n_br_pos_screens(instance, module) {
                             this.pos_widget.action_bar.set_button_disabled('validation',true);
                             var receipt = currentOrder.export_for_printing();
                             var json = currentOrder.export_for_printing();
-                            if (this.pos.config.enviar_pedido_cupom_fiscal) {
+                            if (self.pos.config.enviar_pedido_cupom_fiscal) {
                                 new instance.web.Model('pos.order').call('get_sequence_name', [this.pos.session.uid]).then(function (order_name) {
                                     json['informacoes_adicionais'] = order_name;
                                     self.pos.proxy.send_order_sat(
@@ -464,12 +465,12 @@ function l10n_br_pos_screens(instance, module) {
                                     self.pos.get('selectedOrder').destroy();
                                     });
                             } else {
-                                this.pos.proxy.send_order_sat(
+                                self.pos.proxy.send_order_sat(
                                     currentOrder,
                                     QWeb.render('XmlReceipt',{
                                     receipt: receipt, widget: self,
                                 }), json);
-                                this.pos.get('selectedOrder').destroy();
+                                self.pos.get('selectedOrder').destroy();
                             }
                         }else{
                             this.pos.push_order(currentOrder);
