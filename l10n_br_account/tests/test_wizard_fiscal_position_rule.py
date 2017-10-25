4e6b5b03fb3c08d51c37e4b16e3ce56d07d5f6ea
clementmbr <clement.mombereau@gmail.com>
2017-10-25 10:40:23 -0200

    Correction in tests l10n_br_base and l10n_br_account

--- l10n_br_account/tests/test_wizard_fiscal_position_rule.py
+++ l10n_br_account/tests/test_wizard_fiscal_position_rule.py
@@ -10,14 +10,20 @@ class WizardAccountFiscalPositionRuleTest(TransactionCase):
     """Test the wizard 'wizard.account.fiscal.position.rule' """
     def test_wizard_fiscal_position_rule(self):
 
-        #Definition wizard context
+        # Definition wizard context
         wizard_account_fiscal_position_rule_test = self.env[
             'wizard.account.fiscal.position.rule'].with_context({
-                'active_ids': [self.env.ref('account_fiscal_position_rule.menu_action_account_fiscal_position_rule_template_form').id],
-                'active_id': self.env.ref('account_fiscal_position_rule.menu_action_account_fiscal_position_rule_template_form').id,
+                'active_ids': [self.env.ref(
+                    'account_fiscal_position_rule. \
+                    menu_action_account_fiscal_position_rule_template_form'
+                    ).id],
+                'active_id': self.env.ref(
+                    'account_fiscal_position_rule. \
+                    menu_action_account_fiscal_position \
+                    _rule_template_form').id,
                 })
 
-        #Create Wizard and execute action_create() method
+        # Create Wizard and execute action_create() method
         wizard_account_fiscal_position_rule_test.create({
             'company_id': self.env.ref('base.main_company').id
             }).action_create()
