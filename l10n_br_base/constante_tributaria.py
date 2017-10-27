6e01f2c8c7df0560f68d71faec5f71de9d86bc9b
Luis Felipe Mileo <mileo@kmee.com.br>
2017-10-27 11:14:06 -0200

    [NEW] Constantes tributárias CF-E

--- l10n_br_base/constante_tributaria.py
+++ l10n_br_base/constante_tributaria.py
@@ -4908,3 +4908,28 @@ INTEGRACAO_CARTAO_INTEGRADO = '1'
 INTEGRACAO_CARTAO_NAO_INTEGRADO = '2'
 
 PAIS_BRASIL = '1058'
+
+#
+# Definições para a emissão e controle da CF-e
+#
+AMBIENTE_CFE = (
+    ('1', 'Produção'),
+    ('2', 'Homologação'),
+)
+AMBIENTE_CFE_DICT = dict(AMBIENTE_NFE)
+
+AMBIENTE_CFE_PRODUCAO = '1'
+AMBIENTE_CFE_HOMOLOGACAO = '2'
+
+TIPO_EMISSAO_CFE = (
+    ('1', 'Normal'),
+    ('2', 'Integrador Fiscal'),
+    ('9', 'Contingência offline NFC-e'),
+    ('10', 'Contingência online NFC-e'),
+)
+TIPO_EMISSAO_CFE_DICT = dict(TIPO_EMISSAO_CFE)
+
+TIPO_EMISSAO_CFE_NORMAL = '1'
+TIPO_EMISSAO_CFE_INTEGRADOR_FISCAL = '2'
+TIPO_EMISSAO_CFE_CONTINGENCIA_OFFLINE_NFCE = '9'
+TIPO_EMISSAO_CFE_CONTINGENCIA_ONLINE_NFCE = '10'
