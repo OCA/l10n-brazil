5795508b3076ab9af7b15e66383c7987713ea074
Hendrix Costa <hendrixcosta@gmail.com>
2017-11-16 08:02:41 -0200

    [ADD] Ajustar wrapper para pegar logo da nfe no relatorio do holerite

--- l10n_br_hr_payroll_report/reports/payslip_report_holerite.py
+++ l10n_br_hr_payroll_report/reports/payslip_report_holerite.py
@@ -4,7 +4,6 @@
 
 from openerp.addons.report_py3o.py3o_parser import py3o_report_extender
 
-
 @py3o_report_extender(
     "l10n_br_hr_payroll_report.report_payslip_py3o_report")
 def payslip_report(pool, cr, uid, local_context, context):
@@ -13,4 +12,8 @@ def payslip_report(pool, cr, uid, local_context, context):
     company_logo = \
         payslip_pool.browse(cr, uid, context['active_id']).company_id.logo
 
-    local_context['company_logo'] = company_logo
+    company_nfe_logo =\
+        payslip_pool.browse(cr, uid, context['active_id']).company_id.nfe_logo
+
+    local_context['company_logo'] = \
+        company_nfe_logo if company_nfe_logo else company_logo
