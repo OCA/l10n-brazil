a3748885b79ffcef5d2963b4700d5c5fedc534c9
Luiz Felipe do Divino <lf.divino@gmail.com>
2017-11-17 18:42:45 -0200

    [WIP] Configuracoes sat cfe

--- sped_cfe/models/__init__.py
+++ sped_cfe/models/__init__.py
@@ -8,4 +8,4 @@
 from . import inherited_sped_documento
 from . import inherited_sped_documento_item
 from . import inherited_sped_documento_pagamento
-
+from . import sped_documento_cfe_configuracao
