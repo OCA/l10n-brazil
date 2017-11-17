e1e90c1aa3091195083e95031bd8bfcc4cc00877
Luiz Felipe do Divino <lf.divino@gmail.com>
2017-11-17 15:10:45 -0200

    [FIX] Rotas para a API SatHub

---
+++ sped_cfe/static/src/js/sped_cfe.js
@@ -0,0 +1,44 @@
+odoo.define('sped_cfe.sped_sat', function (require) {
+    "use strict";
+
+    var form_widget = require('web.form_widgets');
+    var Cfe_sat = require('sped_cfe.cfe_sat');
+
+    form_widget.WidgetButton.include({
+        start: function() {
+            var self = this;
+            this.cfe_sat = new Cfe_sat(this);
+            this.funcoes_sat = [
+                'consultar_cfe_sat',
+                'comunicar_certificado_icpbrasil',
+                'enviar_dados_venda',
+                'cancelar_ultima_venda',
+                'teste_fim_a_fim',
+                'consultar_status_operacional',
+                'consultar_numero_sessao',
+                'configurar_interface_de_rede',
+                'associar_assinatura',
+                'atualizar_software_sat',
+                'extrair_logs',
+                'bloquear_sat',
+                'trocar_codigo_de_ativacao'
+            ];
+            return this._super(this);
+        },
+        preparar_parametros: function() {
+            return {
+                    'numero_caixa': 1,
+                    'dados_venda': 1231
+                };
+        },
+        on_click: function() {
+            var self = this;
+            if (this.funcoes_sat.indexOf(this.node.attrs.custom) >= 0) {
+                var params = self.preparar_parametros();
+                this.cfe_sat[this.node.attrs.custom](params);
+            } else {
+                this._super();
+            }
+        }
+    });
+});
