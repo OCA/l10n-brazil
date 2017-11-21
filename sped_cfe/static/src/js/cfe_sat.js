e32313f4775616939d95b99c8a23a35b5adf7a98
Luiz Felipe do Divino <lf.divino@gmail.com>
2017-11-21 13:53:02 -0200

    [ADD] Enviado com sucesso recarregar o documento

--- sped_cfe/static/src/js/cfe_sat.js
+++ sped_cfe/static/src/js/cfe_sat.js
@@ -59,7 +59,9 @@ odoo.define('sped_cfe.cfe_sat', function (require) {
                 params["dados_venda"] = result;
                 var resposta_api_cfe = self.chamada_api_cfe_sat(params, url);
                 resposta_api_cfe.done(function (response) {
-                    new Model('sped.documento').call('processar_resposta_cfe', [params["venda_id"], response]).then(function (result) {}, function (error) {
+                    new Model('sped.documento').call('processar_resposta_cfe', [params["venda_id"], response]).then(function (result) {
+                        location.reload(true);
+                    }, function (error) {
                         alert(error.data.message);
                     });
                 }).fail(function (error) {
