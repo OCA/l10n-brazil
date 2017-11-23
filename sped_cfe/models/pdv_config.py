b78566543996a3a60f216fb359bf9241c18d27c8
Luiz Felipe do Divino <lf.divino@gmail.com>
2017-11-23 11:17:51 -0200

    [FIX] Campos de configuração do PDV

--- sped_cfe/models/pdv_config.py
+++ sped_cfe/models/pdv_config.py
@@ -7,9 +7,14 @@ class ConfiguracaoPDV(models.Model):
     _rec_name = 'pdv_name'
 
     pdv_name = fields.Char(string=u'Nome')
-    localizacao = fields.Char(string=u'Localização')
+
+    company_id = fields.Many2one(
+        string=u'Empresa',
+        comodel_name=u'sped.empresa'
+    )
 
     numero_caixa = fields.Char(string=u'Número de caixa')
+
     tipo = fields.Selection([('SAT', 'SAT'),
                              ], string=u'Tipo')
 
@@ -18,19 +23,31 @@ class ConfiguracaoPDV(models.Model):
         'Impressora',
     )
 
-
-
     tipo_sat = fields.Selection([
         ('local', 'Local'),
         ('rede_interna', 'Rede Interna'),
         ('remoto', 'Remoto'),
     ], string=u'Tipo SAT')
+
     ip = fields.Char(string=u'IP')
+
     ambiente = fields.Selection([
         ('producao', 'Produção'),
         ('homologacao', 'Homologação'),
     ], string=u'Ambiente')
+
     codigo_ativacao = fields.Char(string=u'Código de Ativação')
+
     cnpjsh = fields.Char(string=u'CNPJ Homologação')
+
     ie = fields.Char(string=u'Inscrição Estadual Homologação')
+
     chave_ativacao = fields.Char(string=u'Chave de Ativação')
+
+    cnpj_software_house = fields.Char(
+        string=u"CNPJ Software House"
+    )
+
+    assinatura = fields.Char(
+        string=u"Assinatura"
+    )
