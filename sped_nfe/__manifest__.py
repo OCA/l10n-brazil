693fb88669bbdbe3f06d1512f8d4145bfb62fa16
Luis Felipe Mileo <mileo@kmee.com.br>
2017-12-02 19:51:40 -0200

    [REF] Alteracao dos menus de df-e e manifestacao

--- sped_nfe/__manifest__.py
+++ sped_nfe/__manifest__.py
@@ -24,8 +24,8 @@
         'views/inherited_sped_documento_emissao_nfe_view.xml',
         'views/inherited_sped_documento_emissao_nfce_view.xml',
         'views/sped_documento_carta_correcao_view.xml',
-        'views/sped_consulta_dfe_view.xml',
         'views/sped_manifestacao_destinatario.xml',
+        'views/sped_consulta_dfe_view.xml',
         'security/ir.model.access.csv',
     ],
     'external_dependencies': {
