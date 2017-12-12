da1ecf69f6665e0f7f144c48bb77fdd88ae0da8f
Hendrix Costa <hendrixcosta@gmail.com>
2017-12-12 09:46:23 -0200

    [ADD] validacao de exclusao do movimento financeiro

--- finan/models/finan_lancamento.py
+++ finan/models/finan_lancamento.py
@@ -967,20 +967,21 @@ class FinanLancamento(SpedBase, models.Model):
     def executa_antes_unlink(self, bancos={}):
         for lancamento in self:
             if lancamento.referencia_id:
-                raise UserError('Você não pode excuir um lançamento '
-                    'relacionado a outro documento; verifique se é possível '
-                    'cancelar o documento relacionado.')
+                raise UserError(
+                    'Você não pode excuir um lançamento relacionado a outro '
+                    'documento; verifique se é possível cancelar o documento '
+                    'relacionado.')
 
-            # # Verificar se ja foi gerado boletos para esse lancamentos
-            # attachment = self.env['ir.attachment']
-            # busca = [
-            #     ('res_model', '=', 'finan.lancamento'),
-            #     ('res_id', '=', lancamento.id),
-            # ]
-            # attachment_ids = attachment.search(busca)
-            # if attachment_ids:
-            #     raise UserError(
-            #         'Você não pode excuir um lançamento com Anexos!')
+            # Verificar se ja foi gerado boletos para esse lancamentos
+            attachment = self.env['ir.attachment']
+            busca = [
+                ('res_model', '=', 'finan.lancamento'),
+                ('res_id', '=', lancamento.id),
+            ]
+            attachment_ids = attachment.search(busca)
+            if attachment_ids:
+                raise UserError(
+                    'Você não pode excuir um lançamento com Anexos!')
 
             if lancamento.provisorio:
                 continue
