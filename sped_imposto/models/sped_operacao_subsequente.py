089adb003ae18ab0a3e77968595655c321367a44
Luis Felipe Mileo <mileo@kmee.com.br>
2017-12-29 14:44:46 -0200

    [REF] Geração de operação subsequente

--- sped_imposto/models/sped_operacao_subsequente.py
+++ sped_imposto/models/sped_operacao_subsequente.py
@@ -5,7 +5,24 @@
 from __future__ import division, print_function, unicode_literals
 
 from odoo import api, fields, models, _
-from odoo.addons.l10n_br_base.constante_tributaria import SITUACAO_NFE
+from odoo.addons.l10n_br_base.constante_tributaria import (
+    SITUACAO_NFE,
+    MODELO_FISCAL_CFE,
+    MODELO_FISCAL_NFCE,
+    MODELO_FISCAL_CUPOM_FISCAL_ECF,
+)
+
+
+SITUACAO_SUBSEQUENTE = (
+    ('manual', 'Manualmente'),
+    ('nota_de_cupom', 'Gerar Nota Fiscal de Cupons Fiscais'),
+    ('nota_de_remessa', 'Gerar Nota Fiscal de Remessa'),
+)
+
+OPERACAO_SUBSEQUENTE = SITUACAO_NFE + SITUACAO_SUBSEQUENTE
+
+""" Devemos ficar atentos pois algumas operações subsequentes não geram lançamentos
+financeiros"""
 
 
 class SpedOperacaoSubsequente(models.Model):
@@ -33,11 +50,35 @@ class SpedOperacaoSubsequente(models.Model):
     )
     participante_id = fields.Many2one(
         comodel_name='sped.participante',
+        string='Participante'
     )
     situacao_geracao = fields.Selection(
         string="Situaçao Geraçao",
-        selection=SITUACAO_NFE,
+        selection=OPERACAO_SUBSEQUENTE,
+        required=True,
+        default='manual',
     )
     referenciar_documento = fields.Boolean(
         string="Refenciar documento de origem",
     )
+
+    @api.multi
+    def _confirma_geracao(self, documento):
+        """ Dado um documento fiscal verificamos se podemos gerar a operação
+        :param documento:
+        :return: True permitindo a geração
+        """
+        result = False
+
+        if self.situacao_geracao in [x for x, y in SITUACAO_SUBSEQUENTE]:
+            cupom = documento.filtered(lambda documento: documento.modelo in (
+                MODELO_FISCAL_CFE,
+                MODELO_FISCAL_NFCE,
+                MODELO_FISCAL_CUPOM_FISCAL_ECF,
+            ))
+            if cupom:
+                result = True
+        elif documento.situacao_nfe == self.situacao_geracao:
+            result = True
+
+        return result
