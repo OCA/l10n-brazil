2fcef9570b41c4e42e8f3d2f1fd7e668fd6ade59
Luis Felipe Mileo <mileo@kmee.com.br>
2017-12-29 17:49:53 -0200

    [NEW] Envio de documentos fiscais de forma assincrona

---
+++ sped_queue/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sped Queue',
+    'summary': """
+        Permite o envio assicrono de documentos fiscais""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE INFORMATICA LTDA,Odoo Community Association (OCA)',
+    'website': 'www.kmee.com.br',
+    'depends': [
+        'queue_job',
+        'sped',
+    ],
+    'data': [
+        'views/inherited_sped_operacao_base_view.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ sped_queue/model/sped_documento.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+from odoo import models, fields, api
+from odoo.addons.queue_job.job import job
+
+_logger = logging.getLogger(__name__)
+
+
+class SpedDocumento(models.Model):
+    _inherit = 'sped.documento'
+
+    @api.multi
+    @job
+    def _envia_documento(self):
+        return super(SpedDocumento, self)._envia_documento()
+
+    @api.multi
+    def envia_documento(self):
+        _logger.info('Enviando documento fiscal %s', self.ids)
+
+        enviar_agora = self.filtered(
+            lambda documento: documento.momento_envio_documento == 'now')
+        enviar_depois = self - enviar_agora
+        if enviar_agora:
+            _logger.info('Enviando documento fiscal agora: %s',
+                         enviar_agora.ids)
+            enviar_agora._envia_edoc()
+        if enviar_depois:
+            _logger.info('Enviando documento fiscal depois: %s',
+                         enviar_depois.ids)
+            enviar_depois.with_delay()._envia_edoc()
