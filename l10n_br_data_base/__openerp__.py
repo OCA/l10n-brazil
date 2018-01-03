ae1eef532eedb80778da1456c4b67eaf83d7f873
Magno <magno.costa@akretion.com.br>
2018-01-03 13:16:12 -0200

    Included module as dependecy to avoid errors to validate BIC.

--- l10n_br_data_base/__openerp__.py
+++ l10n_br_data_base/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion                           #
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
@@ -9,6 +9,8 @@
     'version': '8.0.1.0.1',
     'depends': [
         'l10n_br_base',
+        # TODO - https://github.com/OCA/l10n-brazil/issues/588
+        'l10n_br_account_banking_payment',
     ],
     'data': [
         'data/res.bank.csv',
