19c8f307d7ee02400e5a14727c31bc28682185f3
Luis Felipe Mileo <mileo@kmee.com.br>
2018-01-21 22:52:20 -0300

    [FIX] Correção dos imports e outros detalhes da pep8

--- sped_cfe/models/inherited_sped_participante.py
+++ sped_cfe/models/inherited_sped_participante.py
@@ -7,7 +7,7 @@
 
 from __future__ import division, print_function, unicode_literals
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 from odoo.addons.l10n_br_base.models.sped_base import SpedBase
 
 
