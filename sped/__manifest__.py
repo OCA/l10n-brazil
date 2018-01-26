21a0ef6cb30087209c9516974ca178b2cb105ade
gabrielcardoso21 <gacarfaria@unifei.edu.br>
2018-01-26 19:36:51 -0200

    [ADD] Exportação de XMLs para pasta da contabilidade (#502)

--- sped/__manifest__.py
+++ sped/__manifest__.py
@@ -63,6 +63,8 @@
         # Dados de base
         #
         'data/sped_participante_data.xml',
+        'wizard/sped_documento_exportar_xml.xml',
+        'wizard/base_config_settings.xml',
     ],
     'external_dependencies': {
         'python': [
