6386816fb77de46dbf3bcd317a4d5593ab3adb83
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2018-01-26 18:47:05 -0200

    [ADD] Field para guardar a sessão em caso de erro de transmissão (#504)
    - do Cupom Fiscal pelo Integrador

--- sped_cfe/models/inherited_sped_documento.py
+++ sped_cfe/models/inherited_sped_documento.py
@@ -123,6 +123,10 @@ class SpedDocumento(models.Model):
 
     id_fila_validador = fields.Char(string=u'ID Fila Validador')
 
+    numero_identificador_sessao = fields.Char(
+        string=u'Numero identificador sessao'
+    )
+
     def executa_depois_autorizar(self):
         #
         # Este método deve ser alterado por módulos integrados, para realizar
@@ -664,7 +668,8 @@ class SpedDocumento(models.Model):
             elif self.configuracoes_pdv.tipo_sat == 'rede_interna':
                 resposta = cliente.enviar_dados_venda(
                     cfe, self.configuracoes_pdv.codigo_ativacao,
-                    self.configuracoes_pdv.path_integrador
+                    self.configuracoes_pdv.path_integrador,
+                    self.numero_identificador_sessao
                 )
             else:
                 resposta = None
@@ -707,11 +712,21 @@ class SpedDocumento(models.Model):
                        resposta.EEEEE
             mensagem += '\nMensagem: ' + \
                         resposta.mensagem
+            if resposta.resposta.mensagem == u'Erro interno' and \
+                    resposta.resposta.mensagemSEFAZ == u'ERRO' and not \
+                    self.numero_identificador_sessao:
+                self.numero_identificador_sessao = \
+                    resposta.resposta.numeroSessao
             self.mensagem_nfe = mensagem
             self.situacao_nfe = SITUACAO_NFE_REJEITADA
         except Exception as resposta:
             if hasattr(resposta, 'resposta'):
                 self.codigo_rejeicao_cfe = resposta.resposta.EEEEE
+            if resposta.resposta.mensagem == u'Erro interno' and \
+                    resposta.resposta.mensagemSEFAZ == u'ERRO' and not \
+                    self.numero_identificador_sessao:
+                self.numero_identificador_sessao = \
+                    resposta.resposta.numeroSessao
             self.mensagem_nfe = "Falha na conexão com SATHUB"
             self.situacao_nfe = SITUACAO_NFE_REJEITADA
 
@@ -782,7 +797,8 @@ class SpedDocumento(models.Model):
                             'BRL',
                             int(config.numero_caixa),
                             config.chave_acesso_validador,
-                            config.path_integrador
+                            config.path_integrador,
+                            self.numero_identificador_sessao
                         )
                     resposta_pagamento = resposta.split('|')
                     if len(resposta_pagamento[0]) >= 7:
