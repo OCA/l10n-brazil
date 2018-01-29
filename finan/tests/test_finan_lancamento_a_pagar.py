e0b1fa26b3dab45c9c5aaa23476d325487c12414
Bianca Bartolomei <biancarbartolomei@gmail.com>
2018-01-29 11:33:32 -0200

    [FIX]Correção de teste

--- finan/tests/test_finan_lancamento_a_pagar.py
+++ finan/tests/test_finan_lancamento_a_pagar.py
@@ -12,7 +12,7 @@ class FinanLancamentoPagarManual(test_common.SingleTransactionCase):
         super(FinanLancamentoPagarManual, self).setUp()
         self.finan_lancamento = self.env['finan.lancamento']
         self.finan_lancamento_a_pagar = self.finan_lancamento.create({
-            'situacao_divida': 'a vencer',
+            'situacao_divida': 'a_vencer',
             'empresa_id': self.env.ref("l10n_br_base.sped_empresa"
                                        "_regime_normal_sp").id,
             'participante_id': self.env.ref("l10n_br_base.cliente9_mg").id,
@@ -21,26 +21,27 @@ class FinanLancamentoPagarManual(test_common.SingleTransactionCase):
             'numero': 0003,
             'data_documento': fields.Datetime.from_string('2018-02-21 '
                                                           '23:59:59'),
-            'conta_id': self.env.ref("finan.financial_account_101001"),
+            'conta_id': self.env.ref("finan.financial_account_101001").id,
             'data_vencimento': fields.Datetime.from_string('2018-12-21 '
                                                           '23:59:59'),
             'forma_pagamento_id': self.env.ref("finan.finan_forma_"
-                                               "pagamento_001"),
-            'banco_id': self.env.ref("finan.finan_banco_237"),
+                                               "pagamento_001").id,
+            'banco_id': self.env.ref("finan.finan_banco_237").id,
             'vr_documento': 15.50,
             'tipo': 'a_pagar',
             'carteira_id': self.env.ref('finan.finan_carteira_001').id,
         })
 
         self.finan_lancamento_a_pagar.confirma_lancamento()
+
         self.finan_lancamento_pagamento = self.finan_lancamento.create({
             'empresa_id': self.env.ref('l10n_br_base.'
                                        'sped_empresa_regime_normal_sp').id,
             'documento_id':self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
-            'numero': 0003,
-            'data_credito_debito':fields.Datetime.from_string('2016-02-22 00:00:00'),
+            'numero':0003,
+            'data_credito_debito':fields.Datetime.from_string('2018-02-21 23:59:59'),
             'conta_id':self.env.ref('finan.financial_account_101001').id,
-            'data_pagamento':fields.Datetime.from_string('2016-02-22 00:00:00'),
+            'data_pagamento':fields.Datetime.from_string('2018-02-21 23:59:59'),
             'forma_pagamento_id':self.env.ref('finan.finan_forma_pagamento_001').id,
             'banco_id':self.env.ref('finan.finan_banco_237').id,
             'vr_documento':15.50,
@@ -53,25 +54,38 @@ class FinanLancamentoPagarManual(test_common.SingleTransactionCase):
             'vr_tarifas': 0,
         })
 
-    #Teste 1: Conta a pagar com state efetivo e provisório falso
+    def teste1_lancamento_a_pagar_state(self):
 
-    def conta_a_pagar_state_teste1(self):
+        """"
+         Teste 1: Conta a pagar com state efetivo e provisório falso
+        """
 
         self.assertEqual(self.finan_lancamento_a_pagar.provisorio, False)
         self.assertEqual(self.finan_lancamento_a_pagar.state, 'open')
 
-    #Teste 2: Pagamento com state efetivo e provisório falso
-    def pagamento_state_teste2(self):
+    def teste2_lancamento_pagamento_state(self):
+
+        """"
+        Teste 2: Pagamento com state efetivo e provisório falso
+        """
+
         self.assertEqual(self.finan_lancamento_pagamento.provisorio, False)
         self.assertEqual(self.finan_lancamento_pagamento.state, 'paid')
 
-    #Teste 3: Saldo de dívida zerado após pagamento efetuado
-    def saldo_divida_zerado_teste3(self):
-        self.finan_lancamento_pagamento.divida_id = self.\
-            finan_lancamento_a_pagar.id
+    def teste3_saldo_divida(self):
+
+        """"
+        Teste 3: Saldo de dívida zerado após pagamento efetuado
+        """
+
+        self.finan_lancamento_pagamento.divida_id = self.finan_lancamento_a_pagar.id
         self.assertEqual(self.finan_lancamento_a_pagar.vr_saldo, 0)
 
-    #Teste 4: Após saldo zerado, mudança de state para quitado
-    def saldo_divida_state_teste4(self):
-        self.finan_lancamento_pagamento.divida_id = self.\
-            finan_lancamento_a_pagar.id
+    def teste4_a_pagar_state(self):
+
+        """"
+        Teste 4: Após saldo zerado, mudança de state para quitado
+        """
+
+        self.finan_lancamento_pagamento.divida_id = self.finan_lancamento_a_pagar.id
+        self.assertEqual(self.finan_lancamento_a_pagar.state, 'paid')
