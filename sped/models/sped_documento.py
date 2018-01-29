becafeff4f123b434662796b36e05688b4fc4195
gabrielcardoso21 <gacarfaria@unifei.edu.br>
2018-01-29 21:03:20 -0200

    [FIX] CF-e emitidas em produção marcadas como homologação (#510)

--- sped/models/sped_documento.py
+++ sped/models/sped_documento.py
@@ -1113,6 +1113,21 @@ class SpedDocumento(SpedCalculoImposto, models.Model):
 
         return serie
 
+    def _serie_padrao_cfe(self, empresa, ambiente_nfe, tipo_emissao_nfe):
+        if tipo_emissao_nfe == TIPO_EMISSAO_CFE_NORMAL:
+            if ambiente_nfe == AMBIENTE_CFE_PRODUCAO:
+                serie = empresa.serie_cfe_producao
+            else:
+                serie = empresa.serie_cfe_homologacao
+
+        else:
+            if ambiente_nfe == AMBIENTE_CFE_PRODUCAO:
+                serie = empresa.serie_cfe_contingencia_producao
+            else:
+                serie = empresa.serie_cfe_contingencia_homologacao
+
+        return serie
+
     @api.onchange('empresa_id', 'modelo', 'emissao')
     def _onchange_empresa_id(self):
         res = {}
@@ -1186,6 +1201,15 @@ class SpedDocumento(SpedCalculoImposto, models.Model):
                 valores['serie_rps'] = self.empresa_id.serie_rps_producao
             else:
                 valores['serie_rps'] = self.empresa_id.serie_rps_homologacao
+
+        elif self.modelo == MODELO_FISCAL_CFE:
+            valores['ambiente_nfe'] = self.empresa_id.ambiente_cfe
+            valores['tipo_emissao_nfe'] = self.empresa_id.tipo_emissao_cfe
+            valores['serie'] = self._serie_padrao_cfe(
+                self.empresa_id,
+                self.empresa_id.ambiente_cfe,
+                self.empresa_id.tipo_emissao_cfe
+            )
 
         return res
 
