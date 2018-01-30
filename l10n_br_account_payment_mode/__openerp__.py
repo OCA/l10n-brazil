da3af338d284a152dc5f9bf7bd3eb94c84c7fb0c
Magno <magno.costa@akretion.com.br>
2018-01-30 11:10:00 -0200

    Changed license header to standard.

--- l10n_br_account_payment_mode/__openerp__.py
+++ l10n_br_account_payment_mode/__openerp__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 KMEE
+# Copyright (C) 2017-Today - KMEE (<http://kmee.com.br>).
+#  @author Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
