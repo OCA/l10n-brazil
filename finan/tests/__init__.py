3bb41bbe24068e853e5db91702d1657f9f37ec4b
Hendrix Costa <hendrixcosta@gmail.com>
2018-01-31 01:06:09 -0200

    [FIX] Descricao dos testes

--- finan/tests/__init__.py
+++ finan/tests/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # from . import test_financial_receivable
 # from . import test_financial_payable
+
 # # Testing report
 # from . import abstract_test_report
 # from . import test_cash_flow
--- finan/tests/test_finan_lancamento_a_receber.py
+++ finan/tests/test_finan_lancamento_a_receber.py
@@ -15,20 +15,18 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
         self.finan_lancamento_obj = self.env['finan.lancamento']
 
         empresa_id = self.env.ref('l10n_br_base.sped_empresa_regime_normal_sp')
+        documento_id = self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO')
+        forma_pagamento_id = self.env.ref('finan.finan_forma_pagamento_001')
 
         self.finan_lancamento_a_receber = self.finan_lancamento_obj.create({
             'empresa_id': empresa_id.id,
             'participante_id': self.env.ref('l10n_br_base.cliente9_mg').id,
-            'documento_id':
-                self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
+            'documento_id': documento_id.id,
             'numero': 3567,
-            'data_documento':
-                fields.Datetime.from_string('2016-03-21 00:00:00'),
+            'data_documento': fields.Date.today(),
             'conta_id': self.env.ref('finan.financial_account_101001').id,
-            'data_vencimento':
-                fields.Datetime.from_string('2016-03-21 00:00:00'),
-            'forma_pagamento_id':
-                self.env.ref('finan.finan_forma_pagamento_001').id,
+            'data_vencimento':  fields.Date.today(),
+            'forma_pagamento_id': forma_pagamento_id.id,
             'banco_id': self.env.ref('finan.finan_banco_237').id,
             'vr_documento': 265.92,
             'tipo': 'a_receber',
@@ -37,19 +35,14 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
 
         self.finan_lancamento_a_receber.confirma_lancamento()
 
-        self.finan_lancamento_recebimento = self.finan_lancamento.create({
-            'empresa_id': self.env.ref(
-                'l10n_br_base.sped_empresa_regime_normal_sp').id,
-            'documento_id':
-                self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
+        self.finan_lancamento_recebimento = self.finan_lancamento_obj.create({
+            'empresa_id': empresa_id.id,
+            'documento_id': documento_id.id,
             'numero': 3567,
-            'data_credito_debito':
-                fields.Datetime.from_string('2016-03-21 00:00:00'),
+            'data_credito_debito': fields.Date.today(),
             'conta_id': self.env.ref('finan.financial_account_101001').id,
-            'data_pagamento':
-                fields.Datetime.from_string('2016-03-21 00:00:00'),
-            'forma_pagamento_id':
-                self.env.ref('finan.finan_forma_pagamento_001').id,
+            'data_pagamento': fields.Date.today(),
+            'forma_pagamento_id': forma_pagamento_id.id,
             'banco_id': self.env.ref('finan.finan_banco_237').id,
             'vr_documento': 265.92,
             'tipo': 'recebimento',
@@ -63,8 +56,7 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
 
     def teste1_lancamento_a_receber_state(self):
         """"
-        Teste 1:
-        O state de uma conta a receber deve ser efetivo(open)
+        Teste 1: O state de uma conta a receber deve ser efetivo(open)
         e o provisorio deve ser falso
         """
         self.assertEqual(self.finan_lancamento_a_receber.provisorio, False)
@@ -72,16 +64,14 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
 
     def teste2_lancamento_recebimento_state(self):
         """"
-        Teste 2:
-        O provisorio deve ser falso
+        Teste 2: O provisorio deve ser falso
         """
         self.assertEqual(self.finan_lancamento_recebimento.provisorio, False)
         self.assertEqual(self.finan_lancamento_recebimento.state, 'paid')
 
     def teste3_saldo_divida_zerado(self):
         """"
-        Teste 3:
-        O saldo após a divida ter recebimento deve ser zerado
+        Teste 3: O saldo após a divida ter recebimento deve ser zerado
         """
         self.finan_lancamento_recebimento.divida_id = \
             self.finan_lancamento_a_receber.id
@@ -89,8 +79,7 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
 
     def teste4_a_receber_state(self):
         """"
-        Teste 3:
-        Após o recebimento, a dívida tem quer ter state quitado(paid)
+        Teste 4: Após o recebimento, a dívida tem quer ter state quitado(paid)
         """
         self.finan_lancamento_recebimento.divida_id = \
             self.finan_lancamento_a_receber.id
