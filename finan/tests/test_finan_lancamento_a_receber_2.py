047be2661a6c8ba0dfbb2c75978dfdf730a65678
Hendrix Costa <hendrixcosta@gmail.com>
2018-01-31 03:21:23 -0200

    [FIX] PEP8

--- finan/tests/test_finan_lancamento_a_receber_2.py
+++ finan/tests/test_finan_lancamento_a_receber_2.py
@@ -6,6 +6,7 @@
 from openerp import fields
 import openerp.tests.common as test_common
 
+
 class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
 
     def setUp(self):
@@ -18,18 +19,24 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
             'situacao_divida': 'a_vencer',
             'empresa_id': self.env.ref('l10n_br_base.'
                                        'sped_empresa_regime_normal_sp').id,
-            'participante_id':self.env.ref('l10n_br_base.res_partner_akretion').id,
-            'documento_id':self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
-            'numero':8763,
-            'data_documento':fields.Datetime.from_string('2018-01-20 00:00:00'),
-            'conta_id':self.env.ref('finan.financial_account_101001').id,
-            'data_vencimento':fields.Datetime.from_string('2018-01-20 00:00:00'),
-            'forma_pagamento_id':self.env.ref('finan.finan_forma_pagamento_001').id,
-            'banco_id':self.env.ref('finan.finan_banco_001').id,
-            'vr_documento':100.00,
-            'tipo':'a_receber',
-            'carteira_id':self.env.ref('finan.finan_carteira_001').id,
-            'vr_desconto':15.0,
+            'participante_id':
+                self.env.ref('l10n_br_base.res_partner_akretion').id,
+            'documento_id':
+                self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
+            'numero': 8763,
+            'data_documento':
+                fields.Datetime.from_string('2018-01-20 00:00:00'),
+            'conta_id':
+                self.env.ref('finan.financial_account_101001').id,
+            'data_vencimento':
+                fields.Datetime.from_string('2018-01-20 00:00:00'),
+            'forma_pagamento_id':
+                self.env.ref('finan.finan_forma_pagamento_001').id,
+            'banco_id': self.env.ref('finan.finan_banco_001').id,
+            'vr_documento': 100.00,
+            'tipo': 'a_receber',
+            'carteira_id': self.env.ref('finan.finan_carteira_001').id,
+            'vr_desconto': 15.0,
         })
 
         self.finan_lancamento_a_receber.confirma_lancamento()
@@ -38,16 +45,20 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
         self.finan_lancamento_recebimento = self.finan_lancamento.create({
             'empresa_id': self.env.ref('l10n_br_base.'
                                        'sped_empresa_regime_normal_sp').id,
-            'documento_id':self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
-            'numero':8763,
-            'data_credito_debito':fields.Datetime.from_string('2018-01-20 00:00:00'),
-            'conta_id':self.env.ref('finan.financial_account_101001').id,
-            'data_pagamento':fields.Datetime.from_string('2018-01-20 00:00:00'),
-            'forma_pagamento_id':self.env.ref('finan.finan_forma_pagamento_001').id,
-            'banco_id':self.env.ref('finan.finan_banco_001').id,
-            'vr_documento':85.00,
-            'tipo':'recebimento',
-            'vr_movimentado':85.00,
+            'documento_id':
+                self.env.ref('finan.DOCUMENTO_FINANCEIRO_BOLETO').id,
+            'numero': 8763,
+            'data_credito_debito':
+                fields.Datetime.from_string('2018-01-20 00:00:00'),
+            'conta_id': self.env.ref('finan.financial_account_101001').id,
+            'data_pagamento':
+                fields.Datetime.from_string('2018-01-20 00:00:00'),
+            'forma_pagamento_id':
+                self.env.ref('finan.finan_forma_pagamento_001').id,
+            'banco_id': self.env.ref('finan.finan_banco_001').id,
+            'vr_documento': 85.00,
+            'tipo': 'recebimento',
+            'vr_movimentado': 85.00,
             'vr_multa': 0,
             'vr_juros': 0,
             'vr_adiantado': 0,
@@ -56,44 +67,35 @@ class FinanLancamentoReceberManual(test_common.SingleTransactionCase):
         })
 
     def teste1_lancamento_a_receber_state(self):
-
         """"
-        Teste 1:
-        O state de uma conta a receber deve ser efetivo(open)
+        Teste 1: O state de uma conta a receber deve ser efetivo(open)
         e o provisorio dele, falso
         """
-
         self.assertEqual(self.finan_lancamento_a_receber.provisorio, False)
         self.assertEqual(self.finan_lancamento_a_receber.state, 'open')
 
     def teste2_lancamento_recebimento_state(self):
-
         """"
-        Teste 2:
-        O state de um recebimento deve ser quitado(paid)
+        Teste 2: O state de um recebimento deve ser quitado(paid)
         e o provisorio dele, falso
         """
-
         self.assertEqual(self.finan_lancamento_recebimento.provisorio, False)
         self.assertEqual(self.finan_lancamento_recebimento.state, 'paid')
 
     def teste3_saldo_divida_zerado(self):
-
         """"
-        Teste 3:
-        O saldo após a divida ter recebimento deve ser zerado considerando
-        o desconto recebido
+        Teste 3: O saldo após a divida ter recebimento deve ser zerado 
+        considerando o desconto recebido
         """
-
-        self.finan_lancamento_recebimento.divida_id = self.finan_lancamento_a_receber.id
+        self.finan_lancamento_recebimento.divida_id = \
+            self.finan_lancamento_a_receber.id
         self.assertEqual(self.finan_lancamento_a_receber.vr_saldo, 0)
 
     def teste4_a_receber_state(self):
-
         """"
-        Teste 4:
-        Após o recebimento correto, a dívida tem quer ter state quitado(paid)
+        Teste 4: Após o recebimento correto, a dívida tem quer ter 
+        state quitado(paid)
         """
-
-        self.finan_lancamento_recebimento.divida_id = self.finan_lancamento_a_receber.id
+        self.finan_lancamento_recebimento.divida_id = \
+            self.finan_lancamento_a_receber.id
         self.assertEqual(self.finan_lancamento_a_receber.state, 'paid')
