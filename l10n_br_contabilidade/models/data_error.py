c514d1e397ab19fd7f8151515c4b5bbda0cefa14
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-02-04 16:45:27 +0100

    bump copyright year

--- l10n_br_contabilidade/models/data_error.py
+++ l10n_br_contabilidade/models/data_error.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016-2018 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2016 Akretion (<http://akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
