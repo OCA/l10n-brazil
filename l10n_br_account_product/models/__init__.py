3c407f6d7073717c59ad4bce363b6bac6a27943e
renato <renato.lima@akretion.com.br>
2018-02-06 16:27:46 -0200

    create new file to account_fiscal_position

--- l10n_br_account_product/models/__init__.py
+++ l10n_br_account_product/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2015  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import res_partner
+from . import account_fiscal_position
 from . import account_fiscal_position_template
 from . import l10n_br_account_product
 from . import res_company
