70ac0878fb858bae307337783b52da8691fe5326
renato <renato.lima@akretion.com.br>
2018-02-07 12:12:49 -0200

    create new file to import l10n_br_account_product_icms_relief

--- l10n_br_account_product/models/__init__.py
+++ l10n_br_account_product/models/__init__.py
@@ -12,6 +12,7 @@ from . import l10n_br_account_product_cfop
 from . import l10n_br_account_product_document_related
 from . import l10n_br_account_product_import_declaration
 from . import l10n_br_account_product_import_declaration_line
+from . import l10n_br_account_product_icms_relief
 from . import l10n_br_account_product
 from . import res_company
 from . import l10n_br_tax_definition_company_product
