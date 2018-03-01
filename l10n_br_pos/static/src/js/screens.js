cddefc9ab96c19cea816dcb67477828e05781c49
Atilla Silva <atillaa4@hotmail.com>
2018-03-01 14:07:28 -0300

    [WIP] campos de estado, cidade

--- l10n_br_pos/static/src/js/screens.js
+++ l10n_br_pos/static/src/js/screens.js
@@ -316,6 +316,29 @@ function l10n_br_pos_screens(instance, module) {
                 contents.empty();
                 contents.append($(QWeb.render('ClientDetailsEdit',{widget:this,partner:partner})));
                 this.toggle_save_button();
+
+               $( document ).ready(function() {
+                    var country_id = $('.client-address-country').val();
+                    if( country_id != ""){
+                        new instance.web.Model('res.country.state').call('get_states_ids', [country_id]).then(function (result) {
+                        $('.client-address-state').children('option:not(:first)').remove();
+                            $.each(result, function(key, value){
+                                $('.client-address-state').append($("<option></option>")
+                                                  .attr("value",key)
+                                                  .text(value));
+                            });
+                        });
+                        var state_id = $('.client-address-state').val();
+                        new instance.web.Model('l10n_br_base.city').call('get_city_ids', [state_id]).then(function (result) {
+                        $('.client-address-city').children('option:not(:first)').remove();
+                            $.each(result, function(key, value){
+                                $('.client-address-city').append($("<option></option>")
+                                                  .attr("value",key)
+                                                  .text(value));
+                            });
+                        });
+                        }
+                });
 		       $('.client-address-country', this.el).change(function(e){
                 var country_id = $('.client-address-country').val();
                 new instance.web.Model('res.country.state').call('get_states_ids', [country_id]).then(function (result) {
@@ -329,7 +352,7 @@ function l10n_br_pos_screens(instance, module) {
                });
                $('.client-address-state', this.el).change(function(e){
                 var city_id = $('.client-address-state').val();
-                new instance.web.Model('l10n_br_base.city').call('get_city_ids', [city_id]).then(function (result) {
+                new instance.web.Model('l10n_br_base.city').call('get_city_ids', [state_id]).then(function (result) {
                     $('.client-address-city').children('option:not(:first)').remove();
                         $.each(result, function(key, value){
                             $('.client-address-city').append($("<option></option>")
