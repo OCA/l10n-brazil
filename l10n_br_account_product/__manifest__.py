a00c53cb28c93f472cebfad795deaaf72c77dbbd
renato <renato.lima@akretion.com.br>
2018-03-01 15:46:30 -0300

    create new file to split views product_template

--- l10n_br_account_product/__manifest__.py
+++ l10n_br_account_product/__manifest__.py
@@ -41,7 +41,7 @@
         'views/res_company_view.xml',
         'views/account_product_fiscal_classification_template_view.xml',
         'views/account_product_fiscal_classification_view.xml',
-        'views/product_view.xml',
+        'views/product_template_view.xml',
         'views/res_country_view.xml',
         'wizard/l10n_br_account_nfe_export_invoice_view.xml',
         'wizard/l10n_br_account_nfe_export_view.xml',
