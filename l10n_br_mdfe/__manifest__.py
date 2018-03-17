dbff76a981ee386cd58ce6f2eca5bf7f7aa40c51
Luis Felipe Mileo <mileo@kmee.com.br>
2018-03-17 17:32:15 -0300

    [NEW] Base para o módulo de mdf-e

---
+++ l10n_br_mdfe/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 KMEE INFORMATICA LTDA, Grupo Zenir
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Mdfe',
+    'summary': """
+        Emissão de Manifesto Eletrônico de Documentos Fiscais (MDF-e)""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE INFORMATICA LTDA, Grupo Zenir,Odoo Community Association (OCA)',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
