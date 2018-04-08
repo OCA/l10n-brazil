617f98d2c6fa66d2f9e53ad2ecd59bc2811cf3b4
Luis Felipe Mileo <mileo@kmee.com.br>
2018-04-08 13:56:21 -0300

    [NEW] Inclusão de novos campos e validações no res.partner

--- l10n_br_base/models/res_partner.py
+++ l10n_br_base/models/res_partner.py
@@ -2,13 +2,23 @@
 # Copyright (C) 2009 Gabriel C. Stabel
 # Copyright (C) 2009 Renato Lima (Akretion)
 # Copyright (C) 2012 Raphaël Valyi (Akretion)
+# Copyright (C) 2018 Luis Felipe Mileo (KMEE INFORMATICA LTDA)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import re
 
 from odoo import models, fields, api
-from odoo.addons.l10n_br_base.tools import fiscal
 from odoo.exceptions import ValidationError
+
+from odoo.addons.l10n_br_base.tools import fiscal
+from odoo.addons.l10n_br_base.tools.misc import punctuation_rm
+from odoo.addons.l10n_br_base.constante_tributaria import (
+    INDICADOR_IE_DESTINATARIO_NAO_CONTRIBUINTE,
+    INDICADOR_IE_DESTINATARIO_CONTRIBUINTE,
+    INDICADOR_IE_DESTINATARIO,
+    REGIME_TRIBUTARIO,
+    REGIME_TRIBUTARIO_SIMPLES,
+)
 
 
 class ResPartner(models.Model):
@@ -23,9 +33,9 @@ class ResPartner(models.Model):
                 self, without_company=False)
         else:
             address_format = (
-                self.country_id and self.country_id.address_format or
-                "%(street)s\n%(street2)s\n%(city)s"
-                " %(state_code)s%(zip)s\n%(country_name)s")
+                    self.country_id and self.country_id.address_format or
+                    "%(street)s\n%(street2)s\n%(city)s"
+                    " %(state_code)s%(zip)s\n%(country_name)s")
             args = {
                 'state_code': self.state_id and self.state_id.code or '',
                 'state_name': self.state_id and self.state_id.name or '',
@@ -33,7 +43,7 @@ class ResPartner(models.Model):
                 'country_name': self.country_id and self.country_id.name or '',
                 'company_name': self.parent_id and self.parent_id.name or '',
                 'l10n_br_city_name': self.l10n_br_city_id and
-                self.l10n_br_city_id.name or '',
+                                     self.l10n_br_city_id.name or '',
             }
             address_field = ['title', 'street', 'street2', 'zip',
                              'city', 'number', 'district']
@@ -45,7 +55,115 @@ class ResPartner(models.Model):
                 address_format = '%(company_name)s\n' + address_format
             return address_format % args
 
-    cnpj_cpf = fields.Char('CNPJ/CPF', size=18)
+    @api.depends('cnpj_cpf')
+    def _compute_tipo_pessoa(self):
+        for record in self:
+            if not record.cnpj_cpf:
+                record.tipo_pessoa = 'I'
+                record.cnpj_cpf_raiz = ''
+                continue
+
+            if record.cnpj_cpf[:2] == 'EX':
+                record.tipo_pessoa = 'E'
+                record.contribuinte = \
+                    INDICADOR_IE_DESTINATARIO_NAO_CONTRIBUINTE
+                record.cnpj_cpf_raiz = record.cnpj_cpf
+
+            elif len(record.cnpj_cpf) == 18:
+                record.tipo_pessoa = 'J'
+                record.contribuinte = \
+                    INDICADOR_IE_DESTINATARIO_CONTRIBUINTE
+                record.cnpj_cpf_raiz = record.cnpj_cpf[:10]
+
+            else:
+                record.tipo_pessoa = 'F'
+                record.contribuinte = \
+                    INDICADOR_IE_DESTINATARIO_NAO_CONTRIBUINTE
+                record.cnpj_cpf_raiz = record.cnpj_cpf
+
+            record.cnpj_cpf_numero = \
+                punctuation_rm(record.cnpj_cpf)
+
+    @api.depends('eh_consumidor_final', 'street', 'number', 'street2',
+                 'district', 'l10n_br_city_id', 'zip', 'customer',
+                 'supplier')
+    def _compute_exige_cadastro_completo(self):
+        for record in self:
+            if not record.eh_consumidor_final or \
+                    record.customer:
+                record.exige_cnpj_cpf = True
+                record.exige_endereco = True
+                continue
+
+            record.exige_cnpj_cpf = False
+
+            if (record.street or record.number or
+                    record.stree2 or
+                    record.district or record.zip):
+                record.exige_endereco = True
+            else:
+                record.exige_endereco = False
+
+    @api.depends('street', 'number', 'street2', 'district',
+                 'l10n_br_city_id', 'zip')
+    def _compute_endereco_completo(self):
+        for record in self:
+            if not record.street:
+                record.endereco_completo = ''
+                continue
+
+            endereco = record.street
+            endereco += ', '
+            endereco += record.number
+
+            if record.stree2:
+                endereco += ' - '
+                endereco += record.stree2
+
+            endereco += ' - '
+            endereco += record.district
+            endereco += ' - '
+            endereco += record.l10n_br_city.name
+            endereco += '-'
+            endereco += record.l10n_br_city.uf
+            endereco += ' - '
+            endereco += record.zip
+            record.endereco_completo = endereco
+
+
+    contribuinte = fields.Selection(
+        selection=INDICADOR_IE_DESTINATARIO,
+        string='Contribuinte',
+        # required=True,
+    )
+    cnpj_cpf = fields.Char(
+        string='CNPJ/CPF',
+        size=18,
+        index=True,
+        help='''Para records estrangeiros, usar EX9999,
+        onde 9999 é um número a sua escolha'''
+    )
+    cnpj_cpf_raiz = fields.Char(
+        string='Raiz do CNPJ/CPF',
+        size=14,
+        compute='_compute_tipo_pessoa',
+        store=True,
+        index=True,
+    )
+    cnpj_cpf_numero = fields.Char(
+        string='CNPJ/CPF (somente números)',
+        size=14,
+        compute='_compute_tipo_pessoa',
+        store=True,
+        index=True,
+    )
+    tipo_pessoa = fields.Char(
+        string='Tipo pessoa',
+        size=1,
+        compute='_compute_tipo_pessoa',
+        store=True,
+        index=True
+    )
 
     inscr_est = fields.Char('Inscr. Estadual/RG', size=16)
 
@@ -54,7 +172,7 @@ class ResPartner(models.Model):
     suframa = fields.Char('Suframa', size=18)
 
     legal_name = fields.Char(
-        u'Razão Social', size=128,
+        u'Razão Social', size=60,
         help="Nome utilizado em documentos fiscais")
 
     l10n_br_city_id = fields.Many2one(
@@ -64,6 +182,116 @@ class ResPartner(models.Model):
     district = fields.Char('Bairro', size=32)
 
     number = fields.Char(u'Número', size=10)
+    endereco_completo = fields.Char(
+        string='Endereço',
+        compute='_compute_endereco_completo',
+    )
+    exige_cnpj_cpf = fields.Boolean(
+        comodel_name='Exige CNPJ/CPF?',
+        compute='_compute_exige_cadastro_completo',
+    )
+    exige_endereco = fields.Boolean(
+        comodel_name='Exige endereço?',
+        compute='_compute_exige_cadastro_completo',
+    )
+    eh_orgao_publico = fields.Boolean(
+        string='É órgão público?',
+    )
+    eh_cooperativa = fields.Boolean(
+        string='É cooperativa?',
+    )
+    eh_sindicato = fields.Boolean(
+        string='É sindicato?',
+    )
+    eh_consumidor_final = fields.Boolean(
+        string='É consumidor final?',
+    )
+    eh_convenio = fields.Boolean(
+        string='É convênio?',
+    )
+    eh_transportadora = fields.Boolean(
+        string='É transportadora?'
+    )
+    eh_grupo = fields.Boolean(
+        string='É grupo?',
+        index=True,
+    )
+    im = fields.Char(
+        string='Inscrição municipal',
+        size=14
+    )
+    suframa = fields.Char(
+        string='SUFRAMA',
+        size=12
+    )
+    rntrc = fields.Char(
+        string='RNTRC',
+        size=15
+    )
+    cei = fields.Char(
+        string='CEI',
+        size=15
+    )
+    rg_numero = fields.Char(
+        string='RG',
+        size=14
+    )
+    rg_orgao_emissor = fields.Char(
+        string='Órgão emisssor do RG',
+        size=20
+    )
+    rg_data_expedicao = fields.Date(
+        string='Data de expedição do RG'
+    )
+    crc = fields.Char(
+        string='Conselho Regional de Contabilidade',
+        size=14
+    )
+    crc_uf = fields.Many2one(
+        comodel_name='res.country.state',
+        string='UF do CRC',
+        ondelete='restrict'
+    )
+    profissao = fields.Char(
+        string='Cargo',
+        size=40
+    )
+    pais_nacionalidade_id = fields.Many2one(
+        comodel_name='res.country',
+        string='Nacionalidade',
+        ondelete='restrict'
+    )
+    codigo_sindical = fields.Char(
+        comodel_name='Código sindical',
+        size=30
+    )
+    codigo_ans = fields.Char(
+        comodel_name='Código ANS',
+        size=6
+    )
+    transportadora_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Transportadora',
+        ondelete='restrict',
+        domain=[('eh_transportadora', '=', True)]
+    )
+    regime_tributario = fields.Selection(
+        selection=REGIME_TRIBUTARIO,
+        string='Regime tributário',
+        default=REGIME_TRIBUTARIO_SIMPLES,
+        index=True,
+    )
+    estado = fields.Char(
+        string='Estado',
+        related='state_id.code',
+        store=True,
+        index=True,
+    )
+
+    @api.onchange('l10n_br_city_id')
+    def onchange_l10n_br_city_id(self):
+        if self.l10n_br_city_id and self.l10n_br_city_id.cep_unico:
+            self.zip = self.l10n_br_city_id.cep_unico
 
     @api.multi
     @api.constrains('cnpj_cpf', 'inscr_est')
