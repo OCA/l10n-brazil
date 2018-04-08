7d8f9120707892888e5fabaa0ac310ab325e7f7c
Luis Felipe Mileo <mileo@kmee.com.br>
2018-04-08 15:44:26 -0300

    [NEW] Módulo de produtos brasileiro

---
+++ l10n_br_product/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Odoo Community Association
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Product',
+    'summary': """
+        Cadastro de produtos Brasileiro""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA)',
+    'depends': [
+        'l10n_br_base',
+        'product',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
