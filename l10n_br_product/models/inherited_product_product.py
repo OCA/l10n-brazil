98ca556500450c80488b0d88121d52c9b5d860cd
Luis Felipe Mileo <mileo@kmee.com.br>
2018-04-09 18:42:51 -0300

    [MOV] Movendo arquivos importados através do filter branch para o módulo correto

---
+++ l10n_br_product/models/inherited_product_product.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 Taŭga Tecnologia
+#   Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
+#
+
+from __future__ import division, print_function, unicode_literals
+
+from odoo import api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    sped_produto_id = fields.One2many(
+        comodel_name='sped.produto',
+        inverse_name='product_id',
+        string='Produto',
+        ondelete='cascade',
+    )
+
+    @api.model
+    def create(self, vals):
+
+        # Setar o campo obrigatorio do core
+        if 'nome' in vals:
+            vals.update(name=vals.get('nome'))
+
+        # Criar o product.product
+        product_id = super(ProductProduct, self).create(vals)
+
+        # Se a criacao do produto nao vier do sped.produto,
+        # criar o sped.produto referente
+        if "create_from_sped_produto" not in self._context:
+            product_id.create_sped_produto()
+        return product_id
+
+    @api.multi
+    def create_sped_produto(self):
+        sped_produto_obj = self.env["sped.produto"]
+        for product_id in self:
+            new_sped_produto = sped_produto_obj.create({
+                'product_id': product_id.id,
+            })
+        return True
