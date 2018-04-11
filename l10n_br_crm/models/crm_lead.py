c0c34846a81fb661ec4d456733fea90f73331cbb
Sidnei Brianti <scbrianti@gmail.com>
2018-04-11 17:55:06 -0300

    Update crm_lead.py

--- l10n_br_crm/models/crm_lead.py
+++ l10n_br_crm/models/crm_lead.py
@@ -119,6 +119,7 @@ class CrmLead(models.Model):
         )
         if self.partner_id:
             print (str(self.partner_id))
+            self.partner_id = self.partner_id.parent_id
             result['legal_name'] = self.partner_id.legal_name
             result['name_surname'] = self.partner_id.legal_name
             result['cpf'] = self.partner_id.cnpj_cpf
