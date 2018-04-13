dab740c7653308b8ede49f827e49ea4072f731a1
renato <renato.lima@akretion.com.br>
2018-04-13 17:55:36 -0300

    migrate l10n_br_account_product.import.declaration.line object

--- l10n_br_account_product/models/l10n_br_account_product_import_declaration_line.py
+++ l10n_br_account_product/models/l10n_br_account_product_import_declaration_line.py
@@ -2,19 +2,36 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+from odoo import models, fields
 
 
 class ImportDeclarationLine(models.Model):
     _name = 'l10n_br_account_product.import.declaration.line'
 
     import_declaration_id = fields.Many2one(
-        'l10n_br_account_product.import.declaration', u'DI',
-        ondelete='cascade', select=True)
-    sequence = fields.Integer(u'Sequência', default=1, required=True)
-    name = fields.Char(u'Adição', size=3, required=True)
+        comodel_name='l10n_br_account_product.import.declaration',
+        string=u'DI',
+        ondelete='cascade',
+        index=True
+    )
+    sequence = fields.Integer(
+        string=u'Sequência',
+        default=1,
+        required=True
+    )
+    name = fields.Char(
+        string=u'Adição',
+        size=3,
+        required=True
+    )
     manufacturer_code = fields.Char(
-        u'Código do Fabricante', size=60, required=True)
-    amount_discount = fields.Float(u'Valor',
-                                   digits=dp.get_precision('Account'),
-                                   default=0.00)
+        string=u'Código do Fabricante',
+        size=60,
+        required=True
+    )
+    amount_discount = fields.Float(
+        string=u'Valor',
+        digits=dp.get_precision('Account'),
+        default=0.00
+    )
+
