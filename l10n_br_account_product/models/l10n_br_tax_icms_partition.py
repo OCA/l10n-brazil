db7a3a8f77e37fbaa697d38b804cb0b3083b8a54
renato <renato.lima@akretion.com.br>
2018-04-13 18:00:24 -0300

    migrate l10n_br_tax.icms_partition object

--- l10n_br_account_product/models/l10n_br_tax_icms_partition.py
+++ l10n_br_account_product/models/l10n_br_tax_icms_partition.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class L10nBrTaxIcmsPartition(models.Model):
@@ -11,14 +11,14 @@ class L10nBrTaxIcmsPartition(models.Model):
     _description = 'Icms Partition'
 
     date_start = fields.Date(
-        u'Data Inicial',
+        string=u'Data Inicial',
         required=True
     )
     date_end = fields.Date(
-        u'Data Final',
+        string=u'Data Final',
         required=True
     )
     rate = fields.Float(
-        u'Percentual Interestadual de Rateio',
+        string=u'Percentual Interestadual de Rateio',
         required=True
     )
