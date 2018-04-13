c0ca004f0736b35901502bdea3f286d205cdc256
renato <renato.lima@akretion.com.br>
2018-04-13 18:08:56 -0300

    migrate l10n_br_account_product.cest object

--- l10n_br_account_product/models/l10n_br_account_product_cest.py
+++ l10n_br_account_product/models/l10n_br_account_product_cest.py
@@ -2,28 +2,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import re
-
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning as UserError
-from openerp.addons import decimal_precision as dp
-
-from openerp.addons.l10n_br_base.tools import fiscal
-from openerp.addons.l10n_br_account.models.l10n_br_account import TYPE
-
-PRODUCT_FISCAL_TYPE = [
-    ('product', 'Produto')
-]
-
-PRODUCT_FISCAL_TYPE_DEFAULT = PRODUCT_FISCAL_TYPE[0][0]
-
-NFE_IND_IE_DEST = [
-    ('1', '1 - Contribuinte do ICMS'),
-    ('2', '2 - Contribuinte Isento do ICMS'),
-    ('9', '9 - Não Contribuinte')
-]
-
-NFE_IND_IE_DEST_DEFAULT = NFE_IND_IE_DEST[0][0]
+from odoo import models, fields, api
 
 
 class L10nBrAccountProductCest(models.Model):
@@ -31,18 +10,18 @@ class L10nBrAccountProductCest(models.Model):
     _name = 'l10n_br_account_product.cest'
 
     code = fields.Char(
-        u'Código',
+        string=u'Código',
         size=9
     )
     name = fields.Char(
-        u'Nome'
+        string=u'Nome'
     )
     segment = fields.Char(
-        u'Segmento',
+        string=u'Segmento',
         size=32
     )
     item = fields.Char(
-        u'Item',
+        string=u'Item',
         size=4
     )
 
