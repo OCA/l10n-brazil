e4e56a8930d007de08e737ecd1b75c15be5d043a
renato <renato.lima@akretion.com.br>
2018-04-13 17:59:30 -0300

    migrate l10n_br_account_product.ipi_guideline object

--- l10n_br_account_product/models/l10n_br_account_product_ipi_guideline.py
+++ l10n_br_account_product/models/l10n_br_account_product_ipi_guideline.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class L10nBrIPIGuideline(models.Model):
@@ -10,15 +10,29 @@ class L10nBrIPIGuideline(models.Model):
     _name = 'l10n_br_account_product.ipi_guideline'
     _description = 'IPI Guidelines'
 
-    code = fields.Char(u'Código', size=3, required=True)
-    name = fields.Text(u'Descrição Enquadramento Legal do IPI', required=True)
-    cst_group = fields.Selection([('imunidade', u'Imunidade'),
-                                  ('suspensao', u'Suspensão'),
-                                  ('isencao', u'Isenção'),
-                                  ('reducao', u'Redução'),
-                                  ('outros', u'Outros'),
-                                  ], string='Grupo CST', required=True)
-    tax_code_in_id = fields.Many2one(
-        'account.tax.code.template', string=u'CST Entrada')
-    tax_code_out_id = fields.Many2one(
-        'account.tax.code.template', string=u'CST Saída')
+    code = fields.Char(
+        string=u'Código',
+        size=3,
+        required=True
+    )
+    name = fields.Text(
+        string=u'Descrição Enquadramento Legal do IPI',
+        required=True)
+    cst_group = fields.Selection(
+        selection=[('imunidade', u'Imunidade'),
+                   ('suspensao', u'Suspensão'),
+                   ('isencao', u'Isenção'),
+                   ('reducao', u'Redução'),
+                   ('outros', u'Outros')],
+        string='Grupo CST',
+        required=True
+    )
+    cst_in_id = fields.Many2one(
+        comodel_name='l10n_br_account_product.cst',
+        string=u'CST Entrada'
+    )
+    cst_out_id = fields.Many2one(
+        comodel_name='l10n_br_account_product.cst',
+        string=u'CST Saída'
+    )
+
