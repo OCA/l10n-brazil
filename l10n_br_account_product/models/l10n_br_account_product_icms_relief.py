57ee27f6281c359cb4c8405ee073fbf66d834422
renato <renato.lima@akretion.com.br>
2018-04-13 17:58:07 -0300

    migrate l10n_br_account_product.icms_relief object

--- l10n_br_account_product/models/l10n_br_account_product_icms_relief.py
+++ l10n_br_account_product/models/l10n_br_account_product_icms_relief.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class L10nBrIcmsRelief(models.Model):
@@ -10,6 +10,17 @@ class L10nBrIcmsRelief(models.Model):
     _name = 'l10n_br_account_product.icms_relief'
     _description = 'Icms Relief'
 
-    code = fields.Char(u'Código', size=2, required=True)
-    name = fields.Char('Nome', size=256, required=True)
-    active = fields.Boolean('Ativo', default=True)
+    code = fields.Char(
+        string=u'Código',
+        size=2,
+        required=True
+    )
+    name = fields.Char(
+        string=u'Nome',
+        size=256,
+        required=True
+    )
+    active = fields.Boolean(
+        string=u'Ativo',
+        default=True
+    )
