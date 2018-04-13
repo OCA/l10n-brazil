e5c9d4a3e630b936688ad0f4cc2bcb12ec6c81f7
renato <renato.lima@akretion.com.br>
2018-04-13 18:41:32 -0300

    migrate object account.fiscal.position and account.fiscal.position.line

--- l10n_br_account_product/models/res_country_state.py
+++ l10n_br_account_product/models/res_country_state.py
@@ -3,10 +3,10 @@
 # Copyright (C) 2016  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
-from openerp.addons.l10n_br_account.models.l10n_br_account import (
-    L10nBrTaxDefinition
+from .l10n_br_tax_definition import (
+    L10nBrTaxDefinitionCompanyProduct
 )
 
 
@@ -23,32 +23,29 @@ class ResCountryState(models.Model):
             state.product_tax_ids = product_taxes
 
     product_tax_definition_line = fields.One2many(
-        'l10n_br_tax.definition.state.product',
-        'state_id',
-        'Taxes Definitions'
+        comodel_name='l10n_br_tax.definition.state.product',
+        inverse_name='state_id',
+        string=u'Taxes Definitions'
     )
     product_tax_ids = fields.Many2many(
-        'account.tax',
-        string='Product Taxes',
+        comodel_name='account.tax',
+        string=u'Product Taxes',
         compute='_compute_taxes',
         store=True
     )
 
 
-class L10nBrTaxDefinitionStateProduct(L10nBrTaxDefinition, models.Model):
+class L10nBrTaxDefinitionStateProduct(L10nBrTaxDefinitionCompanyProduct,
+                                      models.Model):
     _name = 'l10n_br_tax.definition.state.product'
 
     state_id = fields.Many2one(
-        'res.country.state',
-        u'Estado'
+        comodel_name='res.country.state',
+        string=u'Estado'
     )
     fiscal_classification_id = fields.Many2one(
-        'account.product.fiscal.classification',
-        'Classificação Fiscal'
-    )
-    cest_id = fields.Many2one(
-        'l10n_br_account_product.cest',
-        'CEST'
+        comodel_name='account.product.fiscal.classification',
+        string=u'Classificação Fiscal'
     )
 
     _sql_constraints = [
