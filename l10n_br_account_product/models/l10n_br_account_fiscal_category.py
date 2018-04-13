193d6c9fa567b104dcd8046da6ef72dd802f6f83
renato <renato.lima@akretion.com.br>
2018-04-13 18:10:08 -0300

    migrate l10n_br_account_product.fiscal.category object

--- l10n_br_account_product/models/l10n_br_account_fiscal_category.py
+++ l10n_br_account_product/models/l10n_br_account_fiscal_category.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 from .l10n_br_account_product import (
     PRODUCT_FISCAL_TYPE,
@@ -14,5 +14,9 @@ class L10nBrAccountFiscalCategory(models.Model):
     _inherit = 'l10n_br_account.fiscal.category'
 
     fiscal_type = fields.Selection(
-        PRODUCT_FISCAL_TYPE, 'Tipo Fiscal', required=True,
-        default=PRODUCT_FISCAL_TYPE_DEFAULT)
+        selection=PRODUCT_FISCAL_TYPE,
+        string=u'Tipo Fiscal',
+        required=True,
+        default=PRODUCT_FISCAL_TYPE_DEFAULT
+    )
+
