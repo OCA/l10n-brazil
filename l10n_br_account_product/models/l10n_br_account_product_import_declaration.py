4e79764c1f335d6d17a25274a195837e1368ae2a
renato <renato.lima@akretion.com.br>
2018-04-13 17:53:39 -0300

    migrate l10n_br_account_product.import.declaration object

--- l10n_br_account_product/models/l10n_br_account_product_import_declaration.py
+++ l10n_br_account_product/models/l10n_br_account_product_import_declaration.py
@@ -4,50 +4,85 @@
 
 import re
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class ImportDeclaration(models.Model):
     _name = 'l10n_br_account_product.import.declaration'
 
     invoice_line_id = fields.Many2one(
-        'account.invoice.line', u'Linha de Documento Fiscal',
-        ondelete='cascade', select=True)
-    name = fields.Char(u'Número da DI', size=10, required=True)
-    date_registration = fields.Date(u'Data de Registro', required=True)
+        comodel_name='account.invoice.line',
+        string=u'Linha de Documento Fiscal',
+        ondelete='cascade',
+        index=True
+    )
+    name = fields.Char(
+        string=u'Número da DI',
+        size=10,
+        required=True
+    )
+    date_registration = fields.Date(
+        string=u'Data de Registro',
+        required=True
+    )
     exporting_code = fields.Char(
-        u'Código do Exportador', required=True, size=60)
+        string=u'Código do Exportador',
+        required=True,
+        size=60
+    )
     state_id = fields.Many2one(
-        'res.country.state', u'Estado',
-        domain="[('country_id.code', '=', 'BR')]")
-    location = fields.Char(u'Local', required=True, size=60)
-    date_release = fields.Date(u'Data de Liberação', required=True)
-    type_transportation = fields.Selection([
-        ('1', u'1 - Marítima'),
-        ('2', u'2 - Fluvial'),
-        ('3', u'3 - Lacustre'),
-        ('4', u'4 - Aérea'),
-        ('5', u'5 - Postal'),
-        ('6', u'6 - Ferroviária'),
-        ('7', u'7 - Rodoviária'),
-        ('8', u'8 - Conduto / Rede Transmissão'),
-        ('9', u'9 - Meios Próprios'),
-        ('10', u'10 - Entrada / Saída ficta'),
-    ], u'Transporte Internacional')
+        comodel_name='res.country.state',
+        string=u'Estado',
+        domain="[('country_id.code', '=', 'BR')]"
+    )
+    location = fields.Char(
+        string=u'Local',
+        required=True,
+        size=60
+    )
+    date_release = fields.Date(
+        string=u'Data de Liberação',
+        required=True
+    )
+    type_transportation = fields.Selection(
+        selection=[('1', u'1 - Marítima'),
+                   ('2', u'2 - Fluvial'),
+                   ('3', u'3 - Lacustre'),
+                   ('4', u'4 - Aérea'),
+                   ('5', u'5 - Postal'),
+                   ('6', u'6 - Ferroviária'),
+                   ('7', u'7 - Rodoviária'),
+                   ('8', u'8 - Conduto / Rede Transmissão'),
+                   ('9', u'9 - Meios Próprios'),
+                   ('10', u'10 - Entrada / Saída ficta')],
+        string=u'Transporte Internacional'
+    )
     afrmm_value = fields.Float(
-        'Valor da AFRMM', digits=dp.get_precision('Account'), default=0.00)
-    type_import = fields.Selection([
-        ('1', u'1 - Importação por conta própria'),
-        ('2', u'2 - Importação por conta e ordem'),
-        ('3', u'3 - Importação por encomenda'),
-    ], u'Tipo de Importação', default='1')
-    thirdparty_cnpj = fields.Char('CNPJ', size=18)
+        string=u'Valor da AFRMM',
+        digits=dp.get_precision('Account'),
+        default=0.00
+    )
+    type_import = fields.Selection(
+        selection=[('1', u'1 - Importação por conta própria'),
+                   ('2', u'2 - Importação por conta e ordem'),
+                   ('3', u'3 - Importação por encomenda')],
+        string=u'Tipo de Importação',
+        default='1'
+    )
+    thirdparty_cnpj = fields.Char(
+        string=u'CNPJ',
+        size=18
+    )
     thirdparty_state_id = fields.Many2one(
-        'res.country.state', u'Estado',
-        domain="[('country_id.code', '=', 'BR')]")
+        comodel_name='res.country.state',
+        string=u'Estado',
+        domain="[('country_id.code', '=', 'BR')]"
+    )
     line_ids = fields.One2many(
-        'l10n_br_account_product.import.declaration.line',
-        'import_declaration_id', 'Linhas da DI')
+        comodel_name='l10n_br_account_product.import.declaration.line',
+        inverse_name='import_declaration_id',
+        string=u'Linhas da DI'
+    )
 
     @api.multi
     def onchange_mask_cnpj_cpf(self, thirdparty_cnpj):
@@ -59,3 +94,4 @@ class ImportDeclaration(models.Model):
                     % (val[0:2], val[2:5], val[5:8], val[8:12], val[12:14])
         result['value'].update({'thirdparty_cnpj': thirdparty_cnpj})
         return result
+
