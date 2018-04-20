e371f7cb453f4475dda21d3a770fe3baa3a949e1
Magno <magno.costa@akretion.com.br>
2018-04-20 17:18:29 -0300

    Changes made to adapt code for extracting process happening between repos odoo-brazil-banking and l10n_br_core.

--- l10n_br_account_payment_boleto_brcobranca/models/account_move_line.py
+++ l10n_br_account_payment_boleto_brcobranca/models/account_move_line.py
@@ -13,9 +13,9 @@ _logger = logging.getLogger(__name__)
 
 
 class BoletoWrapper(object):
-    def __init__(self, obj, boleto_cnab_api_data):
+    def __init__(self, boleto_cnab_api_data):
         # wrap the object
-        self._wrapped_obj = obj
+        # self._wrapped_obj = obj
         self.boleto_cnab_api_data = boleto_cnab_api_data
 
     def __getattr__(self, attr):
@@ -38,10 +38,9 @@ dict_brcobranca_bank = {
     '341': 'itau',
     '033': 'santander',
     '748': 'sicredi',
-    # banks implemented in brcobranca but not in Python:
-    # '004': 'banco_nordeste',
-    # '021': 'banestes',
-    # '756': 'sicoob',
+    '004': 'banco_nordeste',
+    '021': 'banestes',
+    '756': 'sicoob',
 }
 
 dict_brcobranca_currency = {
@@ -52,12 +51,16 @@ dict_brcobranca_currency = {
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
     # see the list of brcobranca boleto fields:
-    # https://github.com/kivanio/brcobranca/blob/master/lib/brcobranca/boleto/base.rb
+    # https://github.com/kivanio/brcobranca/blob/master/lib/
+    # brcobranca/boleto/base.rb
     # and test a here:
-    # https://github.com/kivanio/brcobranca/blob/master/spec/brcobranca/boleto/itau_spec.rb
+    # https://github.com/kivanio/brcobranca/blob/master/spec/
+    # brcobranca/boleto/itau_spec.rb
 
     @api.multi
     def send_payment(self):
+
+        super(AccountMoveLine, self).send_payment()
         wrapped_boleto_list = []
 
         for move_line in self:
@@ -69,44 +72,57 @@ class AccountMoveLine(models.Model):
                 raise UserError(
                     _('The Bank %s is not implemented in BRCobranca.') %
                     move_line.payment_mode_id.bank_id.bank.name)
-            boleto_list = super(AccountMoveLine, move_line).send_payment()
 
             precision = self.env['decimal.precision']
             precision_account = precision.precision_get('Account')
-            instrucao_juros = ''
-            if move_line.payment_mode_id.cnab_percent_interest:
+
+            instrucao_juros = ""
+            instrucao_juros_tmp = "APÓS VENCIMENTO COBRAR PERCENTUAL"
+            if move_line.payment_mode_id.instrucao_boleto_perc_mora:
+                instrucao_juros_tmp = \
+                    move_line.payment_mode_id.instrucao_boleto_perc_mora
+            if move_line.payment_mode_id.boleto_perc_mora:
                 valor_juros = round(
                     move_line.debit *
-                    ((move_line.payment_mode_id.cnab_percent_interest / 100)
+                    ((move_line.payment_mode_id.boleto_perc_mora / 100)
                      / 30), precision_account)
                 instrucao_juros = (
-                    "APÓS VENCIMENTO COBRAR PERCENTUAL"
+                    instrucao_juros_tmp.encode('UTF-8') +
                     " DE %s %% AO MÊS ( R$ %s AO DIA )"
                     % (('%.2f' %
-                        move_line.payment_mode_id.cnab_percent_interest
+                        move_line.payment_mode_id.boleto_perc_mora
                         ).replace('.', ','),
                        ('%.2f' % valor_juros).replace('.', ',')))
 
             instrucao_multa = ''
+            instrucao_multa_tmp = "APÓS VENCIMENTO COBRAR MULTA"
+            if move_line.payment_mode_id.instrucao_boleto_perc_multa:
+                instrucao_multa_tmp = \
+                    move_line.payment_mode_id.instrucao_boleto_perc_multa
             if move_line.payment_mode_id.boleto_perc_mora:
                 valor_multa = round(move_line.debit * (
-                    (move_line.payment_mode_id.boleto_perc_mora / 100)
+                    (move_line.payment_mode_id.boleto_perc_multa / 100)
                 ), precision_account)
                 instrucao_multa = (
-                    "APÓS VENCIMENTO COBRAR MULTA "
-                    "PERCENTUAL DE %s %% ( R$ %s )" %
-                    (('%.2f' % move_line.payment_mode_id.boleto_perc_mora
+                    instrucao_multa_tmp.encode('UTF-8') +
+                    " DE %s %% ( R$ %s )" %
+                    (('%.2f' % move_line.payment_mode_id.boleto_perc_multa
                       ).replace('.', ','),
                      ('%.2f' % valor_multa).replace('.', ',')))
 
             instrucao_desconto_vencimento = ''
+            instrucao_desconto_vencimento_tmp =\
+                'CONCEDER ABATIMENTO PERCENTUAL DE'
+            if move_line.payment_term_id.instrucao_discount_perc:
+                instrucao_desconto_vencimento_tmp =\
+                    move_line.payment_term_id.instrucao_discount_perc
             if move_line.payment_term_id.discount_perc:
                 valor_desconto = round(
                     move_line.debit * (
                         move_line.payment_term_id.discount_perc / 100),
                     precision_account)
                 instrucao_desconto_vencimento = (
-                    'CONCEDER ABATIMENTO PERCENTUAL DE %s %% '
+                    instrucao_desconto_vencimento_tmp.encode('UTF-8') + ' %s %% '
                     'ATÉ O VENCIMENTO EM %s ( R$ %s )'
                     % (('%.2f' % move_line.payment_term_id.discount_perc
                         ).replace('.', ','),
@@ -116,51 +132,50 @@ class AccountMoveLine(models.Model):
                        ('%.2f' % valor_desconto).replace('.', ',')
                        ))
 
-            for boleto in boleto_list:
-                boleto_cnab_api_data = {
-                      'bank': bank_name_brcobranca[0],
-                      'valor': str("%.2f" % move_line.debit),
-                      'cedente': move_line.company_id.partner_id.legal_name,
-                      'cedente_endereco':
-                          move_line.company_id.partner_id.street + ', ' +
-                          move_line.company_id.partner_id.number + ' - ' +
-                          move_line.company_id.partner_id.district + ' - ' +
-                          move_line.company_id.partner_id.l10n_br_city_id.name + ' - ' +
-                          'CEP:' + move_line.company_id.partner_id.zip + ' - ' +
-                          move_line.company_id.partner_id.state_id.code,
-                      'documento_cedente': move_line.company_id.cnpj_cpf,
-                      'sacado': move_line.partner_id.legal_name,
-                      'sacado_documento': move_line.partner_id.cnpj_cpf,
-                      'agencia': move_line.payment_mode_id.bank_id.bra_number,
-                      'conta_corrente':
-                          move_line.payment_mode_id.bank_id.acc_number,
-                      'convenio': move_line.payment_mode_id.boleto_convenio,
-                      'carteira': str(move_line.payment_mode_id.boleto_carteira),
-                      'nosso_numero': int(''.join(
-                          i for i in move_line.boleto_own_number if i.isdigit())),
-                      'documento_numero': str(move_line.name).encode('utf-8'),
-                      'data_vencimento': datetime.strptime(
-                          move_line.date_maturity,
-                          '%Y-%m-%d').strftime('%Y/%m/%d'),
-                      'data_documento': datetime.strptime(
-                          move_line.invoice.date_invoice, '%Y-%m-%d').strftime(
-                          '%Y/%m/%d'),
-                      'especie': move_line.payment_mode_id.boleto_especie,
-                      'moeda': dict_brcobranca_currency[boleto.especie],
-                      'aceite': move_line.payment_mode_id.boleto_aceite,
-                      'sacado_endereco':
-                          move_line.partner_id.street + ', ' +
-                          move_line.partner_id.number + ' ' +
-                          move_line.partner_id.l10n_br_city_id.name + ' - ' +
-                          move_line.partner_id.state_id.name,
-                      'data_processamento': datetime.strptime(
-                          move_line.invoice.date_invoice, '%Y-%m-%d').strftime(
-                          '%Y/%m/%d'),
-                      'instrucao1': move_line.payment_mode_id.instrucoes or '',
-                      'instrucao3': instrucao_juros,
-                      'instrucao4': instrucao_multa,
-                      'instrucao5': instrucao_desconto_vencimento,
-                }
-                wrapped_boleto_list.append(
-                    BoletoWrapper(boleto, boleto_cnab_api_data))
+            boleto_cnab_api_data = {
+                  'bank': bank_name_brcobranca[0],
+                  'valor': str("%.2f" % move_line.debit),
+                  'cedente': move_line.company_id.partner_id.legal_name,
+                  'cedente_endereco':
+                      move_line.company_id.partner_id.street + ', ' +
+                      move_line.company_id.partner_id.number + ' - ' +
+                      move_line.company_id.partner_id.district + ' - ' +
+                      move_line.company_id.partner_id.l10n_br_city_id.name
+                      + ' - ' + 'CEP:' + move_line.company_id.partner_id.zip
+                      + ' - ' + move_line.company_id.partner_id.state_id.code,
+                  'documento_cedente': move_line.company_id.cnpj_cpf,
+                  'sacado': move_line.partner_id.legal_name,
+                  'sacado_documento': move_line.partner_id.cnpj_cpf,
+                  'agencia': move_line.payment_mode_id.bank_id.bra_number,
+                  'conta_corrente':
+                      move_line.payment_mode_id.bank_id.acc_number,
+                  'convenio': move_line.payment_mode_id.boleto_convenio,
+                  'carteira': str(move_line.payment_mode_id.boleto_carteira),
+                  'nosso_numero': int(''.join(
+                      i for i in move_line.boleto_own_number if i.isdigit())),
+                  'documento_numero': str(move_line.name).encode('utf-8'),
+                  'data_vencimento': datetime.strptime(
+                      move_line.date_maturity,
+                      '%Y-%m-%d').strftime('%Y/%m/%d'),
+                  'data_documento': datetime.strptime(
+                      move_line.invoice.date_invoice, '%Y-%m-%d').strftime(
+                      '%Y/%m/%d'),
+                  'especie': move_line.payment_mode_id.boleto_especie,
+                  'moeda': dict_brcobranca_currency['R$'],
+                  'aceite': move_line.payment_mode_id.boleto_aceite,
+                  'sacado_endereco':
+                      move_line.partner_id.street + ', ' +
+                      move_line.partner_id.number + ' ' +
+                      move_line.partner_id.l10n_br_city_id.name + ' - ' +
+                      move_line.partner_id.state_id.name,
+                  'data_processamento': datetime.strptime(
+                      move_line.invoice.date_invoice, '%Y-%m-%d').strftime(
+                      '%Y/%m/%d'),
+                  'instrucao1': move_line.payment_mode_id.instrucoes or '',
+                  'instrucao3': instrucao_juros,
+                  'instrucao4': instrucao_multa,
+                  'instrucao5': instrucao_desconto_vencimento,
+            }
+
+            wrapped_boleto_list.append(BoletoWrapper(boleto_cnab_api_data))
         return wrapped_boleto_list
