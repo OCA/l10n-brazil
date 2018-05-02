b9494b3d56ab97ad56b8a77c153667716115036d
Luis Felipe Mileo <mileo@kmee.com.br>
2018-05-02 07:36:50 -0300

    [MIG] Module version

--- l10n_br_resource/__manifest__.py
+++ l10n_br_resource/__manifest__.py
@@ -8,7 +8,7 @@
         This module extend core resource to create important brazilian
         informations. Define a Brazilian calendar and some tools to compute
         dates used in financial and payroll modules""",
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE,Odoo Community Association (OCA)',
     'website': 'www.odoobrasil.org.br',
