6cde305b898735327b3bb829c241ae591d3d90ab
renato <renato.lima@akretion.com.br>
2018-05-11 15:56:44 -0300

    migrate account.payment.term object

--- l10n_br_account_product/models/account_payment_term.py
+++ l10n_br_account_product/models/account_payment_term.py
@@ -2,12 +2,16 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class AccountPaymentTerm(models.Model):
     _inherit = 'account.payment.term'
 
     indPag = fields.Selection(
-        [('0', u'Pagamento à Vista'), ('1', u'Pagamento à Prazo'),
-         ('2', 'Outros')], 'Indicador de Pagamento', default='1')
+        selection=[('0', u'Pagamento à Vista'),
+                   ('1', u'Pagamento à Prazo'),
+                   ('2', 'Outros')],
+        string=u'Indicador de Pagamento',
+        default='1'
+    )
