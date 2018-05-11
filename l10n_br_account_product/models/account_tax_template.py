97f18ac432c5d3e41e5e428b5513643b32b24113
renato <renato.lima@akretion.com.br>
2018-05-11 15:59:39 -0300

    migrate account.tax.template object

--- l10n_br_account_product/models/account_tax_template.py
+++ l10n_br_account_product/models/account_tax_template.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class AccountTaxTemplate(models.Model):
@@ -10,13 +10,22 @@ class AccountTaxTemplate(models.Model):
     _inherit = 'account.tax.template'
 
     icms_base_type = fields.Selection(
-        [('0', 'Margem Valor Agregado (%)'), ('1', 'Pauta (valor)'),
-         ('2', 'Preço Tabelado Máximo (valor)'),
-         ('3', 'Valor da Operação')],
-        'Tipo Base ICMS', required=True, default='0')
+        selection=[('0', 'Margem Valor Agregado (%)'),
+                   ('1', 'Pauta (valor)'),
+                   ('2', 'Preço Tabelado Máximo (valor)'),
+                   ('3', 'Valor da Operação')],
+        string=u'Tipo Base ICMS',
+        required=True,
+        default='0'
+    )
     icms_st_base_type = fields.Selection(
-        [('0', 'Preço tabelado ou máximo  sugerido'),
-         ('1', 'Lista Negativa (valor)'),
-         ('2', 'Lista Positiva (valor)'), ('3', 'Lista Neutra (valor)'),
-         ('4', 'Margem Valor Agregado (%)'), ('5', 'Pauta (valor)')],
-        'Tipo Base ICMS ST', required=True, default='4')
+        selection=[('0', u'Preço tabelado ou máximo  sugerido'),
+                   ('1', u'Lista Negativa (valor)'),
+                   ('2', u'Lista Positiva (valor)'),
+                   ('3', u'Lista Neutra (valor)'),
+                   ('4', u'Margem Valor Agregado (%)'),
+                   ('5', u'Pauta (valor)')],
+        string=u'Tipo Base ICMS ST',
+        required=True,
+        default='4'
+    )
