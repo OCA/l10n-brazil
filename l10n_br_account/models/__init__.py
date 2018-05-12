3dbcbc3d0dcfbb18c7d6650daf04507967414254
renato <renato.lima@akretion.com.br>
2018-05-12 00:47:11 -0300

    added new files in __init__

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -9,7 +9,9 @@ from . import account_fiscal_position
 from . import res_partner
 from . import res_company
 from . import res_config
+from . import account_journal
 from . import account
+from . import wizard_multi_charts_accounts
 from . import account_invoice
 from . import product
 from . import l10n_br_account_product_category
