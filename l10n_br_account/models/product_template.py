2763faf0ab61cd71507aec49ae6a86bf51861782
renato <renato.lima@akretion.com.br>
2018-05-12 03:09:00 -0300

    migrate product.template object

--- l10n_br_account/models/product_template.py
+++ l10n_br_account/models/product_template.py
@@ -4,6 +4,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields
+
 from .l10n_br_account import PRODUCT_FISCAL_TYPE, PRODUCT_FISCAL_TYPE_DEFAULT
 
 
@@ -11,12 +12,15 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     fiscal_category_default_ids = fields.One2many(
-        'l10n_br_account.product.category', 'product_tmpl_id',
-        u'Categoria de Operação Fiscal Padrões')
+        comodel_name='l10n_br_account.product.category',
+        inverse_name='product_tmpl_id',
+        string=u'Categoria de Operação Fiscal Padrões')
+
     service_type_id = fields.Many2one(
-        'l10n_br_account.service.type', u'Tipo de Serviço')
+        comodel_name='l10n_br_account.service.type',
+        string=u'Tipo de Serviço')
+
     fiscal_type = fields.Selection(
-        PRODUCT_FISCAL_TYPE,
-        'Tipo Fiscal',
-        default=PRODUCT_FISCAL_TYPE_DEFAULT
-    )
+        selection=PRODUCT_FISCAL_TYPE,
+        string=u'Tipo Fiscal',
+        default=PRODUCT_FISCAL_TYPE_DEFAULT)
