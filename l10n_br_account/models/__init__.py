41b7299352402043e8d76064e8460fdd953a9df9
renato <renato.lima@akretion.com.br>
2018-05-12 03:14:55 -0300

    update __init__ file

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -3,6 +3,13 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import l10n_br_account
+from . import l10n_br_account_fiscal_category
+from . import l10n_br_account_cnae
+from . import l10n_br_account_service_type
+from . import l10n_br_account_fiscal_document
+from . import l10n_br_account_fiscal_document_serie
+from . import l10n_br_account_fiscal_partner_type
+from . import l10n_br_account_tax_definition
 from . import account_fiscal_position_abstract
 from . import account_fiscal_position_template
 from . import account_fiscal_position
@@ -14,7 +21,7 @@ from . import account
 from . import wizard_multi_charts_accounts
 from . import account_invoice
 from . import account_invoice_line
-from . import product
+from . import product_product
 from . import l10n_br_account_product_category
 from . import account_fiscal_position_rule
 from . import account_move_line
