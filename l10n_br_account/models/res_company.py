c2506cbb726f6ab47c443f71fa65f769c6a17198
Renato Lima <renato.lima@akretion.com>
2018-05-12 00:04:51 -0300

    migrate res.company object

--- l10n_br_account/models/res_company.py
+++ l10n_br_account/models/res_company.py
@@ -18,28 +18,54 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     service_invoice_id = fields.Many2one(
-        'l10n_br_account.fiscal.document',
-        'Documento Fiscal')
+        comodel_name='l10n_br_account.fiscal.document',
+        string=u'Documento Fiscal'
+    )
     document_serie_service_id = fields.Many2one(
-        'l10n_br_account.document.serie', u'Série Fiscais para Serviço',
-        domain="[('company_id', '=', active_id),('active','=',True),"
-        "('fiscal_type','=','service')]")
+        comodel_name='l10n_br_account.document.serie',
+        string=u'Série Fiscais para Serviço',
+        domain="[('company_id', '=', active_id), ('active', '=', True),"
+               "('fiscal_type', '=', 'service')]"
+    )
     annual_revenue = fields.Float(
-        'Faturamento Anual', required=True,
-        digits_compute=dp.get_precision('Account'), default=0.00,
-        help="Faturamento Bruto dos últimos 12 meses")
+        string=u'Faturamento Anual',
+        required=True,
+        digits=dp.get_precision('Account'),
+        default=0.00,
+        help=u"Faturamento Bruto dos últimos 12 meses"
+    )
     fiscal_type = fields.Selection(
-        COMPANY_FISCAL_TYPE, 'Regime Tributário', required=True,
-        default=COMPANY_FISCAL_TYPE_DEFAULT)
+        selection=COMPANY_FISCAL_TYPE,
+        string=u'Regime Tributário',
+        required=True,
+        default=COMPANY_FISCAL_TYPE_DEFAULT
+    )
     cnae_main_id = fields.Many2one(
-        'l10n_br_account.cnae', 'CNAE Primário')
+        comodel_name='l10n_br_account.cnae',
+        string=u'CNAE Primário'
+    )
     cnae_secondary_ids = fields.Many2many(
-        'l10n_br_account.cnae', 'res_company_l10n_br_account_cnae',
-        'company_id', 'cnae_id', 'CNAE Segundários')
-    ecnpj_a1_file = fields.Binary('Arquivo e-CNPJ A1')
-    ecnpj_a1_password = fields.Char('Senha e-CNPJ A1', size=64)
+        comodel_name='l10n_br_account.cnae',
+        relation='res_company_l10n_br_account_cnae',
+        column1='company_id',
+        column2='cnae_id',
+        string=u'CNAE Segundários'
+    )
+    ecnpj_a1_file = fields.Binary(
+        string=u'Arquivo e-CNPJ A1'
+    )
+    ecnpj_a1_password = fields.Char(
+        string=u'Senha e-CNPJ A1',
+        size=64
+    )
     fiscal_rule_parent_id = fields.Many2one(
-        'account.fiscal.position.rule', u'Conjunto de Regras Fiscais',
-        domain="[('parent_id', '=', False)]")
-    ipbt_token = fields.Char(string=u'IPBT Token')
-    ibpt_update_days = fields.Integer(string=u'IPBT Token')
+        comodel_name='account.fiscal.position.rule',
+        string=u'Conjunto de Regras Fiscais',
+        domain="[('parent_id', '=', False)]"
+    )
+    ipbt_token = fields.Char(
+        string=u'IPBT Token'
+    )
+    ibpt_update_days = fields.Integer(
+        string=u'IPBT Token'
+    )
