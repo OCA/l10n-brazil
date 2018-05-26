0c8fb44ebf90d2027cffd424a35a101b4aeef29f
hendrixcosta <hendrixcosta@gmail.com>
2018-05-26 15:30:39 -0300

    [REF] SWubstituir campos obsoletos por campos necessarios à rescisao

--- l10n_br_hr_payroll_report/models/__init__.py
+++ l10n_br_hr_payroll_report/models/__init__.py
@@ -1,4 +1,4 @@
 from . import hr_employee
-from . import hr_contract
+from . import hr_payslip
 from . import hr_field_rescission
 from . import hr_salary_rule
---
+++ l10n_br_hr_payroll_report/models/hr_payslip.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from pybrasil import data
+except ImportError:
+    _logger.info('Cannot import pybrasil')
+
+
+class HrPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    data_aviso_previo_fmt = fields.Char(
+        string=u'Data do aviso prévio',
+        default=' / / ',
+        compute='_compute_data_aviso_previo',
+        store=True,
+    )
+
+    data_afastamento_fmt = fields.Char(
+        string=u'Data do aviso prévio',
+        default=' / / ',
+        compute='_compute_data_afastamento',
+        store=True,
+    )
+
+    @api.depends('dias_aviso_previo_trabalhados')
+    def _compute_data_aviso_previo(self):
+        for payslip_id in self:
+            if payslip_id.dias_aviso_previo_trabalhados:
+                payslip_id.data_aviso_previo_fmt = \
+                    data.formata_data(payslip_id.dias_aviso_previo_trabalhados) or ' '
+            else:
+                payslip_id.data_aviso_previo_fmt = ' / / '
+
+    @api.depends('data_afastamento')
+    def _compute_data_afastamento(self):
+        for payslip_id in self:
+            if payslip_id.data_afastamento:
+                payslip_id.data_afastamento_fmt = \
+                    data.formata_data(payslip_id.data_afastamento)
+            else:
+                payslip_id.data_aviso_previo_fmt = ' / / '
+
+
