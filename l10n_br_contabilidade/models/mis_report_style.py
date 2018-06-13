d15180197ba5e8e67ad65fd3feb11cd4e347e8a3
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-06-13 10:51:42 +0200

    add hide_always in style

--- l10n_br_contabilidade/models/mis_report_style.py
+++ l10n_br_contabilidade/models/mis_report_style.py
@@ -31,6 +31,7 @@ PROPS = [
     'dp',
     'divider',
     'hide_empty',
+    'hide_always',
 ]
 
 TYPE_NUM = 'num'
@@ -132,6 +133,8 @@ class MisReportKpiStyle(models.Model):
                                default='1')
     hide_empty_inherit = fields.Boolean(default=True)
     hide_empty = fields.Boolean(default=False)
+    hide_always_inherit = fields.Boolean(default=True)
+    hide_always = fields.Boolean(default=False)
 
     @api.model
     def merge(self, styles):
