65dfea9577858a09ff760da44d03b8a9083a05ba
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-06-25 17:41:50 -0300

    [ADD] Modelo intermediario turnos no sped

---
+++ sped_esocial/models/sped_esocial_turnos_trabalho.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ABGF
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class SpedEsocialTurnosTrabalho(models.Model):
+    _name = "sped.esocial.turnos.trabalho"
+
+    esocial_id = fields.Many2one(
+        string="e-Social",
+        comodel_name="sped.esocial",
+        required=True,
+    )
+    sped_esocial_turnos_trabalho_id = fields.Many2one(
+        string="Turno de Trabalho",
+        comodel_name="esocial.turnos.trabalho",
+        required=True,
+    )
+    sped_S1050_registro = fields.Many2one(
+        string='Registro S-1050',
+        comodel_name='sped.transmissao',
+    )
