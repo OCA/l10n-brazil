e38d0d747e5cdd428242dd3f9a6e4304fd14449d
Wagner Pereira <wagner.pereira@abgf.gov.br>
2018-06-26 16:58:15 -0300

    [ADD] Mais campos do S-2200 foram adicionados ao método

---
+++ sped_esocial/models/inherited_hr_employee_dependent.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+from openerp.exceptions import ValidationError
+
+
+class HrEmployee(models.Model):
+
+    _inherit = 'hr.employee'
+
+    # Cria campos pais_nac_id e pais_nascto_id para substituir o place_of_birth (que é texto)
+    pais_nascto_id = fields.Many2one(
+        string='País de Nascimento',
+        comodel_name='sped.pais',
+    )
+    pais_nac_id = fields.Many2one(
+        string='Nacionalidade',
+        comodel_name='sped.pais',
+    )
+
+    # Dados que faltam em l10n_br_hr
+    cnh_dt_exped = fields.Date(
+        string='Data de Emissão',
+    )
+    cnh_uf = fields.Many2one(
+        string='UF',
+        comodel_name='res.country.state',
+    )
+    cnh_dt_pri_hab = fields.Date(
+        string='Data da 1ª Hab.',
+    )
+    tp_lograd = fields.Many2one(
+        string='Tipo de Lograd.',
+        comodel_name='sped.tipo_logradouro',
+    )
