e535d8ea4faca382e16757a373275384bfae7619
Wagner Pereira <wagner.pereira@abgf.gov.br>
2018-06-27 11:31:38 -0300

    [FIX] Ajustes no contrato e no Horario de Trabalho para suportar S-2200

--- sped_esocial/models/inherited_resource_calendar_attendance.py
+++ sped_esocial/models/inherited_resource_calendar_attendance.py
@@ -4,6 +4,16 @@
 
 from openerp import api, fields, models
 
+DIADASEMANA = [
+            ('1', 'Segunda-Feira'),
+            ('2', 'Terça-Feira'),
+            ('3', 'Quarta-Feira'),
+            ('4', 'Quinta-Feira'),
+            ('5', 'Sexta-Feira'),
+            ('6', 'Sábado'),
+            ('7', 'Domingo'),
+            ('8', 'Dia variável'),
+        ]
 
 class ResourceCalendarAttendance(models.Model):
 
@@ -13,9 +23,25 @@ class ResourceCalendarAttendance(models.Model):
         string='Turno',
         comodel_name='esocial.turnos.trabalho'
     )
+    name = fields.Char(
+        required=False,
+    )
     hour_from = fields.Float(
         required=False,
     )
     hour_to = fields.Float(
         required=False,
     )
+    dayofweek = fields.Selection(
+        required=False,
+    )
+    diadasemana = fields.Selection(
+        string='Dia da Semana',
+        selection=DIADASEMANA,
+        required=True,
+    )
+
+    @api.onchange('diadasemana')
+    def onchange_diadasemana(self):
+        self.ensure_one()
+        self.name = dict(self._fields['diadasemana'].selection).get(self.diadasemana)
