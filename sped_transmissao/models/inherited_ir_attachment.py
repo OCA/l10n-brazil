82f03272d1bd45725e16b14ecadfd0e7650212e6
Hendrix Costa <hendrixcosta@gmail.com>
2018-07-02 13:52:49 -0300

    [FIX] Alterando Licença

--- sped_transmissao/models/inherited_ir_attachment.py
+++ sped_transmissao/models/inherited_ir_attachment.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-#    Copyright © 2017; Brasil; Taŭga Tecnologia; Todos os direitos reservados
-#    Copyright © 2017; Brazil; Taŭga Technology; All rights reserved
+# Copyright 2018 ABGF
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import base64
 from lxml import etree
--- sped_transmissao/models/sped_ocorrencia.py
+++ sped_transmissao/models/sped_ocorrencia.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 KMEE INFORMATICA LTDA
+# Copyright 2018 ABGF
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
