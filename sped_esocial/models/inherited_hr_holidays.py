c37638de8715e486588eb5bf43e8188289871de4
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-07-17 11:24:01 -0300

    [ADD] Validações de dias de afastamento e-Social

--- sped_esocial/models/inherited_hr_holidays.py
+++ sped_esocial/models/inherited_hr_holidays.py
@@ -12,6 +12,7 @@ class HrHolidays(models.Model):
     sped_esocial_afastamento_id = fields.Many2one(
         string='Evento e-Social',
         comodel_name='sped.esocial.afastamento.temporario',
+        ondelete='cascade',
     )
     situacao_esocial = fields.Selection(
         selection=[
@@ -28,10 +29,37 @@ class HrHolidays(models.Model):
 
     @api.model
     def create(self, vals):
+        self.check_dias_afastamento(vals)
         res = super(HrHolidays, self).create(vals)
         res._gerar_tabela_intermediaria()
 
         return res
+
+    def check_dias_afastamento(self, vals):
+        holidays_status_id = self.env['hr.holidays.status'].browse(
+            vals['holiday_status_id'])
+        if holidays_status_id.esocial_evento_afastamento_id.codigo != '15':
+            self.valida_dias_afastamento(vals)
+        else:
+            self.valida_dias_inicio_ferias(vals)
+
+    def valida_dias_inicio_ferias(self, vals):
+        data_atual = fields.Datetime.from_string(fields.Datetime.now())
+        data_inicio = fields.Datetime.from_string(vals['data_inicio'])
+        if (data_inicio - data_atual).days > 60:
+            raise ValidationError(
+                "O inicío do afastamento por gozo de férias não pode ser "
+                "maior que 60 dias da data atual!"
+            )
+
+    def valida_dias_afastamento(self, vals):
+        data_inicio = fields.Datetime.from_string(vals['data_inicio'])
+        data_fim = fields.Datetime.from_string(vals['data_fim'])
+        if (data_fim - data_inicio).days > 15:
+            raise ValidationError(
+                "Este evento de afastamento não "
+                "pode ser maior do que 15 dias!"
+            )
 
     @api.multi
     def _gerar_tabela_intermediaria(self):
