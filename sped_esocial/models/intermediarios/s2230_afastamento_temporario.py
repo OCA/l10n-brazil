1a288eceff7236352e0b13572bae658f05317d51
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-07-17 10:35:06 -0300

    [ADD] Popula xml do registro S-2230

--- sped_esocial/models/intermediarios/s2230_afastamento_temporario.py
+++ sped_esocial/models/intermediarios/s2230_afastamento_temporario.py
@@ -4,7 +4,6 @@
 
 import pysped
 from openerp import api, fields, models
-from openerp.exceptions import ValidationError
 from pybrasil.inscricao.cnpj_cpf import limpa_formatacao
 
 from .sped_registro_intermediario import SpedRegistroIntermediario
@@ -90,116 +89,45 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
         temporário de um trabalhador
         """
         pass
-        # # Cria o registro
-        # S2205 = pysped.esocial.leiaute.S2205_2()
-        # empregado_id = self.hr_employee_id
-        #
-        # # Popula ideEvento
-        # S2205.tpInsc = '1'
-        # S2205.nrInsc = limpa_formatacao(
-        #     empregado_id.company_id.cnpj_cpf
-        # )[0:8]
-        # S2205.evento.ideEvento.indRetif.valor = '1'
-        # S2205.evento.ideEvento.tpAmb.valor = int(
-        #     empregado_id.company_id.esocial_tpAmb
-        # )
-        # S2205.evento.ideEvento.procEmi.valor = '1'
-        # S2205.evento.ideEvento.verProc.valor = '8.0'
-        #
-        # # Popula ideEmpregador (Dados do Empregador)
-        # S2205.evento.ideEmpregador.tpInsc.valor = '1'
-        # S2205.evento.ideEmpregador.nrInsc.valor = limpa_formatacao(
-        #     empregado_id.company_id.cnpj_cpf)[0:8]
-        #
-        # # Popula ideTrabalhador (Identificação do Trabalhador)
-        # S2205.evento.ideTrabalhador.cpfTrab.valor = limpa_formatacao(
-        #     empregado_id.cpf
-        # )
-        #
-        # # Popula alteracao (Alteração de dados cadastrais do Trabalhador)
-        # S2205.evento.alteracao.dtAlteracao.valor = fields.Datetime.now()
-        #
-        # dados_trabalhador = S2205.evento.alteracao.dadosTrabalhador
-        # dados_trabalhador.nisTrab.valor = limpa_formatacao(
-        #     empregado_id.pis_pasep
-        # )
-        # dados_trabalhador.nmTrab.valor = empregado_id.name
-        # if empregado_id.gender == 'male':
-        #     dados_trabalhador.sexo.valor = 'M'
-        # else:
-        #     dados_trabalhador.sexo.valor = 'F'
-        # dados_trabalhador.racaCor.valor = empregado_id.ethnicity.code
-        # dados_trabalhador.estCiv.valor = ESTADO_CIVIL[
-        #     empregado_id.marital]
-        # dados_trabalhador.grauInstr.valor = \
-        #     empregado_id.educational_attainment.code
-        #
-        # # Popula nascimento (Informações do nascimento)
-        # dados_trabalhador.nascimento.dtNascto.valor = empregado_id.birthday
-        # if empregado_id.naturalidade:
-        #     dados_trabalhador.nascimento.codMunic.valor = \
-        #         empregado_id.naturalidade.state_id.ibge_code + \
-        #         empregado_id.naturalidade.ibge_code
-        #     dados_trabalhador.nascimento.uf.valor = \
-        #         empregado_id.naturalidade.state_id.code
-        # dados_trabalhador.nascimento.paisNascto.valor = \
-        #     empregado_id.pais_nascto_id.codigo
-        # dados_trabalhador.nascimento.paisNac.valor = \
-        #     empregado_id.pais_nac_id.codigo
-        # dados_trabalhador.nascimento.nmMae.valor = empregado_id.mother_name
-        # dados_trabalhador.nascimento.nmPai.valor = empregado_id.father_name
-        #
-        # # Popula documentos (Informações dos documentos pessoais)
-        # ctps = pysped.esocial.leiaute.S2205_CTPS_2()
-        # ctps.nrCtps.valor = empregado_id.ctps
-        # ctps.serieCtps.valor = empregado_id.ctps_series
-        # ctps.ufCtps.valor = empregado_id.ctps_uf_id.code
-        # dados_trabalhador.documentos.CTPS.append(ctps)
-        #
-        # # Popula RIC (Registro de Identificação Civil)
-        # rg = pysped.esocial.leiaute.S2205_RG_2()
-        # rg.nrRg.valor = empregado_id.rg
-        # rg.orgaoEmissor.valor = empregado_id.organ_exp
-        # rg.dtExped.valor = empregado_id.rg_emission
-        # dados_trabalhador.documentos.RIC.append(rg)
-        #
-        # # Popula CNH (Carteira Nacional de Habilitação)
-        # cnh = pysped.esocial.leiaute.S2205_CNH_2()
-        #
-        # # Popula endereco (Informações do endereço do Trabalhador)
-        # endereco_brasil = dados_trabalhador.endereco.brasil
-        # endereco_brasil.tpLograd.valor = empregado_id.address_home_id.tp_lograd.codigo
-        # endereco_brasil.dscLograd.valor = empregado_id.address_home_id.street
-        # endereco_brasil.nrLograd.valor = empregado_id.address_home_id.number \
-        #     if empregado_id.address_home_id.number else 'S/N'
-        # endereco_brasil.bairro.valor = empregado_id.address_home_id.district
-        # endereco_brasil.cep.valor = limpa_formatacao(
-        #     empregado_id.address_home_id.zip)
-        # endereco_brasil.codMunic.valor = \
-        #     empregado_id.address_home_id.state_id.ibge_code + \
-        #     empregado_id.address_home_id.l10n_br_city_id.ibge_code
-        # endereco_brasil.uf.valor = empregado_id.address_home_id.state_id.code
-        #
-        # # Popula Dependentes (Informações dos dependentes)
-        # for dependente in empregado_id.dependent_ids:
-        #     dependente_xml = pysped.esocial.leiaute.S2205_Dependente_2()
-        #
-        #     dependente_xml.tpDep.valor = '03'
-        #     # dependente_xml.tpDep.valor = dependente.dependent_type_id.code
-        #     dependente_xml.nmDep.valor = dependente.dependent_name
-        #     dependente_xml.dtNascto.valor = dependente.dependent_dob
-        #     if dependente.dependent_cpf:
-        #         dependente_xml.cpfDep.valor = dependente.dependent_cpf
-        #     if dependente.dependent_verification:
-        #         dependente_xml.depIRRF.valor = 'S'
-        #     else:
-        #         dependente_xml.depIRRF.valor = 'N'
-        #     dependente_xml.depSF.valor = 'N'
-        #     dependente_xml.incTrab.valor = 'N'
-        #
-        #     dados_trabalhador.dependente.append(dependente_xml)
-        #
-        # return S2205
+        # Cria o registro
+        S2230 = pysped.esocial.leiaute.S2230_2()
+        holiday_id = self.hr_holiday_id
+
+        S2230.evento.ideEvento.indRetif.valor = '1'
+        S2230.evento.ideEvento.tpAmb.valor = \
+            holiday_id.contrato_id.company_id.esocial_tpAmb
+        S2230.evento.ideEvento.procEmi.valor = '1'
+        S2230.evento.ideEvento.verProc.valor = '8.0'
+
+        # Popula ideEmpregador
+        S2230.evento.ideEmpregador.tpInsc.valor = '1'
+        S2230.evento.ideEmpregador.nrInsc.valor = limpa_formatacao(
+            holiday_id.contrato_id.company_id.cnpj_cpf)[0:8]
+
+        # Popula ideVinculo
+        S2230.evento.ideVinculo.cpfTrab.valor = limpa_formatacao(
+            holiday_id.contrato_id.employee_id.cpf)
+        S2230.evento.ideVinculo.nisTrab.valor = limpa_formatacao(
+            holiday_id.contrato_id.employee_id.pis_pasep)
+        S2230.evento.ideVinculo.matricula.valor = \
+            holiday_id.contrato_id.matricula
+
+        # Popula infoAfastamento
+        # Popula iniAfastamento
+        inicio_afastamento = pysped.esocial.leiaute.S2230_IniAfastamento_2()
+        inicio_afastamento.dtIniAfast.valor = holiday_id.data_inicio
+        inicio_afastamento.codMotAfast.valor = \
+            holiday_id.holiday_status_id.esocial_evento_afastamento_id.codigo
+
+        S2230.evento.infoAfastamento.iniAfastamento.append(inicio_afastamento)
+
+        # Popula fimAfastamento
+        fim_afastamento = pysped.esocial.leiaute.S2230_FimAfastamento_2()
+        fim_afastamento.dtTermAfast.valor = holiday_id.data_fim
+
+        S2230.evento.infoAfastamento.fimAfastamento.append(fim_afastamento)
+
+        return S2230
 
     @api.multi
     def retorno_sucesso(self):
