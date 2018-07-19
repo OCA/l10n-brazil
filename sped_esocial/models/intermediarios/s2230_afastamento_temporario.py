f907c132ef2278a72b406677816c8d32987abafe
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-07-19 10:01:57 -0300

    [FIX] Separação entre inicio e fim do Afastamento
    - No modelo intermediário do registro S-2230

--- sped_esocial/models/intermediarios/s2230_afastamento_temporario.py
+++ sped_esocial/models/intermediarios/s2230_afastamento_temporario.py
@@ -28,11 +28,11 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
         comodel_name='hr.holidays',
         required=True,
     )
-    sped_afastamento = fields.Many2many(
+    sped_afastamento = fields.Many2one(
         string='Registro Afastamento',
         comodel_name='sped.registro',
     )
-    situacao_esocial = fields.Selection(
+    situacao_esocial_afastamento = fields.Selection(
         selection=[
             ('1', 'Precisa Transmitir'),
             ('2', 'Transmitida'),
@@ -40,7 +40,22 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
             ('4', 'Sucesso'),
         ],
         string='Situação no e-Social',
-        compute='compute_situacao_esocial',
+        compute='_compute_situacao_esocial',
+    )
+    sped_afastamento_encerrado = fields.Many2one(
+        string='Registro Encerramento Afastamento',
+        comodel_name='sped.registro',
+    )
+
+    situacao_esocial_afastamento_encerramento = fields.Selection(
+        selection=[
+            ('1', 'Precisa Transmitir'),
+            ('2', 'Transmitida'),
+            ('3', 'Erro(s)'),
+            ('4', 'Sucesso'),
+        ],
+        string='Situação no e-Social',
+        compute='_compute_situacao_esocial_encerramento',
     )
 
     @api.depends('hr_holiday_id')
@@ -50,15 +65,28 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
                 record.hr_holiday_id.display_name or '')
 
     @api.depends('sped_afastamento')
-    def compute_situacao_esocial(self):
+    def _compute_situacao_esocial(self):
         for afastamento in self:
             situacao_esocial = '1'
 
-            for afastamento in afastamento.sped_afastamento:
-                situacao_esocial = afastamento.situacao
+            if afastamento.sped_afastamento:
+                situacao_esocial = afastamento.sped_afastamento.situacao
 
             # Popula na tabela
             afastamento.situacao_esocial = situacao_esocial
+
+    @api.depends('sped_afastamento_encerrado')
+    def _compute_situacao_esocial_encerramento(self):
+        for afastamento in self:
+            situacao_esocial = '1'
+
+            if afastamento.sped_afastamento_encerrado:
+                situacao_esocial = \
+                    afastamento.sped_afastamento_encerrado.situacao
+
+            # Popula na tabela
+            afastamento.situacao_esocial_afastamento_encerramento = \
+                situacao_esocial
 
     # Roda a atualização do e-Social (não transmite ainda)
     @api.multi
@@ -80,7 +108,13 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
             }
 
             sped_afastamento = self.env['sped.registro'].create(values)
-            self.sped_afastamento = [(4, sped_afastamento.id)]
+            self.sped_afastamento = sped_afastamento
+
+    def verifica_encerramento_afastamento(self):
+        """
+        Função para verificar
+        :return:
+        """
 
     @api.multi
     def popula_xml(self, ambiente='2', operacao='I'):
@@ -116,19 +150,32 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
             holiday_id.contrato_id.matricula
 
         # Popula infoAfastamento
-        # Popula iniAfastamento
-        inicio_afastamento = pysped.esocial.leiaute.S2230_IniAfastamento_2()
-        inicio_afastamento.dtIniAfast.valor = holiday_id.data_inicio
-        inicio_afastamento.codMotAfast.valor = \
-            holiday_id.holiday_status_id.esocial_evento_afastamento_id.codigo
 
-        S2230.evento.infoAfastamento.iniAfastamento.append(inicio_afastamento)
+        # Popula iniAfastamento
+        bloco_inicio = S2230.evento.infoAfastamento.iniAfastamento
+        if not self.sped_afastamento:
+            inicio_afastamento = pysped.esocial.leiaute.S2230_IniAfastamento_2()
+            inicio_afastamento.dtIniAfast.valor = holiday_id.data_inicio
+            codigo_afastamento = holiday_id.holiday_status_id.\
+                esocial_evento_afastamento_id.codigo
+            inicio_afastamento.codMotAfast.valor = codigo_afastamento
+
+            bloco_inicio.append(inicio_afastamento)
 
         # Popula fimAfastamento
-        fim_afastamento = pysped.esocial.leiaute.S2230_FimAfastamento_2()
-        fim_afastamento.dtTermAfast.valor = holiday_id.data_fim
+        data_atual = fields.Datetime.from_string(fields.Datetime.now())
+        data_fim = fields.Datetime.from_string(holiday_id.data_fim)
+        bloco_fim = S2230.evento.infoAfastamento.fimAfastamento
+        cod_ferias = holiday_id.holiday_status_id.\
+                         esocial_evento_afastamento_id.codigo == '15'
+        if data_atual <= data_fim or cod_ferias:
+            fim_afastamento = pysped.esocial.leiaute.S2230_FimAfastamento_2()
+            fim_afastamento.dtTermAfast.valor = holiday_id.data_fim
 
-        S2230.evento.infoAfastamento.fimAfastamento.append(fim_afastamento)
+            bloco_fim.append(fim_afastamento)
+
+        if bloco_inicio and bloco_fim:
+            self.sped_afastamento_encerrado = self.sped_afastamento
 
         return S2230
 
