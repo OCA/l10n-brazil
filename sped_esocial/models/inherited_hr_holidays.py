450650072946b828c086399261b5b85205904afe
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-07-19 10:01:14 -0300

    [FIX] Campos e funcionalidades do Afastamento
    - No modelo do hr.holidays

--- sped_esocial/models/inherited_hr_holidays.py
+++ sped_esocial/models/inherited_hr_holidays.py
@@ -16,22 +16,69 @@ class HrHolidays(models.Model):
     )
     situacao_esocial = fields.Selection(
         selection=[
-            ('0', 'Inativa'),
-            ('1', 'Ativa'),
-            ('2', 'Precisa Atualizar'),
-            ('3', 'Aguardando Transmissão'),
-            ('9', 'Finalizada'),
+            ('1', 'Precisa Transmitir'),
+            ('2', 'Transmitida'),
+            ('3', 'Erro(s)'),
+            ('4', 'Sucesso'),
         ],
         string='Situação no e-Social',
-        related='sped_esocial_afastamento_id.situacao_esocial',
+        related='sped_esocial_afastamento_id.situacao_esocial_afastamento',
         readonly=True,
     )
+    situacao_esocial_encerramento = fields.Selection(
+        selection=[
+            ('1', 'Precisa Transmitir'),
+            ('2', 'Transmitida'),
+            ('3', 'Erro(s)'),
+            ('4', 'Sucesso'),
+        ],
+        string='Encerramento Afastamento e-Social',
+        related='sped_esocial_afastamento_id.'
+                'situacao_esocial_afastamento_encerramento',
+        readonly=True,
+    )
+    observacao = fields.Text(
+        string='Observação',
+        size=255,
+    )
+    afastamento_encerrado = fields.Boolean(
+        string='Afastamento Encerrado',
+        readonly=True,
+        compute='_compute_afastamento_encerrado'
+    )
+
+    @api.multi
+    def _compute_afastamento_encerrado(self):
+        pass
+
+    @api.multi
+    def encerrar_afastamento(self):
+        pass
+
+    @api.constrains('observacao')
+    def _check_observacao_obrigatoria(self):
+        """
+        Verificar se o tipo de afastamento do e-Social exige uma observação
+        """
+        afastamento_codigo = self.holiday_status_id.esocial_evento_afastamento_id
+        if afastamento_codigo:
+            if afastamento_codigo.codigo == '21' and not self.observacao:
+                raise ValidationError(
+                    "Para este tipo de afastamento é obrigatório detalhar o "
+                    "afastamento no campo 'observação'!"
+                )
 
     @api.model
     def create(self, vals):
         self.check_dias_afastamento(vals)
         res = super(HrHolidays, self).create(vals)
-        res._gerar_tabela_intermediaria()
+
+        return res
+
+    def holidays_first_validate(self):
+        res = super(HrHolidays, self).holidays_first_validate()
+        if res:
+            self._gerar_tabela_intermediaria()
 
         return res
 
@@ -39,10 +86,7 @@ class HrHolidays(models.Model):
         holidays_status_id = self.env['hr.holidays.status'].browse(
             vals['holiday_status_id'])
         if holidays_status_id.esocial_evento_afastamento_id:
-            if holidays_status_id.esocial_evento_afastamento_id.codigo not in [
-                    '15', '17', '18', '19', '20', '33']:
-                self.valida_dias_afastamento(vals)
-            else:
+            if holidays_status_id.esocial_evento_afastamento_id.codigo == '15':
                 self.valida_dias_inicio_ferias(vals)
 
     def valida_dias_inicio_ferias(self, vals):
@@ -54,20 +98,20 @@ class HrHolidays(models.Model):
                 "maior que 60 dias da data atual!"
             )
 
-    def valida_dias_afastamento(self, vals):
-        data_atual = fields.Datetime.from_string(fields.Datetime.now())
-        data_inicio = fields.Datetime.from_string(vals['data_inicio'])
-        data_fim = fields.Datetime.from_string(vals['data_fim'])
-        if (data_inicio - data_atual).days > 0:
-            raise ValidationError(
-                "Este evento de afastamento não "
-                "pode ter seu início no futuro!"
-            )
-        if (data_fim - data_inicio).days > 15:
-            raise ValidationError(
-                "Este evento de afastamento não "
-                "pode ser maior do que 15 dias!"
-            )
+    # def valida_dias_afastamento(self, vals):
+    #     data_atual = fields.Datetime.from_string(fields.Datetime.now())
+    #     data_inicio = fields.Datetime.from_string(vals['data_inicio'])
+    #     data_fim = fields.Datetime.from_string(vals['data_fim'])
+    #     if (data_inicio - data_atual).days > 0:
+    #         raise ValidationError(
+    #             "Este evento de afastamento não "
+    #             "pode ter seu início no futuro!"
+    #         )
+    #     if (data_fim - data_inicio).days > 15:
+    #         raise ValidationError(
+    #             "Este evento de afastamento não "
+    #             "pode ser maior do que 15 dias!"
+    #         )
 
     @api.multi
     def _gerar_tabela_intermediaria(self):
