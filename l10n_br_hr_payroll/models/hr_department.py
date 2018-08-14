dc5d8fe069870314cfed9f6c3c437fa89cb4aa38
Hendrix Costa <hendrixcosta@gmail.com>
2018-08-14 15:18:33 -0300

    [FIX] PEP8

--- l10n_br_hr_payroll/models/hr_department.py
+++ l10n_br_hr_payroll/models/hr_department.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018  Luiz Felipe do Divino - KMEE Informática
+# Copyright (C) 2018  Luiz Felipe do Divino - ABGF
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, models, fields
