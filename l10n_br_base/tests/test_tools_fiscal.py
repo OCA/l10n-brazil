d1e463d10a9b4a0b39dc9532ad5bbb3910227466
Magno <magno.costa@akretion.com.br>
2018-08-15 13:21:02 -0300

    [FIX] PEP8 - imports.

--- l10n_br_base/tests/test_tools_fiscal.py
+++ l10n_br_base/tests/test_tools_fiscal.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from openerp.addons.l10n_br_base.tools.fiscal import validate_pis_pasep
+from ..tools.fiscal import validate_pis_pasep
 from openerp.tests.common import TransactionCase
 
 
