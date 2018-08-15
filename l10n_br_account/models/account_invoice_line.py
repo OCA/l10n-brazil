a045b32cf3cb593c1110186bc16fff855c84fb6a
Magno <magno.costa@akretion.com.br>
2018-08-15 16:33:02 -0300

    Replace api.one to api.multi .

--- l10n_br_account/models/account_invoice_line.py
+++ l10n_br_account/models/account_invoice_line.py
@@ -9,36 +9,40 @@ from odoo.addons import decimal_precision as dp
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
-    @api.one
+    @api.multi
     @api.depends('price_unit', 'discount', 'invoice_line_tax_ids',
                  'quantity', 'product_id', 'invoice_id.partner_id',
                  'invoice_id.currency_id', 'invoice_id.company_id')
     def _compute_price(self):
-        currency = self.invoice_id and self.invoice_id.currency_id or None
-        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
-        taxes = False
-        amount_tax_discount = 0.0
-        if self.invoice_line_tax_ids:
-            taxes = self.invoice_line_tax_ids.compute_all(
-                price, currency,
-                self.quantity,
-                product=self.product_id,
-                partner=self.invoice_id.partner_id)
+        for record in self:
+            currency = (
+                record.invoice_id and record.invoice_id.currency_id or None)
+            price = record.price_unit * (1 - (record.discount or 0.0) / 100.0)
+            taxes = False
+            amount_tax_discount = 0.0
+            if record.invoice_line_tax_ids:
+                taxes = record.invoice_line_tax_ids.compute_all(
+                    price, currency,
+                    record.quantity,
+                    product=record.product_id,
+                    partner=record.invoice_id.partner_id)
 
-            amount_tax_discount = taxes['total_tax_discount']
+                amount_tax_discount = taxes['total_tax_discount']
 
-        self.price_subtotal = price_subtotal_signed = \
-            taxes['total_excluded'] if taxes else self.quantity * price
+            record.price_subtotal = price_subtotal_signed = \
+                taxes['total_excluded'] if taxes else record.quantity * price
 
-        self.amount_tax_discount = amount_tax_discount
+            record.amount_tax_discount = amount_tax_discount
 
-        if (self.invoice_id.currency_id and self.invoice_id.company_id and
-                self.invoice_id.currency_id !=
-                self.invoice_id.company_id.currency_id):
-            price_subtotal_signed = self.invoice_id.currency_id.compute(
-                price_subtotal_signed, self.invoice_id.company_id.currency_id)
-        sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
-        self.price_subtotal_signed = price_subtotal_signed * sign
+            if (record.invoice_id.currency_id and record.invoice_id.company_id
+                and record.invoice_id.currency_id !=
+                    record.invoice_id.company_id.currency_id):
+                price_subtotal_signed = record.invoice_id.currency_id.compute(
+                    price_subtotal_signed,
+                    record.invoice_id.company_id.currency_id)
+            sign = record.invoice_id.type in [
+                'in_refund', 'out_refund'] and -1 or 1
+            record.price_subtotal_signed = price_subtotal_signed * sign
 
     fiscal_category_id = fields.Many2one(
         codmodel_name='l10n_br_account.fiscal.category',
