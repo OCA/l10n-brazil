c68ad32ea046c780ec514a9eb3b0967a28456a76
Magno <magno.costa@akretion.com.br>
2018-08-16 13:56:15 -0300

    [IMP] Tests.

--- l10n_br_account/tests/test_account_customer_invoice.py
+++ l10n_br_account/tests/test_account_customer_invoice.py
@@ -67,10 +67,75 @@ class TestCustomerInvoice(TransactionCase):
                     [('user_type_id', '=', self.env.ref(
                         'account.data_account_type_revenue').id)], limit=1).id,
                 'name': 'product test 5',
-                'uom_id': self.env.ref('product.product_uom_unit').id
+                'uom_id': self.env.ref('product.product_uom_unit').id,
+                'fiscal_category_id': self.fiscal_category.id,
+            })]
+        ))
+        # Invoice with TAXES
+        tax_fixed = self.env['account.tax'].create({
+            'sequence': 10,
+            'name': 'Tax 10.0 (Fixed)',
+            'amount': 10.0,
+            'amount_type': 'fixed',
+            'include_base_amount': True,
+        })
+        tax_percent_included_base_incl = self.env['account.tax'].create({
+            'sequence': 20,
+            'name': 'Tax 50.0% (Percentage of Price Tax Included)',
+            'amount': 50.0,
+            'amount_type': 'division',
+            'include_base_amount': True,
+        })
+        tax_percentage = self.env['account.tax'].create({
+            'sequence': 30,
+            'name': 'Tax 20.0% (Percentage of Price)',
+            'amount': 20.0,
+            'amount_type': 'percent',
+            'include_base_amount': False,
+        })
+        self.invoice_2 = self.env['account.invoice'].create(dict(
+            name='Test Customer Invoice',
+            payment_term_id=self.env.ref(
+                'account.account_payment_term_advance').id,
+            fiscal_category_id=self.fiscal_category.id,
+            partner_id=self.env.ref('base.res_partner_3').id,
+            reference_type="none",
+            invoice_line_ids=[(0, 0, {
+                'product_id': self.env.ref('product.product_product_5').id,
+                'quantity': 5.0,
+                'price_unit': 100.0,
+                'account_id': self.env['account.account'].search(
+                    [('user_type_id', '=', self.env.ref(
+                        'account.data_account_type_revenue').id)], limit=1).id,
+                'name': 'product test 5',
+                'uom_id': self.env.ref('product.product_uom_unit').id,
+                'fiscal_category_id': self.fiscal_category.id,
+                'invoice_line_tax_ids': [(6, 0, [
+                    tax_fixed.id,
+                    tax_percent_included_base_incl.id,
+                    tax_percentage.id
+                ])],
             })]
         ))
 
     def test_state(self):
         self.assertEquals(self.invoice_1.state, 'draft',
                           "Invoice should be in state Draft")
+        self.invoice_1.action_invoice_open()
+        self.assertEquals(self.invoice_1.state, 'open',
+                          "Invoice should be in state Open")
+
+    def test_tax(self):
+        self.invoice_2.action_invoice_open()
+        self.assertEquals(self.invoice_2.state, 'open',
+                          "Invoice should be in state Open")
+        invoice_tax = self.invoice_2.tax_line_ids.sorted(
+            key=lambda r: r.sequence)
+        self.assertEquals(invoice_tax.mapped('amount'), [50.0, 550.0, 220.0])
+        self.assertEquals(invoice_tax.mapped('base'), [500.0, 550.0, 1100.0])
+        assert self.invoice_2.move_id, "Move not created for open invoice"
+        self.invoice_2.pay_and_reconcile(
+            self.env['account.journal'].search(
+                [('type', '=', 'bank')], limit=1), 10050.0)
+        self.assertEquals(self.invoice_2.state, 'paid',
+                          "Invoice should be in state Paid")
