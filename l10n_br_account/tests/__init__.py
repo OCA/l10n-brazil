24f8da9c3a8605382294dbcdd9e5c0b2279cd98d
Magno Costa <magno.costa@akretion.com.br>
2018-08-16 11:11:17 -0300

    Change tests from YAML to Python.

---
+++ l10n_br_account/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @ 2018 Akretion - www.akretion.com.br -
+#   Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_account_customer_invoice
+from . import test_account_supplier_invoice
+from . import test_generate_fiscal_rules
