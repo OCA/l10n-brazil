1d6cae88e1d24499b3efd1f42b0bad9d2aaeef47
Renato Lima <renato.lima@akretion.com>
2018-08-18 18:24:02 -0300

    [MIGR] res.bank objects

--- l10n_br_base/models/res_bank.py
+++ l10n_br_base/models/res_bank.py
@@ -12,17 +12,28 @@ class ResBank(models.Model):
 
     _inherit = 'res.bank'
 
-    district = fields.Char(u'Bairro', size=32)
-    number = fields.Char(u'Número', size=10)
+    district = fields.Char(
+        string=u'Bairro',
+        size=32)
 
-    country_id = fields.Many2one('res.country', related='country',
-                                 string=u'País')
+    number = fields.Char(
+        string=u'Número',
+        size=10)
 
-    state_id = fields.Many2one('res.country.state', related='state',
-                               string=u'Estado')
+    country_id = fields.Many2one(
+        comodel_name='res.country',
+        related='country',
+        string=u'País')
 
-    l10n_br_city_id = fields.Many2one('l10n_br_base.city', u'Municipio',
-                                      domain="[('state_id','=',state_id)]")
+    state_id = fields.Many2one(
+        comodel_name='res.country.state',
+        string=u'Estado',
+        related='state')
+
+    l10n_br_city_id = fields.Many2one(
+        comodel_name='l10n_br_base.city',
+        string=u'Municipio',
+        domain="[('state_id','=',state_id)]")
 
     @api.onchange('l10n_br_city_id')
     def _onchange_l10n_br_city_id(self):
