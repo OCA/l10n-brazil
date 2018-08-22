b3bb301618b8b70b00c46210013399c17da3ddb7
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-22 08:25:48 -0300

    [MIG] odoo imports

--- l10n_br_hr/model/hr_job.py
+++ l10n_br_hr/model/hr_job.py
@@ -2,7 +2,7 @@
 # (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class HrJob(models.Model):
