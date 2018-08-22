0c81e65515a2a426eb8ac77016b7a43359eb5786
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-08-22 17:41:04 -0300

    [ADD] Parcelamento

--- l10n_br_tef/l10n_br_tef.py
+++ l10n_br_tef/l10n_br_tef.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, _
 
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     iface_tef = fields.Boolean(
-        'TEF',
-        help="A TEF terminal is available on the Proxy")
+        string="TEF",
+        help=_("A TEF terminal is available on the Proxy"),
+    )
+
+    institution_selection = fields.Selection(
+        selection=[
+            ('Administradora', 'Administradora'),
+            ('Estabelecimento', 'Estabelecimento')
+        ],
+        string="Institution",
+        help=_("Institution selection for installment payments"),
+        default='Estabelecimento',
+    )
