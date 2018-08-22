62ed876e6fba3e5e50026c1e5f07c97ae9cd41bd
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-22 08:25:48 -0300

    [MIG] odoo imports

--- l10n_br_hr/model/l10n_br_hr.py
+++ l10n_br_hr/model/l10n_br_hr.py
@@ -2,7 +2,7 @@
 # (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class L10nBrHrCbo(models.Model):
