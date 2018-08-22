cfcc1451c56305782f864c7fef5598f9c74473ed
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-22 08:25:48 -0300

    [MIG] odoo imports

--- l10n_br_hr/model/res_company.py
+++ l10n_br_hr/model/res_company.py
@@ -2,7 +2,7 @@
 # (c) 2014 Kmee - Rafael da Silva Lima <rafael.lima@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
