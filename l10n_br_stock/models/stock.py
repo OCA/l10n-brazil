c3d905053f9422face9d0a2e8514ac8954228e2d
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-21 23:28:51 -0300

    [MIG][l10n_br_stock] Rename openerp with odoo
    find . -type f -name '*.py' | xargs sed -i 's/from openerp/from odoo/g'
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_stock/models/stock.py
+++ l10n_br_stock/models/stock.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2014  Luis Felipe Miléo - KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class StockIncoterms(models.Model):
