888882b2fa2e79375e085da7dfae7cb1167c4be0
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-22 21:59:44 -0300

    [MIG] odoo imports

--- l10n_br_sale/wizard/sale_make_invoice_advanced_percentage.py
+++ l10n_br_sale/wizard/sale_make_invoice_advanced_percentage.py
@@ -3,7 +3,7 @@
 #    Copyright (C) 2014 - Michell Stuttgart Faria (<http://www.kmee.com.br>).
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models
+from odoo import models
 
 
 class SaleAdvancePaymentInvoice(models.TransientModel):
