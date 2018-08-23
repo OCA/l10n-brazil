60d5cd8086ec32f8cc3a37e28de100f841f40be3
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-22 21:59:44 -0300

    [MIG] odoo imports

--- l10n_br_sale/models/sale.py
+++ l10n_br_sale/models/sale.py
@@ -3,8 +3,8 @@
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
-from openerp.addons import decimal_precision as dp
+from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
 
 
 class SaleOrder(models.Model):
