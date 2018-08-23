d8d898f0729eb84978c9bafe4fb100f357b38278
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-22 21:59:44 -0300

    [MIG] Bump module version to 10.0.1.0.0

--- l10n_br_sale/__manifest__.py
+++ l10n_br_sale/__manifest__.py
@@ -8,7 +8,7 @@
     'license': 'AGPL-3',
     'author': 'Akretion, Odoo Community Association (OCA)',
     'website': 'http://odoo-brasil.org',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
         'l10n_br_account',
         'account_fiscal_position_rule_sale',
