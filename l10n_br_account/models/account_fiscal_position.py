576772ce5222d7df4eca81fa347636ac55d7d19b
Renato Lima <renato.lima@akretion.com>
2018-08-27 13:39:42 -0300

    comment method

--- l10n_br_account/models/account_fiscal_position.py
+++ l10n_br_account/models/account_fiscal_position.py
@@ -15,29 +15,30 @@ class AccountFiscalPosition(AccountFiscalPositionAbstract,
 
     _inherit = 'account.fiscal.position'
 
-    @api.model
-    def map_tax(self, taxes, product=None, partner=None):
-        result = self.env['account.tax'].browse()
-
-        if self.company_id and \
-                self.env.context.get('type_tax_use') in ('sale', 'all'):
-            if self.env.context.get('fiscal_type', 'product') == 'service':
-                company_taxes = self.company_id.service_tax_ids
-
-            if taxes:
-                taxes |= company_taxes
-
-        for tax in taxes:
-            tax_count = 0
-            for t in self.tax_ids:
-                if (t.tax_src_id == tax or
-                        t.tax_group_id == tax.tax_group_id):
-                    tax_count += 1
-                    if t.tax_dest_id:
-                        result |= t.tax_dest_id
-            if not tax_count:
-                result |= tax
-        return result
+    # TODO l10n_br_account_product's 
+#    @api.model
+#    def map_tax(self, taxes, product=None, partner=None):
+#        result = self.env['account.tax'].browse()
+#
+#        if self.company_id and \
+#                self.env.context.get('type_tax_use') in ('sale', 'all'):
+#            if self.env.context.get('fiscal_type', 'product') == 'service':
+#                company_taxes = self.company_id.service_tax_ids
+#
+#            if taxes:
+#                taxes |= company_taxes
+#
+#        for tax in taxes:
+#            tax_count = 0
+#            for t in self.tax_ids:
+#                if (t.tax_src_id == tax or
+#                        t.tax_group_id == tax.tax_group_id):
+#                    tax_count += 1
+#                    if t.tax_dest_id:
+#                        result |= t.tax_dest_id
+#            if not tax_count:
+#                result |= tax
+#        return result
 
 
 class AccountFiscalPositionTax(AccountFiscalPositionTaxAbstract,
