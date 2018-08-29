846deee2fa98bd53a2c5d82ba36f3a0dfdcd8230
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-29 09:33:04 -0300

    [MIG] odoo imports

--- l10n_br_sale_product/models/sale.py
+++ l10n_br_sale_product/models/sale.py
@@ -2,12 +2,12 @@
 # Copyright (C) 2014  Renato Lima - Akretion                                  #
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
-from openerp import _
-from openerp.exceptions import ValidationError
+from odoo import models, fields, api
+from odoo import _
+from odoo.exceptions import ValidationError
 
-from openerp.addons import decimal_precision as dp
-from openerp.addons.l10n_br_base.tools.misc import calc_price_ratio
+from odoo.addons import decimal_precision as dp
+from odoo.addons.l10n_br_base.tools.misc import calc_price_ratio
 
 
 class SaleOrder(models.Model):
