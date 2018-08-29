b1ee3ed08766ff20a56ed4e526628dc6eaa76776
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2018-08-29 14:51:48 -0300

    [REF]Alter fields name to match inherited model

--- l10n_br_account_banking_payment/models/account_payment.py
+++ l10n_br_account_banking_payment/models/account_payment.py
@@ -13,11 +13,14 @@ class PaymentOrder(models.Model):
     _inherit = 'account.payment.order'
 
     # TODO: Implementar total de juros e outras despesas acessórias.
-    @api.depends('line_ids', 'line_ids.amount')
+    @api.depends('payment_line_ids',
+                 'payment_line_ids.amount_company_currency')
     @api.multi
     def _compute_total(self):
         for record in self:
-            record.total = sum(record.mapped('line_ids.amount') or [0.0])
+            record.total_company_currency = sum(
+                record.mapped('payment_line_ids.amount_company_currency') or
+                [0.0])
 
     @api.multi
     def action_open(self):
@@ -25,7 +28,7 @@ class PaymentOrder(models.Model):
         Validacao para nao confirmar ordem de pagamento vazia
         """
         for record in self:
-            if not record.line_ids:
+            if not record.payment_line_ids:
                 raise ValidationError(_("Impossible confirm empty line!"))
         return super(PaymentOrder, self).action_open()
 
