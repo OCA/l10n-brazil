da71eb2b2dc44cb02d0e03cf7a24b4fe55ba5aa1
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-29 16:21:30 -0300

    [REM] Kill l10n_br_account_payment
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account_payment/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2011  Renato Lima - Akretion                                  #
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-import account_invoice
-import l10n_br_account_payment
-import wizard
