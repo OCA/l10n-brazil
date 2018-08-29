16d19d761e481a679dcdc8eee97a89e4efc15ec1
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-29 09:37:14 -0300

    [MIG] odoo imports

--- l10n_br_stock_account/wizard/stock_invoice_onshipping.py
+++ l10n_br_stock_account/wizard/stock_invoice_onshipping.py
@@ -4,8 +4,8 @@
 
 import ast
 
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning as UserError
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning as UserError
 
 
 class StockInvoiceOnShipping(models.TransientModel):
