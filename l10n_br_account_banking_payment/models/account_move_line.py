029470887c04538f4d116ce1d5c908c5817779c1
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2018-08-29 14:13:08 -0300

    [MIG] odoo imports

--- l10n_br_account_banking_payment/models/account_move_line.py
+++ l10n_br_account_banking_payment/models/account_move_line.py
@@ -3,7 +3,7 @@
 #  Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class AccountMoveLine(models.Model):
--- l10n_br_account_banking_payment/wizard/payment_order_create.py
+++ l10n_br_account_banking_payment/wizard/payment_order_create.py
@@ -3,7 +3,7 @@
 #  Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
+from odoo import models, fields, api, _
 
 
 class PaymentOrderCreate(models.TransientModel):
