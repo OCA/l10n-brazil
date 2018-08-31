5d903d4328a107997986eab8d8526f5c3b19ab59
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-31 17:19:02 -0300

    [MIG] Bump module version to 10.0.1.0.0

--- l10n_br_pos/models/account_journal.py
+++ l10n_br_pos/models/account_journal.py
@@ -2,7 +2,7 @@
 # © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br) - Fernando Marcato
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 WA03_CMP_MP = [
     ('01', u'Dinheiro'),
