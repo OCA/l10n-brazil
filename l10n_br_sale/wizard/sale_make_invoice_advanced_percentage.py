fc037fcbefa750f906c9ea11f2064106734316ac
the-bmb <bruna_braga@unifei.edu.br>
2018-09-06 16:49:11 -0300

    [MIG] l10n_br_sale

--- l10n_br_sale/wizard/sale_make_invoice_advanced_percentage.py
+++ l10n_br_sale/wizard/sale_make_invoice_advanced_percentage.py
@@ -3,41 +3,43 @@
 #    Copyright (C) 2014 - Michell Stuttgart Faria (<http://www.kmee.com.br>).
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import models, api
 
 
 class SaleAdvancePaymentInvoice(models.TransientModel):
     _inherit = 'sale.advance.payment.inv'
 
-    def _get_line_qty(self, cr, uid, line, context=None):
+    @api.one
+    def _get_line_qty(self, line):
         if line.order_id.invoice_quantity == 'order':
             if line.product_uos:
                 return line.product_uos_qty or 0.0
         return line.product_uom_qty
 
-    def _get_line_uom(self, cr, uid, line, context=None):
+    @api.one
+    def _get_line_uom(self, line):
         if line.order_id.invoice_quantity == 'order':
             if line.product_uos:
                 return line.product_uos.id
         return line.product_uom.id
 
-    def _prepare_advance_invoice_vals(self, cr, uid, ids, context=None):
+    @api.one
+    def _prepare_advance_invoice_vals(self):
 
         # Lista de campos
         result = super(
-            SaleAdvancePaymentInvoice, self)._prepare_advance_invoice_vals(
-                cr, uid, ids, context)
+            SaleAdvancePaymentInvoice, self)._prepare_advance_invoice_vals()
 
-        if context is None:
+        if self._context is None:
             context = {}
 
         sale_obj = self.pool['sale.order']
-        wizard = self.browse(cr, uid, ids[0], context)
+        wizard = self.browse(self._ids[0])
         # get invoice type
         payment_type = wizard.advance_payment_method
 
         for res in result:
-            for sale in sale_obj.browse(cr, uid, [res[0]], context=context):
+            for sale in sale_obj.browse([res[0]]):
 
                 # Verificamos o tipo de fatura
                 if payment_type == 'percentage':
@@ -59,18 +61,16 @@ class SaleAdvancePaymentInvoice(models.TransientModel):
                     res_aux = {}
                     res_aux = (order_line_obj.
                                l10n_br_sale_prepare_order_line_invoice_line(
-                                   cr, uid, order_line, res_aux, context))
+                        order_line, res_aux))
 
                     res_aux = order_line_obj. \
                         l10n_br_sale_product_prepare_order_line_invoice_line(
-                            cr, uid, order_line, res_aux, context)
+                        order_line, res_aux)
 
                     tax_list_id = []
 
-                    uosqty = self._get_line_qty(
-                        cr, uid, order_line, context=context)
-                    uos_id = self._get_line_uom(
-                        cr, uid, order_line, context=context)
+                    uosqty = self._get_line_qty(order_line)
+                    uos_id = self._get_line_uom(order_line)
 
                     # Add the ID of tax of order line
                     for tax in order_line.tax_id:
