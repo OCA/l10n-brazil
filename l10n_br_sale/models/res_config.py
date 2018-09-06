3c8ff5eb774ae4d04c8c00e62f9a10ed904e4986
the-bmb <bruna_braga@unifei.edu.br>
2018-09-06 17:08:12 -0300

    [FIX] pep8

--- l10n_br_sale/models/res_config.py
+++ l10n_br_sale/models/res_config.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2014  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID
 from odoo import models, fields, api
 
 
