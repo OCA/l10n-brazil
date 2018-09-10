4ac99ba3eaedb2ef9ee3af39f3dbb7ca03af4067
Renato Lima <renato.lima@akretion.com.br>
2018-09-10 18:06:59 -0300

    added new file to constants

---
+++ l10n_br_account_product/models/l10n_br_account_product.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+PRODUCT_FISCAL_TYPE = [
+    ('product', u'Produto'),
+]
+
+PRODUCT_FISCAL_TYPE_DEFAULT = PRODUCT_FISCAL_TYPE[0][0]
+
+NFE_IND_IE_DEST = [
+    ('1', u'1 - Contribuinte do ICMS'),
+    ('2', u'2 - Contribuinte Isento do ICMS'),
+    ('9', u'9 - Não Contribuinte')
+]
+
+NFE_IND_IE_DEST_DEFAULT = NFE_IND_IE_DEST[0][0]
