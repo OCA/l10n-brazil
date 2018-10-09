282164d9458cc8481d831a1527c1a7535b4951b1
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-10-09 09:25:58 -0300

    [FIX] Função para retificar o registro S-2200

--- sped_esocial/models/intermediarios/s2200_cadastramento_inicial_vinculo_admissao_trabalhador.py
+++ sped_esocial/models/intermediarios/s2200_cadastramento_inicial_vinculo_admissao_trabalhador.py
@@ -137,7 +137,12 @@ class SpedEsocialHrContrato(models.Model, SpedRegistroIntermediario):
         # Se for um registro de Retificação manda o indRetif e o recido do S2200 inicial
         if operacao == 'R':  # Retificação
             S2200.evento.ideEvento.indRetif.valor = '2'
-            S2200.evento.ideEvento.nrRecibo.valor = self.sped_s2200_registro_inclusao.recibo
+
+            registro_para_retificar = self.get_registro_para_retificar(
+                self.sped_s2200_registro_inclusao)
+
+            S2200.evento.ideEvento.nrRecibo.valor = \
+                registro_para_retificar.recibo
 
         # Popula ideEmpregador (Dados do Empregador)
         S2200.evento.ideEmpregador.tpInsc.valor = '1'
@@ -487,6 +492,26 @@ class SpedEsocialHrContrato(models.Model, SpedRegistroIntermediario):
             S2200.evento.vinculo.sucessaoVinc.append(SucessaoVinc)
 
         return S2200, validacao
+
+    def get_registro_para_retificar(self, sped_registro):
+        """
+        Identificar o registro para retificar
+        :return:
+        """
+        # Se tiver registro de retificação com erro ou nao possuir nenhuma
+        # retificação ainda, retornar o registro que veio no parametro
+        retificacao_com_erro = sped_registro.retificacao_ids.filtered(
+            lambda x: x.situacao in ['1', '3'])
+        if retificacao_com_erro or not sped_registro.retificacao_ids:
+            return sped_registro
+
+        # Do contrario navegar ate as retificacoes com sucesso e efetuar a
+        # verificacao de erro novamente
+        else:
+            registro_com_sucesso = sped_registro.retificacao_ids.filtered(
+                lambda x: x.situacao not in ['1', '3'])
+
+            return self.get_registro_para_retificar(registro_com_sucesso[0])
 
     @api.multi
     def retorno_sucesso(self, evento):
