1f8c827f6c7409cdb24251f204150ad69497a2c0
Renato Lima <renato.lima@akretion.com.br>
2018-10-10 02:12:52 -0300

    [REF] added account.tax.group objects, data and fields

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -17,7 +17,9 @@ from . import res_partner
 from . import res_company
 from . import res_config
 from . import account_journal
+from . import account_tax_group
 from . import account_tax
+from . import account_tax_template
 from . import account_invoice
 from . import account_invoice_line
 from . import product_template
