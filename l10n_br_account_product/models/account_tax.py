ab54b7a501e86fa2dd5f0c15350c6c63cd2e046f
Renato Lima <renato.lima@akretion.com.br>
2018-10-10 02:22:15 -0300

    [REF] account.tax.group

--- l10n_br_account_product/models/account_tax.py
+++ l10n_br_account_product/models/account_tax.py
@@ -19,8 +19,8 @@ class AccountTax(models.Model):
                    ('3', u'Valor da Operação')],
         string=u'Tipo Base ICMS',
         required=True,
-        default='0'
-    )
+        default='0')
+
     icms_st_base_type = fields.Selection(
         selection=[('0', u'Preço tabelado ou máximo  sugerido'),
                    ('1', u'Lista Negativa (valor)'),
@@ -30,8 +30,7 @@ class AccountTax(models.Model):
                    ('5', 'Pauta (valor)')],
         string=u'Tipo Base ICMS ST',
         required=True,
-        default='4'
-    )
+        default='4')
 
     # @api.model
     # def _compute_tax(self, cr, uid, taxes, total_line, product, product_qty,
@@ -69,9 +68,6 @@ class AccountTax(models.Model):
 
     #    result['taxes'] = taxes
     #    return result
-
-
-
 
     @api.multi
     def compute_all(self, price_unit, currency=None, quantity=1.0,
