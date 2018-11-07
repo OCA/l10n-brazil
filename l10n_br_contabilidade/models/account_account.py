af55fb180e9b3bc2d2a494b1c6f1997afce6a38a
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-11-07 10:56:44 -0200

    [FIX] Nova visão de conta contábil

--- l10n_br_contabilidade/models/account_account.py
+++ l10n_br_contabilidade/models/account_account.py
@@ -7,6 +7,11 @@ from openerp import api, fields, models
 class Accountaccount(models.Model):
     _inherit = 'account.account'
 
+    natureza_conta_id = fields.Many2one(
+        string='Natureza da Conta',
+        comodel_name='account.natureza',
+    )
+
     @api.multi
     def verificar_contas(self):
         """
