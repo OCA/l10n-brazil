48e6e49d5e5840a23b12ceda7f14a459fee384ba
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-11-08 11:07:17 -0200

    [ADD] Campo sequência no lançamento contábil

--- l10n_br_contabilidade/models/account_move.py
+++ l10n_br_contabilidade/models/account_move.py
@@ -4,15 +4,39 @@
 
 from openerp import api, fields, models
 
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    natureza_conta_id = fields.Many2one(
-        string='Natureza da Conta',
-        comodel_name='account.natureza',
-    )
+    @api.multi
+    def _get_default_sequence(self):
+        ultimo_lancamento = self._get_last_sequence()
+        if ultimo_lancamento.sequencia == 1:
+            return ultimo_lancamento.sequencia
+
+        proximo_numero = ultimo_lancamento.sequencia + 1
+
+        return proximo_numero
 
     centro_custo_id = fields.Many2one(
         string='Centro de Custo',
         comodel_name='account.centro.custo',
     )
+
+    sequencia = fields.Integer(
+        string='Sequência',
+        default=_get_default_sequence,
+        unique=True,
+    )
+
+    def _get_last_sequence(self):
+        return self.search(
+            [('sequencia', '!=', False)], order='sequencia DESC', limit=1
+        )
+
+    @api.model
+    def create(self, vals):
+        if self._get_last_sequence().sequencia == vals['sequencia']:
+            vals['sequencia'] = self._get_default_sequence()
+
+        return super(AccountMove, self).create(vals)
