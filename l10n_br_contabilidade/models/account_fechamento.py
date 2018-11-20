c371ab9e36d7d1266565d5f8b3dbf9728299bde2
Luciano Veras <luciano.veras@abgf.gov.br>
2018-11-20 18:07:11 -0200

    [ADD] Apuração do Resultado do Exercício

--- l10n_br_contabilidade/models/account_fechamento.py
+++ l10n_br_contabilidade/models/account_fechamento.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
-
+import pandas as pd
 
 class AccountFechamento(models.Model):
     _name = 'account.fechamento'
@@ -82,3 +82,76 @@ class AccountFechamento(models.Model):
             # Relacionar os lancamentos ao fechamento atual
             for account_move_id in account_move_ids:
                 account_move_id.account_fechamento_id = record.id
+
+    @api.multi
+    def button_fechar_periodos(self):
+        def create_move():
+
+            return self.env['account.move'].create({
+                'journal_id':1,
+                'period_id':periodo,
+                'date':data_lancamento,
+            }).id
+
+        def create_line(account_id, debito, credito, move_id):
+            self.env['account.move.line'].create({
+                'account_id':account_id,
+                'credit':credito,
+                'debit':debito,
+                'move_id':move_id,
+            })
+
+        df_are = pd.DataFrame(
+            columns=['conta', 'debito', 'credito', 'periodo', 'dt_stop'])
+
+        for move in self.account_move_ids:
+            for line in move.line_id:
+                if line.account_id.user_type.name == 'Resultado':
+                    df_are.loc[line.id] = \
+                        [line.account_id.id, line.debit, line.credit,
+                         move.period_id.id, move.period_id.date_stop]
+
+        data_lancamento = df_are['dt_stop'].max()
+        periodo = df_are[
+            data_lancamento == df_are['dt_stop']].head(1)['periodo']
+
+        df_agrupado = df_are.groupby('conta').sum()
+        df_agrupado['result'] = (df_agrupado['debito'] - df_agrupado['credito'])
+
+        are_debito = self.account_journal_id.default_debit_account_id.id
+        are_credito = self.account_journal_id.default_credit_account_id.id
+
+        for conta in df_agrupado.index:
+            series_conta = df_agrupado.loc[conta]
+            if series_conta['result'] != 0.0:
+                move_id = create_move()
+
+                if series_conta['result'] > 0.0:
+                    create_line(
+                        account_id=conta,
+                        debito=0.0,
+                        credito=series_conta['result'],
+                        move_id=move_id,
+                    )
+
+                    create_line(
+                        account_id=are_debito,
+                        debito=series_conta['result'],
+                        credito=0.0,
+                        move_id=move_id,
+                    )
+
+                elif series_conta['result'] < 0.0:
+                    create_line(
+                        account_id=conta,
+                        debito=series_conta['result'],
+                        credito=0.0,
+                        move_id=move_id,
+                    )
+
+                    create_line(
+                        account_id=are_credito,
+                        debito=0.0,
+                        credito=series_conta['result'],
+                        move_id=move_id,
+                    )
