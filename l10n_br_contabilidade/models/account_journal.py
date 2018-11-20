84d60f68fe0761b3880d9e536d750f4e3e0774eb
Hendrix Costa <hendrix.costa@abgf.gov.br>
2018-11-20 17:06:30 -0200

    [ADD] Contas de fechamento no diario

--- l10n_br_contabilidade/models/account_journal.py
+++ l10n_br_contabilidade/models/account_journal.py
@@ -12,3 +12,13 @@ class AccountJournal(models.Model):
         string=u'Template Padrão do Lançamento',
         comodel_name='account.historico.padrao',
     )
+
+    fechamento_debit_account_id = fields.Many2one(
+        string=u'Conta de Débito para fechamento',
+        comodel_name='account.account',
+    )
+
+    fechamento_credit_account_id = fields.Many2one(
+        string=u'Conta de Crédito para fechamento',
+        comodel_name='account.account',
+    )
