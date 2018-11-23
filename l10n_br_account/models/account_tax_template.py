52d76c474a4252d92e8e8d787a504bccca43172b
Renato Lima <renato.lima@akretion.com>
2018-11-23 10:11:40 -0200

    replace invalid field's attribute

--- l10n_br_account/models/account_tax_template.py
+++ l10n_br_account/models/account_tax_template.py
@@ -12,13 +12,13 @@ class AccountTaxTemplate(models.Model):
 
     base_reduction = fields.Float(
         string='Redution', required=True,
-        digits_compute=dp.get_precision('Account'),
+        digits=dp.get_precision('Account'),
         help="Um percentual decimal em % entre 0-1.",
         default=0.00)
 
     amount_mva = fields.Float(
         string='MVA Percent', required=True,
-        digits_compute=dp.get_precision('Account'),
+        digits=dp.get_precision('Account'),
         help="Um percentual decimal em % entre 0-1.",
         default=0.00)
 
