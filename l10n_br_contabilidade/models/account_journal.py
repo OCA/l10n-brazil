4091921f7ca07c23356be28466cb767503371902
Luciano Veras <luciano.veras@abgf.gov.br>
2018-12-03 15:07:51 -0200

    [REF] Adicionado porcentagem por sequencia de lançamento.

--- l10n_br_contabilidade/models/account_journal.py
+++ l10n_br_contabilidade/models/account_journal.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
-
+from openerp.exceptions import Warning
 
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
@@ -23,11 +23,32 @@ class AccountJournal(models.Model):
         inverse_name='journal_id',
     )
 
+    @api.multi
+    @api.onchange('journal_account_ids')
+    def _verifica_porcentagem_fechamento(self):
+        for record in self:
+            # Verifica se o somatório das sequencias ultrapassam 100%
+            for r in set([ja.sequencia for ja in record.journal_account_ids]):
+                if sum(a.porcentagem for a in record.journal_account_ids.filtered(lambda x: x.sequencia == r)) > 100:
+                    raise Warning(u'Porcentagem ultrapassa 100%.')
+
+    @api.model
+    def write(self, vals):
+        res = super(AccountJournal, self).write(vals)
+
+        self._verifica_porcentagem_fechamento()
+
+        return res
+
 
 class AccountJournalAccount(models.Model):
     _name = 'account.journal.account'
     _description = 'Vincula Contas ao Fechamento para informar porcentagem'
     _order = 'account_id'
+    _sql_constraints = [
+        ('account_journal_ident_unique', 'unique(account_id, journal_id)',
+         'Contas para Fechamento duplicadas.'),
+    ]
 
     account_id = fields.Many2one(
         string=u'Conta',
@@ -39,15 +60,12 @@ class AccountJournalAccount(models.Model):
         comodel_name='account.journal',
     )
 
-    identificacao = fields.Selection(
-        string='Identificação de Fechamanto',
-        selection=[
-            ('', ''),
-            ('debito', 'Débito'),
-            ('credito', 'Crédito'),
-        ],
-    )
-
     porcentagem = fields.Float(
         string=u'Porcentagem'
     )
+
+    sequencia = fields.Integer(
+        string=u'Sequência',
+        default=1,
+    )
+
