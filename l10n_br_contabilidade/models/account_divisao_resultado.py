ddf93010cdf82eba9a3b11569c32773a5f5d675c
Luciano Veras <luciano.veras@abgf.gov.br>
2018-12-04 10:01:14 -0200

    [REF] Separado classe para aquivo único e permissões

---
+++ l10n_br_contabilidade/models/account_divisao_resultado.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ABGF
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class AccountRateioResultado(models.Model):
+    _name = 'account.divisao.resultado'
+    _description = 'Vincula Contas ao Fechamento para informar porcentagem'
+    _order = 'account_id'
+    _sql_constraints = [
+        ('account_journal_ident_unique', 'unique(account_id, journal_id)',
+         'Contas para Fechamento duplicadas.'),
+    ]
+
+    account_id = fields.Many2one(
+        string=u'Conta',
+        comodel_name='account.account',
+    )
+
+    journal_id = fields.Many2one(
+        string=u'Diário',
+        comodel_name='account.journal',
+    )
+
+    porcentagem = fields.Float(
+        string=u'Porcentagem'
+    )
+
+    sequencia = fields.Integer(
+        string=u'Sequência',
+        default=1,
+    )
+
