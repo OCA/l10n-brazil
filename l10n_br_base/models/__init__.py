afdf0f994c5537a56a2c3ab6321b9f5e02d96fb1
Renato Lima <renato.lima@akretion.com.br>
2018-12-23 01:12:51 -0200

    [MIGR] split res_country and res_country_state py files

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -3,8 +3,9 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import res_city
+from . import res_country_state
+from . import res_country
 from . import res_bank
 from . import res_partner
-from . import res_country
 from . import res_company
 from . import res_config_settings
