8762b0d660757ca6037450361e3885ff90612d3a
Renato Lima <renato.lima@akretion.com>
2018-12-23 17:31:07 -0200

    migrated objects

--- l10n_br_base/models/res_bank.py
+++ l10n_br_base/models/res_bank.py
@@ -3,52 +3,38 @@
 #    @author Michell Stuttgart <michellstut@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import re
-
 from odoo import models, fields, api
+
+from ..tools import misc
 
 
 class ResBank(models.Model):
-
     _inherit = 'res.bank'
 
     district = fields.Char(
-        string=u'Bairro',
+        string=u'District',
         size=32)
 
-    number = fields.Char(
-        string=u'Número',
+    street_number = fields.Char(
+        string=u'Number',
         size=10)
 
-    country_id = fields.Many2one(
-        comodel_name='res.country',
-        related='country',
-        string=u'País')
+    city_id = fields.Many2one(
+        comodel_name='res.city',
+        string=u'City',
+        domain="[('state_id', '=', state)]")
 
-    state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        string=u'Estado',
-        related='state')
-
-    l10n_br_city_id = fields.Many2one(
-        comodel_name='l10n_br_base.city',
-        string=u'Municipio',
-        domain="[('state_id','=',state_id)]")
-
-    @api.onchange('l10n_br_city_id')
-    def _onchange_l10n_br_city_id(self):
+    @api.onchange('city_id')
+    def _onchange_city_id(self):
         """ Ao alterar o campo l10n_br_city_id que é um campo relacional
         com o l10n_br_base.city que são os municípios do IBGE, copia o nome
         do município para o campo city que é o campo nativo do módulo base
         para manter a compatibilidade entre os demais módulos que usam o
         campo city.
         """
-        if self.l10n_br_city_id:
-            self.city = self.l10n_br_city_id.name
+        self.city = self.city_id.name
 
     @api.onchange('zip')
     def _onchange_zip(self):
-        if self.zip:
-            val = re.sub('[^0-9]', '', self.zip)
-            if len(val) == 8:
-                self.zip = "%s-%s" % (val[0:5], val[5:8])
+        self.zip = misc.format_zipcode(self.zip,
+                                       self.country_id.code)
