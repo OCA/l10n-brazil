a9dc7a0d5f3f4a57f42d2edbaaa142c3b06c7422
Renato Lima <renato.lima@akretion.com>
2018-12-23 01:04:42 -0200

    [MIGR] rename files py files

--- l10n_br_base/models/res_city.py
+++ l10n_br_base/models/res_city.py
@@ -5,24 +5,13 @@
 from odoo import models, fields
 
 
-class L10nBrBaseCity(models.Model):
+class City(models.Model):
     """ Este objeto persite todos os municípios relacionado a um estado.
     No Brasil é necesário em alguns documentos fiscais informar o código
     do IBGE dos município envolvidos na transação.
     """
-    _name = 'l10n_br_base.city'
-    _description = u'Municipio'
-
-    name = fields.Char(
-        string=u'Nome',
-        size=64,
-        required=True)
-
-    state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        string=u'Estado',
-        required=True)
+    _inherit = 'res.city'
 
     ibge_code = fields.Char(
-        string=u'Código IBGE',
+        string='IBGE Code',
         size=7)
