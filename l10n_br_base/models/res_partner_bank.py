0e9184958351a1337cd4bccf24b0fccfd166417d
Renato Lima <renato.lima@akretion.com>
2018-12-23 13:10:15 -0200

    [MIGR] split file per objects

---
+++ l10n_br_base/models/res_partner_bank.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 Gabriel C. Stabel
+# Copyright (C) 2009 Renato Lima (Akretion)
+# Copyright (C) 2012 Raphaël Valyi (Akretion)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields
+
+
+class ResPartnerBank(models.Model):
+    """ Adiciona campos necessários para o cadastramentos de contas
+    bancárias no Brasil."""
+    _inherit = 'res.partner.bank'
+
+    acc_number = fields.Char(
+        string='Account Number',
+        size=64,
+        required=False)
+
+    bank = fields.Many2one(
+        comodel_name='res.bank',
+        string='Bank',
+        required=False)
+
+    acc_number_dig = fields.Char(
+        string='Account Digit',
+        size=8)
+
+    bra_number = fields.Char(
+        string='Bank Branch',
+        size=8)
+
+    bra_number_dig = fields.Char(
+        string='Bank Branch Digit',
+        size=8)
