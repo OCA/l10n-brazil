581ee1d274d485b7e418d115a69f41a2cf08c9fe
Renato Lima <renato.lima@akretion.com.br>
2018-12-23 01:04:42 -0200

    [MIGR] rename files py files

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -2,9 +2,9 @@
 # Copyright (C) 2015  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import res_city
 from . import res_bank
 from . import res_partner
 from . import res_country
 from . import res_company
-from . import l10n_br_base
-from . import res_config
+from . import res_config_settings
