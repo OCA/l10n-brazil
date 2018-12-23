95d75e118894bfa03601412e794c9471d7647418
Renato Lima <renato.lima@akretion.com.br>
2018-12-23 13:10:15 -0200

    [MIGR] split file per objects

--- l10n_br_base/models/res_partner.py
+++ l10n_br_base/models/res_partner.py
@@ -11,7 +11,7 @@ from ..tools import fiscal
 from odoo.exceptions import ValidationError
 
 
-class ResPartner(models.Model):
+class Partner(models.Model):
     _inherit = 'res.partner'
 
     @api.multi
@@ -19,22 +19,20 @@ class ResPartner(models.Model):
         country_code = self.country_id.code or ''
         if self.country_id and country_code.upper() != 'BR':
             # this ensure other localizations could do what they want
-            return super(ResPartner, self)._display_address(
+            return super(Partner, self)._display_address(
                 without_company=False)
         else:
             address_format = (
                 self.country_id and self.country_id.address_format or
-                "%(street)s\n%(street2)s\n%(city)s"
+                "%(street)s, %(street_number)s\n%(street2)s\n%(city)s"
                 " %(state_code)s%(zip)s\n%(country_name)s")
             args = {
                 'state_code': self.state_id and self.state_id.code or '',
                 'state_name': self.state_id and self.state_id.name or '',
                 'country_code': self.country_id and self.country_id.code or '',
                 'country_name': self.country_id and self.country_id.name or '',
-                'company_name': self.parent_id and self.parent_id.name or '',
-                'l10n_br_city_name': self.l10n_br_city_id and
-                self.l10n_br_city_id.name or '',
-            }
+                'company_name': self.parent_id and self.parent_id.name or ''}
+
             address_field = ['title', 'street', 'street2', 'zip',
                              'city', 'number', 'district']
             for field in address_field:
@@ -45,31 +43,42 @@ class ResPartner(models.Model):
                 address_format = '%(company_name)s\n' + address_format
             return address_format % args
 
-    cnpj_cpf = fields.Char('CNPJ/CPF', size=18)
+    cnpj_cpf = fields.Char(
+        string='CNPJ/CPF',
+        size=18)
 
-    inscr_est = fields.Char('Inscr. Estadual/RG', size=16)
-    other_inscr_est_lines = fields.One2many(
-        'other.inscricoes.estaduais', 'partner_id',
-        string=u'Outras Inscrições Estaduais', ondelete='cascade'
-    )
+    inscr_est = fields.Char(
+        string='State Tax Number/RG',
+        size=16)
 
-    inscr_mun = fields.Char('Inscr. Municipal', size=18)
+    state_tax_number_ids = fields.One2many(
+        string='Others State Tax Number',
+        comodel_name='state.tax.numbers',
+        inverse_name='partner_id',
+        ondelete='cascade')
 
-    suframa = fields.Char('Suframa', size=18)
+    inscr_mun = fields.Char(
+        string='Municipal Tax Number',
+        size=18)
+
+    suframa = fields.Char(
+        string='Suframa',
+        size=18)
 
     legal_name = fields.Char(
-        u'Razão Social', size=128,
-        help="Nome utilizado em documentos fiscais")
+        string='Legal Name',
+        size=128,
+        help="Used in fiscal documents")
 
-    l10n_br_city_id = fields.Many2one(
-        'l10n_br_base.city', u'Município',
-        domain="[('state_id','=',state_id)]")
+    city_id = fields.Many2one(
+        domain="[('state_id', '=', state_id)]")
 
-    district = fields.Char('Bairro', size=32)
+    district = fields.Char(
+        string='District',
+        size=32)
 
-    number = fields.Char(u'Número', size=10)
-
-    union_entity_code = fields.Char(string='Union entity code')
+    union_entity_code = fields.Char(
+        string='Union Entity code')
 
     @api.multi
     @api.constrains('cnpj_cpf', 'inscr_est')
@@ -148,43 +157,25 @@ class ResPartner(models.Model):
     @api.onchange('cnpj_cpf', 'country_id')
     def _onchange_cnpj_cpf(self):
         country = self.country_id.code or ''
-        cpf_cnpj = fiscal.format_cpf_cnpj(self.cnpj_cpf, country,
-                                          self.is_company)
-        if cpf_cnpj:
-            self.cnpj_cpf = cpf_cnpj
-
-    @api.onchange('l10n_br_city_id')
-    def _onchange_l10n_br_city_id(self):
-        """ Ao alterar o campo l10n_br_city_id que é um campo relacional
-        com o l10n_br_base.city que são os municípios do IBGE, copia o nome
-        do município para o campo city que é o campo nativo do módulo base
-        para manter a compatibilidade entre os demais módulos que usam o
-        campo city.
-
-        param int l10n_br_city_id: id do l10n_br_city_id digitado.
-
-        return: dicionário com o nome e id do município.
-        """
-        if self.l10n_br_city_id:
-            self.city = self.l10n_br_city_id.name
+        self.cnpj_cpf = fiscal.format_cpf_cnpj(self.cnpj_cpf,
+                                               country,
+                                               self.is_company)
 
     @api.onchange('zip')
     def _onchange_zip(self):
-        if self.zip:
-            val = re.sub('[^0-9]', '', self.zip)
-            if len(val) == 8:
-                self.zip = "%s-%s" % (val[0:5], val[5:8])
+        self.zip = misc.format_zipcode(self.zip,
+                                       self.country_id.code)
 
     def _address_fields(self):
         """ Returns the list of address fields that are synced from the parent
         when the `use_parent_address` flag is set.
         Extenção para os novos campos do endereço """
-        address_fields = super(ResPartner, self)._address_fields()
-        return list(address_fields + ['l10n_br_city_id', 'number', 'district'])
+        address_fields = super(Partner, self)._address_fields()
+        return list(address_fields + ['district'])
 
     @api.multi
-    @api.constrains('other_inscr_est_lines')
-    def _check_other_ie_lines(self):
+    @api.constrains('state_tax_number_ids')
+    def _check_state_tax_number_ids(self):
         """Checks if field other insc_est is valid,
         this method call others methods because this validation is State wise
         :Return: True or False.
@@ -195,67 +186,16 @@ class ResPartner(models.Model):
                 uf = state_code.lower()
                 valid_ie = fiscal.validate_ie(uf, inscr_est_line.inscr_est)
                 if not valid_ie:
-                    raise ValidationError(u"Inscrição Estadual Invalida!")
+                    raise ValidationError('Invalid State Tax Number!')
                 if inscr_est_line.state_id.id == record.state_id.id:
                     raise ValidationError(
-                        u"Somente pode existir uma Inscrição"
-                        u" Estadual por estado para cada Parceiro!")
+                        'There can only be one state tax'
+                        ' number per state for each partner!')
                 duplicate_ie = record.search([
                     ('state_id', '=', inscr_est_line.state_id.id),
                     ('inscr_est', '=', inscr_est_line.inscr_est)
                 ])
                 if duplicate_ie:
                     raise ValidationError(
-                        u"Inscrição Estadual já usada"
-                        u" por %s" % duplicate_ie.name)
-
-
-class ResPartnerBank(models.Model):
-    """ Adiciona campos necessários para o cadastramentos de contas
-    bancárias no Brasil."""
-    _inherit = 'res.partner.bank'
-
-    number = fields.Char(u'Número', size=10)
-    street = fields.Char('Street', size=128)
-    street2 = fields.Char('Street2', size=128)
-    district = fields.Char('Bairro', size=32)
-    state_id = fields.Many2one(
-        "res.country.state", 'Fed. State',
-        change_default=True, domain="[('country_id','=',country_id)]")
-    l10n_br_city_id = fields.Many2one(
-        'l10n_br_base.city', 'Municipio',
-        domain="[('state_id','=',state_id)]")
-    acc_number = fields.Char("Account Number", size=64, required=False)
-    bank = fields.Many2one('res.bank', 'Bank', required=False)
-    acc_number_dig = fields.Char('Digito Conta', size=8)
-    bra_number = fields.Char(u'Agência', size=8)
-    bra_number_dig = fields.Char(u'Dígito Agência', size=8)
-    zip = fields.Char('CEP', size=24, change_default=True)
-    country_id = fields.Many2one('res.country', 'País', ondelete='restrict')
-
-    @api.multi
-    def onchange_partner_id(self, partner_id):
-        result = super(ResPartnerBank, self).onchange_partner_id(partner_id)
-        partner = self.env['res.partner'].browse(partner_id)
-        result['value']['number'] = partner.number
-        result['value']['district'] = partner.district
-        result['value']['l10n_br_city_id'] = partner.l10n_br_city_id.id
-        return result
-
-
-class OtherInscricoesEstaduais(models.Model):
-    _name = 'other.inscricoes.estaduais'
-
-    partner_id = fields.Many2one('res.partner')
-    inscr_est = fields.Char(
-        size=16, string='Inscr. Estadual', required=True
-    )
-    state_id = fields.Many2one(
-        'res.country.state', 'Estado', required=True
-    )
-
-    _sql_constraints = [
-        ('other_inscricoes_estaduais_id_uniq',
-         'unique (state_id, partner_id)',
-         u'O Parceiro já possui uma Inscrição Estadual para esse Estado!')
-    ]
+                        'State Registration already used'
+                        ' %s' % duplicate_ie.name)
