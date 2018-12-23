ca777d70a4e449565ddf8cfa173bd82381ef66e2
Renato Lima <renato.lima@akretion.com.br>
2018-12-23 13:10:15 -0200

    [MIGR] split file per objects

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -6,6 +6,8 @@ from . import res_city
 from . import res_country_state
 from . import res_country
 from . import res_bank
+from . import res_partner_bank
 from . import res_partner
+from . import state_tax_numbers
 from . import res_company
 from . import res_config_settings
