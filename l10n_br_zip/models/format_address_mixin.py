3645076bf9a24e1ef7f66705cc67f07f255962dd
Renato Lima <renato.lima@akretion.com.br>
2018-12-25 19:55:07 -0200

    [REF] refactoring minix to add zip_search

--- l10n_br_zip/models/format_address_mixin.py
+++ l10n_br_zip/models/format_address_mixin.py
@@ -3,12 +3,26 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, api
+from odoo.exceptions import UserError
 
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+class FormatAddressMixin(models.AbstractModel):
+    _inherit = "format.address.mixin"
 
     @api.multi
     def zip_search(self):
         self.ensure_one()
-        return self.env['l10n_br.zip'].zip_search(self)
+        try:
+            kargs = {
+                'country_id': self.country_id.id,
+                'state_id': self.state_id.id,
+                'city_id': self.city_id.id,
+                'district': self.district,
+                'street': self.street,
+                'zip_code': self.zip}
+        except AttributeError as e:
+            raise UserError(
+                u'Erro a Carregar Atributo: ' + str(e)) 
+
+        return self.env['l10n_br.zip'].zip_search(**kargs)
+
