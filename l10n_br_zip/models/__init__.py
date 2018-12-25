3bb77d1d073254ec3742ab278d28971e46c66bc5
Renato Lima <renato.lima@akretion.com.br>
2018-12-25 19:54:06 -0200

    [MIGR] using minix for address

--- l10n_br_zip/models/__init__.py
+++ l10n_br_zip/models/__init__.py
@@ -3,6 +3,4 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import l10n_br_zip
-from . import res_company
-from . import res_partner
-
+from . import format_address_mixin
