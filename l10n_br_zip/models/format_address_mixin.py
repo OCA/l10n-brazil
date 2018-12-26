0fab7672ae016762d9e5526b66246b4398cd1487
Renato Lima <renato.lima@akretion.com>
2018-12-25 22:20:24 -0200

    [REF] Refactoring zip_search method

--- l10n_br_zip/models/format_address_mixin.py
+++ l10n_br_zip/models/format_address_mixin.py
@@ -12,17 +12,5 @@ class FormatAddressMixin(models.AbstractModel):
     @api.multi
     def zip_search(self):
         self.ensure_one()
-        try:
-            kargs = {
-                'country_id': self.country_id.id,
-                'state_id': self.state_id.id,
-                'city_id': self.city_id.id,
-                'district': self.district,
-                'street': self.street,
-                'zip_code': self.zip}
-        except AttributeError as e:
-            raise UserError(
-                u'Erro a Carregar Atributo: ' + str(e)) 
-
-        return self.env['l10n_br.zip'].zip_search(**kargs)
+        return self.env['l10n_br.zip'].zip_search(self)
 
