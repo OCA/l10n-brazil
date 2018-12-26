1ae6a9f5e0f2100dec38e786bbfc680f6def12ea
Renato Lima <renato.lima@akretion.com>
2018-12-26 01:02:14 -0200

    [MIGR] crm.lead object

--- l10n_br_crm/models/crm_lead.py
+++ l10n_br_crm/models/crm_lead.py
@@ -2,34 +2,65 @@
 # Copyright (C) 2012 - TODAY  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import re
-
-from odoo import models, fields, api
-from odoo.addons.l10n_br_base.tools import fiscal
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-from odoo.tools.translate import _
+
+from odoo.addons.l10n_br_base.tools import misc, fiscal
 
 
-class CrmLead(models.Model):
+class Lead(models.Model):
     """ CRM Lead Case """
     _inherit = "crm.lead"
 
     legal_name = fields.Char(
-        u'Razão Social', size=128,
+        string='Legal Name',
+        size=128,
         help="nome utilizado em documentos fiscais")
-    cnpj = fields.Char('CNPJ', size=18)
-    inscr_est = fields.Char('Inscr. Estadual/RG', size=16)
-    inscr_mun = fields.Char('Inscr. Municipal', size=18)
-    suframa = fields.Char('Suframa', size=18)
-    l10n_br_city_id = fields.Many2one(
-        'l10n_br_base.city', 'Municipio',
-        domain="[('state_id','=',state_id)]")
-    district = fields.Char('Bairro', size=32)
-    number = fields.Char(u'Número', size=10)
-    name_surname = fields.Char(u'Nome e sobrenome', size=128,
-                               help="Nome utilizado em documentos fiscais")
-    cpf = fields.Char('CPF', size=18)
-    rg = fields.Char('RG', size=16)
+
+    cnpj = fields.Char(
+        string='CNPJ',
+        size=18)
+
+    inscr_est = fields.Char(
+        string='Inscr. Estadual/RG',
+        size=16)
+
+    inscr_mun = fields.Char(
+        string='Inscr. Municipal',
+        size=18)
+
+    suframa = fields.Char(
+        string='Suframa',
+        size=18)
+
+    city_id = fields.Many2one(
+        comodel_name='res.city',
+        string='City ID',
+        domain="[('state_id', '=', state_id)]")
+
+    country_id = fields.Many2one(
+        default=lambda self: self.env.ref('base.br'))
+
+    district = fields.Char(
+        string='District',
+        size=32)
+
+    street_number = fields.Char(
+        string=u'Número',
+        size=10)
+
+    name_surname = fields.Char(
+        string=u'Nome e sobrenome',
+        size=128,
+        help="Nome utilizado em documentos fiscais")
+
+    cpf = fields.Char(
+        string='CPF',
+        size=18)
+
+    rg = fields.Char(
+        string='RG',
+        size=16)
 
     @api.multi
     @api.constrains('cnpj')
@@ -37,7 +68,7 @@ class CrmLead(models.Model):
         for record in self:
             country_code = record.country_id.code or ''
             if record.cnpj and country_code.upper() == 'BR':
-                cnpj = re.sub('[^0-9]', '', record.cnpj)
+                cnpj = misc.punctuation_rm(record.cnpj)
                 if not fiscal.validate_cnpj(cnpj):
                     raise ValidationError(_(u'CNPJ inválido!'))
             return True
@@ -48,7 +79,7 @@ class CrmLead(models.Model):
         for record in self:
             country_code = record.country_id.code or ''
             if record.cpf and country_code.upper() == 'BR':
-                cpf = re.sub('[^0-9]', '', record.cpf)
+                cpf = misc.punctuation_rm(record.cpf)
                 if not fiscal.validate_cpf(cpf):
                     raise ValidationError(_(u'CPF inválido!'))
             return True
@@ -72,27 +103,19 @@ class CrmLead(models.Model):
 
     @api.onchange('cnpj', 'country_id')
     def _onchange_cnpj(self):
-        cnpj = None
         country_code = self.country_id.code or ''
-        if self.cnpj and country_code.upper() == 'BR':
-            val = re.sub('[^0-9]', '', self.cnpj)
-            if len(val) == 14:
-                cnpj = "%s.%s.%s/%s-%s" % (
-                    val[0:2], val[2:5], val[5:8], val[8:12], val[12:14])
-            self.cnpj = cnpj
+        self.cnpj = fiscal.format_cpf_cnpj(self.cnpj,
+                                           country_code,
+                                           True)
 
     @api.onchange('cpf', 'country_id')
     def onchange_mask_cpf(self):
-        cpf = None
         country_code = self.country_id.code or ''
-        if self.cpf and country_code.upper() == 'BR':
-            val = re.sub('[^0-9]', '', self.cpf)
-            if len(val) == 11:
-                cpf = "%s.%s.%s-%s"\
-                    % (val[0:3], val[3:6], val[6:9], val[9:11])
-            self.cpf = cpf
+        self.cpf = fiscal.format_cpf_cnpj(self.cpf,
+                                          country_code,
+                                          False)
 
-    @api.onchange('l10n_br_city_id')
+    @api.onchange('city_id')
     def onchange_l10n_br_city_id(self):
         """ Ao alterar o campo l10n_br_city_id que é um campo relacional
         com o l10n_br_base.city que são os municípios do IBGE, copia o nome
@@ -102,26 +125,23 @@ class CrmLead(models.Model):
         param int l10n_br_city_id: id do l10n_br_city_id digitado.
         return: dicionário com o nome e id do município.
         """
-        if self.l10n_br_city_id:
-            self.city = self.l10n_br_city_id.name
+        if self.city_id:
+            self.city = self.city_id.name
 
     @api.onchange('zip')
     def _onchange_zip(self):
-        if self.zip:
-            val = re.sub('[^0-9]', '', self.zip)
-            if len(val) == 8:
-                self.zip = "%s-%s" % (val[0:5], val[5:8])
+        country_code = self.country_id.code or ''
+        self.zip = misc.format_zipcode(self.zip, country_code)
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
-        result = super(CrmLead, self)._onchange_partner_id_values(
-            self.partner_id.id if self.partner_id else False
-        )
+        result = super(Lead, self)._onchange_partner_id_values(
+            self.partner_id.id if self.partner_id else False)
+
         if self.partner_id:
             result['number'] = self.partner_id.number
             result['district'] = self.partner_id.district
-            result['l10n_br_city_id'] = \
-                self.partner_id.l10n_br_city_id.id
+            result['city_id'] = self.partner_id.city_id.id
             if self.partner_id.is_company:
                 result['legal_name'] = self.partner_id.legal_name
                 result['cnpj'] = self.partner_id.cnpj_cpf
@@ -144,7 +164,7 @@ class CrmLead(models.Model):
             :param parent_id : id of the parent partner (False if no parent)
             :returns res.partner record
         """
-        partner_id = super(CrmLead, self)._lead_create_contact(
+        partner_id = super(Lead, self)._lead_create_contact(
             name, is_company, parent_id)
 
         if not lead:
@@ -153,7 +173,7 @@ class CrmLead(models.Model):
         value = {
             'number': lead.number,
             'district': lead.district,
-            'l10n_br_city_id': lead.l10n_br_city_id.id
+            'city_id': lead.city_id.id
         }
 
         if is_company:
