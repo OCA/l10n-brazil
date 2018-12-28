11783b764f3fbf22ca6bcdaace373ef7bfcd2be8
Renato Lima <renato.lima@akretion.com.br>
2018-12-28 00:28:28 -0200

    [MIGR] The first line # -*- coding: utf-8 -*- is not needed anymore in Python 3 if you save it in that encoding.

--- l10n_br_base/tools/misc.py
+++ l10n_br_base/tools/misc.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014  Renato Lima - Akretion
 # Copyright (C) 2015  Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
