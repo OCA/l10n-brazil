629f81f71865d43d5b804bca56c49540dadc5080
Magno Costa <magno.costa@akretion.com.br>
2019-01-15 16:02:48 -0200

    [12.0][MIG] PEP8.

--- l10n_br_base/models/res_config_settings.py
+++ l10n_br_base/models/res_config_settings.py
@@ -1,7 +1,7 @@
 # @ 2016 Kmee - www.kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
