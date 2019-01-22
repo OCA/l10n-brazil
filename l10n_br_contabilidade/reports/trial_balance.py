cf0084ec8373b2b47a439c0150a61f26fd914689
Luciano Veras <luciano.veras@abgf.gov.br>
2019-01-22 13:44:43 -0200

    [DEL] Retira a conta 0 do balancete

--- l10n_br_contabilidade/reports/trial_balance.py
+++ l10n_br_contabilidade/reports/trial_balance.py
@@ -8,6 +8,24 @@ from openerp.addons.account_financial_report_webkit.report.trial_balance \
 from openerp.addons.account_financial_report_webkit.report.webkit_parser_header_fix \
     import HeaderFooterTextWebKitParser
 
+
+def set_context(self, objects, data, ids, report_type=None):
+    """Populate a ledger_lines attribute on each browse record that will
+       be used by mako template"""
+    objects, new_ids, context_report_values = self. \
+        compute_balance_data(data)
+
+    # Retira a conta 0 do relatório
+    objects = objects.filtered(lambda x: x.code != '0')
+
+    self.localcontext.update(context_report_values)
+
+    return super(TrialBalanceWebkit, self).set_context(
+        objects, data, new_ids, report_type=report_type)
+
+
+TrialBalanceWebkit.set_context = set_context
+
 HeaderFooterTextWebKitParser(
     'report.account.abgf_account_report_trial_balance',
     'account.account',
