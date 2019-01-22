12f72459ab5dedbbae3983403ff456a2f2787f23
Renato Lima <renato.lima@akretion.com.br>
2019-01-22 01:51:25 -0200

    [REF] refactoring map_tax and compute_all to use in account.invoice objects

--- l10n_br_account_product/models/__init__.py
+++ l10n_br_account_product/models/__init__.py
@@ -30,5 +30,9 @@ from . import product_template
 from . import account_fiscal_position
 from . import account_fiscal_position_template
 from . import res_country_state
+from . import account_invoice
+from . import account_invoice_line
+from . import account_invoice_tax
+from . import account_tax_template
 from . import wizard_multi_charts_accounts
 from . import account_payment_mode
