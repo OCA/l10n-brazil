38bda4df9db8c45ac0d2e881b93b045e0a9ae82a
Renato Lima <renato.lima@akretion.com.br>
2019-01-23 18:41:35 -0200

    [MIGR] Remove account.invoice.tax object

--- l10n_br_account_product/models/__init__.py
+++ l10n_br_account_product/models/__init__.py
@@ -32,7 +32,6 @@ from . import account_fiscal_position_template
 from . import res_country_state
 from . import account_invoice
 from . import account_invoice_line
-from . import account_invoice_tax
 from . import account_tax_template
 from . import wizard_multi_charts_accounts
 from . import account_payment_mode
