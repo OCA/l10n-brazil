88ea3328620b1b8077948fe86aafcc97474d1b4b
Magno Costa <magno.costa@akretion.com.br>
2019-01-23 11:10:14 -0200

    [10.0][MIG] PEP8.

--- l10n_br_account_product/models/l10n_br_account_product_document_related.py
+++ l10n_br_account_product/models/l10n_br_account_product_document_related.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+import re
+
 from odoo import models, fields, api, _
 from odoo.exceptions import Warning as UserError
 
