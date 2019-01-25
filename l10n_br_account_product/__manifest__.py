cb9eb8581d9df9cee37dc4c8700d6805745cbaff
Renato Lima <renato.lima@akretion.com.br>
2019-01-25 05:07:53 -0200

    [MIGR] l10n_br_account_product data and demo data

--- l10n_br_account_product/__manifest__.py
+++ l10n_br_account_product/__manifest__.py
@@ -18,8 +18,6 @@
     ],
     'data': [
         'l10n_br_account_product_sequence.xml',
-        # TODO
-        # 'account_invoice_workflow.xml',
         'data/l10n_br_account_product.cfop.csv',
         'data/l10n_br_account.fiscal.document.csv',
         'data/l10n_br_account_data.xml',
@@ -58,11 +56,11 @@
         'report/account_invoice_report_view.xml',
     ],
     'demo': [
-        # 'demo/account_tax_demo.xml',
-        # 'demo/base_demo.xml',
-        # 'demo/product_demo.xml',
-        # 'demo/l10n_br_account_product_demo.xml',
-        # 'demo/account_fiscal_position_rule_demo.xml',
+        'demo/account_tax_demo.xml',
+        'demo/base_demo.xml',
+        'demo/product_demo.xml',
+        'demo/l10n_br_account_product_demo.xml',
+        'demo/account_fiscal_position_rule_demo.xml',
         # 'demo/product_taxes.yml',
     ],
     'test': [
