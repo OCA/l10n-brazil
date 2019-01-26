86a2ffa08b60e347bfd14eced8cce78876b7aa03
Renato Lima <renato.lima@akretion.com.br>
2019-01-25 23:10:30 -0200

    pep8

--- l10n_br_account_product/models/account_fiscal_position_template.py
+++ l10n_br_account_product/models/account_fiscal_position_template.py
@@ -4,7 +4,7 @@
 
 from odoo import models, fields
 
-from product_template import PRODUCT_ORIGIN
+from .product_template import PRODUCT_ORIGIN
 
 
 class AccountFiscalPositionTemplate(models.Model):
@@ -13,8 +13,8 @@ class AccountFiscalPositionTemplate(models.Model):
     cfop_id = fields.Many2one(
         comodel_name='l10n_br_account_product.cfop',
         domain="[('internal_type', '=', 'normal')]",
-        string='CFOP'
-    )
+        string='CFOP')
+
     ind_final = fields.Selection(
         selection=[('0', u'Não'),
                    ('1', u'Sim')],
@@ -23,8 +23,7 @@ class AccountFiscalPositionTemplate(models.Model):
         states={'draft': [('readonly', False)]},
         required=False,
         default='0',
-        help=u'Indica operação com Consumidor final.'
-    )
+        help=u'Indica operação com Consumidor final.')
 
 
 class AccountFiscalPositionTaxTemplate(models.Model):
@@ -32,21 +31,25 @@ class AccountFiscalPositionTaxTemplate(models.Model):
 
     fiscal_classification_id = fields.Many2one(
         comodel_name='account.product.fiscal.classification.template',
-        string=u'NCM'
-    )
+        string=u'NCM')
+
     cest_id = fields.Many2one(
         comodel_name='l10n_br_account_product.cest',
-        string=u'CEST'
-    )
+        string=u'CEST')
+
     tax_ipi_guideline_id = fields.Many2one(
         comodel_name='l10n_br_account_product.ipi_guideline',
-        string=u'Enquadramento IPI'
-    )
+        string=u'Enquadramento IPI')
+
     tax_icms_relief_id = fields.Many2one(
         comodel_name='l10n_br_account_product.icms_relief',
-        string=u'Desoneração ICMS'
-    )
+        string=u'Desoneração ICMS')
+
     origin = fields.Selection(
         selection=PRODUCT_ORIGIN,
-        string=u'Origem'
-    )
+        string=u'Origem')
+
+    cst_id = fields.Many2one(
+        comodel_name='l10n_br_account_product.cst',
+        string=u'CST',
+        required=False)
