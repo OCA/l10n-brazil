5a03f56afb7c447bc7ab70ac8cb5738ab4084d2e
Renato Lima <renato.lima@akretion.com.br>
2019-01-26 18:29:48 -0200

    pep8

--- l10n_br_zip_correios/models/l10n_br_zip.py
+++ l10n_br_zip_correios/models/l10n_br_zip.py
@@ -3,8 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, api
-from odoo.addons.l10n_br_zip_correios.models.webservice_client\
-    import WebServiceClient
+from .webservice_client import WebServiceClient
 
 
 class L10nBrZip(models.Model):
