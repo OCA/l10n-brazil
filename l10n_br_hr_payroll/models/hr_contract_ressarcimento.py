6d574edc32191b7d6eed064bde1b33eabbb60f75
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-02-06 13:23:48 -0200

    [ADD] Modelo inicial para ressarcimento

---
+++ l10n_br_hr_payroll/models/hr_contract_ressarcimento.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ABGF.gov.br Hendrix Costa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+
+from openerp import api, fields, models
+
+
+class HrContractRessarcimento(models.Model):
+    _name = b'hr.contract.ressarcimento'
+    _description = 'Ressarcimentos de outros Vínculos do Contrato'
+    _order = "account_period_id DESC"
+
+    state = fields.Selection(
+        selection=[
+            ('aberto', 'Aberto'),
+            ('confirmado', 'Confirmado'),
+            ('provisionado', 'Provisionado'),
+            ('aprovado','Aprovado'),
+        ],
+        string='Situação',
+        default='aberto',
+    )
+
+    contract_id = fields.Many2one(
+        comodel_name="hr.contract",
+        string="Contrato",
+    )
+
+    hr_contract_ressarcimento_line_ids = fields.One2many(
+        inverse_name='hr_contract_ressarcimento_id',
+        comodel_name='hr.contract.ressarcimento.line',
+        string='Ressarcimento do Contratro',
+    )
+
+    account_period_id = fields.Many2one(
+        comodel_name='account.period',
+        string='Competência',
+        domain="[('special', '=', False)]",
+    )
+
+    total = fields.Float(
+        string=u"Total de Ressarcimento",
+        compute='compute_total_ressarcimento',
+        store=True,
+    )
+
+    valor_provisionado = fields.Boolean(
+        string='Valor Provisionado?',
+    )
+
+    partner_ids = fields.Many2many(
+        comodel_name='res.partner',
+        string='Parceiros para notificar:',
+        default=lambda self: self._get_default_partner_ids(),
+    )
+
+    aprovado_por = fields.Many2one(
+        string='Aprovado por:',
+        res_model='res.users',
+    )
+
+    def _get_default_partner_ids(self):
+        """
+        Buscar o último ressarcimento com partner definido e sugerir para o
+        usuário
+        """
+        ressarcimento_id = self.search([('partner_ids','!=', False)],
+                    order="create_date desc", limit=1)
+        return ressarcimento_id.partner_ids
+
+    @api.multi
+    @api.depends('hr_contract_ressarcimento_line_ids')
+    def compute_total_ressarcimento(self):
+        for record in self:
+            record.total = sum(
+                record.hr_contract_ressarcimento_line_ids.mapped('total'))
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = 'Ressarcimento {} [{}]'.format(
+                record.contract_id.employee_id.name,
+                record.account_period_id.name
+            )
+            result.append((record['id'], name))
+        return result
+
+    @api.multi
+    def button_aprovar(self):
+        """
+        Aprovação
+        """
+        for record in self:
+            record.aprovado_por = self.env.user.id
+            record.state = 'aprovado'
+            record.send_mail(situacao='aprovado')
+
+    @api.multi
+    def button_confirm(self):
+        """
+        Operador confirmando e submetendo para aprovação
+        """
+        for record in self:
+            record.state = 'confirmado'
+            record.send_mail(situacao='confirmado')
+
+    @api.multi
+    def button_send_mail(self):
+        """
+        """
+        for record in self:
+            record.send_mail(record.state)
+
+    @api.multi
+    def send_mail(self, situacao='aprovado'):
+        """
+        Email serão mandados em 2 momentos:
+        Confirmação: após criação um ressarcimento deverá ser submetido
+                    à aprovação
+        Aprovação: Email para avisar da pendencia de um ressarcimento aprovação
+        """
+        mail_obj = self.env['mail.mail']
+
+        template_name = \
+            'l10n_br_hr_payroll.' \
+            'email_template_hr_contract_ressarcimento_{}'.format(situacao)
+        template = self.env.ref(template_name, False)
+
+        for record in self:
+            vals = template.generate_email_batch(template.id, [record.id])
+
+        val = vals[self.id]
+
+        emails = self.partner_ids.filtered('email').mapped('email')
+        email_to = ','.join(emails)
+        val.update(email_to=email_to)
+
+        import pprint
+        pprint.pprint(val)
+        mail_id = mail_obj.create(val)
+        mail_obj.send(mail_id)
+
+    @api.multi
+    def write(self, values):
+        if values.get('valor_provisionado'):
+            values.update(state='provisionado')
+        return super(HrContractRessarcimento, self).write(values)
+
+    @api.model
+    def create(self, values):
+        if values.get('valor_provisionado'):
+            values.update(state='provisionado')
+
+        ressarcimento_id = super(HrContractRessarcimento, self).create(values)
+
+        # ressarcimento_id.send_mail('create')
+
+        return ressarcimento_id
+
+
+class HrContractRessarcimentoLine(models.Model):
+    _name = b'hr.contract.ressarcimento.line'
+    _description = 'Linhas dos Ressarcimentos de outros Vínculos'
+    _order = 'descricao'
+
+    hr_contract_ressarcimento_id = fields.Many2one(
+        comodel_name='hr.contract.ressarcimento',
+        string='Ressarcimento do Contratro',
+    )
+
+    descricao = fields.Char(
+        string="Rubricas de Ressarcimento",
+    )
+
+    total = fields.Float(
+        string=u"Valor",
+    )
