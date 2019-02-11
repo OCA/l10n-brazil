2ee06b2e2375d182d03bb2fbc5fa3140d6b9a36c
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-02-11 12:37:12 -0200

    [ADD] Validação roteiro de 1ª Fórmula

--- l10n_br_contabilidade/models/account_event_template.py
+++ l10n_br_contabilidade/models/account_event_template.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
+from openerp.exceptions import Warning
 
 
 class AccountEventTemplate(models.Model):
@@ -20,10 +21,10 @@ class AccountEventTemplate(models.Model):
     account_formula = fields.Selection(
         string=u'Fórmula',
         selection=[
-            (1, '1ª Fórumla'),
-            (2, '2ª Fórumla'),
-            (3, '3ª Fórumla'),
-            (4, '4ª Fórumla'),
+            (1, '1ª Fórmula'),
+            (2, '2ª Fórmula'),
+            (3, '3ª Fórmula'),
+            (4, '4ª Fórmula'),
         ],
     )
 
@@ -32,3 +33,32 @@ class AccountEventTemplate(models.Model):
         comodel_name='account.event.template.line',
         inverse_name='account_event_template_id',
     )
+
+    def validar_primeira_formula(self):
+        if self.account_formula == 1:
+            for partida in self.account_event_template_line_ids:
+                if not partida.account_debito_id or not partida.account_credito_id:
+                    raise Warning(
+                        'Nos lançamentos de 1ª Fórmula é '
+                        'necessário que todas as partidas possuam uma '
+                        'conta de débito e uma de crédito!'
+                    )
+
+    def validar_formula_roteiro_contabil(self):
+        self.validar_primeira_formula()
+
+    @api.model
+    def create(self, vals):
+        res = super(AccountEventTemplate, self).create(vals)
+
+        res.validar_formula_roteiro_contabil()
+
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(AccountEventTemplate, self).create(vals)
+
+        self.validar_formula_roteiro_contabil()
+
+        return res
