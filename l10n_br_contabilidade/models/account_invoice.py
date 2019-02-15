7bf3678545b1c9717bc22f3e9a6c60ba4ab18aa9
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-02-15 15:23:05 -0200

    [ADD] Funcionalidade para criar lançamentos da NFe

--- l10n_br_contabilidade/models/account_invoice.py
+++ l10n_br_contabilidade/models/account_invoice.py
@@ -2,7 +2,14 @@
 # Copyright 2018 ABGF
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning, ValidationError
+from openerp.addons.l10n_br_account_product.constantes import (
+    CAMPO_DOCUMENTO_FISCAL,
+    CAMPO_DOCUMENTO_FISCAL_ITEM,
+    ACCOUNT_AUTOMATICO_PARTICIPANTE,
+    ACCOUNT_AUTOMATICO_PRODUTO,
+)
 
 
 class AccountInvoice(models.Model):
@@ -116,3 +123,103 @@ class AccountInvoice(models.Model):
                 lines = self.env['account.move.line']
             partial_lines += data_line
             self.move_lines = lines - partial_lines
+
+    @api.multi
+    def action_cancel(self):
+        payment_line_obj = self.env['payment.line']
+        for inv in self:
+            pl_line_ids = []
+            for move in inv.move_id:
+                if move and move.line_id:
+                    inv_mv_lines = [x.id for x in move.line_id]
+                    pl_line_ids = payment_line_obj.search(
+                        [('move_line_id', 'in', inv_mv_lines)])
+                if pl_line_ids:
+                    pay_line = payment_line_obj.browse(pl_line_ids)
+                    payment_order_name = ','.join(
+                        map(lambda x: x.order_id.reference, pay_line)
+                    )
+                    raise Warning(
+                        _('Error!'),
+                        _(
+                            "You cannot cancel an invoice which has already "
+                            "been imported in a payment order. Remove it from "
+                            "the following payment order : {}.".format(
+                                payment_order_name)
+                        )
+                    )
+
+        move = self.move_id
+
+        self.write({'state': 'cancel', 'move_id': False})
+        if move:
+            move.button_cancel()
+
+        return True
+
+    def _get_invoice_event_data(self):
+        vals = {}
+        vals['company_id'] = self.company_id.id
+        vals['ref'] = 'nota_fiscal'
+        vals['period'] = self.period_id
+        vals['data'] = self.date_hour_invoice.split(' ')[0]
+        vals['account_event_id'] = self.fiscal_category_id.account_event_id.id
+
+        return vals
+
+    def _get_invoice_move_line_data(self):
+        lines = []
+        for info in CAMPO_DOCUMENTO_FISCAL:
+            info_name = info[0]
+            if self[info_name]:
+                lines.append(
+                    {
+                        'code': info_name,
+                        'valor': self[info_name],
+                        'mes': self.period_id.code,
+                        'num_documento': '{} - {}'.format(
+                            self.internal_number, self.partner_id.name),
+                    }
+                )
+
+        return lines
+
+    @api.multi
+    def action_move_create(self):
+        for inv in self:
+            if not inv.fiscal_category_id.account_event_id:
+                raise ValidationError(
+                    _('Error!'),
+                    _(
+                        'Please define a Roteiro de Evento Contábil '
+                        'in the fiscal category.'
+                    )
+                )
+
+            if not inv.invoice_line:
+                raise ValidationError(
+                    _('No Invoice Lines!'),
+                    _('Please create some invoice lines.')
+                )
+
+            if inv.move_id:
+                inv.move_id.button_return()
+                inv.move_id.unlink()
+
+            if not self.period_id:
+                self.period_id = self.env['account.period'].find(
+                    self.date_hour_invoice)[0]
+
+            account_event_data = self._get_invoice_event_data()
+            account_event_data['lines'] = \
+                self._get_invoice_move_line_data()
+
+            inv.move_id = \
+                inv.fiscal_category_id.account_event_id.gerar_contabilizacao(
+                    account_event_data
+                )
+
+            account_invoice_tax = self.env['account.invoice.tax']
+            compute_taxes = account_invoice_tax.compute(
+                inv.with_context(lang=inv.partner_id.lang))
+            inv.check_tax_lines(compute_taxes)
