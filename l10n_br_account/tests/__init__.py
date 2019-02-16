b7be4ec4ef2846cfb7b7a9611dc7438e7dd206f1
Magno Costa <magno.costa@akretion.com.br>
2019-02-16 09:21:10 -0200

    [10.0][MIG] Remove unused test.

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -6,7 +6,6 @@
 from . import test_account_customer_invoice
 from . import test_account_supplier_invoice
 from . import test_generate_fiscal_rules
-from . import test_generate_multi_charts_accounts
 from . import test_fiscal_partner_type
 from . import test_account_service_type
 from . import test_account_cnae
