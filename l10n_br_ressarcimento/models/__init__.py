994f5c35e7ef23f6e39b1ace10099fca956a0dd3
Luciano <luciano.veras@abgf.gov.br>
2019-02-26 17:02:21 -0300

    [FIX] ABGF

--- l10n_br_ressarcimento/models/__init__.py
+++ l10n_br_ressarcimento/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 KMEE (http://www.kmee.com.br)
+# Copyright (C) 2019 ABGF (http://www.abgf.gov.br)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import contract_ressarcimento
