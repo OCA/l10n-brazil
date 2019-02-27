853d329ad9088020be2b6ab1c4fb7f7f00eb8fc8
Luciano <luciano.veras@abgf.gov.br>
2019-02-27 16:03:36 -0300

    [REF] Lançamento contábil através do evento

--- l10n_br_ressarcimento_account/models/__init__.py
+++ l10n_br_ressarcimento_account/models/__init__.py
@@ -3,4 +3,3 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import contract_ressarcimento
-from . import account_move
--- l10n_br_ressarcimento_account/models/contract_ressarcimento.py
+++ l10n_br_ressarcimento_account/models/contract_ressarcimento.py
@@ -5,31 +5,76 @@
 from datetime import date
 from openerp import api, fields, models
 
+NOME_LANCAMENTO = {
+    'provisionado': u'Ressarcimento(Provisão)',
+    'aprovado': u'Ressarcimento',
+}
+
 
 class ContractRessarcimento(models.Model):
     _inherit = b'contract.ressarcimento'
 
-    account_event_template_id = fields.Many2one(
-        comodel_name='account.event.template',
-        string='Roteiro contábil'
+    account_event_id = fields.Many2one(
+        string='Evento Contábil',
+        comodel_name='account.event',
+        ondelete='restrict',
     )
 
-    account_event_template_provisao_id = fields.Many2one(
-        comodel_name='account.event.template',
-        string='Roteiro contábil'
-    )
+    # account_event_template_provisao_id = fields.Many2one(
+    #     comodel_name='account.event.template',
+    #     string='Roteiro contábil'
+    # )
+    #
+    # account_move_ids = fields.One2many(
+    #     comodel_name='account.move',
+    #     inverse_name='contract_ressarcimento_id',
+    #     string=u'Lançamentos',
+    # )
+    #
+    # account_move_provisao_ids = fields.One2many(
+    #     comodel_name='account.move',
+    #     inverse_name='contract_ressarcimento_provisionado_id',
+    #     string=u'Lançamentos',
+    # )
 
-    account_move_ids = fields.One2many(
-        comodel_name='account.move',
-        inverse_name='contract_ressarcimento_id',
-        string=u'Lançamentos',
-    )
+    @api.multi
+    def gerar_contabilizacao_rubricas(self):
+        """
+        Gerar um dict contendo a contabilização de cada rubrica
+        return { string 'CODE' : float valor}
+        """
 
-    account_move_provisao_ids = fields.One2many(
-        comodel_name='account.move',
-        inverse_name='contract_ressarcimento_provisionado_id',
-        string=u'Lançamentos',
-    )
+        """
+                {
+            'data':         '2019-01-01',
+            'lines':        [{'code': 'LIQUIDO', 'valor': 123,
+                                'historico_padrao': {'mes': '01'}},
+                             {'code': 'INSS', 'valor': 621.03}
+                                'historico_padrao': {'nome': 'Nome do lança'}},
+                            ],
+            'ref':          identificação do módulo de origem
+            'model':        (opcional) model de origem
+            'res_id':       (opcional) id do registro de origem
+            'period_id'     (opcional) account.period
+            'company_id':   (opcional) res.company
+        }
+        """
+        contabilizacao_rubricas = []
+
+        # Roda as Rubricas e Cria os lançamentos contábeis
+        for record in self:
+            # se state = provisionado, pega as linhas do valor provisionado
+            line_ids = 'contract_ressarcimento_provisionado_line_ids' \
+                if record.state == 'provisionado' \
+                else 'contract_ressarcimento_line_ids'
+
+            for line in eval('record.'+line_ids):
+                contabilizacao_rubricas.append((0, 0, {
+                    'code': line.descricao,
+                    'valor': line.total,
+                }))
+
+        return contabilizacao_rubricas
 
     @api.multi
     def button_aprovar(self):
@@ -37,27 +82,25 @@ class ContractRessarcimento(models.Model):
 
         :return:
         """
-        super(ContractRessarcimento, self).button_aprovar()
+        for record in self:
+            print record.state
+            super(ContractRessarcimento, self).button_aprovar()
+            print record.state
+            # Exclui os Lançamento Contábeis anteriors
+            record.account_event_id.unlink()
 
-        # Monta dict para lançamentos contábeis de acordo com o
-        # roteiro escolhido
-        dados = {
-            'data': str(date.today()),
-            'lines': [{'code': 'TOTAL',
-                       'valor': self.total_provisionado
-                       if self.state == 'provisionado' else self.total,
-                       'name': self.account_event_template_id.name, }],
-            'ref': 'Ressarcimento de Contrato',
-            'model': 'contract.ressarcimento',
-            'res_id': self.id,
-            'period_id': self.account_period_provisao_id.id
-            if self.state == 'provisionado' else self.account_period_id.id
-        }
+            rubricas_para_contabilizar = self.gerar_contabilizacao_rubricas()
 
-        if self.state == 'provisionado':
-            self.account_move_provisao_ids = \
-                self.account_event_template_provisao_id.gerar_contabilizacao(
-                    dados=dados)
-        else:
-            self.account_move_ids = self.account_event_template_id.\
-                gerar_contabilizacao(dados=dados)
+            account_event = {
+                'ref': '{} - {} - {}'.format(
+                    NOME_LANCAMENTO.get(record.state),
+                    record.account_period_id.name,
+                    record.contract_id.employee_id.name),
+                # 'data': record.date_from,
+                'account_event_line_ids': rubricas_para_contabilizar,
+            }
+
+            record.account_event_id = \
+                self.env['account.event'].create(account_event)
+
+
