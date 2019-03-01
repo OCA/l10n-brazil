13ed040b62decb62f23a6ed4ec207cd9c78416b3
Magno Costa <magno.costa@akretion.com.br>
2019-03-01 16:52:21 -0300

    [10.0][MIG][IMP] Python Tests.

--- l10n_br_account_product/__init__.py
+++ l10n_br_account_product/__init__.py
@@ -6,3 +6,4 @@ from . import models
 from . import sped
 from . import wizard
 from . import report
+from . import tests
---
+++ l10n_br_account_product/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import test_account_customer_invoice
+from . import test_account_customer_nfe
