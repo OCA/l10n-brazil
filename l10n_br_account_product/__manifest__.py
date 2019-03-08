31e19b619a489780573dbfe04632f8cbfbc4516a
Magno Costa <magno.costa@akretion.com.br>
2019-03-08 15:59:34 -0300

    [10.0][MIG][IMP] Demo data for Supplier NFe.

--- l10n_br_account_product/__manifest__.py
+++ l10n_br_account_product/__manifest__.py
@@ -66,6 +66,7 @@
         'demo/account_invoice_demo.xml',
         'demo/account_nfe_demo.yml',
         'demo/account_nfe_demo.xml',
+        'demo/account_nfe_supplier_demo.xml',
     ],
     'test': [
         # 'test/account_customer_invoice.yml',
