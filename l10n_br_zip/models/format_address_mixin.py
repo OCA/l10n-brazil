493c3743e759bbf971593cfde73a578b40006e2a
Renato Lima <renato.lima@akretion.com>
2019-03-20 03:12:42 -0300

    pep8

--- l10n_br_zip/models/format_address_mixin.py
+++ l10n_br_zip/models/format_address_mixin.py
@@ -2,7 +2,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, api
-from odoo.exceptions import UserError
 
 
 class FormatAddressMixin(models.AbstractModel):
