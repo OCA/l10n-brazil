2e2a4260623e6424875f17859b04ad574b133e3f
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-03-26 16:53:54 -0300

    [FIX] Tratar o beneficiário de todas as rúbricas

--- l10n_br_hr_payroll/models/hr_payslip.py
+++ l10n_br_hr_payroll/models/hr_payslip.py
@@ -2500,7 +2500,7 @@ class HrPayslip(models.Model):
                     #
                     # Tratamos as rubricas específicas que têm beneficiários
                     #
-                    if rule.id in applied_specific_rule and rule.code not in calculated_specifc_rule:
+                    if rule.id in applied_specific_rule:
 
                         lista_rubricas_especificas = applied_specific_rule[rule.id]
 
