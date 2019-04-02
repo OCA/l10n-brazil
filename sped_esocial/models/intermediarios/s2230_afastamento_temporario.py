89cf440e52d9f5d15e4127956df29df714a98c5a
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-04-02 16:04:05 -0300

    [FIX] Lógica para o registro S-2230 do e-Social
    - Melhorada a lógica para a criação do registro de inicio e
    o registro de finalização do afastamento temporário (S-2230) do
    e-Social.

--- sped_esocial/models/intermediarios/s2230_afastamento_temporario.py
+++ sped_esocial/models/intermediarios/s2230_afastamento_temporario.py
@@ -95,21 +95,26 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
         self.ensure_one()
 
         # Criar o registro S-2230 de alteração, se for necessário
+        values = {
+            'tipo': 'esocial',
+            'registro': 'S-2230',
+            'ambiente': self.company_id.esocial_tpAmb,
+            'company_id': self.company_id.id,
+            'operacao': 'I',
+            'evento': 'evtAfastTemp',
+            'origem': ('hr.holidays,%s' % self.hr_holiday_id.id),
+            'origem_intermediario': (
+                    'sped.esocial.afastamento.temporario,%s' % self.id),
+        }
         if not self.sped_afastamento:
-            values = {
-                'tipo': 'esocial',
-                'registro': 'S-2230',
-                'ambiente': self.company_id.esocial_tpAmb,
-                'company_id': self.company_id.id,
-                'operacao': 'I',
-                'evento': 'evtAfastTemp',
-                'origem': ('hr.holidays,%s' % self.hr_holiday_id.id),
-                'origem_intermediario': (
-                        'sped.esocial.afastamento.temporario,%s' % self.id),
-            }
-
             sped_afastamento = self.env['sped.registro'].create(values)
             self.sped_afastamento = sped_afastamento
+            if self.holiday_id.holiday_status_id.esocial_evento_afastamento_id.codigo == '15':
+                self.sped_afastamento_encerrado = sped_afastamento
+        elif not self.sped_afastamento_encerrado:
+            sped_afastamento_encerrado = \
+                self.env['sped.registro'].create(values)
+            self.sped_afastamento_encerrado = sped_afastamento_encerrado
 
     def verifica_encerramento_afastamento(self):
         """
@@ -162,31 +167,43 @@ class SpedAfastamentoTemporario(models.Model, SpedRegistroIntermediario):
 
         # Popula infoAfastamento
 
-        # Popula iniAfastamento
-        bloco_inicio = S2230.evento.infoAfastamento.iniAfastamento
-        inicio_afastamento = pysped.esocial.leiaute.S2230_IniAfastamento_2()
-        inicio_afastamento.dtIniAfast.valor = holiday_id.data_inicio
-        codigo_afastamento = holiday_id.holiday_status_id.\
-            esocial_evento_afastamento_id.codigo
-        inicio_afastamento.codMotAfast.valor = codigo_afastamento
+        if not self.sped_afastamento or self.sped_afastamento.situacao != '4':
+            # Popula iniAfastamento
+            bloco_inicio = S2230.evento.infoAfastamento.iniAfastamento
+            inicio_afastamento = pysped.esocial.leiaute.S2230_IniAfastamento_2()
+            inicio_afastamento.dtIniAfast.valor = holiday_id.data_inicio
+            codigo_afastamento = holiday_id.holiday_status_id.\
+                esocial_evento_afastamento_id.codigo
+            inicio_afastamento.codMotAfast.valor = codigo_afastamento
 
-        bloco_inicio.append(inicio_afastamento)
-        
-        if holiday_id.holiday_status_id.esocial_evento_afastamento_id.codigo == '15':
+            bloco_inicio.append(inicio_afastamento)
+
+            if holiday_id.holiday_status_id.esocial_evento_afastamento_id.codigo == '15':
+                # Popula fimAfastamento
+                data_atual = fields.Datetime.from_string(fields.Datetime.now())
+                data_fim = fields.Datetime.from_string(holiday_id.data_fim)
+                bloco_fim = S2230.evento.infoAfastamento.fimAfastamento
+                cod_ferias = holiday_id.holiday_status_id.\
+                                 esocial_evento_afastamento_id.codigo == '15'
+                if data_atual <= data_fim or cod_ferias:
+                    fim_afastamento = pysped.esocial.leiaute.S2230_FimAfastamento_2()
+                    fim_afastamento.dtTermAfast.valor = holiday_id.data_fim
+
+                    bloco_fim.append(fim_afastamento)
+
+                if bloco_inicio and bloco_fim:
+                    self.sped_afastamento_encerrado = self.sped_afastamento
+        else:
             # Popula fimAfastamento
             data_atual = fields.Datetime.from_string(fields.Datetime.now())
             data_fim = fields.Datetime.from_string(holiday_id.data_fim)
             bloco_fim = S2230.evento.infoAfastamento.fimAfastamento
-            cod_ferias = holiday_id.holiday_status_id.\
-                             esocial_evento_afastamento_id.codigo == '15'
-            if data_atual <= data_fim or cod_ferias:
+
+            if data_fim:
                 fim_afastamento = pysped.esocial.leiaute.S2230_FimAfastamento_2()
                 fim_afastamento.dtTermAfast.valor = holiday_id.data_fim
 
                 bloco_fim.append(fim_afastamento)
-
-            if bloco_inicio and bloco_fim:
-                self.sped_afastamento_encerrado = self.sped_afastamento
 
         return S2230, validacao
 
