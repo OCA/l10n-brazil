ed8c15faaa10e924bc270ae3c1b3e7c0cb972ea4
Renato Lima <renato.lima@akretion.com.br>
2019-04-04 17:59:51 -0300

    [FIX] rebase and cherry-pick conflicts

--- l10n_br_account_product/models/__init__.py
+++ l10n_br_account_product/models/__init__.py
@@ -22,7 +22,6 @@ from . import account_product_fiscal_classification
 from . import wizard_account_product_fiscal_classification
 from . import account_invoice
 from . import account_invoice_line
-from . import account_invoice_tax
 from . import account_payment_term
 from . import account_tax_template
 from . import account_tax
@@ -30,8 +29,5 @@ from . import product_template
 from . import account_fiscal_position
 from . import account_fiscal_position_template
 from . import res_country_state
-from . import account_invoice
-from . import account_invoice_line
-from . import account_tax_template
 from . import wizard_multi_charts_accounts
 from . import account_payment_mode
