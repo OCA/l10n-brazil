e88f50a12375635727f5e1d64a8bb053054c9e78
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-04-12 14:58:10 -0300

    [FIX] Criaçã automatica da conta raiz quando criar um account.depara.plano

--- l10n_br_contabilidade/models/account_depara_plano.py
+++ l10n_br_contabilidade/models/account_depara_plano.py
@@ -2,7 +2,7 @@
 # Copyright 2019 ABGF
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class AccountDeParaPlano(models.Model):
@@ -12,3 +12,30 @@ class AccountDeParaPlano(models.Model):
     name = fields.Char(
         string='Plano de Contas',
     )
+
+    account_account_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Conta Principal',
+    )
+
+    @api.model
+    def create(self, vals):
+        """
+        :param vals:
+        :return:
+        """
+
+        account_account_id = self.env['account.account'].create({
+            'code': '0',
+            'name': vals.get('name'),
+            'type': 'view',
+            'user_type': self.env.ref('account.data_account_type_view').id,
+        })
+
+        vals.update(account_account_id=account_account_id.id)
+
+        account_depara_plano_id = super(AccountDeParaPlano, self).create(vals)
+
+        account_account_id.account_depara_plano_id = account_depara_plano_id
+
+        return account_depara_plano_id
