ae6f7ea450d245f9e78ed9ef01dadfef7e414c10
Renato Lima <renato.lima@akretion.com.br>
2019-04-14 03:12:32 -0300

    Removed not necessary coding UTF-8 in l10n_br_account_product

--- l10n_br_account_product/sped/nfe/serializer/xml.py
+++ l10n_br_account_product/sped/nfe/serializer/xml.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 200(  Renato Lima - Akretion
+# Copyright (C) 2009  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from ..document import NFe200
