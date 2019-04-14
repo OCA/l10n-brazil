3b99901551145f8b268db8d573b965bb063d58d7
Renato Lima <renato.lima@akretion.com>
2019-04-14 03:05:39 -0300

    Removed not necessary coding UTF-8 in l10n_br_account

--- l10n_br_account/models/res_config.py
+++ l10n_br_account/models/res_config.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #    Copyright (C) 2015 KMEE (http://www.kmee.com.br)
 #    @author Luis Felipe Miléo (mileo@kmee.com.br)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
