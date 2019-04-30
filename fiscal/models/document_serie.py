d0da77be4d48c97c6ef838b1db88aef2d3833e08
Renato Lima <renato.lima@akretion.com.br>
2019-04-30 20:54:34 -0300

    [MIGR] Objeto Serie de Documento Fiscal

--- fiscal/models/document_serie.py
+++ fiscal/models/document_serie.py
@@ -1,64 +1,66 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# Copyright (C) 2009  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields, api
 
-from .l10n_br_account import (PRODUCT_FISCAL_TYPE,
-                              PRODUCT_FISCAL_TYPE_DEFAULT)
+from .constants.fiscal import FISCAL_IN_OUT, FISCAL_IN_OUT_DEFAULT
 
 
-class L10nBrAccountDocumentSerie(models.Model):
-    _name = 'l10n_br_account.document.serie'
-    _description = 'Serie de documentos fiscais'
+class DocumentSerie(models.Model):
+    _name = 'fiscal.document.serie'
+    _description = 'Fiscal Document Serie'
 
     code = fields.Char(
-        string=u'Código',
+        string='Code',
         size=3,
         required=True)
 
     name = fields.Char(
-        string=u'Descrição',
+        string='Name',
         required=True)
 
-    active = fields.Boolean(string='Ativo')
+    active = fields.Boolean(
+        string='Active')
 
     fiscal_type = fields.Selection(
-        selection=PRODUCT_FISCAL_TYPE,
-        string=u'Tipo Fiscal',
-        default=PRODUCT_FISCAL_TYPE_DEFAULT)
+        selection=FISCAL_IN_OUT,
+        string=u'Type',
+        default=FISCAL_IN_OUT_DEFAULT)
 
     fiscal_document_id = fields.Many2one(
-        comodel_name='l10n_br_account.fiscal.document',
-        string='Documento Fiscal',
+        comodel_name='fiscal.document',
+        string='Fiscal Document',
         required=True)
 
     company_id = fields.Many2one(
         comodel_name='res.company',
-        string='Empresa',
+        string='Company',
         required=True)
 
     internal_sequence_id = fields.Many2one(
         comodel_name='ir.sequence',
-        string=u'Sequência Interna')
+        domain="[('company_id', '=', company_id)]",
+        string='Sequence')
 
     @api.model
-    def _create_sequence(self, vals):
+    def _create_sequence(self, values):
         """ Create new no_gap entry sequence for every
-         new document serie """
-        seq = {
+        new document serie """
+        sequence = {
             'name': vals['name'],
             'implementation': 'no_gap',
             'padding': 1,
             'number_increment': 1}
-        if 'company_id' in vals:
-            seq['company_id'] = vals['company_id']
-        return self.env['ir.sequence'].create(seq).id
+        if 'company_id' in values:
+            sequence['company_id'] = values['company_id']
+        return self.env['ir.sequence'].create(sequence).id
 
     @api.model
-    def create(self, vals):
+    def create(self, values):
         """ Overwrite method to create a new ir.sequence if
          this field is null """
-        if not vals.get('internal_sequence_id'):
-            vals.update({'internal_sequence_id': self._create_sequence(vals)})
-        return super(L10nBrAccountDocumentSerie, self).create(vals)
+        if not values.get('internal_sequence_id'):
+            values.update(
+                {'internal_sequence_id': self._create_sequence(values)})
+        return super(DocumentSerie, self).create(values)
