3a380a557b487c11d52bbd02a471220f1538b96a
Renato Lima <renato.lima@akretion.com.br>
2019-04-30 19:20:00 -0300

    [MIGR] Objeto CNAE

--- fiscal/models/cnae.py
+++ fiscal/models/cnae.py
@@ -1,52 +1,81 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# Copyright (C) 2009 Renato Lima - Akretion <renato.lima@akretion.com.br>
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields, api
+from odoo.osv import expression
+
+from odoo.addons.l10n_br_base.tools.misc import punctuation_rm
 
 
-class L10nBrAccountCNAE(models.Model):
-    """Classe para cadastro de Código Nacional de Atividade Econômica.
-
-        Os CNAEs são utilizados no cadastro de empresa para definir o
-    ramo de atividade primário e secundários das empresas cadastradas no Odoo.
-    """
-    _name = 'l10n_br_account.cnae'
-    _description = 'Cadastro de CNAE'
+class Cnae(models.Model):
+    _name = 'fiscal.cnae'
+    _description = 'CNAE'
 
     code = fields.Char(
-        string=u'Código',
+        string='Code',
         size=16,
         required=True)
 
+    code_unmasked = fields.Char(
+         string='Unmasked Code',
+         size=10,
+         compute='_compute_code_unmasked',
+         store=True,
+         index=True)
+
     name = fields.Char(
-        string=u'Descrição',
+        string='Description',
         size=64,
         required=True)
 
     version = fields.Char(
-        string=u'Versão',
+        string='Version',
         size=16,
         required=True)
 
     parent_id = fields.Many2one(
-        comodel_name='l10n_br_account.cnae',
-        string=u'CNAE Pai')
+        comodel_name='fiscal.cnae',
+        string='Parent CNAE')
 
     child_ids = fields.One2many(
-        comodel_name='l10n_br_account.cnae',
+        comodel_name='fiscal.cnae',
         inverse_name='parent_id',
-        string=u'CNAEs Filhos')
+        string='Children CNAEs')
 
     internal_type = fields.Selection(
-        [('view', u'Visualização'),
-         ('normal', 'Normal')],
-        u'Tipo Interno',
+        selection=[('view', u'View'),
+                   ('normal', 'Normal')],
+        string='Internal Type',
         required=True,
         default='normal')
+
+    _sql_constraints = [
+        ('fiscal_cnae_code_uniq', 'unique (code)',
+         'CNAE already exists with this code !')]
+
+    @api.depends('code')
+    def _compute_code_unmasked(self):
+        for r in self:
+            # TODO mask code and unmasck
+            r.code_unmasked = punctuation_rm(r.code)
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike',
+                     limit=100, name_get_uid=None):
+        args = args or []
+        domain = []
+        if name:
+            domain = ['|', ('code', operator, name + '%')
+                      ('code_unmasked', operator, name),
+                      ('name', operator, name)]
+
+        recs = self._search(expression.AND([domain, args]), limit=limit,
+                            access_rights_uid=name_get_uid)
+        return self.browse(recs).name_get()
 
     @api.multi
     def name_get(self):
         return [(r.id,
-                u"{0} - {1}".format(r.code, r.name))
+                 "{0} - {1}".format(r.code, r.name))
                 for r in self]
