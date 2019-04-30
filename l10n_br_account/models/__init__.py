29b6f7308c1fdc1ab728b7462a0e295ff1c4e3e4
Renato Lima <renato.lima@akretion.com.br>
2019-04-30 20:47:46 -0300

    Movido arquivo de Série de Documento Fiscal do módulo l10n_br_account para o fiscal

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -2,7 +2,6 @@
 from . import l10n_br_account
 from . import l10n_br_account_fiscal_category
 from . import l10n_br_account_service_type
-from . import l10n_br_account_fiscal_document_serie
 from . import l10n_br_account_fiscal_partner_type
 from . import l10n_br_account_tax_definition
 from . import account_fiscal_position_abstract
