93280704e8f4201f709fe0dd682fa7520d636e95
Renato Lima <renato.lima@akretion.com.br>
2019-04-30 19:42:18 -0300

    [MIGR] Objeto Tipo de Documento Fiscal

--- fiscal/models/document_type.py
+++ fiscal/models/document_type.py
@@ -1,22 +1,51 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# Copyright (C) 2009  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.osv import expression
+
+from .constants.fiscal import DOCUMENT_TYPE
 
 
-class L10nBrAccountFiscalDocument(models.Model):
-    _name = 'l10n_br_account.fiscal.document'
-    _description = 'Tipo de Documento Fiscal'
+class DocumentType(models.Model):
+    _name = 'fiscal.document.type'
+    _description = 'Fiscal Document Type'
 
     code = fields.Char(
-        string=u'Codigo',
+        string='Code',
         size=8,
         required=True)
 
     name = fields.Char(
-        string=u'Descrição',
-        size=64)
+        string='Name',
+        size=128,
+        required=True)
 
     electronic = fields.Boolean(
-        string=u'Eletrônico')
+        string='Is Eletronic')
+
+    type = fields.Selection(
+        selection=DOCUMENT_TYPE,
+        string='Document Type',
+        required=True)
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike',
+                     limit=100, name_get_uid=None):
+        args = args or []
+        domain = []
+        if name:
+            domain = ['|', ('code', operator, name + '%')
+                      ('code_unmasked', operator, name),
+                      ('name', operator, name)]
+
+        recs = self._search(expression.AND([domain, args]), limit=limit,
+                            access_rights_uid=name_get_uid)
+        return self.browse(recs).name_get()
+
+    @api.multi
+    def name_get(self):
+        return [(r.id,
+                 "{0} - {1}".format(r.code, r.name))
+                for r in self]
