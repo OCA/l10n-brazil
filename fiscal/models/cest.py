36e922730c0c44bcd059ccae12739c23ea22e3ad
Renato Lima <renato.lima@akretion.com.br>
2019-04-30 23:34:08 -0300

    [MIGR] Objeto CEST

--- fiscal/models/cest.py
+++ fiscal/models/cest.py
@@ -1,40 +1,94 @@
-# Copyright (C) 2013  Renato Lima - Akretion
+# Copyright (C) 2013  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields, api
+from odoo.osv import expression
+
+from odoo.addons.l10n_br_base.tools.misc import punctuation_rm
+
+from .constants.fiscal import CEST_SEGMENT
 
 
-class L10nBrAccountProductCest(models.Model):
-
-    _name = 'l10n_br_account_product.cest'
+class Cest(models.Model):
+    _name = 'fiscal.cest'
+    _order = 'code'
+    _description = 'CEST'
 
     code = fields.Char(
-        string=u'Código',
-        size=9)
+        string='Code',
+        required=True)
+
+    code_unmasked = fields.Char(
+         string='Unmasked Code',
+         size=10,
+         compute='_compute_code_unmasked',
+         store=True,
+         index=True)
 
     name = fields.Char(
-        string=u'Nome')
-
-    segment = fields.Char(
-        string=u'Segmento',
-        size=32)
+        string='Name',
+        required=True,
+        index=True)
 
     item = fields.Char(
-        string=u'Item',
-        size=4)
+        string='Item',
+        required=True,
+        size=6)
+
+    segment = fields.Selection(
+        selection=CEST_SEGMENT,
+        string='Segment',
+        required=True)
+
+    ncms = fields.Char(
+        string='NCM')
+
+    ncm_ids = fields.Many2many(
+        comodel_name='fiscal.ncm',
+        relation='fiscal_cest_ncm_rel',
+        colunm1='cest_id',
+        colunm2='ncm_id',
+        compute='_compute_ncms',
+        store=True,
+        readonly=True,
+        string='NCMs')
+
+    @api.depends('code')
+    def _compute_code_unmasked(self):
+        for r in self:
+            # TODO mask code and unmasck
+            r.code_unmasked = punctuation_rm(r.code)
+
+    @api.depends('ncms')
+    def _compute_ncms(self):
+        ncm_ids = self.env['fiscal.ncm']
+        for r in self:
+            if r.ncms:
+                ncms = r.ncms.split(",")
+                domain = ['|'] * (len(ncms) - 1)
+                domain += [('code_unmasked', '=', n)
+                          for n in ncms if len(n) == 8]
+                domain += [('code_unmasked', '=ilike', n + '%')
+                           for n in ncms if len(n) < 8]
+                ncm_ids = self.env['fiscal.ncm'].search(domain)
+                r.ncm_ids = ncm_ids
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def _name_search(self, name, args=None, operator='ilike',
+                     limit=100, name_get_uid=None):
         args = args or []
-        recs = self.browse()
+        domain = []
         if name:
-            recs = self.search([('code', operator, name)] + args, limit=limit)
-        if not recs:
-            recs = self.search([('name', operator, name)] + args, limit=limit)
-        return recs.name_get()
+            domain = ['|', ('code', operator, name + '%')
+                      ('code_unmasked', operator, name),
+                      ('name', operator, name)]
+
+        recs = self._search(expression.AND([domain, args]), limit=limit,
+                            access_rights_uid=name_get_uid)
+        return self.browse(recs).name_get()
 
     @api.multi
     def name_get(self):
         return [(r.id,
-                u"{0} - {1}".format(r.code, r.name))
+                 u"{0} - {1}".format(r.code, r.name))
                 for r in self]
