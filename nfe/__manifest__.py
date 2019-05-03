661ba3a7a79a4584b80ba611022510cd4cc3de07
Renato Lima <renato.lima@akretion.com.br>
2019-05-03 13:08:10 -0300

    [ADD] Criano módulo nfe para gestão da NFe

---
+++ nfe/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'NF-e',
+    'summary': 'Brazilian Eletronic Invoice NF-e .',
+    'category': 'Localisation',
+    'license': 'AGPL-3',
+    'author': 'Akretion, '
+              'Odoo Community Association (OCA)',
+    'website': 'http://github.com/OCA/l10n-brazil',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'fiscal'
+    ],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
