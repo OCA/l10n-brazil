873dfb664936223d296a1c8fe4340ee83fa81f90
Renato Lima <renato.lima@akretion.com.br>
2019-05-04 12:25:26 -0300

    Movido objeto wizard charts of accounts do modulo fiscal para o modulo l10n_br_account

--- fiscal/models/__init__.py
+++ fiscal/models/__init__.py
@@ -34,5 +34,4 @@ from . import simplified_tax_range
 # from . import account_fiscal_position
 # from . import account_fiscal_position_template
 # from . import res_country_state
-# from . import wizard_multi_charts_accounts
 # from . import account_payment_mode
