c7220123bb01117a8719b692f4653ed5f81a7e8d
Renato Lima <renato.lima@akretion.com.br>
2019-05-06 20:56:51 -0300

    Movido testes de NFe do módulo fiscal para o módulo nfe

---
+++ nfe/tests/__init__.py
@@ -0,0 +1,6 @@
+
+from . import test_account_customer_invoice
+from . import test_account_customer_nfe
+from . import test_account_supplier_nfe
+from . import test_account_nfe_refund
+from . import test_nfe_export
