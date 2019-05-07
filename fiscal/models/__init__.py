503c43cedf2a6bef7b4af6934b5c48b71f2a772e
Renato Lima <renato.lima@akretion.com.br>
2019-05-07 15:35:32 -0300

    Renomeado l10n_br_account_partner_fiscal_type.py para partner_profile

--- fiscal/models/__init__.py
+++ fiscal/models/__init__.py
@@ -19,9 +19,10 @@ from . import simplified_tax
 from . import simplified_tax_range
 from . import operation
 from . import operation_line
+from . import partner_profile
 from . import res_company
+
 # from . import l10n_br_account_product
-# from . import l10n_br_account_partner_fiscal_type
 # from . import l10n_br_account_product_document_related
 # from . import l10n_br_account_product_import_declaration
 # from . import l10n_br_account_product_import_declaration_line
