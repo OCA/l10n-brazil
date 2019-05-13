8a78c14b738309865b4c120faf971bc703f7bec9
Renato Lima <renato.lima@akretion.com.br>
2019-05-13 17:10:11 -0300

    Correção do search_name e sequencia do objeto de serie de documento fiscal e tipo de documento fiscal

--- fiscal/models/document_serie.py
+++ fiscal/models/document_serie.py
@@ -21,7 +21,8 @@ class DocumentSerie(models.Model):
         required=True)
 
     active = fields.Boolean(
-        string='Active')
+        string='Active',
+        default=True)
 
     fiscal_type = fields.Selection(
         selection=FISCAL_IN_OUT,
@@ -36,7 +37,9 @@ class DocumentSerie(models.Model):
     company_id = fields.Many2one(
         comodel_name='res.company',
         string='Company',
-        required=True)
+        required=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+                'fiscal.document.serie'))
 
     internal_sequence_id = fields.Many2one(
         comodel_name='ir.sequence',
@@ -48,7 +51,7 @@ class DocumentSerie(models.Model):
         """ Create new no_gap entry sequence for every
         new document serie """
         sequence = {
-            'name': vals['name'],
+            'name': values['name'],
             'implementation': 'no_gap',
             'padding': 1,
             'number_increment': 1}
