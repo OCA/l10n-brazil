01067487e2c3e5cd27748fa98c8da1002d2fd589
Luis Felipe Miléo <mileo@kmee.com.br>
2019-05-13 12:47:33 -0300

    [FIX] Typo

--- l10n_br_account_payment_mode/__manifest__.py
+++ l10n_br_account_payment_mode/__manifest__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-10 -*-
+# -*- coding: utf-8 -*-
 # Copyright (C) 2017-Today - KMEE (<http://kmee.com.br>).
 #  @author Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
