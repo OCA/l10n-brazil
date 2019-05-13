9c805863ca3fc5803c639493434472ae0f4d0bfb
Luis Felipe Mileo <mileo@kmee.com.br>
2019-05-13 10:51:39 -0300

    [REF] Renamming files with OCA guidelines

--- l10n_br_resource/models/resource_calendar_leaves.py
+++ l10n_br_resource/models/resource_calendar_leaves.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
-from openerp import fields, models
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
---
+++ l10n_br_resource/wizards/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import workalendar_holiday_import_wizard
