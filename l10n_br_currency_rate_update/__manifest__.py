2bffbcdda3fd3f64fe612c724df3f7a22ee95f95
Renato Lima <renato.lima@akretion.com>
2019-05-13 09:59:19 -0300

    [12.0][NEW] módulo para atualizar cotações de moedas (#683)
    * [ADD] oca_dependencies.txt
    
    * [ADD] l10n_br_currency_rate_update
    
    * [FIX] manifest and method _get_supported_currencies
    
    * Added license comment in __init__ files
    
    * remove comments em oca_depedencies.txt

---
+++ l10n_br_currency_rate_update/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2019 Renato Lima - Akretion <renato.lima@akretion.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Currency Rate Update BR',
+    'summary': 'Update exchange rates using OCA modules for Brazil',
+    'version': '12.0.1.0.0',
+    'author':
+        'Akretion, '
+        'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-brazil',
+    'license': 'AGPL-3',
+    'category': 'Financial Management/Configuration',
+    'depends': [
+        'currency_rate_update',
+    ],
+    'installable': True,
+}
