c55e749bbded1ddc49891f70052bab100f14dab6
Renato Lima <renato.lima@akretion.com>
2019-05-13 19:15:50 -0300

    Movido e renomeado arquivo de settings do módulo l10n_br_account para o módulo fiscal

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -8,7 +8,6 @@ from . import account_fiscal_position_abstract
 from . import account_fiscal_position_template
 from . import account_fiscal_position
 from . import res_company
-from . import res_config
 from . import account_journal
 from . import account_tax
 from . import account_tax_template
