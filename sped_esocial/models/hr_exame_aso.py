51b0354a8fe8e7bcc075c234c1ad0879295d2f10
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-05-16 15:34:13 -0300

    [ADD] Monitoramento de Saúde do Trabalhador
    - Modelo que compõe o registro S-2240 do e-Social

--- sped_esocial/models/hr_exame_aso.py
+++ sped_esocial/models/hr_exame_aso.py
@@ -7,15 +7,15 @@
 from openerp import api, fields, models
 
 ORDEM_EXAME = [
-    ('1', 'Inicial'),
-    ('2', 'Sequencial'),
+    (1, 'Inicial'),
+    (2, 'Sequencial'),
 ]
 
 IND_RESULTADO = [
-    ('1', 'Normal'),
-    ('2', 'Alterado'),
-    ('3', u'Estável'),
-    ('4', 'Agravamento'),
+    (1, 'Normal'),
+    (2, 'Alterado'),
+    (3, u'Estável'),
+    (4, 'Agravamento'),
 ]
 
 
@@ -33,32 +33,22 @@ class HrExameASO(models.Model):
     )
     data_exame = fields.Date(
         string=u'Data Exame',
-        help=u'Nome Layout: dtExm - Data do exame realizado.'
     )
     procedimento_realizado = fields.Many2one(
         string=u'Procedimento Realizado',
         comodel_name='sped.procedimentos_diagnosticos',
-        help=u'Nome Layout: procRealizado - Tamanho: Até 4 Caracteres - Código'
-             u' do procedimento diagnóstico constante da Tabela 27.'
     )
     obs_procedimento = fields.Text(
         string=u'Observações',
         size=999,
-        help=u'Nome Layout: obsProc - Tamanho: Até 999 Caracteres - Observação'
-             u' sobre o procedimento diagnóstico realizado.'
     )
     ordem_exame = fields.Selection(
         string=u'Ordem',
         selection=ORDEM_EXAME,
-        help=u'Nome Layout: ordExame - Tamanho: Até 1 Caracteres - Ordem do'
-             u' Exame: 1 - Inicial; 2 - Sequencial.',
     )
     indicacao_resultado = fields.Selection(
         string=u'Indicação do Resultado',
         selection=IND_RESULTADO,
-        help=u'Nome Layout: indResult - Tamanho: Até 1 Caracteres - Indicação'
-             u' dos Resultados: 1 - Normal; 2 - Alterado; '
-             u'3 - Estável; 4 - Agravamento.',
     )
     hr_saude_trabalhador_id = fields.Many2one(
         comodel_name='hr.saude.trabalhador',
@@ -71,11 +61,3 @@ class HrExameASO(models.Model):
                 record.procedimento_realizado.nome, record.data_exame,
                 record.contract_id.name
             )
-
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form',
-                        toolbar=False, submenu=False):
-        res = super(HrExameASO, self).fields_view_get(
-            view_id, view_type, toolbar=toolbar, submenu=False)
-
-        return res
