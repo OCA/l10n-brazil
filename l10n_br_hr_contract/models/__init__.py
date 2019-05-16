1ad0baf5d75ee10c386f7273f6b981807c417aec
Luis Felipe Mileo <mileo@kmee.com.br>
2019-05-16 17:19:28 -0300

    [REF] Separa classes do contrato em arquivos dedicados
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_hr_contract/models/__init__.py
+++ l10n_br_hr_contract/models/__init__.py
@@ -3,3 +3,9 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import hr_contract
+from . import hr_contract_admission_type
+from . import hr_contract_labor_bond_type
+from . import hr_contract_labor_regime
+from . import hr_contract_labor_resignation_cause
+from . import hr_contract_labor_salary_unit
+from . import hr_contract_notice_termination
