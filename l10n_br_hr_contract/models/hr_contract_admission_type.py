35578e2d8ca48ec958f5239c4cb904338d87fc1f
Luis Felipe Mileo <mileo@kmee.com.br>
2019-05-16 17:19:28 -0300

    [REF] Separa classes do contrato em arquivos dedicados
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ l10n_br_hr_contract/models/hr_contract_admission_type.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016  Daniel Sadamo - KMEE Informática
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields, api
+
+
+class HrContractAdmissionType(models.Model):
+    _name = 'hr.contract.admission.type'
+
+    name = fields.Char(string='Admission type')
+    code = fields.Char(string='Code')
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record['name']
+            if record['code']:
+                name = record['code'] + ' - ' + name
+            result.append((record['id'], name))
+        return result
---
+++ l10n_br_hr_contract/models/hr_contract_labor_regime.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016  Daniel Sadamo - KMEE Informática
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields, api
+
+
+class HrContractLaborRegime(models.Model):
+    _name = 'hr.contract.labor.regime'
+
+    name = fields.Char(string='Labor regime')
+    short_name = fields.Char(string='Short name')
+    code = fields.Char(string='Code', size=1)
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record['name']
+            if record['short_name']:
+                name = record['short_name'] + ' - ' + name
+            result.append((record['id'], name))
+        return result
