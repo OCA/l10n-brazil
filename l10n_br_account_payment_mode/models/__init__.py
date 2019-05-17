4d362ae43e272f4ab160215a1e80b2052d9581fb
Magno Costa <magno.costa@akretion.com.br>
2019-05-17 18:26:14 -0300

    [REF] Deleted module l10n_br_account_payment_mode because files were moved to l10n_br_account_payment_order.

--- l10n_br_account_payment_mode/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017-Today - KMEE (<http://kmee.com.br>).
-#  @author Luis Felipe Miléo - mileo@kmee.com.br
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import payment_mode
-from . import res_partner_bank
