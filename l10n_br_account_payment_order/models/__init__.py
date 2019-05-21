6f60629f003bbb23e5eb9584141719ab1b6007a5
Magno Costa <magno.costa@akretion.com.br>
2019-05-21 12:21:12 -0300

    [REF] Renamed files to the standard.

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -3,7 +3,7 @@
 #  Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_payment
+from . import account_payment_line
 from . import account_move_line
-from . import payment_mode
+from . import account_payment_mode
 from . import res_partner_bank
