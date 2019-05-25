19216f38fbfaa8038dc98e7900a56e410794ac9e
Renato Lima <renato.lima@akretion.com.br>
2019-05-25 16:00:20 -0300

    Definido o módulo l10n_br_account como instalavel e refatoramento dos objetos de impostos

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -1,18 +1,16 @@
 
-from . import l10n_br_account
-from . import l10n_br_account_fiscal_partner_type
-from . import l10n_br_account_tax_definition
+from . import account_tax_abstract
+from . import account_tax_template
+from . import account_tax
+from . import fiscal_tax
 from . import account_fiscal_position_abstract
 from . import account_fiscal_position_template
 from . import account_fiscal_position
 from . import res_company
 from . import account_journal
-from . import account_tax
-from . import account_tax_template
 from . import account_invoice
 from . import account_invoice_line
 from . import product_template
 from . import operation
-from . import account_fiscal_position_rule
 from . import account_move_line
-from . import wizard_multi_charts_accounts
+# from . import wizard_multi_charts_accounts
