43935819ada3fb2cec184d2634205c33d4f62568
Renato Lima <renato.lima@akretion.com.br>
2019-05-25 15:49:48 -0300

    Atualizado tipos de serviços

--- fiscal/models/service_type.py
+++ fiscal/models/service_type.py
@@ -2,12 +2,12 @@
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ServiceType(models.Model):
     _name = 'fiscal.service.type'
-    _inherit = 'fiscal.data.abstract'
+    _inherit = ['fiscal.data.abstract', 'mail.thread', 'mail.activity.mixin']
     _description = 'Service Fiscal Type'
 
     parent_id = fields.Many2one(
@@ -25,3 +25,20 @@ class ServiceType(models.Model):
         string='Internal Type',
         required=True,
         default='normal')
+
+    product_tmpl_ids = fields.One2many(
+        comodel_name='product.template',
+        string='Products',
+        compute='_compute_product_tmpl_info')
+
+    product_tmpl_qty = fields.Integer(
+        string='Products Quantity',
+        compute='_compute_product_tmpl_info')
+
+    @api.one
+    def _compute_product_tmpl_info(self):
+        product_tmpls = self.env['product.template'].search([
+            ('service_type_id', '=', self.id), '|',
+            ('active', '=', False), ('active', '=', True)])
+        self.product_tmpl_ids = product_tmpls
+        self.product_tmpl_qty = len(product_tmpls)
