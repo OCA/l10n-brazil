879e995b684b4e3a6ed13373f3a9e81a05c3cb7f
Renato Lima <renato.lima@akretion.com.br>
2019-05-25 16:00:20 -0300

    Definido o módulo l10n_br_account como instalavel e refatoramento dos objetos de impostos

--- l10n_br_account/models/fiscal_tax.py
+++ l10n_br_account/models/fiscal_tax.py
@@ -1,9 +1,8 @@
-# Copyright (C) 2009  Gabriel C. Stabel
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models
 
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+class FiscalTax(models.Model):
+    _inherit = 'fiscal.tax'
