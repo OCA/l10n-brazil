096891de2e5d5719db0c958ea5438c8e794c01ba
Renato Lima <renato.lima@akretion.com>
2019-05-25 16:00:20 -0300

    Definido o módulo l10n_br_account como instalavel e refatoramento dos objetos de impostos

--- l10n_br_account/models/account_fiscal_position_abstract.py
+++ l10n_br_account/models/account_fiscal_position_abstract.py
@@ -3,40 +3,9 @@
 
 from odoo import fields, api
 
-from .l10n_br_account import TYPE
-
 
 class AccountFiscalPositionAbstract(object):
-
-    fiscal_category_id = fields.Many2one(
-        comodel_name='l10n_br_account.fiscal.category',
-        string=u'Categoria Fiscal')
-
-    type = fields.Selection(
-        selection=TYPE,
-        related='fiscal_category_id.type',
-        readonly=True,
-        store=True,
-        string=u'Fiscal Type')
-
-    inv_copy_note = fields.Boolean(
-        string=u'Copiar Observação na Nota Fiscal')
-
-    asset_operation = fields.Boolean(
-        string=u'Operação de Aquisição de Ativo',
-        help=u"Caso seja marcada essa opção, será "
-             "incluido o IPI na base de calculo do ICMS.")
-
-    state = fields.Selection(
-        selection=[('draft', u'Rascunho'),
-                   ('review', u'Revisão'),
-                   ('approved', u'Aprovada'),
-                   ('unapproved', u'Não Aprovada')],
-        string=u'Status',
-        readonly=True,
-        track_visibility='onchange',
-        index=True,
-        default='draft')
+    pass
 
 
 class AccountFiscalPositionTaxAbstract(object):
