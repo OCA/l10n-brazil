ea217e922983f2fb1564fbcc36abfac743028ed7
Magno Costa <magno.costa@akretion.com.br>
2019-05-28 18:43:31 -0300

    [REF] Removed fields because we should use logical inbound/outbound instead of 'cobranca' and CNAB related fields should be implemented in the below module.

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -7,5 +7,3 @@ from . import account_payment_line
 from . import account_move_line
 from . import account_payment_mode
 from . import res_partner_bank
-from . import account_payment_method
-from . import account_payment_order
