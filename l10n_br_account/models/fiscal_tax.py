4f5de6561b69359447b7f31ad9c243a0ad27aff8
Renato Lima <renato.lima@akretion.com.br>
2019-05-30 12:59:56 -0300

    [MIGR] Migrado os impostos e modelo de impostos

--- l10n_br_account/models/fiscal_tax.py
+++ l10n_br_account/models/fiscal_tax.py
@@ -1,8 +1,14 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import models, api
 
 
 class FiscalTax(models.Model):
     _inherit = 'fiscal.tax'
+
+    @api.model
+    def create(self, values):
+        if not values.get('partner_id'):
+            self.clear_caches()
+            return super(FiscalTax, self).create(values)
