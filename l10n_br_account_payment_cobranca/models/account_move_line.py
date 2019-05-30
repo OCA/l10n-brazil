4b85b17007531d8cf7f6c6aeb5c2afd2b000da59
Luis Felipe Mileo <mileo@kmee.com.br>
2019-05-30 03:09:48 -0300

    [WIP] Geração do PDF
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account_payment_cobranca/models/account_move_line.py
+++ l10n_br_account_payment_cobranca/models/account_move_line.py
@@ -44,41 +44,41 @@ class AccounMoveLine(models.Model):
         u'Nosso Número', readonly=True)
 
     @api.multi
-    def send_payment(self):
+    def generate_boleto(self):
         boleto_list = []
 
         for move_line in self:
-            try:
+            # try:
 
-                if move_line.payment_mode_id.type_payment == '00':
-                    number_type = move_line.company_id.own_number_type
-                    if not move_line.boleto_own_number:
-                        if number_type == '0':
-                            nosso_numero = self.env['ir.sequence'].next_by_id(
-                                move_line.company_id.own_number_sequence.id)
-                        elif number_type == '1':
-                            nosso_numero = \
-                                move_line.transaction_ref.replace('/', '')
-                        else:
-                            nosso_numero = self.env['ir.sequence'].next_by_id(
-                                move_line.payment_mode_id.
-                                internal_sequence_id.id
-                            )
-                    else:
-                        nosso_numero = move_line.boleto_own_number
+                # if move_line.payment_mode_id.type_payment == '00':
+                #     number_type = move_line.company_id.own_number_type
+                #     if not move_line.boleto_own_number:
+                #         if number_type == '0':
+                #             nosso_numero = self.env['ir.sequence'].next_by_id(
+                #                 move_line.company_id.own_number_sequence.id)
+                #         elif number_type == '1':
+                #             nosso_numero = \
+                #                 move_line.transaction_ref.replace('/', '')
+                #         else:
+                #             nosso_numero = self.env['ir.sequence'].next_by_id(
+                #                 move_line.payment_mode_id.
+                #                 internal_sequence_id.id
+                #             )
+                #     else:
+                #         nosso_numero = move_line.boleto_own_number
 
-                    boleto = Boleto.getBoleto(move_line, nosso_numero)
-                    if boleto:
-                        move_line.date_payment_created = date.today()
-                        move_line.transaction_ref = \
-                            boleto.boleto.format_nosso_numero()
-                        move_line.boleto_own_number = nosso_numero
+            boleto = Boleto.getBoleto(move_line, move_line.id)
+                # if boleto:
+                    # move_line.date_payment_created = date.today()
+                    # move_line.transaction_ref = \
+                    #     boleto.boleto.format_nosso_numero()
+                    # move_line.boleto_own_number = nosso_numero
 
-                    boleto_list.append(boleto.boleto)
-            except BoletoException as be:
-                _logger.error(be.message or be.value, exc_info=True)
-                continue
-            except Exception as e:
-                _logger.error(e.message or e.value, exc_info=True)
-                continue
+            boleto_list.append(boleto.boleto)
+            # except BoletoException as be:
+            #     _logger.error(be.message or be.value, exc_info=True)
+            #     continue
+            # except Exception as e:
+            #     _logger.error(e.message or e.value, exc_info=True)
+            #     continue
         return boleto_list
