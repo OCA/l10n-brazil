e2e66d6708b9da02e8eb41e230baf40dd4159a20
Luis Felipe Mileo <mileo@kmee.com.br>
2019-05-30 13:57:13 -0300

    [NEW] Boleto Itau 400
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account_payment_cobranca/models/account_invoice.py
+++ l10n_br_account_payment_cobranca/models/account_invoice.py
@@ -29,13 +29,15 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def action_move_create(self):
+        result = super(AccountInvoice, self).action_move_create()
 
-        for invoice in self:
-            if invoice.company_id.own_number_type == SEQUENCIAL_EMPRESA:
-                sequence = invoice.company_id.get_own_number_sequence()
-            elif invoice.company_id.own_number_type == SEQUENCIAL_FATURA:
-                sequence = invoice.get_invoice_fiscal_number()
-            elif invoice.company_id.own_number_type == SEQUENCIAL_CARTEIRA:
+        for inv in self:
+            # Verificar se é boleto para criar o numero
+            if inv.company_id.own_number_type == SEQUENCIAL_EMPRESA:
+                sequence = inv.company_id.get_own_number_sequence()
+            elif inv.company_id.own_number_type == SEQUENCIAL_FATURA:
+                sequence = inv.get_invoice_fiscal_number()
+            elif inv.company_id.own_number_type == SEQUENCIAL_CARTEIRA:
                 # TODO: Implementar uma sequencia na carteira de cobranca
                 raise NotImplementedError
             else:
@@ -44,27 +46,39 @@ class AccountInvoice(models.Model):
                       u"empresa para determinar o tipo de sequencia utilizada"
                       u" nas cobrancas")
                 )
+            inv.transaction_id = sequence
+            for index, interval in enumerate(inv.move_line_receivable_id):
+                interval.transaction_ref = (
+                    inv.transaction_id + '/' + str(index+1)
+                )
 
-            invoice.transaction_id = sequence
-        return super(AccountInvoice, self).action_move_create()
+        return result
+
 
     @api.multi
-    def finalize_invoice_move_lines(self, move_lines):
-        """ Propagate the transaction_id from the invoice to the move lines.
+    def invoice_validate(self):
+        result = super(AccountInvoice, self).invoice_validate()
+        self.create_account_payment_line()
+        return result
 
-        The transaction id is written on the move lines only if the account is
-        the same than the invoice's one.
-        """
-        move_lines = super(AccountInvoice, self).finalize_invoice_move_lines(
-            move_lines)
-        for invoice in self:
-            if invoice.transaction_id:
-                invoice_account_id = invoice.account_id.id
-                index = 1
-                for line in move_lines:
-                    # line is a tuple (0, 0, {values})
-                    if invoice_account_id == line[2]['account_id']:
-                        line[2]['transaction_ref'] = u'{0}/{1:02d}'.format(
-                            invoice.transaction_id, index)
-                        index += 1
-        return move_lines
+
+    # @api.multi
+    # def finalize_invoice_move_lines(self, move_lines):
+    #     """ Propagate the transaction_id from the invoice to the move lines.
+    #
+    #     The transaction id is written on the move lines only if the account is
+    #     the same than the invoice's one.
+    #     """
+    #     move_lines = super(AccountInvoice, self).finalize_invoice_move_lines(
+    #         move_lines)
+    #     for invoice in self:
+    #         if invoice.transaction_id:
+    #             invoice_account_id = invoice.account_id.id
+    #             index = 1
+    #             for line in move_lines:
+    #                 # line is a tuple (0, 0, {values})
+    #                 if invoice_account_id == line[2]['account_id']:
+    #                     line[2]['transaction_ref'] = u'{0}/{1:02d}'.format(
+    #                         invoice.transaction_id, index)
+    #                     index += 1
+    #     return move_lines
