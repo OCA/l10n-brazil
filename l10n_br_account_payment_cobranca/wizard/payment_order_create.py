4d6c591f543aa27a2ffd56f189cc869aa1e8a923
Luis Felipe Mileo <mileo@kmee.com.br>
2019-06-04 15:34:08 -0300

    [REF] Filtro para permitir a importação de registros com problema
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account_payment_cobranca/wizard/payment_order_create.py
+++ l10n_br_account_payment_cobranca/wizard/payment_order_create.py
@@ -27,17 +27,86 @@ class PaymentOrderCreate(models.TransientModel):
 
         :return:
         """
-        domain = super(PaymentOrderCreate, self)._prepare_move_line_domain()
+        # TODO: Refactory this to call super!
 
+        self.ensure_one()
+        domain = [('reconciled', '=', False),
+                  ('company_id', '=', self.order_id.company_id.id)]
+        if self.journal_ids:
+            domain += [('journal_id', 'in', self.journal_ids.ids)]
+        if self.partner_ids:
+            domain += [('partner_id', 'in', self.partner_ids.ids)]
+        if self.target_move == 'posted':
+            domain += [('move_id.state', '=', 'posted')]
+        if not self.allow_blocked:
+            domain += [('blocked', '!=', True)]
+        if self.date_type == 'due':
+            domain += [
+                '|',
+                ('date_maturity', '<=', self.due_date),
+                ('date_maturity', '=', False)]
+        elif self.date_type == 'move':
+            domain.append(('date', '<=', self.move_date))
+        if self.invoice:
+            domain.append(('invoice_id', '!=', False))
+        if self.payment_mode:
+            if self.payment_mode == 'same':
+                domain.append(
+                    ('payment_mode_id', '=', self.order_id.payment_mode_id.id))
+            elif self.payment_mode == 'same_or_null':
+                domain += [
+                    '|',
+                    ('payment_mode_id', '=', False),
+                    ('payment_mode_id', '=', self.order_id.payment_mode_id.id)]
+
+        if self.order_id.payment_type == 'outbound':
+            # For payables, propose all unreconciled credit lines,
+            # including partially reconciled ones.
+            # If they are partially reconciled with a supplier refund,
+            # the residual will be added to the payment order.
+            #
+            # For receivables, propose all unreconciled credit lines.
+            # (ie customer refunds): they can be refunded with a payment.
+            # Do not propose partially reconciled credit lines,
+            # as they are deducted from a customer invoice, and
+            # will not be refunded with a payment.
+            domain += [
+                ('credit', '>', 0),
+                #  '|',
+                ('account_id.internal_type', '=', 'payable'),
+                #  '&',
+                #  ('account_id.internal_type', '=', 'receivable'),
+                #  ('reconcile_partial_id', '=', False),  # TODO uncomment
+            ]
+        elif self.order_id.payment_type == 'inbound':
+            domain += [
+                ('debit', '>', 0),
+                ('account_id.internal_type', '=', 'receivable'),
+            ]
+        # Exclude lines that are already in a non-cancelled
+        # and non-uploaded payment order; lines that are in a
+        # uploaded payment order are proposed if they are not reconciled,
         paylines = self.env['account.payment.line'].search([
             ('state', 'in', ('draft', 'open', 'generated', 'uploaded')),
             ('move_line_id', '!=', False)])
 
-        if paylines:
-            move_lines_ids = [
-                payline.move_line_id.id for payline in paylines
-                if payline.move_line_id.state_cnab == 'not_accepted'
-            ]
-            domain += [('id', 'not in', move_lines_ids)]
+        move_line_domain = ['draft']
+        if self.allow_error:
+            move_line_domain.append('exporting_error')
+        if self.allow_rejected:
+            move_line_domain.append('not_accepted')
 
+        domain += [
+            ('bank_payment_line_id', '=', False),
+        ]
+
+        domain += [
+            ('state_cnab', 'in', move_line_domain),
+        ]
+
+        # if paylines:
+        #     move_lines_ids = [
+        #         payline.move_line_id.id for payline in paylines
+        #     ]
+        #     domain += [('id', 'not in', move_lines_ids)]
         return domain
