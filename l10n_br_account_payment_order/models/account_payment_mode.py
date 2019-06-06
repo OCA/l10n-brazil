8c42189f388b0c784a50e14c4fa4ab9c44d4c2e0
Magno Costa <magno.costa@akretion.com.br>
2019-06-06 16:14:52 -0300

    [12.0][MIG] Removed code unnecessary on python 3.

--- l10n_br_account_payment_order/models/account_payment_mode.py
+++ l10n_br_account_payment_order/models/account_payment_mode.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2012-Today - KMEE (<http://kmee.com.br>).
 #  @author Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -9,13 +8,13 @@ from odoo import models, fields
 class AccountPaymentMode(models.Model):
     _inherit = 'account.payment.mode'
 
-    internal_sequence_id = fields.Many2one('ir.sequence', u'Sequência')
-    instrucoes = fields.Text(u'Instruções de cobrança')
+    internal_sequence_id = fields.Many2one('ir.sequence', 'Sequência')
+    instrucoes = fields.Text('Instruções de cobrança')
     invoice_print = fields.Boolean(
-        u'Gerar relatorio na conclusão da fatura?')
+        'Gerar relatorio na conclusão da fatura?')
 
     _sql_constraints = [
         ('internal_sequence_id_unique',
          'unique(internal_sequence_id)',
-         u'Sequência já usada! Crie uma sequência unica para cada modo')
+         'Sequência já usada! Crie uma sequência unica para cada modo')
     ]
