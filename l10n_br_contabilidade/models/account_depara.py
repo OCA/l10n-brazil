d16f8f6670999a4c93b2201bfe34b373b05224e4
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-06-25 16:52:20 -0300

    [ADD] Checar se ja existe depara para conta referencia

--- l10n_br_contabilidade/models/account_depara.py
+++ l10n_br_contabilidade/models/account_depara.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
+from openerp.exceptions import Warning
 
 
 class AccountDePara(models.Model):
@@ -42,3 +43,21 @@ class AccountDePara(models.Model):
                 record.account_depara_plano_id.name,
                 record.conta_referencia_id.display_name,
             )
+
+    @api.constrains('conta_referencia_id')
+    def check_conta_referencia_id(self):
+        """
+        Verificar a existência de um DePara para contas referencias
+        """
+        for record in self:
+
+            depara_id = self.search([
+                ('id', '!=', record.id),
+                ('conta_referencia_id','=', record.conta_referencia_id.id),
+            ])
+
+            if depara_id:
+                raise Warning(
+                    'Conta já cadastrada no depara!\n'
+                    'A conta já representa as contas: \n {}'.format(
+                        '\n'.join(depara_id.conta_sistema_id.mapped('code'))))
