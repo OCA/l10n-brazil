cfe819aea25260cce1c957b6f43e679ce94f73ab
Magno Costa <magno.costa@akretion.com.br>
2019-07-11 14:10:07 -0300

    [REF] Removed l10n_br_zip.result object.

--- l10n_br_zip/wizard/l10n_br_zip_search.py
+++ l10n_br_zip/wizard/l10n_br_zip_search.py
@@ -35,7 +35,7 @@ class L10nBrZipSearch(models.TransientModel):
         domain="[('state_id', '=', state_id)]")
 
     zip_ids = fields.Many2many(
-        comodel_name='l10n_br.zip.result',
+        comodel_name='l10n_br.zip',
         relation='zip_search',
         column1='zip_search_id',
         column2='zip_id',
@@ -57,13 +57,6 @@ class L10nBrZipSearch(models.TransientModel):
         string='Nome do Objeto',
         size=100,
         invisible=True)
-
-    @api.model
-    def create(self, vals):
-        result = super(L10nBrZipSearch, self).create(vals)
-        # TO DO : FIX IT
-        #  context.update({'search_id': result})
-        return result
 
     @api.model
     def default_get(self, fields_list):
@@ -89,7 +82,6 @@ class L10nBrZipSearch(models.TransientModel):
 
         data = self
         obj_zip = self.env['l10n_br.zip']
-        obj_zip_result = self.env['l10n_br.zip.result']
 
         domain = obj_zip._set_domain(
             country_id=data.country_id.id,
@@ -101,12 +93,11 @@ class L10nBrZipSearch(models.TransientModel):
         )
 
         # Search zips
-        zips = obj_zip.search(domain)
-        # MAP zip to zip.search.result
-        zip_result_ids = obj_zip_result.map_to_zip_result(
-            zips.ids, data['object_name'], data['address_id'])
-        self.write(
-            {'state': 'done', 'zip_ids': [[6, 0, zip_result_ids]]})
+        zip_ids = obj_zip.search(domain)
+        self.write({
+            'state': 'done',
+            'zip_ids': [[6, 0, [zip.id for zip in zip_ids]]]
+        })
 
         return {
             'type': 'ir.actions.act_window',
@@ -136,100 +127,3 @@ class L10nBrZipSearch(models.TransientModel):
             'target': 'new',
             'nodestroy': True
         }
-
-
-class L10nBrZipResult(models.TransientModel):
-    _name = 'l10n_br.zip.result'
-    _description = 'Zipcode result'
-
-    zip_id = fields.Many2one(
-        comodel_name='l10n_br.zip',
-        string='Zipcode',
-        readonly=True,
-        invisible=True)
-
-    search_id = fields.Many2one(
-        comodel_name='l10n_br.zip.search',
-        string='Search',
-        readonly=True,
-        invisible=True)
-
-    address_id = fields.Integer(
-        string='Id do objeto',
-        invisible=True)
-
-    object_name = fields.Char(
-        string='Nome do objeto',
-        size=100,
-        invisible=True)
-
-    # ZIPCODE data to be shown
-    zip = fields.Char(
-        string='CEP',
-        size=9,
-        readonly=True)
-
-    street = fields.Char(
-        string='Logradouro',
-        size=72,
-        readonly=True)
-
-    street_type = fields.Char(
-        string='Tipo',
-        size=26,
-        readonly=True)
-
-    district = fields.Char(
-        string='District',
-        size=72,
-        readonly=True)
-
-    country_id = fields.Many2one(
-        comodel_name='res.country',
-        string='Country',
-        readonly=True)
-
-    state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        string='State',
-        domain="[('country_id', '=', country_id)]",
-        readonly=True)
-
-    city_id = fields.Many2one(
-        comodel_name='res.city',
-        string='City',
-        required=True,
-        domain="[('state_id', '=', state_id)]",
-        readonly=True)
-
-    def map_to_zip_result(self, zip_data, object_name, address_id):
-        obj_zip = self.env['l10n_br.zip']
-        result = []
-
-        for zip_read in zip_data:
-            if type(zip_read) is int:
-                zip_obj = self.env['l10n_br.zip'].browse(zip_read)
-            else:
-                zip_obj = zip_read
-
-            zip_data = obj_zip.set_result(zip_obj)
-            zip_result_data = zip_data
-            zip_result_data['object_name'] = object_name
-            zip_result_data['address_id'] = address_id
-            del zip_result_data['city']
-
-            zip_result_id = self.create(zip_result_data)
-            result.append(zip_result_id.id)
-        return result
-
-    @api.one
-    def zip_select(self):
-        data = self
-        address_id = data['address_id']
-        object_name = data['object_name']
-        if address_id and object_name:
-            obj = self.env[object_name].browse(address_id)
-            obj_zip = self.env['l10n_br.zip']
-            result = obj_zip.set_result(data)
-            obj.write(result)
-        return True
