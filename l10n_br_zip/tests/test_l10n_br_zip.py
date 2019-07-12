d5c1465872ce99d497de55180f6867cff70eee76
Magno Costa <magno.costa@akretion.com.br>
2019-07-12 14:31:14 -0300

    [REF][IMP] Tests.

--- l10n_br_zip/tests/test_l10n_br_zip.py
+++ l10n_br_zip/tests/test_l10n_br_zip.py
@@ -10,56 +10,63 @@ class L10nBRZipTest(TransactionCase):
 
     def setUp(self):
         super(L10nBRZipTest, self).setUp()
+
         self.zip_obj = self.env['l10n_br.zip']
         self.zip_1 = self.zip_obj.create(dict(
-            zip_code='88032050',
-            city_id=self.env.ref('l10n_br_base.city_4205407').id,
-            state_id=self.env.ref('base.state_br_sc').id,
+            zip_code='01310923',
+            city_id=self.env.ref('l10n_br_base.city_3550308').id,
+            state_id=self.env.ref('base.state_br_sp').id,
             country_id=self.env.ref('base.br').id,
-            street='Donicia',
-            street_type='Rua',
-            district='centro',
+            street='Avenida Paulista 1842',
+            street_type='Avenida',
+            district='Bela Vista',
         ))
         self.company = self.env.ref('base.main_company')
         self.company_1 = self.env['res.company'].create(dict(
             name='teste',
-            street='donicia',
+            street='paulista',
             country_id=self.env.ref('base.br').id,
-            state_id=self.env.ref('base.state_br_sc').id,
-            city_id=self.env.ref('l10n_br_base.city_4205407').id,
+            state_id=self.env.ref('base.state_br_sp').id,
+            city_id=self.env.ref('l10n_br_base.city_3550308').id,
         ))
 
     def test_search_zip_code_company(self):
-        self.company.zip = '88032050'
+        """Test search zip code."""
+        self.company.zip = '01310923'
         self.company.zip_search()
         self.assertEquals(
-            self.company.district, 'centro',
+            self.company.district, 'Bela Vista',
             'Error in method zip_search to mapping field district.')
         self.assertEquals(
-            self.company.street, 'Rua Donicia',
+            self.company.street, 'Avenida Avenida Paulista 1842',
             'Error in method zip_search to mapping field street.')
         self.assertEquals(
-            self.company.city_id.name, u'Florianópolis',
+            self.company.city_id.name, u'São Paulo',
             'Error in method zip_search to mapping field city.')
 
     def test_search_zip_code_by_other_fields_company(self):
+        """Test search by address."""
         self.company_1.zip_search()
         self.assertEquals(
-            self.company_1.zip, '88032-050',
+            self.company_1.zip, '01310-923',
             'Error in method zip_search to mapping zip code from fields'
             'country, state, city and street.')
 
     def test_return_two_results_zip_search(self):
-        self.zip_1 = self.zip_obj.create(dict(
-            zip_code='88032040',
-            city_id=self.env.ref('l10n_br_base.city_4205407').id,
-            state_id=self.env.ref('base.state_br_sc').id,
+        """Test search by address return more the one result."""
+        self.zip_2 = self.zip_obj.create(dict(
+            zip_code='01310940',
+            city_id=self.env.ref('l10n_br_base.city_3550308').id,
+            state_id=self.env.ref('base.state_br_sp').id,
             country_id=self.env.ref('base.br').id,
-            street='Donicia Maria da Costa',
-            street_type='Rua',
-            district='Cacupe',
+            street='Avenida Paulista 900',
+            street_type='Avenida',
+            district='Bela Vista',
         ))
         result = self.company_1.zip_search()
+        obj_zip_search = self.env[
+            'l10n_br.zip.search'].browse(result.get('res_id'))
+
         self.assertEquals(
             result['type'], 'ir.actions.act_window',
             'It should return a action when there are more than one result')
@@ -67,25 +74,54 @@ class L10nBRZipTest(TransactionCase):
             result['res_model'], 'l10n_br.zip.search',
             'It should return the model zip.search')
         self.assertEquals(
-            result['context']['street'], 'donicia',
+            obj_zip_search.street, 'paulista',
             'It should return the correct street')
         self.assertEquals(
-            result['context']['state_id'], self.env.ref('base.state_br_sc').id,
+            obj_zip_search.state_id.id, self.env.ref('base.state_br_sp').id,
             'It should return the correct state')
+        self.assertEquals(
+            len(obj_zip_search.zip_ids), 2,
+            'It should return two Zip Codes.')
+
+        # Test button/method 'New Search'
+        obj_zip_search.zip_new_search()
+        obj_zip_search.street = '900'
+        result = obj_zip_search.zip_search()
+
+        self.assertEquals(
+            len(obj_zip_search.zip_ids), 1,
+            'It should return one Zip Codes.')
+
+        # Test button/method 'Zip Select'
+
+        #  This code obj_zip_search.zip_ids.zip_select() doesn't work.
+        #  In the tests context return empty, by this reason method
+        #  zip_select don't receive fields address_id and object_name, what
+        #  makes tests failed without with_context parameter. This problem
+        #  only happening in the tests on screen doesn't.
+
+        obj_zip_search.zip_ids.with_context(result.get('context')).zip_select()
+
+        self.assertEquals(
+            self.company_1.zip, '01310-940',
+            'It should return the correct zip, failed method zip_select.')
+        self.assertEquals(
+            self.company_1.street, 'Avenida Avenida Paulista 900',
+            'It should return the correct street, failed method zip_select.')
 
     def test_return_pycep_correios(self):
-        """Test PyCEP CORREIOS"""
-        self.company.zip = '08746070'
+        """Test search with PyCEP CORREIOS"""
+        self.company.zip = '04003000'
         self.company.zip_search()
         self.assertEquals(
-            self.company.district, 'Parque Olimpico',
+            self.company.district, 'Paraíso',
             'Error in method zip_search with PyCEP-Correios'
             'to mapping field district.')
         self.assertEquals(
-            self.company.street, 'Rua N',
+            self.company.street, 'Rua Coronel Oscar Porto',
             'Error in method zip_search with PyCEP-Correios'
             'to mapping field street.')
         self.assertEquals(
-            self.company.city_id.name, u'Mogi das Cruzes',
+            self.company.city_id.name, u'São Paulo',
             'Error in method zip_search with PyCEP-Correios'
             'to mapping field city.')
