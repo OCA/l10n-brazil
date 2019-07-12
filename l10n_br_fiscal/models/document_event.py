a19b0935d21eabc5041fd7802019756fbfb9091b
Renato Lima <renato.lima@akretion.com.br>
2019-07-12 18:51:02 -0300

    Rename module fiscal to l10n_br_fiscal

---
+++ l10n_br_fiscal/models/document_event.py
@@ -0,0 +1,117 @@
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# Copyright (C) 2014  KMEE - www.kmee.com.br
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+import datetime
+
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning as UserError
+
+
+class DocumentEvent(models.Model):
+    _name = 'l10n_br_account.document_event'
+
+    type = fields.Selection(
+        selection=[('-1', u'Exception'),
+                   ('0', u'Envio Lote'),
+                   ('1', u'Consulta Recibo'),
+                   ('2', u'Cancelamento'),
+                   ('3', u'Inutilização'),
+                   ('4', u'Consulta NFE'),
+                   ('5', u'Consulta Situação'),
+                   ('6', u'Consulta Cadastro'),
+                   ('7', u'DPEC Recepção'),
+                   ('8', u'DPEC Consulta'),
+                   ('9', u'Recepção Evento'),
+                   ('10', u'Download'),
+                   ('11', u'Consulta Destinadas'),
+                   ('12', u'Distribuição DFe'),
+                   ('13', u'Manifestação')],
+        string='Serviço')
+
+    response = fields.Char(
+        string=u'Descrição',
+        size=64,
+        readonly=True)
+
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        string='Empresa',
+        readonly=True,
+        states={'draft': [('readonly', False)]})
+
+    origin = fields.Char(
+        string=u'Documento de Origem',
+        size=64,
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        help=u"Referência ao documento que gerou o evento.")
+
+    file_sent = fields.Char(
+        string='Envio',
+        readonly=True)
+
+    file_returned = fields.Char(
+        string='Retorno',
+        readonly=True)
+
+    status = fields.Char(
+        string=u'Código',
+        readonly=True)
+
+    message = fields.Char(
+        string=u'Mensagem',
+        readonly=True)
+
+    create_date = fields.Datetime(
+        string=u'Data Criação',
+        readonly=True)
+
+    write_date = fields.Datetime(
+        string=u'Data Alteração',
+        readonly=True)
+
+    end_date = fields.Datetime(
+        string=u'Data Finalização',
+        readonly=True)
+
+    state = fields.Selection(
+        selection=[('draft', 'Rascunho'),
+                   ('send', 'Enviado'),
+                   ('wait', 'Aguardando Retorno'),
+                   ('done', 'Recebido Retorno')],
+        string=u'Status',
+        index=True,
+        readonly=True,
+        default='draft')
+
+    document_event_ids = fields.Many2one(
+        comodel_name='account.invoice',
+        string=u'Documentos')
+
+    cancel_document_event_id = fields.Many2one(
+        comodel_name='l10n_br_account.invoice.cancel',
+        string='Cancelamento')
+
+    invalid_number_document_event_id = fields.Many2one(
+        comodel_name='l10n_br_account.invoice.invalid.number',
+        string=u'Inutilização')
+
+    display_name = fields.Char(
+        string='Nome',
+        compute='_compute_display_name')
+
+    _order = 'write_date desc'
+
+    @api.multi
+    @api.depends('company_id.name', 'origin')
+    def _compute_display_name(self):
+        self.ensure_one()
+        names = ['Evento', self.company_id.name, self.origin]
+        self.display_name = ' / '.join(filter(None, names))
+
+    @api.multi
+    def set_done(self):
+        self.write({'state': 'done',
+                    'end_date': datetime.datetime.now()})
+        return True
---
+++ l10n_br_fiscal/models/tax_ipi_guideline.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2013  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields, api
+
+
+class TaxIpiGuideline(models.Model):
+    _name = 'fiscal.tax.ipi.guideline'
+    _description = 'IPI Guidelines'
+    _inherit = 'fiscal.data.abstract'
+
+    code = fields.Char(
+        size=3)
+
+    cst_group = fields.Selection(
+        selection=[('imunidade', u'Imunidade'),
+                   ('suspensao', u'Suspensão'),
+                   ('isencao', u'Isenção'),
+                   ('reducao', u'Redução'),
+                   ('outros', u'Outros')],
+        string='Group',
+        required=True)
+
+    cst_in_id = fields.Many2one(
+        comodel_name='fiscal.cst',
+        domain=[('domain', '=', 'ipi'),
+                ('type', '=', 'in')],
+        string=u'CST In')
+
+    cst_out_id = fields.Many2one(
+        comodel_name='fiscal.cst',
+        domain=[('domain', '=', 'ipi'),
+                ('type', '=', 'out')],
+        string=u'CST Out')
