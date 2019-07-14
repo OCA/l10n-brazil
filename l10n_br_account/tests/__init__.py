a1672d3cc886180e5a7ca3ddeac23b7790c7d48a
Renato Lima <renato.lima@akretion.com>
2019-07-14 17:42:23 -0300

    [REF] moving tests files from l10n_br_account to l10n_br_fiscal

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -1,7 +1,5 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import test_account_customer_invoice
-from . import test_account_supplier_invoice
-from . import test_generate_fiscal_rules
-from . import test_fiscal_partner_type
-from . import test_account_service_type
-from . import test_account_cnae
+# from . import test_account_customer_invoice
+# from . import test_account_supplier_invoice
+# from . import test_generate_fiscal_rules
