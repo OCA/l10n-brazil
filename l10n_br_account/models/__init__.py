b6cd98edc1f27e70e23f8d695366ce6b2c92a7d1
Renato Lima <renato.lima@akretion.com>
2019-07-14 17:42:23 -0300

    [REF] moving tests files from l10n_br_account to l10n_br_fiscal

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -1,17 +1,18 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import account_tax_group
 from . import account_tax_abstract
 from . import account_tax_template
 from . import account_tax
 from . import fiscal_tax
-from . import account_fiscal_position_abstract
-from . import account_fiscal_position_template
-from . import account_fiscal_position
-from . import res_company
-from . import account_journal
-from . import account_invoice
-from . import account_invoice_line
-from . import product_template
-from . import operation
-from . import account_move_line
+# from . import account_fiscal_position_abstract
+# from . import account_fiscal_position_template
+# from . import account_fiscal_position
+# from . import res_company
+# from . import account_journal
+# from . import account_invoice
+# from . import account_invoice_line
+# from . import product_template
+# from . import operation
+# from . import account_move_line
 # from . import wizard_multi_charts_accounts
