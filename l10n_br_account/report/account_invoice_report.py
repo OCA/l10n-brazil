f13d1688e30b1b251737ae1ae0587cdc6c309911
Renato Lima <renato.lima@akretion.com.br>
2019-07-14 21:50:35 -0300

    pep8

--- l10n_br_account/report/account_invoice_report.py
+++ l10n_br_account/report/account_invoice_report.py
@@ -1,9 +1,8 @@
 # Copyright (C) 2016  Magno Costa - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import models
 
 
 class AccountInvoiceReport(models.Model):
-
     _inherit = 'account.invoice.report'
