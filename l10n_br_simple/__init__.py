730479ade2ff1eab0fb692ab32a72753901d297c
Renato Lima <renato.lima@akretion.com.br>
2019-07-14 21:50:35 -0300

    pep8

--- l10n_br_simple/__init__.py
+++ l10n_br_simple/__init__.py
@@ -1,2 +1,1 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-# Copyright (C) 2009  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
