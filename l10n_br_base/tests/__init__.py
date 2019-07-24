b1514b8813c0dd7bf86150d8d1994a9b3f3a2e43
Luis Felipe Mileo <mileo@kmee.com.br>
2019-07-24 16:45:32 -0300

    [REF] l10n_br_base utilizando erpbrasil.base
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_base/tests/__init__.py
+++ l10n_br_base/tests/__init__.py
@@ -5,9 +5,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_amount_to_text
-from . import test_tools_fiscal
 from . import test_valid_createid
-from . import test_base_validate_ie
 from . import test_base_onchange
 from . import test_other_ie
-from . import test_tools_misc
