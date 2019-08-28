1cd0361e28500ed808c6690dff2ba4d4a37d8aef
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2019-08-28 13:16:18 -0300

    [ADD] Módulo para paralelização da geração de holerites

---
+++ l10n_br_hr_payroll_connector/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Kmee Informática LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Hr Payroll Connector',
+    'summary': """
+        Paralelização da geração de holerites utilizando connector""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Kmee Informática LTDA,Odoo Community Association (OCA)',
+    'website': 'www.kmee.com.br',
+    'depends': [
+        'connector',
+        'l10n_br_hr_payroll',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ l10n_br_hr_payroll_connector/models/hr_payslip_run.py
@@ -0,0 +1,39 @@
+from openerp import models, api, fields
+from openerp.addons.l10n_br_hr_payroll.models.profiling import (
+    clear_prof_data,
+    log_prof_data,
+    profile
+)
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from pybrasil.data import ultimo_dia_mes
+from pybrasil.data import formata_data
+import logging
+
+from openerp.addons.connector.queue.job import job
+from openerp.addons.connector.session import ConnectorSession
+
+_logger = logging.getLogger(__name__)
+
+
+class HrPayslipRun(models.Model):
+    _inherit = "hr.payslip.run"
+
+    @api.multi
+    def gerar_holerites(self):
+        session = ConnectorSession(
+            self.env.cr, self.env.uid, self.env.context)
+        for contrato in self.contract_id:
+            job_gerar_holerite.delay(session, self.id, contrato.id)
+        self.verificar_holerites_gerados()
+        self.busca_holerite_orfao()
+
+
+@job
+def job_gerar_holerite(session, lote_id, contrato_id):
+    lote = session.env['hr.payslip.run'].browse(lote_id)
+    clear_prof_data()
+    contrato = session.env['hr.contract'].browse(contrato_id)
+    lote._gerar_holerite(contrato)
+    log_prof_data()
+    clear_prof_data()
