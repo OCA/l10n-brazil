c1f4a058808e92f9953d3b2d45d6449de041c2ea
Renato <renato.lima@akretion.com>
2019-08-29 17:26:25 -0300

    Update document and document line model

--- l10n_br_fiscal/models/document_line.py
+++ l10n_br_fiscal/models/document_line.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class DocumentLine(models.Model):
