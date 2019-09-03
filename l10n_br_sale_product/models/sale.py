eca4cda0e6475ae2476474d0d0e52559d1b0d495
Magno Costa <magno.costa@akretion.com.br>
2019-09-03 11:36:24 -0300

    [10.0][MIG][FIX] PEP8.

--- l10n_br_sale_product/models/sale.py
+++ l10n_br_sale_product/models/sale.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2014  Renato Lima - Akretion                                  #
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 from odoo.addons import decimal_precision as dp
 from odoo.addons.l10n_br_base.tools.misc import calc_price_ratio
