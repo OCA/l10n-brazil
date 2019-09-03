a6f7b293708b11594ec07b11e4ffa88a58276937
Magno Costa <magno.costa@akretion.com.br>
2019-09-03 13:09:00 -0300

    [10.0][MIG][FIX] Author license header.

--- l10n_br_sale_product/models/sale_line.py
+++ l10n_br_sale_product/models/sale_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# @ 2019 Akretion - www.akretion.com.br -
-#   Magno Costa <magno.costa@akretion.com.br>
+# @ 2014 Akretion - www.akretion.com.br -
+#   Renato Lima <renato.lima@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api, _
