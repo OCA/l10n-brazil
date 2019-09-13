dba1e02d674e1dcf591469fdf93a78aea562c50d
Magno Costa <magno.costa@akretion.com.br>
2019-09-13 15:51:29 -0300

    [10.0][MIG][FIX] PEP8.

--- l10n_br_sale_stock/models/procurement.py
+++ l10n_br_sale_stock/models/procurement.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2014  Renato Lima - Akretion                                  #
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, api, fields
+from odoo import models, api
 
 
 class ProcurementOrder(models.Model):
