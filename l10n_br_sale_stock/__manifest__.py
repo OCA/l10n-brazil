c6b703a3e32b7fd96abbbfc5981893097e86eeec
Magno Costa <magno.costa@akretion.com.br>
2019-09-13 11:06:58 -0300

    [10.0][MIG] Included new dependency from account-fiscal-rule.

--- l10n_br_sale_stock/__manifest__.py
+++ l10n_br_sale_stock/__manifest__.py
@@ -14,6 +14,7 @@
         'sale_stock',
         'l10n_br_sale_product',
         'l10n_br_stock_account',
+        'account_fiscal_position_rule_sale_stock',
     ],
     'data': [
         'views/sale_stock_view.xml',
