f634870330eeb0d48c07fc793a05e9d577bc7761
Raphaël Valyi <rvalyi@akretion.com>
2019-09-13 19:57:33 -0300

    [MIG] l10n_br_stock: Migration to 12.0

--- l10n_br_stock/__manifest__.py
+++ l10n_br_stock/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2009  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
@@ -8,13 +7,12 @@
     'license': 'AGPL-3',
     'author': 'Akretion, Odoo Community Association (OCA)',
     'website': 'http://odoo-brasil.org',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'depends': [
         'stock',
         'l10n_br_base',
     ],
     'data': [
-        'data/l10n_br_stock_data.xml',
         'views/stock_view.xml',
     ],
     'demo': [],
