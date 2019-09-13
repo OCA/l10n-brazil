095a842574d10e9d06ea84894d283f108836c354
Raphaël Valyi <rvalyi@akretion.com>
2019-09-13 19:57:33 -0300

    [MIG] l10n_br_stock: Migration to 12.0

--- l10n_br_stock/models/stock.py
+++ l10n_br_stock/models/stock.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2009  Renato Lima - Akretion
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # Copyright (C) 2014  Luis Felipe Miléo - KMEE - www.kmee.com.br
@@ -7,30 +6,18 @@
 from odoo import models, fields
 
 
-class StockIncoterms(models.Model):
-    _inherit = 'stock.incoterms'
-
-    freight_responsibility = fields.Selection([('0', u'Emitente'),
-                                               ('1', u'Destinatário'),
-                                               ('2', u'Terceiros'),
-                                               ('9', u'Sem Frete')],
-                                              'Frete por Conta',
-                                              required=True,
-                                              default='0')
-
-
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     cnpj_cpf = fields.Char(
-        string=u'CNPJ/CPF',
+        string='CNPJ/CPF',
         related='partner_id.cnpj_cpf',
     )
     legal_name = fields.Char(
-        string=u'Razão Social',
+        string='Razão Social',
         related='partner_id.legal_name',
     )
     ie = fields.Char(
-        string=u'Inscrição Estadual',
+        string='Inscrição Estadual',
         related='partner_id.inscr_est',
     )
