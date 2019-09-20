f74e06d24bf7778879b06bd2fbc8216dbc91f735
Magno Costa <magno.costa@akretion.com.br>
2019-09-20 17:51:40 -0300

    [10.0][MIG][FIX] PEP8.

--- l10n_br_purchase/wizard/stock_invoice_onshipping.py
+++ l10n_br_purchase/wizard/stock_invoice_onshipping.py
@@ -3,7 +3,7 @@
 #   Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 
 class StockInvoiceOnshipping(models.TransientModel):
