49c8f773ca1cc1a46d0a616b7bf662c1c1662421
Luis Malta <luis.malta@kmee.com.br>
2019-09-23 14:49:40 -0300

    [MIG] Refactoring to Python 3

--- l10n_br_hr_contract/models/hr_contract_notice_termination.py
+++ l10n_br_hr_contract/models/hr_contract_notice_termination.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016  Daniel Sadamo - KMEE Informática
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
@@ -7,7 +6,7 @@ from odoo import models, fields
 
 class HrContractNoticeTermination(models.Model):
     _name = 'hr.contract.notice.termination'
-    _description = u'Tipo de aviso prévio'
+    _description = 'Tipo de aviso prévio'
 
     name = fields.Char(
         string='Notice of termination type',
