dd948071f58abef74c8f2e11af95835548e884c1
Luis Malta <luis.malta@kmee.com.br>
2019-09-27 16:03:48 -0300

    [MIG] Upgrade erpbrasil.base version

--- l10n_br_hr/models/hr_employee.py
+++ l10n_br_hr/models/hr_employee.py
@@ -4,7 +4,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from datetime import datetime
-from erpbrasil.base import fiscal
+from erpbrasil.base.fiscal import cnpj_cpf, pis
 from odoo import models, fields, api, _
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 from odoo.exceptions import ValidationError
@@ -55,8 +55,8 @@ class HrEmployee(models.Model):
     @api.constrains('pis_pasep')
     def _validate_pis_pasep(self):
         for record in self:
-            if record.pis_pasep and not fiscal.\
-                    validate_pis_pasep(record.pis_pasep):
+            if record.pis_pasep and not pis.\
+                    validar(record.pis_pasep):
                 raise ValidationError(_('Invalid PIS/PASEP'))
 
     pis_pasep = fields.Char(
@@ -115,9 +115,7 @@ class HrEmployee(models.Model):
 
     @api.onchange('cpf')
     def onchange_cpf(self):
-        country = self.env['res.country'].\
-            search([('id', '=', self.country_id.id)]).code or ''
-        cpf = fiscal.format_cpf_cnpj(self.cpf, country, False)
+        cpf = cnpj_cpf.formata(self.cpf)
         if cpf:
             self.cpf = cpf
 
@@ -125,7 +123,7 @@ class HrEmployee(models.Model):
     @api.constrains('cpf')
     def _check_cpf(self):
         for record in self:
-            if record.cpf and not fiscal.validate_cpf(record.cpf):
+            if record.cpf and not cnpj_cpf.validar(record.cpf):
                 raise ValidationError(_("CPF Invalido!"))
 
     organ_exp = fields.Char(
