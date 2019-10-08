5597067d9560111f750d12a0cb5252563dacb274
Luis Malta <luis.malta@kmee.com.br>
2019-10-08 10:10:07 -0300

    [13.0][MIG] Bump module version

--- l10n_br_base/__manifest__.py
+++ l10n_br_base/__manifest__.py
@@ -9,7 +9,7 @@
     'author': 'Akretion, '
               'Odoo Community Association (OCA)',
     'website': 'http://odoo-brasil.org',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'depends': [
         'base',
         'base_setup',
