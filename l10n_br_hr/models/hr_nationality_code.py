83f9823616cafc72e8341cb3c3355c4d915762e8
Luis Malta <luis.malta@kmee.com.br>
2019-10-11 14:22:09 -0300

    [MIG] Add models descriptions

--- l10n_br_hr/models/hr_nationality_code.py
+++ l10n_br_hr/models/hr_nationality_code.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models
 
 class HrNationalityCode(models.Model):
     _name = 'hr.nationality.code'
+    _description = 'Nationality Codes'
 
     name = fields.Char(
         string='Nationality')
