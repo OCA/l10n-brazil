7e01197e826661289a75f376239c96e47fc410ef
Luis Malta <luis.malta@kmee.com.br>
2019-10-15 09:22:27 -0300

    [IMP] l10n_br_base: black, isort

--- l10n_br_base/models/state_tax_numbers.py
+++ l10n_br_base/models/state_tax_numbers.py
@@ -3,28 +3,25 @@
 # Copyright (C) 2012 Raphaël Valyi (Akretion)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class StateTaxNumbers(models.Model):
-    _name = 'state.tax.numbers'
-    _description = 'State Tax Numbers'
+    _name = "state.tax.numbers"
+    _description = "State Tax Numbers"
 
-    partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Partner')
+    partner_id = fields.Many2one(comodel_name="res.partner", string="Partner")
 
-    inscr_est = fields.Char(
-        string='State Tax Number',
-        size=16,
-        required=True)
+    inscr_est = fields.Char(string="State Tax Number", size=16, required=True)
 
     state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        string='State',
-        required=True)
+        comodel_name="res.country.state", string="State", required=True
+    )
 
     _sql_constraints = [
-        ('l10n_br_base_state_tax_numbers_id_uniq',
-         'unique (state_id, partner_id)',
-         'The Partner already has a State Tax Number for that State!')]
+        (
+            "l10n_br_base_state_tax_numbers_id_uniq",
+            "unique (state_id, partner_id)",
+            "The Partner already has a State Tax Number for that State!",
+        )
+    ]
