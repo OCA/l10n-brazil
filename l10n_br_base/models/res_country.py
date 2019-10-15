c30c8317c1bc6dae1599e7b5f7218616d1d3c7e2
Luis Malta <luis.malta@kmee.com.br>
2019-10-15 09:22:27 -0300

    [IMP] l10n_br_base: black, isort

--- l10n_br_base/models/res_country.py
+++ l10n_br_base/models/res_country.py
@@ -1,20 +1,14 @@
 # Copyright (C) 2009  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Country(models.Model):
-    _inherit = 'res.country'
+    _inherit = "res.country"
 
-    bc_code = fields.Char(
-        string='BC Code',
-        size=5)
+    bc_code = fields.Char(string="BC Code", size=5)
 
-    ibge_code = fields.Char(
-        string='IBGE Code',
-        size=5)
+    ibge_code = fields.Char(string="IBGE Code", size=5)
 
-    siscomex_code = fields.Char(
-        string='Siscomex Code',
-        size=4)
+    siscomex_code = fields.Char(string="Siscomex Code", size=4)
