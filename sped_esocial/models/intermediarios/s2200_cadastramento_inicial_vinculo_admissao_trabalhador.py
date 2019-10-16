c84b9c1be6bfea9f1c912bfa86070b93459f2eb3
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-10-16 15:04:19 -0300

    [FIX] Campo do CPF no employee.dependent

--- sped_esocial/models/intermediarios/s2200_cadastramento_inicial_vinculo_admissao_trabalhador.py
+++ sped_esocial/models/intermediarios/s2200_cadastramento_inicial_vinculo_admissao_trabalhador.py
@@ -342,12 +342,12 @@ class SpedEsocialHrContrato(models.Model, SpedRegistroIntermediario):
             Dependente = pysped.esocial.leiaute.S2200_Dependente_2()
             Dependente.tpDep.valor = dependente.dependent_type_id.code.zfill(2)
             Dependente.nmDep.valor = dependente.name
-            if dependente.precisa_cpf:
-                if not dependente.dependent_cpf:
+            if dependente.dependent_verification:
+                if not dependente.cnpj_cpf:
                     validacao += "O trabalhador {} está faltando o CPF de um dependente !\n".format(
                             self.hr_contract_id.employee_id.name)
                 else:
-                    Dependente.cpfDep.valor = limpa_formatacao(dependente.dependent_cpf)
+                    Dependente.cpfDep.valor = limpa_formatacao(dependente.cnpj_cpf)
             Dependente.dtNascto.valor = dependente.dependent_dob
             Dependente.depIRRF.valor = 'S' if dependente.dependent_verification else 'N'
             Dependente.depSF.valor = 'S' if dependente.dep_sf else 'N'
