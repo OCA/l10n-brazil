36402269d67f1b307f8a51fb91d1217c0204cc08
Renato <renato.lima@akretion.com>
2019-10-16 11:11:11 -0300

    [REF] Added abstract model for HR data objects

--- l10n_br_hr/models/hr_deficiency.py
+++ l10n_br_hr/models/hr_deficiency.py
@@ -1,25 +1,10 @@
 # (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class HrDeficiency(models.Model):
     _name = 'hr.deficiency'
+    _inherit = 'l10n_br_hr.data.abstract'
     _description = 'Deficiencies'
-
-    name = fields.Char(
-        string='Deficiency')
-
-    code = fields.Char(
-        string='Code')
-
-    @api.multi
-    def name_get(self):
-        result = []
-        for record in self:
-            name = record['name']
-            if record['code']:
-                name = record['code'] + ' - ' + name
-            result.append((record['id'], name))
-        return result
--- l10n_br_hr/models/hr_ethnicity.py
+++ l10n_br_hr/models/hr_ethnicity.py
@@ -1,25 +1,10 @@
 # (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class HrEthnicity(models.Model):
     _name = 'hr.ethnicity'
+    _inherit = 'l10n_br_hr.data.abstract'
     _description = 'Ethnicities'
-
-    name = fields.Char(
-        string='Ethnicity')
-
-    code = fields.Char(
-        string='code')
-
-    @api.multi
-    def name_get(self):
-        result = []
-        for record in self:
-            name = record['name']
-            if record['code']:
-                name = record['code'] + ' - ' + name
-            result.append((record['id'], name))
-        return result
