2d0b8155dc4a6413fbbe0efb5df9ebf11d40d5db
Luis Felipe Mileo <mileo@kmee.com.br>
2019-10-17 18:08:24 -0300

    [NEW] Base do módulo de transmissão com generateDS
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ edoc_nfe/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Edoc Nfe',
+    'summary': """
+        Nota Fiscal eletrônica: Transmissão, cancelamento, carta de correção e importação do XML""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://www.kmee.com.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
