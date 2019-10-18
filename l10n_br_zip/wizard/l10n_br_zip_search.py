f3c9c278115d4f836acd13cdf943cd1c48f3f0d0
Luis Malta <luis.malta@kmee.com.br>
2019-10-18 14:48:43 -0300

    [REF] Running pre-commit on l10n_br_zip

--- l10n_br_zip/wizard/l10n_br_zip_search.py
+++ l10n_br_zip/wizard/l10n_br_zip_search.py
@@ -1,79 +1,67 @@
 # Copyright (C) 2011  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class L10nBrZipSearch(models.TransientModel):
-    _name = 'l10n_br.zip.search'
-    _description = 'Zipcode Search'
+    _name = "l10n_br.zip.search"
+    _description = "Zipcode Search"
 
-    zip = fields.Char(
-        string='CEP',
-        size=8)
+    zip = fields.Char(string="CEP", size=8)
 
-    street = fields.Char(
-        string='Logradouro',
-        size=72)
+    street = fields.Char(string="Logradouro", size=72)
 
-    district = fields.Char(
-        string='District',
-        size=72)
+    district = fields.Char(string="District", size=72)
 
-    country_id = fields.Many2one(
-        string='Country',
-        comodel_name='res.country')
+    country_id = fields.Many2one(string="Country", comodel_name="res.country")
 
     state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        string='State',
-        domain="[('country_id', '=', country_id)]")
+        comodel_name="res.country.state",
+        string="State",
+        domain="[('country_id', '=', country_id)]",
+    )
 
     city_id = fields.Many2one(
-        comodel_name='res.city',
-        string='Cidade',
-        domain="[('state_id', '=', state_id)]")
+        comodel_name="res.city", string="Cidade", domain="[('state_id', '=', state_id)]"
+    )
 
     zip_ids = fields.Many2many(
-        comodel_name='l10n_br.zip',
-        relation='zip_search',
-        column1='zip_search_id',
-        column2='zip_id',
-        string='CEPs',
-        readonly=False)
+        comodel_name="l10n_br.zip",
+        relation="zip_search",
+        column1="zip_search_id",
+        column2="zip_id",
+        string="CEPs",
+        readonly=False,
+    )
 
     state = fields.Selection(
-        selection=[('init', 'init'),
-                   ('done', 'done')],
-        string='state',
+        selection=[("init", "init"), ("done", "done")],
+        string="state",
         readonly=True,
-        default='init')
+        default="init",
+    )
 
-    address_id = fields.Integer(
-        string='Id do objeto',
-        invisible=True)
+    address_id = fields.Integer(string="Id do objeto", invisible=True)
 
-    object_name = fields.Char(
-        string='Nome do Objeto',
-        size=100,
-        invisible=True)
+    object_name = fields.Char(string="Nome do Objeto", size=100, invisible=True)
 
     @api.model
     def default_get(self, fields_list):
         data = super(L10nBrZipSearch, self).default_get(fields_list)
 
         context = dict(self._context or {})
-        data['zip'] = context.get('zip', False)
-        data['street'] = context.get('street', False)
-        data['district'] = context.get('district', False)
-        data['country_id'] = context.get('country_id', False)
-        data['state_id'] = context.get('state_id', False)
-        data['city_id'] = context.get('city_id', False)
-        data['address_id'] = context.get('address_id', False)
-        data['object_name'] = context.get('object_name', False)
+        data["zip"] = context.get("zip", False)
+        data["street"] = context.get("street", False)
+        data["district"] = context.get("district", False)
+        data["country_id"] = context.get("country_id", False)
+        data["state_id"] = context.get("state_id", False)
+        data["city_id"] = context.get("city_id", False)
+        data["address_id"] = context.get("address_id", False)
+        data["object_name"] = context.get("object_name", False)
 
-        data['zip_ids'] = context.get('zip_ids', False)
-        data['state'] = 'done'
+        data["zip_ids"] = context.get("zip_ids", False)
+        data["state"] = "done"
 
         return data
 
@@ -82,7 +70,7 @@ class L10nBrZipSearch(models.TransientModel):
 
         self.ensure_one()
         data = self
-        obj_zip = self.env['l10n_br.zip']
+        obj_zip = self.env["l10n_br.zip"]
 
         domain = obj_zip._set_domain(
             country_id=data.country_id.id,
@@ -97,42 +85,35 @@ class L10nBrZipSearch(models.TransientModel):
         zip_ids = obj_zip.search(domain)
 
         context = dict(self.env.context)
-        context.update({
-            'address_id': data.address_id,
-            'object_name': data.object_name,
-        })
+        context.update({"address_id": data.address_id, "object_name": data.object_name})
 
-        self.write({
-            'state': 'done',
-            'zip_ids': [[6, 0, [zip.id for zip in zip_ids]]]
-        })
+        self.write({"state": "done", "zip_ids": [[6, 0, [zip.id for zip in zip_ids]]]})
 
         return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'l10n_br.zip.search',
-            'view_mode': 'form',
-            'view_type': 'form',
-            'res_id': data.id,
-            'views': [(False, 'form')],
-            'target': 'new',
-            'nodestroy': True,
-            'context': context,
+            "type": "ir.actions.act_window",
+            "res_model": "l10n_br.zip.search",
+            "view_mode": "form",
+            "view_type": "form",
+            "res_id": data.id,
+            "views": [(False, "form")],
+            "target": "new",
+            "nodestroy": True,
+            "context": context,
         }
 
     @api.multi
     def zip_new_search(self):
         data = self.read()[0]
         self.ensure_one()
-        self.write({'state': 'init',
-                    'zip_ids': [[6, 0, []]]})
+        self.write({"state": "init", "zip_ids": [[6, 0, []]]})
 
         return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'l10n_br.zip.search',
-            'view_mode': 'form',
-            'view_type': 'form',
-            'res_id': data['id'],
-            'views': [(False, 'form')],
-            'target': 'new',
-            'nodestroy': True
+            "type": "ir.actions.act_window",
+            "res_model": "l10n_br.zip.search",
+            "view_mode": "form",
+            "view_type": "form",
+            "res_id": data["id"],
+            "views": [(False, "form")],
+            "target": "new",
+            "nodestroy": True,
         }
