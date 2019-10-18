437e27fdd2d12923820e094d4277a90ffd31ffea
Luis Malta <luis.malta@kmee.com.br>
2019-10-18 14:48:08 -0300

    [REF] Running pre-commit on l10n_br_hr

--- l10n_br_hr/models/hr_employee_dependent.py
+++ l10n_br_hr/models/hr_employee_dependent.py
@@ -4,111 +4,88 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from erpbrasil.base.fiscal import cnpj_cpf
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class HrEmployeeDependent(models.Model):
-    _name = 'hr.employee.dependent'
-    _description = 'Employee\'s Dependents'
+    _name = "hr.employee.dependent"
+    _description = "Employee's Dependents"
 
     _rec_name = "dependent_name"
-    _inherits = {'res.partner': 'partner_id'}
+    _inherits = {"res.partner": "partner_id"}
 
-    @api.constrains('dependent_cpf')
+    @api.constrains("dependent_cpf")
     def _validate_cpf(self):
         if self.dependent_cpf:
             if not cnpj_cpf.validar(self.dependent_cpf):
-                raise ValidationError(_('Invalid CPF for dependent %s')
-                                      % self.dependent_name)
+                raise ValidationError(
+                    _("Invalid CPF for dependent %s") % self.dependent_name
+                )
 
     def _get_default_employee(self):
-        if self.env.user.has_group('base.group_hr_user'):
+        if self.env.user.has_group("base.group_hr_user"):
             return False
         return self.env.user.employee_ids[0]
 
-    employee_id = fields.Many2one(
-        comodel_name='hr.employee',
-        string='Employee ID')
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee ID")
 
-    dependent_name = fields.Char(
-        string='Dependent name',
-        size=64,
-        required=True)
+    dependent_name = fields.Char(string="Dependent name", size=64, required=True)
 
-    dependent_dob = fields.Date(
-        string='Date of birth',
-        required=True)
+    dependent_dob = fields.Date(string="Date of birth", required=True)
 
     dependent_type_id = fields.Many2one(
-        string='Relatedness',
-        required=True,
-        comodel_name='hr.dependent.type')
+        string="Relatedness", required=True, comodel_name="hr.dependent.type"
+    )
 
-    pension_benefits = fields.Float(
-        string='Allowance value')
+    pension_benefits = fields.Float(string="Allowance value")
 
-    dependent_verification = fields.Boolean(
-        string='Is dependent')
+    dependent_verification = fields.Boolean(string="Is dependent")
 
-    health_verification = fields.Boolean(
-        string='Healthcare plan')
+    health_verification = fields.Boolean(string="Healthcare plan")
 
     dependent_gender = fields.Selection(
-        string='Gender',
-        selection=[
-            ('m', 'Male'),
-            ('f', 'Female')])
+        string="Gender", selection=[("m", "Male"), ("f", "Female")]
+    )
 
-    dependent_rg = fields.Char(
-        string='RG')
+    dependent_rg = fields.Char(string="RG")
 
-    dependent_cpf = fields.Char(
-        string='CPF')
+    dependent_cpf = fields.Char(string="CPF")
 
-    have_alimony = fields.Boolean(
-        string='Tem Pensão?')
+    have_alimony = fields.Boolean(string="Tem Pensão?")
 
     partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Partner',
-        ondelete='cascade',
+        comodel_name="res.partner",
+        string="Partner",
+        ondelete="cascade",
         auto_join=True,
         required=True,
-        help="Parceiro que contem as informações de banco do dependente."
+        help="Parceiro que contem as informações de banco do dependente.",
     )
 
     partner_id_bank_ids = fields.One2many(
-        comodel_name='res.partner.bank',
-        string='Info Bank',
-        related='partner_id.bank_ids',
+        comodel_name="res.partner.bank",
+        string="Info Bank",
+        related="partner_id.bank_ids",
     )
-    dep_sf = fields.Boolean(
-        string='Salário Família?',
-    )
-    inc_trab = fields.Boolean(
-        string='Incapacidade Física ou Mental?',
-    )
-    inc_trab_inss_file = fields.Binary(
-        string='Atestado de incapacidade INSS',
-    )
+    dep_sf = fields.Boolean(string="Salário Família?")
+    inc_trab = fields.Boolean(string="Incapacidade Física ou Mental?")
+    inc_trab_inss_file = fields.Binary(string="Atestado de incapacidade INSS")
     relative_file = fields.Binary(
-        string='Documento comprobatório da relação',
-        help='Certidão de Nascimento / Casamento / etc'
+        string="Documento comprobatório da relação",
+        help="Certidão de Nascimento / Casamento / etc",
     )
 
     @api.model
     def create(self, vals):
         ctx = self.env.context.copy()
-        ctx['create_depentent'] = True
-        ctx['depentent_employee_id'] = vals.get('employee_id', False)
+        ctx["create_depentent"] = True
+        ctx["depentent_employee_id"] = vals.get("employee_id", False)
         #
         # O sudo foi utilizado para evitar a permissão de criação de contato
         # para o funcionário.
         #
-        patient = super(
-            HrEmployeeDependent, self.sudo().with_context(ctx)
-        ).create(vals)
+        patient = super(HrEmployeeDependent, self.sudo().with_context(ctx)).create(vals)
         return patient
 
     @api.onchange('dependent_cpf')
