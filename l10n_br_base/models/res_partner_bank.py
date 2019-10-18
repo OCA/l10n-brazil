ad04b09c00d848d597616ab17ccfa3ee6d8d3485
Luis Malta <luis.malta@kmee.com.br>
2019-10-18 14:44:13 -0300

    [REF] Running pre-commit on l10n_br_base

--- l10n_br_base/models/res_partner_bank.py
+++ l10n_br_base/models/res_partner_bank.py
@@ -3,32 +3,23 @@
 # Copyright (C) 2012 Raphaël Valyi (Akretion)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartnerBank(models.Model):
     """ Adiciona campos necessários para o cadastramentos de contas
     bancárias no Brasil."""
-    _inherit = 'res.partner.bank'
 
-    acc_number = fields.Char(
-        string='Account Number',
-        size=64,
-        required=False)
+    _inherit = "res.partner.bank"
 
-    acc_number_dig = fields.Char(
-        string='Account Digit',
-        size=8)
+    acc_number = fields.Char(string="Account Number", size=64, required=False)
 
-    bra_number = fields.Char(
-        string='Bank Branch',
-        size=8)
+    acc_number_dig = fields.Char(string="Account Digit", size=8)
 
-    bra_number_dig = fields.Char(
-        string='Bank Branch Digit',
-        size=8)
+    bra_number = fields.Char(string="Bank Branch", size=8)
+
+    bra_number_dig = fields.Char(string="Bank Branch Digit", size=8)
 
     bra_bank_bic = fields.Char(
-        string='BIC/Swift Final Code.',
-        size=3,
-        help='Last part of BIC/Swift Code.')
+        string="BIC/Swift Final Code.", size=3, help="Last part of BIC/Swift Code."
+    )
