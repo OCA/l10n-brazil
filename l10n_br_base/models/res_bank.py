5b7a85889583ad7ce77209737b01f835dbc94061
Luis Malta <luis.malta@kmee.com.br>
2019-10-18 14:44:13 -0300

    Running pre-commit on l10n_br_base

--- l10n_br_base/models/res_bank.py
+++ l10n_br_base/models/res_bank.py
@@ -2,13 +2,14 @@
 #    @author Michell Stuttgart <michellstut@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResBank(models.Model):
-    _inherit = 'res.bank'
+    _inherit = "res.bank"
 
     code_bc = fields.Char(
-        string=u'Brazilian Bank Code',
+        string=u"Brazilian Bank Code",
         size=3,
-        help=u'Brazilian Bank Code ex.: 001 is the code of Banco do Brasil')
+        help=u"Brazilian Bank Code ex.: 001 is the code of Banco do Brasil",
+    )
