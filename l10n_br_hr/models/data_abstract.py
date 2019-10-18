1047c1e578c572aa4b4030733bfaf043eefcf3ad
Luis Malta <luis.malta@kmee.com.br>
2019-10-18 14:48:08 -0300

    [REF] Running pre-commit on l10n_br_hr

--- l10n_br_hr/models/data_abstract.py
+++ l10n_br_hr/models/data_abstract.py
@@ -6,32 +6,27 @@ from odoo.osv import expression
 
 
 class DataAbstract(models.AbstractModel):
-    _name = 'l10n_br_hr.data.abstract'
-    _description = 'HR Data Abstract'
-    _order = 'code'
+    _name = "l10n_br_hr.data.abstract"
+    _description = "HR Data Abstract"
+    _order = "code"
 
-    code = fields.Char(
-        string='Code',
-        required=True,
-        index=True)
+    code = fields.Char(string="Code", required=True, index=True)
 
-    name = fields.Text(
-        string='Name',
-        required=True,
-        index=True)
+    name = fields.Text(string="Name", required=True, index=True)
 
     @api.multi
     def name_get(self):
-        return [(r.id, "{0} - {1}".format(r.code, r.name)) for r in self]
+        return [(r.id, "{} - {}".format(r.code, r.name)) for r in self]
 
     @api.model
-    def _name_search(self, name, args=None, operator='ilike',
-                     limit=100, name_get_uid=None):
+    def _name_search(
+        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
         args = args or []
         domain = []
         if name:
-            domain = ['|', ('code', operator, name),
-                      ('name', operator, name)]
-        recs = self._search(expression.AND([domain, args]), limit=limit,
-                            access_rights_uid=name_get_uid)
+            domain = ["|", ("code", operator, name), ("name", operator, name)]
+        recs = self._search(
+            expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid
+        )
         return self.browse(recs).name_get()
