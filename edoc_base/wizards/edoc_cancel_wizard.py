4dd907e98b564a01f9e567c32c50ab0a90dd08ca
Luis Felipe Mileo <mileo@kmee.com.br>
2019-10-21 12:23:11 -0300

    [NEW] Edoc base
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ edoc_base/wizards/edoc_cancel_wizard.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class EdocCancelWizard(models.TransientModel):
+
+    _name = 'edoc.cancel.wizard'
+
+    name = fields.Char()
+
+    @api.multi
+    def doit(self):
+        for wizard in self:
+            # TODO
+            pass
+        action = {
+            'type': 'ir.actions.act_window',
+            'name': 'Action Name',  # TODO
+            'res_model': 'result.model',  # TODO
+            'domain': [('id', '=', result_ids)],  # TODO
+            'view_mode': 'form,tree',
+        }
+        return action
