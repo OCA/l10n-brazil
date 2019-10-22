44d8a876824fb1732c2ace3295115d38fc350cac
Hendrix Costa <hendrix.costa@abgf.gov.br>
2019-10-22 09:32:56 -0300

    [FIX] Flake8

--- l10n_br_hr/models/hr_civil_certificate_type.py
+++ l10n_br_hr/models/hr_civil_certificate_type.py
@@ -1,7 +1,7 @@
 # (c) 2014 Kmee - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import models
 
 
 class HrCivilCertificateType(models.Model):
