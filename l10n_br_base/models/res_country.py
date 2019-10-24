742dcd5451ab2b4cdf98e2f277f3f802bee8026f
Luis Felipe Mileo <mileo@kmee.com.br>
2019-10-24 16:26:09 -0300

    [REF] pre-commit l10n_br_base
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_base/models/res_country.py
+++ l10n_br_base/models/res_country.py
@@ -1,25 +1,16 @@
 # Copyright (C) 2009  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Country(models.Model):
-    _inherit = 'res.country'
+    _inherit = "res.country"
 
-    bc_code = fields.Char(
-        string='BC Code',
-        size=5)
+    bc_code = fields.Char(string="BC Code", size=5)
 
-    ibge_code = fields.Char(
-        string='IBGE Code',
-        size=5)
+    ibge_code = fields.Char(string="IBGE Code", size=5)
 
-    siscomex_code = fields.Char(
-        string='Siscomex Code',
-        size=4)
+    siscomex_code = fields.Char(string="Siscomex Code", size=4)
 
-    nationality_code = fields.Char(
-        string='Nationality Code',
-        size=2,
-    )
+    nationality_code = fields.Char(string="Nationality Code", size=2)
