20c17a059148eacb1fe68e904032aed338e88b8f
Renato <renato.lima@akretion.com>
2019-11-14 15:12:31 -0300

    Update __init__

--- nfe/__init__.py
+++ nfe/__init__.py
@@ -1,5 +1,6 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
-from . import wizards
-from . import sped
-from . import tests
+# from . import wizards
+# from . import sped
+# from . import tests
