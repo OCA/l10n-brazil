174af8e2ae0a455d0a9698554286b0bc1be07f4f
Renato <renato.lima@akretion.com>
2019-11-14 15:12:31 -0300

    Update __init__

--- nfe/models/__init__.py
+++ nfe/models/__init__.py
@@ -1,2 +1,6 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import res_company
+from . import nfe_document
+from . import nfe_document_line
+from . import res_config_settings
