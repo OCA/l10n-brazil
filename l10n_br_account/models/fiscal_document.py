83a25df5d4f8d5c65e16333efe67b77c8915a568
Renato <renato.lima@akretion.com>
2019-11-19 15:51:29 -0300

    Split fiscal document and account invoice

---
+++ l10n_br_account/models/fiscal_document.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# Copyright (C) 2019 - TODAY Raphaël Valyi - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class FiscalDocument(models.Model):
+    _inherit = 'l10n_br_fiscal.document'
+
+    # the following fields collide with account.invoice fields so we use
+    # related field alias to be able to write them through account.invoice
+    fiscal_doc_partner_id = fields.Many2one(
+        related='partner_id',
+        readonly=False)
+
+    fiscal_doc_date = fields.Date(
+        related='date',
+        readonly=False)
+
+    fiscal_doc_company_id = fields.Many2one(
+        related='company_id',
+        readonly=False)
+
+    fiscal_doc_currency_id = fields.Many2one(
+        related='currency_id',
+        readonly=False)
+
+    fiscal_doc_state = fields.Selection(
+        related='state',
+        readonly=False)
