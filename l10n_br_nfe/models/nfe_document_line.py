d78774938b0f8453030c76312b246df1df54d85a
Renato <renato.lima@akretion.com>
2019-11-19 15:57:20 -0300

    Rename nfe module to l10n_br_nfe

---
+++ l10n_br_nfe/models/nfe_document_line.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields
+
+
+class NFeLine(models.Model):
+    _name = 'nfe.document.line'
+    _inherit = 'l10n_br_fiscal.document.line'
+    _table = 'l10n_br_fiscal_document_line'
+    _description = 'NFe Line'
