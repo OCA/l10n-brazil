621d8067430e3ffc9c540dc7494c601086729bb0
Renato <renato.lima@akretion.com>
2019-11-21 15:50:07 -0300

    [FIX] Partner/Company inverse method

--- l10n_br_fiscal/tests/__init__.py
+++ l10n_br_fiscal/tests/__init__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import test_cnae
-from . import test_service_type
-from . import test_partner_profile
+# from . import test_cnae
+# from . import test_service_type
+# from . import test_partner_profile
 from . import test_ibpt_product
 from . import test_ibpt_service
