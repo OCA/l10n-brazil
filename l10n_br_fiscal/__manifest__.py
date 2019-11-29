ee27f53a3cc7e4ab78abd30de1238c27e7ba7c85
Renato Lima <renato.lima@akretion.com>
2019-11-29 11:02:50 -0300

    [ADD] document fiscal tax definition fields and methods

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -66,6 +66,8 @@
         'views/res_company_view.xml',
         'views/document_view.xml',
         'views/document_line_view.xml',
+        'views/document_fiscal_mixin_view.xml',
+        'views/document_fiscal_line_mixin_view.xml',
         'views/res_config_settings_view.xml',
         'views/l10n_br_fiscal_action.xml',
         'views/l10n_br_fiscal_menu.xml',
