6f1057206be099e668a9c9c450f7e1c1e971c862
Raphaël Valyi <rvalyi@akretion.com>
2019-12-01 14:27:12 -0200

    [ADD] l10n_br_nfe: mixins injection+fixes

--- l10n_br_nfe/__manifest__.py
+++ l10n_br_nfe/__manifest__.py
@@ -11,9 +11,11 @@
     "version": "12.0.1.0.0",
     "depends": ["l10n_br_fiscal"],
     "data": [
+        'l10n_br_fiscal',
+        'l10n_br_nfe_spec',
         # Security
         "security/nfe_security.xml",
-        "security/ir.model.access.csv",
+#        'security/ir.model.access.csv',
         # Views
         "views/res_company_view.xml",
         "views/nfe_document_view.xml",
@@ -24,6 +26,7 @@
         # Menu
         # "views/nfe_menu.xml",
     ],
+    'post_init_hook': 'post_init_hook',
     "installable": True,
     "auto_install": False,
 }
