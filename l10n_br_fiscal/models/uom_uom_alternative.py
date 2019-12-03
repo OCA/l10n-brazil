8bce52abbd6ec484a891a5f279203c473b3795cf
Luis Felipe Mileo <mileo@kmee.com.br>
2019-12-03 14:12:45 -0300

    [IMP] Unidades de medidas brasileiras

---
+++ l10n_br_fiscal/models/uom_uom_alternative.py
@@ -0,0 +1,20 @@
+# Copyright 2019 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class UomUomAlternative(models.Model):
+
+    _name = "uom.uom.alternative"
+    _rec_name = 'code'
+
+    code = fields.Char(string="Código")
+    uom_id = fields.Many2one(
+        comodel_name="uom.uom",
+    )
+
+    _sql_constraints = [
+        ('uom_alternative_unique', 'UNIQUE(code, uom_id)',
+            'You can note repeat the alternative name'),
+    ]
