02a2a5dfed1a706e808795c76e3dbc78a7971e9d
Luis Felipe Mileo <mileo@kmee.com.br>
2019-12-03 14:12:45 -0300

    [IMP] Unidades de medidas brasileiras

--- l10n_br_fiscal/models/uom_uom.py
+++ l10n_br_fiscal/models/uom_uom.py
@@ -1,7 +1,7 @@
 # Copyright 2019 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 
 class UomUom(models.Model):
@@ -13,5 +13,25 @@ class UomUom(models.Model):
         "mail.thread",
         "mail.activity.mixin",
     ]
-
     code = fields.Char(size=6)
+    alternative_ids = fields.One2many(
+        comodel_name='uom.uom.alternative',
+        inverse_name='uom_id',
+        string="Alternative names",
+    )
+
+    @api.model
+    def search(self, domain, *args, **kwargs):
+        for sub_domain in list(filter(lambda x: x[0] == 'code', domain)):
+            domain = self._get_code_domain(sub_domain, domain)
+        return super(UomUom, self).search(domain, *args, **kwargs)
+
+    def _get_code_domain(self, sub_domain, domain):
+        code_operator = sub_domain[1]
+        code_value = sub_domain[2]
+        alternative = self.env['uom.uom.alternative'].search(
+            [('code', code_operator, code_value)]).mapped('uom_id')
+        domain = [('id', 'in',alternative.ids)
+                  if x[0] == 'code' and x[2] == code_value
+                  else x for x in domain]
+        return domain
