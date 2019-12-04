c595ed80314f59d4b147d2cbea2728a4e37ceb82
Renato <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_account/models/account_fiscal_position_template.py
+++ l10n_br_account/models/account_fiscal_position_template.py
@@ -1,19 +1,15 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
-from .account_fiscal_position_abstract import (
-    AccountFiscalPositionTaxAbstract
-)
+from .account_fiscal_position_abstract import AccountFiscalPositionTaxAbstract
 
 
-class AccountFiscalPositionTaxTemplate(AccountFiscalPositionTaxAbstract,
-                                       models.Model):
+class AccountFiscalPositionTaxTemplate(AccountFiscalPositionTaxAbstract, models.Model):
 
-    _inherit = 'account.fiscal.position.tax.template'
+    _inherit = "account.fiscal.position.tax.template"
 
     tax_src_id = fields.Many2one(
-        comodel_name='account.tax.template',
-        string=u'Tax on Product',
-        required=False)
+        comodel_name="account.tax.template", string=u"Tax on Product", required=False
+    )
