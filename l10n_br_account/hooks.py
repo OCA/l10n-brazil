a386bec748901332af26fe59a02192752483fd27
Renato <renato.lima@akretion.com>
2019-12-03 23:42:53 -0300

    Added post hook to update account taxes

--- l10n_br_account/hooks.py
+++ l10n_br_account/hooks.py
@@ -31,3 +31,23 @@ def pre_init_hook(cr):
                       'fiscal_document_line_id', 'INTEGER')
     cr.execute("""update account_invoice_line set fiscal_document_line_id=%s
                where fiscal_document_line_id IS NULL;""", (fiscal_doc_line_id,))
+
+
+def post_init_hook(cr, registry):
+    """Relate fiscal taxes to account taxes."""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    l10n_br_chart = env.ref('l10n_br_simple.l10n_br_account_chart_template')
+    companies = env['res.company'].search(
+       [('chart_template_id', '=', l10n_br_chart.id)])
+
+    for company in companies:
+        taxes = env['account.tax'].search([('company_id', '=', company.id)])
+
+        for tax in taxes:
+            if tax.get_external_id():
+                tax_ref = tax.get_external_id().get(tax.id)
+                ref_module, ref_name = tax_ref.split('.')
+                ref_name = ref_name.replace(str(company.id) + '_', '')
+                tax_source_ref = '.'.join([ref_module, ref_name])
+                tax_template = env.ref(tax_source_ref)
+                tax.fiscal_tax_id = tax_template.fiscal_tax_id
