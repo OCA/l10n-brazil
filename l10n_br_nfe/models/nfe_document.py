6144d273fd1d69762dfc878ffbbe8fd18db8a9a4
Renato <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_nfe/models/nfe_document.py
+++ l10n_br_nfe/models/nfe_document.py
@@ -1,11 +1,11 @@
 # Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class NFe(models.Model):
-    _name = 'l10n_br_nfe.document'
-    _inherit = ['l10n_br_fiscal.document', 'l10n_br_fiscal.document.eletronic']
-    _table = 'l10n_br_fiscal_document'
-    _description = 'NFe'
+    _name = "l10n_br_nfe.document"
+    _inherit = ["l10n_br_fiscal.document", "l10n_br_fiscal.document.eletronic"]
+    _table = "l10n_br_fiscal_document"
+    _description = "NFe"
