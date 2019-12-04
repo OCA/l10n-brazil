ef4c43a58fb815386178629432834430bca238a7
Renato <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_fiscal/models/document_invalidate_number.py
+++ l10n_br_fiscal/models/document_invalidate_number.py
@@ -4,113 +4,101 @@
 
 import datetime
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class DocumentEvent(models.Model):
-    _name = 'l10n_br_fiscal.document.event'
+    _name = "l10n_br_fiscal.document.event"
 
     type = fields.Selection(
-        selection=[('-1', u'Exception'),
-                   ('0', u'Envio Lote'),
-                   ('1', u'Consulta Recibo'),
-                   ('2', u'Cancelamento'),
-                   ('3', u'Inutilização'),
-                   ('4', u'Consulta NFE'),
-                   ('5', u'Consulta Situação'),
-                   ('6', u'Consulta Cadastro'),
-                   ('7', u'DPEC Recepção'),
-                   ('8', u'DPEC Consulta'),
-                   ('9', u'Recepção Evento'),
-                   ('10', u'Download'),
-                   ('11', u'Consulta Destinadas'),
-                   ('12', u'Distribuição DFe'),
-                   ('13', u'Manifestação')],
-        string='Serviço')
+        selection=[
+            ("-1", u"Exception"),
+            ("0", u"Envio Lote"),
+            ("1", u"Consulta Recibo"),
+            ("2", u"Cancelamento"),
+            ("3", u"Inutilização"),
+            ("4", u"Consulta NFE"),
+            ("5", u"Consulta Situação"),
+            ("6", u"Consulta Cadastro"),
+            ("7", u"DPEC Recepção"),
+            ("8", u"DPEC Consulta"),
+            ("9", u"Recepção Evento"),
+            ("10", u"Download"),
+            ("11", u"Consulta Destinadas"),
+            ("12", u"Distribuição DFe"),
+            ("13", u"Manifestação"),
+        ],
+        string="Serviço",
+    )
 
-    response = fields.Char(
-        string=u'Descrição',
-        size=64,
-        readonly=True)
+    response = fields.Char(string=u"Descrição", size=64, readonly=True)
 
     company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Empresa',
+        comodel_name="res.company",
+        string="Empresa",
         readonly=True,
-        states={'draft': [('readonly', False)]})
+        states={"draft": [("readonly", False)]},
+    )
 
     origin = fields.Char(
-        string=u'Documento de Origem',
+        string=u"Documento de Origem",
         size=64,
         readonly=True,
-        states={'draft': [('readonly', False)]},
-        help=u"Referência ao documento que gerou o evento.")
+        states={"draft": [("readonly", False)]},
+        help=u"Referência ao documento que gerou o evento.",
+    )
 
-    file_sent = fields.Char(
-        string='Envio',
-        readonly=True)
+    file_sent = fields.Char(string="Envio", readonly=True)
 
-    file_returned = fields.Char(
-        string='Retorno',
-        readonly=True)
+    file_returned = fields.Char(string="Retorno", readonly=True)
 
-    status = fields.Char(
-        string=u'Código',
-        readonly=True)
+    status = fields.Char(string=u"Código", readonly=True)
 
-    message = fields.Char(
-        string=u'Mensagem',
-        readonly=True)
+    message = fields.Char(string=u"Mensagem", readonly=True)
 
-    create_date = fields.Datetime(
-        string=u'Data Criação',
-        readonly=True)
+    create_date = fields.Datetime(string=u"Data Criação", readonly=True)
 
-    write_date = fields.Datetime(
-        string=u'Data Alteração',
-        readonly=True)
+    write_date = fields.Datetime(string=u"Data Alteração", readonly=True)
 
-    end_date = fields.Datetime(
-        string=u'Data Finalização',
-        readonly=True)
+    end_date = fields.Datetime(string=u"Data Finalização", readonly=True)
 
     state = fields.Selection(
-        selection=[('draft', 'Rascunho'),
-                   ('send', 'Enviado'),
-                   ('wait', 'Aguardando Retorno'),
-                   ('done', 'Recebido Retorno')],
-        string=u'Status',
+        selection=[
+            ("draft", "Rascunho"),
+            ("send", "Enviado"),
+            ("wait", "Aguardando Retorno"),
+            ("done", "Recebido Retorno"),
+        ],
+        string=u"Status",
         index=True,
         readonly=True,
-        default='draft')
+        default="draft",
+    )
 
     document_event_ids = fields.Many2one(
-        comodel_name='account.invoice',
-        string=u'Documentos')
+        comodel_name="account.invoice", string=u"Documentos"
+    )
 
     cancel_document_event_id = fields.Many2one(
-        comodel_name='l10n_br_account.invoice.cancel',
-        string='Cancelamento')
+        comodel_name="l10n_br_account.invoice.cancel", string="Cancelamento"
+    )
 
     invalid_number_document_event_id = fields.Many2one(
-        comodel_name='l10n_br_account.invoice.invalid.number',
-        string=u'Inutilização')
+        comodel_name="l10n_br_account.invoice.invalid.number", string=u"Inutilização"
+    )
 
-    display_name = fields.Char(
-        string='Nome',
-        compute='_compute_display_name')
+    display_name = fields.Char(string="Nome", compute="_compute_display_name")
 
-    _order = 'write_date desc'
+    _order = "write_date desc"
 
     @api.multi
-    @api.depends('company_id.name', 'origin')
+    @api.depends("company_id.name", "origin")
     def _compute_display_name(self):
         self.ensure_one()
-        names = ['Evento', self.company_id.name, self.origin]
-        self.display_name = ' / '.join(filter(None, names))
+        names = ["Evento", self.company_id.name, self.origin]
+        self.display_name = " / ".join(filter(None, names))
 
     @api.multi
     def set_done(self):
-        self.write({'state': 'done',
-                    'end_date': datetime.datetime.now()})
+        self.write({"state": "done", "end_date": datetime.datetime.now()})
         return True
--- l10n_br_fiscal/models/tax.py
+++ l10n_br_fiscal/models/tax.py
@@ -1,140 +1,138 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
-from ..constants.fiscal import (
-    FISCAL_IN,
-    FISCAL_OUT,
-    TAX_BASE_TYPE,
-    TAX_BASE_TYPE_PERCENT,
-    TAX_DOMAIN)
-
-from ..constants.icms import (
-    ICMS_BASE_TYPE,
-    ICMS_BASE_TYPE_DEFAULT,
-    ICMS_ST_BASE_TYPE,
-    ICMS_ST_BASE_TYPE_DEFAULT)
-
-from ..constants.pis_cofins import (
-    CST_COFINS_NO_TAXED,
-    CST_PIS_NO_TAXED)
+from ..constants.fiscal import (FISCAL_IN, FISCAL_OUT, TAX_BASE_TYPE,
+                                TAX_BASE_TYPE_PERCENT, TAX_DOMAIN)
+from ..constants.icms import (ICMS_BASE_TYPE, ICMS_BASE_TYPE_DEFAULT,
+                              ICMS_ST_BASE_TYPE, ICMS_ST_BASE_TYPE_DEFAULT)
+from ..constants.pis_cofins import CST_COFINS_NO_TAXED, CST_PIS_NO_TAXED
 
 TAX_DICT_VALUES = {
-    'tax_id': False,
-    'cst_id': False,
-    'cst_code': False,
-    'base_type': 'percent',
-    'base': 0.00,
-    'percent_amount': 0.00,
-    'percent_reduction': 0.00,
-    'value_amount': 0.00,
-    'uot_id': False,
-    'tax_value': 0.00}
+    "tax_id": False,
+    "cst_id": False,
+    "cst_code": False,
+    "base_type": "percent",
+    "base": 0.00,
+    "percent_amount": 0.00,
+    "percent_reduction": 0.00,
+    "value_amount": 0.00,
+    "uot_id": False,
+    "tax_value": 0.00,
+}
 
 
 class Tax(models.Model):
-    _name = 'l10n_br_fiscal.tax'
-    _order = 'sequence, tax_domain, name'
-    _description = 'Tax'
+    _name = "l10n_br_fiscal.tax"
+    _order = "sequence, tax_domain, name"
+    _description = "Tax"
 
-    name = fields.Char(
-        string='Name',
-        size=256,
-        required=True)
+    name = fields.Char(string="Name", size=256, required=True)
 
     sequence = fields.Integer(
-        string='Sequence',
+        string="Sequence",
         default=10,
-        related='tax_group_id.sequence',
+        related="tax_group_id.sequence",
         required=True,
         help="The sequence field is used to define "
-             "order in which the tax lines are applied.")
+        "order in which the tax lines are applied.",
+    )
 
     tax_base_type = fields.Selection(
         selection=TAX_BASE_TYPE,
-        string='Tax Base Type',
+        string="Tax Base Type",
         default=TAX_BASE_TYPE_PERCENT,
-        required=True)
+        required=True,
+    )
 
     percent_amount = fields.Float(
-        string='Percent',
-        default='0.00',
-        digits=dp.get_precision('Fiscal Tax Percent'),
-        required=True)
+        string="Percent",
+        default="0.00",
+        digits=dp.get_precision("Fiscal Tax Percent"),
+        required=True,
+    )
 
     percent_reduction = fields.Float(
-        string='Percent Reduction',
-        default='0.00',
-        digits=dp.get_precision('Fiscal Tax Percent'),
-        required=True)
+        string="Percent Reduction",
+        default="0.00",
+        digits=dp.get_precision("Fiscal Tax Percent"),
+        required=True,
+    )
 
     percent_debit_credit = fields.Float(
-        string='Percent Debit/Credit',
-        default='0.00',
-        digits=dp.get_precision('Fiscal Tax Percent'),
-        required=True)
+        string="Percent Debit/Credit",
+        default="0.00",
+        digits=dp.get_precision("Fiscal Tax Percent"),
+        required=True,
+    )
 
     currency_id = fields.Many2one(
-        comodel_name='res.currency',
-        default=lambda self: self.env.ref('base.BRL'),
-        string='Currency')
+        comodel_name="res.currency",
+        default=lambda self: self.env.ref("base.BRL"),
+        string="Currency",
+    )
 
     value_amount = fields.Float(
-        string='Value',
-        default='0.00',
-        digits=dp.get_precision('Fiscal Tax Value'),
-        required=True)
+        string="Value",
+        default="0.00",
+        digits=dp.get_precision("Fiscal Tax Value"),
+        required=True,
+    )
 
-    uot_id = fields.Many2one(
-        comodel_name='uom.uom',
-        string='Tax UoM')
+    uot_id = fields.Many2one(comodel_name="uom.uom", string="Tax UoM")
 
     tax_group_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.tax.group',
-        string='Fiscal Tax Group',
-        required=True)
+        comodel_name="l10n_br_fiscal.tax.group",
+        string="Fiscal Tax Group",
+        required=True,
+    )
 
     tax_domain = fields.Selection(
         selection=TAX_DOMAIN,
-        related='tax_group_id.tax_domain',
-        string='Tax Domain',
+        related="tax_group_id.tax_domain",
+        string="Tax Domain",
         required=True,
-        readonly=True)
+        readonly=True,
+    )
 
     cst_in_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.cst',
-        string='CST In',
+        comodel_name="l10n_br_fiscal.cst",
+        string="CST In",
         domain="[('cst_type', 'in', ('in', 'all')), "
-               "('tax_domain', '=', tax_domain)]")
+        "('tax_domain', '=', tax_domain)]",
+    )
 
     cst_out_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.cst',
-        string='CST Out',
+        comodel_name="l10n_br_fiscal.cst",
+        string="CST Out",
         domain="[('cst_type', 'in', ('out', 'all')), "
-               "('tax_domain', '=', tax_domain)]")
+        "('tax_domain', '=', tax_domain)]",
+    )
 
     # ICMS Fields
     icms_base_type = fields.Selection(
         selection=ICMS_BASE_TYPE,
-        string='ICMS Base Type',
+        string="ICMS Base Type",
         required=True,
-        default=ICMS_BASE_TYPE_DEFAULT)
+        default=ICMS_BASE_TYPE_DEFAULT,
+    )
 
     icms_st_base_type = fields.Selection(
         selection=ICMS_ST_BASE_TYPE,
-        string=u'Tipo Base ICMS ST',
+        string=u"Tipo Base ICMS ST",
         required=True,
-        default=ICMS_ST_BASE_TYPE_DEFAULT)
+        default=ICMS_ST_BASE_TYPE_DEFAULT,
+    )
 
     _sql_constraints = [
-        ('fiscal_tax_code_uniq', 'unique (name)',
-         'Tax already exists with this name !')]
+        ("fiscal_tax_code_uniq", "unique (name)", "Tax already exists with this name !")
+    ]
 
     def cst_from_tax(self, operation_type=FISCAL_OUT):
         self.ensure_one()
-        cst = self.env['l10n_br_fiscal.cst']
+        cst = self.env["l10n_br_fiscal.cst"]
         if operation_type == FISCAL_IN:
             cst = self.cst_in_id
 
@@ -161,79 +159,71 @@ class Tax(models.Model):
         return self._compute_generic(tax, tax_dict, **kwargs)
 
     def _compute_pis(self, tax, tax_dict, **kwargs):
-        cst = kwargs.get('cst', self.env['l10n_br_fiscal.cst'])
+        cst = kwargs.get("cst", self.env["l10n_br_fiscal.cst"])
         if cst.code not in CST_PIS_NO_TAXED:
             tax_dict = self._compute_generic(tax, tax_dict, **kwargs)
         return tax_dict
 
     def _compute_cofins(self, tax, tax_dict, **kwargs):
-        cst = kwargs.get('cst', self.env['l10n_br_fiscal.cst'])
+        cst = kwargs.get("cst", self.env["l10n_br_fiscal.cst"])
         if cst.code not in CST_COFINS_NO_TAXED:
             tax_dict = self._compute_generic(tax, tax_dict, **kwargs)
         return tax_dict
 
     def _compute_generic(self, tax, tax_dict, **kwargs):
-        company = kwargs.get('company', tax.env.user.company_id)
-        currency = kwargs.get('currency', company.currency_id)
+        company = kwargs.get("company", tax.env.user.company_id)
+        currency = kwargs.get("currency", company.currency_id)
         precision = currency.decimal_places
         # price = kwargs.get('price', 0.00)
         # quantity = kwargs.get('quantity', 0.00)
         # uom_id = kwargs.get('uom_id')
-        fiscal_price = kwargs.get('fiscal_price', 0.00)
-        fiscal_quantity = kwargs.get('fiscal_quantity', 0.00)
+        fiscal_price = kwargs.get("fiscal_price", 0.00)
+        fiscal_quantity = kwargs.get("fiscal_quantity", 0.00)
         # uot_id = kwargs.get('uot_id')
 
-        if tax.tax_base_type == 'percent':
-            tax_dict['base_type'] = tax.tax_base_type
-            tax_dict['percent_amount'] = tax.percent_amount
-            tax_dict['percent_reduction'] = tax.percent_reduction
+        if tax.tax_base_type == "percent":
+            tax_dict["base_type"] = tax.tax_base_type
+            tax_dict["percent_amount"] = tax.percent_amount
+            tax_dict["percent_reduction"] = tax.percent_reduction
 
             # Compute Tax Base
             base = round(fiscal_price * fiscal_quantity, precision)
 
             # Compute Tax Base Reduction
-            base_reduction = round(
-                base * abs(tax.percent_reduction / 100),
-                precision)
+            base_reduction = round(base * abs(tax.percent_reduction / 100), precision)
 
             # Compute Tax Base Amount
             base_amount = base - base_reduction
 
-            tax_dict['base'] = base_amount
+            tax_dict["base"] = base_amount
 
             # Compute Tax Value
-            tax_value = round(
-                base_amount * (tax.percent_amount / 100),
-                precision)
+            tax_value = round(base_amount * (tax.percent_amount / 100), precision)
 
-            tax_dict['tax_value'] = tax_value
+            tax_dict["tax_value"] = tax_value
 
-        if tax.tax_base_type == 'quantity':
+        if tax.tax_base_type == "quantity":
             # Tax base
             base_amount = fiscal_quantity
-            tax_dict['base'] = base_amount
+            tax_dict["base"] = base_amount
 
             # Tax value by unit
-            tax_dict['value_amount'] = tax.value_amount
+            tax_dict["value_amount"] = tax.value_amount
 
-            tax_dict['tax_value'] = round(
-                base_amount * tax.value_amount,
-                precision)
+            tax_dict["tax_value"] = round(base_amount * tax.value_amount, precision)
 
-        if tax.tax_base_type == 'fixed':
+        if tax.tax_base_type == "fixed":
             # Compute Tax Base
             base = round(fiscal_price * fiscal_quantity, precision)
 
             # Compute Tax Base Reduction
-            base_reduction = round(
-                base * abs(tax.percent_reduction / 100),
-                precision)
+            base_reduction = round(base * abs(tax.percent_reduction / 100), precision)
 
             # Compute Tax Base Amount
             base_amount = base_amount - base_reduction
 
-            tax_dict['base'] = base_amount
-            tax_dict['value_amount'] = tax.value_amount
+            tax_dict["base"] = base_amount
+            tax_dict["value_amount"] = tax.value_amount
 
         return tax_dict
 
@@ -256,16 +246,17 @@ class Tax(models.Model):
             taxes[tax.tax_domain] = tax_dict
             try:
                 # Define CST FROM TAX
-                operation_type = kwargs.get('operation_type', FISCAL_OUT)
-                kwargs.update({'cst': tax.cst_from_tax(operation_type)})
+                operation_type = kwargs.get("operation_type", FISCAL_OUT)
+                kwargs.update({"cst": tax.cst_from_tax(operation_type)})
 
-                compute_method = getattr(
-                    self, '_compute_%s' % tax.tax_domain)
-                taxes[tax.tax_domain].update(compute_method(
-                    tax, taxes[tax.tax_domain], **kwargs))
+                compute_method = getattr(self, "_compute_%s" % tax.tax_domain)
+                taxes[tax.tax_domain].update(
+                    compute_method(tax, taxes[tax.tax_domain], **kwargs)
+                )
             except AttributeError:
-                taxes[tax.tax_domain].update(tax._compute_generic(
-                    tax, taxes[tax.tax_domain], **kwargs))
+                taxes[tax.tax_domain].update(
+                    tax._compute_generic(tax, taxes[tax.tax_domain], **kwargs)
+                )
                 # Caso não exista campos especificos dos impostos
                 # no documento fiscal, os mesmos são calculados.
                 continue
