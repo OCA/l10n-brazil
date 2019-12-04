a7e0871113f38489bae0b06ec5d2252c27566137
Renato <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_fiscal/models/document_line.py
+++ l10n_br_fiscal/models/document_line.py
@@ -1,14 +1,14 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class DocumentLine(models.Model):
-    _name = 'l10n_br_fiscal.document.line'
-    _inherit = 'l10n_br_fiscal.document.line.abstract'
-    _description = 'Fiscal Document Line'
+    _name = "l10n_br_fiscal.document.line"
+    _inherit = "l10n_br_fiscal.document.line.abstract"
+    _description = "Fiscal Document Line"
 
     document_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.document',
-        string='Document')
+        comodel_name="l10n_br_fiscal.document", string="Document"
+    )
