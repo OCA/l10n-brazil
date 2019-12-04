69c0c53db7e3a944fd6d417d7f292b16648c8db7
Renato Lima <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_account/models/account_fiscal_position_abstract.py
+++ l10n_br_account/models/account_fiscal_position_abstract.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, api
+from odoo import api, fields
 
 
 class AccountFiscalPositionAbstract(object):
@@ -11,26 +11,21 @@ class AccountFiscalPositionAbstract(object):
 class AccountFiscalPositionTaxAbstract(object):
 
     tax_group_id = fields.Many2one(
-        comodel_name='account.tax.group',
-        string=u'Grupo de Impostos',
+        comodel_name="account.tax.group", string=u"Grupo de Impostos"
     )
 
-    @api.onchange('tax_src_id',
-                  'tax_group_id',
-                  'position_id')
+    @api.onchange("tax_src_id", "tax_group_id", "position_id")
     def _onchange_tax_group(self):
-        type_tax_use = {'input': 'purchase', 'output': 'sale'}
+        type_tax_use = {"input": "purchase", "output": "sale"}
         domain = []
 
         if self.position_id.type:
-            domain = [('type_tax_use', '=',
-                      (type_tax_use.get(self.position_id.type)))]
+            domain = [("type_tax_use", "=", (type_tax_use.get(self.position_id.type)))]
 
         if self.tax_group_id:
-            domain.append(('tax_group_id', '=', self.tax_group_id.id))
+            domain.append(("tax_group_id", "=", self.tax_group_id.id))
 
         if self.tax_src_id:
-            domain.append(('tax_group_id', '=',
-                           self.tax_src_id.tax_group_id.id))
+            domain.append(("tax_group_id", "=", self.tax_src_id.tax_group_id.id))
 
-        return {'domain': {'tax_dest_id': domain, 'tax_src_id': domain}}
+        return {"domain": {"tax_dest_id": domain, "tax_src_id": domain}}
