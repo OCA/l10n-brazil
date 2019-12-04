254960ca287dacacedf8555e17842fa5229f4998
Renato Lima <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_account/models/account_tax_template.py
+++ l10n_br_account/models/account_tax_template.py
@@ -1,23 +1,23 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, api
+from odoo import api, models
 
 
 class AccountTaxTemplate(models.Model):
-    _name = 'account.tax.template'
-    _inherit = ['account.tax.fiscal.abstract', 'account.tax.template']
+    _name = "account.tax.template"
+    _inherit = ["account.tax.fiscal.abstract", "account.tax.template"]
 
     @api.multi
     def _generate_tax(self, company):
         mapping = super(AccountTaxTemplate, self)._generate_tax(company)
 
-        taxes_template = self.browse(
-            mapping.get('tax_template_to_tax').keys())
+        taxes_template = self.browse(mapping.get("tax_template_to_tax").keys())
 
         for tax_template in taxes_template:
-            tax_id = mapping.get('tax_template_to_tax').get(tax_template.id)
-            self.env['account.tax'].browse(tax_id).write(
-                {'fiscal_tax_id': tax_template.fiscal_tax_id.id})
+            tax_id = mapping.get("tax_template_to_tax").get(tax_template.id)
+            self.env["account.tax"].browse(tax_id).write(
+                {"fiscal_tax_id": tax_template.fiscal_tax_id.id}
+            )
 
         return mapping
