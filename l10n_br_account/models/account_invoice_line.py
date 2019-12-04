ac5679bcc698aeccbf93eec19d85482250f5081d
Renato Lima <renato.lima@akretion.com>
2019-12-04 04:32:38 -0300

    [IMP] black, isort

--- l10n_br_account/models/account_invoice_line.py
+++ l10n_br_account/models/account_invoice_line.py
@@ -2,29 +2,27 @@
 # Copyright (C) 2019 - TODAY Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
-    _inherits = {'l10n_br_fiscal.document.line': 'fiscal_document_line_id'}
+    _inherit = "account.invoice.line"
+    _inherits = {"l10n_br_fiscal.document.line": "fiscal_document_line_id"}
 
     # initial account.invoice.line inherits on fiscal.document.line that are
     # disable with active=False in their fiscal_document_line table.
     # To make these invoice lines still visible, we set active=True
     # in the invoice.line table.
-    active = fields.Boolean(
-        string='Active',
-        default=True)
+    active = fields.Boolean(string="Active", default=True)
 
     # this default should be overwritten to False in a module pretending to
     # create fiscal documents from the invoices. But this default here
     # allows to install the l10n_br_account module without creating issues
     # with the existing Odoo invoice (demo or not).
     fiscal_document_line_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.document.line',
-        string='Fiscal Document Line',
+        comodel_name="l10n_br_fiscal.document.line",
+        string="Fiscal Document Line",
         required=True,
         ondelete="cascade",
-        default=lambda self: self.env.ref(
-            'l10n_br_fiscal.fiscal_document_line_dummy'))
+        default=lambda self: self.env.ref("l10n_br_fiscal.fiscal_document_line_dummy"),
+    )
