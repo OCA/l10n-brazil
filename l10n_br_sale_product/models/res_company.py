e764f66e4933b77d6905800884bc58d8288c6965
Renato <renato.lima@akretion.com>
2019-12-10 02:49:42 -0300

    [MIGR] Removed # -*- coding: utf-8 -*- from py files

--- l10n_br_sale_product/models/res_company.py
+++ l10n_br_sale_product/models/res_company.py
@@ -1,23 +1,28 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2013  Renato Lima - Akretion                                  #
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
-    default_ind_pres = fields.Selection([
-        ('0', u'Não se aplica (por exemplo,'
-              u' Nota Fiscal complementar ou de ajuste)'),
-        ('1', u'Operação presencial'),
-        ('2', u'Operação não presencial, pela Internet'),
-        ('3', u'Operação não presencial, Teleatendimento'),
-        ('4', u'NFC-e em operação com entrega em domicílio'),
-        ('5', u'Operação presencial, fora do estabelecimento'),
-        ('9', u'Operação não presencial, outros'),
-    ], u'Tipo de operação',
-        help=u'Indicador de presença do comprador no \
+    default_ind_pres = fields.Selection(
+        [
+            (
+                "0",
+                u"Não se aplica (por exemplo,"
+                u" Nota Fiscal complementar ou de ajuste)",
+            ),
+            ("1", u"Operação presencial"),
+            ("2", u"Operação não presencial, pela Internet"),
+            ("3", u"Operação não presencial, Teleatendimento"),
+            ("4", u"NFC-e em operação com entrega em domicílio"),
+            ("5", u"Operação presencial, fora do estabelecimento"),
+            ("9", u"Operação não presencial, outros"),
+        ],
+        u"Tipo de operação",
+        help=u"Indicador de presença do comprador no \
             \nestabelecimento comercial no momento \
-            \nda operação.')
+            \nda operação.",
+    )
--- l10n_br_sale_product/models/sale_line.py
+++ l10n_br_sale_product/models/sale_line.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
 # @ 2014 Akretion - www.akretion.com.br -
 #   Renato Lima <renato.lima@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-
+from odoo import _, api, fields, models
 from odoo.addons import decimal_precision as dp
+from odoo.exceptions import ValidationError
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    @api.depends('price_unit', 'tax_id', 'discount', 'product_uom_qty')
+    @api.depends("price_unit", "tax_id", "discount", "product_uom_qty")
     def _amount_line(self):
         for record in self:
             price = record._calc_line_base_price()
@@ -25,71 +23,63 @@ class SaleOrderLine(models.Model):
                 fiscal_position=record.fiscal_position_id,
                 insurance_value=record.insurance_value,
                 freight_value=record.freight_value,
-                other_costs_value=record.other_costs_value)
+                other_costs_value=record.other_costs_value,
+            )
 
-            record.price_subtotal = \
-                record.order_id.pricelist_id.currency_id.round(
-                    taxes['total_excluded'])
+            record.price_subtotal = record.order_id.pricelist_id.currency_id.round(
+                taxes["total_excluded"]
+            )
             record.price_gross = record._calc_price_gross(qty)
-            record.discount_value = \
-                record.order_id.pricelist_id.currency_id.round(
-                    record.price_gross - (price * qty))
+            record.discount_value = record.order_id.pricelist_id.currency_id.round(
+                record.price_gross - (price * qty)
+            )
 
     insurance_value = fields.Float(
-        string='Insurance',
-        default=0.0,
-        digits=dp.get_precision('Account'))
+        string="Insurance", default=0.0, digits=dp.get_precision("Account")
+    )
     other_costs_value = fields.Float(
-        string='Other costs',
-        default=0.0,
-        digits=dp.get_precision('Account'))
+        string="Other costs", default=0.0, digits=dp.get_precision("Account")
+    )
     freight_value = fields.Float(
-        string='Freight',
-        default=0.0,
-        digits=dp.get_precision('Account'))
+        string="Freight", default=0.0, digits=dp.get_precision("Account")
+    )
     discount_value = fields.Float(
-        compute='_amount_line',
-        string='Vlr. Desc. (-)',
-        digits=dp.get_precision('Sale Price'))
+        compute="_amount_line",
+        string="Vlr. Desc. (-)",
+        digits=dp.get_precision("Sale Price"),
+    )
     price_gross = fields.Float(
-        compute='_amount_line',
-        string='Vlr. Bruto',
-        digits=dp.get_precision('Sale Price'))
+        compute="_amount_line",
+        string="Vlr. Bruto",
+        digits=dp.get_precision("Sale Price"),
+    )
     price_subtotal = fields.Float(
-        compute='_amount_line',
-        string='Subtotal',
-        digits=dp.get_precision('Sale Price'))
-    customer_order = fields.Char(
-        string=u"Pedido do Cliente",
-        size=15)
-    customer_order_line = fields.Char(
-        string=u"Item do Pedido do Cliente",
-        size=6)
+        compute="_amount_line", string="Subtotal", digits=dp.get_precision("Sale Price")
+    )
+    customer_order = fields.Char(string=u"Pedido do Cliente", size=15)
+    customer_order_line = fields.Char(string=u"Item do Pedido do Cliente", size=6)
 
     @api.onchange("customer_order_line")
     def _check_customer_order_line(self):
-        if (self.customer_order_line and
-                not self.customer_order_line.isdigit()):
+        if self.customer_order_line and not self.customer_order_line.isdigit():
             raise ValidationError(
-                _(u"Customer order line must be "
-                  "a number with up to six digits")
+                _(u"Customer order line must be " "a number with up to six digits")
             )
 
     @api.multi
     def _prepare_invoice_line(self, qty):
         self.ensure_one()
-        result = super(
-            SaleOrderLine, self)._prepare_invoice_line(qty)
+        result = super(SaleOrderLine, self)._prepare_invoice_line(qty)
 
-        result['insurance_value'] = self.insurance_value
-        result['other_costs_value'] = self.other_costs_value
-        result['freight_value'] = self.freight_value
-        result['partner_order'] = self.customer_order
-        result['partner_order_line'] = self.customer_order_line
+        result["insurance_value"] = self.insurance_value
+        result["other_costs_value"] = self.other_costs_value
+        result["freight_value"] = self.freight_value
+        result["partner_order"] = self.customer_order
+        result["partner_order_line"] = self.customer_order_line
 
-        if self.product_id.fiscal_type == 'product':
+        if self.product_id.fiscal_type == "product":
             if self.fiscal_position_id:
                 cfop = self.fiscal_position_id.cfop_id
                 if cfop:
-                    result['cfop_id'] = cfop.id
+                    result["cfop_id"] = cfop.id
         return result
