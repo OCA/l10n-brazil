f5f14df8ff67a8ff9dbecc8da6c8cd860ec4dd43
Renato <renato.lima@akretion.com>
2019-12-10 02:49:42 -0300

    [MIGR] Removed # -*- coding: utf-8 -*- from py files

--- l10n_br_sale/models/res_company.py
+++ l10n_br_sale/models/res_company.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     sale_fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category',
-        u'Categoria Fiscal Padrão de Vendas',
-        domain="[('journal_type', '=', 'sale')]")
+        "l10n_br_account.fiscal.category",
+        u"Categoria Fiscal Padrão de Vendas",
+        domain="[('journal_type', '=', 'sale')]",
+    )
