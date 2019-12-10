ae21282eccad7715f3b059d8d882ab6d4ea46675
Renato <renato.lima@akretion.com>
2019-12-10 03:31:01 -0300

    Renamed files

--- l10n_br_sale/models/__init__.py
+++ l10n_br_sale/models/__init__.py
@@ -2,6 +2,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import res_company
-from . import res_config
+from . import res_config_settings
 from . import sale
 from . import sale_line
