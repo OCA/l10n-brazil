371a514058c32a43222393bfc70b3dfde5473fce
Renato <renato.lima@akretion.com>
2019-12-12 01:51:53 -0300

    [MIGR] split l10n_br_purchase, l10n_br_purchase_stock

---
+++ l10n_br_purchase_stock/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2009  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Brazilian Localization Purchase Stock',
+    'license': 'AGPL-3',
+    'category': 'Localisation',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'website': 'http://odoo-brasil.org',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'purchase_stock',
+        'l10n_br_purchase',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
