2d3018cd539a9898994f4cbf443a59f2f94cf8c2
Renato <renato.lima@akretion.com>
2019-12-12 01:59:16 -0300

    [MIGR] migrate l10n_br_purchase objects

--- l10n_br_purchase/models/purchase.py
+++ l10n_br_purchase/models/purchase.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2009  Renato Lima - Akretion, Gabriel C. Stabel
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _name = "purchase.order"
+    _inherit = ["purchase.order", "l10n_br_fiscal.document.mixin"]
 
     @api.one
-    @api.depends('order_line.price_unit', 'order_line.product_qty',
-                 'order_line.taxes_id')
+    @api.depends(
+        "order_line.price_unit", "order_line.product_qty", "order_line.taxes_id"
+    )
     def _compute_amount(self):
         amount_untaxed = 0.0
         amount_tax = 0.0
@@ -22,96 +23,61 @@ class PurchaseOrder(models.Model):
             amount_untaxed = amount_tax = 0.0
             for line in order.order_line:
                 amount_untaxed += line.price_subtotal
-                if (order.company_id.tax_calculation_rounding_method
-                        == 'round_globally'):
+                if order.company_id.tax_calculation_rounding_method == "round_globally":
                     taxes = line.taxes_id.compute_all(
-                        line.price_unit, line.order_id.currency_id,
-                        line.product_qty, product=line.product_id,
-                        partner=line.order_id.partner_id)
+                        line.price_unit,
+                        line.order_id.currency_id,
+                        line.product_qty,
+                        product=line.product_id,
+                        partner=line.order_id.partner_id,
+                    )
                     amount_tax += sum(
-                        t.get('amount', 0.0) for t in taxes.get('taxes', []))
+                        t.get("amount", 0.0) for t in taxes.get("taxes", [])
+                    )
                 else:
                     amount_tax += line.price_tax
-            order.update({
-                'amount_untaxed': order.currency_id.round(amount_untaxed),
-                'amount_tax': order.currency_id.round(amount_tax),
-                'amount_total': amount_untaxed + amount_tax,
-            })
+            order.update(
+                {
+                    "amount_untaxed": order.currency_id.round(amount_untaxed),
+                    "amount_tax": order.currency_id.round(amount_tax),
+                    "amount_total": amount_untaxed + amount_tax,
+                }
+            )
 
-        self.amount_untaxed = self.currency_id.round(
-            amount_untaxed)
+        self.amount_untaxed = self.currency_id.round(amount_untaxed)
         self.amount_tax = self.currency_id.round(amount_tax)
-        self.amount_total = self.currency_id.round(
-            amount_untaxed + amount_tax)
+        self.amount_total = self.currency_id.round(amount_untaxed + amount_tax)
 
     @api.model
-    @api.returns('l10n_br_account.fiscal_category')
+    @api.returns("l10n_br_account.fiscal_category")
     def _default_fiscal_category(self):
-        company = self.env['res.company'].browse(self.env.user.company_id.id)
+        company = self.env["res.company"].browse(self.env.user.company_id.id)
         return company.purchase_fiscal_category_id
 
-    fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category', 'Categoria Fiscal',
-        default=_default_fiscal_category,
-        domain="""[('type', '=', 'input'), ('state', '=', 'approved'),
-            ('journal_type', '=', 'purchase')]""")
     amount_untaxed = fields.Monetary(
-        compute='_compute_amount', digits=dp.get_precision('Purchase Price'),
-        string='Untaxed Amount', store=True, help="The amount without tax")
+        compute="_compute_amount",
+        digits=dp.get_precision("Purchase Price"),
+        string="Untaxed Amount",
+        store=True,
+        help="The amount without tax",
+    )
     amount_tax = fields.Monetary(
-        compute='_compute_amount', digits=dp.get_precision('Purchase Price'),
-        string='Taxes', store=True, help="The tax amount")
+        compute="_compute_amount",
+        digits=dp.get_precision("Purchase Price"),
+        string="Taxes",
+        store=True,
+        help="The tax amount",
+    )
     amount_total = fields.Monetary(
-        compute='_compute_amount', digits=dp.get_precision('Purchase Price'),
-        string='Total', store=True, help="The total amount")
-
-    cnpj_cpf = fields.Char(
-        string=u'CNPJ/CPF',
-        related='partner_id.cnpj_cpf',
-    )
-    legal_name = fields.Char(
-        string=u'Razão Social',
-        related='partner_id.legal_name',
-    )
-    ie = fields.Char(
-        string=u'Inscrição Estadual',
-        related='partner_id.inscr_est',
+        compute="_compute_amount",
+        digits=dp.get_precision("Purchase Price"),
+        string="Total",
+        store=True,
+        help="The total amount",
     )
 
-    @api.model
-    def _fiscal_position_map(self, **kwargs):
-        ctx = dict(self.env.context)
-        if ctx.get('fiscal_category_id'):
-            kwargs['fiscal_category_id'] = ctx.get('fiscal_category_id')
-        ctx.update({
-            'use_domain': ('use_purchase', '=', True),
-            'fiscal_category_id': ctx.get('fiscal_category_id')
-        })
-        return self.env['account.fiscal.position.rule'].with_context(
-            ctx).apply_fiscal_mapping(**kwargs)
+    cnpj_cpf = fields.Char(string="CNPJ/CPF", related="partner_id.cnpj_cpf")
 
-    @api.onchange(
-        'fiscal_category_id', 'fiscal_position_id', 'company_id')
-    def onchange_fiscal(self):
-        if self.partner_id and self.company_id and self.fiscal_category_id:
-            kwargs = {
-                'company_id': self.company_id,
-                'partner_id': self.partner_id,
-                'partner_invoice_id': self.partner_id,
-                'partner_shipping_id': self.dest_address_id,
-                'fiscal_category_id': self.fiscal_category_id,
-                'context': dict(self.env.context),
-            }
-            fiscal_position = self._fiscal_position_map(**kwargs)
+    legal_name = fields.Char(string="Razão Social", related="partner_id.legal_name")
 
-            if fiscal_position:
-                self.fiscal_position_id = fiscal_position.id
-
-    @api.model
-    def _prepare_picking(self):
-        vals = super(PurchaseOrder, self)._prepare_picking()
-        vals.update({
-            'fiscal_category_id': (self.fiscal_category_id.id),
-            'fiscal_position_id': self.fiscal_position_id.id,
-        })
-        return vals
+    ie = fields.Char(string="Inscrição Estadual", related="partner_id.inscr_est")
