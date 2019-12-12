66ee2825df087433098ffbf2adaab23ef8df8039
Renato Lima <renato.lima@akretion.com>
2019-12-12 01:42:43 -0300

    [MIGR] __manifest__

--- l10n_br_purchase/__manifest__.py
+++ l10n_br_purchase/__manifest__.py
@@ -1,28 +1,25 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2009  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Brazilian Localization Purchase',
-    'license': 'AGPL-3',
-    'category': 'Localisation',
-    'author': 'Akretion, Odoo Community Association (OCA)',
-    'website': 'http://odoo-brasil.org',
-    'version': '10.0.1.0.0',
-    'depends': [
-        'l10n_br_stock_account',
-        'account_fiscal_position_rule_purchase',
+    "name": "Brazilian Localization Purchase",
+    "license": "AGPL-3",
+    "category": "Localisation",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "http://odoo-brasil.org",
+    "version": "12.0.1.0.0",
+    "depends": ["purchase", "l10n_br_account", "l10n_br_fiscal"],
+    "data": [
+        # Security
+        "security/ir.model.access.csv",
+        "security/l10n_br_purchase_security.xml",
+        # Data
+        "data/l10n_br_purchase_data.xml",
+        # View
+        "views/purchase_view.xml",
+        "views/res_company_view.xml",
     ],
-    'data': [
-        'data/l10n_br_purchase_data.xml',
-        'views/purchase_view.xml',
-        'views/res_company_view.xml',
-        'security/ir.model.access.csv',
-        'security/l10n_br_purchase_security.xml',
-    ],
-    'demo': [
-        'demo/l10n_br_purchase_demo.xml'
-    ],
-    'installable': True,
-    'auto_install': False,
+    "demo": ["demo/l10n_br_purchase_demo.xml"],
+    "installable": True,
+    "auto_install": False,
 }
