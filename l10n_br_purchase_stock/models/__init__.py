8641cd8c236fa4789011f3eeeae28f8bfd8f0350
Renato <renato.lima@akretion.com>
2019-12-12 03:07:46 -0300

    [MIGR] l10n_br_purchase_stock

---
+++ l10n_br_purchase_stock/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2015  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import purchase
+from . import procurement
+from . import purchase_line
