e3127e7db12c4f31177a9f22f7d7413e962d2006
Renato <renato.lima@akretion.com>
2019-12-12 03:07:46 -0300

    [MIGR] l10n_br_purchase_stock

---
+++ l10n_br_purchase_stock/models/purchase.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2009  Renato Lima - Akretion, Gabriel C. Stabel
+# Copyright (C) 2012  Raphaël Valyi - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.model
+    def _prepare_picking(self):
+        values = super(PurchaseOrder, self)._prepare_picking()
+        values.update({
+            'operation_id': (self.operation_id.id),
+        })
+        return values
