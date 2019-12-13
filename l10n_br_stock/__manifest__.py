799716c2042628799eecb7434f43e11653c57e0f
Raphaël Valyi <rvalyi@akretion.com>
2019-12-13 21:28:41 -0200

    after stock_account extraction, module author is only KMEE

--- l10n_br_stock/__manifest__.py
+++ l10n_br_stock/__manifest__.py
@@ -1,11 +1,8 @@
-# Copyright (C) 2009  Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 {
     'name': 'Brazilian Localization Warehouse',
     'category': 'Localisation',
     'license': 'AGPL-3',
-    'author': 'Akretion, Odoo Community Association (OCA)',
+    'author': 'KMEE, Odoo Community Association (OCA)',
     'website': 'http://odoo-brasil.org',
     'version': '12.0.1.0.0',
     'depends': [
