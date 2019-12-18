4ee705693fdacf2c8424f34aebfb9e4538e48ee8
Renato <renato.lima@akretion.com>
2019-12-18 16:51:09 -0300

    [REF] module cleanup

--- l10n_br_account/models/res_company.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import models
-
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
