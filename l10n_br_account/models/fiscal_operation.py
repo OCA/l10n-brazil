5b8c7a1b019cbdf2e02bd63097dd2cb34e4fb88d
Renato <renato.lima@akretion.com>
2019-12-18 16:47:48 -0300

    [REF] fiscal operation

--- l10n_br_account/models/fiscal_operation.py
+++ l10n_br_account/models/fiscal_operation.py
@@ -1,10 +1,18 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
-# Copyright (C) 2014  KMEE - www.kmee.com.br
+# Copyright (C) 2019 - TODAY Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import fields, models
 
 
 class Operation(models.Model):
-    _name = "l10n_br_fiscal.operation"
-    _description = "Fiscal Operation"
+    _inherit = "l10n_br_fiscal.operation"
+
+    journal_id = fields.Many2one(
+        comodel_name="account.journal",
+        string="Account Journal",
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        domain="[('type', 'in', {'out': ['sale'], 'in': "
+               "['purchase'], 'all': ['sale', 'purchase']}.get("
+               "operation_type, []))]")
