90b43a051e643185000b6a047aaad1b515c67f8c
Luis Malta <luis.malta@kmee.com.br>
2019-12-18 08:28:19 -0300

    [12.0][MIG] Correct description

--- l10n_br_resource/wizards/workalendar_holiday_import_wizard.py
+++ l10n_br_resource/wizards/workalendar_holiday_import_wizard.py
@@ -22,7 +22,7 @@ _INTERVALS = {
 class WorkalendarHolidayImport(models.TransientModel):
 
     _name = "wizard.workalendar.holiday.import"
-    _description = 'Wizard de import de férias'
+    _description = 'Wizard de import de feriados'
 
     @api.multi
     @api.depends("start_date", "interval_number", "interval_type")
