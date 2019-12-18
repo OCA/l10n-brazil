12ece5341102967cf4cc57d9564b21cf33e6654e
Luis Felipe Mileo <mileo@kmee.com.br>
2019-12-18 09:30:44 -0300

    [FIX] pre-commit l10n_br_hr_contract

--- l10n_br_hr_contract/models/hr_contract_labor_resignation_cause.py
+++ l10n_br_hr_contract/models/hr_contract_labor_resignation_cause.py
@@ -1,30 +1,25 @@
 # Copyright (C) 2016  Daniel Sadamo - KMEE Informática
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HrContractResignationCause(models.Model):
-    _name = 'hr.contract.resignation.cause'
-    _description = 'Motivo da demissão'
+    _name = "hr.contract.resignation.cause"
+    _description = "Motivo da demissão"
 
-    name = fields.Char(
-        string='Resignation cause',
-        required=True,)
+    name = fields.Char(string="Resignation cause", required=True)
 
-    code = fields.Char(
-        string='Resignation cause code',
-        required=True,)
+    code = fields.Char(string="Resignation cause code", required=True)
 
-    fgts_withdraw_code = fields.Char(
-        string='FGTS withdrawal code',)
+    fgts_withdraw_code = fields.Char(string="FGTS withdrawal code")
 
     @api.multi
     def name_get(self):
         result = []
         for record in self:
-            name = record['name']
-            if record['code']:
-                name = record['code'] + ' - ' + name
-            result.append((record['id'], name))
+            name = record["name"]
+            if record["code"]:
+                name = record["code"] + " - " + name
+            result.append((record["id"], name))
         return result
