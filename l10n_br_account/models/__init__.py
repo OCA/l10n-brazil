34faf171f90407379eb921b597b6e66346dd44ee
Renato Lima <renato.lima@akretion.com>
2019-12-18 16:51:09 -0300

    [REF] module cleanup

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -5,18 +5,9 @@ from . import account_tax_abstract
 from . import account_tax_template
 from . import account_tax
 from . import fiscal_tax
-
-# from . import account_fiscal_position_abstract
-# from . import account_fiscal_position_template
-# from . import account_fiscal_position
-# from . import res_company
 from . import fiscal_operation
 from . import fiscal_document
 from . import fiscal_document_line
 from . import account_invoice
 from . import account_invoice_line
-
-# from . import product_template
-# from . import operation
-# from . import account_move_line
-# from . import wizard_multi_charts_accounts
+from . import account_move_line
