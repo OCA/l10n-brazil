1ff841654e4011b794ab87971486c9ee647bbc99
Luis Felipe Mileo <mileo@kmee.com.br>
2019-12-18 09:30:44 -0300

    [FIX] pre-commit l10n_br_hr_contract

--- l10n_br_hr_contract/models/hr_contract_admission_type.py
+++ l10n_br_hr_contract/models/hr_contract_admission_type.py
@@ -1,29 +1,23 @@
 # Copyright (C) 2016  Daniel Sadamo - KMEE Informática
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HrContractAdmissionType(models.Model):
-    _name = 'hr.contract.admission.type'
-    _description = 'Tipo de admissão do trabalhador'
+    _name = "hr.contract.admission.type"
+    _description = "Tipo de admissão do trabalhador"
 
-    name = fields.Char(
-        string='Admission type',
-        required=True,
-    )
+    name = fields.Char(string="Admission type", required=True)
 
-    code = fields.Char(
-        string='Code',
-        required=True,
-    )
+    code = fields.Char(string="Code", required=True)
 
     @api.multi
     def name_get(self):
         result = []
         for record in self:
-            name = record['name']
-            if record['code']:
-                name = record['code'] + ' - ' + name
-            result.append((record['id'], name))
+            name = record["name"]
+            if record["code"]:
+                name = record["code"] + " - " + name
+            result.append((record["id"], name))
         return result
--- l10n_br_hr_contract/models/hr_contract_labor_salary_unit.py
+++ l10n_br_hr_contract/models/hr_contract_labor_salary_unit.py
@@ -1,41 +1,37 @@
 # Copyright (C) 2016  Daniel Sadamo - KMEE Informática
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HrContractSalaryUnit(models.Model):
-    _name = 'hr.contract.salary.unit'
-    _description = 'Unidade de pagamento da parte fixa da remuneração'
+    _name = "hr.contract.salary.unit"
+    _description = "Unidade de pagamento da parte fixa da remuneração"
 
-    name = fields.Char(
-        string='Salary unit',
-        required=True,)
+    name = fields.Char(string="Salary unit", required=True)
 
-    code = fields.Char(
-        string='Code',
-        required=True,)
+    code = fields.Char(string="Code", required=True)
 
     @api.multi
     def name_get(self):
         result = []
         for record in self:
-            name = record['name']
-            if name == 'Monthly':
-                name = 'Por mês'
-            elif name == 'Biweekly':
-                name = 'Por 15 dias'
-            elif name == 'Weekly':
-                name = 'Por semana'
-            elif name == 'Daily':
-                name = 'Por dia'
-            elif name == 'Hourly':
-                name = 'Por hora'
-            elif name == 'Task':
-                name = 'Por tarefa'
-            elif name == 'Others':
-                name = 'Outros'
+            name = record["name"]
+            if name == "Monthly":
+                name = "Por mês"
+            elif name == "Biweekly":
+                name = "Por 15 dias"
+            elif name == "Weekly":
+                name = "Por semana"
+            elif name == "Daily":
+                name = "Por dia"
+            elif name == "Hourly":
+                name = "Por hora"
+            elif name == "Task":
+                name = "Por tarefa"
+            elif name == "Others":
+                name = "Outros"
 
-            name = record['code'] + ' - ' + name
-            result.append((record['id'], name))
+            name = record["code"] + " - " + name
+            result.append((record["id"], name))
         return result
