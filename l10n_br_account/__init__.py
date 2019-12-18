8688afb84b95dd55cba00048fe4f354a734fe615
Renato Lima <renato.lima@akretion.com>
2019-12-18 13:46:23 -0300

    [MIGR] invoice refund wizard

--- l10n_br_account/__init__.py
+++ l10n_br_account/__init__.py
@@ -3,7 +3,6 @@
 from .hooks import pre_init_hook
 from .hooks import post_init_hook
 from . import models
-
-# from . import wizards
+from . import wizards
 from . import report
 from . import tests
