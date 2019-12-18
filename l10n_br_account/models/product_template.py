c57f6452700bb89d5fcfd6afb9e6094b8e14e4e5
Renato Lima <renato.lima@akretion.com>
2019-12-18 16:51:09 -0300

    [REF] module cleanup

--- l10n_br_account/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2009  Gabriel C. Stabel
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import models
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
