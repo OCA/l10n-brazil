2f72391ef69060df6a79f78271b4230d8f6836fe
Renato <renato.lima@akretion.com>
2019-12-18 13:46:23 -0300

    [MIGR] invoice refund wizard

--- l10n_br_account/wizards/__init__.py
+++ l10n_br_account/wizards/__init__.py
@@ -1,2 +1,4 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from . import account_invoice_refund
-from . import l10n_br_account_invoice_costs_ratio
+# from . import l10n_br_account_invoice_costs_ratio
