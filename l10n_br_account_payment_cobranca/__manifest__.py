0572a3db399ab45e2ba947fc32db91d6a51e8e48
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2019-12-19 16:12:54 -0300

    [MIG] Bump version to 12.0.1.0.0

--- l10n_br_account_payment_cobranca/__manifest__.py
+++ l10n_br_account_payment_cobranca/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Account Payment CNAB",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
