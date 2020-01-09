4a21e725c6df4a302a1e97856ad95ab6aa7654dc
Luis Felipe Mileo <mileo@kmee.com.br>
2020-01-09 16:17:09 -0300

    [NEW] Export to xml many2one, monetary, datetime fields
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- spec_driven_model/models/spec_export.py
+++ spec_driven_model/models/spec_export.py
@@ -1,7 +1,7 @@
 # Copyright 2019 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, models, fields
 from nfelib.v4_00 import leiauteNFe
 
 try:
@@ -42,27 +42,53 @@ class AbstractSpecMixin(models.AbstractModel):
                 xml_required_fields.append(item)
         kwargs = {}
 
+        if not class_obj._generateds_type:
+            return
+
+        #  FIXME: leiauteNFe hardcoded
+        ds_class = getattr(leiauteNFe, class_obj._generateds_type)
+        ds_class_sepc = {i.name:i for i in ds_class.member_data_items_}
+
         for xml_required_field in xml_required_fields:
+            print(self[xml_required_field])
+            print(xml_required_field)
+            print(self._fields[xml_required_field].type)
+
             if self[xml_required_field]:  # FIXME: and class_obj._field_prefix:
+
+                # FIXME: xml_required_field.replace(class_obj._field_prefix, '')
+                field_spec_name = xml_required_field.replace('nfe40_', '')
+                member_spec = ds_class_sepc[field_spec_name]
+
                 # if self._fields[xml_required_field]._fields[xml_required_field].relational:
                 if self._fields[xml_required_field].type == 'many2one':
-                    continue
-                    self[xml_required_field]
+                    if not self._fields[xml_required_field]._attrs.get('original_spec_model'):
+                        continue
+                    field_data = self[xml_required_field]._build_generateds(
+                        class_item=self._fields[xml_required_field]._attrs.get('original_spec_model')
+                    )
                 elif self._fields[xml_required_field].type == 'one2many':
                     continue
                     relational_data = []
                     for relational_field in self[xml_required_field]:
+                        pass
                         relational_data.append()
                     field_data = self[xml_required_field]
+                elif self._fields[xml_required_field].type == 'datetime':
+                    field_data = fields.Datetime.context_timestamp(
+                        self,
+                        fields.Datetime.from_string(self[xml_required_field])
+                    ).isoformat('T')
+                elif self._fields[xml_required_field].type == 'monetary':
+                    if member_spec.data_type[0] == 'TDec_1302':
+                        field_data = str("%.2f" % self[xml_required_field])
+                    else:
+                        raise NotImplementedError
                 else:
                     field_data = self[xml_required_field]
-                kwargs[
-                    # xml_required_field.replace(class_obj._field_prefix, '')
-                    xml_required_field.replace('nfe40_', '')
-                ] = field_data
-        if class_obj._generateds_type and kwargs:
-            #  FIXME: leiauteNFe hardcoded
-            ds_class = getattr(leiauteNFe, class_obj._generateds_type)
+
+                kwargs[field_spec_name] = field_data
+        if kwargs:
             ds_object = ds_class(**kwargs)
             return ds_object
 
