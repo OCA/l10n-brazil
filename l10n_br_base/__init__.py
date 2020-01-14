cc922d2c3732c8152ce485379fd254c146972398
Renato Lima <renato.lima@akretion.com>
2020-01-14 14:43:38 -0300

    Define l10n_br_simple as default COA for brazilian companies

--- l10n_br_base/__init__.py
+++ l10n_br_base/__init__.py
@@ -3,3 +3,24 @@
 
 from . import models
 from . import tests
+
+from odoo.addons import account
+from odoo import api, SUPERUSER_ID
+
+# Install Simple Chart of Account Template for Brazilian Companies
+_auto_install_l10n_original = account._auto_install_l10n
+
+
+def _auto_install_l10n_br_simple(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    country_code = env.user.company_id.country_id.code
+    if country_code and country_code.upper() == "BR":
+        module_ids = env["ir.module.module"].search(
+            [("name", "in", ("l10n_br_simple",)), ("state", "=", "uninstalled")]
+        )
+        module_ids.sudo().button_install()
+    else:
+        _auto_install_l10n_original(cr, registry)
+
+
+account._auto_install_l10n = _auto_install_l10n_br_simple
