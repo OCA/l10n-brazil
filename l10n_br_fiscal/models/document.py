5afc9723a0c43ce1c29bee1729d74aed8209aa03
Renato Lima <renato.lima@akretion.com>
2020-01-15 18:20:42 -0300

    [FIX] mixin fields in fiscal document

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -1,13 +1,28 @@
 # Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Document(models.Model):
     _name = "l10n_br_fiscal.document"
     _inherit = "l10n_br_fiscal.document.abstract"
     _description = "Fiscal Document"
+
+    @api.model
+    def _default_operation(self):
+        # TODO add in res.company default Operation?
+        return self.env['l10n_br_fiscal.operation']
+
+    @api.model
+    def _operation_domain(self):
+        domain = [('state', '=', 'approved')]
+        return domain
+
+    operation_id = fields.Many2one(
+        default=_default_operation,
+        domain=lambda self: self._operation_domain()
+    )
 
     line_ids = fields.One2many(
         comodel_name="l10n_br_fiscal.document.line",
