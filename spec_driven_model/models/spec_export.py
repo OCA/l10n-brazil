6f97e1cfc283c0b5cc7e9db2666572c1bd49d0b5
Luis Felipe Mileo <mileo@kmee.com.br>
2020-01-22 08:39:32 -0300

    [REF] Clean code
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- spec_driven_model/models/spec_export.py
+++ spec_driven_model/models/spec_export.py
@@ -13,6 +13,95 @@ except ImportError:
 class AbstractSpecMixin(models.AbstractModel):
     _inherit = 'spec.mixin'
 
+    def _get_ds_class(self, class_obj):
+        #  FIXME: leiauteNFe hardcoded
+        return getattr(leiauteNFe, class_obj._generateds_type)
+
+    def _export_field(self, xsd_fields, class_obj, export_dict):
+        ds_class = self._get_ds_class(class_obj)
+        ds_class_sepc = {i.name: i for i in ds_class.member_data_items_}
+
+        for xsd_field in xsd_fields:
+            # TODO: Export number required fields with Zero.
+            # xsd_required = self.env[class_name]._fields[xsd_field]._attrs.get(
+            #     'xsd_required')
+
+            # FIXME: xsd_field.replace(class_obj._field_prefix, '')
+            field_spec_name = xsd_field.replace('nfe40_', '')
+            member_spec = ds_class_sepc[field_spec_name]
+
+            # if not self[xsd_field]:
+            #     continue
+
+            # print(self[xsd_field])
+            # print(xsd_field)
+            # print(self._fields[xsd_field].type)
+            # print(member_spec.data_type[0])
+
+            if self._fields[xsd_field].type == 'many2one':
+                field_data = self._export_many2one(xsd_field, class_obj)
+            elif self._fields[xsd_field].type == 'one2many':
+                field_data = self._export_one2many(xsd_field, class_obj)
+            elif self._fields[xsd_field].type == 'datetime' and self[xsd_field]:
+                field_data = self._export_datetime(xsd_field)
+            elif self._fields[xsd_field].type == 'date' and self[xsd_field]:
+                field_data = self._export_date(xsd_field)
+            elif self._fields[xsd_field].type in ('float', 'monetary') and self[xsd_field] is not False:
+                field_data = self._export_float_monetary(
+                    xsd_field, member_spec)
+            else:
+                field_data = self[xsd_field]
+
+            if not self[xsd_field] and not field_data:
+                continue
+
+            # print(field_data)
+            export_dict[field_spec_name] = field_data
+
+    def _export_many2one(self, field_name, class_obj=None):
+        if self._fields[field_name]._attrs.get('original_spec_model'):
+            field_data = self[field_name]._build_generateds(
+                class_name=self._fields[field_name]._attrs.get(
+                    'original_spec_model')
+            )
+        else:
+            # continue
+            try:
+                field_data = self._build_generateds(
+                    class_obj._fields[field_name].comodel_name)
+            except:
+                field_data = self[field_name]._build_generateds(
+                    class_obj._fields[field_name].comodel_name)
+        return field_data
+
+    def _export_one2many(self, field_name, class_obj=None):
+        relational_data = []
+        for relational_field in self[field_name]:
+            relational_data.append(
+                relational_field._build_generateds(
+                    class_obj._fields[field_name].comodel_name
+                )
+            )
+        return relational_data
+
+    def _export_float_monetary(self, field_name, member_spec):
+        if member_spec.data_type[0]:
+            TDec = ''.join(filter(lambda x: x.isdigit(),
+                                  member_spec.data_type[0]))[-2:]
+            format = "%.{0}f".format(TDec)
+            return str(format % self[field_name])
+        else:
+            raise NotImplementedError
+
+    def _export_date(self, field_name):
+        return str(self[field_name])
+
+    def _export_datetime(self, field_name):
+        return str(fields.Datetime.context_timestamp(
+            self,
+            fields.Datetime.from_string(self[field_name])
+        ).isoformat('T'))
+
     def _get_model_classes(self):
         classes = [getattr(x, '_name', None) for x in type(self).mro()]
         return classes
@@ -35,17 +124,9 @@ class AbstractSpecMixin(models.AbstractModel):
         return spec_classes
 
     def _build_generateds(self, class_name=False):
-
         if not class_name:
             if hasattr(self, '_stacked'):
                 class_name = self._stacked
-                # spec_classes = self._get_spec_classes()
-                # specs = {}
-                # for spec_class in spec_classes:
-                #     specs[spec_class] = self._build_generateds(
-                #         class_name=spec_class
-                #     )
-                # pass
             else:
                 class_name = self._name
 
@@ -53,76 +134,15 @@ class AbstractSpecMixin(models.AbstractModel):
         if not class_obj._generateds_type:
             return
 
-        # Remember to replace with generators
-        xml_required_fields = [
+        xsd_fields = (
             i for i in self.env[class_name]._fields if
             self.env[class_name]._fields[i]._attrs.get('xsd')
-        ]
+        )
 
         kwargs = {}
-        #  FIXME: leiauteNFe hardcoded
-        ds_class = getattr(leiauteNFe, class_obj._generateds_type)
-        ds_class_sepc = {i.name: i for i in ds_class.member_data_items_}
 
-        for xml_required_field in xml_required_fields:
-
-            # FIXME: xml_required_field.replace(class_obj._field_prefix, '')
-            field_spec_name = xml_required_field.replace('nfe40_', '')
-            member_spec = ds_class_sepc[field_spec_name]
-
-            # if not self[xml_required_field]:
-            #     continue
-
-            # print(self[xml_required_field])
-            # print(xml_required_field)
-            # print(self._fields[xml_required_field].type)
-            # print(member_spec.data_type[0])
-
-            if self._fields[xml_required_field].type == 'many2one':
-                if self._fields[xml_required_field]._attrs.get('original_spec_model'):
-                    field_data = self[xml_required_field]._build_generateds(
-                        class_name=self._fields[xml_required_field]._attrs.get('original_spec_model')
-                    )
-                else:
-                    # continue
-                    try:
-                        field_data = self._build_generateds(
-                            class_obj._fields[xml_required_field].comodel_name)
-                    except:
-                        field_data = self[xml_required_field]._build_generateds(
-                            class_obj._fields[xml_required_field].comodel_name)
-            elif self._fields[xml_required_field].type == 'one2many':
-                relational_data = []
-                for relational_field in self[xml_required_field]:
-                    relational_data.append(
-                        relational_field._build_generateds(
-                            class_obj._fields[xml_required_field].comodel_name
-                        )
-                    )
-                field_data = relational_data
-            elif self._fields[xml_required_field].type == 'datetime' and self[xml_required_field]:
-                field_data = str(fields.Datetime.context_timestamp(
-                    self,
-                    fields.Datetime.from_string(self[xml_required_field])
-                ).isoformat('T'))
-            elif self._fields[xml_required_field].type == 'date' and self[xml_required_field]:
-                field_data = str(self[xml_required_field])
-            elif self._fields[xml_required_field].type in ('float', 'monetary') and self[xml_required_field] is not False:
-                if member_spec.data_type[0]:
-                    TDec = ''.join(filter(lambda x: x.isdigit(),
-                                          member_spec.data_type[0]))[-2:]
-                    format = "%.{0}f".format(TDec)
-                    field_data = str(format % self[xml_required_field])
-                else:
-                    raise NotImplementedError
-            else:
-                field_data = self[xml_required_field]
-
-            if not self[xml_required_field] and not field_data:
-                continue
-
-            # print(field_data)
-            kwargs[field_spec_name] = field_data
+        ds_class = self._get_ds_class(class_obj)
+        self._export_field(xsd_fields, class_obj, export_dict=kwargs)
 
         if kwargs:
             ds_object = ds_class(**kwargs)
