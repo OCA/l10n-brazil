c3786c92494695b578848b0f7d62ffd8babba8bb
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-05 18:56:50 -0300

    [NEW] Eventos eletronicos
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/models/document_event.py
+++ l10n_br_fiscal/models/document_event.py
@@ -2,13 +2,42 @@
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+import os
 import datetime
+import base64
+from erpbrasil.base.misc import punctuation_rm
 
+from odoo.tools import config
 from odoo import api, fields, models
+from odoo.exceptions import UserError
+
+CODIGO_NOME = {
+    "55": "nf-e",
+    "SE": "nfs-e",
+    "65": "nfc-e",
+}
+
+
+def caminho_empresa(company_id, document):
+    db_name = company_id._cr.dbname
+    cnpj = punctuation_rm(company_id.cnpj_cpf)
+
+    filestore = config.filestore(db_name)
+    path = '/'.join([filestore, 'edoc', document, cnpj])
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError:
+            raise RedirectWarning(
+                _(u'Erro!'),
+                _(u"""Verifique as permissões de escrita
+                    e o caminho da pasta"""))
+    return path
+
 
 
 class DocumentEvent(models.Model):
-    _name = "l10n_br_account.document_event"
+    _name = "l10n_br_fiscal.document_event"
 
     type = fields.Selection(
         selection=[
@@ -75,8 +104,8 @@ class DocumentEvent(models.Model):
         default="draft",
     )
 
-    document_event_ids = fields.Many2one(
-        comodel_name="account.invoice", string=u"Documentos"
+    fiscal_document_event_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document", string=u"Documentos"
     )
 
     cancel_document_event_id = fields.Many2one(
@@ -98,7 +127,111 @@ class DocumentEvent(models.Model):
         names = ["Evento", self.company_id.name, self.origin]
         self.display_name = " / ".join(filter(None, names))
 
+    xml_sent_id = fields.Many2one(
+        comodel_name="ir.attachment", string="XML", copy=False, readony=True
+    )
+    xml_returned_id = fields.Many2one(
+        comodel_name="ir.attachment",
+        string="XML de autorização",
+        copy=False,
+        readony=True,
+    )
+
+    @staticmethod
+    def monta_caminho(ambiente, company_id, chave):
+        caminho = caminho_empresa(company_id, chave[:2])
+
+        if ambiente == 1:
+            caminho = os.path.join(caminho, "producao/")
+        else:
+            caminho = os.path.join(caminho, "homologacao/")
+
+        data = "20" + chave[2:4] + "-" + chave[4:6]
+        serie = chave[22:25]
+        numero = chave[25:34]
+
+        caminho = os.path.join(caminho, data + "/")
+        caminho = os.path.join(caminho, serie + "-" + numero + "/")
+
+        try:
+            os.makedirs(caminho)
+        except:
+            pass
+        return caminho
+
+    def _grava_arquivo_disco(self, arquivo, file_name):
+        save_dir = self.monta_caminho(
+            ambiente=int(self.company_id.nfe_environment),
+            company_id=self.company_id,
+            chave=self.fiscal_document_event_id.key or self.fiscal_document_event_id.number  # FIXME:,
+        )
+        file_path = os.path.join(save_dir, file_name)
+        try:
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+            f = open(file_path, "w")
+        except IOError:
+            raise UserError(
+                _(u"Erro!"),
+                _(
+                    u"""Não foi possível salvar o arquivo
+                    em disco, verifique as permissões de escrita
+                    e o caminho da pasta"""
+                ),
+            )
+        else:
+            f.write(arquivo)
+            f.close()
+        return file_path
+
+    def _grava_anexo(
+        self, arquivo, extensao_sem_ponto, autorizacao=False, sequencia=False
+    ):
+        self.ensure_one()
+
+        file_name = ""
+        file_name += self.fiscal_document_event_id.key or self.fiscal_document_event_id.number  # FIXME:
+        file_name += "-"
+        if autorizacao:
+            file_name += "proc-"
+        if sequencia:
+            file_name += str(sequencia) + "-"
+        file_name += CODIGO_NOME[
+            self.fiscal_document_event_id.document_type_id.code]
+        file_name += "." + extensao_sem_ponto
+
+        file_path = self._grava_arquivo_disco(arquivo, file_name)
+
+        ir_attachment_id = self.env["ir.attachment"].search(
+            [
+                ("res_model", "=", self._name),
+                ("res_id", "=", self.id),
+                ("name", "=", file_name),
+            ]
+        )
+        ir_attachment_id.unlink()
+
+        attachment_id = ir_attachment_id.create(
+            {
+                "name": file_name,
+                "datas_fname": file_name,
+                "res_model": self._name,
+                "res_id": self.id,
+                "datas": base64.b64encode(arquivo.encode('utf-8')),
+                "mimetype": "application/" + extensao_sem_ponto,
+                "type": "binary",
+            }
+        )
+
+        if autorizacao:
+            vals = {"file_returned": file_path, "xml_returned_id": attachment_id.id}
+        else:
+            vals = {"file_sent": file_path, "xml_sent_id": attachment_id.id}
+        self.write(vals)
+        return attachment_id
+
     @api.multi
-    def set_done(self):
-        self.write({"state": "done", "end_date": datetime.datetime.now()})
-        return True
+    def set_done(self, arquivo_xml):
+        self._grava_anexo(arquivo_xml, "xml", autorizacao=True)
+        self.write({"state": "done", "end_date": fields.Datetime.now()})
+
