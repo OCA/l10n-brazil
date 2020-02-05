89dc50cb8c8119d51df5389ebd8118ba8ca8fe37
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-05 18:56:50 -0300

    [NEW] Eventos eletronicos
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/models/document_correction.py
+++ l10n_br_fiscal/models/document_correction.py
@@ -5,25 +5,23 @@
 from odoo import api, fields, models
 
 
-class DocumentCancel(models.Model):
-    _name = "l10n_br_fiscal.document.cancel"
-    _description = "Documento Eletrônico no Sefaz"
+class DocumentCorrection(models.Model):
+    _name = "l10n_br_fiscal.document.correction"
+    _description = "Carta de Correção no Sefaz"
 
-    partner_id = fields.Many2one(
-        comodel_name="res.partner", related="invoice_id.partner_id", string="Cliente"
+    motivo = fields.Text(string=u"Motivo", readonly=True, required=True)
+
+    sequencia = fields.Char(
+        string=u"Sequência", help=u"Indica a sequência da carta de correcão"
     )
 
-    justificative = fields.Char(
-        string="Justificativa", size=255, readonly=True, required=True
-    )
-
-    cancel_document_event_ids = fields.One2many(
-        comodel_name="l10n_br_account.document_event",
-        inverse_name="cancel_document_event_id",
+    cce_document_event_ids = fields.One2many(
+        comodel_name="l10n_br_fiscal.document_event",
+        inverse_name="document_event_ids",
         string=u"Eventos",
     )
 
-    display_name = fields.Char(string=u"Nome", compute="_compute_display_name")
+    display_name = fields.Char(string="Name", compute="_compute_display_name")
 
     @api.multi
     @api.depends("invoice_id.number", "invoice_id.partner_id.name")
@@ -31,18 +29,3 @@ class DocumentCancel(models.Model):
         self.ensure_one()
         names = ["Fatura", self.invoice_id.number, self.invoice_id.partner_id.name]
         self.display_name = " / ".join(filter(None, names))
-
-    @api.multi
-    def _check_justificative(self):
-        for invalid in self:
-            if len(invalid.justificative) < 15:
-                return False
-        return True
-
-    _constraints = [
-        (
-            _check_justificative,
-            u"Justificativa deve ter tamanho mínimo de 15 caracteres.",
-            ["justificative"],
-        )
-    ]
