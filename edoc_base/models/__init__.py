53ccb6b74793b3a8a94a67aa42c64364abefceb7
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-07 17:20:41 -0300

    [REM] Legacy module edoc_base
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- edoc_base/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_invoice
-from . import res_company
-from . import l10n_br_account_document_event
--- edoc_base/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2016  Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import misc
