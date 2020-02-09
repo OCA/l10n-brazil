48cb5681503aa6ab2f3cc3ce8d941f816c207e78
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-09 01:29:40 -0300

    [REF] Flake8/pylint: l10n_br_account
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account/models/fiscal_tax_group.py
+++ l10n_br_account/models/fiscal_tax_group.py
@@ -25,4 +25,4 @@ class FiscalTaxGroup(models.Model):
     def create(self, values):
         if not values.get("partner_id"):
             self.clear_caches()
-            return super(FiscalTax, self).create(values)
+            return super(FiscalTaxGroup, self).create(values)
