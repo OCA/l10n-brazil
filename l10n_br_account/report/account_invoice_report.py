3fbdd1f4062865626065645c010c4271a5792044
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-09 01:29:40 -0300

    [REF] Flake8/pylint: l10n_br_account
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account/report/account_invoice_report.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2016  Magno Costa - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import models
-
-
-class AccountInvoiceReport(models.Model):
-    _inherit = "account.invoice.report"
