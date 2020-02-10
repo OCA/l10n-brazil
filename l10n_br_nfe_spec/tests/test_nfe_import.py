5c61823433b1d090f78ad845724984cc327a06ad
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-10 01:23:03 -0300

    [FIX] Test lint errors
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_nfe_spec/tests/test_nfe_import.py
+++ l10n_br_nfe_spec/tests/test_nfe_import.py
@@ -1,7 +1,10 @@
+import logging
 import os
 from nfelib.v4_00 import leiauteNFe_sub as nfe_sub
 from odoo.tests.common import TransactionCase
 from odoo.addons import l10n_br_nfe_spec
+
+_logger = logging.getLogger(__name__)
 
 
 class NFeImportTest(TransactionCase):
@@ -9,8 +12,8 @@ class NFeImportTest(TransactionCase):
         path = os.path.join(l10n_br_nfe_spec.__path__[0],
                             'tests', 'nfe', 'v4_00', 'leiauteNFe')
         for filename in os.listdir(path):
-            print(filename, dir(self.env["nfe.40.infnfe"]))
+            _logger.info(filename, dir(self.env["nfe.40.infnfe"]))
             obj = nfe_sub.parse('%s/%s' % (path, filename,))
-            print(obj.infNFe)
+            _logger.info(obj.infNFe)
             nfe = self.env["nfe.40.infnfe"].build(obj.infNFe)
-            print(nfe.nfe40_emit.nfe40_CNPJ)
+            _logger.info(nfe.nfe40_emit.nfe40_CNPJ)
