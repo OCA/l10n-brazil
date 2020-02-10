cd4ee60f25cb99a33d91726fb79d2a25783becda
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-09 21:55:52 -0300

    [IMP] Document events: invalidate, cancel, correction
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/models/document_cancel.py
+++ l10n_br_fiscal/models/document_cancel.py
@@ -10,27 +10,48 @@ class DocumentCancel(models.Model):
     _name = "l10n_br_fiscal.document.cancel"
     _description = "Fiscal Document Cancel Record"
 
+    document_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document",
+        string="Documento",
+        index=True,
+    )
+
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        related="document_id.partner_id",
+        string="Partner",
+        index=True,
+    )
+
+    company_id = fields.Many2one(
+        comodel_name="res.partner",
+        related="document_id.partner_id",
+        string="Company",
+        index=True,
+    )
+
     justificative = fields.Char(
         string="Justificative",
         size=255,
         required=True)
 
-    document_event_ids = fields.One2many(
+    cancel_document_event_ids = fields.One2many(
         comodel_name="l10n_br_fiscal.document.event",
-        inverse_name="document_cancel_id",
-        string="Events")
+        inverse_name="cancel_document_event_id",
+        string=u"Eventos",
+    )
 
     display_name = fields.Char(
         string="Name",
         compute="_compute_display_name")
 
     @api.multi
-    @api.depends("document_event_ids")
+    @api.depends("document_id.number", "document_id.partner_id.name")
     def _compute_display_name(self):
-        for record in self:
-            if record.document_event_ids:
-                # TODO
-                record.display_name = record.document_event_ids[0].origin
+        self.ensure_one()
+        names = ["Fatura", self.document_id.number,
+                 self.document_id.partner_id.name]
+        self.display_name = " / ".join(filter(None, names))
 
     @api.multi
     @api.constrains("justificative")
@@ -38,4 +59,5 @@ class DocumentCancel(models.Model):
         for record in self:
             if len(record.justificative) < 15:
                 raise ValidationError(_(
-                    "Justificativa deve ter tamanho mínimo de 15 caracteres."))
+                    "Justificativa deve ter tamanho mínimo de 15 caracteres.")
+                )
