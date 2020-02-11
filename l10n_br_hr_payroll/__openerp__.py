9ffc0994a4dd846ca7826af5531f1aabc1091960
Hendrix Costa <hendrix.costa@abgf.gov.br>
2020-02-11 10:58:54 -0300

    [REF] Nome do módulo

--- l10n_br_hr_payroll/__openerp__.py
+++ l10n_br_hr_payroll/__openerp__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2016 KMEE (http://www.kmee.com.br)
+# Copyright (C) 2020 ABGF (http://www.abgf.gov.br)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
@@ -7,7 +8,7 @@
     'category': 'Localization',
     'license': 'AGPL-3',
     'author': 'KMEE, Odoo Community Association (OCA)',
-    'maintainer': 'KMEE',
+    'maintainer': ['KMEE', 'ABGF'],
     'website': 'http://www.kmee.com.br',
     'version': '8.0.0.0.1',
     'depends': [
@@ -65,7 +66,6 @@
         'views/l10n_br_hr_contract_change/jornada.xml',
         'views/l10n_br_hr_contract_change/filiacao_sindical.xml',
         'views/l10n_br_hr_contract_change/cargo_atividade.xml',
-
         'views/l10n_br_hr_acordo_coletivo.xml',
 
         # wizards
