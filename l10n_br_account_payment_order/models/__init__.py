e7de0be18fd4f7bb1d388988134c7e6b842374bf
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2020-02-12 15:32:04 -0300

    [NEW] Operation type field for payments

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -2,7 +2,9 @@
 #  Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_payment_line
+
 from . import account_move_line
 from . import account_payment_mode
+from . import account_payment_order
+from . import account_payment_line
 from . import res_partner_bank
