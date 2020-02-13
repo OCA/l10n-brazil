3296afb68cf6758abce765316d4e902f02b7a0a8
Hendrix Costa <hendrix.costa@abgf.gov.br>
2020-02-13 16:34:57 -0300

    [FIX] Cálculo progressivo do INSS

--- l10n_br_hr_payroll/models/l10n_br_hr_social_security_tax.py
+++ l10n_br_hr_payroll/models/l10n_br_hr_social_security_tax.py
@@ -4,15 +4,14 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import logging
+from datetime import datetime
 
-from openerp import api, fields, models
-from openerp import exceptions, _
+from openerp import api, fields, models, exceptions, _
 
 _logger = logging.getLogger(__name__)
 
 try:
     from pybrasil.valor.decimal import Decimal, ROUND_DOWN
-
 except ImportError:
     _logger.info('Cannot import pybrasil')
 
@@ -22,30 +21,34 @@ class L10nBrHrSocialTax(models.Model):
     _description = 'Brazilian HR - Social Security Tax Table'
     _order = 'year desc, max_wage'
 
-    year = fields.Integer(string='Year', required=True)
-
-    start_period_id = fields.Many2one(
-        string=u'Período Inicial',
-        comodel_name='account.period',
-        domain=[('special', '=', False)],
+    year = fields.Char(
+        string='Ano Referência',
+        size=4,
         required=True,
+        default=datetime.now().year,
     )
 
-    end_period_id = fields.Many2one(
-        string=u'Período Final',
-        comodel_name='account.period',
-        domain=[('special', '=', False)],
-        required=True,
+    date_start = fields.Date(
+        string=u"Data de início",
     )
 
-    min_wage = fields.Float(string='Min Wage', required=True)
+    date_stop = fields.Date(
+        string=u"Data Final",
+    )
 
-    max_wage = fields.Float(string='Wage up to', required=True)
+    min_wage = fields.Float(string='Faixa Salarial de:', required=True)
+
+    max_wage = fields.Float(string='Faixa salarial até:', required=True)
 
     rate = fields.Float(
-        string='Rate (%)',
-        help='Valor em percentagem. Ex.: 8% | 9%',
+        string='Alíquota (%)',
+        help='Alíquota Ex.: 8% | 9%',
         required=True,
+    )
+
+    calculo_progressivo = fields.Boolean(
+        string='Cálculo Progressivo?',
+        help='Utilizar a Fórmula Progressiva da Aposentadoria',
     )
 
     @api.onchange('year')
@@ -54,13 +57,11 @@ class L10nBrHrSocialTax(models.Model):
         """
         for record in self:
             if record.year:
-                record.start_period_id = self.env['account.period'].\
-                    find('{}-01-01'.format(record.year))
-                record.end_period_id = self.env['account.period'].\
-                    find('{}-12-01'.format(record.year))
+                record.date_start = '{}-01-01'.format(record.year)
+                record.date_stop = '{}-12-31'.format(record.year)
             else:
-                record.start_period_id = False
-                record.end_period_id = False
+                record.date_start = False
+                record.date_stop = False
 
     def find(self, date_from):
         """
@@ -68,8 +69,8 @@ class L10nBrHrSocialTax(models.Model):
         :return:
         """
         faixas_vigentes_ids = self.search([
-            ('start_period_id.date_start', '<=', date_from),
-            ('end_period_id.date_stop', '>=', date_from),
+            ('date_start', '<=', date_from),
+            ('date_stop', '>=', date_from),
         ])
 
         return faixas_vigentes_ids
@@ -82,23 +83,42 @@ class L10nBrHrSocialTax(models.Model):
             result.append((record['id'], name))
         return result
 
+    def forma_de_calculo_progressivo(self, tabela_vigente):
+        """
+        """
+        return any(tabela_vigente.mapped('calculo_progressivo'))
+
     def _compute_inss(self, BASE_INSS, date_from):
         ano = fields.Datetime.from_string(date_from).year
         tabela_vigente = self.find(date_from)
+        inss = 0
 
-        if tabela_vigente:
-            for faixa in tabela_vigente:
-                if BASE_INSS < faixa.max_wage:
-                    inss = Decimal(BASE_INSS)
-                    inss *= Decimal(faixa.rate) / 100
-                    inss = inss.quantize(Decimal('0.01'), ROUND_DOWN)
-                    return inss, faixa.rate
-
-            inss = Decimal(tabela_vigente[-1].max_wage)
-            inss *= Decimal(tabela_vigente[-1].rate) / 100
-            inss = inss.quantize(Decimal('0.01'), ROUND_DOWN)
-            return inss, tabela_vigente[-1].rate
-
-        else:
+        if not tabela_vigente:
             raise exceptions.Warning(
                 _('Tabela de INSS do ano {} Não encontrada!'.format(ano)))
+
+        base_inss = Decimal(BASE_INSS)
+        for faixa in tabela_vigente:
+
+            if BASE_INSS < faixa.max_wage:
+                inss += base_inss * Decimal(faixa.rate) / 100
+                # inss = inss.quantize(Decimal('0.01'), ROUND_DOWN)
+                return inss, faixa.rate
+
+            else:
+                # Quando for forma progressiva, acumula o valor de cada faixa
+                if self.forma_de_calculo_progressivo(tabela_vigente):
+                    inss += (Decimal(faixa.max_wage) - Decimal(faixa.min_wage)) * Decimal(faixa.rate) / 100
+                    # inss = inss.quantize(Decimal('0.01'), ROUND_DOWN)
+                    base_inss -= Decimal(faixa.max_wage)
+
+        # Forma progressiva e ainda nao encontrou o teto, entao retornar
+        # valor acumulado
+        if self.forma_de_calculo_progressivo(tabela_vigente):
+            return inss, faixa.rate
+
+        # Forma tradicional de calculo
+        inss = Decimal(tabela_vigente[-1].max_wage)
+        inss *= Decimal(tabela_vigente[-1].rate) / 100
+        inss = inss.quantize(Decimal('0.01'), ROUND_DOWN)
+        return inss, tabela_vigente[-1].rate
