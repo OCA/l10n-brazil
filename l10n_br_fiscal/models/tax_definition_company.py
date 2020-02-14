8595811695a403e13759aae52169c3ed93788239
Renato Lima <renato.lima@akretion.com>
2020-02-14 11:23:21 -0300

    [REF] tax definition

--- l10n_br_fiscal/models/tax_definition_company.py
+++ l10n_br_fiscal/models/tax_definition_company.py
@@ -3,24 +3,17 @@
 
 from odoo import fields, models
 
-from ..constants.fiscal import FISCAL_IN_OUT, FISCAL_OUT
-
 
 class TaxDefinitionCompany(models.Model):
-    _name = "l10n_br_fiscal.tax.definition.company"
-    _inherit = "l10n_br_fiscal.tax.definition.abstract"
+    _inherit = "l10n_br_fiscal.tax.definition"
     _description = "Tax Definition Company"
 
-    company_id = fields.Many2one(comodel_name="res.company", string="Company")
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company")
 
-    type_in_out = fields.Selection(
-        selection=FISCAL_IN_OUT, string="Type", default=FISCAL_OUT
-    )
-
-    _sql_constraints = [
-        (
-            "fiscal_tax_definition_cfop_uniq",
-            "unique (company_id, tax_group_id)",
-            "Tax Definition already exists for this Company and Group !",
-        )
-    ]
+    _sql_constraints = [(
+        "fiscal_tax_definition_company_group_uniq",
+        "unique (company_id, tax_group_id)",
+        "Tax Definition already exists for this Company and Group !",
+    )]
