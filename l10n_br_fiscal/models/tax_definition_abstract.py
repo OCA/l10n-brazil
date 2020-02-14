9cb2ed2c0bb40dc3c2ca648f99adff3fd658887e
Renato Lima <renato.lima@akretion.com>
2020-02-14 11:23:21 -0300

    [REF] tax definition

--- l10n_br_fiscal/models/tax_definition_abstract.py
+++ l10n_br_fiscal/models/tax_definition_abstract.py
@@ -3,50 +3,59 @@
 
 from odoo import api, fields, models
 
-from ..constants.fiscal import FISCAL_IN_OUT, FISCAL_OUT, TAX_DOMAIN
+from ..constants.fiscal import (
+    FISCAL_IN_OUT,
+    FISCAL_OUT,
+    TAX_DOMAIN
+)
 
 
-class TaxDefinitionAbstract(models.AbstractModel):
-    _name = "l10n_br_fiscal.tax.definition.abstract"
-    _description = "Tax Definition Abstract"
+class TaxDefinition(models.Model):
+    _name = "l10n_br_fiscal.tax.definition"
+    _description = "Tax Definition"
 
     type_in_out = fields.Selection(
-        selection=FISCAL_IN_OUT, string="Type", required=True, default=FISCAL_OUT
-    )
+        selection=FISCAL_IN_OUT,
+        string="Type",
+        required=True,
+        default=FISCAL_OUT)
 
     tax_group_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax.group", string="Tax Group"
-    )
+        comodel_name="l10n_br_fiscal.tax.group",
+        string="Tax Group")
 
-    custom_tax = fields.Boolean(string="Custom Tax")
+    custom_tax = fields.Boolean(
+        string="Custom Tax")
 
     tax_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.tax",
         domain="[('tax_group_id', '=', tax_group_id)]",
-        string="Tax",
-    )
+        string="Tax")
 
     cst_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.cst",
         domain="[('cst_type', 'in', (type_in_out, 'all')),"
         "('tax_domain', '=', tax_domain)]",
-        string="CST",
-    )
+        string="CST")
 
-    cst_code = fields.Char(string="CST Code", related="cst_id.code")
+    cst_code = fields.Char(
+        string="CST Code",
+        related="cst_id.code")
 
     tax_domain = fields.Selection(
         selection=TAX_DOMAIN,
         related="tax_group_id.tax_domain",
         store=True,
-        string="Tax Domain",
-    )
+        string="Tax Domain")
 
-    is_taxed = fields.Boolean(string="Taxed?")
+    is_taxed = fields.Boolean(
+        string="Taxed?")
 
-    is_debit_credit = fields.Boolean(string="Debit/Credit?")
+    is_debit_credit = fields.Boolean(
+        string="Debit/Credit?")
 
-    tax_retention = fields.Boolean(string="Tax Retention?")
+    tax_retention = fields.Boolean(
+        string="Tax Retention?")
 
     @api.onchange("is_taxed")
     def _onchange_tribute(self):
