aae30dfe0f8d6276261f778166978129a69dd744
Luis Felipe Mileo <mileo@kmee.com.br>
2020-02-13 23:49:31 -0300

    [NEW] Módulo de relatórios contabeis
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ l10n_br_mis_report/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Relatórios contábeis brasileiros: Balanço Patrimonial e DRE',
+    'summary': """
+        Templates de relatórios contábeis brasileiros: Balanço Patrimonial e DRE""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-brazil',
+    'maintainers': ['mileo'],
+    "development_status": "Alpha",
+    'depends': ['l10n_br_account'],
+    'data': [
+        # 'data/mis_report_styles.xml',
+
+        'data/mis_report_bp.xml',
+        'data/mis_report_dre.xml',
+
+        # 'data/mis_report_dfc.xml',
+        # 'data/mis_report_dmpl.xml',
+        # 'data/mis_report_dva.xml',
+    ],
+}
