5387e004bc8261597411570094d7cd2671c1ab83
Renato Lima <renato.lima@akretion.com>
2020-02-14 11:23:21 -0300

    [REF] tax definition

--- l10n_br_fiscal/models/tax_definition_cfop.py
+++ l10n_br_fiscal/models/tax_definition_cfop.py
@@ -1,29 +1,23 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
-
-from ..constants.fiscal import FISCAL_IN_OUT, FISCAL_OUT
+from odoo import api, fields, models
 
 
 class TaxDefinitionCFOP(models.Model):
-    _name = "l10n_br_fiscal.tax.definition.cfop"
-    _inherit = "l10n_br_fiscal.tax.definition.abstract"
+    _inherit = "l10n_br_fiscal.tax.definition"
     _description = "Tax Definition CFOP"
 
-    cfop_id = fields.Many2one(comodel_name="l10n_br_fiscal.cfop", string="CFOP")
+    cfop_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.cfop",
+        string="CFOP")
 
-    type_in_out = fields.Selection(
-        selection=FISCAL_IN_OUT,
-        string="Type",
-        related="cfop_id.type_in_out",
-        default=FISCAL_OUT,
-    )
+    _sql_constraints = [(
+        "fiscal_tax_definition_cfop_uniq",
+        "unique (cfop_id, tax_group_id)",
+        "Tax Definition already exists with this CFOP, Group !")]
 
-    _sql_constraints = [
-        (
-            "fiscal_tax_definition_cfop_uniq",
-            "unique (cfop_id, tax_group_id)",
-            "Tax Definition already exists with this CFOP, Group !",
-        )
-    ]
+    @api.onchange('cfop_id')
+    def _onchange_cfop_id(self):
+        if self.cfop_id:
+            self.type_in_out = self.cfop_id.type_in_out
