874c871284165d9fe36821ca84d04882df73e035
Renato Lima <renato.lima@akretion.com>
2020-02-17 16:48:13 -0300

    [ADD] Tax definition view file

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -34,7 +34,7 @@
         "views/cst_view.xml",
         "views/tax_group_view.xml",
         "views/tax_view.xml",
-        "views/tax_icms_regulation_view.xml",
+        "views/tax_definition_view.xml",
         "views/tax_pis_cofins_view.xml",
         "views/tax_pis_cofins_base_view.xml",
         "views/tax_pis_cofins_credit_view.xml",
