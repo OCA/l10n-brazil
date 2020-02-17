5fc0eb6a4eabed28b3ebbd1b522746ee92d61702
Renato Lima <renato.lima@akretion.com>
2020-02-17 16:52:28 -0300

    pep8

--- l10n_br_fiscal/models/ncm.py
+++ l10n_br_fiscal/models/ncm.py
@@ -48,75 +48,69 @@ class Ncm(models.Model):
 
     code_unmasked = fields.Char(size=8)
 
-    exception = fields.Char(string="Exception", size=2)
+    exception = fields.Char(
+        string="Exception",
+        size=2)
 
     tax_ipi_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.tax",
         string="Tax IPI",
-        domain="[('tax_domain', '=', 'ipi')]",
-    )
+        domain="[('tax_domain', '=', 'ipi')]")
 
     tax_ii_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.tax",
         string="Tax II",
-        domain="[('tax_domain', '=', 'ii')]",
-    )
+        domain="[('tax_domain', '=', 'ii')]")
 
-    uot_id = fields.Many2one(comodel_name="uom.uom", string="Tax UoM")
+    uot_id = fields.Many2one(
+        comodel_name="uom.uom",
+        string="Tax UoM")
 
-    uoe_id = fields.Many2one(comodel_name="uom.uom", string="Export UoM")
+    uoe_id = fields.Many2one(
+        comodel_name="uom.uom",
+        string="Export UoM")
 
     tax_estimate_ids = fields.One2many(
         comodel_name="l10n_br_fiscal.tax.estimate",
         inverse_name="ncm_id",
         string=u"Estimate Taxes",
-        readonly=True,
-    )
+        readonly=True)
 
     product_tmpl_ids = fields.One2many(
         comodel_name="product.template",
         string="Products",
-        compute="_compute_product_tmpl_info",
-    )
+        compute="_compute_product_tmpl_info")
 
     product_tmpl_qty = fields.Integer(
-        string="Products Quantity", compute="_compute_product_tmpl_info"
-    )
+        string="Products Quantity",
+        compute="_compute_product_tmpl_info")
 
     estimate_tax_national = fields.Float(
         string="Estimate Tax Nacional Percent",
         store=True,
         readonly=True,
         digits=dp.get_precision("Fiscal Tax Percent"),
-        compute="_compute_amount",
-    )
+        compute="_compute_amount")
 
     estimate_tax_imported = fields.Float(
         string="Estimate Tax Imported Percent",
         store=True,
         readonly=True,
         digits=dp.get_precision("Fiscal Tax Percent"),
-        compute="_compute_amount",
-    )
+        compute="_compute_amount")
 
-    _sql_constraints = [
-        (
-            "fiscal_ncm_code_exception_uniq",
-            "unique (code, exception)",
-            "NCM already exists with this code !",
-        )
-    ]
+    _sql_constraints = [(
+        "fiscal_ncm_code_exception_uniq",
+        "unique (code, exception)",
+        "NCM already exists with this code !")]
 
     @api.one
     def _compute_product_tmpl_info(self):
-        product_tmpls = self.env["product.template"].search(
-            [
-                ("ncm_id", "=", self.id),
-                "|",
-                ("active", "=", False),
-                ("active", "=", True),
-            ]
-        )
+        product_tmpls = self.env["product.template"].search([
+            ("ncm_id", "=", self.id), "|",
+            ("active", "=", False),
+            ("active", "=", True)])
+
         self.product_tmpl_ids = product_tmpls
         self.product_tmpl_qty = len(product_tmpls)
 
@@ -132,8 +126,7 @@ class Ncm(models.Model):
                 config = DeOlhoNoImposto(
                     company.ibpt_token,
                     punctuation_rm(company.cnpj_cpf),
-                    company.state_id.code,
-                )
+                    company.state_id.code)
 
                 result = get_ibpt_product(config, ncm.code_unmasked)
 
@@ -155,8 +148,11 @@ class Ncm(models.Model):
                 )
 
             except Exception as e:
-                _logger.warning("NCM Tax Estimate Failure: %s" % e)
-                ncm.message_post(body=str(e), subject=_("NCM Tax Estimate Failure"))
+                _logger.warning(
+                    "NCM Tax Estimate Failure: %s" % e)
+                ncm.message_post(
+                    body=str(e),
+                    subject=_("NCM Tax Estimate Failure"))
                 continue
 
     @api.model
