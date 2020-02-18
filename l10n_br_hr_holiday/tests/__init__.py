f0a507bb7d594f12b0609fd1bc64d9a24468f3cc
Hendrix Costa <hendrix.costa@abgf.gov.br>
2020-02-18 11:47:39 -0300

    [FIX] File name

--- l10n_br_hr_holiday/tests/__init__.py
+++ l10n_br_hr_holiday/tests/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import test_hr_holiday
+from . import test_hr_leave
 from . import test_resource_calendar
