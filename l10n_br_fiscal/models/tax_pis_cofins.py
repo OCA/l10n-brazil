54766fe7cf603abef56375292c872780ca43e417
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-02-19 11:24:05 -0300

    [FIX] Função não importada

--- l10n_br_fiscal/models/tax_pis_cofins.py
+++ l10n_br_fiscal/models/tax_pis_cofins.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class TaxPisCofins(models.Model):
