1af882aaa9a6594db448a030890610934ce496e7
Renato Lima <renato.lima@akretion.com>
2020-03-03 18:20:15 -0300

    [ADD] module version in manifest

--- l10n_br_simple/__manifest__.py
+++ l10n_br_simple/__manifest__.py
@@ -8,6 +8,7 @@
     "license": "AGPL-3",
     "author": "Akretion, " "Odoo Community Association (OCA)",
     "website": "http://github.com/OCA/l10n-brazil",
+    "version": "12.0.1.0.0",
     "depends": ["account"],
     "data": [
         "data/l10n_br_chart_data.xml",
