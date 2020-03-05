557eb5f50aecc3da7e964627a43d0cb8f7694e80
Renato Lima <renato.lima@akretion.com>
2020-03-05 17:19:24 -0300

    [REF] Adapting tax definitiion for operation line

--- l10n_br_fiscal/models/tax_definition_company.py
+++ l10n_br_fiscal/models/tax_definition_company.py
@@ -1,12 +1,11 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class TaxDefinitionCompany(models.Model):
     _inherit = "l10n_br_fiscal.tax.definition"
-    _description = "Tax Definition Company"
 
     company_id = fields.Many2one(
         comodel_name="res.company",
@@ -15,5 +14,5 @@ class TaxDefinitionCompany(models.Model):
     _sql_constraints = [(
         "fiscal_tax_definition_company_group_uniq",
         "unique (company_id, tax_group_id)",
-        "Tax Definition already exists for this Company and Group !",
+        _("Tax Definition already exists for this Company and Group !"),
     )]
