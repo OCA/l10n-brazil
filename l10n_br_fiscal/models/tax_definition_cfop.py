8f31dbf1052bb470e1511a90dc1abbf15db0fc3c
Renato Lima <renato.lima@akretion.com>
2020-03-05 17:19:24 -0300

    [REF] Adapting tax definitiion for operation line

--- l10n_br_fiscal/models/tax_definition_cfop.py
+++ l10n_br_fiscal/models/tax_definition_cfop.py
@@ -1,12 +1,11 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class TaxDefinitionCFOP(models.Model):
     _inherit = "l10n_br_fiscal.tax.definition"
-    _description = "Tax Definition CFOP"
 
     cfop_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.cfop",
@@ -15,7 +14,7 @@ class TaxDefinitionCFOP(models.Model):
     _sql_constraints = [(
         "fiscal_tax_definition_cfop_uniq",
         "unique (cfop_id, tax_group_id)",
-        "Tax Definition already exists with this CFOP, Group !")]
+        _("Tax Definition already exists with this CFOP, Group !"))]
 
     @api.onchange('cfop_id')
     def _onchange_cfop_id(self):
