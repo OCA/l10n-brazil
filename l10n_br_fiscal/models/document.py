815eeb78587063566c6308784f1ed82aafe0194b
Renato Lima <renato.lima@akretion.com>
2020-03-05 16:30:22 -0300

    [ADD] fiscal document number constraint

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -2,8 +2,15 @@
 # Copyright (C) 2019  KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
 from erpbrasil.base import misc
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+from ..constants.fiscal import (
+    DOCUMENT_ISSUER_COMPANY,
+    DOCUMENT_ISSUER_PARTNER
+)
 
 
 class Document(models.Model):
@@ -45,6 +52,28 @@ class Document(models.Model):
     # TODO Porque não usar o campo state do fiscal.document???
     state = fields.Selection(related="state_edoc")
 
+    @api.multi
+    @api.constrains("number")
+    def _check_number(self):
+        for record in self:
+            domain = [
+                ("id", "!=", record.id),
+                ('active', '=', True),
+                ('company_id', '=', record.company_id.id),
+                ('issuer', '=', record.issuer),
+                ('document_type_id', '=', record.document_type_id.id),
+                ('document_serie', '=', record.document_serie),
+                ('number', '=', record.number)]
+
+            if not record.issuer == DOCUMENT_ISSUER_PARTNER:
+                domain.append(('partner_id', '=', record.partner_id.id))
+
+            if record.env["l10n_br_fiscal.document"].search(domain):
+                raise ValidationError(_(
+                    "There is already a fiscal document with this "
+                    "Serie: {0}, Number: {1} !".format(
+                        record.document_serie, record.number)))
+
     # TODO Este método deveria estar no fiscal document abstract
     def document_number(self):
         super(Document, self).document_number()
@@ -53,7 +82,8 @@ class Document(models.Model):
                     not record.key:
                 record._generate_key()
 
-    # TODO este método deveria estar no Deveria
+    # TODO - este método deveria estar no l10n_br_nfe e o calculo da chave
+    # Deveria estar no erpbrasil.base.fiscal
     def _generate_key(self):
         company = self.company_id.partner_id
         chave = str(company.state_id and company.state_id.ibge_code or "").zfill(2)
