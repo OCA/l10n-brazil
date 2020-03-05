f2f40b9f26161fc2c0bcd26451498e2ca12835d4
Renato Lima <renato.lima@akretion.com>
2020-03-05 17:19:24 -0300

    [REF] Adapting tax definitiion for operation line

--- l10n_br_fiscal/models/__init__.py
+++ l10n_br_fiscal/models/__init__.py
@@ -30,6 +30,7 @@ from . import tax_definition
 from . import tax_definition_company
 from . import tax_definition_cfop
 from . import tax_definition_icms
+from . import tax_definition_operation_line
 from . import icms_regulation
 from . import document_type
 from . import document_serie
