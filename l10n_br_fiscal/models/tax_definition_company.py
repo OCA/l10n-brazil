59e965922df60557ce4001f65ffc006318a66c1f
Renato Lima <renato.lima@akretion.com>
2020-03-08 23:37:29 -0300

    [REF] tax definition model and constraints and added view

--- l10n_br_fiscal/models/tax_definition_company.py
+++ l10n_br_fiscal/models/tax_definition_company.py
@@ -1,18 +1,24 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class TaxDefinitionCompany(models.Model):
     _inherit = "l10n_br_fiscal.tax.definition"
 
-    company_id = fields.Many2one(
-        comodel_name="res.company",
-        string="Company")
+    @api.multi
+    @api.constrains("company_id")
+    def _check_company_id(self):
+        for record in self:
+            if record.company_id:
+                domain = [
+                    ("id", "!=", record.id),
+                    ('company_id', '=', record.company_id.id),
+                    ('tax_group_id', '=', record.tax_group_id.id)]
 
-    _sql_constraints = [(
-        "fiscal_tax_definition_company_group_uniq",
-        "unique (company_id, tax_group_id)",
-        _("Tax Definition already exists for this Company and Group !"),
-    )]
+                if record.env["l10n_br_fiscal.tax.definition"].search(domain):
+                    raise ValidationError(_(
+                        "Tax Definition already exists "
+                        "for this Company and Tax Group !"))
