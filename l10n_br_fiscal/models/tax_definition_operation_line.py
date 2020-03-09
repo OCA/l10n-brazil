d27ca8418b35506d60fad411308b1268e2797c10
Renato Lima <renato.lima@akretion.com>
2020-03-08 23:37:29 -0300

    [REF] tax definition model and constraints and added view

--- l10n_br_fiscal/models/tax_definition_operation_line.py
+++ l10n_br_fiscal/models/tax_definition_operation_line.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class TaxDefinitionOperationLine(models.Model):
@@ -11,8 +12,17 @@ class TaxDefinitionOperationLine(models.Model):
         comodel_name="l10n_br_fiscal.operation.line",
         string="Operation Line")
 
-    _sql_constraints = [(
-        "fiscal_tax_definition_operation_line_group_uniq",
-        "unique (operation_line_id, tax_group_id)",
-        _("Tax Definition already exists for this Operation Line and Group !"),
-    )]
+    @api.multi
+    @api.constrains("operation_line_id")
+    def _check_operation_line_id(self):
+        for record in self:
+            if record.operation_line_id:
+                domain = [
+                    ("id", "!=", record.id),
+                    ('operation_line_id', '=', record.operation_line_id.id),
+                    ('tax_group_id', '=', record.tax_group_id.id)]
+
+                if record.env["l10n_br_fiscal.tax.definition"].search(domain):
+                    raise ValidationError(_(
+                        "Tax Definition already exists "
+                        "for this Operation Line and Tax Group !"))
