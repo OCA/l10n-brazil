a61785a417210b7d17abd61eb71f6142cfbb2dde
Renato Lima <renato.lima@akretion.com>
2020-03-08 23:37:29 -0300

    [REF] tax definition model and constraints and added view

--- l10n_br_fiscal/models/tax_definition_icms.py
+++ l10n_br_fiscal/models/tax_definition_icms.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class TaxDefinitionICMS(models.Model):
@@ -20,3 +21,19 @@ class TaxDefinitionICMS(models.Model):
         comodel_name='res.country.state',
         string='To State',
         domain=[('country_id.code', '=', 'BR')])
+
+    @api.multi
+    @api.constrains("icms_regulation_id", "state_from_id")
+    def _check_icms(self):
+        for record in self:
+            if record.icms_regulation_id:
+                domain = [
+                    ("id", "!=", record.id),
+                    ('icms_regulation_id', '=', record.company_id.id),
+                    ('state_from_id', '=', record.state_from_id.id),
+                    ('tax_group_id', '=', record.tax_group_id.id)]
+
+                if record.env["l10n_br_fiscal.tax.definition"].search(domain):
+                    raise ValidationError(_(
+                        "Tax Definition already exists "
+                        "for this ICMS and Tax Group !"))
