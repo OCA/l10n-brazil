01a57c8e4f65c2e4305a62786326b269b73fe19c
Renato Lima <renato.lima@akretion.com>
2020-03-10 00:51:12 -0300

    [FIX] rebase regrassion

--- l10n_br_account/models/account_tax_abstract.py
+++ l10n_br_account/models/account_tax_abstract.py
@@ -9,42 +9,15 @@ class AccountTaxAbstract(models.AbstractModel):
     _name = "account.tax.fiscal.abstract"
     _description = "Account Tax Fiscal Abstract"
 
-    fiscal_tax_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax", string="Fiscal Tax"
-    )
-
-    fiscal_cst_in_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.cst",
-        related="fiscal_tax_id.cst_in_id",
-        string="CST Input",
-    )
-
-    fiscal_cst_out_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.cst",
-        related="fiscal_tax_id.cst_out_id",
-        string="CST Output",
-    )
-
-    fiscal_tax_domain = fields.Selection(
-        selection=TAX_DOMAIN, related="fiscal_tax_id.tax_domain", string="Tax Domain"
-    )
-
     @api.onchange("fiscal_tax_id")
-    def _onchange_fiscal_tax_id(self):
-        if self.fiscal_tax_id:
-            fiscal_tax = self.fiscal_tax_id
+    def _onchange_fiscal_tax_ids(self):
+        if self.fiscal_tax_ids:
+            fiscal_tax = self.fiscal_tax_ids[0]
             fiscal_type = {"sale": _("Out"), "purchase": _("In")}
-
-            if fiscal_tax.tax_base_type == 'percent':
-                type_amount = 'percent'
-                tax_amount = fiscal_tax.percent_amount
-            else:
-                type_amount = 'fixed'
-                tax_amount = fiscal_tax.value_amount
 
             self.amount_type = type_amount
             self.amount = tax_amount
-            self.description = fiscal_tax.name
+            self.description = fiscal_tax.tax_group_id.name
 
             colect_account_id = fiscal_tax.tax_group_id.colect_account_id.id
             recover_account_id = fiscal_tax.tax_group_id.recover_account_id.id
