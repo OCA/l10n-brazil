b84da872d06f6fc920a04ac88056191bfd0f1451
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-11 21:19:40 -0300

    [REF] Herança do modelo abstrato no cancelamento, inutilização e carta de correção

--- l10n_br_fiscal/models/document_cancel.py
+++ l10n_br_fiscal/models/document_cancel.py
@@ -3,61 +3,15 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
 
 
 class DocumentCancel(models.Model):
     _name = "l10n_br_fiscal.document.cancel"
+    _inherit = "l10n_br_fiscal.event.abstract"
     _description = "Fiscal Document Cancel Record"
-
-    document_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.document",
-        string="Documento",
-        index=True,
-    )
-
-    partner_id = fields.Many2one(
-        comodel_name="res.partner",
-        related="document_id.partner_id",
-        string="Partner",
-        index=True,
-    )
-
-    company_id = fields.Many2one(
-        comodel_name="res.partner",
-        related="document_id.partner_id",
-        string="Company",
-        index=True,
-    )
-
-    justificative = fields.Char(
-        string="Justificative",
-        size=255,
-        required=True)
 
     cancel_document_event_ids = fields.One2many(
         comodel_name="l10n_br_fiscal.document.event",
         inverse_name="cancel_document_event_id",
         string=u"Eventos",
     )
-
-    display_name = fields.Char(
-        string="Name",
-        compute="_compute_display_name")
-
-    @api.multi
-    @api.depends("document_id.number", "document_id.partner_id.name")
-    def _compute_display_name(self):
-        self.ensure_one()
-        names = ["Fatura", self.document_id.number,
-                 self.document_id.partner_id.name]
-        self.display_name = " / ".join(filter(None, names))
-
-    @api.multi
-    @api.constrains("justificative")
-    def _check_justificative(self):
-        for record in self:
-            if len(record.justificative) < 15:
-                raise ValidationError(_(
-                    "Justificativa deve ter tamanho mínimo de 15 caracteres.")
-                )
