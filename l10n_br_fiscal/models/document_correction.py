0d0416cb4c46a481b22fe4a70c5c9d7670fa303f
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-11 21:19:40 -0300

    [REF] Herança do modelo abstrato no cancelamento, inutilização e carta de correção

--- l10n_br_fiscal/models/document_correction.py
+++ l10n_br_fiscal/models/document_correction.py
@@ -7,50 +7,15 @@ from odoo import api, fields, models
 
 class DocumentCorrection(models.Model):
     _name = "l10n_br_fiscal.document.correction"
+    _inherit = "l10n_br_fiscal.event.abstract"
     _description = "Fiscal Document Correction Record"
 
-    document_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.document",
-        string="Documento",
-        index=True,
+    sequencia = fields.Char(
+        string=u"Sequência", help=u"Indica a sequência da carta de correcão"
     )
-
-    partner_id = fields.Many2one(
-        comodel_name="res.partner",
-        related="document_id.partner_id",
-        string="Partner",
-        index=True,
-    )
-
-    company_id = fields.Many2one(
-        comodel_name="res.partner",
-        related="document_id.partner_id",
-        string="Company",
-        index=True,
-    )
-
-    justificative = fields.Char(
-        string="Justificativa", size=255, readonly=True, required=True
-    )
-
-    sequence = fields.Char(
-        string="Sequence",
-        help="Indica a sequência da carta de correcão")
 
     cce_document_event_ids = fields.One2many(
         comodel_name="l10n_br_fiscal.document.event",
         inverse_name="correction_document_event_id",
         string=u"Eventos",
     )
-
-    display_name = fields.Char(
-        string="Name",
-        compute="_compute_display_name")
-
-    @api.multi
-    @api.depends("document_id.number", "document_id.partner_id")
-    def _compute_display_name(self):
-        self.ensure_one()
-        names = ["Fatura", self.document_id.number,
-                 self.document_id.partner_id.name]
-        self.display_name = " / ".join(filter(None, names))
