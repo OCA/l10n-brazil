ac8badc3dc9b440de2194484f2bf395082089227
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-11 21:36:30 -0300

    [ADD] Menu para visualização dos eventos

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -88,6 +88,10 @@
         "views/uom_uom.xml",
         "views/operation_dashboard_view.xml",
         "views/closing.xml",
+        "views/document_event_view.xml",
+        "views/document_invalidate_number.xml",
+        "views/document_cancel.xml",
+        "views/document_correction.xml",
     ],
     "demo": [
         # Some demo data is being loaded via post_init_hook in hook file
