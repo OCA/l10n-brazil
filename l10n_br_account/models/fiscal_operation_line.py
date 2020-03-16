4e35578abe5d7a586d8776c703cf71dbff40553c
Luis Felipe Mileo <mileo@kmee.com.br>
2020-03-15 22:02:57 -0300

    [NEW] Template at fiscal_opereration_line
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account/models/fiscal_operation_line.py
+++ l10n_br_account/models/fiscal_operation_line.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # Copyright (C) 2019 - TODAY Raphaël Valyi - Akretion
+# Copyright (C) 2020 - TODAY Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields, models
@@ -13,3 +14,10 @@ class OperationLine(models.Model):
         string='Fiscal Position',
         readonly=True,
         states={'draft': [('readonly', False)]})
+
+    move_template_id = fields.Many2one(
+        comodel_name='l10n_br_account.move.template',
+        string='Move Template',
+        readonly=True,
+        states={'draft': [('readonly', False)]}
+    )
