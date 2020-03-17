f1f251c846e75e64912cdda71482170800c80247
Luis Felipe Mileo <mileo@kmee.com.br>
2020-03-17 16:21:07 -0300

    [NEW] Contabilização de Operações de Vendas
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account/__manifest__.py
+++ l10n_br_account/__manifest__.py
@@ -23,6 +23,8 @@
         # data
         "data/account_tax_group.xml",
         "data/account_tax_template.xml",
+        "data/l10n_br_account_move_history_data.xml",
+        "data/l10n_br_account_move_template_data.xml",
 
         "views/l10n_br_account_menu.xml",
         # Views
--- l10n_br_account/models/l10n_br_account_move_history.py
+++ l10n_br_account/models/l10n_br_account_move_history.py
@@ -11,4 +11,9 @@ class MoveHistory(models.Model):
     _description = 'Histórico Contábil'
 
     code = fields.Char(required=False)
+    history = fields.Text(
+        string="History",
+        required=True
+    )
 
+    # TODO: Refactoring fiscal.comment to use the same engine, move it to l10n_br_base
