de5422eba488b605af54e014f5bfac85f40edeea
Magno Costa <magno.costa@akretion.com.br>
2020-03-17 12:39:18 -0300

    [12.0][MIG][WIP] Integrate models from l10n_br_sale_product to l10n_br_sale.

--- l10n_br_sale/models/res_company.py
+++ l10n_br_sale/models/res_company.py
@@ -14,3 +14,23 @@ class Company(models.Model):
     copy_note = fields.Boolean(
         string="Copy Sale note on invoice",
         default=False)
+
+    default_ind_pres = fields.Selection(
+        [
+            (
+                "0",
+                u"Não se aplica (por exemplo,"
+                u" Nota Fiscal complementar ou de ajuste)",
+            ),
+            ("1", u"Operação presencial"),
+            ("2", u"Operação não presencial, pela Internet"),
+            ("3", u"Operação não presencial, Teleatendimento"),
+            ("4", u"NFC-e em operação com entrega em domicílio"),
+            ("5", u"Operação presencial, fora do estabelecimento"),
+            ("9", u"Operação não presencial, outros"),
+        ],
+        u"Tipo de operação",
+        help=u"Indicador de presença do comprador no \
+            \nestabelecimento comercial no momento \
+            \nda operação.",
+    )
