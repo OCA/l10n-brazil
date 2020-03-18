ac1237ffd805b0fd91df0df992ef4ddd59caa39a
Renato Lima <renato.lima@akretion.com>
2020-03-17 23:10:34 -0300

    [ADD] ICMS ST and ICMS Difal fields in Fiscal Document Line

--- l10n_br_fiscal/models/tax_ipi_control_seal.py
+++ l10n_br_fiscal/models/tax_ipi_control_seal.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2020  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import models
 
 
 class TaxIpiControlSeal(models.Model):
