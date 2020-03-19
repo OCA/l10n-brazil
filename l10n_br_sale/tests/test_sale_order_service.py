90552d54d1fca3e9640c4bc37d83482988e757af
Magno Costa <magno.costa@akretion.com.br>
2020-03-19 13:33:23 -0300

    [12.0][MIG][WIP] Integrated tests form l10n_br_sale_service to l10n_br_sale.

--- l10n_br_sale/tests/test_sale_order_service.py
+++ l10n_br_sale/tests/test_sale_order_service.py
@@ -5,71 +5,41 @@ from openerp.tests import common
 
 
 class TestSaleOrderService(common.TransactionCase):
+
     def setUp(self):
         super(TestSaleOrderService, self).setUp()
-        cr, uid = self.cr, self.uid
-        # Usefull models
-        self.ir_model_data = self.registry("ir.model.data")
-        self.sale_order_line = self.registry("sale.order.line")
-        self.sale_order = self.registry("sale.order")
-        self.account_invoice = self.registry("account.invoice")
-        self.product_id = self.ir_model_data.get_object_reference(
-            cr, uid, "product", "product_product_8"
-        )[1]
-        self.product_service_id = self.ir_model_data.get_object_reference(
-            cr, uid, "product", "product_product_consultant"
-        )[1]
-        # partner Akretion
-        self.partner_id = self.ir_model_data.get_object_reference(
-            cr, uid, "l10n_br_base", "res_partner_akretion"
-        )[1]
+        self.sale_order_service = self.env.ref(
+            "l10n_br_sale.sale_order_service")
 
     def test_sale_order_service(self):
-        cr, uid, context = self.cr, self.uid, {}
-        # Create sale order with two sale order line
+        """ Test Sale Order with service and product
+            to check if create two invoices. """
 
-        so_id = self.sale_order.create(
-            cr, uid, vals={"partner_id": self.partner_id}, context=context
-        )
+        self.sale_order_service.onchange_partner_id()
+        self.sale_order_service.onchange_partner_shipping_id()
 
-        obj_sale_order = self.sale_order.browse(cr, uid, [so_id], context=context)
+        for line in self.sale_order_service.order_line:
+            line._onchange_product_id()
+            line._onchange_operation_id()
+            line._onchange_operation_line_id()
+            line._onchange_fiscal_taxes()
+            self.assertTrue(
+                line.operation_id,
+                "Error to mapping Operation on Sale Order Line.",
+            )
+            self.assertTrue(
+                line.operation_line_id,
+                "Error to mapping Operation Line on Sale Order Line.",
+            )
 
-        # O onchange abaixo é necessário para preencher a Posição Fiscal
-        obj_sale_order.onchange_fiscal()
-
-        self.sale_order_line.create(
-            cr,
-            uid,
-            values={
-                "order_id": so_id,
-                "product_id": self.product_id,
-                "product_uom_qty": 1,
-                "fiscal_category_id": obj_sale_order.fiscal_category_id.id,
-            },
-            context=context,
-        )
-        self.sale_order_line.create(
-            cr,
-            uid,
-            values={
-                "order_id": so_id,
-                "product_id": self.product_service_id,
-                "product_uom_qty": 1,
-                "fiscal_category_id": obj_sale_order.fiscal_category_id.id,
-            },
-            context=context,
-        )
         # Confirm sale order
-        self.sale_order.action_button_confirm(cr, uid, [so_id], context=context)
-        # Create Invoice
-        self.sale_order.manual_invoice(cr, uid, [so_id], context=context)
+        self.sale_order_service.action_confirm()
 
-        for line in obj_sale_order.order_line:
-            # O onchange abaixo é necessário para preencher as Posições Fiscais
-            line.onchange_fiscal()
+        # Create Invoice
+        self.sale_order_service.action_invoice_create(final=True)
 
         # Verify if the two Invoices were created
         count_invoice = 0
-        for order in obj_sale_order.invoice_ids:
+        for order in self.sale_order_service.invoice_ids:
             count_invoice += 1
         self.assertTrue(count_invoice == 2)
