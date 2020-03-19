21715f8fbdfd09d81c28bb7177636298856e9826
Renato Lima <renato.lima@akretion.com>
2020-03-19 17:00:44 -0300

    Cleanup l10n_br_account

--- l10n_br_account/models/account_tax_abstract.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import models
-
-
-class AccountTaxAbstract(models.AbstractModel):
-    _name = "account.tax.fiscal.abstract"
-    _description = "Account Tax Fiscal Abstract"
