c58cdf236114f75c51890064e812c229e74133b5
Renato Lima <renato.lima@akretion.com>
2020-03-19 17:03:54 -0300

    cleanup l10n_br_account

--- l10n_br_account/models/fiscal_document_line.py
+++ l10n_br_account/models/fiscal_document_line.py
@@ -10,20 +10,38 @@ class FiscalDocumentLine(models.Model):
 
     # the following fields collide with account.invoice.line fields so we use
     # related field alias to be able to write them through account.invoice.line
-    fiscal_doc_line_name = fields.Text(related="name", readonly=False)
+    fiscal_doc_line_name = fields.Text(
+        related="name",
+        readonly=False)
 
-    fiscal_doc_line_partner_id = fields.Many2one(related="partner_id", readonly=False)
+    fiscal_doc_line_partner_id = fields.Many2one(
+        related="partner_id",
+        readonly=False)
 
-    fiscal_doc_line_company_id = fields.Many2one(related="company_id", readonly=False)
+    fiscal_doc_line_company_id = fields.Many2one(
+        related="company_id",
+        readonly=False)
 
-    fiscal_doc_line_currency_id = fields.Many2one(related="currency_id", readonly=False)
+    fiscal_doc_line_currency_id = fields.Many2one(
+        related="currency_id",
+        readonly=False)
 
-    fiscal_doc_line_product_id = fields.Many2one(related="product_id", readonly=False)
+    fiscal_doc_line_product_id = fields.Many2one(
+        related="product_id",
+        readonly=False)
 
-    fiscal_doc_line_uom_id = fields.Many2one(related="uom_id", readonly=False)
+    fiscal_doc_line_uom_id = fields.Many2one(
+        related="uom_id",
+        readonly=False)
 
-    fiscal_doc_line_quantity = fields.Float(related="quantity", readonly=False)
+    fiscal_doc_line_quantity = fields.Float(
+        related="quantity",
+        readonly=False)
 
-    fiscal_doc_line_price_unit = fields.Float(related="price_unit", readonly=False)
+    fiscal_doc_line_price_unit = fields.Float(
+        related="price_unit",
+        readonly=False)
 
-    fiscal_doc_line_discount = fields.Monetary(related="discount_value", readonly=False)
+    fiscal_doc_line_discount = fields.Monetary(
+        related="discount_value",
+        readonly=False)
