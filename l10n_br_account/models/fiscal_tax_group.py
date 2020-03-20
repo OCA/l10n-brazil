0900bb3bc81fcbacae488327a3793dedc18be755
Renato Lima <renato.lima@akretion.com>
2020-03-19 23:53:34 -0300

    [FIX] cleanup l10n_br_account and l10n_br_fiscal

--- l10n_br_account/models/fiscal_tax_group.py
+++ l10n_br_account/models/fiscal_tax_group.py
@@ -1,11 +1,11 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class FiscalTaxGroup(models.Model):
-    _inherit = "l10n_br_fiscal.tax.group"
+    _inherit = 'l10n_br_fiscal.tax.group'
 
     colect_account_id = fields.Many2one(
         comodel_name='account.account',
