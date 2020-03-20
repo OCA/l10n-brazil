5055104b61a17658bb1c1b13495dd236c564154a
Renato Lima <renato.lima@akretion.com>
2020-03-19 23:53:34 -0300

    [FIX] cleanup l10n_br_account and l10n_br_fiscal

--- l10n_br_account/models/fiscal_document.py
+++ l10n_br_account/models/fiscal_document.py
@@ -12,20 +12,25 @@ class FiscalDocument(models.Model):
     # related field alias to be able to write them through account.invoice
     fiscal_doc_partner_id = fields.Many2one(
         related="partner_id",
+        string="Fiscal Doc Partner",
         readonly=False)
 
     fiscal_doc_date = fields.Datetime(
         related="date",
+        string="Fiscal Doc Date",
         readonly=False)
 
     fiscal_doc_company_id = fields.Many2one(
         related="company_id",
+        string="Fiscal Doc Company",
         readonly=False)
 
     fiscal_doc_currency_id = fields.Many2one(
         related="currency_id",
+        string="Fiscal Doc Currency",
         readonly=False)
 
     fiscal_doc_state = fields.Selection(
         related="state",
+        string="Fiscal Doc State",
         readonly=False)
