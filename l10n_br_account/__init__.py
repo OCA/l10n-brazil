37ce1add02e8492618b9c474a85fee5e3377eec3
Renato Lima <renato.lima@akretion.com>
2020-03-20 11:28:48 -0300

    [REF] Rename l10n_br_account's tests

--- l10n_br_account/__init__.py
+++ l10n_br_account/__init__.py
@@ -5,4 +5,3 @@ from .hooks import post_init_hook
 
 from . import models
 from . import wizards
-from . import tests
--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -1,5 +1,4 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-# from . import test_account_customer_invoice
-# from . import test_account_supplier_invoice
-# from . import test_generate_fiscal_rules
+from . import test_customer_invoice_dummy
+from . import test_supplier_invoice_dummy
