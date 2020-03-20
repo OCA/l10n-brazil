b5ea76f322677d7d2eaa08fc75edff89bd458ede
Renato Lima <renato.lima@akretion.com>
2020-03-19 23:53:34 -0300

    [FIX] cleanup l10n_br_account and l10n_br_fiscal

--- l10n_br_fiscal/models/document_cancel.py
+++ l10n_br_fiscal/models/document_cancel.py
@@ -2,47 +2,40 @@
 # Copyright (C) 2014  KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class DocumentCancel(models.Model):
     _name = "l10n_br_fiscal.document.cancel"
-    _description = "Documento Eletrônico no Sefaz"
-
-    partner_id = fields.Many2one(
-        comodel_name="res.partner", related="invoice_id.partner_id", string="Cliente"
-    )
+    _description = "Fiscal Document Cancel Record"
 
     justificative = fields.Char(
-        string="Justificativa", size=255, readonly=True, required=True
-    )
+        string="Justificative",
+        size=255,
+        required=True)
 
-    cancel_document_event_ids = fields.One2many(
-        comodel_name="l10n_br_fiscal.document_event",
-        inverse_name="cancel_document_event_id",
-        string=u"Eventos",
-    )
+    document_event_ids = fields.One2many(
+        comodel_name="l10n_br_fiscal.document.event",
+        inverse_name="document_cancel_id",
+        string="Events")
 
-    display_name = fields.Char(string=u"Nome", compute="_compute_display_name")
+    display_name = fields.Char(
+        string="Name",
+        compute="_compute_display_name")
 
     @api.multi
-    @api.depends("invoice_id.number", "invoice_id.partner_id.name")
+    @api.depends("document_event_ids")
     def _compute_display_name(self):
-        self.ensure_one()
-        names = ["Fatura", self.invoice_id.number, self.invoice_id.partner_id.name]
-        self.display_name = " / ".join(filter(None, names))
+        for record in self:
+            if record.document_event_ids:
+                # TODO
+                record.display_name = record.document_event_ids[0].origin
 
     @api.multi
+    @api.constrains("justificative")
     def _check_justificative(self):
-        for invalid in self:
-            if len(invalid.justificative) < 15:
-                return False
-        return True
-
-    _constraints = [
-        (
-            _check_justificative,
-            u"Justificativa deve ter tamanho mínimo de 15 caracteres.",
-            ["justificative"],
-        )
-    ]
+        for record in self:
+            if len(record.justificative) < 15:
+                raise ValidationError(_(
+                    "Justificativa deve ter tamanho mínimo de 15 caracteres."))
