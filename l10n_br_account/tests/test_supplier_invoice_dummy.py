e65ef1b74bda72042c6500b86da0ecc95c18fa59
Renato Lima <renato.lima@akretion.com>
2020-03-21 01:48:15 -0300

    [REF] l10n_br_account tests

--- l10n_br_account/tests/test_supplier_invoice_dummy.py
+++ l10n_br_account/tests/test_supplier_invoice_dummy.py
@@ -13,8 +13,8 @@ class TestSupplierInvoice(TransactionCase):
                 code="X1020",
                 name="Product Purchase - (test)",
                 user_type_id=self.env.ref("account.data_account_type_revenue").id,
-            )
-        )
+            ))
+
         self.purchase_journal = self.env["account.journal"].create(
             dict(
                 name="Purchase Journal - (test)",
@@ -23,46 +23,13 @@ class TestSupplierInvoice(TransactionCase):
                 refund_sequence=True,
                 default_debit_account_id=self.purchase_account.id,
                 default_credit_account_id=self.purchase_account.id,
-                revenue_expense=True,
-            )
-        )
-        self.fiscal_category = self.env["l10n_br_account.fiscal.category"].create(
-            dict(
-                code="Compras Teste",
-                name="Compras Teste",
-                type="input",
-                journal_type="purchase",
-                property_journal=self.purchase_journal.id,
-            )
-        )
-        self.fiscal_position = self.env["account.fiscal.position"].create(
-            dict(
-                name="Compras Teste",
-                type="input",
-                company_id=self.env.ref("base.main_company").id,
-                fiscal_category_id=self.fiscal_category.id,
-            )
-        )
-        self.fiscal_position_rule = self.env["account.fiscal.position.rule"].create(
-            dict(
-                name="Compras",
-                description="Compras",
-                company_id=self.env.ref("base.main_company").id,
-                from_country=self.env.ref("base.br").id,
-                fiscal_category_id=self.fiscal_category.id,
-                fiscal_position_id=self.fiscal_position.id,
-                use_purchase=True,
-                use_invoice=True,
-                use_picking=True,
             )
         )
         self.invoice_1 = self.env["account.invoice"].create(
             dict(
                 name="Test Supplier Invoice",
                 payment_term_id=self.env.ref("account.account_payment_term_advance").id,
-                fiscal_category_id=self.fiscal_category.id,
                 partner_id=self.env.ref("base.res_partner_3").id,
-                reference_type="none",
                 journal_id=self.purchase_journal.id,
                 invoice_line_ids=[
                     (
@@ -87,8 +54,7 @@ class TestSupplierInvoice(TransactionCase):
                             )
                             .id,
                             "name": "product test 5",
-                            "uom_id": self.env.ref("product.product_uom_unit").id,
-                            "fiscal_category_id": self.fiscal_category.id,
+                            "uom_id": self.env.ref("uom.product_uom_unit").id,
                         },
                     )
                 ],
