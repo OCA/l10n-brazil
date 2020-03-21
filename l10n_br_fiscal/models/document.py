0cab58845689b12a4dc49991db70213f7610add0
Renato Lima <renato.lima@akretion.com>
2020-03-21 03:03:49 -0300

    [REF] refactoring new features and cleanup

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -105,8 +105,8 @@ class Document(models.Model):
         readonly=True)
 
     close_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.close",
-        string="Close ID",)
+        comodel_name='l10n_br_fiscal.closing',
+        string='Close ID')
 
     document_type = fields.Char(
         related='document_type_id.code',
