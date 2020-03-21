e4f2d3192b18602024e21ce279e70faeba74024d
Renato Lima <renato.lima@akretion.com>
2020-03-21 03:03:49 -0300

    [REF] refactoring new features and cleanup

--- l10n_br_account/tests/test_customer_invoice_dummy.py
+++ l10n_br_account/tests/test_customer_invoice_dummy.py
@@ -2,7 +2,6 @@
 #   Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
 
