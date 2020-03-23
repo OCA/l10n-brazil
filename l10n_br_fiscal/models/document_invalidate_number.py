84abe2fa5c95302862697b5dceb97177c6fc2fed
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-23 15:30:54 -0300

    [ADD] Envio de Inutilização no módulo l10n_br_nfe

--- l10n_br_fiscal/models/document_invalidate_number.py
+++ l10n_br_fiscal/models/document_invalidate_number.py
@@ -6,10 +6,6 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 from odoo.addons.l10n_br_fiscal.constants.fiscal import \
     SITUACAO_EDOC_INUTILIZADA
-from erpbrasil.assinatura import certificado as cert
-from erpbrasil.edoc import NFe as edoc_nfe
-from erpbrasil.transmissao import TransmissaoSOAP
-from requests import Session
 
 
 class DocumentInvalidateNumber(models.Model):
@@ -101,22 +97,6 @@ class DocumentInvalidateNumber(models.Model):
                     u'Você não pode excluir uma sequência concluída.'))
         return super(InvalidateNumber, self).unlink()
 
-    def _processador(self):
-        if not self.company_id.certificate_nfe_id:
-            raise UserError(_("Certificado não encontrado"))
-
-        certificado = cert.Certificado(
-            arquivo=self.company_id.certificate_nfe_id.file,
-            senha=self.company_id.certificate_nfe_id.password,
-        )
-        session = Session()
-        session.verify = False
-        transmissao = TransmissaoSOAP(certificado, session)
-        return edoc_nfe(
-            transmissao, self.company_id.state_id.ibge_code,
-            versao='4.00', ambiente='2'
-        )
-
     @api.multi
     def action_invalidate(self):
         for record in self:
@@ -138,43 +118,20 @@ class DocumentInvalidateNumber(models.Model):
     @api.multi
     def invalidate(self, event_id):
         for record in self:
-            processador = record._processador()
-
-            evento = processador.inutilizacao(
-                cnpj=record.company_id.cnpj_cpf,
-                mod=record.document_serie_id.document_type_id.code,
-                serie=record.document_serie_id.code,
-                num_ini=record.number_start,
-                num_fin=record.number_end,
-                justificativa=record.justificative
-            )
-
-            processo = processador.envia_inutilizacao(
-                evento=evento
-            )
-
-            event_id.write({
-                'file_sent': processo.envio_xml,
-                'file_returned': processo.retorno.content,
-                'status': processo.resposta.infInut.cStat,
-                'message': processo.resposta.infInut.xMotivo,
-            })
-
-            if processo.resposta.infInut.cStat == '102':
-                event_id.state = 'done'
-                record.state = 'done'
-                if record.document_id:
-                    record.document_id.state_edoc = SITUACAO_EDOC_INUTILIZADA
-                else:
-                    for number in range(record.number_start,
-                                        record.number_end + 1):
-                        record.env['l10n_br_fiscal.document'].create({
-                            'document_serie_id': record.document_serie_id.id,
-                            'document_type_id':
-                                record.document_serie_id.document_type_id.id,
-                            'company_id': record.company_id.id,
-                            'state_edoc': SITUACAO_EDOC_INUTILIZADA,
-                            'issuer': 'company',
-                            'number': str(number),
-                        })
+            event_id.state = 'done'
+            record.state = 'done'
+            if record.document_id:
+                record.document_id.state_edoc = SITUACAO_EDOC_INUTILIZADA
+            else:
+                for number in range(record.number_start,
+                                    record.number_end + 1):
+                    record.env['l10n_br_fiscal.document'].create({
+                        'document_serie_id': record.document_serie_id.id,
+                        'document_type_id':
+                            record.document_serie_id.document_type_id.id,
+                        'company_id': record.company_id.id,
+                        'state_edoc': SITUACAO_EDOC_INUTILIZADA,
+                        'issuer': 'company',
+                        'number': str(number),
+                    })
 
