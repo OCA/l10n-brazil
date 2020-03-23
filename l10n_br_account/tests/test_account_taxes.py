cb61525b1995c0ec4872632bdba1c526f5617118
Renato Lima <renato.lima@akretion.com>
2020-03-23 02:28:58 -0300

    [FIX] generate_tax

--- l10n_br_account/tests/test_account_taxes.py
+++ l10n_br_account/tests/test_account_taxes.py
@@ -13,14 +13,9 @@ class TestAccountTaxes(TransactionCase):
         self.l10n_br_company = self.env['res.company'].create({
             'name': 'Empresa Teste do Plano de Contas Simplificado'})
 
-    def test_load_coa(self):
-        """Test to install the chart of accounts template in a new company"""
         self.env.user.company_ids += self.l10n_br_company
         self.env.user.company_id = self.l10n_br_company
         self.l10n_br_simple_coa.try_loading_for_current_company()
-
-        self.assertEquals(
-            self.l10n_br_simple_coa, self.l10n_br_company.chart_template_id)
 
     def test_account_taxes(self):
         """Test if account taxes are related with fiscal taxes"""
@@ -29,7 +24,7 @@ class TestAccountTaxes(TransactionCase):
 
         is_fiscal_taxes = False
         for tax in account_taxes:
-            if tax.tax_group_id.fiscal_group_id and tax.fiscal_tax_ids:
+            if tax.tax_group_id.fiscal_tax_group_id and tax.fiscal_tax_ids:
                 is_fiscal_taxes = True
 
         assert is_fiscal_taxes, "There are not fiscal taxes related"
