a51a7f17ff93900b5d9a85bed5bac9e0e233dbc7
Renato Lima <renato.lima@akretion.com>
2020-03-23 21:40:33 -0300

    [REF] added domain_field_codes in pis_cofins object

--- l10n_br_fiscal/models/tax_pis_cofins.py
+++ l10n_br_fiscal/models/tax_pis_cofins.py
@@ -1,16 +1,30 @@
 # Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
+
+from ..constants.fiscal import (
+    TAX_DOMAIN_COFINS,
+    TAX_DOMAIN_COFINS_ST,
+    TAX_DOMAIN_PIS,
+    TAX_DOMAIN_PIS_ST
+)
+
+from ..tools import misc
 
 
 class TaxPisCofins(models.Model):
-    _name = "l10n_br_fiscal.tax.pis.cofins"
-    _description = "Tax PIS/COFINS"
+    _name = 'l10n_br_fiscal.tax.pis.cofins'
+    _description = 'Tax PIS/COFINS'
 
-    code = fields.Char(string="Code", required=True)
+    code = fields.Char(
+        string='Code',
+        required=True)
 
-    name = fields.Text(string="Name", required=True, index=True)
+    name = fields.Text(
+        string='Name',
+        required=True,
+        index=True)
 
     piscofins_type = fields.Selection(
         selection=[
@@ -21,81 +35,69 @@ class TaxPisCofins(models.Model):
         default="ncm",
         string="Type",
         required=True,
-        index=True,
-    )
+        index=True)
 
     tax_pis_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax",
-        string="Tax PIS",
-        domain="[('tax_domain', '=', 'pis')]",
-    )
+        comodel_name='l10n_br_fiscal.tax',
+        string='Tax PIS',
+        domain=[('tax_domain', '=', TAX_DOMAIN_PIS)])
 
     tax_pis_st_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax",
-        string="Tax PIS ST",
-        domain="[('tax_domain', '=', 'pisst')]",
-    )
+        comodel_name='l10n_br_fiscal.tax',
+        string='Tax PIS ST',
+        domain=[('tax_domain', '=', TAX_DOMAIN_PIS_ST)])
 
     tax_cofins_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax",
-        string="Tax COFINS",
-        domain="[('tax_domain', '=', 'cofins')]",
-    )
+        comodel_name='l10n_br_fiscal.tax',
+        string='Tax COFINS',
+        domain=[('tax_domain', '=', TAX_DOMAIN_COFINS)])
 
     tax_cofins_st_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax",
-        string="Tax COFINS ST",
-        domain="[('tax_domain', '=', 'cofinsst')]",
-    )
+        comodel_name='l10n_br_fiscal.tax',
+        string='Tax COFINS ST',
+        domain=[('tax_domain', '=', TAX_DOMAIN_COFINS_ST)])
 
-    ncms = fields.Char(string="NCM")
+    ncms = fields.Char(
+        string='NCM')
 
-    ncm_exception = fields.Char(string="NCM Exeption")
+    ncm_exception = fields.Char(
+        string='NCM Exeption')
 
-    not_in_ncms = fields.Char(string="Not in NCM")
+    not_in_ncms = fields.Char(
+        string='Not in NCM')
 
     ncm_ids = fields.Many2many(
-        comodel_name="l10n_br_fiscal.ncm",
-        relation="fiscal_pis_cofins_ncm_rel",
-        colunm1="piscofins_id",
-        colunm2="ncm_id",
-        compute="_compute_ncms",
+        comodel_name='l10n_br_fiscal.ncm',
+        relation='fiscal_pis_cofins_ncm_rel',
+        colunm1='piscofins_id',
+        colunm2='ncm_id',
+        compute='_compute_ncms',
         store=True,
         readonly=True,
-        string="NCMs",
-    )
+        string='NCMs')
 
-    @api.depends("ncms")
+    @api.multi
+    @api.depends('ncms')
     def _compute_ncms(self):
-        ncm = self.env["l10n_br_fiscal.ncm"]
-        domain = False
+        ncm = self.env['l10n_br_fiscal.ncm']
         for r in self:
-            # Clear Field
-            domain = False
+            domain = []
+
+            # Clear Field to recompute
             r.ncm_ids = False
             if r.ncms:
-                ncms = r.ncms.split(";")
-                domain = ["|"] * (len(ncms) - 1)
-                domain += [("code_unmasked", "=", n) for n in ncms if len(n) == 8]
-                domain += [
-                    ("code_unmasked", "=ilike", n + "%") for n in ncms if len(n) < 8
-                ]
+                domain = misc.domain_field_codes(r.ncms)
 
             if r.not_in_ncms:
-                not_in_ncms = r.not_in_ncms.split(";")
-                domain += [
-                    ("code_unmasked", "=", n) for n in not_in_ncms if len(n) == 8
-                ]
-
-                domain += [
-                    ("code_unmasked", "not ilike", n + "%")
-                    for n in not_in_ncms
-                    if len(n) < 8
-                ]
+                domain += misc.domain_field_codes(
+                    field_codes=r.not_in_ncms,
+                    operator1="!=", operator2="not ilike")
 
             if r.ncm_exception:
-                ncm_exception = r.ncm_exception.split(";")
-                domain += [("exception", "=", n) for n in ncm_exception]
+                domain += misc.domain_field_codes(
+                    field_codes=r.ncm_exception,
+                    field_name="exception",
+                    code_size=2)
 
             if domain:
                 r.ncm_ids = ncm.search(domain)
