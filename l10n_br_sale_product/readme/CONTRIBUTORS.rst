63d4d5939537f9ea99cfb642ca8d2420f5b10943
Magno Costa <magno.costa@akretion.com.br>
2020-03-25 15:11:06 -0300

    [12.0][MIG][REF] Remove modules l10n_br_sale_product and l10n_br_sale_service; methods, views and tests was moved to l10n_br_sale.

--- l10n_br_sale_product/readme/CONTRIBUTORS.rst
+++
@@ -1,3 +0,0 @@
-* Renato Lima <renato.lima@akretion.com.br>
-* Raphaël Valyi <raphael.valyi@akretion.com.br>
-* Luis Felipe Miléo <mileo@kmee.com.br>
--- l10n_br_sale_service/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (C) 2016  Magno Costa - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-# from . import test_sale_order_service
