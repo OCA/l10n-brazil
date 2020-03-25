d72acde1a1fd9ec127f84a557c43e7252211ffde
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-25 15:37:48 -0300

    [IMP] Separate each class in its own file

--- l10n_br_nfe/models/res_city.py
+++ l10n_br_nfe/models/res_city.py
@@ -1,11 +1,10 @@
-# Copyright 2019 Akretion (Raphaël Valyi <raphael.valyi@akretion.com>)
+# Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import api, models
 
 
 class ResCity(models.Model):
-    _inherit = 'res.city'
+    _inherit = "res.city"
     _nfe_search_keys = ['ibge_code']
 
     # TODO understand why this is still required
