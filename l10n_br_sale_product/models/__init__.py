0c82a631c9171f394c60476437b160021ca03dd1
Magno Costa <magno.costa@akretion.com.br>
2020-03-25 15:11:06 -0300

    [12.0][MIG][REF] Remove modules l10n_br_sale_product and l10n_br_sale_service; methods, views and tests was moved to l10n_br_sale.

--- l10n_br_sale_product/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2015  Renato Lima - Akretion                                  #
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import res_company
-from . import sale
-from . import sale_line
