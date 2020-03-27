bd6fdc99f1725e2639a0bca82d0309826dc2b794
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-27 17:56:38 -0300

    [FIX] CFOP

--- l10n_br_nfe/models/__init__.py
+++ l10n_br_nfe/models/__init__.py
@@ -7,6 +7,3 @@ from . import res_country_state
 from . import res_company
 from . import res_config_settings
 from . import cfop
-from . import document_cancel
-from . import document_correction
-from . import document_invalidate_number
