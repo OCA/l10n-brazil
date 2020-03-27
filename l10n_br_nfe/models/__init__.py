98f70807470302a3691efbf3c311e6c78237ddb2
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-27 16:21:40 -0300

    [REM] Stacked from res_partner and res_company

--- l10n_br_nfe/models/__init__.py
+++ l10n_br_nfe/models/__init__.py
@@ -1,9 +1,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import res_partner
-from . import res_company
 from . import document
 from . import document_line
 from . import res_city
 from . import res_country_state
+from . import res_company
 from . import res_config_settings
