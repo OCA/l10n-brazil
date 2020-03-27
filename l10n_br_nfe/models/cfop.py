b91f6bd018c27c0a455cfc20ce1942c2f47f8b17
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-03-27 17:56:38 -0300

    [FIX] CFOP

---
+++ l10n_br_nfe/models/cfop.py
@@ -0,0 +1,8 @@
+# Copyright 2020 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class Cfop(models.Model):
+    _inherit = "l10n_br_fiscal.cfop"
+    _nfe_search_keys = ['code']
