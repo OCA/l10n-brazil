8ea8e3660b2d3e652ac1557468fc00ef2abed12f
Renato Lima <renato.lima@akretion.com>
2020-03-30 14:09:23 -0300

    [REF] tax definition to create lines with ncm and cest filters

--- l10n_br_fiscal/models/tax_definition_icms.py
+++ l10n_br_fiscal/models/tax_definition_icms.py
@@ -6,11 +6,11 @@ from odoo.exceptions import ValidationError
 
 
 class TaxDefinitionICMS(models.Model):
-    _inherit = "l10n_br_fiscal.tax.definition"
+    _inherit = 'l10n_br_fiscal.tax.definition'
 
     icms_regulation_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.icms.regulation",
-        string="ICMS Regulation")
+        comodel_name='l10n_br_fiscal.icms.regulation',
+        string='ICMS Regulation')
 
     state_from_id = fields.Many2one(
         comodel_name='res.country.state',
@@ -23,17 +23,19 @@ class TaxDefinitionICMS(models.Model):
         domain=[('country_id.code', '=', 'BR')])
 
     @api.multi
-    @api.constrains("icms_regulation_id", "state_from_id")
+    @api.constrains('icms_regulation_id', 'state_from_id')
     def _check_icms(self):
         for record in self:
             if record.icms_regulation_id:
                 domain = [
-                    ("id", "!=", record.id),
-                    ('icms_regulation_id', '=', record.company_id.id),
+                    ('id', '!=', record.id),
+                    ('icms_regulation_id', '=', record.icms_regulation_id.id),
                     ('state_from_id', '=', record.state_from_id.id),
-                    ('tax_group_id', '=', record.tax_group_id.id)]
+                    ('state_to_id', '=', record.state_to_id.id),
+                    ('tax_group_id', '=', record.tax_group_id.id),
+                    ('tax_id', '=', record.tax_id.id)]
 
-                if record.env["l10n_br_fiscal.tax.definition"].search_count(
+                if record.env['l10n_br_fiscal.tax.definition'].search_count(
                         domain):
                     raise ValidationError(_(
                         "Tax Definition already exists "
