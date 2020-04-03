e13f27fd4126001617f3abb099c4119347ce52e6
Renato Lima <renato.lima@akretion.com>
2020-04-02 23:49:36 -0300

    pep8

--- l10n_br_fiscal/models/product_template.py
+++ l10n_br_fiscal/models/product_template.py
@@ -13,99 +13,99 @@ from ..constants.icms import ICMS_ORIGIN, ICMS_ORIGIN_DEFAULT
 
 
 class ProductTemplate(models.Model):
-    _inherit = "product.template"
+    _inherit = 'product.template'
 
     def _get_default_ncm_id(self):
-        fiscal_type = self.env.context.get("default_fiscal_type")
+        fiscal_type = self.env.context.get('default_fiscal_type')
         if fiscal_type == PRODUCT_FISCAL_TYPE_SERVICE:
             return self.env.ref(NCM_FOR_SERVICE_REF)
 
     fiscal_type = fields.Selection(
         selection=PRODUCT_FISCAL_TYPE,
-        string="Fiscal Type")
+        string='Fiscal Type')
 
     icms_origin = fields.Selection(
         selection=ICMS_ORIGIN,
-        string="ICMS Origin",
+        string='ICMS Origin',
         default=ICMS_ORIGIN_DEFAULT)
 
     ncm_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.ncm",
+        comodel_name='l10n_br_fiscal.ncm',
         index=True,
         default=_get_default_ncm_id,
-        string="NCM")
+        string='NCM')
 
     nbm_id = fields.Many2one(
         comodel_name='l10n_br_fiscal.nbm',
         index=True,
-        string="NBM")
+        string='NBM')
 
     fiscal_genre_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.product.genre",
-        string="Fiscal Genre")
+        comodel_name='l10n_br_fiscal.product.genre',
+        string='Fiscal Genre')
 
     service_type_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.service.type",
-        string="Service Type LC 166",
+        comodel_name='l10n_br_fiscal.service.type',
+        string='Service Type LC 166',
         domain="[('internal_type', '=', 'normal')]")
 
     fiscal_genre_code = fields.Char(
-        related="fiscal_genre_id.code",
+        related='fiscal_genre_id.code',
         store=True,
-        string="Fiscal Genre Code")
+        string='Fiscal Genre Code')
 
     ipi_guideline_class_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax.ipi.guideline.class",
-        string="IPI Guideline Class")
+        comodel_name='l10n_br_fiscal.tax.ipi.guideline.class',
+        string='IPI Guideline Class')
 
     ipi_control_seal_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.tax.ipi.control.seal",
-        string="IPI Control Seal")
+        comodel_name='l10n_br_fiscal.tax.ipi.control.seal',
+        string='IPI Control Seal')
 
     nbs_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.nbs",
+        comodel_name='l10n_br_fiscal.nbs',
         index=True,
-        string="NBS")
+        string='NBS')
 
     cest_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.cest",
+        comodel_name='l10n_br_fiscal.cest',
         index=True,
-        string="CEST",
+        string='CEST',
         domain="[('ncm_ids', '=', ncm_id)]")
 
     uoe_id = fields.Many2one(
-        comodel_name="uom.uom",
-        related="ncm_id.uoe_id",
+        comodel_name='uom.uom',
+        related='ncm_id.uoe_id',
         store=True,
-        string="Export UoM")
+        string='Export UoM')
 
     uoe_factor = fields.Float(
-        string="Export UoM Factor",
+        string='Export UoM Factor',
         default=1.00)
 
     uot_id = fields.Many2one(
-        comodel_name="uom.uom",
-        string="Tax UoM")
+        comodel_name='uom.uom',
+        string='Tax UoM')
 
     uot_factor = fields.Float(
-        string="Tax UoM Factor",
+        string='Tax UoM Factor',
         default=0.00)
 
     # TODO add percent of estimate taxes
 
-    @api.onchange("fiscal_type")
+    @api.onchange('fiscal_type')
     def _onchange_fiscal_type(self):
         for r in self:
             if r.fiscal_type == PRODUCT_FISCAL_TYPE_SERVICE:
                 r.ncm_id = self.env.ref(NCM_FOR_SERVICE_REF)
 
-    @api.onchange("ncm_id", "fiscal_genre_id")
+    @api.onchange('ncm_id', 'fiscal_genre_id')
     def _onchange_ncm_id(self):
         for r in self:
             if r.ncm_id:
-                r.fiscal_genre_id = self.env["l10n_br_fiscal.product.genre"].search(
-                    [("code", "=", r.ncm_id.code[0:2])]
-                )
+                r.fiscal_genre_id = self.env[
+                    'l10n_br_fiscal.product.genre'].search(
+                        [('code', '=', r.ncm_id.code[0:2])])
 
             if r.fiscal_genre_id.code == PRODUCT_FISCAL_TYPE_SERVICE:
                 r.ncm_id = self.env.ref(NCM_FOR_SERVICE_REF)
