264a8f31df5ee083ef6456133d3a6b54d568978a
Magno Costa <magno.costa@akretion.com.br>
2020-04-03 16:38:54 -0300

    [12.0][MIG][FIX] Included necessary fields to work with mixin and inherit.

--- l10n_br_purchase/models/purchase_line.py
+++ l10n_br_purchase/models/purchase_line.py
@@ -2,9 +2,24 @@
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import models, fields
 
+from ...l10n_br_fiscal.constants.fiscal import (
+    TAX_FRAMEWORK,
+)
 
 class PurchaseOrderLine(models.Model):
     _name = "purchase.order.line"
     _inherit = ["purchase.order.line", "l10n_br_fiscal.document.line.mixin"]
+
+    fiscal_tax_ids = fields.Many2many(
+        comodel_name='l10n_br_fiscal.tax',
+        relation='fiscal_sale_line_tax_rel',
+        column1='document_id',
+        column2='fiscal_tax_id',
+        string="Fiscal Taxes")
+
+    tax_framework = fields.Selection(
+        selection=TAX_FRAMEWORK,
+        related="order_id.company_id.tax_framework",
+        string="Tax Framework")
