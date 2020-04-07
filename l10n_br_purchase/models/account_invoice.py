8ae357a9d2b1a3dc46143e96223ff7cc322d0291
Magno Costa <magno.costa@akretion.com.br>
2020-04-07 16:27:36 -0300

    [12.0][MIG][WIP] Creation of invoice and document fiscal from purchase order.

---
+++ l10n_br_purchase/models/account_invoice.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2020  Magno Costa - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    def _prepare_invoice_line_from_po_line(self, line):
+        data = super(
+            AccountInvoice, self
+        )._prepare_invoice_line_from_po_line(line)
+
+        data['operation_id'] = line.operation_id.id
+        data['operation_line_id'] = line.operation_line_id.id
+
+        return data
+
+    @api.model
+    def create(self, vals):
+
+        vals['purchase_id'] = vals['invoice_line_ids'][0][2].get('purchase_id')
+        # TODO - Are we need to identify when should create fiscal document ?
+        vals['fiscal_document_id'] = False
+
+        # TODO - Is there a better way to make it ? Should exist
+        if vals['invoice_line_ids'][0][2].get('purchase_id'):
+            purchase = self.env['purchase.order'].browse(
+                vals['invoice_line_ids'][0][2].get('purchase_id'))
+
+            vals['operation_id'] = purchase.operation_id.id
+            vals['operation_type'] = purchase.operation_type
+
+            for line in vals['invoice_line_ids']:
+                if line[2]['purchase_line_id']:
+                    purchase_line = self.env[
+                        'purchase.order.line'].browse(
+                        line[2]['purchase_line_id'])
+                    document_type_id = purchase_line.operation_line_id.get_document_type(
+                        purchase_line.order_id.company_id).id
+                    # TODO - Fields below should be filled by method
+                    #  _prepare_invoice_line_from_po_line() above, but
+                    #  the dict line[2] don't bring the fields, it's seems
+                    #  there is another method removing the fields
+                    line[2]['operation_id'] = purchase_line.operation_id.id
+                    line[2]['operation_line_id'] = purchase_line.operation_line_id.id
+
+            vals['document_type_id'] = document_type_id
+
+        invoice = super(AccountInvoice, self).create(vals)
+
+        return invoice
