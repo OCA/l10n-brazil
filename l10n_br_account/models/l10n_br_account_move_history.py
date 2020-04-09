dc955e2771e172a5cdd2685d8cdd163da99f73a1
Luis Felipe Mileo <mileo@kmee.com.br>
2020-04-08 22:07:00 -0300

    [REF] move history comment engine
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account/models/l10n_br_account_move_history.py
+++ l10n_br_account/models/l10n_br_account_move_history.py
@@ -1,7 +1,8 @@
 # Copyright 2020 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.addons.l10n_br_fiscal.tools.misc import compute_message as cm
 
 
 class MoveHistory(models.Model):
@@ -11,9 +12,28 @@ class MoveHistory(models.Model):
     _description = 'Histórico Contábil'
 
     code = fields.Char(required=False)
-    history = fields.Text(
+    comment = fields.Text(
         string="History",
         required=True
     )
 
-    # TODO: Refactoring fiscal.comment to use the same engine, move it to l10n_br_base
+    test_comment = fields.Text(string="Test Comment")
+
+    @api.multi
+    def object_selection_values(self):
+        return [('l10n_br_fiscal.document', "Fiscal Document"),
+                ('l10n_br_fiscal.document.line', "Fiscal Document Line")]
+
+    object_id = fields.Reference(
+        string='Reference',
+        selection=lambda self: self.object_selection_values(),
+        ondelete="set null"
+    )
+
+    def action_test_message(self):
+        vals = {
+            'user': self.env.user,
+            'ctx': self._context,
+            'doc': self.object_id
+        }
+        self.test_comment = cm(self, vals)
