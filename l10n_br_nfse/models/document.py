61c172f480424d25540761899c20da1eee9e84ea
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-04-09 16:06:00 -0300

    [ADD] Adaptation to version 12

--- l10n_br_nfse/models/document.py
+++ l10n_br_nfse/models/document.py
@@ -40,9 +40,9 @@ from odoo.addons.l10n_br_fiscal.constants.fiscal import (
 from .res_company import PROCESSADOR
 
 
-def fiter_processador_edoc_nfe(record):
+def fiter_processador_edoc_nfse(record):
     if (record.processador_edoc == PROCESSADOR and
-            record.fiscal_document_id.code in [
+            record.document_type_id.code in [
                 MODELO_FISCAL_NFSE,
             ]):
         return True
@@ -63,7 +63,7 @@ class Document(models.Model):
         readonly=True,
     )
 
-    def _procesador_erpbrasil_nfse(self):
+    def _processador_erpbrasil_nfse(self):
         certificado = cert.Certificado(
             arquivo=self.company_id.nfe_a1_file,
             senha=self.company_id.nfe_a1_password
@@ -85,20 +85,19 @@ class Document(models.Model):
         )
 
     @api.multi
-    def _edoc_export(self):
-        super(Document, self)._edoc_export()
-        for record in self.filtered(fiter_processador_edoc_nfe):
+    def _document_export(self):
+        super(Document, self)._document_export()
+        for record in self.filtered(fiter_processador_edoc_nfse):
             edoc = record.serialize()[0]
-            procesador = record._procesador_erpbrasil_nfse()
-            xml_file = procesador._generateds_to_string_etree(edoc)[0]
-            event_id = self._gerar_evento(xml_file, type="0")
+            processador = record._processador_erpbrasil_nfse()
+            xml_file = processador._generateds_to_string_etree(edoc)[0]
+            event_id = self._gerar_evento(xml_file, event_type="0")
             record.autorizacao_event_id = event_id
-            record._change_state(SITUACAO_EDOC_A_ENVIAR)
 
     def _serialize(self, edocs):
         edocs = super(Document, self)._serialize(edocs)
         for record in self.filtered(
-                fiter_processador_edoc_nfe).filtered(fiter_provedor_ginfes):
+                fiter_processador_edoc_nfse).filtered(fiter_provedor_ginfes):
             edocs.append(record.serialize_nfse())
         return edocs
 
@@ -121,6 +120,8 @@ class Document(models.Model):
             tomador_cpf = misc.punctuation_rm(
                 self.partner_id.cnpj_cpf or '')
 
+        partner_cep = misc.punctuation_rm(self.partner_id.zip)
+
         if self.partner_id.country_id.id != self.company_id.country_id.id:
             address_invoice_state_code = 'EX'
             address_invoice_city = 'Exterior'
@@ -134,9 +135,9 @@ class Document(models.Model):
             address_invoice_city_code = ('%s%s') % (
                 self.partner_id.state_id.ibge_code,
                 self.partner_id.city_id.ibge_code)
-            partner_cep = misc.punctuation_rm(self.partner_id.zip)
 
-        prestador_cnpj = misc.punctuation_rm(self.company_id.partner_id.cnpj_cpf)
+        prestador_cnpj = misc.punctuation_rm(
+            self.company_id.partner_id.cnpj_cpf)
         prestador_im = misc.punctuation_rm(
             self.company_id.partner_id.inscr_mun or '')
 
@@ -160,8 +161,6 @@ class Document(models.Model):
                         Valores=tcValores(
                             ValorServicos=self.amount_total,
                             ValorDeducoes=None,
-                            # ValorPis=self.pis_value,
-                            # ValorCofins=self.cofins_value,
                             ValorInss=None,
                             ValorIr=None,
                             ValorCsll=None,
@@ -175,9 +174,7 @@ class Document(models.Model):
                             DescontoIncondicionado=None,
                         ),
                         ItemListaServico='417',
-                        # ItemListaServico='105',
                         CodigoCnae=None,
-                        # CodigoTributacaoMunicipio='105:6202300',
                         CodigoTributacaoMunicipio='417/8511200',
                         Discriminacao=normalize('NFKD', str(
                             self.invoice_line_ids[0].name[:120] or '')
@@ -238,22 +235,23 @@ class Document(models.Model):
         return lote_rps
 
     @api.multi
-    def _edoc_send(self):
-        super(Document, self)._edoc_send()
-        for record in self.filtered(fiter_processador_edoc_nfe):
-            procesador = record._procesador_erpbrasil_nfse()
+    def _eletronic_document_send(self):
+        super(Document, self)._eletronic_document_send()
+        for record in self.filtered(fiter_processador_edoc_nfse):
+            processador = record._processador_erpbrasil_nfse()
 
             protocolo = ''
+            vals = dict()
 
             for edoc in record.serialize():
                 processo = None
-                for p in procesador.processar_documento(edoc):
+                for p in processador.processar_documento(edoc):
                     processo = p
 
-                    if processo.webservice == u'RecepcionarLoteRpsV3':
+                    if processo.webservice == 'RecepcionarLoteRpsV3':
                         protocolo = processo.resposta.Protocolo
 
-                if processo.webservice == u'ConsultarSituacaoLoteRpsV3':
+                if processo.webservice == 'ConsultarSituacaoLoteRpsV3':
                     mensagem = ''
                     if processo.resposta.Situacao == 1:
                         mensagem = _('Não Recebido')
@@ -268,13 +266,13 @@ class Document(models.Model):
                         mensagem = _('Procesado com Sucesso')
 
                     vals = {
-                        'edoc_status_code': processo.resposta.Situacao,
-                        'edoc_status_message': mensagem,
-                        'edoc_protocol_number': protocolo,
+                        'codigo_situacao': processo.resposta.Situacao,
+                        'motivo_situacao': mensagem,
+                        'protocolo_autorizacao': protocolo,
                     }
 
                 if processo.resposta.Situacao in (3, 4):
-                    processo = procesador.consultar_lote_rps(protocolo)
+                    processo = processador.consultar_lote_rps(protocolo)
 
                     if processo.resposta:
 
@@ -285,7 +283,7 @@ class Document(models.Model):
                                 mensagem_completa += (
                                     mr.Codigo + ' - ' +
                                     mr.Mensagem +
-                                    u' - Correção: ' +
+                                    ' - Correção: ' +
                                     mr.Correcao + '\n'
                                 )
                                 vals['edoc_error_message'] = mensagem_completa
