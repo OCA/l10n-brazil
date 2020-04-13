1ed00fd52595fe69a20d826b2a170a759404e9a5
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-04-13 10:40:59 -0300

    [FIX] Details

--- l10n_br_nfse/__manifest__.py
+++ l10n_br_nfse/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,10 +5,10 @@
     'name': 'Edoc Nfse',
     'summary': """
         NFS-E""",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'KMEE INFORMATICA LTDA,Odoo Community Association (OCA)',
-    'website': 'https://kmee.com.br',
+    'author': 'KMEE, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-brazil',
     'external_dependencies': {
         'python': [
             'erpbrasil.edoc',
--- l10n_br_nfse/models/document.py
+++ l10n_br_nfse/models/document.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
