e03a6968246475cf2643577eb95b51ae253d07cc
Magno Costa <magno.costa@akretion.com.br>
2020-04-13 14:53:59 -0300

    [12.0][MIG][REF] Models.

--- l10n_br_stock_account/models/stock_account.py
+++ l10n_br_stock_account/models/stock_account.py
@@ -1,81 +1,39 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields, api
 
+from ...l10n_br_fiscal.constants.fiscal import (
+    TAX_FRAMEWORK,
+)
+
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    def _default_fiscal_category(self):
-        return self.env.user.company_id.stock_fiscal_category_id
-
-    fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category', 'Categoria Fiscal',
-        readonly=True, domain="[('state', '=', 'approved')]",
-        states={'draft': [('readonly', False)]},
-        default=_default_fiscal_category)
-    fiscal_position_id = fields.Many2one(
-        'account.fiscal.position', u'Posição Fiscal',
-        domain="[('fiscal_category_id','=',fiscal_category_id)]",
-        readonly=True, states={'draft': [('readonly', False)]})
-
-    def _fiscal_position_map(self, **kwargs):
-        ctx = dict(self.env.context)
-        if ctx.get('fiscal_category_id'):
-            kwargs['fiscal_category_id'] = ctx.get('fiscal_category_id')
-        ctx.update({'use_domain': ('use_picking', '=', True)})
-        return self.env['account.fiscal.position.rule'].with_context(
-            ctx).apply_fiscal_mapping(**kwargs)
-
-    @api.onchange('fiscal_category_id', 'company_id')
-    def onchange_fiscal(self):
-        if self.partner_id and self.company_id and self.fiscal_category_id:
-            kwargs = {
-                'partner_id': self.partner_id,
-                'partner_invoice_id': self.partner_id,
-                'partner_shipping_id': self.partner_id,
-                'company_id': self.company_id,
-                'context': dict(self.env.context),
-                'fiscal_category_id': self.fiscal_category_id,
-            }
-            obj_fiscal_position = self._fiscal_position_map(**kwargs)
-            if obj_fiscal_position:
-                self.fiscal_position_id = obj_fiscal_position.id
+    _name = "stock.picking"
+    _inherit = [
+        _name,
+        "l10n_br_fiscal.document.mixin",
+    ]
 
 
 class StockMove(models.Model):
-    _inherit = "stock.move"
+    _name = "stock.move"
+    _inherit = [
+        _name,
+        "l10n_br_fiscal.document.line.mixin",
+    ]
 
-    fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category', 'Categoria Fiscal', readonly=True,
-        domain="[('type', '=', 'output'), ('journal_type', '=', 'sale')]",
-        states={'draft': [('readonly', False)],
-                'sent': [('readonly', False)]})
-    fiscal_position_id = fields.Many2one(
-        'account.fiscal.position', 'Fiscal Position', readonly=True,
-        domain="[('fiscal_category_id','=',fiscal_category_id)]",
-        states={'draft': [('readonly', False)],
-                'sent': [('readonly', False)]})
+    fiscal_tax_ids = fields.Many2many(
+        comodel_name='l10n_br_fiscal.tax',
+        relation='fiscal_move_line_tax_rel',
+        column1='document_id',
+        column2='fiscal_tax_id',
+        string="Fiscal Taxes")
 
-    def _fiscal_position_map(self, **kwargs):
-        ctx = dict(self.env.context)
-        ctx.update({'use_domain': ('use_picking', '=', True)})
-
-        partner = kwargs.get('partner_id')
-        obj_fp_rule = self.env['account.fiscal.position.rule']
-        product_fc_id = obj_fp_rule.with_context(
-            ctx).product_fiscal_category_map(
-            kwargs.get('product_id'),
-            kwargs.get('fiscal_category_id'),
-            partner.state_id.id)
-
-        if product_fc_id:
-            kwargs['fiscal_category_id'] = product_fc_id
-
-        return self.env['account.fiscal.position.rule'].with_context(
-            ctx).apply_fiscal_mapping(**kwargs)
+    tax_framework = fields.Selection(
+        selection=TAX_FRAMEWORK,
+        related="picking_id.company_id.tax_framework",
+        string="Tax Framework")
 
     @api.onchange('product_id')
     def onchange_product_id(self):
@@ -110,29 +68,12 @@ class StockMove(models.Model):
             result_super.update(result)
         return result_super
 
-    @api.onchange('fiscal_category_id', 'fiscal_position_id')
-    def onchange_fiscal(self):
-        if self.picking_id.partner_id and self.picking_id.company_id \
-                and self.fiscal_category_id:
-            kwargs = {
-                'partner_id': self.picking_id.partner_id,
-                'product_id': self.product_id,
-                'partner_invoice_id': self.picking_id.partner_id,
-                'partner_shipping_id': self.picking_id.partner_id,
-                'company_id': self.picking_id.company_id,
-                'context': dict(self.env.context),
-                'fiscal_category_id': self.fiscal_category_id,
-            }
-            fiscal_position = self._fiscal_position_map(**kwargs)
-            if fiscal_position:
-                self.fiscal_position_id = fiscal_position.id
-
     def _get_new_picking_values(self):
         """ Prepares a new picking for this move as it could not be assigned to
         another picking. This method is designed to be inherited. """
         result = super(StockMove, self)._get_new_picking_values()
         result.update({
-            'fiscal_category_id': self.fiscal_category_id.id,
-            'fiscal_position_id': self.fiscal_position_id.id,
+            'operation_id': self.operation_id.id,
+            # 'operation_line_id': self.operation_line_id.id,
         })
         return result
