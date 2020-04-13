0fbab8ea20321516685b5ba588180e0b314bc9f9
Renato Lima <renato.lima@akretion.com>
2020-04-13 17:47:49 -0300

    [FIX] test, sale order line fields and demo data

--- l10n_br_sale/tests/test_l10n_br_sale_product.py
+++ l10n_br_sale/tests/test_l10n_br_sale_product.py
@@ -10,8 +10,7 @@ class TestL10nBRSaleProduct(common.TransactionCase):
         super(TestL10nBRSaleProduct, self).setUp()
         self.sale_object = self.env["sale.order"]
         self.sale_stock = self.sale_object.browse(
-            self.ref("l10n_br_sale.l10n_br_sale_product_demo_1")
-        )
+            self.ref("l10n_br_sale.l10n_br_sale_product_demo_1"))
 
     def test_l10n_br_sale_product(self):
         """Test fields implemented by l10n_br_sale_product"""
@@ -24,32 +23,34 @@ class TestL10nBRSaleProduct(common.TransactionCase):
             line._onchange_operation_line_id()
             line._onchange_fiscal_taxes()
             line.price_unit = 250
+
             self.assertTrue(
                 line.operation_line_id,
-                "Operation Line is missing in Sale Order Line.",
-            )
+                "Operation Line is missing in Sale Order Line.")
 
         self.assertEquals(
             self.sale_stock.amount_total,
             1012.0,
-            u"Error to apply discount on sale order.",
-        )
+            "Error to apply discount on sale order.")
+
         self.assertEquals(
             self.sale_stock.amount_freight,
             6.0,
-            u"Error to calculate Total Amount Freight.",
-        )
+            "Error to calculate Total Amount Freight.")
+
         self.assertEquals(
-            self.sale_stock.amount_costs, 2.0, u"Error to calculate Total Amount Costs."
-        )
+            self.sale_stock.amount_costs, 2.0,
+            "Error to calculate Total Amount Costs.")
+
         self.assertEquals(
-            self.sale_stock.amount_extra, 12.0, u"Error to calculate Total Amount Extra"
-        )
+            self.sale_stock.amount_extra, 12.0,
+            "Error to calculate Total Amount Extra")
+
         self.assertEquals(
             self.sale_stock.amount_insurance,
             4.0,
-            u"Error to calculate Total Amount Extra",
-        )
+            "Error to calculate Total Amount Extra")
+
         self.sale_stock.action_confirm()
         # Create and check invoice
         self.sale_stock.action_invoice_create(final=True)
@@ -57,14 +58,14 @@ class TestL10nBRSaleProduct(common.TransactionCase):
             self.assertEquals(
                 invoice.amount_untaxed,
                 1000.0,
-                u"Error to apply discount on invoice" u" created from sale order.",
-            )
+                "Error to apply discount on invoice "
+                "created from sale order.")
+
             for line in invoice.invoice_line_ids:
                 self.assertTrue(
                     line.company_id,
-                    "Error to inform field company_id on Sale Order Line.",
-                )
+                    "Error to inform field company_id on Sale Order Line.")
+
                 self.assertTrue(
                     line.partner_id,
-                    "Error to inform field partner_id on Sale Order Line.",
-                )
+                    "Error to inform field partner_id on Sale Order Line.")
