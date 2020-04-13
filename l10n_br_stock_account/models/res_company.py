ae644c01d8549147e7e0a9616d3ce2210facfa9a
Magno Costa <magno.costa@akretion.com.br>
2020-04-13 14:53:59 -0300

    [12.0][MIG][REF] Models.

--- l10n_br_stock_account/models/res_company.py
+++ l10n_br_stock_account/models/res_company.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2011  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
@@ -8,16 +7,20 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    stock_fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category',
-        u'Categoria Fiscal Padrão Estoque')
-    stock_in_fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category',
-        u'Categoria Fiscal Padrão de Entrada',
-        domain="[('journal_type', 'in', ('sale_refund', 'purchase')), "
-        "('fiscal_type', '=', 'product'), ('type', '=', 'input')]")
-    stock_out_fiscal_category_id = fields.Many2one(
-        'l10n_br_account.fiscal.category',
-        u'Categoria Fiscal Padrão Saída',
-        domain="[('journal_type', 'in', ('purchase_refund', 'sale')), "
-        "('fiscal_type', '=', 'product'), ('type', '=', 'output')]")
+    stock_fiscal_operation_id = fields.Many2one(
+        comodel_name='l10n_br_fiscal.operation',
+        string='Operação Fiscal Padrão de Estoque')
+    stock_in_fiscal_operation_id = fields.Many2one(
+        comodel_name='l10n_br_fiscal.operation',
+        string=u'Categoria Fiscal Padrão de Entrada',
+        # TODO - domain
+        # domain="[('journal_type', 'in', ('sale_refund', 'purchase')), "
+        # "('fiscal_type', '=', 'product'), ('type', '=', 'input')]"
+    )
+    stock_out_fiscal_operation_id = fields.Many2one(
+        comodel_name='l10n_br_fiscal.operation',
+        string=u'Categoria Fiscal Padrão Saída',
+        # TODO - domain
+        # domain="[('journal_type', 'in', ('purchase_refund', 'sale')), "
+        # "('fiscal_type', '=', 'product'), ('type', '=', 'output')]"
+    )
