4ec878c5d7913ad94ad25f45f0469aa2c200a689
Magno Costa <magno.costa@akretion.com.br>
2020-04-16 17:56:25 -0300

    [12.0][MIG][FIX] Map taxes.

--- l10n_br_stock_account/models/stock_account.py
+++ l10n_br_stock_account/models/stock_account.py
@@ -35,45 +35,70 @@ class StockMove(models.Model):
         related="picking_id.company_id.tax_framework",
         string="Tax Framework")
 
-    @api.onchange('product_id')
-    def onchange_product_id(self):
-        context = dict(self.env.context)
-        parent_fiscal_category_id = context.get('parent_fiscal_category_id')
-        if context.get('company_id'):
-            company_id = context['company_id']
-        else:
-            company_id = self.env.user.company_id.id
-
-        result = {'value': {}}
-        result['value']['invoice_state'] = context.get('parent_invoice_state')
-
-        if parent_fiscal_category_id and self.product_id and self.partner_id:
-
-            kwargs = {
-                'partner_id': self.partner_id,
-                'product_id': self.product_id,
-                'partner_invoice_id': self.partner_id,
-                'partner_shipping_id': self.partner_id,
-                'fiscal_category_id': parent_fiscal_category_id,
-                'company_id': company_id,
-                'context': context
-            }
-
-            result.update(self._fiscal_position_map(**kwargs))
-
-        result_super = super(StockMove, self).onchange_product_id()
-        if result_super.get('value'):
-            result_super.get('value').update(result['value'])
-        else:
-            result_super.update(result)
-        return result_super
+    # TODO - Is there any reason to keep it ?
+    # @api.onchange('product_id')
+    # def onchange_product_id(self):
+    #     context = dict(self.env.context)
+    #     parent_fiscal_category_id = context.get('parent_fiscal_category_id')
+    #     if context.get('company_id'):
+    #         company_id = context['company_id']
+    #     else:
+    #         company_id = self.env.user.company_id.id
+    #
+    #     result = {'value': {}}
+    #     result['value']['invoice_state'] = context.get('parent_invoice_state')
+    #
+    #     if parent_fiscal_category_id and self.product_id and self.partner_id:
+    #
+    #         kwargs = {
+    #             'partner_id': self.partner_id,
+    #             'product_id': self.product_id,
+    #             'partner_invoice_id': self.partner_id,
+    #             'partner_shipping_id': self.partner_id,
+    #             'fiscal_category_id': parent_fiscal_category_id,
+    #             'company_id': company_id,
+    #             'context': context
+    #         }
+    #
+    #         result.update(self._fiscal_position_map(**kwargs))
+    #
+    #     result_super = super(StockMove, self).onchange_product_id()
+    #     if result_super.get('value'):
+    #         result_super.get('value').update(result['value'])
+    #     else:
+    #         result_super.update(result)
+    #     return result_super
 
     def _get_new_picking_values(self):
         """ Prepares a new picking for this move as it could not be assigned to
         another picking. This method is designed to be inherited. """
         result = super(StockMove, self)._get_new_picking_values()
-        result.update({
-            'operation_id': self.operation_id.id,
-            # 'operation_line_id': self.operation_line_id.id,
-        })
+        result.update({'operation_id': self.operation_id.id})
         return result
+
+    @api.onchange("operation_line_id")
+    def _onchange_operation_line_id(self):
+
+        # Reset Taxes
+        self._remove_all_fiscal_tax_ids()
+        if self.operation_line_id:
+            mapping_result = self.operation_line_id.map_fiscal_taxes(
+                # TODO - We need to inherit this method to identify company and
+                #  the TAX FRAMEWORK. Check if there are a better way to do
+                company=self.company_id or self.picking_id.company_id,
+                partner=self.partner_id,
+                product=self.product_id,
+                ncm=self.ncm_id,
+                nbm=self.nbm_id,
+                nbs=self.nbs_id,
+                cest=self.cest_id)
+
+            self.cfop_id = mapping_result['cfop']
+            taxes = self.env['l10n_br_fiscal.tax']
+            for tax in mapping_result['taxes'].values():
+                taxes |= tax
+            self.fiscal_tax_ids = taxes
+            self._update_taxes()
+
+        if not self.operation_line_id:
+            self.cfop_id = False
