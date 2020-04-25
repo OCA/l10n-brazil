b2363a3d0ddf599ff37a02cb2c2cbc8dd4381a12
Renato Lima <renato.lima@akretion.com>
2020-04-25 13:37:22 -0300

    [REF] purchase order and purchase order line

--- l10n_br_purchase/models/res_company.py
+++ l10n_br_purchase/models/res_company.py
@@ -5,10 +5,11 @@ from odoo import fields, models
 
 
 class Company(models.Model):
-    _inherit = "res.company"
+    _inherit = 'res.company'
 
     purchase_fiscal_operation_id = fields.Many2one(
-        comodel_name="l10n_br_fiscal.operation",
-        string="Default Fiscal Operation for Purchase",
-        domain="[('state', '=', 'approved')]",
+        comodel_name='l10n_br_fiscal.operation',
+        string='Default Fiscal Operation for Purchase',
+        domain="[('state', '=', 'approved'), "
+               "('fiscal_type', '=', 'purchase')]"
     )
