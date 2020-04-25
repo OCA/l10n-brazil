26fb6b8a738286bb9f0d7b3f030a1a220ad63646
Renato Lima <renato.lima@akretion.com>
2020-04-25 18:18:13 -0300

    pep8

--- l10n_br_purchase_stock/models/purchase_line.py
+++ l10n_br_purchase_stock/models/purchase_line.py
@@ -2,10 +2,7 @@
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from datetime import datetime
-
-from odoo import api, fields, models
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import api, models
 
 
 class PurchaseOrderLine(models.Model):
@@ -18,10 +15,10 @@ class PurchaseOrderLine(models.Model):
          This function returns a list of
         dictionary ready to be used in stock.move's create()
         """
-        self.ensure_one()
-        vals = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
-        vals[0].update({
-            'fiscal_category_id': self.fiscal_category_id.id,
-            'fiscal_position_id': self.fiscal_position_id.id,
+        values = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
+        values[0].update({
+            'operation_id': self.operation_id.id,
+            'operation_line_id': self.operation_line_id.id,
+            'cfop_id': self.cfop_id.id,
         })
-        return vals
+        return values
