d9179bdcaa2373c2687f67e643c757393d1b216c
clementmbr <clement.mombereau@akretion.com.br>
2020-04-24 21:43:06 -0300

    [IMP] Add short CSV files to load NCM, NBM, NBS and CEST code lists.
    These short CSV files are loaded by default in DEMO mode while the
    full CSV files are loaded by default in WITHOUT_DEMO mode.
    
    This aims to save nearly few minutes in loading time.
    On a local Odoo, it took 1min30s to install both 'base' and 'l10n_br_fiscal'
    in DEMO mode with the short CSV files, whereas it took 04min40s to load
    the same modules in WITHOUT_DEMO mode and the full CSV files.
    
    Flags like `load_ncm`, `load_nbm`, `load_nbs` or `load_cest` must be added in
    the odoo.cfg file in order to load the respective full CSV files in DEMO mode.
    
    On the other side, flags like `skip_ncm`, `skip_nbm` (and so on...) must be added
    in the odoo.cfg file in WITHOUT_DEMO mode in order not to load any CSV files.
    
    The short CSV files were created with a selection of NCM, NBM, NBS and CEST codes
    used in the other DEMO files.
    The script used to create these short CSV can be found here :
    https://gist.github.com/clementmbr/fa635dc8cbd83d3e56002ccb7ba67a21

--- l10n_br_fiscal/hooks.py
+++ l10n_br_fiscal/hooks.py
@@ -1,7 +1,10 @@
 # Copyright (C) 2019 - Renato Lima Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import tools
+import logging
+from odoo import _, tools
+
+_logger = logging.getLogger(__name__)
 
 
 def post_init_hook(cr, registry):
@@ -14,17 +17,15 @@ def post_init_hook(cr, registry):
         "data/l10n_br_fiscal.tax.ipi.control.seal.csv",
         "data/l10n_br_fiscal.tax.ipi.guideline.csv",
         "data/l10n_br_fiscal.tax.ipi.guideline.class.csv",
-        "data/l10n_br_fiscal.ncm.csv",
-        "data/l10n_br_fiscal.cest.csv",
-        "data/l10n_br_fiscal.nbm.csv",
         "data/l10n_br_fiscal.tax.pis.cofins.base.csv",
         "data/l10n_br_fiscal.tax.pis.cofins.credit.csv",
-        "data/l10n_br_fiscal.nbs.csv",
         "data/l10n_br_fiscal.service.type.csv",
         "data/simplified_tax_data.xml",
         "data/operation_data.xml",
         "data/l10n_br_fiscal_tax_icms_data.xml",
     ]
+
+    _logger.info(_("Loading l10n_br_fiscal fiscal files. It may take a minute..."))
 
     for file in files:
         tools.convert_file(
@@ -38,16 +39,71 @@ def post_init_hook(cr, registry):
             report=None,
         )
 
-    if not tools.config['without_demo']:
-        demofiles = [
-            "demo/company_demo.xml",
-            "demo/product_demo.xml",
-            "demo/partner_demo.xml",
-            "demo/fiscal_document_demo.xml",
-            "demo/fiscal_operation_demo.xml",
-        ]
+    if not tools.config["without_demo"]:
+        demofiles = []
+        # Load only demo CSV files with few lines instead of thousands
+        # unless a flag mention the contrary
+        if not tools.config.get("load_ncm"):
+            demofiles.append("data/l10n_br_fiscal.ncm-demo.csv")
+        else:
+            demofiles.append("data/l10n_br_fiscal.ncm.csv")
+        if not tools.config.get("load_nbm"):
+            demofiles.append("data/l10n_br_fiscal.nbm-demo.csv")
+        else:
+            demofiles.append("data/l10n_br_fiscal.nbm.csv")
+        if not tools.config.get("load_nbs"):
+            demofiles.append("data/l10n_br_fiscal.nbs-demo.csv")
+        else:
+            demofiles.append("data/l10n_br_fiscal.nbs.csv")
+        if not tools.config.get("load_cest"):
+            demofiles.append("data/l10n_br_fiscal.cest-demo.csv")
+        else:
+            demofiles.append("data/l10n_br_fiscal.cest.csv")
+
+        _logger.info(_("Loading l10n_br_fiscal demo files."))
+
+        demofiles.extend(
+            [
+                "demo/company_demo.xml",
+                "demo/product_demo.xml",
+                "demo/partner_demo.xml",
+                "demo/fiscal_document_demo.xml",
+                "demo/fiscal_operation_demo.xml",
+            ]
+        )
 
         for f in demofiles:
+            tools.convert_file(
+                cr,
+                "l10n_br_fiscal",
+                f,
+                None,
+                mode="init",
+                noupdate=False,
+                kind="demo",
+                report=None,
+            )
+
+    elif tools.config["without_demo"]:
+        prodfiles = []
+        # Load full CSV files with few lines unless a flag mention the contrary
+        if not tools.config.get("skip_ncm"):
+            prodfiles.append("data/l10n_br_fiscal.ncm.csv")
+        if not tools.config.get("skip_nbm"):
+            prodfiles.append("data/l10n_br_fiscal.nbm.csv")
+        if not tools.config.get("skip_nbs"):
+            prodfiles.append("data/l10n_br_fiscal.nbs.csv")
+        if not tools.config.get("skip_cest"):
+            prodfiles.append("data/l10n_br_fiscal.cest.csv")
+
+        _logger.info(
+            _(
+                "Loading l10n_br_fiscal production files. It may take at least"
+                " 3 minutes..."
+            )
+        )
+
+        for f in prodfiles:
             tools.convert_file(
                 cr,
                 "l10n_br_fiscal",
