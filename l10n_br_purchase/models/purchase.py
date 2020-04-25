9cf057b7c8ccc3c7f36d805f072fca16eafe8a81
Renato Lima <renato.lima@akretion.com>
2020-04-25 13:37:22 -0300

    [REF] purchase order and purchase order line

--- l10n_br_purchase/models/purchase.py
+++ l10n_br_purchase/models/purchase.py
@@ -2,82 +2,46 @@
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-
 from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
 
 class PurchaseOrder(models.Model):
-    _name = "purchase.order"
-    _inherit = ["purchase.order", "l10n_br_fiscal.document.mixin"]
-
-    @api.one
-    @api.depends(
-        "order_line.price_unit", "order_line.product_qty", "order_line.taxes_id"
-    )
-    def _compute_amount(self):
-        amount_untaxed = 0.0
-        amount_tax = 0.0
-
-        for order in self:
-            amount_untaxed = amount_tax = 0.0
-            for line in order.order_line:
-                amount_untaxed += line.price_subtotal
-                if order.company_id.tax_calculation_rounding_method == "round_globally":
-                    taxes = line.taxes_id.compute_all(
-                        line.price_unit,
-                        line.order_id.currency_id,
-                        line.product_qty,
-                        product=line.product_id,
-                        partner=line.order_id.partner_id,
-                    )
-                    amount_tax += sum(
-                        t.get("amount", 0.0) for t in taxes.get("taxes", [])
-                    )
-                else:
-                    amount_tax += line.price_tax
-            order.update(
-                {
-                    "amount_untaxed": order.currency_id.round(amount_untaxed),
-                    "amount_tax": order.currency_id.round(amount_tax),
-                    "amount_total": amount_untaxed + amount_tax,
-                }
-            )
-
-        self.amount_untaxed = self.currency_id.round(amount_untaxed)
-        self.amount_tax = self.currency_id.round(amount_tax)
-        self.amount_total = self.currency_id.round(amount_untaxed + amount_tax)
+    _name = 'purchase.order'
+    _inherit = ['purchase.order', 'l10n_br_fiscal.document.mixin']
 
     @api.model
-    @api.returns("l10n_br_account.fiscal_category")
-    def _default_fiscal_category(self):
-        company = self.env["res.company"].browse(self.env.user.company_id.id)
-        return company.purchase_fiscal_category_id
+    def _default_operation(self):
+        return self.env.user.company_id.purchase_fiscal_operation_id
 
-    amount_untaxed = fields.Monetary(
-        compute="_compute_amount",
-        digits=dp.get_precision("Purchase Price"),
-        string="Untaxed Amount",
-        store=True,
-        help="The amount without tax",
-    )
-    amount_tax = fields.Monetary(
-        compute="_compute_amount",
-        digits=dp.get_precision("Purchase Price"),
-        string="Taxes",
-        store=True,
-        help="The tax amount",
-    )
-    amount_total = fields.Monetary(
-        compute="_compute_amount",
-        digits=dp.get_precision("Purchase Price"),
-        string="Total",
-        store=True,
-        help="The total amount",
+    @api.model
+    def _operation_domain(self):
+        domain = [
+            ('state', '=', 'approved'),
+            ('fiscal_type', 'in', ('purchase', 'other', 'purchase_refund'))]
+        return domain
+
+    operation_id = fields.Many2one(
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        default=_default_operation,
+        domain=lambda self: self._operation_domain(),
     )
 
-    cnpj_cpf = fields.Char(string="CNPJ/CPF", related="partner_id.cnpj_cpf")
+    cnpj_cpf = fields.Char(
+        string='CNPJ/CPF',
+        related='partner_id.cnpj_cpf'),
 
-    legal_name = fields.Char(string="Razão Social", related="partner_id.legal_name")
+    legal_name = fields.Char(
+        string='Legal Name',
+        related='artner_id.legal_name',
+    )
 
-    ie = fields.Char(string="Inscrição Estadual", related="partner_id.inscr_est")
+    ie = fields.Char(
+        string='State Tax Number/RG',
+        related='partner_id.inscr_est',
+    )
+
+    @api.onchange('operation_id')
+    def _onchange_operation_id(self):
+        self.fiscal_position_id = self.operation_id.fiscal_position_id
