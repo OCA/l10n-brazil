bdf59a3d88ae65f822ce7b875577e4a779cb90ce
Renato Lima <renato.lima@akretion.com>
2020-04-27 19:15:54 -0300

    split file model by objects

--- l10n_br_stock_account/models/__init__.py
+++ l10n_br_stock_account/models/__init__.py
@@ -2,5 +2,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import res_company
-from . import stock_account
+from . import stock_picking
+from . import stock_move
 from . import stock_rule
