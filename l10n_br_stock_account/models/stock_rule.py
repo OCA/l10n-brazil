4f60daf2c3c17ace1f9c6ac1a2bc2a4216426935
Magno Costa <magno.costa@akretion.com.br>
2020-04-27 18:28:22 -0300

    [12.0][MIG][FIX] PEP8.

--- l10n_br_stock_account/models/stock_rule.py
+++ l10n_br_stock_account/models/stock_rule.py
@@ -5,15 +5,15 @@
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class StockRule(models.Model):
     _name = "stock.rule"
     _inherit = [
-     _name,
-     "stock.invoice.state.mixin",
-     "l10n_br_fiscal.document.line.mixin"
+        _name,
+        "stock.invoice.state.mixin",
+        "l10n_br_fiscal.document.line.mixin"
     ]
 
     # TODO - The method don't work because in _get_stock_move_values
@@ -44,21 +44,30 @@ class StockRule(models.Model):
         qty_left = product_qty
         move_values = {
             'name': name[:2000],
-            'company_id': self.company_id.id or self.location_src_id.company_id.id or self.location_id.company_id.id or values['company_id'].id,
+            'company_id':
+                self.company_id.id or self.location_src_id.company_id.id or
+                self.location_id.company_id.id or values['company_id'].id,
             'product_id': product_id.id,
             'product_uom': product_uom.id,
             'product_uom_qty': qty_left,
-            'partner_id': self.partner_address_id.id or (values.get('group_id', False) and values['group_id'].partner_id.id) or False,
+            'partner_id':
+                self.partner_address_id.id or (
+                    values.get('group_id', False) and
+                    values['group_id'].partner_id.id) or False,
             'location_id': self.location_src_id.id,
             'location_dest_id': location_id.id,
-            'move_dest_ids': values.get('move_dest_ids', False) and [(4, x.id) for x in values['move_dest_ids']] or [],
+            'move_dest_ids':
+                values.get('move_dest_ids', False) and [
+                    (4, x.id) for x in values['move_dest_ids']] or [],
             'rule_id': self.id,
             'procure_method': self.procure_method,
             'origin': origin,
             'picking_type_id': self.picking_type_id.id,
             'group_id': group_id,
-            'route_ids': [(4, route.id) for route in values.get('route_ids', [])],
-            'warehouse_id': self.propagate_warehouse_id.id or self.warehouse_id.id,
+            'route_ids':
+                [(4, route.id) for route in values.get('route_ids', [])],
+            'warehouse_id':
+                self.propagate_warehouse_id.id or self.warehouse_id.id,
             'date': date_expected,
             'date_expected': date_expected,
             'propagate': self.propagate,
