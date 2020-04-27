033d0abbf8d7979c9946f1db007fe514b783aa10
Renato Lima <renato.lima@akretion.com>
2020-04-27 19:09:43 -0300

    [REF] manifest file

--- l10n_br_stock_account/__manifest__.py
+++ l10n_br_stock_account/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Localisation',
     'license': 'AGPL-3',
     'author': 'Akretion, Odoo Community Association (OCA)',
-    'website': 'http://odoo-brasil.org',
+    'website': 'http://github.com/OCA/l10n-brazil',
     'version': '12.0.1.0.0',
     'depends': [
         'stock_account',
@@ -15,15 +15,23 @@
         'l10n_br_account',
     ],
     'data': [
+        # Security
+        'security/ir.model.access.csv',
+
+        # Data
         'data/l10n_br_stock_account_data.xml',
+
+        # View
         'views/stock_account_view.xml',
         'views/res_company_view.xml',
         'views/stock_rule_view.xml',
+
+        # Wizard
         'wizard/stock_invoice_onshipping_view.xml',
         'wizard/stock_return_picking_view.xml',
-        'security/ir.model.access.csv',
     ],
     'demo': [
+        # Demo
         'demo/l10n_br_stock_account_demo.xml',
     ],
     'installable': True,
