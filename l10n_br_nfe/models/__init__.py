763f6e0fe69667dd85832ddbfc3d16de3fdf49d2
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-04-30 16:05:55 -0300

    [FIX] Order of import

--- l10n_br_nfe/models/__init__.py
+++ l10n_br_nfe/models/__init__.py
@@ -1,9 +1,9 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import res_company
 from . import document
 from . import document_line
 from . import res_city
 from . import res_country_state
-from . import res_company
 from . import res_config_settings
 from . import cfop
