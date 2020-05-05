7b8a1f8cd7fdeb7160d59069b56247b74f698bb1
Hendrix Costa <hendrixcosta@gmail.com>
2020-05-04 21:52:31 -0300

    [FIX] Rename Data

--- l10n_br_simple/__manifest__.py
+++ l10n_br_simple/__manifest__.py
@@ -14,7 +14,7 @@
         "data/l10n_br_chart_data.xml",
         "data/account_group_data.xml",
         "data/account.account.template.csv",
-        "data/account_data.xml",
+        "data/account_tax_group_data.xml",
         "data/account_tax_template_data.xml",
     ],
 }
