61572ae6c7993fe5852a40ec01cb972bd8139ecf
Luis Felipe Mileo <mileo@kmee.com.br>
2020-05-06 16:20:22 -0300

    [FIX] Clean code payment in fiscal
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/models/document_workflow.py
+++ l10n_br_fiscal/models/document_workflow.py
@@ -312,6 +312,7 @@ class DocumentWorkflow(models.AbstractModel):
         to_confirm = self.filtered(lambda inv: inv.state_edoc != SITUACAO_EDOC_A_ENVIAR)
         if to_confirm:
             to_confirm._document_confirm()
+            to_confirm.generate_financial()
 
     def _no_eletronic_document_send(self):
         self._change_state(SITUACAO_EDOC_AUTORIZADA)
