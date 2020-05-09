45db2d3aeb60c0711c2a5d3e926c32e77ea4068f
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-09 17:48:05 -0300

    [12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple

--- l10n_br_base/__init__.py
+++ l10n_br_base/__init__.py
@@ -11,16 +11,16 @@ from odoo import api, SUPERUSER_ID
 _auto_install_l10n_original = account._auto_install_l10n
 
 
-def _auto_install_l10n_br_simple_coa(cr, registry):
+def _auto_install_l10n_br_coa_simple(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     country_code = env.user.company_id.country_id.code
     if country_code and country_code.upper() == "BR":
         module_ids = env["ir.module.module"].search(
-            [("name", "in", ("l10n_br_simple_coa",)), ("state", "=", "uninstalled")]
+            [("name", "in", ("l10n_br_coa_simple",)), ("state", "=", "uninstalled")]
         )
         module_ids.sudo().button_install()
     else:
         _auto_install_l10n_original(cr, registry)
 
 
-account._auto_install_l10n = _auto_install_l10n_br_simple_coa
+account._auto_install_l10n = _auto_install_l10n_br_coa_simple
