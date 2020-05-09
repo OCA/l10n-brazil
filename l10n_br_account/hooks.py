4cb95cbad1c8b08eb631eebbd3e4e0adfa1c1e06
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-09 17:48:05 -0300

    [12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple

--- l10n_br_account/hooks.py
+++ l10n_br_account/hooks.py
@@ -38,10 +38,10 @@ def pre_init_hook(cr):
 def post_init_hook(cr, registry):
     """Relate fiscal taxes to account taxes."""
     env = api.Environment(cr, SUPERUSER_ID, {})
-    l10n_br_simple_chart = env.ref(
-        "l10n_br_simple_coa.l10n_br_simple_chart_template")
+    l10n_br_coa_simple_chart = env.ref(
+        "l10n_br_coa_simple.l10n_br_coa_simple_chart_template")
     companies = env["res.company"].search(
-        [("chart_template_id", "=", l10n_br_simple_chart.id)]
+        [("chart_template_id", "=", l10n_br_coa_simple_chart.id)]
     )
 
     for company in companies:
