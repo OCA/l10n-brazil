aad08948e2d64d0ea396345763adb44f4879f59b
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-09 17:48:05 -0300

    [12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple

--- l10n_br_account/tests/test_account_taxes.py
+++ l10n_br_account/tests/test_account_taxes.py
@@ -8,14 +8,14 @@ class TestAccountTaxes(TransactionCase):
     def setUp(self):
         super(TestAccountTaxes, self).setUp()
 
-        self.l10n_br_simple_coa = self.env.ref(
-            'l10n_br_simple_coa.l10n_br_simple_chart_template')
+        self.l10n_br_coa_simple = self.env.ref(
+            'l10n_br_coa_simple.l10n_br_coa_simple_chart_template')
         self.l10n_br_company = self.env['res.company'].create({
             'name': 'Empresa Teste do Plano de Contas Simplificado'})
 
         self.env.user.company_ids += self.l10n_br_company
         self.env.user.company_id = self.l10n_br_company
-        self.l10n_br_simple_coa.try_loading_for_current_company()
+        self.l10n_br_coa_simple.try_loading_for_current_company()
 
     def test_account_taxes(self):
         """Test if account taxes are related with fiscal taxes"""
