d949fe91a7154459c0ef2fb3883ab1d2699aeaef
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-09 17:48:05 -0300

    [12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple

--- l10n_br_coa_simple/tests/__init__.py
+++ l10n_br_coa_simple/tests/__init__.py
@@ -1,3 +1,11 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+<<<<<<< HEAD
 from . import test_l10n_br_coa_simple
+=======
+<<<<<<< HEAD:l10n_br_fiscal/tools/__init__.py
+from . import misc
+=======
+from . import test_l10n_br_coa_simple
+>>>>>>> [12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple:l10n_br_coa_simple/tests/__init__.py
+>>>>>>> [12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple
