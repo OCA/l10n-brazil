816abf561fc1ed51f76f39af664acc1e38348a3f
Renato Lima <renato.lima@akretion.com>
2020-05-11 23:49:43 -0300

    Update l10n_br_sale depedency

--- l10n_br_sale/__manifest__.py
+++ l10n_br_sale/__manifest__.py
@@ -10,7 +10,7 @@
         'Odoo Community Association (OCA)',
     "website": "http://odoo-brasil.org",
     "version": "12.0.1.0.0",
-    "depends": ["sale", "l10n_br_account"],
+    "depends": ["sale_management", "l10n_br_account"],
     "data": [
         # Data
         "data/company_data.xml",
