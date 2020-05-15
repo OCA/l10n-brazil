c0131f1f2e068a01e9756fd855e1c9813ae7d1bb
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-15 16:25:06 -0300

    [ADD] Some changes in modules structure

--- l10n_br_coa/__manifest__.py
+++ l10n_br_coa/__manifest__.py
@@ -8,9 +8,10 @@
         Base Brasilian Localization of Chart of Account""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'KMEE,Odoo Community Association (OCA)',
+    'author': 'Akretion, KMEE, Odoo Community Association (OCA)',
     'website': 'https://github.com/oca/l10n-brazil',
     'depends': [
+        "account"
     ],
     'data': [
         'security/l10n_br_account_tax_template.xml',
