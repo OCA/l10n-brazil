b052785708bb403a8198c4611f4da3221a6717eb
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-18 10:24:49 -0300

    [REM] Unnecessary in python3

--- l10n_br_coa/models/account_chart_template.py
+++ l10n_br_coa/models/account_chart_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,6 @@ from odoo import models
 
 
 class AccountChartTemplate(models.Model):
-
     _inherit = 'account.chart.template'
 
     def try_loading_for_current_company(self):
