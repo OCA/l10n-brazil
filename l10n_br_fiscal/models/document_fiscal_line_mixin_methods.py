ee3df960f7e5b5a8a0196353931be45bc5925460
Luis Felipe Miléo <mileo@kmee.com.br>
2020-05-19 19:35:49 -0300

    Update document_fiscal_line_mixin_methods.py

--- l10n_br_fiscal/models/document_fiscal_line_mixin_methods.py
+++ l10n_br_fiscal/models/document_fiscal_line_mixin_methods.py
@@ -76,7 +76,7 @@ class FiscalDocumentLineMixinMethods(models.AbstractModel):
     @api.model
     def fields_view_get(self, view_id=None, view_type="form",
                         toolbar=False, submenu=False):
-        model_view = super(FiscalDocumentLineMixin, self).fields_view_get(
+        model_view = super(FiscalDocumentLineMixinMethods, self).fields_view_get(
             view_id, view_type, toolbar, submenu)
 
         if view_type == 'form':
