8af24c2e5ebbfcdbf0681f8097c42cc0bad4cf4b
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-20 10:37:18 -0300

    [REF] Tests for all l10n_br_coa charts installed

--- l10n_br_account/tests/test_account_taxes.py
+++ l10n_br_account/tests/test_account_taxes.py
@@ -8,23 +8,29 @@ class TestAccountTaxes(TransactionCase):
     def setUp(self):
         super(TestAccountTaxes, self).setUp()
 
-        self.l10n_br_coa_simple = self.env.ref(
-            'l10n_br_coa_simple.l10n_br_coa_simple_chart_template')
         self.l10n_br_company = self.env['res.company'].create({
             'name': 'Empresa Teste do Plano de Contas Simplificado'})
 
         self.env.user.company_ids += self.l10n_br_company
         self.env.user.company_id = self.l10n_br_company
-        self.l10n_br_coa_simple.try_loading_for_current_company()
 
     def test_account_taxes(self):
         """Test if account taxes are related with fiscal taxes"""
-        account_taxes = self.env['account.tax'].search(
-            [('company_id', '=', self.l10n_br_company.id)])
+        l10n_br_coa_charts = self.env["account.chart.template"].search(
+            []).filtered(
+                lambda chart: chart.get_external_id().get(
+                    chart.id).split('.')[0].startswith("l10n_br_coa_")
+                if chart.get_external_id().get(chart.id) else False
+        )
+        for l10n_br_coa_chart in l10n_br_coa_charts:
+            l10n_br_coa_chart.try_loading_for_current_company()
+            account_taxes = self.env['account.tax'].search(
+                [('company_id', '=', self.l10n_br_company.id)])
 
-        is_fiscal_taxes = False
-        for tax in account_taxes:
-            if tax.tax_group_id.fiscal_tax_group_id and tax.fiscal_tax_ids:
-                is_fiscal_taxes = True
+            is_fiscal_taxes = False
+            for tax in account_taxes:
+                if tax.tax_group_id.fiscal_tax_group_id and \
+                        tax.fiscal_tax_ids:
+                    is_fiscal_taxes = True
 
-        assert is_fiscal_taxes, "There are not fiscal taxes related"
+            assert is_fiscal_taxes, "There are not fiscal taxes related"
