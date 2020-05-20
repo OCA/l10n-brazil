50ca7dc02e828699914154af9e930293e4a17335
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-20 13:40:46 -0300

    [ADD] Set accounts in template lines at chart loading

--- l10n_br_coa/models/account_chart_template.py
+++ l10n_br_coa/models/account_chart_template.py
@@ -101,5 +101,10 @@ class AccountChartTemplate(models.Model):
             for template in \
                     self.env['l10n_br_account.tax.template'].search([]):
                 template.create_account_tax_templates(self.id)
-        return super(AccountChartTemplate, self
-                     ).try_loading_for_current_company()
+        super(AccountChartTemplate, self).try_loading_for_current_company()
+        if self.env['ir.module.module'].search_count([
+            ('name', '=', 'l10n_br_account'),
+            ('state', '=', 'installed'),
+        ]):
+            from odoo.addons.l10n_br_account.hooks import set_accounts
+            set_accounts(self.env, self)
