84287ffaae34f9e17b636f856570f69e5f73747a
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-20 10:29:27 -0300

    [ADD] Update taxes fiscal_tax_ids in all l10n_br_coa charts

--- l10n_br_account/hooks.py
+++ l10n_br_account/hooks.py
@@ -35,13 +35,9 @@ def pre_init_hook(cr):
     )
 
 
-def post_init_hook(cr, registry):
-    """Relate fiscal taxes to account taxes."""
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    l10n_br_coa_simple_chart = env.ref(
-        "l10n_br_coa_simple.l10n_br_coa_simple_chart_template")
+def load_fiscal_taxes(env, l10n_br_coa_chart):
     companies = env["res.company"].search(
-        [("chart_template_id", "=", l10n_br_coa_simple_chart.id)]
+        [("chart_template_id", "=", l10n_br_coa_chart.id)]
     )
 
     for company in companies:
@@ -52,6 +48,21 @@ def post_init_hook(cr, registry):
                 tax_ref = tax.get_external_id().get(tax.id)
                 ref_module, ref_name = tax_ref.split(".")
                 ref_name = ref_name.replace(str(company.id) + "_", "")
+                template_source_ref = ".".join(["l10n_br_coa", ref_name])
+                template_source = env.ref(template_source_ref)
                 tax_source_ref = ".".join([ref_module, ref_name])
                 tax_template = env.ref(tax_source_ref)
-                tax.fiscal_tax_ids = tax_template.fiscal_tax_ids
+                tax.fiscal_tax_ids = tax_template.fiscal_tax_ids = \
+                    template_source.fiscal_tax_ids
+
+
+def post_init_hook(cr, registry):
+    """Relate fiscal taxes to account taxes."""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    l10n_br_coa_charts = env["account.chart.template"].search([]).filtered(
+        lambda chart: chart.get_external_id().get(
+            chart.id).split('.')[0].startswith("l10n_br_coa_")
+        if chart.get_external_id().get(chart.id) else False
+    )
+    for l10n_br_coa_chart in l10n_br_coa_charts:
+        load_fiscal_taxes(env, l10n_br_coa_chart)
