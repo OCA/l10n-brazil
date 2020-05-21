5d67344df782d6c01941c255054467fd3b74aca3
Renato Lima <renato.lima@akretion.com>
2020-05-21 17:47:04 -0300

    [FIX] stock picking type

--- l10n_br_stock_account/models/stock_picking_type.py
+++ l10n_br_stock_account/models/stock_picking_type.py
@@ -4,25 +4,11 @@
 from odoo import models, fields
 
 
-class ResCompany(models.Model):
-    _inherit = 'res.company'
+class StockPickingType(models.Model):
+    _inherit = 'stock.picking.type'
 
-    stock_fiscal_operation_id = fields.Many2one(
+    fiscal_operation_id = fields.Many2one(
         comodel_name='l10n_br_fiscal.operation',
-        string='Operação Padrão de Estoque',
-        domain="[('state', '=', 'approved')]"
-    )
-
-    stock_in_fiscal_operation_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.operation',
-        string='Operação Padrão de Entrada',
-        domain="[('state', '=', 'approved'), "
-               "('operation_type', 'in', ('in', 'all'))]",
-    )
-
-    stock_out_fiscal_operation_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.operation',
-        string='Operação Padrão Saída',
-        domain="[('state', '=', 'approved'), "
-               "('operation_type', 'in', ('out', 'all'))]",
+        string='Default Fiscal Operation',
+        domain="[('state', '=', 'approved')]",
     )
