4e0c6c8b9c1bf82334d041f07c9e05e68dc2e264
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-22 16:42:48 -0300

    [REF] Set lines in template lines

--- l10n_br_coa/models/account_chart_template.py
+++ l10n_br_coa/models/account_chart_template.py
@@ -8,91 +8,91 @@ class AccountChartTemplate(models.Model):
     _inherit = 'account.chart.template'
 
     icms_sale_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale ICMS Credit Account',
     )
     icms_sale_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale ICMS Debit Account',
     )
     icms_st_sale_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale ICMS ST Credit Account',
     )
     icms_st_sale_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale ICMS ST Debit Account',
     )
     ipi_sale_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale IPI Credit Account',
     )
     ipi_sale_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale IPI Debit Account',
     )
     pis_sale_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale PIS Credit Account',
     )
     pis_sale_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale PIS Debit Account',
     )
     cofins_sale_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale COFINS Credit Account',
     )
     cofins_sale_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale COFINS Debit Account',
     )
     sale_revenue_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale Revenue Credit Account',
     )
     sale_revenue_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale Revenue Debit Account',
     )
     resale_revenue_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Resale Revenue Credit Account',
     )
     resale_revenue_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Resale Revenue Debit Account',
     )
     sale_st_revenue_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale ST Revenue Credit Account',
     )
     sale_st_revenue_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale ST Revenue Debit Account',
     )
     resale_st_revenue_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Resale ST Revenue Credit Account',
     )
     resale_st_revenue_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Resale ST Revenue Debit Account',
     )
     sale_return_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale Return Credit Account',
     )
     sale_return_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Sale Return Debit Account',
     )
     purchase_return_credit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Purchase Return Credit Account',
     )
     purchase_return_debit_id = fields.Many2one(
-        comodel_name='account.account',
+        comodel_name='account.account.template',
         string='Purchase Return Debit Account',
     )
 
