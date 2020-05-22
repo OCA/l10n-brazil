5f755f33653184fa375d0c5a4ad3b51731af51f0
Luis Felipe Mileo <mileo@kmee.com.br>
2020-05-22 02:18:48 -0300

    [FIX] document_abstract removal
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    fixup! [NEW] Template at document and document line

--- l10n_br_account/models/fiscal_document.py
+++ l10n_br_account/models/fiscal_document.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
-# Copyright (C) 2019 - TODAY Raphaël Valyi - Akretion
 # Copyright (C) 2020 - TODAY Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
@@ -13,33 +11,6 @@ class FiscalDocument(models.Model):
     def _compute_move_template_ids(self):
         for record in self:
             record.move_template_ids = record.line_ids.mapped('move_template_id')
-
-    # the following fields collide with account.invoice fields so we use
-    # related field alias to be able to write them through account.invoice
-    fiscal_doc_partner_id = fields.Many2one(
-        related="partner_id",
-        string="Fiscal Doc Partner",
-        readonly=False)
-
-    fiscal_doc_date = fields.Datetime(
-        related="date",
-        string="Fiscal Doc Date",
-        readonly=False)
-
-    fiscal_doc_company_id = fields.Many2one(
-        related="company_id",
-        string="Fiscal Doc Company",
-        readonly=False)
-
-    fiscal_doc_currency_id = fields.Many2one(
-        related="currency_id",
-        string="Fiscal Doc Currency",
-        readonly=False)
-
-    fiscal_doc_state = fields.Selection(
-        related="state",
-        string="Fiscal Doc State",
-        readonly=False)
 
     account_id = fields.Many2one(
         comodel_name="account.account",
@@ -65,10 +36,6 @@ class FiscalDocument(models.Model):
         index=True,
         copy=False,
         help="Link to the automatically generated Journal Items.")
-
-    payment_term_id = fields.Many2one(
-        comodel_name='account.payment.term',
-    )
 
     def _generate_financial_account_moves(self, move_lines):
         self.financial_ids.generate_move(move_lines)
