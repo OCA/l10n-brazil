42ba0f332883cc0e0ce6415615e0ebe52b56299a
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-25 10:01:30 -0300

    [ADD] Remove old accounting to load new chart template

--- l10n_br_coa/__init__.py
+++ l10n_br_coa/__init__.py
@@ -11,6 +11,27 @@ def install_chart_template(env, chart_template_id, desired_tax_frameworks):
             if company_id.chart_template_id != chart_template_id:
                 env.user.company_ids |= company_id
                 env.user.company_id = company_id
+                if chart_template_id.existing_accounting(company_id):
+                    journal_ids = env['account.journal'].search([
+                        ('company_id', '=', company_id.id)
+                    ]).filtered(lambda journal: not journal.update_posted)
+                    journal_ids.write({'update_posted': True})
+                    model_to_check = ['account.bank.statement',
+                                      'account.invoice', 'account.move.line',
+                                      'account.payment']
+                    for model in model_to_check:
+                        records = env[model].search([
+                            ('company_id', '=', company_id.id)
+                        ])
+                        if model == 'account.bank.statement':
+                            records.mapped(
+                                'line_ids').button_cancel_reconciliation()
+                        elif model == 'account.invoice':
+                            records.action_cancel()
+                            records.action_invoice_draft()
+                            records.write({'move_name': False})
+                        records.unlink()
+                    journal_ids.write({'update_posted': False})
                 chart_template_id.load_for_current_company(15.0, 15.0)
 
         if env['ir.module.module'].search_count([
