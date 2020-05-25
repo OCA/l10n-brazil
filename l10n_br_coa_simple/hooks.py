4186a63ac1f7d77225e5500ef65f5f2aeee4d671
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-05-25 09:27:49 -0300

    [REF] Reuse common code

--- l10n_br_coa_simple/hooks.py
+++ l10n_br_coa_simple/hooks.py
@@ -2,6 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import SUPERUSER_ID, api
+from odoo.addons.l10n_br_coa import install_chart_template
 
 
 def post_init_hook(cr, registry):
@@ -9,22 +10,4 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     chart_template_id = env.ref(
         'l10n_br_coa_simple.l10n_br_coa_simple_chart_template')
-    original_company_id = env.user.company_id
-    for company_id in env["res.company"].search([]):
-        country_code = company_id.country_id.code
-        if country_code and country_code.upper() == "BR" and \
-                company_id.tax_framework != '3':
-            if company_id.chart_template_id != chart_template_id:
-                env.user.company_ids |= company_id
-                env.user.company_id = company_id
-                chart_template_id.load_for_current_company(15.0, 15.0)
-
-        if env['ir.module.module'].search_count([
-            ('name', '=', 'l10n_br_account'),
-            ('state', '=', 'installed'),
-        ]):
-            from odoo.addons.l10n_br_account.hooks import load_fiscal_taxes
-            load_fiscal_taxes(env, env.ref(
-                'l10n_br_coa_simple.l10n_br_coa_simple_chart_template'))
-
-    env.user.company_id = original_company_id
+    install_chart_template(env, chart_template_id, ['1', '2'])
