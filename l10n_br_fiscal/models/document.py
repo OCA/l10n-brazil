cfad56fa1103f5ee7b6cec62cea1d69075d1cdfd
Renato Lima <renato.lima@akretion.com>
2020-05-25 13:36:07 -0300

    [FIX] Remove company from fiscal document dummy

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -251,7 +251,6 @@ class Document(models.Model):
     company_id = fields.Many2one(
         comodel_name='res.company',
         string='Company',
-        required=True,
         default=lambda self: self.env['res.company']._company_default_get(
             'l10n_br_fiscal.document'),
     )
