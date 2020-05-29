f18c5fe1a2e5228f779a2a52979b19da2cad0ef5
Renato Lima <renato.lima@akretion.com>
2020-05-29 14:47:49 -0300

    Update l10n_br_sale_stock

--- l10n_br_sale_stock/models/procurement.py
+++ l10n_br_sale_stock/models/procurement.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2014  Renato Lima - Akretion                                  #
+# Copyright (C) 2014  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, api
