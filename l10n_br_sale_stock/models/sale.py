e18b58d8a425e5816ddb6122a70d8d17354a8310
Renato Lima <renato.lima@akretion.com>
2020-05-29 14:47:49 -0300

    Update l10n_br_sale_stock

--- l10n_br_sale_stock/models/sale.py
+++ l10n_br_sale_stock/models/sale.py
@@ -1,29 +1,15 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013  Raphaël Valyi - Akretion                                #
-# Copyright (C) 2014  Renato Lima - Akretion                                  #
+# Copyright (C) 2013  Raphaël Valyi - Akretion
+# Copyright (C) 2014  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, api
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.model
-    def _prepare_invoice(self, order, lines):
-        """Prepare the dict of values to create the new invoice for a
-           sale order. This method may be overridden to implement custom
-           invoice generation (making sure to call super() to establish
-           a clean extension chain).
-
-           :param browse_record order: sale.order record to invoice
-           :param list(int) line: list of invoice line IDs that must be
-                                  attached to the invoice
-           :return: dict of value to create() the invoice
-        """
-        result = super(SaleOrder, self)._prepare_invoice(order, lines)
-
-        if order.incoterm:
-            result['incoterm'] = order.incoterm.id
-
-        return result
+    @api.multi
+    def _prepare_procurement_values(self, group_id=False):
+        values = super()._prepare_procurement_values(group_id)
+        # TODO copy fiscal operation
+        return values
