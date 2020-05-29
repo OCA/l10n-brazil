d2e4bd9825043c91e2a97ff82d817f5ef2304381
Renato Lima <renato.lima@akretion.com>
2020-05-29 17:09:31 -0300

    [MIGR] models

--- l10n_br_sale_stock/models/__init__.py
+++ l10n_br_sale_stock/models/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2015  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import procurement
+# from . import procurement
 from . import sale
