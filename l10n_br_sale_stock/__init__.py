ce231527f1ad4ec3c1f8f663eb8b839968172f9e
Renato Lima <renato.lima@akretion.com>
2020-05-29 14:47:49 -0300

    Update l10n_br_sale_stock

--- l10n_br_sale_stock/__init__.py
+++ l10n_br_sale_stock/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013  Raphaël Valyi - Akretion                                #
-# Copyright (C) 2013  Renato Lima - Akretion                                  #
+# Copyright (C) 2013  Raphaël Valyi - Akretion
+# Copyright (C) 2013  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
