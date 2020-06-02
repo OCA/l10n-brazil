120c63040862bdbc13f12cb626cad1bd04979157
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-06-02 15:50:20 -0300

    [FIX] Class name

--- l10n_br_nfse/models/document_line.py
+++ l10n_br_nfse/models/document_line.py
@@ -5,7 +5,7 @@ from lxml import etree
 from odoo import api, fields, models
 
 
-class Document(models.Model):
+class DocumentLine(models.Model):
     _inherit = 'l10n_br_fiscal.document.line'
 
     fiscal_deductions_value = fields.Monetary(
@@ -22,7 +22,7 @@ class Document(models.Model):
 
     @api.onchange("product_id")
     def _onchange_product_id_fiscal(self):
-        super(Document, self)._onchange_product_id_fiscal()
+        super(DocumentLine, self)._onchange_product_id_fiscal()
         if self.product_id and self.product_id.fiscal_deductions_value:
             self.fiscal_deductions_value = \
                 self.product_id.fiscal_deductions_value
@@ -32,14 +32,14 @@ class Document(models.Model):
     def _compute_taxes(self, taxes, cst=None):
         discount_value = self.discount_value
         self.discount_value += self.fiscal_deductions_value
-        res = super(Document, self)._compute_taxes(taxes, cst)
+        res = super(DocumentLine, self)._compute_taxes(taxes, cst)
         self.discount_value = discount_value
         return res
 
     @api.model
     def fields_view_get(self, view_id=None, view_type="form",
                         toolbar=False, submenu=False):
-        model_view = super(Document, self).fields_view_get(
+        model_view = super(DocumentLine, self).fields_view_get(
             view_id, view_type, toolbar, submenu)
 
         if view_type == 'form':
