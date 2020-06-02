1b4923dec288743dcec8d5e9913dde9a7f3a06e7
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-06-02 15:08:28 -0300

    [ADD] Field other_retentions_value

--- l10n_br_nfse/models/document_line.py
+++ l10n_br_nfse/models/document_line.py
@@ -1,5 +1,7 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from lxml import etree
+
 from odoo import api, fields, models
 
 
@@ -8,6 +10,10 @@ class Document(models.Model):
 
     fiscal_deductions_value = fields.Monetary(
         string='Fiscal Deductions',
+        default=0.00,
+    )
+    other_retentions_value = fields.Monetary(
+        string='Other Retentions',
         default=0.00,
     )
 
@@ -24,3 +30,23 @@ class Document(models.Model):
         res = super(Document, self)._compute_taxes(taxes, cst)
         self.discount_value = discount_value
         return res
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type="form",
+                        toolbar=False, submenu=False):
+        model_view = super(Document, self).fields_view_get(
+            view_id, view_type, toolbar, submenu)
+
+        if view_type == 'form':
+            try:
+                doc = etree.fromstring(model_view.get('arch'))
+                field = doc.xpath("//field[@name='issqn_wh_value']")[0]
+                parent = field.getparent()
+                parent.insert(parent.index(field)+1, etree.XML(
+                    '<field name="other_retentions_value"/>'
+                ))
+
+                model_view["arch"] = etree.tostring(doc, encoding='unicode')
+            except Exception:
+                return model_view
+        return model_view
