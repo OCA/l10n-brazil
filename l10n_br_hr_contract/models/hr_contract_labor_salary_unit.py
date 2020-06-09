9e0c40df29d7798eaf3121504c14e882ad340f12
Hendrix Costa <hendrixcosta@gmail.com>
2020-06-09 20:35:48 -0300

    [REF] Model salary unit

--- l10n_br_hr_contract/models/hr_contract_labor_salary_unit.py
+++ l10n_br_hr_contract/models/hr_contract_labor_salary_unit.py
@@ -6,32 +6,20 @@ from odoo import api, fields, models
 
 class HrContractSalaryUnit(models.Model):
     _name = "hr.contract.salary.unit"
-    _description = "Unidade de pagamento da parte fixa da remuneração"
+    _description = "Unidade de pagamento da parte fixa da remuneração " \
+                   "e-Social - S-2200 undSalFixo"
 
-    name = fields.Char(string="Salary unit", required=True)
+    name = fields.Char(
+        string="Salary unit",
+        required=True,
+    )
 
-    code = fields.Char(string="Code", required=True)
+    code = fields.Char(
+        string="Code",
+        required=True,
+    )
 
     @api.multi
     def name_get(self):
-        result = []
-        for record in self:
-            name = record["name"]
-            if name == "Monthly":
-                name = "Por mês"
-            elif name == "Biweekly":
-                name = "Por 15 dias"
-            elif name == "Weekly":
-                name = "Por semana"
-            elif name == "Daily":
-                name = "Por dia"
-            elif name == "Hourly":
-                name = "Por hora"
-            elif name == "Task":
-                name = "Por tarefa"
-            elif name == "Others":
-                name = "Outros"
-
-            name = record["code"] + " - " + name
-            result.append((record["id"], name))
-        return result
+        return [(record.id, "{} - {} ".format(record.code, record.name))
+                for record in self]
