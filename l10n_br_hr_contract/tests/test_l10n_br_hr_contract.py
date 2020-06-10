698de074a625fbc0bc6d7ae6ecea1c2d0a3a5cdd
Hendrix Costa <hendrixcosta@gmail.com>
2020-06-10 00:37:43 -0300

    [FIX] PEP8

--- l10n_br_hr_contract/tests/test_l10n_br_hr_contract.py
+++ l10n_br_hr_contract/tests/test_l10n_br_hr_contract.py
@@ -15,7 +15,8 @@ class TestL10nBrContract(TransactionCase):
                 "notes": "Demo notes",
                 "trial_date_end": "2016-03-01",
                 "date_start": "2016-03-02",
-                # "working_hours": self.env["resource.calendar"].search([])[0].id,
+                # "working_hours":
+                #     self.env["resource.calendar"].search([])[0].id,
                 "admission_type_id": self.env["hr.contract.admission.type"]
                 .search([])[0]
                 .code,
@@ -25,19 +26,20 @@ class TestL10nBrContract(TransactionCase):
                 "labor_regime_id": self.env["hr.contract.labor.regime"]
                 .search([])[0]
                 .code,
-                "salary_unit": self.env["hr.contract.salary.unit"].search([])[0].code,
+                "salary_unit":
+                    self.env["hr.contract.salary.unit"].search([])[0].code,
                 "union_cnpj": "00.874.955/0001-78",
                 "union_entity_code": "DU",
                 "discount_union_contribution": True,
                 "monthly_hours": 180,
                 "weekly_hours": 45,
                 "resignation_date": "2016-06-20",
-                "resignation_cause_id": self.env["hr.contract.resignation.cause"]
-                .search([])[0]
-                .code,
-                "notice_of_termination_id": self.env["hr.contract.notice.termination"]
-                .search([])[0]
-                .id,
+                "resignation_cause_id":
+                    self.env["hr.contract.resignation.cause"].
+                    search([])[0].code,
+                "notice_of_termination_id":
+                    self.env["hr.contract.notice.termination"].
+                    search([])[0].id,
                 "notice_of_termination_date": "2016-06-20",
                 "by_death": "143710 01 55 2011 4 08192 439 3151559-28",
             }
@@ -47,7 +49,8 @@ class TestL10nBrContract(TransactionCase):
 
     def test_admission_type(self):
         admission_type = (
-            self.env["hr.contract.admission.type"].search([])[0].name_get()[0][1]
+            self.env["hr.contract.admission.type"].
+            search([])[0].name_get()[0][1]
         )
         self.assertEqual(
             admission_type,
@@ -58,18 +61,19 @@ class TestL10nBrContract(TransactionCase):
 
     def test_labor_regime(self):
         labor_regime = (
-            self.env["hr.contract.labor.regime"].search([])[0].name_get()[0][1]
+            self.env["hr.contract.labor.regime"].
+            search([])[0].name_get()[0][1]
         )
         self.assertEqual(
             labor_regime,
             "CLT - Consolidação das Leis de " "Trabalho",
-            "The labor regime name by name_get is not valid," " expected 'code - name'",
+            "The labor regime name by name_get is not valid,"
+            " expected 'code - name'",
         )
 
     def test_labor_bond_type(self):
-        labor_bond = (
-            self.env["hr.contract.labor.bond.type"].search([])[0].name_get()[0][1]
-        )
+        labor_bond_obj = self.env["hr.contract.labor.bond.type"]
+        labor_bond = labor_bond_obj.search([])[0].name_get()[0][1]
         self.assertEqual(
             labor_bond,
             "10 - Trabalhador urbano vinculado a"
@@ -81,17 +85,18 @@ class TestL10nBrContract(TransactionCase):
         )
 
     def test_salary_unit(self):
-        salary_unit = self.env["hr.contract.salary.unit"].search([])[0].name_get()[0][1]
+        salary_unit = \
+            self.env["hr.contract.salary.unit"].search([])[0].name_get()[0][1]
         self.assertEqual(
             salary_unit,
-            "1 - Por hora",
-            "The salary unit name by name_get is not valid," " expected 'code - name'",
+            "1 - Hourly ",
+            "The salary unit name by name_get is not valid,"
+            " expected 'code - name'",
         )
 
     def test_resignation_cause(self):
-        resignation_cause = (
-            self.env["hr.contract.resignation.cause"].search([])[0].name_get()[0][1]
-        )
+        res_cause_obj = self.env["hr.contract.resignation.cause"]
+        resignation_cause = res_cause_obj.search([])[0].name_get()[0][1]
         self.assertEqual(
             resignation_cause,
             "10 - Rescisão de contrato de"
