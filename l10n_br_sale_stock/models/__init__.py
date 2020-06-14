e93fc39cd33a4dfe5c3bc47baf8500bea72029a6
Renato Lima <renato.lima@akretion.com>
2020-06-14 19:22:12 -0300

    [MIGR] l10n_br_sale_stock cleanup

--- l10n_br_sale_stock/models/__init__.py
+++ l10n_br_sale_stock/models/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2015  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-# from . import procurement
-from . import sale
+from . import sale_order_line
