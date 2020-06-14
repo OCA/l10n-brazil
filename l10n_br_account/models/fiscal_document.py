77f9a9e2f6de5599824c8dbf8e7546db1d29b7fd
Renato Lima <renato.lima@akretion.com>
2020-06-14 14:30:50 -0300

    [REF] check unlink fiscal document related a invoice

---
+++ l10n_br_account/models/fiscal_document.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class FiscalDocument(models.Model):
+    _inherit = 'l10n_br_fiscal.document'
+
+    @api.multi
+    def unlink(self):
+        invoices = self.env['account.invoice'].search(
+            [('fiscal_document_id', 'in', self.ids)])
+        invoices.unlink()
+        return super().unlink()
