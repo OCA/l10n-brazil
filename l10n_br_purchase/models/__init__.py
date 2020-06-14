4780110ca155e2e79641e39feb33f5697fb634bd
Renato Lima <renato.lima@akretion.com>
2020-06-14 00:32:44 -0300

    Rename files

--- l10n_br_purchase/models/__init__.py
+++ l10n_br_purchase/models/__init__.py
@@ -2,6 +2,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import res_company
-from . import purchase
-from . import purchase_line
+from . import purchase_order
+from . import purchase_order_line
 from . import account_invoice
