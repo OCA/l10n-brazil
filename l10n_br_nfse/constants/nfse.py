83941367c0ec5a3eccac5a2f4892ef10bbed0a46
Luis Malta <luis.malta@kmee.com.br>
2020-06-16 11:19:58 -0300

    [ADD] NFSe constants

---
+++ l10n_br_nfse/constants/nfse.py
@@ -0,0 +1,7 @@
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+NFSE_ENVIRONMENTS = [("1", "Produção"), ("2", "Homologação")]
+
+
+NFSE_ENVIRONMENT_DEFAULT = "2"
