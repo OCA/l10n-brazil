40d871ac7b43298d21a6d36d29b4bfa3019f2ac8
Magno Costa <magno.costa@akretion.com.br>
2020-06-17 12:54:27 -0300

    [12.0][MIG][REF][WIP] Join modules Boleto and CNAB.

--- l10n_br_account_payment_boleto_brcobranca/__manifest__.py
+++ l10n_br_account_payment_boleto_brcobranca/__manifest__.py
@@ -4,9 +4,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Br Account Payment Boleto Brcobranca',
+    'name': 'L10n Br Account Payment Brcobranca',
     'description': """
-        Imprime boletos usando a Gem brcobranca do Boletosimples""",
+        Gera Boletos, CNAB de Remessa e Retorno usando
+         a Gem brcobranca do Boletosimples""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Akretion',
@@ -16,7 +17,13 @@
     ],
     'data': [
         'views/account_invoice_view.xml',
+        'views/payment_mode.xml',
+        'views/l10n_br_cnab_retorno_view.xml',
+        'data/cnab_data.xml',
     ],
     'demo': [
     ],
+    'test': [
+        'tests/invoice_create.yml'
+    ]
 }
