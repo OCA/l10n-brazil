2affdf830c96521b274e47f86d583560c76ddcd9
Luis Malta <luis.malta@kmee.com.br>
2020-06-17 10:50:48 -0300

    [REF] Rename module l10n_br_coa_generic

--- l10n_br_coa_generic/tests/__init__.py
+++ l10n_br_coa_generic/tests/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2020 Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import test_l10n_br_cfc_coa
+from . import test_l10n_br_cfc_generic
--- l10n_br_coa_generic/tests/test_l10n_br_cfc_generic.py
+++ l10n_br_coa_generic/tests/test_l10n_br_cfc_generic.py
@@ -4,21 +4,21 @@
 from odoo.tests.common import TransactionCase
 
 
-class L10nBrCfcCOA(TransactionCase):
+class L10nBrCfcGeneric(TransactionCase):
     def setUp(self):
-        super(L10nBrCfcCOA, self).setUp()
+        super(L10nBrCfcGeneric, self).setUp()
 
-        self.l10n_br_cfc_coa = self.env.ref(
-            'l10n_br_coa_cfc.l10n_br_cfc_chart_template')
+        self.l10n_br_cfc_generic = self.env.ref(
+            'l10n_br_coa_generic.l10n_br_cfc_chart_template')
         self.l10n_br_company = self.env['res.company'].create({
             'name': 'Empresa Teste do Plano de Contas CFC'
         })
 
-    def test_l10n_br_cfc_coa(self):
+    def test_l10n_br_cfc_generic(self):
         """Test to install the chart of accounts template in a new company"""
         self.env.user.company_ids += self.l10n_br_company
         self.env.user.company_id = self.l10n_br_company
-        self.l10n_br_cfc_coa.try_loading_for_current_company()
+        self.l10n_br_cfc_generic.try_loading_for_current_company()
 
         self.assertEquals(
-            self.l10n_br_cfc_coa, self.l10n_br_company.chart_template_id)
+            self.l10n_br_cfc_generic, self.l10n_br_company.chart_template_id)
