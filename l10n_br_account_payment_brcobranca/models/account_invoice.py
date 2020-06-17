4a4c1d3ab0d20e7fa8bdc01b7b782be53a6714b0
Magno Costa <magno.costa@akretion.com.br>
2020-06-17 12:55:24 -0300

    [12.0][MIG][REF] Join modules Boleto and CNAB.

---
+++ l10n_br_account_payment_brcobranca/models/account_invoice.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Akretion
+# @author Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from __future__ import with_statement
+
+import base64
+import requests
+import json
+import tempfile
+
+from odoo import models, api, fields, _
+from odoo.exceptions import Warning as UserError
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    file_boleto_pdf_id = fields.Many2one(
+        comodel_name="ir.attachment",
+        string="Boleto PDF",
+        ondelete="restrict",
+        copy=False)
+
+    def gera_boleto_pdf(self):
+        file_pdf = self.file_boleto_pdf_id
+        self.file_boleto_pdf_id = False
+        file_pdf.unlink()
+
+        receivable_ids = self.mapped('move_line_receivable_ids')
+
+        boleto_list = receivable_ids.send_payment()
+        print('BOLETO LIST', boleto_list)
+        if not boleto_list:
+            raise UserError(
+                'Error !', ('Não é possível gerar os boletos\n'
+                            'Certifique-se que a fatura esteja confirmada e o '
+                            'forma de pagamento seja duplicatas'))
+
+        boletos = [b.boleto_cnab_api_data for b in boleto_list]
+
+        content = json.dumps(boletos)
+        f = open(tempfile.mktemp(), 'w')
+        f.write(content)
+        f.close()
+        files = {'data': open(f.name, 'rb')}
+
+        # TODO - Name of service should be a parameter ?
+        #  For docky users check the name of service
+        #  defined in dev.docker-compose.yml
+        res = requests.post("http://brcobranca:9292/api/boleto/multi",
+                            data={'type': 'pdf'}, files=files)
+        if str(res.status_code)[0] == '2':
+            pdf_string = res.content
+        else:
+           raise UserError(res.text.encode('utf-8'))
+
+        # TODO - Name File
+        file_name = 'boleto-teste.pdf'
+
+        self.file_pdf_id = self.env['ir.attachment'].create(
+            {
+                "name": file_name,
+                "datas_fname": file_name,
+                "res_model": self._name,
+                "res_id": self.id,
+                "datas": base64.b64encode(pdf_string),
+                "mimetype": "application/pdf",
+                "type": "binary",
+            }
+        )
+
+    def _target_new_tab(self, attachment_id):
+        if attachment_id:
+            return {
+                'type' : 'ir.actions.act_url',
+                'url': '/web/content/{id}/{nome}'.format(
+                    id=attachment_id.id,
+                    nome=attachment_id.name),
+                'target': 'new',
+                }
+
+    def view_boleto_pdf(self):
+        if not self.file_boleto_pdf_id:
+            self.gera_boleto_pdf()
+        return self._target_new_tab(self.file_boleto_pdf_id)
---
+++ l10n_br_account_payment_brcobranca/models/l10n_br_cnab.py
@@ -0,0 +1,352 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion - Renato Lima <renato.lima@akretion.com.br>
+# Copyright 2017 Akretion - Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import base64
+import requests
+import json
+import datetime
+
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning as UserError
+
+DICT_OCORRENCIAS_BRADESCO = {
+    '02': u'Entrada Confirmada (verificar motivo na posição 319 a 328)',
+    '03': u'Entrada Rejeitada ( verificar motivo na posição 319 a 328)',
+    '06': u'Liquidação normal (sem motivo)',
+    '09': u'Baixado Automat. via Arquivo (verificar motivo posição 319 a 328)',
+    '10': u'Baixado conforme instruções da Agência('
+          u'verificar motivo pos.319 a 328)',
+    '11': u'Em Ser - Arquivo de Títulos pendentes (sem motivo)',
+    '12': u'Abatimento Concedido (sem motivo)',
+    '13': u'Abatimento Cancelado (sem motivo)',
+    '14': u'Vencimento Alterado (sem motivo)',
+    '15': u'Liquidação em Cartório (sem motivo)',
+    '16': u'Título Pago em Cheque – Vinculado',
+    '17': u'Liquidação após baixa ou Título não registrado (sem motivo)',
+    '18': u'Acerto de Depositária (sem motivo)',
+    '19': u'Confirmação Receb. Inst. de Protesto '
+          u'(verificar motivo pos.295 a 295)',
+    '20': u'Confirmação Recebimento Instrução Sustação de'
+          u' Protesto (sem motivo)',
+    '21': u'Acerto do Controle do Participante (sem motivo)',
+    '22': u'Título Com Pagamento Cancelado',
+    '23': u'Entrada do Título em Cartório (sem motivo)',
+    '24': u'Entrada rejeitada por CEP Irregular'
+          u' (verificar motivo pos.319 a 328)',
+    '25': u'Confirmação Receb.Inst.de Protesto Falimentar'
+          u' (verificar pos.295 a 295)',
+    '27': u'Baixa Rejeitada (verificar motivo posição 319 a 328)',
+    '28': u'Débito de tarifas/custas (verificar motivo na posição 319 a 328)',
+    '29': u'Ocorrências do Pagador (NOVO)',
+    '30': u'Alteração de Outros Dados Rejeitados '
+          u'(verificar motivo pos.319 a 328)',
+    '32': u'Instrução Rejeitada (verificar motivo posição 319 a 328)',
+    '33': u'Confirmação Pedido Alteração Outros Dados (sem motivo)',
+    '34': u'Retirado de Cartório e Manutenção Carteira (sem motivo)',
+    '35': u'Desagendamento do débito automático '
+          u'(verificar motivos pos. 319 a 328)',
+    '40': u'Estorno de pagamento (NOVO)',
+    '55': u'Sustado judicial (NOVO)',
+    '68': u'Acerto dos dados do rateio de Crédito (verificar motivo posição de'
+          u' status do registro tipo 3)',
+    '69': u'Cancelamento dos dados do rateio (verificar motivo posição de'
+          u' status do registro tipo 3)',
+    '073': u'Confirmação Receb. Pedido de Negativação (NOVO)',
+    '074': u'Confir Pedido de Excl de Negat (com ou sem baixa) (NOVO)',
+    '00': u'Nota: Para as ocorrências sem motivos, as posições serão'
+          u' informadas com Zeros.',
+}
+
+DICT_OCORRENCIAS_ITAU = {
+    '02': u'ENTRADA CONFIRMADA COM POSSIBILIDADE DE MENSAGEM'
+          u' (NOTA 20 – TABELA 10)',
+    '03': u'ENTRADA REJEITADA (NOTA 20 – TABELA 1)',
+    '04': u'ALTERAÇÃO DE DADOS – NOVA ENTRADA OU ALTERAÇÃO/EXCLUSÃO'
+          u' DE DADOS ACATADA',
+    '05': u'ALTERAÇÃO DE DADOS – BAIXA',
+    '06': u'LIQUIDAÇÃO NORMAL',
+    '07': u'LIQUIDAÇÃO PARCIAL – COBRANÇA INTELIGENTE (B2B)',
+    '08': u'LIQUIDAÇÃO EM CARTÓRIO',
+    '09': u'BAIXA SIMPLES',
+    '10': u'BAIXA POR TER SIDO LIQUIDADO',
+    '11': u'EM SER (SÓ NO RETORNO MENSAL)',
+    '12': u'ABATIMENTO CONCEDIDO',
+    '13': u'ABATIMENTO CANCELADO',
+    '14': u'VENCIMENTO ALTERADO',
+    '15': u'BAIXAS REJEITADAS (NOTA 20 – TABELA 4)',
+    '16': u'INSTRUÇÕES REJEITADAS (NOTA 20 – TABELA 3)',
+    '17': u'ALTERAÇÃO/EXCLUSÃO DE DADOS REJEITADOS (NOTA 20 – TABELA 2)',
+    '18': u'COBRANÇA CONTRATUAL – INSTRUÇÕES/ALTERAÇÕES'
+          u' REJEITADAS/PENDENTES (NOTA 20 – TABELA 5)',
+    '19': u'CONFIRMA RECEBIMENTO DE INSTRUÇÃO DE PROTESTO',
+    '20': u'CONFIRMA RECEBIMENTO DE INSTRUÇÃO DE SUSTAÇÃO'
+          u' DE PROTESTO /TARIFA',
+    '21': u'CONFIRMA RECEBIMENTO DE INSTRUÇÃO DE NÃO PROTESTAR',
+    '23': u'TÍTULO ENVIADO A CARTÓRIO/TARIFA',
+    '24': u'INSTRUÇÃO DE PROTESTO REJEITADA / SUSTADA / PENDENTE'
+          u' (NOTA 20 – TABELA 7)',
+    '25': u'ALEGAÇÕES DO PAGADOR (NOTA 20 – TABELA 6)',
+    '26': u'TARIFA DE AVISO DE COBRANÇA',
+    '27': u'TARIFA DE EXTRATO POSIÇÃO (B40X)',
+    '28': u'TARIFA DE RELAÇÃO DAS LIQUIDAÇÕES',
+    '29': u'TARIFA DE MANUTENÇÃO DE TÍTULOS VENCIDOS',
+    '30': u'DÉBITO MENSAL DE TARIFAS (PARA ENTRADAS E BAIXAS)',
+    '32': u'BAIXA POR TER SIDO PROTESTADO',
+    '33': u'CUSTAS DE PROTESTO',
+    '34': u'CUSTAS DE SUSTAÇÃO',
+    '35': u'CUSTAS DE CARTÓRIO DISTRIBUIDOR',
+    '36': u'CUSTAS DE EDITAL',
+    '37': u'TARIFA DE EMISSÃO DE BOLETO/TARIFA DE ENVIO DE DUPLICATA',
+    '38': u'TARIFA DE INSTRUÇÃO',
+    '39': u'TARIFA DE OCORRÊNCIAS',
+    '40': u'TARIFA MENSAL DE EMISSÃO DE BOLETO/TARIFA MENSAL'
+          u' DE ENVIO DE DUPLICATA',
+    '41': u'DÉBITO MENSAL DE TARIFAS – EXTRATO DE POSIÇÃO (B4EP/B4OX)',
+    '42': u'DÉBITO MENSAL DE TARIFAS – OUTRAS INSTRUÇÕES',
+    '43': u'DÉBITO MENSAL DE TARIFAS – MANUTENÇÃO DE TÍTULOS VENCIDOS',
+    '44': u'DÉBITO MENSAL DE TARIFAS – OUTRAS OCORRÊNCIAS',
+    '45': u'DÉBITO MENSAL DE TARIFAS – PROTESTO',
+    '46': u'DÉBITO MENSAL DE TARIFAS – SUSTAÇÃO DE PROTESTO',
+    '47': u'BAIXA COM TRANSFERÊNCIA PARA DESCONTO',
+    '48': u'CUSTAS DE SUSTAÇÃO JUDICIAL',
+    '51': u'TARIFA MENSAL REF A ENTRADAS BANCOS CORRESPONDENTES NA CARTEIRA',
+    '52': u'TARIFA MENSAL BAIXAS NA CARTEIRA',
+    '53': u'TARIFA MENSAL BAIXAS EM BANCOS CORRESPONDENTES NA CARTEIRA',
+    '54': u'TARIFA MENSAL DE LIQUIDAÇÕES NA CARTEIRA',
+    '55': u'TARIFA MENSAL DE LIQUIDAÇÕES EM BANCOS'
+          u' CORRESPONDENTES NA CARTEIRA',
+    '56': u'CUSTAS DE IRREGULARIDADE',
+    '57': u'INSTRUÇÃO CANCELADA (NOTA 20 – TABELA 8)',
+    '59': u'BAIXA POR CRÉDITO EM C/C ATRAVÉS DO SISPAG',
+    '60': u'ENTRADA REJEITADA CARNÊ (NOTA 20 – TABELA 1)',
+    '61': u'TARIFA EMISSÃO AVISO DE MOVIMENTAÇÃO DE TÍTULOS (2154)',
+    '62': u'DÉBITO MENSAL DE TARIFA – AVISO DE MOVIMENTAÇÃO DE TÍTULOS (2154)',
+    '63': u'TÍTULO SUSTADO JUDICIALMENTE',
+    '64': u'ENTRADA CONFIRMADA COM RATEIO DE CRÉDITO',
+    '65': u'PAGAMENTO COM CHEQUE – AGUARDANDO COMPENSAÇÃO',
+    '69': u'CHEQUE DEVOLVIDO (NOTA 20 – TABELA 9)',
+    '71': u'ENTRADA REGISTRADA, AGUARDANDO AVALIAÇÃO',
+    '72': u'BAIXA POR CRÉDITO EM C/C ATRAVÉS DO SISPAG'
+          u' SEM TÍTULO CORRESPONDENTE',
+    '73': u'CONFIRMAÇÃO DE ENTRADA NA COBRANÇA SIMPLES –'
+          u' ENTRADA NÃO ACEITA NA COBRANÇA CONTRATUAL',
+    '74': u'INSTRUÇÃO DE NEGATIVAÇÃO EXPRESSA REJEITADA (NOTA 20 – TABELA 11)',
+    '75': u'CONFIRMAÇÃO DE RECEBIMENTO DE INSTRUÇÃO DE ENTRADA'
+          u' EM NEGATIVAÇÃO EXPRESSA',
+    '76': u'CHEQUE COMPENSADO',
+    '77': u'CONFIRMAÇÃO DE RECEBIMENTO DE INSTRUÇÃO DE EXCLUSÃO DE'
+          u' ENTRADA EM NEGATIVAÇÃO EXPRESSA',
+    '78': u'CONFIRMAÇÃO DE RECEBIMENTO DE INSTRUÇÃO DE CANCELAMENTO DE'
+          u' NEGATIVAÇÃO EXPRESSA',
+    '79': u'NEGATIVAÇÃO EXPRESSA INFORMACIONAL (NOTA 20 – TABELA 12)',
+    '80': u'CONFIRMAÇÃO DE ENTRADA EM NEGATIVAÇÃO EXPRESSA – TARIFA',
+    '82': u'CONFIRMAÇÃO DO CANCELAMENTO DE NEGATIVAÇÃO EXPRESSA – TARIFA',
+    '83': u'CONFIRMAÇÃO DE EXCLUSÃO DE ENTRADA EM NEGATIVAÇÃO'
+          u' EXPRESSA POR LIQUIDAÇÃO – TARIFA',
+    '85': u'TARIFA POR BOLETO (ATÉ 03 ENVIOS) COBRANÇA ATIVA ELETRÔNICA',
+    '86': u'TARIFA EMAIL COBRANÇA ATIVA ELETRÔNICA',
+    '87': u'TARIFA SMS COBRANÇA ATIVA ELETRÔNICA',
+    '88': u'TARIFA MENSAL POR BOLETO (ATÉ 03 ENVIOS)'
+          u' COBRANÇA ATIVA ELETRÔNICA',
+    '89': u'TARIFA MENSAL EMAIL COBRANÇA ATIVA ELETRÔNICA',
+    '90': u'TARIFA MENSAL SMS COBRANÇA ATIVA ELETRÔNICA',
+    '91': u'TARIFA MENSAL DE EXCLUSÃO DE ENTRADA DE NEGATIVAÇÃO EXPRESSA',
+    '92': u'TARIFA MENSAL DE CANCELAMENTO DE NEGATIVAÇÃO EXPRESSA',
+    '93': u'TARIFA MENSAL DE EXCLUSÃO DE NEGATIVAÇÃO EXPRESSA POR LIQUIDAÇÃO',
+}
+
+
+class L10nBrHrCnab(models.Model):
+    _inherit = "l10n_br.cnab"
+
+    account_journal = fields.Many2one(
+        'account.journal', 'Journal used in Bank Statement',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        help='Journal used in create of Bank Statement.'
+    )
+    cnab_type = fields.Selection(
+        [('cnab400', u'CNAB 400')], 'CNAB Type File',
+        default='cnab400',
+        readonly=True,
+        states={'draft': [('readonly', False)]}
+    )
+    bank = fields.Selection(
+        [('bradesco', u'Bradesco'), ('itau', u'Itaú')],
+        string='Bank', states={'draft': [('readonly', False)]}
+    )
+
+    @api.multi
+    def processar_arquivo_retorno(self):
+
+        files = {'data': base64.b64decode(self.arquivo_retorno)}
+        res = requests.post(
+            "http://boleto_cnab_api:9292/api/retorno",
+            data={
+                'type': self.cnab_type,
+                'bank': self.bank,
+            }, files=files)
+
+        if res.status_code != 201:
+            raise UserError(res.text)
+
+        string_result = res.json()
+        data = json.loads(string_result)
+
+        self.processar_arquivo_retorno_cnab400(data)
+
+    @api.multi
+    def processar_arquivo_retorno_cnab400(self, data):
+
+        lote_id = self.env['l10n.br.cnab.lote'].create({'cnab_id': self.id})
+
+        quantidade_registros = 0
+        total_valores = 0
+        balance_end_real = 0.0
+        line_statement_vals = []
+
+        for dict_line in data:
+            if int(dict_line['codigo_registro']) != 1:
+                # Bradesco
+                # Existe o codigo de registro 9 que eh um totalizador
+                # porem os campos estao colocados em outras posicoes
+                # que nao estao mapeadas no BRCobranca
+                # Itau
+                # 9 - Registro Trailer do Arquivo
+                # 4 e 5 - Registro de Detalhe (Opcional)
+                continue
+
+            quantidade_registros += 1
+            account_move_line = self.env['account.move.line'].search(
+                [('boleto_own_number', '=', dict_line['nosso_numero'][:11])]
+            )
+            payment_line = self.env['payment.line'].search(
+                [('move_line_id', '=', account_move_line.id)]
+            )
+
+            valor_titulo = float(
+                str(dict_line['valor_titulo'][0:11] + '.' +
+                    dict_line['valor_titulo'][11:]))
+
+            valor_recebido = 0.0
+            if dict_line['valor_recebido']:
+                valor_recebido = float(
+                    str(dict_line['valor_recebido'][0:11] + '.' +
+                        dict_line['valor_recebido'][11:]))
+            total_valores += valor_titulo
+
+            if (dict_line['data_ocorrencia'] == '000000' or
+                    not dict_line['data_ocorrencia']):
+                data_ocorrencia = dict_line['data_de_ocorrencia']
+            else:
+                data_ocorrencia = datetime.datetime.strptime(
+                    str(dict_line['data_ocorrencia']), "%d%m%y").date()
+
+            # Cada Banco pode possuir um Codigo de Ocorrencia distinto
+            if self.bank == 'bradesco':
+                descricao_ocorrencia = DICT_OCORRENCIAS_BRADESCO[
+                       dict_line['codigo_ocorrencia']].encode('utf-8')
+            if self.bank == 'itau':
+                descricao_ocorrencia = DICT_OCORRENCIAS_ITAU[
+                       dict_line['codigo_ocorrencia']].encode('utf-8')
+
+            if not account_move_line:
+                vals_evento = {
+                   'lote_id': lote_id.id,
+                   'ocorrencias': descricao_ocorrencia,
+                   'data_ocorrencia': data_ocorrencia,
+                   'str_motiv_a':
+                       u' * - BOLETO NÃO ENCONTRADO DENTRO DO PROGRAMA',
+                   'nosso_numero': dict_line['nosso_numero'],
+                   'seu_numero': dict_line['documento_numero'],
+                   'valor_titulo': valor_titulo,
+                }
+                self.env['l10n_br.cnab.evento'].create(vals_evento)
+                continue
+
+            if (dict_line['data_credito'] == '000000' or
+                    not dict_line['data_credito']):
+                data_credito = dict_line['data_credito']
+            else:
+                data_credito = datetime.datetime.strptime(
+                    str(dict_line['data_credito']), "%d%m%y").date()
+
+            if (str(dict_line['codigo_ocorrencia']) in ('06', '17')
+                    and self.bank == 'bradesco') or (
+                        str(dict_line['codigo_ocorrencia']) in ('06', '10') and
+                        self.bank == 'itau'):
+                vals_evento = {
+                    'lote_id': lote_id.id,
+                    'data_ocorrencia': data_ocorrencia,
+                    'data_real_pagamento': data_credito.strftime("%Y-%m-%d"),
+                    # 'segmento': evento.servico_segmento,
+                    # 'favorecido_nome':
+                    #    obj_account_move_line.company_id.partner_id.name,
+                    'favorecido_conta_bancaria':
+                        account_move_line.payment_mode_id.bank_id.id,
+                    'nosso_numero': dict_line['nosso_numero'],
+                    'seu_numero': dict_line['documento_numero'] or
+                        account_move_line.name,
+                    # 'tipo_moeda': evento.credito_moeda_tipo,
+                    'valor_titulo': valor_titulo,
+                    'valor_pagamento': valor_recebido,
+                    'ocorrencias': descricao_ocorrencia,
+                    'bank_payment_line_id':
+                        payment_line.bank_line_id.id or False,
+                }
+
+                # Monta o dicionario que sera usado
+                # para criar o Extrato Bancario
+                balance_end_real += valor_recebido
+                line_statement_vals.append({
+                    'name': account_move_line.name or '?',
+                    'amount': valor_recebido,
+                    'partner_id': account_move_line.partner_id.id,
+                    'ref': account_move_line.ref,
+                    'date': account_move_line.date,
+                    'amount_currency': valor_recebido,
+                    'currency_id': account_move_line.currency_id.id,
+                })
+
+            else:
+                vals_evento = {
+                    'lote_id': lote_id.id,
+                    'ocorrencias': descricao_ocorrencia,
+                    'data_ocorrencia': data_ocorrencia,
+                    'nosso_numero': dict_line['nosso_numero'],
+                    'seu_numero': account_move_line.name,
+                    'valor_titulo': valor_titulo,
+                }
+
+            self.env['l10n_br.cnab.evento'].create(vals_evento)
+
+        lote_id.total_valores = total_valores
+        lote_id.qtd_registros = quantidade_registros
+        self.num_lotes = 1
+        self.num_eventos = quantidade_registros
+
+        # Criacao de um Extrato Bancario para ser conciliado
+        # pelo usuario permitindo assim o tratamento de valores
+        # a mais ou a menos pelo operador
+        if line_statement_vals:
+            vals_bank_statement = {
+                'journal_id': self.account_journal.id,
+                'balance_end_real': balance_end_real,
+            }
+            statement = self.env[
+                'account.bank.statement'].create(vals_bank_statement)
+            statement_line_obj = self.env['account.bank.statement.line']
+            for line in line_statement_vals:
+                line['statement_id'] = statement.id
+                statement_line_obj.create(line)
+
+        return self.write({'state': 'done'})
+
+
+class L10nBrHrCnabEvento(models.Model):
+    _inherit = "l10n_br.cnab.evento"
+
+    data_ocorrencia = fields.Date(string=u"Data da Ocorrência")
+    valor_titulo = fields.Float(string=u"Valor do Título")
