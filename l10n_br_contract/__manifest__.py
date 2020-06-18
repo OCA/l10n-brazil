fd1274c2fd96f7d92adffff07291f585c5ab23e9
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-06-18 16:28:03 -0300

    [ADD] Module l10n_br_contract

---
+++ l10n_br_contract/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Contract',
+    'summary': """
+        Brazilian Localization Contract""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE INFORMATICA LTDA,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/l10n-brazil.git',
+    'depends': [
+        'l10n_br_account', 'contract'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
