444ff4a8a309170af54c0db0844a8455b600074f
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-06-19 15:56:35 -0300

    [ADD] Data filling

--- l10n_br_contract/models/contract_contract.py
+++ l10n_br_contract/models/contract_contract.py
@@ -2,15 +2,40 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class ContractContract(models.Model):
 
     _inherit = 'contract.contract'
 
+    @api.multi
+    def _prepare_invoice(self, date_invoice, journal=None):
+        invoice_vals = super()._prepare_invoice(date_invoice, journal)
+        invoice_vals['fiscal_document_id'] = False
+        invoice_vals['operation_type'] = 'out'
+        invoice_vals['document_type_id'] = self.company_id.document_type_id.id
+        invoice_vals['fiscal_operation_id'] = \
+            self.env.ref('l10n_br_fiscal.fo_venda').id
+        invoice_vals['document_serie_id'] = \
+            self.env['l10n_br_fiscal.document.serie'].search([
+                ('document_type_id', '=', invoice_vals['document_type_id']),
+                ('company_id', '=', self.company_id.id),
+            ], limit=1).id
+        return invoice_vals
+
     @api.model
-    def _finalize_and_create_invoices(self, invoices_values):
-        for invoice in invoices_values:
-            invoice['fiscal_document_id'] = False
-        return super()._finalize_and_create_invoices(invoices_values)
+    def _finalize_invoice_creation(self, invoices):
+        super()._finalize_invoice_creation(invoices)
+        for invoice in invoices:
+            invoice.fiscal_document_id._onchange_document_serie_id()
+            invoice.fiscal_document_id._onchange_company_id()
+            invoice.fiscal_document_id._onchange_partner_id()
+            invoice.fiscal_document_id._onchange_fiscal_operation_id()
+            for line in invoice.fiscal_document_id.line_ids:
+                line._onchange_product_id_fiscal()
+                line._onchange_commercial_quantity()
+                line._onchange_ncm_id()
+                line._onchange_fiscal_operation_id()
+                line._onchange_fiscal_operation_line_id()
+                line._onchange_fiscal_taxes()
