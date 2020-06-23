9108f9ca0e2782ccd9ada2d0b8abc33d452f8e7e
Renato Lima <renato.lima@akretion.com>
2020-06-23 16:58:45 -0300

    [REF] Allow delete only fiscal documents with draft state

--- l10n_br_account/models/fiscal_document.py
+++ l10n_br_account/models/fiscal_document.py
@@ -2,7 +2,12 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+from odoo.addons.l10n_br_fiscal.constants.fiscal import (
+    SITUACAO_EDOC_EM_DIGITACAO,
+)
 
 
 class FiscalDocument(models.Model):
@@ -105,6 +110,13 @@ class FiscalDocument(models.Model):
 
     @api.multi
     def unlink(self):
+        draft_documents = self.filtered(
+            lambda d: d.state == SITUACAO_EDOC_EM_DIGITACAO)
+
+        if draft_documents:
+            UserError(_("You cannot delete a fiscal document "
+                        "which is not draft state."))
+
         invoices = self.env['account.invoice'].search(
             [('fiscal_document_id', 'in', self.ids)])
         invoices.unlink()
