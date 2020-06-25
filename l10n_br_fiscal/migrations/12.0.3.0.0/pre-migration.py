6ed27d724483c7052a1c85a5e708f21cf06be0a9
Renato Lima <renato.lima@akretion.com>
2020-06-25 12:13:51 -0300

    [REF] rename operation_type to fiscal_operation_type to avoid conflict with payment order field

---
+++ l10n_br_fiscal/migrations/12.0.3.0.0/pre-migration.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020 - TODAY Renato Lima - Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+_column_renames = {
+    'l10n_br_fiscal_document': [
+        ('operation_type', 'fiscal_operation_type')],
+    'l10n_br_fiscal_operation': [
+        ('operation_type', 'fiscal_operation_type')],
+    'l10n_br_fiscal_operation_line': [
+        ('operation_type', 'fiscal_operation_type')],
+}
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    openupgrade.rename_columns(env.cr, _column_renames)
