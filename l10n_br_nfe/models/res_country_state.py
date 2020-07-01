aeadf4eb63e3a78ef91a0ee453c99f25fc1bd114
Raphaël Valyi <rvalyi@akretion.com>
2020-07-01 07:49:08 -0300

    restored res_country_state as https://github.com/OCA/l10n-brazil/pull/753

--- l10n_br_nfe/models/res_country_state.py
+++ l10n_br_nfe/models/res_country_state.py
@@ -1,4 +1,4 @@
-# Copyright 2020 KMEE INFORMATICA LTDA
+# Copyright 2019 Akretion (Raphaël Valyi <raphael.valyi@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models
 
