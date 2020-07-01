bc536812e25c4ab5c95e437dd7dbc2cead7eb956
Luis Felipe Mileo <mileo@kmee.com.br>
2020-07-01 13:49:08 -0300

    [NEW] Adapting l10n_br_fiscal payment mode
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/models/payment_mode.py
+++ l10n_br_fiscal/models/payment_mode.py
@@ -12,11 +12,20 @@ class FiscalPaymentMode(models.Model):
 
     _name = 'l10n_br_fiscal.payment.mode'
     _description = 'Forma de Pagamento'
-    # _table = 'account_payment_mode'
+    _table = 'account_payment_mode'
 
     name = fields.Char()
     forma_pagamento = fields.Selection(
         selection=FORMA_PAGAMENTO,
         string='Forma de pagamento',
         default=FORMA_PAGAMENTO_OUTROS,
+    )
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        string='Company',
+        required=True,
+        ondelete='restrict',
+        default=lambda self: self.env['res.company']._company_default_get(
+            'l10n_br_fiscal.payment.mode'
+        )
     )
