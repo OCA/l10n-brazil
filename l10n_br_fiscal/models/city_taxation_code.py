4d081528003cdfeab9f16371edae8bd89dcf993c
Luis Malta <luis.malta@kmee.com.br>
2020-07-03 12:12:12 -0300

    [REF] Move city taxation code to l10n_br_fiscal

---
+++ l10n_br_fiscal/models/city_taxation_code.py
@@ -0,0 +1,36 @@
+# Copyright 2020 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class CityTaxationCode(models.Model):
+
+    _name = 'l10n_br_fiscal.city.taxation.code'
+    _description = 'City Taxation Code'
+
+    name = fields.Char(
+        string='Description',
+    )
+
+    code = fields.Char(
+        string='Code',
+    )
+
+    service_type_id = fields.Many2one(
+        comodel_name='l10n_br_fiscal.service.type',
+        string='Service Type',
+        domain="[('internal_type', '=', 'normal')]"
+    )
+
+    state_id = fields.Many2one(
+        comodel_name='res.country.state',
+        string="State",
+        domain=[('country_id.code', '=', 'BR')]
+    )
+
+    city_id = fields.Many2one(
+        string="City",
+        comodel_name="res.city",
+        domain="[('state_id', '=', state_id)]",
+    )
