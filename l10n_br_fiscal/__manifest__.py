87752fd95834c4f532aae6b69afd0aef90298ff8
Luis Malta <luis.malta@kmee.com.br>
2020-07-03 12:12:12 -0300

    [REF] Move city taxation code to l10n_br_fiscal

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -97,6 +97,7 @@
         "views/document_invalidate_number.xml",
         "views/document_cancel.xml",
         "views/document_correction.xml",
+        "views/city_taxation_code.xml",
     ],
     "demo": [
         # Some demo data is being loaded via post_init_hook in hook file
