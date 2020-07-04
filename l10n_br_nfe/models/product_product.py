7bb43b4446dcf66920721d82c2e9a991c5d7aefc
Raphaël Valyi <rvalyi@akretion.com>
2020-07-04 15:34:17 -0300

    fixed product mapping

--- l10n_br_nfe/models/product_product.py
+++ l10n_br_nfe/models/product_product.py
@@ -1,15 +1,20 @@
-# Copyright 2019 Akretion (Raphaël Valyi <raphael.valyi@akretion.com>)
+# Copyright 2020 Akretion (Raphaël Valyi <raphael.valyi@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, models
 
 
+# The generateDS prod mixin (prod XML tag) cannot be injected in
+# the product.product object because the tag includes attributes from the
+# Odoo fiscal document line. So a part of the mapping is done
+# in the fiscal document line:
+# from Odoo -> XML by using related fields/_compute
+# from XML -> Odoo by overriding the product create method
 class ProductProduct(models.Model):
     _inherit = "product.product"
     _nfe_search_keys = ['default_code', 'barcode']
 
     @api.model
     def create(self, vals):
-        print("CREATE PRODUCT FROM NFE", vals, self._context)
         parent_dict = self._context.get('parent_dict')
         if parent_dict:
             vals['standard_price'] = parent_dict.get('nfe40_vUnCom')
@@ -33,7 +38,6 @@ class ProductProduct(models.Model):
                         {'name': parent_dict['nfe40_NCM'],
                          'code': parent_dict['nfe40_NCM']})
                     vals['ncm_id'] = ncm.id
-            print("VALS, NCM_IDS", vals, ncm_ids)
         product = super().create(vals)
         product.product_tmpl_id._onchange_ncm_id()
         return product
