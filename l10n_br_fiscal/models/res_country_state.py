7a0afa7f543b29dfc2d1a1e9c7bbb9749c15e6a7
Renato Lima <renato.lima@akretion.com>
2020-07-07 14:39:00 -0300

    [ADD] tax_definition in res.country.state

--- l10n_br_fiscal/models/res_country_state.py
+++ l10n_br_fiscal/models/res_country_state.py
@@ -2,8 +2,14 @@
 # Copyright (C) 2016  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import fields, models
 
 
 class ResCountryState(models.Model):
-    _inherit = "res.country.state"
+    _inherit = 'res.country.state'
+
+    tax_definition_ids = fields.Many2many(
+        comodel_name='l10n_br_fiscal.tax.definition',
+        string='Tax Definitions',
+        domain="['|', ('state_from_ids', '=', id), ('state_to_ids', '=', id)]",
+    )
