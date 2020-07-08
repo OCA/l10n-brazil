e93363b1d437a66d1eaf076f9f7d95f1984f2156
Renato Lima <renato.lima@akretion.com>
2020-07-08 15:58:02 -0300

    [REF] add related fields from company and partner in fiscal document

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -211,36 +211,97 @@ class Document(models.Model):
 
     partner_legal_name = fields.Char(
         string='Legal Name',
+        related='partner_id.legal_name',
     )
 
     partner_name = fields.Char(
         string='Name',
+        related='partner_id.name',
     )
 
     partner_cnpj_cpf = fields.Char(
         string='CNPJ',
+        related='partner_id.cnpj_cpf',
     )
 
     partner_inscr_est = fields.Char(
         string='State Tax Number',
+        related='partner_id.inscr_est',
     )
 
     partner_inscr_mun = fields.Char(
         string='Municipal Tax Number',
+        related='partner_id.inscr_mun',
     )
 
     partner_suframa = fields.Char(
         string='Suframa',
+        related='partner_id.suframa',
     )
 
     partner_cnae_main_id = fields.Many2one(
         comodel_name='l10n_br_fiscal.cnae',
         string='Main CNAE',
+        related='partner_id.cnae_main_id',
     )
 
     partner_tax_framework = fields.Selection(
         selection=TAX_FRAMEWORK,
         string='Tax Framework',
+        related='partner_id.tax_framework',
+    )
+
+    partner_street = fields.Char(
+        string='Partner Street',
+        related='partner_id.street',
+    )
+
+    partner_number = fields.Char(
+        string='Partner Number',
+        related='partner_id.number',
+    )
+
+    partner_street2 = fields.Char(
+        string='Partner Street2',
+        related='partner_id.street2',
+    )
+
+    partner_district = fields.Char(
+        string='Partner District',
+        related='partner_id.district',
+    )
+
+    partner_country_id = fields.Many2one(
+        comodel_name='res.country',
+        string='Partner Country',
+        related='partner_id.country_id',
+    )
+
+    partner_state_id = fields.Many2one(
+        comodel_name='res.country.state',
+        string='Partner State',
+        related='partner_id.state_id',
+    )
+
+    partner_city_id = fields.Many2one(
+        comodel_name='res.city',
+        string='Partner City',
+        related='partner_id.city_id',
+    )
+
+    partner_zip = fields.Char(
+        string='Partner Zip',
+        related='partner_id.zip',
+    )
+
+    partner_phone = fields.Char(
+        string='Partner Phone',
+        related='partner_id.phone',
+    )
+
+    partner_is_company = fields.Boolean(
+        string='Partner Is Company?',
+        related='partner_id.is_company',
     )
 
     partner_shipping_id = fields.Many2one(
@@ -262,37 +323,91 @@ class Document(models.Model):
 
     company_legal_name = fields.Char(
         string='Company Legal Name',
+        related='company_id.legal_name',
     )
 
     company_name = fields.Char(
         string='Company Name',
         size=128,
+        related='company_id.name',
     )
 
     company_cnpj_cpf = fields.Char(
         string='Company CNPJ',
+        related='company_id.cnpj_cpf',
     )
 
     company_inscr_est = fields.Char(
         string='Company State Tax Number',
+        related='company_id.inscr_est',
     )
 
     company_inscr_mun = fields.Char(
         string='Company Municipal Tax Number',
+        related='company_id.inscr_mun',
     )
 
     company_suframa = fields.Char(
         string='Company Suframa',
+        related='company_id.suframa',
     )
 
     company_cnae_main_id = fields.Many2one(
         comodel_name='l10n_br_fiscal.cnae',
         string='Company Main CNAE',
+        related='company_id.cnae_main_id',
     )
 
     company_tax_framework = fields.Selection(
         selection=TAX_FRAMEWORK,
         string='Company Tax Framework',
+        related='company_id.tax_framework',
+    )
+
+    company_street = fields.Char(
+        string='Company Street',
+        related='company_id.street',
+    )
+
+    company_number = fields.Char(
+        string='Company Number',
+        related='company_id.number',
+    )
+
+    company_street2 = fields.Char(
+        string='Company Street2',
+        related='company_id.street2',
+    )
+    company_district = fields.Char(
+        string='Company District',
+        related='company_id.district',
+    )
+
+    company_country_id = fields.Many2one(
+        comodel_name='res.country',
+        string='Company Country',
+        related='company_id.country_id',
+    )
+
+    company_state_id = fields.Many2one(
+        comodel_name='res.country.state',
+        string='Company State',
+        related='company_id.city_id',
+    )
+
+    company_city_id = fields.Many2one(
+        comodel_name='res.city',
+        string='Company City',
+        related='company_id.city_id',
+    )
+    company_zip = fields.Char(
+        string='Company ZIP',
+        related='company_id.zip',
+    )
+
+    company_phone = fields.Char(
+        string='Company Phone',
+        related='company_id.phone',
     )
 
     currency_id = fields.Many2one(
@@ -487,10 +602,6 @@ class Document(models.Model):
         copy=True,
     )
 
-    additional_data = fields.Text(
-        string='Additional Data',
-    )
-
     # TODO esse campo deveria ser calculado de
     # acordo com o fiscal_document_id
     document_section = fields.Selection(
@@ -562,49 +673,6 @@ class Document(models.Model):
         default=False,
     )
 
-    def _prepare_address_fields(self, company_id, partner_id):
-        result = {}
-        if company_id:
-            company = self.env['res.company'].browse(company_id)
-            result.update(self._prepare_company_vals(company))
-
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            result.update(self._prepare_partner_vals(partner))
-        return result
-
-    def _prepare_company_vals(self, company=None):
-        values = {}
-        company = company or self.company_id
-        if self.company_id:
-            values.update({
-                'company_legal_name': company.legal_name,
-                'company_name': company.name,
-                'company_cnpj_cpf': company.cnpj_cpf,
-                'company_inscr_est': company.inscr_est,
-                'company_inscr_mun': company.inscr_mun,
-                'company_suframa': company.suframa,
-                'company_cnae_main_id': company.cnae_main_id.id,
-                'company_tax_framework': company.tax_framework,
-            })
-        return values
-
-    def _prepare_partner_vals(self, partner=None):
-        values = {}
-        partner = partner or self.partner_id
-        if partner:
-            values.update({
-                'partner_legal_name': partner.legal_name,
-                'partner_name': partner.name,
-                'partner_cnpj_cpf': partner.cnpj_cpf,
-                'partner_inscr_est': partner.inscr_est,
-                'partner_inscr_mun': partner.inscr_mun,
-                'partner_suframa': partner.suframa,
-                'partner_cnae_main_id': partner.cnae_main_id.id,
-                'partner_tax_framework': partner.tax_framework,
-            })
-        return values
-
     def _document_comment_vals(self):
         return {
             'user': self.env.user,
@@ -655,31 +723,12 @@ class Document(models.Model):
     def create(self, values):
         if not values.get('date'):
             values['date'] = self._date_server_format()
-
-        if 'company_id' or 'partner_id' in values:
-            values.update(self._prepare_address_fields(
-                company_id=values.get('company_id'),
-                partner_id=values.get('partner_id')
-            ))
         return super().create(values)
-
-    @api.multi
-    def write(self, values):
-        for fdoc in self:
-            if 'company_id' or 'partner_id' in values:
-                values.update(fdoc._prepare_address_fields(
-                    company_id=values.get('company_id'),
-                    partner_id=values.get('partner_id')
-                ))
-        return super().write(values)
 
     @api.onchange('company_id')
     def _onchange_company_id(self):
-        self.update(self._prepare_company_vals())
-
-    @api.onchange('partner_id')
-    def _onchange_partner_id(self):
-        self.update(self._prepare_partner_vals())
+        if self.company_id:
+            self.currency_id = self.company_id.currency_id
 
     @api.multi
     @api.onchange('document_section')
