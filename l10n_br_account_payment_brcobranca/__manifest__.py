78c13fb36908c85f5a46b9f4064b6910442e37e8
Magno Costa <magno.costa@akretion.com.br>
2020-07-14 17:47:42 -0300

    [FIX] Included Bank Statement in Remessa/Retorno menu.

--- l10n_br_account_payment_brcobranca/__manifest__.py
+++ l10n_br_account_payment_brcobranca/__manifest__.py
@@ -19,6 +19,7 @@
         'views/account_invoice_view.xml',
         'views/l10n_br_cnab_retorno_view.xml',
         'views/res_config_settings_view.xml',
+        'views/account_bank_statement_view.xml',
         'data/res_config_settings_data.xml',
         'security/ir.model.access.csv',
     ],
