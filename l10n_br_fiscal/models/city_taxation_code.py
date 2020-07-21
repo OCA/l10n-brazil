6893a3b6a70eacda93c1e0247aa091ccdabcec2a
Luis Malta <luis.malta@kmee.com.br>
2020-07-21 10:14:44 -0300

    [FIX] Flake8

--- l10n_br_fiscal/models/city_taxation_code.py
+++ l10n_br_fiscal/models/city_taxation_code.py
@@ -1,7 +1,7 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class CityTaxationCode(models.Model):
