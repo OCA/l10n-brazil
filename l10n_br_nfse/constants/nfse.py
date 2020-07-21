c2c67ae92b0208d125e436b6d02d5878f6a65866
Luis Malta <luis.malta@kmee.com.br>
2020-07-21 08:22:26 -0300

    [FIX] Flake8

--- l10n_br_nfse/constants/nfse.py
+++ l10n_br_nfse/constants/nfse.py
@@ -8,27 +8,27 @@ NFSE_ENVIRONMENT_DEFAULT = "2"
 
 
 OPERATION_NATURE = [
-            ('1', 'Tributação no município'),
-            ('2', 'Tributação fora do município'),
-            ('3', 'Isenção'),
-            ('4', 'Imune'),
-            ('5', 'Exigibilidade suspensa por decisão judicial'),
-            ('6', 'Exigibilidade suspensa por procedimento administrativo'),
-        ]
+    ('1', 'Tributação no município'),
+    ('2', 'Tributação fora do município'),
+    ('3', 'Isenção'),
+    ('4', 'Imune'),
+    ('5', 'Exigibilidade suspensa por decisão judicial'),
+    ('6', 'Exigibilidade suspensa por procedimento administrativo'),
+]
 
 
 RPS_TYPE = [
-            ('1', 'Recibo provisório de Serviços'),
-            ('2', 'RPS Nota Fiscal Conjugada (Mista)'),
-            ('3', 'Cupom'),
-        ]
+    ('1', 'Recibo provisório de Serviços'),
+    ('2', 'RPS Nota Fiscal Conjugada (Mista)'),
+    ('3', 'Cupom'),
+]
 
 
 TAXATION_SPECIAL_REGIME = [
-            ('1', 'Microempresa Municipal'),
-            ('2', 'Estimativa'),
-            ('3', 'Sociedade de Profissionais'),
-            ('4', 'Cooperativa'),
-            ('5', 'Microempresario Individual(MEI)'),
-            ('6', 'Microempresario e Empresa de Pequeno Porte(ME EPP)'),
-        ]
+    ('1', 'Microempresa Municipal'),
+    ('2', 'Estimativa'),
+    ('3', 'Sociedade de Profissionais'),
+    ('4', 'Cooperativa'),
+    ('5', 'Microempresario Individual(MEI)'),
+    ('6', 'Microempresario e Empresa de Pequeno Porte(ME EPP)'),
+]
