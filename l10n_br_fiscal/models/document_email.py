c90659d47e5f7ca74069b0ab7713662169e1b7d2
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-07-28 00:49:42 -0300

    [12.0][IMP] l10n_br_fiscal: Filtered email_template by document_type

--- l10n_br_fiscal/models/document_email.py
+++ l10n_br_fiscal/models/document_email.py
@@ -1,23 +1,11 @@
 # Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import base64
-import logging
-from odoo.exceptions import UserError
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from ..constants.fiscal import (
     SITUACAO_EDOC,
-    SITUACAO_EDOC_EM_DIGITACAO,
-    SITUACAO_EDOC_A_ENVIAR,
-    SITUACAO_EDOC_ENVIADA,
-    SITUACAO_EDOC_REJEITADA,
-    SITUACAO_EDOC_AUTORIZADA,
-    SITUACAO_EDOC_CANCELADA,
-    SITUACAO_EDOC_DENEGADA,
-    SITUACAO_EDOC_INUTILIZADA,
 )
 
-_logger = logging.getLogger(__name__)
 
 class DocumentEmail(models.Model):
 
@@ -26,12 +14,16 @@ class DocumentEmail(models.Model):
 
     name = fields.Char(
         string="Name",
+        readonly=True,
+        store=True,
+        copy=False,
         compute="_compute_name",
     )
 
     active = fields.Boolean(
         string='Active',
-        default=True)
+        default=True,
+    )
 
     company_id = fields.Many2one(
         comodel_name='res.company',
@@ -53,14 +45,15 @@ class DocumentEmail(models.Model):
         required=True,
         company_dependent=True,
         track_visibility="onchange",
-        index=True
+        index=True,
     )
 
     email_template = fields.Many2one(
         comodel_name="mail.template",
         string="Fiscal Document E-mail Template",
+        required=True,
         help="Select the email template that will be sent when "
-        "this document state change."
+             "this document state change.",
     )
 
     @api.multi
@@ -71,24 +64,29 @@ class DocumentEmail(models.Model):
             if not document_type:
                 document_type = "Others Document Types"
             if record.state_edoc:
-                record.name = document_type + ' - ' \
-                              + record.state_edoc
+                record.name = document_type + ' - ' + record.state_edoc
+
+    _sql_constraints = [(
+        'name_company_unique',
+        'unique(name)',
+        'This name is already used by another email definition!'
+        )]
+
 
 class Document(models.Model):
     _inherit = 'l10n_br_fiscal.document'
 
     def _after_change_state(self, old_state, new_state):
-        super(Document, self)._after_change_state(
+        super()._after_change_state(
             old_state, new_state
         )
         self.send_email(new_state)
 
     def send_email(self, new_state):
-        # if new_state == SITUACAO_EDOC_AUTORIZADA:
-        self.env.user.company_id.email_template. \
-            send_mail(self.id)
+        email_template = self._get_email_template(new_state)
+        if email_template:
+            email_template.send_mail(self.id)
 
-        #
         # if not mail:
         #     raise UserError(_('Modelo de email padrão não configurado'))
         # # atts = self._find_attachment_ids_email()
@@ -110,3 +108,11 @@ class Document(models.Model):
         #     message_type='email', subtype='mt_comment',
         #     # attachment_ids=atts + mail.attachment_ids.ids, **values
         # )
+
+    def _get_email_template(self, new_state):
+        email_template = \
+            self.document_type_id\
+                .document_email_ids.filtered(
+                    lambda e: e.state_edoc == new_state)\
+                .mapped('email_template')
+        return email_template
