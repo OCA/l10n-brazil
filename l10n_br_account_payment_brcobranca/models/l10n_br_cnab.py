5c51d9003d853068b5760627a134ba9e51b8f9f3
Magno Costa <magno.costa@akretion.com.br>
2020-07-30 15:53:15 -0300

    [FIX] Removed unnecessary code for python3.

--- l10n_br_account_payment_brcobranca/models/l10n_br_cnab.py
+++ l10n_br_account_payment_brcobranca/models/l10n_br_cnab.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion - Renato Lima <renato.lima@akretion.com.br>
 # Copyright 2017 Akretion - Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
