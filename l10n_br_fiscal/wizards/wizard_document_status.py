1ba6d3ead2afe0bfe71e5b9ba45cf55d597e7db5
Luis Malta <luis.malta@kmee.com.br>
2020-08-05 07:27:12 -0300

    [REF] Generic Wizard Document Status

--- l10n_br_fiscal/wizards/wizard_document_status.py
+++ l10n_br_fiscal/wizards/wizard_document_status.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models
 
 class DocumentStatusWizard(models.TransientModel):
     _name = "l10n_br_fiscal.document.status.wizard"
-    _description = "Fiscal Document Status Inquiry on SEFAZ"
+    _description = "Fiscal Document Status"
 
     state = fields.Selection(
         selection=[
@@ -19,72 +19,10 @@ class DocumentStatusWizard(models.TransientModel):
         readonly=True,
         default="init")
 
-    version = fields.Text(
-        string="Version",
-        readonly=True)
+    document_status = fields.Text(
+        string="Status", readonly=True)
 
-    # TODO Este campo deveria estar no módulo l10n_br_nfe
-    # Esta classe deveria ser um abstract
-    nfe_environment = fields.Selection(
-        selection=[
-            ("1", u"Produção"),
-            ("2", u"Homologação")],
-        string="Ambiente")
-
-    xMotivo = fields.Text(
-        string="Motivo", readonly=True)
-
-    # FIXME
-    cUF = fields.Integer(
-        string="Codigo Estado",
-        readonly=True)
-
-    chNFe = fields.Char(
-        string="Chave de Acesso NFE",
-        size=44)
-
-    protNFe = fields.Text(
-        string="Protocolo NFE",
-        readonly=True)
-
-    retCancNFe = fields.Text(
-        string="Cancelamento NFE",
-        readonly=True)
-
-    procEventoNFe = fields.Text(
-        sting="Processamento Evento NFE",
-        readonly=True)
 
     @api.multi
     def get_document_status(self):
-        for data in self:
-            # Call some method from l10n_br_account to check chNFE
-            call_result = {
-                "version": "2.01",
-                "nfe_environment": "2",
-                "xMotivo": "101",
-                "cUF": 27,
-                "chNFe": data.chNFe,
-                "protNFe": "123",
-                "retCancNFe": "",
-                "procEventoNFe": "",
-                "state": "done",
-            }
-            data.write(call_result)
-
-            view_rec = self.env["ir.model.data"].get_object_reference(
-                "l10n_br_account_product",
-                "l10n_br_account_product_document_status_sefaz_form",
-            )
-            view_id = view_rec and view_rec[1] or False
-
-            return {
-                "view_type": "form",
-                "view_id": [view_id],
-                "view_mode": "form",
-                "res_model": "l10n_br_account_product.document_status_sefaz",
-                "res_id": data.id,
-                "type": "ir.actions.act_window",
-                "target": "new",
-                "context": data.env.context,
-            }
+        pass
