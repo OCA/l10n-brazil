6ec2b72679750d85dc97a020a5fd15f724d3fb2b
Renato Lima <renatonlima@gmail.com>
2020-08-16 23:37:02 -0300

    [FIX] Account invoice line compute

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import account_document_line_mixin
 from . import account_tax_group
 from . import fiscal_tax_group
 from . import account_tax_template
