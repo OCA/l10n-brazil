97a1ae0b853e4c7a25ee6f456b0b337e9b58d40a
Renato Lima <renatonlima@gmail.com>
2020-08-18 16:54:05 -0300

    [REF] prepare l10n_br_account_payment_order to merge with l10n_br_payment_order_cobranca

--- l10n_br_account_payment_order/models/account_payment_mode.py
+++ l10n_br_account_payment_order/models/account_payment_mode.py
@@ -1,31 +1,30 @@
 # Copyright (C) 2012-Today - KMEE (<http://kmee.com.br>).
 #  @author Luis Felipe Miléo - mileo@kmee.com.br
+#  @author Renato Lima - renato.lima@akretion.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
-
-OPERATION_TYPE = [
-    ('1', 'Pagamento'),
-    ('2', 'Debito em conta corrente'),
-]
+from odoo import _, fields, models
 
 
 class AccountPaymentMode(models.Model):
-    _inherit = "account.payment.mode"
+    _inherit = 'account.payment.mode'
 
-    internal_sequence_id = fields.Many2one("ir.sequence", "Sequência")
-    instrucoes = fields.Text("Instruções de cobrança")
-    invoice_print = fields.Boolean("Gerar relatorio na conclusão da fatura?")
-
-    operation_type = fields.Selection(
-        selection=OPERATION_TYPE,
-        string='Tipo de Operação'
+    internal_sequence_id = fields.Many2one(
+        comodel_name='ir.sequence',
+        string='Sequência',
     )
 
-    _sql_constraints = [
-        (
-            "internal_sequence_id_unique",
-            "unique(internal_sequence_id)",
-            "Sequência já usada! Crie uma sequência unica para cada modo",
+    instrucoes = fields.Text(
+        string='Instruções de cobrança',
+    )
+
+    invoice_print = fields.Boolean(
+        string='Gerar relatorio na conclusão da fatura?'
+    )
+
+    _sql_constraints = [(
+        "internal_sequence_id_unique",
+        "unique(internal_sequence_id)",
+        _("Sequência já usada! Crie uma sequência unica para cada modo"),
         )
     ]
