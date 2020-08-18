b1d3d2d4379615f892f72add99af71f2a6590dac
Renato Lima <renatonlima@gmail.com>
2020-08-18 16:54:05 -0300

    [REF] prepare l10n_br_account_payment_order to merge with l10n_br_payment_order_cobranca

--- l10n_br_account_payment_order/models/account_payment_line.py
+++ l10n_br_account_payment_order/models/account_payment_line.py
@@ -2,31 +2,13 @@
 #  Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
-from odoo.tools.float_utils import float_round as round
-from .account_payment_mode import OPERATION_TYPE
+from odoo.tools.float_utils import float_round as round # TODO check round methods in 12.0
 
 
 class PaymentLine(models.Model):
     _inherit = 'account.payment.line'
-
-    @api.model
-    def _get_info_partner(self, partner_record):
-        if not partner_record:
-            return False
-        st = partner_record.street or ''
-        n = partner_record.street_number or ''
-        st1 = partner_record.street2 or ''
-        zip = partner_record.zip or ''
-        city = partner_record.city_id.name or ''
-        uf = partner_record.state_id.code or ''
-        zip_city = city + '-' + uf + '\n' + zip
-        cntry = partner_record.country_id and \
-            partner_record.country_id.name or ''
-        cnpj = partner_record.cnpj_cpf or ''
-        return partner_record.legal_name or '' + "\n" + cnpj + "\n" + st \
-            + ", " + n + "  " + st1 + "\n" + zip_city + "\n" + cntry
 
     @api.multi
     @api.depends('percent_interest', 'amount_currency')
@@ -38,16 +20,17 @@ class PaymentLine(models.Model):
                 record.amount_currency * (
                     record.percent_interest / 100), precision)
 
-    linha_digitavel = fields.Char(string="Linha Digitável")
-    percent_interest = fields.Float(string="Percentual de Juros",
-                                    digits=dp.get_precision('Account'))
-    amount_interest = fields.Float(string="Valor Juros",
-                                   compute='_compute_interest',
-                                   digits=dp.get_precision('Account'))
+    linha_digitavel = fields.Char(
+        string='Linha Digitável',
+    )
 
-    operation_type = fields.Selection(
-        selection=OPERATION_TYPE,
-        string='Tipo de Operação',
-        related='order_id.operation_type',
-        store=True
+    percent_interest = fields.Float(
+        string='Percentual de Juros',
+        digits=dp.get_precision('Account'),
+    )
+
+    amount_interest = fields.Float(
+        string='Valor Juros',
+        compute='_compute_interest',
+        digits=dp.get_precision('Account'),
     )
