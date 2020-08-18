e95b7af722868f62d3cd65ae90346e7cb5bb9567
Renato Lima <renatonlima@gmail.com>
2020-08-18 16:54:05 -0300

    [REF] prepare l10n_br_account_payment_order to merge with l10n_br_payment_order_cobranca

--- l10n_br_account_payment_order/models/res_partner_bank.py
+++ l10n_br_account_payment_order/models/res_partner_bank.py
@@ -9,15 +9,4 @@ from odoo.exceptions import Warning as UserError
 class ResPartnerBank(models.Model):
     _inherit = "res.partner.bank"
 
-    @api.multi
-    @api.constrains("bra_number")
-    def check_bra_number(self):
-        for record in self:
-            if record.bank_id.code_bc == "033":
-                if len(record.bra_number) > 4:
-                    raise UserError(
-                        _(
-                            u"O cógido da Agencia Bancaria do Santander"
-                            u" deve ter no máximo quatro caracteres."
-                        )
-                    )
+    # TODO REMOVE THIS FILE
