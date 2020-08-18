bee4feb9d5f4f487a63862f2d986afb1ce057ee4
Renato Lima <renatonlima@gmail.com>
2020-08-18 16:54:05 -0300

    [REF] prepare l10n_br_account_payment_order to merge with l10n_br_payment_order_cobranca

--- l10n_br_account_payment_order/models/account_payment_order.py
+++ l10n_br_account_payment_order/models/account_payment_order.py
@@ -3,13 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
-from .account_payment_mode import OPERATION_TYPE
 
 
 class AccountPaymentOrder(models.Model):
     _inherit = 'account.payment.order'
-
-    operation_type = fields.Selection(
-        selection=OPERATION_TYPE,
-        string='Tipo de Operação',
-    )
