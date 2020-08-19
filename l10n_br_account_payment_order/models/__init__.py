5ea2d9f6a2838c319e08a9654c0948da8d80af01
Renato Lima <renatonlima@gmail.com>
2020-08-19 18:29:37 -0300

    Update manifest files

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -2,9 +2,16 @@
 #  Luis Felipe Miléo - mileo@kmee.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
+from . import res_company
+from . import account_invoice
+from . import account_move
 from . import account_move_line
 from . import account_payment_mode
 from . import account_payment_order
 from . import account_payment_line
+from . import account_payment
 from . import res_partner_bank
+from . import bank_payment_line
+from . import l10n_br_cnab
+from . import l10n_br_cnab_evento
+from . import l10n_br_cnab_lote
--- l10n_br_account_payment_order/wizards/__init__.py
+++ l10n_br_account_payment_order/wizards/__init__.py
@@ -4,4 +4,4 @@
 #   @author Fernando Marcato <fernando.marcato@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import payment_order_create
+from . import account_payment_line_create
