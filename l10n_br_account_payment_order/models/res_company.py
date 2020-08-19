7d54b24169d940ad94e750f6cac1d7cf9f0e054d
Renato Lima <renatonlima@gmail.com>
2020-08-19 17:02:11 -0300

    Moved files from l10n_br_account_payment_cobranca to l10n_br_account_payment_order

---
+++ l10n_br_account_payment_order/models/res_company.py
@@ -0,0 +1,37 @@
+# © 2012 KMEE INFORMATICA LTDA
+#   @author Luis Felipe Mileo <mileo@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+from ..constantes import (SEQUENCIAL_CARTEIRA, SEQUENCIAL_EMPRESA,
+                          SEQUENCIAL_FATURA)
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    own_number_type = fields.Selection(
+        selection=[
+            (SEQUENCIAL_EMPRESA, "Sequêncial único por empresa"),
+            (SEQUENCIAL_FATURA, "Numero sequêncial da Fatura"),
+            (SEQUENCIAL_CARTEIRA, "Sequêncial único por carteira"),
+        ],
+        string="Tipo de nosso número",
+        default=SEQUENCIAL_FATURA,
+    )
+
+    own_number_sequence = fields.Many2one(
+        comodel_name="ir.sequence", string="Sequência do Nosso Número"
+    )
+
+    environment = fields.Selection(
+        string="Ambiente",
+        selection=[("1", "HOMOLOGAÇÃO"), ("2", "PRODUÇÃO")],
+        default="1",
+    )
+
+    @api.multi
+    def get_own_number_sequence(self):
+        self.ensure_one()
+        return self.own_number_sequence.next_by_id()
