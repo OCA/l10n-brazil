80f303607c04110a86dcead31a86630f7a2123b1
Renato Lima <renatonlima@gmail.com>
2020-08-19 18:29:37 -0300

    Update manifest files

--- l10n_br_account_payment_order/__manifest__.py
+++ l10n_br_account_payment_order/__manifest__.py
@@ -17,17 +17,46 @@
         'account_cancel',
     ],
     'data': [
+        # Security]
+        'security/cnab_cobranca_security.xml',
+        'security/ir.model.access.csv',
+
+        # Data
+        'data/cnab_data.xml',
+        'data/l10n_br_payment_export_type.xml',
+        'data/boleto_data.xml',
+        'data/ir_cron.xml',
+        'data/account_analytic_tag_data.xml',
+
+        # Reports
+        'reports/report_print_button_view.xml',
+
+        # Views
         'views/account_due_list.xml',
         'views/account_payment_order.xml',
-        # 'views/account_payment_order_menu_views.xml',
         'views/account_payment_line.xml',
         'views/account_payment_mode.xml',
+        'views/res_company.xml',
+        'views/bank_payment_line.xml',
+        'views/l10n_br_cnab_retorno_view.xml',
+        'views/l10n_br_cnab_evento_views.xml',
+        'views/account_invoice.xml',
+        'views/account_move_line.xml',
+        # 'views/l10n_br_payment_cnab.xml',
+        # 'views/l10n_br_cobranca_cnab.xml',
+        # 'views/l10n_br_cobranca_cnab_lines.xml',
+        # 'views/account_payment_order_menu_views.xml', TODO REMOVE
+
+        # Wizards
+        'wizard/account_payment_line_create_view.xml',
     ],
     'demo': [
         'demo/res_partner_bank.xml',
         'demo/account_journal.xml',
         'demo/account_payment_mode.xml',
         'demo/account_payment_order.xml',
+        'demo/res_users.xml',
+        'demo/account_invoice.xml',
     ],
     'installable': True,
 }
