a464b4af9e8bc51d22bac36be44b22566755f3fc
Renato Lima <renato.lima@akretion.com>
2020-08-19 23:30:41 -0300

    [FIX] Invoice amount taxes

--- l10n_br_coa/models/account_tax_template.py
+++ l10n_br_coa/models/account_tax_template.py
@@ -6,7 +6,7 @@ from odoo import models
 
 class AccountTaxTemplate(models.Model):
     _name = 'account.tax.template'
-    _inherit = [ 'account.tax.mixin', 'account.tax.template']
+    _inherit = ['account.tax.mixin', 'account.tax.template']
 
     def _get_tax_vals(self, company, tax_template_to_tax):
         values = super()._get_tax_vals(company, tax_template_to_tax)
