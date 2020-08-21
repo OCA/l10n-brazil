0a6ef242a9ff2a66f52398221c135cc09f87911f
Renato Lima <renatonlima@gmail.com>
2020-08-21 12:32:20 -0300

    [ADD] company_id field in fiscal document email for multi-company

--- l10n_br_fiscal/models/document_email.py
+++ l10n_br_fiscal/models/document_email.py
@@ -28,7 +28,6 @@ class DocumentEmail(models.Model):
     company_id = fields.Many2one(
         comodel_name='res.company',
         string='Company',
-        readonly=True,
     )
 
     document_type_id = fields.Many2one(
