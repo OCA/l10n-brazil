012500f1b088b59ca6926bc77d75a69e296fff96
Renato Lima <renatonlima@gmail.com>
2020-08-23 20:34:51 -0300

    pep8

--- l10n_br_sale/models/sale_order_line.py
+++ l10n_br_sale/models/sale_order_line.py
@@ -123,8 +123,9 @@ class SaleOrderLine(models.Model):
         if not self.env.user.has_group('l10n_br_sale.group_discount_per_value'):
             if self.discount:
                 self.discount_value = (
-                    (self.product_uom_qty * self.price_unit) * (
-                    self.discount / 100))
+                    (self.product_uom_qty * self.price_unit)
+                    * (self.discount / 100)
+                )
 
     @api.onchange('discount_value')
     def _onchange_discount_value(self):
