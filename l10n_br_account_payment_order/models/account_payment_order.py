e1b87a2a7c4fd1adb4a370196c08f67203ab0d9f
Magno Costa <magno.costa@akretion.com.br>
2020-08-25 15:52:27 -0300

    [MIG][FIX] Method missing in extract to Bank API module.

--- l10n_br_account_payment_order/models/account_payment_order.py
+++ l10n_br_account_payment_order/models/account_payment_order.py
@@ -88,24 +88,6 @@ class AccountPaymentOrder(models.Model):
         domain=[('is_export_error', '=', True)],
     )
 
-    def _confirm_debit_orders_api(self):
-        """
-        Method create to confirm all bank_api exclusive account.payment.order
-        :return:
-        """
-        _logger.info('_confirm_debit_orders_api()')
-
-        order_ids = self.search(
-            [('active', '=', False), ('state', '=', 'draft'), ('name', 'ilike', 'api')]
-        )
-
-        for order_id in order_ids:
-            try:
-                order_id.draft2open()
-                order_id.active = True
-            except Exception as e:
-                _logger.warn(str(e))
-
     @api.model
     def _prepare_bank_payment_line(self, paylines):
         result = super()._prepare_bank_payment_line(paylines)
