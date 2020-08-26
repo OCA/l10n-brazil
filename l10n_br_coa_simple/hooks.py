442232d41b2e4da6499518840dfe80895ee930c5
Renato Lima <renatonlima@gmail.com>
2020-08-26 17:11:37 -0300

    [REF] l10n_br_coa_simple: load COA for demo using hook

--- l10n_br_coa_simple/hooks.py
+++ l10n_br_coa_simple/hooks.py
@@ -1,16 +1,23 @@
 # Copyright (C) 2020 - Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID, api
+from odoo import api, tools, SUPERUSER_ID
 
 
 def post_init_hook(cr, registry):
-    """Relate fiscal taxes to account taxes."""
     env = api.Environment(cr, SUPERUSER_ID, {})
+    coa_simple_tmpl = env.ref(
+        'l10n_br_coa_simple.l10n_br_coa_simple_chart_template')
     if env['ir.module.module'].search_count([
         ('name', '=', 'l10n_br_account'),
         ('state', '=', 'installed'),
     ]):
         from odoo.addons.l10n_br_account.hooks import load_fiscal_taxes
-        load_fiscal_taxes(env, env.ref(
-            'l10n_br_coa_simple.l10n_br_coa_simple_chart_template'))
+        # Relate fiscal taxes to account taxes.
+        load_fiscal_taxes(env, coa_simple_tmpl)
+
+    # Load COA to Demo Company
+    if not tools.config.get('without_demo'):
+        env.user.company_id = env.ref(
+            'l10n_br_fiscal.empresa_simples_nacional')
+        coa_simple_tmpl.try_loading_for_current_company()
