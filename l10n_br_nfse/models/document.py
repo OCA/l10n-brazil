9e899cc993b524c49db215c8de05bca49fb296a2
Luis Malta <luis.malta@kmee.com.br>
2020-08-26 08:07:48 -0300

    [REF] Rename Cultural Sponsor field

--- l10n_br_nfse/models/document.py
+++ l10n_br_nfse/models/document.py
@@ -179,7 +179,7 @@ class Document(models.Model):
             if self.company_id.tax_framework in TAX_FRAMEWORK_SIMPLES_ALL
             else '2',
             'incentivador_cultural': '1'
-            if self.company_id.cultural_encourager else '2',
+            if self.company_id.cultural_sponsor else '2',
             'status': '1',
             'rps_substitiuido': None,
             'intermediario_servico': None,
