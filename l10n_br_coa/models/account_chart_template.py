76937724d9cc738d1e18d1169ab09553bbc085ef
Renato Lima <renato.lima@akretion.com>
2020-08-26 17:04:30 -0300

    [REF] remove defaults account_sale_tax_id and account_purchase_tax_id in l10n_br we config taxes using fiscal definition object

--- l10n_br_coa/models/account_chart_template.py
+++ l10n_br_coa/models/account_chart_template.py
@@ -8,8 +8,18 @@ class AccountChartTemplate(models.Model):
     _inherit = 'account.chart.template'
 
     def try_loading_for_current_company(self):
+        self.ensure_one()
         if self.currency_id == self.env.ref('base.BRL'):
             for template in \
                     self.env['l10n_br_account.tax.template'].search([]):
                 template.create_account_tax_templates(self.id)
         return super().try_loading_for_current_company()
+
+    def load_for_current_company(self, sale_tax_rate, purchase_tax_rate):
+        super().load_for_current_company(sale_tax_rate, purchase_tax_rate)
+        # Remove Company default taxes configuration
+        if self.currency_id == self.env.ref('base.BRL'):
+            self.env.user.company_id.write({
+                'account_sale_tax_id': False,
+                'account_purchase_tax_id': False,
+            })
