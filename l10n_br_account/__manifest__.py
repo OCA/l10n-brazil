be18319d208998cf3b20abae3aaaaa26b1c05c59
Renato Lima <renatonlima@gmail.com>
2020-08-27 20:18:42 -0300

    [REF] rename file names

--- l10n_br_account/__manifest__.py
+++ l10n_br_account/__manifest__.py
@@ -14,8 +14,8 @@
         'security/ir.model.access.csv',
 
         # data
-        "data/l10n_br_account_tax_data.xml",
-        "data/l10n_br_account_tax_group_data.xml",
+        "data/account_tax_group.xml",
+        "data/account_tax_template.xml",
 
         # Views
         "views/account_tax_view.xml",
