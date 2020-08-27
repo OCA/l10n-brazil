31b9ab6cd63adcf834a0c9b829936399aa6eeaa7
Luis Malta <luis.malta@kmee.com.br>
2020-08-27 12:39:11 -0300

    [ADD] Updated README

--- l10n_br_nfse_ginfes/__manifest__.py
+++ l10n_br_nfse_ginfes/__manifest__.py
@@ -2,12 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Edoc Nfse Ginfes',
+    'name': 'NFS-e (Ginfes)',
     'summary': """
-        NFS-E Ginfes""",
+        NFS-e (Ginfes)""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE, Odoo Community Association (OCA)',
+    'maintainers': ['gabrielcardoso21', 'mileo', 'luismalta'],
     'website': 'https://github.com/OCA/l10n-brazil',
     'external_dependencies': {
         'python': [
