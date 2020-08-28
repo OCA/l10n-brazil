264743b7206d7cadbad6ee2911f9462489248830
Luis Malta <luis.malta@kmee.com.br>
2020-08-28 09:50:36 -0300

    [REF] Rename module l10n_br_nfse_ginfes

--- l10n_br_nfse_ginfes/__manifest__.py
+++ l10n_br_nfse_ginfes/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Edoc Nfse Ginfes',
+    'name': 'NFS-e (Ginfes)',
     'summary': """
-        NFS-E Ginfes""",
+        NFS-e (Ginfes)""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE, Odoo Community Association (OCA)',
