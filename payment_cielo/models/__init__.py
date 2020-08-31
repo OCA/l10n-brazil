6323c0bc64966fcbcb9170caaba8c8a62b97a32b
Diego Paradeda <paradeda.diego@gmail.com>
2020-08-31 12:36:31 -0300

    [FIX] Copyright

--- payment_cielo/models/__init__.py
+++ payment_cielo/models/__init__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Copyright 2020 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import payment_acquirer
 from . import payment_transaction
