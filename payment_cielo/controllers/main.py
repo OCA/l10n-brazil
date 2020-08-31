abbaec361dcc9aba85c89cbb2263f24243e98865
Diego Paradeda <paradeda.diego@gmail.com>
2020-08-31 12:43:16 -0300

    [REF] Cleaner code

--- payment_cielo/controllers/main.py
+++ payment_cielo/controllers/main.py
@@ -12,7 +12,7 @@ from odoo.addons.payment.controllers.portal import PaymentProcessing
 _logger = logging.getLogger(__name__)
 
 
-class StripeController(http.Controller):
+class CieloController(http.Controller):
 
     @http.route(['/payment/cielo'], type='json', auth='public')
     def payment_cielo(self, **kwargs):
@@ -124,7 +124,7 @@ class StripeController(http.Controller):
             response = tx._create_cielo_charge(acquirer_ref=payment_token_id.acquirer_ref, email=cielo_token['email'])
         else:
             response = tx._create_cielo_charge(tokenid=cielo_token['id'], email=cielo_token['email'])
-        _logger.info('Stripe: entering form_feedback with post data %s', pprint.pformat(response))
+        _logger.info('Cielo: entering form_feedback with post data %s', pprint.pformat(response))
         if response:
             request.env['payment.transaction'].sudo().with_context(lang=None).form_feedback(response, 'cielo')
         # add the payment transaction into the session to let the page /payment/process to handle it
--- payment_cielo/tests/__init__.py
+++ payment_cielo/tests/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import test_stripe
+from . import test_cielo
