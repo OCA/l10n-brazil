96cd9269936c20c1beee3286d060b4ac1f2418f4
Luis Malta <luis.malta@kmee.com.br>
2020-09-03 08:46:21 -0300

    [REF] CNAE Code on prepare_line_servico

--- l10n_br_nfse/models/document.py
+++ l10n_br_nfse/models/document.py
@@ -133,7 +133,7 @@ class Document(models.Model):
     def _prepare_dados_servico(self):
         self.line_ids.ensure_one()
         result = {
-            'codigo_cnae': None,
+
         }
         result.update(self.line_ids.prepare_line_servico())
         result.update(self.company_id.prepare_company_servico())
