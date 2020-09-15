cfe28fd144517dcd9046d9487ce6bc511828f33d
Magno Costa <magno.costa@akretion.com.br>
2020-09-15 17:00:21 -0300

    [REF] Create CNAB Return Log, to store the file and the lines because there is the possibility to file don't create account.move .

--- l10n_br_account_payment_brcobranca/__manifest__.py
+++ l10n_br_account_payment_brcobranca/__manifest__.py
@@ -20,6 +20,8 @@
         'views/l10n_br_cnab_retorno_view.xml',
         'views/res_config_settings_view.xml',
         'views/account_journal_view.xml',
+        'views/account_move_view.xml',
+        'views/cnab_return_log_view.xml',
         'data/res_config_settings_data.xml',
         'security/ir.model.access.csv',
     ],
