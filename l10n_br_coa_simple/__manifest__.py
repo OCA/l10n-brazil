c8e4f2170ea0c008e58a6b0be708ad371985ad7f
Renato Lima <renato.lima@akretion.com>
2020-09-15 19:13:50 -0300

    [ADD] Account Tax group properties

--- l10n_br_coa_simple/__manifest__.py
+++ l10n_br_coa_simple/__manifest__.py
@@ -14,6 +14,7 @@
         "data/l10n_br_coa_simple_template.xml",
         "data/account_group.xml",
         "data/account.account.template.csv",
+        "data/account_tax_group.xml",
         "data/l10n_br_coa_simple_template_post.xml",
     ],
     "post_init_hook": "post_init_hook",
