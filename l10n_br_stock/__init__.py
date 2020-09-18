406aca9020bb5f3595d90a618f19e3d77b0fd623
Luis Felipe Mileo <mileo@kmee.com.br>
2020-09-18 16:03:13 -0300

    [NEW] l10n_br_stock external ids of warehouse records
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_stock/__init__.py
+++ l10n_br_stock/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (C) 2009  Renato Lima - Akretion
+# Copyright (C) 2020  Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
+from .hooks import post_init_hook
