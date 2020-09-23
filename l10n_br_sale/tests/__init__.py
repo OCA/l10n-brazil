e57f717f932bb5b411c5c5b4aeb113fb6e2f8f1e
Renato Lima <renato.lima@akretion.com>
2020-09-22 22:49:43 -0300

    Rename files

--- l10n_br_sale/tests/__init__.py
+++ l10n_br_sale/tests/__init__.py
@@ -1,5 +1,5 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import test_l10n_br_sale
-from . import test_l10n_br_sale_product
-from . import test_sale_order_service
+from . import test_l10n_br_sale_sn
+from . import test_l10n_br_sale_lc
