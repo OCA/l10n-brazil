d90a747fbb287a4fb9c6228bf17d195ff97dec97
Diego Paradeda <paradeda.diego@gmail.com>
2020-09-24 16:30:50 -0300

    [ADD] Tax fields, insurance and other_costs from sale order line to stock move

--- l10n_br_sale_stock/models/stock_rule.py
+++ l10n_br_sale_stock/models/stock_rule.py
@@ -5,9 +5,24 @@ from odoo import api, models
 
 
 class StockRule(models.Model):
-    """ A rule describe what a procurement should do; produce, buy, move, ... """
+    """ A rule describe what a procurement should do; produce, buy, move,
+    ... """
     _inherit = 'stock.rule'
 
     def _get_custom_move_fields(self):
         res = super(StockRule, self)._get_custom_move_fields()
-        return res + ['freight_value']
+        res += [
+            'freight_value',
+            'insurance_value',
+            'other_costs_value',
+            'cofins_tax_id',
+            'cofins_cst_id',
+            'pis_tax_id',
+            'pis_cst_id',
+            'ipi_tax_id',
+            'ipi_cst_id',
+            'icmssn_tax_id',
+            'icms_tax_id',
+            'icms_cst_id',
+            ]
+        return res
