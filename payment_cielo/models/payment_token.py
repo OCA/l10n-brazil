9ad4221bb04132acf24cc5aa9177ea6b82a0b0b5
Diego Paradeda <paradeda.diego@gmail.com>
2020-10-06 17:09:46 -0300

    [ADD] Tokenize

--- payment_cielo/models/payment_token.py
+++ payment_cielo/models/payment_token.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
+import pprint
+import requests
 
 from odoo import api, fields, models
 
@@ -14,31 +16,72 @@ class PaymentTokenCielo(models.Model):
     card_number = fields.Char(
         string="Number",
         required=False,
-    )
+        )
 
     card_holder = fields.Char(
         string="Holder",
         required=False,
-    )
+        )
 
     card_exp = fields.Char(
         string="Expiration date",
         required=False,
-    )
+        )
 
     card_cvc = fields.Char(
         string="cvc",
         required=False,
-    )
+        )
 
     card_brand = fields.Char(
         string="Brand",
         required=False,
-    )
+        )
+
+    cielo_token = fields.Char(
+        string="Token",
+        required=False,
+        )
+
+    def _cielo_tokenize(self, values):
+        aquirer_id = self.env.ref('payment_cielo.payment_acquirer_cielo')
+        api_url_create_card = 'https://%s/1/card' % (
+            aquirer_id._get_cielo_api_url())
+
+        partner_id = self.env['res.partner'].browse(values['partner_id'])
+        cielo_expiry = str(values['cc_expiry'][:2]) + '/20' + str(
+            values['cc_expiry'][-2:])
+
+        if values['cc_brand'] == 'mastercard':
+            values['cc_brand'] = 'master'
+
+        tokenize_params = {
+            "CustomerName": partner_id.name,
+            "CardNumber": values['cc_number'].replace(' ', ''),
+            "Holder": values['cc_holder_name'],
+            "ExpirationDate": cielo_expiry,
+            "Brand": values['cc_brand'],
+            }
+
+        _logger.info(
+            '_cielo_tokenize: Sending values to URL %s, values:\n%s',
+            api_url_create_card, pprint.pformat(tokenize_params))
+        r = requests.post(api_url_create_card,
+                          json=tokenize_params,
+                          headers=aquirer_id._get_cielo_api_headers())
+        # TODO: Salvar token
+        res = r.json()
+        _logger.info('_create_cielo_charge: Values received:\n%s',
+                     pprint.pformat(res))
+
+        return res
 
     @api.model
     def cielo_create(self, values):
         token = values.get('cielo_token')
+
+        token = self._cielo_tokenize(values)
+        values['card_token'] = token['CardToken']
         description = None
         # payment_acquirer = self.env['payment.acquirer'].browse(
         #     values.get('acquirer_id'))
@@ -64,7 +107,7 @@ class PaymentTokenCielo(models.Model):
         customer_params = {
             # 'source': token['id'],
             'description': description or token["card"]["name"]
-        }
+            }
 
         res = {
             'acquirer_ref': partner_id.id,
@@ -76,12 +119,13 @@ class PaymentTokenCielo(models.Model):
             'card_cvc': values['cvc'],
             'card_holder': values['cc_holder_name'],
             'card_brand': values['cc_brand'],
-        }
+            'cielo_token': values['card_token'],
+            }
 
         # pop credit card info to info sent to create
-        # for field_name in ["cc_number", "cvc", "cc_holder_name",
-        # "cc_expiry", "cc_brand", "cielo_token"]:
-        #     values.pop(field_name, None)
+        for field_name in ["card_number", "card_cvc", "card_holder",
+        "card_exp"]:
+            res.pop(field_name, None)
         return res
 
     # CURRENTLY UNUSED
