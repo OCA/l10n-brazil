de971cf5f44467e544feb69cffd874832d93b155
Renato Lima <renatonlima@gmail.com>
2020-10-07 12:16:46 -0300

    [FIX] informações adicionais da NF-e

--- l10n_br_nfe/models/document_line.py
+++ l10n_br_nfe/models/document_line.py
@@ -137,6 +137,10 @@ class NFeLine(spec_models.StackedModel):
         related='ipi_value',
     )
 
+    nfe40_infAdProd = fields.Char(
+        related='additional_data',
+    )
+
     @api.depends('icms_cst_id')
     def _compute_choice11(self):
         for record in self:
