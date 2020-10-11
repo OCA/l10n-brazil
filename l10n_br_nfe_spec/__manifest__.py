f4012f1f2d7ed18e17689309760fa38a0c6cfb23
Raphaël Valyi <rvalyi@akretion.com>
2020-10-11 12:26:27 -0300

    [ADD] l10n_br_nfe_spec clean initial addition

---
+++ l10n_br_nfe_spec/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    'name': 'nfe spec',
+    'version': '12.0.1.0.0',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'license': 'LGPL-3',
+    'category': 'Accounting',
+    'summary': 'nfe spec',
+    'depends': ['base'],
+    'installable': True,
+    'application': False,
+}
