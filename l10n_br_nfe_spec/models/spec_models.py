7c3954c33041979a5aac4f61604cd7250948dd7a
Raphaël Valyi <rvalyi@akretion.com>
2020-10-11 12:26:27 -0300

    [ADD] l10n_br_nfe_spec clean initial addition

---
+++ l10n_br_nfe_spec/models/spec_models.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+
+class NfeSpecMixin(models.AbstractModel):
+    _description = "Abstract Model"
+    _name = 'spec.mixin.nfe'
+    # TODO exact schema version
+    # TODO tab name...
+
+    brl_currency_id = fields.Many2one(
+        comodel_name='res.currency',
+        string='Moeda',
+        compute='_compute_brl_currency_id',
+        default=lambda self: self.env.ref('base.BRL').id,
+    )
+
+    def _compute_brl_currency_id(self):
+        for item in self:
+            item.currency_id = self.env.ref('base.BRL').id
