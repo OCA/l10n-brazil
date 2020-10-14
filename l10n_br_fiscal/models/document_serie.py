c66915443615532683d4fe74c8204a21e6534780
Renato Lima <renatonlima@gmail.com>
2020-10-14 13:24:41 -0300

    [ADD] Invalid number o2m field in document serie

--- l10n_br_fiscal/models/document_serie.py
+++ l10n_br_fiscal/models/document_serie.py
@@ -46,7 +46,14 @@ class DocumentSerie(models.Model):
         string='Sequence')
 
     sequence_number_next = fields.Integer(
-        related='internal_sequence_id.number_next')
+        related='internal_sequence_id.number_next',
+    )
+
+    invalidate_number_id = fields.One2many(
+        comodel_name='l10n_br_fiscal.invalidate.number',
+        inverse_name='document_serie_id',
+        string='Invalidate Number Range',
+    )
 
     @api.model
     def _create_sequence(self, values):
