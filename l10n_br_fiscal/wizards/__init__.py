b6115830275a3a45aebf5e72c442cc8feaf48626
Renato Lima <renatonlima@gmail.com>
2020-10-14 10:39:31 -0300

    Rename wizards files names

--- l10n_br_fiscal/wizards/__init__.py
+++ l10n_br_fiscal/wizards/__init__.py
@@ -1,6 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import base_wizard_mixin
-from . import wizard_document_cancel
-from . import wizard_document_correction
-from . import wizard_document_invalidate
+from . import document_cancel_wizard
+from . import document_correction_wizard
+from . import invalidate_number_wizard
