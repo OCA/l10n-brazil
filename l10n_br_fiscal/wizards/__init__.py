f744f52f1bf48c09c1e95ea2915bebcc15ff203a
Renato Lima <renatonlima@gmail.com>
2020-10-14 10:11:08 -0300

    [ADD] Basse Wizard Mixin

--- l10n_br_fiscal/wizards/__init__.py
+++ l10n_br_fiscal/wizards/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import base_wizard_mixin
 from . import wizard_document_status
 from . import wizard_document_cancel
 from . import wizard_document_correction
