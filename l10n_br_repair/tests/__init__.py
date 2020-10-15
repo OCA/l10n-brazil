86589c6a040172b91314347ca79add5df96429a6
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-10-15 02:03:27 -0300

    [IMP] iniciando desenvolvimento de testes utilizando os testes do modulo l10n_br_sale

---
+++ l10n_br_repair/tests/__init__.py
@@ -0,0 +1,5 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+# from . import test_l10n_br_repair
+# from . import test_l10n_br_repair_sn
+# from . import test_l10n_br_repair_lc
