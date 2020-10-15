19329de623701157fadb084319976437044c43d0
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-10-15 02:42:50 -0300

    [FIX] readme

--- l10n_br_repair/readme/CONTRIBUTORS.rst
+++ l10n_br_repair/readme/CONTRIBUTORS.rst
@@ -2,3 +2,4 @@
 * Eduardo Aparício <eduardo.aparicio@escodoo.com.br>
 * Renato Lima <renato.lima@akretion.com.br>
 * Luis Felipe Mileo <mileo@kmee.com.br>
+* Raphaël Valyi <raphael.valyi@akretion.com.br>
--- l10n_br_repair/tests/test_l10n_br_repair_sn.py
+++ l10n_br_repair/tests/test_l10n_br_repair_sn.py
@@ -1,7 +1,5 @@
-# @ 2019 Akretion - www.akretion.com.br -
-#   Magno Costa <magno.costa@akretion.com.br>
-#   Renato Lima <renato.lima@akretion.com.br>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2020 - TODAY, Marcel Savegnago - Escodoo - www.escodoo.com.br
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from .test_l10n_br_repair import L10nBrRepairBaseTest
 
