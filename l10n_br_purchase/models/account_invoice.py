d35d70238666e5dabad134693ed415d18eb9b639
Renato Lima <renato.lima@akretion.com>
2020-10-20 16:17:30 -0300

    [REF] account.invoice methods

--- l10n_br_purchase/models/account_invoice.py
+++ l10n_br_purchase/models/account_invoice.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2020  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
