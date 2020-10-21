3132317fd9024ec742f31d42486b577413cf4364
Renato Lima <renatonlima@gmail.com>
2020-10-20 22:38:22 -0300

    [FIX] l10n_br_purchase tests

---
+++ l10n_br_purchase/tests/test_l10n_br_purchase_sn.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2020  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+from .test_l10n_br_purchase import L10nBrPurchaseBaseTest
+
+
+class TestL10nBrPurchaseSN(L10nBrPurchaseBaseTest):
+
+    def setUp(self):
+        super().setUp()
+        self.company = self.env.ref('l10n_br_base.empresa_simples_nacional')
+        self.po_products = self.env.ref('l10n_br_purchase.sn_po_only_products')
