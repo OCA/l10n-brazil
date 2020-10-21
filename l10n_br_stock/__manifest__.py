73818f2d505596ee59cc75ac3edddcbb88487efe
Luis Felipe Mileo <mileo@kmee.com.br>
2020-10-21 00:42:08 -0300

    [MIG][l10n_br_stock] Bump Module version
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_stock/__manifest__.py
+++ l10n_br_stock/__manifest__.py
@@ -4,7 +4,7 @@
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
     "website": "http://odoo-brasil.org",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "depends": ["stock", "l10n_br_base"],
     "data": ["views/stock_view.xml"],
     "demo": [
