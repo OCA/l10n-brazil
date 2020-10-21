2fec50f935bb8a069908ea7b007b627717b307da
Renato Lima <renatonlima@gmail.com>
2020-10-20 22:38:37 -0300

    [FIX] l10n_br_purchase_stock tests

--- l10n_br_purchase_stock/tests/test_l10n_br_purchase_stock.py
+++ l10n_br_purchase_stock/tests/test_l10n_br_purchase_stock.py
@@ -6,7 +6,22 @@
 from odoo.addons.l10n_br_purchase.tests import test_l10n_br_purchase
 
 
-class L10nBrPurchase(test_l10n_br_purchase.L10nBrPurchaseBaseTest):
+class L10nBrPurchaseStockBase(test_l10n_br_purchase.L10nBrPurchaseBaseTest):
 
-    def setUp(self):
-        pass
+    def _picking_purchase_order(self, order):
+        self.assertEqual(
+            order.picking_count, 1,
+            'Purchase: one picking should be created"'
+        )
+
+        picking = order.picking_ids[0]
+        # TODO write qty_done with qty ordered
+        picking.move_line_ids.write({'qty_done': 5.0})
+        picking.button_validate()
+        self.assertEqual(
+            order.order_line.mapped('qty_received'), [4.0, 2.0],
+            'Purchase: all products should be received"')
+
+    def test_l10n_br_purchase_products(self):
+        super().test_l10n_br_purchase_products()
+        self._picking_purchase_order(self.po_products)
