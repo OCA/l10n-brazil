5a964fd318185015cd93366f1c46e2241f3318a6
Diego Paradeda <paradeda.diego@gmail.com>
2020-10-26 14:34:10 -0300

    [ADD] Better tests

--- payment_cielo/tests/test_cielo.py
+++ payment_cielo/tests/test_cielo.py
@@ -16,13 +16,12 @@ class CieloCommon(PaymentAcquirerCommon):
         self.cielo.write({
             'cielo_merchant_id': 'be87a4be-a40d-4a2d-b2c8-b8b6cc19cddd',
             'cielo_merchant_key': 'POHAWRXFBSIXTMTFVBCYSKNWZBMOATDNYUQDGBUE',
-        })
+            })
 
 
-@odoo.tests.tagged('post_install', '-at_install', '-standard', 'external')
+@odoo.tests.tagged('post_install', '-at_install')
 class CieloTest(CieloCommon):
 
-    @unittest.skip("")
     def test_10_cielo_s2s(self):
         self.assertEqual(self.cielo.environment, 'test',
                          'test without test environment')
@@ -31,7 +30,7 @@ class CieloTest(CieloCommon):
         self.cielo.write({
             'cielo_merchant_id': 'be87a4be-a40d-4a2d-b2c8-b8b6cc19cddd',
             'cielo_merchant_key': 'POHAWRXFBSIXTMTFVBCYSKNWZBMOATDNYUQDGBUE',
-        })
+            })
 
         # Create payment meethod for Cielo
         payment_token = self.env['payment.token'].create({
@@ -42,7 +41,7 @@ class CieloTest(CieloCommon):
             'cc_brand': 'visa',
             'cvc': '111',
             'cc_holder_name': 'Johndoe',
-        })
+            })
 
         # Create transaction
         tx = self.env['payment.transaction'].create({
@@ -53,26 +52,16 @@ class CieloTest(CieloCommon):
             'payment_token_id': payment_token.id,
             'type': 'server2server',
             'amount': 115.0
-        })
+            })
         tx.cielo_s2s_do_transaction()
-
-    def test_20_cielo_form_render(self):
-        self.assertEqual(self.cielo.environment, 'test',
-                         'test without test environment')
-
-        # ----------------------------------------
-        # Test: button direct rendering
-        # ----------------------------------------
-
-        # render the button
-        self.env['payment.transaction'].create({
-            'acquirer_id': self.cielo.id,
-            'amount': 320.0,
-            'reference': 'SO404',
-            'currency_id': self.currency_euro.id,
-        })
-        self.cielo.render('SO404', 320.0, self.currency_euro.id,
-                          values=self.buyer_values).decode('utf-8')
+        self.assertEqual(tx.state, 'authorized',
+                         'transaction state should be authorized')
+        tx.action_capture()
+        self.assertEqual(tx.state, 'done',
+                         'transaction state should be done')
+        tx.cielo_s2s_void_transaction()
+        self.assertEqual(tx.state, 'done',
+                         'transaction state should be done')
 
     @unittest.skip(
         "as the test is post-install and because payment_strip_sca changes"
@@ -106,38 +95,43 @@ class CieloTest(CieloCommon):
             u'receipt_email': None,
             u'receipt_number': None,
             u'refunded': False,
-            u'refunds': {u'data': [],
-                         u'has_more': False,
-                         u'object': u'list',
-                         u'total_count': 0,
-                         u'url':
-                             u'/v1/charges/ch_172xfnGMfVJxozLwEjSfpfxD/refunds'},
+            u'refunds': {
+                u'data': [],
+                u'has_more': False,
+                u'object': u'list',
+                u'total_count': 0,
+                u'url':
+                    u'/v1/charges/ch_172xfnGMfVJxozLwEjSfpfxD/refunds'
+                },
             u'shipping': None,
-            u'source': {u'address_city': None,
-                        u'address_country': None,
-                        u'address_line1': None,
-                        u'address_line1_check': None,
-                        u'address_line2': None,
-                        u'address_state': None,
-                        u'address_zip': None,
-                        u'address_zip_check': None,
-                        u'brand': u'Visa',
-                        u'country': u'US',
-                        u'customer': None,
-                        u'cvc_check': u'pass',
-                        u'dynamic_last4': None,
-                        u'exp_month': 2,
-                        u'exp_year': 2022,
-                        u'fingerprint': u'9tJA9bUEuvEb3Ell',
-                        u'funding': u'credit',
-                        u'id': u'card_172xfjGMfVJxozLw1QO6gYNM',
-                        u'last4': u'4242',
-                        u'metadata': {},
-                        u'name': u'norbert.buyer@example.com',
-                        u'object': u'card',
-                        u'tokenization_method': None},
+            u'source': {
+                u'address_city': None,
+                u'address_country': None,
+                u'address_line1': None,
+                u'address_line1_check': None,
+                u'address_line2': None,
+                u'address_state': None,
+                u'address_zip': None,
+                u'address_zip_check': None,
+                u'brand': u'Visa',
+                u'country': u'US',
+                u'customer': None,
+                u'cvc_check': u'pass',
+                u'dynamic_last4': None,
+                u'exp_month': 2,
+                u'exp_year': 2022,
+                u'fingerprint': u'9tJA9bUEuvEb3Ell',
+                u'funding': u'credit',
+                u'id': u'card_172xfjGMfVJxozLw1QO6gYNM',
+                u'last4': u'4242',
+                u'metadata': {},
+                u'name': u'norbert.buyer@example.com',
+                u'object': u'card',
+                u'tokenization_method': None
+                },
             u'statement_descriptor': None,
-            u'status': u'succeeded'}
+            u'status': u'succeeded'
+            }
 
         tx = self.env['payment.transaction'].create({
             'amount': 4700.0,
@@ -145,7 +139,8 @@ class CieloTest(CieloCommon):
             'currency_id': self.currency_euro.id,
             'reference': 'SO100-1',
             'partner_name': 'Norbert Buyer',
-            'partner_country_id': self.country_france.id})
+            'partner_country_id': self.country_france.id
+            })
 
         # validate it
         tx.form_feedback(cielo_post_data, 'Cielo')
@@ -161,16 +156,21 @@ class CieloTest(CieloCommon):
             'currency_id': self.currency_euro.id,
             'reference': 'SO100-2',
             'partner_name': 'Norbert Buyer',
-            'partner_country_id': self.country_france.id})
+            'partner_country_id': self.country_france.id
+            })
         # simulate an error
         cielo_post_data['status'] = 'error'
-        cielo_post_data.update({u'error': {
-            u'message': u"Your card's expiration year is invalid.",
-            u'code': u'invalid_expiry_year', u'type': u'card_error',
-            u'param': u'exp_year'}})
+        cielo_post_data.update({
+            u'error': {
+                u'message': u"Your card's expiration year is invalid.",
+                u'code': u'invalid_expiry_year', u'type': u'card_error',
+                u'param': u'exp_year'
+                }
+            })
         with mute_logger('odoo.addons.payment_cielo.models.payment'):
             with mute_logger('odoo.addons.payment_cielo_sca.models.payment'):
                 tx.form_feedback(cielo_post_data, 'cielo')
         # check state
         self.assertEqual(tx.state, 'cancel',
-                         'Stipe: erroneous validation did not put tx into error state')
+                         'Cielo: erroneous validation did not put tx into '
+                         'error state')
