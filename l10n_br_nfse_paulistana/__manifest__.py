c1dc7c7ce19252ec2bc090074b921d5c9b53de76
Luis Malta <luis.malta@kmee.com.br>
2020-10-27 11:26:01 -0300

    [FIX] manifest module name

--- l10n_br_nfse_paulistana/__manifest__.py
+++ l10n_br_nfse_paulistana/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Edoc Nfse Paulistana',
+    'name': 'NFS-e (Nota Paulistana)',
     'summary': """
-        NFS-E Paulistana""",
+        NFS-e (Nota Paulistana)""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE, Odoo Community Association (OCA)',
