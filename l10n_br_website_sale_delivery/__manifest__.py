25e1a47c341c85e112791e0da6ab8837bcd838ff
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2020-10-27 10:27:56 -0300

    [REF] Rename module and fix dependencies

---
+++ l10n_br_website_sale_delivery/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Website Sale Delivery',
+    'summary': """
+        Implements Brazilian freight values for delivery.""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'www.kmee.com.br',
+    'depends': [
+        'website_sale_delivery',
+    ],
+    'data': [
+        'views/website_sale_delivery_templates.xml'
+    ],
+    'demo': [
+    ],
+}
