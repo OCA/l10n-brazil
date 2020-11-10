0522b17cd9e1378c53794a6f2469a7890c1cadc8
Magno Costa <magno.costa@akretion.com.br>
2020-11-10 15:26:22 -0300

    [IMP+FIX] CNAB Movement Instruction Code 02, forbidden register account.payment with CNAB payment methods.

--- l10n_br_account_payment_order/models/account_payment.py
+++ l10n_br_account_payment_order/models/account_payment.py
@@ -3,7 +3,8 @@
 #   @author  Hugo Uchôas Borges <hugo.borges@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, models, _
+from odoo.exceptions import UserError
 
 
 class AccountPayment(models.Model):
@@ -11,6 +12,25 @@ class AccountPayment(models.Model):
 
     @api.multi
     def post(self):
+
+        # TODO - Rodando 2 For para não chamar o super em caso do
+        #  metodo de pagto ser CNAB, necessário devido o problema abaixo
+        for rec in self:
+            if rec.payment_method_code in ('240', '400', '500'):
+                # TODO - Idealmente isso deveria ser resolvido com um
+                #  domain=[('code', 'not in', ('400','240','500'))]
+                #  no campo payment_method_id, mas mesmo adicionando isso na
+                #  visão ou sobre escrevendo o campo não funciona e não gera
+                #  erros, não consegui identificar o motivo do problema (
+                #  seria algum problema no orm ? Não encontrei um issue aberto
+                #  com referencia a isso ), para reproduzir o erro basta tentar
+                #  incluir o domain no campo como comentado acima e testar
+                #  na tela.
+                #  Testar na migração.
+                raise UserError(_(
+                    'CNAB Payment Method can be used to make'
+                    ' Payments here, just used in Payment Orders,'
+                    ' choose another one.'))
         super().post()
         for record in self:
             record.invoice_ids.create_account_payment_line_baixa(record.amount)
