1f628f9811be400cb1e4fb5dbd31164d4c1c25f3
Diego Paradeda <paradeda.diego@gmail.com>
2020-11-10 12:23:38 -0300

    [REF] Remove no longer used files and set v8 -> v12

--- l10n_br_delivery/__manifest__.py
+++ l10n_br_delivery/__manifest__.py
@@ -3,24 +3,22 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Brazilian Localization Delivery',
+    'name': 'L10n Br Delivery',
+    'summary': """
+        This module changes the delivery model strategy to match brazilian
+        standards.""",
     'license': 'AGPL-3',
-    'author': 'Akretion, Odoo Community Association (OCA)',
-    'website': 'http://odoo-brasil.org',
-    'version': '8.0.1.0.0',
+    'author': 'KMEE INFORMATICA LTDA,Odoo Community Association (OCA)',
+    'website': 'kmee.com.br',
+    'version': '"12.0.2.1.0',
     'depends': [
         'l10n_br_fiscal',
         'l10n_br_sale_stock',
         'delivery',
     ],
     'data': [
-        'views/account_invoice_view.xml',
-        'views/delivery_view.xml',
-        'views/stock_view.xml',
-        'views/l10n_br_delivery_view.xml',
-        'security/ir.model.access.csv',
     ],
     'demo': [],
     'category': 'Localisation',
-    'installable': False,
+    'installable': True,
 }
--- l10n_br_delivery/models/__init__.py
+++ l10n_br_delivery/models/__init__.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2015  Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import account_invoice
-from . import delivery
-from . import l10n_br_delivery
-from . import sale
-from . import stock
+from . import sale_order
+from . import stock_picking
 from . import document
