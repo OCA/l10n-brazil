4807d643e4a49ca03256a09c3b9b867a2c8454b3
Magno Costa <magno.costa@akretion.com.br>
2020-11-14 17:41:44 -0300

    [FIX] CNAB : Movement Instruction Code can be different.

---
+++ l10n_br_account_payment_order/models/l10n_br_cnab_mov_intruction_code.py
@@ -0,0 +1,51 @@
+# Copyright 2020 Akretion
+# @author Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, fields
+
+
+class L10nBrCNABMovInstructionCode(models.Model):
+    _name = 'l10n_br_cnab.mov.instruction.code'
+    _description = 'CNAB Movement Instruction Code'
+
+    name = fields.Char(
+        string='Name',
+        index=True,
+    )
+    code = fields.Char(
+        string='Code',
+        index=True,
+    )
+    bank_id = fields.Many2one(
+        string='Bank',
+        comodel_name='res.bank',
+        index=True,
+    )
+    payment_method_id = fields.Many2one(
+        comodel_name='account.payment.method',
+        string='Payment Method',
+        index=True,
+    )
+    # Fields used to create domain
+    bank_code_bc = fields.Char(
+        related='bank_id.code_bc',
+        store=True,
+    )
+    payment_method_code = fields.Char(
+        related='payment_method_id.code',
+        store=True,
+    )
+    comment = fields.Text(
+        string='Comment'
+    )
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            result.append((
+                record.id,  '%s - %s' % (
+                    record.code, record.name)
+            ))
+        return result
