7635a64e7ec2140536d35408e3284bfd05f4b17c
Magno Costa <magno.costa@akretion.com.br>
2020-11-17 11:31:29 -0300

    [FIX+REF] CNAB: Movement Instruction and Return Code.

--- l10n_br_account_payment_order/models/l10n_br_cnab_mov_intruction_code.py
+++ l10n_br_account_payment_order/models/l10n_br_cnab_mov_intruction_code.py
@@ -2,7 +2,7 @@
 # @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class L10nBrCNABMovInstructionCode(models.Model):
