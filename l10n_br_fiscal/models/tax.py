abe04fad70bce4e6f1aa1d850608e358d15ec50b
Luis Felipe Mileo <mileo@kmee.com.br>
2020-11-17 23:46:52 -0300

    [FIX] Calculo ICMS Simples Nacional

--- l10n_br_fiscal/models/tax.py
+++ l10n_br_fiscal/models/tax.py
@@ -476,7 +476,7 @@ class Tax(models.Model):
         if partner.ind_ie_dest in (NFE_IND_IE_DEST_1, NFE_IND_IE_DEST_2):
             if cst.code in ICMS_SN_CST_WITH_CREDIT:
                 icms_sn_percent = round(
-                    icmssn_range.total_tax_percent *
+                    company.simplifed_tax_percent *
                     (icmssn_range.tax_icms_percent / 100), 2)
 
                 tax_dict["percent_amount"] = icms_sn_percent
