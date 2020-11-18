5254b48ba80a35dd5d2cdb6030ddd9351261332f
Magno Costa <magno.costa@akretion.com.br>
2020-11-18 12:28:29 -0300

    [IMP] Relation between Journal Entry and CNAB Log.

--- l10n_br_account_payment_order/models/account_move.py
+++ l10n_br_account_payment_order/models/account_move.py
@@ -1,12 +1,25 @@
 # © 2019 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, models
+from odoo import _, api, models, fields
 from odoo.exceptions import ValidationError
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    cnab_return_log_id = fields.Many2one(
+        string='CNAB Return Log',
+        comodel_name='l10n_br_cnab.return.log',
+        readonly=True,
+        inverse_name='move_id'
+    )
+
+    # Usados para deixar invisivel o campo
+    # relacionado ao CNAB na visao
+    is_cnab = fields.Boolean(
+        string='Is CNAB ?'
+    )
 
     @api.multi
     def unlink(self):
