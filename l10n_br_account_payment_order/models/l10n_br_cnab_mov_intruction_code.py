7fb3547bf0ae09291a900f640645b51d12e106f6
Magno Costa <magno.costa@akretion.com.br>
2020-11-24 10:16:48 -0300

    [FIX] CNAB 240 is not standardized.

--- l10n_br_account_payment_order/models/l10n_br_cnab_mov_intruction_code.py
+++ l10n_br_account_payment_order/models/l10n_br_cnab_mov_intruction_code.py
@@ -10,6 +10,24 @@ class L10nBrCNABMovInstructionCode(models.Model):
     _inherit = 'l10n_br_cnab.data.abstract'
     _description = 'CNAB Movement Instruction Code'
 
+    bank_ids = fields.Many2many(
+        string='Bank',
+        comodel_name='res.bank',
+        relation='l10n_br_cnab_mov_instruction_code_bank_rel',
+        column1='bank_id',
+        column2='l10n_br_cnab_mov_instruction_code_id',
+        track_visibility='always',
+    )
+
+    payment_method_ids = fields.Many2many(
+        comodel_name='account.payment.method',
+        string='Payment Method',
+        relation='l10n_br_cnab_mov_instruction_code_payment_method_rel',
+        column1='payment_method_id',
+        column2='l10n_br_cnab_mov_instruction_code_id',
+        track_visibility='always',
+    )
+
     comment = fields.Text(
         string='Comment'
     )
