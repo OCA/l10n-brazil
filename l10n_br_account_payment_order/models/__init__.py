8f31a49e398e518f8b71f4f9834c28a26f6eaf2b
Luis Felipe Mileo <mileo@kmee.com.br>
2020-11-25 09:29:41 -0300

    [NEW] bc_code no account.journal

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -4,6 +4,7 @@
 
 from . import res_company
 from . import account_invoice
+from . import account_journal
 from . import account_move
 from . import account_move_line
 from . import account_payment_mode
