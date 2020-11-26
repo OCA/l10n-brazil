e4c4a8dbab5f6ee0db3747fc8ddea276f2001f95
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2020-11-26 13:42:01 -0300

    [IMP] Invoice pickings to be invoiced before invoice sale order

---
+++ l10n_br_sale_stock/models/sale_order.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2020  Gabriel Cardoso de Faria - KMEE
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_invoice_create(self):
+        picking_ids = self.picking_ids.filtered(
+            lambda p: p.invoice_state == '2binvoiced' and p.state == 'done')
+        if picking_ids:
+            wizard = self.env['stock.invoice.onshipping'].with_context(
+                active_model='stock.picking',
+                active_ids=picking_ids.ids).create({})
+            wizard.write(wizard.default_get(wizard.fields_get().keys()))
+            invoices = wizard._action_generate_invoices()
+            wizard._update_picking_invoice_status(
+                invoices.mapped("picking_ids"))
+        result = super().action_invoice_create()
+        return result + invoices.ids if invoices else result
