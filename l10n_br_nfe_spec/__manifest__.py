489a99a8ad04c6fff06e6fe9517f27869e6fdb33
Raphaël Valyi <rvalyi@akretion.com>
2020-12-07 13:42:05 -0300

    l10n_br_nfe_spec: basic readme

--- l10n_br_nfe_spec/__manifest__.py
+++ l10n_br_nfe_spec/__manifest__.py
@@ -1,12 +1,14 @@
 
 {
-    'name': 'nfe spec',
-    'version': '12.0.1.0.0',
-    'author': 'Akretion, Odoo Community Association (OCA)',
-    'license': 'LGPL-3',
-    'category': 'Accounting',
-    'summary': 'nfe spec',
-    'depends': ['base'],
-    'installable': True,
-    'application': False,
+    "name": "nfe spec",
+    "version": "12.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Accounting",
+    "summary": "nfe spec",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+    "development_status": "Production/Stable",
+    "maintainers": ["rvalyi"],
 }
