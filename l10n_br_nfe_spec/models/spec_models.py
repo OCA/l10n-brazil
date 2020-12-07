c5c7249328e3dc0f7c31b143d28b73b38634fa00
Raphaël Valyi <rvalyi@akretion.com>
2020-12-07 14:55:56 -0300

    missing license headers

--- l10n_br_nfe_spec/models/spec_models.py
+++ l10n_br_nfe_spec/models/spec_models.py
@@ -1,3 +1,6 @@
+# Copyright 2019-2020 Akretion - Raphael Valyi <raphael.valyi@akretion.com>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+
 from odoo import models, fields
 
 
--- l10n_br_nfe_spec/tests/test_nfe_import.py
+++ l10n_br_nfe_spec/tests/test_nfe_import.py
@@ -1,3 +1,6 @@
+# Copyright 2020 Akretion - Raphael Valyi <raphael.valyi@akretion.com>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+
 from datetime import datetime
 import pkg_resources
 import re
