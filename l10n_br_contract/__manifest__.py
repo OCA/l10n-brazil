1f34efc45a527bd35d463725bddb50d32841b2eb
Luis Malta <luis.malta@kmee.com.br>
2020-12-09 15:29:41 -0300

    [FIX] l10n_br_contract manifest

--- l10n_br_contract/__manifest__.py
+++ l10n_br_contract/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Br Contract',
+    'name': 'Brazilian Localization Contract',
     'summary': """
-        Brazilian Localization Contract""",
+        Customization of Contract module for implementations in Brazil.""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'KMEE INFORMATICA LTDA,Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/l10n-brazil.git',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/l10n-brazil',
     'depends': [
-        'l10n_br_account', 'l10n_br_fiscal', 'contract'
+        'l10n_br_account', 'contract'
     ],
     'data': [
         'views/contract_view.xml',
