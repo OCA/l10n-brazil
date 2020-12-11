d2a26b8041fc58a84b2298c99f01caed285b0b9d
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2020-12-11 12:04:45 -0300

    [REF] Inherit fiscal mixins

--- l10n_br_contract/models/contract_line.py
+++ l10n_br_contract/models/contract_line.py
@@ -6,13 +6,16 @@ from odoo import api, models
 
 class ContractLine(models.Model):
 
-    _inherit = 'contract.line'
+    _name = 'contract.line'
+    _inherit = [_name, 'l10n_br_fiscal.document.line.mixin']
 
     @api.multi
     def _prepare_invoice_line(self, invoice_id=False, invoice_values=False):
-        invoice_line_vals = super()._prepare_invoice_line(invoice_id,
-                                                          invoice_values)
-        invoice_line_vals['fiscal_operation_id'] =\
-            self.contract_id.fiscal_operation_id.id
+        invoice_line_vals = self._prepare_br_fiscal_dict()
+        invoice_line_vals.update(
+            super()._prepare_invoice_line(invoice_id, invoice_values)
+        )
+        # invoice_line_vals['fiscal_operation_id'] =\
+        #     self.contract_id.fiscal_operation_id.id
         invoice_line_vals['contract_line_id'] = self.id
         return invoice_line_vals
