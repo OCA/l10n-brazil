d0cde6548375a23afe00632b5682a3ac887d7d8f
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2020-12-11 12:04:45 -0300

    [REF] Inherit fiscal mixins

--- l10n_br_contract/models/contract_contract.py
+++ l10n_br_contract/models/contract_contract.py
@@ -6,7 +6,9 @@ from odoo import api, fields, models
 
 class ContractContract(models.Model):
 
-    _inherit = 'contract.contract'
+    _name = 'contract.contract'
+    _inherit = [_name, 'l10n_br_fiscal.document.mixin']
+
     document_count = fields.Integer(compute="_compute_document_count")
 
     fiscal_operation_id = fields.Many2one(
@@ -47,41 +49,49 @@ class ContractContract(models.Model):
 
     @api.multi
     def _prepare_invoice(self, date_invoice, journal=None):
-        invoice_vals = super()._prepare_invoice(date_invoice, journal)
-        invoice_vals['fiscal_document_id'] = False
-        invoice_vals['company_id'] = self.company_id.id
-        invoice_vals['fiscal_operation_type'] = 'out'
-        invoice_vals['document_type_id'] = self.company_id.document_type_id.id
-        if invoice_vals['document_type_id'] == \
-            self.env['l10n_br_fiscal.document.type'].search([
-                ('code', '=', 'SE')], limit=1).id:
-            invoice_vals['document_section'] = 'nfse_recibos'
-        invoice_vals['fiscal_operation_id'] = \
-            self.env.ref('l10n_br_fiscal.fo_venda').id
-        invoice_vals['document_serie_id'] = \
-            self.env['l10n_br_fiscal.document.serie'].search([
-                ('document_type_id', '=', invoice_vals['document_type_id']),
-                ('company_id', '=', self.company_id.id),
-            ], limit=1).id
+        invoice_vals = self._prepare_br_fiscal_dict()
+        invoice_vals.update(super()._prepare_invoice(date_invoice, journal))
+        # invoice_vals['fiscal_document_id'] = False
+        # invoice_vals['company_id'] = self.company_id.id
+        # invoice_vals['fiscal_operation_type'] = 'out'
+        # invoice_vals['document_type_id'] = self.company_id.document_type_id.id
+        # if invoice_vals['document_type_id'] == \
+        #     self.env['l10n_br_fiscal.document.type'].search([
+        #         ('code', '=', 'SE')], limit=1).id:
+        #     invoice_vals['document_section'] = 'nfse_recibos'
+        # invoice_vals['fiscal_operation_id'] = \
+        #     self.env.ref('l10n_br_fiscal.fo_venda').id
+        # invoice_vals['document_serie_id'] = \
+        #     self.env['l10n_br_fiscal.document.serie'].search([
+        #         ('document_type_id', '=', invoice_vals['document_type_id']),
+        #         ('company_id', '=', self.company_id.id),
+        #     ], limit=1).id
         return invoice_vals
 
     @api.model
     def _finalize_invoice_creation(self, invoices):
         super()._finalize_invoice_creation(invoices)
-        for invoice in invoices:
-            invoice.fiscal_document_id._onchange_document_serie_id()
-            invoice.fiscal_document_id._onchange_company_id()
-            invoice.fiscal_document_id._onchange_fiscal_operation_id()
-            if hasattr(invoice.fiscal_document_id, 'rps_number') and \
-                    invoice.fiscal_document_id.number:
-                invoice.fiscal_document_id.rps_number = \
-                    invoice.fiscal_document_id.number
-                invoice.fiscal_document_id.number = False
-            for line in invoice.fiscal_document_id.line_ids:
-                line._onchange_product_id_fiscal()
-                line._onchange_fiscal_operation_id()
-                line._onchange_ncm_id()
-                line.price_unit = line.contract_line_id.price_unit
-                line._onchange_commercial_quantity()
-                line._onchange_fiscal_operation_line_id()
-                line._onchange_fiscal_taxes()
+
+        # TODO: Verificar se é necessário
+        # for invoice in invoices:
+        #     invoice.fiscal_document_id._onchange_document_serie_id()
+        #     invoice.fiscal_document_id._onchange_company_id()
+        #     invoice.fiscal_document_id._onchange_fiscal_operation_id()
+
+            # TODO: Verificar se o número da nfse está correto
+            # if hasattr(invoice.fiscal_document_id, 'rps_number') and \
+            #         invoice.fiscal_document_id.number:
+            #     invoice.fiscal_document_id.rps_number = \
+            #         invoice.fiscal_document_id.number
+            #     invoice.fiscal_document_id.number = False
+
+            # TODO: Verificar se todos os campos fiscais estão sendo
+            #  corretamente calculados
+            # for line in invoice.fiscal_document_id.line_ids:
+            #     line._onchange_product_id_fiscal()
+            #     line._onchange_fiscal_operation_id()
+            #     line._onchange_ncm_id()
+            #     line.price_unit = line.contract_line_id.price_unit
+            #     line._onchange_commercial_quantity()
+            #     line._onchange_fiscal_operation_line_id()
+            #     line._onchange_fiscal_taxes()
