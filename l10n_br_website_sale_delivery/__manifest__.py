bf0fc64d48564d30ebde84316904421242110029
Luis Felipe Mileo <mileo@kmee.com.br>
2020-12-14 09:18:48 -0300

    [FIX] Module dependencies, typo and contributors

--- l10n_br_website_sale_delivery/__manifest__.py
+++ l10n_br_website_sale_delivery/__manifest__.py
@@ -2,15 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Br Website Sale Delivery',
+    'name': 'Brazilian Localization Website Sale Delivery',
     'summary': """
         Implements Brazilian freight values for delivery.""",
     'version': '12.0.1.1.0',
     'license': 'AGPL-3',
     'author': 'KMEE,Odoo Community Association (OCA)',
+    'development_status': 'Alpha',
+    'maintainers': ['DiegoParadeda'],
     'website': 'https://github.com/oca/l10n-brazil',
     'depends': [
         'website_sale_delivery',
+        'l10n_br_delivery',
+        'l10n_br_website_sale',
     ],
     'data': [
         'views/website_sale_delivery_templates.xml',
--- l10n_br_website_sale_delivery/readme/CONTRIBUTORS.rst
+++ l10n_br_website_sale_delivery/readme/CONTRIBUTORS.rst
@@ -1,3 +1,2 @@
-* Renato Lima <renato.lima@akretion.com.br>
 * Diego Paradeda <diego.paradeda@kmee.com.br>
 * Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
