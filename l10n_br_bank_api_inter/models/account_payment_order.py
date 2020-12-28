5dd0540c416314ea401a432682ce65511e9c9aa8
Luis Malta <luis.malta@kmee.com.br>
2020-12-28 15:24:49 -0300

    [REF] Respota data for boleto item

--- l10n_br_bank_api_inter/models/account_payment_order.py
+++ l10n_br_bank_api_inter/models/account_payment_order.py
@@ -124,10 +124,15 @@ class AccountPaymentOrder(models.Model):
             data = self._generate_bank_inter_boleto_data()
             for item in data:
                 resposta = self.api.boleto_inclui(item._emissao_data())
-                item.nosso_numero = resposta['nossoNumero']
-                item.seu_numero = resposta['seuNumero']
-                item.linha_digitavel = resposta['linhaDigitavel']
-                item.barcode = resposta['codigoBarras']
+                bank_line_id = self.bank_line_ids.filtered(
+                    lambda line: line.document_number == item._identifier)
+                payment_line_id = self.payment_line_ids.filtered(
+                    lambda line: line.document_number == item._identifier)
+                bank_line_id.own_number = resposta['nossoNumero']
+                payment_line_id.own_number = resposta['nossoNumero']
+                # bank_line_id.seu_numero = resposta['seuNumero']
+                # bank_line_id.linha_digitavel = resposta['linhaDigitavel']
+                # bank_line_id.barcode = resposta['codigoBarras']
         return False, False
 
     def _gererate_bank_inter_api(self):
