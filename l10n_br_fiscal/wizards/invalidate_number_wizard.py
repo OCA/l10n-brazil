f9404f9a8e5e5029298ad67673b6404c319ccfe8
Renato Lima <renatonlima@gmail.com>
2020-12-30 10:40:48 -0300

    [REF] Update Fiscal Invalidate Number wizard

--- l10n_br_fiscal/wizards/invalidate_number_wizard.py
+++ l10n_br_fiscal/wizards/invalidate_number_wizard.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2020  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import _, api, fields, models
 
 
 class InvalidateNumberWizard(models.TransientModel):
@@ -10,34 +10,42 @@ class InvalidateNumberWizard(models.TransientModel):
     _description = 'Invalidate Number Wizard'
     _inherit = 'l10n_br_fiscal.base.wizard.mixin'
 
+    invalidate_mode = fields.Selection(
+        selection=[
+            ('one_number', _('Only one number')),
+            ('range_numbers', _('Range of numbers')),
+        ],
+        string='Invalidate',
+        default='one_number',
+    )
+
+    number_start = fields.Integer(
+        string='Initial Number',
+        required=1,
+    )
+
+    number_end = fields.Integer(
+        string='Final Number',
+        required=1,
+    )
+
+    @api.onchange('invalidate_mode')
+    def _onchange_invalidate_mode(self):
+        for wizard in self:
+            if (wizard.invalidate_mode == 'one_number'
+                    and wizard.document_id.number):
+                wizard.number_start = wizard.document_id.number
+                wizard.number_end = wizard.document_id.number
+
     @api.multi
     def doit(self):
-        return {'type': 'ir.actions.act_window_close'}
-
-    # TODO
-    # @api.multi
-    # def doit(self):
-    #     for wizard in self:
-    #
-    #         inut = self.env['l10n_br_fiscal.invalidate.number'].create({
-    #             'company_id': document_id.company_id.id,
-    #             'fiscal_document_id': document_id.id,
-    #             'document_serie_id': document_id.document_serie_id.id,
-    #             'number_start': document_id.number,
-    #             'number_end': document_id.number,
-    #             'state': 'draft',
-    #         })
-    #         event_id = self.env['l10n_br_fiscal.event'].create({
-    #             'type': '3',
-    #             'response': 'Inutilização do número %s ao número %s' % (
-    #                 document_id.number, document_id.number),
-    #             'company_id': document_id.company_id.id,
-    #             'origin': 'NFe-%s' % document_id.number,
-    #             'date': fields.Datetime.now(),
-    #             'state': 'draft',
-    #             'invalid_number_document_event_id': inut.id,
-    #             'fiscal_document_id': fiscal_document_id.id,
-    #         })
-    #
-    #         inut.invalidate(event_id)
-    #     return {"type": "ir.actions.act_window_close"}
+        for wizard in self:
+            invalidate = self.env['l10n_br_fiscal.invalidate.number'].create({
+                'company_id': wizard.document_id.company_id.id,
+                'fiscal_document_id': wizard.document_id.id,
+                'document_serie_id': wizard.document_id.document_serie_id.id,
+                'number_start': wizard.document_id.number,
+                'number_end': wizard.document_id.number,
+            })
+            invalidate.invalidate()
+        self._close()
