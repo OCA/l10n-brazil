5462519b5434af5d34e5a4a80c4a138d421bf591
Renato Lima <renatonlima@gmail.com>
2021-01-08 15:09:44 -0300

    [FIX] header files

--- l10n_br_zip/models/res_config_settings.py
+++ l10n_br_zip/models/res_config_settings.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019  Renato Lima (Akretion)                                  #
+# Copyright (C) 2019  Renato Lima (Akretion)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields, models
