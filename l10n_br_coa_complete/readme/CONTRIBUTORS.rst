e862b5aa9af2061574f3d6ad9d38cda2ec9d07b8
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-01-09 12:23:52 -0300

    [ADD] add coa complete

---
+++ l10n_br_coa_complete/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+* Luis Felipe Mileo <mileo@kmee.com.br>
+* Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
+* Renato Lima <renato.lima@akretion.com.br>
---
+++ l10n_br_coa_complete/tests/test_l10n_br_coa_complete.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2020  Luis Felipe Mileo - KMEE
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class L10nBrCoaComplete(TransactionCase):
+    def setUp(self):
+        super().setUp()
+
+        self.l10n_br_coa_complete = self.env.ref(
+            'l10n_br_coa_complete.l10n_br_coa_complete_template')
+        self.l10n_br_company = self.env['res.company'].create({
+            'name': 'Empresa Teste do Plano de Contas CFC'
+        })
+
+    def test_l10n_br_coa_complete(self):
+        """Test to install the chart of accounts template in a new company"""
+        self.env.user.company_ids += self.l10n_br_company
+        self.env.user.company_id = self.l10n_br_company
+        self.l10n_br_coa_complete.try_loading_for_current_company()
+
+        self.assertEquals(
+            self.l10n_br_coa_complete, self.l10n_br_company.chart_template_id)
