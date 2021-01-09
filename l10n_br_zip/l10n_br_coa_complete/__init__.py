36bbb2ba9536450e434f1d41db7cbf48604be7d2
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-01-09 12:23:52 -0300

    [ADD] add coa complete

---
+++ l10n_br_zip/l10n_br_coa_complete/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2020 - TODAY, Marcel Savegnago - Escodoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from .hooks import post_init_hook
+
+from . import tests
---
+++ l10n_br_zip/l10n_br_coa_complete/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2020 - TODAY, Escodoo
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Plano de Contas Genérico',
+    'summary': """
+        Plano de Contas Genérico para empresas Simples,
+        Presumido, Real, SA, Consolidação""",
+    'version': '12.0.1.0.0',
+    'license': 'LGPL-3',
+    'author': 'Escodoo, Odoo Community Association (OCA)',
+    'maintainers': ['marcelsavegnago'],
+    'website': 'http://github.com/OCA/l10n-brazil',
+    'images': ['static/description/banner.png'],
+    'depends': ['l10n_br_coa'],
+    'data': [
+        'data/l10n_br_coa_complete_template.xml',
+        'data/account_group.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_group.xml',
+        'data/account_fiscal_position_template.xml',
+        'data/l10n_br_coa_complete_template_post.xml',
+    ],
+    'post_init_hook': 'post_init_hook',
+}
