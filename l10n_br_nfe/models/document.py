e03ddac98e36f32dabe8ab75f3cd3c56d943bc35
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-01-16 10:59:39 -0300

    [IMP] l10n_br_nfe: amount_estimate_tax

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -210,6 +210,10 @@ class NFe(spec_models.StackedModel):
         related='amount_freight_value',
     )
 
+    nfe40_vTotTrib = fields.Monetary(
+        related='amount_estimate_tax'
+    )
+
     @api.multi
     @api.depends('fiscal_operation_type')
     def _compute_nfe_data(self):
