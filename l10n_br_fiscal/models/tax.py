f43d1283e10c25a1e0447e65c6276e8c5c3773b6
Renato Lima <renato.lima@akretion.com>
2021-01-19 12:09:23 -0300

    [REF] added NBS arg in compute_taxes method

--- l10n_br_fiscal/models/tax.py
+++ l10n_br_fiscal/models/tax.py
@@ -599,6 +599,7 @@ class Tax(models.Model):
             other_costs_value,
             freight_value,
             ncm,
+            nbs,
             nbm,
             cest,
             operation_line,
