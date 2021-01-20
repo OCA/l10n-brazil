6c5804c6100b41f28fcddd0f54603faee797fe89
Renato Lima <renato.lima@akretion.com>
2021-01-20 15:12:43 -0300

    [REF] move method from product.template to  fiscal product mixin

--- l10n_br_fiscal/models/product_template.py
+++ l10n_br_fiscal/models/product_template.py
@@ -1,12 +1,12 @@
 # Copyright (C) 2013  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 from ..constants.fiscal import (
-    NCM_FOR_SERVICE_REF,
     PRODUCT_FISCAL_TYPE,
     PRODUCT_FISCAL_TYPE_SERVICE,
+    NCM_FOR_SERVICE_REF,
     TAX_ICMS_OR_ISSQN,
     TAX_DOMAIN_ICMS,
 )
@@ -15,7 +15,8 @@ from ..constants.icms import ICMS_ORIGIN, ICMS_ORIGIN_DEFAULT
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _name = 'product.template'
+    _inherit = ['product.template', 'l10n_br_fiscal.product.mixin']
 
     def _get_default_ncm_id(self):
         fiscal_type = self.env.context.get('default_fiscal_type')
@@ -98,22 +99,3 @@ class ProductTemplate(models.Model):
     uot_factor = fields.Float(
         string='Tax UoM Factor',
         default=0.00)
-
-    # TODO add percent of estimate taxes
-
-    @api.onchange('fiscal_type')
-    def _onchange_fiscal_type(self):
-        for r in self:
-            if r.fiscal_type == PRODUCT_FISCAL_TYPE_SERVICE:
-                r.ncm_id = self.env.ref(NCM_FOR_SERVICE_REF)
-
-    @api.onchange('ncm_id', 'fiscal_genre_id')
-    def _onchange_ncm_id(self):
-        for r in self:
-            if r.ncm_id:
-                r.fiscal_genre_id = self.env[
-                    'l10n_br_fiscal.product.genre'].search(
-                        [('code', '=', r.ncm_id.code[0:2])])
-
-            if r.fiscal_genre_id.code == PRODUCT_FISCAL_TYPE_SERVICE:
-                r.ncm_id = self.env.ref(NCM_FOR_SERVICE_REF)
