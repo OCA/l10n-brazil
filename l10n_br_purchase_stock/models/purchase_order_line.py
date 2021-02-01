7b900851c19479a107a639a440dc4c83cc5ad400
Renato Lima <renatonlima@gmail.com>
2021-02-01 12:02:53 -0300

    [REF] added purchase create invoice policy

--- l10n_br_purchase_stock/models/purchase_order_line.py
+++ l10n_br_purchase_stock/models/purchase_order_line.py
@@ -17,4 +17,7 @@ class PurchaseOrderLine(models.Model):
         values = super()._prepare_stock_moves(picking)
         for v in values:
             v.update(self._prepare_br_fiscal_dict())
+            if (self.env.user.company_id.purchase_create_invoice_policy
+                    == 'stock_picking'):
+                v['invoice_state'] = '2binvoiced'
         return values
