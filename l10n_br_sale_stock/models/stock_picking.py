a8f82ef9515b2d05ee19a6a0870353906c5060e7
Magno Costa <magno.costa@akretion.com.br>
2021-02-02 11:05:51 -0300

    [FIX+REF] Name class, remove field with copy=True because it already get this information from get methods and inform partner_invoice_id to create Invoice when is Sale Order.

--- l10n_br_sale_stock/models/stock_picking.py
+++ l10n_br_sale_stock/models/stock_picking.py
@@ -1,12 +1,16 @@
 # Copyright (C) 2020  Gabriel Cardoso de Faria - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import models, api
 
 
-class SaleOrder(models.Model):
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    invoice_state = fields.Selection(
-        copy=True,
-    )
+    @api.multi
+    def _get_partner_to_invoice(self):
+        self.ensure_one()
+        partner = self.partner_id
+        if self.sale_id:
+           partner = self.sale_id.partner_invoice_id
+        return partner.address_get(['invoice']).get('invoice')
