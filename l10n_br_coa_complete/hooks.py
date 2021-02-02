29adbf21da5067dd9091b934f59151d42faf5e71
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-02 12:17:24 -0300

    [FIX] remove comments

--- l10n_br_coa_complete/hooks.py
+++ l10n_br_coa_complete/hooks.py
@@ -15,14 +15,3 @@ def post_init_hook(cr, registry):
         from odoo.addons.l10n_br_account.hooks import load_fiscal_taxes
         # Relate fiscal taxes to account taxes.
         load_fiscal_taxes(env, coa_complete_tmpl)
-
-    # # Load COA to Demo Company
-    # if not tools.config.get('without_demo'):
-    #     user_admin = env.ref('base.user_admin')
-    #     company = env.ref(
-    #         'l10n_br_base.empresa_lucro_presumido', raise_if_not_found=False)
-    #     if company:
-    #         user_admin.company_id = company
-    #         coa_complete_tmpl.sudo(
-    #             user=user_admin.id).try_loading_for_current_company()
-    #         user_admin.company_id = env.ref('base.main_company')
