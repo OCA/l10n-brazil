8b44a22a8efe3e20ae46786ce4291530cf887856
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-02 00:54:15 -0300

    [IMP] create mixin and mixin methods

--- l10n_br_repair/models/__init__.py
+++ l10n_br_repair/models/__init__.py
@@ -1,5 +1,7 @@
 from . import res_company
 from . import res_config_settings
 from . import repair_order
+from . import repair_line_mixin_methods
+from . import repair_line_mixin
 from . import repair_line
 from . import repair_fee
