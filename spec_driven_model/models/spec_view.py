aa968e12b2d8faaa577fef21f08a2bd5899f8758
Raphaël Valyi <rvalyi@akretion.com>
2021-02-09 01:58:53 -0300

    disable Gabriel debug export_xml button as it breaks views

--- spec_driven_model/models/spec_view.py
+++ spec_driven_model/models/spec_view.py
@@ -93,15 +93,15 @@ class SpecViewMixin(models.AbstractModel):
     def _build_spec_fragment(self, container=None):
         if container is None:
             container = E.group()
+#            an export button that can help debug but messes views
+#            view_child = E.group()
+#            view_child.append(E.button(
+#                name='export_xml',
+#                type='object',
+#                string='Export',
+#            ))
 
-            view_child = E.group()
-            view_child.append(E.button(
-                name='export_xml',
-                type='object',
-                string='Export',
-            ))
-
-        container.append(view_child)
+#        container.append(view_child)
         fields = []
         if hasattr(type(self), '_stacked') and type(self)._stacked:
             # we want the root of what is stacked to recreate the hierarchy
