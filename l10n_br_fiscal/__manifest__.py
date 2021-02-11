c4347edcde7862306378bc08e1a212e3a5381710
Luis Felipe Mileo <mileo@kmee.com.br>
2021-02-10 23:54:43 -0300

    [NEW] Consumidor anonimo

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -38,6 +38,7 @@
         "data/l10n_br_fiscal_server_action.xml",
         "data/ir_cron.xml",
         "data/l10n_br_fiscal_comment_data.xml",
+        "data/res_partner_data.xml",
 
         # Views
         "views/cnae_view.xml",
