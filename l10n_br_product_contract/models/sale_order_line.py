c35244414a78a2f2a818dfbe0916bdc3c94bacd3
Luis Felipe Mileo <mileo@kmee.com.br>
2021-02-11 14:08:47 -0300

    [NEW] L10n BR Product Contract

---
+++ l10n_br_product_contract/models/sale_order_line.py
@@ -0,0 +1,20 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+
+    @api.multi
+    def _prepare_contract_line_values(
+        self, contract, predecessor_contract_line_id=False
+    ):
+        vals = self._prepare_br_fiscal_dict()
+        vals.update(super()._prepare_contract_line_values(
+            contract, predecessor_contract_line_id
+        ))
+        return vals
