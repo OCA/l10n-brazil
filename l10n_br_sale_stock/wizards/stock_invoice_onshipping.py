b7d63ac4c88eef849cb5f3b1ec0354c56afced2d
Magno Costa <magno.costa@akretion.com.br>
2021-02-12 11:56:26 -0300

    [REM] Unnecessary method.

--- l10n_br_sale_stock/wizards/stock_invoice_onshipping.py
+++ l10n_br_sale_stock/wizards/stock_invoice_onshipping.py
@@ -9,23 +9,6 @@ class StockInvoiceOnshipping(models.TransientModel):
     _inherit = 'stock.invoice.onshipping'
 
     @api.multi
-    def _simulate_invoice_line_onchange(self, values, price_unit=None):
-        """
-        Simulate onchange for invoice line
-        :param values: dict
-        :return: dict
-        """
-        new_values = super()._simulate_invoice_line_onchange(
-            values.copy(), price_unit=price_unit)
-        line = self.env['account.invoice.line'].new(new_values.copy())
-        if price_unit:
-            line.price_unit = price_unit
-            line._compute_price()
-        new_values.update(line._convert_to_write(line._cache))
-        values.update(new_values)
-        return values
-
-    @api.multi
     def _build_invoice_values_from_pickings(self, pickings):
         """
         Build dict to create a new invoice from given pickings
