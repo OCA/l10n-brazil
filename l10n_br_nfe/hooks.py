6cce60917c576d545f60600f1c33a8a25a3369ef
Raphaël Valyi <rvalyi@akretion.com>
2021-02-17 10:30:15 -0300

    pre_init_hook was moved to l10n_br_base

--- l10n_br_nfe/hooks.py
+++ l10n_br_nfe/hooks.py
@@ -12,25 +12,6 @@ from odoo.addons.spec_driven_model import hooks
 _logger = logging.getLogger(__name__)
 
 
-def pre_init_hook(cr):
-    """
-    The objective of this hook is to ensure the Brazil country is
-    translated as "Brasil" in pt_BR to get the NFe tests pass
-    even if the pt_BR language pack is not installed.
-    """
-    cr.execute("""SELECT id
-    FROM ir_translation
-    WHERE name='res.country,name' AND
-    lang='pt_BR'""")
-    if not cr.fetchone():
-        cr.execute(
-            """
-            INSERT INTO ir_translation
-            (name, res_id, lang, type, src, value, module, state)
-            VALUES ('res.country,name', 31, 'pt_BR', 'model',
-            'Brazil', 'Brasil', 'base', 'translated');
-            """)
-
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
