99f9f5ffa6a063d239536f5c26e260fa84df97df
Luis Felipe Mileo <mileo@kmee.com.br>
2021-03-01 15:17:05 -0300

    [NEW] Res Partner Fiscal Document Relation close: #1151

--- l10n_br_fiscal/models/res_partner.py
+++ l10n_br_fiscal/models/res_partner.py
@@ -25,6 +25,13 @@ class ResPartner(models.Model):
             [('default', '=', True), ('is_company', '=', is_company)],
             limit=1)
 
+    @api.multi
+    def _compute_fiscal_document_count(self):
+        for partner in self:
+            partner.fiscal_documents_count = partner.fiscal_document_ids.search_count(
+                [('partner_id', '=', partner.id)]
+            )
+
     tax_framework = fields.Selection(
         selection=TAX_FRAMEWORK,
         default=TAX_FRAMEWORK_NORMAL,
@@ -79,6 +86,19 @@ class ResPartner(models.Model):
         track_visibility='onchange',
     )
 
+    fiscal_documents_count = fields.Integer(
+        compute='_compute_fiscal_document_count',
+        string="Fiscal Document Count",
+    )
+
+    fiscal_document_ids = fields.One2many(
+        'l10n_br_fiscal.document',
+        'partner_id',
+        string='Fiscal Documents',
+        readonly=True,
+        copy=False
+    )
+
     def _inverse_fiscal_profile(self):
         for p in self:
             p._onchange_fiscal_profile_id()
@@ -101,3 +121,25 @@ class ResPartner(models.Model):
             if p.ind_ie_dest == NFE_IND_IE_DEST_9:
                 p.inscr_est = False
                 p.state_tax_number_ids = False
+
+    @api.multi
+    def action_view_document(self):
+        action = self.env.ref('l10n_br_fiscal.document_out_action').read()[0]
+        if len(self.fiscal_document_ids) > 1:
+            action['domain'] = [
+                ('id', 'in', self.fiscal_document_ids.ids),
+            ]
+        elif len(self.fiscal_document_ids) == 1:
+            form_view = [
+                (self.env.ref('l10n_br_fiscal.document_form').id, 'form'),
+            ]
+            if 'views' in action:
+                action['views'] = form_view + [(state, view) for state, view
+                                               in action['views'] if
+                                               view != 'form']
+            else:
+                action['views'] = form_view
+            action['res_id'] = self.fiscal_document_ids.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
