13c052a87540f378734c8b65f8c84242bade6e56
Renato Lima <renato.lima@akretion.com>
2021-03-03 17:54:05 -0300

    [ADD] fiscal wizard until events is not refactoring

--- l10n_br_account/wizards/__init__.py
+++ l10n_br_account/wizards/__init__.py
@@ -1,3 +1,5 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import account_invoice_refund
+from . import wizard_document_cancel
+from . import wizard_document_correction
