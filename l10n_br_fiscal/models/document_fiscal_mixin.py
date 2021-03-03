dcb75d45980c50bccecd9f39aa984bb74251819e
Renato Lima <renato.lima@akretion.com>
2021-03-03 10:18:05 -0300

    [REF] moved amount compute and computed fields from fiscal document to fiscal document mixin

--- l10n_br_fiscal/models/document_fiscal_mixin.py
+++ l10n_br_fiscal/models/document_fiscal_mixin.py
@@ -87,3 +87,227 @@ class FiscalDocumentMixin(models.AbstractModel):
         string='Final Consumption Operation',
         default=FINAL_CUSTOMER_YES,
     )
+
+    currency_id = fields.Many2one(
+        comodel_name='res.currency',
+        default=lambda self: self.env.user.company_id.currency_id,
+        store=True,
+        readonly=True,
+    )
+
+    amount_untaxed = fields.Monetary(
+        string='Amount Untaxed',
+        compute='_compute_amount',
+    )
+
+    amount_icms_base = fields.Monetary(
+        string='ICMS Base',
+        compute='_compute_amount',
+    )
+
+    amount_icms_value = fields.Monetary(
+        string='ICMS Value',
+        compute='_compute_amount',
+    )
+
+    amount_icmsst_base = fields.Monetary(
+        string='ICMS ST Base',
+        compute='_compute_amount',
+    )
+
+    amount_icmsst_value = fields.Monetary(
+        string='ICMS ST Value',
+        compute='_compute_amount',
+    )
+
+    amount_icmssn_value = fields.Monetary(
+        string='ICMSSN Value',
+        compute='_compute_amount',
+    )
+
+    amount_icmsfcp_base = fields.Monetary(
+        string='ICMS FCP Base',
+        compute='_compute_amount',
+    )
+
+    amount_icmsfcp_value = fields.Monetary(
+        string='ICMS FCP Value',
+        compute='_compute_amount',
+    )
+
+    amount_ipi_base = fields.Monetary(
+        string='IPI Base',
+        compute='_compute_amount',
+    )
+
+    amount_ipi_value = fields.Monetary(
+        string='IPI Value',
+        compute='_compute_amount',
+    )
+
+    amount_pis_base = fields.Monetary(
+        string='PIS Base',
+        compute='_compute_amount',
+    )
+
+    amount_pis_value = fields.Monetary(
+        string='PIS Value',
+        compute='_compute_amount',
+    )
+
+    amount_pis_ret_base = fields.Monetary(
+        string='PIS Ret Base',
+        compute='_compute_amount',
+    )
+
+    amount_pis_ret_value = fields.Monetary(
+        string='PIS Ret Value',
+        compute='_compute_amount',
+    )
+
+    amount_cofins_base = fields.Monetary(
+        string='COFINS Base',
+        compute='_compute_amount',
+    )
+
+    amount_cofins_value = fields.Monetary(
+        string='COFINS Value',
+        compute='_compute_amount',
+    )
+
+    amount_cofins_ret_base = fields.Monetary(
+        string='COFINS Ret Base',
+        compute='_compute_amount',
+    )
+
+    amount_cofins_ret_value = fields.Monetary(
+        string='COFINS Ret Value',
+        compute='_compute_amount',
+    )
+
+    amount_issqn_base = fields.Monetary(
+        string='ISSQN Base',
+        compute='_compute_amount',
+    )
+
+    amount_issqn_value = fields.Monetary(
+        string='ISSQN Value',
+        compute='_compute_amount',
+    )
+
+    amount_issqn_ret_base = fields.Monetary(
+        string='ISSQN Ret Base',
+        compute='_compute_amount',
+    )
+
+    amount_issqn_ret_value = fields.Monetary(
+        string='ISSQN Ret Value',
+        compute='_compute_amount',
+    )
+
+    amount_csll_base = fields.Monetary(
+        string='CSLL Base',
+        compute='_compute_amount',
+    )
+
+    amount_csll_value = fields.Monetary(
+        string='CSLL Value',
+        compute='_compute_amount',
+    )
+
+    amount_csll_ret_base = fields.Monetary(
+        string='CSLL Ret Base',
+        compute='_compute_amount',
+    )
+
+    amount_csll_ret_value = fields.Monetary(
+        string='CSLL Ret Value',
+        compute='_compute_amount',
+    )
+
+    amount_irpj_base = fields.Monetary(
+        string='IRPJ Base',
+        compute='_compute_amount',
+    )
+
+    amount_irpj_value = fields.Monetary(
+        string='IRPJ Value',
+        compute='_compute_amount',
+    )
+
+    amount_irpj_ret_base = fields.Monetary(
+        string='IRPJ Ret Base',
+        compute='_compute_amount',
+    )
+
+    amount_irpj_ret_value = fields.Monetary(
+        string='IRPJ Ret Value',
+        compute='_compute_amount',
+    )
+
+    amount_inss_base = fields.Monetary(
+        string='INSS Base',
+        compute='_compute_amount',
+    )
+
+    amount_inss_value = fields.Monetary(
+        string='INSS Value',
+        compute='_compute_amount',
+    )
+
+    amount_inss_wh_base = fields.Monetary(
+        string='INSS Ret Base',
+        compute='_compute_amount',
+    )
+
+    amount_inss_wh_value = fields.Monetary(
+        string='INSS Ret Value',
+        compute='_compute_amount',
+    )
+
+    amount_estimate_tax = fields.Monetary(
+        string='Amount Estimate Tax',
+        compute='_compute_amount',
+    )
+
+    amount_tax = fields.Monetary(
+        string='Amount Tax',
+        compute='_compute_amount',
+    )
+
+    amount_total = fields.Monetary(
+        string='Amount Total',
+        compute='_compute_amount',
+    )
+
+    amount_tax_withholding = fields.Monetary(
+        string="Amount Tax Withholding",
+        compute='_compute_amount')
+
+    amount_financial = fields.Monetary(
+        string='Amount Financial',
+        compute='_compute_amount',
+    )
+
+    amount_discount_value = fields.Monetary(
+        string='Amount Discount',
+        compute='_compute_amount',
+    )
+
+    amount_insurance_value = fields.Monetary(
+        string='Insurance Value',
+        default=0.00,
+        compute='_compute_amount',
+    )
+
+    amount_costs_value = fields.Monetary(
+        string='Other Costs',
+        default=0.00,
+        compute='_compute_amount',
+    )
+
+    amount_freight_value = fields.Monetary(
+        string='Freight Value',
+        default=0.00,
+        compute='_compute_amount',
+    )
