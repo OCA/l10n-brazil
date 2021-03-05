6be50817116df4ad8a84954d03843849457ffa1d
Magno Costa <magno.costa@akretion.com.br>
2021-03-05 16:49:12 -0300

    [FIX] If Sale Create Invoice Policy are stock_picking should be not possible create Invoice from Sale Order for type product.

--- l10n_br_sale_stock/models/sale_order_line.py
+++ l10n_br_sale_stock/models/sale_order_line.py
@@ -14,8 +14,43 @@ class SaleOrderLine(models.Model):
         values = self._prepare_br_fiscal_dict()
         values.update(super()._prepare_procurement_values(group_id))
         # Incluir o invoice_state
-        if self.order_id.company_id.sale_create_invoice_policy \
+        if self.order_id.company_id.sale_create_invoice_policy\
                 == 'stock_picking':
             values['invoice_state'] = '2binvoiced'
 
         return values
+
+    # no trigger product_id.invoice_policy to avoid retroactively changing SO
+    @api.depends(
+        'qty_invoiced', 'qty_delivered', 'product_uom_qty', 'order_id.state')
+    def _get_to_invoice_qty(self):
+        """
+        Compute the quantity to invoice. If the invoice policy is order,
+        the quantity to invoice is calculated from the ordered quantity.
+        Otherwise, the quantity delivered is used.
+        """
+        super()._get_to_invoice_qty()
+
+        for line in self:
+            if line.order_id.state in ['sale', 'done']:
+                if line.product_id.invoice_policy == 'order':
+                    if line.order_id.company_id.sale_create_invoice_policy ==\
+                            'stock_picking' and\
+                            line.product_id.type == 'product':
+                        # O correto seria que ao selecionar
+                        # sale_create_invoice_policy 'stock_picking' os
+                        # produtos tenham o campo invoice_policy definidos para
+                        # 'delivery', porém para evitar que seja criada uma
+                        # Fatura a partir do Pedido de Venda estamos
+                        # alterando isso mesmo para os produtos definidos com
+                        # 'order', já que a Politica de Criação da Fatura no
+                        # caso do Tipo Produto está definida para ser a
+                        # partir do stock.picking .
+                        # TODO: Essa seria a melhor opção ? Por enquanto pelo
+                        #  que vi para ter o mesmo resultado, que é no caso
+                        #  sale_create_invoice_policy 'stock_picking' só ser
+                        #  possível criar a partir do sale.order Faturas das
+                        #  linhas que sejam type service sim, a outra opção
+                        #  seria sobre escrever o metodo action_invoice_create
+                        #  sem ser possível chamar o super.
+                        line.qty_to_invoice = 0
