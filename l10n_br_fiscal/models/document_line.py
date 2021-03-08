53d8e9e0ecc29dffc010d79b872e0f577f9b9792
Renato Lima <renato.lima@akretion.com>
2021-03-08 13:51:54 -0300

    [REF] update compute attribute

--- l10n_br_fiscal/models/document_line.py
+++ l10n_br_fiscal/models/document_line.py
@@ -69,31 +69,31 @@ class DocumentLine(models.Model):
     # Amount Fields
     amount_untaxed = fields.Monetary(
         string='Amount Untaxed',
-        compute='_compute_amount',
+        compute='_compute_amounts',
         default=0.00,
     )
 
     amount_tax = fields.Monetary(
         string='Amount Tax',
-        compute='_compute_amount',
+        compute='_compute_amounts',
         default=0.00,
     )
 
     amount_fiscal = fields.Monetary(
         string='Amount Fiscal',
-        compute='_compute_amount',
+        compute='_compute_amounts',
         default=0.00,
     )
 
     amount_financial = fields.Monetary(
         string='Amount Financial',
-        compute='_compute_amount',
+        compute='_compute_amounts',
         default=0.00,
     )
 
     amount_total = fields.Monetary(
         string='Amount Total',
-        compute='_compute_amount',
+        compute='_compute_amounts',
         default=0.00,
     )
 
