dd0c3f210967768af0d425fe55d36c747d9b6cde
Luis Felipe Mileo <mileo@kmee.com.br>
2021-03-12 23:25:38 -0300

    [ADD] Fiscal document related on AML

--- l10n_br_account/models/account_move_line.py
+++ l10n_br_account/models/account_move_line.py
@@ -2,9 +2,16 @@
 #   @author Magno Costa <magno.costa@akretion.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import models, fields, api
 
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
     _order = 'date_maturity, date desc, id desc'
+
+    fiscal_document_id = fields.Many2one(
+        comodel_name='l10n_br_fiscal.document',
+        related='invoice_id.fiscal_document_id',
+        store=True,
+    )
+
