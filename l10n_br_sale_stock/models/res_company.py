64ccd8921293a1d0cea3e3650c4846524287c917
Magno Costa <magno.costa@akretion.com.br>
2021-03-16 10:32:47 -0300

    [FIX] Import order.

--- l10n_br_sale_stock/models/res_company.py
+++ l10n_br_sale_stock/models/res_company.py
@@ -2,7 +2,7 @@
 # @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, _
+from odoo import _, fields, models
 
 
 class ResCompany(models.Model):
