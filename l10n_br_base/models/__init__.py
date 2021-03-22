01408e8ff2bf64e93b07c82a8157bcd4f19c5ffd
Renato Lima <renato.lima@akretion.com>
2021-03-22 12:53:59 -0300

    [FIX] mixin and import

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -5,7 +5,7 @@ from . import res_bank
 from . import res_city
 from . import res_country_state
 from . import res_country
-from . import data_mixin
+from . import party_mixin
 from . import res_partner_bank
 from . import res_partner
 from . import state_tax_numbers
