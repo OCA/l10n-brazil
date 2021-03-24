0e2b1e06b2ca251ecf1aa2a6195ff82c51b67251
Luis Felipe Mileo <mileo@kmee.com.br>
2021-03-24 14:19:04 -0300

    [NEW] Fake certificate for furder valdation

--- l10n_br_fiscal/models/certificate.py
+++ l10n_br_fiscal/models/certificate.py
@@ -98,6 +98,11 @@ class Certificate(models.Model):
         string="Is Valid?",
         store=False)
 
+    is_fake = fields.Boolean(
+        readonly=True,
+        default=False,
+    )
+
     @api.model
     def _certificate_data(self, cert_file, cert_password):
         values = {}
