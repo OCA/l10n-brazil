c024e2fb76757868eae74212de5e8e3ec1d7f6b8
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2021-03-25 14:41:23 -0300

    [FIX] Change comodel name

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -251,6 +251,9 @@ class NFe(spec_models.StackedModel):
     nfe40_infCpl = fields.Char(
         compute='_compute_nfe40_additional_data',
     )
+    nfe40_transporta = fields.Many2one(
+        comodel_name='res.partner'
+    )
 
     nfe40_infRespTec = fields.Many2one(
         comodel_name='res.partner',
