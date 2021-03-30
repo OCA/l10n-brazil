bc401192add684beb17f51a6c6a51d3448260a25
Magno Costa <magno.costa@akretion.com.br>
2021-03-30 10:29:50 -0300

    [REF] Created object to register Boleto and Payment fields to separated and make more clean code in payment mode.

--- l10n_br_account_payment_order/models/account_payment_mode.py
+++ l10n_br_account_payment_order/models/account_payment_mode.py
@@ -4,42 +4,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
-from odoo.addons import decimal_precision as dp
 from odoo.exceptions import ValidationError, UserError
 
 from ..constants import (
-    AVISO_FAVORECIDO,
-    CODIGO_FINALIDADE_TED,
-    COMPLEMENTO_TIPO_SERVICO,
     FORMA_LANCAMENTO,
     TIPO_SERVICO,
-    BOLETO_ESPECIE,
 )
 
 
 class AccountPaymentMode(models.Model):
     _name = 'account.payment.mode'
-    _inherit = ['account.payment.mode', 'mail.thread']
-
-    instructions = fields.Text(
-        string='Instruções de cobrança',
-    )
-
-    invoice_print = fields.Boolean(
-        string='Gerar relatorio na conclusão da fatura?'
-    )
-
-    condition_issuing_paper = fields.Selection(
-        selection=[
-            ('1', 'Banco emite e Processa'),
-            ('2', 'Cliente emite e banco processa')],
-        string='Condição Emissão de Papeleta',
-        default='1',
-    )
-
-    communication_2 = fields.Char(
-        string='Comunicação para o sacador avalista',
-    )
+    _inherit = ['account.payment.mode', 'mail.thread',
+                'l10n_br_cnab.boleto.fields', 'l10n_br_cnab.payment.fields']
 
     service_type = fields.Selection(
         selection=TIPO_SERVICO,
@@ -53,239 +29,39 @@ class AccountPaymentMode(models.Model):
         help='Campo G029 do CNAB',
     )
 
-    code_convetion = fields.Char(
-        string='Código do Convênio no Banco',
-        size=20,
-        help='Campo G007 do CNAB',
-        track_visibility='always',
-    )
-
-    doc_finality_code = fields.Selection(
-        selection=COMPLEMENTO_TIPO_SERVICO,
-        string='Complemento do Tipo de Serviço',
-        help='Campo P005 do CNAB',
-    )
-
-    ted_finality_code = fields.Selection(
-        selection=CODIGO_FINALIDADE_TED,
-        string='Código Finalidade da TED',
-        help='Campo P011 do CNAB',
-    )
-
-    complementary_finality_code = fields.Char(
-        string='Código de finalidade complementar',
-        size=2,
-        help='Campo P013 do CNAB',
-    )
-
-    favored_warning = fields.Selection(
-        selection=AVISO_FAVORECIDO,
-        string='Aviso ao Favorecido',
-        help='Campo P006 do CNAB',
-        default=0,
-    )
-
-    # A exportação CNAB não se encaixa somente nos parâmetros de
-    # débito e crédito.
-    boleto_wallet = fields.Char(
-        string='Carteira',
-        size=3,
-        track_visibility='always',
-    )
-
-    boleto_modality = fields.Char(
-        string='Modalidade',
-        size=2,
-        track_visibility='always',
-    )
-
-    boleto_variation = fields.Char(
-        string='Variação',
-        size=2,
-        track_visibility='always',
-    )
-
-    boleto_accept = fields.Selection(
-        selection=[
-            ('S', 'Sim'),
-            ('N', 'Não')],
-        string='Aceite',
-        default='N',
-        track_visibility='always',
-    )
-
-    boleto_species = fields.Selection(
-        selection=BOLETO_ESPECIE,
-        string='Espécie do Título',
-        default='01',
-        track_visibility='always',
-    )
-
-    # Na configuração ou implementação de outros campos é
-    # melhor seguir a idéia abaixo pois os campos não são usados com
-    # frequencia e incluir um campo do tipo Char permitindo que seja
-    # informado o valor de acordo com a configuração do Boleto ao
-    # invês de diversos campos do Tipo Select para cada Banco parece
-    # ser melhor.
-    # [ Deixado manualmente, pois cada banco parece ter sua tabela.
-    # ('0', u'Sem instrução'),
-    # ('1', u'Protestar (Dias Corridos)'),
-    # ('2', u'Protestar (Dias Úteis)'),
-    # ('3', u'Não protestar'),
-    # ('7', u'Negativar (Dias Corridos)'),
-    # ('8', u'Não Negativar')
-    # ]
-    boleto_protest_code = fields.Char(
-        string='Código de Protesto',
-        default='0',
-        help='Código adotado pela FEBRABAN para identificar o tipo '
-             'de prazo a ser considerado para o protesto.',
-        track_visibility='always',
-    )
-
-    boleto_days_protest = fields.Char(
-        string='Número de Dias para Protesto',
-        size=2,
-        help='Número de dias decorrentes após a data de vencimento '
-             'para inicialização do processo de cobrança via protesto.',
-        track_visibility='always',
-    )
-
-    generate_own_number = fields.Boolean(
-        string='Gerar nosso número?',
-        default=True,
-        help='Dependendo da carteira, banco, etc. '
-             'O nosso número pode ser gerado pelo banco.',
-    )
-
-    product_tax_id = fields.Many2one(
-        comodel_name='product.product',
-        string='Taxa Adicional',
-        track_visibility='always',
-    )
-
-    product_tax_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Conta de Taxa do Produto',
-        help='Conta padrão para a Taxa do Produto',
-        track_visibility='always',
-    )
-
     cnab_sequence_id = fields.Many2one(
         comodel_name='ir.sequence',
         string='Sequencia do Arquivo CNAB',
         track_visibility='always',
     )
 
-    boleto_byte_idt = fields.Char(
-        string='Byte IDT',
-        size=1,
-        help='Byte de identificação do cedente do bloqueto '
-             'utilizado para compor o nosso número, '
-             'usado pelos bancos Sicred/Unicred e Sicoob.',
-        track_visibility='always',
+    # Fields used to make invisible banks specifics fields
+    bank_id = fields.Many2one(
+        related='fixed_journal_id.bank_id',
     )
 
-    boleto_post = fields.Char(
-        string='Posto da Cooperativa de Crédito',
-        size=2,
-        help='Código do Posto da Cooperativa de Crédito,'
-             ' usado pelos bancos Sicred/Unicred e Sicoob.',
-        track_visibility='always',
-    )
-
-    # Field used to make invisible banks specifics fields
     bank_code_bc = fields.Char(
         related='fixed_journal_id.bank_id.code_bc',
     )
 
-    own_number_sequence_id = fields.Many2one(
-        comodel_name='ir.sequence',
-        string='Sequência do Nosso Número',
-        help='Para usar essa Sequencia é preciso definir o campo Tipo do '
-             'Nosso Número como Sequencial Único por Carteira no cadastro da '
-             'empresa',
-        track_visibility='always',
-    )
-
-    # Field used to make invisible own_number_sequence_id
     own_number_type = fields.Selection(
         related='fixed_journal_id.company_id.own_number_type',
     )
 
-    boleto_interest_code = fields.Char(
-        string='Código da Mora',
-        size=1,
-        help='Código adotado pela FEBRABAN para identificação '
-             'do tipo de pagamento de mora de juros.',
-        track_visibility='always',
-    )
-
-    boleto_interest_perc = fields.Float(
-        string='Percentual de Juros de Mora',
-        digits=dp.get_precision('Account'),
-        track_visibility='always',
-    )
-
-    # TODO - criar outro campo para separar a Conta Contabil de Multa ?
-    #  o valor vem somado ao Juros Mora no retorno do cnab 400 unicred,
-    #  isso seria o padrão dos outros bancos ?
-    interest_fee_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Conta Contabil de Juros Mora e Multa',
-        help='Conta padrão para Juros Mora',
-        track_visibility='always',
-    )
-
-    boleto_fee_code = fields.Char(
-        string='Código da Multa',
-        size=1,
-        help='Código adotado pela FEBRABAN para identificação '
-             'do tipo de pagamento de multa.',
-        track_visibility='always',
-    )
-
-    boleto_fee_perc = fields.Float(
-        string='Percentual de Multa',
-        digits=dp.get_precision('Account'),
-        track_visibility='always',
-    )
-
-    boleto_discount_perc = fields.Float(
-        string=u"Percentual de Desconto até a Data de Vencimento",
-        digits=dp.get_precision('Account'),
-        track_visibility='always',
-    )
-
-    discount_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Conta Contabil de Desconto',
-        help='Conta padrão para Desconto',
-        track_visibility='always',
-    )
-
-    rebate_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Conta Contabil de Abatimanto',
-        help='Conta padrão para Abatimento',
-        track_visibility='always',
-    )
-
-    tariff_charge_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Conta Contabil Tarifa Bancaria',
-        help='Conta padrão para a Tarifa Bancaria',
-        track_visibility='always',
-    )
+    # Codigos de Retorno do Movimento
 
     # TODO: Campos many2many não estão sendo registrados pelo track_visibility.
     #  Debate no Odoo https://github.com/odoo/odoo/issues/10149
     #  Modulo na OCA v10 que faria isso
     #  https://github.com/OCA/social/tree/10.0/mail_improved_tracking_value
-    #  Migração do Modulo ainda para a v11
-    #  https://github.com/OCA/social/pull/254
-    #  Devemos incluir esse modulo nas Dependencias OCA do repo e se necessário
-    #  fazer essa migração da 12 para poder usa-lo aqui.
+    #  Migração do Modulo para a v12 https://github.com/OCA/social/pull/677
+    #  Devemos incluir esse modulo nas Dependencias OCA para poder usa-lo aqui
+    #  já que sem ele o campo que armazena os codigos que devem ser usados para
+    #  Baixa/Liquidação está sem a rastreabilidade a outra opção seria usar o
+    #  modulo auditlog https://github.com/OCA/server-tools/tree/12.0/auditlog.
+
+    # TODO: Ligação com o payment_mode_id não permite extrair para o objeto
+    #  l10n_br_cnab.boleto.fields, teria alguma forma de fazer ?
     # Podem existir diferentes codigos, mesmo no 240
     cnab_liq_return_move_code_ids = fields.Many2many(
         comodel_name='l10n_br_cnab.return.move.code',
@@ -293,112 +69,6 @@ class AccountPaymentMode(models.Model):
         column1='cnab_liq_return_move_code_id',
         column2='payment_mode_id',
         string='CNAB Liquidity Return Move Code',
-        track_visibility='always',
-    )
-
-    # Codigo de Remessa/Inclusão de Registro Detalhe Liberado
-    cnab_sending_code_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Sending Movement Instruction Code',
-        help='Sending Movement Instruction Code',
-        track_visibility='always',
-    )
-
-    # Codigo para Título/Pagamento Direto ao Fornecedor -Baixar
-    cnab_write_off_code_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Write Off Movement Instruction Code',
-        help='Write Off Movement Instruction Code',
-        track_visibility='always',
-    )
-
-    # Codigo para Alteração do Valor do Titulo
-    cnab_code_change_title_value_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Change Title Value Movement Instruction Code',
-        help='CNAB Movement Instruction Code for Change Title Value.',
-        track_visibility='always',
-    )
-
-    # Codigo para Alteração da Data de Vencimento
-    cnab_code_change_maturity_date_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Change Maturity Date Movement Instruction Code',
-        help='CNAB Movement Instruction Code for Change Maturity Date.',
-        track_visibility='always',
-    )
-
-    # Codigo para Protestar Título
-    cnab_code_protest_title_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Protest Tittle Instruction Code',
-        help='CNAB Movement Instruction Code for Protest Tittle.',
-        track_visibility='always',
-    )
-
-    # Codigo para Suspender Protesto e Manter em Carteira
-    cnab_code_suspend_protest_keep_wallet_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Suspend Protest an Keep in Wallet Instruction Code',
-        help='CNAB Movement Instruction Code for'
-             ' Suspend Protest and Keep in Wallet.',
-        track_visibility='always',
-    )
-
-    # Codigo para Suspender Protesto e Baixar Título
-    cnab_code_suspend_protest_write_off_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Suspend Protest an Writte Off Instruction Code',
-        help='CNAB Movement Instruction Code for'
-             ' Suspend Protest and Writte Off.',
-        track_visibility='always',
-    )
-
-    # Codigo para Conceder Abatimento
-    cnab_code_grant_rebate_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Grant Rebate Instruction Code',
-        help='CNAB Movement Instruction Code for'
-             ' Grant Rebate.',
-        track_visibility='always',
-    )
-
-    # Codigo para Cancelar Abatimento
-    cnab_code_cancel_rebate_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Cancel Rebate Instruction Code',
-        help='CNAB Movement Instruction Code for'
-             ' Cancel Rebate.',
-        track_visibility='always',
-    )
-
-    # Codigo para Conceder Desconto
-    cnab_code_grant_discount_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Grant Discount Instruction Code',
-        help='CNAB Movement Instruction Code for'
-             ' Grant Discount.',
-        track_visibility='always',
-    )
-
-    # Codigo para Cancelar Abatimento
-    cnab_code_cancel_discount_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Cancel Discount Instruction Code',
-        help='CNAB Movement Instruction Code for'
-             ' Cancel Discount.',
-        track_visibility='always',
-    )
-
-    # Field used to make invisible banks specifics fields
-    bank_id = fields.Many2one(
-        related='fixed_journal_id.bank_id',
-    )
-
-    not_payment_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Conta Contabil para Não Pagamento/Inadimplência',
-        help='Conta padrão para Não Pagamento/Inadimplência',
         track_visibility='always',
     )
 
