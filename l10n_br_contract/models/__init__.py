4a9b7a8bef55e8fae8605f9a71b1343954bb1555
Renato Lima <renato.lima@akretion.com>
2021-04-01 22:46:03 -0300

    Remove fiscal document

--- l10n_br_contract/models/__init__.py
+++ l10n_br_contract/models/__init__.py
@@ -1,5 +1,6 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import contract_contract
 from . import contract_line
-from . import document_line
 from . import account_invoice_line
 from . import res_company
