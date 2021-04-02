bb32b0fb39811c5e9edc8c0dfc9e17fc6b738b7a
Renato Lima <renato.lima@akretion.com>
2021-04-01 22:50:40 -0300

    [REF] Contract and Line object

--- l10n_br_contract/models/contract_line.py
+++ l10n_br_contract/models/contract_line.py
@@ -2,26 +2,39 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, fields
+from ...l10n_br_fiscal.constants.fiscal import TAX_FRAMEWORK
 
 
 class ContractLine(models.Model):
-
     _name = 'contract.line'
     _inherit = [_name, 'l10n_br_fiscal.document.line.mixin']
 
     company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Company',
-        default=lambda self: self.contract_id.company_id,
+        related='contract_id.company_id',
+    )
+
+    fiscal_tax_ids = fields.Many2many(
+        comodel_name='l10n_br_fiscal.tax',
+        relation='fiscal_contract_line_tax_rel',
+        column1='document_id',
+        column2='fiscal_tax_id',
+        string='Fiscal Taxes',
+    )
+
+    tax_framework = fields.Selection(
+        selection=TAX_FRAMEWORK,
+        related='contract_id.company_id.tax_framework',
+        string='Tax Framework',
+    )
+
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        related='contract_id.partner_id',
+        string='Partner',
     )
 
     @api.multi
     def _prepare_invoice_line(self, invoice_id=False, invoice_values=False):
-        invoice_line_vals = self._prepare_br_fiscal_dict()
-        invoice_line_vals.update(
-            super()._prepare_invoice_line(invoice_id, invoice_values)
-        )
-        invoice_line_vals['fiscal_operation_id'] =\
-            self.contract_id.fiscal_operation_id.id
-        invoice_line_vals['contract_line_id'] = self.id
-        return invoice_line_vals
+        values = super()._prepare_invoice_line(invoice_id, invoice_values)
+        values.update(self._prepare_br_fiscal_dict())
+        return values
