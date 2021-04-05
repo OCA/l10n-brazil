92b93cf46825354c66fe871be89d8a1c6c19c173
Renato Lima <renato.lima@akretion.com>
2021-04-05 15:43:04 -0300

    [REF] l10n_br_purchase add default fiscal document type

--- l10n_br_purchase/models/account_invoice.py
+++ l10n_br_purchase/models/account_invoice.py
@@ -10,11 +10,13 @@ class AccountInvoice(models.Model):
 
     def _prepare_invoice_line_from_po_line(self, line):
         values = super()._prepare_invoice_line_from_po_line(line)
-        values.update(line._prepare_br_fiscal_dict(default=False))
+        values.update(line._prepare_br_fiscal_dict())
         return values
 
     @api.onchange('purchase_id')
     def purchase_order_change(self):
         if self.purchase_id:
             self.fiscal_operation_id = self.purchase_id.fiscal_operation_id
+            if not self.document_type_id:
+                self.document_type_id = self.company_id.document_type_id
         return super().purchase_order_change()
