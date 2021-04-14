b17b0dc0d4195d0a3d98f7d1fa7b375f6df73a07
Renato Lima <renato.lima@akretion.com>
2021-04-14 17:37:25 -0300

    [ADD] l10n_br_sale migration script

---
+++ l10n_br_sale/migrations/12.0.4.1.0/pre-migration.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2021 - TODAY Renato Lima - Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+_columns_rename = {
+    'sale_order': [
+        ('amount_gross', 'amount_price_gross'),
+    ],
+}
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    for table in _columns_rename.keys():
+        for rename_column in _columns_rename[table]:
+            if openupgrade.column_exists(env.cr, table, rename_column[0]):
+                openupgrade.rename_columns(
+                    env.cr, {table: [rename_column]})
