8c288b9eb816cc1766d949face2747960c2f9788
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-04-13 21:37:43 -0300

    [IMP] l10n_br_stock_account: get fiscal_position from fiscal_operation

--- l10n_br_stock_account/wizards/stock_invoice_onshipping.py
+++ l10n_br_stock_account/wizards/stock_invoice_onshipping.py
@@ -49,6 +49,9 @@ class StockInvoiceOnshipping(models.TransientModel):
 
         fiscal_vals["document_type_id"] = document_type_id
 
+        fiscal_position_id = pick.fiscal_operation_id.fiscal_position_id.id
+        fiscal_vals['fiscal_position_id'] = fiscal_position_id
+
         document_serie = document_type.get_document_serie(
             pick.company_id, pick.fiscal_operation_id
         )
