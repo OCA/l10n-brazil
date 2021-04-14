df8fb4573020fe3aa93da0805e26cc68cfa8f706
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-13 22:47:19 -0300

    [NEW] Test base class and manual workflow

---
+++ l10n_br_account_payment_order/tests/test_invoice_manual_workflow.py
@@ -0,0 +1,40 @@
+# @ 2021 KMEE - kmee.com.br
+#   Luis Felipe Mileo <mileo@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo.tests import tagged
+
+from odoo.addons.l10n_br_account_payment_order.\
+    tests.test_base_class import TestL10nBrAccountPaymentOder
+
+
+@tagged('post_install', '-at_install')
+class TestPaymentOrderManualWorkflow(TestL10nBrAccountPaymentOder):
+
+    def setUp(self):
+        super().setUp()
+        # Get Invoice for test
+        self.invoice_manual_test = self.env.ref(
+            'l10n_br_account_payment_order.'
+            'demo_invoice_manual_test'
+        )
+
+    def _invoice_confirm_flow(self):
+        self.invoice_manual_test.action_invoice_open()
+
+        # I check that the invoice state is "Open"
+        self.assertEquals(self.invoice_manual_test.state, 'open')
+
+    def test_invoice_button(self):
+        """ Test Invoice Button to Add to a payment order"""
+        self._invoice_confirm_flow()
+        self.invoice_manual_test.create_account_payment_line()
+        self._invoice_payment_order_all_workflow(
+            self.invoice_manual_test
+        )
+
+    def test_payment_order_wizard(self):
+        """ Test Payment Order Wizard"""
+        self._invoice_confirm_flow()
+        payment_mode_id = self.invoice_manual_test.payment_mode_id
+        self.import_with_po_wizard(payment_mode_id)
