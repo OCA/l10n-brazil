16ae0304ccfb7a6ce2a201c74dadaec292059209
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-15 14:42:27 -0300

    [NEW] Receita WS

---
+++ l10n_br_receitaws/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'ReceitaWS',
+    'summary': """
+        Integração com os Webservices da ReceitaWS""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/l10n-brazil.git',
+    'depends': [
+        'l10n_br_base',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ l10n_br_receitaws/models/l10n_br_base_party_mixin.py
@@ -0,0 +1,91 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from requests import get
+
+from odoo import api, models
+
+from erpbrasil.base.misc import punctuation_rm
+from erpbrasil.base.fiscal.cnpj_cpf import validar_cnpj
+
+
+class PartyMixin(models.AbstractModel):
+
+    _inherit = 'l10n_br_base.party.mixin'
+
+    @api.onchange("cnpj_cpf")
+    def _onchange_cnpj_cpf(self):
+        result = super()._onchange_cnpj_cpf()
+        cnpj_cpf = punctuation_rm(self.cnpj_cpf)
+        if cnpj_cpf and validar_cnpj(cnpj_cpf):
+            response = get('https://www.receitaws.com.br/v1/cnpj/' + cnpj_cpf)
+            try:
+                data = response.json()
+            except:
+                data = None
+            # TODO: Separar os multiplos telefones
+            # values = {
+            #     'atividade_principal': [{
+            #         'text': 'Fabricação ...',
+            #          'code': '10.91-1-01'
+            #     }],
+            #     'data_situacao': '03/09/9999',
+            #     'abertura': '12/11/9999',
+            #     'tipo': 'MATRIZ',
+            #     'telefone': '(12) 9999-9999/ (12) 9999-9999',
+            #     'qsa': [
+            #         {'qual': '49-Sócio-Administrador', 'nome': 'LUIS'},
+            #         {'qual': '22-Sócio', 'nome': 'MARIA'}
+            #     ],
+            #     'situacao': 'ATIVA',
+            #     'porte': 'EMPRESA DE PEQUENO PORTE',
+            #     'natureza_juridica': '206-2 - Sociedade Empresária Limitada',
+            #     'ultima_atualizacao': '2021-02-08T13:33:39.365Z',
+            #     'status': 'OK',
+            #     'complemento': '',
+            #     'efr': '',
+            #     'motivo_situacao': '',
+            #     'situacao_especial': '',
+            #     'data_situacao_especial': '',
+            #     'atividades_secundarias': [
+            #         {'code': '00.00-0-00', 'text': 'Não informada'}
+            #     ],
+            #     'capital_social': '0.00',
+            #     'extra': {},
+            #     'billing': {'free': True, 'database': True}
+            # }
+            self.company_type = 'company'
+            if data.get('nome') != '':
+                self.legal_name = data['nome']
+            if data.get('fantasia') != '':
+                self.name = data['fantasia']
+            if data.get('email') != '':
+                self.email = data['email']
+            if data.get('complemento') != '':
+                self.street2 = data['complemento']
+            if data.get('cep') != '':
+                self.zip = data['cep']
+            if data.get('numero') != '':
+                self.street_number = data['numero']
+            if data.get('logradouro') != '':
+                self.street = data['logradouro']
+            if data.get('bairro') != '':
+                self.district = data['bairro']
+
+            # TODO: Separar os multiplos telefones
+            if data.get('telefone') != '':
+                self.phone = data['telefone']
+            if data.get('uf') != '':
+                state_id = self.env["res.country.state"].search([
+                    ("code", "=", data['uf']),
+                    ("country_id", "=", self.country_id.id)
+                ], limit=1)
+                self.state_id = state_id
+
+                if data.get('municipio') != '':
+                    city_id = self.env["res.city"].search([
+                        ("name", "=ilike", data["municipio"]),
+                        ("state_id.id", "=", state_id.id)])
+                    if len(city_id) == 1:
+                        self.city_id = city_id
+        return result
