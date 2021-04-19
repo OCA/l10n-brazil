082860e7434ee96b0fb548beb31d9556128bdee3
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-19 00:48:22 -0300

    [FIX] flake8

--- l10n_br_product_contract/models/sale_order.py
+++ l10n_br_product_contract/models/sale_order.py
@@ -1,7 +1,7 @@
-# Copyright 2021 KMEE
+# Copyright 2021 KMEE - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
--- l10n_br_product_contract/models/sale_order_line.py
+++ l10n_br_product_contract/models/sale_order_line.py
@@ -1,13 +1,12 @@
-# Copyright 2021 KMEE
+# Copyright 2021 KMEE - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
-
 
     @api.multi
     def _prepare_contract_line_values(
