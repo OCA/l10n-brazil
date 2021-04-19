d1b35c3df45422483a686008a69507878ac713fb
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-19 00:38:32 -0300

    [FIX] flake8

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -66,7 +66,7 @@ class NFe(spec_models.StackedModel):
         "nfe.40.infadic",
         "nfe.40.cobr",
         "nfe.40.fat",
-	]
+    ]
     _stacked = 'nfe.40.infnfe'
     _stack_skip = ('nfe40_veicTransp')
     _field_prefix = 'nfe40_'
--- l10n_br_nfe/models/payment.py
+++ l10n_br_nfe/models/payment.py
@@ -1,7 +1,7 @@
 # Copyright 2020 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields
 from odoo.addons.spec_driven_model.models import spec_models
 
 
