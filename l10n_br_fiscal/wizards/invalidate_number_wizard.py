d0067ce074bdaa3457b313c89761f2105673255d
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-20 03:22:16 -0300

    [REF] Clean invalidate process

--- l10n_br_fiscal/wizards/invalidate_number_wizard.py
+++ l10n_br_fiscal/wizards/invalidate_number_wizard.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2020  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import api, models
 
 
 class InvalidateNumberWizard(models.TransientModel):
@@ -10,42 +10,16 @@ class InvalidateNumberWizard(models.TransientModel):
     _description = 'Invalidate Number Wizard'
     _inherit = 'l10n_br_fiscal.base.wizard.mixin'
 
-    invalidate_mode = fields.Selection(
-        selection=[
-            ('one_number', _('Only one number')),
-            ('range_numbers', _('Range of numbers')),
-        ],
-        string='Invalidate',
-        default='one_number',
-    )
-
-    number_start = fields.Integer(
-        string='Initial Number',
-        required=1,
-    )
-
-    number_end = fields.Integer(
-        string='Final Number',
-        required=1,
-    )
-
-    @api.onchange('invalidate_mode')
-    def _onchange_invalidate_mode(self):
-        for wizard in self:
-            if (wizard.invalidate_mode == 'one_number'
-                    and wizard.document_id.number):
-                wizard.number_start = wizard.document_id.number
-                wizard.number_end = wizard.document_id.number
-
     @api.multi
     def doit(self):
         for wizard in self:
             invalidate = self.env['l10n_br_fiscal.invalidate.number'].create({
                 'company_id': wizard.document_id.company_id.id,
-                'fiscal_document_id': wizard.document_id.id,
+                'document_type_id': wizard.document_id.document_type_id.id,
                 'document_serie_id': wizard.document_id.document_serie_id.id,
                 'number_start': wizard.document_id.number,
                 'number_end': wizard.document_id.number,
+                'justification': wizard.justification,
             })
-            invalidate.invalidate()
+            invalidate._invalidate(wizard.document_id)
         self._close()
