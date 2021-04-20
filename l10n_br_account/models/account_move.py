4387fa35471096f8f533bdf59d4cdf1a63f37c2b
Renato Lima <renato.lima@akretion.com>
2021-04-20 17:11:25 -0300

    [REF] Account move Withholding invoice methods

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -14,34 +14,50 @@ from odoo.addons.l10n_br_fiscal.constants.fiscal import (
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def _prepare_wh_invoice(self, move_line, fiscal_tax_group):
+    def _prepare_wh_invoice(self, move_line, fiscal_group):
         wh_date_invoice = fields.Date.context_today(self)
         wh_due_invoice = wh_date_invoice.replace(
-            day=fiscal_tax_group.wh_due_day)
+            day=fiscal_group.wh_due_day)
         values = {
-            'partner_id': fiscal_tax_group.partner_id.id,
+            'partner_id': fiscal_group.partner_id.id,
             'date_invoice': wh_date_invoice,
             'date_due': wh_due_invoice + relativedelta(months=1),
             'type': 'in_invoice',
+            'account_id': fiscal_group.partner_id.property_account_payable_id.id,
             'journal_id': move_line.journal_id.id,
-            'origin': move_line.invoice_id.number, # TODO
+            'origin': move_line.invoice_id.number,
         }
         return values
 
-    def _prepare_wh_invoice_line(self, invoice, move_line, fiscal_tax_group):
+    def _prepare_wh_invoice_line(self, invoice, move_line):
         values = {
             'name': move_line.name,
-            'quantity': 1.0,
+            'quantity': move_line.quantity,
+            'uom_id': move_line.product_uom_id,
             'price_unit': abs(move_line.balance),
             'invoice_id': invoice.id,
             'account_id': move_line.account_id.id,
+            'wh_move_line_id': move_line.id,
             'account_analytic_id': move_line.analytic_account_id.id,
         }
         return values
 
-    def create_wh_invoice(self):
+    def _finalize_invoices(self, invoices):
+        for invoice in invoices:
+            invoice.compute_taxes()
+            for line in invoice.invoice_line_ids:
+                # Use additional field helper function (for account extensions)
+                line._set_additional_fields(invoice)
+            invoice._onchange_cash_rounding()
+
+    @api.multi
+    def create_wh_invoices(self):
         for move in self:
             for line in move.line_ids.filtered(lambda l: l.tax_line_id):
+                # Create Wh Invoice only for supplier invoice
+                if line.invoice_id and line.invoice_id.type != 'in_invoice':
+                    continue
+
                 account_tax_group = line.tax_line_id.tax_group_id
                 if account_tax_group and account_tax_group.fiscal_tax_group_id:
                     fiscal_group = account_tax_group.fiscal_tax_group_id
@@ -50,23 +66,40 @@ class AccountMove(models.Model):
                             self._prepare_wh_invoice(line, fiscal_group))
 
                         self.env['account.invoice.line'].create(
-                            self._prepare_wh_invoice_line(
-                                invoice, line, fiscal_group))
+                            self._prepare_wh_invoice_line(invoice, line))
 
+                        self._finalize_invoices(invoice)
                         invoice.action_invoice_open()
 
-    def create_deductible_lines(self):
-        pass
+    @api.multi
+    def _withholding_validate(self):
+        for m in self:
+            invoices = self.env['account.invoice.line'].search(
+                [('wh_move_line_id', 'in', m.mapped('line_ids').ids)]
+            ).mapped('invoice_id')
 
+            invoices.filtered(
+                lambda i: i.state == 'open').action_invoice_cancel()
+
+            invoices.filtered(
+                lambda i: i.state == 'cancel').action_invoice_draft()
+            invoices.invalidate_cache()
+            invoices.filtered(
+                lambda i: i.state == 'draft').unlink()
+
+    @api.multi
     def post(self, invoice=False):
-        dummy_doc = self.env.ref('l10n_br_fiscal.fiscal_document_dummy')
         result = super().post(invoice)
-        self.create_wh_invoice()
-        self.create_deductible_lines()
+        self.create_wh_invoices()
         if invoice:
-            if (invoice.fiscal_document_id != dummy_doc
+            if (invoice.document_type_id
                     and invoice.document_electronic
                     and invoice.issuer == DOCUMENT_ISSUER_COMPANY
                     and invoice.state_edoc != SITUACAO_EDOC_AUTORIZADA):
                 self.button_cancel()
         return result
+
+    @api.multi
+    def button_cancel(self):
+        self._withholding_validate()
+        return super().button_cancel()
