3dd4f2c4b59e212d93da522925d6ae712dc4b0ed
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-22 20:48:10 -0300

    [FIX] Fiscal Document Workflow

--- l10n_br_fiscal/models/document_eletronic.py
+++ l10n_br_fiscal/models/document_eletronic.py
@@ -178,7 +178,7 @@ class DocumentEletronic(models.AbstractModel):
         electronic = self - no_electronic
         electronic._eletronic_document_send()
 
-    def _exec_after_SITUACAO_EDOC_A_ENVIAR(self, old_state, new_state):
+    def _exec_before_SITUACAO_EDOC_A_ENVIAR(self, old_state, new_state):
         super(DocumentEletronic, self)._exec_before_SITUACAO_EDOC_A_ENVIAR(
             old_state, new_state
         )
