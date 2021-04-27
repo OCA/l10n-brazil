d6fdbb3c8c53a35d3a7b98cb515a2b46afee0bf1
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-27 01:56:56 -0300

    [REF] Document status

--- l10n_br_nfse/wizards/wizard_document_status.py
+++ l10n_br_nfse/wizards/wizard_document_status.py
@@ -5,34 +5,28 @@ from odoo import api, fields, models
 
 
 class WizardDocumentStatus(models.TransientModel):
-
-    _inherit = 'l10n_br_fiscal.document.status.wizard'
+    _name = 'l10n_br_fiscal.document.status.wizard'
+    _inherit = 'l10n_br_fiscal.base.wizard.mixin'
 
     rps_number = fields.Char(
         string='RPS Number',
-        default=lambda self: self.env['l10n_br_fiscal.document'].browse(
-            self._context.get('active_ids', [])).rps_number
+        default=lambda self: self.env[
+            self._context.get('active_model')
+        ].browse(self._context.get('active_ids', [])).rps_number
     )
 
     @api.multi
     def get_document_status(self):
-        for data in self:
-            document_id = self.env['l10n_br_fiscal.document'].browse(
-                self._context.get('active_ids', []))[0]
-            call_result = {
-                "document_status": document_id.action_consultar_nfse_rps(),
-                "rps_number": data.rps_number,
-                "state": "done",
-            }
-            data.write(call_result)
+        self.write({
+            "document_status": self.document_id.action_consultar_nfse_rps(),
+            "rps_number": self.document_id.rps_number,
+            "state": "done",
+        })
+        return self._reopen()
 
-            return {
-                "name": "Fiscal Document Inquiry",
-                "view_type": "form",
-                "view_mode": "form",
-                "res_model": "l10n_br_fiscal.document.status.wizard",
-                "res_id": data.id,
-                "type": "ir.actions.act_window",
-                "target": "new",
-                "context": data.env.context,
-            }
+    @api.multi
+    def doit(self):
+        for wizard in self:
+            if wizard.document_id:
+                return wizard.get_document_status()
+        self._close()
