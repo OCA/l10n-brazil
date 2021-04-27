4a86e684451cedce9b01a56d67302e920f73fc53
Magno Costa <magno.costa@akretion.com.br>
2021-04-27 17:23:18 -0300

    [IMP] Included field for amount weight and volume in sale order.

--- l10n_br_delivery/models/sale_order.py
+++ l10n_br_delivery/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, models
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -28,3 +28,31 @@ class SaleOrder(models.Model):
                 price_unit = order.carrier_id.rate_shipment(order)['price']
                 order.amount_freight_value = price_unit
         return True
+
+    # TODO: Deveria exisitr o Peso Total Liquido e Peso Total Bruto como era na
+    #  v8, aparetemente agora so existe o campo weight no cadastro do Produto
+    amount_weight = fields.Float(
+        string=_('Amount Weight'),
+        compute='_compute_amount_weight'
+    )
+
+    amount_volume = fields.Float(
+        string=_('Amount Volume'),
+        compute='_compute_amount_volume'
+    )
+
+    def _compute_amount_weight(self):
+
+        for record in self:
+            amount_weight = 0.0
+            for l in record.order_line:
+                amount_weight += l.product_qty * l.product_id.weight
+            record.amount_weight = amount_weight
+
+    def _compute_amount_volume(self):
+
+        for record in self:
+            amount_volume = 0.0
+            for l in record.order_line:
+                amount_volume += l.product_qty * l.product_id.volume
+            record.amount_volume = amount_volume
