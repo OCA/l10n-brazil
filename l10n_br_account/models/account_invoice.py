7448838362dbcb79b3dbab535ca31a748534e3da
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-11 15:12:28 -0300

    [NEW] Optional Use Origin Taxes on refund
    [FIX] Correção pos rebase

--- l10n_br_account/models/account_invoice.py
+++ l10n_br_account/models/account_invoice.py
@@ -497,7 +497,7 @@ class AccountInvoice(models.Model):
     def refund(self, date_invoice=None, date=None, description=None, journal_id=None):
         new_invoices = super(AccountInvoice, self).refund(
             date_invoice, date, description, journal_id)
-        
+
         force_fiscal_operation_id = False
         if self.env.context.get('force_fiscal_operation_id'):
             force_fiscal_operation_id = self.env['l10n_br_fiscal.operation'].browse(
