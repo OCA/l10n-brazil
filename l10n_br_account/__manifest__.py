f9f236b57da63ac44a5a50d04a3eb2c17400599e
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2021-05-13 11:32:22 -0300

    [REF] Rename views files

--- l10n_br_account/__manifest__.py
+++ l10n_br_account/__manifest__.py
@@ -24,8 +24,8 @@
         'views/fiscal_operation_line_view.xml',
         'views/account_invoice_view.xml',
         'views/account_invoice_line_view.xml',
-        'views/nfe_document_line_view.xml',
-        'views/nfe_document_view.xml',
+        'views/document_line_view.xml',
+        'views/document_view.xml',
         'views/fiscal_invoice_view.xml',
         'views/fiscal_invoice_line_view.xml',
 
