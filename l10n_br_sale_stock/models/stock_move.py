01763ce19aefdc90a178d6e0e3ee2370e90ba21b
Raphaël Valyi <rvalyi@akretion.com>
2021-05-16 14:02:32 -0300

    remove useless default @api.multi

--- l10n_br_sale_stock/models/stock_move.py
+++ l10n_br_sale_stock/models/stock_move.py
@@ -2,13 +2,12 @@
 # Copyright (C) 2021  Magno Costa - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models
+from odoo import models
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    @api.multi
     def _get_price_unit_invoice(self, inv_type, partner, qty=1):
         result = super()._get_price_unit_invoice(inv_type, partner, qty)
         # Caso tenha Sale Line já vem desagrupado aqui devido ao KEY
