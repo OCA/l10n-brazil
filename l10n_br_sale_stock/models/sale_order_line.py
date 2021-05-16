f0c75f160663fc7d07b3b5f1cce7d5c0e9c9f8e6
Raphaël Valyi <rvalyi@akretion.com>
2021-05-16 14:02:32 -0300

    remove useless default @api.multi

--- l10n_br_sale_stock/models/sale_order_line.py
+++ l10n_br_sale_stock/models/sale_order_line.py
@@ -9,7 +9,6 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.multi
     def _prepare_procurement_values(self, group_id=False):
         values = self._prepare_br_fiscal_dict()
         values.update(super()._prepare_procurement_values(group_id))
