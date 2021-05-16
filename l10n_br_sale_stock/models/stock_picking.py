1261a5d920a0289ef2d4c311f8f12d16e8d479fc
Raphaël Valyi <rvalyi@akretion.com>
2021-05-16 14:02:32 -0300

    remove useless default @api.multi

--- l10n_br_sale_stock/models/stock_picking.py
+++ l10n_br_sale_stock/models/stock_picking.py
@@ -1,13 +1,12 @@
 # Copyright (C) 2021  Magno Costa - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models
+from odoo import models
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.multi
     def _get_partner_to_invoice(self):
         self.ensure_one()
         partner = self.partner_id
