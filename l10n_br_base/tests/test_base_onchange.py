e2eee5c7b53db68aaabc44ceea9f26c6f141da2b
Raphaël Valyi <rvalyi@akretion.com>
2021-05-19 01:10:58 -0300

    flake8 B902 'cls' used for instance method

--- l10n_br_base/tests/test_base_onchange.py
+++ l10n_br_base/tests/test_base_onchange.py
@@ -30,38 +30,38 @@ class L10nBrBaseOnchangeTest(SavepointCase):
                 {"name": "Partner Test 01", "zip": "29161-695"}
             )
 
-    def test_onchange(cls):
+    def test_onchange(self):
         """
         Call all the onchange methods in l10n_br_base
         """
-        cls.company_01._onchange_cnpj_cpf()
-        cls.company_01._onchange_city_id()
-        cls.company_01._onchange_zip()
-        cls.company_01._onchange_state()
+        self.company_01._onchange_cnpj_cpf()
+        self.company_01._onchange_city_id()
+        self.company_01._onchange_zip()
+        self.company_01._onchange_state()
 
-        cls.partner_01._onchange_cnpj_cpf()
-        cls.partner_01._onchange_city_id()
-        cls.partner_01._onchange_zip()
+        self.partner_01._onchange_cnpj_cpf()
+        self.partner_01._onchange_city_id()
+        self.partner_01._onchange_zip()
 
-    def test_inverse_fields(cls):
-        cls.company_01.inscr_mun = "692015742119"
-        cls.assertEquals(
-            cls.company_01.partner_id.inscr_mun,
+    def test_inverse_fields(self):
+        self.company_01.inscr_mun = "692015742119"
+        self.assertEquals(
+            self.company_01.partner_id.inscr_mun,
             "692015742119",
             "The inverse function to field inscr_mun failed.",
         )
-        cls.company_01.suframa = "1234"
-        cls.assertEquals(
-            cls.company_01.partner_id.suframa,
+        self.company_01.suframa = "1234"
+        self.assertEquals(
+            self.company_01.partner_id.suframa,
             "1234",
             "The inverse function to field suframa failed.",
         )
 
-    def test_display_address(cls):
-        partner = cls.env.ref("l10n_br_base.res_partner_akretion")
+    def test_display_address(self):
+        partner = self.env.ref("l10n_br_base.res_partner_akretion")
         partner._onchange_city_id()
         display_address = partner._display_address()
-        cls.assertEquals(
+        self.assertEquals(
             display_address,
             "Akretion Sao Paulo\n"
             "Avenida Paulista, 807 CJ 2315\nCentro"
@@ -69,11 +69,11 @@ class L10nBrBaseOnchangeTest(SavepointCase):
             "The function _display_address failed.",
         )
 
-    def test_display_address_parent_id(cls):
-        partner = cls.env.ref("l10n_br_base.res_partner_address_ak2")
+    def test_display_address_parent_id(self):
+        partner = self.env.ref("l10n_br_base.res_partner_address_ak2")
         partner._onchange_city_id()
         display_address = partner._display_address()
-        cls.assertEquals(
+        self.assertEquals(
             display_address,
             "Akretion Rio de Janeiro\n"
             "Rua Acre, 47 sala 1310\nCentro"
@@ -81,10 +81,10 @@ class L10nBrBaseOnchangeTest(SavepointCase):
             "The function _display_address with parent_id failed.",
         )
 
-    def test_other_country_display_address(cls):
-        partner = cls.env.ref("l10n_br_base.res_partner_exterior")
+    def test_other_country_display_address(self):
+        partner = self.env.ref("l10n_br_base.res_partner_exterior")
         display_address = partner._display_address()
-        cls.assertEquals(
+        self.assertEquals(
             display_address,
             "Cliente Exterior\n3404  Edgewood"
             " Road\n\nJonesboro"
@@ -92,11 +92,11 @@ class L10nBrBaseOnchangeTest(SavepointCase):
             "The function _display_address for other country failed.",
         )
 
-    def test_display_address_without_company(cls):
-        partner = cls.env.ref("l10n_br_base.res_partner_akretion")
+    def test_display_address_without_company(self):
+        partner = self.env.ref("l10n_br_base.res_partner_akretion")
         partner._onchange_city_id()
         display_address = partner._display_address(without_company=True)
-        cls.assertEquals(
+        self.assertEquals(
             display_address,
             "Avenida Paulista, 807 CJ 2315\nCentro"
             "\n01311-915 - São Paulo-SP\nBrazil",
