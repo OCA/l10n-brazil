743f17c32f8a5fd276a7c5de2f29d7a0708db122
Magno Costa <magno.costa@akretion.com.br>
2021-05-19 13:20:07 -0300

    [IMP] Included fields to be able to used at inherit objects.

---
+++ l10n_br_delivery/models/fiscal_document_mixin.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2021-Today - Akretion (<http://www.akretion.com>).
+# @author Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class FiscalDocumentMixin(models.AbstractModel):
+    _inherit =  'l10n_br_fiscal.document.mixin'
+
+    def _get_default_incoterm(self):
+        return self.env.user.company_id.incoterm_id
+
+    # Esta sendo implementado aqui para existir nos objetos herdados
+    incoterm_id = fields.Many2one(
+        comodel_name='account.incoterms',
+        string='Incoterm',
+        help=_('International Commercial Terms are a series of'
+               ' predefined commercial terms used in international'
+               ' transactions.')
+    )
+
+    carrier_id = fields.Many2one(
+        comodel_name='delivery.carrier',
+        string='Carrier',
+        ondelete='cascade',
+    )
