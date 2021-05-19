e6488a8bc999123d91eac253f80de082bbbcd70a
Magno Costa <magno.costa@akretion.com.br>
2021-05-19 18:05:40 -0300

    [FIX] Flake8.

--- l10n_br_delivery/models/fiscal_document_mixin.py
+++ l10n_br_delivery/models/fiscal_document_mixin.py
@@ -2,11 +2,11 @@
 # @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 
 
 class FiscalDocumentMixin(models.AbstractModel):
-    _inherit =  'l10n_br_fiscal.document.mixin'
+    _inherit = 'l10n_br_fiscal.document.mixin'
 
     def _get_default_incoterm(self):
         return self.env.user.company_id.incoterm_id
