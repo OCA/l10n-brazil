bba32818827aa3f171cf8a53d842992a2adb2c1d
Magno Costa <magno.costa@akretion.com.br>
2021-05-20 12:49:54 -0300

    [FIX+IMP] Test with duplicate name and check the quantity, price and fiscal_price in case of grouping pickings.

--- l10n_br_stock_account/tests/test_invoicing_picking.py
+++ l10n_br_stock_account/tests/test_invoicing_picking.py
@@ -235,8 +235,11 @@ class InvoicingPickingTest(SavepointCase):
         self.assertIn(picking, invoice.picking_ids)
         self.assertIn(picking2, invoice.picking_ids)
         for inv_line in invoice.invoice_line_ids:
-            # qty = 3 because 1 move + duplicate one + 1 new
-            self.assertAlmostEqual(inv_line.quantity, 3)
+            # qty = 4 because 2 for each stock.move
+            self.assertEqual(inv_line.quantity, 4)
+            # Price Unit e Fiscal Price devem ser positivos
+            self.assertEqual(inv_line.price_unit, 100.0)
+            self.assertEqual(inv_line.fiscal_price, 100.0)
             self.assertTrue(
                 inv_line.invoice_line_tax_ids,
                 'Error to map Sale Tax in invoice.line.')
@@ -248,7 +251,7 @@ class InvoicingPickingTest(SavepointCase):
         # Should be equals because we delete the invoice
         self.assertEquals(nb_invoice_before, nb_invoice_after)
 
-    def test_picking_invoicing_by_product2(self):
+    def test_picking_invoicing_by_product3(self):
         """
         Test the invoice generation grouped by partner/product with 2
         picking and 3 moves per picking, but 1 picking are the one
