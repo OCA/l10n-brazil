18efc58812a5a40ec19b42112051ff366a13eec7
Magno Costa <magno.costa@akretion.com.br>
2021-05-20 09:53:50 -0300

    [REF] Included myself, Magno Costa, as author in the files where i has 25% of code.

--- l10n_br_account_payment_order/models/account_payment.py
+++ l10n_br_account_payment_order/models/account_payment.py
@@ -1,7 +1,9 @@
 # © 2012 KMEE INFORMATICA LTDA
 #   @author  Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
 #   @author  Hugo Uchôas Borges <hugo.borges@kmee.com.br>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright (C) 2021-Today - Akretion (<http://www.akretion.com>).
+# @author Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, _
 from odoo.exceptions import UserError
