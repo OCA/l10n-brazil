e6547536b21b4348281e1dd7f577c5d40742fc74
Magno Costa <magno.costa@akretion.com.br>
2021-05-21 11:02:28 -0300

    [REF] Changed method Delivery Price to get value from amount_freight_value, to don't has different values.

--- l10n_br_delivery/models/carrier.py
+++ l10n_br_delivery/models/carrier.py
@@ -17,3 +17,22 @@ class Carrier(models.Model):
         string='Codigo ANTT',
         size=32,
     )
+
+    def rate_shipment(self, order):
+        """ Compute the price of the order shipment
+
+        :param order: record of sale.order
+        :return dict: {'success': boolean,
+                       'price': a float,
+                       'error_message': a string containing an error message,
+                       'warning_message': a string containing a warning message}
+                       # TODO maybe the currency code?
+        """
+        self.ensure_one()
+        res = super().rate_shipment(order)
+        # TODO: Localização deveria ter uma maior aderencia
+        #  aos metodos do core, mapear melhor os processos,
+        #  com dados de demo e testes.
+        res['price'] = order.amount_freight_value
+
+        return res
