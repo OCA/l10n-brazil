5c4add05c737e71148eec851b5c18f71a40d788f
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-26 19:38:49 -0300

    [REF] api.multi

--- l10n_br_product_contract/models/sale_order.py
+++ l10n_br_product_contract/models/sale_order.py
@@ -1,14 +1,13 @@
 # Copyright 2021 KMEE - Luis Felipe Mileo <mileo@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    @api.multi
     def _prepare_contract_value(self, contract_template):
         self.ensure_one()
         vals = self._prepare_br_fiscal_dict()
