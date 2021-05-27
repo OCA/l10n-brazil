e4c16b68cedd5bb6bee284efe77dfdf7518c6d50
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-27 20:02:54 -0300

    [REF] Convert self ->cls

--- l10n_br_account_payment_order/tests/test_payment_mode.py
+++ l10n_br_account_payment_order/tests/test_payment_mode.py
@@ -10,63 +10,64 @@ from odoo.exceptions import ValidationError
 
 class TestPaymentMode(TransactionCase):
 
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        if not self.env['account.account'].search([]):
+        if not cls.env['account.account'].search([]):
             raise unittest.SkipTest('No accounts defined')
 
-        self.company = self.env.ref('base.main_company')
-        self.journal_c1 = self.env['account.journal'].create({
+        cls.company = cls.env.ref('base.main_company')
+        cls.journal_c1 = cls.env['account.journal'].create({
             'name': 'Journal 1',
             'code': 'J1',
             'type': 'bank',
-            'company_id': self.company.id,
+            'company_id': cls.company.id,
         })
 
-        self.account = self.env['account.account'].search([
+        cls.account = cls.env['account.account'].search([
             ('reconcile', '=', True),
-            ('company_id', '=', self.company.id)
+            ('company_id', '=', cls.company.id)
         ], limit=1)
 
-        self.type_240 = self.env.ref(
+        cls.type_240 = cls.env.ref(
             'l10n_br_account_payment_order.payment_mode_type_cnab240')
 
-        self.type_400 = self.env.ref(
+        cls.type_400 = cls.env.ref(
             'l10n_br_account_payment_order.payment_mode_type_cnab400')
 
-        self.type_500 = self.env.ref(
+        cls.type_500 = cls.env.ref(
             'l10n_br_account_payment_order.payment_mode_type_cnab500')
 
-        self.payment_mode_test_240 = self.env['account.payment.mode'].create({
+        cls.payment_mode_test_240 = cls.env['account.payment.mode'].create({
             'name': 'Banco Teste 240',
             'bank_account_link': 'fixed',
-            'payment_method_id': self.type_240.id,
-            'company_id': self.company.id,
-            'fixed_journal_id': self.journal_c1.id,
+            'payment_method_id': cls.type_240.id,
+            'company_id': cls.company.id,
+            'fixed_journal_id': cls.journal_c1.id,
             'group_lines': False,
             'generate_move': False,
             'post_move': False,
         })
 
-        self.payment_mode_test_400 = self.env['account.payment.mode'].create({
+        cls.payment_mode_test_400 = cls.env['account.payment.mode'].create({
             'name': 'Banco Teste 400',
             'bank_account_link': 'fixed',
-            'payment_method_id': self.type_400.id,
-            'company_id': self.company.id,
-            'fixed_journal_id': self.journal_c1.id,
+            'payment_method_id': cls.type_400.id,
+            'company_id': cls.company.id,
+            'fixed_journal_id': cls.journal_c1.id,
             'group_lines': False,
             'generate_move': False,
             'post_move': False,
 
         })
 
-        self.payment_mode_test_500 = self.env['account.payment.mode'].create({
+        cls.payment_mode_test_500 = cls.env['account.payment.mode'].create({
             'name': 'Banco Teste 500',
             'bank_account_link': 'fixed',
-            'payment_method_id': self.type_500.id,
-            'company_id': self.company.id,
-            'fixed_journal_id': self.journal_c1.id,
+            'payment_method_id': cls.type_500.id,
+            'company_id': cls.company.id,
+            'fixed_journal_id': cls.journal_c1.id,
             'group_lines': False,
             'generate_move': False,
             'post_move': False,
