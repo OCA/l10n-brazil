73f1520a49066e36da03a10845a7008a2787eb83
Raphaël Valyi <rvalyi@akretion.com>
2021-05-27 12:10:21 -0300

    m2m column typo fix with migration script

--- l10n_br_fiscal/__manifest__.py
+++ l10n_br_fiscal/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Akretion, Odoo Community Association (OCA)",
     "maintainers": ["renatonlima"],
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "12.0.14.0.1",
+    "version": "12.0.15.0.0",
     "depends": [
         "uom",
         "decimal_precision",
