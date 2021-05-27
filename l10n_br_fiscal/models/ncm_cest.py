6115386970fc2b4b7256797551af5d3c0f3b230b
Raphaël Valyi <rvalyi@akretion.com>
2021-05-27 12:10:21 -0300

    m2m column typo fix with migration script

--- l10n_br_fiscal/models/ncm_cest.py
+++ l10n_br_fiscal/models/ncm_cest.py
@@ -10,7 +10,7 @@ class NCM(models.Model):
     cest_ids = fields.Many2many(
         comodel_name='l10n_br_fiscal.cest',
         relation='fiscal_cest_ncm_rel',
-        colunm1='ncm_id',
-        colunm2='cest_id',
+        column1='ncm_id',
+        column2='cest_id',
         readonly=True,
         string='CESTs')
