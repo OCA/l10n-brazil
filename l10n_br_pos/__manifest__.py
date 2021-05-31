b0ff4713f46c1594065cffdb8f5cb30191083650
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-31 08:56:48 -0300

    [MIG] Enable views

--- l10n_br_pos/__manifest__.py
+++ l10n_br_pos/__manifest__.py
@@ -12,6 +12,7 @@
     "depends": [
         'l10n_br_stock_account',
         'l10n_br_zip',
+        'l10n_br_base',
         # 'pos_payment_term',
         # 'pos_order_picking_link',
         'stock_picking_invoice_link',
@@ -22,18 +23,21 @@
     # },
     'data': [
         # "wizard/l10n_br_pos_order_return.xml",
-        # "views/pos_template.xml",
-        # "views/point_of_sale_view.xml",
-        # "views/point_of_sale_report.xml",
-        # "views/pos_order_line_view.xml",
-        # "views/res_partner.xml",
-        # "views/account_journal_view.xml",
         # "wizard/sat_xml_periodic_export.xml",
+
+        # "views/res_partner.xml",
         # "views/account_invoice_view.xml"
+
         "views/res_company.xml",
+        "views/point_of_sale_view.xml",
+        "views/pos_order_line_view.xml",
+        "views/account_journal_view.xml",
+
+        # "views/point_of_sale_report.xml",
+        # "views/pos_template.xml",
     ],
     "qweb": [
-        # 'static/src/xml/pos.xml',
+        'static/src/xml/pos.xml',
     ],
     "installable": True,
 }
