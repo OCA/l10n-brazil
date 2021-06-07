17407d4b3addcb4d1dcf1b65c115d398e3300c72
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:57:02 -0300

    [IMP] l10n_br_sale_stock: black, isort, prettier

--- l10n_br_sale_stock/models/res_config_settings.py
+++ l10n_br_sale_stock/models/res_config_settings.py
@@ -6,9 +6,8 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     sale_create_invoice_policy = fields.Selection(
-        related='company_id.sale_create_invoice_policy',
-        readonly=False
+        related="company_id.sale_create_invoice_policy", readonly=False
     )
