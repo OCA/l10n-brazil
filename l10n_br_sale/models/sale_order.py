08e5caab2d755426ee98ce8fbed31bd508655f5b
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:41:59 -0300

    [IMP] l10n_br_sale: black, isort, prettier

--- l10n_br_sale/models/sale_order.py
+++ l10n_br_sale/models/sale_order.py
@@ -2,8 +2,9 @@
 # Copyright (C) 2012  Raphaël Valyi - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from lxml import etree
 from functools import partial
+
+from lxml import etree
 
 from odoo import api, fields, models
 from odoo.tools import float_is_zero
@@ -11,8 +12,8 @@ from odoo.tools.misc import formatLang
 
 
 class SaleOrder(models.Model):
-    _name = 'sale.order'
-    _inherit = [_name, 'l10n_br_fiscal.document.mixin']
+    _name = "sale.order"
+    _inherit = [_name, "l10n_br_fiscal.document.mixin"]
 
     @api.model
     def _default_fiscal_operation(self):
@@ -24,117 +25,118 @@ class SaleOrder(models.Model):
 
     @api.model
     def _fiscal_operation_domain(self):
-        domain = [('state', '=', 'approved')]
+        domain = [("state", "=", "approved")]
         return domain
 
     fiscal_operation_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.operation',
+        comodel_name="l10n_br_fiscal.operation",
         readonly=True,
-        states={'draft': [('readonly', False)]},
+        states={"draft": [("readonly", False)]},
         default=_default_fiscal_operation,
         domain=lambda self: self._fiscal_operation_domain(),
     )
 
     ind_pres = fields.Selection(
         readonly=True,
-        states={'draft': [('readonly', False)]},
+        states={"draft": [("readonly", False)]},
     )
 
     copy_note = fields.Boolean(
-        string='Copiar Observação no documentos fiscal',
+        string="Copiar Observação no documentos fiscal",
         default=_default_copy_note,
     )
 
     cnpj_cpf = fields.Char(
-        string='CNPJ/CPF',
-        related='partner_id.cnpj_cpf',
+        string="CNPJ/CPF",
+        related="partner_id.cnpj_cpf",
     )
 
     legal_name = fields.Char(
-        string='Legal Name',
-        related='partner_id.legal_name',
+        string="Legal Name",
+        related="partner_id.legal_name",
     )
 
     ie = fields.Char(
-        string='State Tax Number/RG',
-        related='partner_id.inscr_est',
+        string="State Tax Number/RG",
+        related="partner_id.inscr_est",
     )
 
     discount_rate = fields.Float(
-        string='Discount',
+        string="Discount",
         readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+        states={"draft": [("readonly", False)], "sent": [("readonly", False)]},
     )
 
     comment_ids = fields.Many2many(
-        comodel_name='l10n_br_fiscal.comment',
-        relation='sale_order_comment_rel',
-        column1='sale_id',
-        column2='comment_id',
-        string='Comments',
+        comodel_name="l10n_br_fiscal.comment",
+        relation="sale_order_comment_rel",
+        column1="sale_id",
+        column2="comment_id",
+        string="Comments",
     )
 
     def _get_amount_lines(self):
         """Get object lines instaces used to compute fields"""
-        return self.mapped('order_line')
+        return self.mapped("order_line")
 
-    @api.depends('order_line')
+    @api.depends("order_line")
     def _compute_amount(self):
         super()._compute_amount()
 
-    @api.depends('order_line.price_total')
+    @api.depends("order_line.price_total")
     def _amount_all(self):
         """Compute the total amounts of the SO."""
         for order in self:
             order._compute_amount()
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type="form",
-                        toolbar=False, submenu=False):
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
 
-        order_view = super().fields_view_get(
-            view_id, view_type, toolbar, submenu
-        )
+        order_view = super().fields_view_get(view_id, view_type, toolbar, submenu)
 
-        if view_type == 'form':
-            sub_form_view = order_view.get(
-                'fields', {}).get('order_line', {}).get(
-                'views', {}).get('form', {}).get('arch', {})
+        if view_type == "form":
+            sub_form_view = (
+                order_view.get("fields", {})
+                .get("order_line", {})
+                .get("views", {})
+                .get("form", {})
+                .get("arch", {})
+            )
 
-            view = self.env['ir.ui.view']
+            view = self.env["ir.ui.view"]
 
             sub_form_node = etree.fromstring(
-                self.env['sale.order.line'].fiscal_form_view(sub_form_view))
+                self.env["sale.order.line"].fiscal_form_view(sub_form_view)
+            )
 
             sub_arch, sub_fields = view.postprocess_and_fields(
-                'sale.order.line', sub_form_node, None)
+                "sale.order.line", sub_form_node, None
+            )
 
-            order_view['fields']['order_line']['views']['form'][
-                'fields'] = sub_fields
+            order_view["fields"]["order_line"]["views"]["form"]["fields"] = sub_fields
 
-            order_view['fields']['order_line']['views']['form'][
-                'arch'] = sub_arch
+            order_view["fields"]["order_line"]["views"]["form"]["arch"] = sub_arch
 
         return order_view
 
-    @api.onchange('discount_rate')
+    @api.onchange("discount_rate")
     def onchange_discount_rate(self):
         for order in self:
             for line in order.order_line:
                 if line.discount_fixed:
                     continue
-                if self.env.user.has_group(
-                        'l10n_br_sale.group_discount_per_value'):
-                    line.discount_value = (
-                        (line.product_uom_qty * line.price_unit)
-                        * (order.discount_rate / 100)
+                if self.env.user.has_group("l10n_br_sale.group_discount_per_value"):
+                    line.discount_value = (line.product_uom_qty * line.price_unit) * (
+                        order.discount_rate / 100
                     )
                     line._onchange_discount_value()
                 else:
                     line.discount = order.discount_rate
                     line._onchange_discount_percent()
 
-    @api.onchange('fiscal_operation_id')
+    @api.onchange("fiscal_operation_id")
     def _onchange_fiscal_operation_id(self):
         super()._onchange_fiscal_operation_id()
         self.fiscal_position_id = self.fiscal_operation_id.fiscal_position_id
@@ -144,25 +146,27 @@ class SaleOrder(models.Model):
         result = super()._prepare_invoice()
         result.update(self._prepare_br_fiscal_dict())
 
-        document_type_id = self._context.get('document_type_id')
+        document_type_id = self._context.get("document_type_id")
 
         if document_type_id:
-            document_type = self.env['l10n_br_fiscal.document.type'].browse(
-                document_type_id)
+            document_type = self.env["l10n_br_fiscal.document.type"].browse(
+                document_type_id
+            )
         else:
             document_type = self.company_id.document_type_id
             document_type_id = self.company_id.document_type_id.id
 
         if document_type:
-            result['document_type_id'] = document_type_id
+            result["document_type_id"] = document_type_id
             document_serie = document_type.get_document_serie(
-                self.company_id, self.fiscal_operation_id)
+                self.company_id, self.fiscal_operation_id
+            )
             if document_serie:
-                result['document_serie_id'] = document_serie.id
+                result["document_serie_id"] = document_serie.id
 
         if self.fiscal_operation_id:
             if self.fiscal_operation_id.journal_id:
-                result['journal_id'] = self.fiscal_operation_id.journal_id.id
+                result["journal_id"] = self.fiscal_operation_id.journal_id.id
 
         return result
 
@@ -176,8 +180,7 @@ class SaleOrder(models.Model):
         document_type_list = []
 
         for invoice_id in inv_ids:
-            invoice_created_by_super = self.env[
-                'account.invoice'].browse(invoice_id)
+            invoice_created_by_super = self.env["account.invoice"].browse(invoice_id)
 
             # Identify how many Document Types exist
             for inv_line in invoice_created_by_super.invoice_line_ids:
@@ -185,36 +188,41 @@ class SaleOrder(models.Model):
                 if inv_line.display_type:
                     continue
 
-                fiscal_document_type = \
+                fiscal_document_type = (
                     inv_line.fiscal_operation_line_id.get_document_type(
-                        inv_line.invoice_id.company_id)
+                        inv_line.invoice_id.company_id
+                    )
+                )
 
                 if fiscal_document_type.id not in document_type_list:
                     document_type_list.append(fiscal_document_type.id)
 
             # Check if there more than one Document Type
-            if ((fiscal_document_type.id !=
-                    invoice_created_by_super.document_type_id.id) or
-                    (len(document_type_list) > 1)):
+            if (
+                fiscal_document_type.id != invoice_created_by_super.document_type_id.id
+            ) or (len(document_type_list) > 1):
 
                 # Remove the First Document Type,
                 # already has Invoice created
-                invoice_created_by_super.document_type_id =\
-                    document_type_list.pop(0)
+                invoice_created_by_super.document_type_id = document_type_list.pop(0)
 
                 for document_type in document_type_list:
-                    document_type = self.env[
-                        'l10n_br_fiscal.document.type'].browse(document_type)
+                    document_type = self.env["l10n_br_fiscal.document.type"].browse(
+                        document_type
+                    )
 
-                    inv_obj = self.env['account.invoice']
+                    inv_obj = self.env["account.invoice"]
                     invoices = {}
                     references = {}
                     invoices_origin = {}
                     invoices_name = {}
 
                     for order in self:
-                        group_key = order.id if grouped else (
-                            order.partner_invoice_id.id, order.currency_id.id)
+                        group_key = (
+                            order.id
+                            if grouped
+                            else (order.partner_invoice_id.id, order.currency_id.id)
+                        )
 
                         if group_key not in invoices:
                             inv_data = order.with_context(
@@ -229,17 +237,20 @@ class SaleOrder(models.Model):
                         elif group_key in invoices:
                             if order.name not in invoices_origin[group_key]:
                                 invoices_origin[group_key].append(order.name)
-                            if (order.client_order_ref and
-                               order.client_order_ref not in
-                               invoices_name[group_key]):
-                                invoices_name[group_key].append(
-                                    order.client_order_ref)
+                            if (
+                                order.client_order_ref
+                                and order.client_order_ref
+                                not in invoices_name[group_key]
+                            ):
+                                invoices_name[group_key].append(order.client_order_ref)
 
                     # Update Invoice Line
                     for inv_line in invoice_created_by_super.invoice_line_ids:
-                        fiscal_document_type = \
+                        fiscal_document_type = (
                             inv_line.fiscal_operation_line_id.get_document_type(
-                                inv_line.invoice_id.company_id)
+                                inv_line.invoice_id.company_id
+                            )
+                        )
                         if fiscal_document_type.id == document_type.id:
                             inv_line.invoice_id = invoice.id
 
@@ -278,23 +289,30 @@ class SaleOrder(models.Model):
             fmt = partial(
                 formatLang,
                 self.with_context(lang=order.partner_id.lang).env,
-                currency_obj=currency
+                currency_obj=currency,
             )
             res = {}
             for line in order.order_line:
-                taxes = line._compute_taxes(line.fiscal_tax_ids)['taxes']
+                taxes = line._compute_taxes(line.fiscal_tax_ids)["taxes"]
                 for tax in line.fiscal_tax_ids:
                     computed_tax = taxes.get(tax.tax_domain)
                     pr = order.currency_id.rounding
                     if computed_tax and not float_is_zero(
-                            computed_tax.get('tax_value', 0.0), precision_rounding=pr):
+                        computed_tax.get("tax_value", 0.0), precision_rounding=pr
+                    ):
                         group = tax.tax_group_id
-                        res.setdefault(group, {'amount': 0.0, 'base': 0.0})
-                        res[group]['amount'] += computed_tax.get('tax_value', 0.0)
-                        res[group]['base'] += computed_tax.get('base', 0.0)
+                        res.setdefault(group, {"amount": 0.0, "base": 0.0})
+                        res[group]["amount"] += computed_tax.get("tax_value", 0.0)
+                        res[group]["base"] += computed_tax.get("base", 0.0)
             res = sorted(res.items(), key=lambda l: l[0].sequence)
-            order.amount_by_group = [(
-                l[0].name, l[1]['amount'], l[1]['base'],
-                fmt(l[1]['amount']), fmt(l[1]['base']),
-                len(res),
-            ) for l in res]
+            order.amount_by_group = [
+                (
+                    l[0].name,
+                    l[1]["amount"],
+                    l[1]["base"],
+                    fmt(l[1]["amount"]),
+                    fmt(l[1]["base"]),
+                    len(res),
+                )
+                for l in res
+            ]
--- l10n_br_sale/tests/test_l10n_br_sale_sn.py
+++ l10n_br_sale/tests/test_l10n_br_sale_sn.py
@@ -7,11 +7,10 @@ from .test_l10n_br_sale import L10nBrSaleBaseTest
 
 
 class TestL10nBrSaleSN(L10nBrSaleBaseTest):
-
     @classmethod
     def setUpClass(self):
         super().setUpClass()
-        self.company = self.env.ref('l10n_br_base.empresa_simples_nacional')
-        self.so_products = self.env.ref('l10n_br_sale.sn_so_only_products')
-        self.so_services = self.env.ref('l10n_br_sale.sn_so_only_services')
-        self.so_prod_srv = self.env.ref('l10n_br_sale.sn_so_product_service')
+        self.company = self.env.ref("l10n_br_base.empresa_simples_nacional")
+        self.so_products = self.env.ref("l10n_br_sale.sn_so_only_products")
+        self.so_services = self.env.ref("l10n_br_sale.sn_so_only_services")
+        self.so_prod_srv = self.env.ref("l10n_br_sale.sn_so_product_service")
