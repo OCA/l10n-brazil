5204b159fdbd6449f9d1781ab463e1210a9a1691
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:47:18 -0300

    [IMP] l10n_br_purchase: black, isort, prettier

--- l10n_br_purchase/tests/test_l10n_br_purchase_lp.py
+++ l10n_br_purchase/tests/test_l10n_br_purchase_lp.py
@@ -6,8 +6,7 @@ from .test_l10n_br_purchase import L10nBrPurchaseBaseTest
 
 
 class TestL10nBrPurchaseLC(L10nBrPurchaseBaseTest):
-
     def setUp(self):
         super().setUp()
-        self.company = self.env.ref('l10n_br_base.empresa_lucro_presumido')
-        self.po_products = self.env.ref('l10n_br_purchase.lp_po_only_products')
+        self.company = self.env.ref("l10n_br_base.empresa_lucro_presumido")
+        self.po_products = self.env.ref("l10n_br_purchase.lp_po_only_products")
