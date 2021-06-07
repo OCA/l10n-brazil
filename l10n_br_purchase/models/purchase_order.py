222bf993f68147e6e1a4bf51b8b1c251ce37a97a
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:47:18 -0300

    [IMP] l10n_br_purchase: black, isort, prettier

--- l10n_br_purchase/models/purchase_order.py
+++ l10n_br_purchase/models/purchase_order.py
@@ -5,12 +5,13 @@
 from lxml import etree
 
 from odoo import api, fields, models
+
 # from odoo.addons.l10n_br_fiscal.constants.fiscal import DOCUMENT_ISSUER_PARTNER
 
 
 class PurchaseOrder(models.Model):
-    _name = 'purchase.order'
-    _inherit = [_name, 'l10n_br_fiscal.document.mixin']
+    _name = "purchase.order"
+    _inherit = [_name, "l10n_br_fiscal.document.mixin"]
 
     @api.model
     def _default_fiscal_operation(self):
@@ -19,60 +20,63 @@ class PurchaseOrder(models.Model):
     @api.model
     def _fiscal_operation_domain(self):
         domain = [
-            ('state', '=', 'approved'),
-            ('fiscal_type', 'in', ('purchase', 'other', 'purchase_refund'))]
+            ("state", "=", "approved"),
+            ("fiscal_type", "in", ("purchase", "other", "purchase_refund")),
+        ]
         return domain
 
     fiscal_operation_id = fields.Many2one(
-        comodel_name='l10n_br_fiscal.operation',
+        comodel_name="l10n_br_fiscal.operation",
         readonly=True,
-        states={'draft': [('readonly', False)]},
+        states={"draft": [("readonly", False)]},
         default=_default_fiscal_operation,
         domain=lambda self: self._fiscal_operation_domain(),
     )
 
     cnpj_cpf = fields.Char(
-        string='CNPJ/CPF',
-        related='partner_id.cnpj_cpf',
+        string="CNPJ/CPF",
+        related="partner_id.cnpj_cpf",
     )
 
     legal_name = fields.Char(
-        string='Legal Name',
-        related='partner_id.legal_name',
+        string="Legal Name",
+        related="partner_id.legal_name",
     )
 
     ie = fields.Char(
-        string='State Tax Number/RG',
-        related='partner_id.inscr_est',
+        string="State Tax Number/RG",
+        related="partner_id.inscr_est",
     )
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type="form",
-                        toolbar=False, submenu=False):
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
 
-        order_view = super().fields_view_get(
-            view_id, view_type, toolbar, submenu
-        )
+        order_view = super().fields_view_get(view_id, view_type, toolbar, submenu)
 
-        if view_type == 'form':
-            sub_form_view = order_view.get(
-                'fields', {}).get('order_line', {}).get(
-                'views', {}).get('form', {}).get('arch', {})
+        if view_type == "form":
+            sub_form_view = (
+                order_view.get("fields", {})
+                .get("order_line", {})
+                .get("views", {})
+                .get("form", {})
+                .get("arch", {})
+            )
 
-            view = self.env['ir.ui.view']
+            view = self.env["ir.ui.view"]
 
             sub_form_node = etree.fromstring(
-                self.env['purchase.order.line'].fiscal_form_view(
-                    sub_form_view))
+                self.env["purchase.order.line"].fiscal_form_view(sub_form_view)
+            )
 
             sub_arch, sub_fields = view.postprocess_and_fields(
-                'purchase.order.line', sub_form_node, None)
+                "purchase.order.line", sub_form_node, None
+            )
 
-            order_view['fields']['order_line']['views']['form'][
-                'fields'] = sub_fields
+            order_view["fields"]["order_line"]["views"]["form"]["fields"] = sub_fields
 
-            order_view['fields']['order_line']['views']['form'][
-                'arch'] = sub_arch
+            order_view["fields"]["order_line"]["views"]["form"]["arch"] = sub_arch
 
         return order_view
 
@@ -108,18 +112,18 @@ class PurchaseOrder(models.Model):
     #     result['context'].update(fiscal_dict)
     #     return result
 
-    @api.onchange('fiscal_operation_id')
+    @api.onchange("fiscal_operation_id")
     def _onchange_fiscal_operation_id(self):
         self.fiscal_position_id = self.fiscal_operation_id.fiscal_position_id
 
     def _get_amount_lines(self):
         """Get object lines instaces used to compute fields"""
-        return self.mapped('order_line')
+        return self.mapped("order_line")
 
-    @api.depends('order_line')
+    @api.depends("order_line")
     def _compute_amount(self):
         super()._compute_amount()
 
-    @api.depends('order_line.price_total')
+    @api.depends("order_line.price_total")
     def _amount_all(self):
         self._compute_amount()
