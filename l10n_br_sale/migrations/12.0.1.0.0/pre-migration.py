e183f90fc4a9e47b25fd44cbbed84cbc8c64092b
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:41:59 -0300

    [IMP] l10n_br_sale: black, isort, prettier

--- l10n_br_sale/migrations/12.0.1.0.0/pre-migration.py
+++ l10n_br_sale/migrations/12.0.1.0.0/pre-migration.py
@@ -4,9 +4,10 @@
 from openupgradelib import openupgrade
 
 _columns_rename = {
-    'sale_order_line': [
-        ('customer_order', 'partner_order'),
-        ('customer_order_line', 'partner_order_line')],
+    "sale_order_line": [
+        ("customer_order", "partner_order"),
+        ("customer_order_line", "partner_order_line"),
+    ],
 }
 
 
@@ -15,5 +16,4 @@ def migrate(env, version):
     for table in _columns_rename.keys():
         for rename_column in _columns_rename[table]:
             if openupgrade.column_exists(env.cr, table, rename_column[0]):
-                openupgrade.rename_columns(
-                    env.cr, {table: [rename_column]})
+                openupgrade.rename_columns(env.cr, {table: [rename_column]})
