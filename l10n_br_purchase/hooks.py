ae31311ef6235144307874551917b61c78e2c6d9
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:47:18 -0300

    [IMP] l10n_br_purchase: black, isort, prettier

--- l10n_br_purchase/hooks.py
+++ l10n_br_purchase/hooks.py
@@ -1,35 +1,41 @@
 # Copyright (C) 2020  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, tools, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api, tools
 
 
 def post_init_hook(cr, registry):
 
-    if not tools.config['without_demo']:
+    if not tools.config["without_demo"]:
         env = api.Environment(cr, SUPERUSER_ID, {})
-        purchase_orders = env['purchase.order'].search(
-            [('company_id', '!=', env.ref('base.main_company').id)])
+        purchase_orders = env["purchase.order"].search(
+            [("company_id", "!=", env.ref("base.main_company").id)]
+        )
 
         for order in purchase_orders:
-            defaults = order.with_context(
-                company_id=order.company_id.id).default_get(order._fields)
-            defaults.update({
-                'name': order.name,
-                'company_id': order.company_id.id,
-                'fiscal_operation_id': order.fiscal_operation_id.id,
-            })
+            defaults = order.with_context(company_id=order.company_id.id).default_get(
+                order._fields
+            )
+            defaults.update(
+                {
+                    "name": order.name,
+                    "company_id": order.company_id.id,
+                    "fiscal_operation_id": order.fiscal_operation_id.id,
+                }
+            )
             order.write(defaults)
 
         # Load COA Fiscal Operation properties
         company = env.ref(
-            'l10n_br_base.empresa_simples_nacional',
-            raise_if_not_found=False)
+            "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
+        )
         # COA Simple Fiscal Operation properties
-        if company and env['ir.module.module'].search_count([
-            ('name', '=', 'l10n_br_coa_simple'),
-            ('state', '=', 'installed'),
-        ]):
+        if company and env["ir.module.module"].search_count(
+            [
+                ("name", "=", "l10n_br_coa_simple"),
+                ("state", "=", "installed"),
+            ]
+        ):
             tools.convert_file(
                 cr,
                 "l10n_br_purchase",
@@ -42,14 +48,16 @@ def post_init_hook(cr, registry):
             )
 
         company_lc = env.ref(
-            'l10n_br_base.empresa_lucro_presumido',
-            raise_if_not_found=False)
+            "l10n_br_base.empresa_lucro_presumido", raise_if_not_found=False
+        )
 
         # COA Generic Fiscal Operation properties
-        if company_lc and env['ir.module.module'].search_count([
-            ('name', '=', 'l10n_br_coa_generic'),
-            ('state', '=', 'installed'),
-        ]):
+        if company_lc and env["ir.module.module"].search_count(
+            [
+                ("name", "=", "l10n_br_coa_generic"),
+                ("state", "=", "installed"),
+            ]
+        ):
             tools.convert_file(
                 cr,
                 "l10n_br_purchase",
--- l10n_br_purchase/tests/test_l10n_br_purchase_sn.py
+++ l10n_br_purchase/tests/test_l10n_br_purchase_sn.py
@@ -6,8 +6,7 @@ from .test_l10n_br_purchase import L10nBrPurchaseBaseTest
 
 
 class TestL10nBrPurchaseSN(L10nBrPurchaseBaseTest):
-
     def setUp(self):
         super().setUp()
-        self.company = self.env.ref('l10n_br_base.empresa_simples_nacional')
-        self.po_products = self.env.ref('l10n_br_purchase.sn_po_only_products')
+        self.company = self.env.ref("l10n_br_base.empresa_simples_nacional")
+        self.po_products = self.env.ref("l10n_br_purchase.sn_po_only_products")
