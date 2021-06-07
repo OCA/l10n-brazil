e7f26202ee6145f0fa187c4e6a3d9ee71700eb21
Raphaël Valyi <rvalyi@akretion.com>
2021-06-07 00:41:59 -0300

    [IMP] l10n_br_sale: black, isort, prettier

--- l10n_br_sale/migrations/12.0.4.0.0/pre-migration.py
+++ l10n_br_sale/migrations/12.0.4.0.0/pre-migration.py
@@ -4,13 +4,13 @@
 from openupgradelib import openupgrade
 
 _columns_rename = {
-    'sale_order': [
-        ('amount_discount', 'amount_discount_value'),
-        ('amount_freight', 'amount_freight_value'),
-        ('amount_insurance', 'amount_insurance_value'),
-        ('amount_costs_value', 'amount_other_value')],
-    'sale_order_line': [
-        ('other_costs_value', 'other_value')],
+    "sale_order": [
+        ("amount_discount", "amount_discount_value"),
+        ("amount_freight", "amount_freight_value"),
+        ("amount_insurance", "amount_insurance_value"),
+        ("amount_costs_value", "amount_other_value"),
+    ],
+    "sale_order_line": [("other_costs_value", "other_value")],
 }
 
 
@@ -19,5 +19,4 @@ def migrate(env, version):
     for table in _columns_rename.keys():
         for rename_column in _columns_rename[table]:
             if openupgrade.column_exists(env.cr, table, rename_column[0]):
-                openupgrade.rename_columns(
-                    env.cr, {table: [rename_column]})
+                openupgrade.rename_columns(env.cr, {table: [rename_column]})
