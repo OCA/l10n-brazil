a96837027e7be1a4e9f92a8f50990f43ea65a9ce
Ygor Carvalho <ygcarvalh@gmail.com>
2021-06-09 12:13:11 -0300

    [IMP] 'Simples Nacional' xml tags

--- l10n_br_nfe/models/document_line.py
+++ l10n_br_nfe/models/document_line.py
@@ -369,6 +369,11 @@ class NFeLine(spec_models.StackedModel):
             'CSOSN': self.icms_cst_id.code,
             'pCredSN': str('%.04f' % self.icmssn_percent),
             'vCredICMSSN': str("%.02f" % self.icmssn_credit_value),
+
+            # FUNDO DE COMBATE À POBREZA
+            'vBCFCPST': str('%.02f' % self.icmsfcp_base),
+            'pFCPST': str('%.04f' % self.icmsfcp_percent),
+            'vFCPST': str('%.02f' % self.icmsfcpst_value),
         }
         return icms
 
