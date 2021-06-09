d88e43cd153b7fb3328d21c3b1d9e9ed654cdb51
Raphaël Valyi <rvalyi@akretion.com>
2021-06-09 00:23:20 -0300

    [IMP] l10n_br_crm: black, isort, prettier

--- l10n_br_crm/migrations/12.0.1.0.0/post-migration.py
+++ l10n_br_crm/migrations/12.0.1.0.0/post-migration.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 KMEE INFORMATICA LTDA - Gabriel Cardoso de Faria
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -9,10 +8,12 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     cr = env.cr
     cr.execute(
-        '''UPDATE crm_lead cl SET city_id=(
+        """UPDATE crm_lead cl SET city_id=(
         SELECT id FROM res_city WHERE ibge_code=(
         SELECT ibge_code FROM l10n_br_base_city WHERE id=cl.l10n_br_city_id))
-        ''')
+        """
+    )
     cr.execute(
-        '''UPDATE crm_lead cl SET street_number=number;
-        ''')
+        """UPDATE crm_lead cl SET street_number=number;
+        """
+    )
