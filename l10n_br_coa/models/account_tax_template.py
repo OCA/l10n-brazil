5e4af08a2098ca7d3e11890200479d437d495175
Raphaël Valyi <rvalyi@akretion.com>
2021-06-08 21:30:25 -0300

    [IMP] l10n_br_coa: black, isort, prettier

--- l10n_br_coa/models/account_tax_template.py
+++ l10n_br_coa/models/account_tax_template.py
@@ -5,10 +5,10 @@ from odoo import models
 
 
 class AccountTaxTemplate(models.Model):
-    _name = 'account.tax.template'
-    _inherit = ['account.tax.mixin', 'account.tax.template']
+    _name = "account.tax.template"
+    _inherit = ["account.tax.mixin", "account.tax.template"]
 
     def _get_tax_vals(self, company, tax_template_to_tax):
         values = super()._get_tax_vals(company, tax_template_to_tax)
-        values['deductible'] = self.deductible
+        values["deductible"] = self.deductible
         return values
