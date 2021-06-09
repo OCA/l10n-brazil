42b7d984661671dff731311828943bca1926a4ab
Ygor Carvalho <ygcarvalh@gmail.com>
2021-06-09 15:11:25 -0300

    [ADD] 'Simples Nacional' test file functions

---
+++ l10n_br_nfe/tests/test_nfe_serialize_sn.py
@@ -0,0 +1,12 @@
+# @ 2020 KMEE INFORMATICA LTDA - www.kmee.com.br -
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from .test_nfe_serialize import TestNFeExport
+
+
+class TestNFeExportSN(TestNFeExport):
+    def setUp(self):
+        super().setUp()
+
+        self.nfe_list = [
+
+        ]
