ef0ec697191312fc60d03e5a7f5b28b465004fe5
Raphaël Valyi <rvalyi@akretion.com>
2021-06-12 12:20:05 -0300

    [IMP] l10n_br_account: black, isort, prettier

--- l10n_br_account/migrations/12.0.7.0.0/pre-migration.py
+++ l10n_br_account/migrations/12.0.7.0.0/pre-migration.py
@@ -4,16 +4,14 @@
 
 from openupgradelib import openupgrade
 
-
 _column_renames = {
     # l10n_br_account/models/account_tax.py
-    'l10n_br_fiscal_account_tax_rel': [
-        ('l10n_br_fiscal_tax_id', 'fiscal_tax_id'),
+    "l10n_br_fiscal_account_tax_rel": [
+        ("l10n_br_fiscal_tax_id", "fiscal_tax_id"),
     ],
-
     # l10n_br_account/models/account_tax_template.py
-    'l10n_br_fiscal_account_template_tax_rel': [
-        ('l10n_br_fiscal_tax_id', 'fiscal_tax_id'),
+    "l10n_br_fiscal_account_template_tax_rel": [
+        ("l10n_br_fiscal_tax_id", "fiscal_tax_id"),
     ],
 }
 
@@ -21,9 +19,15 @@ _column_renames = {
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     openupgrade.rename_columns(env.cr, _column_renames)
-    openupgrade.rename_tables(env.cr, [
-        ('l10n_br_fiscal_account_tax_rel', 'fiscal_account_tax_rel')
-    ])
-    openupgrade.rename_tables(env.cr, [
-        ('l10n_br_fiscal_account_template_tax_rel', 'fiscal_account_template_tax_rel')
-    ])
+    openupgrade.rename_tables(
+        env.cr, [("l10n_br_fiscal_account_tax_rel", "fiscal_account_tax_rel")]
+    )
+    openupgrade.rename_tables(
+        env.cr,
+        [
+            (
+                "l10n_br_fiscal_account_template_tax_rel",
+                "fiscal_account_template_tax_rel",
+            )
+        ],
+    )
