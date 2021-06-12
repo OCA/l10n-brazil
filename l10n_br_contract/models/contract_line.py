980ea9c09f7a09a67e62f2e4b28f98ea6be00c98
Raphaël Valyi <rvalyi@akretion.com>
2021-06-12 13:01:26 -0300

    [IMP] l10n_br_contract: black, isort, prettier

--- l10n_br_contract/models/contract_line.py
+++ l10n_br_contract/models/contract_line.py
@@ -1,36 +1,37 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
+
 from ...l10n_br_fiscal.constants.fiscal import TAX_FRAMEWORK
 
 
 class ContractLine(models.Model):
-    _name = 'contract.line'
-    _inherit = [_name, 'l10n_br_fiscal.document.line.mixin']
+    _name = "contract.line"
+    _inherit = [_name, "l10n_br_fiscal.document.line.mixin"]
 
     company_id = fields.Many2one(
-        related='contract_id.company_id',
+        related="contract_id.company_id",
     )
 
     fiscal_tax_ids = fields.Many2many(
-        comodel_name='l10n_br_fiscal.tax',
-        relation='fiscal_contract_line_tax_rel',
-        column1='document_id',
-        column2='fiscal_tax_id',
-        string='Fiscal Taxes',
+        comodel_name="l10n_br_fiscal.tax",
+        relation="fiscal_contract_line_tax_rel",
+        column1="document_id",
+        column2="fiscal_tax_id",
+        string="Fiscal Taxes",
     )
 
     tax_framework = fields.Selection(
         selection=TAX_FRAMEWORK,
-        related='contract_id.company_id.tax_framework',
-        string='Tax Framework',
+        related="contract_id.company_id.tax_framework",
+        string="Tax Framework",
     )
 
     partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        related='contract_id.partner_id',
-        string='Partner',
+        comodel_name="res.partner",
+        related="contract_id.partner_id",
+        string="Partner",
     )
 
     @api.multi
