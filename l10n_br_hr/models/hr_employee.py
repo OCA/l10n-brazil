60d74ab3f2629556816c8aeddc7c1ef7dc0ba9c1
Raphaël Valyi <rvalyi@akretion.com>
2021-06-12 16:18:14 -0300

    [IMP] l10n_br_hr: black, isort, prettier

--- l10n_br_hr/models/hr_employee.py
+++ l10n_br_hr/models/hr_employee.py
@@ -4,6 +4,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from erpbrasil.base.fiscal import cnpj_cpf, pis
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
