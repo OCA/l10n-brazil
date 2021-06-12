2fea53015c4963c31426a2f569c7beaa49e63b64
Raphaël Valyi <rvalyi@akretion.com>
2021-06-12 13:01:26 -0300

    [IMP] l10n_br_contract: black, isort, prettier

--- l10n_br_contract/__manifest__.py
+++ l10n_br_contract/__manifest__.py
@@ -2,25 +2,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Brazilian Localization Contract',
-    'summary': """
+    "name": "Brazilian Localization Contract",
+    "summary": """
         Customization of Contract module for implementations in Brazil.""",
-    'version': '12.0.4.1.0',
-    'license': 'AGPL-3',
-    'author': 'KMEE,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-brazil',
-    'depends': [
-        'l10n_br_account', 'contract'
+    "version": "12.0.4.1.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": ["l10n_br_account", "contract"],
+    "data": [
+        "data/company.xml",
+        "views/res_company.xml",
+        "views/contract_view.xml",
+        "views/contract_line.xml",
     ],
-    'data': [
-        'data/company.xml',
-
-        'views/res_company.xml',
-        'views/contract_view.xml',
-        'views/contract_line.xml',
-    ],
-    'demo': [
-        'demo/company.xml',
-        'demo/contract_demo.xml',
+    "demo": [
+        "demo/company.xml",
+        "demo/contract_demo.xml",
     ],
 }
