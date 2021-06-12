3333fa5a0b09be6d60497b068c4385396da1a96d
Raphaël Valyi <rvalyi@akretion.com>
2021-06-12 18:08:06 -0300

    [IMP] l10n_br_portal: black, isort, prettier

--- l10n_br_portal/__manifest__.py
+++ l10n_br_portal/__manifest__.py
@@ -2,23 +2,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Br Portal',
-    'summary': """
+    "name": "L10n Br Portal",
+    "summary": """
         Campos Brasileiros no Portal""",
-    'version': '12.0.1.2.0',
-    'license': 'AGPL-3',
-    'author': 'KMEE,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-brazil',
-    'depends': [
-        'portal',
-        'l10n_br_zip',
+    "version": "12.0.1.2.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": [
+        "portal",
+        "l10n_br_zip",
     ],
-    'demo': [
-        'demo/res_users_demo.xml',
+    "demo": [
+        "demo/res_users_demo.xml",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/portal_templates.xml',
+    "data": [
+        "views/assets.xml",
+        "views/portal_templates.xml",
     ],
-    'auto_install': True,
+    "auto_install": True,
 }
