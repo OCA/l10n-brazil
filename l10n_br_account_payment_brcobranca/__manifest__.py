e2865d9925b5a205902a4e56afa14650b3d85c93
Raphaël Valyi <rvalyi@akretion.com>
2021-06-13 21:14:28 -0300

    [IMP] l10n_br_account_payment_brcobranca: black, isort, prettier

--- l10n_br_account_payment_brcobranca/__manifest__.py
+++ l10n_br_account_payment_brcobranca/__manifest__.py
@@ -3,27 +3,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Br Account Payment Brcobranca',
-    'description': """
+    "name": "L10n Br Account Payment Brcobranca",
+    "description": """
         Gera Boletos, CNAB de Remessa e Retorno usando
          a Gem brcobranca do Boletosimples""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Akretion',
-    'website': 'www.akretion.com',
-    'depends': [
-        'l10n_br_account_payment_order',
-        'account_move_base_import',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": [
+        "l10n_br_account_payment_order",
+        "account_move_base_import",
     ],
-    'data': [
-        'views/account_invoice_view.xml',
-        'views/res_config_settings_view.xml',
-        'views/account_journal_view.xml',
-        'data/res_config_settings_data.xml',
-        'security/ir.model.access.csv',
-        'wizard/import_statement_view.xml'
+    "data": [
+        "views/account_invoice_view.xml",
+        "views/res_config_settings_view.xml",
+        "views/account_journal_view.xml",
+        "data/res_config_settings_data.xml",
+        "security/ir.model.access.csv",
+        "wizard/import_statement_view.xml",
     ],
-    'demo': [
-        'demo/account_journal_demo.xml'
-    ],
+    "demo": ["demo/account_journal_demo.xml"],
 }
