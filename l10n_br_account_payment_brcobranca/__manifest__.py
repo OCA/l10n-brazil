dfaeb0973aa12c90ab15594468b3df22bf31609f
Magno Costa <magno.costa@akretion.com.br>
2021-06-14 12:09:21 -0300

    [FIX] Pre commit.

--- l10n_br_account_payment_brcobranca/__manifest__.py
+++ l10n_br_account_payment_brcobranca/__manifest__.py
@@ -3,13 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "L10n Br Account Payment Brcobranca",
-    "description": """
-        Gera Boletos, CNAB de Remessa e Retorno usando
-         a Gem brcobranca do Boletosimples""",
+    "name": "L10n Br Account Payment BRCobranca",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Akretion",
+    "author": "Akretion, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "depends": [
         "l10n_br_account_payment_order",
