90b2b0b868a62892d447f4d877343c7ff8a0a425
Raphaël Valyi <rvalyi@akretion.com>
2021-06-20 01:04:24 -0300

    [REF] api.model 1st, ds_class -> binding_class

--- spec_driven_model/models/spec_export.py
+++ spec_driven_model/models/spec_export.py
@@ -3,7 +3,7 @@
 from io import StringIO
 import logging
 import sys
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 _logger = logging.getLogger(__name__)
@@ -12,9 +12,45 @@ _logger = logging.getLogger(__name__)
 class AbstractSpecMixin(models.AbstractModel):
     _inherit = 'spec.mixin'
 
-    def _get_ds_class(self, class_obj):
+    @api.model
+    def _get_binding_class(self, class_obj):
         binding_module = sys.modules[self._binding_module]
         return getattr(binding_module, class_obj._generateds_type)
+
+    @api.model
+    def _get_model_classes(self):
+        classes = [getattr(x, '_name', None) for x in type(self).mro()]
+        return classes
+
+    @api.model
+    def _get_spec_classes(self, classes=False):
+        if not classes:
+            classes = self._get_model_classes()
+        spec_classes = []
+        for c in set(classes):
+            if c is None:
+                continue
+            if not c.startswith("%s." % (self._schema_name,)):
+                continue
+            # the following filter to fields to show
+            # when several XSD class are injected in the same object
+            if self._context.get('spec_class') and c != self._context[
+                    'spec_class']:
+                continue
+            spec_classes.append(c)
+        return spec_classes
+
+    @api.model
+    def _print_xml(self, binding_instance):
+        if not binding_instance:
+            return
+        output = StringIO()
+        binding_instance.export(
+            output,
+            0,
+            pretty_print=True,
+        )
+        output.close()
 
     def _export_fields(self, xsd_fields, class_obj, export_dict):
         """
@@ -24,8 +60,8 @@ class AbstractSpecMixin(models.AbstractModel):
         sub binding instances already properly instanciated.
         """
         self.ensure_one()
-        ds_class = self._get_ds_class(class_obj)
-        ds_class_spec = {i.name: i for i in ds_class.member_data_items_}
+        binding_class = self._get_binding_class(class_obj)
+        binding_class_spec = {i.name: i for i in binding_class.member_data_items_}
 
         for xsd_field in xsd_fields:
             if not xsd_field:
@@ -34,10 +70,10 @@ class AbstractSpecMixin(models.AbstractModel):
                     and xsd_field not in self._stacking_points.keys():
                 continue
             field_spec_name = xsd_field.replace(class_obj._field_prefix, '')
-            if not ds_class_spec.get(field_spec_name):
+            if not binding_class_spec.get(field_spec_name):
                 # this can happen with a o2m generated foreign key for instance
                 continue
-            member_spec = ds_class_spec[field_spec_name]
+            member_spec = binding_class_spec[field_spec_name]
             field_data = self._export_field(xsd_field, class_obj, member_spec)
             if xsd_field in self._stacking_points.keys():
                 if not field_data:
@@ -83,6 +119,7 @@ class AbstractSpecMixin(models.AbstractModel):
             return self[xsd_field]
 
     def _export_many2one(self, field_name, xsd_required, class_obj=None):
+        self.ensure_one()
         if field_name in self._stacking_points.keys():
             return self._build_generateds(
                 class_name=self._stacking_points[field_name].comodel_name
@@ -92,6 +129,7 @@ class AbstractSpecMixin(models.AbstractModel):
                 class_obj._fields[field_name].comodel_name)
 
     def _export_one2many(self, field_name, class_obj=None):
+        self.ensure_one()
         relational_data = []
         for relational_field in self[field_name]:
             field_data = relational_field._build_generateds(
@@ -102,6 +140,7 @@ class AbstractSpecMixin(models.AbstractModel):
 
     def _export_float_monetary(self, field_name, member_spec, class_obj,
                                xsd_required):
+        self.ensure_one()
         if member_spec.data_type[0]:
             TDec = ''.join(filter(lambda x: x.isdigit(),
                                   member_spec.data_type[0]))[-2:]
@@ -111,34 +150,16 @@ class AbstractSpecMixin(models.AbstractModel):
             raise NotImplementedError
 
     def _export_date(self, field_name):
+        self.ensure_one()
         return str(self[field_name])
 
     def _export_datetime(self, field_name):
+        self.ensure_one()
         return str(fields.Datetime.context_timestamp(
             self,
             fields.Datetime.from_string(self[field_name])
         ).isoformat('T'))
 
-    def _get_model_classes(self):
-        classes = [getattr(x, '_name', None) for x in type(self).mro()]
-        return classes
-
-    def _get_spec_classes(self, classes=False):
-        if not classes:
-            classes = self._get_model_classes()
-        spec_classes = []
-        for c in set(classes):
-            if c is None:
-                continue
-            if not c.startswith("%s." % (self._schema_name,)):
-                continue
-            # the following filter to fields to show
-            # when several XSD class are injected in the same object
-            if self._context.get('spec_class') and c != self._context[
-                    'spec_class']:
-                continue
-            spec_classes.append(c)
-        return spec_classes
 
     def _build_generateds(self, class_name=False):
         """
@@ -166,42 +187,29 @@ class AbstractSpecMixin(models.AbstractModel):
         )
 
         kwargs = {}
-
-        ds_class = self._get_ds_class(class_obj)
+        binding_class = self._get_binding_class(class_obj)
         self._export_fields(xsd_fields, class_obj, export_dict=kwargs)
-
         if kwargs:
-            ds_object = ds_class(**kwargs)
-            return ds_object
-
-    def _print_xml(self, ds_object):
-        if not ds_object:
-            return
-        output = StringIO()
-        ds_object.export(
-            output,
-            0,
-            pretty_print=True,
-        )
-        output.close()
+            binding_instance = binding_class(**kwargs)
+            return binding_instance
 
     def export_xml(self, print_xml=True):
         self.ensure_one()
         result = []
 
         if hasattr(self, '_stacked'):
-            ds_object = self._build_generateds()
+            binding_instance = self._build_generateds()
             if print_xml:
-                self._print_xml(ds_object)
-            result.append(ds_object)
+                self._print_xml(binding_instance)
+            result.append(binding_instance)
 
         else:
             spec_classes = self._get_spec_classes()
             for class_name in spec_classes:
-                ds_object = self._build_generateds(class_name)
+                binding_instance = self._build_generateds(class_name)
                 if print:
-                    self._print_xml(ds_object)
-                result.append(ds_object)
+                    self._print_xml(binding_instance)
+                result.append(binding_instance)
         return result
 
     def export_ds(self):
