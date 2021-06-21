0dc1b36d5eb5ba06c9fe85787326256b7115c845
Magno Costa <magno.costa@akretion.com.br>
2021-06-21 14:38:47 -0300

    [IMP] Tests, make automatic reconciliation.

--- l10n_br_account_payment_brcobranca/tests/test_return_import.py
+++ l10n_br_account_payment_brcobranca/tests/test_return_import.py
@@ -1,5 +1,8 @@
 # Copyright 2021 Akretion - Raphaël Valyi <raphael.valyi@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# Copyright (C) 2021-Today - Akretion (<http://www.akretion.com>).
+# @author Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import base64
 import os
@@ -10,6 +13,9 @@ from odoo.modules import get_resource_path
 from odoo.tests import SavepointCase, tagged
 
 _module_ns = "odoo.addons.l10n_br_account_payment_brcobranca"
+_provider_class_pay_order = (
+    _module_ns + ".models.account_payment_order" + ".PaymentOrder"
+)
 _provider_class = _module_ns + ".parser.cnab_file_parser" + ".CNABFileParser"
 
 
@@ -34,22 +40,63 @@ class TestReturnImport(SavepointCase):
         cls.cnab_log_obj = cls.env["l10n_br_cnab.return.log"]
         cls.account_id = cls.env.ref("account.a_recv")
         cls.bank_account = cls.env.ref("account.bnk")
-        cls.journal = cls.env.ref("account.bank_journal")
         cls.import_wizard_obj = cls.env["credit.statement.import"]
-        cls.partner = cls.env.ref("base.res_partner_12")
-        cls.journal.write(
-            {
-                "used_for_import": True,
-                "import_type": "cnab400",
-                "partner_id": cls.partner.id,
-                "commission_account_id": cls.account_id.id,
-                "receivable_account_id": cls.account_id.id,
-                "create_counterpart": True,
-                "bank_account_id": cls.env.ref(
-                    "l10n_br_account_payment_order.main_company_bank_unicredi"
-                ).id,
-            }
+
+        # Get Invoice for test
+        cls.invoice_unicred_1 = cls.env.ref(
+            "l10n_br_account_payment_order."
+            "demo_invoice_payment_order_unicred_cnab400"
         )
+        cls.invoice_unicred_2 = cls.env.ref(
+            "l10n_br_account_payment_brcobranca."
+            "demo_invoice_brcobranca_unicred_cnab400"
+        )
+
+        cls.journal = cls.env.ref(
+            "l10n_br_account_payment_order.unicred_journal"
+        )
+
+        # I validate invoice by creating on
+        cls.invoice_unicred_1.action_invoice_open()
+        cls.invoice_unicred_2.action_invoice_open()
+
+        # Para evitar erros nos testes de variação da Sequencia do
+        # Nosso Numero/own_number quando se roda mais de uma vez ou
+        # devido a diferença entre os comandos feitos pelo Travis
+        cls.invoice_unicred_1_own_numbers = []
+        for line in cls.invoice_unicred_1.financial_move_line_ids:
+            # No arquivo de retorno vem o NOSSO NUMERO + Digito Verificador
+            cls.invoice_unicred_1_own_numbers.append(line.own_number + "0")
+
+        cls.invoice_unicred_2_own_numbers = []
+        for line in cls.invoice_unicred_2.financial_move_line_ids:
+            # No arquivo de retorno vem o NOSSO NUMERO + Digito Verificador
+            cls.invoice_unicred_2_own_numbers.append(line.own_number + "0")
+
+        payment_order = cls.env["account.payment.order"].search(
+            [("payment_mode_id", "=", cls.invoice_unicred_1.payment_mode_id.id)]
+        )
+
+        # Open payment order
+        payment_order.draft2open()
+
+        # Generate
+        file_name = get_resource_path(
+            "l10n_br_account_payment_brcobranca",
+            "tests",
+            "data",
+            "teste_remessa-unicred_400-1.REM",
+        )
+        with open(file_name, "rb") as f:
+            mocked_response = f.read()
+            with mock.patch(
+                _provider_class_pay_order + "._get_data_from_brcobranca",
+                return_value=mocked_response,
+            ):
+                payment_order.open2generated()
+
+        # Confirm Upload
+        payment_order.generated2uploaded()
 
     def _import_file(self, file_name):
         """import a file using the wizard
@@ -84,7 +131,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000010",
+                # "nosso_numero": "00000000000000010",
+                "nosso_numero": self.invoice_unicred_1_own_numbers[0],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -127,7 +175,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000029",
+                # "nosso_numero": "00000000000000029",
+                "nosso_numero": self.invoice_unicred_1_own_numbers[1],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -216,6 +265,7 @@ class TestReturnImport(SavepointCase):
                 "data",
                 "CNAB400UNICRED_valor_menor_1.RET",
             )
+            # Se não for um codigo cnab de liquidação retorna apenas o LOG criado.
             log = self._import_file(file_name)
 
         self.assertEqual("Banco UNICRED - Conta 371", log.name)
@@ -231,7 +281,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000010",
+                # "nosso_numero": "00000000000000090",
+                "nosso_numero": self.invoice_unicred_1_own_numbers[0],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -274,7 +325,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000029",
+                # "nosso_numero": "00000000000000109",
+                "nosso_numero": self.invoice_unicred_1_own_numbers[1],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -364,8 +416,11 @@ class TestReturnImport(SavepointCase):
                 "data",
                 "CNAB400UNICRED_valor_menor_2.RET",
             )
-            log = self._import_file(file_name)
-        self.assertEqual("Banco UNICRED - Conta 371", log.name)
+            # Se for um codigo cnab de liquidação retorna as account.move criadas.
+            moves = self._import_file(file_name)
+        self.assertEqual("Retorno CNAB - Banco UNICRED - Conta 371", moves.name)
+        # I check that the invoice state is "Paid"
+        self.assertEqual(self.invoice_unicred_1.state, "paid")
 
     def test_valor_maior_3(self):
 
@@ -378,7 +433,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000030",
+                # "nosso_numero": "00000000000000030",
+                "nosso_numero": self.invoice_unicred_2_own_numbers[0],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -421,7 +477,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000049",
+                # "nosso_numero": "00000000000000049",
+                "nosso_numero": self.invoice_unicred_2_own_numbers[1],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -511,6 +568,7 @@ class TestReturnImport(SavepointCase):
                 "data",
                 "CNAB400UNICRED_valor_maior_3.RET",
             )
+            # Se não for um codigo cnab de liquidação retorna apenas o LOG criado.
             log = self._import_file(file_name)
 
         self.assertEqual("Banco UNICRED - Conta 371", log.name)
@@ -526,7 +584,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000030",
+                # "nosso_numero": "00000000000000110",
+                "nosso_numero": self.invoice_unicred_2_own_numbers[0],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -569,7 +628,8 @@ class TestReturnImport(SavepointCase):
                 "agencia_sem_dv": "1234",
                 "cedente_com_dv": "000003719",
                 "convenio": None,
-                "nosso_numero": "00000000000000049",
+                # "nosso_numero": "00000000000000129",
+                "nosso_numero": self.invoice_unicred_2_own_numbers[1],
                 "tipo_cobranca": None,
                 "tipo_cobranca_anterior": None,
                 "natureza_recebimento": None,
@@ -660,6 +720,9 @@ class TestReturnImport(SavepointCase):
                 "data",
                 "CNAB400UNICRED_valor_maior_4.RET",
             )
-            log = self._import_file(file_name)
+            # Se for um codigo cnab de liquidação retorna as account.move criadas
+            moves = self._import_file(file_name)
 
-        self.assertEqual("Banco UNICRED - Conta 371", log.name)
+        self.assertEqual("Retorno CNAB - Banco UNICRED - Conta 371", moves.name)
+        # I check that the invoice state is "Paid"
+        self.assertEqual(self.invoice_unicred_2.state, "paid")
