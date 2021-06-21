1f4852c09d8b86cba608d5619d546b625d58ec79
Renato Lima <renato.lima@akretion.com>
2021-06-21 15:42:10 -0300

    [REF] Add argument to get deductible taxes

--- l10n_br_account/models/account_tax_group.py
+++ l10n_br_account/models/account_tax_group.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2009  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountTaxGroup(models.Model):
@@ -11,3 +11,11 @@ class AccountTaxGroup(models.Model):
         comodel_name="l10n_br_fiscal.tax.group",
         string="Fiscal Tax Group",
     )
+
+    @api.multi
+    def deductible_tax(self, type_tax_use='sale'):
+        return self.env['account.tax'].search([
+            ('type_tax_use', '=', type_tax_use),
+            ('tax_group_id', '=', self.id),
+            ('deductible', '=', True),
+            ('company_id', '=', self.env.user.company_id.id)], limit=1)
