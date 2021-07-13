389d1bbc2b5aba326256a4dbd0ad75539276d206
Magno Costa <magno.costa@akretion.com.br>
2021-07-13 12:34:28 -0300

    [FIX] Pre commit.

--- l10n_br_account_payment_order/__manifest__.py
+++ l10n_br_account_payment_order/__manifest__.py
@@ -3,47 +3,41 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Brazilian Payment Order',
-    'version': '12.0.3.0.1',
-    'license': 'AGPL-3',
-    'author': "KMEE, "
-              "Akretion, "
-              "Odoo Community Association (OCA)",
+    "name": "Brazilian Payment Order",
+    "version": "12.0.3.0.1",
+    "license": "AGPL-3",
+    "author": "KMEE, " "Akretion, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    'category': 'Banking addons',
-    'depends': [
-        'l10n_br_base',
-        'account_payment_order',
-        'account_due_list',
-        'account_cancel',
+    "category": "Banking addons",
+    "depends": [
+        "l10n_br_base",
+        "account_payment_order",
+        "account_due_list",
+        "account_cancel",
     ],
-    'data': [
+    "data": [
         # Security
-        'security/cnab_cobranca_security.xml',
-        'security/ir.model.access.csv',
-
+        "security/cnab_cobranca_security.xml",
+        "security/ir.model.access.csv",
         # Data
-        'data/cnab_data.xml',
-        'data/l10n_br_payment_export_type.xml',
-        'data/boleto_data.xml',
-        'data/account_analytic_tag_data.xml',
-        'data/l10n_br_cnab_return_move_code_data.xml',
-        'data/l10n_br_cnab_mov_instruction_code_data.xml',
-
+        "data/cnab_data.xml",
+        "data/l10n_br_payment_export_type.xml",
+        "data/boleto_data.xml",
+        "data/account_analytic_tag_data.xml",
+        "data/l10n_br_cnab_return_move_code_data.xml",
+        "data/l10n_br_cnab_mov_instruction_code_data.xml",
         # Reports
-        'reports/report_print_button_view.xml',
-
+        "reports/report_print_button_view.xml",
         # Wizards
-        'wizards/account_payment_line_create_view.xml',
-        'wizards/account_move_line_change.xml',
-
+        "wizards/account_payment_line_create_view.xml",
+        "wizards/account_move_line_change.xml",
         # Views
-        'views/account_journal.xml',
-        'views/account_payment_order.xml',
-        'views/account_payment_line.xml',
-        'views/account_payment_mode.xml',
-        'views/res_company.xml',
-        'views/bank_payment_line.xml',
+        "views/account_journal.xml",
+        "views/account_payment_order.xml",
+        "views/account_payment_line.xml",
+        "views/account_payment_mode.xml",
+        "views/res_company.xml",
+        "views/bank_payment_line.xml",
         # TODO - Separação dos dados de importação para um objeto especifico
         #  cnab.return.log armazenando o LOG do Arquivo de Retorno CNAB
         #  de forma separada e permitindo a integração com a alteração feita no
@@ -57,25 +51,25 @@
         # 'views/l10n_br_payment_cnab.xml',
         # 'views/l10n_br_cobranca_cnab.xml',
         # 'views/l10n_br_cobranca_cnab_lines.xml',
-        'views/l10n_br_cnab_return_log_view.xml',
-        'views/account_invoice.xml',
-        'views/account_move_line.xml',
-        'views/l10n_br_cnab_return_move_code_view.xml',
-        'views/account_payment_views.xml',
-        'views/l10n_br_cnab_mov_instruction_code_view.xml',
-        'views/account_move_view.xml'
+        "views/l10n_br_cnab_return_log_view.xml",
+        "views/account_invoice.xml",
+        "views/account_move_line.xml",
+        "views/l10n_br_cnab_return_move_code_view.xml",
+        "views/account_payment_views.xml",
+        "views/l10n_br_cnab_mov_instruction_code_view.xml",
+        "views/account_move_view.xml",
     ],
-    'demo': [
-        'demo/account_payment_method.xml',
-        'demo/mov_instruction_code.xml',
-        'demo/res_partner_bank.xml',
-        'demo/account_account.xml',
-        'demo/account_journal.xml',
-        'demo/ir_sequence.xml',
-        'demo/account_payment_mode.xml',
-        'demo/account_invoice.xml',
-        'demo/res_users.xml',
-        'demo/account_payment_order.xml',
+    "demo": [
+        "demo/account_payment_method.xml",
+        "demo/mov_instruction_code.xml",
+        "demo/res_partner_bank.xml",
+        "demo/account_account.xml",
+        "demo/account_journal.xml",
+        "demo/ir_sequence.xml",
+        "demo/account_payment_mode.xml",
+        "demo/account_invoice.xml",
+        "demo/res_users.xml",
+        "demo/account_payment_order.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- l10n_br_account_payment_order/models/account_payment_line.py
+++ l10n_br_account_payment_order/models/account_payment_line.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+
 from odoo.addons import decimal_precision as dp
 
 from ..constants import (
@@ -14,116 +15,117 @@ from ..constants import (
 
 
 class AccountPaymentLine(models.Model):
-    _inherit = 'account.payment.line'
+    _inherit = "account.payment.line"
 
     digitable_line = fields.Char(
-        string='Linha Digitável',
+        string="Linha Digitável",
     )
 
     percent_interest = fields.Float(
-        string='Percentual de Juros',
-        digits=dp.get_precision('Account'),
+        string="Percentual de Juros",
+        digits=dp.get_precision("Account"),
     )
 
     amount_interest = fields.Float(
-        string='Valor Juros',
-        compute='_compute_interest',
-        digits=dp.get_precision('Account'),
+        string="Valor Juros",
+        compute="_compute_interest",
+        digits=dp.get_precision("Account"),
     )
 
     own_number = fields.Char(
-        string='Nosso Numero',
+        string="Nosso Numero",
     )
 
     document_number = fields.Char(
-        string='Número documento',
+        string="Número documento",
     )
 
     company_title_identification = fields.Char(
-        string='Identificação Titulo Empresa',
+        string="Identificação Titulo Empresa",
     )
 
     doc_finality_code = fields.Selection(
         selection=COMPLEMENTO_TIPO_SERVICO,
-        string='Complemento do Tipo de Serviço',
-        help='Campo P005 do CNAB',
+        string="Complemento do Tipo de Serviço",
+        help="Campo P005 do CNAB",
     )
 
     ted_finality_code = fields.Selection(
         selection=CODIGO_FINALIDADE_TED,
-        string='Código Finalidade da TED',
-        help='Campo P011 do CNAB',
+        string="Código Finalidade da TED",
+        help="Campo P011 do CNAB",
     )
 
     complementary_finality_code = fields.Char(
-        string='Código de finalidade complementar',
+        string="Código de finalidade complementar",
         size=2,
-        help='Campo P013 do CNAB',
+        help="Campo P013 do CNAB",
     )
 
     favored_warning = fields.Selection(
         selection=AVISO_FAVORECIDO,
-        string='Aviso ao Favorecido',
-        help='Campo P006 do CNAB',
-        default='0',
+        string="Aviso ao Favorecido",
+        help="Campo P006 do CNAB",
+        default="0",
     )
 
     rebate_value = fields.Monetary(
-        string='Valor do Abatimento',
-        help='Campo G045 do CNAB',
+        string="Valor do Abatimento",
+        help="Campo G045 do CNAB",
     )
 
     discount_value = fields.Monetary(
-        string='Valor do Desconto',
-        help='Campo G046 do CNAB',
+        string="Valor do Desconto",
+        help="Campo G046 do CNAB",
     )
 
     interest_value = fields.Monetary(
-        string='Valor da Mora',
-        help='Campo G047 do CNAB',
+        string="Valor da Mora",
+        help="Campo G047 do CNAB",
     )
 
     fee_value = fields.Monetary(
-        string='Valor da Multa',
-        help='Campo G048 do CNAB',
+        string="Valor da Multa",
+        help="Campo G048 do CNAB",
     )
 
     payment_mode_id = fields.Many2one(
-        comodel_name='account.payment.mode',
-        track_visibility='onchange',
+        comodel_name="account.payment.mode",
+        track_visibility="onchange",
     )
 
     # Campo não usado no BRCobranca
     movement_type = fields.Selection(
         selection=TIPO_MOVIMENTO,
-        string='Tipo de Movimento',
-        help='Campo G060 do CNAB',
-        default='0',
+        string="Tipo de Movimento",
+        help="Campo G060 do CNAB",
+        default="0",
     )
 
     mov_instruction_code_id = fields.Many2one(
-        comodel_name='l10n_br_cnab.mov.instruction.code',
-        string='Código da Instrução para Movimento',
-        help='Campo G061 do CNAB',
+        comodel_name="l10n_br_cnab.mov.instruction.code",
+        string="Código da Instrução para Movimento",
+        help="Campo G061 do CNAB",
     )
 
     # Usados para deixar invisiveis/somente leitura
     # os campos relacionados ao CNAB
     payment_method_id = fields.Many2one(
-        comodel_name='account.payment.method',
-        related='payment_mode_id.payment_method_id',
-        string='Payment Method',
+        comodel_name="account.payment.method",
+        related="payment_mode_id.payment_method_id",
+        string="Payment Method",
     )
 
     payment_method_code = fields.Char(
-        related='payment_method_id.code',
-        readonly=True, store=True,
-        string='Payment Method Code'
+        related="payment_method_id.code",
+        readonly=True,
+        store=True,
+        string="Payment Method Code",
     )
 
     communication_type = fields.Selection(
         selection_add=[
-            ('cnab', 'CNAB'),
+            ("cnab", "CNAB"),
         ]
     )
 
@@ -138,21 +140,14 @@ class AccountPaymentLine(models.Model):
     # "account_payment_line"
     # TODO: Verificar a possibilidade de alteração no
     #  modulo account_payment_order na v14
-    move_line_id = fields.Many2one(
-        ondelete=None
-    )
-    ml_maturity_date = fields.Date(
-        related=None
-    )
+    move_line_id = fields.Many2one(ondelete=None)
+    ml_maturity_date = fields.Date(related=None)
     # Para manter a rastreabilidade está sendo adicionado uma relação
     # com a account.invoice referente, já que a AML pode ser apagada
-    invoice_id = fields.Many2one(
-        comodel_name='account.invoice',
-        string='Fatura'
-    )
+    invoice_id = fields.Many2one(comodel_name="account.invoice", string="Fatura")
 
     @api.multi
-    @api.depends('percent_interest', 'amount_currency', 'currency_id')
+    @api.depends("percent_interest", "amount_currency", "currency_id")
     def _compute_interest(self):
         for record in self:
             record.amount_interest = record.currency_id.round(
@@ -163,19 +158,19 @@ class AccountPaymentLine(models.Model):
     def default_get(self, fields_list):
         res = super().default_get(fields_list)
         mode = (
-            self.env['account.payment.order']
-            .browse(self.env.context.get('order_id'))
+            self.env["account.payment.order"]
+            .browse(self.env.context.get("order_id"))
             .payment_mode_id
         )
         if mode.doc_finality_code:
-            res.update({'doc_finality_code': mode.doc_finality_code})
+            res.update({"doc_finality_code": mode.doc_finality_code})
         if mode.ted_finality_code:
-            res.update({'ted_finality_code': mode.ted_finality_code})
+            res.update({"ted_finality_code": mode.ted_finality_code})
         if mode.complementary_finality_code:
             res.update(
-                {'complementary_finality_code': mode.complementary_finality_code}
+                {"complementary_finality_code": mode.complementary_finality_code}
             )
         if mode.favored_warning:
-            res.update({'favored_warning': mode.favored_warning})
+            res.update({"favored_warning": mode.favored_warning})
 
         return res
