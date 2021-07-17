f3a2e57e055e16f32ab104f79d3759ba8845319e
Luis Felipe Mileo <mileo@kmee.com.br>
2021-07-17 11:53:37 -0300

    [REF] Edoc Purpouse on Operation

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -470,6 +470,7 @@ class Document(models.Model):
         super()._onchange_fiscal_operation_id()
         if self.fiscal_operation_id:
             self.fiscal_operation_type = self.fiscal_operation_id.fiscal_operation_type
+            self.edoc_purpose = self.fiscal_operation_id.edoc_purpose
 
         if self.issuer == DOCUMENT_ISSUER_COMPANY and not self.document_type_id:
             self.document_type_id = self.company_id.document_type_id
