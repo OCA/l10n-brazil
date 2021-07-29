f816a471dffe84705e9f82571b8c45e372548d8f
Magno Costa <magno.costa@akretion.com.br>
2021-07-29 17:27:05 -0300

    [IMP] Pre-commit for module l10n_br_stock_account_report.

--- l10n_br_stock_account_report/__manifest__.py
+++ l10n_br_stock_account_report/__manifest__.py
@@ -2,24 +2,23 @@
 # @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Brazilian Localization Stock Account Report',
-    'version': '12.0.1.0.0',
-    'author': 'Akretion,'
-              'Odoo Community Association (OCA)',
-    'category': 'Reports/QWeb',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_br_stock_account',
+    "name": "Brazilian Localization Stock Account Report",
+    "version": "12.0.1.0.0",
+    "author": "Akretion," "Odoo Community Association (OCA)",
+    "category": "Reports/QWeb",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_br_stock_account",
     ],
-    'website': 'https://github.com/OCA/l10n-brazil',
-    'development_status': 'Mature',
-    'maintainers': [
-        'mbcosta',
+    "website": "https://github.com/OCA/l10n-brazil",
+    "development_status": "Mature",
+    "maintainers": [
+        "mbcosta",
     ],
-    'data': [
-        'report/l10n_br_stock_account_p7_report.xml',
-        'report/l10n_br_stock_account_p7_report_view.xml',
-        'wizards/l10n_br_stock_account_p7_wizard_report_view.xml'
+    "data": [
+        "report/l10n_br_stock_account_p7_report.xml",
+        "report/l10n_br_stock_account_p7_report_view.xml",
+        "wizards/l10n_br_stock_account_p7_wizard_report_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
