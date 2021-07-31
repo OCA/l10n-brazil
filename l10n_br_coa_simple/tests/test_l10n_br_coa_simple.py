7974dd68aa193cce479d628ddd69178cac37331d
Raphaël Valyi <rvalyi@akretion.com>
2021-07-31 09:36:31 -0300

    [IMP] l10n_br_coa_simple: black, isort, prettier

--- l10n_br_coa_simple/tests/test_l10n_br_coa_simple.py
+++ l10n_br_coa_simple/tests/test_l10n_br_coa_simple.py
@@ -9,10 +9,11 @@ class L10nBrSimpleCOA(TransactionCase):
         super(L10nBrSimpleCOA, self).setUp()
 
         self.l10n_br_coa_simple = self.env.ref(
-            'l10n_br_coa_simple.l10n_br_coa_simple_chart_template')
-        self.l10n_br_company = self.env['res.company'].create({
-            'name': 'Empresa Teste do Plano de Contas Simplificado'
-        })
+            "l10n_br_coa_simple.l10n_br_coa_simple_chart_template"
+        )
+        self.l10n_br_company = self.env["res.company"].create(
+            {"name": "Empresa Teste do Plano de Contas Simplificado"}
+        )
 
     def test_l10n_br_coa_simple(self):
         """Test to install the chart of accounts template in a new company"""
@@ -21,4 +22,5 @@ class L10nBrSimpleCOA(TransactionCase):
         self.l10n_br_coa_simple.try_loading_for_current_company()
 
         self.assertEqual(
-            self.l10n_br_coa_simple, self.l10n_br_company.chart_template_id)
+            self.l10n_br_coa_simple, self.l10n_br_company.chart_template_id
+        )
