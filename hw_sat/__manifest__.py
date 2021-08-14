e37bac1f4170c7353deec5b1435dd8f3e7920a21
Luis Felipe Mileo <mileo@kmee.com.br>
2021-08-14 14:59:06 -0300

    [MIG] bump module version

---
+++ hw_sat/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'POS Fiscal Hardware Driver',
+    'version': '12.0.1.0.0',
+    'category': 'Hardware Drivers',
+    'author': 'KMEE, Odoo Community Association (OCA)',
+    'depends': ['hw_proxy'],
+    # 'external_dependencies': {
+    #     'python' : ['usb.core','serial','qrcode'],
+    # },
+    'test': [
+    ],
+    'installable': False,
+}
