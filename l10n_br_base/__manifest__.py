9b3e2dd087b5dd40a0178aa7b1da093a4878f29a
Raphaël Valyi <rvalyi@akretion.com>
2021-08-17 22:21:27 -0300

    [MIG] l10n_br_base: Migration to 14.0

--- l10n_br_base/__manifest__.py
+++ l10n_br_base/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["renatonlima", "rvalyi"],
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["base", "base_setup", "base_address_city", "base_address_extended"],
     "data": [
         "security/ir.model.access.csv",
