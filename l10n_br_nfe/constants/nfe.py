f4da34c3df13c55cead8986683ce39f2b7231d0a
Renato Lima <renato.lima@akretion.com>
2021-08-19 14:41:50 -0300

    [ADD] nfe_transmission, nfe tpEmis company default field

--- l10n_br_nfe/constants/nfe.py
+++ l10n_br_nfe/constants/nfe.py
@@ -11,3 +11,18 @@ NFE_ENVIRONMENTS = [("1", "Produção"), ("2", "Homologação")]
 
 
 NFE_ENVIRONMENT_DEFAULT = "2"
+
+
+NFE_TRANSMISSIONS = [
+    ("1", "1 - Emissão Normal"),
+    ("2", "2 - Contingência FS-IA (Não Implementado)"),
+    ("3", "3 - Contingência SCAN (Não Implementado)"),
+    ("4", "4 - Contingência EPEC (Não Implementado)"),
+    ("5", "5 - Contingência FS-DA (Não Implementado)"),
+    ("6", "6 - Contingência SVC-AN (Não Implementado)"),
+    ("7", "7 - Contingência SVC-RS (Não Implementado)"),
+    ("9", "9 - Contingência off-line da NFC-e (Não Implementado)"),
+]
+
+
+NFE_TRANSMISSION_DEFAULT = "1"
