9aff92ed9f2c591818d60dc18b699b5ba5843273
Magno Costa <magno.costa@akretion.com.br>
2021-08-20 10:06:07 -0300

    [REF] Included dependecy of module delivery_carrier_partner, less code here.

--- l10n_br_delivery/models/carrier.py
+++ l10n_br_delivery/models/carrier.py
@@ -7,12 +7,6 @@ from odoo import fields, models
 class Carrier(models.Model):
     _inherit = 'delivery.carrier'
 
-    partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Transportadora',
-        domain=[('supplier', '=', True)],
-    )
-
     antt_code = fields.Char(
         string='Codigo ANTT',
         size=32,
