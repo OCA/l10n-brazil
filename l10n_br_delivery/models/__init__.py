021ea510740416b500a9c2f1b0b1a1df0e804f00
Magno Costa <magno.costa@akretion.com.br>
2021-08-20 10:09:56 -0300

    [REF] Standard name of files.

--- l10n_br_delivery/models/__init__.py
+++ l10n_br_delivery/models/__init__.py
@@ -1,6 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import carrier
+from . import delivery_carrier
 from . import vehicle
 from . import shipment
 from . import sale_order
