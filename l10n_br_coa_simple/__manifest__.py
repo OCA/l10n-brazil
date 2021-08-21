0208ba9bee85a91b2918d1664621c273ee35b1ae
Raphaël Valyi <rvalyi@akretion.com>
2021-08-21 11:47:15 -0300

    [MIG] l10n_br_coa_simple: Migration to 13.0

--- l10n_br_coa_simple/__manifest__.py
+++ l10n_br_coa_simple/__manifest__.py
@@ -8,7 +8,7 @@
     "license": "AGPL-3",
     "author": "Akretion, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "12.0.2.2.0",
+    "version": "13.0.1.0.0",
     "depends": ["l10n_br_coa"],
     "data": [
         "data/l10n_br_coa_simple_template.xml",
