7903c72469339d61bba1ea0a3d5ca591010aeb06
Raphaël Valyi <rvalyi@akretion.com>
2021-08-23 15:30:43 -0300

    [REF] maintainers + dev status + desc

--- l10n_br_coa_simple/__manifest__.py
+++ l10n_br_coa_simple/__manifest__.py
@@ -2,11 +2,13 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Brazilian - Simple Accounting",
-    "summary": "Brazilian Simple Chart of Account",
-    "category": "Localization",
+    "name": "Plano de Contas do Simples ITG 1000",
+    "summary": "Plano de Contas ITG 1000 para Microempresas e Empresa de Pequeno Porte",
+    "category": "Accounting",
     "license": "AGPL-3",
     "author": "Akretion, " "Odoo Community Association (OCA)",
+    "maintainers": ["renatonlima"],
+    "development_status": "Production/Stable",
     "website": "https://github.com/OCA/l10n-brazil",
     "version": "13.0.1.0.0",
     "depends": ["l10n_br_coa"],
