a739db85f67e0ddaae653d3e618dd682e72c76dc
Raphaël Valyi <rvalyi@akretion.com>
2021-08-28 19:48:05 -0300

    [REF] +incorterm_id in fiscal doc fixes #1596

--- l10n_br_account/models/__init__.py
+++ l10n_br_account/models/__init__.py
@@ -5,6 +5,7 @@ from . import account_tax_group
 from . import fiscal_tax_group
 from . import account_tax_template
 from . import account_tax
+from . import fiscal_document_mixin
 from . import fiscal_tax
 from . import fiscal_operation
 from . import fiscal_operation_line
--- l10n_br_delivery/models/fiscal_document_mixin.py
+++ l10n_br_delivery/models/fiscal_document_mixin.py
@@ -11,15 +11,6 @@ class FiscalDocumentMixin(models.AbstractModel):
     def _get_default_incoterm(self):
         return self.env.user.company_id.incoterm_id
 
-    # Esta sendo implementado aqui para existir nos objetos herdados
-    incoterm_id = fields.Many2one(
-        comodel_name='account.incoterms',
-        string='Incoterm',
-        help='International Commercial Terms are a series of'
-               ' predefined commercial terms used in international'
-               ' transactions.'
-    )
-
     carrier_id = fields.Many2one(
         comodel_name='delivery.carrier',
         string='Carrier',
