b3e4af5a991766ac807d3bad5a55a09cc5b48650
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-08-29 16:36:43 -0300

    [FIX] l10n_br_stock_account: get refound fiscal operation when the picking is set to be invoice or not.

--- l10n_br_stock_account/wizards/stock_return_picking.py
+++ l10n_br_stock_account/wizards/stock_return_picking.py
@@ -16,42 +16,41 @@ class StockReturnPicking(models.TransientModel):
         """
         new_picking_id, pick_type_id = super()._create_returns()
 
-        if self.invoice_state == "2binvoiced":
+        picking_obj = self.env["stock.picking"]
+        picking = picking_obj.browse(new_picking_id)
 
-            picking_obj = self.env["stock.picking"]
-            move_obj = self.env["stock.move"]
+        origin_picking = self.env["stock.picking"].browse(self.env.context["active_id"])
 
-            origin_picking = self.env["stock.picking"].browse(
-                self.env.context["active_id"]
-            )
+        if origin_picking.fiscal_operation_id:
+
             refund_fiscal_operation = (
                 origin_picking.fiscal_operation_id.return_fiscal_operation_id
             )
 
             if not refund_fiscal_operation:
-                raise UserError(
-                    _(
-                        "This Fiscal Operation does not has Fiscal"
-                        " Operation for Returns!"
+                if self.invoice_state == "2binvoiced":
+                    raise UserError(
+                        _(
+                            "This Fiscal Operation does not has Fiscal"
+                            " Operation for Returns!"
+                        )
                     )
-                )
+            else:
+                picking.fiscal_operation_id = refund_fiscal_operation.id
+                move_obj = self.env["stock.move"]
+                for move in picking.move_lines:
+                    ret_move = move.origin_returned_move_id
+                    fiscal_op = ret_move.fiscal_operation_id.return_fiscal_operation_id
+                    fiscal_op_line = ret_move.fiscal_operation_line_id.line_refund_id
 
-            picking = picking_obj.browse(new_picking_id)
-            picking.fiscal_operation_id = refund_fiscal_operation.id
-
-            for move in picking.move_lines:
-                ret_move = move.origin_returned_move_id
-                fiscal_op = ret_move.fiscal_operation_id.return_fiscal_operation_id
-                fiscal_op_line = ret_move.fiscal_operation_line_id.line_refund_id
-
-                line_values = {
-                    "fiscal_operation_id": fiscal_op.id,
-                    "fiscal_operation_line_id": fiscal_op_line.id,
-                }
-                write_move = move_obj.browse(move.id)
-                write_move.write(line_values)
-                write_move._onchange_product_id_fiscal()
-                write_move._onchange_fiscal_operation_id()
-                write_move._onchange_fiscal_operation_line_id()
+                    line_values = {
+                        "fiscal_operation_id": fiscal_op.id,
+                        "fiscal_operation_line_id": fiscal_op_line.id,
+                    }
+                    write_move = move_obj.browse(move.id)
+                    write_move.write(line_values)
+                    write_move._onchange_product_id_fiscal()
+                    write_move._onchange_fiscal_operation_id()
+                    write_move._onchange_fiscal_operation_line_id()
 
         return new_picking_id, pick_type_id
