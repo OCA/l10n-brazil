847a8fd99b0e9272658171752c0e3d46fa0e2f37
Raphaël Valyi <rvalyi@akretion.com>
2021-08-29 23:00:36 -0300

    [REF] fiscal tax cleanup

--- l10n_br_fiscal/models/document_fiscal_line_mixin_methods.py
+++ l10n_br_fiscal/models/document_fiscal_line_mixin_methods.py
@@ -316,7 +316,7 @@ class FiscalDocumentLineMixinMethods(models.AbstractModel):
             taxes = self.env["l10n_br_fiscal.tax"]
             for tax in mapping_result["taxes"].values():
                 taxes |= tax
-            self.fiscal_tax_ids = [(6, 0, taxes.ids)]
+            self.fiscal_tax_ids = taxes
             self._update_taxes()
             self.comment_ids = self.fiscal_operation_line_id.comment_ids
 
