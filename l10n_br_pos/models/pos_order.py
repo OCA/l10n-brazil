3119cf4ffe93c1d30020181b74e559edfe1e96da
Luis Felipe Mileo <mileo@kmee.com.br>
2021-09-15 10:34:16 -0300

    [NEW] Pos.order fiscal mixin

--- l10n_br_pos/models/pos_order.py
+++ l10n_br_pos/models/pos_order.py
@@ -12,81 +12,163 @@ from odoo.addons.l10n_br_pos.models.pos_config import \
     SIMPLIFIED_INVOICE_TYPE
 from odoo.tools.translate import _
 
+from odoo.addons.l10n_br_fiscal.constants.fiscal import (
+    NFCE_IND_PRES_DEFAULT,
+)
+
 
 class PosOrder(models.Model):
-    _inherit = 'pos.order'
+    _name = 'pos.order'
+    _inherit = [_name, "l10n_br_fiscal.document.mixin"]
+
+    cnpj_cpf = fields.Char(
+        string="CNPJ/CPF",
+        related=False,
+        readonly=True,
+    )
+
+    legal_name = fields.Char(
+        string="Legal Name",
+        related="partner_id.legal_name",
+    )
+
+    ie = fields.Char(
+        string="State Tax Number/RG",
+        related="partner_id.inscr_est",
+    )
+
+    # Fiscal document fields
+
+    document_number = fields.Char(
+        string="Document Number",
+        copy=False,
+        index=True,
+        readonly=True,
+    )
+
+    document_key = fields.Char(
+        string="Key",
+        copy=False,
+        index=True,
+        readonly=True,
+    )
+
+    ind_pres = fields.Selection(
+        default=NFCE_IND_PRES_DEFAULT,
+    )
+
+    document_type_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document.type",
+        readonly=True,
+    )
+
+    document_type = fields.Char(
+        related="document_type_id.code",
+        store=True,
+    )
+
+    operation_name = fields.Char(
+        string="Operation Name",
+        copy=False,
+    )
+
+    document_electronic = fields.Boolean(
+        related="document_type_id.electronic",
+        string="Electronic?",
+        store=True,
+    )
+
+    date_in_out = fields.Datetime(
+        string="Date Move",
+        copy=False,
+    )
+
+    document_serie_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document.serie",
+        domain="[('active', '=', True)," "('document_type_id', '=', document_type_id)]",
+        readonly=True,
+    )
+
+    document_serie = fields.Char(
+        string="Serie Number",
+        readonly=True,
+    )
+
+    partner_shipping_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Shipping Address",
+    )
+
+    edoc_purpose = fields.Selection(
+        selection=[
+            ("1", "Normal"),
+            ("2", "Complementar"),
+            ("3", "Ajuste"),
+            ("4", "Devolução de mercadoria"),
+        ],
+        string="Finalidade",
+        default="1",
+        readonly=True,
+    )
+
+    @api.multi
+    def _get_amount_lines(self):
+        """Get object lines instaces used to compute fields"""
+        return self.mapped("lines")
+
+    @api.depends("lines")
+    def _compute_amount(self):
+        super()._compute_amount()
+
+    @api.depends("lines.price_subtotal_incl")
+    def _amount_all(self):
+        """Compute the total amounts of the SO."""
+        for order in self:
+            order._compute_amount()
 
     @api.model
     def _order_fields(self, ui_order):
         result = super()._order_fields(ui_order)
         temp = {
-            "cfe_return": ui_order.get("cfe_return"),
-            "num_sessao_sat": ui_order.get("num_sessao_sat"),
-            "chave_cfe": ui_order.get("chave_cfe"),
+            # "cfe_return": ui_order.get("cfe_return"),
+            # "num_sessao_sat": ui_order.get("num_sessao_sat"),
+            # "chave_cfe": ui_order.get("chave_cfe"),
             "cnpj_cpf": ui_order.get("cnpj_cpf"),
+            "fiscal_operation_id": ui_order.get("fiscal_operation_id"),
+            "document_type_id": ui_order.get("document_type_id"),
         }
         result.update(temp)
         return result
 
-    @api.model
-    def _pos_order_type(self):
-        return SIMPLIFIED_INVOICE_TYPE + [('nfe', 'NF-E')]
-
-    simplified = fields.Boolean(string='Simplified invoice', default=True)
-    fiscal_document_type = fields.Selection(
-        string='Fiscal Document Type',
-        selection='_pos_order_type',
-        states={'draft': [('readonly', False)]},
-        readonly=True
-    )
-
-    cfe_return = fields.Binary('Retorno Cfe')
-
-    chave_cfe = fields.Char('Chave da Cfe')
-
-    num_sessao_sat = fields.Char('Número Sessão SAT envio Cfe')
-
-    pos_order_associated = fields.Many2one('pos.order', 'Venda Associada')
-
-    canceled_order = fields.Boolean('Venda Cancelada', readonly=True)
-
-    cfe_cancelamento_return = fields.Binary('Retorno Cfe Cancelamento')
-
-    chave_cfe_cancelamento = fields.Char('Chave da Cfe Cancelamento')
-
-    num_sessao_sat_cancelamento = fields.Char(
-        'Número Sessão SAT Cancelamento'
-    )
-
-    cnpj_cpf = fields.Char(
-        string='CNPJ/CPF',
-    )
-
-    @api.one
-    def action_invoice(self):
-        self.simplified = False
-        self.fiscal_document_type = 'nfe'
-
-    @api.multi
-    def simplified_limit_check(self):
-        for order in self:
-            if not order.simplified:
-                continue
-            limit = order.session_id.config_id.simplified_invoice_limit
-            amount_total = order.amount_total
-            precision_digits = dp.get_precision('Account')(self.env.cr)[1]
-            # -1 or 0: amount_total <= limit, simplified
-            #       1: amount_total > limit, can not be simplified
-            simplified = (
-                float_compare(amount_total, limit,
-                              precision_digits=precision_digits) <= 0)
-            # Change simplified flag if incompatible
-            if not simplified:
-                order.write(
-                    {'simplified': simplified,
-                     'fiscal_document_type':
-                         order.session_id.config_id.simplified_invoice_type
-                     })
+    # @api.model
+    # def _pos_order_type(self):
+    #     return SIMPLIFIED_INVOICE_TYPE + [('nfe', 'NF-E')]
+    #
+    # simplified = fields.Boolean(string='Simplified invoice', default=True)
+    # fiscal_document_type = fields.Selection(
+    #     string='Fiscal Document Type',
+    #     selection='_pos_order_type',
+    #     states={'draft': [('readonly', False)]},
+    #     readonly=True
+    # )
+    #
+    # cfe_return = fields.Binary('Retorno Cfe')
+    #
+    # chave_cfe = fields.Char('Chave da Cfe')
+    #
+    # num_sessao_sat = fields.Char('Número Sessão SAT envio Cfe')
+    #
+    # pos_order_associated = fields.Many2one('pos.order', 'Venda Associada')
+    #
+    # canceled_order = fields.Boolean('Venda Cancelada', readonly=True)
+    #
+    # cfe_cancelamento_return = fields.Binary('Retorno Cfe Cancelamento')
+    #
+    # chave_cfe_cancelamento = fields.Char('Chave da Cfe Cancelamento')
+    #
+    # num_sessao_sat_cancelamento = fields.Char(
+    #     'Número Sessão SAT Cancelamento'
+    # )
 
     # @api.multi
     # def write(self, vals):
@@ -108,17 +190,6 @@ class PosOrder(models.Model):
     #     order.simplified_limit_check()
     #     return order
 
-    @api.model
-    def _process_order(self, order):
-        order_id = super()._process_order(order)
-        # order_id = self.browse(order_id)
-        # for statement in order_id.statement_ids:
-        #     if statement.journal_id.sat_payment_mode == '05' and statement.journal_id.pagamento_funcionarios:
-        #         order_id.partner_id.credit_funcionario -= statement.amount
-        #     elif statement.journal_id.sat_payment_mode == "05":
-        #         order_id.partner_id.credit_limit -= statement.amount
-        return order_id
-
     # @api.multi
     # def create_picking(self):
     #     super().create_picking()
@@ -136,118 +207,155 @@ class PosOrder(models.Model):
     #         {'value': {}}, **kwargs
     #     )['value']['fiscal_position']
     #     return True
-
-    @api.model
-    def return_orders_from_session(self, **kwargs):
-        orders_session = {'Orders': []}
-        orders = self.search(
-            [
-                ('session_id', '=', kwargs['session_id']),
-                ('state', '=', 'paid'),
-                ('chave_cfe', '!=', '')
-            ], limit=5, order="id DESC"
-        )
-        for order in orders:
-            order_vals = {
-                'id': order.id,
-                'name': order.name,
-                'pos_reference': order.pos_reference,
-                'partner': order.partner_id.name,
-                'date': order.date_order,
-                'total': '%.2f' % order.amount_total,
-                'chave_cfe': order.chave_cfe,
-                'canceled_order': order.canceled_order,
-                'can_cancel': False,
-            }
-            orders_session['Orders'].append(order_vals)
-            orders_session['Orders'][0]['can_cancel'] = True
-        return orders_session
-
-    @api.model
-    def refund(self, ids, dados):
-        """Create a copy of order  for refund order"""
-        clone_list = []
-
-        for order in self.browse(ids):
-
-            for statement in order.statement_ids:
-                if statement.journal_id.sat_payment_mode == "05":
-                    order.partner_id.credit_limit += statement.amount
-
-            current_session_ids = self.env['pos.session'].search([
-                ('state', '!=', 'closed'),
-                ('user_id', '=', self.env.uid)]
-            )
-
-            # if not current_session_ids:
-            #     raise osv.except_osv(_('Error!'), _('To return product(s),
-            # you need to open a session that will be used to register
-            # the refund.'))
-
-            clone_id = order.copy()
-
-            clone_id.write({
-                'name': order.name + ' CANCEL',
-                'pos_reference': order.pos_reference + ' CANCEL',
-                'session_id': current_session_ids.id,
-                'date_order': time.strftime('%Y-%m-%d %H:%M:%S'),
-                'pos_order_associated': order.id,
-                'canceled_order': True,
-                'chave_cfe': '',
-                'cfe_return': '',
-                'num_sessao_sat': ''
-            })
-
-            clone_list.append(clone_id.id)
-
-        for clone in self.browse(clone_list):
-            for order_line in clone.lines:
-                order_line.write({
-                    'qty': -order_line.qty
-                })
-
-            statements = self.env['account.bank.statement.line']
-
-            for statement in order.statement_ids:
-                vals = {
-                    'name': statement.display_name + " CANCEL",
-                    'statement_id': statement.statement_id.id,
-                    'ref': statement.ref,
-                    'pos_statement_id': clone.id,
-                    'journal_id': statement.journal_id.id,
-                    'amount': statement.amount * -1,
-                    'date': statement.date,
-                    'partner_id': statement.partner_id.id
-                    if statement.partner_id else False,
-                    'account_id': statement.account_id.id
-                }
-
-                statements.create(vals)
-
-            clone.action_paid()
-            parent_order = self.browse(clone.pos_order_associated.id)
-            parent_order.write({
-                'canceled_order': True,
-                'pos_order_associated': clone.id,
-                'cfe_cancelamento_return': dados['xml'],
-                'chave_cfe_cancelamento': dados['numSessao'],
-                'num_sessao_sat_cancelamento': dados['chave_cfe'],
-            })
-
-        return True
-
-    @api.model
-    def retornar_order_by_id(self, order_id):
-        order = self.browse(order_id)
-
-        dados_reimpressao = {
-            'order_id': order_id,
-            'chaveConsulta': order.chave_cfe,
-            'doc_destinatario': order.partner_id.cnpj_cpf if order.partner_id
-            else False,
-            'xml_cfe_cacelada': order.cfe_cancelamento_return,
-            'xml_cfe_venda': order.cfe_return,
-            'canceled_order': order.canceled_order,
-        }
-
-        return dados_reimpressao
+    #
+    # @api.model
+    # def _process_order(self, order):
+    #     order_id = super()._process_order(order)
+    #     # order_id = self.browse(order_id)
+    #     # for statement in order_id.statement_ids:
+    #     #     if statement.journal_id.sat_payment_mode == '05' and statement.journal_id.pagamento_funcionarios:
+    #     #         order_id.partner_id.credit_funcionario -= statement.amount
+    #     #     elif statement.journal_id.sat_payment_mode == "05":
+    #     #         order_id.partner_id.credit_limit -= statement.amount
+    #     return order_id
+    #
+    # @api.model
+    # def return_orders_from_session(self, **kwargs):
+    #     orders_session = {'Orders': []}
+    #     orders = self.search(
+    #         [
+    #             ('session_id', '=', kwargs['session_id']),
+    #             ('state', '=', 'paid'),
+    #             ('chave_cfe', '!=', '')
+    #         ], limit=5, order="id DESC"
+    #     )
+    #     for order in orders:
+    #         order_vals = {
+    #             'id': order.id,
+    #             'name': order.name,
+    #             'pos_reference': order.pos_reference,
+    #             'partner': order.partner_id.name,
+    #             'date': order.date_order,
+    #             'total': '%.2f' % order.amount_total,
+    #             'chave_cfe': order.chave_cfe,
+    #             'canceled_order': order.canceled_order,
+    #             'can_cancel': False,
+    #         }
+    #         orders_session['Orders'].append(order_vals)
+    #         orders_session['Orders'][0]['can_cancel'] = True
+    #     return orders_session
+    #
+    # @api.model
+    # def refund(self, ids, dados):
+    #     """Create a copy of order  for refund order"""
+    #     clone_list = []
+    #
+    #     for order in self.browse(ids):
+    #
+    #         for statement in order.statement_ids:
+    #             if statement.journal_id.sat_payment_mode == "05":
+    #                 order.partner_id.credit_limit += statement.amount
+    #
+    #         current_session_ids = self.env['pos.session'].search([
+    #             ('state', '!=', 'closed'),
+    #             ('user_id', '=', self.env.uid)]
+    #         )
+    #
+    #         # if not current_session_ids:
+    #         #     raise osv.except_osv(_('Error!'), _('To return product(s),
+    #         # you need to open a session that will be used to register
+    #         # the refund.'))
+    #
+    #         clone_id = order.copy()
+    #
+    #         clone_id.write({
+    #             'name': order.name + ' CANCEL',
+    #             'pos_reference': order.pos_reference + ' CANCEL',
+    #             'session_id': current_session_ids.id,
+    #             'date_order': time.strftime('%Y-%m-%d %H:%M:%S'),
+    #             'pos_order_associated': order.id,
+    #             'canceled_order': True,
+    #             'chave_cfe': '',
+    #             'cfe_return': '',
+    #             'num_sessao_sat': ''
+    #         })
+    #
+    #         clone_list.append(clone_id.id)
+    #
+    #     for clone in self.browse(clone_list):
+    #         for order_line in clone.lines:
+    #             order_line.write({
+    #                 'qty': -order_line.qty
+    #             })
+    #
+    #         statements = self.env['account.bank.statement.line']
+    #
+    #         for statement in order.statement_ids:
+    #             vals = {
+    #                 'name': statement.display_name + " CANCEL",
+    #                 'statement_id': statement.statement_id.id,
+    #                 'ref': statement.ref,
+    #                 'pos_statement_id': clone.id,
+    #                 'journal_id': statement.journal_id.id,
+    #                 'amount': statement.amount * -1,
+    #                 'date': statement.date,
+    #                 'partner_id': statement.partner_id.id
+    #                 if statement.partner_id else False,
+    #                 'account_id': statement.account_id.id
+    #             }
+    #
+    #             statements.create(vals)
+    #
+    #         clone.action_paid()
+    #         parent_order = self.browse(clone.pos_order_associated.id)
+    #         parent_order.write({
+    #             'canceled_order': True,
+    #             'pos_order_associated': clone.id,
+    #             'cfe_cancelamento_return': dados['xml'],
+    #             'chave_cfe_cancelamento': dados['numSessao'],
+    #             'num_sessao_sat_cancelamento': dados['chave_cfe'],
+    #         })
+    #
+    #     return True
+    #
+    # @api.model
+    # def retornar_order_by_id(self, order_id):
+    #     order = self.browse(order_id)
+    #
+    #     dados_reimpressao = {
+    #         'order_id': order_id,
+    #         'chaveConsulta': order.chave_cfe,
+    #         'doc_destinatario': order.partner_id.cnpj_cpf if order.partner_id
+    #         else False,
+    #         'xml_cfe_cacelada': order.cfe_cancelamento_return,
+    #         'xml_cfe_venda': order.cfe_return,
+    #         'canceled_order': order.canceled_order,
+    #     }
+    #
+    #     return dados_reimpressao
+    #
+    # @api.one
+    # def action_invoice(self):
+    #     self.simplified = False
+    #     self.fiscal_document_type = 'nfe'
+    #
+    # @api.multi
+    # def simplified_limit_check(self):
+    #     for order in self:
+    #         if not order.simplified:
+    #             continue
+    #         limit = order.session_id.config_id.simplified_invoice_limit
+    #         amount_total = order.amount_total
+    #         precision_digits = dp.get_precision('Account')(self.env.cr)[1]
+    #         # -1 or 0: amount_total <= limit, simplified
+    #         #       1: amount_total > limit, can not be simplified
+    #         simplified = (
+    #             float_compare(amount_total, limit,
+    #                           precision_digits=precision_digits) <= 0)
+    #         # Change simplified flag if incompatible
+    #         if not simplified:
+    #             order.write(
+    #                 {'simplified': simplified,
+    #                  'fiscal_document_type':
+    #                      order.session_id.config_id.simplified_invoice_type
+    #                  })
