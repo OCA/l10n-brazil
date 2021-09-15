f9884dea1d9ad3e571b5e99bddb815a500dd7758
Luis Felipe Mileo <mileo@kmee.com.br>
2021-09-15 10:34:16 -0300

    [NEW] Pos.order fiscal mixin

--- l10n_br_pos/models/pos_order_line.py
+++ l10n_br_pos/models/pos_order_line.py
@@ -5,28 +5,29 @@ from odoo import models, fields, api
 
 
 class PosOrderLine(models.Model):
-    _inherit = 'pos.order.line'
+    _name = 'pos.order.line'
+    _inherit = [_name, "l10n_br_fiscal.document.line.mixin"]
 
-    @api.multi
-    def _buscar_produtos_devolvidos(self):
-        for record in self:
-            if record.order_id.chave_cfe:
-                rel_documentos = self.env[
-                    'l10n_br_account_product.document.related'].search(
-                    [
-                        ('access_key', '=', record.order_id.chave_cfe[3:])
-                    ]
-                )
-                qtd_devolvidas = 0
-                for documento in rel_documentos:
-                    if documento.invoice_id.state in ('open', 'sefaz_export'):
-                        for line in documento.invoice_id.invoice_line:
-                            if record.product_id == line.product_id:
-                                qtd_devolvidas += line.quantity
-                record.qtd_produtos_devolvidos = qtd_devolvidas
-
-    qtd_produtos_devolvidos = fields.Integer(
-        string="Quantidade devolvida",
-        compute=_buscar_produtos_devolvidos
-    )
-
+    # @api.multi
+    # def _buscar_produtos_devolvidos(self):
+    #     for record in self:
+    #         if record.order_id.chave_cfe:
+    #             rel_documentos = self.env[
+    #                 'l10n_br_account_product.document.related'].search(
+    #                 [
+    #                     ('access_key', '=', record.order_id.chave_cfe[3:])
+    #                 ]
+    #             )
+    #             qtd_devolvidas = 0
+    #             for documento in rel_documentos:
+    #                 if documento.invoice_id.state in ('open', 'sefaz_export'):
+    #                     for line in documento.invoice_id.invoice_line:
+    #                         if record.product_id == line.product_id:
+    #                             qtd_devolvidas += line.quantity
+    #             record.qtd_produtos_devolvidos = qtd_devolvidas
+    #
+    # qtd_produtos_devolvidos = fields.Integer(
+    #     string="Quantidade devolvida",
+    #     compute=_buscar_produtos_devolvidos
+    # )
+    #
