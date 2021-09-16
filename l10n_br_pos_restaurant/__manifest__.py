0ae397b28074488e8da64528781c085a7f354299
Luis Felipe Mileo <mileo@kmee.com.br>
2021-09-16 16:24:47 -0300

    [NEW] l10n_br_pos_restaurant

---
+++ l10n_br_pos_restaurant/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Pos Restaurant',
+    'summary': """
+        Brasil Pos Restaurant""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/l10n-brazil',
+    'depends': [
+        'l10n_br_pos',
+        'pos_restaurant',
+    ],
+    'data': [
+    ],
+    'demo': [
+        'demo/product_template_demo.xml',
+    ],
+}
