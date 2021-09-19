3c5d4632486b125d1599effd49bdd421f39aacd3
Luis Felipe Mileo <mileo@kmee.com.br>
2021-09-19 18:22:42 -0300

    [IMP] Insert CPF/CNPJ in CFe

--- l10n_br_pos/models/product_template.py
+++ l10n_br_pos/models/product_template.py
@@ -50,11 +50,11 @@ class ProductTemplate(models.Model):
     def create(self, vals):
         product_id = super().create(vals)
         if vals.get('available_in_pos'):
-            product_id.update_pos_fiscal_map(vals)
+            product_id.update_pos_fiscal_map()
         return product_id
 
     @api.multi
     def write(self, vals):
         if vals.get('available_in_pos'):
-            self.update_pos_fiscal_map(vals)
+            self.update_pos_fiscal_map()
         return super().write(vals)
