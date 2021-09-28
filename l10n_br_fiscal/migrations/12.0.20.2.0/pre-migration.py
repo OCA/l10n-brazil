ac974278d2c5652be408aab7f7509beea5e8811d
Renato Lima <renato.lima@akretion.com>
2021-09-28 11:58:56 -0300

    [ADD] script migration for l10n_br_fiscal.document.related

--- l10n_br_fiscal/migrations/12.0.20.2.0/pre-migration.py
+++ l10n_br_fiscal/migrations/12.0.20.2.0/pre-migration.py
@@ -16,3 +16,13 @@ def migrate(env, version):
             document_key = regexp_replace(document_key, '[^0-9]+', '', 'g')
         """,
     )
+
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE
+            l10n_br_fiscal_document_related
+        SET
+            document_key = regexp_replace(document_key, '[^0-9]+', '', 'g')
+        """,
+    )
