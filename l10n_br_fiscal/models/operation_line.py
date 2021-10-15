6a6e4d7ad00faaa7483c19cb6468e78f80ecec0c
Raphaël Valyi <rvalyi@akretion.com>
2021-10-15 16:31:39 -0300

    [MIG] track_visibility=onchange -> tracking=True

--- l10n_br_fiscal/models/operation_line.py
+++ l10n_br_fiscal/models/operation_line.py
@@ -135,7 +135,7 @@ class OperationLine(models.Model):
         default=OPERATION_STATE_DEFAULT,
         index=True,
         readonly=True,
-        track_visibility="onchange",
+        tracking=True,
         copy=False,
     )
 
