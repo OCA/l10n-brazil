35a506225f40cc3631174c6aca35b21a472130d0
Raphaël Valyi <rvalyi@akretion.com>
2021-10-17 13:05:50 -0300

    [MIG] l10n_br_nfe_spec: Migration to 14.0

--- l10n_br_nfe_spec/__manifest__.py
+++ l10n_br_nfe_spec/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "nfe spec",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Accounting",
