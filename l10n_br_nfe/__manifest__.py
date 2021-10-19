43538fe24cd99a8da9585cb6a26e4492314e80df
Raphaël Valyi <rvalyi@akretion.com>
2021-10-19 02:15:31 -0300

    [MIG] l10n_br_nfe: Migration to 14.0

--- l10n_br_nfe/__manifest__.py
+++ l10n_br_nfe/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Akretion," "KMEE," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "development_status": "Beta",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": [
         "l10n_br_fiscal",
         "l10n_br_nfe_spec",
