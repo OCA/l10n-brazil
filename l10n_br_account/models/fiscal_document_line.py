3cfd67f084ea9268dbfc4c212cfbd4a865a54557
Raphaël Valyi <rvalyi@akretion.com>
2021-10-19 12:42:13 -0300

    [MIG] sed invoice_line_id -> line_id
    find l10n_br_account -type f -exec sed -i 's/invoice_line_id/line_id/g' {} \;

--- l10n_br_account/models/fiscal_document_line.py
+++ l10n_br_account/models/fiscal_document_line.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class FiscalDocumentLine(models.Model):
     _inherit = "l10n_br_fiscal.document.line"
 
-    invoice_line_ids = fields.One2many(
+    line_ids = fields.One2many(
         comodel_name="account.move.line",
         inverse_name="fiscal_document_line_id",
         string="Invoice Lines",
