e4b3651554109fbd3bbb0a139ced711b023aa4bc
Renato Lima <renato.lima@akretion.com>
2021-11-03 16:47:04 -0300

    [ADD] format.address.mixin object

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2015  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import format_address_mixin
 from . import res_bank
 from . import res_city
 from . import res_country_state
