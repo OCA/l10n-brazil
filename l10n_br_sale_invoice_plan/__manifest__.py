aee284405edc4523e48093d5e00612e4a48161b8
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-11-20 13:07:31 -0300

    [ADD] l10n_br_sale_invoice_plan module

---
+++ l10n_br_sale_invoice_plan/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Brazilian Localization Sale Invoice Plan",
+    "summary": """
+        Brazilian Localization Sale Invoice Plan""",
+    "category": "Localisation",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "maintainers": ["marcelsavegnago"],
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": [
+        "l10n_br_sale",
+        "sale_invoice_plan",
+    ],
+    "demo": ["demo/sale_order_demo.xml"],
+}
