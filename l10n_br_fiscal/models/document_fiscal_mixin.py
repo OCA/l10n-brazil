43893681c3af754b1ebdd052034e5adc960c9614
Renato Lima <renato.lima@akretion.com>
2021-11-26 17:47:38 -0300

    Set amount fields as store=True

--- l10n_br_fiscal/models/document_fiscal_mixin.py
+++ l10n_br_fiscal/models/document_fiscal_mixin.py
@@ -165,11 +165,13 @@ class FiscalDocumentMixin(models.AbstractModel):
     amount_icms_destination_value = fields.Monetary(
         string="ICMS Destination Value",
         compute="_compute_amount",
+        store=True,
     )
 
     amount_icms_origin_value = fields.Monetary(
         string="ICMS Origin Value",
         compute="_compute_amount",
+        store=True,
     )
 
     amount_ipi_base = fields.Monetary(
@@ -187,16 +189,19 @@ class FiscalDocumentMixin(models.AbstractModel):
     amount_ii_base = fields.Monetary(
         string="II Base",
         compute="_compute_amount",
+        store=True,
     )
 
     amount_ii_value = fields.Monetary(
         string="II Value",
         compute="_compute_amount",
+        store=True,
     )
 
     amount_ii_customhouse_charges = fields.Monetary(
         string="Customhouse Charges",
         compute="_compute_amount",
+        store=True,
     )
 
     amount_pis_base = fields.Monetary(
