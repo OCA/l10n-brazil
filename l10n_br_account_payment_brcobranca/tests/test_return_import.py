c8ba4d50b841575f26a09a0166e93bf5bd230869
Neto <netosjb@yahoo.com.br>
2021-12-10 12:25:42 -0300

    ailos bank implementation in brcobranca

--- l10n_br_account_payment_brcobranca/tests/test_return_import.py
+++ l10n_br_account_payment_brcobranca/tests/test_return_import.py
@@ -51,11 +51,16 @@ class TestReturnImport(SavepointCase):
             "demo_invoice_brcobranca_unicred_cnab400"
         )
 
+        cls.invoice_ailos_1 = cls.env.ref(
+            "l10n_br_account_payment_order." "demo_invoice_payment_order_ailos_cnab240"
+        )
+
         cls.journal = cls.env.ref("l10n_br_account_payment_order.unicred_journal")
 
         # I validate invoice by creating on
         cls.invoice_unicred_1.action_invoice_open()
         cls.invoice_unicred_2.action_invoice_open()
+        cls.invoice_ailos_1.action_invoice_open()
 
         # Para evitar erros nos testes de variação da Sequencia do
         # Nosso Numero/own_number quando se roda mais de uma vez ou
@@ -69,6 +74,10 @@ class TestReturnImport(SavepointCase):
         for line in cls.invoice_unicred_2.financial_move_line_ids:
             # No arquivo de retorno vem o NOSSO NUMERO + Digito Verificador
             cls.invoice_unicred_2_own_numbers.append(line.own_number + "0")
+
+        cls.invoice_ailos_1_own_numbers = []
+        for line in cls.invoice_ailos_1.financial_move_line_ids:
+            cls.invoice_ailos_1_own_numbers.append(line.own_number)
 
         payment_order = cls.env["account.payment.order"].search(
             [("payment_mode_id", "=", cls.invoice_unicred_1.payment_mode_id.id)]
@@ -729,3 +738,161 @@ class TestReturnImport(SavepointCase):
         self.assertEqual("Retorno CNAB - Banco UNICRED - Conta 371", moves.name)
         # I check that the invoice state is "Paid"
         self.assertEqual(self.invoice_unicred_2.state, "paid")
+
+    def test_ailos_return(self):
+
+        mocked_response = [
+            {
+                "codigo_registro": "03",
+                "codigo_ocorrencia": "06",
+                "data_ocorrencia": None,
+                "agencia_com_dv": None,
+                "agencia_sem_dv": "1234",
+                "cedente_com_dv": "000003719",
+                "convenio": None,
+                "nosso_numero": "00000000000" + self.invoice_ailos_1_own_numbers[0],
+                "tipo_cobranca": None,
+                "tipo_cobranca_anterior": None,
+                "natureza_recebimento": None,
+                "carteira_variacao": None,
+                "desconto": "0000000000000",
+                "iof": None,
+                "carteira": None,
+                "comando": None,
+                "data_liquidacao": None,
+                "data_vencimento": "06072021",
+                "valor_titulo": "0000000030000",
+                "banco_recebedor": "136",
+                "agencia_recebedora_com_dv": "12343",
+                "especie_documento": None,
+                "data_credito": "06072021",
+                "valor_tarifa": "0000180",
+                "outras_despesas": None,
+                "juros_desconto": None,
+                "iof_desconto": None,
+                "valor_abatimento": "0000000000000",
+                "desconto_concedito": None,
+                "valor_recebido": "0000000030000",
+                "juros_mora": "0000000000000",
+                "outros_recebimento": None,
+                "abatimento_nao_aproveitado": None,
+                "valor_lancamento": None,
+                "indicativo_lancamento": None,
+                "indicador_valor": None,
+                "valor_ajuste": None,
+                "sequencial": "00000",
+                "arquivo": None,
+                "motivo_ocorrencia": [],
+                "documento_numero": None,
+            },
+            {
+                "codigo_registro": "03",
+                "codigo_ocorrencia": "06",
+                "data_ocorrencia": None,
+                "agencia_com_dv": None,
+                "agencia_sem_dv": "1234",
+                "cedente_com_dv": "000003719",
+                "convenio": None,
+                "nosso_numero": "00000000000" + self.invoice_ailos_1_own_numbers[1],
+                "tipo_cobranca": None,
+                "tipo_cobranca_anterior": None,
+                "natureza_recebimento": None,
+                "carteira_variacao": None,
+                "desconto": "0000000000000",
+                "iof": None,
+                "carteira": None,
+                "comando": None,
+                "data_liquidacao": None,
+                "data_vencimento": "06072021",
+                "valor_titulo": "0000000070000",
+                "banco_recebedor": "136",
+                "agencia_recebedora_com_dv": "12343",
+                "especie_documento": None,
+                "data_credito": "06072021",
+                "valor_tarifa": "0000180",
+                "outras_despesas": None,
+                "juros_desconto": None,
+                "iof_desconto": None,
+                "valor_abatimento": "0000000000000",
+                "desconto_concedito": None,
+                "valor_recebido": "0000000070000",
+                "juros_mora": "0000000000000",
+                "outros_recebimento": None,
+                "abatimento_nao_aproveitado": None,
+                "valor_lancamento": None,
+                "indicativo_lancamento": None,
+                "indicador_valor": None,
+                "valor_ajuste": None,
+                "sequencial": "00000",
+                "arquivo": None,
+                "motivo_ocorrencia": [],
+                "documento_numero": None,
+            },
+            {
+                "codigo_registro": "9",
+                "codigo_ocorrencia": "00",
+                "data_ocorrencia": None,
+                "agencia_com_dv": None,
+                "agencia_sem_dv": "0000",
+                "cedente_com_dv": "000000000",
+                "convenio": None,
+                "nosso_numero": "00          00000",
+                "tipo_cobranca": None,
+                "tipo_cobranca_anterior": None,
+                "natureza_recebimento": None,
+                "carteira_variacao": None,
+                "desconto": "0000000",
+                "iof": None,
+                "carteira": None,
+                "comando": None,
+                "data_liquidacao": None,
+                "data_vencimento": "00000000",
+                "valor_titulo": "0000000000000",
+                "banco_recebedor": "00",
+                "agencia_recebedora_com_dv": "",
+                "especie_documento": None,
+                "data_credito": "00000000",
+                "valor_tarifa": "",
+                "outras_despesas": None,
+                "juros_desconto": None,
+                "iof_desconto": None,
+                "valor_abatimento": "0000000000000",
+                "desconto_concedito": None,
+                "valor_recebido": "",
+                "juros_mora": "",
+                "outros_recebimento": None,
+                "abatimento_nao_aproveitado": None,
+                "valor_lancamento": None,
+                "indicativo_lancamento": None,
+                "indicador_valor": None,
+                "valor_ajuste": None,
+                "sequencial": "000016",
+                "arquivo": None,
+                "motivo_ocorrencia": [],
+                "documento_numero": None,
+            },
+        ]
+        # Verifica se a fatura está aberta, antes de fazer o retorno.
+        self.assertEqual(self.invoice_ailos_1.state, "open")
+
+        self.journal = self.env.ref("l10n_br_account_payment_order.ailos_journal")
+
+        with mock.patch(
+            _provider_class + "._get_brcobranca_retorno",
+            return_value=mocked_response,
+        ):
+            file_name = get_resource_path(
+                "l10n_br_account_payment_brcobranca",
+                "tests",
+                "data",
+                "CNAB240AILOS.RET",
+            )
+
+            # Se for um codigo cnab de liquidação retorna as account.move criadas
+            moves = self._import_file(file_name)
+
+        self.assertEqual(
+            "Retorno CNAB - Banco COOP CENTRAL AILOS - Conta 373", moves.name
+        )
+        # I check that the invoice state is "Paid"
+        self.assertEqual(self.invoice_ailos_1.state, "paid")
