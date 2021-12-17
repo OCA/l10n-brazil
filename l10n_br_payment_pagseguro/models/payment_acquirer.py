a31078c3f4ccb091c9ba3fe15034a2f76c3b74ec
Cristiano Rodrigues <cristiano_one@yahoo.com.br>
2021-12-17 13:57:34 -0300

    [IMP] Successful transaction

--- l10n_br_payment_pagseguro/models/payment_acquirer.py
+++ l10n_br_payment_pagseguro/models/payment_acquirer.py
@@ -88,6 +88,8 @@ class PaymentAcquirerPagseguro(models.Model):
         PAGSEGURO_HEADERS = {
             'Authorization': f'Bearer {self.pagseguro_token}',
             'Content-Type': 'application/json',
+            'x-api-version': '4.0',
+            'x-idempotency-key': '',
         }
 
         return PAGSEGURO_HEADERS
