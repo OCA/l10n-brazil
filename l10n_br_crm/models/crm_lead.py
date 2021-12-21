5add56aab5cc55dc1b478c36f4773982e91c40bc
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-12-21 14:05:25 -0300

    [MIG] l10n_br_crm: Migration to 13.0

--- l10n_br_crm/models/crm_lead.py
+++ l10n_br_crm/models/crm_lead.py
@@ -52,7 +52,6 @@ class Lead(models.Model):
 
     rg = fields.Char(string="RG", size=16)
 
-    @api.multi
     @api.constrains("cnpj")
     def _check_cnpj(self):
         for record in self:
@@ -63,7 +62,6 @@ class Lead(models.Model):
                     raise ValidationError(_("CNPJ inválido!"))
             return True
 
-    @api.multi
     @api.constrains("cpf")
     def _check_cpf(self):
         for record in self:
@@ -74,7 +72,6 @@ class Lead(models.Model):
                     raise ValidationError(_("CPF inválido!"))
             return True
 
-    @api.multi
     @api.constrains("inscr_est")
     def _check_ie(self):
         """Checks if company register number in field insc_est is valid,
@@ -136,7 +133,6 @@ class Lead(models.Model):
                 result["name_surname"] = self.partner_id.legal_name
         self.update(result)
 
-    @api.multi
     def _create_lead_partner_data(self, name, is_company, parent_id=False):
         """extract data from lead to create a partner
         :param name : furtur name of the partner
