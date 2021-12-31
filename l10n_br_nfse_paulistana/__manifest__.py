2a9eeb574a00680a22edccc1cc46159f78fa666d
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-12-31 15:05:55 -0300

    [IMP] l10n_br_nfse_paulistana: apply pre-commit

--- l10n_br_nfse_paulistana/__manifest__.py
+++ l10n_br_nfse_paulistana/__manifest__.py
@@ -2,26 +2,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'NFS-e (Nota Paulistana)',
-    'summary': """
+    "name": "NFS-e (Nota Paulistana)",
+    "summary": """
         NFS-e (Nota Paulistana)""",
-    'version': '12.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': 'KMEE, Odoo Community Association (OCA)',
-    'maintainers': ['gabrielcardoso21', 'mileo', 'luismalta'],
+    "version": "12.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "maintainers": ["gabrielcardoso21", "mileo", "luismalta"],
     "development_status": "Beta",
-    'website': 'https://github.com/OCA/l10n-brazil',
-    'external_dependencies': {
-        'python': [
-            'erpbrasil.edoc',
-            'erpbrasil.assinatura',
-            'erpbrasil.transmissao',
-            'erpbrasil.base',
-            'nfselib.paulistana',
-            'unidecode',
+    "website": "https://github.com/OCA/l10n-brazil",
+    "external_dependencies": {
+        "python": [
+            "erpbrasil.edoc",
+            "erpbrasil.assinatura",
+            "erpbrasil.transmissao",
+            "erpbrasil.base",
+            "nfselib.paulistana",
+            "unidecode",
         ],
     },
-    'depends': [
-        'l10n_br_nfse',
+    "depends": [
+        "l10n_br_nfse",
     ],
 }
