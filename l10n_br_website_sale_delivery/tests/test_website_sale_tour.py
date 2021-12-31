3f1dfa18ced17f2c2c56d10dc1935cdaa244773e
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-12-31 17:11:02 -0300

    [IMP] l10n_br_website_delivery: apply pre-commit

--- l10n_br_website_sale_delivery/tests/test_website_sale_tour.py
+++ l10n_br_website_sale_delivery/tests/test_website_sale_tour.py
@@ -4,41 +4,37 @@
 
 from unittest import mock
 
-from odoo.tests import tagged
-from odoo.tests import HttpCase
+from odoo.tests import HttpCase, tagged
 
-_module_ns = 'odoo.addons.l10n_br_zip'
-_provider_class = (
-    _module_ns
-    + '.models.l10n_br_zip'
-    + '.L10nBrZip'
-)
+_module_ns = "odoo.addons.l10n_br_zip"
+_provider_class = _module_ns + ".models.l10n_br_zip" + ".L10nBrZip"
 
 
-@tagged('post_install', '-at_install')
+@tagged("post_install", "-at_install")
 class TestUi(HttpCase):
-
     def test_01_l10n_br_website_sale_delivery_tour(self):
-        self.env.ref("delivery.free_delivery_carrier").sudo().write({
-            'fixed_price': 7,
-            'free_over': True,
-            'amount': 10000,
-            })
+        self.env.ref("delivery.free_delivery_carrier").sudo().write(
+            {
+                "fixed_price": 7,
+                "free_over": True,
+                "amount": 10000,
+            }
+        )
         tour = (
             "odoo.__DEBUG__.services['web_tour.tour']",
             "l10n_br_website_sale_delivery_tour",
-            )
+        )
 
         mocked_response = {
-            "zip_code": '12246250',
+            "zip_code": "12246250",
             "street": " Rua do Aruana",
             "district": "Parque Residencial Aquarius",
-            "city_id": self.env.ref('l10n_br_base.city_3549904').id,
-            "state_id": self.env.ref('base.state_br_sp').id,
-            "country_id": self.env.ref('base.br').id,
+            "city_id": self.env.ref("l10n_br_base.city_3549904").id,
+            "state_id": self.env.ref("base.state_br_sp").id,
+            "country_id": self.env.ref("base.br").id,
         }
         with mock.patch(
-            _provider_class + '._consultar_cep',
+            _provider_class + "._consultar_cep",
             return_value=mocked_response,
         ):
             self.browser_js(
@@ -46,9 +42,8 @@ class TestUi(HttpCase):
                 code="%s.run('%s')" % tour,
                 ready="%s.tours.%s.ready" % tour,
                 login="admin",
-                timeout=5000
-                )
+                timeout=5000,
+            )
         # check result
-        record = self.env['sale.order'].search(
-            [], limit=1)
+        record = self.env["sale.order"].search([], limit=1)
         self.assertEqual(record.amount_freight_value, 7.0)
