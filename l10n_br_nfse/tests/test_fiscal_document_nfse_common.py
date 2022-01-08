93c9d581564f5a06f98dd797578d2f4b0958c952
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-01-08 13:27:46 -0300

    [IMP] l10n_br_nfse: increase test coverage

--- l10n_br_nfse/tests/test_fiscal_document_nfse_common.py
+++ l10n_br_nfse/tests/test_fiscal_document_nfse_common.py
@@ -3,7 +3,11 @@
 
 from odoo.tests.common import TransactionCase
 
-from odoo.addons.l10n_br_fiscal.constants.fiscal import PROCESSADOR_OCA
+from odoo.addons.l10n_br_fiscal.constants.fiscal import (
+    PROCESSADOR_NENHUM,
+    PROCESSADOR_OCA,
+)
+from odoo.addons.l10n_br_nfse.models.document import filter_processador_edoc_nfse
 
 
 class TestFiscalDocumentNFSeCommon(TransactionCase):
@@ -65,6 +69,61 @@ class TestFiscalDocumentNFSeCommon(TransactionCase):
             " for Venda de Serviço de Contribuinte Dentro do Estado.",
         )
 
+        # IBGE Code
+        self.assertEqual(
+            str(self.company.prepare_company_servico().get("codigo_municipio")),
+            "3132404",
+            "Error to mappping IBGE Code 3132404"
+            " for Venda de Serviço de Contribuinte Dentro do Estado.",
+        )
+
+        # test _prepare_dados_servico()
+        self.assertEqual(
+            str(self.nfse_same_state._prepare_dados_servico().get("codigo_municipio")),
+            "3132404",
+            "Error to mappping IBGE Code 3132404"
+            " for Venda de Serviço de Contribuinte Dentro do Estado.",
+        )
+
+        # test _prepare_dados_tomador()
+        self.assertEqual(
+            str(self.nfse_same_state._prepare_dados_tomador().get("codigo_municipio")),
+            "3550308",
+            "Error to mappping IBGE Code 3550308"
+            " for Venda de Serviço de Contribuinte Dentro do Estado.",
+        )
+
+        # Test with Processador OCA
+        self.assertTrue(filter_processador_edoc_nfse(self.nfse_same_state))
+
+        # test without Processador
+        self.company.processador_edoc = PROCESSADOR_NENHUM
+        self.assertFalse(filter_processador_edoc_nfse(self.nfse_same_state))
+
+        # Test res.partner.prepare_partner_tomador
+        self.assertEqual(
+            str(
+                self.nfse_same_state.partner_id.prepare_partner_tomador(
+                    self.company.country_id.id
+                ).get("codigo_municipio")
+            ),
+            "3550308",
+            "Error to mappping IBGE Code 3550308"
+            " for Venda de Serviço de Contribuinte Dentro do Estado.",
+        )
+
+        # Test res.partner.prepare_partner_tomador (Exterior)
+        self.assertEqual(
+            str(
+                self.nfse_same_state.partner_id.prepare_partner_tomador(1).get(
+                    "codigo_municipio"
+                )
+            ),
+            "9999999",
+            "Error to mappping IBGE Code 9999999"
+            " for Venda de Serviço de Contribuinte Dentro do Estado.",
+        )
+
         for line in self.nfse_same_state.line_ids:
             line._onchange_product_id_fiscal()
             line._onchange_commercial_quantity()
@@ -72,6 +131,14 @@ class TestFiscalDocumentNFSeCommon(TransactionCase):
             line._onchange_fiscal_operation_id()
             line._onchange_fiscal_operation_line_id()
             line._onchange_fiscal_taxes()
+
+            # prepare_line_servico()
+            self.assertEqual(
+                line.prepare_line_servico().get("codigo_tributacao_municipio"),
+                "6311900",
+                "Error to mappping City Taxation Code 6311900"
+                " for Venda de Serviço de Contribuinte Dentro do Estado.",
+            )
 
             # Fiscal Deductions Value
             self.assertEqual(
@@ -86,5 +153,15 @@ class TestFiscalDocumentNFSeCommon(TransactionCase):
                 line.city_taxation_code_id.code,
                 "6311900",
                 "Error to mappping City Taxation Code 6311900"
+                " for Venda de Serviço de Contribuinte Dentro do Estado.",
+            )
+
+            # Fiscal Deductions Value
+            line.product_id.fiscal_deductions_value = 10
+            line._onchange_product_id_fiscal()
+            self.assertEqual(
+                line.fiscal_deductions_value,
+                10.0,
+                "Error to mappping Fiscal Deductions Value 10.0"
                 " for Venda de Serviço de Contribuinte Dentro do Estado.",
             )
