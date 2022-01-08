2c4fa781d5b8fea62b4399d94a7fca6251843c66
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-01-08 13:27:00 -0300

    [IMP] l10n_br_nfse: dead code elimination

--- l10n_br_nfse/models/document.py
+++ l10n_br_nfse/models/document.py
@@ -38,12 +38,6 @@ def filter_processador_edoc_nfse(record):
     return False
 
 
-def fiter_provedor(record):
-    if record.company_id.provedor_nfse:
-        return True
-    return False
-
-
 class Document(models.Model):
 
     _inherit = "l10n_br_fiscal.document"
