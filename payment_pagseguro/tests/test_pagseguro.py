ca201cf8e580bb9216fb788a7178051db7aaeac9
Cristiano Rodrigues <cristiano_one@yahoo.com.br>
2022-01-12 18:29:15 -0300

    [FIX] Removed tour wait time of test

--- payment_pagseguro/tests/test_pagseguro.py
+++ payment_pagseguro/tests/test_pagseguro.py
@@ -2,16 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import odoo
-from odoo.tests.common import HttpCase
 
 
 @odoo.tests.tagged("post_install", "-at_install")
-class PagseguroTest(HttpCase):
+class PagseguroTest(odoo.tests.HttpCase):
+
     def test_buy_pagseguro(self):
-        self.browser_js(
-            "/",
+        self.phantom_js(
+            "/shop",
             "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_pagseguro')",
             "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_pagseguro.ready",
             login="admin",
-            timeout=40 * 60,
+            timeout=20000
         )
