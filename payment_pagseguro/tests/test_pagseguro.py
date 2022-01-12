d55aae817afd15537499636082ce094181bc3852
Cristiano Rodrigues <cristiano_one@yahoo.com.br>
2022-01-12 14:27:38 -0300

    [FIX] Pagseguro tour website publish

--- payment_pagseguro/tests/test_pagseguro.py
+++ payment_pagseguro/tests/test_pagseguro.py
@@ -1,21 +1,17 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-
 import odoo
 from odoo.tests.common import HttpCase
-
-_logger = logging.getLogger(__name__)
 
 
 @odoo.tests.tagged("post_install", "-at_install")
 class PagseguroTest(HttpCase):
-    def test_buy_with_pagseguro(self):
+    def test_buy_pagseguro(self):
         self.browser_js(
             "/",
-            "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_with_pagseguro')",
-            "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_with_pagseguro.ready",
+            "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_pagseguro')",
+            "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_pagseguro.ready",
             login="admin",
-            timeout=60 * 60,
+            timeout=40 * 60,
         )
