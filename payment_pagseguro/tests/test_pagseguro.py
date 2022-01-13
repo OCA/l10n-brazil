aaf3f0f02e97011193c0f71176dea1516c2e2c29
Cristiano Rodrigues <cristiano_one@yahoo.com.br>
2022-01-13 11:02:29 -0300

    [FIX] Insert payment flow pagseguro data

--- payment_pagseguro/tests/test_pagseguro.py
+++ payment_pagseguro/tests/test_pagseguro.py
@@ -8,10 +8,10 @@ import odoo
 class PagseguroTest(odoo.tests.HttpCase):
 
     def test_buy_pagseguro(self):
-        self.phantom_js(
+        self.browser_js(
             "/shop",
             "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_pagseguro')",
             "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_pagseguro.ready",
             login="admin",
-            timeout=20000
+            timeout=20000,
         )
