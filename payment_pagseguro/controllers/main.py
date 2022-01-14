58b634ab0a68afc3525efcbb8b57883d45ceffec
Cristiano Rodrigues <cristiano_one@yahoo.com.br>
2022-01-14 08:47:04 -0300

    [REF] Add license, improve readme and remove unnecessary functions

--- payment_pagseguro/controllers/main.py
+++ payment_pagseguro/controllers/main.py
@@ -1,3 +1,6 @@
+# Copyright 2020 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import requests
 import werkzeug
 
--- payment_pagseguro/readme/INSTALL.rst
+++ payment_pagseguro/readme/INSTALL.rst
@@ -1,3 +1,1 @@
-This module depends on:
-
-* payment
+This module depends on * payment and web_tour such to function correctly.
