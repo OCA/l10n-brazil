80e6664ce1cab0db82ff0a06ea526011abde5b25
Diego Paradeda <paradeda.diego@gmail.com>
2022-01-18 15:03:03 -0300

    [FIX] broken logic when using 'cpf' -> always raises

--- l10n_br_fiscal/models/document_related.py
+++ l10n_br_fiscal/models/document_related.py
@@ -74,7 +74,6 @@ class DocumentRelated(models.Model):
 
     @api.constrains("cnpj_cpf")
     def _check_cnpj_cpf(self):
-        result = True
         for record in self:
             if record.cnpj_cpf:
                 disable_cnpj_ie_validation = (
@@ -86,15 +85,11 @@ class DocumentRelated(models.Model):
                 )
 
                 if not disable_cnpj_ie_validation:
-                    if record.cpfcnpj_type == "cnpj":
-                        if not fiscal.cnpj_cpf.validar(record.cnpj_cpf):
-                            result = False
+                    if not fiscal.cnpj_cpf.validar(record.cnpj_cpf):
+                        if record.cpfcnpj_type == "cnpj":
                             document = "CNPJ"
-                    elif record.cpfcnpj_type == "cpf":
-                        result = False
-                        document = "CPF"
-
-                    if not result:
+                        elif record.cpfcnpj_type == "cpf":
+                            document = "CPF"
                         raise ValidationError(_("{} Invalid!").format(document))
 
     @api.constrains("inscr_est", "state_id")
