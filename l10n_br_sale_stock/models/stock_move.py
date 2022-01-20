439153b68c5f60b62286382146f405ccdc65cc78
Magno Costa <magno.costa@akretion.com.br>
2022-01-20 17:11:52 -0300

    [MIG] Because of the changed in method _get_new_picking_values are necessary to get the main Fiscal Operation from Sale Order to used in creation of Stock Picking.

--- l10n_br_sale_stock/models/stock_move.py
+++ l10n_br_sale_stock/models/stock_move.py
@@ -25,8 +25,16 @@ class StockMove(models.Model):
         # sobre escrevendo o partner_id e acaba criando um picking
         # sem o partner_id caso esse dict atualize o do super
         values = {}
+        fiscal_operation = False
         if self.sale_line_id:
             values = self.sale_line_id.order_id._prepare_br_fiscal_dict()
+            fiscal_operation = self.sale_line_id.order_id.fiscal_operation_id
 
         values.update(super()._get_new_picking_values())
+        # O self pode conter mais de uma stock.move por isso a Operação Fiscal
+        # usada é a do cabeçalho do Pedido de Venda já que nas linhas podem ser
+        # usadas diferentes Operações Fiscais
+        if fiscal_operation:
+            values.update({"fiscal_operation_id": fiscal_operation.id})
+
         return values
