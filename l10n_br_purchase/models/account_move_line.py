71e7a6b8f87329f0a3efa71fa2a0e995555dc84a
Magno Costa <magno.costa@akretion.com.br>
2022-01-21 16:17:48 -0300

    [MIG] Now there are a specific method to pass fields in creation of account.move.line .

--- l10n_br_purchase/models/account_move_line.py
+++ l10n_br_purchase/models/account_move_line.py
@@ -1,16 +1,14 @@
 # Copyright (C) 2021  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.model
-    def new(self, values=None, ref=None):
-        if not values:
-            values = {}
+    def _prepare_account_move_line(self, move):
+        values = super()._prepare_account_move_line(move)
         if values.get("purchase_line_id"):
             line = self.env["purchase.order.line"].browse(
                 values.get("purchase_line_id")
@@ -18,4 +16,5 @@ class AccountMoveLine(models.Model):
             fiscal_values = line._prepare_br_fiscal_dict()
             fiscal_values.update(values)
             values.update(fiscal_values)
-        return super().new(values, ref)
+
+        return values
