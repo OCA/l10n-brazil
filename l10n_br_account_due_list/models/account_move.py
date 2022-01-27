9afcf6715a4a5aecd8b8a9a8c736617b5f002c3a
Renato Lima <renato.lima@akretion.com>
2022-01-27 10:15:11 -0300

    [REF] financial_move_line_ids and payment_move_line_ids fields

--- l10n_br_account_due_list/models/account_move.py
+++ l10n_br_account_due_list/models/account_move.py
@@ -1,39 +1,24 @@
 # Copyright (C) 2021 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
-    financial_move_line_ids = fields.Many2many(
+    financial_move_line_ids = fields.One2many(
         comodel_name="account.move.line",
-        relation="account_invoice_account_financial_move_line_rel",
-        compute="_compute_financial",
-        store=True,
+        inverse_name="move_id",
         string="Financial Move Lines",
+        copy=False,
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        domain="[('account_id.user_type_id.type', 'in', ('receivable', 'payable'))]",
     )
 
-    payment_move_line_ids = fields.Many2many(
-        "account.move.line",
+    payment_move_line_ids = fields.One2many(
+        comodel_name="account.payment",
+        inverse_name="move_id",
         string="Payment Move Lines",
-        compute="_compute_payments",
-        store=True,
     )
-
-    @api.depends("line_ids", "state")
-    def _compute_financial(self):
-        for move in self:
-            lines = move.line_ids.filtered(
-                lambda l: l.account_id.internal_type in ("receivable", "payable")
-            )
-            move.financial_move_line_ids = lines.sorted()
-
-    @api.depends("line_ids.amount_residual")
-    def _compute_payments(self):
-        for move in self:
-            move.payment_move_line_ids = [
-                aml.id
-                for partial, amount, aml in move._get_reconciled_invoices_partials()
-            ]
