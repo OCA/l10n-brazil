ffb4edceabdfff638a15e4a735b55fe31f3a4573
Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
2022-01-28 14:04:55 -0300

    [ADD] Operation line configuration

--- l10n_br_pos/__manifest__.py
+++ l10n_br_pos/__manifest__.py
@@ -25,6 +25,7 @@
         "python": ['satcomum'],
     },
     "data": [
+        "data/operation_line_data.xml",
         "security/l10n_br_pos_product_fiscal_map.xml",
         # "data/l10n_br_fiscal_cfop_data.xml",
         "data/account_journal_data.xml",
