a2f98b073e81182f8d46f5732483fdf41292306e
Renato Lima <renato.lima@akretion.com>
2022-02-03 10:59:30 -0300

    [REF] rename line_ids to account_line_ids for account.move.line in fiscal.document.line

--- l10n_br_account/models/fiscal_document_line.py
+++ l10n_br_account/models/fiscal_document_line.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class FiscalDocumentLine(models.Model):
     _inherit = "l10n_br_fiscal.document.line"
 
-    line_ids = fields.One2many(
+    account_line_ids = fields.One2many(
         comodel_name="account.move.line",
         inverse_name="fiscal_document_line_id",
         string="Invoice Lines",
