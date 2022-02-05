f06cd636b63b83887263071bd870b04e5665e498
Renato Lima <renato.lima@akretion.com>
2022-02-05 20:07:10 -0300

    Avoid warning message: selection attribute will be ignored as the field is related

--- l10n_br_nfe/models/res_config_settings.py
+++ l10n_br_nfe/models/res_config_settings.py
@@ -3,48 +3,36 @@
 
 from odoo import fields, models
 
-from ..constants.nfe import (
-    NFE_DANFE_LAYOUTS,
-    NFE_ENVIRONMENTS,
-    NFE_TRANSMISSIONS,
-    NFE_VERSIONS,
-)
-
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     nfe_version = fields.Selection(
         string="NF-e Version",
-        selection=NFE_VERSIONS,
         related="company_id.nfe_version",
         readonly=False,
     )
 
     nfe_environment = fields.Selection(
         string="NFe Environment",
-        selection=NFE_ENVIRONMENTS,
         related="company_id.nfe_environment",
         readonly=False,
     )
 
     nfe_transmission = fields.Selection(
         string="NFe Transmission",
-        selection=NFE_TRANSMISSIONS,
         related="company_id.nfe_transmission",
         readonly=False,
     )
 
     nfe_danfe_layout = fields.Selection(
         string="NFe Layout",
-        selection=NFE_DANFE_LAYOUTS,
         related="company_id.nfe_danfe_layout",
         readonly=False,
     )
 
     nfce_danfe_layout = fields.Selection(
         string="NFCe Layout",
-        selection=NFE_DANFE_LAYOUTS,
         related="company_id.nfce_danfe_layout",
         readonly=False,
     )
