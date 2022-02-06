beac56aa62757a5a5703f7bc257e5fc252bf8378
Renato Lima <renato.lima@akretion.com>
2022-02-05 21:36:58 -0300

    [REF] onchange to update repartition_line_ids with correct factor_percent for deductible taxes

--- l10n_br_coa/models/account_tax_mixin.py
+++ l10n_br_coa/models/account_tax_mixin.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 - TODAY Renato Lima - Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountTaxMixin(models.AbstractModel):
@@ -12,3 +12,15 @@ class AccountTaxMixin(models.AbstractModel):
         string="Deductible Tax?",
         default=True,
     )
+
+    @api.onchange("deductible")
+    def _onchange_deductible(self):
+        for repartition in self.invoice_repartition_line_ids.filtered(
+            lambda r: r.repartition_type == "tax"
+        ):
+            repartition.factor_percent = 100 if not self.deductible else -100
+
+        for repartition in self.refund_repartition_line_ids.filtered(
+            lambda r: r.repartition_type == "tax"
+        ):
+            repartition.factor_percent = 100 if not self.deductible else -100
