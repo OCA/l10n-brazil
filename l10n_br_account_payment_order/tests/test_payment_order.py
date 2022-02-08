b93c3de890c4384fbf569d7545c8257510270c95
Magno Costa <magno.costa@akretion.com.br>
2022-02-08 18:01:17 -0300

    [MIG] Tests.

--- l10n_br_account_payment_order/tests/test_payment_order.py
+++ l10n_br_account_payment_order/tests/test_payment_order.py
@@ -2,7 +2,6 @@
 #   Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.exceptions import ValidationError
 from odoo.tests import SavepointCase, tagged
 
 
@@ -18,11 +17,10 @@ class TestPaymentOrder(SavepointCase):
 
     def test_payment_mode_without_payment_order(self):
         """ Test Invoice when Payment Mode not generate Payment Order. """
-        self.invoice_cheque._onchange_payment_mode_id()
         # I validate invoice by creating on
-        self.invoice_cheque.action_invoice_open()
-        # I check that the invoice state is "Open"
-        self.assertEqual(self.invoice_cheque.state, "open")
+        self.invoice_cheque.post()
+        # I check that the invoice state is "posted"
+        self.assertEqual(self.invoice_cheque.state, "posted")
         payment_order = self.env["account.payment.order"].search(
             [("payment_mode_id", "=", self.invoice_cheque.payment_mode_id.id)]
         )
@@ -31,11 +29,17 @@ class TestPaymentOrder(SavepointCase):
     def test_bra_number_constrains(self):
         """ Test bra_number constrains. """
         self.banco_bradesco = self.env["res.bank"].search([("code_bc", "=", "033")])
-        with self.assertRaises(ValidationError):
-            self.env["res.partner.bank"].create(
+        try:
+            result = self.env["res.partner.bank"].create(
                 dict(
                     bank_id=self.banco_bradesco.id,
                     partner_id=self.ref("l10n_br_base.res_partner_akretion"),
                     bra_number="12345",
                 )
             )
+        except Exception:
+            result = False
+        self.assertFalse(
+            result,
+            "Error to check included bank BRA Number greater than four caracters",
+        )
