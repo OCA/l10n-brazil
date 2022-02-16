20fbf690dbef36a1299d31d178cb00e73dd3ea8e
Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
2022-02-16 13:21:14 -0300

    [ADD] Fiscal configuration

--- l10n_br_pos/__manifest__.py
+++ l10n_br_pos/__manifest__.py
@@ -26,6 +26,8 @@
     },
     "data": [
         "data/operation_line_data.xml",
+        "data/l10n_br_fiscal.tax.csv",
+        "data/icms_tax_definition_data.xml",
         "security/l10n_br_pos_product_fiscal_map.xml",
         # "data/l10n_br_fiscal_cfop_data.xml",
         "data/account_journal_data.xml",
