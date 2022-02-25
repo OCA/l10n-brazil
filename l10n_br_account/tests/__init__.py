c69bb78b6e6cac92106ad3820ed718f377c7cf88
Neto <netosjb@yahoo.com.br>
2022-02-25 16:27:30 -0300

    [FIX] add case tests for fiscal dummy in company

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -4,3 +4,4 @@ from . import test_account_taxes
 from . import test_customer_invoice_dummy
 from . import test_supplier_invoice_dummy
 from . import test_invoice_refund
+from . import test_company_fiscal_dummy
