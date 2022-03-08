5b79f33b7fc7aa5044f6e7128ba214700dae3055
Luiz Felipe do Divino <lf.divino@gmail.com>
2022-03-08 11:19:44 -0300

    [FIX] Contorno ao envio de e-mail pela pos.order

--- l10n_br_pos/models/__init__.py
+++ l10n_br_pos/models/__init__.py
@@ -13,5 +13,5 @@ from . import res_partner
 from . import stock_move
 from . import l10n_br_pos_product_fiscal_map
 from . import product_template
-
+from . import mail_thread
 # from . import ir_sequence
