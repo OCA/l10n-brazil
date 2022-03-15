ba1ec2a06511b3a51195fcb70b9c2b3e3f38aaf2
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-03-15 15:43:59 -0300

    [UPD] Rollback changes that forced imported document state to be 'authorized'

--- l10n_br_nfe/models/res_city.py
+++ l10n_br_nfe/models/res_city.py
@@ -2,9 +2,17 @@
 # Copyright 2021 Akretion (Renato Lima <renato.lima@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class ResCity(models.Model):
     _inherit = "res.city"
     _nfe_search_keys = ["ibge_code"]
+
+    # nfe40_cMun = fields.Char(related="ibge_code", readonly=True)
+    # nfe40_xMun = fields.Char(related="name", readonly=True)
+    # nfe40_UF = fields.Char(related="state_id.code")
+    # nfe40_cPais = fields.Char(related="country_id.bc_code")
+    # nfe40_xPais = fields.Char(related="country_id.name")
+
+
