37759e673943d8c3f840a292b527110a1bd88e8a
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-03-21 21:47:30 -0300

    [MIG] l10n_br_purchase: Migration to 14.0

--- l10n_br_purchase/models/account_move.py
+++ l10n_br_purchase/models/account_move.py
@@ -8,11 +8,6 @@ from odoo import api, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _prepare_invoice_line_from_po_line(self, line):
-        values = super()._prepare_invoice_line_from_po_line(line)
-        values.update(line._prepare_br_fiscal_dict())
-        return values
-
     @api.onchange("purchase_vendor_bill_id", "purchase_id")
     def _onchange_purchase_auto_complete(self):
         if self.purchase_id:
