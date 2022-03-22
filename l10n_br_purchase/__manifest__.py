ad2564ad7dae1265fee3ec27e5d5c07f2ead5394
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-03-21 23:21:09 -0300

    [IMP] l10n_br_purchase: black, isort, prettier

--- l10n_br_purchase/__manifest__.py
+++ l10n_br_purchase/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Localisation",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["purchase", "l10n_br_account"],
     "data": [
         # Security
