178dbb2bf2f073b2454858a92e659fab5ff2577c
Magno Costa <magno.costa@akretion.com.br>
2022-03-24 09:45:25 -0300

    [REF] Synchronize Sale Partner Shipping in Stock Picking.

--- l10n_br_sale_stock/models/sale_order.py
+++ l10n_br_sale_stock/models/sale_order.py
@@ -34,3 +34,29 @@ class SaleOrder(models.Model):
                     button_create_invoice_invisible = True
 
         self.button_create_invoice_invisible = button_create_invoice_invisible
+
+    @api.onchange("partner_shipping_id")
+    def _onchange_partner_shipping_id(self):
+        """
+        Caso ocorra a alteração do campo Endereço de Entrega/partner_shipping_id
+        depois do Pedido confirmado os stock.picking relacionados ficam com o
+        partner_id anterior, o que é errado, o metodo original apenas mostra uma
+        mensagem de alerta na tela orientando o usuário a corrigir manualmente,
+        mas como na localização com esse modulo pode se definir a criação da
+        Invoice a partir do stock.picking é melhor garantir a alteração do campo
+        partner_id da stock.picking afim de evitar erros na criação da Invoice.
+        :return: super()
+        """
+        # TODO: Verificar essa questão na migração a partir da v14
+
+        pickings = self.picking_ids.filtered(
+            lambda p: p.state not in ["done", "cancel"]
+            and p.partner_id != self.partner_shipping_id
+        )
+        # Atribuição da forma abaixo por algum motivo
+        # não funciona apenas o write
+        # for picking in pickings:
+        #    picking.partner_id = self.partner_shipping_id
+        pickings.write({"partner_id": self.partner_shipping_id.id})
+
+        return super()._onchange_partner_shipping_id()
