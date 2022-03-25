cb088105acb6925acceb03a7da2f7736b3c7aa25
Kilian Macedo Melcher <kilian.melcher@gmail.com>
2022-03-25 16:17:28 -0300

    [REF] Implement get field for reusage and add tests

--- l10n_br_receitaws/models/l10n_br_base_party_mixin.py
+++ l10n_br_receitaws/models/l10n_br_base_party_mixin.py
@@ -5,7 +5,8 @@ from erpbrasil.base.fiscal.cnpj_cpf import validar_cnpj
 from erpbrasil.base.misc import punctuation_rm
 from requests import get
 
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class PartyMixin(models.AbstractModel):
@@ -20,79 +21,62 @@ class PartyMixin(models.AbstractModel):
             try:
                 data = response.json()
             except ValueError:
-                data = None
-            # TODO: Separar os multiplos telefones
-            # cnae_main_id
-            #     'atividade_principal': [{
-            #         'text': 'Fabricação ...',
-            #          'code': '10.91-1-01'
-            #     }],
+                raise ValidationError(_("Não foi possível ler a resposta do receitaws"))
 
-            #     'data_situacao': '03/09/9999',
-            #     'abertura': '12/11/9999',
-            #     'tipo': 'MATRIZ',
+            if data.get("status") == "ERROR":
+                raise ValidationError(_(data.get("message")))
 
-            #     'qsa': [
-            #         {'qual': '49-Sócio-Administrador', 'nome': 'LUIS'},
-            #         {'qual': '22-Sócio', 'nome': 'MARIA'}
-            #     ],
-            #     'situacao': 'ATIVA',
-            #     'porte': 'EMPRESA DE PEQUENO PORTE',
-            #     'natureza_juridica': '206-2 - Sociedade Empresária Limitada',
-            #     'ultima_atualizacao': '2021-02-08T13:33:39.365Z',
-            #     'status': 'OK',
-            #     'complemento': '',
-            #     'efr': '',
-            #     'motivo_situacao': '',
-            #     'situacao_especial': '',
-            #     'data_situacao_especial': '',
-            #     'atividades_secundarias': [
-            #         {'code': '00.00-0-00', 'text': 'Não informada'}
-            #     ],
-            #     'capital_social': '0.00',
-            #     'extra': {},
-            #     'billing': {'free': True, 'database': True}
-            # }
             self.company_type = "company"
-            if data.get("nome").title() != "":
-                self.legal_name = data["nome"].title()
-            if data.get("fantasia").title() != "":
-                self.name = data["fantasia"].title()
-            if data.get("email").title() != "":
-                self.email = data["email"].lower()
-            if data.get("complemento") != "":
-                self.street2 = data["complemento"].title()
-            if data.get("cep") != "":
-                self.zip = data["cep"]
-            if data.get("logradouro") != "":
-                self.street = data["logradouro"].title()
-            if data.get("numero") != "":
-                self.street_number = data["numero"]
-            if data.get("bairro") != "":
-                self.district = data["bairro"].title()
+            self.legal_name = self.get_field(data, "nome", title=True)
+            self.name = self.get_field(data, "fantasia", title=True)
+            self.email = self.get_field(data, "email", lower=True)
+            self.street = self.get_field(data, "logradouro", title=True)
+            self.street2 = self.get_field(data, "complemento", title=True)
+            self.district = self.get_field(data, "bairro", title=True)
+            self.street_number = self.get_field(data, "numero")
+            self.zip = self.get_field(data, "cep")
+            self.get_phone(data)
+            self.get_state_city(data)
 
-            # TODO: Separar os multiplos telefones
-            if data.get("telefone") != "":
-                self.phone = data["telefone"]
-                #     'telefone': '(12) 9999-9999/ (12) 9999-9999',
-
-            if data.get("uf") != "":
-                state_id = self.env["res.country.state"].search(
-                    [
-                        ("code", "=", data["uf"]),
-                        ("country_id", "=", self.country_id.id),
-                    ],
-                    limit=1,
-                )
-                self.state_id = state_id
-
-                if data.get("municipio") != "":
-                    city_id = self.env["res.city"].search(
-                        [
-                            ("name", "=ilike", data["municipio"].title()),
-                            ("state_id.id", "=", state_id.id),
-                        ]
-                    )
-                    if len(city_id) == 1:
-                        self.city_id = city_id
         return result
+
+    def get_phone(self, data):
+        if data.get("telefone") != "":
+            phones = data["telefone"].split("/")
+            self.phone = phones[0]
+            if len(phones) > 1:
+                self.mobile = phones[1]
+            #     'telefone': '(12) 9999-9999/ (12) 9999-9999',
+
+    def get_state_city(self, data):
+        if data.get("uf") != "":
+            state_id = self.env["res.country.state"].search(
+                [
+                    ("code", "=", data["uf"]),
+                    ("country_id", "=", self.country_id.id),
+                ],
+                limit=1,
+            )
+            self.state_id = state_id
+
+            if data.get("municipio") != "":
+                city_id = self.env["res.city"].search(
+                    [
+                        ("name", "=ilike", data["municipio"].title()),
+                        ("state_id.id", "=", state_id.id),
+                    ]
+                )
+                if len(city_id) == 1:
+                    self.city_id = city_id
+
+    @staticmethod
+    def get_field(data, name, title=False, lower=False):
+        value = False
+        if data.get(name) != "":
+            value = data[name]
+            if lower:
+                value = value.lower()
+            elif title:
+                value = value.title()
+
+        return value
---
+++ l10n_br_receitaws/tests/test_party_mixin.py
@@ -0,0 +1,44 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.exceptions import ValidationError
+from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
+
+
+@tagged("post_install", "-at_install")
+class TestPartyMixin(TransactionCase):
+    def setUp(self):
+        super(TestPartyMixin, self).setUp()
+
+        self.model = self.env["res.partner"]
+
+    def test_onchange_cnpj_cpf(self):
+        kilian = self.model.create({"name": "Kilian", "cnpj_cpf": "44.356.113/0001-08"})
+
+        kilian._onchange_cnpj_cpf()
+
+        self.assertEqual(kilian.company_type, "company")
+        self.assertEqual(kilian.legal_name, "Kilian Macedo Melcher 08777131460")
+        self.assertEqual(kilian.name, "Kilian Macedo Melcher")
+        self.assertEqual(kilian.email, "kilian.melcher@gmail.com")
+        self.assertEqual(kilian.street, "R Luiza Bezerra Motta")
+        self.assertEqual(kilian.street2, "Bloco E;Apt 302")
+        self.assertEqual(kilian.street_number, "950")
+        self.assertEqual(kilian.zip, "58.410-410")
+        self.assertEqual(kilian.district, "Catole")
+        self.assertEqual(kilian.phone, "(83) 8665-0905")
+        self.assertEqual(kilian.state_id.code, "PB")
+        self.assertEqual(kilian.city_id.name, "Campina Grande")
+
+    def test_onchange_cnpj_cpf_fail(self):
+        invalido = self.model.create({"name": "invalido", "cnpj_cpf": "00000000000000"})
+
+        with self.assertRaises(ValidationError):
+            invalido._onchange_cnpj_cpf()
+
+    def test_onchange_cnpj_cpf_multiple_phones(self):
+        self.model.search([("name", "=", "Isla")])  # TODO implement test
+
+        # 92.666.056/0001-06
+        # isla._onchange_cnpj_cpf()
