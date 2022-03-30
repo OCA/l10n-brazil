9dfc59f7e49939ef2c02fce37c66778cb46f013f
Magno Costa <magno.costa@akretion.com.br>
2022-03-30 17:15:02 -0300

    [REF+FIX] Included License Header, made module auto install and fix module version.

--- l10n_br_account_nfe/__manifest__.py
+++ l10n_br_account_nfe/__manifest__.py
@@ -1,11 +1,18 @@
+# Copyright (C) 2022-Today - Engenere (<https://engenere.one>).
+# @author Antônio S. Pereira Neto <neto@engenere.one>
+# @author Felipe Motter Pereira <felipe@engenere.one>
+# Copyright (C) 2022-Today - Akretion (<https://akretion.com/pt-BR>).
+# @author Magno Costa <magno.costa@akretion.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "Account NFe/NFC-e Integration",
-    "summary": "Integration between account and NFe",
+    "summary": "Integration between l10n_br_account and l10n_br_nfe",
     "category": "Localisation",
     "license": "AGPL-3",
-    "author": "Odoo Community Association (OCA)",
+    "author": "Engenere," "Akretion," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "12.0.0.0.0",
+    "version": "12.0.1.0.0",
     "development_status": "Alpha",
     "depends": [
         "l10n_br_nfe",
@@ -18,4 +25,6 @@
     "demo": [
         "demo/account_invoice_demo.xml",
     ],
+    "installable": True,
+    "auto_install": True,
 }
