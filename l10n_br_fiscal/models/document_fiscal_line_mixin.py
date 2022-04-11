da95fff0135dcb13889e006ba4d5431f2288e17e
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-04-11 19:08:32 -0300

    [IMP] l10n_br_fiscal: add cnae_id on document_fiscal_line_mixin

--- l10n_br_fiscal/models/document_fiscal_line_mixin.py
+++ l10n_br_fiscal/models/document_fiscal_line_mixin.py
@@ -853,3 +853,8 @@ class FiscalDocumentLineMixin(models.AbstractModel):
     estimate_tax = fields.Monetary(
         string="Estimate Tax",
     )
+
+    cnae_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.cnae",
+        string="CNAE Code",
+    )
