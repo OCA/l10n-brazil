96b59dc753cf3949cf87f33c9db2a7e50fea7919
Raphaël Valyi <rvalyi@akretion.com>
2022-05-04 20:29:36 -0300

    [REF] xsdata-odoo l10n_br_nfe_spec mixins
    export XSDATA_SCHEMA=nfe; export XSDATA_VERSION=40; export XSDATA_SKIP="^ICMS.ICMS\d+|^ICMS.ICMSSN\d+"
    xsdata generate /tmp/nfelib/nfelib/schemas/nfe/v4_0 --package nfelib.odoo.nfe.v4_0 --output=odoo
    mv nfelib/odoo/nfe/v4_0 l10n_br_nfe_spec/models/v4_0
    (na verdade foram conservado apenas os arquivos significativos/uteis e o
    __init__.py tambem foi esvaziado)

--- l10n_br_nfe_spec/models/spec_models.py
+++ l10n_br_nfe_spec/models/spec_models.py
@@ -11,8 +11,8 @@ class NfeSpecMixin(models.AbstractModel):
     _schema_name = "nfe"
     _schema_version = "4.0.0"
     _odoo_module = "l10n_br_nfe"
-    _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_00.leiauteNFe"
-    _binding_module = "nfelib.v4_00.retEnviNFe"
+    _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
+    _binding_module = "nfelib.nfe.v4_0.leiaute_nfe_v4_00"
     _spec_tab_name = "NFe"
 
     brl_currency_id = fields.Many2one(
