476f9c7e6c9eefb4b7083334ebd9a6f3fbb49d8d
SandipVyas-SerpentCS <sandip.v.serpentcs@gmail.com>
2022-05-27 21:32:22 +0530

    [14.0][IMP] Added property reservation related implementation for Brazil country.

---
+++ l10n_br_sale/models/res_config_settings.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2014  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    copy_note = fields.Boolean(
+        string="Copy notes in Fiscal documents",
+        related="company_id.copy_note",
+        readonly=False,
+    )
+
+    delivery_costs = fields.Selection(
+        related="company_id.delivery_costs", readonly=False
+    )
