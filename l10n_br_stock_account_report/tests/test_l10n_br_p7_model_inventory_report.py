4c9bdd62f89e7d55318c1c691ecdec1363f426e0
Renato Lima <renato.lima@akretion.com>
2022-06-02 10:48:50 -0300

    l10n_br_stock_account_report: pre-commit after pre-commit version upgrade

--- l10n_br_stock_account_report/tests/test_l10n_br_p7_model_inventory_report.py
+++ l10n_br_stock_account_report/tests/test_l10n_br_p7_model_inventory_report.py
@@ -14,7 +14,7 @@ class L10nBRP7ModelInventoryReportTest(SavepointCase):
         cls.p7_report_wizard = cls.env["l10n_br.p7.model.inventory.report.wizard"]
 
     def test_l10n_br_p7_model_inventory_report(self):
-        """ Test Brazilian P7 Model Inventory Report."""
+        """Test Brazilian P7 Model Inventory Report."""
 
         wizard_obj = self.p7_report_wizard.with_context(
             default_compute_at_date=0,
