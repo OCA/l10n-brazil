da6f032f39b891ae21cd81fb39eba178e4cff32f
Renato Lima <renato.lima@akretion.com>
2022-06-04 23:58:12 -0300

    [MIG] l10n_br_sale_stock: Migration to 14.0

--- l10n_br_sale_stock/__manifest__.py
+++ l10n_br_sale_stock/__manifest__.py
@@ -8,7 +8,7 @@
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "maintainers": ["renatonlima", "mbcosta"],
     "depends": [
         "sale_stock",
