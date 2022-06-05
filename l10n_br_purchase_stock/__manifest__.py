f10f06185cd63c3caa9e8b64489fa40da2693461
Renato Lima <renato.lima@akretion.com>
2022-06-05 00:19:47 -0300

    [MIG] l10n_br_purchase_stock: Migration to 14.0

--- l10n_br_purchase_stock/__manifest__.py
+++ l10n_br_purchase_stock/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Localisation",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": [
         "l10n_br_purchase",
         "l10n_br_stock_account",
