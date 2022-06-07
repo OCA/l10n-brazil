3ed4fd4c9d55d85de9645d451c4448568c248dcb
Magno Costa <magno.costa@akretion.com.br>
2022-06-07 12:39:15 -0300

    [REF] l10n_br_stock: Included script migration to rename field Inscrição Estadual.

--- l10n_br_stock/__manifest__.py
+++ l10n_br_stock/__manifest__.py
@@ -4,7 +4,7 @@
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "depends": ["stock", "l10n_br_base"],
     "data": ["views/stock_view.xml"],
     "demo": [
