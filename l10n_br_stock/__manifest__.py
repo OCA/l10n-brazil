75cb7f0b4fedc4800f639638ddbe0001578b96a8
Magno Costa <magno.costa@akretion.com.br>
2022-06-07 12:45:22 -0300

    [REF] l10n_br_stock: Standardized name files.

--- l10n_br_stock/__manifest__.py
+++ l10n_br_stock/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://github.com/OCA/l10n-brazil",
     "version": "14.0.2.0.0",
     "depends": ["stock", "l10n_br_base"],
-    "data": ["views/stock_view.xml"],
+    "data": ["views/stock_picking_view.xml"],
     "demo": [
         "demo/res_users_demo.xml",
         "demo/stock_location_demo.xml",
