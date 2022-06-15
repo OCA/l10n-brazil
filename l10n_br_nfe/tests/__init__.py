e48a4cef8517dfa331ee3fe68fd1ae1798975102
Renan Hiroki Bastos <hirokibastos@gmail.com>
2022-06-15 12:38:11 -0300

    [ADD] Added tests and run pre-commit

--- l10n_br_nfe/tests/__init__.py
+++ l10n_br_nfe/tests/__init__.py
@@ -5,6 +5,7 @@
 # from . import test_nfe_export
 from . import test_nfe_structure
 from . import test_nfe_import
+from . import test_nfe_import_wizard
 from . import test_nfe_serialize
 from . import test_nfe_serialize_lc
 from . import test_nfe_serialize_sn
