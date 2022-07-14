c09e78502b84412eeb6ea363331b7e0dc918665c
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-07-14 15:41:15 -0300

    [MIG] l10n_br_delivery: Migration to 14.0

--- l10n_br_delivery/models/stock_picking.py
+++ l10n_br_delivery/models/stock_picking.py
@@ -1,14 +1,13 @@
 # Copyright 2020 KMEE INFORMATICA LTDA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class StockPicking(models.Model):
 
     _inherit = "stock.picking"
 
-    @api.multi
     def _add_delivery_cost_to_so(self):
         # disable this function since, if called, adds a delivery line to
         # order -> strategy no longer used, view amount_freight
