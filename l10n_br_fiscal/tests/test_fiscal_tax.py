1a161aa62c84c2612c3f86f625fc98fa71bc257a
Renato Lima <renato.lima@akretion.com>
2022-08-01 12:19:58 -0300

    [ADD] fiscal tax compute method tests

--- l10n_br_fiscal/tests/test_fiscal_tax.py
+++ l10n_br_fiscal/tests/test_fiscal_tax.py
@@ -3,23 +3,294 @@
 
 from odoo.tests import common
 
+from ..constants.fiscal import FINAL_CUSTOMER_NO, FINAL_CUSTOMER_YES
+from ..constants.icms import ICMS_ORIGIN_DEFAULT
+
 
 class TestFiscalTax(common.TransactionCase):
     def setUp(self):
         super().setUp()
 
-        self.company_lucro_presumido = self.env.ref(
-            "l10n_br_base.empresa_lucro_presumido"
+    def test_compute_taxes_01(self):
+        """Testa o calculo dos impostos venda para pessoa física"""
+
+        kwargs = {
+            "company": self.env.ref("l10n_br_base.empresa_lucro_presumido"),
+            "partner": self.env.ref("l10n_br_base.res_partner_cliente5_pe"),
+            "product": self.env.ref("product.product_product_12"),
+            "price_unit": 3.143539,
+            "quantity": 11.000,
+            "uom_id": self.env.ref("uom.product_uom_unit"),
+            "fiscal_price": 3.143539,
+            "fiscal_quantity": 11.000,
+            "uot_id": self.env.ref("uom.product_uom_unit"),
+            "discount_value": 0.00,
+            "insurance_value": 0.00,
+            "other_value": 0.00,
+            "freight_value": 0.00,
+            "ii_customhouse_charges": 0.00,
+            "ii_iof_value": 0.00,
+            "ncm": self.env.ref("l10n_br_fiscal.ncm_72132000"),
+            "nbs": False,
+            "nbm": False,
+            "cest": False,
+            "operation_line": self.env.ref("l10n_br_fiscal.fo_venda_venda"),
+            "cfop": self.env.ref("l10n_br_fiscal.cfop_6101"),
+            "icmssn_range": False,
+            "icms_origin": ICMS_ORIGIN_DEFAULT,
+            "ind_final": FINAL_CUSTOMER_YES,
+        }
+
+        fiscal_taxes = self.env["l10n_br_fiscal.tax"]
+        fiscal_taxes |= (
+            self.env.ref("l10n_br_fiscal.tax_icms_7")
+            + self.env.ref("l10n_br_fiscal.tax_ipi_15")
+            + self.env.ref("l10n_br_fiscal.tax_pis_0_65")
+            + self.env.ref("l10n_br_fiscal.tax_cofins_3")
         )
-        self.company_simples = self.env.ref("l10n_br_base.empresa_simples_nacional")
 
-        self._switch_user_company(self.env.user, self.company_lucro_presumido)
+        compute_result = fiscal_taxes.compute_taxes(**kwargs)
 
-    def _switch_user_company(self, user, company):
-        """Add a company to the user's allowed & set to current."""
-        user.write(
-            {
-                "company_ids": [(6, 0, (company + user.company_ids).ids)],
-                "company_id": company.id,
+        test_results = {
+            "taxes": {
+                "ipi": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 15.0,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 5.19,
+                },
+                "icms": {
+                    "base": 39.77,
+                    "base_reduction": 0.0,
+                    "percent_amount": 7.0,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 2.78,
+                    "add_to_base": 5.19,
+                },
+                "pis": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 0.65,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 0.22,
+                },
+                "cofins": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 3.0,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 1.04,
+                },
             }
+        }
+
+        # ICMS
+        self.assertEqual(
+            compute_result["taxes"]["icms"]["base"],
+            test_results["taxes"]["icms"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["icms"]["percent_amount"],
+            test_results["taxes"]["icms"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["icms"]["tax_value"],
+            test_results["taxes"]["icms"]["tax_value"],
+        )
+
+        # IPI
+        self.assertEqual(
+            compute_result["taxes"]["ipi"]["base"],
+            test_results["taxes"]["ipi"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["ipi"]["percent_amount"],
+            test_results["taxes"]["ipi"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["ipi"]["tax_value"],
+            test_results["taxes"]["ipi"]["tax_value"],
+        )
+
+        # PIS
+        self.assertEqual(
+            compute_result["taxes"]["pis"]["base"],
+            test_results["taxes"]["pis"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["pis"]["percent_amount"],
+            test_results["taxes"]["pis"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["pis"]["tax_value"],
+            test_results["taxes"]["pis"]["tax_value"],
+        )
+
+        # COFINS
+        self.assertEqual(
+            compute_result["taxes"]["cofins"]["base"],
+            test_results["taxes"]["cofins"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["cofins"]["percent_amount"],
+            test_results["taxes"]["cofins"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["cofins"]["tax_value"],
+            test_results["taxes"]["cofins"]["tax_value"],
+        )
+
+    def test_compute_taxes_02(self):
+        """Testa o calculo dos impostos venda para pessoa física"""
+
+        kwargs = {
+            "company": self.env.ref("l10n_br_base.empresa_lucro_presumido"),
+            "partner": self.env.ref("l10n_br_base.res_partner_cliente5_pe"),
+            "product": self.env.ref("product.product_product_12"),
+            "price_unit": 3.143539,
+            "quantity": 11.000,
+            "uom_id": self.env.ref("uom.product_uom_unit"),
+            "fiscal_price": 3.143539,
+            "fiscal_quantity": 11.000,
+            "uot_id": self.env.ref("uom.product_uom_unit"),
+            "discount_value": 0.00,
+            "insurance_value": 0.00,
+            "other_value": 0.00,
+            "freight_value": 0.00,
+            "ii_customhouse_charges": 0.00,
+            "ii_iof_value": 0.00,
+            "ncm": self.env.ref("l10n_br_fiscal.ncm_72132000"),
+            "nbs": False,
+            "nbm": False,
+            "cest": False,
+            "operation_line": self.env.ref("l10n_br_fiscal.fo_venda_venda"),
+            "cfop": self.env.ref("l10n_br_fiscal.cfop_6101"),
+            "icmssn_range": False,
+            "icms_origin": ICMS_ORIGIN_DEFAULT,
+            "ind_final": FINAL_CUSTOMER_NO,
+        }
+
+        fiscal_taxes = self.env["l10n_br_fiscal.tax"]
+        fiscal_taxes |= (
+            self.env.ref("l10n_br_fiscal.tax_icms_7")
+            + self.env.ref("l10n_br_fiscal.tax_ipi_15")
+            + self.env.ref("l10n_br_fiscal.tax_pis_0_65")
+            + self.env.ref("l10n_br_fiscal.tax_cofins_3")
+        )
+
+        compute_result = fiscal_taxes.compute_taxes(**kwargs)
+
+        test_results = {
+            "taxes": {
+                "ipi": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 15.0,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 5.19,
+                },
+                "icms": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 7.0,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 2.42,
+                    "add_to_base": 0.0,
+                },
+                "pis": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 0.65,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 0.22,
+                },
+                "cofins": {
+                    "base": 34.58,
+                    "base_reduction": 0.0,
+                    "percent_amount": 3.0,
+                    "percent_reduction": 0.0,
+                    "value_amount": 0.0,
+                    "tax_value": 1.04,
+                },
+            }
+        }
+
+        # ICMS
+        self.assertEqual(
+            compute_result["taxes"]["icms"]["base"],
+            test_results["taxes"]["icms"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["icms"]["percent_amount"],
+            test_results["taxes"]["icms"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["icms"]["tax_value"],
+            test_results["taxes"]["icms"]["tax_value"],
+        )
+
+        # IPI
+        self.assertEqual(
+            compute_result["taxes"]["ipi"]["base"],
+            test_results["taxes"]["ipi"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["ipi"]["percent_amount"],
+            test_results["taxes"]["ipi"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["ipi"]["tax_value"],
+            test_results["taxes"]["ipi"]["tax_value"],
+        )
+
+        # PIS
+        self.assertEqual(
+            compute_result["taxes"]["pis"]["base"],
+            test_results["taxes"]["pis"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["pis"]["percent_amount"],
+            test_results["taxes"]["pis"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["pis"]["tax_value"],
+            test_results["taxes"]["pis"]["tax_value"],
+        )
+
+        # COFINS
+        self.assertEqual(
+            compute_result["taxes"]["cofins"]["base"],
+            test_results["taxes"]["cofins"]["base"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["cofins"]["percent_amount"],
+            test_results["taxes"]["cofins"]["percent_amount"],
+        )
+
+        self.assertEqual(
+            compute_result["taxes"]["cofins"]["tax_value"],
+            test_results["taxes"]["cofins"]["tax_value"],
         )
