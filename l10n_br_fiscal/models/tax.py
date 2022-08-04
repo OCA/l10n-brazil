a8b7a31980e4dba08d4a1a162082881a611c4d3b
Renato Lima <renato.lima@akretion.com>
2022-08-04 16:33:31 -0300

    [FIX] add_to_base def

--- l10n_br_fiscal/models/tax.py
+++ l10n_br_fiscal/models/tax.py
@@ -475,20 +475,14 @@ class Tax(models.Model):
         return self._compute_tax(tax, taxes_dict, **kwargs)
 
     def _compute_icmsst(self, tax, taxes_dict, **kwargs):
+        tax_dict = taxes_dict.get(tax.tax_domain)
         # partner = kwargs.get("partner")
         # company = kwargs.get("company")
-        add_to_base = []
 
         # Get Computed IPI Tax
         tax_dict_ipi = taxes_dict.get("ipi", {})
-        add_to_base.append(tax_dict_ipi.get("tax_value", 0.00))
+        tax_dict["add_to_base"] += tax_dict_ipi.get("tax_value", 0.00)
 
-        kwargs.update(
-            {
-                "add_to_base": sum(add_to_base),
-                "icmsst_base_type": tax.icmsst_base_type,
-            }
-        )
         if taxes_dict.get(tax.tax_domain):
             taxes_dict[tax.tax_domain]["icmsst_mva_percent"] = tax.icmsst_mva_percent
 
@@ -509,15 +503,13 @@ class Tax(models.Model):
         cst = kwargs.get("cst", self.env["l10n_br_fiscal.cst"])
         icmssn_range = kwargs.get("icmssn_range")
 
-        add_to_base = []
-
         # Get Computed IPI Tax
         tax_dict_ipi = taxes_dict.get("ipi", {})
 
         # Partner not ICMS's Contributor
         if partner.ind_ie_dest == NFE_IND_IE_DEST_9:
             # Add IPI in ICMS Base
-            add_to_base.append(tax_dict_ipi.get("tax_value", 0.00))
+            tax_dict["add_to_base"] += tax_dict_ipi.get("tax_value", 0.00)
 
         # Partner ICMS's Contributor
         if partner.ind_ie_dest in (NFE_IND_IE_DEST_1, NFE_IND_IE_DEST_2):
@@ -530,12 +522,6 @@ class Tax(models.Model):
 
                 tax_dict["percent_amount"] = icms_sn_percent
                 tax_dict["value_amount"] = icms_sn_percent
-
-        kwargs.update(
-            {
-                "add_to_base": sum(add_to_base),
-            }
-        )
 
         taxes_dict.update(
             self._compute_tax_base(tax, taxes_dict.get(tax.tax_domain), **kwargs)
