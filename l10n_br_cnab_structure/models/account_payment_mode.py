2e222dbdadcaa72b45b75fa19f303667cccf615f
Neto <netosjb@yahoo.com.br>
2022-08-16 22:06:08 -0300

    [IMP] add cnab structure field in payment mode

--- l10n_br_cnab_structure/models/account_payment_mode.py
+++ l10n_br_cnab_structure/models/account_payment_mode.py
@@ -5,11 +5,11 @@ class AccountPaymentMode(models.Model):
 
     _inherit = "account.payment.mode"
 
-    cnab_file_id = fields.Many2one(
-        comodel_name="l10n_br_cnab.file",
+    cnab_structure_id = fields.Many2one(
+        comodel_name="l10n_br_cnab.structure",
     )
 
     cnab_batch_id = fields.Many2one(
         comodel_name="l10n_br_cnab.batch",
-        domain="[('cnab_file_id', '=', cnab_file_id)]",
+        domain="[('cnab_structure_id', '=', cnab_structure_id)]",
     )
