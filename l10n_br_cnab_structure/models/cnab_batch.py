9efa90fb3a45d36c64abde71deddb7b46ad2d158
Neto <netosjb@yahoo.com.br>
2022-08-20 22:51:06 -0300

    [IMP] adds the batch_line so that it can be referenced in the dynamic content.

--- l10n_br_cnab_structure/models/cnab_batch.py
+++ l10n_br_cnab_structure/models/cnab_batch.py
@@ -44,7 +44,9 @@ class CNABBatch(models.Model):
         batch_lines = []
         record_sequence = 0
         batch_lines.append(
-            self.get_header().output(pay_order, seq_batch=batch_sequence)
+            self.get_header().output(
+                pay_order, seq_batch=batch_sequence, batch_lines=batch_lines
+            )
         )
         for bank_line in pay_order.bank_line_ids:
             for segment in self.get_segments():
@@ -54,13 +56,15 @@ class CNABBatch(models.Model):
                         bank_line,
                         seq_batch=batch_sequence,
                         seq_batch_record=record_sequence,
+                        batch_lines=batch_lines,
                     )
                 )
         batch_lines.append(
             self.get_trailer().output(
                 pay_order,
                 seq_batch=batch_sequence,
-                qty_batch_record=len(batch_lines) + 1,
+                qty_batch_records=len(batch_lines) + 1,
+                batch_lines=batch_lines,
             )
         )
         return batch_lines
