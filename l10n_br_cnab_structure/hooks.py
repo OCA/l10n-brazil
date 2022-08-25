277267b28b55f818644a4b9ab1e4e38ea4c08bed
Neto <netosjb@yahoo.com.br>
2022-08-25 20:26:51 -0300

    [FIX] load demo

--- l10n_br_cnab_structure/hooks.py
+++ l10n_br_cnab_structure/hooks.py
@@ -28,3 +28,21 @@ def post_init_hook(cr, registry):
             noupdate=True,
             kind="init",
         )
+
+    if not tools.config["without_demo"]:
+        demofiles = [
+            "demo/payment_mode.xml",
+        ]
+
+        _logger.info(_("Loading l10n_br_cnab_structure demo files."))
+
+        for file in demofiles:
+            tools.convert_file(
+                cr,
+                "l10n_br_cnab_structure",
+                file,
+                None,
+                mode="init",
+                noupdate=True,
+                kind="demo",
+            )
