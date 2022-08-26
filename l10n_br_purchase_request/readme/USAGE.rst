9321e0b97f299f95643086c960b5f12dcf3fd3a5
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-26 14:42:14 -0300

    [ADD] l10n_br_purchase_request

---
+++ l10n_br_purchase_request/readme/USAGE.rst
@@ -0,0 +1,6 @@
+To use this module, you need to:
+
+#. Go to Purchase Request
+#. Create or select a Purchase Request
+#. Approve the Purchase Request
+#. Create RFQ
---
+++ l10n_br_purchase_request/wizards/purchase_request_line_make_purchase_order.py
@@ -0,0 +1,20 @@
+# Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
+
+    _inherit = "purchase.request.line.make.purchase.order"
+
+    @api.multi
+    def make_purchase_order(self):
+        res = super().make_purchase_order()
+        purchases = self.env["purchase.order"].search(res.get("domain"))
+        for purchase in purchases:
+            if purchase.fiscal_operation_id:
+                for line in purchase.order_line:
+                    line._onchange_product_id_fiscal()
+                    line._onchange_fiscal_tax_ids()
+        return res
