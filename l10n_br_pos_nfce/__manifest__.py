e5b300f01a021db66ef20b4092cd9bd24b44241e
Luis Felipe Mileo <mileo@kmee.com.br>
2022-08-28 20:30:54 -0300

    [NEW] l10n_br_pos_nfce module scaffold
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ l10n_br_pos_nfce/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Pos Nfce",
+    "summary": """
+        NFC-E no Ponto de Venda""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "development_status": "Alpha",
+    "maintainers": ["mileo", "sadamo", "gabrielcardoso21", "lfdivino"],
+    "depends": [
+        "l10n_br_pos",
+    ],
+    "data": [
+        "views/pos_template.xml",
+    ],
+    "demo": [],
+    "installable": True,
+}
