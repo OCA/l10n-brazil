d77e7088f2eb063ac174ca9f1429a05b8421f55f
Luis Felipe Mileo <mileo@kmee.com.br>
2022-08-28 20:30:54 -0300

    [NEW] l10n_br_pos_nfce module scaffold
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ l10n_br_pos_nfce/static/src/js/db.js
@@ -0,0 +1,10 @@
+/*
+Copyright (C) 2022-Today KMEE (https://kmee.com.br)
+@author: Luis Felipe Mileo <mileo@kmee.com.br>
+ License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+odoo.define("l10n_br_pos.db", function () {
+    "use strict";
+
+});
