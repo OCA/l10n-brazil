a10417713e3ceeb36448d76af762722586e0e12a
Luis Felipe Mileo <mileo@kmee.com.br>
2022-08-28 20:30:11 -0300

    [NEW] l10n_br_pos_cfe module scaffold
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ l10n_br_pos_cfe/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Pos Cfe",
+    "summary": """CF-e""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "category": "Point Of Sale",
+    "development_status": "Alpha",
+    "maintainers": ["mileo", "sadamo", "gabrielcardoso21", "lfdivino"],
+    "depends": [
+        "l10n_br_pos",
+    ],
+    "data": [
+    ],
+    "demo": [],
+    "installable": True,
+}
