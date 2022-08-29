64aaf3274099638c8cd2a686a3f522ab994cdf1d
Luis Felipe Mileo <mileo@kmee.com.br>
2022-08-29 09:43:59 -0300

    [ADD] NFC-e constant [MOV] FISCAL_PAYMENT_MODE
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_fiscal/constants/fiscal.py
+++ l10n_br_fiscal/constants/fiscal.py
@@ -296,6 +296,7 @@ NFE_IND_PRES = [
 
 
 NFE_IND_PRES_DEFAULT = "0"
+NFCE_IND_PRES_DEFAULT = "1"
 
 FINAL_CUSTOMER = [("0", "Não"), ("1", "Sim")]
 
@@ -501,4 +502,24 @@ EVENT_ENV_HML = "hml"
 EVENT_ENVIRONMENT = [
     (EVENT_ENV_PROD, "Production"),
     (EVENT_ENV_HML, "Homologation"),
+]
+
+FISCAL_PAYMENT_MODE = [
+    ("01", "01 - Dinheiro"),
+    ("02", "02 - Cheque"),
+    ("03", "03 - Cartão de Crédito"),
+    ("04", "04 - Cartão de Débito"),
+    ("05", "05 - Crédito de Loja"),
+    ("10", "10 - Vale Alimentação"),
+    ("11", "11 - Vale Refeição"),
+    ("12", "12 - Vale Presente"),
+    ("13", "13 - Vale Combustível"),
+    ("14", "14 - Duplicata Mercanti"),
+    ("15", "15 - Boleto Bancário"),
+    ("16", "16 - Depósito Bancário"),
+    ("17", "17 - Pagamento Instantâneo (PIX)"),
+    ("18", "18 - Transferência bancária, Carteira Digital"),
+    ("19", "19 - Programa de fidelidade, Cashback, Crédito Virtual"),
+    ("90", "90 - Sem Pagamento"),
+    ("99", "99 - Outros"),
 ]
