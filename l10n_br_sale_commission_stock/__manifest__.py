394936699b305f203ece871fd11fea4f29821205
Renato Lima <renato.lima@akretion.com>
2022-08-31 19:16:10 -0300

    [ADD] new module l10n_br_sale_commission_stock

---
+++ l10n_br_sale_commission_stock/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Brazilian Localization Sales Commission and Warehouse",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "version": "12.0.1.0.0",
+    "maintainers": ["renatonlima"],
+    "depends": [
+        "l10n_br_sale_stock",
+        "l10n_br_sale_commission",
+    ],
+    "demo": [
+        "demo/sale_order.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
