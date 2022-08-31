22aa85add881f2d3f2ba85b70b03a17204f238d4
Magno Costa <magno.costa@akretion.com.br>
2022-08-31 14:17:02 -0300

    [MIG] l10n_br_stock_account: Create specific Journals to Simples Remessa and Entrada Remessa.

--- l10n_br_stock_account/hooks.py
+++ l10n_br_stock_account/hooks.py
@@ -13,6 +13,32 @@ def post_init_hook(cr, registry):
         company = env.ref(
             "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
         )
+
+        # Caso o modulo l10n_br_purcase ou l10n_br_sale esteja instalado,
+        # o que acontece na forma que o Travis faz os testes, acontece o
+        # erro abaixo:
+        #   File "/odoo/src/odoo/sql_db.py", line 300, in execute
+        #     res = self._obj.execute(query, params)
+        # psycopg2.IntegrityError: duplicate key value violates
+        # unique constraint "ir_property_unique_index"
+        # DETAIL:  Key (fields_id, COALESCE(company_id, 0),
+        # COALESCE(res_id, ''::character varying))=(13446, 1,
+        # l10n_br_fiscal.operation,6) already exists.
+        # TODO: teria uma solução melhor?
+        l10n_br_sale_or_purchase_installed = False
+        if env["ir.module.module"].search_count(
+            [
+                ("name", "=", "l10n_br_purchase"),
+                ("state", "=", "installed"),
+            ]
+        ) or env["ir.module.module"].search_count(
+            [
+                ("name", "=", "l10n_br_sale"),
+                ("state", "=", "installed"),
+            ]
+        ):
+            l10n_br_sale_or_purchase_installed = True
+
         # COA Simple Fiscal Operation properties
         if company and env["ir.module.module"].search_count(
             [
@@ -20,15 +46,35 @@ def post_init_hook(cr, registry):
                 ("state", "=", "installed"),
             ]
         ):
-            tools.convert_file(
-                cr,
-                "l10n_br_stock_account",
-                "demo/fiscal_operation_simple.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
-            )
+            if not l10n_br_sale_or_purchase_installed:
+                tools.convert_file(
+                    cr,
+                    "l10n_br_stock_account",
+                    "demo/fiscal_operation_simple.xml",
+                    None,
+                    mode="init",
+                    noupdate=True,
+                    kind="init",
+                )
+            else:
+                env.ref("l10n_br_fiscal.fo_simples_remessa")
+                data_op_fiscal = "l10n_br_fiscal.operation," + str(
+                    env.ref("l10n_br_fiscal.fo_simples_remessa").id
+                )
+                main_company = env.ref("base.main_company", raise_if_not_found=False)
+                simples_remessa_main_company = env["ir.property"].search(
+                    [
+                        ("res_id", "=", data_op_fiscal),
+                        ("company_id", "=", main_company.id),
+                    ]
+                )
+
+                data_journal = "account.journal," + str(
+                    env.ref(
+                        "l10n_br_stock_account.simples_remessa_journal_main_company"
+                    ).id
+                )
+                simples_remessa_main_company.value_reference = data_journal
 
         company_lc = env.ref(
             "l10n_br_base.empresa_lucro_presumido", raise_if_not_found=False
@@ -41,12 +87,31 @@ def post_init_hook(cr, registry):
                 ("state", "=", "installed"),
             ]
         ):
-            tools.convert_file(
-                cr,
-                "l10n_br_stock_account",
-                "demo/fiscal_operation_generic.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
-            )
+            if not l10n_br_sale_or_purchase_installed:
+                tools.convert_file(
+                    cr,
+                    "l10n_br_stock_account",
+                    "demo/fiscal_operation_generic.xml",
+                    None,
+                    mode="init",
+                    noupdate=True,
+                    kind="init",
+                )
+            else:
+                data_op_fiscal = "l10n_br_fiscal.operation," + str(
+                    env.ref("l10n_br_fiscal.fo_simples_remessa").id
+                )
+                simples_remessa_company_lc = env["ir.property"].search(
+                    [
+                        ("res_id", "=", data_op_fiscal),
+                        ("company_id", "=", company_lc.id),
+                    ]
+                )
+
+                data_journal = "account.journal," + str(
+                    env.ref(
+                        "l10n_br_stock_account.simples_remessa_journal_lucro_presumido"
+                    ).id
+                )
+
+                simples_remessa_company_lc.value_reference = data_journal
