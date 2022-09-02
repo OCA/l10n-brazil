e113160ebe4215af10b22d2b21b58892cee0d636
Neto <netosjb@yahoo.com.br>
2022-09-01 21:56:21 -0300

    [IMP] add cnab payment way

---
+++ l10n_br_cnab_structure/models/cnab_payment_way.py
@@ -0,0 +1,32 @@
+# Copyright 2022 Engenere
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import _, api, fields, models
+
+
+class CNABPaymentWay(models.Model):
+    """CNAB Payment Way Code"""
+
+    _name = "cnab.payment.way"
+    _description = "CNAB Payment Way Code"
+
+    name = fields.Char(compute="_compute_name")
+    code = fields.Char(required=True)
+    description = fields.Char()
+
+    cnab_structure_id = fields.Many2one(
+        comodel_name="l10n_br_cnab.structure",
+        string="Cnab Structure",
+        ondelete="cascade",
+        required=True,
+    )
+
+    batch_id = fields.Many2one(
+        comodel_name="l10n_br_cnab.batch",
+        string="Cnab Batch",
+    )
+
+    @api.depends("code", "name")
+    def _compute_name(self):
+        for rec in self:
+            rec.name = f"{rec.code} - {rec.description}"
