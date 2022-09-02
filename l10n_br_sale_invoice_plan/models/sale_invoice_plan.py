413f6af2edfd85955304b13f8e8e31d7e9ac0ab6
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-09-02 17:02:51 -0300

    [MIG] l10n_br_sale_invoice_plan: Migration to 14.0

--- l10n_br_sale_invoice_plan/models/sale_invoice_plan.py
+++ l10n_br_sale_invoice_plan/models/sale_invoice_plan.py
@@ -1,20 +1,19 @@
 # Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleInvoicePlan(models.Model):
 
     _inherit = "sale.invoice.plan"
 
-    @api.multi
-    def _compute_new_invoice_quantity(self, invoice):
+    def _compute_new_invoice_quantity(self, invoice_move):
 
-        super()._compute_new_invoice_quantity(invoice=invoice)
+        super()._compute_new_invoice_quantity(invoice_move=invoice_move)
 
-        if invoice:
-            for line in invoice.invoice_line_ids:
+        if invoice_move:
+            for line in invoice_move.invoice_line_ids:
                 line.write({"fiscal_quantity": line.quantity})
                 line._onchange_fiscal_tax_ids()
-            invoice._onchange_invoice_line_ids()
+            invoice_move._onchange_invoice_line_ids()
