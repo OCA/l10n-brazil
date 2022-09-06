b92d16df50523639b1fa8715ee8efd44646b8ab8
Luis Felipe Mileo <mileo@kmee.com.br>
2022-09-06 16:21:05 -0300

    [IMP] Pos Product Fiscal Map
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_pos/models/product_template.py
+++ l10n_br_pos/models/product_template.py
@@ -1,7 +1,7 @@
 # © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 # from odoo.addons.queue_job.job import job
 
@@ -16,10 +16,11 @@ class ProductTemplate(models.Model):
     )
 
     # @job
+    @api.depends_context('company')
     def update_pos_fiscal_map(self):
         for record in self:
             pos_config_ids = record.env["pos.config"].search(
-                [("company_id", "=", self.env.user.company_id.id)]
+                [("company_id", "=", self.env.company.id)]
             )
             with_maps_pos_config_id = record.pos_fiscal_map_ids.mapped("pos_config_id")
             to_create_ids = pos_config_ids - with_maps_pos_config_id
@@ -33,26 +34,24 @@ class ProductTemplate(models.Model):
                         "pos_config_id": pos_config_id.id,
                         "product_tmpl_id": record.id,
                         "partner_id": pos_config_id.partner_id.id,
+                        "company_id": self.env.company.id
                     }
                 )
 
                 pos_fiscal_map_id._onchange_product_id_fiscal()
-                pos_fiscal_map_id._onchange_icms_regulation_id()
+                # pos_fiscal_map_id._onchange_icms_regulation_id()
                 pos_fiscal_map_id._onchange_fiscal_operation_id()
                 pos_fiscal_map_id._onchange_fiscal_operation_line_id()
                 pos_fiscal_map_id._onchange_fiscal_taxes()
 
-    #
-    #
-    # @api.model
-    # def create(self, vals):
-    #     product_id = super().create(vals)
-    #     if vals.get("available_in_pos"):
-    #         product_id.with_delay().update_pos_fiscal_map()
-    #     return product_id
-    #
-    # @api.multi
-    # def write(self, vals):
-    #     if vals.get("available_in_pos"):
-    #         self.with_delay().update_pos_fiscal_map()
-    #     return super().write(vals)
+    @api.model
+    def create(self, vals):
+        product_id = super().create(vals)
+        if vals.get("available_in_pos"):
+            product_id.update_pos_fiscal_map()
+        return product_id
+
+    def write(self, vals):
+        if vals.get("available_in_pos"):
+            self.update_pos_fiscal_map()
+        return super().write(vals)
