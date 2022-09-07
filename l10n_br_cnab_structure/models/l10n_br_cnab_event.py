d000271ddd7740511c2b65149d5a4c0c94ce91ef
Neto <netosjb@yahoo.com.br>
2022-09-06 22:42:29 -0300

    [REF] cnab: your_number and map bank_payment_line_id

--- l10n_br_cnab_structure/models/l10n_br_cnab_event.py
+++ l10n_br_cnab_structure/models/l10n_br_cnab_event.py
@@ -1,25 +1,36 @@
 # Copyright 2022 Engenere
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import _, fields, models
+from odoo import _, fields, models, api
 
 
 class CNABReturnEvent(models.Model):
 
     _inherit = "l10n_br_cnab.return.event"
 
-    bank_line_ref = fields.Char(
-        string="Bank Line Reference",
-        compute="_compute_bank_line_ref",
-        inverse="_inverse_bank_line_ref",
-    )
-
-    def _compute_bank_line_ref(self):
-        for rec in self:
-            rec.bank_line_ref = rec.bank_payment_line_id.name
-
-    def _inverse_bank_line_ref(self):
-        for rec in self:
-            rec.bank_payment_line_id = self.env["bank.payment.line"].search(
-                [("name", "=", rec.bank_line_ref)]
+    @api.model
+    def create(self, vals):
+        """
+        When for cnab outbound payment processed by this module the
+        bank_payment_line_id is filled based on your_number field
+        """
+        event = super().create(vals)
+        if not event.cnab_return_log_id.cnab_structure_id:
+            # if there is no cnab_structure_id it is because the return file is not being
+            # processed by this module, so there is nothing to do here.
+            return event
+        if event.cnab_return_log_id.type == "outbound":
+            bank_payment_line_id = self.env["bank.payment.line"].search(
+                [("name", "=", event.your_number)]
             )
+            event.bank_payment_line_id = bank_payment_line_id
+
+        # TODO:
+        # quando for cobrança temos que mapear o bank line pelo campo nosso numero e
+        # também pelo journal do banco. porém o bank line não tem journal, mapear depois.
+        # if  event.cnab_return_log_id.type == "inbound":
+        #     bank_payment_line_id = self.env["bank.payment.line"].search(
+        #         [("name", "=", event.own_numner)]
+        #     )
+        #     event.bank_payment_line_id = bank_payment_line_id
+        return event
