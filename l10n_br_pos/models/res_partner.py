7c624f741b6eaee4a52a0bbe3d675e60ce8df3c5
Luis Felipe Mileo <mileo@kmee.com.br>
2022-09-07 19:33:54 -0300

    [IMP] Partner creation from point of sale
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_pos/models/res_partner.py
+++ l10n_br_pos/models/res_partner.py
@@ -1,61 +1,35 @@
 # © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+import logging
+
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from erpbrasil.base.fiscal import cnpj_cpf
+except ImportError:
+    _logger.error("Biblioteca erpbrasil.base não instalada")
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    data_alteracao = fields.Date()
-    credit_funcionario = fields.Float("Crédito do Funcionário")
-
     @api.model
-    def get_credit_limit(self, partner):
-        partner_id = self.browse(partner)
-        return partner_id.credit_limit
+    def create_from_ui(self, partner):
+        if partner.get("vat") and cnpj_cpf.validar_cpf(partner["vat"]):
+            partner["cnpj_cpf"] = cnpj_cpf.formata(partner["vat"])
+            partner["company_type"] = "person"
+        elif partner.get("vat") and cnpj_cpf.validar_cnpj(partner["vat"]):
+            partner["cnpj_cpf"] = cnpj_cpf.formata(partner["vat"])
+            partner["company_type"] = "company"
 
-    def _mask_cnpj_cpf(self, cpfcnpj_type, cnpj_cpf):
-        if cnpj_cpf:
-            if cpfcnpj_type == "cnpj" and len(cnpj_cpf) == 14:
-                cnpj_cpf = "%s.%s.%s/%s-%s" % (
-                    cnpj_cpf[0:2],
-                    cnpj_cpf[2:5],
-                    cnpj_cpf[5:8],
-                    cnpj_cpf[8:12],
-                    cnpj_cpf[12:14],
-                )
-            elif cpfcnpj_type == "cpf" and len(cnpj_cpf) == 11:
-                cnpj_cpf = "%s.%s.%s-%s" % (
-                    cnpj_cpf[0:3],
-                    cnpj_cpf[3:6],
-                    cnpj_cpf[6:9],
-                    cnpj_cpf[9:11],
-                )
-        return cnpj_cpf
+        if partner.get("cnpj_cpf"):
+            partner.pop("vat")
 
-    # @api.model
-    # def create_from_ui(self, partner):
-    #     from erpbrasil.base import misc
+        if partner.get("name"):
+            partner["legal_name"] = partner["name"]
 
-    #     cnpj_cpf = misc.punctuation_rm(partner["vat"])
-    #     cnpj_cpf_type = "cpf" if len(cnpj_cpf) == 11 else "cnpj"
-    #     partner["data_alteracao"] = fields.Date.today()
-    #     partner["cnpj_cpf"] = self._mask_cnpj_cpf(cnpj_cpf_type, cnpj_cpf)
-    #     if partner.get("whatsapp") and partner.get("opt_out"):
-    #         partner["whatsapp"] = "sim" == partner["whatsapp"]
-    #         partner["opt_out"] = "sim" == partner["opt_out"]
-    #     else:
-    #         partner["whatsapp"] = False
-    #         partner["opt_out"] = True
-
-    #     res = super(ResPartner, self).create_from_ui(partner)
-    #     partner_id = self.browse(res)
-    #     partner_id.legal_name = partner["name"]
-    #     if partner_id.company_id:
-    #         partner_id.company_id = False
-    #     # if cnpj_cpf_type:
-    #     #     fiscal_type = self.env.ref('l10n_br_account.partner_fiscal_type_4')
-    #     #     partner_id.partner_fiscal_type_id = fiscal_type.id
-
-    #     return res
+        res = super(ResPartner, self).create_from_ui(partner)
+        return res
