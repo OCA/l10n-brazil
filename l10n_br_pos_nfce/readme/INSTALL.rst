77103455b0e111f90b2564dbe56904f28e81cb8b
Luis Felipe Mileo <mileo@kmee.com.br>
2022-09-08 15:20:30 -0300

    [NEW] README files
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ l10n_br_pos_nfce/readme/INSTALL.rst
@@ -0,0 +1,5 @@
+Esse módulo depende:
+
+* l10n_br_pos
+
+Alem disso você vi precisar configurar o Backend para emitir NFC-e.
--- l10n_br_pos_nfce/static/src/js/devices.js
+++ l10n_br_pos_nfce/static/src/js/devices.js
@@ -4,7 +4,6 @@ Copyright (C) 2022-Today KMEE (https://kmee.com.br)
  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-odoo.define("l10n_br_pos.devices", function (require) {
+odoo.define("l10n_br_pos.devices", function () {
     "use strict";
-    var devices = require("point_of_sale.devices");
 });
