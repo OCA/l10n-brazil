120580ce370bd95f3249a35def0b875e754d2921
Luis Felipe Mileo <mileo@kmee.com.br>
2022-09-08 15:20:30 -0300

    [NEW] README files
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_pos_nfce/static/src/js/screens.js
+++ l10n_br_pos_nfce/static/src/js/screens.js
@@ -4,7 +4,6 @@ Copyright (C) 2022-Today KMEE (https://kmee.com.br)
  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-odoo.define("l10n_br_pos.screens", function (require) {
+odoo.define("l10n_br_pos.screens", function () {
     "use strict";
-    var screens = require("point_of_sale.screens");
 });
