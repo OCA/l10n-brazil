6fead1de5c3d7733bf2f58d8abdc108e9cacefdc
Neto <netosjb@yahoo.com.br>
2022-09-08 22:31:31 -0300

    [IMP] cnab: add occurrence code in return map

--- l10n_br_cnab_structure/models/l10n_br_cnab_event.py
+++ l10n_br_cnab_structure/models/l10n_br_cnab_event.py
@@ -8,29 +8,62 @@ class CNABReturnEvent(models.Model):
 
     _inherit = "l10n_br_cnab.return.event"
 
+    occurrence_code_1 = fields.Char("")
+    occurrence_code_2 = fields.Char("")
+    occurrence_code_3 = fields.Char("")
+    occurrence_code_4 = fields.Char("")
+    occurrence_code_5 = fields.Char("")
+
     @api.model
     def create(self, vals):
-        """
-        When for cnab outbound payment processed by this module the
-        bank_payment_line_id is filled based on your_number field
-        """
+        """Override Create Method"""
         event = super().create(vals)
         if not event.cnab_return_log_id.cnab_structure_id:
             # if there is no cnab_structure_id it is because the return file is not being
             # processed by this module, so there is nothing to do here.
             return event
-        if event.cnab_return_log_id.type == "outbound":
-            bank_payment_line_id = self.env["bank.payment.line"].search(
-                [("name", "=", event.your_number)]
-            )
-            event.bank_payment_line_id = bank_payment_line_id
-
-        # TODO:
-        # quando for cobrança temos que mapear o bank line pelo campo nosso numero e
-        # também pelo journal do banco. porém o bank line não tem journal, mapear depois.
-        # if  event.cnab_return_log_id.type == "inbound":
-        #     bank_payment_line_id = self.env["bank.payment.line"].search(
-        #         [("name", "=", event.own_numner)]
-        #     )
-        #     event.bank_payment_line_id = bank_payment_line_id
+        event.load_bank_payment_line()
+        event.load_description_occurrences()
         return event
+
+    def load_bank_payment_line(self):
+        """
+        When for cnab outbound payment processed by this module the
+        bank_payment_line_id is filled based on your_number field
+        """
+        for event in self:
+            if event.cnab_return_log_id.type == "outbound":
+                bank_payment_line_id = self.env["bank.payment.line"].search(
+                    [("name", "=", event.your_number)]
+                )
+                event.bank_payment_line_id = bank_payment_line_id
+
+    def get_description_occurrence(self, event_code):
+        """Get occurrence description by occurrence code"""
+        occurrence_id = (
+            self.cnab_return_log_id.cnab_structure_id.cnab_occurrence_ids.filtered(
+                lambda a: a.code == event_code
+            )
+        )
+        if occurrence_id:
+            description = occurrence_id.name
+        else:
+            description = event_code + "-" + "DESCRIPTION CODE NOT FOUND"
+        return description
+
+    def load_description_occurrences(self):
+        """Generate occurrence description for all occurrences"""
+        for event in self:
+            occurrences = ""
+            event_codes = (
+                event.occurrence_code_1,
+                event.occurrence_code_2,
+                event.occurrence_code_3,
+                event.occurrence_code_4,
+                event.occurrence_code_5,
+            )
+            for code in event_codes:
+                if not code:
+                    continue
+                occurrences += event.get_description_occurrence(code)
+            event.occurrences = occurrences
