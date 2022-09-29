276f5478bd58958faa7087e5b63bfbf02138b3f2
Raphaël Valyi <rvalyi@akretion.com>
2022-09-29 20:31:20 -0300

    [MIG] l10n_br_stock_account_report: Migration to 14.0

--- l10n_br_stock_account_report/__manifest__.py
+++ l10n_br_stock_account_report/__manifest__.py
@@ -2,16 +2,16 @@
 # @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Brazilian Localization Stock Account Report",
-    "version": "12.0.1.2.0",
-    "author": "Akretion," "Odoo Community Association (OCA)",
+    "name": "P7 Stock Valuation Report",
+    "version": "14.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
     "category": "Reports/QWeb",
     "license": "AGPL-3",
     "depends": [
         "l10n_br_stock_account",
     ],
     "website": "https://github.com/OCA/l10n-brazil",
-    "development_status": "Mature",
+    "development_status": "Beta",
     "maintainers": [
         "mbcosta",
     ],
