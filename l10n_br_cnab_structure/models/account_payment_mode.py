2dfc110aab912bbc339a02f9fb791687b03ad6f7
Neto <netosjb@yahoo.com.br>
2022-10-01 13:01:16 -0300

    [MOV] cnab: move payment way fields to l10n_br_account_payment_order

--- l10n_br_cnab_structure/models/account_payment_mode.py
+++ l10n_br_cnab_structure/models/account_payment_mode.py
@@ -2,7 +2,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import _, api, fields, models
-from odoo.addons.l10n_br_account.models.account_invoice import AccountMove
 
 
 class AccountPaymentMode(models.Model):
@@ -11,12 +10,6 @@ class AccountPaymentMode(models.Model):
     """
 
     _inherit = "account.payment.mode"
-
-    account_payment_way_ids = fields.Many2many(
-        comodel_name="account.payment.way",
-        string="Payment Ways",
-        help="Allowed payment ways",
-    )
 
     cnab_processor = fields.Selection(
         selection_add=[("oca_processor", "OCA Processor")],
