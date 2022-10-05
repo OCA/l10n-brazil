b18565675861b265ab3aa317ac6966ea5d4f1855
Felipe <felipe@engenere.one>
2022-10-04 22:05:57 -0300

    [REF] cnab: Update Copyrights and License

--- l10n_br_cnab_structure/cnab/cnab.py
+++ l10n_br_cnab_structure/cnab/cnab.py
@@ -1,6 +1,7 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
+# Copyright (C) 2022-Today - Engenere (<https://engenere.one>).
+# @author Antônio S. Pereira Neto <neto@engenere.one>
+# @author Felipe Motter Pereira <felipe@engenere.one>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import yaml
 from dataclasses import fields
