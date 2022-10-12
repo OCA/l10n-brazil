146d0c332bfcb9c9ebca2bfc88055f8ce4551f9b
Raphaël Valyi <rvalyi@akretion.com>
2022-10-12 01:01:53 -0300

    [REF] extract the l10n_br_fiscal_closing module

---
+++ l10n_br_fiscal_closing/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Fiscal Closing",
+    "summary": """
+        Fechamento fiscal do periodo""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": [
+        "l10n_br_fiscal",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/closing.xml",
+    ],
+    "demo": [],
+}
