6298f5280ba2dd66f842778320c50148e99ae135
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-10-14 20:14:16 -0300

    [IMP] l10n_br_repair: black, isort, prettier

--- l10n_br_repair/__manifest__.py
+++ l10n_br_repair/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Brazilian Localization Repair",
     "summary": """
         Brazilian Localization Repair""",
-    "version": "12.0.9.0.0",
+    "version": "14.0.1.0.0",
     "category": "Localisation",
     "license": "AGPL-3",
     "author": "Escodoo, " "Odoo Community Association (OCA)",
