1d47d7645761e3d607611fb9e689a3f981b986cc
Luis Felipe Miléo <mileo@kmee.com.br>
2022-10-24 16:34:35 -0300

    [MIG][l10n_br_base] bump module version 15.0.1.0.0

--- l10n_br_base/__manifest__.py
+++ l10n_br_base/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["renatonlima", "rvalyi"],
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "14.0.3.16.1",
+    "version": "15.0.1.0.0",
     "depends": ["base", "base_setup", "base_address_city", "base_address_extended"],
     "data": [
         "security/ir.model.access.csv",
@@ -38,11 +38,6 @@
     "pre_init_hook": "pre_init_hook",
     "development_status": "Mature",
     "external_dependencies": {
-        "python": [
-            "num2words",
-            "erpbrasil.base>=2.3.0",
-            "phonenumbers",
-            "email-validator",
-        ]
+        "python": ["num2words", "erpbrasil.base", "phonenumbers", "email_validator"]
     },
 }
