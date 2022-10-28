eb8b4a5218f8ba29bd2a7fd69014f6240cf3db2e
Luiz Felipe do Divino <lf.divino@gmail.com>
2022-10-28 10:46:38 -0300

    [FIX] Correctly pos.order compute amount

--- l10n_br_pos/models/pos_order.py
+++ l10n_br_pos/models/pos_order.py
@@ -252,6 +252,10 @@ class PosOrder(models.Model):
         """Get object lines instaces used to compute fields"""
         return self.mapped("lines")
 
+    @api.depends("lines")
+    def _compute_amount(self):
+        super()._compute_amount()
+
     @api.depends("lines.price_subtotal_incl")
     def _amount_all(self):
         """Compute the total amounts of the SO."""
@@ -550,9 +554,3 @@ class PosOrder(models.Model):
             mail_create_nosubscribe=True,
             mail_notrack=True,
         ).refund()
-
-    def _compute_amount(self):
-        result = super(PosOrder, self)._compute_amount()
-        for order in self:
-            order._onchange_amount_all()
-        return result
