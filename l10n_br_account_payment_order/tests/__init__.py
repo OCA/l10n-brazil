7eef2e8856e75efd50f1b8e189756b034e9e1537
Neto <netosjb@yahoo.com.br>
2022-10-28 00:14:20 -0300

    [IMP] l10n_br_account_payment_order: add pix support

--- l10n_br_account_payment_order/tests/__init__.py
+++ l10n_br_account_payment_order/tests/__init__.py
@@ -9,3 +9,4 @@ from . import test_payment_order_inbound
 from . import test_payment_order_change
 from . import test_invoice_manual_workflow
 from . import test_payment_mode
+from . import test_payment_order_outbund_pix
