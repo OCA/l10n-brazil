7b7bfaf9581dcd85f00cf1ef6c58ea5bf6b5db82
Neto <netosjb@yahoo.com.br>
2022-10-27 23:54:31 -0300

    [IMP] l10n_br_cnab_structure: support for payment modes with fixed and variable banks"

--- l10n_br_cnab_structure/models/account_payment_oder.py
+++ l10n_br_cnab_structure/models/account_payment_oder.py
@@ -9,10 +9,35 @@ from odoo import api, fields, models
 class AccountPaymentOrder(models.Model):
     _inherit = "account.payment.order"
 
-    cnab_structure_id = fields.Many2one(
-        comodel_name="l10n_br_cnab.structure",
-        related="payment_mode_id.cnab_structure_id",
+    cnab_processor = fields.Selection(
+        selection="_selection_cnab_processor", compute="_compute_cnab_processor"
     )
+
+    cnab_structure_id = fields.Many2one(
+        comodel_name="l10n_br_cnab.structure", compute="_compute_cnab_structure_id"
+    )
+
+    @api.model
+    def _selection_cnab_processor(self):
+        return self.env["account.payment.mode"]._selection_cnab_processor()
+
+    def _compute_cnab_processor(self):
+        for order in self:
+            order.cnab_processor = order.payment_mode_id.cnab_processor
+            if order.cnab_processor:
+                return
+            if order.payment_type == "outbound":
+                order.cnab_processor = order.journal_id.default_outbound_cnab_processor
+
+    def _compute_cnab_structure_id(self):
+        for order in self:
+            order.cnab_structure_id = order.payment_mode_id.cnab_structure_id
+            if order.cnab_structure_id:
+                return
+            if order.payment_type == "outbound":
+                order.cnab_structure_id = (
+                    order.journal_id.default_outbound_cnab_structure_id
+                )
 
     def get_file_name(self, cnab_type):
         context_today = fields.Date.context_today(self)
@@ -41,30 +66,19 @@ class AccountPaymentOrder(models.Model):
         # Se não for um caso CNAB deve chamar o super
         if (
             cnab_type not in ("240", "400", "500")
-            or self.payment_mode_id.cnab_processor != "oca_processor"
+            or self.cnab_processor != "oca_processor"
         ):
             return super().generate_payment_file()
 
         str_file = self.cnab_structure_id.output(self).encode("utf-8")
         return str_file, self.get_file_name(cnab_type)
 
-    @api.model
-    def _prepare_bank_payment_line(self, paylines):
-        vals = super()._prepare_bank_payment_line(paylines)
-        vals.update(
-            {
-                "cnab_payment_way_id": paylines[
-                    0
-                ].order_id.payment_mode_id.cnab_payment_way_id.id,
-            }
-        )
-        return vals
-
     def generated2uploaded(self):
-        super().generated2uploaded()
+        result = super().generated2uploaded()
         for payment_line in self.payment_line_ids:
             # No caso de Cancelamento da Invoice a AML é apagada
             if payment_line.move_line_id:
                 # Importante para saber a situação do CNAB no caso
                 # de um pagto feito por fora ( dinheiro, deposito, etc)
                 payment_line.move_line_id.cnab_state = "exported"
+        return result
