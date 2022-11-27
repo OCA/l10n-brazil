66d914fa16e788c4de20c0438aaa552c34d078c1
Raphaël Valyi <rvalyi@akretion.com>
2022-11-27 11:13:19 -0300

    [FIX] proper binding module and xsd_implicit attr

--- l10n_br_nfe_spec/models/spec_models.py
+++ l10n_br_nfe_spec/models/spec_models.py
@@ -12,7 +12,7 @@ class NfeSpecMixin(models.AbstractModel):
     _schema_version = "4.0.0"
     _odoo_module = "l10n_br_nfe"
     _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
-    _binding_module = "nfelib.nfe.v4_0.leiaute_nfe_v4_00"
+    _binding_module = "nfelib.bindings.nfe.v4_0.leiaute_nfe_v4_00"
     _spec_tab_name = "NFe"
 
     brl_currency_id = fields.Many2one(
@@ -23,7 +23,7 @@ class NfeSpecMixin(models.AbstractModel):
     )
 
     def _valid_field_parameter(self, field, name):
-        if name in ("xsd_type", "xsd_required", "choice"):
+        if name in ("xsd_type", "xsd_required", "choice", "xsd_implicit"):
             return True
         else:
             return super()._valid_field_parameter(field, name)
