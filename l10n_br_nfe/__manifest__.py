9da22aabe62f1226cc04bfd5e1a8238c2b71c97f
Raphaël Valyi <rvalyi@akretion.com>
2022-11-28 10:45:16 -0300

    [REF] demo NFe import with xsdata

--- l10n_br_nfe/__manifest__.py
+++ l10n_br_nfe/__manifest__.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2019  Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-
 {
     "name": "NF-e",
     "summary": "Brazilian Eletronic Invoice NF-e .",
