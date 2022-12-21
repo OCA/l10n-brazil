2d50b10ba965cfe09cbf039b6e9bc244ceeaea41
Luiz Felipe do Divino <lf.divino@gmail.com>
2022-12-21 12:53:48 -0300

    [FIX] Client cpf/cnpj validation

--- l10n_br_pos/static/src/js/Screens/PaymentScreen/PaymentScreen.js
+++ l10n_br_pos/static/src/js/Screens/PaymentScreen/PaymentScreen.js
@@ -9,16 +9,48 @@ Copyright (C) 2016-Today KMEE (https://kmee.com.br)
 odoo.define("l10n_br_pos.PaymentScreen", function (require) {
     "use strict";
 
+    const core = require("web.core");
     const PaymentScreen = require("point_of_sale.PaymentScreen");
     const Registries = require("point_of_sale.Registries");
+    const util = require("l10n_br_pos.util");
+    const {Gui} = require("point_of_sale.Gui");
+    const _t = core._t;
 
     const L10nBrPosPaymentScreen = (PaymentScreen) =>
         class extends PaymentScreen {
+            check_valid_cpf_cnpj(order) {
+                let result = true;
+                const client = order.get_client();
+
+                if (client) {
+                    let cnpj_cpf = null;
+                    if (client.cnpj_cpf) {
+                        cnpj_cpf = client.cnpj_cpf;
+                    } else {
+                        cnpj_cpf = client.name;
+                    }
+
+                    result = util.validate_cnpj_cpf(cnpj_cpf);
+                    if (!result) {
+                        order.set_client(null);
+                    }
+                }
+
+                return result;
+            }
+
             async _isOrderValid(isForceValidate) {
                 var result = super._isOrderValid(isForceValidate);
                 var order = this.env.pos.get_order();
-                result = await order.document_send(this);
-                return result;
+                const valid_cpf_cnpj = this.check_valid_cpf_cnpj(order);
+                if (valid_cpf_cnpj) {
+                    result = await order.document_send(this);
+                    return result;
+                }
+                Gui.showPopup("ErrorPopup", {
+                    title: _t("Invalid CNPJ / CPF !"),
+                    body: _t("Enter a valid CNPJ / CPF number"),
+                });
             }
         };
 
