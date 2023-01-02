a0db19e92996bffdea512a3c889a1b913efc68a2
Ygor Carvalho <ygcarvalh@gmail.com>
2023-01-02 16:03:58 -0300

    [IMP] New test cases for the module

---
+++ l10n_br_pos/tests/test_l10n_br_pos_config.py
@@ -0,0 +1,38 @@
+# Copyright 2022 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestL10nBrPosConfig(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.pos_config = self.env.ref("point_of_sale.pos_config_main")
+
+    def test_pos_config_sales_fiscal_operation(self):
+        self.pos_config.out_pos_fiscal_operation_id = self.env.ref(
+            "l10n_br_fiscal.fo_venda"
+        )
+        self.pos_config._compute_allowed_tax()
+        self.assertEqual(
+            6,
+            len(self.pos_config.out_pos_fiscal_operation_line_ids),
+            "Tax operations lines were not found.",
+        )
+
+    def test_pos_config_false_fiscal_operation(self):
+        self.pos_config.out_pos_fiscal_operation_id = False
+        self.pos_config._compute_allowed_tax()
+        self.assertEqual(
+            0,
+            len(self.pos_config.out_pos_fiscal_operation_line_ids),
+            "There should be no lines when the fiscal operation is false.",
+        )
+
+    def test_update_pos_config_fiscal_map(self):
+        self.pos_config.update_pos_fiscal_map()
+        self.assertEqual(
+            36,
+            len(self.pos_config.pos_fiscal_map_ids),
+            "No tax data was generated for the products.",
+        )
