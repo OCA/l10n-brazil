97d05e4f8c4d7e69dccf7e7b3b7be4513c411d2a
Luiz Felipe do Divino <lf.divino@gmail.com>
2023-02-06 18:33:52 -0300

    [ADD] POS cfe xml export at fiscal closing

--- l10n_br_pos_cfe/__manifest__.py
+++ l10n_br_pos_cfe/__manifest__.py
@@ -14,6 +14,7 @@
     "depends": [
         "point_of_sale",
         "l10n_br_pos",
+        "l10n_br_fiscal_closing",
     ],
     "external_dependencies": {
         "python": ["satcomum"],
@@ -21,6 +22,8 @@
     "data": [
         # Views
         "views/pos_payment_method_view.xml",
+        "views/closing_view.xml",
+        "views/pos_config_view.xml",
         # Templates
         "views/pos_template.xml",
     ],
