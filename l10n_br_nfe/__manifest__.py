5c5a746cc30b525a393e7e6fa85cb98a8ccb3066
Renato Lima <renatonlima@gmail.com>
2023-02-17 11:42:17 -0300

    Allows edit nfe40_cProd if product_id is None

--- l10n_br_nfe/__manifest__.py
+++ l10n_br_nfe/__manifest__.py
@@ -24,6 +24,7 @@
         # Views
         "views/res_company_view.xml",
         "views/nfe_document_view.xml",
+        "views/nfe_document_line_view.xml",
         "views/res_config_settings_view.xml",
         # Wizards
         "wizards/import_document.xml",
