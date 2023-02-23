a8fb3bfeb466e0fa4913cb6d0fe35d829762ebd7
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-02-23 15:09:54 -0300

    [FIX] l10n_br_account: remove active field from account.move.line

--- l10n_br_account/models/account_invoice_line.py
+++ l10n_br_account/models/account_invoice_line.py
@@ -30,12 +30,6 @@ class AccountMoveLine(models.Model):
     _inherit = [_name, "l10n_br_fiscal.document.line.mixin.methods"]
     _inherits = {"l10n_br_fiscal.document.line": "fiscal_document_line_id"}
 
-    # some account.move.line records _inherits from an fiscal.document.line that is
-    # disabled with active=False (dummy record) in the l10n_br_fiscal_document_line table.
-    # To make the invoice lines still visible, we set active=True
-    # in the account_move_line table.
-    active = fields.Boolean(default=True, depends_context=["move_id"])
-
     fiscal_document_line_id = fields.Many2one(
         comodel_name="l10n_br_fiscal.document.line",
         string="Fiscal Document Line",
