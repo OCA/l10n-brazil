1cb810ddbdc1e31bfc4ee9a0e21a9b10b59ec320
Antônio Neto <neto@engenere.one>
2023-02-23 23:56:25 -0300

    [IMP] l10n_br_sale: add unittest for pricelist

--- l10n_br_sale/tests/__init__.py
+++ l10n_br_sale/tests/__init__.py
@@ -4,3 +4,4 @@ from . import test_l10n_br_sale
 from . import test_l10n_br_sale_discount
 from . import test_l10n_br_sale_sn
 from . import test_l10n_br_sale_lc
+from . import test_l10n_br_sale_pricelist
