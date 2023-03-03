40310d5c846f7510d9608095c407a4fe2893caa1
Ygor Carvalho <ygcarvalh@gmail.com>
2023-03-03 08:51:30 -0300

    [IMP] l10n_br_pos_nfce: return of NFCe information
    they were generated during the invoice creation process and the
    transmission process to Sefaz.

--- l10n_br_pos_nfce/models/pos_order.py
+++ l10n_br_pos_nfce/models/pos_order.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2023  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+import pytz
+
 from odoo import api, models
 
 
@@ -102,6 +104,7 @@ class PosOrder(models.Model):
 
     @api.model
     def create_from_ui(self, orders, draft=False):
+        local_timezone = pytz.timezone("America/Sao_Paulo")
         res = super(PosOrder, self).create_from_ui(orders, draft)
         for option in res:
             order = self.env["pos.order"].search([("id", "=", option["id"])])
@@ -112,6 +115,24 @@ class PosOrder(models.Model):
                 {
                     "status_description": order.account_move.status_description,
                     "status_code": order.account_move.status_code,
+                    "authorization_protocol": order.account_move.fiscal_document_id.authorization_protocol,
+                    "document_key": order.account_move.fiscal_document_id.document_key,
+                    "document_number": order.account_move.fiscal_document_id.document_number.zfill(
+                        9
+                    ),
+                    "document_serie": order.account_move.fiscal_document_id.document_serie.zfill(
+                        3
+                    ),
+                    "url_consulta": order.account_move.estado_de_consulta_da_nfce(),
+                    "qr_code": order.account_move._monta_qrcode(),
+                    "authorization_date": order.account_move.fiscal_document_id.authorization_date.strftime(
+                        "%m/%d/%Y %H:%M:%S"
+                    ),
+                    "document_date": order.account_move.fiscal_document_id.document_date.astimezone(
+                        local_timezone
+                    ).strftime(
+                        "%m/%d/%Y %H:%M:%S"
+                    ),
                 }
             )
         return res
