8d87ace9a179a8bcf9b5881bc3e1a32c09ea4521
Ygor Carvalho <ygcarvalh@gmail.com>
2023-03-08 09:08:11 -0300

    [IMP] l10n_br_pos_nfce: nfce danfe layout and delay to print qrcode

---
+++ l10n_br_pos_nfce/static/src/js/Screens/ReceiptScreen/ReceiptScreen.js
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2016-Today KMEE (https://kmee.com.br)
+@author: Luis Felipe Mileo <mileo@kmee.com.br>
+@author: Luiz Felipe do Divino <luiz.divino@kmee.com.br>
+@author: Ygor de Carvalho Andrade <ygor.carvalho@kmee.com.br>
+ License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+odoo.define("l10n_br_pos_nfce.ReceiptScreen", function (require) {
+    "use strict";
+
+    const ReceiptScreen = require("point_of_sale.ReceiptScreen");
+    const Registries = require("point_of_sale.Registries");
+
+    const L10nBrPosNfceReceiptScreen = (ReceiptScreen) =>
+        class extends ReceiptScreen {
+            // @override
+            async handleAutoPrint() {
+                if (this._shouldAutoPrint()) {
+                    setTimeout(() => this.cfePrinting(), 1000);
+                }
+            }
+            async cfePrinting() {
+                await this.printReceipt();
+                if (this.currentOrder._printed && this._shouldCloseImmediately()) {
+                    this.whenClosing();
+                }
+            }
+        };
+
+    Registries.Component.extend(ReceiptScreen, L10nBrPosNfceReceiptScreen);
+
+    return L10nBrPosNfceReceiptScreen;
+});
