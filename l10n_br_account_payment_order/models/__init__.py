bb8b3fcc2f25000568616b93802ce8cddcaa8e40
Magno Costa <magno.costa@akretion.com.br>
2023-03-08 14:42:43 -0300

    [REM] l10n_br_account_payment_order: Remove unused file.

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -8,16 +8,6 @@ from . import account_payment_mode
 from . import account_payment_order
 from . import account_payment_line
 from . import account_payment
-
-# TODO - Separação dos dados de importação para um objeto especifico
-#  cnab.return.log armazenando o LOG do Arquivo de Retorno CNAB
-#  de forma separada e permitindo a integração com a alteração feita no
-#  modulo do BRCobranca onde se esta utilizando o modulo
-#  account_base_move_import para fazer essa tarefa de wizard de importação,
-#  o objeto l10n_br_cnab esta comentado para permitir, caso seja necessário,
-#  a implementação de outra forma de importação pois tem os metodos que eram
-#  usados pela KMEE e o historico git do arquivo
-# from . import l10n_br_cnab
 from . import l10n_br_cnab_event
 from . import l10n_br_cnab_lot
 from . import l10n_br_cnab_return_log
