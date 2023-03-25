8a92c106a71e86e662ae76df8ee829af761e65c5
Felipe Motter <felipe@engenere.one>
2023-03-25 15:13:07 -0300

    [IMP] l10n_br_account: discount tests

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -5,3 +5,4 @@ from . import test_customer_invoice_dummy
 from . import test_supplier_invoice_dummy
 from . import test_invoice_refund
 from . import test_company_fiscal_dummy
+from . import test_invoice_general_cases
