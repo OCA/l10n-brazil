c35cba926170c385dc0e584ea07adf879b404791
Magno Costa <magno.costa@akretion.com.br>
2023-03-31 15:26:36 -0300

    [IMP] l10n_br_sale: Included tests to check if the Amount values from Sale Order are equals in Invoice created.

--- l10n_br_sale/tests/test_l10n_br_sale.py
+++ l10n_br_sale/tests/test_l10n_br_sale.py
@@ -178,10 +178,70 @@ class L10nBrSaleBaseTest(SavepointCase):
                 " dictionary from Sale Order.",
             )
 
+            # Testa se os Valores Totais estão iguais entre o Pedido e Fatura
+            self.assertEqual(
+                sale_order.amount_total,
+                invoice.amount_total,
+                "Error field Amount Total in Invoice" " are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_tax,
+                invoice.amount_tax,
+                "Error field Amount Tax in Invoice" " are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_untaxed,
+                invoice.amount_untaxed,
+                "Error field Amount Untaxed in Invoice"
+                " are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_price_gross,
+                invoice.amount_price_gross,
+                "Error field Amount Price Gross in Invoice"
+                " are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_financial_total,
+                invoice.amount_financial_total,
+                "Error field Amount Financial Total in "
+                "Invoice are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_financial_total_gross,
+                invoice.amount_financial_total_gross,
+                "Error field Amount Financial Total Gross"
+                " in Invoice are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_freight_value,
+                invoice.amount_freight_value,
+                "Error field Amount Freight in Invoice"
+                " are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_insurance_value,
+                invoice.amount_insurance_value,
+                "Error field Amount Insurance in Invoice"
+                " are different from Sale Order.",
+            )
+            self.assertEqual(
+                sale_order.amount_other_value,
+                invoice.amount_other_value,
+                "Error field Amount Other Values in Invoice"
+                " are different from Sale Order.",
+            )
+
             for line in invoice.invoice_line_ids:
                 self.assertTrue(
                     line.fiscal_operation_line_id,
                     "Error to included Operation Line from Sale Order Line.",
+                )
+                self.assertEqual(
+                    line.price_total,
+                    line.sale_line_ids[0].price_total,
+                    "Error field Price Total in Invoice Line"
+                    " are different from Sale Order Line.",
                 )
 
     def test_l10n_br_sale_products(self):
