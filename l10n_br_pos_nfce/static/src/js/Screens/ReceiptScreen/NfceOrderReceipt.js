7a01ed65e74caa98e629c027138c506733439b65
Ygor Carvalho <ygcarvalh@gmail.com>
2023-04-03 16:00:25 -0300

    [REF] l10n_br_pos_nfce: change NFCe order receipt owl component

--- l10n_br_pos_nfce/static/src/js/Screens/ReceiptScreen/NfceOrderReceipt.js
+++ l10n_br_pos_nfce/static/src/js/Screens/ReceiptScreen/NfceOrderReceipt.js
@@ -4,25 +4,24 @@ odoo.define("l10n_br_pos_nfce.NfceOrderReceipt", function (require) {
     const OrderReceipt = require("point_of_sale.OrderReceipt");
     const Registries = require("point_of_sale.Registries");
 
-    const NfceOrderReceipt = (OrderReceipt) =>
-        class extends OrderReceipt {
-            get isHomologationEnvironment() {
-                return this.receipt.nfce_environment === "2";
-            }
+    class NFCeOrderReceipt extends OrderReceipt {
+        get isHomologationEnvironment() {
+            return this.receipt.nfce_environment === "2";
+        }
 
-            get hasChange() {
-                return this.receipt.change;
-            }
+        get hasChange() {
+            return this.receipt.change;
+        }
 
-            get orderChange() {
-                return this.receipt.change.toLocaleString("pt-br", {
-                    minimumFractionDigits: 2,
-                });
-            }
-        };
+        get orderChange() {
+            return this.receipt.change.toLocaleString("pt-br", {
+                minimumFractionDigits: 2,
+            });
+        }
+    }
 
-    Registries.Component.extend(OrderReceipt, NfceOrderReceipt);
-    OrderReceipt.template = "NfceOrderReceipt";
+    Registries.Component.add(NFCeOrderReceipt);
+    NFCeOrderReceipt.template = "NfceOrderReceipt";
 
-    return OrderReceipt;
+    return NFCeOrderReceipt;
 });
