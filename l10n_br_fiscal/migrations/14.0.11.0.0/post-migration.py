d4303965829a790ed8458d19b40e58164693a624
Felipe Motter <felipe@engenere.one>
2023-05-09 15:32:21 -0300

    [IMP] l10n_br_fiscal: update ncms table

---
+++ l10n_br_fiscal/migrations/14.0.11.0.0/post-migration.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Engenere - Felipe Motter Pereira
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+from odoo import tools
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+
+    tools.convert_file(
+        env.cr,
+        "l10n_br_fiscal",
+        "data/l10n_br_fiscal.ncm.csv",
+        None,
+        mode="init",
+        noupdate=True,
+        kind="init",
+    )
