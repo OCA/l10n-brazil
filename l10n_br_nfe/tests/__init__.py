9d62ae7a99a90673823831dd9adc76e21276372a
Raphaël Valyi <rvalyi@akretion.com>
2023-05-17 02:14:13 -0300

    [DEL] remove useless files

--- l10n_br_nfe/tests/__init__.py
+++ l10n_br_nfe/tests/__init__.py
@@ -1,8 +1,3 @@
-# from . import test_account_customer_invoice
-# from . import test_account_customer_nfe
-# from . import test_account_supplier_nfe
-# from . import test_account_nfe_refund
-# from . import test_nfe_export
 from . import test_nfe_structure
 from . import test_nfe_import
 from . import test_nfe_serialize
