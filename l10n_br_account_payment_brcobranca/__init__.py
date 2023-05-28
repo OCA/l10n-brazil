d7c025001f81dc7aec083d72692691bbdf07df26
Raphaël Valyi <rvalyi@akretion.com>
2023-05-27 22:28:32 -0300

    [LINT] no copyright in init + maintainers

--- l10n_br_account_payment_brcobranca/__init__.py
+++ l10n_br_account_payment_brcobranca/__init__.py
@@ -1,7 +1,3 @@
-# Copyright 2017 Akretion
-# @author Raphaël Valyi <raphael.valyi@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import models
 from . import parser
 from . import wizard
