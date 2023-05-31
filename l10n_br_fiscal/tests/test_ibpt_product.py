45a89d810d241ae9775d7bd34bdb7d8acd1307c7
Raphaël Valyi <rvalyi@akretion.com>
2023-05-30 22:26:38 -0300

    [CI] only run IBPT tests certain days
    otherwise run a dummy mocked test for minimal coverage

--- l10n_br_fiscal/tests/test_ibpt_product.py
+++ l10n_br_fiscal/tests/test_ibpt_product.py
@@ -1,13 +1,14 @@
 # Copyright 2019 Akretion - Renato Lima <renato.lima@akretion.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from .test_ibpt import TestIbpt
+from unittest import mock
+
+from .test_ibpt import TestIbpt, mocked_requests_get, not_every_day_test
 
 
 class TestIbptProduct(TestIbpt):
     @classmethod
     def setUpClass(cls):
-
         super().setUpClass()
         cls.ncm_85030010 = cls.env.ref("l10n_br_fiscal.ncm_85030010")
         cls.ncm_85014029 = cls.env.ref("l10n_br_fiscal.ncm_85014029")
@@ -24,6 +25,18 @@ class TestIbptProduct(TestIbpt):
             name="Product Test 3 - With NCM: 8501.40.29", ncm=cls.ncm_85014029
         )
 
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_mock(self, mock_get):
+        api_status = self.env.company.ibpt_api
+        self.env.company.ibpt_api = True  # force to run the mocked query
+        self.ncm_85030010.action_ibpt_inquiry()
+        ncms = self.ncm_model.search(
+            [("id", "in", (self.ncm_85030010.id, self.ncm_85014029.id))]
+        )
+        ncms._scheduled_update()
+        self.env.company.ibpt_api = api_status
+
+    @not_every_day_test
     def test_update_ibpt_product(self):
         """Check tax estimate update"""
 
@@ -40,6 +53,7 @@ class TestIbptProduct(TestIbpt):
             [("ncm_id", "in", (self.ncm_85030010.id, self.ncm_85014029.id))]
         ).unlink()
 
+    @not_every_day_test
     def test_ncm_count_product_template(self):
         """Check product template relation with NCM"""
 
@@ -49,6 +63,7 @@ class TestIbptProduct(TestIbpt):
         self.assertEqual(self.ncm_85030010.product_tmpl_qty, 2)
         self.assertEqual(self.ncm_85014029.product_tmpl_qty, 1)
 
+    @not_every_day_test
     def test_update_scheduled(self):
         """Check NCM update scheduled"""
 
