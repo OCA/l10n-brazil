37e9d9a92e8c08e2d8a73e900c833ec4d03a35e6
Raphaël Valyi <rvalyi@akretion.com>
2023-05-30 22:26:38 -0300

    [CI] only run IBPT tests certain days
    otherwise run a dummy mocked test for minimal coverage

--- l10n_br_fiscal/tests/test_ibpt_service.py
+++ l10n_br_fiscal/tests/test_ibpt_service.py
@@ -1,13 +1,14 @@
 # Copyright 2019 Akretion - Renato Lima <renato.lima@akretion.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from .test_ibpt import TestIbpt
+from unittest import mock
+
+from .test_ibpt import TestIbpt, mocked_requests_get, not_every_day_test
 
 
 class TestIbptService(TestIbpt):
     @classmethod
     def setUpClass(cls):
-
         super().setUpClass()
         cls.nbs_115069000 = cls.env.ref("l10n_br_fiscal.nbs_115069000")
         cls.nbs_124043300 = cls.env.ref("l10n_br_fiscal.nbs_124043300")
@@ -24,6 +25,14 @@ class TestIbptService(TestIbpt):
             name="Product Test 3 - With NBS: 1.2404.33.00", nbs=cls.nbs_124043300
         )
 
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_mock(self, mock_get):
+        api_status = self.env.company.ibpt_api
+        self.env.company.ibpt_api = True  # force to run the mocked query
+        self.nbs_115069000.action_ibpt_inquiry()
+        self.env.company.ibpt_api = api_status
+
+    @not_every_day_test
     def test_update_ibpt_service(self):
         """Check tax estimate update"""
 
@@ -40,6 +49,7 @@ class TestIbptService(TestIbpt):
             [("nbs_id", "in", (self.nbs_115069000.id, self.nbs_124043300.id))]
         ).unlink()
 
+    @not_every_day_test
     def test_nbs_count_product_template(self):
         """Check product template relation with NBS"""
 
@@ -49,6 +59,7 @@ class TestIbptService(TestIbpt):
         self.assertEqual(self.nbs_115069000.product_tmpl_qty, 2)
         self.assertEqual(self.nbs_124043300.product_tmpl_qty, 1)
 
+    @not_every_day_test
     def test_update_scheduled(self):
         """Check NBS update scheduled"""
 
