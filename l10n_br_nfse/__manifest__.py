d045d988da4c8a0acd393a8595c25680e2b9b77e
Raphaël Valyi <rvalyi@akretion.com>
2023-06-09 18:49:49 -0300

    [REF] erpbrasil.assinatura>=1.7.0 dep
    and simplify transitive deps

--- l10n_br_nfse/__manifest__.py
+++ l10n_br_nfse/__manifest__.py
@@ -12,10 +12,10 @@
     "website": "https://github.com/OCA/l10n-brazil",
     "external_dependencies": {
         "python": [
-            "erpbrasil.edoc",
-            "erpbrasil.assinatura",
-            "erpbrasil.transmissao",
             "erpbrasil.base>=2.3.0",
+            "erpbrasil.edoc",
+            "erpbrasil.assinatura>=1.7.0",
+            "erpbrasil.transmissao",
         ],
     },
     "depends": [
