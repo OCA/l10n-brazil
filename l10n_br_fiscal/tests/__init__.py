ebad18ad5d585fbd9837409f1aeb38fe40d9cce3
Renato Lima <renato.lima@akretion.com>
2023-06-15 18:06:38 -0300

    [REF] create _get_search_domain method in tax.definition

--- l10n_br_fiscal/tests/__init__.py
+++ l10n_br_fiscal/tests/__init__.py
@@ -5,6 +5,7 @@ from . import (
     test_fiscal_document_generic,
     test_fiscal_document_nfse,
     test_fiscal_tax,
+    test_tax_benefit,
     test_ibpt_product,
     test_ibpt_service,
     test_icms_regulation,
