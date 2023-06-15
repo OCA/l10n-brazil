cdc3b489c0dd59f3433c726599d26bf7b2084f1d
Raphaël Valyi <rvalyi@akretion.com>
2023-06-15 04:08:40 -0300

    [TEST] test NFe webservices with mocks

--- l10n_br_nfe/tests/__init__.py
+++ l10n_br_nfe/tests/__init__.py
@@ -3,4 +3,5 @@ from . import test_nfe_import
 from . import test_nfe_serialize
 from . import test_nfe_serialize_lc
 from . import test_nfe_serialize_sn
+from . import test_nfe_webservices
 from . import test_nfe_xml_validation
