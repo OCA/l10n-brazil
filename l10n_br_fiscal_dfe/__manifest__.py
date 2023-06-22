0db127f896427ba187f36d5209f2b685faa086cd
Felipe Zago <felipe.zago@kmee.com.br>
2023-06-22 11:05:19 -0300

    [ADD] l10n_br_fiscal_dfe

---
+++ l10n_br_fiscal_dfe/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "L10n BR Fiscal Dfe",
+    "summary": """
+        Distribuição de documentos fiscais""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": ["l10n_br_fiscal"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mde/mde_views.xml",
+        "views/dfe/dfe_views.xml",
+        "views/l10n_br_fiscal_menu.xml",
+    ],
+    "demo": [],
+    "external_dependencies": {
+        "python": [
+            "erpbrasil.edoc>=2.4.0",
+        ],
+    },
+}
