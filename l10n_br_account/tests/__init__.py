000abd9b6e16f3b274ca50936c80af23db967922
Raphaël Valyi <rvalyi@akretion.com>
2023-06-25 00:47:57 -0300

    [REF] faster tests with SavePointCase

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -1,8 +1,11 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import test_account_taxes
-from . import test_customer_invoice_dummy
-from . import test_supplier_invoice_dummy
-from . import test_invoice_refund
-from . import test_company_fiscal_dummy
-from . import test_invoice_general_cases
+from . import (
+    test_account_taxes,
+    test_company_fiscal_dummy,
+    test_customer_invoice_dummy,
+    test_document_date,
+    test_invoice_refund,
+    test_move_discount,
+    test_supplier_invoice_dummy,
+)
