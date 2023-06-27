1db478f0101ddfcb1bfc4b73ffae68134e8dd76b
Raphaël Valyi <rvalyi@akretion.com>
2023-06-27 00:24:06 -0300

    [REF] l10n_br_fiscal_certificate extraction

---
+++ l10n_br_fiscal_certificate/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2023  Raphaël Valyi - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "A1 fiscal certificate management for Brazil",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["renatonlima"],
+    "website": "https://github.com/OCA/l10n-brazil",
+    "development_status": "Production/Stable",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "l10n_br_fiscal",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/certificate_view.xml",
+        "views/res_company_view.xml",
+        "views/res_config_settings_view.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "external_dependencies": {
+        "python": [
+            "erpbrasil.assinatura>=1.7.0",
+        ]
+    },
+}
