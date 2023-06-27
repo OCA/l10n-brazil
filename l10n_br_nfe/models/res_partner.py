731106b2d8c2349472fb3db4180f362365fa4f67
Felipe Zago <felipe.zago@kmee.com.br>
2023-06-27 11:02:36 -0300

    [FIX] match res partner m2o

--- l10n_br_nfe/models/res_partner.py
+++ l10n_br_nfe/models/res_partner.py
@@ -222,7 +222,9 @@ class ResPartner(spec_models.SpecModel):
 
     @api.model
     def match_or_create_m2o(self, rec_dict, parent_dict, model=None):
-        domain_cnpj = []
+        if model is not None and model != self:
+            return
+
         if parent_dict.get("nfe40_CNPJ"):
             rec_dict["cnpj_cpf"] = parent_dict["nfe40_CNPJ"]
             domain_cnpj = [
@@ -230,14 +232,17 @@ class ResPartner(spec_models.SpecModel):
                 ("cnpj_cpf", "=", rec_dict["cnpj_cpf"]),
                 ("cnpj_cpf", "=", format_cnpj_cpf(rec_dict["cnpj_cpf"])),
             ]
+            match = self.search(domain_cnpj, limit=1)
+            if match:
+                return match.id
 
-        match = self.search(domain_cnpj, limit=1)
-        if match:
-            return match.id
+        vals = self._prepare_import_dict(
+            rec_dict, model=model, parent_dict=parent_dict, defaults_model=model
+        )
         if self._context.get("dry_run"):
-            rec_id = self.new(rec_dict).id
+            rec_id = self.new(vals).id
         else:
-            rec_id = self.with_context(parent_dict=parent_dict).create(rec_dict).id
+            rec_id = self.with_context(parent_dict=parent_dict).create(vals).id
         return rec_id
 
     def _export_field(self, xsd_field, class_obj, member_spec, export_value=None):
