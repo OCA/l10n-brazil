d5f918325cc876c574c30efce3b60d9ce6aa2355
Raphaël Valyi <rvalyi@akretion.com>
2023-07-06 04:34:29 -0300

    [TEST] add account move common test class

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import common
 from . import test_account_taxes
 from . import test_customer_invoice_dummy
 from . import test_document_date
