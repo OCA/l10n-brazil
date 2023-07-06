ee60f29dfd139f2b0674756937b4b3480edb82ed
Raphaël Valyi <rvalyi@akretion.com>
2023-07-06 04:35:23 -0300

    [TEST] add move tests for Simples Nacional

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -1,6 +1,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import common
+from . import test_account_move_sn
 from . import test_account_taxes
 from . import test_customer_invoice_dummy
 from . import test_document_date
