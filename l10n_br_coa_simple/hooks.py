670469ff4a44f6330ba7728de5880c6aae767cae
Raphaël Valyi <rvalyi@akretion.com>
2023-07-08 02:31:10 -0300

    [REF] simplify CoA hook

--- l10n_br_coa_simple/hooks.py
+++ l10n_br_coa_simple/hooks.py
@@ -7,16 +7,6 @@ from odoo import SUPERUSER_ID, api, tools
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     coa_simple_tmpl = env.ref("l10n_br_coa_simple.l10n_br_coa_simple_chart_template")
-    if env["ir.module.module"].search_count(
-        [
-            ("name", "=", "l10n_br_account"),
-            ("state", "=", "installed"),
-        ]
-    ):
-        from odoo.addons.l10n_br_account.hooks import load_fiscal_taxes
-
-        # Relate fiscal taxes to account taxes.
-        load_fiscal_taxes(env, coa_simple_tmpl)
 
     # Load COA to Demo Company
     if not tools.config.get("without_demo"):
