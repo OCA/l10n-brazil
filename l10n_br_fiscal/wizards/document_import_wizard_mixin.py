82ea8b5c281bd5a07b67bc1bf2955987635f50e3
Felipe Zago <felipe.zago@kmee.com.br>
2023-07-18 18:44:39 -0300

    [ADD] document import wizard base

---
+++ l10n_br_fiscal/wizards/document_import_wizard_mixin.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2023  Felipe Zago Rodrigues - Kmee
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+from odoo.addons.l10n_br_fiscal.constants.fiscal import FISCAL_IN_OUT_ALL
+
+
+class DocumentImportWizardMixin(models.TransientModel):
+
+    _name = "l10n_br_fiscal.document.import.wizard.mixin"
+    _inherit = "l10n_br_fiscal.base.wizard.mixin"
+
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        default=lambda self: self.env.user.company_id,
+    )
+
+    importing_type = fields.Selection(
+        selection=[("xml_file", "XML File")], string="Importing Type", required=True
+    )
+
+    xml = fields.Binary(string="XML to Import")
+
+    fiscal_operation_type = fields.Selection(
+        string="Fiscal Operation Type", selection=FISCAL_IN_OUT_ALL
+    )
