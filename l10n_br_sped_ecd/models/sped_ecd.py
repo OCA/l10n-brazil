2f7a3f031772d34b20354eb168bb32a859d8958f
Raphaël Valyi <rvalyi@akretion.com>
2023-08-13 16:57:18 -0300

    [ADD] extracted l10n_br_sped_ecd module

---
+++ l10n_br_sped_ecd/models/sped_ecd.py
@@ -0,0 +1,298 @@
+from odoo import api, fields, models
+
+
+class Registro0000(models.Model):
+    _description = 'Concrete model for ecd 0000'
+    _name = 'l10n_br_sped.ecd.0000'
+    _inherit = 'l10n_br_sped.ecd.9.0000'
+
+class Registro0007(models.Model):
+    _description = 'Concrete model for ecd 0007'
+    _name = 'l10n_br_sped.ecd.0007'
+    _inherit = 'l10n_br_sped.ecd.9.0007'
+
+class Registro0020(models.Model):
+    _description = 'Concrete model for ecd 0020'
+    _name = 'l10n_br_sped.ecd.0020'
+    _inherit = 'l10n_br_sped.ecd.9.0020'
+
+class Registro0035(models.Model):
+    _description = 'Concrete model for ecd 0035'
+    _name = 'l10n_br_sped.ecd.0035'
+    _inherit = 'l10n_br_sped.ecd.9.0035'
+
+class Registro0150(models.Model):
+    _description = 'Concrete model for ecd 0150'
+    _name = 'l10n_br_sped.ecd.0150'
+    _inherit = 'l10n_br_sped.ecd.9.0150'
+
+class Registro0180(models.Model):
+    _description = 'Concrete model for ecd 0180'
+    _name = 'l10n_br_sped.ecd.0180'
+    _inherit = 'l10n_br_sped.ecd.9.0180'
+
+class RegistroI010(models.Model):
+    _description = 'Concrete model for ecd I010'
+    _name = 'l10n_br_sped.ecd.i010'
+    _inherit = 'l10n_br_sped.ecd.9.i010'
+
+class RegistroI012(models.Model):
+    _description = 'Concrete model for ecd I012'
+    _name = 'l10n_br_sped.ecd.i012'
+    _inherit = 'l10n_br_sped.ecd.9.i012'
+
+class RegistroI015(models.Model):
+    _description = 'Concrete model for ecd I015'
+    _name = 'l10n_br_sped.ecd.i015'
+    _inherit = 'l10n_br_sped.ecd.9.i015'
+
+class RegistroI020(models.Model):
+    _description = 'Concrete model for ecd I020'
+    _name = 'l10n_br_sped.ecd.i020'
+    _inherit = 'l10n_br_sped.ecd.9.i020'
+
+class RegistroI030(models.Model):
+    _description = 'Concrete model for ecd I030'
+    _name = 'l10n_br_sped.ecd.i030'
+    _inherit = 'l10n_br_sped.ecd.9.i030'
+
+class RegistroI050(models.Model):
+    _description = 'Concrete model for ecd I050'
+    _name = 'l10n_br_sped.ecd.i050'
+    _inherit = 'l10n_br_sped.ecd.9.i050'
+
+class RegistroI051(models.Model):
+    _description = 'Concrete model for ecd I051'
+    _name = 'l10n_br_sped.ecd.i051'
+    _inherit = 'l10n_br_sped.ecd.9.i051'
+
+class RegistroI052(models.Model):
+    _description = 'Concrete model for ecd I052'
+    _name = 'l10n_br_sped.ecd.i052'
+    _inherit = 'l10n_br_sped.ecd.9.i052'
+
+class RegistroI053(models.Model):
+    _description = 'Concrete model for ecd I053'
+    _name = 'l10n_br_sped.ecd.i053'
+    _inherit = 'l10n_br_sped.ecd.9.i053'
+
+class RegistroI075(models.Model):
+    _description = 'Concrete model for ecd I075'
+    _name = 'l10n_br_sped.ecd.i075'
+    _inherit = 'l10n_br_sped.ecd.9.i075'
+
+class RegistroI100(models.Model):
+    _description = 'Concrete model for ecd I100'
+    _name = 'l10n_br_sped.ecd.i100'
+    _inherit = 'l10n_br_sped.ecd.9.i100'
+
+class RegistroI150(models.Model):
+    _description = 'Concrete model for ecd I150'
+    _name = 'l10n_br_sped.ecd.i150'
+    _inherit = 'l10n_br_sped.ecd.9.i150'
+
+class RegistroI155(models.Model):
+    _description = 'Concrete model for ecd I155'
+    _name = 'l10n_br_sped.ecd.i155'
+    _inherit = 'l10n_br_sped.ecd.9.i155'
+
+class RegistroI157(models.Model):
+    _description = 'Concrete model for ecd I157'
+    _name = 'l10n_br_sped.ecd.i157'
+    _inherit = 'l10n_br_sped.ecd.9.i157'
+
+class RegistroI200(models.Model):
+    _description = 'Concrete model for ecd I200'
+    _name = 'l10n_br_sped.ecd.i200'
+    _inherit = 'l10n_br_sped.ecd.9.i200'
+
+class RegistroI250(models.Model):
+    _description = 'Concrete model for ecd I250'
+    _name = 'l10n_br_sped.ecd.i250'
+    _inherit = 'l10n_br_sped.ecd.9.i250'
+
+class RegistroI300(models.Model):
+    _description = 'Concrete model for ecd I300'
+    _name = 'l10n_br_sped.ecd.i300'
+    _inherit = 'l10n_br_sped.ecd.9.i300'
+
+class RegistroI310(models.Model):
+    _description = 'Concrete model for ecd I310'
+    _name = 'l10n_br_sped.ecd.i310'
+    _inherit = 'l10n_br_sped.ecd.9.i310'
+
+class RegistroI350(models.Model):
+    _description = 'Concrete model for ecd I350'
+    _name = 'l10n_br_sped.ecd.i350'
+    _inherit = 'l10n_br_sped.ecd.9.i350'
+
+class RegistroI355(models.Model):
+    _description = 'Concrete model for ecd I355'
+    _name = 'l10n_br_sped.ecd.i355'
+    _inherit = 'l10n_br_sped.ecd.9.i355'
+
+class RegistroI500(models.Model):
+    _description = 'Concrete model for ecd I500'
+    _name = 'l10n_br_sped.ecd.i500'
+    _inherit = 'l10n_br_sped.ecd.9.i500'
+
+class RegistroI510(models.Model):
+    _description = 'Concrete model for ecd I510'
+    _name = 'l10n_br_sped.ecd.i510'
+    _inherit = 'l10n_br_sped.ecd.9.i510'
+
+class RegistroI550(models.Model):
+    _description = 'Concrete model for ecd I550'
+    _name = 'l10n_br_sped.ecd.i550'
+    _inherit = 'l10n_br_sped.ecd.9.i550'
+
+    RZ_CONT = fields.Char()  # according to pdf specification
+
+    @api.model
+    def read_register(cls, line, version):
+        vals = {"RZ_CONT": "|".join(line.split("|")[2:][:-1])}
+        return vals
+
+    def generate_register_text(self, sped, version, line_count={}):
+        code = self._name[-4:].upper()
+        vals_list = self.read(["RZ_CONT", "reg_I555_ids"])
+        for vals in vals_list:
+            sped.write("\n|%s|" % (code,))
+            sped.write(vals["RZ_CONT"])
+            sped.write("|")
+            line_count[0] += 1
+            children = self.env["l10n_br_sped.ecd.i555"].search(
+                [("id", "in", vals["reg_I555_ids"])]
+            )
+            for child in children:
+                child.generate_register_text(
+                    sped, version, line_count
+                )
+        return sped
+
+class RegistroI555(models.Model):
+    _description = 'Concrete model for ecd I555'
+    _name = 'l10n_br_sped.ecd.i555'
+    _inherit = 'l10n_br_sped.ecd.9.i555'
+
+    RZ_CONT_TOT = fields.Char()  # according to pdf specification
+
+    @api.model
+    def read_register(cls, line, version):
+        vals = {"RZ_CONT_TOT": "|".join(line.split("|")[2:][:-1])}
+        return vals
+
+    def generate_register_text(self, sped, version, line_count={}):
+        code = self._name[-4:].upper()
+        keys = [i[0] for i in self._fields.items()]
+        vals_list = self.read(keys)
+        for vals in vals_list:
+            sped.write("\n|%s|" % (code,))
+            sped.write(vals.get("RZ_CONT_TOT", ""))
+            sped.write("|")
+            line_count[0] += 1
+
+class RegistroJ005(models.Model):
+    _description = 'Concrete model for ecd J005'
+    _name = 'l10n_br_sped.ecd.j005'
+    _inherit = 'l10n_br_sped.ecd.9.j005'
+
+class RegistroJ100(models.Model):
+    _description = 'Concrete model for ecd J100'
+    _name = 'l10n_br_sped.ecd.j100'
+    _inherit = 'l10n_br_sped.ecd.9.j100'
+
+class RegistroJ150(models.Model):
+    _description = 'Concrete model for ecd J150'
+    _name = 'l10n_br_sped.ecd.j150'
+    _inherit = 'l10n_br_sped.ecd.9.j150'
+
+class RegistroJ210(models.Model):
+    _description = 'Concrete model for ecd J210'
+    _name = 'l10n_br_sped.ecd.j210'
+    _inherit = 'l10n_br_sped.ecd.9.j210'
+
+class RegistroJ215(models.Model):
+    _description = 'Concrete model for ecd J215'
+    _name = 'l10n_br_sped.ecd.j215'
+    _inherit = 'l10n_br_sped.ecd.9.j215'
+
+class RegistroJ800(models.Model):
+    _description = 'Concrete model for ecd J800'
+    _name = 'l10n_br_sped.ecd.j800'
+    _inherit = 'l10n_br_sped.ecd.9.j800'
+
+class RegistroJ801(models.Model):
+    _description = 'Concrete model for ecd J801'
+    _name = 'l10n_br_sped.ecd.j801'
+    _inherit = 'l10n_br_sped.ecd.9.j801'
+
+class RegistroJ900(models.Model):
+    _description = 'Concrete model for ecd J900'
+    _name = 'l10n_br_sped.ecd.j900'
+    _inherit = 'l10n_br_sped.ecd.9.j900'
+
+class RegistroJ930(models.Model):
+    _description = 'Concrete model for ecd J930'
+    _name = 'l10n_br_sped.ecd.j930'
+    _inherit = 'l10n_br_sped.ecd.9.j930'
+
+class RegistroJ932(models.Model):
+    _description = 'Concrete model for ecd J932'
+    _name = 'l10n_br_sped.ecd.j932'
+    _inherit = 'l10n_br_sped.ecd.9.j932'
+
+class RegistroJ935(models.Model):
+    _description = 'Concrete model for ecd J935'
+    _name = 'l10n_br_sped.ecd.j935'
+    _inherit = 'l10n_br_sped.ecd.9.j935'
+
+class RegistroK030(models.Model):
+    _description = 'Concrete model for ecd K030'
+    _name = 'l10n_br_sped.ecd.k030'
+    _inherit = 'l10n_br_sped.ecd.9.k030'
+
+class RegistroK100(models.Model):
+    _description = 'Concrete model for ecd K100'
+    _name = 'l10n_br_sped.ecd.k100'
+    _inherit = 'l10n_br_sped.ecd.9.k100'
+
+class RegistroK110(models.Model):
+    _description = 'Concrete model for ecd K110'
+    _name = 'l10n_br_sped.ecd.k110'
+    _inherit = 'l10n_br_sped.ecd.9.k110'
+
+class RegistroK115(models.Model):
+    _description = 'Concrete model for ecd K115'
+    _name = 'l10n_br_sped.ecd.k115'
+    _inherit = 'l10n_br_sped.ecd.9.k115'
+
+class RegistroK200(models.Model):
+    _description = 'Concrete model for ecd K200'
+    _name = 'l10n_br_sped.ecd.k200'
+    _inherit = 'l10n_br_sped.ecd.9.k200'
+
+class RegistroK210(models.Model):
+    _description = 'Concrete model for ecd K210'
+    _name = 'l10n_br_sped.ecd.k210'
+    _inherit = 'l10n_br_sped.ecd.9.k210'
+
+class RegistroK300(models.Model):
+    _description = 'Concrete model for ecd K300'
+    _name = 'l10n_br_sped.ecd.k300'
+    _inherit = 'l10n_br_sped.ecd.9.k300'
+
+class RegistroK310(models.Model):
+    _description = 'Concrete model for ecd K310'
+    _name = 'l10n_br_sped.ecd.k310'
+    _inherit = 'l10n_br_sped.ecd.9.k310'
+
+class RegistroK315(models.Model):
+    _description = 'Concrete model for ecd K315'
+    _name = 'l10n_br_sped.ecd.k315'
+    _inherit = 'l10n_br_sped.ecd.9.k315'
+
+class Registro9900(models.Model):
+    _description = 'Concrete model for ecd 9900'
+    _name = 'l10n_br_sped.ecd.9900'
+    _inherit = 'l10n_br_sped.ecd.9.9900'
