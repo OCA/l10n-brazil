51a5bdee8ff926dbc186e4526873cb074c70759c
Raphaël Valyi <rvalyi@akretion.com>
2023-08-13 16:57:18 -0300

    [ADD] l10n_br_sped_ecd: extracted module

---
+++ l10n_br_sped_ecd/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 - TODAY Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "SPED ECD",
+    "description": """
+        Tabelas ECD do SPED""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "www.akretion.com",
+    "depends": ["base", "l10n_br_sped_base"],
+    "data": [
+        #        'security/ir.model.access.csv',
+        #        'views/sped_ecd.xml',
+    ],
+    "demo": [],
+}
---
+++ l10n_br_sped_ecd/models/__init__.py
@@ -0,0 +1,2 @@
+from . import sped_ecd_spec_9
+from . import sped_ecd
