8b77c404436f54252d9f1996a7b6613bf6f6833f
Raphaël Valyi <rvalyi@akretion.com>
2023-08-13 22:06:46 -0300

    [ADD] extracted l10n_br_sped_ecf module

---
+++ l10n_br_sped_ecf/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'SPED Spec',
+    'description': """
+        Tabelas do SPED (ECD, ECF, EFD e FCI), geridas a partir da lib python-sped""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Akretion',
+    'website': 'www.akretion.com',
+    'depends': ['l10n_br_sped_base'],
+    'data': [
+#        'security/ir.model.access.csv',
+#        'views/sped_ecf.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ l10n_br_sped_ecf/models/__init__.py
@@ -0,0 +1,2 @@
+from . import sped_ecf_spec_8
+from . import sped_ecf
