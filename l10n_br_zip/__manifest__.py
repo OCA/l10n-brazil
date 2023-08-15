e1daae0984fc29fc936cde9b50d138ec0ee7431c
Magno Costa <magno.costa@akretion.com.br>
2023-08-15 15:58:45 -0300

    [FIX] l10n_br_zip: ZIP search button in Company View.

--- l10n_br_zip/__manifest__.py
+++ l10n_br_zip/__manifest__.py
@@ -15,6 +15,7 @@
         "views/l10n_br_zip_view.xml",
         "views/res_partner_address_view.xml",
         "views/res_config_settings_view.xml",
+        "views/res_company_views.xml",
         "wizard/l10n_br_zip_search_view.xml",
         "security/ir.model.access.csv",
     ],
