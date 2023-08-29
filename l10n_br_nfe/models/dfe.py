42b42872bf8c6d6fbb321acd717d3ec21d73c87b
Felipe Zago <felipe.zago@kmee.com.br>
2023-08-29 17:21:18 -0300

    [FIX] parse procNFe xml

--- l10n_br_nfe/models/dfe.py
+++ l10n_br_nfe/models/dfe.py
@@ -128,4 +128,8 @@ class DFe(models.Model):
     @api.model
     def parse_procNFe(self, xml):
         binding = TnfeProc.from_xml(xml.read().decode())
-        return self.env["l10n_br_fiscal.document"].import_nfe_xml(binding)
+        return (
+            self.env["nfe.40.infnfe"]
+            .with_context(tracking_disable=True, edoc_type="in", dry_run=False)
+            .build_from_binding(binding.NFe.infNFe)
+        )
