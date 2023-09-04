634dc4569cb62991d62e1a2b9dfcaa82b1e27d97
Raphaël Valyi <rvalyi@akretion.com>
2023-09-04 03:38:19 -0300

    [REF] l10n_br_sped_efd_icms_ipi as generated

--- l10n_br_sped_efd_icms_ipi/__manifest__.py
+++ l10n_br_sped_efd_icms_ipi/__manifest__.py
@@ -1,20 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'SPED Spec',
-    'description': """
-        Tabelas do SPED (ECD, ECF, EFD e FCI), geridas a partir da lib python-sped""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Akretion',
-    'website': 'www.akretion.com',
-    'depends': ['l10n_br_sped_base'],
-    'data': [
-#        'security/ir.model.access.csv',
-#        'views/sped_efd_icms_ipi.xml',
+    "name": "SPED - EFD ICMS IPI",
+    "description": """
+        Tabelas do EFD ICMS IPI do SPED""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "development_status": "Alpha",
+    "maintainers": ["renatonlima", "rvalyi"],
+    "depends": ["l10n_br_sped_base", "l10n_br_account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sped_efd_icms_ipi.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
+    "application": True,
+    "post_init_hook": "post_init_hook",
 }
