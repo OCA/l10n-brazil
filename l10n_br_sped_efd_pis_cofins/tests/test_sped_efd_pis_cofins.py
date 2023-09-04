6e8240193dcb7d5645568d4df4296e55109aafa0
Raphaël Valyi <rvalyi@akretion.com>
2023-09-04 04:09:32 -0300

    [REF] l10n_br_sped_efd_pis_cofins: as generated

--- l10n_br_sped_efd_pis_cofins/tests/test_sped_efd_pis_cofins.py
+++ l10n_br_sped_efd_pis_cofins/tests/test_sped_efd_pis_cofins.py
@@ -1,14 +1,14 @@
-# Copyright 2022 Akretion - Raphael Valyi <raphael.valyi@akretion.com>
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+# Copyright 2023 - TODAY, Akretion - Raphael Valyi <raphael.valyi@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
 from os import path
 
 from odoo.tests import common
+
 from odoo.addons import l10n_br_sped_efd_pis_cofins
 
 
 class SpedTest(common.TransactionCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -17,12 +17,12 @@ class SpedTest(common.TransactionCase):
     def test_import_efd_pis_cofins(self):
         self.env["l10n_br_sped.mixin"].flush_registers("efd_pis_cofins")
         file_path = path.join(self.demo_path, "demo_efd_pis_cofins_multi.txt")
-        self.env["l10n_br_sped.mixin"].import_file(file_path, "efd_pis_cofins")
-        sped = self.env["l10n_br_sped.mixin"].generate_sped_text("efd_pis_cofins")
+        sped_mixin = self.env["l10n_br_sped.mixin"]
+        declaration = sped_mixin.import_file(file_path, "efd_pis_cofins")
+        sped = sped_mixin.generate_sped_text(declaration)
         with open(file_path) as f:
-            target_content = f.read()
-            # FIXME there is still an error with the D100 register and 
+            f.read()
+            # FIXME there is still an error with the D100 register and
             # COD_CTA coming either False either empty, appending an extra | or not.
+            print(sped)
             # self.assertEqual(sped.strip(), target_content.strip())
-            pass
-        # print(sped)
