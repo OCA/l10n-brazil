7a5944074a18aeb57e077bb7dcbbf75d41e48c77
Raphaël Valyi <rvalyi@akretion.com>
2023-09-04 03:38:19 -0300

    [REF] l10n_br_sped_efd_icms_ipi as generated

--- l10n_br_sped_efd_icms_ipi/tests/test_sped_efd_icms_ipi.py
+++ l10n_br_sped_efd_icms_ipi/tests/test_sped_efd_icms_ipi.py
@@ -1,14 +1,14 @@
-# Copyright 2022 Akretion - Raphael Valyi <raphael.valyi@akretion.com>
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+# Copyright 2023 - TODAY, Akretion - Raphael Valyi <raphael.valyi@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
 from os import path
 
 from odoo.tests import common
+
 from odoo.addons import l10n_br_sped_efd_icms_ipi
 
 
 class SpedTest(common.TransactionCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -17,9 +17,10 @@ class SpedTest(common.TransactionCase):
     def test_import_efd_icms_ipi(self):
         self.env["l10n_br_sped.mixin"].flush_registers("efd_icms_ipi")
         file_path = path.join(self.demo_path, "demo_efd_icms_ipi.txt")
-        self.env["l10n_br_sped.mixin"].import_file(file_path, "efd_icms_ipi")
-        sped = self.env["l10n_br_sped.mixin"].generate_sped_text("efd_icms_ipi")
+        sped_mixin = self.env["l10n_br_sped.mixin"]
+        declaration = sped_mixin.import_file(file_path, "efd_icms_ipi")
+        sped = sped_mixin.generate_sped_text(declaration)
         with open(file_path) as f:
             target_content = f.read()
+            # print(sped)
             self.assertEqual(sped.strip(), target_content.strip())
-        # print(sped)
