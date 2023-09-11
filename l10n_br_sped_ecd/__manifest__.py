de493b2359bd5fd27d2206036009040d45771b97
Raphaël Valyi <rvalyi@akretion.com>
2023-09-10 23:10:26 -0300

    [ADD] l10n_br_sped_ecd: readme

--- l10n_br_sped_ecd/__manifest__.py
+++ l10n_br_sped_ecd/__manifest__.py
@@ -3,19 +3,23 @@
 
 {
     "name": "SPED - ECD",
-    "description": """
-        Tabelas ECD do SPED""",
+    "summary": """
+        Arquivo ECD do SPED""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "development_status": "Alpha",
-    "maintainers": ["renatonlima", "rvalyi"],
+    "maintainers": ["rvalyi", "renatonlima"],
     "depends": ["l10n_br_sped_base", "l10n_br_account"],
+    "external_dependencies": {
+        "python": [
+            "erpbrasil.base>=2.3.0",
+        ]
+    },
     "data": [
         "security/ir.model.access.csv",
         "views/sped_ecd.xml",
-        #        "views/sped_declaration_ecd.xml",
     ],
     "demo": [],
     "application": True,
