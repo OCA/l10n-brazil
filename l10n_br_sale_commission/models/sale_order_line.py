4992ad2f2357f48d8a1cfea718c3f76d6cddea02
Bruno <bruno.botti@kmee.com.br>
2023-09-11 11:55:01 -0300

    [FIX] onchange cfop method

--- l10n_br_sale_commission/models/sale_order_line.py
+++ l10n_br_sale_commission/models/sale_order_line.py
@@ -17,4 +17,4 @@ class SaleOrderLine(models.Model):
         super()._onchange_fiscal_operation_line_id()
         for record in self:
             if record.cfop_id and not record.cfop_id.finance_move:
-                record.agents = False
+                record.agent_ids = False
