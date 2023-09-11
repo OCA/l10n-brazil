0423ceadc84d9b94555e968c150c7a1a00b33602
Raphaël Valyi <rvalyi@akretion.com>
2023-09-10 23:11:08 -0300

    [ADD] l10n_br_sped_efd_icms_ipi: readme

--- l10n_br_sped_efd_icms_ipi/__manifest__.py
+++ l10n_br_sped_efd_icms_ipi/__manifest__.py
@@ -3,15 +3,20 @@
 
 {
     "name": "SPED - EFD ICMS IPI",
-    "description": """
-        Tabelas do EFD ICMS IPI do SPED""",
+    "summary": """
+        Arquivo EFD ICMS IPI do SPED / SPED Fiscal""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "development_status": "Alpha",
-    "maintainers": ["renatonlima", "rvalyi"],
+    "maintainers": ["rvalyi", "renatonlima"],
     "depends": ["l10n_br_sped_base", "l10n_br_account"],
+    "external_dependencies": {
+        "python": [
+            "erpbrasil.base>=2.3.0",
+        ]
+    },
     "data": [
         "security/ir.model.access.csv",
         "views/sped_efd_icms_ipi.xml",
