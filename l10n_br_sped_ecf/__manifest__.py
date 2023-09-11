33a987aa5de6b7645e3a95f1becacc3837791f80
Raphaël Valyi <rvalyi@akretion.com>
2023-09-10 23:10:42 -0300

    [ADD] l10n_br_sped_ecf: readme

--- l10n_br_sped_ecf/__manifest__.py
+++ l10n_br_sped_ecf/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     "name": "SPED - ECF",
-    "description": """
-        Tabelas ECF""",
+    "summary": """
+        Arquivo ECF do SPED""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "development_status": "Alpha",
-    "maintainers": ["renatonlima", "rvalyi"],
+    "maintainers": ["rvalyi", "renatonlima"],
     "depends": ["l10n_br_sped_base", "l10n_br_account"],
     "data": [
         "security/ir.model.access.csv",
