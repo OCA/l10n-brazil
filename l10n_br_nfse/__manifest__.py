cfed99905339647a99b833488a693cb6a206e143
Felipe Zago <felipe.zago@kmee.com.br>
2023-09-14 17:54:59 -0300

    [FIX] l10n_br_nfse: erpbrasil.transmissão dependency

--- l10n_br_nfse/__manifest__.py
+++ l10n_br_nfse/__manifest__.py
@@ -14,7 +14,7 @@
         "python": [
             "erpbrasil.edoc>=2.5.2",
             "erpbrasil.assinatura>=1.7.0",
-            "erpbrasil.transmissao",
+            "erpbrasil.transmissao>=1.1.0",
             "erpbrasil.base>=2.3.0",
         ],
     },
