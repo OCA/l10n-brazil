71d8ec0beb72463acb99b81d1702df2d43c8c131
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-09-19 16:25:23 -0300

    [FIX] l10n_br_base: fix inverse_state

--- l10n_br_base/models/res_company.py
+++ l10n_br_base/models/res_company.py
@@ -54,12 +54,15 @@ class Company(models.Model):
         for company in self:
             company.partner_id.cnpj_cpf = company.cnpj_cpf
 
+    def _inverse_inscr_est(self):
+        """Write the l10n_br specific functional fields."""
+        for company in self:
+            company.partner_id.inscr_est = company.inscr_est
+
     def _inverse_state(self):
         """Write the l10n_br specific functional fields."""
         for company in self:
-            company.partner_id.write(
-                {"state_id": company.state_id.id, "inscr_est": company.inscr_est}
-            )
+            company.partner_id.state_id = company.state_id
 
     def _inverse_state_tax_number_ids(self):
         """Write the l10n_br specific functional fields."""
@@ -119,7 +122,7 @@ class Company(models.Model):
 
     inscr_est = fields.Char(
         compute="_compute_address",
-        inverse="_inverse_state",
+        inverse="_inverse_inscr_est",
     )
 
     state_tax_number_ids = fields.One2many(
@@ -160,3 +163,11 @@ class Company(models.Model):
                 raise e
 
         return result
+
+    @api.onchange("state_id")
+    def _onchange_state_id(self):
+        for record in self:
+            super()._onchange_state_id()
+            record.inscr_est = False
+            record.partner_id.inscr_est = False
+            record.partner_id.state_id = record.state_id
