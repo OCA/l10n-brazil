633d9737653aa777a4837018cba961f5e9013e1b
Felipe Zago <felipe.zago@kmee.com.br>
2023-09-26 15:43:25 -0300

    [FIX] l10n_br_nfe: adapt code to work with multiple document types.

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -76,6 +76,7 @@ class NFe(spec_models.StackedModel):
     _name = "l10n_br_fiscal.document"
     _inherit = ["l10n_br_fiscal.document", "nfe.40.infnfe", "nfe.40.fat"]
     _stacked = "nfe.40.infnfe"
+    _binding_module = "nfelib.nfe.bindings.v4_0.leiaute_nfe_v4_00"
     _field_prefix = "nfe40_"
     _schema_name = "nfe"
     _schema_version = "4.0.0"
@@ -869,6 +870,9 @@ class NFe(spec_models.StackedModel):
         return edocs
 
     def _processador(self):
+        if self.document_type not in (MODELO_FISCAL_NFE, MODELO_FISCAL_NFCE):
+            return super()._processador()
+
         self._check_nfe_environment()
         certificado = self.env.company._get_br_ecertificate()
         session = Session()
@@ -972,6 +976,10 @@ class NFe(spec_models.StackedModel):
 
     def _valida_xml(self, xml_file):
         self.ensure_one()
+
+        if self.document_type not in (MODELO_FISCAL_NFE, MODELO_FISCAL_NFCE):
+            return super()._valida_xml(xml_file)
+
         erros = Nfe.schema_validation(xml_file)
         erros = "\n".join(erros)
         self.write({"xml_error_message": erros or False})
@@ -994,7 +1002,11 @@ class NFe(spec_models.StackedModel):
         super()._exec_after_SITUACAO_EDOC_AUTORIZADA(old_state, new_state)
 
     def _generate_key(self):
-        for record in self.filtered(filter_processador_edoc_nfe):
+        records = self.filtered(filter_processador_edoc_nfe)
+        if not records:
+            return super()._generate_key()
+
+        for record in records:
             date = fields.Datetime.context_timestamp(record, record.document_date)
 
             required_fields_gen_edoc = []
@@ -1041,8 +1053,8 @@ class NFe(spec_models.StackedModel):
                     "l10n_br_fiscal.document.supplement"
                 ].create(
                     {
-                        "nfe40_qrCode": self.get_nfce_qrcode(),
-                        "nfe40_urlChave": self.get_nfce_qrcode_url(),
+                        "qrcode": self.get_nfce_qrcode(),
+                        "url_key": self.get_nfce_qrcode_url(),
                     }
                 )
 
--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -1,18 +1,24 @@
 # Copyright 2023 KMEE (Felipe Zago Rodrigues <felipe.zago@kmee.com.br>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import fields
 
 from odoo.addons.spec_driven_model.models import spec_models
 
 
 class NFeSupplement(spec_models.StackedModel):
     _name = "l10n_br_fiscal.document.supplement"
+    _inherit = ["l10n_br_fiscal.document.supplement", "nfe.40.infnfesupl"]
     _description = "NFe Supplement Document"
-    _inherit = "nfe.40.infnfesupl"
     _stacked = "nfe.40.infnfesupl"
+    _binding_module = "nfelib.nfe.bindings.v4_0.leiaute_nfe_v4_00"
     _field_prefix = "nfe40_"
     _schema_name = "nfe"
     _schema_version = "4.0.0"
     _odoo_module = "l10n_br_nfe"
     _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
     _spec_tab_name = "NFe"
+
+    nfe40_qrCode = fields.Char(related="qrcode")
+
+    nfe40_urlChave = fields.Char(related="url_key")
