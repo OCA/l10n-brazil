fbaa73753651d9693f3bcd8935fe729e9b3f616c
Magno Costa <magno.costa@akretion.com.br>
2023-09-28 18:03:24 -0300

    l10n_br_base: Removed unnecessary License Header in __init__ files when there are only imports.

--- l10n_br_base/models/__init__.py
+++ l10n_br_base/models/__init__.py
@@ -1,6 +1,3 @@
-# Copyright (C) 2015  Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from . import format_address_mixin
 from . import res_bank
 from . import res_city
