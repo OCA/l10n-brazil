ca1d572e0bdc1364a9cf49d8759f2fae97d89f83
Magno Costa <magno.costa@akretion.com.br>
2023-09-29 18:24:31 -0300

    [DOC] l10n_br_account_payment_brcobranca: Removed unnecessary License Header in __init__ files when there are only imports.

--- l10n_br_account_payment_brcobranca/models/__init__.py
+++ l10n_br_account_payment_brcobranca/models/__init__.py
@@ -1,7 +1,3 @@
-# Copyright 2017 Akretion
-# @author Raphaël Valyi <raphael.valyi@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import account_move_line
 from . import account_move
 from . import account_payment_order
