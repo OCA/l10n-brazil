115bb4591e324bb3f1a9148cb10c9648d30358fd
Felipe Zago <felipe.zago@kmee.com.br>
2023-10-03 12:07:29 -0300

    [ADD] l10n_br_nfe: add _binding_module and _field_prefix to models

--- l10n_br_nfe/models/document_line.py
+++ l10n_br_nfe/models/document_line.py
@@ -72,6 +72,7 @@ class NFeLine(spec_models.StackedModel):
     _schema_version = "4.0.0"
     _odoo_module = "l10n_br_nfe"
     _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
+    _binding_module = "nfelib.nfe.bindings.v4_0.leiaute_nfe_v4_00"
     _spec_tab_name = "NFe"
     _stacking_points = {}
     # all m2o below this level will be stacked even if not required:
