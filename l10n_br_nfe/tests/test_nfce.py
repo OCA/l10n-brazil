b77e2387760f6efbf9d835754e5e21509f3247d9
Felipe Zago <felipe.zago@kmee.com.br>
2023-10-05 17:02:11 -0300

    [ADD] l10n_br_nfe: processor tests

--- l10n_br_nfe/tests/test_nfce.py
+++ l10n_br_nfe/tests/test_nfce.py
@@ -154,6 +154,12 @@ class TestNFCe(TestNFeExport):
         self.assertIsNotNone(qr_code)
         self.assertIsNotNone(qr_code_url)
 
+    def test_processor(self):
+        self.document_id.document_type_id = False
+
+        processador = self.document_id._processador()
+        self.assertIsNone(processador)
+
     @nfe_mock({"nfeAutorizacaoLote": "retEnviNFe/autorizada.xml"})
     def test_prepare_nfce_payment(self):
         amount = self.document_id.amount_financial_total / 2
