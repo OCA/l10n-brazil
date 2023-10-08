0f844fa4503aae0b85e126696f8a6542f5259456
Raphaël Valyi <rvalyi@akretion.com>
2023-10-08 11:57:05 -0300

    [DEL] already covered by customer_invoice_dummy

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -8,5 +8,4 @@ from . import test_customer_invoice_dummy
 from . import test_document_date
 from . import test_invoice_refund
 from . import test_move_discount
-from . import test_supplier_invoice_dummy
 from . import test_multi_localizations_invoice
