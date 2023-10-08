ac0a5f1d6ca223c297e12a4229e4869f8960ca7c
Raphaël Valyi <rvalyi@akretion.com>
2023-10-08 14:41:00 -0300

    [REF] renamed non fiscal move test

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -4,7 +4,7 @@ from . import common
 from . import test_account_move_sn
 from . import test_account_move_lc
 from . import test_account_taxes
-from . import test_customer_invoice_dummy
+from . import test_non_fiscal_move
 from . import test_document_date
 from . import test_invoice_refund
 from . import test_move_discount
