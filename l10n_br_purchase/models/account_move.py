54d1695eea076c2f715c4686331533baba1994ac
Magno Costa <magno.costa@akretion.com.br>
2023-10-27 17:58:20 -0300

    [REF] l10n_br_purchase: Adapted module to make compatible with the international cases.

--- l10n_br_purchase/models/account_move.py
+++ l10n_br_purchase/models/account_move.py
@@ -11,7 +11,10 @@ class AccountMove(models.Model):
     @api.onchange("purchase_vendor_bill_id", "purchase_id")
     def _onchange_purchase_auto_complete(self):
         if self.purchase_id:
-            self.fiscal_operation_id = self.purchase_id.fiscal_operation_id
-            if not self.document_type_id:
-                self.document_type_id = self.company_id.document_type_id
+            if self.purchase_id.fiscal_operation_id:
+                self.fiscal_operation_id = self.purchase_id.fiscal_operation_id
+                if not self.document_type_id:
+                    # Testes não passam por aqui, qual seria esse caso de uso?
+                    # Porque se não houver o codigo pode ser removido
+                    self.document_type_id = self.company_id.document_type_id
         return super()._onchange_purchase_auto_complete()
