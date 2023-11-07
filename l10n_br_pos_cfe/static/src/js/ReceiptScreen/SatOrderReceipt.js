504bcaed275998d13c4c49a8b6360074b324bcac
Diego Paradeda <paradeda.diego@gmail.com>
2023-11-07 14:16:29 -0300

    [ADD] taxes and aprox taxes fields in cfe

--- l10n_br_pos_cfe/static/src/js/ReceiptScreen/SatOrderReceipt.js
+++ l10n_br_pos_cfe/static/src/js/ReceiptScreen/SatOrderReceipt.js
@@ -20,6 +20,39 @@ odoo.define("l10n_br_pos_cfe.SatOrderReceipt", function (require) {
                     this.rounding
                 ).toFixed(2);
             }
+
+            get est_state_tax_total() {
+                let state_tax = 0;
+                const lines = this.props.order.get_orderlines();
+                for (var i = 0, len = lines.length; i < len; i++) {
+                    const line_price = lines[i].price * lines[i].quantity;
+                    const product_tmpl_id = lines[i].product.product_tmpl_id;
+                    const map = this.props.order.pos.fiscal_map_by_template_id[
+                        product_tmpl_id
+                    ];
+
+                    state_tax += (line_price * map.icms_percent) / 100;
+                }
+
+                return state_tax;
+            }
+
+            get est_federal_tax_total() {
+                let federal_tax = 0;
+                const lines = this.props.order.get_orderlines();
+                for (var i = 0, len = lines.length; i < len; i++) {
+                    const line_price = lines[i].price * lines[i].quantity;
+                    const product_tmpl_id = lines[i].product.product_tmpl_id;
+                    const map = this.props.order.pos.fiscal_map_by_template_id[
+                        product_tmpl_id
+                    ];
+
+                    federal_tax += (line_price * map.pis_percent) / 100;
+                    federal_tax += (line_price * map.cofins_percent) / 100;
+                }
+
+                return federal_tax;
+            }
         };
 
     Registries.Component.extend(OrderReceipt, SatOrderReceipt);
