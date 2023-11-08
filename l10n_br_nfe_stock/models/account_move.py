67baad4f931d2826aedde7b1140e4cddc4e835e0
Renan Hiroki Bastos <hirokibastos@gmail.com>
2023-11-08 13:48:58 -0300

    [FIX] Add actions to account move

---
+++ l10n_br_nfe_stock/models/account_move.py
@@ -0,0 +1,34 @@
+# Copyright 2023 KMEE (Renan Hiroki Bastos <renan.hiroki@kmee.com.br>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def action_open_purchase(self):
+        result = self.env.ref("l10n_br_nfe_stock.action_purchase_tree_all").read()[0]
+        purchase_ids = self.mapped("linked_purchase_ids")
+
+        if len(purchase_ids) == 1:
+            result = self.env.ref("l10n_br_nfe_stock.action_purchase_form_all").read()[
+                0
+            ]
+            result["res_id"] = purchase_ids[0].id
+        else:
+            result["domain"] = "[('id', 'in', %s)]" % (purchase_ids.ids)
+
+        return result
+
+    def action_open_picking(self):
+        result = self.env.ref("l10n_br_nfe_stock.action_picking_tree_all").read()[0]
+        picking_ids = self.mapped("linked_picking_ids")
+
+        if len(picking_ids) == 1:
+            result = self.env.ref("l10n_br_nfe_stock.action_picking_form_all").read()[0]
+            result["res_id"] = picking_ids[0].id
+        else:
+            result["domain"] = "[('id', 'in', %s)]" % (picking_ids.ids)
+
+        return result
