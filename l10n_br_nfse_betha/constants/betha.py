8aad9eca1da357ab777f8ec2b4c96762c44d62a5
Antônio Neto <neto@engenere.one>
2023-11-07 22:13:32 -0300

    [ADD] l10n_br_nfse_betha: nfse with betha provider

---
+++ l10n_br_nfse_betha/constants/betha.py
@@ -0,0 +1,4 @@
+RECEPCIONAR_LOTE_RPS = ["EnviarLoteRpsEnvio"]
+CONSULTAR_SITUACAO_LOTE_RPS = ["ConsultarSituacaoLoteRpsEnvio"]
+CANCELAR_NFSE = ["CancelarNfseEnvio"]
+CONSULTAR_NFSE_POR_RPS = ["ConsultarNfsePorRpsEnvio"]
