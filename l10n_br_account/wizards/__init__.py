55f08d84874c0afba0c97e53e582b08a01719de1
Raphaël Valyi <rvalyi@akretion.com>
2023-11-09 01:36:55 -0300

    [IMP] import account.move with NFe/other edocs

--- l10n_br_account/wizards/__init__.py
+++ l10n_br_account/wizards/__init__.py
@@ -1,3 +1,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import account_move_reversal
+from . import wizard_document_cancel
+from . import wizard_document_correction
+from . import wizard_document_invalidate
+from . import document_import_wizard_mixin
