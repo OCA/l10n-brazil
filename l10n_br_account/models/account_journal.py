76e6238204ca30e39d928a7705f023cd2a7425a9
Raphaël Valyi <rvalyi@akretion.com>
2023-11-09 01:38:05 -0300

    [IMP] import fiscal documents with native upload button

--- l10n_br_account/models/account_journal.py
+++ l10n_br_account/models/account_journal.py
@@ -1,12 +1,16 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# Copyright 2023 Akretion (Raphaẽl Valyi <raphael.valyi@akretion.com.br>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import models
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    revenue_expense = fields.Boolean(
-        string="Gera Financeiro",
-    )
+    def create_invoice_from_attachment(self, attachment_ids=None):
+        if self.env.company.country_id.code != "BR" or len(attachment_ids) < 1:
+            return super().create_invoice_from_attachment(attachment_ids=attachment_ids)
+        attachments = self.env["ir.attachment"].browse(attachment_ids)
+        return self.env[
+            "l10n_br_fiscal.document.import.wizard.mixin"
+        ]._get_importer_action(attachments)
