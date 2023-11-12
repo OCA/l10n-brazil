ef669d4a325a17f2050fcbd70c0d5a3f4d3fd5ef
Antônio Neto <neto@engenere.one>
2023-11-12 14:00:43 -0300

    [ADD] l10n_br_account_nfse

---
+++ l10n_br_account_nfse/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022-Today - Engenere (<https://engenere.one>).
+# @author Antônio S. Pereira Neto <neto@engenere.one>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account NFS-e Integration",
+    "summary": "Integration between l10n_br_account and l10n_br_nfse",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "Engenere, Odoo Community Association (OCA)",
+    "maintainers": ["antoniospneto"],
+    "website": "https://github.com/OCA/l10n-brazil",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "depends": [
+        "l10n_br_nfse",
+        "l10n_br_account",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
