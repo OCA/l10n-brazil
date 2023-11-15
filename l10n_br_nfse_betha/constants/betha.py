c9052fe36141029027388f72853a19b7587494a4
Antônio Neto <neto@engenere.one>
2023-11-14 22:34:35 -0300

    [FIX] l10n_br_nfse_betha: service name

--- l10n_br_nfse_betha/constants/betha.py
+++ l10n_br_nfse_betha/constants/betha.py
@@ -1,4 +1,4 @@
 RECEPCIONAR_LOTE_RPS = ["EnviarLoteRpsEnvio"]
-CONSULTAR_SITUACAO_LOTE_RPS = ["ConsultarSituacaoLoteRpsEnvio"]
+CONSULTAR_SITUACAO_LOTE_RPS = ["ConsultarLoteRpsEnvio"]
 CANCELAR_NFSE = ["CancelarNfseEnvio"]
 CONSULTAR_NFSE_POR_RPS = ["ConsultarNfsePorRpsEnvio"]
