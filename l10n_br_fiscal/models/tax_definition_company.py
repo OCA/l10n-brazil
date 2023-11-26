297da1504699e688b7380941368f14fb21b38f8f
Raphaël Valyi <rvalyi@akretion.com>
2023-11-26 16:15:15 -0300

    [MIG] l10n_br_fiscal: Migration to 16.0
    [CI] disable W7930(file-not-used)

--- l10n_br_fiscal/models/tax_definition_company.py
+++ l10n_br_fiscal/models/tax_definition_company.py
@@ -5,6 +5,7 @@ from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
+# pylint: disable=consider-merging-classes-inherited
 class TaxDefinitionCompany(models.Model):
     _inherit = "l10n_br_fiscal.tax.definition"
 
