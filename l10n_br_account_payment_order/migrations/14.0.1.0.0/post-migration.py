ac8da20b355c4001ed131c3de223998505cfe070
Antônio Neto <neto@engenere.one>
2023-12-10 19:20:56 -0300

    [IMP] l10n_br_account_payment_order: major migration script

---
+++ l10n_br_account_payment_order/migrations/14.0.1.0.0/post-migration.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Engenere - Antônio S. P. Neto
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move am
+        SET instructions = ai.instructions
+        FROM account_invoice ai
+        WHERE ai.id = am.old_invoice_id AND ai.instructions IS NOT NULL
+        """,
+    )
