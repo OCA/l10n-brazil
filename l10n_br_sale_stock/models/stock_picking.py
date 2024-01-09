35874f26608faea308880c72c48feeccf442653b
Magno Costa <magno.costa@akretion.com.br>
2024-01-09 12:19:00 -0300

    [FIX] l10n_br_sale_stock: proper partner_id
    When Picking has a Sale Order related the Partner used to create the Invoice should be the partner_invoice_id of Sale, because the Partner of Picking can be the partner_shipping_id of Sale Order.
    
    [FIX] l10n_br_sale_stock: Get Fiscal Partner

--- l10n_br_sale_stock/models/stock_picking.py
+++ l10n_br_sale_stock/models/stock_picking.py
@@ -8,8 +8,21 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def _get_partner_to_invoice(self):
+        """
+        If the partner has some invoicing contact defined
+        partner_invoice_id is auto filled, but it can also be changed.
+        partner_invoice_id is used if different from partner_id
+        """
         self.ensure_one()
-        partner = self.partner_id
+        partner_id = super()._get_partner_to_invoice()
         if self.sale_id:
-            partner = self.sale_id.partner_invoice_id
-        return partner.address_get(["invoice"]).get("invoice")
+            if partner_id != self.sale_id.partner_invoice_id.id:
+                partner_id = self.sale_id.partner_invoice_id.id
+        return partner_id
+
+    def _get_fiscal_partner(self):
+        self.ensure_one()
+        partner = super()._get_fiscal_partner()
+        if partner != self._get_partner_to_invoice():
+            partner = self._get_partner_to_invoice()
+        return partner
