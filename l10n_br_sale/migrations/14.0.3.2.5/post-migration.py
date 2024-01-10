2936a478fc288e1a612c72ed7413efadca3b99cc
André Marcos Ferreira <andremarcosfe@gmail.com>
2024-01-10 05:09:06 -0300

    [FIX] pre-commit

--- l10n_br_sale/migrations/14.0.3.2.5/post-migration.py
+++ l10n_br_sale/migrations/14.0.3.2.5/post-migration.py
@@ -1,10 +1,10 @@
 from openupgradelib import openupgrade
 
 _field_renames = [
-    ('res.company', 'res_company', 'delivery_costs', None),
+    ("res.company", "res_company", "delivery_costs", None),
 ]
 
 @openupgrade.migrate()
 def migrate(env, version):
-    if openupgrade.column_exists(env.cr, 'res_company', 'delivery_costs'):
+    if openupgrade.column_exists(env.cr, "res_company", "delivery_costs"):
         openupgrade.drop_columns(env.cr, _field_renames)
