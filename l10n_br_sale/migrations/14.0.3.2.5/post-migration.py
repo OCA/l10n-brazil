c3452c7d7982d6c841d7e4491a475825f8abb4c6
Andre Marcos Ferreira <andremarcosfe@gmail.com>
2024-03-07 09:26:17 -0300

    [FIX] rename variable to fields_remove

--- l10n_br_sale/migrations/14.0.3.2.5/post-migration.py
+++ l10n_br_sale/migrations/14.0.3.2.5/post-migration.py
@@ -1,10 +1,10 @@
 from openupgradelib import openupgrade
 
-_field_renames = [
+_fields_remove = [
     ("res.company", "res_company", "delivery_costs", None),
 ]
 
 @openupgrade.migrate()
 def migrate(env, version):
     if openupgrade.column_exists(env.cr, "res_company", "delivery_costs"):
-        openupgrade.drop_columns(env.cr, _field_renames)
+        openupgrade.drop_columns(env.cr, _fields_remove)
