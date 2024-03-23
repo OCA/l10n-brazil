829d0e8ba2fe6dbb3a959f87c6bfdbdad08a727d
Raphaël Valyi <rvalyi@akretion.com>
2024-03-23 19:34:36 -0300

    [MIG] l10n_br_nfse_focus: Migration to 16.0

--- l10n_br_nfse_focus/__manifest__.py
+++ l10n_br_nfse_focus/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "NFS-e (FocusNFE)",
     "summary": """
         NFS-e (FocusNFE)""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE, Escodoo, Odoo Community Association (OCA)",
     "maintainers": [
