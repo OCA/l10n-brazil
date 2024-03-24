8f296b6cff0940526c135e6c2429370378d90f77
Raphaël Valyi <rvalyi@akretion.com>
2024-03-24 13:02:32 -0300

    [REF] l10n_br_account use setUpClass

--- l10n_br_account/tests/test_move_discount.py
+++ l10n_br_account/tests/test_move_discount.py
@@ -5,55 +5,56 @@ from odoo.tests import TransactionCase
 
 
 class TestInvoiceDiscount(TransactionCase):
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        self.company = self.env.ref("l10n_br_base.empresa_lucro_presumido")
+        cls.company = cls.env.ref("l10n_br_base.empresa_lucro_presumido")
 
         # set default user company
-        companies = self.env["res.company"].search([])
-        self.env.user.company_ids = [(6, 0, companies.ids)]
-        self.env.user.company_id = self.company
+        companies = cls.env["res.company"].search([])
+        cls.env.user.company_ids = [(6, 0, companies.ids)]
+        cls.env.user.company_id = cls.company
 
-        self.invoice_account_id = self.env["account.account"].create(
+        cls.invoice_account_id = cls.env["account.account"].create(
             {
-                "company_id": self.company.id,
-                "user_type_id": self.env.ref("account.data_account_type_receivable").id,
+                "company_id": cls.company.id,
+                "user_type_id": cls.env.ref("account.data_account_type_receivable").id,
                 "code": "RECTEST",
                 "name": "Test receivable account",
                 "reconcile": True,
             }
         )
 
-        self.invoice_journal = self.env["account.journal"].create(
+        cls.invoice_journal = cls.env["account.journal"].create(
             {
-                "company_id": self.company.id,
+                "company_id": cls.company.id,
                 "name": "Invoice Journal - (test)",
                 "code": "INVTEST",
                 "type": "sale",
             }
         )
 
-        self.invoice_line_account_id = self.env["account.account"].create(
+        cls.invoice_line_account_id = cls.env["account.account"].create(
             {
-                "company_id": self.company.id,
-                "user_type_id": self.env.ref("account.data_account_type_revenue").id,
+                "company_id": cls.company.id,
+                "user_type_id": cls.env.ref("account.data_account_type_revenue").id,
                 "code": "705070",
                 "name": "Product revenue account (test)",
             }
         )
 
-        self.fiscal_operation_id = self.env.ref("l10n_br_fiscal.fo_venda")
-        self.fiscal_operation_id.deductible_taxes = True
+        cls.fiscal_operation_id = cls.env.ref("l10n_br_fiscal.fo_venda")
+        cls.fiscal_operation_id.deductible_taxes = True
 
-        product_id = self.env.ref("product.product_product_7")
+        product_id = cls.env.ref("product.product_product_7")
 
         invoice_line_vals = [
             (
                 0,
                 0,
                 {
-                    "account_id": self.invoice_line_account_id.id,
+                    "account_id": cls.invoice_line_account_id.id,
                     "product_id": product_id.id,
                     "quantity": 1,
                     "price_unit": 1000.0,
@@ -62,20 +63,20 @@ class TestInvoiceDiscount(TransactionCase):
             )
         ]
 
-        self.move_id = (
-            self.env["account.move"]
+        cls.move_id = (
+            cls.env["account.move"]
             .with_context(check_move_validity=False)
             .create(
                 {
-                    "company_id": self.company.id,
-                    "document_serie_id": self.env.ref(
+                    "company_id": cls.company.id,
+                    "document_serie_id": cls.env.ref(
                         "l10n_br_fiscal.empresa_lc_document_55_serie_1"
                     ).id,
-                    "journal_id": self.invoice_journal.id,
-                    "invoice_user_id": self.env.user.id,
-                    "fiscal_operation_id": self.fiscal_operation_id,
+                    "journal_id": cls.invoice_journal.id,
+                    "invoice_user_id": cls.env.user.id,
+                    "fiscal_operation_id": cls.fiscal_operation_id,
                     "move_type": "out_invoice",
-                    "currency_id": self.company.currency_id.id,
+                    "currency_id": cls.company.currency_id.id,
                     "invoice_line_ids": invoice_line_vals,
                 }
             )
