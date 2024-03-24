e82dda0632fc5ae1363ccc2bcac94ed51466301c
Raphaël Valyi <rvalyi@akretion.com>
2024-03-24 13:52:30 -0300

    [IMP] l10n_br_base unaccent=False

--- l10n_br_base/models/res_country_state.py
+++ l10n_br_base/models/res_country_state.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class CountryState(models.Model):
     _inherit = "res.country.state"
 
-    ibge_code = fields.Char(string="IBGE Code", size=2)
+    ibge_code = fields.Char(string="IBGE Code", size=2, unaccent=False)
