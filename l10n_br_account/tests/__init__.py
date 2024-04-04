034156d4ff8b5cb6e01c36c579f701be051ecd0f
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-04-04 16:56:36 -0300

    [IMP] l10n_br_account: add create wh invoice test

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -9,3 +9,4 @@ from . import test_document_date
 from . import test_invoice_refund
 from . import test_move_discount
 from . import test_multi_localizations_invoice
+from . import test_account_wh_invoice
