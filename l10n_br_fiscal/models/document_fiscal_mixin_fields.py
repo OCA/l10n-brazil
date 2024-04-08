c0bcf98f20e1ba4479019b8871e096d35203561b
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-04-08 11:24:18 -0300

    [ADD] l10n_br_fiscal add amount_icmsfcpst_value

--- l10n_br_fiscal/models/document_fiscal_mixin_fields.py
+++ l10n_br_fiscal/models/document_fiscal_mixin_fields.py
@@ -154,6 +154,12 @@ class FiscalDocumentMixinFields(models.AbstractModel):
         store=True,
     )
 
+    amount_icmsfcpst_value = fields.Monetary(
+        string="ICMS FCP ST Value",
+        compute="_compute_amount",
+        store=True,
+    )
+
     amount_icms_destination_value = fields.Monetary(
         string="ICMS Destination Value",
         compute="_compute_amount",
