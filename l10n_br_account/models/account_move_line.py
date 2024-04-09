57a1a27f7461cff93a5053c7798601c0494c8cfa
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-04-09 15:35:34 -0300

    [RFC] l10n_br_account: extract withholding invoice features to l10n_br_account_withholding

--- l10n_br_account/models/account_move_line.py
+++ l10n_br_account/models/account_move_line.py
@@ -3,8 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 # pylint: disable=api-one-deprecated
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 from .account_move import InheritsCheckMuteLogger
 
@@ -99,12 +98,6 @@ class AccountMoveLine(models.Model):
 
     allow_csll_irpj = fields.Boolean(
         compute="_compute_allow_csll_irpj",
-    )
-
-    wh_move_line_id = fields.Many2one(
-        comodel_name="account.move.line",
-        string="WH Account Move Line",
-        ondelete="restrict",
     )
 
     discount = fields.Float(
@@ -261,13 +254,6 @@ class AccountMoveLine(models.Model):
             result = super().write(values)
 
         for line in self:
-            if line.wh_move_line_id and (
-                "quantity" in values or "price_unit" in values
-            ):
-                raise UserError(
-                    _("You cannot edit an invoice related to a withholding entry")
-                )
-
             cleaned_vals = line.move_id._cleanup_write_orm_values(line, values)
             if not cleaned_vals:
                 continue
