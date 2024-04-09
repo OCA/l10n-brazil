689d4f829aef8f00e40c4e3608ef5269fd28dd76
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-04-09 15:35:34 -0300

    [RFC] l10n_br_account: extract withholding invoice features to l10n_br_account_withholding

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2020 - TODAY Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
@@ -703,33 +702,6 @@ class AccountMove(models.Model):
 
         return new_moves
 
-    def _prepare_wh_invoice(self, move_line, fiscal_group):
-        wh_date_invoice = move_line.move_id.date
-        wh_due_invoice = wh_date_invoice.replace(day=fiscal_group.wh_due_day)
-        values = {
-            "partner_id": fiscal_group.partner_id.id,
-            "date": wh_date_invoice,
-            "date_due": wh_due_invoice + relativedelta(months=1),
-            "type": "in_invoice",
-            "account_id": fiscal_group.partner_id.property_account_payable_id.id,
-            "journal_id": move_line.journal_id.id,
-            "origin": move_line.move_id.name,
-        }
-        return values
-
-    def _prepare_wh_invoice_line(self, invoice, move_line):
-        values = {
-            "name": move_line.name,
-            "quantity": move_line.quantity,
-            "uom_id": move_line.product_uom_id,
-            "price_unit": abs(move_line.balance),
-            "move_id": invoice.id,
-            "account_id": move_line.account_id.id,
-            "wh_move_line_id": move_line.id,
-            "account_analytic_id": move_line.analytic_account_id.id,
-        }
-        return values
-
     def _finalize_invoices(self, invoices):
         for invoice in invoices:
             invoice.compute_taxes()
@@ -737,40 +709,6 @@ class AccountMove(models.Model):
                 # Use additional field helper function (for account extensions)
                 line._set_additional_fields(invoice)
             invoice._onchange_cash_rounding()
-
-    def create_wh_invoices(self):
-        for move in self:
-            for line in move.line_ids.filtered(lambda line: line.tax_line_id):
-                # Create Wh Invoice only for supplier invoice
-                if line.move_id and line.move_id.type != "in_invoice":
-                    continue
-
-                account_tax_group = line.tax_line_id.tax_group_id
-                if account_tax_group and account_tax_group.fiscal_tax_group_id:
-                    fiscal_group = account_tax_group.fiscal_tax_group_id
-                    if fiscal_group.tax_withholding:
-                        invoice = self.env["account.move"].create(
-                            self._prepare_wh_invoice(line, fiscal_group)
-                        )
-
-                        self.env["account.move.line"].create(
-                            self._prepare_wh_invoice_line(invoice, line)
-                        )
-
-                        self._finalize_invoices(invoice)
-                        invoice.action_post()
-
-    def _withholding_validate(self):
-        for m in self:
-            invoices = (
-                self.env["account.move.line"]
-                .search([("wh_move_line_id", "in", m.mapped("line_ids").ids)])
-                .mapped("move_id")
-            )
-            invoices.filtered(lambda i: i.state == "open").button_cancel()
-            invoices.filtered(lambda i: i.state == "cancel").button_draft()
-            invoices.invalidate_cache()
-            invoices.filtered(lambda i: i.state == "draft").unlink()
 
     def post(self, invoice=False):
         # TODO FIXME migrate: no more invoice keyword
@@ -788,9 +726,6 @@ class AccountMove(models.Model):
     def button_cancel(self):
         for doc in self.filtered(lambda d: d.document_type_id):
             doc.fiscal_document_id.action_document_cancel()
-        # Esse método é responsavel por verificar se há alguma fatura de impostos
-        # retidos associada a essa fatura e cancela-las também.
-        self._withholding_validate()
         return super().button_cancel()
 
     # TODO: Por ora esta solução contorna o problema
