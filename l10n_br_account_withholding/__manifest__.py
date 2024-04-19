07d737725c58e443162da58f13abcccbacbabd0e
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-04-19 13:24:19 -0300

    [MIG] l10n_br_account_withholding: Migration to 15.0

--- l10n_br_account_withholding/__manifest__.py
+++ l10n_br_account_withholding/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "L10n Br Account Withholding",
     "summary": """
         Brazilian Withholding Invoice Generator""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Akretion,Odoo Community Association (OCA)",
     "maintainers": ["marcelsavegnago", "renatonlima"],
