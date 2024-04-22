be33514955d47b6f65d97e0e8c32f1f9728d46e6
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-04-22 10:35:02 -0300

    [FIX] l10n_br_fiscal: compute icms_relief_value only if icms_relief_id is defined in the fiscal document line

--- l10n_br_fiscal/models/document_fiscal_line_mixin_methods.py
+++ l10n_br_fiscal/models/document_fiscal_line_mixin_methods.py
@@ -196,6 +196,7 @@ class FiscalDocumentLineMixinMethods(models.AbstractModel):
             icms_origin=self.icms_origin,
             icms_cst_id=self.icms_cst_id,
             ind_final=self.ind_final,
+            icms_relief_id=self.icms_relief_id,
         )
 
     @api.depends("tax_icms_or_issqn", "partner_is_public_entity")
@@ -761,6 +762,8 @@ class FiscalDocumentLineMixinMethods(models.AbstractModel):
         "icmssn_tax_id",
         "icmsst_tax_id",
         "icmsfcp_tax_id",
+        "icms_relief_id",
+        "icms_relief_value",
         "ipi_tax_id",
         "ii_tax_id",
         "pis_tax_id",
