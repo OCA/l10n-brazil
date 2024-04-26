93efd45305af0dc745d0500e1dd8819ad281080d
Magno Costa <magno.costa@akretion.com.br>
2024-04-26 18:06:52 -0300

    [REM] l10n_br_account_payment_order: Remove journal_entry_ref field, field come from extraction and keeped to avoid incompatibiles, but it's not be used for while.
    [UPD] Update l10n_br_account_payment_order.pot

--- l10n_br_account_payment_order/__manifest__.py
+++ l10n_br_account_payment_order/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Brazilian Payment Order",
-    "version": "14.0.5.2.0",
+    "version": "14.0.5.3.0",
     "license": "AGPL-3",
     "author": "KMEE, Akretion, Odoo Community Association (OCA)",
     "maintainers": ["mbcosta"],
