5377630e44109726fdca223abab7894fd8087d3a
Renato Lima <renato.lima@akretion.com>
2024-06-06 12:24:22 -0300

    [15.0][MIG] l10n_br_cnpj_search: Migration to 15.0

--- l10n_br_cnpj_search/__manifest__.py
+++ l10n_br_cnpj_search/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Brazilian Localization CNPJ Search",
     "summary": """
         Integração com os Webservices da ReceitaWS e SerPro""",
-    "version": "14.0.1.5.2",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
