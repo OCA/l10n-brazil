754a6253ca7b218c19cd0b3ca388207841bc7c07
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-07-22 11:02:00 -0300

    [WIP] l10n_br_delivery.. wip

--- l10n_br_delivery/models/fiscal_document_mixin.py
+++ l10n_br_delivery/models/fiscal_document_mixin.py
@@ -27,3 +27,39 @@ class FiscalDocumentMixin(models.AbstractModel):
         string="Carrier",
         ondelete="restrict",
     )
+
+    expected_date = fields.Datetime(
+        string="Expected Date",
+        help="Expected date for the goods to be delivered to the recipient.",
+    )
+
+    # Remetente
+    partner_sendering_id = fields.Many2one(
+        "res.partner",
+        string="Sender",
+        help="Responsible for sending the goods, usually the issuer of the NFe.",
+    )
+
+    # Expedidor
+    partner_shippering_id = fields.Many2one(
+        "res.partner",
+        string="Shipper",
+        help="The one responsible for delivering the cargo to the carrier when \
+            the shipment is not carried out by the sender.",
+    )
+
+    # # Destinatário
+    # partner_shipping_id = fields.Many2one(
+    #     "res.partner",
+    #     string="Recipient",
+    #     help="The one who receives the goods at the end of the transport \
+    #         route, can be an individual or a company.",
+    # )
+
+    # Recebedor
+    partner_receivering_id = fields.Many2one(
+        "res.partner",
+        string="Receiver",
+        help="Actor who receives the goods. He is considered an intermediary \
+            between the issuer and the final recipient.",
+    )
