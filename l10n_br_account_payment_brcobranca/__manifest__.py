c00bbb8297b4d7c84e3dd7572dee06302ff0aa80
Antônio Neto <neto@engenere.one>
2024-07-25 17:26:33 -0300

    [MIG] l10n_br_account_payment_brcobranca: Migration to 16.0

--- l10n_br_account_payment_brcobranca/__manifest__.py
+++ l10n_br_account_payment_brcobranca/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "L10n Br Account Payment BRCobranca",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, " "Odoo Community Association (OCA)",
     "maintainers": ["rvalyi", "mbcosta"],
