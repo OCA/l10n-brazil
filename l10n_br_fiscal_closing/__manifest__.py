def2c9d556d85fb9574e00cb6a30b0c7184411f9
Raphaël Valyi <rvalyi@akretion.com>
2024-08-14 03:50:40 +0000

    [REF] l10n_br_fiscal_closing: depend on l10n_br_fiscal_edi

--- l10n_br_fiscal_closing/__manifest__.py
+++ l10n_br_fiscal_closing/__manifest__.py
@@ -2,15 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "L10n Br Fiscal Closing",
-    "summary": """
-        Fechamento fiscal do periodo""",
-    "version": "16.0.1.0.1",
+    "name": "Fechamento fiscal do período",
+    "summary": "Period fiscal closing",
+    "version": "16.0.2.0.0",
     "license": "AGPL-3",
     "author": "KMEE,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
     "depends": [
-        "l10n_br_fiscal",
+        "l10n_br_fiscal_edi",
     ],
     "data": [
         "security/ir.model.access.csv",
