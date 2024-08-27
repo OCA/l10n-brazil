8d4952ace3285a6188c2e13a71006ee7cf0f4c31
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-08-27 11:11:24 -0300

    [IMP] l10n_br_delivery: rename proxy fields to avoid conflicts

--- l10n_br_delivery/models/fiscal_document_mixin.py
+++ l10n_br_delivery/models/fiscal_document_mixin.py
@@ -20,7 +20,10 @@ class FiscalDocumentMixin(models.AbstractModel):
         " transactions.",
     )
 
-    fiscal_incoterm_id = fields.Many2one(related="incoterm_id")
+    # proxy fields to enable writing the related (shadowed) fields
+    # to the fiscal doc line from the aml through the _inherits system
+    # despite they have the same names.
+    fiscal_proxy_incoterm_id = fields.Many2one(related="incoterm_id")
 
     carrier_id = fields.Many2one(
         comodel_name="delivery.carrier",
