a2249c46411de354625d19c1b8ac6ac0f1db6475
Antônio Neto <neto@engenere.one>
2024-08-29 20:54:26 -0300

    [FIX] l10n_br_base: get address view

--- l10n_br_base/models/format_address_mixin.py
+++ l10n_br_base/models/format_address_mixin.py
@@ -7,8 +7,12 @@ from odoo import models
 class FormatAddressMixin(models.AbstractModel):
     _inherit = "format.address.mixin"
 
-    def _fields_view_get_address(self, arch):
+    def _view_get_address(self, arch):
+        # By default, the functionality to dynamically alter the view of address fields
+        # only works for the `res.partner`. This workaround clears the model
+        # reference in the view, enabling any model that extends the abstract
+        # 'format.address.mixin' to have its address view dynamically modified as well.
         address_view_id = self.env.company.country_id.address_view_id.sudo()
         if address_view_id.model != self._name:
             address_view_id.model = None
-        return super()._fields_view_get_address(arch)
+        return super()._view_get_address(arch)
