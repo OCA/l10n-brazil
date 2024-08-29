bd77cc35049a1cf245ea342bd665658a784adbc8
Magno Costa <magno.costa@akretion.com.br>
2024-08-29 16:25:39 -0300

    [FIX] l10n_br_sale_stock: Get Default OP Fiscal

--- l10n_br_sale_stock/models/stock_picking.py
+++ l10n_br_sale_stock/models/stock_picking.py
@@ -26,3 +26,17 @@ class StockPicking(models.Model):
         if partner != self._get_partner_to_invoice():
             partner = self._get_partner_to_invoice()
         return partner
+
+    def _get_default_fiscal_operation(self):
+        fiscal_operation = super()._get_default_fiscal_operation()
+        if self.sale_id:
+            if self.sale_id.fiscal_operation_id:
+                # Evita a inconsistência de ter o Pedido de Vendas com uma
+                # OP Fiscal e a Ordem de Seleção outra, quando o campo
+                # invoice_state é alterado, o usuário pode alterar o campo
+                # mas dessa forma forçamos a decisão de não usar a mesma
+                # do Pedido.
+                if fiscal_operation != self.sale_id.fiscal_operation_id:
+                    fiscal_operation = self.sale_id.fiscal_operation_id
+
+        return fiscal_operation
