5558bfe54c5016b74b7155f514dba7b763cc47f6
Raphaël Valyi <rvalyi@akretion.com>
2024-08-31 04:20:52 +0000

    [REF] l10n_br_sale: fiscal_document rename

--- l10n_br_sale/models/__init__.py
+++ l10n_br_sale/models/__init__.py
@@ -1,8 +1,5 @@
-# Copyright (C) 2015  Renato Lima - Akretion
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from . import account_move
-from . import fiscal_document
+from . import document
 from . import res_company
 from . import res_config_settings
 from . import sale_order
