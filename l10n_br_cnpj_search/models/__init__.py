4bf89621f045ac6abab56c3677fe7d84f60a8f7f
Antônio Neto <neto@engenere.one>
2024-09-07 03:04:32 -0300

    l10n_br_cnpj_search: merge webservice classes
    resolve pre-commit:
    l10n_br_cnpj_search/models/serpro_webservice.py:19:
    [R8180(consider-merging-classes-inherited), SerproWebservice]
    Consider merging classes inherited to "l10n_br_cnpj_search.webservice.abstract" from l10n_br_cnpj_search/models/receitaws_webservice.py:11:4.

--- l10n_br_cnpj_search/models/__init__.py
+++ l10n_br_cnpj_search/models/__init__.py
@@ -3,7 +3,5 @@
 from . import l10n_br_base_party_mixin
 from . import res_config_settings
 from . import cnpj_webservice
-from . import receitaws_webservice
-from . import serpro_webservice
 from . import res_partner
 from . import res_company
