98e0bbf5570e89d4cc19fb0fa7f03e61e85889e1
Antônio Neto <neto@engenere.one>
2024-09-17 22:57:35 -0300

    [MIG] l10n_br_purchase_stock: Migration to 15.0

--- l10n_br_purchase_stock/__manifest__.py
+++ l10n_br_purchase_stock/__manifest__.py
@@ -7,7 +7,8 @@
     "category": "Localisation",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "14.0.1.3.1",
+    "version": "15.0.1.0.0",
+    "development_status": "Beta",
     "depends": [
         "l10n_br_purchase",
         "l10n_br_stock_account",
