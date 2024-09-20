e1f5691e8a3912780a58243eeeddf4f22798549b
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-09-20 16:29:12 -0300

    [IMP] l10n_br_delivery_nfe: add product volume fields

--- l10n_br_delivery_nfe/__manifest__.py
+++ l10n_br_delivery_nfe/__manifest__.py
@@ -10,10 +10,17 @@
     "maintainers": ["mbcosta"],
     "website": "https://github.com/OCA/l10n-brazil",
     "version": "14.0.1.1.0",
-    "depends": ["l10n_br_nfe", "l10n_br_account", "l10n_br_delivery"],
+    "depends": [
+        "l10n_br_nfe",
+        "l10n_br_account",
+        "l10n_br_delivery",
+        "product_net_weight",
+    ],
     "data": [
         # Views
-        "views/nfe_document_view.xml"
+        "views/nfe_document_view.xml",
+        "views/product_product_view.xml",
+        "views/product_template_view.xml",
     ],
     "installable": True,
     "auto_install": True,
