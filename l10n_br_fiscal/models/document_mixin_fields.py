5f0a1eaa0eabafecc72a966507ce01a4a0d64e1a
Felipe Zago <felipe.zago@kmee.com.br>
2024-09-25 15:14:59 -0300

    [IMP] l10n_br_fiscal: mdfe serie, demo and others

--- l10n_br_fiscal/models/document_mixin_fields.py
+++ l10n_br_fiscal/models/document_mixin_fields.py
@@ -467,3 +467,7 @@ class FiscalDocumentMixinFields(models.AbstractModel):
         index=True,
         unaccent=False,
     )
+
+    key_random_code = fields.Char(string="Document Key Random Code")
+    key_check_digit = fields.Char(string="Document Key Check Digit")
+    total_weight = fields.Float()
