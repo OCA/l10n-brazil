f942de1bae391423d22eb6883e1f348714f1e33a
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-09-25 15:13:54 -0300

    [IMP] l10n_br_mdfe: escodoo work

--- l10n_br_mdfe/hooks.py
+++ l10n_br_mdfe/hooks.py
@@ -2,20 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import SUPERUSER_ID, api
 
-from odoo.addons.spec_driven_model import hooks
-
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    hooks.register_hook(
-        env,
-        "l10n_br_mdfe",
-        "odoo.addons.l10n_br_mdfe_spec.models.v3_0.mdfe_tipos_basico_v3_00",
-    )
-
-    hooks.post_init_hook(
-        cr,
-        registry,
-        "l10n_br_mdfe",
-        "odoo.addons.l10n_br_mdfe_spec.models.v3_0.mdfe_tipos_basico_v3_00",
-    )
+    env["mdfe.30.tmdfe_infnfe"]._register_hook()
