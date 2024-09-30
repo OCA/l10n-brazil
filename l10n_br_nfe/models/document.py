fb1d240d5abfd39f09bd0ebbd8dba6b52667bb6b
Raphaël Valyi <rvalyi@akretion.com>
2024-09-30 05:27:43 +0000

    [REF] l10n_br_nfe: inscr_est->l10n_br_ie_code 1/2

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -406,7 +406,7 @@ class NFe(spec_models.StackedModel):
                 stn_id = self.company_id.state_tax_number_ids.filtered(
                     lambda stn: stn.state_id == dest_state_id
                 )
-                iest = stn_id.inscr_est
+                iest = stn_id.l10n_br_ie_code
                 iest = re.sub("[^0-9]+", "", iest)
         self.company_inscr_est_st = iest
 
@@ -1550,7 +1550,7 @@ class NFe(spec_models.StackedModel):
 
     def _prepare_nfce_danfe_values(self):
         return {
-            "company_ie": self.company_id.inscr_est,
+            "company_ie": self.company_id.l10n_br_ie_code,
             "company_cnpj": self.company_id.cnpj_cpf,
             "company_legal_name": self.company_id.legal_name,
             "company_street": self.company_id.street,
