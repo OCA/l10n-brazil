6325545c600a02ba2c93e181211d675d34fff820
Bruno <bruno.botti@kmee.com.br>
2024-10-07 11:06:13 -0300

    [IMP] payment_cielo: apply pre-commit auto fixes

--- payment_cielo/models/payment_acquirer.py
+++ payment_cielo/models/payment_acquirer.py
@@ -9,24 +9,31 @@ _logger = logging.getLogger(__name__)
 
 
 class PaymentAcquirerCielo(models.Model):
-    _inherit = 'payment.acquirer'
+    _inherit = "payment.acquirer"
 
-    provider = fields.Selection(selection_add=[('cielo', 'Cielo')])
-    cielo_merchant_key = fields.Char(required_if_provider='cielo',
-                                     groups='base.group_user')
-    cielo_merchant_id = fields.Char(string='Cielo Merchant Id',
-                                    required_if_provider='cielo',
-                                    groups='base.group_user')
-    cielo_image_url = fields.Char(
-        "Checkout Image URL", groups='base.group_user')
+    provider = fields.Selection(selection_add=[("cielo", "Cielo")])
+    cielo_merchant_key = fields.Char(
+        required_if_provider="cielo", groups="base.group_user"
+    )
+    cielo_merchant_id = fields.Char(
+        string="Cielo Merchant Id",
+        required_if_provider="cielo",
+        groups="base.group_user",
+    )
+    cielo_image_url = fields.Char("Checkout Image URL", groups="base.group_user")
 
     @api.multi
     def cielo_s2s_form_validate(self, data):
         """Validates user input"""
         self.ensure_one()
         # mandatory fields
-        for field_name in ["cc_number", "cvc", "cc_holder_name", "cc_expiry",
-                           "cc_brand"]:
+        for field_name in [
+            "cc_number",
+            "cvc",
+            "cc_holder_name",
+            "cc_expiry",
+            "cc_brand",
+        ]:
             if not data.get(field_name):
                 return False
         return True
@@ -38,15 +45,21 @@ class PaymentAcquirerCielo(models.Model):
         Secret card info should be empty by this point.
 
         """
-        payment_token = self.env['payment.token'].sudo().create({
-            'cc_number': data['cc_number'],
-            'cc_holder_name': data['cc_holder_name'],
-            'cc_expiry': data['cc_expiry'],
-            'cc_brand': data['cc_brand'],
-            'cvc': data['cvc'],
-            'acquirer_id': int(data['acquirer_id']),
-            'partner_id': int(data['partner_id']),
-            })
+        payment_token = (
+            self.env["payment.token"]
+            .sudo()
+            .create(
+                {
+                    "cc_number": data["cc_number"],
+                    "cc_holder_name": data["cc_holder_name"],
+                    "cc_expiry": data["cc_expiry"],
+                    "cc_brand": data["cc_brand"],
+                    "cvc": data["cvc"],
+                    "acquirer_id": int(data["acquirer_id"]),
+                    "partner_id": int(data["partner_id"]),
+                }
+            )
+        )
         return payment_token
 
     @api.model
@@ -56,10 +69,10 @@ class PaymentAcquirerCielo(models.Model):
         Takes environment in consideration.
 
         """
-        if self.environment == 'test':
-            return 'apisandbox.cieloecommerce.cielo.com.br'
-        if self.environment == 'prod':
-            return 'api.cieloecommerce.cielo.com.br'
+        if self.environment == "test":
+            return "apisandbox.cieloecommerce.cielo.com.br"
+        if self.environment == "prod":
+            return "api.cieloecommerce.cielo.com.br"
 
     @api.multi
     def _get_cielo_api_headers(self):
@@ -69,18 +82,18 @@ class PaymentAcquirerCielo(models.Model):
         merchant_id and merchant_key need to be defined.
 
         """
-        if self.environment == 'test':
+        if self.environment == "test":
             CIELO_HEADERS = {
-                'MerchantId': 'be87a4be-a40d-4a2d-b2c8-b8b6cc19cddd',
-                'MerchantKey': 'POHAWRXFBSIXTMTFVBCYSKNWZBMOATDNYUQDGBUE',
-                'Content-Type': 'application/json',
-                }
-        if self.environment == 'prod':
+                "MerchantId": "be87a4be-a40d-4a2d-b2c8-b8b6cc19cddd",
+                "MerchantKey": "POHAWRXFBSIXTMTFVBCYSKNWZBMOATDNYUQDGBUE",
+                "Content-Type": "application/json",
+            }
+        if self.environment == "prod":
             CIELO_HEADERS = {
-                'MerchantId': self.cielo_merchant_id,
-                'MerchantKey': self.cielo_merchant_key,
-                'Content-Type': 'application/json',
-                }
+                "MerchantId": self.cielo_merchant_id,
+                "MerchantKey": self.cielo_merchant_key,
+                "Content-Type": "application/json",
+            }
         return CIELO_HEADERS
 
     def _get_feature_support(self):
@@ -95,6 +108,6 @@ class PaymentAcquirerCielo(models.Model):
                         object
         """
         res = super(PaymentAcquirerCielo, self)._get_feature_support()
-        res['tokenize'].append('cielo')
-        res['authorize'].append('cielo')
+        res["tokenize"].append("cielo")
+        res["authorize"].append("cielo")
         return res
