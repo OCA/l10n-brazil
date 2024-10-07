b7ddc7cfeb9c4ec60c799538a24a5ae3346f64dc
Magno Costa <magno.costa@akretion.com.br>
2024-10-07 19:49:19 -0300

    [REF+IMP] l10n_br_sale_stock: Extract new module

--- l10n_br_sale_stock/README.rst
+++ l10n_br_sale_stock/README.rst
@@ -55,6 +55,16 @@ No configuration required.
 Changelog
 =========
 
+14.0.2.0.0 (2024-04-26)
+~~~~~~~~~~~~~~~~~~~~~~~
+* [REF] Extraction to module sale_stock_picking_invoicing
+  https://github.com/OCA/account-invoicing/pull/1025
+
+14.0.1.0.0 (2022-09-16)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* [MIG] Migration to version 14.0
+
 12.0.1.0.0 (2020-05-29)
 ~~~~~~~~~~~~~~~~~~~~~~~
 
--- l10n_br_sale_stock/models/stock_picking.py
+++ l10n_br_sale_stock/models/stock_picking.py
@@ -7,19 +7,6 @@ from odoo import models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def _get_partner_to_invoice(self):
-        """
-        If the partner has some invoicing contact defined
-        partner_invoice_id is auto filled, but it can also be changed.
-        partner_invoice_id is used if different from partner_id
-        """
-        self.ensure_one()
-        partner_id = super()._get_partner_to_invoice()
-        if self.sale_id:
-            if partner_id != self.sale_id.partner_invoice_id.id:
-                partner_id = self.sale_id.partner_invoice_id.id
-        return partner_id
-
     def _get_fiscal_partner(self):
         self.ensure_one()
         partner = super()._get_fiscal_partner()
