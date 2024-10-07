91c9044072195fa4d8bbafc73bf4089d2cbeae55
Raphaël Valyi <rvalyi@akretion.com>
2024-10-07 19:09:48 +0000

    [REF] l10n_br_nfe: multi-schemas compat

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -79,18 +79,20 @@ def filter_processador_edoc_nfe(record):
 class NFe(spec_models.StackedModel):
     _name = "l10n_br_fiscal.document"
     _inherit = ["l10n_br_fiscal.document", "nfe.40.infnfe", "nfe.40.fat"]
-    _stacked = "nfe.40.infnfe"
-    _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
+    _nfe40_spec_settings = {
+        "module": "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00",
+        "stacking_mixin": "nfe.40.infnfe",
+        "stacking_points": {},
+        # all m2o at this level will be stacked even if not required:
+        "stacking_force_paths": (
+            "infnfe.total",
+            "infnfe.infAdic",
+            "infnfe.exporta",
+            "infnfe.cobr",
+            "infnfe.cobr.fat",
+        ),
+    }
     _nfe_search_keys = ["nfe40_Id"]
-
-    # all m2o at this level will be stacked even if not required:
-    _force_stack_paths = (
-        "infnfe.total",
-        "infnfe.infAdic",
-        "infnfe.exporta",
-        "infnfe.cobr",
-        "infnfe.cobr.fat",
-    )
 
     # When dynamic stacking is applied the NFe structure is:
     INFNFE_TREE = """
@@ -671,7 +673,7 @@ class NFe(spec_models.StackedModel):
         denormalized inner attribute has been set.
         """
         self.ensure_one()
-        if field_name in self._stacking_points.keys():
+        if field_name in self._get_stacking_points().keys():
             if field_name == "nfe40_ISSQNtot" and not any(
                 t == "issqn"
                 for t in self.nfe40_det.mapped("product_id.tax_icms_or_issqn")
@@ -679,7 +681,9 @@ class NFe(spec_models.StackedModel):
                 return False
 
             elif (not xsd_required) and field_name not in ["nfe40_enderDest"]:
-                comodel = self.env[self._stacking_points.get(field_name).comodel_name]
+                comodel = self.env[
+                    self._get_stacking_points().get(field_name).comodel_name
+                ]
                 fields = [
                     f
                     for f in comodel._fields
@@ -687,7 +691,7 @@ class NFe(spec_models.StackedModel):
                     and f in self._fields.keys()
                     and f
                     # don't try to nfe40_fat id when reading nfe40_cobr for instance
-                    not in self._stacking_points.keys()
+                    not in self._get_stacking_points().keys()
                 ]
                 sub_tag_read = self.read(fields)[0]
                 if not any(
@@ -894,11 +898,11 @@ class NFe(spec_models.StackedModel):
         ):
             record.flush_model()
             self.env.invalidate_all()
-            inf_nfe = record.export_ds()[0]
+            inf_nfe = record.export_ds("nfe", "40")[0]
 
             inf_nfe_supl = None
             if record.nfe40_infNFeSupl:
-                inf_nfe_supl = record.nfe40_infNFeSupl.export_ds()[0]
+                inf_nfe_supl = record.nfe40_infNFeSupl.export_ds("nfe", "40")[0]
 
             nfe = Nfe(infNFe=inf_nfe, infNFeSupl=inf_nfe_supl, signature=None)
             edocs.append(nfe)
@@ -1345,7 +1349,7 @@ class NFe(spec_models.StackedModel):
         document = (
             self.env["nfe.40.infnfe"]
             .with_context(tracking_disable=True, edoc_type=edoc_type, dry_run=False)
-            .build_from_binding(binding.NFe.infNFe)
+            .build_from_binding("nfe", "40", binding.NFe.infNFe)
         )
 
         if edoc_type == "in" and document.company_id.cnpj_cpf != cnpj_cpf.formata(
