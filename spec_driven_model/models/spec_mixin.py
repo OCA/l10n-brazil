2a102b43944dae9e6d69fcaa57b48ddf9f17ed2b
Raphaël Valyi <rvalyi@akretion.com>
2024-10-08 04:00:52 +0000

    [REF] spec_driven_model: simplify and more robust
    ensure generic _stacking_points is readonly

--- spec_driven_model/models/spec_mixin.py
+++ spec_driven_model/models/spec_mixin.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
 from odoo import api, models
+from odoo.tools import frozendict
 
 from .spec_models import SPEC_MIXIN_MAPPINGS, SpecModel, StackedModel
 
@@ -21,7 +22,15 @@ class SpecMixin(models.AbstractModel):
     _description = "root abstract model meant for xsd generated fiscal models"
     _name = "spec.mixin"
     _inherit = ["spec.mixin_export", "spec.mixin_import"]
-    _stacking_points = {}
+
+    # actually _stacking_points are model and even schema specific
+    # but the legacy code used it extensively so a first defensive
+    # action we can take is to use a frozendict so it is readonly
+    # at least. In the future the whole stacking system should be
+    # scoped under a given schema and version as in
+    # https://github.com/OCA/l10n-brazil/pull/3424
+    _stacking_points = frozendict({})
+
     # _spec_module = 'override.with.your.python.module'
     # _binding_module = 'your.pyhthon.binding.module'
     # _odoo_module = 'your.odoo_module'
