f142fcaa1f3dee3a6e03e0fd1930d3424fc88bed
Tiago Amaral <tiagoamral23@gmail.com>
2024-10-14 07:47:52 -0300

    [IMP] l10n_br_cnpj_search: solves cnpj duplication problem in tests

--- l10n_br_cnpj_search/tests/test_serpro.py
+++ l10n_br_cnpj_search/tests/test_serpro.py
@@ -25,87 +25,6 @@ class TestTestSerPro(TestCnpjCommon):
         self.set_param("serpro_schema", "basica")
 
     def test_serpro_basica(self):
-        dummy_basica = self.model.create(
-            {"name": "Dummy Basica", "cnpj_cpf": "34.238.864/0001-68"}
-        )
-        dummy_basica._onchange_cnpj_cpf()
-
-        action_wizard = dummy_basica.action_open_cnpj_search_wizard()
-        wizard_context = action_wizard.get("context")
-        wizard = (
-            self.env["partner.search.wizard"].with_context(**wizard_context).create({})
-        )
-        wizard.action_update_partner()
-        self.assertEqual(
-            dummy_basica.legal_name,
-            "Uhieqkx Whnhiwd Nh Fixkhuuwphmvx Nh Nwnxu (Uhifix)",
-        )
-        self.assertEqual(dummy_basica.company_type, "company")
-        self.assertEqual(dummy_basica.name, "Uhifix Uhnh")
-        self.assertEqual(dummy_basica.email, "EMPRESA@XXXXXX.BR")
-        self.assertEqual(dummy_basica.street_name, "Nh Biwmnh Wihw Mxivh")
-        self.assertEqual(dummy_basica.street2, "Lote V")
-        self.assertEqual(dummy_basica.street_number, "Q.601")
-        self.assertEqual(dummy_basica.zip, "70836900")
-        self.assertEqual(dummy_basica.district, "Asa Norte")
-        self.assertEqual(dummy_basica.phone, "(61) 22222222")
-        self.assertEqual(dummy_basica.mobile, "(61) 22222222")
-        self.assertEqual(dummy_basica.state_id.code, "DF")
-        self.assertEqual(dummy_basica.equity_capital, 0)
-        self.assertEqual(dummy_basica.cnae_main_id.code, "6204-0/00")
-
-    def test_serpro_not_found(self):
-        # In the Trial version there are only a few registered CNPJ records
-        invalid = self.model.create(
-            {"name": "invalid", "cnpj_cpf": "44.356.113/0001-08"}
-        )
-        invalid._onchange_cnpj_cpf()
-
-        with self.assertRaises(ValidationError):
-            action_wizard = invalid.action_open_cnpj_search_wizard()
-            wizard_context = action_wizard.get("context")
-            self.env["partner.search.wizard"].with_context(**wizard_context).create({})
-
-    def assert_socios(self, partner, expected_cnpjs):
-        socios = self.model.search_read(
-            [("id", "in", partner.child_ids.ids)],
-            fields=["name", "cnpj_cpf", "company_type"],
-        )
-
-        for s in socios:
-            s.pop("id")
-
-        expected_socios = [
-            {
-                "name": "Joana Alves Mundim Pena",
-                "cnpj_cpf": expected_cnpjs["Joana"],
-                "company_type": "person",
-            },
-            {
-                "name": "Luiza Aldenora",
-                "cnpj_cpf": expected_cnpjs["Aldenora"],
-                "company_type": "person",
-            },
-            {
-                "name": "Luiza Araujo De Oliveira",
-                "cnpj_cpf": expected_cnpjs["Araujo"],
-                "company_type": "person",
-            },
-            {
-                "name": "Luiza Barbosa Bezerra",
-                "cnpj_cpf": expected_cnpjs["Barbosa"],
-                "company_type": "person",
-            },
-            {
-                "name": "Marcelo Antonio Barros De Cicco",
-                "cnpj_cpf": expected_cnpjs["Marcelo"],
-                "company_type": "person",
-            },
-        ]
-
-        self.assertEqual(socios, expected_socios)
-
-    def test_serpro_empresa(self):
         mocked_response = {
             "ni": "34238864000168",
             "nomeEmpresarial": "UHIEQKX WHNHIWD NH  FIXKHUUWPHMVX NH NWNXU (UHIFIX)",
@@ -205,13 +124,195 @@ class TestTestSerPro(TestCnpjCommon):
             "odoo.addons.l10n_br_cnpj_search.models.cnpj_webservice.CNPJWebservice.validate",
             return_value=mocked_response,
         ):
-            self.model.search([("cnpj_cpf", "=", "34.238.864/0001-68")]).write(
+            dummy_basica = self.model.create(
+                {"name": "Dummy Basica", "cnpj_cpf": "34.238.864/0001-68"}
+            )
+            dummy_basica._onchange_cnpj_cpf()
+
+            action_wizard = dummy_basica.action_open_cnpj_search_wizard()
+            wizard_context = action_wizard.get("context")
+            wizard = (
+                self.env["partner.search.wizard"]
+                .with_context(**wizard_context)
+                .create({})
+            )
+            wizard.action_update_partner()
+            self.assertEqual(
+                dummy_basica.legal_name,
+                "Uhieqkx Whnhiwd Nh  Fixkhuuwphmvx Nh Nwnxu (Uhifix)",
+            )
+            self.assertEqual(dummy_basica.company_type, "company")
+            self.assertEqual(dummy_basica.name, "Uhifix Uhnh")
+            self.assertEqual(dummy_basica.email, "EMPRESA@XXXXXX.BR")
+            self.assertEqual(dummy_basica.street_name, "Nh Biwmnh Wihw Mxivh")
+            self.assertEqual(dummy_basica.street2, "Lote V")
+            self.assertEqual(dummy_basica.street_number, "Q.601")
+            self.assertEqual(dummy_basica.zip, "70836900")
+            self.assertEqual(dummy_basica.district, "Asa Norte")
+            self.assertEqual(dummy_basica.phone, "(61) 22222222")
+            self.assertEqual(dummy_basica.mobile, "(61) 22222222")
+            self.assertEqual(dummy_basica.state_id.code, "DF")
+            self.assertEqual(dummy_basica.equity_capital, 0)
+            self.assertEqual(dummy_basica.cnae_main_id.code, "6204-0/00")
+
+    def test_serpro_not_found(self):
+        # In the Trial version there are only a few registered CNPJ records
+        invalid = self.model.create(
+            {"name": "invalid", "cnpj_cpf": "44.356.113/0001-08"}
+        )
+        invalid._onchange_cnpj_cpf()
+
+        with self.assertRaises(ValidationError):
+            action_wizard = invalid.action_open_cnpj_search_wizard()
+            wizard_context = action_wizard.get("context")
+            self.env["partner.search.wizard"].with_context(**wizard_context).create({})
+
+    def assert_socios(self, partner, expected_cnpjs):
+        socios = self.model.search_read(
+            [("id", "in", partner.child_ids.ids)],
+            fields=["name", "cnpj_cpf", "company_type"],
+        )
+
+        for s in socios:
+            s.pop("id")
+
+        expected_socios = [
+            {
+                "name": "Joana Alves Mundim Pena",
+                "cnpj_cpf": expected_cnpjs["Joana"],
+                "company_type": "person",
+            },
+            {
+                "name": "Luiza Aldenora",
+                "cnpj_cpf": expected_cnpjs["Aldenora"],
+                "company_type": "person",
+            },
+            {
+                "name": "Luiza Araujo De Oliveira",
+                "cnpj_cpf": expected_cnpjs["Araujo"],
+                "company_type": "person",
+            },
+            {
+                "name": "Luiza Barbosa Bezerra",
+                "cnpj_cpf": expected_cnpjs["Barbosa"],
+                "company_type": "person",
+            },
+            {
+                "name": "Marcelo Antonio Barros De Cicco",
+                "cnpj_cpf": expected_cnpjs["Marcelo"],
+                "company_type": "person",
+            },
+        ]
+
+        self.assertEqual(socios, expected_socios)
+
+    def test_serpro_empresa(self):
+        mocked_response = {
+            "ni": "34238864000249",
+            "nomeEmpresarial": "UHIEQKX WHNHIWD NH  FIXKHUUWPHMVX NH NWNXU (UHIFIX)",
+            "nomeFantasia": "UHIFIX UHNH",
+            "telefones": [
+                {"ddd": "61", "numero": "22222222"},
+                {"ddd": "61", "numero": "22222222"},
+            ],
+            "cep": "70836900",
+            "correioEletronico": "EMPRESA@XXXXXX.BR",
+            "socios": [
+                {
+                    "tipoSocio": "2",
+                    "cpf": "07119488449",
+                    "nome": "LUIZA ARAUJO DE OLIVEIRA",
+                    "qualificacao": "49",
+                    "dataInclusao": "2014-01-01",
+                    "pais": {"codigo": "105", "descricao": "BRASIL"},
+                    "representanteLegal": {
+                        "cpf": "00000000000",
+                        "nome": "",
+                        "qualificacao": "00",
+                    },
+                },
+                {
+                    "tipoSocio": "2",
+                    "cpf": "23982012600",
+                    "nome": "JOANA ALVES MUNDIM PENA",
+                    "qualificacao": "49",
+                    "dataInclusao": "2014-01-01",
+                    "pais": {"codigo": "105", "descricao": "BRASIL"},
+                    "representanteLegal": {
+                        "cpf": "00000000000",
+                        "nome": "",
+                        "qualificacao": "00",
+                    },
+                },
+                {
+                    "tipoSocio": "2",
+                    "cpf": "13946994415",
+                    "nome": "LUIZA BARBOSA BEZERRA",
+                    "qualificacao": "49",
+                    "dataInclusao": "2014-01-01",
+                    "pais": {"codigo": "105", "descricao": "BRASIL"},
+                    "representanteLegal": {
+                        "cpf": "00000000000",
+                        "nome": "",
+                        "qualificacao": "00",
+                    },
+                },
+                {
+                    "tipoSocio": "2",
+                    "cpf": "00031298702",
+                    "nome": "MARCELO ANTONIO BARROS DE CICCO",
+                    "qualificacao": "49",
+                    "dataInclusao": "2014-01-01",
+                    "pais": {"codigo": "105", "descricao": "BRASIL"},
+                    "representanteLegal": {
+                        "cpf": "00000000000",
+                        "nome": "",
+                        "qualificacao": "00",
+                    },
+                },
+                {
+                    "tipoSocio": "2",
+                    "cpf": "76822320300",
+                    "nome": "LUIZA ALDENORA",
+                    "qualificacao": "49",
+                    "dataInclusao": "2014-01-01",
+                    "pais": {"codigo": "105", "descricao": "BRASIL"},
+                    "representanteLegal": {
+                        "cpf": "00000000000",
+                        "nome": "",
+                        "qualificacao": "00",
+                    },
+                },
+            ],
+            "endereco": {
+                "tipoLogradouro": "SETOR",
+                "logradouro": "NH BIWMNH WIHW MXIVH",
+                "numero": "Q.601",
+                "complemento": "LOTE V",
+                "cep": "70836900",
+                "bairro": "ASA NORTE",
+                "municipio": {"codigo": "9701", "descricao": "BRASILIA"},
+                "uf": "DF",
+                "pais": {"codigo": "105", "descricao": "BRASIL"},
+            },
+            "naturezaJuridica": {"codigo": "2011", "descricao": "Empresa Pública"},
+            "capitalSocial": 0,
+            "cnaePrincipal": {
+                "codigo": "6204000",
+                "descricao": "Consultoria em tecnologia da informação",
+            },
+        }
+        with mock.patch(
+            "odoo.addons.l10n_br_cnpj_search.models.cnpj_webservice.CNPJWebservice.validate",
+            return_value=mocked_response,
+        ):
+            self.model.search([("cnpj_cpf", "=", "34.238.864/0002-49")]).write(
                 {"active": False}
             )
             self.set_param("serpro_schema", "empresa")
 
             dummy_empresa = self.model.create(
-                {"name": "Dummy Empresa", "cnpj_cpf": "34.238.864/0001-68"}
+                {"name": "Dummy Empresa", "cnpj_cpf": "34.238.864/0002-49"}
             )
 
             dummy_empresa._onchange_cnpj_cpf()
@@ -233,6 +334,7 @@ class TestTestSerPro(TestCnpjCommon):
         }
 
         self.assert_socios(dummy_empresa, expected_cnpjs)
+        dummy_empresa.unlink()
 
     def test_serpro_qsa(self):
         mocked_response = {
