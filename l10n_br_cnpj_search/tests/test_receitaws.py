f1d93983822c3718283619c0a6bbf9d2c9b52ec0
Tiago Amaral <tiagoamral23@gmail.com>
2024-10-14 08:26:56 -0300

    [IMP] l10n_br_cnpj_search: transfer the ws mock to comon

--- l10n_br_cnpj_search/tests/test_receitaws.py
+++ l10n_br_cnpj_search/tests/test_receitaws.py
@@ -26,30 +26,9 @@ class TestReceitaWS(TestCnpjCommon):
             }
         )
 
-        mocked_response = {
-            "nome": "Kilian Macedo Melcher 08777131460",
-            "fantasia": "Kilian Macedo Melcher 08777131460",
-            "email": "kilian.melcher@gmail.com",
-            "logradouro": "Rua Luiza Bezerra Motta",
-            "complemento": "Bloco E;Apt 302",
-            "numero": "950",
-            "cep": "58.410-410",
-            "bairro": "Catole",
-            "uf": "PB",
-            "telefone": "(83) 8665-0905",
-            "municipio": "CAMPINA GRANDE",
-            "natureza_juridica": "213-5 - Empresário (Individual)",
-            "capital_social": "3000.00",
-            "atividade_principal": [
-                {
-                    "code": "47.51-2-01",
-                    "text": "********",
-                }
-            ],
-        }
         with mock.patch(
             "odoo.addons.l10n_br_cnpj_search.models.cnpj_webservice.CNPJWebservice.validate",
-            return_value=mocked_response,
+            return_value=self.mocked_response_ws_1,
         ):
             action_wizard = kilian.action_open_cnpj_search_wizard()
             wizard_context = action_wizard.get("context")
@@ -87,31 +66,9 @@ class TestReceitaWS(TestCnpjCommon):
             self.env["partner.search.wizard"].with_context(**wizard_context).create({})
 
     def test_receita_ws_multiple_phones(self):
-        mocked_response = {
-            "nome": "ISLA SEMENTES LTDA.",
-            "fantasia": "",
-            "email": "contabilidade@isla.com.br",
-            "logradouro": "AVENIDA SEVERO DULLIUS",
-            "complemento": "Bloco E;Apt 302",
-            "numero": "124",
-            "cep": "90.200-310",
-            "bairro": "ANCHIETA",
-            "uf": "RS",
-            "telefone": "(51) 9852-9561 / (51) 2136-6600",
-            "municipio": "PORTO ALEGRE",
-            "natureza_juridica": "206-2 - Sociedade Empresária Limitada",
-            "capital_social": "10606804.00",
-            "atividade_principal": [
-                {
-                    "code": "46.89-3-99",
-                    "text": """Comércio atacadista especializado em outros
-                     produtos intermediários não especificados anteriormente""",
-                }
-            ],
-        }
         with mock.patch(
             "odoo.addons.l10n_br_cnpj_search.models.cnpj_webservice.CNPJWebservice.validate",
-            return_value=mocked_response,
+            return_value=self.mocked_response_ws_2,
         ):
             isla = self.model.create({"name": "Isla", "cnpj_cpf": "92.666.056/0001-06"})
             isla._onchange_cnpj_cpf()
