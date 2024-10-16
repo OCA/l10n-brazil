7bf8d261a81a2eb4eaccf6de3da8b886fdfb13a0
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-10-16 10:28:08 -0300

    [IMP] l10n_br_nfe: add document.supplement inherit

--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -8,7 +8,7 @@ from odoo.addons.spec_driven_model.models import spec_models
 class NFeSupplement(spec_models.StackedModel):
     _name = "l10n_br_fiscal.document.supplement"
     _description = "NFe Supplement Document"
-    _inherit = "nfe.40.infnfesupl"
+    _inherit = ["l10n_br_fiscal.document.supplement", "nfe.40.infnfesupl"]
     _stacked = "nfe.40.infnfesupl"
     _stacking_points = {}
     _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
