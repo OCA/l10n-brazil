9b154a177ce956e877d0f3b64c36ce04e7ea869a
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-10-16 10:28:08 -0300

    [RFC] l10n_br_nfe: document.supplement refactor

--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -1,14 +1,15 @@
 # Copyright 2023 KMEE (Felipe Zago Rodrigues <felipe.zago@kmee.com.br>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import fields
 
 from odoo.addons.spec_driven_model.models import spec_models
 
 
-class NFeSupplement(spec_models.StackedModel):
+class NFeSupplement(spec_models.SpecModel):
     _name = "l10n_br_fiscal.document.supplement"
-    _description = "NFe Supplement Document"
-    _inherit = "nfe.40.infnfesupl"
+    _inherit = ["l10n_br_fiscal.document.supplement", "nfe.40.infnfesupl"]
 
-    _nfe40_odoo_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
-    _nfe40_stacking_mixin = "nfe.40.infnfesupl"
+    nfe40_qrCode = fields.Char(related="qrcode")
+
+    nfe40_urlChave = fields.Char(related="url_key")
