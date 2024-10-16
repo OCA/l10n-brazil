e5d50a4ac451d2afda397f38bb52d65489d8cf8d
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-10-16 11:16:42 -0300

    [IMP] l10n_br_nfe: add nfe40_qrCode and urlChave relation

--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
+from odoo import fields
+
 from odoo.addons.spec_driven_model.models import spec_models
 
 
@@ -12,3 +14,7 @@ class NFeSupplement(spec_models.StackedModel):
     _stacked = "nfe.40.infnfesupl"
     _stacking_points = {}
     _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
+
+    nfe40_qrCode = fields.Char(related="qrcode")
+
+    nfe40_urlChave = fields.Char(related="url_key")
