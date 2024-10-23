48ad3578c143caa6bc1797adc2cebbe4dcb530bd
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-10-23 11:54:51 -0300

    [IMP] l10n_br_mdfe_spec: bump minor version

--- l10n_br_mdfe_spec/__manifest__.py
+++ l10n_br_mdfe_spec/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "mdfe spec",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Accounting",
