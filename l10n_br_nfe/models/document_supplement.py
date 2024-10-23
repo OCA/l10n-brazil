d3e094d00524c7c74beb2685c58b8bc4dd6e18a5
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-10-23 09:26:03 -0300

    [RFC] l10n_br_nfe: document.supplement refactor to stackedmodel

--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -7,9 +7,12 @@ from odoo import fields
 from odoo.addons.spec_driven_model.models import spec_models
 
 
-class NFeSupplement(spec_models.SpecModel):
+class NFeSupplement(spec_models.StackedModel):
     _name = "l10n_br_fiscal.document.supplement"
     _inherit = ["l10n_br_fiscal.document.supplement", "nfe.40.infnfesupl"]
+
+    _nfe40_odoo_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
+    _nfe40_stacking_mixin = "nfe.40.infnfesupl"
 
     nfe40_qrCode = fields.Char(related="qrcode")
 
