198a0dfe7ed78bbbba5af48d61b589c477e981fe
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-10-23 09:26:03 -0300

    [RFC] l10n_br_nfe: document.supplement refactor to stackedmodel

--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -6,9 +6,16 @@ from odoo import fields
 from odoo.addons.spec_driven_model.models import spec_models
 
 
-class NFeSupplement(spec_models.SpecModel):
+class NFeSupplement(spec_models.StackedModel):
+    # FIXME: NFeSupplement should actually inherit from spec_models.SpecModel
+    # but it seems we had broken NFe or MDFe or CTe tests with SpecModel
+    # it's probably a spec_driven_model framework issue... So it has been reverted
+    # to StackedModel in https://github.com/OCA/l10n-brazil/pull/3445
     _name = "l10n_br_fiscal.document.supplement"
     _inherit = ["l10n_br_fiscal.document.supplement", "nfe.40.infnfesupl"]
+
+    _nfe40_odoo_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
+    _nfe40_stacking_mixin = "nfe.40.infnfesupl"
 
     nfe40_qrCode = fields.Char(related="qrcode")
 
