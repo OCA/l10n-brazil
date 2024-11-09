d5209af6e04dc45b673c63cf02d82713087df322
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-11-09 16:52:23 -0300

    [HACK] l10n_br_cte: hack

--- l10n_br_cte/models/document.py
+++ l10n_br_cte/models/document.py
@@ -86,21 +86,19 @@ class CTe(spec_models.StackedModel):
     _inherit = [
         "l10n_br_fiscal.document",
         "cte.40.infcte",
-        "cte.40.imp",
-        "cte.40.fat",
     ]
     _cte40_odoo_module = (
         "odoo.addons.l10n_br_cte_spec.models.v4_0.cte_tipos_basico_v4_00"
     )
     _cte40_stacking_mixin = "cte.40.infcte"
-    _cte40_stacking_skip_paths = (
-        "cte40_fluxo",
-        "cte40_semData",
-        "cte40_noInter",
-        "cte40_comHora",
-        "cte40_noPeriodo",
-        "cte40_NFref_ide_id",
-    )
+    # _cte40_stacking_skip_paths = (
+    #     "cte40_fluxo",
+    #     "cte40_semData",
+    #     "cte40_noInter",
+    #     "cte40_comHora",
+    #     "cte40_noPeriodo",
+    #     "cte40_NFref_ide_id",
+    # )
 
     # all m2o at this level will be stacked even if not required:
     _cte40_stacking_force_paths = (
