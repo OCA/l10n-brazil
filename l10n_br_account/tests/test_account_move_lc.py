552c61ea0f75dffaae0fd0515938211dce8db3fb
Raphaël Valyi <rvalyi@akretion.com>
2024-11-16 15:54:08 +0000

    [FIX] l10n_br_account: state tests when no EDI
    since https://github.com/OCA/l10n-brazil/pull/3012
    when l10n_br_fiscal_edi is not installed, these tests were failing

--- l10n_br_account/tests/test_account_move_lc.py
+++ l10n_br_account/tests/test_account_move_lc.py
@@ -1716,16 +1716,20 @@ class AccountMoveLucroPresumido(AccountMoveBRCommon):
         self.assertEqual(document_id.state, "em_digitacao")
         self.move_out_venda.action_post()
         self.assertEqual(self.move_out_venda.state, "posted")
-        self.assertEqual(document_id.state, "a_enviar")
-        self.move_out_venda.button_draft()
-        self.assertEqual(self.move_out_venda.state, "draft")
-        self.assertEqual(document_id.state, "em_digitacao")
-        document_id.action_document_confirm()
-        self.assertEqual(self.move_out_venda.state, "posted")
-        self.assertEqual(document_id.state, "a_enviar")
-        document_id.action_document_back2draft()
-        self.assertEqual(self.move_out_venda.state, "draft")
-        self.assertEqual(document_id.state, "em_digitacao")
+        fiscal_edi = self.env["ir.module.module"].search(
+            [("name", "=", "l10n_br_fiscal_edi")]
+        )
+        if fiscal_edi and fiscal_edi.state == "installed":
+            self.assertEqual(document_id.state, "a_enviar")
+            self.move_out_venda.button_draft()
+            self.assertEqual(self.move_out_venda.state, "draft")
+            self.assertEqual(document_id.state, "em_digitacao")
+            document_id.action_document_confirm()
+            self.assertEqual(self.move_out_venda.state, "posted")
+            self.assertEqual(document_id.state, "a_enviar")
+            document_id.action_document_back2draft()
+            self.assertEqual(self.move_out_venda.state, "draft")
+            self.assertEqual(document_id.state, "em_digitacao")
 
     def test_document_deny(self):
         document_id = self.move_out_venda.fiscal_document_id
