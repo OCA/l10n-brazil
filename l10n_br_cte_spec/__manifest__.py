5f29fc50211da296728d7aa0802732265fb00cc5
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-12-10 11:19:57 -0300

    [IMP] l10n_br_cte_spec: bump version

--- l10n_br_cte_spec/__manifest__.py
+++ l10n_br_cte_spec/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "cte spec",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Accounting",
