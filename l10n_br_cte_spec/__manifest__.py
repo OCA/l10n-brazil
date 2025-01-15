139df8f4c3302df266b386a4373f659a28ace7dd
Raphaël Valyi <rvalyi@akretion.com>
2025-01-15 23:19:30 +0000

    [MIG] l10n_br_cte_spec: Migration to 15.0
    don't format generated CTe code

--- l10n_br_cte_spec/__manifest__.py
+++ l10n_br_cte_spec/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "cte spec",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Accounting",
