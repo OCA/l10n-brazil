e3be944a3a34fcf80e42f1852a828417860a9837
Luis Felipe Miléo <mileo@kmee.com.br>
2025-01-17 10:32:22 -0300

    [MIG] remove l10n_br_nfe.import_xml

---
+++ l10n_br_nfe/migrations/14.0.20.0.0/pre-migration.py
@@ -0,0 +1,24 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+def delete_model(cr, model_name):
+    openupgrade.logged_query(
+        cr,
+        """
+        DELETE FROM ir_model_access
+        WHERE model_id = (SELECT id FROM ir_model where model = '%s')
+        """
+        % model_name,
+    )
+    openupgrade.logged_query(
+        cr, "DELETE FROM ir_model_fields WHERE model = '%s'" % model_name
+    )
+    openupgrade.logged_query(cr, "DELETE FROM ir_model WHERE model = '%s'" % model_name)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    delete_model(env.cr, "l10n_br_nfe.import_xml")
