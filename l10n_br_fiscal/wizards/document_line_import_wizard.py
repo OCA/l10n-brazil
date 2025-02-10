1f2710c93f74a3aa2a4d42afdfdd74ebde589f53
Luis Felipe Miléo <mileo@kmee.com.br>
2025-02-10 06:49:19 -0300

    [REF][l10n_br_fiscal] doc line wizard uom match

--- l10n_br_fiscal/wizards/document_line_import_wizard.py
+++ l10n_br_fiscal/wizards/document_line_import_wizard.py
@@ -24,23 +24,50 @@ class L10nBrFiscalDocumentLineImportWizard(models.TransientModel):
     import_ncm_id = fields.Many2one("l10n_br_fiscal.ncm")
     import_cfop_id = fields.Many2one("l10n_br_fiscal.cfop", string="CFOP to Import")
     import_uom_id = fields.Many2one("uom.uom", string="UoM to Import")
+    # import_uom_id = fields.Many2one("uom.uom", string="UoM to Import")
 
     @api.model
     def default_get(self, fields_list):
         values = super().default_get(fields_list)
-        values["document_line_id"] = self.env.context.get("active_id")
-        self._prepare_default_values(values)
-        values.update(values)
+
+        # Obter o ID da linha do documento fiscal do contexto
+        document_line_id = self.env.context.get("active_id")
+        if document_line_id:
+            document_line = self.env["l10n_br_fiscal.document.line"].browse(
+                document_line_id
+            )
+
+            # Definir os valores padrão com base na linha do documento fiscal
+            values.update(
+                {
+                    "document_line_id": document_line_id,
+                    "import_product_id": document_line.product_id.id,
+                    "import_qty": document_line.quantity,
+                    "import_ncm_id": document_line.ncm_id.id,
+                    "import_cfop_id": document_line.cfop_id.id,
+                    "import_uom_id": document_line.uom_id.id,
+                }
+            )
+
         return values
+
+    def _prepare_onchange_document_line_id(self):
+        return {}
 
     def _prepare_default_values(self, values):
         return {}
 
-    def _prepare_update_values(self, values):
-        return {}
-
-    def _prepare_onchange_document_line_id(self):
-        return {}
+    def _prepare_update_values(self):
+        # Preencher todos as unidades que forem iguais do documento, para acelerar o
+        # processo
+        # Preencher a unidade alternativa
+        return {
+            "product_id": self.import_product_id.id,
+            "quantity": self.import_qty,
+            "ncm_id": self.import_ncm_id.id,
+            "cfop_id": self.import_cfop_id.id,
+            "uom_id": self.import_uom_id.id,
+        }
 
     @api.onchange("document_line_id")
     def onchange_document_line_id(self):
@@ -49,14 +76,12 @@ class L10nBrFiscalDocumentLineImportWizard(models.TransientModel):
 
     def update_document_data(self):
         self.ensure_one()
-        document_line_id = self.env.context.get("active_id")
-        if document_line_id:
-            document_line = self.env["l10n_br_fiscal.document.line"].browse(
-                document_line_id
-            )
-            values = {}
-            self._prepare_update_values(values)
-            document_line.write(values)
+        if self.document_line_id:
+            values = self._prepare_update_values()
+            self.document_line_id.write(values)
+            self._check_alternative_uom(self.import_uom_id, self.document_uom)
+            # self._check_alternative_uom(self.import_uom_id, self.document_uom_trib)
+            self._check_other_lines_info(values)
 
     def _navigate_product(self, direction=False):
         self.ensure_one()
@@ -90,3 +115,16 @@ class L10nBrFiscalDocumentLineImportWizard(models.TransientModel):
 
     def action_previous_product(self):
         return self._navigate_product(-1)
+
+    def _check_alternative_uom(self, uom_id, alternative):
+        if uom_id and uom_id.code != alternative:
+            alternative_ids = uom_id.alternative_ids.filtered(
+                lambda r: r.code == alternative
+            )
+            if not alternative_ids:
+                self.env["uom.uom.alternative"].create(
+                    {"code": alternative, "uom_id": uom_id.id}
+                )
+
+    def _check_other_lines_info(self, values):
+        pass
