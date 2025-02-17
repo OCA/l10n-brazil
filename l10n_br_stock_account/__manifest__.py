3cddc02bfb26d19af1ccb5f16ee71c5bdb9ea6cf
Magno Costa <magno.costa@akretion.com.br>
2025-02-17 20:41:29 -0300

    [REF] l10n_br_stock_account: Inventory by method

--- l10n_br_stock_account/__manifest__.py
+++ l10n_br_stock_account/__manifest__.py
@@ -35,7 +35,6 @@
         "demo/company_demo.xml",
         "demo/l10n_br_stock_account_demo.xml",
         "demo/account_journal_demo.xml",
-        "demo/stock_inventory_demo.xml",
     ],
     "installable": True,
     "post_init_hook": "post_init_hook",
