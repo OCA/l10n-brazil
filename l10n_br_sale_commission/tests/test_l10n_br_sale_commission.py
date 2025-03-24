7399bd2507bd758545b5bb32d0c581587a7860e8
Magno Costa <magno.costa@akretion.com.br>
2025-03-24 17:41:04 -0300

    [REF]l10n_br_sale_commission:Tests,rem unnec. code

--- l10n_br_sale_commission/tests/test_l10n_br_sale_commission.py
+++ l10n_br_sale_commission/tests/test_l10n_br_sale_commission.py
@@ -1,62 +1,38 @@
 # Copyright (C) 2022 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import dateutil.relativedelta
+from datetime import date
 
-from odoo import fields
-from odoo.tests import TransactionCase
+from dateutil.relativedelta import relativedelta
+
+from odoo.tests import Form, TransactionCase
 
 
 class TestL10nBrSalesCommission(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.sale_order = cls.env.ref("l10n_br_sale_commission.so_commission_br")
-        cls.advance_inv_model = cls.env["sale.advance.payment.inv"]
-        cls.settle_model = cls.env["commission.settlement"]
-        cls.make_settle_model = cls.env["commission.make.settle"]
-        cls.make_inv_model = cls.env["commission.make.invoice"]
-        # A criação da Comissão valida se o Diário
-        # usado pertence a mesma empresa.
-        # TODO - erro q acontece apenas no teste não na tela
-        #  o campo default traz um Diário de outra empresa
-        cls.journal = cls.env["account.journal"].search(
-            [
-                ("type", "=", "purchase"),
-                ("company_id", "=", cls.sale_order.company_id.id),
-            ],
-            limit=1,
-        )
 
     def test_commission_config(self):
-        config = self.env["res.config.settings"].create(
-            {
-                "commission_document_type_id": self.env.ref(
-                    "l10n_br_fiscal.document_55"
-                ).id,
-                "commission_gen_br_fiscal_doc": False,
-            }
+        config_form = Form(self.env["res.config.settings"])
+        config_form.commission_gen_br_fiscal_doc = True
+        config_form.commission_document_type_id = self.env.ref(
+            "l10n_br_fiscal.document_55"
         )
+        config = config_form.save()
         config.execute()
-        config._onchange_commission_document_type_id()
-        config._onchange_commission_gen_br_fiscal_doc()
 
     def test_commission_config_wo_doc_type(self):
-        config = self.env["res.config.settings"].create(
-            {
-                "commission_document_type_id": False,
-                "commission_gen_br_fiscal_doc": False,
-            }
-        )
+        config_form = Form(self.env["res.config.settings"])
+        config_form.commission_gen_br_fiscal_doc = False
+        config = config_form.save()
         config.execute()
-        config._onchange_commission_document_type_id()
-        config._onchange_commission_gen_br_fiscal_doc()
 
     def test_sale_order_commission_br(self):
         """
         Test Brazilian Commission
         """
-        sale_order = self.sale_order
+        sale_order = self.env.ref("l10n_br_sale_commission.so_commission_br")
         sale_order.action_confirm()
         self.assertEqual(len(sale_order.invoice_ids), 0)
         sale_order._create_invoices(final=True)
@@ -65,25 +41,14 @@ class TestL10nBrSalesCommission(TransactionCase):
             invoice.action_post()
             self.assertEqual(invoice.state, "posted")
 
-        wizard = self.make_settle_model.create(
-            {
-                "date_to": (
-                    fields.Datetime.from_string(fields.Datetime.now())
-                    + dateutil.relativedelta.relativedelta(months=1)
-                ),
-                "settlement_type": "sale_invoice",
-            }
-        )
+        # Cria o Settlements
+        with Form(self.env["commission.make.settle"]) as wiz_form:
+            wiz_form.date_to = date.today() + relativedelta(months=1)
+            wiz_form.settlement_type = "sale_invoice"
+            wiz = wiz_form.save()
+            wiz.action_settle()
 
-        settlements = self.settle_model.search(
-            [
-                ("state", "=", "settled"),
-            ]
-        )
-
-        wizard.action_settle()
-
-        settlements = self.settle_model.search(
+        settlements = self.env["commission.settlement"].search(
             [
                 ("state", "=", "settled"),
             ]
@@ -91,52 +56,38 @@ class TestL10nBrSalesCommission(TransactionCase):
 
         self.assertEqual(len(settlements), 1, "Settlements not was created.")
 
-        # Invoice Wizard
-        wizard_obj = self.make_inv_model
-        fields_list = wizard_obj.fields_get().keys()
-        wizard_values = wizard_obj.default_get(fields_list)
-        wizard_values.get("commission_document_type_id")
-        wizard_values.get("fiscal_operation_id")
+        # Cria a Fatura das Comissões/Settlements
+        with Form(self.env["commission.make.invoice"]) as wiz_form:
+            wiz = wiz_form.save()
+            wiz.button_create()
 
-        # TODO - por algum motivo aqui no teste o campo do Diário
-        #  está vindo de uma outra empresa e por isso o metodo
-        #  não encontra os settlement referente, o erro não
-        #  acontece na tela.
-        settlement_product_id = self.env.ref("product.expense_product")
-        wizard_values.update(
-            {
-                "journal_id": self.journal.id,
-                "product_id": settlement_product_id.id,
-            }
+        settlements = self.env["commission.settlement"].search(
+            [("state", "=", "invoiced")]
         )
-        wizard = wizard_obj.create(wizard_values)
-
-        # Quick test onchange doc type
-        wizard._onchange_commission_document_type_id()
-
-        wizard.button_create()
-
-        settlements = self.settle_model.search([("state", "=", "invoiced")])
         for settlement in settlements:
             self.assertNotEqual(
                 len(settlement.invoice_id),
                 0,
                 "Settlements need to be in Invoiced State.",
             )
-            # TODO: test fiscal_document_id in the future?
-            # self.assertEqual(
-            #     settlement.invoice_id.fiscal_document_id.document_type_id.id,
-            #     wizard_values.get("commission_document_type_id"),
-            #     "Fiscal Document with wrong Fiscal Document Type.",
-            # )
-            # self.assertEqual(
-            #     settlement.invoice_id.fiscal_document_id.fiscal_operation_id.id,
-            #     wizard_values.get("fiscal_operation_id"),
-            #     "Fiscal Document with wrong Fiscal Operation.",
-            # )
+            self.assertEqual(
+                settlement.invoice_id.fiscal_document_id.document_type_id,
+                self.env.ref("l10n_br_fiscal.document_SE"),
+                "Fiscal Document with wrong Fiscal Document Type.",
+            )
+            self.assertEqual(
+                settlement.invoice_id.fiscal_document_id.fiscal_operation_id,
+                self.env.ref("l10n_br_fiscal.fo_compras"),
+                "Fiscal Document with wrong Fiscal Operation.",
+            )
             for line in settlement.invoice_id.invoice_line_ids:
                 self.assertEqual(
-                    line.product_id.id,
-                    settlement_product_id.id,
+                    line.product_id,
+                    self.env.ref("l10n_br_sale_commission.service_commission"),
                     "Fiscal Document with wrong Product.",
+                )
+                self.assertEqual(
+                    line.settlement_id,
+                    settlement,
+                    "Settlement not informed in Move Line",
                 )
