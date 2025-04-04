962876408be123630377ec5946b9a56d7e1abd83
Raphaël Valyi <rvalyi@akretion.com>
2025-04-04 12:55:25 +0000

    [REM] l10n_br_nfse_paulistana: remove _onchange_ncm_id

--- l10n_br_nfse_paulistana/tests/test_fiscal_document_nfse_paulistana.py
+++ l10n_br_nfse_paulistana/tests/test_fiscal_document_nfse_paulistana.py
@@ -40,7 +40,6 @@ class TestFiscalDocumentNFSePaulistana(TestFiscalDocumentNFSeCommon):
         for line in self.nfse_same_state.fiscal_line_ids:
             line._onchange_product_id_fiscal()
             line._onchange_commercial_quantity()
-            line._onchange_ncm_id()
             line._onchange_fiscal_operation_id()
             line._onchange_fiscal_operation_line_id()
             line._onchange_fiscal_taxes()
