efb4909ffaf9ce06ca02e79082988bf081eb23b0
Raphaël Valyi <rvalyi@akretion.com>
2025-04-17 21:16:51 +0000

    [FIX] l10n_br_nfe: doc supl to SpecModel

--- l10n_br_nfe/models/document_supplement.py
+++ l10n_br_nfe/models/document_supplement.py
@@ -6,17 +6,10 @@ from odoo import fields
 from odoo.addons.spec_driven_model.models import spec_models
 
 
-class NFeSupplement(spec_models.StackedModel):
-    # FIXME: NFeSupplement should actually inherit from spec_models.SpecModel
-    # but it seems we had broken NFe or MDFe or CTe tests with SpecModel
-    # it's probably a spec_driven_model framework issue... So it has been reverted
-    # to StackedModel in https://github.com/OCA/l10n-brazil/pull/3445
+class NFeSupplement(spec_models.SpecModel):
     _name = "l10n_br_fiscal.document.supplement"
     _inherit = ["l10n_br_fiscal.document.supplement", "nfe.40.infnfesupl"]
-
-    _nfe40_odoo_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
-    _nfe40_stacking_mixin = "nfe.40.infnfesupl"
+    _nfe40_binding_type = "Tnfe.InfNfeSupl"  # avoid ambiguity with CTe and MDFe modules
 
     nfe40_qrCode = fields.Char(related="qrcode")
-
     nfe40_urlChave = fields.Char(related="url_key")
