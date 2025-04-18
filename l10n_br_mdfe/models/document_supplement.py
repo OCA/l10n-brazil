7fdf7946cb08d2c90b5091ee5c9fcb42b683cb8a
Raphaël Valyi <rvalyi@akretion.com>
2025-04-18 15:12:07 +0000

    [FIX] l10n_br_mdfe: doc supl to SpecModel

--- l10n_br_mdfe/models/document_supplement.py
+++ l10n_br_mdfe/models/document_supplement.py
@@ -6,12 +6,9 @@ from odoo import fields
 from odoo.addons.spec_driven_model.models import spec_models
 
 
-class MDFeSupplement(spec_models.StackedModel):
+class MDFeSupplement(spec_models.SpecModel):
     _name = "l10n_br_fiscal.document.supplement"
     _inherit = ["l10n_br_fiscal.document.supplement", "mdfe.30.infmdfesupl"]
-    _mdfe30_odoo_module = (
-        "odoo.addons.l10n_br_mdfe_spec.models.v3_0.mdfe_tipos_basico_v3_00"
-    )
-    _mdfe30_stacking_mixin = "mdfe.30.infmdfesupl"
+    _mdfe30_binding_type = "Tmdfe.InfMdfeSupl"  # avoid ambiguity with NFe and CTe
 
     mdfe30_qrCodMDFe = fields.Char(related="qrcode")
