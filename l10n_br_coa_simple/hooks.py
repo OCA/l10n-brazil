ed6e60f9038a00c2884b219c9e14a1a78e55f56c
Raphaël Valyi <rvalyi@akretion.com>
2025-04-20 16:07:40 +0000

    [REF] l10n_br_coa_simple: rm brittle without_demo

--- l10n_br_coa_simple/hooks.py
+++ l10n_br_coa_simple/hooks.py
@@ -6,21 +6,22 @@ from odoo import SUPERUSER_ID, api, tools
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    coa_simple_tmpl = env.ref("l10n_br_coa_simple.l10n_br_coa_simple_chart_template")
 
-    # Load COA to Demo Company
-    if not tools.config.get("without_demo"):
-        company = env.ref(
-            "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
+    # Load COA for demo Company
+    company_sn = env.ref(
+        "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
+    )
+    if company_sn:
+        coa_simple_tmpl = env.ref(
+            "l10n_br_coa_simple.l10n_br_coa_simple_chart_template"
         )
-        if company:
-            coa_simple_tmpl.try_loading(company=company)
-            tools.convert_file(
-                cr,
-                "l10n_br_coa_simple",
-                "demo/account_journal.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
-            )
+        coa_simple_tmpl.try_loading(company=company_sn)
+        tools.convert_file(
+            cr,
+            "l10n_br_coa_simple",
+            "demo/account_journal.xml",
+            None,
+            mode="init",
+            noupdate=True,
+            kind="init",
+        )
