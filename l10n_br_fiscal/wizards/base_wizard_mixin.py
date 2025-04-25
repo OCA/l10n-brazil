74d46873d6114e76b002d140cca87ea857bf8d29
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-04-25 10:56:08 -0300

    [MIG] : Migration to 17.0 - (active_id - BaseWizardMixin)

--- l10n_br_fiscal/wizards/base_wizard_mixin.py
+++ l10n_br_fiscal/wizards/base_wizard_mixin.py
@@ -68,7 +68,7 @@ class BaseWizardMixin(models.TransientModel):
         active_model = self._context.get("active_model")
 
         if self._prepare_key_fields().get(active_model):
-            active_id = self._context["active_id"]
+            active_id = self._context["id"]
             active_vals = (
                 self.env[active_model]
                 .browse(active_id)
