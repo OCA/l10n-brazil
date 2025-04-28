9808bbbd310f4381fa9b64422cc7b5c7af1420a3
Raphaël Valyi <rvalyi@akretion.com>
2025-04-28 14:18:37 +0000

    [REM] l10n_br_delivery: remove useless proxy field

--- l10n_br_delivery/models/document_mixin.py
+++ l10n_br_delivery/models/document_mixin.py
@@ -20,13 +20,6 @@ class FiscalDocumentMixin(models.AbstractModel):
         " transactions.",
     )
 
-    # proxy fields to enable writing the related (shadowed) fields
-    # to the fiscal doc line from the aml through the _inherits system
-    # despite they have the same names.
-    fiscal_proxy_incoterm_id = fields.Many2one(
-        string="Fiscal Proxy Incoterm", related="incoterm_id"
-    )
-
     carrier_id = fields.Many2one(
         comodel_name="delivery.carrier",
         string="Carrier",
