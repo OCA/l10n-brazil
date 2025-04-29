a31b007063b8e02b2af4ea4bf366726ba83e5f7e
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-04-29 17:25:37 -0300

    [MIG] l10n_br_account: hook

--- l10n_br_account/hooks.py
+++ l10n_br_account/hooks.py
@@ -1,15 +1,11 @@
 # Copyright (C) 2019 - Raphaël Valyi Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID, api
 
-
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """Relate fiscal taxes to account taxes."""
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    l10n_br_coa_charts = env["account.chart.template"].search(
-        [("parent_id", "=", env.ref("l10n_br_coa.l10n_br_coa_template").id)]
-    )
-
-    for l10n_br_coa_chart in l10n_br_coa_charts:
-        l10n_br_coa_chart.load_fiscal_taxes()
+    for company in env["res.company"].with_context(active_test=False).search([]):
+        if "br_oca" in env["account.chart.template"]._get_parent_template(
+            company.chart_template
+        ):
+            env["account.chart.template"].try_loading(company.chart_template, company)
