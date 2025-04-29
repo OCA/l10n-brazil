3e1ecf36c3f5a2e2550d9828cda096f723222223
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-04-29 12:27:02 -0300

    [MIG] l10n_br_account: account.chart.template refactor

--- l10n_br_account/__manifest__.py
+++ l10n_br_account/__manifest__.py
@@ -19,12 +19,8 @@
         # security
         "security/ir.model.access.csv",
         "security/l10n_br_account_security.xml",
-        # data
-        "data/account_tax_group.xml",
-        "data/account_tax_template.xml",
         # Views
         "views/account_tax_view.xml",
-        "views/account_tax_template_view.xml",
         "views/fiscal_operation_view.xml",
         "views/fiscal_operation_line_view.xml",
         "views/account_move_view.xml",
