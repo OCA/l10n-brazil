7a5529845a61f4317b6d95a7619e6a002c30efa0
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-04-30 11:14:19 -0300

    [MIG] l10n_br_account: refund_method

--- l10n_br_account/tests/test_invoice_refund.py
+++ l10n_br_account/tests/test_invoice_refund.py
@@ -33,7 +33,7 @@ class TestInvoiceRefund(TransactionCase):
         cls.reverse_vals = {
             "date": fields.Date.from_string("2019-02-01"),
             "reason": "no reason",
-            "refund_method": "refund",
+            # "refund_method": "refund",
             "journal_id": cls.refund_journal.id,
         }
 
