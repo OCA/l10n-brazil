2688f48bde7b88074dd35056280f79d7b7c0593a
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-04-30 11:24:08 -0300

    [MIG] l10n_br_account: compute_needed_terms

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -343,9 +343,9 @@ class AccountMove(models.Model):
                         key = frozendict(
                             {
                                 "move_id": invoice.id,
-                                "date_maturity": fields.Date.to_date(term.get("date")),
-                                "discount_date": term.get("discount_date"),
                             }
+                            # "date_maturity": fields.Date.to_date(term.get("date")),
+                            # "discount_date": term.get("discount_date"),
                             # "discount_percentage": term.get("discount_percentage"),
                         )
                         values = {
