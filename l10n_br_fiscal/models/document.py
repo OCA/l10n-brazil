f1f6f6f9e3222316f05cd822b79583d6be4de91c
Renato Lima <renatonlima@gmail.com>
2025-05-05 15:40:28 -0300

    [IMP] l10n_br_fiscal: Updated edoc purpose and added edoc refund type for credit and debit

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -16,6 +16,8 @@ from ..constants.fiscal import (
     DOCUMENT_ISSUER_PARTNER,
     EDOC_PURPOSE,
     EDOC_PURPOSE_NORMAL,
+    EDOC_REFUND_DEBIT_TYPE,
+    EDOC_REFUND_CREDIT_TYPE,
     FISCAL_IN_OUT_DICT,
     MODELO_FISCAL_CTE,
     MODELO_FISCAL_NFCE,
@@ -159,6 +161,16 @@ class Document(models.Model):
         selection=EDOC_PURPOSE,
         string="Finalidade",
         default=EDOC_PURPOSE_NORMAL,
+    )
+
+    edoc_refund_debit_type = fields.Selection(
+        selection=EDOC_REFUND_DEBIT_TYPE,
+        string="Tipo de Nota de Débito",
+    )
+
+    edoc_refund_credit_type = fields.Selection(
+        selection=EDOC_REFUND_CREDIT_TYPE,
+        string="Tipo de Nota de Crédito",
     )
 
     document_type = fields.Char(
