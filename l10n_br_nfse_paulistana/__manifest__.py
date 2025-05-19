ca97add833138778dc9e9761c1610f85d0c6ece6
Tiago Amaral <tiagoamral23@gmail.com>
2025-05-19 14:18:04 -0300

    [MIG] l10n_br_nfse_paulistana: Migration to 15.0

--- l10n_br_nfse_paulistana/__manifest__.py
+++ l10n_br_nfse_paulistana/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "NFS-e (Nota Paulistana)",
     "summary": """
         NFS-e (Nota Paulistana)""",
-    "version": "14.0.1.2.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
     "maintainers": ["gabrielcardoso21", "mileo", "luismalta"],
