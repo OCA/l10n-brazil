c1657e90db752527007a49df54e67da6bcceef00
Raphaël Valyi <rvalyi@akretion.com>
2025-05-29 04:58:37 +0000

    [MIG] l10n_br_account: adapt NFe import to v16

--- l10n_br_account/models/account_journal.py
+++ l10n_br_account/models/account_journal.py
@@ -7,9 +7,11 @@ from odoo import models
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    def create_invoice_from_attachment(self, attachment_ids=None):
+    def create_document_from_attachment(self, attachment_ids=None):
         if self.env.company.country_id.code != "BR" or len(attachment_ids) < 1:
-            return super().create_invoice_from_attachment(attachment_ids=attachment_ids)
+            return super().create_document_from_attachment(
+                attachment_ids=attachment_ids
+            )
         attachments = self.env["ir.attachment"].browse(attachment_ids)
         return self.env[
             "l10n_br_fiscal.document.import.wizard.mixin"
