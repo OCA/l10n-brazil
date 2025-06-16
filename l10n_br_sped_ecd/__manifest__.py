011d60e2c5b80c7f48d25060518d1fda08386017
Raphaël Valyi <rvalyi@akretion.com>
2025-06-16 04:08:47 +0000

    [MIG] l10n_br_sped_ecd: Migration to 16.0

--- l10n_br_sped_ecd/__manifest__.py
+++ l10n_br_sped_ecd/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "SPED - ECD",
     "summary": """
         Arquivo ECD do SPED""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
