21c78b7deab7d8c14c82e3ff037bfc5cc84bdb9e
Raphaël Valyi <rvalyi@akretion.com>
2025-06-18 02:53:23 +0000

    [FIX] l10n_br_sped_efd_pis_cofins: delete cascade

--- l10n_br_sped_efd_pis_cofins/models/sped_mixin_efd_pis_cofins.py
+++ l10n_br_sped_efd_pis_cofins/models/sped_mixin_efd_pis_cofins.py
@@ -13,6 +13,7 @@ class SpecMixinEFDPISCOFINS(models.AbstractModel):
     declaration_id = fields.Many2one(
         comodel_name="l10n_br_sped.efd_pis_cofins.0000",
         required=True,
+        ondelete="cascade",
     )
 
     state = fields.Selection(related="declaration_id.state")
