57964747ae589112efe0061223eddc0d8481d39a
Renato Lima <renatonlima@gmail.com>
2025-06-29 16:00:00 -0300

    [FIX] l10n_br_fiscal: _onchange_fiscal_operation_id removed fields not present in mixin fields

--- l10n_br_fiscal/models/document_mixin_methods.py
+++ l10n_br_fiscal/models/document_mixin_methods.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import Command, api, models
+from odoo import api, models
 
 from ..constants.fiscal import (
     COMMENT_TYPE_COMMERCIAL,
@@ -61,29 +61,9 @@ class FiscalDocumentMixinMethods(models.AbstractModel):
     def _onchange_fiscal_operation_id(self):
         if self.fiscal_operation_id:
             self.fiscal_operation_type = self.fiscal_operation_id.fiscal_operation_type
-            self.edoc_purpose = self.fiscal_operation_id.edoc_purpose
 
             if self.issuer == DOCUMENT_ISSUER_COMPANY and not self.document_type_id:
                 self.document_type_id = self.company_id.document_type_id
-
-            subsequent_documents = [Command.set({})]
-            for subsequent_id in self.fiscal_operation_id.mapped(
-                "operation_subsequent_ids"
-            ):
-                subsequent_documents.append(
-                    (
-                        0,
-                        0,
-                        {
-                            "source_document_id": self.id,
-                            "subsequent_operation_id": subsequent_id.id,
-                            "fiscal_operation_id": (
-                                subsequent_id.subsequent_operation_id.id
-                            ),
-                        },
-                    )
-                )
-            self.document_subsequent_ids = subsequent_documents
 
     def _get_amount_lines(self):
         """
