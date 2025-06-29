5e0496a6396377e436a4d3b17e1bc39fff548e87
Raphaël Valyi <rvalyi@akretion.com>
2025-06-29 22:57:14 +0000

    TEST WIP  -> KO!
     2025-06-29 23:02:36,976 650 ERROR odoo odoo.addons.l10n_br_account.tests.test_invoice_refund: ERROR: TestInvoiceRefund.test_refund
    Traceback (most recent call last):
      File "/opt/odoo-venv/lib/python3.10/site-packages/odoo/addons/l10n_br_account/tests/test_invoice_refund.py", line 112, in test_refund
        invoice.fiscal_operation_id = self.env.ref("l10n_br_fiscal.fo_venda")
      File "/opt/odoo/odoo/fields.py", line 1337, in __set__
        records.write({self.name: write_value})
      File "/opt/odoo/addons/purchase/models/account_invoice.py", line 149, in write
        res = super(AccountMove, self).write(vals)
      File "/opt/odoo/addons/account/models/account_move.py", line 2359, in write
        with self.env.protecting(self._get_protected_vals(vals, self)), self._check_balanced(container):
      File "/usr/lib/python3.10/contextlib.py", line 142, in __exit__
        next(self.gen)
      File "/opt/odoo/addons/account/models/account_move.py", line 1773, in _check_balanced
        raise UserError(error_msg)
    odoo.exceptions.UserError: An error has occurred.
    
    The move (Test Refund Invoice) is not balanced.
    The total of debits equals R$ 0.00 and the total of credits equals R$ 15.00.
    You might want to specify a default account on journal "Refund Journal - (test)" to automatically balance each move.
    
    2025-06-29 23:02:36,979 650 INFO odoo odoo.addons.l10n_br_account.tests.test_invoice_refund: Starting TestInvoiceRefund.test_refund_force_fiscal_operation ...
    2025-06-29 23:02:37,046 650 INFO odoo odoo.addons.l10n_br_account.tests.test_invoice_refund: ======================================================================
    2025-06-29 23:02:37,047 650 ERROR odoo odoo.addons.l10n_br_account.tests.test_invoice_refund: ERROR: TestInvoiceRefund.test_refund_force_fiscal_operation
    Traceback (most recent call last):
      File "/opt/odoo-venv/lib/python3.10/site-packages/odoo/addons/l10n_br_account/tests/test_invoice_refund.py", line 141, in test_refund_force_fiscal_operation
        invoice.fiscal_operation_id = self.env.ref("l10n_br_fiscal.fo_venda")
      File "/opt/odoo/odoo/fields.py", line 1337, in __set__
        records.write({self.name: write_value})
      File "/opt/odoo/addons/purchase/models/account_invoice.py", line 149, in write
        res = super(AccountMove, self).write(vals)
      File "/opt/odoo/addons/account/models/account_move.py", line 2359, in write
        with self.env.protecting(self._get_protected_vals(vals, self)), self._check_balanced(container):
      File "/usr/lib/python3.10/contextlib.py", line 142, in __exit__
        next(self.gen)
      File "/opt/odoo/addons/account/models/account_move.py", line 1773, in _check_balanced
        raise UserError(error_msg)
    odoo.exceptions.UserError: An error has occurred.
    
    The move (Draft Invoice Test Refund Invoice) is not balanced.
    The total of debits equals R$ 0.00 and the total of credits equals R$ 15.00.
    You might want to specify a default account on journal "Refund Journal - (test)" to automatically balance each move.

--- l10n_br_cte/__manifest__.py
+++ l10n_br_cte/__manifest__.py
@@ -41,7 +41,7 @@
         "demo/fiscal_document_demo.xml",
     ],
     "post_init_hook": "post_init_hook",
-    "installable": True,
+    "installable": False,
     "auto_install": False,
     "external_dependencies": {
         "python": [
