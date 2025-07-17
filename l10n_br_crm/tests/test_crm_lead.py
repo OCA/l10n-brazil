91857440fbf2ea30384066c7d4ca24b01a2959a2
Raphaël Valyi <rvalyi@akretion.com>
2025-07-17 06:07:27 +0000

    [REF] l10n_br_crm: inscr_est->l10n_br_ie_code

--- l10n_br_crm/tests/test_crm_lead.py
+++ l10n_br_crm/tests/test_crm_lead.py
@@ -20,7 +20,7 @@ class CrmLeadTest(TransactionCase):
                 "cnpj": "56.647.352/0001-98",
                 "stage_id": cls.env.ref("crm.stage_lead1").id,
                 "partner_name": "Test Partner",
-                "inscr_est": "079.798.013.363",
+                "l10n_br_ie_code": "079.798.013.363",
                 "inscr_mun": "99999999",
             }
         )
@@ -44,7 +44,7 @@ class CrmLeadTest(TransactionCase):
                 "cnpj": "57.240.310/0001-09",
                 "stage_id": cls.env.ref("crm.stage_lead1").id,
                 "partner_name": "Test Partner 1",
-                "inscr_est": "041.092.540.590",
+                "l10n_br_ie_code": "041.092.540.590",
                 "inscr_mun": "99999999",
                 "country_id": cls.env.ref("base.br").id,
                 "state_id": cls.env.ref("base.state_br_sp").id,
@@ -57,7 +57,7 @@ class CrmLeadTest(TransactionCase):
                 "name": "Test Lead Partner",
                 "legal_name": "Test Lead Partner",
                 "cnpj_cpf": "22.898.817/0001-61",
-                "inscr_est": "041.092.540.590",
+                "l10n_br_ie_code": "041.092.540.590",
                 "inscr_mun": "99999999",
                 "l10n_br_isuf_code": "99999999",
                 "street_number": "1225",
@@ -117,7 +117,8 @@ class CrmLeadTest(TransactionCase):
         )
         self.assertTrue(self.obj_partner.cnpj_cpf, "The field CNPJ not was filled.")
         self.assertTrue(
-            self.obj_partner.inscr_est, "The field Inscrição Estadual not was filled"
+            self.obj_partner.l10n_br_ie_code,
+            "The field Inscrição Estadual not was filled",
         )
         self.assertTrue(
             self.obj_partner.inscr_mun, "The field Inscrição Municipal not was filled"
@@ -170,7 +171,7 @@ class CrmLeadTest(TransactionCase):
 
         self.assertTrue(self.obj_partner.name, "The field Name was not filled.")
         self.assertTrue(self.obj_partner.cnpj_cpf, "The field CNPJ was not filled.")
-        self.assertTrue(self.obj_partner.inscr_est, "The field RG was not filled")
+        self.assertTrue(self.obj_partner.l10n_br_ie_code, "The field RG was not filled")
 
     def test_lead_won_contact(self):
         """Test to mark the Lead as won"""
@@ -195,7 +196,8 @@ class CrmLeadTest(TransactionCase):
         )
         self.assertTrue(self.obj_partner.cnpj_cpf, "The field CNPJ not was filled.")
         self.assertTrue(
-            self.obj_partner.inscr_est, "The field Inscrição Estadual not was filled"
+            self.obj_partner.l10n_br_ie_code,
+            "The field Inscrição Estadual not was filled",
         )
         self.assertTrue(
             self.obj_partner.inscr_mun, "The field Inscrição Municipal not was filled"
@@ -226,10 +228,10 @@ class CrmLeadTest(TransactionCase):
         )
 
         self.assertEqual(
-            self.crm_lead_company_1.inscr_est,
+            self.crm_lead_company_1.l10n_br_ie_code,
             "041.092.540.590",
             "In the change of the partner \
-                         the field inscr_est was not automatically filled.",
+                         the field l10n_br_ie_code was not automatically filled.",
         )
 
         self.assertEqual(
