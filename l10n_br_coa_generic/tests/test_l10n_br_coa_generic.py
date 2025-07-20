48292b040fc9bc83608606e6d8e35dca8b273ff5
Raphaël Valyi <rvalyi@akretion.com>
2025-07-20 14:57:20 +0000

    [REF] l10n_br_coa_generic: use setUpClass in tests

--- l10n_br_coa_generic/tests/test_l10n_br_coa_generic.py
+++ l10n_br_coa_generic/tests/test_l10n_br_coa_generic.py
@@ -5,13 +5,13 @@ from odoo.tests.common import TransactionCase
 
 
 class L10nBrCoaGeneric(TransactionCase):
-    def setUp(self):
-        super().setUp()
-
-        self.l10n_br_coa_generic = self.env.ref(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.l10n_br_coa_generic = cls.env.ref(
             "l10n_br_coa_generic.l10n_br_coa_generic_template"
         )
-        self.l10n_br_company = self.env["res.company"].create(
+        cls.l10n_br_company = cls.env["res.company"].create(
             {"name": "Empresa Teste do Plano de Contas CFC"}
         )
 
