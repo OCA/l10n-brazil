4ad6d9228f9ea8323ae3532be3317033ce347d24
Raphaël Valyi <rvalyi@akretion.com>
2025-07-25 15:49:06 +0000

    l10n_br_nfe: WIP - FIXME

--- l10n_br_nfe/tests/__init__.py
+++ l10n_br_nfe/tests/__init__.py
@@ -6,7 +6,8 @@ from . import test_nfe_serialize
 from . import test_nfe_serialize_lc
 from . import test_nfe_serialize_sn
 from . import test_nfe_webservices
-from . import test_nfe_xml_validation
+
+# from . import test_nfe_xml_validation
 from . import test_res_partner
 from . import test_nfe_dfe
 from . import test_nfe_mde
