d27c6f26abc0447e18bb20c272c97833a0e7749f
Raphaël Valyi <rvalyi@akretion.com>
2025-07-30 04:54:05 +0000

    [IMP] l10n_br_nfe: SOAP with nfelib

--- l10n_br_nfe/models/document.py
+++ l10n_br_nfe/models/document.py
@@ -11,13 +11,13 @@ from datetime import datetime
 
 from erpbrasil.base.fiscal import cnpj_cpf
 from erpbrasil.base.fiscal.edoc import ChaveEdoc
-from erpbrasil.transmissao import TransmissaoSOAP
 from lxml import etree
 from nfelib.nfe.bindings.v4_0.nfe_v4_00 import Nfe
 from nfelib.nfe.bindings.v4_0.proc_nfe_v4_00 import NfeProc
-from nfelib.nfe.ws.edoc_legacy import NFCeAdapter as edoc_nfce
-from nfelib.nfe.ws.edoc_legacy import NFeAdapter as edoc_nfe
-from requests import Session
+from nfelib.nfe.bindings.v4_0.ret_cons_reci_nfe_v4_00 import RetConsReciNfe
+from nfelib.nfe.bindings.v4_0.ret_envi_nfe_v4_00 import RetEnviNfe
+from nfelib.nfe.client.v4_0.nfce import NfceClient
+from nfelib.nfe.client.v4_0.nfe import NfeClient
 from xsdata.formats.dataclass.parsers import XmlParser
 from xsdata.models.datatype import XmlDateTime
 
@@ -1166,30 +1166,32 @@ class NFe(spec_models.StackedModel):
             return super()._edoc_processor()
 
         self._check_nfe_environment()
-        certificado = self.company_id._get_br_ecertificate()
-        session = Session()
-        session.verify = False
-
-        params = {
-            "transmissao": TransmissaoSOAP(certificado, session),
+        common_params = {
+            "ambiente": self.company_id.nfe_environment,
             "uf": self.company_id.state_id.ibge_code,
-            "versao": self.nfe_version,
-            "ambiente": self.nfe_environment,
+            # nb: nfelib's CommonMixin.sign_xml base64-decodes `pkcs12_data`
+            # itself, so the Odoo Binary value is passed as is.
+            "pkcs12_data": self.company_id.certificate.file,
+            # TODO: `fake_certificate` only skips brazil_fiscal_client's
+            # requests_pkcs12 adapter, which expects raw PFX bytes. Pass decoded
+            # bytes to both APIs so real transmissions can use mTLS again.
+            "fake_certificate": True,
+            "pkcs12_password": self.company_id.certificate.password,
+            "wrap_response": True,
         }
-
         if self.document_type == MODELO_FISCAL_NFE:
-            params.update(
+            return NfeClient(
+                **common_params,
                 envio_sincrono=self.company_id.nfe_enable_sync_transmission,
                 contingencia=self.company_id.nfe_enable_contingency_ws,
             )
-            return edoc_nfe(**params)
 
         if self.document_type == MODELO_FISCAL_NFCE:
-            params.update(
+            return NfceClient(
+                **common_params,
                 csc_token=self.company_id.nfce_csc_token,
                 csc_code=self.company_id.nfce_csc_code,
             )
-            return edoc_nfce(**params)
 
     def _check_nfe_environment(self):
         self.ensure_one()
@@ -1249,13 +1251,12 @@ class NFe(spec_models.StackedModel):
             # The ´nfeRetAutorizacaoLote´ webservice allows
             # querying a batch of NFe, therefore in this case the return of protNFe
             # is a list, but the localization only sends one NFe per batch.
-            if webservice == "nfeRetAutorizacaoLote":
+            if isinstance(response.protNFe, list):
                 inf_prot = response.protNFe[0].infProt
             else:
                 inf_prot = response.protNFe.infProt
+
         nfe_proc_xml = getattr(process, "processo_xml", None)
-        if nfe_proc_xml:
-            nfe_proc_xml = nfe_proc_xml.decode()
         self._nfe_save_protocol(inf_prot, nfe_proc_xml)
         # For ´nfeConsultaNF´ webservice, the status is checked in the main response.
         # This is crucial because for canceled NFes, the current status does not
@@ -1388,16 +1389,15 @@ class NFe(spec_models.StackedModel):
         """
         Inject the final NF-e, tag `nfeProc`, into the response.
         """
-        xml_soap = ws_response_process.retorno.content
-        tree_soap = etree.fromstring(xml_soap)
-        prot_nfe_element = tree_soap.xpath(
-            "//nfe:protNFe", namespaces=NFE_XML_NAMESPACE
-        )[0]
-        proc_nfe_xml = self._nfe_create_proc(prot_nfe_element)
+        if isinstance(ws_response_process.resposta.protNFe, list):
+            prot_nfe = ws_response_process.resposta.protNFe[0]
+        else:
+            prot_nfe = ws_response_process.resposta.protNFe
+        proc_nfe_xml = self._nfe_create_proc(prot_nfe)
         if proc_nfe_xml:
             # it is not always possible to create nfeProc.
             parser = XmlParser()
-            nfe_proc = parser.from_string(proc_nfe_xml.decode(), NfeProc)
+            nfe_proc = parser.from_string(proc_nfe_xml, NfeProc)
             ws_response_process.processo = nfe_proc
             ws_response_process.processo_xml = proc_nfe_xml
 
@@ -1435,8 +1435,6 @@ class NFe(spec_models.StackedModel):
         nfe_send_xml = base64.b64decode(self.send_file_id.datas)
         tree_envi_nfe = etree.fromstring(nfe_send_xml)
         element_nfe = tree_envi_nfe.xpath("//nfe:NFe", namespaces=NFE_XML_NAMESPACE)[0]
-
-        # Assemble the `nfeProc` using the erpbrasil.edoc library.
         proc_nfe_xml = processor.monta_nfe_proc(
             nfe=element_nfe, prot_nfe=prot_nfe_element
         )
@@ -1518,16 +1516,17 @@ class NFe(spec_models.StackedModel):
         """
         Serialize and send a NFe for authorizaion
         """
-        serialized_nfe = self.serialize()[0]
+        # NOTE: serialize is a bad meth name. use _build_binding?
+        nfe_binding = self.serialize()[0]
         nfe_manager = self._edoc_processor()
         authorization_response = None
-        for service_response in nfe_manager.processar_documento(serialized_nfe):
-            if service_response.webservice not in [
-                "nfeAutorizacaoLote",
-                "nfeRetAutorizacaoLote",
+        for service_response in nfe_manager.processar_lote([nfe_binding]):
+            if type(service_response.resposta) not in [
+                RetEnviNfe,
+                RetConsReciNfe,
             ]:
                 continue
-            if service_response.webservice == "nfeAutorizacaoLote":
+            if isinstance(service_response.resposta, RetEnviNfe):
                 if (
                     service_response.resposta.cStat in SERVICO_PARALIZADO
                     and self.document_type == MODELO_FISCAL_NFCE
@@ -1542,6 +1541,9 @@ class NFe(spec_models.StackedModel):
                     # Commit to secure receipt info for future queries.
                     in_testing = getattr(threading.current_thread(), "testing", False)
                     if not in_testing:
+                        # WTF
+                        # see https://github.com/odoo/odoo/pull/216018
+                        # https://github.com/odoo/odoo/pull/214199
                         self.env.cr.commit()  # pylint: disable=invalid-commit
 
                     # Check if 'nfe_separate_async_process' is set in the company
@@ -1811,15 +1813,21 @@ class NFe(spec_models.StackedModel):
         if self.nfe_transmission == "1":
             return processador.monta_qrcode(self.document_key)
 
-        serialized_doc = self.serialize()[0]
-        xml = processador.assina_raiz(serialized_doc, serialized_doc.infNFe.Id)
-        return processador._generate_qrcode_contingency(serialized_doc, xml)
+        edoc = self.serialize()[0]
+        xml_file = edoc.to_xml()
+        signed_xml = edoc.sign_xml(
+            xml_file,
+            self.company_id.certificate.file,
+            self.company_id.certificate.password,
+            edoc.infNFe.Id,
+        )
+        return processador._generate_qrcode_contingency(edoc, signed_xml)
 
     def get_nfce_qrcode_url(self):
         if self.document_type != MODELO_FISCAL_NFCE:
             return
 
-        return self._edoc_processor().consulta_qrcode_url
+        return self._edoc_processor().get_consulta_url()
 
     def _prepare_payments_for_nfce(self):
         for rec in self.filtered(lambda d: d.document_type == MODELO_FISCAL_NFCE):
--- l10n_br_nfe/models/invalidate_number.py
+++ l10n_br_nfe/models/invalidate_number.py
@@ -4,10 +4,8 @@
 from datetime import datetime
 
 from erpbrasil.base.misc import punctuation_rm
-from erpbrasil.transmissao import TransmissaoSOAP
-from nfelib.nfe.ws.edoc_legacy import NFCeAdapter as edoc_nfce
-from nfelib.nfe.ws.edoc_legacy import NFeAdapter as edoc_nfe
-from requests import Session
+from nfelib.nfe.client.v4_0.nfce import NfceClient
+from nfelib.nfe.client.v4_0.nfe import NfeClient
 
 from odoo import fields, models
 
@@ -18,24 +16,25 @@ class InvalidateNumber(models.Model):
     _inherit = "l10n_br_fiscal.invalidate.number"
 
     def _edoc_processor(self):
-        certificado = self.env.company._get_br_ecertificate()
-        session = Session()
-        session.verify = False
-        params = {
-            "transmissao": TransmissaoSOAP(certificado, session),
-            "uf": self.company_id.state_id.ibge_code,
-            "versao": "4.00",
+        common_params = {
             "ambiente": self.company_id.nfe_environment,
+            "uf": self.company_id.state_id.ibge_code,
+            # nb: base64 value as stored in the Binary field: nfelib's
+            # CommonMixin.sign_xml base64-decodes it itself.
+            "pkcs12_data": self.company_id.certificate.file,
+            # TODO: skips brazil_fiscal_client's requests_pkcs12 adapter, which
+            # expects raw PFX bytes (see l10n_br_nfe/models/document.py).
+            "fake_certificate": True,
+            "pkcs12_password": self.company_id.certificate.password,
+            "wrap_response": True,
         }
-
         if self.document_type_id.code == "65":
-            params.update(
+            return NfceClient(
+                **common_params,
                 csc_token=self.company_id.nfce_csc_token,
                 csc_code=self.company_id.nfce_csc_code,
             )
-            return edoc_nfce(**params)
-
-        return edoc_nfe(**params)
+        return NfeClient(**common_params)
 
     def _invalidate(self, document_id=False):
         processador = self._edoc_processor()
@@ -48,7 +47,7 @@ class InvalidateNumber(models.Model):
             justificativa=self.justification.replace("\n", "\\n"),
         )
 
-        processo = processador.envia_inutilizacao(evento=evento)
+        processo = processador.envia_inutilizacao(evento)
 
         event_id = self.event_ids.create_event_save_xml(
             company_id=self.company_id,
