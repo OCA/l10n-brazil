16f670aff456edd7d28f7976d88c864d7bc6da58
Antônio Neto <neto@engenere.one>
2025-08-02 12:32:24 -0300

    [REF] l10n_br_account: account layer for fiscal document

--- l10n_br_account/__manifest__.py
+++ l10n_br_account/__manifest__.py
@@ -31,16 +31,10 @@
         "views/account_move_view.xml",
         "views/document_line_view.xml",
         "views/document_view.xml",
-        "views/fiscal_invoice_view.xml",
-        "views/fiscal_invoice_line_view.xml",
         # Wizards
         "wizards/account_move_reversal_view.xml",
         "wizards/wizard_document_status.xml",
         "wizards/document_import_wizard_mixin.xml",
-        # Actions
-        "views/l10n_br_account_action.xml",
-        # Menus
-        "views/l10n_br_account_menu.xml",
         # Report
         #        "report/account_invoice_report_view.xml",
         "views/res_partner_view.xml",
