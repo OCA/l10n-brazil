ce100fce37f184e864b3c348e553d48ab439bb22
Raphaël Valyi <rvalyi@akretion.com>
2025-08-14 10:11:38 +0000

    [REF] l10n_br_fiscal: extra edition/changes tests

--- l10n_br_fiscal/models/document_line_mixin_methods.py
+++ l10n_br_fiscal/models/document_line_mixin_methods.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from contextlib import nullcontext
 from copy import deepcopy
 
 from lxml import etree
