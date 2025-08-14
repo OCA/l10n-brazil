997090e78facb03707f3b25f778415b90a6d85d0
Magno Costa <magno.costa@akretion.com.br>
2025-08-14 16:00:29 -0300

    [REF]l10n_br_sale_commission:Get Default by Browse

--- l10n_br_sale_commission/wizards/wizard_invoice.py
+++ l10n_br_sale_commission/wizards/wizard_invoice.py
@@ -1,4 +1,6 @@
-# Copyright (C) 2022 - TODAY Renato Lima - Akretion
+# Copyright (C) 2022-Today - Akretion (<http://www.akretion.com>).
+# @author Renato Lima <renato.lima@akretion.com.br>
+# @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
@@ -12,51 +14,26 @@ class CommissionMakeInvoice(models.TransientModel):
         document_type_id = get_param(
             "l10n_br_sale_commission.commission_document_type_id"
         )
-        document_type_search = False
         if document_type_id:
-            # TODO - Diferenças entre usar o search e o browse
-            #  l10n_br_fiscal.document.type(39,)
-            #  l10n_br_fiscal.document.type('39',)
-            #  No caso do browse com aspas '39' retorna erro na tela ao abrir
-            #  o wizard:
-            #      Database fetch misses ids (('39',)) and has extra ids ((39,)),
-            #      may be caused by a type incoherence in a previous request/
-            #  Testar na migração
-            # document_type = (
-            #     self.env["l10n_br_fiscal.document.type"]
-            #     .sudo()
-            #    .browse(document_type_id)
-
-            document_type_search = (
+            return (
                 self.env["l10n_br_fiscal.document.type"]
                 .sudo()
-                .search([("id", "=", document_type_id)])
+                .browse(int(document_type_id))
             )
-        return document_type_search
 
     def _default_fiscal_operation_id(self):
         get_param = self.env["ir.config_parameter"].sudo().get_param
         fiscal_operation_id = get_param(
             "l10n_br_sale_commission.commission_fiscal_operation_id"
         )
-        fiscal_operation = False
         if fiscal_operation_id:
-            # TODO - Mesmo erro da diferença entre o objeto com aspas
-            fiscal_operation = self.env["l10n_br_fiscal.operation"].search(
-                [("id", "=", fiscal_operation_id)]
-            )
-
-        return fiscal_operation
+            return self.env["l10n_br_fiscal.operation"].browse(int(fiscal_operation_id))
 
     def _default_product_id(self):
         get_param = self.env["ir.config_parameter"].sudo().get_param
         product_id = get_param("l10n_br_sale_commission.commission_product_id")
-        product = False
         if product_id:
-            # TODO - Mesmo erro da diferença entre o objeto com aspas
-            product = self.env["product.product"].search([("id", "=", product_id)])
-
-        return product
+            return self.env["product.product"].browse(int(product_id))
 
     commission_gen_br_fiscal_doc = fields.Boolean(
         string="Generate Brazilian Fiscal Document",
