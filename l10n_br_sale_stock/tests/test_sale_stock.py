43b138c045fa8d3d069a2f4007b6877c44fa971b
Raphaël Valyi <rvalyi@akretion.com>
2025-08-14 15:59:13 +0000

    [FIX] l10n_br_sale_stock: fix tests

--- l10n_br_sale_stock/tests/test_sale_stock.py
+++ l10n_br_sale_stock/tests/test_sale_stock.py
@@ -49,6 +49,8 @@ class TestSaleStock(TestBrPickingInvoicingCommon):
             line.product_uom_qty = 3
 
         self.so = sale_form.save()
+        for line in self.so.order_line:
+            line._compute_tax_fields()
 
         # confirm our standard so, check the picking
         self.so.action_confirm()
@@ -138,6 +140,8 @@ class TestSaleStock(TestBrPickingInvoicingCommon):
         ).product_id.list_price = 100
         sale_order_form = Form(sale_order_2)
         sale_order = sale_order_form.save()
+        for line in sale_order.order_line:
+            line._compute_tax_fields()
         sale_order.action_confirm()
         # Metodo de criação da fatura a partir do sale.order
         # deve gerar apenas a linha de serviço
@@ -407,6 +411,8 @@ class TestSaleStock(TestBrPickingInvoicingCommon):
         sale_order_1 = self.env.ref("l10n_br_sale_stock.lucro_presumido-sale_order_1")
         sale_order_form = Form(sale_order_1)
         sale_order = sale_order_form.save()
+        for line in sale_order.order_line:
+            line._compute_tax_fields()
         sale_order.incoterm = self.env.ref("account.incoterm_FOB")
 
         sale_order.action_confirm()
