7d9a7b583552293e950fb94fab4d86c7327710c2
Magno Costa <magno.costa@akretion.com.br>
2025-08-14 16:11:53 -0300

    [IMP] l10n_br_sale_commission: Check Refund case

--- l10n_br_sale_commission/tests/test_l10n_br_sale_commission.py
+++ l10n_br_sale_commission/tests/test_l10n_br_sale_commission.py
@@ -1,4 +1,6 @@
-# Copyright (C) 2022 - TODAY Renato Lima - Akretion
+# Copyright (C) 2022-Today - Akretion (<http://www.akretion.com>).
+# @author Renato Lima <renato.lima@akretion.com.br>
+# @author Magno Costa <magno.costa@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from datetime import date
@@ -28,19 +30,7 @@ class TestL10nBrSalesCommission(TransactionCase):
         config = config_form.save()
         config.execute()
 
-    def test_sale_order_commission_br(self):
-        """
-        Test Brazilian Commission
-        """
-        sale_order = self.env.ref("l10n_br_sale_commission.so_commission_br")
-        sale_order.action_confirm()
-        self.assertEqual(len(sale_order.invoice_ids), 0)
-        sale_order._create_invoices(final=True)
-        self.assertNotEqual(len(sale_order.invoice_ids), 0)
-        for invoice in sale_order.invoice_ids:
-            invoice.action_post()
-            self.assertEqual(invoice.state, "posted")
-
+    def _get_settlements_invoice(self):
         # Cria o Settlements
         with Form(self.env["commission.make.settle"]) as wiz_form:
             wiz_form.date_to = date.today() + relativedelta(months=1)
@@ -75,6 +65,10 @@ class TestL10nBrSalesCommission(TransactionCase):
                 self.env.ref("l10n_br_fiscal.document_SE"),
                 "Fiscal Document with wrong Fiscal Document Type.",
             )
+            self.assertTrue(
+                settlement.invoice_id.fiscal_document_id.document_serie_id,
+                "Fiscal Document withiout Document Serie.",
+            )
             self.assertEqual(
                 settlement.invoice_id.fiscal_document_id.fiscal_operation_id,
                 self.env.ref("l10n_br_fiscal.fo_compras"),
@@ -91,3 +85,110 @@ class TestL10nBrSalesCommission(TransactionCase):
                     settlement,
                     "Settlement not informed in Move Line",
                 )
+                self.assertTrue(
+                    line.fiscal_operation_id,
+                    "Fiscal Operation not informed in Move Line",
+                )
+                self.assertTrue(
+                    line.fiscal_operation_line_id,
+                    "Fiscal Operation Line not informed in Move Line",
+                )
+
+                # Verifica a necessidade de rodar o onchange_fiscal_operation_id
+                self.assertTrue(
+                    line.fiscal_tax_ids,
+                    "Fiscal Tax not informed in Move Line",
+                )
+                self.assertTrue(
+                    line.icms_cst_id,
+                    "ICMS CST not informed in Move Line",
+                )
+                # A Linha de Operação Fiscal 'Prestação de Serviço'
+                # da Operação Fiscal 'Compras' não tem CFOP.
+                # TODO: Essa Linha de OP Fiscal deveria ter CFOP?
+                # self.assertTrue(
+                #    line.cfop_id,
+                #    "CFOP not informed in Move Line",
+                # )
+
+        return settlements.mapped("invoice_id")
+
+    def test_sale_order_commission_br(self):
+        """Test Brazilian Commission"""
+
+        sale_order = self.env.ref("l10n_br_sale_commission.so_commission_br")
+        sale_order.action_confirm()
+        self.assertEqual(len(sale_order.invoice_ids), 0)
+        sale_order._create_invoices(final=True)
+        self.assertNotEqual(len(sale_order.invoice_ids), 0)
+
+        invoice_with_commission = sale_order.mapped("invoice_ids")
+        invoice_with_commission.action_post()
+        self.assertEqual(invoice_with_commission.state, "posted")
+
+        # Pagamento
+        journal_cash = self.env["account.journal"].search(
+            [("type", "=", "cash"), ("company_id", "=", self.env.company.id)],
+            limit=1,
+        )
+
+        payment_register = Form(
+            self.env["account.payment.register"].with_context(
+                active_model="account.move",
+                active_ids=invoice_with_commission.ids,
+            )
+        )
+        payment_register.journal_id = journal_cash
+        method_lines = journal_cash._get_available_payment_method_lines(
+            "inbound"
+        ).filtered(lambda x: x.code == "manual")
+        payment_register.payment_method_line_id = method_lines[0]
+        payment_register.amount = invoice_with_commission.amount_total
+        payment_register.save()._create_payments()
+
+        # Cria o Settlements
+        settlement_invoice = self._get_settlements_invoice()
+        self.assertEqual(settlement_invoice.move_type, "in_invoice")
+        settlement_invoice.action_post()
+
+        # Refund
+
+        # Alteração do 'type' do 'account.journal' necessária para evitar erro:
+        #   addons/account/models/account_move.py", line 1823,
+        #   in _check_journal_move_type
+        #     raise ValidationError(_("Cannot create a sale document in a non
+        #     sale journal"))
+        #     odoo.exceptions.ValidationError: Cannot create a sale document
+        #     in a non sale journal
+        # TODO: Fazer essa alteração nos Dados? Ou criar um novo Diário para
+        #  Devoluções de Venda?
+        devolution_journal = self.env.ref(
+            "l10n_br_coa_simple.general_journal_main_company"
+        )
+        devolution_journal.type = "sale"
+
+        refund_invoice_with_commission = invoice_with_commission._reverse_moves(
+            default_values_list=[
+                {
+                    "invoice_date": invoice_with_commission.invoice_date,
+                }
+            ]
+        )
+        self.assertEqual(
+            invoice_with_commission.invoice_line_ids.agent_ids.agent_id,
+            refund_invoice_with_commission.invoice_line_ids.agent_ids.agent_id,
+        )
+        refund_invoice_with_commission.invoice_line_ids.agent_ids._compute_amount()
+        refund_invoice_with_commission.action_post()
+
+        # Refund Commission to be Pay
+        settlements_invoice = self._get_settlements_invoice()
+        refund_settlement_invoice = settlements_invoice.filtered(
+            lambda st: st.move_type == "in_refund"
+        )
+        self.assertEqual(
+            len(refund_settlement_invoice),
+            1,
+            "Refund Commission Invoice was not Created.",
+        )
+        refund_settlement_invoice.action_post()
