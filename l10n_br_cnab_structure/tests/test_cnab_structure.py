85569680cf32b720b40b17b1a6612d15722b922c
Kaynnan Lemes <kaynnanx1@gmail.com>
2025-08-23 00:30:04 -0300

    [IMP] l10n_br_cnab_structure: unique sequence per segment
    (cherry picked from commit ee3747635883f3b20b61f2d0430ec5e3e7330501)

--- l10n_br_cnab_structure/tests/test_cnab_structure.py
+++ l10n_br_cnab_structure/tests/test_cnab_structure.py
@@ -1,9 +1,13 @@
 # Copyright (C) 2022 - Engenere (<https://engenere.one>).
+# Copyright (C) 2025 Escodoo (https://www.escodoo.com.br)
 # @author Antônio S. Pereira Neto <neto@engenere.one>
+# @author Kaynnan Lemes <kaynnan.lemes@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 import base64
+import tempfile
+from pathlib import Path
 
 from odoo import fields
 from odoo.tests import Form
@@ -36,12 +40,14 @@ class TestCNABStructure(AccountTestInvoicingCommon):
         cls.account_payment_method_model = cls.env["account.payment.method"]
         cls.res_partner_pix_model = cls.env["res.partner.pix"]
         cls.bank_341 = cls.env.ref("l10n_br_base.res_bank_341")
+        cls.bank_001 = cls.env.ref("l10n_br_base.res_bank_001")
         cls.import_wizard_obj = cls.env["cnab.import.wizard"]
         cls.cnab_log_obj = cls.env["l10n_br_cnab.return.log"]
         cls.partner_a.update({"cnpj_cpf": "45823449000198"})
         cls.cnab_structure_itau_240 = cls.env.ref(
             "l10n_br_cnab_structure.cnab_itau_240"
         )
+        cls.cnab_structure_bb_240 = cls.env.ref("l10n_br_cnab_structure.cnab_bb_240")
         cls.res_partner_pix_model.create(
             {
                 "partner_id": cls.partner_a.id,
@@ -67,6 +73,24 @@ class TestCNABStructure(AccountTestInvoicingCommon):
                 "bank_id": cls.bank_341.id,
             }
         )
+        cls.bb_bank_account = cls.res_partner_bank_model.create(
+            {
+                "acc_number": "12345",  # Example BB account number
+                "bra_number": "1234",  # Example BB branch number
+                "bank_id": cls.bank_001.id,
+                "company_id": cls.company.id,
+                "partner_id": cls.company.partner_id.id,
+            }
+        )
+        cls.bank_journal_bb = cls.env["account.journal"].create(
+            {
+                "name": "BB Bank",
+                "type": "bank",
+                "code": "BNK_BB",
+                "bank_account_id": cls.bb_bank_account.id,
+                "bank_id": cls.bank_001.id,
+            }
+        )
         cls.outbound_payment_method = cls.env.ref(
             "l10n_br_account_payment_order.payment_mode_type_cnab240_out"
         )
@@ -88,6 +112,30 @@ class TestCNABStructure(AccountTestInvoicingCommon):
                         [
                             cls.env.ref(
                                 "l10n_br_cnab_structure.cnab_itau_240_pay_way_45"
+                            ).id
+                        ],
+                    )
+                ],
+            }
+        )
+        cls.pix_mode_bb = cls.payment_mode_model.create(
+            {
+                "bank_account_link": "fixed",
+                "name": "Pix Transfer BB",
+                "company_id": cls.company.id,
+                "payment_method_id": cls.outbound_payment_method.id,
+                "payment_mode_domain": "pix_transfer",
+                "payment_order_ok": True,
+                "fixed_journal_id": cls.bank_journal_bb.id,
+                "cnab_processor": "oca_processor",
+                "cnab_structure_id": cls.cnab_structure_bb_240.id,
+                "cnab_payment_way_ids": [
+                    (
+                        6,
+                        0,
+                        [
+                            cls.env.ref(
+                                "l10n_br_cnab_structure.cnab_bb_240_pay_way_45"
                             ).id
                         ],
                     )
@@ -392,3 +440,84 @@ class TestCNABStructure(AccountTestInvoicingCommon):
         self.assertFalse(cnab_field_id.content_source_field)
         field_select_wizard.action_confirm()
         self.assertEqual(cnab_field_id.content_source_field, "company_partner_bank_id")
+
+    def test_file_generate_bb_seq_detail_with_temp_files(self):
+        """Test BB 240 detail sequence with unique_seq_per_segment both False and True
+        using temporary files to compare generated CNAB output.
+        """
+        # Create an invoice for Banco do Brasil (BB)
+        invoice_bb = self.env["account.move"].create(
+            {
+                "partner_id": self.partner_a.id,
+                "move_type": "in_invoice",
+                "ref": "Test BB Invoice",
+                "invoice_date": fields.Date.today(),
+                "company_id": self.company.id,
+                "payment_mode_id": self.pix_mode_bb.id,
+                "journal_id": self.company_data["default_journal_purchase"].id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product_a.id,
+                            "quantity": 1.0,
+                            "price_unit": 150.0,
+                        },
+                    )
+                ],
+            }
+        )
+        invoice_bb.action_post()
+
+        # Add invoice to payment order
+        self.env["account.invoice.payment.line.multi"].with_context(
+            active_model="account.move", active_ids=invoice_bb.ids
+        ).create({}).run()
+
+        payment_order = self.env["account.payment.order"].search(
+            [
+                ("state", "=", "draft"),
+                ("payment_type", "=", "outbound"),
+                ("company_id", "=", self.company.id),
+            ]
+        )
+        self.assertEqual(len(payment_order), 1)
+
+        cnab_structure = self.cnab_structure_bb_240
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_path = Path(tmp_dir)
+
+            # --- Case 1: unique_seq_per_segment = False ---
+            cnab_structure.unique_seq_per_segment = False
+            payment_order.draft2open()
+            action_false = payment_order.open2generated()
+            cnab_file_false = self.attachment_model.browse(action_false["res_id"])
+            cnab_data_false = base64.b64decode(cnab_file_false.datas).decode()
+
+            tmp_file_false = tmp_path / "bb_seq_false.rem"
+            tmp_file_false.write_text(cnab_data_false)
+
+            # Check that all detail segment sequences are the same
+            lines_false = cnab_data_false.splitlines()
+            seqs_false = [line[8:13] for line in lines_false if line[7] == "3"]
+            self.assertTrue(
+                all(seq == seqs_false[0] for seq in seqs_false),
+                "Sequences should be identical when unique_seq_per_segment = False",
+            )
+
+            # --- Case 2: unique_seq_per_segment = True ---
+            cnab_structure.unique_seq_per_segment = True
+            action_true = payment_order.open2generated()
+            cnab_file_true = self.attachment_model.browse(action_true["res_id"])
+            cnab_data_true = base64.b64decode(cnab_file_true.datas).decode()
+
+            tmp_file_true = tmp_path / "bb_seq_true.rem"
+            tmp_file_true.write_text(cnab_data_true)
+
+            # Check that all detail segment sequences are unique and sorted
+            lines_true = cnab_data_true.splitlines()
+            seqs_true = [line[8:13] for line in lines_true if line[7] == "3"]
+            self.assertEqual(seqs_true, sorted(seqs_true))
+            self.assertEqual(len(seqs_true), len(set(seqs_true)))
