97805d88d2e2a49e1c55193bf6032322c9aeed6e
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-09-09 15:20:00 -0300

    [IMP] l10n_br_base: remove version specifier dependency

--- l10n_br_base/__manifest__.py
+++ l10n_br_base/__manifest__.py
@@ -40,7 +40,7 @@
     "external_dependencies": {
         "python": [
             "num2words",
-            "erpbrasil.base>=2.3.0",
+            "erpbrasil.base",
             "phonenumbers",
             "email-validator",
         ]
