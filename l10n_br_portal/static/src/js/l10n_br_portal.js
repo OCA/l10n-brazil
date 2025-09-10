7caef4a5c8a9bb23f47faee97476f959f6f2c1c5
Raphaël Valyi <rvalyi@akretion.com>
2025-09-10 02:45:00 +0000

    [MIG] l10n_br_portal: Migration to 17.0

--- l10n_br_portal/static/src/js/l10n_br_portal.js
+++ l10n_br_portal/static/src/js/l10n_br_portal.js
@@ -1,70 +1,87 @@
 /* eslint no-unused-vars: "off", no-undef: "off" */
 
-odoo.define("l10n_br_portal.l10n_br_portal", function (require) {
-    "use strict";
+odoo.define(
+    "l10n_br_portal.l10n_br_portal",
+    ["@web/legacy/js/public/public_widget", "@web/core/network/rpc"],
+    function (require) {
+        "use strict";
 
-    require("web.dom_ready");
-    var ajax = require("web.ajax");
+        var publicWidgetModule = require("@web/legacy/js/public/public_widget");
+        var publicWidget = publicWidgetModule.default || publicWidgetModule;
+        var rpc = require("@web/core/network/rpc").rpc;
 
-    if (!$(".o_portal").length) {
-        return $.Deferred().reject("DOM doesn't contain '.o_portal'");
-    }
+        publicWidget.registry.l10nBrPortalDetails = publicWidget.Widget.extend({
+            selector: ".o_portal_details",
+            events: {
+                "change select[name='state_id']": "_onChangeState",
+                "change input[name='zipcode']": "_onChangeZipcode",
+            },
 
-    if ($(".input-vat").length) {
-        new Cleave(".input-vat", {
-            blocks: [2, 3, 3, 4, 2],
-            delimiters: [".", ".", "-"],
-            numericOnly: true,
-            onValueChanged: function (e) {
-                if (e.target.rawValue.length > 11) {
-                    this.properties.blocks = [2, 3, 3, 4, 2];
-                    this.properties.delimiters = [".", ".", "/", "-"];
-                } else {
-                    this.properties.blocks = [3, 3, 3, 3];
-                    this.properties.delimiters = [".", ".", "-"];
+            start: function () {
+                if (this.$(".input-vat").length) {
+                    this.cleaveVat = new Cleave(this.$(".input-vat")[0], {
+                        blocks: [2, 3, 3, 4, 2],
+                        delimiters: [".", ".", "-"],
+                        numericOnly: true,
+                        onValueChanged: function (e) {
+                            if (e.target.rawValue.length > 11) {
+                                this.properties.blocks = [2, 3, 3, 4, 2];
+                                this.properties.delimiters = [".", ".", "/", "-"];
+                            } else {
+                                this.properties.blocks = [3, 3, 3, 3];
+                                this.properties.delimiters = [".", ".", "-"];
+                            }
+                        },
+                    });
                 }
+
+                if (this.$(".input-zipcode").length) {
+                    this.cleaveZipcode = new Cleave(this.$(".input-zipcode")[0], {
+                        blocks: [5, 3],
+                        delimiter: "-",
+                        numericOnly: true,
+                    });
+                }
+
+                this.stateOptions = this.$(
+                    "select[name='city_id']:enabled option:not(:first)"
+                );
+                this.$("select[name='state_id']").change();
+
+                return this._super.apply(this, arguments);
+            },
+
+            _onChangeState: function (e) {
+                if (!this.stateOptions) {
+                    return;
+                }
+                var select = this.$("select[name='city_id']");
+                this.stateOptions.detach();
+                var displayedState = this.stateOptions.filter(
+                    "[data-state_id=" + ($(e.currentTarget).val() || 0) + "]"
+                );
+                var nb = displayedState.appendTo(select).show().length;
+                select.parent().toggle(nb >= 1);
+            },
+
+            _onChangeZipcode: function (e) {
+                var vals = {zipcode: this.$('input[name="zipcode"]').val()};
+                console.log("Changing ZIP");
+
+                rpc("/l10n_br/zip_search", vals).then((data) => {
+                    if (data.error) {
+                        console.log("Falha ao consultar cep");
+                    } else {
+                        this.$('input[name="district"]').val(data.district);
+                        this.$('input[name="street_name"]').val(data.street_name);
+                        this.$('select[name="country_id"]')
+                            .val(data.country_id)
+                            .change();
+                        this.$('select[name="state_id"]').val(data.state_id).change();
+                        this.$('select[name="city_id"]').val(data.city_id);
+                    }
+                });
             },
         });
     }
-
-    if ($(".input-zipcode").length) {
-        // Apply ZIP code mask only when the field exists.
-        new Cleave(".input-zipcode", {
-            blocks: [5, 3],
-            delimiter: "-",
-            numericOnly: true,
-        });
-    }
-
-    if ($(".o_portal_details").length) {
-        var state_options = $("select[name='city_id']:enabled option:not(:first)");
-        $(".o_portal_details").on("change", "select[name='state_id']", function () {
-            var select = $("select[name='city_id']");
-            state_options.detach();
-            var displayed_state = state_options.filter(
-                "[data-state_id=" + ($(this).val() || 0) + "]"
-            );
-            var nb = displayed_state.appendTo(select).show().length;
-            select.parent().toggle(nb >= 1);
-        });
-        $(".o_portal_details").find("select[name='state_id']").change();
-        $(".o_portal_details").on("change", "input[name='zipcode']", function () {
-            var vals = {zipcode: $('input[name="zipcode"]').val()};
-            console.log("Changing ZIP");
-            ajax.jsonRpc("/l10n_br/zip_search", "call", vals).then(function (data) {
-                if (data.error) {
-                    // TODO: Retornar nos campos error e error_message
-                    console.log("Falha ao consultar cep");
-                } else {
-                    $('input[name="district"]').val(data.district);
-                    $('input[name="street_name"]').val(data.street_name);
-                    $('select[name="country_id"]').val(data.country_id);
-                    $('select[name="country_id"]').change();
-                    $('select[name="state_id"]').val(data.state_id);
-                    $('select[name="state_id"]').change();
-                    $('select[name="city_id"]').val(data.city_id);
-                }
-            });
-        });
-    }
-});
+);
