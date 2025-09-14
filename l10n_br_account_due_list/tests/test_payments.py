50f4fcbcf9226802a6099c46214278bcb3d430ef
Raphaël Valyi <rvalyi@akretion.com>
2025-09-14 14:22:45 +0000

    [IMP] l10n_br_account_due_list: compat with l10n_br (tax base_reduction required)

--- l10n_br_account_due_list/tests/test_payments.py
+++ l10n_br_account_due_list/tests/test_payments.py
@@ -2,59 +2,18 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields
-from odoo.tests import Form, common, tagged
+from odoo.tests import Form, tagged
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestPayments(common.TransactionCase):
+@tagged("post_install_l10n", "post_install", "-at_install")
+class TestPayments(AccountTestInvoicingCommon):
     @classmethod
+    @AccountTestInvoicingCommon.setup_country("us")
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.account_receivable = cls.env["account.account"].create(
-            {
-                "name": "Test receivable account",
-                "code": "ACCRV",
-                "account_type": "asset_receivable",
-                "reconcile": True,
-            }
-        )
-        cls.account_payable = cls.env["account.account"].create(
-            {
-                "name": "Test payable account",
-                "code": "ACCPAY",
-                "account_type": "liability_payable",
-                "reconcile": True,
-            }
-        )
-        cls.partner_1 = cls.env["res.partner"].create(
-            {
-                "name": "Mr. Odoo",
-                "property_account_receivable_id": cls.account_receivable.id,
-                "property_account_payable_id": cls.account_payable.id,
-            }
-        )
-        cls.tax1 = cls.env["account.tax"].create(
-            {
-                "name": "TAX 15%",
-                "amount_type": "percent",
-                "type_tax_use": "purchase",
-                "amount": 15.0,
-                "price_include": True,
-            }
-        )
-        cls.tax2 = cls.env["account.tax"].create(
-            {
-                "name": "TAX 15% (br)",
-                "amount_type": "percent",
-                "type_tax_use": "purchase",
-                "amount": 15.0,
-                "price_include": True,
-            }
-        )
-        cls.journal = cls.env["account.journal"].create(
-            {"name": "Test purchase journal", "code": "TPUR", "type": "purchase"}
-        )
-        cls.invoice_line = cls.env["account.move.line"]
 
     def test_01_receivable(self):
         invoice_form = Form(
@@ -62,20 +21,23 @@ class TestPayments(common.TransactionCase):
                 default_move_type="out_invoice",
             )
         )
-        invoice_form.partner_id = self.partner_1
+        invoice_form.partner_id = self.partner_a
         invoice_form.ref = "Test l10n_br_account_due_list"
         with invoice_form.invoice_line_ids.new() as line_form:
             line_form.name = "Line 1"
             line_form.price_unit = 200.0
             line_form.quantity = 1
             line_form.tax_ids.clear()
-            line_form.tax_ids.add(self.tax1)
+            line_form.tax_ids.add(self.tax_sale_a)
         invoice = invoice_form.save()
         invoice.action_post()
 
         self.assertEqual(len(invoice.due_line_ids), 1)
         self.assertEqual(invoice.due_line_ids[0].debit, 230)
-        self.assertEqual(invoice.due_line_ids[0].account_id, self.account_receivable)
+        self.assertEqual(
+            invoice.due_line_ids[0].account_id,
+            self.company_data["default_account_receivable"],
+        )
         self.assertEqual(len(invoice.payment_move_line_ids), 0)
 
         # register payment
@@ -91,7 +53,8 @@ class TestPayments(common.TransactionCase):
         self.assertEqual(len(invoice.payment_move_line_ids), 1)
         self.assertEqual(invoice.payment_move_line_ids[0].credit, 230)
         self.assertEqual(
-            invoice.payment_move_line_ids[0].account_id, self.account_receivable
+            invoice.payment_move_line_ids[0].account_id,
+            self.company_data["default_account_receivable"],
         )
 
     def test_02_payable(self):
@@ -100,7 +63,7 @@ class TestPayments(common.TransactionCase):
                 default_move_type="in_invoice",
             )
         )
-        invoice_form.partner_id = self.partner_1
+        invoice_form.partner_id = self.partner_a
         invoice_form.ref = "Test l10n_br_account_due_list"
         invoice_form.invoice_date = fields.Date.today()
         with invoice_form.invoice_line_ids.new() as line_form:
@@ -108,13 +71,16 @@ class TestPayments(common.TransactionCase):
             line_form.price_unit = 100.0
             line_form.quantity = 1
             line_form.tax_ids.clear()
-            line_form.tax_ids.add(self.tax2)
+            line_form.tax_ids.add(self.tax_purchase_a)
         invoice = invoice_form.save()
         invoice.action_post()
 
         self.assertEqual(len(invoice.due_line_ids), 1)
         self.assertEqual(invoice.due_line_ids[0].credit, 115)
-        self.assertEqual(invoice.due_line_ids[0].account_id, self.account_payable)
+        self.assertEqual(
+            invoice.due_line_ids[0].account_id,
+            self.company_data["default_account_payable"],
+        )
         self.assertEqual(len(invoice.payment_move_line_ids), 0)
 
         # register payment
@@ -130,5 +96,6 @@ class TestPayments(common.TransactionCase):
         self.assertEqual(len(invoice.payment_move_line_ids), 1)
         self.assertEqual(invoice.payment_move_line_ids[0].debit, 115)
         self.assertEqual(
-            invoice.payment_move_line_ids[0].account_id, self.account_payable
+            invoice.payment_move_line_ids[0].account_id,
+            self.company_data["default_account_payable"],
         )
