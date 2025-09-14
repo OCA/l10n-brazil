e566800e68d0a37fa28dd57778b80673952a63a0
Raphaël Valyi <rvalyi@akretion.com>
2025-09-14 13:17:20 +0000

    [ADD] l10n_br_base_l10n_br_compat

---
+++ l10n_br_base_l10n_br_compat/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2025 - TODAY Raphaël Valyi - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "l10n_br Compatibility",
+    "summary": "Compatibility with the l10n_br module",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["rvalyi"],
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": ["l10n_br_base", "l10n_br"],
+    "version": "18.0.1.0.0",
+    "data": [
+        "views/res_partner_views.xml",
+        "views/res_company_view.xml",
+    ],
+    "auto_install": True,
+}
