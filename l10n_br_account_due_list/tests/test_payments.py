4d6ae240849f184cfb33e9971274aa94164ea3f2
Raphaël Valyi <rvalyi@akretion.com>
2025-09-14 13:17:20 +0000

    [ADD] l10n_br_base_l10n_br_compat

--- l10n_br_account_due_list/tests/test_payments.py
+++ l10n_br_account_due_list/tests/test_payments.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields
-from odoo.tests import Form, common
+from odoo.tests import Form, common, tagged
 
 
 class TestPayments(common.TransactionCase):
