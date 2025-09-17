ff803e9ec2bfa88e353d33b627d7590db797e04f
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-09-17 09:57:48 -0300

    [FIX] l10n_br_portal: enable tests after fix chrome version

--- l10n_br_portal/tests/__init__.py
+++ l10n_br_portal/tests/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2016 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
-# from . import test_load_process
+from . import test_load_process
