960bed3fbf4d086944dc35bc1d11551a7b7753fd
Magno Costa <magno.costa@akretion.com.br>
2025-10-01 15:30:19 -0300

    [IMP] l10n_br_account_payment_brcobranca: Tests
    (cherry picked from commit 9b24744b0cc92cf8c8439a2b7408b1aa50d203bd)

--- l10n_br_account_payment_brcobranca/tests/common.py
+++ l10n_br_account_payment_brcobranca/tests/common.py
@@ -6,6 +6,7 @@ import base64
 import logging
 import os
 
+from odoo import Command
 from odoo.modules import get_resource_path
 from odoo.tests import tagged
 
@@ -137,18 +138,6 @@ class TestBRCobrancaCommon(CNABTestCommon):
             },
         )
 
-        cls.bank_account_ailos = create_with_form_res_partner_bank(
-            cls.env,
-            {
-                "acc_number": "374",
-                "acc_number_dig": "0",
-                "bra_number": "1236",
-                "bra_number_dig": "1",
-                "bank_id": cls.env.ref("l10n_br_base.res_bank_085"),
-                "partner_id": cls.env.company.partner_id,
-            },
-        )
-
         cls.bank_account_nordeste = create_with_form_res_partner_bank(
             cls.env,
             {
@@ -272,24 +261,6 @@ class TestBRCobrancaCommon(CNABTestCommon):
             },
         )
 
-        cls.cnab_seq_ailos = create_with_form_ir_sequence(
-            cls.env,
-            cls.common_sequence_values
-            | {
-                "name": "Sequencia Arquivo CNAB - AILOS 240",
-                "code": "Sequencia Arquivo CNAB - AILOS 240",
-            },
-        )
-
-        cls.own_number_seq_ailos = create_with_form_ir_sequence(
-            cls.env,
-            cls.common_sequence_values
-            | {
-                "name": "Nosso número AILOS",
-                "code": "nosso.numero",
-            },
-        )
-
         cls.cnab_seq_nordeste = create_with_form_ir_sequence(
             cls.env,
             cls.common_sequence_values
@@ -312,6 +283,7 @@ class TestBRCobrancaCommon(CNABTestCommon):
         cls.cnab_config_cef.cnab_processor = "brcobranca"
         cls.cnab_config_itau_400.cnab_processor = "brcobranca"
         cls.cnab_config_itau_240.cnab_processor = "brcobranca"
+        cls.cnab_config_ailos_240.cnab_processor = "brcobranca"
 
         cls.common_cnab_config_values.update({"cnab_processor": "brcobranca"})
 
@@ -366,9 +338,7 @@ class TestBRCobrancaCommon(CNABTestCommon):
         cls.cnab_config_unicred.write(
             {
                 "liq_return_move_code_ids": [
-                    (
-                        6,
-                        0,
+                    Command.set(
                         [
                             cls.env.ref(
                                 "l10n_br_account_payment_order.unicred_240_400_return_01"
@@ -530,55 +500,6 @@ class TestBRCobrancaCommon(CNABTestCommon):
                     "l10n_br_account_payment_order.santander_240_instruction_02"
                 ),
             },
-        )
-
-        cls.cnab_config_ailos = create_with_form_l10n_br_cnab_config(
-            cls.env,
-            cls.common_cnab_config_values
-            | {
-                "name": "Banco AILOS - CNAB 240 (inbound)",
-                "bank_id": cls.env.ref("l10n_br_base.res_bank_085"),
-                "payment_method_id": cls.pay_method_type_240,
-                "boleto_wallet": "01",
-                "boleto_variation": "35",
-                "boleto_discount_code_id": cls.env.ref(
-                    "l10n_br_account_payment_order.ailos_240_boleto_discount_code_1"
-                ),
-                "boleto_interest_code": "2",
-                "cnab_sequence_id": cls.cnab_seq_ailos,
-                "own_number_sequence_id": cls.own_number_seq_ailos,
-                "cnab_company_bank_code": "101004",
-                "sending_code_id": cls.env.ref(
-                    "l10n_br_account_payment_order.ailos_instruction_01"
-                ),
-                "write_off_code_id": cls.env.ref(
-                    "l10n_br_account_payment_order.ailos_instruction_02"
-                ),
-                "boleto_fee_code_id": cls.env.ref(
-                    "l10n_br_account_payment_order.febrabam_240_boleto_fee_code_2"
-                ),
-            },
-        )
-        cls.cnab_config_ailos.write(
-            {
-                "liq_return_move_code_ids": [
-                    (
-                        6,
-                        0,
-                        [
-                            cls.env.ref(
-                                "l10n_br_account_payment_order.ailos_240_return_06"
-                            ).id,
-                            cls.env.ref(
-                                "l10n_br_account_payment_order.ailos_240_return_76"
-                            ).id,
-                            cls.env.ref(
-                                "l10n_br_account_payment_order.ailos_240_return_77"
-                            ).id,
-                        ],
-                    )
-                ],
-            }
         )
 
         cls.cnab_config_nordeste = create_with_form_l10n_br_cnab_config(
@@ -759,25 +680,6 @@ class TestBRCobrancaCommon(CNABTestCommon):
             }
         )
 
-        cls.journal_ailos = create_with_form_account_journal(
-            cls.env,
-            {
-                "name": "Banco AILOS",
-                "type": "bank",
-                "code": "BNC16",
-                "bank_account_id": cls.bank_account_ailos,
-                "edi_format_ids": False,
-                "used_for_import": True,
-                "import_type": "cnab240",
-                "return_auto_reconcile": True,
-            },
-            [
-                {
-                    "name": "AILOS CNAB 240",
-                    "payment_method_id": cls.pay_method_type_240,
-                }
-            ],
-        )
         cls.journal_ailos.write(
             {
                 "used_for_import": True,
@@ -886,18 +788,6 @@ class TestBRCobrancaCommon(CNABTestCommon):
             },
         )
 
-        cls.pay_mode_ailos = create_with_form_account_payment_mode(
-            cls.env,
-            cls.common_pay_mode_values
-            | {
-                "name": "Cobrança AILOS 240",
-                "bank_id": cls.env.ref("l10n_br_base.res_bank_085"),
-                "fixed_journal_id": cls.journal_ailos,
-                "payment_method_id": cls.pay_method_type_240,
-                "cnab_config_id": cls.cnab_config_ailos,
-            },
-        )
-
         cls.pay_mode_nordeste = create_with_form_account_payment_mode(
             cls.env,
             cls.common_pay_mode_values
@@ -996,19 +886,6 @@ class TestBRCobrancaCommon(CNABTestCommon):
             },
             cls.inv_line_common_values,
         )
-
-        cls.invoice_ailos_240 = create_with_form_account_move(
-            cls.env,
-            cls.inv_common_values
-            | {
-                "name": "AILOS CNAB 240",
-                "payment_mode_id": cls.pay_mode_ailos,
-            },
-            cls.inv_line_common_values,
-        )
-        # Altera o valor total para 1000
-        for line in cls.invoice_ailos_240.invoice_line_ids:
-            line.tax_ids = False
 
         cls.invoice_nordeste_400 = create_with_form_account_move(
             cls.env,
