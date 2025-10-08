815879727cce60a802fbcacaec5cbf98cee285c9
Raphaël Valyi <rvalyi@akretion.com>
2025-10-08 00:29:40 +0000

    [FIX] l10n_br_account: fix aml edition via fiscal menu

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -195,7 +195,7 @@ class AccountMove(models.Model):
     def default_get(self, fields_list):
         defaults = super().default_get(fields_list)
         move_type = self.env.context.get("default_move_type", "out_invoice")
-        if move_type != "entry":
+        if move_type and move_type != "entry":
             defaults["fiscal_operation_type"] = MOVE_TO_OPERATION[move_type]
             if defaults["fiscal_operation_type"] == FISCAL_OUT:
                 defaults["issuer"] = DOCUMENT_ISSUER_COMPANY
