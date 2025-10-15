cd1324eb65d025137e1a10407afa23145a880c62
Raphaël Valyi <rvalyi@akretion.com>
2025-10-15 14:46:58 +0000

    [FIX] l10n_br_sale_commission: don't depend on cfop_id
    fixes https://github.com/OCA/l10n-brazil/issues/4154

--- l10n_br_sale_commission/models/account_move_line.py
+++ l10n_br_sale_commission/models/account_move_line.py
@@ -8,7 +8,7 @@ from odoo import api, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.depends("move_id.partner_id", "cfop_id")
+    @api.depends("move_id.partner_id", "product_id", "fiscal_operation_line_id")
     def _compute_agent_ids(self):
         res = super()._compute_agent_ids()
         for record in self.filtered(lambda ln: ln.cfop_id):
--- l10n_br_sale_commission/models/sale_order_line.py
+++ l10n_br_sale_commission/models/sale_order_line.py
@@ -8,7 +8,11 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.depends("order_id.partner_id", "cfop_id")
+    # NOTE: this compute cannot depend on cfop_id directly because
+    # _compute_fiscal_tax_ids would trigger it and it would somewho set
+    # _compute_tax_fields for recompute loosing possible custom tax values.
+    # See https://github.com/OCA/l10n-brazil/issues/4154
+    @api.depends("order_id.partner_id", "product_id", "fiscal_operation_line_id")
     def _compute_agent_ids(self):
         res = super()._compute_agent_ids()
         for record in self.filtered(lambda ln: ln.cfop_id):
