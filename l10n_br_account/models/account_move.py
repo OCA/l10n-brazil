1dde7ea56e36ed75c854e6bd6f77f4b27a6e9ce0
Antonio Neto <neto@engenere.one>
2025-11-03 15:35:14 -0300

    [REF] l10n_br_account: fiscal fields and computes

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -60,7 +60,6 @@ class AccountMove(models.Model):
         ondelete="cascade",
         store=True,
         readonly=False,
-        compute="_compute_fiscal_document_id",
     )
 
     fiscal_document_ids = fields.One2many(
@@ -78,68 +77,10 @@ class AccountMove(models.Model):
         compute="_compute_fiscal_operation_type",
     )
 
-    # -------------------------------------------------------------------------
-    # SHADOWED FIELDS SYNC
-    # These fields have the same name in account.move
-    # and l10n_br_fiscal.document. So they wouldn't get updated
-    # by the _inherits system. An alternative would be changing their name
-    # in l10n_br_fiscal but that would make the code unreadable and fiscal mixin
-    # methods would fail to do what we expect from them in the Odoo objects.
-    # -------------------------------------------------------------------------
-
-    user_id = fields.Many2one(inverse="_inverse_user_id")
-    partner_shipping_id = fields.Many2one(inverse="_inverse_partner_shipping_id")
-
-    @api.onchange("company_id")
-    def _inverse_company_id(self):
-        for move in self:
-            for doc in move.fiscal_document_ids:
-                doc.company_id = move.company_id
-        return super()._inverse_company_id()
-
-    @api.onchange("currency_id")
-    def _inverse_currency_id(self):
-        for move in self:
-            for doc in move.fiscal_document_ids:
-                doc.currency_id = move.currency_id
-        return super()._inverse_currency_id()
-
-    @api.onchange("partner_id")
-    def _inverse_partner_id(self):
-        for move in self:
-            for doc in move.fiscal_document_ids:
-                doc.partner_id = move.partner_id
-        return super()._inverse_partner_id()
-
     @api.onchange("user_id")
     def _inverse_user_id(self):
-        for move in self:
-            for doc in move.fiscal_document_ids:
-                doc.user_id = move.user_id
-
-    @api.onchange("partner_shipping_id")
-    def _inverse_partner_shipping_id(self):
-        for move in self:
-            for doc in move.fiscal_document_ids:
-                doc.partner_shipping_id = move.partner_shipping_id
-
-    @api.onchange("document_type_id")
-    def _inverse_document_type_id(self):
-        if (self.document_type_id and not self.fiscal_document_id) or (
-            not self.document_type_id and self.fiscal_document_id
-        ):
-            self.env.add_to_compute(self._fields["fiscal_document_id"], self)
-
-    def _compute_fiscal_document_id(self):
-        for move in self:
-            if move.document_type_id and not move.fiscal_document_id:
-                move.fiscal_document_id = (
-                    self.env["l10n_br_fiscal.document"].create({}).id
-                )
-            elif not move.document_type_id and move.fiscal_document_id:
-                bad_fiscal_doc = move.fiscal_document_id
-                move.fiscal_document_id = False
-                bad_fiscal_doc.action_document_cancel()
+        for line in self:
+            line.proxy_user_id = line.user_id
 
     @api.constrains("fiscal_document_id", "document_type_id")
     def _check_fiscal_document_type(self):
@@ -152,7 +93,7 @@ class AccountMove(models.Model):
                     )
                 )
 
-    @api.depends("line_ids", "invoice_line_ids", "fiscal_document_id")
+    @api.depends("line_ids", "fiscal_document_id")
     def _compute_fiscal_document_ids(self):
         for move in self:
             docs = move.fiscal_document_id
@@ -207,7 +148,7 @@ class AccountMove(models.Model):
         if self.env.company.country_id.code != "BR" or view_type != "form":
             return arch, view
         if view_type == "form" and self.env.company.country_id.code == "BR":
-            arch = self.env["account.move.line"].inject_fiscal_fields(arch)
+            arch = self.env["l10n_br_fiscal.document.line"].inject_fiscal_fields(arch)
 
         for tax_totals_node in arch.xpath(
             "//field[@name='tax_totals'][@widget='account-tax-totals-field']"
@@ -238,8 +179,11 @@ class AccountMove(models.Model):
         "line_ids.payment_id.state",
         "line_ids.full_reconcile_id",
         "state",
-        "ind_final",
-        "line_ids.cfop_id",
+        "direction_sign",
+        "fiscal_operation_id",
+        "fiscal_line_ids.cfop_id",
+        "fiscal_line_ids.fiscal_amount_untaxed",
+        "fiscal_line_ids.fiscal_amount_tax",
     )
     def _compute_amount(self):
         if "force_fiscal_amount_recompute" in self._context:
@@ -586,7 +530,6 @@ class AccountMove(models.Model):
                     force_fiscal_operation_id
                     or line.fiscal_operation_id.return_fiscal_operation_id
                 )
-                line._onchange_fiscal_operation_id()
 
             # This method is in l10n_br_fiscal_subsequent_document module, the IF
             # is necessary to avoid a 'glue module' or direct dependence.
--- l10n_br_account/models/document_line.py
+++ l10n_br_account/models/document_line.py
@@ -15,17 +15,78 @@ class FiscalDocumentLine(models.Model):
         string="Invoice Lines",
     )
 
+    move_id = fields.Many2one(
+        comodel_name="account.move",
+        related="account_line_ids.move_id",
+        store=True,
+        precompute=True,
+        string="Invoice",
+    )
+
+    document_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document",
+        string="Fiscal Document",
+        compute="_compute_document_id",
+        store=True,
+        readonly=False,
+        precompute=True,
+        index=True,
+        check_company=True,
+        ondelete="cascade",
+    )
+
+    # -------------------------------------------------------------------------
+    # PROXY FIELDS FOR _inherits SHADOWED NAMES
+    # -------------------------------------------------------------------------
+    # When using _inherits (delegation), fields with identical names on both the
+    # child and delegated model may not synchronize correctly. To avoid ORM sync
+    # issues, we define proxy_* fields related to the delegated document fields.
+    # Then the child "original" fields point to the proxies, ensuring consistency
+    # and editability.
+
+    proxy_company_id = fields.Many2one(
+        related="document_id.company_id",
+        comodel_name="res.company",
+        string="Company (proxy)",
+        help="Technical Field.",
+        readonly=False,
+        store=True,
+        precompute=True,
+    )
+    proxy_partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Partner (proxy)",
+        help="Technical Field.",
+    )
+    proxy_product_id = fields.Many2one(
+        comodel_name="product.product",
+        string="Product (proxy)",
+    )
+    proxy_name = fields.Char(
+        string="Name (proxy)",
+        help="Technical Field.",
+    )
+    proxy_quantity = fields.Float(
+        string="Quantity (proxy)",
+        help="Technical Field.",
+    )
+    proxy_price_unit = fields.Float(
+        string="Unit Price (proxy)",
+        help="Technical mirror.",
+    )
+
     # -------------------------------------------------------------------------
     # SHADOWED FIELDS SYNC
     # -------------------------------------------------------------------------
 
-    proxy_product_id = fields.Many2one(
-        comodel_name="product.product",
-        string="Product (proxy)",
-        help="Technical Field.",
+    company_id = fields.Many2one(
+        related="proxy_company_id",
+        comodel_name="res.company",
+        string="Company",
+        store=True,
         readonly=False,
+        precompute=True,
     )
-
     product_id = fields.Many2one(
         related="proxy_product_id",
         comodel_name="product.product",
@@ -34,11 +95,60 @@ class FiscalDocumentLine(models.Model):
         precompute=True,
         readonly=False,
     )
-
-    name = fields.Char(inverse="_inverse_name")
-    quantity = fields.Float(inverse="_inverse_quantity")
-    price_unit = fields.Float(inverse="_inverse_price_unit")
     uom_id = fields.Many2one(inverse="_inverse_uom_id")
+    name = fields.Char(
+        compute="_compute_name",
+        store=True,
+        precompute=True,
+        readonly=False,
+    )
+    quantity = fields.Float(
+        related="proxy_quantity",
+        string="Quantity",
+        store=True,
+        precompute=True,
+        readonly=False,
+    )
+
+    @api.depends("product_id", "proxy_name")
+    def _compute_name(self):
+        for line in self:
+            line.name = line.proxy_name or line.product_id.display_name or False
+
+    # Do not depend on `document_id.partner_id`, the inverse is taking care of that
+    @api.depends("proxy_partner_id")
+    def _compute_partner_id(self):
+        for line in self:
+            line.partner_id = line.proxy_partner_id or line.document_id.partner_id
+
+    @api.depends("product_id", "fiscal_operation_id", "proxy_price_unit")
+    def _compute_price_unit_fiscal(self):  # pylint: disable=missing-return
+        lines_from_account = self._records_from_account()
+        lines_from_fiscal = self - lines_from_account
+        if lines_from_fiscal:
+            super(FiscalDocumentLine, lines_from_fiscal)._compute_price_unit_fiscal()
+        for line in lines_from_account:
+            line.price_unit = line.proxy_price_unit
+
+    def _records_from_account(self):
+        account_flag = self._context.get("create_from_account")
+        return self.filtered(lambda r: r.account_line_ids or account_flag)
+
+    @api.depends("move_id.fiscal_document_id")
+    def _compute_document_id(self):
+        """
+        Ensures that the `document_id` field is updated even when the document line is
+        a new record (NewId) and has not yet been saved.
+        """
+        for line in self:
+            is_draft = line.id != line._origin.id
+            if (
+                is_draft
+                and line.move_id
+                and line.move_id.fiscal_document_id
+                and not line.document_id
+            ):
+                line.document_id = line.move_id.fiscal_document_id
 
     @api.onchange("product_id")
     def _inverse_product_id(self):
@@ -87,15 +197,16 @@ class FiscalDocumentLine(models.Model):
                 if aml.product_uom_id != line.uom_id:
                     aml.product_uom_id = line.uom_id
 
-    @api.depends("product_id", "uom_id", "account_line_ids.uot_id")
-    def _compute_uot_id(self):
-        """
-        Ensure doc line uot_id is consistent with custom aml uot if any.
-        """
-        line_with_aml_uot = self.filtered(lambda line: line.account_line_ids.uot_id)
-        for line in line_with_aml_uot:
-            line.uot_id = line.account_line_ids.uot_id
-        return super(FiscalDocumentLine, self - line_with_aml_uot)._compute_uot_id()
+    @api.model
+    def _sync_shadow_fields(self, vals):
+        if "quantity" not in vals and "proxy_quantity" in vals:
+            vals["quantity"] = vals["proxy_quantity"]
+        if "price_unit" not in vals and "proxy_price_unit" in vals:
+            vals["price_unit"] = vals["proxy_price_unit"]
+        if "name" not in vals and "proxy_name" in vals:
+            vals["name"] = vals["proxy_name"]
+        if "product_id" not in vals and "proxy_product_id" in vals:
+            vals["product_id"] = vals["proxy_product_id"]
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -108,13 +219,19 @@ class FiscalDocumentLine(models.Model):
         account.move.line records with NULL values for fiscal_document_line_id where
         necessary.
         """
+        for vals in vals_list:
+            self._sync_shadow_fields(vals)
 
         if self._context.get("create_from_account"):
             # Filter out the dictionaries that do not meet the conditions
             filtered_vals_list = [
                 vals
                 for vals in vals_list
-                if vals.get("document_id") and vals.get("fiscal_operation_line_id")
+                if vals.get("document_id")
+                and (
+                    vals.get("fiscal_operation_id")
+                    or vals.get("fiscal_operation_line_id")
+                )
             ]
             # Stop execution and return empty if no dictionary meets the conditions
             if not filtered_vals_list:
