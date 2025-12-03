8c1c42eef6ed40864d46be452d7a6d47361134ba
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-12-03 11:35:55 -0300

    [FIX] l10n_br_nfse_barueri: fixes

--- l10n_br_nfse_barueri/constants/barueri.py
+++ l10n_br_nfse_barueri/constants/barueri.py
@@ -2,4 +2,4 @@ ENVIO_LOTE_RPS = ["NFeLoteEnviarArquivo"]
 
 CONSULTAR_SITUACAO_LOTE_RPS = ["NFeLoteStatusArquivo"]
 
-CONSULTAR_NFSE_POR_RPS = ["NFeLoteListarArquivos"]
+CONSULTAR_NFSE_POR_RPS = ["NFeLoteBaixarArquivo"]
