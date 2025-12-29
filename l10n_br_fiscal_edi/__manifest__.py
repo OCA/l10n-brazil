c469098ca615c7f069da0504855b3f8c4fb86137
Raphaël Valyi <rvalyi@akretion.com>
2025-12-29 02:21:19 +0000

    [REF] l10n_br_fiscal_edi: mv doc import wiz to l10n_br_fiscal_edi

--- l10n_br_fiscal_edi/__manifest__.py
+++ l10n_br_fiscal_edi/__manifest__.py
@@ -28,6 +28,7 @@
         "wizards/document_correction_wizard.xml",
         "wizards/document_status_wizard.xml",
         "wizards/invalidate_number_wizard.xml",
+        "wizards/document_import_wizard.xml",
         # Actions
         "views/l10n_br_fiscal_action.xml",
         # Menus
