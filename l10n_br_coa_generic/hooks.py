fde0ced784b50732ba35f13a6b6a55bc0d7a05ed
Diego Paradeda <diego.paradeda@kmee.com.br>
2026-01-27 15:07:13 -0300

    [MIG] l10n_br_coa_generic: Migration to 17.0
    [WIP] remove tax.group population from COA

--- l10n_br_coa_generic/hooks.py
+++ l10n_br_coa_generic/hooks.py
@@ -1,21 +1,19 @@
 # Copyright (C) 2020 - Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID, api, tools
+from odoo import tools
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    coa_generic_tmpl = env.ref("l10n_br_coa_generic.l10n_br_coa_generic_template")
-
-    # Load COA for demo Company
+def _post_init_hook(env):
+    # Load COA for LP Company
     company_lc = env.ref(
         "l10n_br_base.empresa_lucro_presumido", raise_if_not_found=False
     )
     if company_lc:
-        coa_generic_tmpl.try_loading(company=company_lc)
+        chart_template = env["account.chart.template"]
+        chart_template.try_loading("br_oca_generic", company_lc, install_demo=True)
         tools.convert_file(
-            cr,
+            env,
             "l10n_br_coa_generic",
             "demo/account_journal.xml",
             None,
