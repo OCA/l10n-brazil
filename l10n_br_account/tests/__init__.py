973e090f640b04167ae3b7cfc1870b5e2121ebe2
Raphaël Valyi <rvalyi@akretion.com>
2026-03-25 01:52:49 +0000

    [REF] l10n_br_account: test_tax_computation

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -1,8 +1,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import test_tax_computation
 from . import test_account_move_sn
 from . import test_account_move_lc
-from . import test_account_taxes
 from . import test_move_edition
 from . import test_move_document_discount
 from . import test_invoice_refund
