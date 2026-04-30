5e42ff46bef669a920a9977da7585b33068cfd58
Diego Paradeda <diego.paradeda@kmee.com.br>
2026-04-30 11:10:23 -0300

    [MIG] l10n_br_coa_simple: Migration to 17.0

--- l10n_br_coa_simple/hooks.py
+++ l10n_br_coa_simple/hooks.py
@@ -1,23 +1,19 @@
 # Copyright (C) 2020 - Gabriel Cardoso de Faria <gabriel.cardoso@kmee.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID, api, tools
+from odoo import tools
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-
-    # Load COA for demo Company
+def post_init_hook(env):
+    # Load COA for SN Company
     company_sn = env.ref(
         "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
     )
     if company_sn:
-        coa_simple_tmpl = env.ref(
-            "l10n_br_coa_simple.l10n_br_coa_simple_chart_template"
-        )
-        coa_simple_tmpl.try_loading(company=company_sn)
+        chart_template = env["account.chart.template"]
+        chart_template.try_loading("br_oca_simple", company_sn, install_demo=True)
         tools.convert_file(
-            cr,
+            env,
             "l10n_br_coa_simple",
             "demo/account_journal.xml",
             None,
--- l10n_br_coa_simple/tests/test_l10n_br_coa_simple.py
+++ l10n_br_coa_simple/tests/test_l10n_br_coa_simple.py
@@ -8,16 +8,21 @@ class L10nBrSimpleCOA(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.l10n_br_coa_simple = cls.env.ref(
-            "l10n_br_coa_simple.l10n_br_coa_simple_chart_template"
-        )
+
         cls.l10n_br_company = cls.env["res.company"].create(
             {"name": "Empresa Teste do Plano de Contas Simplificado"}
         )
 
     def test_l10n_br_coa_simple(self):
         """Test installing the chart of accounts template in a new company"""
-        self.l10n_br_coa_simple.try_loading(company=self.l10n_br_company)
-        self.assertEqual(
-            self.l10n_br_coa_simple, self.l10n_br_company.chart_template_id
+
+        chart_template = self.env["account.chart.template"]
+        chart_template.try_loading(
+            "br_oca_simple", self.l10n_br_company, install_demo=True
         )
+
+        # Verify the chart template was loaded
+        self.assertEqual(self.l10n_br_company.chart_template, "br_oca_simple")
+
+        # Verify anglo saxon accounting is enabled
+        self.assertTrue(self.l10n_br_company.anglo_saxon_accounting)
