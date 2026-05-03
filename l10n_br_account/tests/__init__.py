f494806b90ec8c6b00702ea035176d8f0e66b4ba
Raphaël Valyi <rvalyi@akretion.com>
2026-05-03 03:21:28 +0000

    [REF] l10n_br_account: mv tests to move_workflow

--- l10n_br_account/tests/__init__.py
+++ l10n_br_account/tests/__init__.py
@@ -8,3 +8,4 @@ from . import test_move_document_discount
 from . import test_invoice_refund
 from . import test_multi_localizations_invoice
 from . import test_payment_status
+from . import test_move_workflow
