akretion/l10n_br_core

[REF] l10n_br_sale: Inform Journal in Fiscal OP

l10n_br_sale/hooks.py
@@ -1,13 +1,14 @@
 # Copyright (C) 2020  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID, api, tools
+from odoo import SUPERUSER_ID, api
+
+from odoo.addons.l10n_br_fiscal.tools import set_journal_in_fiscal_operation
 
 
 def post_init_hook(cr, registry):
-    cr.execute("select demo from ir_module_module where name='l10n_br_sale';")
-    if cr.fetchone()[0]:
-        env = api.Environment(cr, SUPERUSER_ID, {})
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    if env.ref("base.module_base").demo:
         sale_orders = env["sale.order"].search(
             [("company_id", "!=", env.ref("base.main_company").id)]
         )
@@ -23,6 +24,14 @@ def post_init_hook(cr, registry):
             order.write(defaults)
 
         # Load COA Fiscal Operation properties
+        sale_set_journal_in_fiscal_operation(cr)
+
+
+def sale_set_journal_in_fiscal_operation(cr):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    if env.ref("base.module_base").demo:
+        # Create or Inform Fiscal Operation in Journal for Property of Companies
+        # Load COA Fiscal Operation properties
         company = env.ref(
             "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
         )
@@ -33,14 +42,51 @@ def post_init_hook(cr, registry):
                 ("state", "=", "installed"),
             ]
         ):
-            tools.convert_file(
+            # Load Fiscal Operation Main Company
+            set_journal_in_fiscal_operation(
                 cr,
-                "l10n_br_sale",
-                "demo/fiscal_operation_simple.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
+                env.ref("base.main_company"),
+                [
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_venda",
+                        "journal": "l10n_br_coa_simple.sale_journal_main_company",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_bonificacao",
+                        "journal": "l10n_br_coa_simple.general_journal_main_company",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_devolucao_venda",
+                        "journal": "l10n_br_coa_simple.general_journal_main_company",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_simples_remessa",
+                        "journal": "l10n_br_coa_simple.general_journal_main_company",
+                    },
+                ],
+            )
+
+            set_journal_in_fiscal_operation(
+                cr,
+                company,
+                [
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_venda",
+                        "journal": "l10n_br_coa_simple.sale_journal_empresa_sn",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_bonificacao",
+                        "journal": "l10n_br_coa_simple.general_journal_empresa_sn",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_devolucao_venda",
+                        "journal": "l10n_br_coa_simple.general_journal_empresa_sn",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_simples_remessa",
+                        "journal": "l10n_br_coa_simple.general_journal_empresa_sn",
+                    },
+                ],
             )
 
         company_lc = env.ref(
@@ -54,12 +100,25 @@ def post_init_hook(cr, registry):
                 ("state", "=", "installed"),
             ]
         ):
-            tools.convert_file(
+            set_journal_in_fiscal_operation(
                 cr,
-                "l10n_br_sale",
-                "demo/fiscal_operation_generic.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
+                company_lc,
+                [
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_venda",
+                        "journal": "l10n_br_coa_generic.sale_journal_empresa_lp",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_bonificacao",
+                        "journal": "l10n_br_coa_generic.general_journal_empresa_lp",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_devolucao_venda",
+                        "journal": "l10n_br_coa_generic.general_journal_empresa_lp",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_simples_remessa",
+                        "journal": "l10n_br_coa_generic.general_journal_empresa_lp",
+                    },
+                ],
             )

OCA/l10n-brazil

l10n_br_sale/hooks.py
@@ -1,13 +1,14 @@
 # Copyright (C) 2020  Renato Lima - Akretion <renato.lima@akretion.com.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import SUPERUSER_ID, api, tools
+from odoo import SUPERUSER_ID, api
+
+from odoo.addons.l10n_br_fiscal.tools import set_journal_in_fiscal_operation
 
 
 def post_init_hook(cr, registry):
-    cr.execute("select demo from ir_module_module where name='l10n_br_sale';")
-    if cr.fetchone()[0]:
-        env = api.Environment(cr, SUPERUSER_ID, {})
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    if env.ref("base.module_l10n_br_sale").demo:
         sale_orders = env["sale.order"].search(
             [("company_id", "!=", env.ref("base.main_company").id)]
         )
@@ -23,6 +24,14 @@ def post_init_hook(cr, registry):
             order.write(defaults)
 
         # Load COA Fiscal Operation properties
+        sale_set_journal_in_fiscal_operation(cr)
+
+
+def sale_set_journal_in_fiscal_operation(cr):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    if env.ref("base.module_l10n_br_sale").demo:
+        # Create or Inform Fiscal Operation in Journal for Property of Companies
+        # Load COA Fiscal Operation properties
         company = env.ref(
             "l10n_br_base.empresa_simples_nacional", raise_if_not_found=False
         )
@@ -33,14 +42,51 @@ def post_init_hook(cr, registry):
                 ("state", "=", "installed"),
             ]
         ):
-            tools.convert_file(
+            # Load Fiscal Operation Main Company
+            set_journal_in_fiscal_operation(
                 cr,
-                "l10n_br_sale",
-                "demo/fiscal_operation_simple.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
+                env.ref("base.main_company"),
+                [
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_venda",
+                        "journal": "l10n_br_coa_simple.sale_journal_main_company",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_bonificacao",
+                        "journal": "l10n_br_coa_simple.general_journal_main_company",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_devolucao_venda",
+                        "journal": "l10n_br_coa_simple.general_journal_main_company",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_simples_remessa",
+                        "journal": "l10n_br_coa_simple.general_journal_main_company",
+                    },
+                ],
+            )
+
+            set_journal_in_fiscal_operation(
+                cr,
+                company,
+                [
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_venda",
+                        "journal": "l10n_br_coa_simple.sale_journal_empresa_sn",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_bonificacao",
+                        "journal": "l10n_br_coa_simple.general_journal_empresa_sn",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_devolucao_venda",
+                        "journal": "l10n_br_coa_simple.general_journal_empresa_sn",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_simples_remessa",
+                        "journal": "l10n_br_coa_simple.general_journal_empresa_sn",
+                    },
+                ],
             )
 
         company_lc = env.ref(
@@ -54,12 +100,25 @@ def post_init_hook(cr, registry):
                 ("state", "=", "installed"),
             ]
         ):
-            tools.convert_file(
+            set_journal_in_fiscal_operation(
                 cr,
-                "l10n_br_sale",
-                "demo/fiscal_operation_generic.xml",
-                None,
-                mode="init",
-                noupdate=True,
-                kind="init",
+                company_lc,
+                [
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_venda",
+                        "journal": "l10n_br_coa_generic.sale_journal_empresa_lp",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_bonificacao",
+                        "journal": "l10n_br_coa_generic.general_journal_empresa_lp",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_devolucao_venda",
+                        "journal": "l10n_br_coa_generic.general_journal_empresa_lp",
+                    },
+                    {
+                        "fiscal_operation": "l10n_br_fiscal.fo_simples_remessa",
+                        "journal": "l10n_br_coa_generic.general_journal_empresa_lp",
+                    },
+                ],
             )