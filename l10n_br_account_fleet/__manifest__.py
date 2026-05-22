63189a8eb7ebbd488881b14eecaa98189c8ceedf
Cristiano Mafra Junior <cristiano.mafracontanto@gmail.com>
2026-05-22 20:45:49 -0300

    [MIG] l10n_br_account_fleet: Migration to 17.0

--- l10n_br_account_fleet/__manifest__.py
+++ l10n_br_account_fleet/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Brazilian Localization Account Fleet",
     "summary": """
         Brazilian Localization Account Fleet""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
     "maintainers": ["marcelsavegnago"],
