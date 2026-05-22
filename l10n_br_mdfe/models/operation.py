a73629434e94cda55c7d8f461e654430ca0fe998
rodrigo <rodrigomadu85@gmail.com>
2026-05-22 15:01:27 -0300

    [FIX]Fixed module l10n_br_mdfe with improvements.

--- l10n_br_mdfe/models/operation.py
+++ l10n_br_mdfe/models/operation.py
@@ -1,7 +1,7 @@
 # Copyright 2024 KMEE
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class MDFeOperation(models.Model):
