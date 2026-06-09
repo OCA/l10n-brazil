5cac5a5fbec7a49b296cfe885e362cb3290eeefd
Ygor Carvalho <ygcarvalh@gmail.com>
2026-06-09 14:30:33 -0300

    [ADD] l10n_br_nfse_nacional: test ADN lifecycle

--- l10n_br_nfse_nacional/tests/test_nfse_structure.py
+++ l10n_br_nfse_nacional/tests/test_nfse_structure.py
@@ -1,8 +1,11 @@
 from odoo.tests import TransactionCase
 
+
 class NfseStructure(TransactionCase):
     def test_inherited_fields(self):
         # Checks if fields were properly mapped into concrete models
         self.assertIn("nfse10_CNPJ", self.env["res.company"]._fields.keys())
         self.assertIn("nfse10_opSimpNac", self.env["res.company"]._fields.keys())
-        self.assertIn("nfse10_cServ", self.env["l10n_br_fiscal.document.line"]._fields.keys())
+        self.assertIn(
+            "nfse10_cServ", self.env["l10n_br_fiscal.document.line"]._fields.keys()
+        )
