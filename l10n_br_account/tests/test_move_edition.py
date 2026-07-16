bd89021e633d37eed73339915b5a8659080fc22b
Renato Lima <renatonlima@gmail.com>
2026-07-15 23:20:41 -0300

    [IMP] l10n_br_account: ibs/cbs fiscal tax test
    (cherry picked from commit f5603ceb87c579b424139adab265fb4a55630769)
    (cherry picked from commit f017b22766c7166351c79e0cb72e8c3a2caed9c1)

--- l10n_br_account/tests/test_move_edition.py
+++ l10n_br_account/tests/test_move_edition.py
@@ -252,7 +252,7 @@ class TestMoveEdition(TransactionCase):
             line_form.price_unit = 42
             line_form.quantity = 5
 
-            self.assertEqual(len(line_form.fiscal_tax_ids), 4)
+            self.assertEqual(len(line_form.fiscal_tax_ids), 6)
             self.assertEqual(
                 line_form.icms_tax_id, self.env.ref("l10n_br_fiscal.tax_icms_7")
             )
@@ -267,7 +267,7 @@ class TestMoveEdition(TransactionCase):
             line_form.fiscal_operation_line_id = self.env.ref(
                 "l10n_br_fiscal.fo_venda_venda"
             )
-            self.assertEqual(len(line_form.fiscal_tax_ids), 4)
+            self.assertEqual(len(line_form.fiscal_tax_ids), 6)
             self.assertEqual(
                 line_form.icms_tax_id, self.env.ref("l10n_br_fiscal.tax_icms_7")
             )
