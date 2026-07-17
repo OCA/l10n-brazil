c4c43d2b49e785b333dc208b3e4ae919f2331e43
Raphaël Valyi <rvalyi@akretion.com>
2026-07-17 01:24:37 -0300

    [REF] l10n_br_crm: migration script for legacy cnpj/cpf fields

--- l10n_br_crm/__manifest__.py
+++ l10n_br_crm/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Akretion, Odoo Community Association (OCA)",
     "maintainers": ["renatonlima", "rvalyi", "mbcosta"],
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "19.0.1.2.1",
+    "version": "19.0.2.0.0",
     "depends": ["l10n_br_base", "crm"],
     "data": ["views/crm_lead_view.xml", "views/crm_quick_create_opportunity_form.xml"],
     "installable": True,
