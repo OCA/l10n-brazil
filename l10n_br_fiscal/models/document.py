2b737d47b8023abb9a1f369243d2dea09e7b0a41
Raphaël Valyi <rvalyi@akretion.com>
2026-08-04 11:46:24 -0300

    [FIX] l10n_br_fiscal: restore send and consult receipt buttons
    The 'Enviar' and 'Consultar Recibo' buttons were removed during the
    FSM refactor, leaving documents stuck in 'enviada' with no UI path
    to consult the async receipt or re-send. Also restore the
    action_document_send placeholder in the base module.
    
    (cherry picked from commit b3f6ad4d2e5658bc2c471b067c3d3373a5cfce7a)

--- l10n_br_fiscal/models/document.py
+++ l10n_br_fiscal/models/document.py
@@ -490,6 +490,13 @@ class Document(models.Model):
         """Cancel the fiscal document, changing its state to 'cancel'"""
         self.write({"state_edoc": DOCUMENT_STATE_CANCEL})
 
+    def action_document_send(self):
+        """Placeholder to be overridden by l10n_br_fiscal_edi. Defined here
+        so that l10n_br_account (which does not depend on l10n_br_fiscal_edi)
+        can call it without crashing.
+        """
+        pass
+
     def action_document_invalidate(self):
         """Placeholder to be overridden by l10n_br_fiscal_edi. Defined here
         so that l10n_br_account (which does not depend on l10n_br_fiscal_edi)
