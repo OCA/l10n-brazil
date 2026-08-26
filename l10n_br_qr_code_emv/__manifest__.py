9c93777bf806a6884362344ed63759a7213d605f
Ygor Carvalho <ygcarvalh@gmail.com>
2026-08-26 18:03:19 -0300

    [ADD] l10n_br_qr_code_emv: Pix BR Code on invoices

---
+++ l10n_br_qr_code_emv/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2026 - KMEE
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Brazilian Localization QR Code EMV (Pix)",
+    "summary": "Pix BR Code on invoices, over the EMV merchant-presented QR standard.",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "development_status": "Beta",
+    "maintainers": ["ygcarvalh"],
+    "version": "18.0.1.0.0",
+    "depends": ["account_qr_code_emv", "l10n_br_base"],
+    "installable": True,
+}
