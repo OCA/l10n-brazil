dac32396ac37cb65838d428650379dbb5763a475
Raphaël Valyi <rvalyi@akretion.com>
2026-08-31 18:20:14 -0300

    [IMP] l10n_br_mdfe: drop per-comodel import search keys override files
    The search keys are now declared on the schema mixin (spec.mixin.mdfe in
    l10n_br_mdfe_spec). Deletes res_city.py and res_country_state.py.

--- l10n_br_mdfe/models/res_city.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2025 Akretion (Raphaël Valyi <raphael.valyi@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import models
-
-
-class ResCity(models.Model):
-    _inherit = "res.city"
-    _mdfe_search_keys = ["ibge_code"]
