fcab0a4fc586df242feaa2ffd14364be40d56c90
espadas <henriqueep@hotmail.com>
2026-09-23 10:18:00 -0300

    [FIX] l10n_br_website_sale: migrate l10n_br_address.js

--- l10n_br_website_sale/static/src/js/l10n_br_address.js
+++ l10n_br_website_sale/static/src/js/l10n_br_address.js
@@ -1,4 +1,4 @@
-/* eslint no-unused-vars: "off", no-undef: "off" */
+/* global Cleave */
 
 odoo.define("l10n_br_website_sale.l10n_br_address", function (require) {
     "use strict";
@@ -8,50 +8,46 @@ odoo.define("l10n_br_website_sale.l10n_br_address", function (require) {
 
     var $checkout_autoformat_selector = $(".checkout_autoformat");
 
-    if (!$checkout_autoformat_selector.length) {
-        return $.Deferred().reject("DOM doesn't contain '.checkout_autoformat'");
-    }
-
-    function formatCpfCnpj(inputValue) {
-        // Remove non-numeric characters
-        let value = inputValue.replace(/\D/g, "");
-
-        // Truncate the value to 11 digits if it's more than 11 and less than 14
-        if (value.length > 11 && value.length < 14) {
-            value = value.substring(0, 11);
-        }
-        // Truncate the value to 14 digits if it's 14 or more
-        else if (value.length >= 14) {
-            value = value.substring(0, 14);
-        }
-
-        if (value.length <= 11) {
-            // Format as CPF
-            value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-        } else {
-            // Format as CNPJ
-            value = value.replace(
-                /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
-                "$1.$2.$3/$4-$5"
-            );
-        }
-
-        return value;
-    }
-
-    $("#input_cnpj_cpf").on("blur", function () {
-        var value = $(this).val();
-        var formattedValue = formatCpfCnpj(value);
-        $(this).val(formattedValue);
-    });
-
-    var zip_cleave = new Cleave(".input-zipcode", {
-        blocks: [5, 3],
-        delimiter: "-",
-        numericOnly: true,
-    });
-
     if ($checkout_autoformat_selector.length) {
+        function formatCpfCnpj(inputValue) {
+            // Remove non-numeric characters
+            let value = inputValue.replace(/\D/g, "");
+
+            // Truncate the value to 11 digits if it's more than 11 and less than 14
+            if (value.length > 11 && value.length < 14) {
+                value = value.substring(0, 11);
+            }
+            // Truncate the value to 14 digits if it's 14 or more
+            else if (value.length >= 14) {
+                value = value.substring(0, 14);
+            }
+
+            if (value.length <= 11) {
+                // Format as CPF
+                value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+            } else {
+                // Format as CNPJ
+                value = value.replace(
+                    /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+                    "$1.$2.$3/$4-$5"
+                );
+            }
+
+            return value;
+        }
+
+        $("#input_cnpj_cpf").on("blur", function () {
+            var value = $(this).val();
+            var formattedValue = formatCpfCnpj(value);
+            $(this).val(formattedValue);
+        });
+
+        new Cleave(".input-zipcode", {
+            blocks: [5, 3],
+            delimiter: "-",
+            numericOnly: true,
+        });
+
         $checkout_autoformat_selector.on(
             "change",
             "select[name='country_id']",
@@ -75,6 +71,7 @@ odoo.define("l10n_br_website_sale.l10n_br_address", function (require) {
                 });
             }
         );
+
         $checkout_autoformat_selector.on(
             "change",
             "select[name='state_id']",
@@ -103,6 +100,7 @@ odoo.define("l10n_br_website_sale.l10n_br_address", function (require) {
                 });
             }
         );
+
         $checkout_autoformat_selector.on("change", "input[name='zip']", function () {
             var vals = {zipcode: $('input[name="zip"]').val()};
             console.log("Changing ZIP code");
@@ -115,15 +113,19 @@ odoo.define("l10n_br_website_sale.l10n_br_address", function (require) {
                 } else {
                     var state_id_selector = $('select[name="state_id"]');
                     var city_id_selector = $('select[name="city_id"]');
+
                     // Set the district and street name based on the ZIP code data
                     $('input[name="district"]').val(data.district);
                     $('input[name="street_name"]').val(data.street_name);
+
                     // Set the value of the state selector and trigger its change event
                     state_id_selector.val(data.state_id).change();
+
                     // Wait for the city selector to be populated, then set its value and trigger its change event
                     setTimeout(() => {
                         city_id_selector.val(data.city_id).change();
                     }, 1000);
+
                     $('a:contains("Next")').css("display", "block");
                 }
             });
